a2148a30e666022eed848c65a4c089da52d87a8a
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-19 16:18:00 -0700

    Fix stb URL to not point to specific SHA instead of master

--- ports/stb/portfile.cmake
+++ ports/stb/portfile.cmake
@@ -1,15 +1,15 @@
 include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
-    URL "https://github.com/nothings/stb/archive/master.zip"
-    FILENAME "stb.zip"
-    MD5 23bbf81dcfa7871b785e1c45d2ad24f5
+    URL "https://github.com/nothings/stb/archive/e713a69f1ea6ee1e0d55725ed0731520045a5993.zip"
+    FILENAME "stb-e713a69f1ea6ee1e0d55725ed0731520045a5993.zip"
+    MD5 5d81d3036610045d5a8076728c4e2f7e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 # Put the licence file where vcpkg expects it
-file(RENAME ${CURRENT_BUILDTREES_DIR}/src/stb-master ${CURRENT_BUILDTREES_DIR}/src/stb)
+file(RENAME ${CURRENT_BUILDTREES_DIR}/src/stb-e713a69f1ea6ee1e0d55725ed0731520045a5993 ${CURRENT_BUILDTREES_DIR}/src/stb)
 file(COPY ${CURRENT_BUILDTREES_DIR}/src/stb/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/stb/README.md)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/stb/README.md ${CURRENT_PACKAGES_DIR}/share/stb/copyright.)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/stb/README.md ${CURRENT_PACKAGES_DIR}/share/stb/copyright)
 
 # Copy the stb header files
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/src/stb/ DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN "*.h")
