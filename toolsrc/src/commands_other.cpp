4f80532991f5b9d4d1eae455a91f289e9bea081a
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-22 00:06:37 -0700

    Place owns_command() in a separate cpp file

--- toolsrc/src/commands_other.cpp
+++ toolsrc/src/commands_other.cpp
@@ -73,22 +73,6 @@ namespace vcpkg
         exit(EXIT_SUCCESS);
     }
 
-    void owns_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths)
-    {
-        args.check_max_args(1);
-        if (args.command_arguments.size() == 0)
-        {
-            System::println(System::color::error, "Error: owns requires a pattern to search for as the first argument.");
-            std::cout <<
-                "example:\n"
-                "    vcpkg owns .dll\n";
-            exit(EXIT_FAILURE);
-        }
-        StatusParagraphs status_db = database_load_check(paths);
-        search_file(paths, args.command_arguments[0], status_db);
-        exit(EXIT_SUCCESS);
-    }
-
     void internal_test_command(const vcpkg_cmd_arguments& /*args*/, const vcpkg_paths& /*paths*/)
     {
         //        auto data = FormatEventData("test");
