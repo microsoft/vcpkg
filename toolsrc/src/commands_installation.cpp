03b43aad42c8eca03937148cda7afffceeeeca1c
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-21 23:49:20 -0700

    Place command_cache in a separate cpp file

--- toolsrc/src/commands_installation.cpp
+++ toolsrc/src/commands_installation.cpp
@@ -109,38 +109,6 @@ namespace vcpkg
         exit(EXIT_SUCCESS);
     }
 
-    void cache_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths)
-    {
-        args.check_max_args(0);
-
-        auto begin_it = fs::directory_iterator(paths.packages);
-        auto end_it = fs::directory_iterator();
-
-        if (begin_it == end_it)
-        {
-            System::println("No packages are cached.");
-            exit(EXIT_SUCCESS);
-        }
-
-        for (; begin_it != end_it; ++begin_it)
-        {
-            const auto& path = begin_it->path();
-
-            auto file_contents = Files::get_contents(path / "CONTROL");
-            if (auto text = file_contents.get())
-            {
-                auto pghs = parse_paragraphs(*text);
-                if (pghs.size() != 1)
-                    continue;
-
-                auto src = BinaryParagraph(pghs[0]);
-                System::println(src.displayname().c_str());
-            }
-        }
-
-        exit(EXIT_SUCCESS);
-    }
-
     void build_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths, const triplet& default_target_triplet)
     {
         std::vector<package_spec> specs = args.parse_all_arguments_as_package_specs(default_target_triplet);
