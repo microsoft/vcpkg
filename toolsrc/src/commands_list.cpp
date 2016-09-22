97b79e1f0c2c7e0e9ef2346c5b32b98e42a00155
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-21 23:54:23 -0700

    Place command_list() in a separate cpp file

---
+++ toolsrc/src/commands_list.cpp
@@ -0,0 +1,32 @@
+#include "vcpkg_Commands.h"
+#include "vcpkg.h"
+#include "vcpkg_System.h"
+
+namespace vcpkg
+{
+    void list_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths)
+    {
+        args.check_max_args(0);
+
+        std::vector<std::string> packages_output;
+        for (auto&& pgh : database_load_check(paths))
+        {
+            if (pgh->state == install_state_t::not_installed && pgh->want == want_t::purge)
+                continue;
+            packages_output.push_back(Strings::format("%-27s %-16s %s",
+                                                      pgh->package.displayname(),
+                                                      pgh->package.version,
+                                                      shorten_description(pgh->package.description)));
+        }
+        std::sort(packages_output.begin(), packages_output.end());
+        for (auto&& package : packages_output)
+        {
+            System::println(package.c_str());
+        }
+        if (packages_output.empty())
+        {
+            System::println("No packages are installed. Did you mean `search`?");
+        }
+        exit(EXIT_SUCCESS);
+    }
+}
--- toolsrc/src/commands_other.cpp
+++ toolsrc/src/commands_other.cpp
@@ -95,32 +95,6 @@ namespace vcpkg
         exit(System::cmd_execute(cmdline));
     }
 
-    void list_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths)
-    {
-        args.check_max_args(0);
-
-        std::vector<std::string> packages_output;
-        for (auto&& pgh : database_load_check(paths))
-        {
-            if (pgh->state == install_state_t::not_installed && pgh->want == want_t::purge)
-                continue;
-            packages_output.push_back(Strings::format("%-27s %-16s %s",
-                                                      pgh->package.displayname(),
-                                                      pgh->package.version,
-                                                      shorten_description(pgh->package.description)));
-        }
-        std::sort(packages_output.begin(), packages_output.end());
-        for (auto&& package : packages_output)
-        {
-            System::println(package.c_str());
-        }
-        if (packages_output.empty())
-        {
-            System::println("No packages are installed. Did you mean `search`?");
-        }
-        exit(EXIT_SUCCESS);
-    }
-
     void import_command(const vcpkg_cmd_arguments& args, const vcpkg_paths& paths)
     {
         if (args.command_arguments.size() != 3)
