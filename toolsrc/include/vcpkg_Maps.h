addecbfdefe9f00214db9af438b0fe255e92a16a
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-22 23:28:14 -0700

    Introduce vcpkg_Maps utility namespace

---
+++ toolsrc/include/vcpkg_Maps.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace vcpkg { namespace Maps
+{
+    template <typename K, typename V>
+    std::unordered_set<K> extract_key_set(const std::unordered_map<K, V>& input_map)
+    {
+        std::unordered_set<K> key_set;
+        for (auto const& element : input_map)
+        {
+            key_set.insert(element.first);
+        }
+        return key_set;
+    }
+}}
