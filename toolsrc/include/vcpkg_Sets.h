c699d5715b2e768ef2f7742a1894742df982038b
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-22 23:27:46 -0700

    Introduce vcpkg::Sets utility namespace

---
+++ toolsrc/include/vcpkg_Sets.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vcpkg_Checks.h"
+#include <unordered_set>
+
+namespace vcpkg { namespace Sets
+{
+    template <typename T, typename Container>
+    void remove_all(std::unordered_set<T>* input_set, Container remove_these)
+    {
+        Checks::check_throw(input_set != nullptr, "Input set cannot be null");
+        for (const T& r : remove_these)
+        {
+            input_set->erase(r);
+        }
+    }
+}}
