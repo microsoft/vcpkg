8b7c0594ab982f1e1abb18832588fe9e7f19dce9
Alexander Karatarakis <alkarata@microsoft.com>
2016-09-26 16:46:31 -0700

    Fix URL to URLS in portfile
    URLS is the new name. It was changed in another pull request

--- ports/constexpr/portfile.cmake
+++ ports/constexpr/portfile.cmake
@@ -1,6 +1,6 @@
 include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
-    URL "https://github.com/elbeno/constexpr/archive/a98b1db39c909e0130d21d3910d4faf97035a625.zip"
+    URLS "https://github.com/elbeno/constexpr/archive/a98b1db39c909e0130d21d3910d4faf97035a625.zip"
     FILENAME "constexpr-a98b1db39c909e0130d21d3910d4faf97035a625.zip"
     SHA512 847e09f9df30cb5fbd8aa280679ff359d73c9e9454ffe3090f66975a15665080629e9a664d057f039b17430d42b5e5f5f3f92831e73c15024060991090209c2e
 )
