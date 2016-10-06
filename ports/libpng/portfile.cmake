b54881eacc300cdde7c9c9ad474f86a74ed684f7
Alexander Karatarakis <alkarata@microsoft.com>
2016-10-06 13:15:43 -0700

    [libpng] Add static library building

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -1,3 +1,4 @@
+include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libpng-1.6.24)
 
@@ -12,10 +13,19 @@ vcpkg_apply_patches(
     PATCHES "${CMAKE_CURRENT_LIST_DIR}/use-abort-on-all-platforms.patch"
 )
 
+if(VCPKG_BUILD_SHARED_LIBS)
+    set(PNG_STATIC_LIBS OFF)
+    set(PNG_SHARED_LIBS ON)
+else()
+    set(PNG_STATIC_LIBS ON)
+    set(PNG_SHARED_LIBS OFF)
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DPNG_STATIC=OFF
+        -DPNG_STATIC=${PNG_STATIC_LIBS}
+        -DPNG_SHARED=${PNG_SHARED_LIBS}
         -DPNG_TESTS=OFF
         -DSKIP_INSTALL_PROGRAMS=ON
         -DSKIP_INSTALL_EXECUTABLES=ON
@@ -27,16 +37,18 @@ vcpkg_configure_cmake(
 vcpkg_build_cmake()
 vcpkg_install_cmake()
 
-file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/share
-)
+if(VCPKG_BUILD_SHARED_LIBS)
+    vcpkg_copy_pdbs()
+else()
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libpng16_static.lib ${CURRENT_PACKAGES_DIR}/lib/libpng16.lib)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16_staticd.lib ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16d.lib)
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
 file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libpng ${CURRENT_PACKAGES_DIR}/share/libpng)
 file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libpng/libpng16-debug.cmake ${CURRENT_PACKAGES_DIR}/share/libpng/libpng16-debug.cmake)
-file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/lib/libpng
-)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/libpng)
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpng)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/libpng/LICENSE ${CURRENT_PACKAGES_DIR}/share/libpng/copyright)
-vcpkg_copy_pdbs()
 
