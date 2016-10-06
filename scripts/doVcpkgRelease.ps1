57b638fea9375793cdc155c18d114759d7f9ba0e
Alexander Karatarakis <alkarata@microsoft.com>
2016-10-05 17:31:27 -0700

    Fix release script to properly checkout .vcpkg-root in the temp dirs

--- scripts/doVcpkgRelease.ps1
+++ scripts/doVcpkgRelease.ps1
@@ -51,11 +51,9 @@ for ($disableMetrics = 0; $disableMetrics -le 1; $disableMetrics++)
     # Partial checkout for building vcpkg
     $dotGitDir = "$vcpkgRootDir\.git"
     $workTreeForBuildOnly = "$buildPath"
-    $checkoutThisDirForBuildOnly1 = ".\scripts" # Must be relative to the root of the repository
-    $checkoutThisDirForBuildOnly2 = ".\toolsrc" # Must be relative to the root of the repository
+    $checkoutForBuildOnly = ".\scripts",".\toolsrc",".vcpkg-root" # Must be relative to the root of the repository
     Write-Verbose("Creating partial temporary checkout: $buildPath")
-    git --git-dir="$dotGitDir" --work-tree="$workTreeForBuildOnly" checkout $gitHash -f -q -- $checkoutThisDirForBuildOnly1
-    git --git-dir="$dotGitDir" --work-tree="$workTreeForBuildOnly" checkout $gitHash -f -q -- $checkoutThisDirForBuildOnly2
+    git --git-dir="$dotGitDir" --work-tree="$workTreeForBuildOnly" checkout $gitHash -f -q -- $checkoutForBuildOnly
 
     & "$buildPath\scripts\bootstrap.ps1" -disableMetrics $disableMetrics
 
