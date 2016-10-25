516a1680aa48fc36ebea7a8ca1b082d65b4edc5f
Alexander Karatarakis <alkarata@microsoft.com>
2016-10-25 14:02:04 -0700

    [wt] Rename downloaded file to include wt in the name

--- ports/wt/portfile.cmake
+++ ports/wt/portfile.cmake
@@ -4,7 +4,7 @@ set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/wt-3.3.6)
 
 vcpkg_download_distfile(ARCHIVE_FILE
     URLS "https://github.com/kdeforche/wt/archive/3.3.6.tar.gz"
-    FILENAME "3.3.6.tar.gz"
+    FILENAME "wt-3.3.6.tar.gz"
     SHA512 d6135713d300bcd3956a4fd673af73be0561eb5d6aa91c47255b966950ddb411217e6a826c1d11c0d941dbb6d006c9926231d7334b21de6256cca331066e18a6
 )
 vcpkg_extract_source_archive(${ARCHIVE_FILE})
