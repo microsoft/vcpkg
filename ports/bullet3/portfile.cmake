c716b6bc60871f3c1adf5b1f2710646e6fdf3f8d
Alexander Karatarakis <alkarata@microsoft.com>
2016-10-26 12:09:34 -0700

    [bullet3] Only call vcpkg_copy_pdbs() on dynamic builds

--- ports/bullet3/portfile.cmake
+++ ports/bullet3/portfile.cmake
@@ -33,4 +33,6 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/bullet3)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/bullet3/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/bullet3/copyright)
 
-vcpkg_copy_pdbs()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    vcpkg_copy_pdbs()
+endif()
