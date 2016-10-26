60cb31b054ded6f9e0d6605a3de16e0cec55b19e
Alexander Karatarakis <alkarata@microsoft.com>
2016-10-26 12:11:36 -0700

    [catch] Download from specific sha, not master branch

--- ports/catch/portfile.cmake
+++ ports/catch/portfile.cmake
@@ -10,14 +10,13 @@ include(${CMAKE_TRIPLET_FILE})
 include(vcpkg_common_functions)
 
 vcpkg_download_distfile(HEADER
-    URLS "https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp"
-    #https://raw.githubusercontent.com/philsquared/Catch/master/LICENSE_1_0.txt
+    URLS "https://raw.githubusercontent.com/philsquared/Catch/e27c4ee04282f60aefcc9b1062a74f92cf6c1a2b/single_include/catch.hpp"
     FILENAME "catch.hpp"
-    SHA512 64eec1c291826ef235433729dc18f150e47d2a761f80ec47264be08b3f756b310c6e3aa2c6c3c7b5e2f345f86489d561d9a694506654823077d0f6c7c22df7cd
+    SHA512 c2fec38227bb1725c30f955583dbd012f86eef83512a0c154e91b77249df372db067710ae110463eb07adec722d214114fd6a2cebff7ee43c5fd567a6a1ba221
 )
 
 vcpkg_download_distfile(LICENSE
-    URLS "https://raw.githubusercontent.com/philsquared/Catch/master/LICENSE_1_0.txt"
+    URLS "https://raw.githubusercontent.com/philsquared/Catch/e27c4ee04282f60aefcc9b1062a74f92cf6c1a2b/LICENSE_1_0.txt"
     FILENAME "License_1_0.txt"
     SHA512 d6078467835dba8932314c1c1e945569a64b065474d7aced27c9a7acc391d52e9f234138ed9f1aa9cd576f25f12f557e0b733c14891d42c16ecdc4a7bd4d60b8
 )
