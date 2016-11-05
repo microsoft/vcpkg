ef4be3ab1d5619e13a0fe975772a4be200f47a6f
Robert Schumacher <roschuma@microsoft.com>
2016-11-05 03:17:06 -0700

    [curl] Update to 7.51.0

--- ports/curl/portfile.cmake
+++ ports/curl/portfile.cmake
@@ -3,11 +3,11 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
     message(FATAL_ERROR "Static building not supported yet")
 endif()
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/curl-7.48.0)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/curl-curl-7_51_0)
 vcpkg_download_distfile(ARCHIVE_FILE
-    URLS "https://curl.haxx.se/download/curl-7.48.0.tar.bz2"
-    FILENAME "curl-7.48.0.tar.bz2"
-    SHA512 9bb554eaf4ccaced0fa9b38de4f381eab84b96c1aa07a45d83ddfd38a925044d0fe9fac517263f67f009d2294a31c33dedb2267defbab0cb14f96091bbed5f92
+    URLS "https://github.com/curl/curl/archive/curl-7_51_0.tar.gz"
+    FILENAME "curl-7.51.0.tar.gz"
+    SHA512 88ec572efb1b2fb793dc26b627e54863718e774343283f0eb92022ce252f7798332d9d3f20f63e45c38576614a000abbf12570e91e14a118f150e0378f1a27e5
 )
 vcpkg_extract_source_archive(${ARCHIVE_FILE})
 
