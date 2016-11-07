355d79ea4098834634606f3eca65d1261474f0d9
Barath Kannan <barathsotd@gmail.com>
2016-11-07 23:25:20 +1100

    remove tests from base repository

--- tests/qt5/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <qsysinfo.h>
-#include <qstring.h>
-#include <cstdio>
-int main(int argc, char** argv) {
-	auto buildABI = QSysInfo::buildAbi().toStdString();
-	fprintf(stdout, "%s\n", buildABI.c_str());
-	printf("%d\n", QSysInfo::windowsVersion());
-	return 0;
-}
