34c585901ed8c32d8c7b1243225c1eee97ab3026
Mårten Nordheim <Morten242@gmail.com>
2016-11-11 01:14:01 +0100

    Add /m switch to vcpkg_install_cmake
    (https://github.com/Microsoft/vcpkg/issues/258#issuecomment-259839046)

--- scripts/cmake/vcpkg_install_cmake.cmake
+++ scripts/cmake/vcpkg_install_cmake.cmake
@@ -1,7 +1,7 @@
 function(vcpkg_install_cmake)
     message(STATUS "Package ${TARGET_TRIPLET}-rel")
     vcpkg_execute_required_process(
-        COMMAND ${CMAKE_COMMAND} --build . --config Release --target install
+        COMMAND ${CMAKE_COMMAND} --build . --config Release --target install -- /m
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
         LOGNAME package-${TARGET_TRIPLET}-rel
     )
@@ -9,7 +9,7 @@ function(vcpkg_install_cmake)
 
     message(STATUS "Package ${TARGET_TRIPLET}-dbg")
     vcpkg_execute_required_process(
-        COMMAND ${CMAKE_COMMAND} --build . --config Debug --target install
+        COMMAND ${CMAKE_COMMAND} --build . --config Debug --target install -- /m
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
         LOGNAME package-${TARGET_TRIPLET}-dbg
     )
