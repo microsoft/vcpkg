692d95c865f271d0805a60fff63fb319c1985ad3
Robert Schumacher <roschuma@microsoft.com>
2016-11-26 02:51:12 -0800

    [vcpkg] Do not return references to locals

--- toolsrc/src/BuildInfo.cpp
+++ toolsrc/src/BuildInfo.cpp
@@ -14,13 +14,13 @@ namespace vcpkg
         return this->m_linkage;
     }
 
-    const std::regex& BuildType::crt_regex() const
+    const std::regex BuildType::crt_regex() const
     {
         const std::regex r(this->m_crt_regex_as_string, std::regex_constants::icase);
         return r;
     }
 
-    const std::string& BuildType::toString() const
+    const std::string BuildType::toString() const
     {
         const std::string s = Strings::format("[%s,%s]", to_string(this->m_config), to_string(this->m_linkage));
         return s;
@@ -151,7 +151,7 @@ namespace vcpkg
     const OutdatedDynamicCrt OutdatedDynamicCrt::MSVCRT20_DLL = OutdatedDynamicCrt("msvcrt20.dll", R"(msvcrt20\.dll)");;
     const OutdatedDynamicCrt OutdatedDynamicCrt::MSVCRT40_DLL = OutdatedDynamicCrt("msvcrt40.dll", R"(msvcrt40\.dll)");;
 
-    const std::regex& OutdatedDynamicCrt::crt_regex() const
+    const std::regex OutdatedDynamicCrt::crt_regex() const
     {
         const std::regex r(this->m_crt_regex_as_string, std::regex_constants::icase);
         return r;
