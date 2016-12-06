a05a033d744ad28b1eb8c8faa8ef8c21854c05f7
Robert Schumacher <roschuma@microsoft.com>
2016-12-06 13:58:04 -0800

    [aws-sdk-cpp] Check VCPKG_CRT_LINKAGE instead of VCPKG_LIBRARY_LINKAGE. Rename archive to avoid collisions with other libraries.

--- ports/aws-sdk-cpp/portfile.cmake
+++ ports/aws-sdk-cpp/portfile.cmake
@@ -2,7 +2,7 @@ include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/aws-sdk-cpp-1.0.34)
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/aws/aws-sdk-cpp/archive/1.0.34.tar.gz"
-    FILENAME "1.0.34.tar.gz"
+    FILENAME "aws-sdk-cpp-1.0.34.tar.gz"
     SHA512 21ca03eb323eecb55c29866b73c07956a36aad7c9c051eb7ca201cfd356c3f9732c89898cf0c89660d6c1279dc52438bb389b37d613bf741bae81bb3e773a3c5
 )
 vcpkg_extract_source_archive(${ARCHIVE})
@@ -13,7 +13,7 @@ vcpkg_apply_patches(
 		${CMAKE_CURRENT_LIST_DIR}/drop_git.patch
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+if(VCPKG_CRT_LINKAGE STREQUAL static)
 	set(FORCE_SHARED_CRT OFF)
 else()
 	set(FORCE_SHARED_CRT ON)
@@ -37,7 +37,7 @@ file(REMOVE_RECURSE
 	${CURRENT_PACKAGES_DIR}/nuget
 	${CURRENT_PACKAGES_DIR}/debug/nuget)
 
-if(${VCPKG_LIBRARY_LINKAGE} STREQUAL dynamic)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
 	file(GLOB LIB_FILES          ${CURRENT_PACKAGES_DIR}/bin/*.lib)
 	file(GLOB DEBUG_LIB_FILES    ${CURRENT_PACKAGES_DIR}/debug/bin/*.lib)
 	file(COPY ${LIB_FILES}       DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
