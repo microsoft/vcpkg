e8b46b89d09ce9c5c0775ae29be9925cce8f16a1
Alexander Karatarakis <alkarata@microsoft.com>
2016-12-07 15:19:16 -0800

    [assimp] Extract variable SOURCE_PATH

--- ports/assimp/portfile.cmake
+++ ports/assimp/portfile.cmake
@@ -7,6 +7,7 @@
 #
 
 include(vcpkg_common_functions)
+SET(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/assimp-3.3.1)
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/assimp/assimp/archive/v3.3.1.zip"
     FILENAME "assimp-3.3.1.zip"
@@ -15,7 +16,7 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/assimp-3.3.1
+    SOURCE_PATH ${SOURCE_PATH}
     OPTIONS -DASSIMP_BUILD_TESTS=False
             -DASSIMP_BUILD_ASSIMP_VIEW=False
             -DASSIMP_BUILD_ZLIB=False
@@ -33,7 +34,7 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(COPY ${CURRENT_BUILDTREES_DIR}/src/assimp-3.3.1/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/assimp)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/assimp)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/assimp/LICENSE ${CURRENT_PACKAGES_DIR}/share/assimp/copyright)
 
 vcpkg_copy_pdbs()
