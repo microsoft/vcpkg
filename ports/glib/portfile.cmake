ccd3e399f023129a39bb9a402324676a9391a970
codicodi <rob.ceglinski@gmail.com>
2017-01-14 02:15:34 +0100

    [glib] use win_iconv and add subdir for tools

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -17,15 +17,10 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 63d7b5ece8d7d15b10e06ef10a8e0a4446f30a8d9c97daa3352ec00dfef622728bd42ed24cbf67e553166a0f9e29a3e3d024d1d085b9804fd67608db438b6771)
 
 vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/use-libiconv-on-windows.patch)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
 file(REMOVE_RECURSE ${SOURCE_PATH}/glib/pcre)
-file(REMOVE ${SOURCE_PATH}/glib/win_iconv.c)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -35,7 +30,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
-vcpkg_copy_tool_dependencies()
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/glib)
 
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/glib)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/glib/COPYING ${CURRENT_PACKAGES_DIR}/share/glib/copyright)
