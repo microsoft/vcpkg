2bc4064b1e99b31bd550982011e251f97d5a0aef
Alexander Karatarakis <alkarata@microsoft.com>
2017-01-13 17:30:48 -0800

    [vcpkg_build_cmake] Add option to specify the 64-bit toolset

--- scripts/cmake/vcpkg_build_cmake.cmake
+++ scripts/cmake/vcpkg_build_cmake.cmake
@@ -1,7 +1,16 @@
 function(vcpkg_build_cmake)
+    cmake_parse_arguments(_bc "MSVC_64_TOOLSET" "" "" ${ARGN})
+
+    set(MSVC_EXTRA_ARGS)
+
+    # Specifies the architecture of the toolset, NOT the architecture of the produced binary
+    if (_bc_MSVC_64_TOOLSET)
+        list(APPEND MSVC_EXTRA_ARGS "/p:PreferredToolArchitecture=x64")
+    endif()
+
     message(STATUS "Build ${TARGET_TRIPLET}-rel")
     vcpkg_execute_required_process(
-        COMMAND ${CMAKE_COMMAND} --build . --config Release -- /p:VCPkgLocalAppDataDisabled=true /m
+        COMMAND ${CMAKE_COMMAND} --build . --config Release -- /p:VCPkgLocalAppDataDisabled=true /m ${MSVC_EXTRA_ARGS}
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
         LOGNAME build-${TARGET_TRIPLET}-rel
     )
@@ -9,7 +18,7 @@ function(vcpkg_build_cmake)
 
     message(STATUS "Build ${TARGET_TRIPLET}-dbg")
     vcpkg_execute_required_process(
-        COMMAND ${CMAKE_COMMAND} --build . --config Debug -- /p:VCPkgLocalAppDataDisabled=true /m
+        COMMAND ${CMAKE_COMMAND} --build . --config Debug -- /p:VCPkgLocalAppDataDisabled=true /m ${MSVC_EXTRA_ARGS}
         WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
         LOGNAME build-${TARGET_TRIPLET}-dbg
     )
