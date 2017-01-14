eb7ccc1223f568d6d2df21b27eb890be93bc8186
codicodi <rob.ceglinski@gmail.com>
2017-01-14 02:55:41 +0100

    Revert "[glib] use win_iconv and add subdir for tools"
    This reverts commit ccd3e399f023129a39bb9a402324676a9391a970.

--- scripts/cmake/vcpkg_copy_tool_dependencies.cmake
+++ scripts/cmake/vcpkg_copy_tool_dependencies.cmake
@@ -1,8 +1,8 @@
-# Copy dlls for all tools in TOOL_DIR
+# Copy dlls for all tools in ${CURRENT_PACKAGES_DIR}/tools
 
-function(vcpkg_copy_tool_dependencies TOOL_DIR)
+function(vcpkg_copy_tool_dependencies)
     macro(search_for_dependencies PATH_TO_SEARCH)
-        file(GLOB TOOLS ${TOOL_DIR}/*.exe  ${TOOL_DIR}/*.dll)
+        file(GLOB TOOLS ${CURRENT_PACKAGES_DIR}/tools/*.exe ${CURRENT_PACKAGES_DIR}/tools/*.dll)
         foreach(TOOL ${TOOLS})
             execute_process(COMMAND powershell -noprofile -executionpolicy UnRestricted -nologo
                 -file ${VCPKG_ROOT_DIR}/scripts/buildsystems/msbuild/applocal.ps1
