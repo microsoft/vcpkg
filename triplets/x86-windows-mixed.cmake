357d79ae16f9417311743b71da043242264f071f
Doug Crawford <doug.crawford@intelight-its.com>
2017-01-14 20:53:26 -0700

    custom x86 mixed triplet
    Builds luajit and lua modules with dyanmic linkage and everything
    else with static linkage

---
+++ triplets/x86-windows-mixed.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x86)
+
+if(${PORT} MATCHES "luajit|lua-lsqlite3|lua-cjson")
+	set(VCPKG_CRT_LINKAGE dynamic)
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
+else()
+	set(VCPKG_CRT_LINKAGE static)
+	set(VCPKG_LIBRARY_LINKAGE static)
+endif()
