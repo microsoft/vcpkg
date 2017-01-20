1380525e5f92fb0ff576a98343238389dfdd7fad
codicodi <rob.ceglinski@gmail.com>
2017-01-20 13:38:56 +0100

    [fontconfig] install additional license

--- ports/fontconfig/portfile.cmake
+++ ports/fontconfig/portfile.cmake
@@ -6,12 +6,15 @@ vcpkg_download_distfile(ARCHIVE
     FILENAME "fontconfig-2.12.1.tar.gz"
     SHA512 0959a80522e09551e49ec7b2383b7dfb319d4e1c058ad0b55bb35d3f675acbb7ff08c9c30a8798b731070687f84dd3d2ff7e28aafac6ecfa9d3f85c5847c0955)
 
-# Download single-header implementation of dirent API for Windows.
+# Download single-header implementation of dirent API for Windows and it's license
 vcpkg_download_distfile(DIRENT_H
     URLS "https://raw.githubusercontent.com/tronkko/dirent/8b1db5092479a73d47eafd3de739b27e876e6bf3/include/dirent.h"
     FILENAME "fontconfig-dirent.h"
     SHA512 dc9e63fd9cf4ccffdc052f92933633ef9d09dfcfe3b1d15f1f32c99349babd36a62d02283e3d8ba7766d92817be015eb211f11efc4fa52cc90d532a34d1ae785)
-
+vcpkg_download_distfile(DIRENT_LICENSE
+    URLS "https://raw.githubusercontent.com/tronkko/dirent/8b1db5092479a73d47eafd3de739b27e876e6bf3/LICENSE"
+    FILENAME "fontconfig-dirent-license"
+    SHA512 58c294f80b679252dbee9687ff6bda660fe1ed6f94506e1b9edc19358de98b274b25b3697bdcd34becb28a4f186c6d321a16ab616164e2fb378b37357fc71e4f)
 
 vcpkg_extract_source_archive(${ARCHIVE})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
@@ -32,3 +35,5 @@ vcpkg_copy_pdbs()
 
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/fontconfig)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/fontconfig/COPYING ${CURRENT_PACKAGES_DIR}/share/fontconfig/copyright)
+file(COPY ${DIRENT_LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/fontconfig)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/fontconfig/fontconfig-dirent-license ${CURRENT_PACKAGES_DIR}/share/fontconfig/dirent-for-vs-copyright)
