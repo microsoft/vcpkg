95b67ab8e97135538034d220b9658ad9286429c6
codicodi <rob.ceglinski@gmail.com>
2017-01-26 11:12:55 +0100

    [cairo] update port

--- ports/cairo/portfile.cmake
+++ ports/cairo/portfile.cmake
@@ -7,11 +7,11 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cairo-1.14.6)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cairo-1.15.4)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.cairographics.org/releases/cairo-1.14.6.tar.xz"
-    FILENAME "cairo-1.14.6.tar.xz"
-    SHA512 e2aa17a33b95b68d407b53ac321cca15b0c148eb49b8639c75b2af1e75e7b417a2168ea978dabb8581b341f0f45dc042d3b1a56b01ab525b1984015f0865316b
+    URLS "http://cairographics.org/snapshots/cairo-1.15.4.tar.xz"
+    FILENAME "cairo-1.15.4.tar.xz"
+    SHA512 ac3e6879fcf0876bca9f801cdf9e970ef1822644228cdd21962d0bf5db5fc074973f4ae651eb9c76b44fffd405cf0a0c7cbb228dba96b835ea137a2740277ee9
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
@@ -43,6 +43,8 @@ file(COPY
 "${SOURCE_PATH}/src/cairo-svg.h"
 "${SOURCE_PATH}/cairo-version.h"
 "${SOURCE_PATH}/src/cairo-win32.h"
+"${SOURCE_PATH}/util/cairo-gobject/cairo-gobject.h"
+"${SOURCE_PATH}/src/cairo-ft.h"
 DESTINATION
 ${CURRENT_PACKAGES_DIR}/include
 )
