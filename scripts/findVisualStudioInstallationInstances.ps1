7207316ed405c334be1ac507d76dd45a5979903d
Alexander Karatarakis <alkarata@microsoft.com>
2017-02-06 15:26:35 -0800

    Don't download nupkg. nuget.exe auto-downloads it

--- scripts/findVisualStudioInstallationInstances.ps1
+++ scripts/findVisualStudioInstallationInstances.ps1
@@ -14,29 +14,6 @@ $nugetexe = & $scriptsDir\fetchDependency.ps1 "nuget"
 $nugetPackageDir = "$downloadsDir\nuget-packages"
 
 $SetupAPIVersion = "1.3.269-rc"
-$url = "https://api.nuget.org/packages/microsoft.visualstudio.setup.configuration.native.$SetupAPIVersion.nupkg"
-$downloadName = "microsoft.visualstudio.setup.configuration.native.$SetupAPIVersion.nupkg"
-$downloadPath = "$downloadsDir\$downloadName"
-
-if (!(Test-Path $downloadPath))
-{
-    try {
-        Start-BitsTransfer -Source $url -Destination $downloadPath -ErrorAction Stop
-    }
-    catch [System.Exception] {
-        # If BITS fails for any reason, delete any potentially partially downloaded files and continue
-        if (Test-Path $downloadPath)
-        {
-            Remove-Item $downloadPath
-        }
-    }
-}
-if (!(Test-Path $downloadPath))
-{
-    Write-Host("Downloading $downloadName...")
-    (New-Object System.Net.WebClient).DownloadFile($url, $downloadPath)
-}
-
 $nugetOutput = & $nugetexe install Microsoft.VisualStudio.Setup.Configuration.Native -Pre -Source $downloadsDir -OutputDirectory $nugetPackageDir 2>&1
 
 $SetupConsoleExe = "$nugetPackageDir\Microsoft.VisualStudio.Setup.Configuration.Native.$SetupAPIVersion\tools\x86\Microsoft.VisualStudio.Setup.Configuration.Console.exe"
