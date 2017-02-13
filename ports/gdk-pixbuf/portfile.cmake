18abe2944da14e741ece014205510643eeb6577d
codicodi <rob.ceglinski@gmail.com>
2017-02-14 00:27:59 +0100

    [gdk-pixbuf] disable static builds

--- ports/gdk-pixbuf/portfile.cmake
+++ ports/gdk-pixbuf/portfile.cmake
@@ -1,3 +1,7 @@
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    message(STATUS "Warning: Static building not supported. Building dynamic.")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
 
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/gdk-pixbuf-2.36.5)
