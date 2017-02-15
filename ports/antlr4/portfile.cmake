9dac4b073ae58e3af0a22f53275f6f36f66f2865
Alexander Karatarakis <alkarata@microsoft.com>
2017-02-14 18:35:40 -0800

    [antlr4] Make identation consistent

--- ports/antlr4/portfile.cmake
+++ ports/antlr4/portfile.cmake
@@ -9,9 +9,9 @@ vcpkg_download_distfile(ARCHIVE
 
 # license not exist in antlr folder.
 vcpkg_download_distfile(LICENSE
-	URLS https://raw.githubusercontent.com/antlr/antlr4/4.6/LICENSE.txt
-	FILENAME "antlr4-copyright"
-	SHA512 c72ae3d5c9f3f07160405b5ca44f01116a9602d82291d6cd218fcc5ec6e8baf985e4baa2acf3d621079585385708bd171c96ef44dd808e60c40a48bc1f56c9ae
+    URLS https://raw.githubusercontent.com/antlr/antlr4/4.6/LICENSE.txt
+    FILENAME "antlr4-copyright"
+    SHA512 c72ae3d5c9f3f07160405b5ca44f01116a9602d82291d6cd218fcc5ec6e8baf985e4baa2acf3d621079585385708bd171c96ef44dd808e60c40a48bc1f56c9ae
 )
 
 vcpkg_extract_source_archive(${ARCHIVE})
@@ -22,52 +22,52 @@ vcpkg_apply_patches(
 )
 
 if (${VCPKG_LIBRARY_LINKAGE} STREQUAL static)
-	set(DEBUG_CONFIG   "Debug Static")
-	set(RELEASE_CONFIG "Release Static")
+    set(DEBUG_CONFIG   "Debug Static")
+    set(RELEASE_CONFIG "Release Static")
 else()
-	set(DEBUG_CONFIG   "Debug DLL")
-	set(RELEASE_CONFIG "Release DLL")
+    set(DEBUG_CONFIG   "Debug DLL")
+    set(RELEASE_CONFIG "Release DLL")
 endif()
 
 vcpkg_build_msbuild(
-	PROJECT_PATH ${SOURCE_PATH}/antlr4cpp-vs2015.vcxproj
-	DEBUG_CONFIGURATION ${DEBUG_CONFIG}
-	RELEASE_CONFIGURATION ${RELEASE_CONFIG}
+    PROJECT_PATH ${SOURCE_PATH}/antlr4cpp-vs2015.vcxproj
+    DEBUG_CONFIGURATION ${DEBUG_CONFIG}
+    RELEASE_CONFIGURATION ${RELEASE_CONFIG}
 )
 
-file (MAKE_DIRECTORY 
-	${CURRENT_PACKAGES_DIR}/include)
+file (MAKE_DIRECTORY
+    ${CURRENT_PACKAGES_DIR}/include)
 FILE(COPY            ${SOURCE_PATH}/src/
      DESTINATION     ${CURRENT_PACKAGES_DIR}/include
      FILES_MATCHING PATTERN "*.h")
 
-file (MAKE_DIRECTORY 
-	${CURRENT_PACKAGES_DIR}/lib
-	${CURRENT_PACKAGES_DIR}/debug/lib)
+file (MAKE_DIRECTORY
+    ${CURRENT_PACKAGES_DIR}/lib
+    ${CURRENT_PACKAGES_DIR}/debug/lib)
 
 file(COPY       ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.lib
-	DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 file(COPY       ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.lib
-	DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
 
 if (${VCPKG_LIBRARY_LINKAGE} STREQUAL static)
-	vcpkg_apply_patches(
-		SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-		PATCHES     ${CMAKE_CURRENT_LIST_DIR}/static.patch
-	)
+    vcpkg_apply_patches(
+        SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
+        PATCHES     ${CMAKE_CURRENT_LIST_DIR}/static.patch
+    )
 else()
-	file (MAKE_DIRECTORY 
-		${CURRENT_PACKAGES_DIR}/bin
-		${CURRENT_PACKAGES_DIR}/debug/bin)
-	
-	file(COPY
-		${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.dll
-		${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.pdb
-		DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-	file(COPY
-		${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.dll
-		${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.pdb
-		DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file (MAKE_DIRECTORY
+        ${CURRENT_PACKAGES_DIR}/bin
+        ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+    file(COPY
+        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.dll
+        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.pdb
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(COPY
+        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.dll
+        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.pdb
+        DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
 endif()
 
 file(INSTALL ${LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/antlr4 RENAME copyright)
