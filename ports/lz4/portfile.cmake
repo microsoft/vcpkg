058c8a825e04f8ce90ee2865cf11813f6ad2a37e
codicodi <rob.ceglinski@gmail.com>
2017-02-25 15:46:19 +0100

    [lz4] update to 1.7.5

--- ports/lz4/portfile.cmake
+++ ports/lz4/portfile.cmake
@@ -1,10 +1,10 @@
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/lz4-1.7.4.2)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/lz4-1.7.5)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/lz4/lz4/archive/v1.7.4.2.zip"
-    FILENAME "lz4-1.7.4.2.zip"
-    SHA512 c9a65031225ccda43ad4c7622e9f36762c18e58b4aaf43b1a33f219186fb55c43ca354f574a1591188db39f57631351b1b90f96e0c150e28de08dcb65c6759d0)
+    URLS "https://github.com/lz4/lz4/archive/v1.7.5.zip"
+    FILENAME "lz4-1.7.5.zip"
+    SHA512 09968b67a5cd8555f6e1d95b99971a82d228c6d8d9f9dd9e9a33c9633bed9bcf1e370c2ff44e58c6ca72d103c149585b3e83061c690f3e857eb5f53d586f86a4)
 
 vcpkg_extract_source_archive(${ARCHIVE})
 
@@ -12,6 +12,7 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS_DEBUG
         -DLZ4_SKIP_INCLUDES=ON)
 
