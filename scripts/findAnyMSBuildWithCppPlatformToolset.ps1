c81edf75928e01d11bbffbec3ec549e903c37fef
Alexander Karatarakis <alkarata@microsoft.com>
2017-02-24 16:11:48 -0800

    Rewrite MSBuild detection
    Add -Verbose messages. Look for VS2015 in Program files as well as registry

--- scripts/findAnyMSBuildWithCppPlatformToolset.ps1
+++ scripts/findAnyMSBuildWithCppPlatformToolset.ps1
@@ -7,63 +7,156 @@ param(
     [switch]$DisableVS2015 = $False
 )
 
+if ($DisableVS2017 -and $DisableVS2015)
+{
+    throw "Both VS2015 and VS2017 were disabled."
+}
+
+function New-MSBuildInstance()
+{
+    param ($msbuildExePath, $toolsetVersion)
+
+    $instance = new-object PSObject
+    $instance | add-member -type NoteProperty -Name msbuildExePath -Value $msbuildExePath
+    $instance | add-member -type NoteProperty -Name toolsetVersion -Value $toolsetVersion
+
+    return $instance
+}
+
+Write-Verbose "Executing $($MyInvocation.MyCommand.Name) with DisableVS2017=$DisableVS2017, DisableVS2015=$DisableVS2015"
 $scriptsDir = split-path -parent $MyInvocation.MyCommand.Definition
 
-if (-not $DisableVS2017)
-{
-    # VS2017
-    $VisualStudio2017InstallationInstances = & $scriptsDir\findVisualStudioInstallationInstances.ps1
-    foreach ($instance in $VisualStudio2017InstallationInstances)
-    {
-        $VCFolder= "$instance\VC\Tools\MSVC\"
+$validInstances = New-Object System.Collections.ArrayList
 
-        if (Test-Path $VCFolder)
-        {
-            return "$instance\MSBuild\15.0\Bin\MSBuild.exe","v141"
-        }
+# VS2017
+Write-Verbose "`n`n"
+Write-Verbose "Checking for MSBuild from VS2017 instances..."
+$VisualStudio2017InstallationInstances = & $scriptsDir\findVisualStudioInstallationInstances.ps1
+Write-Verbose "VS2017 Candidates: $([system.String]::Join(',', $VisualStudio2017InstallationInstances))"
+foreach ($instanceCandidate in $VisualStudio2017InstallationInstances)
+{
+    $VCFolder= "$instanceCandidate\VC\Tools\MSVC\"
+
+    if (Test-Path $VCFolder)
+    {
+        $instance = New-MSBuildInstance "$instanceCandidate\MSBuild\15.0\Bin\MSBuild.exe" "v141"
+        Write-Verbose "Found $instance"
+        $validInstances.Add($instance) > $null
     }
 }
 
-if (-not $DisableVS2015)
+# VS2015 - in Program Files
+Write-Verbose "`n`n"
+Write-Verbose "Checking for MSBuild from VS2015 in Program Files..."
+$CandidateProgramFiles = $(& $scriptsDir\getProgramFiles32bit.ps1), $(& $scriptsDir\getProgramFilesPlatformBitness.ps1)
+Write-Verbose "Program Files Candidate locations: $([system.String]::Join(',', $CandidateProgramFiles))"
+foreach ($ProgramFiles in $CandidateProgramFiles)
 {
-    # Try to locate VS2015 through the Registry
+    $clExe= "$ProgramFiles\Microsoft Visual Studio 14.0\VC\bin\cl.exe"
+
+    if (Test-Path $clExe)
+    {
+        $instance = New-MSBuildInstance "$ProgramFiles\MSBuild\14.0\Bin\MSBuild.exe" "v140"
+        Write-Verbose "Found $instance"
+        $validInstances.Add($instance) > $null
+    }
+}
+
+# VS2015 - through the registry
+function NewCppRegistryPair()
+{
+    param ($visualStudioEntry, $msBuildEntry)
+
+    $instance = new-object PSObject
+    $instance | add-member -type NoteProperty -Name visualStudioEntry -Value $visualStudioEntry
+    $instance | add-member -type NoteProperty -Name msBuildEntry -Value $msBuildEntry
+
+    return $instance
+}
+
+Write-Verbose "`n`n"
+Write-Verbose "Checking for MSBuild from VS2015 through the registry..."
+
+$registryPairs =
+$(NewCppRegistryPair "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\visualstudio\14.0" "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\msbuild\toolsversions\14.0"),
+$(NewCppRegistryPair "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\visualstudio\14.0" "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\msbuild\toolsversions\14.0")
+
+foreach ($pair in $registryPairs)
+{
+    $vsEntry = $pair.visualStudioEntry
+    Write-Verbose "$vsEntry - Checking"
     try
     {
-        # First ensure the compiler was installed (optional in 2015)
-        # In 64-bit systems, this is under the Wow6432Node.
-        try
-        {
-            $VS14InstallDir = $(gp Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\visualstudio\14.0 InstallDir -erroraction Stop | % { $_.InstallDir })
-            Write-Verbose "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\visualstudio\14.0 - Found"
-        }
-        catch
-        {
-            $VS14InstallDir = $(gp Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\visualstudio\14.0 InstallDir -erroraction Stop | % { $_.InstallDir })
-            Write-Verbose "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\visualstudio\14.0 - Found"
-        }
-        if (!(Test-Path "${VS14InstallDir}..\..\VC\bin\cl.exe")) { throw }
-        Write-Verbose "${VS14InstallDir}..\..\VC\bin\cl.exe - Found"
-
-
-        try
-        {
-            $MSBuild14 = $(gp Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\msbuild\toolsversions\14.0 MSBuildToolsPath -erroraction Stop | % { $_.MSBuildToolsPath })
-            Write-Verbose "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\msbuild\toolsversions\14.0 - Found"
-        }
-        catch
-        {
-            $MSBuild14 = $(gp Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\msbuild\toolsversions\14.0 MSBuildToolsPath -erroraction Stop | % { $_.MSBuildToolsPath })
-            Write-Verbose "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\msbuild\toolsversions\14.0 - Found"
-        }
-        if (!(Test-Path "${MSBuild14}MSBuild.exe")) { throw }
-        Write-Verbose "${MSBuild14}MSBuild.exe - Found"
-
-        return "${MSBuild14}MSBuild.exe","v140"
+        $VS14InstallDir = $(gp $vsEntry InstallDir -erroraction Stop | % { $_.InstallDir })
+        Write-Verbose "$vsEntry - Found"
     }
     catch
     {
-        Write-Verbose "Unable to locate a VS2015 installation with C++ support"
+        Write-Verbose "$vsEntry - Not Found"
+        continue
+    }
+
+    Write-Verbose "$VS14InstallDir - Obtained from registry"
+    # We want "${VS14InstallDir}..\..\VC\bin\cl.exe"
+    # Doing Split-path to avoid the ..\.. from appearing in the output
+    $clExePath = Split-path $VS14InstallDir -Parent
+    $clExePath = Split-path $clExePath -Parent
+    $clExePath = "$clExePath\VC\bin\cl.exe"
+
+    if (!(Test-Path $clExePath))
+    {
+        Write-Verbose "$clExePath - Not Found"
+        continue
+    }
+
+    Write-Verbose "$clExePath - Found"
+
+    $msbuildEntry = $pair.msBuildEntry
+    Write-Verbose "$msbuildEntry - Checking"
+    try
+    {
+        $MSBuild14 = $(gp $msbuildEntry MSBuildToolsPath -erroraction Stop | % { $_.MSBuildToolsPath })
+        Write-Verbose "$msbuildEntry - Found"
+    }
+    catch
+    {
+        Write-Verbose "$msbuildEntry - Not Found"
+        continue
+    }
+
+    Write-Verbose "${MSBuild14} - Obtained from registry"
+    $msbuildPath = "${MSBuild14}MSBuild.exe"
+    if (!(Test-Path $msbuildPath))
+    {
+        Write-Verbose "$msbuildPath - Not Found"
+        continue
+    }
+
+    $instance = New-MSBuildInstance $msbuildPath "v140"
+    Write-Verbose "Found $instance"
+    $validInstances.Add($instance) > $null
+}
+
+Write-Verbose "`n`n`n"
+Write-Verbose "The following MSBuild instances were found:"
+foreach ($instance in $validInstances)
+{
+    Write-Verbose $instance
+}
+
+# Selecting
+foreach ($instance in $validInstances)
+{
+    if (!$DisableVS2017 -and $instance.toolsetVersion -eq "v141")
+    {
+        return $instance.msbuildExePath, $instance.toolsetVersion
+    }
+
+    if (!$DisableVS2015 -and $instance.toolsetVersion -eq "v140")
+    {
+        return $instance.msbuildExePath, $instance.toolsetVersion
     }
 }
+
 
 throw "Could not find MSBuild version with C++ support. VS2015 or VS2017 (with C++) needs to be installed."
