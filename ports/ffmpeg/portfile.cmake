6c26a42d54e30a22bc1c3a455646242e23e2f0df
Robert Schumacher <roschuma@microsoft.com>
2017-03-02 10:35:27 -0800

    [ffmpeg] Disable static building.

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -1,3 +1,8 @@
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    message(STATUS "Building as static libraries not currently supported. Building as DLLs instead.")
+    set(VCPKG_LIBRARY_LINKAGE "dynamic")
+endif()
+
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ffmpeg-3.2.4)
 vcpkg_download_distfile(ARCHIVE
