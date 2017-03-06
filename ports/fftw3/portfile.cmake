e89d6f54a3931834991baf69b7289104305bd415
Nicolas Riebesel <nicolas.riebesel@gmx.com>
2017-03-06 08:19:08 +0100

    Make use of static and dynamic linking

--- ports/fftw3/portfile.cmake
+++ ports/fftw3/portfile.cmake
@@ -16,10 +16,12 @@ vcpkg_download_distfile(ARCHIVE
 
 vcpkg_extract_source_archive(${ARCHIVE})
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/fix-dynamic.patch)
+if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
+	vcpkg_apply_patches(
+			SOURCE_PATH ${SOURCE_PATH}
+			PATCHES
+					${CMAKE_CURRENT_LIST_DIR}/fix-dynamic.patch)
+endif()
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/config.h DESTINATION ${SOURCE_PATH})
