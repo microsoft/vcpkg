da09a8136d32890ab5f3804d9abcc5f9685a30e0
Alexander Karatarakis <alkarata@microsoft.com>
2017-03-09 14:17:00 -0800

    [eigen3] Update to 3.3.3. Resolves #754

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -1,10 +1,10 @@
 #header-only library
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/eigen-eigen-26667be4f70b)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/eigen-eigen-67e894c6cd8f)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://bitbucket.org/eigen/eigen/get/3.3.0.tar.bz2"
-    FILENAME "eigen-3.3.0.tar.bz2"
-    SHA512 a1919accc9fcf64eca17b3e60a78eefeb1a187c261c28b8480604f36cf366cd6323e966d6b948f8a4ce0ae3213a3815bccd34661d1d8fb33315def4304bf163e
+    URLS "http://bitbucket.org/eigen/eigen/get/3.3.3.tar.bz2"
+    FILENAME "eigen-3.3.3.tar.bz2"
+    SHA512 bb5a8b761371e516f0a344a7c9f6e369e21c2907c8548227933ca6010fc607a66c8d6ff7c41b1aec3dea7d482ce8c2a09e38ae5c7a2c5b16bdd8007e7a81ecc3
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
