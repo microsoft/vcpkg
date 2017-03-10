47b53b9015deaa2f569d788447254c8bc3de96a9
Alexander Karatarakis <alkarata@microsoft.com>
2017-03-09 18:24:04 -0800

    Add lazy.h

---
+++ toolsrc/include/lazy.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace vcpkg
+{
+    template <typename T>
+    class lazy
+    {
+    public:
+        lazy() : value(T()), initialized(false) {}
+
+        template <class F>
+        T const& get_lazy(F& f) const
+        {
+            if (!initialized)
+            {
+                value = f();
+                initialized = true;
+            }
+            return value;
+        }
+
+    private:
+        mutable T value;
+        mutable bool initialized;
+    };
+}
