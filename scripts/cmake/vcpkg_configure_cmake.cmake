microsoft/vcpkg

commit 8dc2699e5dc77bf04a2d3455f05a31cb6b0f7c86

[vcpkg-configure-cmake] Use toolchain in internal builds.

scripts/cmake/vcpkg_configure_cmake.cmake
@@ -64,10 +64,11 @@ function(vcpkg_configure_cmake)
         "-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON"
         "-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON"
         "-DCMAKE_FIND_PACKAGE_NO_SYSTEM_PACKAGE_REGISTRY=ON"
-        "-DBoost_COMPILER=-vc140"
         "-DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=TRUE"
         "-DCMAKE_VERBOSE_MAKEFILE=ON"
-        "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TRIPLET_FILE}"
+        "-DVCPKG_APPLOCAL_DEPS=OFF"
+        "-DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT_DIR}/scripts/buildsystems/vcpkg.cmake"
+        "-DCMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION=ON"
     )
     if(DEFINED VCPKG_CRT_LINKAGE AND VCPKG_CRT_LINKAGE STREQUAL dynamic)
         list(APPEND _csc_OPTIONS_DEBUG

scripts/buildsystems/vcpkg.cmake
@@ -59,6 +59,16 @@ if(NOT VCPKG_TOOLCHAIN)
 
     set(Boost_COMPILER "-vc140")
 
+    file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
+    set(CMAKE_SYSTEM_IGNORE_PATH
+        "${_programfiles}/OpenSSL"
+        "${_programfiles}/OpenSSL-Win32"
+        "${_programfiles}/OpenSSL-Win64"
+        "C:/OpenSSL/"
+        "C:/OpenSSL-Win32/"
+        "C:/OpenSSL-Win64/"
+    )
+
     set(CMAKE_PROGRAM_PATH ${CMAKE_PROGRAM_PATH} ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools)
 
     option(VCPKG_APPLOCAL_DEPS "Automatically copy dependencies into the output directory for executables." ON)

commit 5414655ea46fde2e87943f93d3356533b5b183e4

[vcpkg] Fix regression introduced in 8dc2699e5d. Fixes #873. Fixes #871. Fixes #874. Fixes #876.
When using the toolchain file, VCPKG_TARGET_TRIPLET was not specified, which resulted in all libraries being built against x86-windows.

scripts/cmake/vcpkg_configure_cmake.cmake
@@ -59,6 +59,7 @@ function(vcpkg_configure_cmake)
 
 
     list(APPEND _csc_OPTIONS
+        "-DVCPKG_TARGET_TRIPLET=${TARGET_TRIPLET}"
         "-DCMAKE_CXX_FLAGS= /DWIN32 /D_WINDOWS /W3 /utf-8 /GR /EHsc /MP"
         "-DCMAKE_C_FLAGS= /DWIN32 /D_WINDOWS /W3 /utf-8 /MP"
         "-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON"