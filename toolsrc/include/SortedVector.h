69bfe5d0eeb9a7020db6fbf10ec702a0e1df72ae
Alexander Karatarakis <alkarata@microsoft.com>
2017-04-03 16:36:07 -0700

    [SortedVector] Add default constructor

--- toolsrc/include/SortedVector.h
+++ toolsrc/include/SortedVector.h
@@ -13,6 +13,8 @@ namespace vcpkg
         using size_type = typename std::vector<T>::size_type;
         using iterator = typename std::vector<T>::const_iterator;
 
+        SortedVector() : m_data() {}
+
         explicit SortedVector(std::vector<T> v) : m_data(std::move(v))
         {
             if (!std::is_sorted(m_data.begin(), m_data.end()))
@@ -20,6 +22,7 @@ namespace vcpkg
                 std::sort(m_data.begin(), m_data.end());
             }
         }
+
         template <class Compare>
         SortedVector(std::vector<T> v, Compare comp) : m_data(std::move(v))
         {
