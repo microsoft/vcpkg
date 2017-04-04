d9a403453904261c6b34c5d33557134b9fe3a8a0
Alexander Karatarakis <alkarata@microsoft.com>
2017-04-03 17:12:00 -0700

    Join declaration with assignment

--- toolsrc/src/vcpkg_metrics_uploader.cpp
+++ toolsrc/src/vcpkg_metrics_uploader.cpp
@@ -7,10 +7,8 @@ using namespace vcpkg;
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
-    LPWSTR* szArgList;
     int argCount;
-
-    szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
+    LPWSTR * szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
 
     Checks::check_exit(VCPKG_LINE_INFO, argCount == 2, "Requires exactly one argument, the path to the payload file");
     Metrics::upload(Files::read_contents(szArgList[1]).value_or_exit(VCPKG_LINE_INFO));
