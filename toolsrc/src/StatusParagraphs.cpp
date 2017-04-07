ad24821dce97926e8eba995f4c18feaeea247b77
Alexander Karatarakis <alkarata@microsoft.com>
2017-04-07 14:25:12 -0700

    Fix find_installed()

--- toolsrc/src/StatusParagraphs.cpp
+++ toolsrc/src/StatusParagraphs.cpp
@@ -32,7 +32,7 @@ namespace vcpkg
     StatusParagraphs::const_iterator StatusParagraphs::find_installed(const std::string& name, const Triplet& target_triplet) const
     {
         const const_iterator it = find(name, target_triplet);
-        if (it != end() && (*it)->want == Want::INSTALL)
+        if (it != end() && (*it)->want == Want::INSTALL && (*it)->state == InstallState::INSTALLED)
         {
             return it;
         }
