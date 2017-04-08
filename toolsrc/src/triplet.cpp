13482879f82b15294ebdeb04b3084584a3b468a1
Alexander Karatarakis <alkarata@microsoft.com>
2017-04-07 17:44:24 -0700

    Triplet.to_string()

--- toolsrc/src/triplet.cpp
+++ toolsrc/src/triplet.cpp
@@ -11,16 +11,6 @@ namespace vcpkg
     const Triplet Triplet::X64_UWP = from_canonical_name("x64-uwp");
     const Triplet Triplet::ARM_UWP = from_canonical_name("arm-uwp");
 
-    std::string to_string(const Triplet& t)
-    {
-        return t.canonical_name();
-    }
-
-    std::string to_printf_arg(const Triplet& t)
-    {
-        return to_string(t);
-    }
-
     bool operator==(const Triplet& left, const Triplet& right)
     {
         return left.canonical_name() == right.canonical_name();
@@ -33,7 +23,7 @@ namespace vcpkg
 
     std::ostream& operator<<(std::ostream& os, const Triplet& t)
     {
-        return os << to_string(t);
+        return os << t.to_string();
     }
 
     Triplet Triplet::from_canonical_name(const std::string& triplet_as_string)
@@ -62,5 +52,10 @@ namespace vcpkg
     {
         auto it = std::find(this->m_canonical_name.cbegin(), this->m_canonical_name.cend(), '-');
         return std::string(it + 1, this->m_canonical_name.cend());
+    }
+
+    const std::string& Triplet::to_string() const
+    {
+        return this->m_canonical_name;
     }
 }
