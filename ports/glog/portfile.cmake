c31c026c12e7ad86b2ce5d523dbb2801aaf1a658
Han Hu <huhan8807@gmail.com>
2017-04-19 13:48:01 +0800

    install cmake config files for glog

--- ports/glog/portfile.cmake
+++ ports/glog/portfile.cmake
@@ -14,6 +14,10 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 24506ad1cc05e8361379b925ecfc8f32cc47692a47598401cca340eb2a528fe28b8b0d3636983056c7910469d105095bd3bacacff6278bffa18d85603c3dbfa8
 )
 vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-cmake-install-files.patch"
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -24,10 +28,27 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/glog)
+
+file(GLOB GLOG_CMAKE_FILES 
+    ${CURRENT_PACKAGES_DIR}/cmake/lib/glog/*.cmake 
+    ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/glog/*.cmake
+    ${CURRENT_PACKAGES_DIR}/debug/share/glog/*.cmake)
+file(COPY ${GLOG_CMAKE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/share/glog)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+# changes target search path
+file(READ ${CURRENT_PACKAGES_DIR}/debug/share/glog/glog-targets-debug.cmake GLOG_DEBUG_MODULE)
+string(REPLACE "\${_IMPORT_PREFIX}" "\${_IMPORT_PREFIX}/debug" GLOG_DEBUG_MODULE "${GLOG_DEBUG_MODULE}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/glog/glog-targets-debug.cmake "${GLOG_DEBUG_MODULE}")
+
+# remove not used cmake files
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share )
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake )
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake )
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/glog)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/glog/COPYING ${CURRENT_PACKAGES_DIR}/share/glog/copyright)
