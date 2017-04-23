0468c2de706167bbec615c534104e901c747541b
James Chang <twmr7@outlook.com>
2017-04-23 21:23:25 +0800

    [cppzmq] update to 4.2.1 release

--- ports/cppzmq/portfile.cmake
+++ ports/cppzmq/portfile.cmake
@@ -1,15 +1,16 @@
 #header-only library
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cppzmq-7faa9b061843fcbceb7ed94984ee8f20284ee759)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cppzmq-4.2.1)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/zeromq/cppzmq/archive/7faa9b061843fcbceb7ed94984ee8f20284ee759.zip"
-    FILENAME "cppzmq-7faa.zip"
-    SHA512 10ba10f0e9a2387dc75fec01c2629b969f23d6152596a475474b701a4efccc4007c8eae5ec2a89f7f26e7d117f36016aaead16bf3325a8780bfd6419d84ac54e
+    URLS "https://github.com/zeromq/cppzmq/archive/v4.2.1.zip"
+    FILENAME "cppzmq-4.2.1.zip"
+    SHA512 ee75ce4bd28ecb5ef660d1ed6f5522654eced6ded8745dc0c61df351f4ff0ff8980d1bd848b2649fcce4aa539a457e56e55b0a59cb49f44b0a29875d0ea28dce
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 # cppzmq is a single header library, so we just need to copy that file in the include directory
 file(INSTALL ${SOURCE_PATH}/zmq.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/zmq_addon.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cppzmq)
