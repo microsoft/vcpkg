656ba237d745342f1dfbc1de07a280a9f5926442
İsmail Dönmez <ismail@i10z.com>
2017-04-24 14:19:26 +0200

    Use msys64 on 64bit systems

--- scripts/cmake/vcpkg_acquire_msys.cmake
+++ scripts/cmake/vcpkg_acquire_msys.cmake
@@ -1,9 +1,16 @@
 function(vcpkg_acquire_msys PATH_TO_ROOT_OUT)
   set(TOOLPATH ${DOWNLOADS}/tools/msys2)
-  set(TOOLSUBPATH msys32)
-  set(URL "https://sourceforge.net/projects/msys2/files/Base/i686/msys2-base-i686-20161025.tar.xz/download")
-  set(ARCHIVE "msys2-base-i686-20161025.tar.xz")
-  set(HASH c9260a38e0c6bf963adeaea098c4e376449c1dd0afe07480741d6583a1ac4c138951ccb0c5388bd148e04255a5c1a23bf5ee2d58dcd6607c14f1eaa5639a7c85)
+  if (TRIPLET_SYSTEM_ARCH MATCHES "x64")
+    set(TOOLSUBPATH msys64)
+    set(URL "https://sourceforge.net/projects/msys2/files/Base/x86_64/msys2-base-x86_64-20161025.tar.xz/download"
+    set(ARCHIVE "msys2-base-x86_64-20161025.tar.xz")
+    set(HASH 6c4c18ec59db80b8269698d074866438a624f1ce735ee5005a01b148b02e8f2e966ae381aa1cb4c50f6226c3b7feb271e36907cf26580df084d695b3c9f5c0eb)
+  else()
+    set(TOOLSUBPATH msys32)
+    set(URL "https://sourceforge.net/projects/msys2/files/Base/i686/msys2-base-i686-20161025.tar.xz/download")
+    set(ARCHIVE "msys2-base-i686-20161025.tar.xz")
+    set(HASH c9260a38e0c6bf963adeaea098c4e376449c1dd0afe07480741d6583a1ac4c138951ccb0c5388bd148e04255a5c1a23bf5ee2d58dcd6607c14f1eaa5639a7c85)
+  endif()
 
   set(PATH_TO_ROOT ${TOOLPATH}/${TOOLSUBPATH})
 
