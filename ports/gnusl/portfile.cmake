d75fcbb431e58715c3470e86ec9697948049f24c
Martin Müller <muemart@users.noreply.github.com>
2017-05-02 09:10:21 +0200

    Rename port from libgsl to gnusl

--- ports/gnusl/portfile.cmake
+++ ports/gnusl/portfile.cmake
@@ -37,7 +37,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libgsl)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/libgsl/COPYING ${CURRENT_PACKAGES_DIR}/share/libgsl/copyright)
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/gnusl)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/gnusl/COPYING ${CURRENT_PACKAGES_DIR}/share/gnusl/copyright)
 
 vcpkg_copy_pdbs()
