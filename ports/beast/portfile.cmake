ca26510f03418055d05d51bcb2385cbc7a511d12
Robert Schumacher <roschuma@microsoft.com>
2017-05-03 04:49:43 -0700

    [beast] Use vcpkg_from_github

--- ports/beast/portfile.cmake
+++ ports/beast/portfile.cmake
@@ -1,12 +1,13 @@
 # header only
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/beast-1.0.0-b30)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/vinniefalco/Beast/archive/v1.0.0-b30.zip"
-    FILENAME "beast-1.0.0-b30.zip"
-    SHA512 af801748efabafef1b7ae817be9da9480dcf881b3037f92e5997e42255399bd7b22772bb2a5c9aab7d01c31c7995c4d23a41f4b7f6ccdef18d9a8a15906fd43f
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO vinniefalco/Beast
+    REF v1.0.0-b30
+    SHA512 78b1b09d6785e8b782bea72b6849936c0be45df1fd137db832c0afe1b09af122e0fd69e25321bcd8681f03015d94345a40f9650a5560f12c73457e4cc82e2f30
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/beast RENAME copyright)
