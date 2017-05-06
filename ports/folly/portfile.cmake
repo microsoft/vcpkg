0aa8055fbd8e721f1829d73f92eaf3868bd70a36
Arkadiy Shapkin <arkady.shapkin@gmail.com>
2017-05-06 12:55:19 +0300

    [folly] Initial commit for Facebook Folly library

---
+++ ports/folly/portfile.cmake
@@ -0,0 +1,47 @@
+if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
+  message(FATAL_ERROR "Folly only supports the x64 architecture.")
+endif()
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported yet. Building static.")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebook/folly
+    REF v2017.05.08.00
+    SHA512 1f689ec288e2a5de28223bddd68fdbb37c26c46ed9d9b831cacc46580788b9bf8eed151a043a4905172e509c70ed3d845689bf2c0cd45ce05fbe76907e2049aa
+    HEAD_REF master
+)
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-cmakelists.patch"
+)
+
+if(VCPKG_CRT_LINKAGE STREQUAL static)
+    set(MSVC_USE_STATIC_RUNTIME ON)
+else()
+    set(MSVC_USE_STATIC_RUNTIME OFF)
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DMSVC_USE_STATIC_RUNTIME=${MSVC_USE_STATIC_RUNTIME}
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/folly)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/folly/LICENSE ${CURRENT_PACKAGES_DIR}/share/folly/copyright)
