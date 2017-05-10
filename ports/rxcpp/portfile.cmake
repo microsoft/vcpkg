29cc5780feaacbfa77dabbc2230f29c57dbbef29
Robert Schumacher <roschuma@microsoft.com>
2017-05-09 20:55:58 -0700

    [rxcpp] Use vcpkg_from_github()

--- ports/rxcpp/portfile.cmake
+++ ports/rxcpp/portfile.cmake
@@ -1,12 +1,14 @@
 #header-only library
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/RxCpp-4.0.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/Reactive-Extensions/RxCpp/archive/v4.0.0.tar.gz"
-    FILENAME "RxCpp-v4.0.0.tar.gz"
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Reactive-Extensions/RxCpp
+    REF v4.0.0
     SHA512 df7582925e749fb7f9fbc085566d57a46571e18202aacaa7d35cf9da8152b29dd2886b2d07dae93e538f2a0f7b91d93fb3da23c34c7ea4467772515c794a25b7
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 file(INSTALL
 	${SOURCE_PATH}/Rx/v2/src/rxcpp
