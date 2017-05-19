a8caf080b8e2fb7d254c62f75825059954230ade
Robert Schumacher <roschuma@microsoft.com>
2017-05-18 20:03:52 -0700

    [doctest] Switch to using vcpkg_from_github to support --head

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -1,12 +1,12 @@
 #header-only library
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/doctest-1.2.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/onqtam/doctest/archive/1.2.0.zip"
-    FILENAME "doctest-1.2.0.zip"
-    SHA512 7b7ee66458a9d6e43aab57cced6c5e565bec414664300d80d8d08f5a90e19ecb9685762b3462927b7ecd890bd9fb0dde53b2b034e29e559f484d328cc6403aa7
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO onqtam/doctest
+    REF 1.2.0
+    SHA512 7e01e4b817e2200fcd7216342b184ddb60723180bb0a69e24d8c54b7b7d5d983bbd7a2fb0c629e87c95127f4213cca11beb95d079880313d2445703e289c23b5
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/doctest RENAME copyright)
