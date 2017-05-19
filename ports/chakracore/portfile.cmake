900613d7f066af7121dd06512ace07dcfbf8693b
Robert Schumacher <roschuma@microsoft.com>
2017-05-19 16:57:06 -0700

    [chakracore] Fix VS2017 builds, reformat to 4-spaces indentation.

--- ports/chakracore/portfile.cmake
+++ ports/chakracore/portfile.cmake
@@ -2,6 +2,9 @@
     message(STATUS "Warning: Static building not supported yet. Building dynamic.")
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
+if (VCPKG_CRT_LINKAGE STREQUAL static)
+    message(FATAL_ERROR "Static linking of the CRT is not yet supported.")
+endif()
 include(vcpkg_common_functions)
 
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ChakraCore-1.4.1)
@@ -15,44 +18,51 @@ vcpkg_extract_source_archive(${ARCHIVE_FILE})
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES 
-		${CMAKE_CURRENT_LIST_DIR}/disable_warning_as_error.patch
+        ${CMAKE_CURRENT_LIST_DIR}/disable_warning_as_error.patch
 )
+
+find_path(COR_H_PATH cor.h)
+if(COR_H_PATH MATCHES "NOTFOUND")
+    message(FATAL_ERROR "Could not find <cor.h>. Ensure the NETFXSDK is installed.")
+endif()
+get_filename_component(NETFXSDK_PATH "${COR_H_PATH}/../.." ABSOLUTE)
 
 vcpkg_build_msbuild(
     PROJECT_PATH ${SOURCE_PATH}/Build/Chakra.Core.sln
+    OPTIONS "/p:DotNetSdkRoot=${NETFXSDK_PATH}/"
 )
 
 file(INSTALL
-	${SOURCE_PATH}/lib/jsrt/ChakraCore.h
-	${SOURCE_PATH}/lib/jsrt/ChakraCommon.h
-	${SOURCE_PATH}/lib/jsrt/ChakraCommonWindows.h
-	${SOURCE_PATH}/lib/jsrt/ChakraDebug.h
+    ${SOURCE_PATH}/lib/jsrt/ChakraCore.h
+    ${SOURCE_PATH}/lib/jsrt/ChakraCommon.h
+    ${SOURCE_PATH}/lib/jsrt/ChakraCommonWindows.h
+    ${SOURCE_PATH}/lib/jsrt/ChakraDebug.h
     DESTINATION ${CURRENT_PACKAGES_DIR}/include
 )
 file(INSTALL
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.dll
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.pdb
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.dll
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.pdb
     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
 )
 file(INSTALL
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/Chakracore.lib
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/Chakracore.lib
     DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
 )
 file(INSTALL
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.dll
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.pdb
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.dll
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.pdb
     DESTINATION ${CURRENT_PACKAGES_DIR}/bin
 )
 file(INSTALL
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/Chakracore.lib
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/Chakracore.lib
     DESTINATION ${CURRENT_PACKAGES_DIR}/lib
 )
 file(INSTALL
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ch.exe
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/GCStress.exe
-	${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/rl.exe
-	DESTINATION ${CURRENT_PACKAGES_DIR}/tools/chakracore)
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ch.exe
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/GCStress.exe
+    ${SOURCE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/rl.exe
+    DESTINATION ${CURRENT_PACKAGES_DIR}/tools/chakracore)
 vcpkg_copy_pdbs()
 file(INSTALL
-	${SOURCE_PATH}/LICENSE.txt
-	DESTINATION ${CURRENT_PACKAGES_DIR}/share/ChakraCore RENAME copyright)
+    ${SOURCE_PATH}/LICENSE.txt
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/ChakraCore RENAME copyright)
