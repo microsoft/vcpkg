3940773ab8eb0e29c0d5e567899271490e113784
Jack Chang <wei0831@gmail.com>
2017-05-25 15:10:43 -0700

    [uwebsockets] update to 0.14.3

--- ports/uwebsockets/portfile.cmake
+++ ports/uwebsockets/portfile.cmake
@@ -1,10 +1,10 @@
 include(vcpkg_common_functions)
-set(VERSION 0.14.2)
+set(VERSION 0.14.3)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/uwebsockets-${VERSION})
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/uWebSockets/uWebSockets/archive/v${VERSION}.zip"
     FILENAME "uwebsockets-v${VERSION}.zip"
-    SHA512 b6389a00a310d77ec55273c1b9499679b13f5e430c31fbc5dd5847780455115d95d5a439cd82dddc537d85c4afb5db4cacefb6db5b3f9681ff142d6ab9ef5024
+    SHA512 a64a6ee91d411f4285deb10b9b7620b4c4a2ff0f8137864380968349f7786c406ba44644b6ca589ee41c1e5adfa1ee1cf3697e54ecf5d459ab8bf69a3a4def7e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
