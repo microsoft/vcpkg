cad6321b8ea068528a1526224a5deec93c9aa051
Robert Schumacher <roschuma@microsoft.com>
2017-05-28 04:46:21 -0700

    [thrift] Small cleanup, added an unstable ref back in since the last stable doesn't build.

--- ports/thrift/portfile.cmake
+++ ports/thrift/portfile.cmake
@@ -3,17 +3,11 @@ include(vcpkg_common_functions)
 if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     message(STATUS "Warning: Dynamic building not supported. Building static.") # See note below
     set(VCPKG_LIBRARY_LINKAGE static)
+
+    # As per Ben Craig thrift comment see https://issues.apache.org/jira/browse/THRIFT-1834
+    # Currently, Thrift is designed to be packaged as a static library. As a static library, the consuming program / dll will only pull in the object files that it needs, so the per-binary size increase should be pretty small.
+    # Thrift isn't a very good candidate to become a dynamic library. No attempts are made to preserve binary compatibility, or to provide a C / COM-like interface to make binary compatibility easy.
 endif()
-
-#Note: last stable fails to build. So use --head instead 
-
-if(NOT VCPKG_USE_HEAD_VERSION)
-    message(FATAL_ERROR "Package does not specify REF. It must built using --head.")
- endif()
-
-# As per Ben Craig thrift comment see https://issues.apache.org/jira/browse/THRIFT-1834
-# Currently, Thrift is designed to be packaged as a static library. As a static library, the consuming program / dll will only pull in the object files that it needs, so the per-binary size increase should be pretty small.
-# Thrift isn't a very good candidate to become a dynamic library. No attempts are made to preserve binary compatibility, or to provide a C / COM-like interface to make binary compatibility easy.
 
 vcpkg_find_acquire_program(FLEX)
 vcpkg_find_acquire_program(BISON)
@@ -21,12 +15,25 @@ vcpkg_find_acquire_program(BISON)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apache/thrift
+    REF 72ca60debae1d9fb35d9f0085118873669006d7f
+    SHA512 6050c66b176ad596d9632b224b8fb49aa3b823c977dacad729dbcdefb0da60eb983d3a32d9326b4317bae6755fb1fc4cee2cc6c282b3e1636a4528844c0f5915
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS   -DWITH_SHARED_LIB=OFF -DWITH_STATIC_LIB=ON  -DBUILD_TESTING=off -DBUILD_JAVA=off -DBUILD_C_GLIB=off -DBUILD_PYTHON=off -DBUILD_CPP=on -DBUILD_HASKELL=off -DBUILD_TUTORIALS=off -DFLEX_EXECUTABLE=${FLEX} -DBISON_EXECUTABLE=${BISON}
+    OPTIONS
+        -DWITH_SHARED_LIB=OFF
+        -DWITH_STATIC_LIB=ON
+        -DBUILD_TESTING=off
+        -DBUILD_JAVA=off
+        -DBUILD_C_GLIB=off
+        -DBUILD_PYTHON=off
+        -DBUILD_CPP=on
+        -DBUILD_HASKELL=off
+        -DBUILD_TUTORIALS=off
+        -DFLEX_EXECUTABLE=${FLEX}
+        -DBISON_EXECUTABLE=${BISON}
 )
 
 vcpkg_install_cmake()
@@ -34,9 +41,10 @@ vcpkg_install_cmake()
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/thrift RENAME copyright)
 
 file(GLOB EXES "${CURRENT_PACKAGES_DIR}/bin/*.exe")
-
 if(EXES)
-    file(COPY ${EXES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
+    file(COPY ${EXES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/thrift)
+    file(REMOVE ${EXES})
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/thrift)
 endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
