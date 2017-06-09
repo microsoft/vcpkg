741caef3fb352f6fbd5d4d59592f0c2629892064
sigman <sigman@ioupg.com>
2017-06-09 15:09:05 +0300

    [Magnum][Corrade] Cleanup and Audio support
    Enabled audio support in magnum by default via openal-soft dependency.

--- ports/corrade/portfile.cmake
+++ ports/corrade/portfile.cmake
@@ -23,9 +23,6 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA # Disable this option if project cannot be built with Ninja
     OPTIONS -DBUILD_STATIC=${BUILD_STATIC}
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
 
 vcpkg_install_cmake()
