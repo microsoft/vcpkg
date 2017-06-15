d1c32d241f8c06dc7169ea43070032229b2a7eb0
Robert Schumacher <roschuma@microsoft.com>
2017-06-15 10:18:14 -0700

    [catch] Rename downloaded files to not conflict with older versions or other ports

--- ports/catch/portfile.cmake
+++ ports/catch/portfile.cmake
@@ -1,26 +1,18 @@
-# Common Ambient Variables:
-#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
-#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
-#   PORT is the current port name (zlib, etc)
-#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#
-
-#header-only library
 include(vcpkg_common_functions)
 
+set(CATCH_VERSION v1.9.5)
+
 vcpkg_download_distfile(HEADER
-    URLS "https://github.com/philsquared/Catch/releases/download/v1.9.5/catch.hpp"
-    FILENAME "catch.hpp"
+    URLS "https://github.com/philsquared/Catch/releases/download/${CATCH_VERSION}/catch.hpp"
+    FILENAME "catch-${CATCH_VERSION}.hpp"
     SHA512 4e5dc6959903282c90c7e7981238248072c7f5d4083ccfcd62ca8db494bed46cbd280624d1bafaf2221ca02e7877a4cc319f1adee06dfaeee9da3757b7a7ba56
 )
 
 vcpkg_download_distfile(LICENSE
-    URLS "https://raw.githubusercontent.com/philsquared/Catch/v1.9.5/LICENSE.txt"
-    FILENAME "LICENSE.txt"
+    URLS "https://raw.githubusercontent.com/philsquared/Catch/${CATCH_VERSION}/LICENSE.txt"
+    FILENAME "catch-LICENSE-${CATCH_VERSION}.txt"
     SHA512 d6078467835dba8932314c1c1e945569a64b065474d7aced27c9a7acc391d52e9f234138ed9f1aa9cd576f25f12f557e0b733c14891d42c16ecdc4a7bd4d60b8
 )
 
-file(COPY ${HEADER} DESTINATION ${CURRENT_PACKAGES_DIR}/include )
-file(COPY ${LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/catch )
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/catch/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/catch/copyright)
+file(INSTALL ${HEADER} DESTINATION ${CURRENT_PACKAGES_DIR}/include RENAME catch.hpp)
+file(INSTALL ${LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/catch RENAME copyright)
