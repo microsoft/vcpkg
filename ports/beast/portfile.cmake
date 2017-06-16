ce7d0d3743ddda3cc2e6fe158930c474312ab91c
Vinnie Falco <vinnie.falco@gmail.com>
2017-06-16 09:23:09 -0700

    [beast] Update to version 59

--- ports/beast/portfile.cmake
+++ ports/beast/portfile.cmake
@@ -4,8 +4,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vinniefalco/Beast
-    REF 3f8097b6fddce8463084ce5c0d69db9a9079c2b8
-    SHA512 843cc0ddea35987e4f5eeaabcbcf3317135979eec9645a324740afa2a0af5041af787ee05e14d55d2ce17c72e227eacbadbe87c49e1bb6d7a0b075ad9694d92d
+    REF 4e4bcf8b1114229713eec0cf5fd392e5bdd76717
+    SHA512 514b93dccd483b1b847d9274f8a6fe9f4c32fdae6e1c45410abd3bb3554fa901aac8d6fc666a76420e1c4fa729e2ce7f95afa459faf01614de41995040eeb7bb
     HEAD_REF master
 )
 
