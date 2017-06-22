03de2e421b1e40d3a2c9eb60889e983b7d83b49b
Robert Schumacher <roschuma@microsoft.com>
2017-06-21 18:17:41 -0700

    [evpp] Simplify port to use vcpkg_from_github()

--- ports/evpp/portfile.cmake
+++ ports/evpp/portfile.cmake
@@ -1,15 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 set(EVPP_LOCAL_TEST OFF)
@@ -23,34 +11,26 @@ if (EVPP_LOCAL_TEST)
         FILENAME "evpp-${EVPP_VERSION}.zip"
         SHA512 ${EVPP_HASH}
     )
+    vcpkg_extract_source_archive(${ARCHIVE})
 else ()
-    set(EVPP_HASH 13986f81efc7f831274cc55819980ee57d6a8729867901beafd3d31c50682de304a9552912ace1c7e0a92ee2df41a8c456b5f77f4721d622c97584b38b68cd37)
-    set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/evpp-${EVPP_VERSION})
-    vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/Qihoo360/evpp/archive/v${EVPP_VERSION}.zip"
-        FILENAME "evpp-${EVPP_VERSION}.zip"
-        SHA512 ${EVPP_HASH}
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO Qihoo360/evpp
+        REF v${EVPP_VERSION}
+        SHA512 08226fe9853c1984f6554ede8f79a5767eec1d12ff2ff7172eef6f715ac7ea3f495b2336876823842408bd92b0ad99c9a3d506c07fc0add369f5cfa777f0406a
+        HEAD_REF master
     )
 endif ()
 
-vcpkg_extract_source_archive(${ARCHIVE})
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT_DIR}/scripts/buildsystems/vcpkg.cmake -DEVPP_VCPKG_BUILD=ON
+    OPTIONS -DEVPP_VCPKG_BUILD=ON
 )
 
 vcpkg_install_cmake()
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/evpp)
 
-#remove duplicated files
+# remove duplicated include files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-# remove not used cmake files
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share )
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake )
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake )
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/evpp)
