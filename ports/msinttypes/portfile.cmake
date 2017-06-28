04e60310274b24820ec62313458aae001bcc035f
atkawa7 <atkawa7@yahoo.com>
2017-06-28 11:52:22 -0700

    [msinttypes] required by #1343

---
+++ ports/msinttypes/portfile.cmake
@@ -0,0 +1,13 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO chemeris/msinttypes
+    REF 7636cabe55318824dc702d15b69711f5d7c30250
+    SHA512 1c3c350d12c6b69e1cb6469f742afc126d50fd92e137ecacdb8367e320350cd42d7d41fbb0aa38d6a13aefbef5308f9ec89825e9b80a932f552a889f63b35cb2
+    HEAD_REF master
+)
+
+file(INSTALL ${SOURCE_PATH}/inttypes.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/stdint.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/stdint.h DESTINATION ${CURRENT_PACKAGES_DIR}/share/msinttypes RENAME copyright)
