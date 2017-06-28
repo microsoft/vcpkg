a88a904849e03cc807942f78edf06ba7b464b6cd
Robert Schumacher <roschuma@microsoft.com>
2017-06-28 14:16:48 -0700

    [kinectsdk1] Move includes to match how the SDK expects them to be included

--- ports/kinectsdk1/portfile.cmake
+++ ports/kinectsdk1/portfile.cmake
@@ -1,15 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 get_filename_component(KINECTSDK10_DIR "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Kinect;SDKInstallPath]" ABSOLUTE CACHE)
@@ -24,13 +12,15 @@ file(
         "${KINECTSDK10_DIR}/inc/NuiSensor.h"
         "${KINECTSDK10_DIR}/inc/NuiSkeleton.h"
     DESTINATION
-        ${CURRENT_PACKAGES_DIR}/include/kinectsdk1
+        ${CURRENT_PACKAGES_DIR}/include
 )
 
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
     set(ARCHITECTURE x86)
 elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
     set(ARCHITECTURE amd64)
+else()
+    message(FATAL_ERROR "This port does not currently support architecture: ${VCPKG_TARGET_ARCHITECTURE}")
 endif()
 
 file(
