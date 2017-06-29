49d00f460453fbbedd9cdee635720d0bc84de9cb
atkawa7 <atkawa7@yahoo.com>
2017-06-28 21:46:53 -0700

    [rocksdb] fixed third party deps

--- ports/rocksdb/portfile.cmake
+++ ports/rocksdb/portfile.cmake
@@ -6,73 +6,31 @@ if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
   message(FATAL_ERROR "Rocksdb only supports x64")
 endif()
 
-set(VCPKG_PLATFORM_TOOLSET v140)
-set(MSVS_VERSION 2015)
-
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO facebook/rocksdb
-  REF v5.4.6
-  SHA512 fe804335ef1b6e37df9b76ab665c1071253d62848878732e541e88444d9d226a1ac70a53a51641e1f554974711781d524d5069ac672589de7d2ec59874ec9290
+  REF 18c63af6ef2b9f014c404b88488ae52e6fead03c
+  SHA512 8dd4d27768feba6d9ddb61debe6cae21fa6d25c27dc347cba3b28cc39d2c1fa860dba7c8adedba4b40883eccccca190b60941cf958855c6b70ec5a3b96c20ac5
   HEAD_REF master
 )
 
-LIST(APPEND ENV_REPLACE_LIST
-  "set(GFLAGS_HOME \${CURRENT_INSTALLED_DIR})"
-  "set(GFLAGS_INCLUDE \${GFLAGS_HOME}/include)"
-  "set(GFLAGS_LIB_DEBUG \${GFLAGS_HOME}/debug/lib/gflags.lib)"
-  "set(GFLAGS_LIB_RELEASE \${GFLAGS_HOME}/lib/gflags.lib)"
-  "set(SNAPPY_HOME \${CURRENT_INSTALLED_DIR})"
-  "set(SNAPPY_INCLUDE \${SNAPPY_HOME}/include)"
-  "set(SNAPPY_LIB_DEBUG \${SNAPPY_HOME}/debug/lib/snappy.lib)"
-  "set(SNAPPY_LIB_RELEASE \${SNAPPY_HOME}/lib/snappy.lib)"
-  "set(LZ4_HOME \${CURRENT_INSTALLED_DIR})"
-  "set(LZ4_INCLUDE \${LZ4_HOME}/include)"
-  "set(LZ4_LIB_DEBUG \${LZ4_HOME}/debug/lib/lz4.lib)"
-  "set(LZ4_LIB_RELEASE \${LZ4_HOME}/lib/lz4.lib)"
-  "set(ZLIB_HOME \${CURRENT_INSTALLED_DIR})"
-  "set(ZLIB_INCLUDE \${ZLIB_HOME}/include)"
-  "set(ZLIB_LIB_DEBUG \${ZLIB_HOME}/debug/lib/zlibd.lib)"
-  "set(ZLIB_LIB_RELEASE \${ZLIB_HOME}/lib/zlib.lib)"
-)
-LIST(APPEND ENV_FIND_LIST
-  "set(GFLAGS_HOME \$ENV{THIRDPARTY_HOME}/Gflags.Library)"
-  "set(GFLAGS_INCLUDE \${GFLAGS_HOME}/inc/include)"
-  "set(GFLAGS_LIB_DEBUG \${GFLAGS_HOME}/bin/debug/amd64/gflags.lib)"
-  "set(GFLAGS_LIB_RELEASE \${GFLAGS_HOME}/bin/retail/amd64/gflags.lib)"
-  "set(SNAPPY_HOME \$ENV{THIRDPARTY_HOME}/Snappy.Library)"
-  "set(SNAPPY_INCLUDE \${SNAPPY_HOME}/inc/inc)"
-  "set(SNAPPY_LIB_DEBUG \${SNAPPY_HOME}/bin/debug/amd64/snappy.lib)"
-  "set(SNAPPY_LIB_RELEASE \${SNAPPY_HOME}/bin/retail/amd64/snappy.lib)"
-  "set(LZ4_HOME \$ENV{THIRDPARTY_HOME}/LZ4.Library)"
-  "set(LZ4_INCLUDE \${LZ4_HOME}/inc/include)"
-  "set(LZ4_LIB_DEBUG \${LZ4_HOME}/bin/debug/amd64/lz4.lib)"
-  "set(LZ4_LIB_RELEASE \${LZ4_HOME}/bin/retail/amd64/lz4.lib)"
-  "set(ZLIB_HOME \$ENV{THIRDPARTY_HOME}/ZLIB.Library)"
-  "set(ZLIB_INCLUDE \${ZLIB_HOME}/inc/include)"
-  "set(ZLIB_LIB_DEBUG \${ZLIB_HOME}/bin/debug/amd64/zlib.lib)"
-  "set(ZLIB_LIB_RELEASE \${ZLIB_HOME}/bin/retail/amd64/zlib.lib)"
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+    "${CMAKE_CURRENT_LIST_DIR}/fix-building-both-static-and-shared.patch"
+    "${CMAKE_CURRENT_LIST_DIR}/fix-third-party-deps.patch"
 )
 
-message(STATUS "${SOURCE_PATH}/thirdparty.inc is here")
 
-LIST( LENGTH ENV_REPLACE_LIST COUNT )
-MATH(EXPR COUNT "${COUNT}-1")
-
-file(READ "${SOURCE_PATH}/thirdparty.inc" THIRDY_PARTY)
-foreach( INDEX RANGE ${COUNT})
-  list (GET ENV_REPLACE_LIST ${INDEX} TO_REPLACE)
-  list (GET ENV_FIND_LIST ${INDEX} TO_FIND)
-  message(STATUS "Replacing ${TO_FIND} to  ${TO_REPLACE} ")
-  string(REPLACE ${TO_FIND} ${TO_REPLACE} THIRDY_PARTY "${THIRDY_PARTY}")
-endforeach()
-
-file(WRITE "${SOURCE_PATH}/thirdparty.inc" "${THIRDY_PARTY}")
-
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+  set(BUILD_STATIC_LIBRARY ON)
+else()
+  set(BUILD_STATIC_LIBRARY OFF)
+endif()
 
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
   OPTIONS
   -DGIT_EXECUTABLE=${GIT}
   -DGFLAGS=1
@@ -80,7 +38,12 @@ vcpkg_configure_cmake(
   -DLZ4=1
   -DZLIB=1
   -DCURRENT_INSTALLED_DIR=${CURRENT_INSTALLED_DIR}
+  -DBUILD_STATIC_LIBRARY=${BUILD_STATIC_LIBRARY}
+  -DFAIL_ON_WARNINGS=OFF
+  OPTIONS_DEBUG
+  -DDISABLE_INSTALL_HEADERS=ON
 )
 
+vcpkg_install_cmake(DISABLE_PARALLEL)
 
-vcpkg_build_cmake()
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/rocksdb RENAME copyright)
