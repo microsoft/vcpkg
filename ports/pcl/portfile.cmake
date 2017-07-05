dc2da6dff4dd65efe9bb6a088aa59507678ae0fb
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2017-07-06 00:30:18 +0900

    Update PCL 1.8.1 RC1
    Update PCL 1.8.1 RC1

--- ports/pcl/portfile.cmake
+++ ports/pcl/portfile.cmake
@@ -11,13 +11,14 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/pcl-pcl-1.8.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.8.0.zip"
-    FILENAME "pcl-1.8.0.zip"
-    SHA512 932f7e2101707003712e53d9310c6ba8304b8d325997a71a45d052c329cd9465f1d390c6c53a11bcb01d65e808c7701452ea06f116a0bd779d8098fdf3246ca8
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO PointCloudLibrary/pcl
+    REF pcl-1.8.1rc1
+    SHA512 c719f7ff8cc5be3cfb5f01c89727e94858ed0cf5d50e2b884599e4a5b289564b4e2843979406b62adee1f2cee7332195dcd6219e99accef5700f3119758eb53f
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
@@ -26,13 +27,6 @@ vcpkg_apply_patches(
             "${CMAKE_CURRENT_LIST_DIR}/find_flann.patch"
             "${CMAKE_CURRENT_LIST_DIR}/find_qhull.patch"
             "${CMAKE_CURRENT_LIST_DIR}/find_openni2.patch"
-            # Fix for PCL 1.8.0
-            "${CMAKE_CURRENT_LIST_DIR}/1635.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/1788.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/1823.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/1830.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/1855.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/1856.patch"
 )
 
 if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
@@ -50,6 +44,7 @@ vcpkg_configure_cmake(
         -DBUILD_tools=OFF
         # PCL
         -DPCL_BUILD_WITH_BOOST_DYNAMIC_LINKING_WIN32=${CRT_LINKAGE}
+        -DPCL_BUILD_WITH_FLANN_DYNAMIC_LINKING_WIN32=${CRT_LINKAGE}
         -DPCL_SHARED_LIBS=${CRT_LINKAGE}
         # WITH
         -DWITH_CUDA=OFF
@@ -64,7 +59,6 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/pcl_2d_release.dll ${CURRENT_PACKAGES_DIR}/debug/bin/pcl_2d_debug.dll)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/pcl)
