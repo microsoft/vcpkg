ce69ed555265ec85fd4c24a44600354b94930758
Robert Schumacher <roschuma@microsoft.com>
2017-07-07 23:11:46 -0700

    [libconfig] Simplify CMake. Move header patching into the portfile.

--- ports/libconfig/portfile.cmake
+++ ports/libconfig/portfile.cmake
@@ -26,6 +26,21 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+foreach(FILE ${CURRENT_PACKAGES_DIR}/include/libconfig.h++ ${CURRENT_PACKAGES_DIR}/include/libconfig.h)
+  file(READ ${FILE} _contents)
+  string(REPLACE "defined(LIBCONFIGXX_EXPORTS)" "0" _contents "${_contents}")
+  string(REPLACE "defined(LIBCONFIG_EXPORTS)" "0" _contents "${_contents}")
+
+  if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    string(REPLACE "defined(LIBCONFIGXX_STATIC)" "0" _contents "${_contents}")
+    string(REPLACE "defined(LIBCONFIG_STATIC)" "0" _contents "${_contents}")
+  else()
+    string(REPLACE "defined(LIBCONFIGXX_STATIC)" "1" _contents "${_contents}")
+    string(REPLACE "defined(LIBCONFIG_STATIC)" "1" _contents "${_contents}")
+  endif()
+  file(WRITE ${FILE} "${_contents}")
+endforeach()
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libconfig RENAME copyright)
 
 vcpkg_copy_pdbs()
