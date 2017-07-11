afbe116fb7d6a39a8e3d8e0714712dffd11d691c
atkawa7 <atkawa7@yahoo.com>
2017-07-10 20:45:34 -0700

    [apr-util] use find_library

--- ports/apr-util/portfile.cmake
+++ ports/apr-util/portfile.cmake
@@ -1,5 +1,4 @@
 include(vcpkg_common_functions)
-include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/apr-util-1.6.0)
 vcpkg_download_distfile(ARCHIVE
   URLS "https://archive.apache.org/dist/apr/apr-util-1.6.0-win32-src.zip"
@@ -8,34 +7,29 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-set(APR_HOME ${VCPKG_ROOT_DIR}/packages/apr_${TARGET_TRIPLET})
-set(EXPAT_HOME ${VCPKG_ROOT_DIR}/packages/expat_${TARGET_TRIPLET})
-set(EXPAT_LIB ${EXPAT_HOME}/lib/expat.lib)
-set(EXPAT_LIB_DEBUG ${EXPAT_HOME}/debug/lib/expat.lib)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  set(BUILD_SHARED_LIBRARY ON)
-else()
-  set(BUILD_SHARED_LIBRARY OFF)
-endif()
 
 vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/use-vcpkg-expat.patch"
+  SOURCE_PATH ${SOURCE_PATH}
+  PATCHES "${CMAKE_CURRENT_LIST_DIR}/use-vcpkg-expat.patch"
 )
 
 
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-  -DAPR_HOME=${APR_HOME}
-  -DEXPAT_LIB=${EXPAT_LIB}
-  -DEXPAT_HOME=${EXPAT_HOME}
-  -DBUILD_SHARED_LIBRARY=${BUILD_SHARED_LIBRARY}
-  OPTIONS_DEBUG -DEXPAT_LIB=${EXPAT_LIB_DEBUG} -DDISABLE_INSTALL_HEADERS=ON
+  PREFER_NINJA
+  OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON
 )
 
 vcpkg_install_cmake()
+
+
+file(READ ${CURRENT_PACKAGES_DIR}/include/apu.h  APU_H)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  string(REPLACE "defined(APU_DECLARE_EXPORT)" "1" APU_H "${APU_H}")
+else()
+  string(REPLACE "defined(APU_DECLARE_STATIC)" "1" APU_H "${APU_H}")
+endif()
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/apu.h "${APU_H}")
 
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/apr-util RENAME copyright)
