062883535b5900cf5169b8f1494d872403656ae5
codicodi <rob.ceglinski@gmail.com>
2017-07-13 17:21:30 +0200

    [cairo] update to 1.15.6

--- ports/cairo/portfile.cmake
+++ ports/cairo/portfile.cmake
@@ -7,33 +7,28 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cairo-1.15.4)
+set(CAIRO_VERSION 1.15.6)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/cairo-${CAIRO_VERSION})
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://cairographics.org/snapshots/cairo-1.15.4.tar.xz"
-    FILENAME "cairo-1.15.4.tar.xz"
-    SHA512 ac3e6879fcf0876bca9f801cdf9e970ef1822644228cdd21962d0bf5db5fc074973f4ae651eb9c76b44fffd405cf0a0c7cbb228dba96b835ea137a2740277ee9
+    URLS "http://cairographics.org/snapshots/cairo-${CAIRO_VERSION}.tar.xz"
+    FILENAME "cairo-${CAIRO_VERSION}.tar.xz"
+    SHA512 e4eccba745a03545001ebd6f1a7b87bee4b932f80a2d877a764500ad5c18011a5bac585f31eec6d0b65ccaa818de0df2b3b99f0792111363bd3554cfa7f688b6
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES "${CMAKE_CURRENT_LIST_DIR}/Initialize-mutexes-for-static-builds-for-win32.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/export-only-in-shared-build.patch"
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists_cairo.txt DESTINATION ${SOURCE_PATH}/src)
-file(RENAME ${SOURCE_PATH}/src/CMakeLists_cairo.txt ${SOURCE_PATH}/src/CMakeLists.txt)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH}/src)
 file(COPY ${CURRENT_PORT_DIR}/cairo-features.h DESTINATION ${SOURCE_PATH}/src)
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-    )
-elseif (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-    )
-endif()
+vcpkg_configure_cmake(
+    PREFER_NINJA
+    SOURCE_PATH ${SOURCE_PATH}/src
+)
 
 vcpkg_install_cmake()
 
