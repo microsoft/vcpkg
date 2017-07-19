40d2e35ca6fdf443a1b2b4d7aedf0a93cba47041
Robert Schumacher <roschuma@microsoft.com>
2017-07-18 17:20:01 -0700

    [duktape] Stamp headers with the correct DUK_F_DLL_BUILD value.

--- ports/duktape/portfile.cmake
+++ ports/duktape/portfile.cmake
@@ -8,13 +8,10 @@ vcpkg_download_distfile(ARCHIVE_FILE
 )
 vcpkg_extract_source_archive(${ARCHIVE_FILE})
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    # Patch duk_config.h to remove 'undef DUK_F_DLL_BUILD'
-    vcpkg_apply_patches(
-        SOURCE_PATH ${SOURCE_PATH}
-        PATCHES "${CMAKE_CURRENT_LIST_DIR}/duk_config.h.patch"
-    )
-endif()
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/duk_config.h.patch"
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${CMAKE_PATH}
@@ -23,11 +20,13 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-# Revert duk_config.h to its original state
-# This is always done in case the file has already been previously patched
 set(DUK_CONFIG_H_PATH "${CURRENT_PACKAGES_DIR}/include/duk_config.h")
 file(READ ${DUK_CONFIG_H_PATH} CONTENT)
-string(REPLACE "// #undef DUK_F_DLL_BUILD" "#undef DUK_F_DLL_BUILD" CONTENT "${CONTENT}")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    string(REPLACE "// #undef DUK_F_DLL_BUILD" "#undef DUK_F_DLL_BUILD\n#define DUK_F_DLL_BUILD 1" CONTENT "${CONTENT}")
+else()
+    string(REPLACE "// #undef DUK_F_DLL_BUILD" "#undef DUK_F_DLL_BUILD\n#define DUK_F_DLL_BUILD 0" CONTENT "${CONTENT}")
+endif()
 file(WRITE ${DUK_CONFIG_H_PATH} "${CONTENT}")
 
 # Remove debug include
