148a6e0a5b205e8e83a95d48a996d8d07b50a9cd
atkawa7 <atkawa7@yahoo.com>
2017-07-25 17:01:55 -0700

    [libkml] cmake fixup

--- ports/libkml/portfile.cmake
+++ ports/libkml/portfile.cmake
@@ -12,6 +12,8 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cxxopts RENAME copyright)
-
 vcpkg_copy_pdbs()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libkml RENAME copyright)
