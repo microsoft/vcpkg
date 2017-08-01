cc822ead1cce550a0f0f77f39a14c02b43244276
Robert Schumacher <roschuma@microsoft.com>
2017-08-01 15:23:45 -0700

    [tacopie][cpp-redis] Update cpp-redis to depend on tacopie. Fix tacopie port.

--- ports/cpp-redis/portfile.cmake
+++ ports/cpp-redis/portfile.cmake
@@ -7,10 +7,12 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Cylix/cpp_redis
-    REF 3.5.1
-    SHA512  2c50cf777d5955f7bcb94a55514fac444d0dcacc2df343dd89969889be7653a793620dbaac9d6dd0f444eee7f0664c4eb96a1d83477d207143660764afeea129
+    REF 3.5.2
+    SHA512  d19445c93fad9fba39c7aed07b2d196ec0c96366324a2a2ee856c930683b5428fe8b5bc46de4b2b23112aae83f8229a4703c2355d1c74831602ec3a7f8dac315
     HEAD_REF master
 )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/tacopie/CMakeLists.txt DESTINATION ${SOURCE_PATH}/tacopie)
 
 if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
     set(MSVC_RUNTIME_LIBRARY_CONFIG "/MD")
@@ -19,14 +21,13 @@ else()
 endif()
 
 # cpp-redis forcibly removes "/RTC1" in its cmake file. Because this is an ABI-sensitive flag, we need to re-add it in a form that won't be detected.
-list(APPEND VCPKG_CXX_FLAGS_DEBUG "-RTC1")
-list(APPEND VCPKG_C_FLAGS_DEBUG "-RTC1")
+set(VCPKG_CXX_FLAGS_DEBUG "${VCPKG_CXX_FLAGS_DEBUG} -RTC1")
+set(VCPKG_C_FLAGS_DEBUG "${VCPKG_C_FLAGS_DEBUG} -RTC1")
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DUSE_CUSTOM_TCP_CLIENT=TRUE
         -DMSVC_RUNTIME_LIBRARY_CONFIG=${MSVC_RUNTIME_LIBRARY_CONFIG}
 )
 
@@ -37,7 +38,7 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bi
 file(GLOB_RECURSE FILES "${CURRENT_PACKAGES_DIR}/include/*")
 foreach(file ${FILES})
     file(READ ${file} _contents)
-    string(REPLACE "ifndef __CPP_REDIS_USE_CUSTOM_TCP_CLIENT" "if 0" _contents "${_contents}")
+    string(REPLACE "ifndef __CPP_REDIS_USE_CUSTOM_TCP_CLIENT" "if 1" _contents "${_contents}")
     file(WRITE ${file} "${_contents}")
 endforeach()
 
