7399ea0d6ee6095266c8aa7d4c7706c53e30e5d1
Robert Schumacher <roschuma@microsoft.com>
2017-08-08 15:10:54 -0700

    [jemalloc] Update to use vcpkg_from_github. Avoid adding config scripts that are not present in upstream. Rename DLLs to 'jemalloc' to match expectations elsewhere.

--- ports/jemalloc/portfile.cmake
+++ ports/jemalloc/portfile.cmake
@@ -5,51 +5,29 @@ endif()
 
 include(vcpkg_common_functions)
 
-set(GIT_URL "https://github.com/jemalloc/jemalloc-cmake.git")
-set(GIT_REF "jemalloc-cmake.4.3.1")
-
-if(NOT EXISTS "${DOWNLOADS}/jemalloc-cmake.git")
-    message(STATUS "Cloning")
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} clone --bare ${GIT_URL} ${DOWNLOADS}/jemalloc-cmake.git
-        WORKING_DIRECTORY ${DOWNLOADS}
-        LOGNAME clone
-    )
-endif()
-
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
-
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-    message(STATUS "Adding worktree")
-    file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR})
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} worktree add -f --detach ${SOURCE_PATH} ${GIT_REF}
-        WORKING_DIRECTORY ${DOWNLOADS}/jemalloc-cmake.git
-        LOGNAME worktree
-    )
-    message(STATUS "Patching")
-    vcpkg_apply_patches(
-        SOURCE_PATH ${SOURCE_PATH}
-        PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-cmakelists.patch"
-        PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-utilities.patch"
-    )
-endif()
-
-# jemalloc uses git to get it version
-find_program(GIT NAMES git git.cmd)
-get_filename_component(GIT_EXE_PATH ${GIT} DIRECTORY)
-set(ENV{PATH} "${GIT_EXE_PATH};$ENV{PATH}")
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jemalloc/jemalloc-cmake
+    REF jemalloc-cmake.4.3.1
+    SHA512 e94b62ec3a53acc0ab5acb247d7646bc172108e80f592bb41c2dd50d181cbbeb33d623adf28415ffc0a0e2de3818af2dfe4c04af75ac891ef5042bc5bb186886
+    HEAD_REF master
+)
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        "${CMAKE_CURRENT_LIST_DIR}/fix-cmakelists.patch"
+        "${CMAKE_CURRENT_LIST_DIR}/fix-utilities.patch"
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS -DGIT_FOUND=OFF -DCMAKE_DISABLE_FIND_PACKAGE_Git=ON
 )
 
 vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
-
-vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
