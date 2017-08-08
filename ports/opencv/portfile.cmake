17033cc0c63d913a56f8c66c97cc73d2d4372b63
jasjuang <jasjuang@gmail.com>
2017-08-08 14:26:13 -0700

    update opencv to 3.3.0

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -3,11 +3,11 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/opencv-3.2.0)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/opencv-3.3.0)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/opencv/opencv/archive/3.2.0.zip"
-    FILENAME "opencv-3.2.0.zip"
-    SHA512 c6418d2a7654fe9d50611e756778df4c6736f2de76b85773efbf490bb475dd95ec1041fe57a87163ce11a7db44430cd378c8416af3319f979ced92532bf5ebb5
+    URLS "https://github.com/opencv/opencv/archive/3.3.0.zip"
+    FILENAME "opencv-3.3.0.zip"
+    SHA512 14430c6225926e5118daccb57c7276d9f9160c90a034b2c73a09b73ac90ba7ebd3ae78cccffb4a10b58bb0e5e16ebd03bf617030fa74cc67d9d18366bf6b4951
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
@@ -19,19 +19,19 @@ vcpkg_apply_patches(
 )
 file(REMOVE_RECURSE ${SOURCE_PATH}/3rdparty/libjpeg ${SOURCE_PATH}/3rdparty/libpng ${SOURCE_PATH}/3rdparty/zlib ${SOURCE_PATH}/3rdparty/libtiff)
 
-# Uncomment the following lines and the lines under OPTIONS to build opencv_contrib
-# Important: after uncommenting you've add protobuf dependency within CONTROL file
-#SET(CONTRIB_SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/opencv_contrib-3.2.0)
-#vcpkg_download_distfile(CONTRIB_ARCHIVE
-#    URLS "https://github.com/opencv/opencv_contrib/archive/3.2.0.zip"
-#    FILENAME "opencv_contrib-3.2.0.zip"
-#    SHA512 da6cda7a7ae1d722967e18f9b8d60895b93bbc3664dfdb1645cb4d8b337a9c4207b9073fd546a596c48a489f92d15191aa34c7c607167b536fbe4937b8424b43
-#)
-#vcpkg_extract_source_archive(${CONTRIB_ARCHIVE})
-#vcpkg_apply_patches(
-#    SOURCE_PATH ${CONTRIB_SOURCE_PATH}
-#    PATCHES "${CMAKE_CURRENT_LIST_DIR}/open_contrib-remove-waldboost.patch"
-#)
+# Comment out the following 11 lines if you don't want to build with opencv_contrib
+# Important: remember to also update the CONTROL file
+SET(CONTRIB_SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/opencv_contrib-3.3.0)
+vcpkg_download_distfile(CONTRIB_ARCHIVE
+   URLS "https://github.com/opencv/opencv_contrib/archive/3.3.0.zip"
+   FILENAME "opencv_contrib-3.3.0.zip"
+   SHA512 1c76d49689459708117acfbd0893cbfb915fbd0defff95702fb388a29d12b50fb53fbf246e64e68aa3adb347aa45ff478df5e2e8c6d9cfa57a628744bbb1bd04
+)
+vcpkg_extract_source_archive(${CONTRIB_ARCHIVE})
+vcpkg_apply_patches(
+   SOURCE_PATH ${CONTRIB_SOURCE_PATH}
+   PATCHES "${CMAKE_CURRENT_LIST_DIR}/open_contrib-remove-waldboost.patch"
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -60,11 +60,12 @@ vcpkg_configure_cmake(
         -DWITH_CUBLAS=OFF
         -DWITH_OPENCLAMDBLAS=OFF
         -DWITH_LAPACK=OFF
+        -DBUILD_opencv_dnn=OFF
 
-        # uncomment the following 3 lines to build opencv_contrib modules
-        #-DOPENCV_EXTRA_MODULES_PATH=${CONTRIB_SOURCE_PATH}/modules
-        #-DBUILD_PROTOBUF=OFF
-        #-DUPDATE_PROTO_FILES=ON
+        # comment the following 3 lines if you don't want to build opencv_contrib modules
+        -DOPENCV_EXTRA_MODULES_PATH=${CONTRIB_SOURCE_PATH}/modules
+        -DBUILD_PROTOBUF=OFF
+        -DUPDATE_PROTO_FILES=ON
     OPTIONS_DEBUG
         -DINSTALL_HEADERS=OFF
         -DINSTALL_OTHER=OFF
@@ -72,13 +73,12 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(READ ${CURRENT_PACKAGES_DIR}/debug/share/opencv/OpenCVModules-debug.cmake OPENCV_DEBUG_MODULE)
-string(REPLACE "\${_IMPORT_PREFIX}" "\${_IMPORT_PREFIX}/debug" OPENCV_DEBUG_MODULE "${OPENCV_DEBUG_MODULE}")
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules-debug.cmake "${OPENCV_DEBUG_MODULE}")
-
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/opencv)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/opencv/LICENSE ${CURRENT_PACKAGES_DIR}/share/opencv/copyright)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/LICENSE)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/LICENSE)
 
 vcpkg_copy_pdbs()
