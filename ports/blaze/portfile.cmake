e5ec89e10295744755358f0ba2b591a59315a6df
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-08-20 05:56:02 +0200

    [blaze] update to Blaze 3.2

--- ports/blaze/portfile.cmake
+++ ports/blaze/portfile.cmake
@@ -1,11 +1,11 @@
 #header-only library
 include(vcpkg_common_functions)
 
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/blaze-3.1)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/blaze-3.2)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://bitbucket.org/blaze-lib/blaze/downloads/blaze-3.1.tar.gz"
-    FILENAME "blaze-3.1.tar.gz"
-    SHA512 fe03a7615d4105d6a869cfd69b3db3165b838eff53cdff7adbbd5ae9d753aa009bbab50925463c6704f9530a4c4ad5605e373b3cbaee96ca982a474a665ed756
+    URLS "https://bitbucket.org/blaze-lib/blaze/downloads/blaze-3.2.tar.gz"
+    FILENAME "blaze-3.2.tar.gz"
+    SHA512 33d2bb0a49a33e71c88a45ab9e8418160c09b877b3ebe5ff7aa48ec0973e28e8a282374604d56f1b5cf2722946e4ca84aa2b401a341240a2ab9debd72505148e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
