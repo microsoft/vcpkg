3e6a19754bd56aa902583fde451110d3274ff5cd
glachancecmaisonneuve <glachance@cmaisonneuve.qc.ca>
2017-08-22 23:15:19 -0400

    no more renaming control version number was wrong (thank you for spotting that)

--- ports/unrar/portfile.cmake
+++ ports/unrar/portfile.cmake
@@ -32,19 +32,7 @@ file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/unrar.dll  DESTINAT
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/unrar.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/unrar.dll  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/unrar.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-#copy pdbs now, while filenames still match
 vcpkg_copy_pdbs()
-
-#RarLabs has historically distributed it's x64 architecture unrar library as UnRAR64.dll
-if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    file(RENAME ${CURRENT_PACKAGES_DIR}/bin/unrar.dll        ${CURRENT_PACKAGES_DIR}/bin/UnRAR64.dll)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/unrar.lib        ${CURRENT_PACKAGES_DIR}/lib/UnRAR64.lib)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/unrar.dll  ${CURRENT_PACKAGES_DIR}/debug/bin/UnRAR64.dll)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/unrar.lib  ${CURRENT_PACKAGES_DIR}/debug/lib/UnRAR64.lib)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/bin/unrar.pdb        ${CURRENT_PACKAGES_DIR}/bin/UnRAR64.pdb)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/unrar.pdb  ${CURRENT_PACKAGES_DIR}/debug/bin/UnRAR64.pdb)
-endif()
 
 #COPYRIGHT
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/unrar RENAME copyright)
