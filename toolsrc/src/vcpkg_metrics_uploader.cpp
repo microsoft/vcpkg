330b29732c8b178639f78c6fad10b9ec84f7669b
atkawa7 <atkawa7@yahoo.com>
2017-08-26 12:17:39 -0700

    [vcpkg] free memory CommandLineToArgvW

--- toolsrc/src/vcpkg_metrics_uploader.cpp
+++ toolsrc/src/vcpkg_metrics_uploader.cpp
@@ -13,4 +13,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
     Checks::check_exit(VCPKG_LINE_INFO, argCount == 2, "Requires exactly one argument, the path to the payload file");
     auto v = Files::get_real_filesystem().read_contents(szArgList[1]).value_or_exit(VCPKG_LINE_INFO);
     Metrics::g_metrics.lock()->upload(v);
+    LocalFree(szArgList);
+    return 0;
 }
