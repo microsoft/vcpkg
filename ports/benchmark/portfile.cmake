a3ab5251232ff571a6ec503ad936166eb2cb3953
Robert Schumacher <roschuma@microsoft.com>
2017-09-21 01:20:51 -0700

    [benchmark] Reset to latest stable, pulling back important patch.

--- ports/benchmark/portfile.cmake
+++ ports/benchmark/portfile.cmake
@@ -12,16 +12,28 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/benchmark
-    REF 6d8339dd97afea4633e54ed4b42307aff4386040
-    SHA512 d9b67ad9876c99102668364e89041bda24090aca39335155624183412d8e8c9e7a9f0585af859c0380af39a3ce40f6db1601bb3397a584f20edca760b31188a2
+    REF v1.2.0
+    SHA512 859063669fd84e847f04624013f0b2b734d75d90cada247682eaf345b86c88a9bc2320250e128f2361e37f402b3fb56a18c493ec6038973744a005a452d693ba
     HEAD_REF master
+)
+
+vcpkg_download_distfile(PATCH
+    URLS "https://github.com/efcs/benchmark/commit/536b0b82b8ec12fc7e17e6d243633618f294a739.diff"
+    FILENAME google-benchmark-1.2.0-536b0b82.patch
+    SHA512 ed42cc0014741c8039c0fca5b4317b2ed09d06a25c91f49a48be6dce921e39469b002c088794c1ea73dc759166e20cb685b47f809ba28dddd95b5f3263be03cd
+)
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        "${PATCH}"
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-    	-DBENCHMARK_ENABLE_TESTING=OFF
+        -DBENCHMARK_ENABLE_TESTING=OFF
 )
 
 vcpkg_install_cmake()
