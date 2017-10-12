9ddc98988fb8d7c886963fa7568395ecd5785eaa
Robert Schumacher <roschuma@microsoft.com>
2017-10-12 10:52:00 -0700

    [vcpkg-scripts] Detect and prompt for proxy in more cases

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -4,10 +4,6 @@ param(
     [string]$disableMetrics = "0"
 )
 
-$webclient=New-Object System.Net.WebClient
-$creds=Get-Credential
-$webclient.Proxy.Credentials=$creds
-
 $scriptsDir = split-path -parent $MyInvocation.MyCommand.Definition
 $vcpkgRootDir = & $scriptsDir\findFileRecursivelyUp.ps1 $scriptsDir .vcpkg-root
 
