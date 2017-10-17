f5ff89b671309d1788178a1f4fe47a69648e3ec9
xoviat <xoviat@users.noreply.github.com>
2017-10-16 21:32:04 -0500

    FIX: use the correct function arguments

--- ports/arb/portfile.cmake
+++ ports/arb/portfile.cmake
@@ -2,10 +2,10 @@ include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/arb-2.11.1)
 vcpkg_from_github(
     OUT_SOURCE_PATH ${SOURCE_PATH}
-    REPO <fredrik-johansson/arb>
-    [REF <v2.11.1>]
-    [SHA512 <45d0d7f8cc350...>]
-    [HEAD_REF <master>]
+    REPO fredrik-johansson/arb
+    REF v2.11.1
+    SHA512 45d0d7f8cc350
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
