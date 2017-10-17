5d9984aa8e4a8ea8bbf4022d69c76adbc3466a00
xoviat <xoviat@users.noreply.github.com>
2017-10-16 22:35:54 -0500

    FIX: copy build files

--- ports/arb/portfile.cmake
+++ ports/arb/portfile.cmake
@@ -10,6 +10,8 @@ vcpkg_from_github(
 
 file(REMOVE ${SOURCE_PATH}/CMakeLists.txt)
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/gettimeofday.c DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/gettimeofday.h DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
