886bc2965d0f7fa5d30121cf28aef1265279494a
Mateusz Adamczyk <matek_a@outlook.com>
2017-10-24 20:57:31 +0200

    Use unicode

--- ports/atkmm/portfile.cmake
+++ ports/atkmm/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/fix_properties.patch
+    PATCHES ${CMAKE_CURRENT_LIST_DIR}/fix_properties.patch ${CMAKE_CURRENT_LIST_DIR}/fix_charset.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/msvc_recommended_pragmas.h DESTINATION ${SOURCE_PATH}/MSVC_Net2013)
