da04e942f2878b360f146a2751480c3133e3b817
Robert Schumacher <roschuma@microsoft.com>
2017-10-27 23:55:48 -0700

    [boost] Only define BOOST_ALL_NO_LIB if not already defined. Fixes #2061.

--- ports/boost/portfile.cmake
+++ ports/boost/portfile.cmake
@@ -258,7 +258,7 @@ file(
 
 # Disable Boost auto-link.
 file(APPEND ${CURRENT_PACKAGES_DIR}/include/boost/config/user.hpp
-	"\n#define BOOST_ALL_NO_LIB\n"
+    "\n#ifndef BOOST_ALL_NO_LIB\n#define BOOST_ALL_NO_LIB\n#endif\n"
 )
 file(APPEND ${CURRENT_PACKAGES_DIR}/include/boost/config/user.hpp
     "\n#undef BOOST_ALL_DYN_LINK\n"
