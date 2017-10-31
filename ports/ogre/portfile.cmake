f4b09f5782eeeb336d5f5a558aa0006e2cc40327
LazyHamster <ariman@inbox.ru>
2017-10-31 12:50:23 +0300

    [ogre] Update port to Ogre version 1.10.8.

--- ports/ogre/portfile.cmake
+++ ports/ogre/portfile.cmake
@@ -1,18 +1,14 @@
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/sinbad-ogre-dd30349ea667)
+
+set(OGRE_VERSION 1.10.8)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ogre-${OGRE_VERSION})
+
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://bitbucket.org/sinbad/ogre/get/v1-9-0.zip"
-    FILENAME "ogre-v1-9-0.zip"
-    SHA512 de7315a2450ecf0d9073e6a8f0c54737e041016f7ad820556d10701c7d23eefab9d3473476a8e95447c30ab21518b8e4cfb0271db72494ea67a3dea284c9a3d3
+    URLS "https://github.com/OGRECave/ogre/archive/v1.10.8.zip"
+    FILENAME "ogre-1.10.8.zip"
+    SHA512 c7d962fe7fb8c46a4e15bb6e2bb68c67f0cc2a0d04a8f53e03fb9572c76df3679dcd117137c6624f2f56a8eda108723817dbaa616ecb7dc4cfd6a644a6bc4356
 )
 vcpkg_extract_source_archive(${ARCHIVE})
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-use-vcpkg-freeimage.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/0002-ogre-cmake-dir-as-option.patch"
-            "${CMAKE_CURRENT_LIST_DIR}/0003-use-flat-installation.patch"
-)
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
     set(OGRE_STATIC ON)
@@ -20,41 +16,68 @@ else()
     set(OGRE_STATIC OFF)
 endif()
 
+# Configure features
+
+if("d3d9" IN_LIST FEATURES)
+    set(WITH_D3D9 ON)
+else()
+    set(WITH_D3D9 OFF)
+endif()
+
+if("java" IN_LIST FEATURES)
+    set(WITH_JAVA ON)
+else()
+    set(WITH_JAVA OFF)
+endif()
+
+if("python" IN_LIST FEATURES)
+    set(WITH_PYTHON ON)
+else()
+    set(WITH_PYTHON OFF)
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS -DOGRE_USE_BOOST:BOOL=OFF 
-            -DOGRE_BUILD_RENDERSYSTEM_D3D9:BOOL=OFF
-            -DOGRE_INSTALL_DEPENDENCIES:BOOL=OFF
-            -DOGRE_COPY_DEPENDENCIES:BOOL=OFF
-            -DOGRE_BUILD_TOOLS:BOOL=OFF
-            -DOGRE_CMAKE_DIR:STRING=share/ogre
-            -DOGRE_STATIC:BOOL=${OGRE_STATIC}
-            -DOGRE_INSTALL_SAMPLES:BOOL=OFF
-            -DOGRE_INSTALL_TOOLS:BOOL=OFF
-            # We disable this option because it is broken and we rely on vcpkg_copy_pdbs
-            -DOGRE_INSTALL_PDB:BOOL=OFF
-            -DOGRE_BUILD_DOCS:BOOL=OFF
-            -DOGRE_INSTALL_DOCS:BOOL=OFF
-            -DOGRE_INSTALL_SAMPLES_SOURCE:BOOL=OFF
-            -DOGRE_NO_INSTALLATION_SUFFIXES_ON_WIN32:BOOL=ON
+    PREFER_NINJA
+    OPTIONS
+        -DOGRE_BUILD_DEPENDENCIES=OFF
+        -DOGRE_BUILD_SAMPLES=OFF
+        -DOGRE_BUILD_TESTS=OFF
+        -DOGRE_BUILD_TOOLS=OFF
+        -DOGRE_BUILD_MSVC_MP=ON
+        -DOGRE_BUILD_MSVC_ZM=ON
+        -DOGRE_INSTALL_DEPENDENCIES=OFF
+        -DOGRE_INSTALL_DOCS=OFF
+        -DOGRE_INSTALL_PDB=OFF
+        -DOGRE_INSTALL_SAMPLES=OFF
+        -DOGRE_INSTALL_TOOLS=OFF
+        -DOGRE_INSTALL_CMAKE=ON
+        -DOGRE_INSTALL_VSPROPS=OFF
+        -DOGRE_STATIC=${OGRE_STATIC}
+        -DOGRE_UNITY_BUILD=OFF
+        -DOGRE_USE_STD11=ON
+        -DOGRE_NODE_STORAGE_LEGACY=OFF
+        -DOGRE_BUILD_RENDERSYSTEM_D3D11=ON
+        -DOGRE_BUILD_RENDERSYSTEM_GL=ON
+        -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON
+        -DOGRE_BUILD_RENDERSYSTEM_GLES=OFF
+        -DOGRE_BUILD_RENDERSYSTEM_GLES2=OFF
+# Optional stuff
+        -DOGRE_BUILD_COMPONENT_JAVA=${WITH_JAVA}
+        -DOGRE_BUILD_COMPONENT_PYTHON=${WITH_PYTHON}
+        -DOGRE_BUILD_RENDERSYSTEM_D3D9=${WITH_D3D9}
 )
 
 vcpkg_install_cmake()
 
-# Add a OGREConfig.cmake to simplify the process of finding vcpkg OGRE
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/OGREConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/ogre)
-
-# Remove debug includes
+# Remove unwanted files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-# Remove debug CMake files 
-# Note that at the moment OGRE do not export imported targets, 
-# so we do not need to copy the debug imported targets in the 
-# release CMake path
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/CMake)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/ogre)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/ogre/COPYING ${CURRENT_PACKAGES_DIR}/share/ogre/copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/ogre RENAME copyright)
+
+# Move installed CMake scripts to share folder
+file(RENAME ${CURRENT_PACKAGES_DIR}/CMake ${CURRENT_PACKAGES_DIR}/share/ogre/CMake)
 
 vcpkg_copy_pdbs()
