ed418874384c0f8e43cb7d5ac517fdb75d6473c2
jasjuang <jasjuang@gmail.com>
2017-11-03 22:47:41 -0700

    [dlib] update to 19.7 and use vcpkg_from_github

--- ports/dlib/portfile.cmake
+++ ports/dlib/portfile.cmake
@@ -1,18 +1,13 @@
-# Common Ambient Variables:
-#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
-#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
-#   PORT is the current port name (zlib, etc)
-#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dlib-19.4)
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://dlib.net/files/dlib-19.4.tar.bz2"
-    FILENAME "dlib-19.4.tar.bz2"
-    SHA512 c5ae22c507b57a13d880d79e9671730829114d0276508b0a41b373d3abae9057d960fce84fafe1be468d943910853baaa70c88f2516e20a0c41f3895bf217f7b
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO davisking/dlib
+    REF v19.7
+    SHA512 a3877066e04a411d96e910f4229c60a86971a9290e840aa4a5b2f0b102e9b8c37bfede259b80b71ba066d21eb0aa2565808e51d0eab6397ff5fd2bac60dcedd5
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+
 file(REMOVE_RECURSE ${SOURCE_PATH}/dlib/external/libjpeg)
 file(REMOVE_RECURSE ${SOURCE_PATH}/dlib/external/libpng)
 file(REMOVE_RECURSE ${SOURCE_PATH}/dlib/external/zlib)
@@ -63,6 +58,6 @@ string(REPLACE "#define DLIB_DISABLE_ASSERTS" "#if defined(NDEBUG)\n#define DLIB
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/dlib/config.h ${_contents})
 
 # Handle copyright
-file(COPY ${CURRENT_PACKAGES_DIR}/share/doc/dlib/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dlib)
+file(COPY ${SOURCE_PATH}/dlib/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/dlib)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/dlib/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/dlib/COPYRIGHT)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/doc)
