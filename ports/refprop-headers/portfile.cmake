429e189e83cd4ecbe1596dd86630ac7b9442e5d8
xoviat <xoviat@users.noreply.github.com>
2017-11-07 11:03:34 -0600

    fix

--- ports/refprop-headers/portfile.cmake
+++ ports/refprop-headers/portfile.cmake
@@ -4,7 +4,7 @@ set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/refprop-headers-${PORT_VERSION})
 
 vcpkg_from_github(
     OUT_SOURCE_PATH ${SOURCE_PATH}
-    REPO CoolProp/IF97
+    REPO CoolProp/REFPROP-headers
     REF 882aec454b2bc3d5323b8691736ff09c288f4ed6
     SHA512 23ee3df4ffe21b2d790efa27a1b8ea5fa4fce0a274d78e493a2d71043670420e19216f925d23d04f6139ca084a21b97028bd2547f3dbd00ffbb33d0c0bbfece5
     HEAD_REF master
