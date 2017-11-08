1f50421b43a5589cd8903fd5f60f990ae7f0625d
Chris Bond <cbond01@gmail.com>
2017-11-07 17:11:31 -0800

    updated control and port file for latest version of sqlite-modern-cpp

--- ports/sqlite-modern-cpp/portfile.cmake
+++ ports/sqlite-modern-cpp/portfile.cmake
@@ -1,20 +1,14 @@
 # header only
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/sqlite_modern_cpp-2.4)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/sqlite_modern_cpp-3.2)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/aminroosta/sqlite_modern_cpp/archive/v2.4.tar.gz"
-    FILENAME "sqlite_modern_cpp-2.4.tar.gz"
-    SHA512 99d8220c9dcbf7383c75ef8061bc792a4ea0b7e6e1290992f1604f66e77fcb5055af8c54c2d82b6a8d331359e2829d987b7528208f032f32699e1349296792db
+    URLS "https://github.com/aminroosta/sqlite_modern_cpp/archive/v3.2.tar.gz"
+    FILENAME "sqlite_modern_cpp-3.2.tar.gz"
+    SHA512 7e54cc41713247c9f6373a441854e8dace8e334e6ee29f870f11bc3fd3b53b5cff4e1a6d4c7e3cda33509b70a3f9a47363922a589b9a6d0730ce6dc0c884d878
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
 file(INSTALL ${SOURCE_PATH}/hdr/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
-# Handle copyright
-vcpkg_download_distfile(LICENSE
-	URLS https://raw.githubusercontent.com/aminroosta/sqlite_modern_cpp/1d7747fcbb16325ec6673477b06f0c780de24a27/License.txt
-	FILENAME "sqlite_modern_cpp-2.4-license-mit.txt"
-	SHA512 4ffc41d14902b37841463b9e9274537cb48523a7ab7e5fbbbd14a01820d141e367851b0496aa18546ddab96100e7381db7fc35621c795a97c3290b618e18a8bd
-)
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/sqlite-modern-cpp)
-file(INSTALL ${LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/sqlite-modern-cpp RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/sqlite-modern-cpp RENAME copyright)
