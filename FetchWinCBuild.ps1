7c1fd22d4980e9fe0a3f53751af1a0ee7b269b4f
Alexander Karatarakis <alkarata@microsoft.com>
2017-11-12 02:10:18 -0800

    Rename to avoid warning

--- FetchWinCBuild.ps1
+++ FetchWinCBuild.ps1
@@ -4,7 +4,7 @@ param(
     [string]$destinationRoot
 )
 
-function MyCopy-Item
+function MyCopyItem
 {
     param(
         [string]$fromPath,
@@ -21,21 +21,21 @@ $from = "\\vcfs\Builds\VS\feature_WinC\$buildnumber"
 $to = $destinationRoot -replace "\\$" # Remove potential trailing backslash
 
 Write-Host "Copying x86ret"
-MyCopy-Item  "$from\binaries.x86ret\bin\i386" "$to\bin\HostX86\x86"
-MyCopy-Item  "$from\binaries.x86ret\bin\x86_amd64" "$to\bin\HostX86\x64"
-MyCopy-Item  "$from\binaries.x86ret\bin\x86_arm" "$to\bin\HostX86\arm"
+MyCopyItem  "$from\binaries.x86ret\bin\i386" "$to\bin\HostX86\x86"
+MyCopyItem  "$from\binaries.x86ret\bin\x86_amd64" "$to\bin\HostX86\x64"
+MyCopyItem  "$from\binaries.x86ret\bin\x86_arm" "$to\bin\HostX86\arm"
 
 Write-Host "Copying amd64ret"
-MyCopy-Item  "$from\binaries.amd64ret\bin\amd64" "$to\bin\HostX64\x64"
-MyCopy-Item  "$from\binaries.amd64ret\bin\amd64_x86" "$to\bin\HostX64\x86"
-MyCopy-Item  "$from\binaries.amd64ret\bin\amd64_arm" "$to\bin\HostX64\arm"
+MyCopyItem  "$from\binaries.amd64ret\bin\amd64" "$to\bin\HostX64\x64"
+MyCopyItem  "$from\binaries.amd64ret\bin\amd64_x86" "$to\bin\HostX64\x86"
+MyCopyItem  "$from\binaries.amd64ret\bin\amd64_arm" "$to\bin\HostX64\arm"
 
 # Only copy files and directories that already exist in the VS installation.
 Write-Host "Copying inc, atlmfc, lib"
-MyCopy-Item  "$from\binaries.x86ret\inc" "$to\include"
-MyCopy-Item  "$from\binaries.x86ret\atlmfc" "$to\atlmfc"
-MyCopy-Item  "$from\binaries.x86ret\lib\i386" "$to\lib\x86"
-MyCopy-Item  "$from\binaries.amd64ret\lib\amd64" "$to\lib\x64"
+MyCopyItem  "$from\binaries.x86ret\inc" "$to\include"
+MyCopyItem  "$from\binaries.x86ret\atlmfc" "$to\atlmfc"
+MyCopyItem  "$from\binaries.x86ret\lib\i386" "$to\lib\x86"
+MyCopyItem  "$from\binaries.amd64ret\lib\amd64" "$to\lib\x64"
 
 # a = archive
 # -t7z = type is 7z
