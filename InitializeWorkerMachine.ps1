43a1566394ed8bfcd0188e529e0453c0c3ba97a1
Alexander Karatarakis <alkarata@microsoft.com>
2017-11-11 23:06:10 -0800

    Merge UnattendedVSInstall into InitializeWorkerMachine

--- InitializeWorkerMachine.ps1
+++ InitializeWorkerMachine.ps1
@@ -4,7 +4,7 @@ param
     [Parameter(Mandatory=$true)]
     [String]$PAT,
     [Parameter(Mandatory=$true)]
-    [String]$adminPassword
+    [String]$adminPass
 )
 
 $scriptsDir = split-path -parent $MyInvocation.MyCommand.Definition
@@ -31,54 +31,82 @@ function Recipe
     }
 }
 
+function Invoke-Executable()
+{
+    param ( [Parameter(Mandatory=$true)][string]$executable,
+                                        [string]$arguments = "",
+                                        [switch]$wait)
+
+    Write-Verbose "Executing: ${executable} ${arguments}"
+    $process = Start-Process -FilePath $executable -ArgumentList $arguments -PassThru
+    if ($wait)
+    {
+        Wait-Process -InputObject $process
+        $ec = $process.ExitCode
+        Write-Verbose "Execution terminated with exit code $ec."
+    }
+}
+
+function UnattendedVSinstall
+{
+    param(
+        [Parameter(Mandatory=$true)][string]$installPath,
+        [Parameter(Mandatory=$true)][string]$nickname
+    )
+
+    # References
+    # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
+    # https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community#desktop-development-with-c
+
+    $url = "https://aka.ms/vs/15/release/vs_community.exe"
+    $filename = "vs_Community.exe"
+
+    Recipe "$scriptsDir\$filename" {
+        $url = "https://aka.ms/vs/15/release/vs_community.exe"
+
+        Remove-Item "$filename.part" -Recurse -Force -ErrorAction SilentlyContinue
+        Start-BitsTransfer -Source $url -Destination "$filename.part" -ErrorAction Stop
+        Move-Item -Path "$filename.part" -Destination $filename -ErrorAction Stop
+    }
+
+    Write-Host "Updating VS Installer"
+    Invoke-Executable ".\$filename" "--update --quiet --wait --norestart" -wait:$true
+
+    Write-Host "Installing Visual Studio"
+    $arguments = ("--installPath $installPath",
+    "--add Microsoft.VisualStudio.Workload.NativeDesktop",
+    "--add Microsoft.VisualStudio.Workload.Universal",
+    "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+    "--add Microsoft.VisualStudio.Component.VC.Tools.ARM",
+    "--add Microsoft.VisualStudio.Component.VC.Tools.ARM64",
+    "--add Microsoft.VisualStudio.Component.VC.ATL",
+    "--add Microsoft.VisualStudio.Component.VC.ATLMFC",
+    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop",
+    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP",
+    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP.Native",
+    "--add Microsoft.VisualStudio.ComponentGroup.UWP.VC",
+    "--add Microsoft.Component.VC.Runtime.OSSupport",
+    "--nickname $nickname",
+    "--quiet",
+    "--wait",
+    "--norestart") -join " "
+
+    Invoke-Executable ".\$filename" "$arguments" -wait:$true
+}
+
 powercfg /SETACVALUEINDEX SCHEME_BALANCED SUB_SLEEP STANDBYIDLE 0
 
 $filename = "vs_Community.exe"
 
-Recipe "$scriptsDir\vs_Community.exe" {
-    $url = "https://aka.ms/vs/15/release/vs_community.exe"
+$unstablePath = "C:\VS2017\Unstable"
+$stablePath = "C:\VS2017\Stable"
 
-    Remove-Item "$filename.part" -Recurse -Force -ErrorAction SilentlyContinue
-    Start-BitsTransfer -Source $url -Destination "$filename.part" -ErrorAction Stop
-    Move-Item -Path "$filename.part" -Destination $filename -ErrorAction Stop
+Recipe $unstablePath {
+    UnattendedVSinstall -installPath $unstablePath -nickname "Unstable"
 }
 
-Recipe "C:\VS2017\Unstable" {
-    & ".\$filename" --installPath "C:\VS2017\Unstable" `
-    --add Microsoft.VisualStudio.Workload.NativeDesktop `
-    --add Microsoft.VisualStudio.Workload.Universal `
-    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-    --add Microsoft.VisualStudio.Component.VC.Tools.ARM `
-    --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 `
-    --add Microsoft.VisualStudio.Component.VC.ATL `
-    --add Microsoft.VisualStudio.Component.VC.ATLMFC `
-    --add Microsoft.Component.VC.Runtime.OSSupport `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP.Native `
-    --add Microsoft.Component.VC.Runtime.OSSupport `
-    --add Microsoft.VisualStudio.ComponentGroup.UWP.VC `
-    --nickname Unstable `
-    --passive
-}
-
-Recipe "C:\VS2017\Stable" {
-    & ".\$filename" --installPath "C:\VS2017\Stable" `
-    --add Microsoft.VisualStudio.Workload.NativeDesktop `
-    --add Microsoft.VisualStudio.Workload.Universal `
-    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
-    --add Microsoft.VisualStudio.Component.VC.Tools.ARM `
-    --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 `
-    --add Microsoft.VisualStudio.Component.VC.ATL `
-    --add Microsoft.VisualStudio.Component.VC.ATLMFC `
-    --add Microsoft.Component.VC.Runtime.OSSupport `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP `
-    --add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP.Native `
-    --add Microsoft.Component.VC.Runtime.OSSupport `
-    --add Microsoft.VisualStudio.ComponentGroup.UWP.VC `
-    --nickname Stable `
-    --passive
+Recipe $stablePath {
+    UnattendedVSinstall -installPath $stablePath -nickname "Stable"
 }
 
 Recipe "C:\vsts\_work" {
@@ -90,16 +118,16 @@ Recipe "C:\vsts\_work" {
         Recipe $file {
             $tmp = "$scriptsDir\vsts-agent-win7-x64-2.124.0.zip.tmp"
             $WC = New-Object System.Net.WebClient
-            rm $tmp
+            Remove-Item $tmp
             $WC.DownloadFile("https://github.com/Microsoft/vsts-agent/releases/download/v2.124.0/vsts-agent-win7-x64-2.124.0.zip", $tmp)
-            mv $tmp "$scriptsDir\vsts-agent-win7-x64-2.124.0.zip"
+            Move-Item $tmp "$scriptsDir\vsts-agent-win7-x64-2.124.0.zip"
         }
 
         Microsoft.PowerShell.Archive\Expand-Archive -path $file -destinationpath "C:\vsts" -ErrorAction Stop
 
     }
 
-    pushd "C:\vsts"
+    Push-Location "C:\vsts"
 
     & ".\config.cmd" `
     --unattended `
@@ -111,10 +139,10 @@ Recipe "C:\vsts\_work" {
     --replace `
     --runAsService `
     --windowsLogonAccount Administrator `
-    --windowsLogonPassword $adminPassword `
+    --windowsLogonPassword $adminPass `
     --work "C:\vsts\_work"
 
-    popd
+    Pop-Location
 
 }
 
