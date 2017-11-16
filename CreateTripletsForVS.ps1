b1cbef83652d98e85ae11818f7c27477922f1257
Alexander Karatarakis <alkarata@microsoft.com>
2017-11-15 23:45:52 -0800

    Create triplets for uwp. Add .cmake extension to the generated triplets

--- CreateTripletsForVS.ps1
+++ CreateTripletsForVS.ps1
@@ -20,7 +20,19 @@ set(VCPKG_TARGET_ARCHITECTURE $architecture)
 set(VCPKG_CRT_LINKAGE $linkage)
 set(VCPKG_LIBRARY_LINKAGE $linkage)
 set(VCPKG_VISUAL_STUDIO_PATH "$vsInstallPath")
-"@ | Out-File -FilePath "$outputDir\$architecture-windows-$linkage-$vsInstallNickname" -Encoding ASCII
+"@ | Out-File -FilePath "$outputDir\$architecture-windows-$linkage-$vsInstallNickname.cmake" -Encoding ASCII
         }
+
+        $linkage = "dynamic"
+        @"
+set(VCPKG_TARGET_ARCHITECTURE $architecture)
+set(VCPKG_CRT_LINKAGE $linkage)
+set(VCPKG_LIBRARY_LINKAGE $linkage)
+set(VCPKG_VISUAL_STUDIO_PATH "$vsInstallPath")
+
+set(VCPKG_CMAKE_SYSTEM_NAME WindowsStore)
+set(VCPKG_CMAKE_SYSTEM_VERSION 10.0)
+"@  | Out-File -FilePath "$outputDir\$architecture-uwp-$linkage-$vsInstallNickname.cmake" -Encoding ASCII
+
     }
 #}
