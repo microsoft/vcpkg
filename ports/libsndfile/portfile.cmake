395829c5450f0f964cbfc08e1aac5b4559729aed
Aybe <aybe@users.noreply.github.com>
2017-11-21 06:54:11 +0100

    [libsndfile] fixed missing return !

--- ports/libsndfile/portfile.cmake
+++ ports/libsndfile/portfile.cmake
@@ -23,6 +23,7 @@ vcpkg_apply_patches(
 	SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libsndfile-6830c421899e32f8d413a903a21a9b6cf384d369
 	PATCHES
 		"${CMAKE_CURRENT_LIST_DIR}/uwp-createfile-getfilesize.patch"
+		"${CMAKE_CURRENT_LIST_DIR}/uwp-createfile-getfilesize-addendum.patch"
 )
 
 if (VCPKG_CRT_LINKAGE STREQUAL "dynamic")
