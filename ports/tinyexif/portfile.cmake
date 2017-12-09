fa1f33e262cdf4f0f63b8f94a82e54630b1e7fb3
Robert Schumacher <roschuma@microsoft.com>
2017-12-09 00:09:02 -0800

    [tinyexif] Some cleanup

--- ports/tinyexif/portfile.cmake
+++ ports/tinyexif/portfile.cmake
@@ -1,14 +1,3 @@
-set(BUILD_SHARED_VALUE ON)
-set(BUILD_STATIC_VALUE OFF)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-	set(BUILD_SHARED_VALUE OFF)
-	set(BUILD_STATIC_VALUE ON)
-endif()
-set(CRT_STATIC_LIBS_VALUE OFF)
-if(VCPKG_CRT_LINKAGE STREQUAL "static")
-	set(CRT_STATIC_LIBS_VALUE ON)
-endif()
-
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
@@ -19,12 +8,12 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DBUILD_SHARED_LIBS=${BUILD_SHARED_VALUE}
-        -DBUILD_STATIC_LIBS=${BUILD_STATIC_VALUE}
-        -DLINK_CRT_STATIC_LIBS=${CRT_STATIC_LIBS_VALUE}
+        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
         -DBUILD_DEMO=OFF
 )
 
@@ -35,8 +24,6 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/tinyexif")
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/tinyexif)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/tinyexif/README.md)
+file(INSTALL ${SOURCE_PATH}/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/tinyexif RENAME copyright)
