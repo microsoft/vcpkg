7a2120dba1fb606b06f555965908db438d10e019
Robert Schumacher <roschuma@microsoft.com>
2017-12-13 11:02:04 -0800

    [vcpkg] Revert revert of #2369.

--- toolsrc/include/vcpkg/commands.h
+++ toolsrc/include/vcpkg/commands.h
@@ -104,6 +104,7 @@ namespace vcpkg::Commands
 
     namespace Version
     {
+        const char* base_version();
         const std::string& version();
         void warn_if_vcpkg_version_mismatch(const VcpkgPaths& paths);
         void perform_and_exit(const VcpkgCmdArguments& args);
