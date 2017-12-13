063f96faa0260d52e5ccedc401aac64d8a8f59a2
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-12-13 22:07:42 +0100

    Part 3

--- toolsrc/src/vcpkgmetricsuploader.cpp
+++ toolsrc/src/vcpkgmetricsuploader.cpp
@@ -9,12 +9,12 @@ using namespace vcpkg;
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
-    // int argCount;
-    // LPWSTR* szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
+    int argCount;
+    LPWSTR* szArgList = CommandLineToArgvW(GetCommandLineW(), &argCount);
 
-    // Checks::check_exit(VCPKG_LINE_INFO, argCount == 2, "Requires exactly one argument, the path to the payload file");
-    // auto v = Files::get_real_filesystem().read_contents(szArgList[1]).value_or_exit(VCPKG_LINE_INFO);
-    // Metrics::g_metrics.lock()->upload(v);
-    // LocalFree(szArgList);
+    Checks::check_exit(VCPKG_LINE_INFO, argCount == 2, "Requires exactly one argument, the path to the payload file");
+    auto v = Files::get_real_filesystem().read_contents(szArgList[1]).value_or_exit(VCPKG_LINE_INFO);
+    Metrics::g_metrics.lock()->upload(v);
+    LocalFree(szArgList);
     return 0;
 }
