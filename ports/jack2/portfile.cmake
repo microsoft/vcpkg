386e69c6b2f2047624cf4b2123cb53e810e78ae1
clvn <clvn@users.noreply.github.com>
2018-01-01 15:43:41 +0100

    [jack2] Prefer using vcpkg_from_github. Removed check for Windows Store

--- ports/jack2/portfile.cmake
+++ ports/jack2/portfile.cmake
@@ -5,19 +5,13 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "WindowsStore not supported")
-endif()
-
-set(VERSION 1.9.12)
-
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/jack2-${VERSION})
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/jackaudio/jack2/releases/download/v${VERSION}/jack2-${VERSION}.tar.gz"
-    FILENAME "jack2-${VERSION}.tar.gz"
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jackaudio/jack2
+    REF v1.9.12
     SHA512 f0271dfc8f8e2f2489ca52f431ad4fa420665816d6c67a01a76da1d4b5ae91f6dad8c4e3309ec5e0c159c9d312ed56021ab323d74bce828ace26f1b8d477ddfa
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 # Install headers and a statically built JackWeakAPI.c
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
