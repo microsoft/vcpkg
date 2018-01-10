443af9686b9ea44b5f4b5a08fb822cf25f5c5cd9
Koby Kahane <kobykahane@gmail.com>
2018-01-10 15:40:27 +0200

    Update TCLAP to 1.2.2.

--- ports/tclap/portfile.cmake
+++ ports/tclap/portfile.cmake
@@ -1,10 +1,10 @@
 #header-only library
 include(vcpkg_common_functions)
-set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/tclap-1.2.1")
+set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/tclap-1.2.2")
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz"
-    FILENAME "tclap-1.2.1.tar.gz"
-    SHA512 8bd6ee724600880840048c7b36f02d31b1aa4910b17f80fb04aef89b1f1917856d9979ec488edbd457b66d9d689aea97540abb842a8b902bbd75c66a6e07b9b1
+    URLS "http://downloads.sourceforge.net/project/tclap/tclap-1.2.2.tar.gz"
+    FILENAME "tclap-1.2.2.tar.gz"
+    SHA512 516ec17f82a61277922bc8c0ed66973300bf42a738847fbbd2912c6405c34f94a13e47dc964854a5b26a9a9f1f518cce682ca54e769d6016851656c647866107
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
