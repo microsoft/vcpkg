83041dfa6297fb16bb08fd78218dbb6d69b0c5b0
Robert Schumacher <roschuma@microsoft.com>
2018-01-22 22:45:59 -0800

    import sys

--- scanner.py
+++ scanner.py
@@ -6,6 +6,7 @@ import json
 import smtplib
 from email.message import EmailMessage
 import datetime
+import sys
 
 def get_github():
     githubdoc = []
@@ -16,13 +17,16 @@ def get_github():
         print("total count = {}".format(jdoc['total_count']))
         today = datetime.datetime.utcnow()
         try:
-            for item in jdoc['items'][:30]:
-                updated_at = item['updated_at']
-                dt_updated_at = datetime.datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
-                if today - dt_updated_at > datetime.timedelta(days=3):
-                    break
-                repo = item['repository_url'][29:]
-                githubdoc.append("""<div class="github"><div>{} comments</div><div>updated {}</div><div>{}</div><div><a href="{}">{}</a></div></div>""".format(item['comments'], updated_at, repo, item['html_url'], html.escape(item['title'])))
+            if 'items' in jdoc:
+                for item in jdoc['items'][:30]:
+                    updated_at = item['updated_at']
+                    dt_updated_at = datetime.datetime.strptime(updated_at, "%Y-%m-%dT%H:%M:%SZ")
+                    if today - dt_updated_at > datetime.timedelta(days=3):
+                        break
+                    repo = item['repository_url'][29:]
+                    githubdoc.append("""<div class="github"><div>{} comments</div><div>updated {}</div><div>{}</div><div><a href="{}">{}</a></div></div>""".format(item['comments'], updated_at, repo, item['html_url'], html.escape(item['title'])))
+            else:
+                githubdoc.append("""<div class="github">{}</div>""".format(html.escape(json.dumps(jdoc))))
         except:
             githubdoc.append("<div>{}</div>".format(html.escape(sys.exc_info())))
 
@@ -34,8 +38,11 @@ def get_google():
     with urllib.request.urlopen('https://www.googleapis.com/customsearch/v1?key=AIzaSyAgDJeOcCtOgYSLPboFLf_3SsV9TqSYTFE&cx=016162264654347836401:8cwjt4hwu-k&q=vcpkg+-site:github.com&dateRestrict=d5&exactTerms=vcpkg') as res:
         jdoc = json.loads(res.read().decode('utf-8'))
         try:
-            for item in jdoc['items'][:30]:
-                googledoc.append("""<div class="google"><div><a href="{}">{}</a></div><div>{}</div></div>""".format(item['htmlFormattedUrl'], item['htmlTitle'][:120], item['htmlSnippet']))
+            if 'items' in jdoc:
+                for item in jdoc['items'][:30]:
+                    googledoc.append("""<div class="google"><div><a href="{}">{}</a></div><div>{}</div></div>""".format(item['htmlFormattedUrl'], item['htmlTitle'][:120], item['htmlSnippet']))
+            else:
+                googledoc.append("""<div class="google">{}</div>""".format(html.escape(json.dumps(jdoc))))
         except:
             googledoc.append("<div>{}</div>".format(html.escape(sys.exc_info())))
 
@@ -52,9 +59,12 @@ def get_stackoverflow():
         doc = gzip.decompress(doc1)
         jdoc = json.loads(doc.decode('utf-8'))
         try:
-            for item in jdoc['items'][:30]:
-                activitydate = datetime.datetime.fromtimestamp(int(item['last_activity_date'])).isoformat()
-                stackdoc.append("""<div class="stacko"><div><a href="{}">{}</a></div><div>Last Activity: {}</div><div>views: {}</div></div>""".format(item['link'], html.escape(item['title']), activitydate, item['view_count']))
+            if 'items' in jdoc:
+                for item in jdoc['items'][:30]:
+                    activitydate = datetime.datetime.fromtimestamp(int(item['last_activity_date'])).isoformat()
+                    stackdoc.append("""<div class="stacko"><div><a href="{}">{}</a></div><div>Last Activity: {}</div><div>views: {}</div></div>""".format(item['link'], html.escape(item['title']), activitydate, item['view_count']))
+            else:
+                stackdoc.append("""<div class="stacko">{}</div>""".format(html.escape(json.dumps(jdoc))))
         except:
             stackdoc.append("<div>{}</div>".format(html.escape(sys.exc_info())))
     return "<h2>Stackoverflow last 14 days ({} posts)</h2>{}".format(len(stackdoc), "".join(stackdoc))
