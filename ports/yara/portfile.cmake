8374c7d9e21c9c2d6467f24491300b5695070229
atkawa7 <atkawa7@yahoo.com>
2018-01-29 20:53:31 +0200

    [yara] init

---
+++ ports/yara/portfile.cmake
@@ -0,0 +1,26 @@
+include(vcpkg_common_functions)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  message(WARNING "Building static")
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO VirusTotal/yara
+  REF e3439e4ead4ed5d3b75a0b46eaf15ddda2110bb9
+  SHA512 2cb837c8e2617e66766b45339a99f3f441f842cb14ce7efe38f5ec2141a51696a400279d98514be4471234c8ef7c99419f56f5b7398af13d771294b2923090aa
+  HEAD_REF dev
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON -DDISABLE_INSTALL_TOOLS=ON
+)
+
+vcpkg_install_cmake()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/yara RENAME copyright)
