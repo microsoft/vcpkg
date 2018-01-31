d5ed5d6ced178592e9894f78674529974b1cf603
Alexander Karatarakis <alkarata@microsoft.com>
2018-01-31 14:53:19 -0800

    Extract some functions into VcpkgPowershellUtils-Private.ps1

--- InitializeWorkerMachine.ps1
+++ InitializeWorkerMachine.ps1
@@ -11,79 +11,17 @@ param
 
 $scriptsDir = split-path -parent $MyInvocation.MyCommand.Definition
 . "$scriptsDir\VcpkgPowershellUtils.ps1"
-
-function Recipe
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory=$true)][String]$filepath,
-        [Parameter(Mandatory=$true)][ScriptBlock]$Action
-    )
-
-    Write-Verbose "Starting recipe for $filepath"
-
-    if(!(Test-Path $filepath))
-    {
-        Write-Verbose "Invoking recipe for $filepath"
-        $Action.Invoke()
-    }
-    if(!(Test-Path $filepath))
-    {
-        throw "failed $filepath"
-    }
-}
-
-function UnattendedVSinstall
-{
-    param(
-        [Parameter(Mandatory=$true)][string]$installPath,
-        [Parameter(Mandatory=$true)][string]$nickname
-    )
-
-    # References
-    # https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
-    # https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community#desktop-development-with-c
-
-    $filename = "vs_Community.exe"
-    Recipe "$scriptsDir\$filename" {
-        vcpkgDownloadFile "https://aka.ms/vs/15/release/vs_community.exe" $scriptsDir\$filename
-    }
-
-    Write-Host "Updating VS Installer"
-    vcpkgInvokeCommand "$scriptsDir\$filename" "--update --quiet --wait --norestart"
-
-    Write-Host "Installing Visual Studio"
-    $arguments = ("--installPath $installPath",
-    "--add Microsoft.VisualStudio.Workload.NativeDesktop",
-    "--add Microsoft.VisualStudio.Workload.Universal",
-    "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
-    "--add Microsoft.VisualStudio.Component.VC.Tools.ARM",
-    "--add Microsoft.VisualStudio.Component.VC.Tools.ARM64",
-    "--add Microsoft.VisualStudio.Component.VC.ATL",
-    "--add Microsoft.VisualStudio.Component.VC.ATLMFC",
-    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.Desktop",
-    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP",
-    "--add Microsoft.VisualStudio.Component.Windows10SDK.16299.UWP.Native",
-    "--add Microsoft.VisualStudio.ComponentGroup.UWP.VC",
-    "--add Microsoft.Component.VC.Runtime.OSSupport",
-    "--nickname $nickname",
-    "--quiet",
-    "--wait",
-    "--norestart") -join " "
-
-    vcpkgInvokeCommand "$scriptsDir\$filename" "$arguments"
-}
+. "$scriptsDir\VcpkgPowershellUtils-Private.ps1"
 
 # Power Settings (so machine does not go to sleep)
 powercfg /SETACVALUEINDEX SCHEME_BALANCED SUB_SLEEP STANDBYIDLE 0
 
-$unstablePath = "C:\VS2017\Unstable"
+$unstablePath = $VISUAL_STUDIO_2017_UNSTABLE_PATH
 Recipe $unstablePath {
     UnattendedVSinstall -installPath $unstablePath -nickname "Unstable"
 }
 
-$stablePath = "C:\VS2017\Stable"
+$stablePath = $VISUAL_STUDIO_2017_STABLE_PATH
 Recipe $stablePath {
     UnattendedVSinstall -installPath $stablePath -nickname "Stable"
 }
