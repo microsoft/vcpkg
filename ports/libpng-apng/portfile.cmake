7c8be76d3ca8747f59d844f8da1db68290449d83
Albert Huang <alberthdev@users.noreply.github.com>
2018-02-01 21:54:36 -0500

    Update portfile to use 7Z to extract patch + fix others

--- ports/libpng-apng/portfile.cmake
+++ ports/libpng-apng/portfile.cmake
@@ -16,7 +16,20 @@ vcpkg_download_distfile(LIBPNG_APNG_PATCH_ARCHIVE
     SHA512 0777b8e55aeee207ee92479f2258ef1f60f16d7951fdbc6d89a80ef533b86dadecd1ef659d6fe7602d8ea3a8e711a096b0f77ee09b993799b73dfffddfe5dd3c
 )
 
-vcpkg_extract_source_archive(${LIBPNG_APNG_PATCH_ARCHIVE})
+vcpkg_find_acquire_program(7Z)
+
+vcpkg_execute_required_process(
+    COMMAND ${7Z} x ${LIBPNG_APNG_PATCH_ARCHIVE} -aoa
+    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src
+    LOGNAME extract-patch.log
+)
+
+find_program(GIT NAMES git git.cmd)
+
+# sed and awk are installed with git but in a different directory
+get_filename_component(GIT_EXE_PATH ${GIT} DIRECTORY)
+set(AWK_EXE_PATH "${GIT_EXE_PATH}/../usr/bin")
+set(ENV{PATH} "$ENV{PATH};${AWK_EXE_PATH}")
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
@@ -40,6 +53,7 @@ vcpkg_configure_cmake(
         -DPNG_STATIC=${PNG_STATIC_LIBS}
         -DPNG_SHARED=${PNG_SHARED_LIBS}
         -DPNG_TESTS=OFF
+        -DPNG_PREFIX=a
         -DSKIP_INSTALL_PROGRAMS=ON
         -DSKIP_INSTALL_EXECUTABLES=ON
         -DSKIP_INSTALL_FILES=ON
@@ -56,8 +70,8 @@ endif()
 
 # Remove CMake config files as they are incorrectly generated and everyone uses built-in FindPNG anyway.
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/libpng ${CURRENT_PACKAGES_DIR}/debug/lib/libpng)
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpng)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/libpng/LICENSE ${CURRENT_PACKAGES_DIR}/share/libpng/copyright)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpng-apng)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/libpng-apng/LICENSE ${CURRENT_PACKAGES_DIR}/share/libpng-apng/copyright)
 
 vcpkg_copy_pdbs()
 
