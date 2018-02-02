17d20556906dcf7e10ab94a1a45bfa76275478ca
Alexander Karatarakis <alkarata@microsoft.com>
2018-02-01 17:39:51 -0800

    Place CreateTripletsForVS in powershellUtils-private

--- VcpkgPowershellUtils-Private.ps1
+++ VcpkgPowershellUtils-Private.ps1
@@ -131,7 +131,48 @@ function UnattendedVSupdate
     Write-Host "Updating Visual Studio... done."
 }
 
+function CreateTripletsForVS
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)][String]$vsInstallPath,
+        [Parameter(Mandatory=$true)][String]$vsInstallNickname,
+        [Parameter(Mandatory=$true)][String]$outputDir
+    )
+
+    $vsInstallPath = $vsInstallPath -replace "\\","/"
+    $vsInstallNickname = $vsInstallNickname.ToLower()
+
+    foreach ($architecture in @("x86", "x64"))
+    {
+        foreach ($linkage in @("dynamic", "static"))
+        {
+            @"
+set(VCPKG_TARGET_ARCHITECTURE $architecture)
+set(VCPKG_CRT_LINKAGE $linkage)
+set(VCPKG_LIBRARY_LINKAGE $linkage)
+set(VCPKG_VISUAL_STUDIO_PATH "$vsInstallPath")
+"@ | Out-File -FilePath "$outputDir\$architecture-windows-$linkage-$vsInstallNickname.cmake" -Encoding ASCII
+        }
+
+        $linkage = "dynamic"
+        @"
+set(VCPKG_TARGET_ARCHITECTURE $architecture)
+set(VCPKG_CRT_LINKAGE $linkage)
+set(VCPKG_LIBRARY_LINKAGE $linkage)
+set(VCPKG_VISUAL_STUDIO_PATH "$vsInstallPath")
+
+set(VCPKG_CMAKE_SYSTEM_NAME WindowsStore)
+set(VCPKG_CMAKE_SYSTEM_VERSION 10.0)
+"@  | Out-File -FilePath "$outputDir\$architecture-uwp-$linkage-$vsInstallNickname.cmake" -Encoding ASCII
+
+    }
+}
+
 # Constants
 $VISUAL_STUDIO_2017_STABLE_PATH = "C:\VS2017\Stable"
 $VISUAL_STUDIO_2017_UNSTABLE_PATH = "C:\VS2017\Unstable"
 
+$VISUAL_STUDIO_2017_STABLE_NICKNAME = "Stable"
+$VISUAL_STUDIO_2017_UNSTABLE_NICKNAME = "Unstable"
