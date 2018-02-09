fc888e46178675bb708448f24cb07a72b13d7cd1
Hiroshi Miura <miurahr@linux.com>
2018-02-09 13:29:00 +0900

    fix tiff detection error when static build

--- ports/libgeotiff/portfile.cmake
+++ ports/libgeotiff/portfile.cmake
@@ -13,7 +13,8 @@ vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-directory-output.patch")
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/fix-directory-output.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/fix-cmake-tiff-detection.patch")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
   set(BUILD_SHARED_LIBS ON)
@@ -26,6 +27,10 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
             -DWITH_UTILITIES=OFF
+            -DWITH_TIFF=ON
+            -DWITH_PROJ4=ON
+            -DWITH_ZLIB=ON
+            -DWITH_JPEG=ON
 )
 
 vcpkg_build_cmake()
