c2518b9a89aa24afde78c44e38aa00a90480a732
Robert Schumacher <roschuma@microsoft.com>
2018-02-13 16:12:55 -0800

    [vcpkg-cmake-toolchain] Only applocal dependencies for shared libraries

--- scripts/buildsystems/vcpkg.cmake
+++ scripts/buildsystems/vcpkg.cmake
@@ -163,7 +163,8 @@ function(add_library name)
     list(FIND ARGV "INTERFACE" INTERFACE_IDX)
     list(FIND ARGV "ALIAS" ALIAS_IDX)
     if(IMPORTED_IDX EQUAL -1 AND INTERFACE_IDX EQUAL -1 AND ALIAS_IDX EQUAL -1)
-        if(VCPKG_APPLOCAL_DEPS AND _VCPKG_TARGET_TRIPLET_PLAT MATCHES "windows|uwp")
+        get_target_property(IS_LIBRARY_SHARED ${name} TYPE)
+        if(VCPKG_APPLOCAL_DEPS AND _VCPKG_TARGET_TRIPLET_PLAT MATCHES "windows|uwp" AND IS_LIBRARY_SHARED STREQUAL "SHARED_LIBRARY")
             add_custom_command(TARGET ${name} POST_BUILD
                 COMMAND powershell -noprofile -executionpolicy Bypass -file ${_VCPKG_TOOLCHAIN_DIR}/msbuild/applocal.ps1
                     -targetBinary $<TARGET_FILE:${name}>
