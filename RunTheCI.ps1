b3813847faa4465b656bf2cf62c23ed1311d03fd
Alexander Karatarakis <alkarata@microsoft.com>
2018-02-14 17:44:31 -0800

    Fix bootstrap call

--- RunTheCI.ps1
+++ RunTheCI.ps1
@@ -13,8 +13,7 @@ $tripletFilePath = "$vcpkgRootDir\triplets\$Triplet.cmake"
 $vsInstallPath = findVSInstallPathFromTriplet $tripletFilePath
 
 Write-Host "Bootstrapping vcpkg ..."
-Write-Host "###$vcpkgRootDir\scripts\bootstrap.ps1###"
-vcpkgInvokeCommand "$vcpkgRootDir\scripts\bootstrap.ps1" -Arguments "-Verbose -withVSPath $vsInstallPath"
+& "$vcpkgRootDir\scripts\bootstrap.ps1" -Verbose -withVSPath $vsInstallPath
 Write-Host "Bootstrapping vcpkg ... done."
 
 $packagesDir = "$vcpkgRootDir\packages"
