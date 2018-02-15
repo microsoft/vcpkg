2ca939e22cad65304995148f195590806f77b1a0
Alexander Karatarakis <alkarata@microsoft.com>
2018-02-14 18:16:04 -0800

    bool, not switch

--- PrepareInstalledDir.ps1
+++ PrepareInstalledDir.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)][string]$Triplet
-    [Parameter(Mandatory=$true)][switch]$incremental,
+    [Parameter(Mandatory=$true)][bool]$incremental,
 )
 
 $scriptsDir = split-path -parent $script:MyInvocation.MyCommand.Definition
