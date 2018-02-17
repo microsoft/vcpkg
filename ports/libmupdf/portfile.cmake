9944d4cf5902a8c6050c13960632ff3e443ed21c
Robert Schumacher <roschuma@microsoft.com>
2018-02-17 02:23:26 -0800

    [jbig2dec][libmupdf] Extract jbig2dec, remove remaining vendored 3rdparty

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -1,26 +1,18 @@
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    message(STATUS "Warning: Dynamic building not supported. Building static.")
-    set(VCPKG_LIBRARY_LINKAGE static)
-endif()
-
 include(vcpkg_common_functions)
-set (MUPDF_VERSION 1.12.0)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/mupdf-${MUPDF_VERSION}-source)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://mupdf.com/downloads/archive/mupdf-${MUPDF_VERSION}-source.tar.gz"
-    FILENAME "mupdf.tar.gz"
-    SHA512 11ae620e55e9ebd5844abd7decacc0dafc90dd1f4907ba6ed12f5c725d3920187fc730a7fc33979bf3ff9451da7dbb51f34480a878083e2064f3455555f47d96
-)
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/missing-includes.patch
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ArtifexSoftware/mupdf
+    REF 1.12.0
+    SHA512 893a1958e34355acf73624e9c47f4a97adf13d5fe33604ac384df9ac22a56ef7c18e02143eaffc3c2a08f460e4c71fee00c094b6d6696f8446977bb18f65e3da
+    HEAD_REF master
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
     PREFER_NINJA
 )
 
@@ -32,4 +24,4 @@ vcpkg_copy_pdbs()
 
 #copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/${PORT}/COPYING ${CURRENT_PACKAGES_DIR}/share/${PORT}/COPYRIGHT)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/${PORT}/COPYING ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
