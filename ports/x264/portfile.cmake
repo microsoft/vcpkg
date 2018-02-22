4bfe7441fc766762b1a609a808d08fb2c68bb496
Robert Schumacher <roschuma@microsoft.com>
2018-02-21 20:12:15 -0800

    [x264] Simplify portfile. Use GitHub mirror instead of trying to run git clone.

--- ports/x264/portfile.cmake
+++ ports/x264/portfile.cmake
@@ -1,63 +1,20 @@
-# This portfile adds the Qt Cryptographic Arcitecture
-# Changes to the original build:
-#   No -qt5 suffix, which is recommended just for Linux
-#   Output directories according to vcpkg
-#   Updated certstore. See certstore.pem in the output dirs
-#
-
-#if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-#    message(STATUS "Warning: Static building not supported yet. Building dynamic.")
-#    set(VCPKG_LIBRARY_LINKAGE dynamic)
-#endif()
-
 include(vcpkg_common_functions)
+
 set(X264_VERSION 152)
 
-find_program(GIT git)
-#vcpkg_find_acquire_program(PERL)
-#get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-#set(ENV{PATH} "$ENV{PATH};${PERL_EXE_PATH}")
-
-# Set git variables to x264 version 2.2.0 commit 
-set(GIT_URL "git://git.videolan.org/x264.git")
-set(GIT_REF "e9a5903edf8ca59ef20e6f4894c196f135af735e") # Commit
-
-# Prepare source dir
-if(NOT EXISTS "${DOWNLOADS}/x264.git")
-    message(STATUS "Cloning")
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} clone --bare ${GIT_URL} ${DOWNLOADS}/x264.git
-        WORKING_DIRECTORY ${DOWNLOADS}
-        LOGNAME clone
-    )
-endif()
-message(STATUS "Cloning done")
-
-if(NOT EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
-    message(STATUS "Adding worktree")
-    file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR})
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} worktree add -f --detach ${CURRENT_BUILDTREES_DIR}/src ${GIT_REF}
-        WORKING_DIRECTORY ${DOWNLOADS}/x264.git
-        LOGNAME worktree
-    )
-endif()
-message(STATUS "Adding worktree done")
-
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
-
-# Apply the patch to install to the expected folders
-#vcpkg_apply_patches(
-#    SOURCE_PATH ${SOURCE_PATH}
-#    PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001-fix-path-for-vcpkg.patch
-#)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mirror/x264
+    REF e9a5903edf8ca59ef20e6f4894c196f135af735e
+    SHA512 063da238264b33ab7ccf097c1f8a7d6b1bf1f0777b433ccbb6ab98090f050fa4d289eeff37b701b8fd7fb5ad460b7fa13d61b68b3f397bc78a8eaa50379e4878
+    HEAD_REF master
+)
 
 # Acquire tools
 vcpkg_acquire_msys(MSYS_ROOT)
 
 # Insert msys into the path between the compiler toolset and windows system32. This prevents masking of "link.exe" but DOES mask "find.exe".
 string(REPLACE ";$ENV{SystemRoot}\\system32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\system32;" NEWPATH "$ENV{PATH}")
-string(REPLACE ";$ENV{SystemRoot}\\System32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\System32;" NEWPATH "${NEWPATH}")
 set(ENV{PATH} "${NEWPATH}")
 set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
 
@@ -69,21 +26,18 @@ vcpkg_execute_required_process(
 set(AUTOMAKE_DIR ${MSYS_ROOT}/usr/share/automake-1.15)
 #file(COPY ${AUTOMAKE_DIR}/config.guess ${AUTOMAKE_DIR}/config.sub DESTINATION ${SOURCE_PATH}/source)
 
-#set(CONFIGURE_OPTIONS "--host=i686-pc-mingw32 --disable-samples --disable-tests")
-set(CONFIGURE_OPTIONS "--host=i686-pc-mingw32 --enable-strip --disable-lavf --disable-swscale --disable-asm")
+set(CONFIGURE_OPTIONS "--host=i686-pc-mingw32 --enable-strip --disable-lavf --disable-swscale --disable-asm --disable-avs --disable-ffms --disable-gpac --disable-lsmash")
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --enable-shared")
 else()
     set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --enable-static")
 endif()
 
-#set(CONFIGURE_OPTIONS_RELASE "--disable-debug --enable-release --prefix=${CURRENT_PACKAGES_DIR}")
-#set(CONFIGURE_OPTIONS_DEBUG  "--enable-debug --disable-release --prefix=${CURRENT_PACKAGES_DIR}/debug")
-set(CONFIGURE_OPTIONS_RELASE "--prefix=${CURRENT_PACKAGES_DIR}")
+set(CONFIGURE_OPTIONS_RELEASE "--prefix=${CURRENT_PACKAGES_DIR}")
 set(CONFIGURE_OPTIONS_DEBUG  "--enable-debug --prefix=${CURRENT_PACKAGES_DIR}/debug")
 
-if(VCPKG_CRT_LINKAGE STREQUAL static)
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
     set(X264_RUNTIME "-MT")
 else()
     set(X264_RUNTIME "-MD")
@@ -98,7 +52,7 @@ set(ENV{CXXFLAGS} "${X264_RUNTIME} -O2 -Oi -Zi")
 set(ENV{LDFLAGS} "-DEBUG -INCREMENTAL:NO -OPT:REF -OPT:ICF")
 vcpkg_execute_required_process(
     COMMAND ${BASH} --noprofile --norc -c 
-        "CC=cl ${SOURCE_PATH}/configure ${CONFIGURE_OPTIONS} ${CONFIGURE_OPTIONS_RELASE}"
+        "CC=cl ${SOURCE_PATH}/configure ${CONFIGURE_OPTIONS} ${CONFIGURE_OPTIONS_RELEASE}"
     WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
     LOGNAME "configure-${TARGET_TRIPLET}-rel")
 message(STATUS "Configuring ${TARGET_TRIPLET}-rel done")
@@ -137,52 +91,31 @@ vcpkg_execute_required_process(
     LOGNAME "build-${TARGET_TRIPLET}-dbg")
 message(STATUS "Package ${TARGET_TRIPLET}-dbg done")
 
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/x264)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/x264.exe ${CURRENT_PACKAGES_DIR}/tools/x264/x264.exe)
+
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/bin
-    ${CURRENT_PACKAGES_DIR}/debug/bin
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/share
-    ${CURRENT_PACKAGES_DIR}/debug/share
     ${CURRENT_PACKAGES_DIR}/lib/pkgconfig
     ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
-    ${CURRENT_PACKAGES_DIR}/lib/x264
-    ${CURRENT_PACKAGES_DIR}/debug/lib/x264)
+    ${CURRENT_PACKAGES_DIR}/debug/bin/x264.exe
+    ${CURRENT_PACKAGES_DIR}/debug/include
+)
 
-#file(GLOB TEST_LIBS
-#    ${CURRENT_PACKAGES_DIR}/lib/*test*
-#    ${CURRENT_PACKAGES_DIR}/debug/lib/*test*)
-#file(REMOVE ${TEST_LIBS})
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    # copy x264 dlls from lib to bin
-    file(GLOB RELEASE_DLLS ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/libx264-${X264_VERSION}.dll)
-    file(GLOB DEBUG_DLLS ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/libx264-${X264_VERSION}.dll)
-    file(COPY ${RELEASE_DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-    file(COPY ${DEBUG_DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libx264.dll.lib ${CURRENT_PACKAGES_DIR}/lib/libx264.lib)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libx264.dll.lib ${CURRENT_PACKAGES_DIR}/debug/lib/libx264.lib)
 else()
-    # rename static libraries to match import libs
-    # see https://gitlab.kitware.com/cmake/cmake/issues/16617
-    foreach(MODULE dt in io tu uc)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/x264${MODULE}.lib ${CURRENT_PACKAGES_DIR}/lib/x264${MODULE}.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/x264${MODULE}.lib ${CURRENT_PACKAGES_DIR}/debug/lib/x264${MODULE}.lib)
-    endforeach()
-
     # force U_STATIC_IMPLEMENTATION macro
-    foreach(HEADER x264.h)
-        file(READ ${CURRENT_PACKAGES_DIR}/include/unicode/${HEADER} HEADER_CONTENTS)
-        string(REPLACE "defined(U_STATIC_IMPLEMENTATION)" "1" HEADER_CONTENTS "${HEADER_CONTENTS}")
-        file(WRITE ${CURRENT_PACKAGES_DIR}/include/unicode/${HEADER} "${HEADER_CONTENTS}")
-    endforeach()
+    file(READ ${CURRENT_PACKAGES_DIR}/include/x264.h HEADER_CONTENTS)
+    string(REPLACE "defined(U_STATIC_IMPLEMENTATION)" "1" HEADER_CONTENTS "${HEADER_CONTENTS}")
+    file(WRITE ${CURRENT_PACKAGES_DIR}/include/x264.h "${HEADER_CONTENTS}")
+
+    file(REMOVE_RECURSE
+        ${CURRENT_PACKAGES_DIR}/bin
+        ${CURRENT_PACKAGES_DIR}/debug/bin
+    )
 endif()
 
-# remove any remaining dlls in /lib
-file(GLOB DUMMY_DLLS ${CURRENT_PACKAGES_DIR}/lib/*.dll ${CURRENT_PACKAGES_DIR}/debug/lib/*.dll)
-if(DUMMY_DLLS)
-    file(REMOVE ${DUMMY_DLLS})
-endif()
-
-# Generates warnings about missing pdbs for icudt.dll
-# This is expected because ICU database contains no executable code
 vcpkg_copy_pdbs()
 
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/x264)
