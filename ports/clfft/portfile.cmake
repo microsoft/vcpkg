c7fa41face21fbff3b0f613855d8cdc6fc7bd50f
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2018-02-23 12:50:01 +0100

    clFFT installs

--- ports/clfft/portfile.cmake
+++ ports/clfft/portfile.cmake
@@ -28,6 +28,13 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/clFFT")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(INSTALL
+        "${SOURCE_PATH}/LICENSE"
+    DESTINATION
+        ${CURRENT_PACKAGES_DIR}/share/clfft/copyright
+)
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH "CMake")
 
 vcpkg_copy_pdbs()
