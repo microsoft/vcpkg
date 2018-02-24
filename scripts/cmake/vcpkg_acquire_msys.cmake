4bf48ddadc6dfda11254af63b2b099571cefeccd
Jacob Zhong <cmpute@qq.com>
2018-02-24 15:15:09 +0800

    Add documentation for PACKAGES in vcpkg_require_msys

--- scripts/cmake/vcpkg_acquire_msys.cmake
+++ scripts/cmake/vcpkg_acquire_msys.cmake
@@ -11,6 +11,10 @@
 ## ### MSYS_ROOT_VAR
 ## An out-variable that will be set to the path to MSYS2.
 ##
+## ### PACAKGES
+## A list of packages to be ensured available in msys.
+## To ensure a package is available:`vcpkg_acquire_msys(MSYS_ROOT PACKAGES make)`
+##
 ## ## Notes
 ## A call to `vcpkg_acquire_msys` will usually be followed by a call to `bash.exe`:
 ## ```cmake
@@ -22,19 +26,6 @@
 ##     WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
 ##     LOGNAME build-${TARGET_TRIPLET}-rel
 ## )
-## ```
-## To ensure a package is available:
-## ```cmake
-## vcpkg_acquire_msys(MSYS_ROOT)
-## set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
-##
-## message(STATUS "Installing MSYS Packages")
-## vcpkg_execute_required_process(
-##     COMMAND
-##         ${BASH} --noprofile --norc -c
-##             'PATH=/usr/bin:\$PATH pacman -Sy --noconfirm --needed make'
-##     WORKING_DIRECTORY ${MSYS_ROOT}
-##     LOGNAME pacman-${TARGET_TRIPLET})
 ## ```
 ##
 ## ## Examples
