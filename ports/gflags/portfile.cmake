8165aae12cfd2f6d12d321b16deb3c3241943800
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-03-01 09:08:57 +0100

    [gflags] remove patch, fixed upstream

--- ports/gflags/portfile.cmake
+++ ports/gflags/portfile.cmake
@@ -12,11 +12,6 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/revert-fix-install.patch"
-)
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
