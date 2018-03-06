849ccd8b32bcf5d9748abdec92446b4407d622d6
Carlos O'Ryan <coryan@google.com>
2018-03-06 18:26:38 +0000

    Initial port for google-cloud-cpp

---
+++ ports/google-cloud-cpp/portfile.cmake
@@ -0,0 +1,52 @@
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported yet. Building static.")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+include(vcpkg_common_functions)
+
+set(GOOGLE_CLOUD_CPP_VERSION 0bd7b17b08ab4fd14fc6de580cd762f0603bffab)
+vcpkg_download_distfile(GOOGLE_CLOUD_CPP
+    URLS "https://github.com/coryan/google-cloud-cpp/archive/0bd7b17b08ab4fd14fc6de580cd762f0603bffab.zip"
+    FILENAME "gcpp-${GOOGLE_CLOUD_CPP_VERSION}.zip"
+    SHA512 72236e4b75e86611ed6befe6a5d097dd8055648692510cd02f8419f167128359b9c278e229ab42288597ccb236dd9e946292155863839d3e93a0492a422237d4
+)
+
+set(GOOGLEAPIS_VERSION 92f10d7033c6fa36e1a5a369ab5aa8bafd564009)
+vcpkg_download_distfile(GOOGLEAPIS
+    URLS "https://github.com/google/googleapis/archive/92f10d7033c6fa36e1a5a369ab5aa8bafd564009.zip"
+    FILENAME "googleapis-${GOOGLEAPIS_VERSION}.zip"
+    SHA512 4280ece965a231f6a0bb3ea38a961d15babd9eac517f9b0d57e12f186481bbab6a27e4f0ee03ba3c587c9aa93d3c2e6c95f67f50365c65bb10594f0229279287
+)
+
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-src/google-cloud-cpp)
+if(EXISTS ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-src)
+    file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-src)
+endif()
+
+vcpkg_extract_source_archive(${GOOGLE_CLOUD_CPP} ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-src)
+file(RENAME ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-src/google-cloud-cpp-${GOOGLE_CLOUD_CPP_VERSION} ${SOURCE_PATH})
+vcpkg_extract_source_archive(${GOOGLEAPIS} ${SOURCE_PATH}/third_party)
+file(REMOVE_RECURSE ${SOURCE_PATH}/third_party/googleapis)
+file(RENAME ${SOURCE_PATH}/third_party/googleapis-${GOOGLEAPIS_VERSION} ${SOURCE_PATH}/third_party/googleapis)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DGOOGLE_CLOUD_CPP_GRPC_PROVIDER=vcpkg
+        -DGOOGLE_CLOUD_CPP_GMOCK_PROVIDER=vcpkg
+)
+
+# gRPC runs built executables during the build, so they need access to the installed DLLs.
+set(ENV{PATH} "$ENV{PATH};${CURRENT_INSTALLED_DIR}/bin;${CURRENT_INSTALLED_DIR}/debug/bin")
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/google-cloud-cpp)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/google-cloud-cpp RENAME copyright)
+
+vcpkg_copy_pdbs()
