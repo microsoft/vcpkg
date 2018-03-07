5ac857315d72e8645f745b4a32deca9dfcc54f5c
Robert Schumacher <roschuma@microsoft.com>
2018-03-07 09:42:44 -0800

    [shogun] Add additional dependencies, force static linking

--- ports/shogun/portfile.cmake
+++ ports/shogun/portfile.cmake
@@ -1,3 +1,8 @@
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    message("shogun only supports static library linkage")
+    set(VCPKG_LIBRARY_LINKAGE "static")
+endif()
+
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
@@ -8,21 +13,49 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+file(REMOVE_RECURSE ${SOURCE_PATH}/cmake/external)
+file(MAKE_DIRECTORY ${SOURCE_PATH}/cmake/external)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/MSDirent.cmake DESTINATION ${SOURCE_PATH}/cmake/external)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-    	-DBUILD_META_EXAMPLES=OFF
-    	-DBUILD_EXAMPLES=OFF
-    	-DUSE_SVMLIGHT=OFF
-    	-DENABLE_TESTING=OFF
+        -DBUILD_META_EXAMPLES=OFF
+        -DBUILD_EXAMPLES=OFF
+        -DUSE_SVMLIGHT=OFF
+        -DENABLE_TESTING=OFF
+        -DLICENSE_GPL_SHOGUN=OFF
+        # Conflicting definitions in OpenBLAS and Eigen
+        -DENABLE_EIGEN_LAPACK=OFF
+
+        -DCMAKE_DISABLE_FIND_PACKAGE_JSON=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_ViennaCL=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_TFLogger=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_GLPK=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_CPLEX=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_ARPACK=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_Mosek=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_LpSolve=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_ColPack=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_ARPREC=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_Ctags=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_CCache=TRUE
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=TRUE
+
+        -DINSTALL_TARGETS=shogun-static
 )
 
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/shogun)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+file(REMOVE_RECURSE
+    # This directory is empty given the settings above
+    ${CURRENT_PACKAGES_DIR}/include/shogun/mathematics/linalg/backend
+    ${CURRENT_PACKAGES_DIR}/debug/include
+    ${CURRENT_PACKAGES_DIR}/debug/share
+)
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/shogun)
