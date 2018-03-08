44faa08dc367912dca867219380143d60c359688
PETITGENET AMBROISE <apetitgenet@adgroupe.priv>
2018-03-08 20:47:53 +0100

    Use original reposotory

--- ports/fmem/portfile.cmake
+++ ports/fmem/portfile.cmake
@@ -14,9 +14,9 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Ambrou/fmem
-    REF v1.1
-    SHA512 09c331e7fb0e7d9b145ff2b643a31b41361c0284e5347f90c2e06e785836b46bc76f9c04f95412f143f0e2214a7691f240fb0c5ad8f166f312c32d04fa4a99df
+    REPO c-libs/fmem
+    REF 2ccee3d2fb2fc72370a9bb2131bfc9167e0d9022
+    SHA512 4a63332eb5df7f30bdad9e4233171b5c21dd2b092e525e9dcc4f602295ffff50c555c80fd74d964bc3daeffd8001a9b852f1769ef3161259dd8a3cae3ca3a4df
     HEAD_REF master
 )
 
