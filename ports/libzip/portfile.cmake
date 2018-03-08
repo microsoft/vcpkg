aaa8258d5918bf3065127ab98bf05f8928877d65
Michał Janiszewski <janisozaur@gmail.com>
2018-03-08 15:57:02 +0100

    [libzip] Update with latest patches

--- ports/libzip/portfile.cmake
+++ ports/libzip/portfile.cmake
@@ -2,8 +2,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nih-at/libzip
-    REF rel-1-4-0
-    SHA512 3d8c5e64c567d2b91670ea041228d74cc8415116dfeb5c9bcf587ab817618eace668c5171122eeccf2a5f25242c2439c5f60b361f99a06274ab58aea720fe0bb
+    REF ad1f02d3b2aac532a89cbf8fd21f55677789b82d
+    SHA512 b1ed427916ec555ead454d39a244502f64bbc8799c20394944797b878196f1b9a9dda64be218fe89bb1747c841d7b36e8e7fb7b0be1fbdf777f049cd823635d6
 )
 
 # Patch cmake and configuration to allow static builds
@@ -11,7 +11,6 @@ vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES
         "${CMAKE_CURRENT_LIST_DIR}/cmake_dont_build_more_than_needed.patch"
-        "${CMAKE_CURRENT_LIST_DIR}/fix-attribute.patch"
 )
 
 vcpkg_configure_cmake(
