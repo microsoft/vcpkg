a577ee6efe80787eb3bcde454675578a80fb1d99
PETITGENET AMBROISE <apetitgenet@adgroupe.priv>
2018-03-10 19:15:06 +0100

    Use original reposotory

--- ports/gherkin-c/portfile.cmake
+++ ports/gherkin-c/portfile.cmake
@@ -15,17 +15,17 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Ambrou/gherkin-c
-    REF 9aba7b5e743398dfc1c82f6a82dde146fece5170
-    SHA512 e2bff1dd6943689e09d4210076fa7986f1f34cbbd48f03235df6c389a40fcb0c8256d514ea95e17475055dfeb5da106dcbc961a3dbfd0ddd3f814946290c18d8
+    REPO c-libs/gherkin-c
+    REF e63e83104b835e217140e9dc77d9ce2bb50f234e
+    SHA512 a99b3f695257b45df8ff7b8ec46bff28991cf2b9bc51a25247550471c724bd14ee64340db684f096131f47d7f4ff278d23dda546e7dfe29134bbc1dbccaf0d1e
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     #PREFER_NINJA
-    #OPTIONS 
-    #    -DBUILD_GHERKIN_TESTS=OFF
+    OPTIONS 
+        -DBUILD_GHERKIN_TESTS=OFF
 )
 vcpkg_install_cmake()
 
