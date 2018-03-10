5b40b2659c8abf3ba3858b3c317f831d41ed54db
PETITGENET AMBROISE <apetitgenet@adgroupe.priv>
2018-03-10 10:23:16 +0100

    Add gherkin-c port in vcpkg Microsoft tool

--- ports/gherkin-c/portfile.cmake
+++ ports/gherkin-c/portfile.cmake
@@ -24,8 +24,8 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     #PREFER_NINJA
-    OPTIONS 
-        -DBUILD_GHERKIN_TESTS=OFF
+    #OPTIONS 
+    #    -DBUILD_GHERKIN_TESTS=OFF
 )
 vcpkg_install_cmake()
 
