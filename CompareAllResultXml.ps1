e8731fc02aedfea97222f793f6c9cfb96a465355
Robert Schumacher <roschuma@microsoft.com>
2018-03-10 14:29:28 -0800

    Handle WinC triplets

--- CompareAllResultXml.ps1
+++ CompareAllResultXml.ps1
@@ -10,14 +10,26 @@ $myDir = split-path -parent $script:MyInvocation.MyCommand.Definition
 $leftresults = ls \\vcpkg-000\General\Results\${leftBuild}_*.xml
 $rightresults = ls \\vcpkg-000\General\Results\${rightBuild}_*.xml
 
+Write-Verbose "Left results"
+Write-Verbose "$leftresults"
+
+Write-Verbose "Right results"
+Write-Verbose "$rightresults"
+
 $allresults = @($leftresults) + @($rightresults)
 
-$groups = $allresults | % { $a = @($_.name -split "_"); New-Object PSObject -Property @{ "id"=$a[0]; "triplet"=$($a[1] -split ".xml")[0]; "file"=$_ } } | group triplet
+$groups = $allresults | % { $a = @($_.name -split "_", 2); New-Object PSObject -Property @{ "id"=$a[0]; "triplet"=$($a[1] -replace "(-dynamic)?(-stable|-unstable.*)?\.xml",""); "file"=$_ } } | group triplet
 
+$groups
 
 $groups | % {
     $_.Name
-    if ($_.Count -ne 2) { "Not enough results: "+$($_.Group | % file); continue }
-
-    & $myDir/CompareResultXml.ps1 $_.Group[0].file $_.Group[1].file
+    if ($_.Count -ne 2)
+    {
+        "Not enough results: "+$($_.Group | % file)
+    }
+    else
+    {
+        & $myDir/CompareResultXml.ps1 $_.Group[0].file $_.Group[1].file
+    }
 }
