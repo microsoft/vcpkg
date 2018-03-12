f2ab816e81518e5f7060c11cab24200c1256f239
gouranga <code@gouranga.ru>
2018-03-12 19:45:33 +0300

    [WTL] Update to 10.0.7336

--- ports/wtl/portfile.cmake
+++ ports/wtl/portfile.cmake
@@ -1,9 +1,9 @@
 #header-only library
 include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://downloads.sourceforge.net/project/wtl/WTL%209.1/WTL%209.1.5321%20Final/WTL91_5321_Final.zip?r=&ts=1477467616&use_mirror=netix"
-    FILENAME "WTL91_5321_Final.zip"
-    SHA512 52c583f6773839f7ad7fccf0ecba44ad00f41af4ae97d217619cc380ea9b71b90638ae35a5995f9eb08854db423896fec5f06b5cbd853f118eeddd05238a460a
+    URLS "http://downloads.sourceforge.net/project/wtl/WTL%2010/WTL%2010.0.7336/WTL10_7336.zip"
+    FILENAME "WTL10_7336.zip"
+    SHA512 68368be0b35fba97ed63fd684e98c077ab94ea2ce928693da7f1f48e2547486109e045e026218a7c8652aec46930b79572679c0b79afaa75a3dbf98572e4d9b5
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
@@ -13,3 +13,4 @@ file(COPY ${CURRENT_BUILDTREES_DIR}/src/MS-PL.txt DESTINATION ${CURRENT_PACKAGES
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/wtl/MS-PL.txt ${CURRENT_PACKAGES_DIR}/share/wtl/copyright)
 
 file(COPY ${CURRENT_BUILDTREES_DIR}/src/samples DESTINATION ${CURRENT_PACKAGES_DIR}/share/wtl)
+file(COPY ${CURRENT_BUILDTREES_DIR}/src/appwizard DESTINATION ${CURRENT_PACKAGES_DIR}/share/wtl)
