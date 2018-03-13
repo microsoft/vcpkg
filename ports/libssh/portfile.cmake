9de17844e0201a27132149e392152525f8e8d34b
Robert Schumacher <roschuma@microsoft.com>
2018-03-13 07:37:20 -0700

    [libssh] Only build one flavor at a time, don't build examples.

--- ports/libssh/portfile.cmake
+++ ports/libssh/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL WindowsStore)
-  message(FATAL_ERROR "WindowsStore not supported")
+    message(FATAL_ERROR "WindowsStore not supported")
 endif()
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libssh-0.7.5)
 vcpkg_download_distfile(ARCHIVE
@@ -11,23 +11,35 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES ${CMAKE_CURRENT_LIST_DIR}/build-one-flavor.patch
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" WITH_STATIC_LIB)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DWITH_STATIC_LIB=ON
+    OPTIONS
+        -DWITH_STATIC_LIB=${WITH_STATIC_LIB}
+        -DWITH_EXAMPLES=OFF
+        -DWITH_TESTING=OFF
+        -DWITH_NACL=OFF
+        -DWITH_GSSAPI=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  file(RENAME ${CURRENT_PACKAGES_DIR}/lib/static/ssh.lib ${CURRENT_PACKAGES_DIR}/lib/ssh.lib)
-  file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/static/ssh.lib ${CURRENT_PACKAGES_DIR}/debug/lib/ssh.lib)
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/static/ssh.lib ${CURRENT_PACKAGES_DIR}/lib/ssh.lib)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/static/ssh.lib ${CURRENT_PACKAGES_DIR}/debug/lib/ssh.lib)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 
-  file(READ ${CURRENT_PACKAGES_DIR}/include/libssh/libssh.h _contents)
-  string(REPLACE "#ifdef LIBSSH_STATIC" "#if 1" _contents "${_contents}")
-  file(WRITE ${CURRENT_PACKAGES_DIR}/include/libssh/libssh.h "${_contents}")
+    file(READ ${CURRENT_PACKAGES_DIR}/include/libssh/libssh.h _contents)
+    string(REPLACE "#ifdef LIBSSH_STATIC" "#if 1" _contents "${_contents}")
+    file(WRITE ${CURRENT_PACKAGES_DIR}/include/libssh/libssh.h "${_contents}")
 endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/static ${CURRENT_PACKAGES_DIR}/debug/lib/static)
