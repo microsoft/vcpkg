01c4289d068e071f6ef2d64ac3328644a62a7f7b
Robert Schumacher <roschuma@microsoft.com>
2018-03-14 11:34:59 -0700

    [zeromq] Update

--- ports/zeromq/portfile.cmake
+++ ports/zeromq/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeromq/libzmq
-    REF 18498f620f0f6d4076981ea16eb5760fe4d28dc2
-    SHA512 0c4a5c72455411f47283da3cad381600101be19a62437ad8e2c38e5f18fb6d621a3136e402c6eb9ba153f3d6333da9902335c2dacd8405094d4d1269df28d4af
+    REF 59516ed51a5c8ce7e1de0d9f938141f68507dd3c
+    SHA512 3a7b9875cdc09e5b24acb007fe4052e879fccd6e326b93b0c1e3fcffc13964e4de04289bda050846fe7ac1281fbb7ee03874b44db1323dada518c809417a2129
     HEAD_REF master
 )
 
@@ -31,7 +31,7 @@ vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/ZeroMQ)
+vcpkg_fixup_cmake_targets(CONFIG_PATH CMake)
 
 file(READ ${CURRENT_PACKAGES_DIR}/share/zeromq/ZeroMQConfig.cmake _contents)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
