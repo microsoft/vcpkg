3c62dc2b16f8eeb5a73c837ecea6c2310ec258cf
Youngho Kim <melchi45@gmail.com>
2018-03-22 12:51:45 +0900

    add mosquitto library for MQTT
    add mosquitto package version 1.4.15

--- ports/mosquitto/portfile.cmake
+++ ports/mosquitto/portfile.cmake
@@ -14,23 +14,12 @@ vcpkg_apply_patches(
         ${CMAKE_CURRENT_LIST_DIR}/cmake.patch
 )
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" AND VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    vcpkg_acquire_msys(MSYS_ROOT PACKAGES perl gcc diffutils make)
-else()
-    vcpkg_acquire_msys(MSYS_ROOT PACKAGES diffutils make)
-endif()
-set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
-set(ENV{INCLUDE} "${CURRENT_INSTALLED_DIR}/include;$ENV{INCLUDE}")
-set(ENV{LIB} "${CURRENT_INSTALLED_DIR}/lib;$ENV{LIB}")
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" LWS_WITH_STATIC)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" LWS_WITH_SHARED)
+file(COPY ${CURRENT_PORT_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+	PREFER_NINJA
     OPTIONS
-        -DLWS_WITH_STATIC=${LWS_WITH_STATIC}
-        -DLWS_WITH_SHARED=${LWS_WITH_SHARED}
 		-DWITH_SRV=OFF
 		-DWITH_WEBSOCKETS=ON
 		-DWITH_TLS=ON
@@ -51,43 +40,55 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-#vcpkg_fixup_cmake_targets(CONFIG_PATH "cmake")
-#vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/mosquitto")
-#if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/cmake/curl)
-#    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/curl)
-#elseif(EXISTS ${CURRENT_PACKAGES_DIR}/share/curl)
-#    vcpkg_fixup_cmake_targets(CONFIG_PATH share/curl)
-#endif()
-
-file(GLOB EXP_FILES ${CURRENT_PACKAGES_DIR}/*.exp ${CURRENT_PACKAGES_DIR}/debug/lib/*.exp)
-file(GLOB LIB_FILES ${CURRENT_PACKAGES_DIR}/*.lib ${CURRENT_PACKAGES_DIR}/debug/bin/*.lib)
-file(GLOB EXE_FILES ${CURRENT_PACKAGES_DIR}/*.exe ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-
-
+# Remove debug/include
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/aclfile.example)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/mosquitto.conf)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/mosquitto.exe)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/mosquitto_passwd.exe)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/mosquitto_pub.exe)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/mosquitto_sub.exe)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/pskfile.example)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/pwfile.example)
-#file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/libwebsockets-test-server)
-#file(READ ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsConfig.cmake LIBWEBSOCKETSCONFIG_CMAKE)
-#string(REPLACE "/../include" "/../../include" LIBWEBSOCKETSCONFIG_CMAKE "${LIBWEBSOCKETSCONFIG_CMAKE}")
-#file(WRITE ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsConfig.cmake "${LIBWEBSOCKETSCONFIG_CMAKE}")
-#file(READ ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsTargets-debug.cmake LIBWEBSOCKETSTARGETSDEBUG_CMAKE)
-#string(REPLACE "websockets_static.lib" "websockets.lib" LIBWEBSOCKETSTARGETSDEBUG_CMAKE "${LIBWEBSOCKETSTARGETSDEBUG_CMAKE}")
-#file(WRITE ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsTargets-debug.cmake "${LIBWEBSOCKETSTARGETSDEBUG_CMAKE}")
-#file(READ ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsTargets-release.cmake LIBWEBSOCKETSTARGETSRELEASE_CMAKE)
-#string(REPLACE "websockets_static.lib" "websockets.lib" LIBWEBSOCKETSTARGETSRELEASE_CMAKE "${LIBWEBSOCKETSTARGETSRELEASE_CMAKE}")
-#file(WRITE ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LibwebsocketsTargets-release.cmake "${LIBWEBSOCKETSTARGETSRELEASE_CMAKE}")
-#file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libwebsockets)
-#file(RENAME ${CURRENT_PACKAGES_DIR}/share/libwebsockets/LICENSE ${CURRENT_PACKAGES_DIR}/share/libwebsockets/copyright)
+
+# fix *.cmake
+#vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/mosquitto")
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+
+# Rename dll
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto.dll ${CURRENT_PACKAGES_DIR}/bin/mosquitto.dll)
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquittopp.dll ${CURRENT_PACKAGES_DIR}/bin/mosquittopp.dll)
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_passwd.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_passwd.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_pub.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_pub.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_sub.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_sub.exe)
+
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto.dll ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto.dll)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquittopp.dll ${CURRENT_PACKAGES_DIR}/debug/bin/mosquittopp.dll)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_passwd.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_passwd.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_pub.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_pub.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_sub.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_sub.exe)
+
+# Remove exe files
+file(GLOB EXE ${CURRENT_PACKAGES_DIR}/bin/*.exe)
+file(GLOB DEBUG_EXE ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
+file(REMOVE ${EXE})
+file(REMOVE ${DEBUG_EXE})
+
+file(GLOB LIB_FILES          	${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/*.lib)
+file(GLOB DEBUG_LIB_FILES    	${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/*.lib)
+file(GLOB CPP_LIB_FILES         ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/cpp/*.lib)
+file(GLOB DEBUG_CPP_LIB_FILES   ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/cpp/*.lib)
+
+file(COPY ${DEBUG_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
+file(COPY ${DEBUG_CPP_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
+file(COPY ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
+file(COPY ${CPP_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
+
+
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/mosquitto RENAME copyright)
+
 #if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
 #    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/websockets_static.lib ${CURRENT_PACKAGES_DIR}/debug/lib/websockets.lib)
 #    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/websockets_static.lib ${CURRENT_PACKAGES_DIR}/lib/websockets.lib)
 #endif ()
+
+# Copy pdb
 vcpkg_copy_pdbs()
