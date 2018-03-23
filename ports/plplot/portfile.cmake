7e61e3eb1b5397b5d3f9fcaf56c0e3b7750c39fd
Robert Schumacher <roschuma@microsoft.com>
2018-03-23 16:22:38 -0700

    [plplot] Improve handling of cmake scripts. Avoid DLLS_WITHOUT_LIBS. Disable unused dependencies and require some.

--- ports/plplot/portfile.cmake
+++ ports/plplot/portfile.cmake
@@ -3,7 +3,6 @@ include(vcpkg_common_functions)
 set(PLPLOT_VERSION 5.13.0)
 set(PLPLOT_HASH 1d5cb5da17d4bde6d675585bff1f8dcb581719249a0b2687867e767703f8dab0870e7ea44b9549a497f4ac0141a3cabf6761c49520c0e2b26ffe581468512cbb)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/plplot-${PLPLOT_VERSION})
-SET(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://sourceforge.net/projects/plplot/files/plplot/${PLPLOT_VERSION}%20Source/plplot-${PLPLOT_VERSION}.tar.gz/download"
@@ -27,6 +26,11 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
+        -DENABLE_tcl=OFF
+        -DPL_HAVE_QHULL=OFF
+        -DENABLE_qt=OFF
+        -DPLPLOT_USE_QT5=OFF
+        -DENABLE_ocaml=OFF
         -DPL_DOUBLE=ON
         -DENABLE_wxwidgets=${ENABLE_wxwidgets}
         -DPLD_wxpng=${ENABLE_wxwidgets}
@@ -41,6 +45,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/plplot)
+
 # Remove unnecessary tool
 file(REMOVE
     ${CURRENT_PACKAGES_DIR}/debug/bin/pltek.exe
@@ -52,35 +58,14 @@ file(REMOVE_RECURSE
         ${CURRENT_PACKAGES_DIR}/debug/include
 )
 
-# Copy CMake Package Config scripts
-file(COPY
-        ${CURRENT_PACKAGES_DIR}/lib/cmake/plplot
-    DESTINATION
-        ${CURRENT_PACKAGES_DIR}/share
-    FILES_MATCHING PATTERN
-        "*.cmake"
-)
-file(COPY
-        ${CURRENT_PACKAGES_DIR}/debug/lib/cmake/plplot
-    DESTINATION
-        ${CURRENT_PACKAGES_DIR}/debug/share
-    FILES_MATCHING PATTERN
-        "*.cmake"
-)
-file(REMOVE_RECURSE
-        ${CURRENT_PACKAGES_DIR}/lib/cmake
-        ${CURRENT_PACKAGES_DIR}/debug/lib/cmake
-)
-
 file(INSTALL
     ${SOURCE_PATH}/Copyright
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/plplot
     RENAME copyright
 )
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "share/plplot")
-
 vcpkg_copy_pdbs()
+
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/share
 )
