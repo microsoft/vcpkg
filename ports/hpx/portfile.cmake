444e25056cbc786a238047d1458d18fda071f269
Robert Schumacher <roschuma@microsoft.com>
2018-03-24 15:38:40 -0700

    [hpx] Use vcpkg_fixup_cmake_targets

--- ports/hpx/portfile.cmake
+++ ports/hpx/portfile.cmake
@@ -1,9 +1,9 @@
 include(vcpkg_common_functions)
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)                                          
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
     message(STATUS "Warning: Static building of HPX not supported yet. Building dynamic.") 
-    set(VCPKG_LIBRARY_LINKAGE dynamic)                                              
-endif()                                                                             
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -13,15 +13,12 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-SET(BOOST_PATH "${CURRENT_INSTALLED_DIR}/share/boost")
-SET(HWLOC_PATH "${CURRENT_INSTALLED_DIR}/share/hwloc")
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DBOOST_ROOT=${BOOST_PATH}
-        -DHWLOC_ROOT=${HWLOC_ROOT}
+        "-DBOOST_ROOT=${CURRENT_INSTALLED_DIR}/share/boost"
+        "-DHWLOC_ROOT=${CURRENT_INSTALLED_DIR}/share/hwloc"
         -DHPX_WITH_VCPKG=ON
         -DHPX_WITH_HWLOC=ON
         -DHPX_WITH_TESTS=OFF
@@ -33,45 +30,56 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 # post build cleanup
-if(NOT VCPKG_USE_HEAD_VERSION)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/share/hpx-1.1.0 ${CURRENT_PACKAGES_DIR}/share/hpx)
-else()
-    file(RENAME ${CURRENT_PACKAGES_DIR}/share/hpx-1.2.0 ${CURRENT_PACKAGES_DIR}/share/hpx)
-endif()
+file(GLOB SHARE_DIR ${CURRENT_PACKAGES_DIR}/share/hpx-*)
+file(RENAME ${SHARE_DIR} ${CURRENT_PACKAGES_DIR}/share/hpx)
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/HPX)
+
+file(GLOB_RECURSE CMAKE_FILES "${CURRENT_PACKAGES_DIR}/share/hpx/*.cmake")
+foreach(CMAKE_FILE IN LISTS CMAKE_FILES)
+    file(READ ${CMAKE_FILE} _contents)
+    string(REGEX REPLACE
+        "lib/([A-Za-z0-9_.-]+\\.dll)"
+        "bin/\\1"
+        _contents "${_contents}")
+    string(REGEX REPLACE
+        "lib/hpx/([A-Za-z0-9_.-]+\\.dll)"
+        "bin/\\1"
+        _contents "${_contents}")
+    file(WRITE ${CMAKE_FILE} "${_contents}")
+endforeach()
+
+file(READ "${CURRENT_PACKAGES_DIR}/share/hpx/HPXMacros.cmake" _contents)
+string(REPLACE "set(CMAKE_MODULE_PATH \${CMAKE_MODULE_PATH} \"\${CMAKE_CURRENT_LIST_DIR}/../../lib/cmake/HPX\")" "list(APPEND CMAKE_MODULE_PATH \"\${CMAKE_CURRENT_LIST_DIR}\")" _contents "${_contents}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/hpx/HPXMacros.cmake" "${_contents}")
 
 file(INSTALL
     ${SOURCE_PATH}/LICENSE_1_0.txt
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/hpx RENAME copyright)
 
-file(GLOB __hpx_cmakes ${CURRENT_PACKAGES_DIR}/lib/cmake/HPX/*.*)
-foreach(__hpx_cmake ${__hpx_cmakes})
-    file(COPY ${__hpx_cmake} DESTINATION ${CURRENT_PACKAGES_DIR}/share/hpx/cmake)
-    file(REMOVE ${__hpx_cmake})
-endforeach()
+file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/lib/*.dll)
+if(DLLS)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE ${DLLS})
+endif()
 
-file(GLOB __hpx_dlls ${CURRENT_PACKAGES_DIR}/lib/*.dll)
-foreach(__hpx_dll ${__hpx_dlls})
-    file(COPY ${__hpx_dll} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-    file(REMOVE ${__hpx_dll})
-endforeach()
+file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/lib/hpx/*.dll)
+if(DLLS)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE ${DLLS})
+endif()
 
-file(GLOB __hpx_dlls ${CURRENT_PACKAGES_DIR}/lib/hpx/*.dll)
-foreach(__hpx_dll ${__hpx_dlls})
-    file(COPY ${__hpx_dll} DESTINATION ${CURRENT_PACKAGES_DIR}/bin/hpx)
-    file(REMOVE ${__hpx_dll})
-endforeach()
+file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/debug/lib/*.dll)
+if(DLLS)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(REMOVE ${DLLS})
+endif()
 
-file(GLOB __hpx_dlls ${CURRENT_PACKAGES_DIR}/debug/lib/*.dll)
-foreach(__hpx_dll ${__hpx_dlls})
-    file(COPY ${__hpx_dll} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(REMOVE ${__hpx_dll})
-endforeach()
-
-file(GLOB __hpx_dlls ${CURRENT_PACKAGES_DIR}/debug/lib/hpx/*.dll)
-foreach(__hpx_dll ${__hpx_dlls})
-    file(COPY ${__hpx_dll} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin/hpx)
-    file(REMOVE ${__hpx_dll})
-endforeach()
+file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/debug/lib/hpx/*.dll)
+if(DLLS)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(REMOVE ${DLLS})
+endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/bazel)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
@@ -84,4 +92,3 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
 
 vcpkg_copy_pdbs()
-
