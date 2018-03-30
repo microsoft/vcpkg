912ff8a3adcd058f08f23f512d230a809ee356bc
Robert Schumacher <roschuma@microsoft.com>
2018-03-30 16:31:24 -0700

    [mosquitto] Use patches to simplify portfile.

--- ports/mosquitto/portfile.cmake
+++ ports/mosquitto/portfile.cmake
@@ -1,5 +1,14 @@
 include(vcpkg_common_functions)
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    message("mosquitto only supports dynamic linkage")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+    message(FATAL_ERROR "mosquitto does not support static CRT linkage")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO eclipse/mosquitto
@@ -11,31 +20,24 @@ vcpkg_from_github(
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/0001-win64-cmake.patch
+        "${CMAKE_CURRENT_LIST_DIR}/0001-win64-cmake.patch"
+        "${CMAKE_CURRENT_LIST_DIR}/cmake.patch"
+        "${CMAKE_CURRENT_LIST_DIR}/cmake-2.patch"
 )
-
-file(COPY ${CURRENT_PORT_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-	PREFER_NINJA
+    PREFER_NINJA
     OPTIONS
-		-DWITH_SRV=OFF
-		-DWITH_WEBSOCKETS=ON
-		-DWITH_TLS=ON
-		-DWITH_TLS_PSK=ON
-		-DWITH_THREADING=ON
-		-DVCPKG_ROOT_DIR=${VCPKG_ROOT_DIR}
-		-DTARGET_TRIPLET=${TARGET_TRIPLET}
-		-DCMAKE_VS_INCLUDE_INSTALL_TO_DEFAULT_BUILD=OFF
-	OPTIONS_RELEASE
-		-DENABLE_DEBUG=OFF
-		-DOPTIMIZE=1
-	OPTIONS_DEBUG
-		-DENABLE_DEBUG=ON
-		-DDEBUGGABLE=1
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+        -DWITH_SRV=OFF
+        -DWITH_WEBSOCKETS=ON
+        -DWITH_TLS=ON
+        -DWITH_TLS_PSK=ON
+        -DWITH_THREADING=ON
+    OPTIONS_RELEASE
+        -DENABLE_DEBUG=OFF
+    OPTIONS_DEBUG
+        -DENABLE_DEBUG=ON
 )
 
 vcpkg_install_cmake()
@@ -43,52 +45,12 @@ vcpkg_install_cmake()
 # Remove debug/include
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-# fix *.cmake
-#vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/mosquitto")
-
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-# Rename dll
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto.dll ${CURRENT_PACKAGES_DIR}/bin/mosquitto.dll)
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquittopp.dll ${CURRENT_PACKAGES_DIR}/bin/mosquittopp.dll)
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_passwd.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_passwd.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_pub.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_pub.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/mosquitto_sub.exe ${CURRENT_PACKAGES_DIR}/bin/mosquitto_sub.exe)
-
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto.dll ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto.dll)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquittopp.dll ${CURRENT_PACKAGES_DIR}/debug/bin/mosquittopp.dll)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_passwd.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_passwd.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_pub.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_pub.exe)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/mosquitto_sub.exe ${CURRENT_PACKAGES_DIR}/debug/bin/mosquitto_sub.exe)
-
-# Remove exe files
 file(GLOB EXE ${CURRENT_PACKAGES_DIR}/bin/*.exe)
 file(GLOB DEBUG_EXE ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
 file(REMOVE ${EXE})
 file(REMOVE ${DEBUG_EXE})
 
-file(GLOB LIB_FILES          	${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/Release/*.lib)
-file(GLOB DEBUG_LIB_FILES    	${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/Debug/*.lib)
-file(GLOB CPP_LIB_FILES         ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib/cpp/Release/*.lib)
-file(GLOB DEBUG_CPP_LIB_FILES   ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib/cpp/Debug/*.lib)
-
-file(COPY ${DEBUG_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
-file(COPY ${DEBUG_CPP_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/)
-file(COPY ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
-file(COPY ${CPP_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
-
-
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/mosquitto RENAME copyright)
-
-#if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-#    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/websockets_static.lib ${CURRENT_PACKAGES_DIR}/debug/lib/websockets.lib)
-#    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/websockets_static.lib ${CURRENT_PACKAGES_DIR}/lib/websockets.lib)
-#endif ()
 
 # Copy pdb
 vcpkg_copy_pdbs()
