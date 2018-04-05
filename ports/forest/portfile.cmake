008d8d1c46b5fe2bcd5c01776bf0cd3768013542
George Fotopoulos <xorz57@gmail.com>
2018-04-06 01:55:00 +0300

    Update portfile.cmake

--- ports/forest/portfile.cmake
+++ ports/forest/portfile.cmake
@@ -1,10 +1,10 @@
 # Ηeader-only library
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/forest-4.5.0)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/forest-7.0.1)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/xorz57/forest/archive/4.5.0.zip"
-    FILENAME "forest-4.5.0.zip"
+    URLS "https://github.com/xorz57/forest/archive/7.0.1.zip"
+    FILENAME "forest-7.0.1.zip"
     SHA512 ae256ad38802d0827cfcd45ffae35ddb95cf74e38cf3e5d806f6e2215f701abfb8159f82e2bb6362788fe96a9f9008429d366e7abbc7980b29b3528052cfe43e
 )
 vcpkg_extract_source_archive(${ARCHIVE})
