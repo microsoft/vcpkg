78434c55213d705ff464a4c413c2fe29437fd89e
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-04-24 10:16:59 +0200

    Fix PCL compile tools
    Add missing boost definition using a patch.
    
    Better compile from HEAD.

--- ports/pcl/portfile.cmake
+++ ports/pcl/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_apply_patches(
             "${CMAKE_CURRENT_LIST_DIR}/find_openni2.patch"
             "${CMAKE_CURRENT_LIST_DIR}/find_cuda.patch"
             "${CMAKE_CURRENT_LIST_DIR}/vs2017-15.4-workaround.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/boost_uuid_random_generator_compat.patch"
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PCL_SHARED_LIBS)
