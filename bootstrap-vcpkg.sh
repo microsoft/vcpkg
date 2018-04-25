237268f29d90c270f9955eb86cc5471bbb47d22a
Alexander Karatarakis <alkarata@microsoft.com>
2018-04-25 13:19:49 -0700

    [sh] Change to #!/bin/sh

--- bootstrap-vcpkg.sh
+++ bootstrap-vcpkg.sh
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 #!/usr/bin/env bash
+=======
+#!/bin/sh
+>>>>>>> Stashed changes
 
 vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 $vcpkgRootDir/scripts/bootstrap.sh
