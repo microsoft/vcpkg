d1b9a8637c84d185dd2c99249bae41864987bf05
Alexander Karatarakis <alkarata@microsoft.com>
2018-04-25 14:36:31 -0700

    [sh] Remove merge conflict lines

--- bootstrap-vcpkg.sh
+++ bootstrap-vcpkg.sh
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-#!/usr/bin/env bash
-=======
 #!/bin/sh
->>>>>>> Stashed changes
 
 vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
 $vcpkgRootDir/scripts/bootstrap.sh
