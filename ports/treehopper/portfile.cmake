01ef24dd654022465831efc21468fa9c7f3a9474
Jay Carlson <jcarlson@unl.edu>
2018-04-27 23:48:11 -0500

    Bump to 1.11.3

--- ports/treehopper/portfile.cmake
+++ ports/treehopper/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO treehopper-electronics/treehopper-sdk
-    REF 1.11.2
-    SHA512 6bf8bf175d1488ebfb44d9949a07c1a6754dff58ba7ea1ee745ef25eba087016eea978f025f5f73429a13b9e0e45cc074f1e1cdb86b2f76867d8ff9c086811cd
+    REF 1.11.3
+    SHA512 65b748375b798787c8b59f9657151f340920c939c3f079105b9b78f4e3b775125598106c6dfa4feba111a64d30f007003a70110ac767802a7dd1127a25c9fb14
     HEAD_REF master)
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIB)
