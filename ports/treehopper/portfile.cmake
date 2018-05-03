921437f896617fc274367d56e13b7203bc4db4fb
Robert Schumacher <roschuma@microsoft.com>
2018-05-02 21:40:03 -0700

    [treehopper] Fix cmake targets. Block non-dynamic usage.

--- ports/treehopper/portfile.cmake
+++ ports/treehopper/portfile.cmake
@@ -1,13 +1,19 @@
 include(vcpkg_common_functions)
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    message("treehopper only supports dynamic library linkage")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+    message(FATAL_ERROR "treehopper does not support static crt linkage")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO treehopper-electronics/treehopper-sdk
     REF 1.11.3
     SHA512 65b748375b798787c8b59f9657151f340920c939c3f079105b9b78f4e3b775125598106c6dfa4feba111a64d30f007003a70110ac767802a7dd1127a25c9fb14
     HEAD_REF master)
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIB)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}/C++/API/
@@ -16,6 +22,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/treehopper)
 
 file(INSTALL ${SOURCE_PATH}/C++/API/inc/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/Treehopper/)
 
