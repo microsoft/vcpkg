245f4f179e1b94a54a8adf87062bb579bc92bf7f
Robert Schumacher <roschuma@microsoft.com>
2018-05-02 21:46:21 -0700

    [cmark] Only build one of static/dynamic

--- ports/cmark/portfile.cmake
+++ ports/cmark/portfile.cmake
@@ -1,5 +1,10 @@
 include(vcpkg_common_functions)
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    # Note: fails due to missing cmark_export.h -- fix should be to always generate the correct export header.
+    message(FATAL_ERROR "cmark does not currently support static library linkage")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO commonmark/cmark
@@ -8,9 +13,16 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" CMARK_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" CMARK_SHARED)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DCMARK_TESTS=OFF
+        -DCMARK_SHARED=${CMARK_SHARED}
+        -DCMARK_STATIC=${CMARK_STATIC}
 )
 
 vcpkg_install_cmake()
@@ -21,6 +33,8 @@ file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/cm
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+else()
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/cmark.exe ${CURRENT_PACKAGES_DIR}/debug/bin/cmark.exe)
 endif()
