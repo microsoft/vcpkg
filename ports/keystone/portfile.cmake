a1ec38454e70636e1c9f2262fbb1ad4b4c0e216f
Robert Schumacher <roschuma@microsoft.com>
2018-05-18 08:10:23 -0700

    [keystone] Small portfile simplifications

--- ports/keystone/portfile.cmake
+++ ports/keystone/portfile.cmake
@@ -1,11 +1,13 @@
 include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
+    REPO keystone-engine/keystone
     REF 0.9.1
-    REPO "keystone-engine/keystone"
     SHA512 3bd9c3793d5862a0a8628b52da381625e62445f661dbd5ed715723e7d9db01ce3f9586e5ca0a5cc71e8ceb6c912050f5c28e3ccd540e72c8abcfd2f0d1bbae17
     HEAD_REF master
 )
+
+vcpkg_find_acquire_program(PYTHON2)
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" KEYSTONE_BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" KEYSTONE_BUILD_SHARED)
@@ -16,12 +18,10 @@ vcpkg_configure_cmake(
     OPTIONS
         -DKEYSTONE_BUILD_STATIC=${KEYSTONE_BUILD_STATIC}
         -DKEYSTONE_BUILD_SHARED=${KEYSTONE_BUILD_SHARED}
+        -DPYTHON_EXECUTABLE=${PYTHON2}
 
         # Add support for only a subset of architectures
         #-DLLVM_TARGETS_TO_BUILD="AArch64;X86"
-
-    OPTIONS_RELEASE
-        -DCMAKE_BUILD_TYPE=Release
 )
 
 vcpkg_install_cmake()
@@ -34,17 +34,18 @@ if(EXES)
 endif()
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+else()
+    # Move DLLs
+    file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/lib/*.dll)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE ${DLLS})
+    file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/debug/lib/*.dll)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(REMOVE ${DLLS})
 endif()
-
-# Move DLLs
-file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/lib/*.dll)
-file(INSTALL ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(REMOVE ${DLLS})
-file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/debug/lib/*.dll)
-file(INSTALL ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(REMOVE ${DLLS})
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/COPYING
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/keystone 
-    RENAME copyright)
+    RENAME copyright
+)
