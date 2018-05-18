555e80f5d7d77782c561968936c1d9ea1f30d994
Robert Schumacher <roschuma@microsoft.com>
2018-05-18 05:14:37 -0700

    [sdl2pp] Use patch instead of replacing CMakeLists.txt. Use vcpkg_from_github()

--- ports/sdl2pp/portfile.cmake
+++ ports/sdl2pp/portfile.cmake
@@ -1,30 +1,22 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libSDL2pp-0.16.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/libSDL2pp/libSDL2pp/archive/0.16.0.zip"
-    FILENAME "0.16.0.zip"
-    SHA512 fb8f89cf0bc04478c0b1d14654003224456afd849bddb1f352e8d78a308fcd0957f065f7d4513479c83edc9ceac931eeb527886be1b1ac92d0fa1f3e707138f9
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO libSDL2pp/libSDL2pp
+    REF 0.16.0
+    SHA512 36603a0b1c3ba9294fffa5368357866e5689ceed9743352ff52c096d8b0070cc3f8708a5e837c10c871b410b6bda3ed7e8e3b95cb9afc136d91afb035cde6361
+    HEAD_REF master
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/find-debug-libs.patch"
 )
-vcpkg_extract_source_archive(${ARCHIVE})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL2PP_STATIC)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DSDL2PP_STATIC=1
-    OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA
+    OPTIONS
+        -DSDL2PP_WITH_EXAMPLES=OFF
+        -DSDL2PP_WITH_TESTS=OFF
+        -DSDL2PP_STATIC=${SDL2PP_STATIC}
 )
 
 vcpkg_install_cmake()
