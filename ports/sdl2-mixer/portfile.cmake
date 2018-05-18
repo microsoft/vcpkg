31f418c2b3594766d36167a1f0ba8e3e592a4dc5
Ghabry <gabriel+github@mastergk.de>
2018-05-18 15:57:24 +0200

    SDL2 mixer: Add features for libflac, libmodplus, libvorbis and mpg123

--- ports/sdl2-mixer/portfile.cmake
+++ ports/sdl2-mixer/portfile.cmake
@@ -8,14 +8,38 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive(${ARCHIVE})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
+if("libflac" IN_LIST FEATURES)
+    set(SDL_MIXER_ENABLE_FLAC ON)
+else()
+    set(SDL_MIXER_ENABLE_FLAC OFF)
+endif()
+
+if("libmodplug" IN_LIST FEATURES)
+    set(SDL_MIXER_ENABLE_MOD ON)
+else()
+    set(SDL_MIXER_ENABLE_MOD OFF)
+endif()
+
+if("libvorbis" IN_LIST FEATURES)
+    set(SDL_MIXER_ENABLE_OGGVORBIS ON)
+else()
+    set(SDL_MIXER_ENABLE_OGGVORBIS OFF)
+endif()
+
+if("mpg123" IN_LIST FEATURES)
+    set(SDL_MIXER_ENABLE_MP3 ON)
+else()
+    set(SDL_MIXER_ENABLE_MP3 OFF)
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DSDL_MIXER_ENABLE_MP3=ON       # mpg123
-        -DSDL_MIXER_ENABLE_FLAC=ON      # libflac
-        -DSDL_MIXER_ENABLE_MOD=ON       # libmodplug
-        -DSDL_MIXER_ENABLE_OGGVORBIS=ON # libvorbis 
+        -DSDL_MIXER_ENABLE_MP3=${SDL_MIXER_ENABLE_MP3}             # mpg123
+        -DSDL_MIXER_ENABLE_FLAC=${SDL_MIXER_ENABLE_FLAC}           # libflac
+        -DSDL_MIXER_ENABLE_MOD=${SDL_MIXER_ENABLE_MOD}             # libmodplug
+        -DSDL_MIXER_ENABLE_OGGVORBIS=${SDL_MIXER_ENABLE_OGGVORBIS} # libvorbis 
     OPTIONS_DEBUG
         -DSDL_MIXER_SKIP_HEADERS=ON)
 
