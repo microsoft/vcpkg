b142632afdba938a7fc7386074c5d8446e1d7ab0
Robert Schumacher <roschuma@microsoft.com>
2018-05-19 18:08:17 -0700

    [soci] Upgrade to 3.2.3

--- ports/soci/portfile.cmake
+++ ports/soci/portfile.cmake
@@ -2,26 +2,29 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SOCI/soci
-    REF 6eb1a3e9775ab7cdbf0f7f5aa5891792313cd8d9
-    SHA512 0d0127e422934c5ac707184b519b7682cb67d1480ebecf56520d085c9d29381075c1e2f7bfd8f5b7873ce3cc8ce35ba793e06f0c1f8bb506a83949cd27d15015
+    REF 3.2.3
+    SHA512 8c597b37efe82c85e6d951f66cb0f818d2c12cb673914bc7b322bc0a9da676e6c02f221c9104fb06d1b4b02fed4e5a4fb872dd3370b9117f248c3b948faf4fb3
     HEAD_REF master
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/no-define-snprintf.patch"
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SOCI_DYNAMIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SOCI_STATIC)
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH ${SOURCE_PATH}/src
+    PREFER_NINJA
     OPTIONS
         -DSOCI_TESTS=OFF
         -DSOCI_CXX_C11=ON
-        -DSOCI_LIBDIR=lib # This is to always have output in the lib folder and not lib64 for 64-bit builds
+        -DSOCI_LIBDIR:STRING=lib # This is to always have output in the lib folder and not lib64 for 64-bit builds
+        -DLIBDIR:STRING=lib
         -DSOCI_STATIC=${SOCI_STATIC}
         -DSOCI_SHARED=${SOCI_DYNAMIC}
 
-        -DWITH_BOOST=ON
         -DWITH_SQLITE3=ON
 
+        -DWITH_BOOST=OFF
         -DWITH_MYSQL=OFF
         -DWITH_ODBC=OFF
         -DWITH_ORACLE=OFF
@@ -32,14 +35,10 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(RENAME ${CURRENT_PACKAGES_DIR}/cmake/SOCI.cmake ${CURRENT_PACKAGES_DIR}/cmake/SOCIConfig.cmake)
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
-
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/soci)
+file(COPY ${SOURCE_PATH}/src/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/soci)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/soci/LICENSE_1_0.txt ${CURRENT_PACKAGES_DIR}/share/soci/copyright)
 
 vcpkg_copy_pdbs()
