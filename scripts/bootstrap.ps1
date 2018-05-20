a739df905c129eb13d7060419a78f5a693974b63
Alexander Karatarakis <alkarata@microsoft.com>
2018-05-19 17:54:50 -0700

    Merge VcpkgPowershellUtils into bootstrap.ps1

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -5,10 +5,33 @@ param(
 )
 Set-StrictMode -Version Latest
 $scriptsDir = split-path -parent $script:MyInvocation.MyCommand.Definition
-. "$scriptsDir\VcpkgPowershellUtils.ps1"
-
 $vcpkgRootDir = $scriptsDir
 $withVSPath = $withVSPath -replace "\\$" # Remove potential trailing backslash
+function vcpkgHasProperty([Parameter(Mandatory=$true)][AllowNull()]$object, [Parameter(Mandatory=$true)]$propertyName)
+{
+    if ($object -eq $null)
+    {
+        return $false
+    }
+
+    return [bool]($object.psobject.Properties | Where-Object { $_.Name -eq "$propertyName"})
+}
+
+function getProgramFiles32bit()
+{
+    $out = ${env:PROGRAMFILES(X86)}
+    if ($out -eq $null)
+    {
+        $out = ${env:PROGRAMFILES}
+    }
+
+    if ($out -eq $null)
+    {
+        throw "Could not find [Program Files 32-bit]"
+    }
+
+    return $out
+}
 
 while (!($vcpkgRootDir -eq "") -and !(Test-Path "$vcpkgRootDir\.vcpkg-root"))
 {
