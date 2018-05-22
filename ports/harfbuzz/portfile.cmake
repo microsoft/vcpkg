9e87854955392fe1a29ea7cfbe78e6b14e101e04
past-due <30942300+past-due@users.noreply.github.com>
2018-05-22 15:24:29 -0400

    [harfbuzz] Use Features for UCDN & Glib
    Permit configuration of UCDN and Glib Unicode callback support via features.

--- ports/harfbuzz/portfile.cmake
+++ ports/harfbuzz/portfile.cmake
@@ -15,12 +15,23 @@ vcpkg_apply_patches(
         "${CMAKE_CURRENT_LIST_DIR}/find-package-freetype-2.patch"
 )
 
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_CMAKE_SYSTEM_NAME)
-    SET(HAVE_GLIB "OFF")
-    SET(BUILTIN_UCDN "ON")
-else()
-    SET(HAVE_GLIB "ON")
-    SET(BUILTIN_UCDN "OFF")
+## Unicode callbacks
+
+# Builtin (UCDN)
+set(BUILTIN_UCDN OFF)
+if("ucdn" IN_LIST FEATURES)
+    set(BUILTIN_UCDN ON)
+endif()
+
+# Glib
+set(HAVE_GLIB OFF)
+if("glib" IN_LIST FEATURES)
+    set(HAVE_GLIB ON)
+endif()
+
+# At least one Unicode callback must be specified, or harfbuzz compilation fails
+if(NOT (BUILTIN_UCDN OR HAVE_GLIB))
+    message(FATAL_ERROR "Error: At least one Unicode callback must be specified (ucdn, glib).")
 endif()
 
 vcpkg_configure_cmake(
@@ -28,8 +39,8 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         -DHB_HAVE_FREETYPE=ON
-        -DHB_HAVE_GLIB=${HAVE_GLIB}
         -DHB_BUILTIN_UCDN=${BUILTIN_UCDN}
+        -DHB_HAVE_GLIB=${HAVE_GLIB}
     OPTIONS_DEBUG
         -DSKIP_INSTALL_HEADERS=ON
 )
