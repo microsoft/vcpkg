7046c470bd8dc3db9a2eaefc93b90f03a2c449e5
Alexander Karatarakis <alkarata@microsoft.com>
2018-05-30 18:48:31 -0700

    [boost-iostream] Fix version and SOURCE_PATH reference

--- ports/boost-iostreams/portfile.cmake
+++ ports/boost-iostreams/portfile.cmake
@@ -19,7 +19,7 @@ vcpkg_download_distfile(LICENSE
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_apply_patches(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ost-1.67.0-28e6d2af2f
+    SOURCE_PATH ${SOURCE_PATH}
     PATCHES "${CURRENT_PORT_DIR}/Removeseekpos.patch"
 )
 
