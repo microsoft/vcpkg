e2c34caa1f0018fbec3e7d967e0c08b64e3a099c
Robert Schumacher <roschuma@microsoft.com>
2018-06-16 15:53:29 -0700

    [json11] Adjust version due to lack of upstream version scheme

--- ports/json11/portfile.cmake
+++ ports/json11/portfile.cmake
@@ -15,18 +15,15 @@ endif()
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DJSON11_WITH_CMAKE_PACKAGE:BOOL=ON
-            -DBUILD_STATIC_LIBS:BOOL=ON
-            -DBUILD_SHARED_LIBS:BOOL=OFF
-            -DJSON11_BUILD_TESTS:BOOL=OFF
-            -DJSON11_ENABLE_DR1467_CANARY:BOOL=OFF
+    OPTIONS
+        -DJSON11_BUILD_TESTS:BOOL=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/json11)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/json11/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/json11/copyright)
-
