8e6b372f77acdd0decca812dff038e69a8294697
Alexander Karatarakis <alkarata@microsoft.com>
2018-06-18 17:10:10 -0700

    [nanorange] Fix description, --head only, don't use the single-header

--- ports/nanorange/portfile.cmake
+++ ports/nanorange/portfile.cmake
@@ -4,8 +4,6 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tcbrindle/NanoRange
-    REF 60be620449c762794b0664e4232c461ed4a51d82
-    SHA512 9f03597c80a4fa2d287dca571cdfbddc9d93ee4402bf73d9dcb3dbc45b93931b0f028e068ff2d165a9efdfdb5761223139f7f0966d405689dcc1794710281c80
     HEAD_REF master
 )
 
@@ -17,7 +15,8 @@ vcpkg_from_github(
 #vcpkg_build_cmake()
 #</tests>
 
-file(COPY ${SOURCE_PATH}/single_include/nanorange.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+# Copy header files
+file(COPY ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR} FILES_MATCHING PATTERN "*.hpp")
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/nanorange)
