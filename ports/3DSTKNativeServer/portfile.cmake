36f3070a9d08537d8bd6d70662575699180f8e9a
Kanishk Tantia <t-katan@microsoft.com>
2018-06-22 14:54:32 -0400

    Added github REF support so no need for releases

--- ports/3DSTKNativeServer/portfile.cmake
+++ ports/3DSTKNativeServer/portfile.cmake
@@ -12,19 +12,27 @@
 
 include(vcpkg_common_functions)
 
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/v1.0)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/CatalystCode/3DStreamingToolkit/archive/v1.0.tar.gz"
-    FILENAME "v1.0.tar.gz"
-    SHA512 344ca5199965a01d53874df41cf4d1f7010e7f7240abd65237d2444fc5146fa20bbd89233c268598ab907f10b1f1a0a5331631ea7023c27832d640baa06cb41a
+#vcpkg_download_distfile(ARCHIVE
+#    URLS "https://github.com/CatalystCode/3DStreamingToolkit/archive/v1.0.tar.gz"
+#    FILENAME "v1.0.tar.gz"
+#    SHA512 344ca5199965a01d53874df41cf4d1f7010e7f7240abd65237d2444fc5146fa20bbd89233c268598ab907f10b1f1a0a5331631ea7023c27832d640baa06cb41a
+#)
+
+#vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO CatalystCode/3DStreamingToolkit
+    REF 56afbb5d97da14e5422a9aea50fed1ad34a7e1b4
+    SHA512 538d79bf218c73f77e2e14a37131a7a341512ad80417b13802534fb09b464815ebb900cba141b084a412501277a06537f4b7107cb7e3ba1d06d2ba476ef8f462
+    HEAD_REF master 
 )
 
-vcpkg_extract_source_archive(${ARCHIVE})
-
 execute_process(
-    COMMAND powershell.exe -ExecutionPolicy Bypass ${CURRENT_BUILDTREES_DIR}/src/3DStreamingToolkit-1.0/Utilities/setup.ps1
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src/3DStreamingToolkit-1.0
+    COMMAND powershell.exe -ExecutionPolicy Bypass ${SOURCE_PATH}/Utilities/setup.ps1
+    WORKING_DIRECTORY ${SOURCE_PATH}
     
 )
 
