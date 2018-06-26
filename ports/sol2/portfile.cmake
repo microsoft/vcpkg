b0bc41a4a1514330cf70a80bb4f734316da62c96
Robert Schumacher <roschuma@microsoft.com>
2018-06-25 21:42:36 -0700

    [sol2] Simplification and cleanup. Don't remove upstream's include directory.

--- ports/sol2/portfile.cmake
+++ ports/sol2/portfile.cmake
@@ -6,21 +6,14 @@ vcpkg_from_github(
     REF v2.20.3
     SHA512 f1d39a6762c7c9c40bffd08129a80c4c8dd70ffcaadc195cbcd681471e6978176d0ae06d4b8db6aabc05b0ae2670c10bd287b9e859ec7810f2441c3c6c8fe521
     HEAD_REF develop
-)
-
-message(STATUS "SPATH -> ${SOURCE_PATH}")
-
-vcpkg_apply_patches(                                                  
-    SOURCE_PATH ${SOURCE_PATH}                                        
-    PATCHES                                                           
-    ${CMAKE_CURRENT_LIST_DIR}/sol2_fix_targets.patch
-    ${CMAKE_CURRENT_LIST_DIR}/sol2_fix_install_interface.patch
+    PATCHES
+        ${CMAKE_CURRENT_LIST_DIR}/sol2_fix_targets.patch
+        ${CMAKE_CURRENT_LIST_DIR}/sol2_fix_install_interface.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
 )
 
 vcpkg_install_cmake()
