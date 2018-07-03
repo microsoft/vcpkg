f6836e5f1a9a31a1ed54d8fc4de49875af0bbe08
Robert Schumacher <roschuma@microsoft.com>
2018-07-03 01:05:59 -0700

    [pegtl] fixup_cmake_targets

--- ports/pegtl/portfile.cmake
+++ ports/pegtl/portfile.cmake
@@ -1,31 +1,23 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO taocpp/pegtl
-	REF 2.6.0
-	SHA512 e70aa70961c41d2b3dc4874429b2be491eed12f229a11d83ffe98cb3ee7a2b39061f9f9ec86f3ed02a6c347a875cdc52bd82112c4deb29d46e2002110e256f9f
-	HEAD_REF master)
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO taocpp/pegtl
+    REF 2.6.0
+    SHA512 e70aa70961c41d2b3dc4874429b2be491eed12f229a11d83ffe98cb3ee7a2b39061f9f9ec86f3ed02a6c347a875cdc52bd82112c4deb29d46e2002110e256f9f
+    HEAD_REF master
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
         -DPEGTL_BUILD_TESTS=OFF
         -DPEGTL_BUILD_EXAMPLES=OFF
 )
 
 vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/pegtl/cmake)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
