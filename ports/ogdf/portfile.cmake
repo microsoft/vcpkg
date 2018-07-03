90bef5f6ed0798375dd0c60219fbce4e1f2ed4fe
Robert Schumacher <roschuma@microsoft.com>
2018-07-03 05:23:44 -0700

    [odgf] Escape : in URL

--- ports/ogdf/portfile.cmake
+++ ports/ogdf/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/2018-03-28/OGDF-snapshot)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.ogdf.net/lib/exe/fetch.php/tech:ogdf-snapshot-2018-03-28.zip"
+    URLS "http://www.ogdf.net/lib/exe/fetch.php/tech%3aogdf-snapshot-2018-03-28.zip"
     FILENAME "ogdf-2018-03-28.zip"
     SHA512 a6ddb33bc51dca4d59fcac65ff66459043b11ce5303e9d40e4fc1756adf84a0af7d0ac7debab670111e7a145dcdd9373c0e350d5b7e831b169811f246b6e19b6
 )
