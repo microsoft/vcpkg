98e8c22eafc42d0e3b91a005973b49578e0fe14f
Qi-rui Chen <Qi-rui.Chen.2015@my.bristol.ac.uk>
2018-07-03 12:41:53 +0100

    Fix glfw3:x64-linux producing incorrect target (#3803)
    * Fix glfw3 creating incorrect target
    
    glfw3Target.cmake uses a semicolon seperated list which needs to be
    escaped with quotes on write
    
    * Fix semicolon stripping
    
    * Fix typo
    
    * [dlib][glfw3][libodb][signalrclient] Bump versions

--- ports/dlib/portfile.cmake
+++ ports/dlib/portfile.cmake
@@ -69,7 +69,7 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/dlib/external/libpng/arm)
 file(READ ${CURRENT_PACKAGES_DIR}/include/dlib/config.h _contents)
 string(REPLACE "/* #undef ENABLE_ASSERTS */" "#if defined(_DEBUG)\n#define ENABLE_ASSERTS\n#endif" _contents ${_contents})
 string(REPLACE "#define DLIB_DISABLE_ASSERTS" "#if !defined(_DEBUG)\n#define DLIB_DISABLE_ASSERTS\n#endif" _contents ${_contents})
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/dlib/config.h ${_contents})
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/dlib/config.h "${_contents}")
 
 file(READ ${CURRENT_PACKAGES_DIR}/share/dlib/dlib.cmake _contents)
 string(REPLACE
