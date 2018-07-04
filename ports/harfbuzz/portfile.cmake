67901e2493b40f93de9823de5896d59884abcaa2
Robert Schumacher <roschuma@microsoft.com>
2018-07-04 07:03:12 -0700

    [harfbuzz][pango] Merge conflicts. Pango depends on harfbuzz[glib]

--- ports/harfbuzz/portfile.cmake
+++ ports/harfbuzz/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO behdad/harfbuzz
-    REF 1.7.6
-    SHA512 90b51475d532377fe1686264a40f4a5555fc024f6984c1e3c114d8f4662e0c7f4a13e6a95e35fd5b3c54d1bb1b3cb97840b23f5ec2c6a799da61db1614b0fce3
+    REF 1.8.2
+    SHA512 83a5126f17e57b05c36779f91ea9c2454731d7e6f514fce134ae5e652972a8231723773e0e0f64bfe8588f8cfcf2e041031a3328ee3102d440b2ac427aa1d764
     HEAD_REF master
 )
 
@@ -14,6 +14,11 @@ vcpkg_apply_patches(
         "${CMAKE_CURRENT_LIST_DIR}/0001-fix-uwp-build.patch"
         "${CMAKE_CURRENT_LIST_DIR}/find-package-freetype-2.patch"
 )
+
+SET(HB_HAVE_ICU "OFF")
+if("icu" IN_LIST FEATURES)
+    SET(HB_HAVE_ICU "ON")
+endif()
 
 ## Unicode callbacks
 
@@ -40,6 +45,7 @@ vcpkg_configure_cmake(
     OPTIONS
         -DHB_HAVE_FREETYPE=ON
         -DHB_BUILTIN_UCDN=${BUILTIN_UCDN}
+        -DHB_HAVE_ICU=${HB_HAVE_ICU}
         -DHB_HAVE_GLIB=${HAVE_GLIB}
     OPTIONS_DEBUG
         -DSKIP_INSTALL_HEADERS=ON
