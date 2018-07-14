8cfc78e890b18762f1f5d0aff346c397de2e6d56
Aybe <aybe@users.noreply.github.com>
2018-07-14 23:03:36 +0200

    [liblzma] Enabled UWP builds

--- ports/liblzma/portfile.cmake
+++ ports/liblzma/portfile.cmake
@@ -1,7 +1,3 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "${PORT} does not currently support UWP")
-endif()
-
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
@@ -10,6 +6,11 @@ vcpkg_from_github(
     REF v5.2.3
     SHA512 d126666e58c6536aa7ae6aa6aac480f421e25aa61c4b5e7adb3de7b99423275a94d583ceaf0b15d559eaf9bc9be18f381cd46e49b1f8cb238c1d715876731063
     HEAD_REF master
+)
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/v5.2.3-b3437cea7b
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/enable-uwp-builds.patch"
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
