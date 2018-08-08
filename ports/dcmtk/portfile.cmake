7350e396ee0284be8dd8c2a4e0736c285623492f
Robert Schumacher <roschuma@microsoft.com>
2018-08-07 23:09:03 -0700

    [dcmtk] Cleanup and use vcpkg_from_github()

--- ports/dcmtk/portfile.cmake
+++ ports/dcmtk/portfile.cmake
@@ -1,37 +1,23 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/dcmtk-DCMTK-3.6.3)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/DCMTK/dcmtk/archive/DCMTK-3.6.3.zip"
-    FILENAME "3.6.3.zip"
-    SHA512 4b6770d0661ccbc24f078cde6ca3fc09ef4e66839f3884646395f686a1b7798ab6e490efcc725e53d41125068fe3b47f305e0f4f57c6f8f1040acc37630e26d5
-)
-vcpkg_extract_source_archive(${ARCHIVE})
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    message("DCMTK only supports static library linkage")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO DCMTK/dcmtk
+    REF DCMTK-3.6.3
+    SHA512 5863d0c05f046075b998bced7c8c71bf8e969dd366f26d48cdf26012ea744ae4a22784a5c3c12e12b0f188e997c93a6890ef0c3c336865ea93f13c45f70b258d
+    HEAD_REF master
     PATCHES ${CMAKE_CURRENT_LIST_DIR}/dcmtk.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA
     OPTIONS
-        -DBUILD_SHARED_LIBS=OFF
         -DDCMTK_WITH_DOXYGEN=OFF
         -DDCMTK_WITH_ZLIB=OFF
         -DDCMTK_WITH_OPENSSL=OFF
@@ -44,7 +30,7 @@ vcpkg_configure_cmake(
         -DDCMTK_ENABLE_BUILTIN_DICTIONARY=ON
         -DDCMTK_ENABLE_PRIVATE_TAGS=ON
         -DBUILD_APPS=OFF
-        -DDCMTK_ENABLE_CXX11=ON 
+        -DDCMTK_ENABLE_CXX11=ON
         -DCMAKE_DEBUG_POSTFIX="d"
     OPTIONS_DEBUG
         -DINSTALL_HEADERS=OFF
@@ -54,19 +40,8 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets() # combines release and debug build configurations
+vcpkg_fixup_cmake_targets()
 
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-  file(READ ${CURRENT_PACKAGES_DIR}/debug/share/DCMTKTargets-debug.cmake DCMTK_CONFIG_LIB)
-  string(REPLACE "PREFIX}/lib"
-                 "PREFIX}/debug/lib" DCMTK_CONFIG_LIB "${DCMTK_CONFIG_LIB}")
-  string(REPLACE "PREFIX}/bin"
-                 "PREFIX}/debug/bin" DCMTK_CONFIG_LIB "${DCMTK_CONFIG_LIB}")
-  file(WRITE ${CURRENT_PACKAGES_DIR}/share/DCMTKTargets-debug.cmake "${DCMTK_CONFIG_LIB}")
-endif()
-
-# the following lines should be called only after the cmake config has been built properly
-#file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/cmake)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
