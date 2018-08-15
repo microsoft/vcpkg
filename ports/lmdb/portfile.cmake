22dc26332e850b2d748da171952085a148abf21d
Robert Schumacher <roschuma@microsoft.com>
2018-08-15 11:32:05 -0700

    [lmdb] Fix broken SOURCE_PATH

--- ports/lmdb/portfile.cmake
+++ ports/lmdb/portfile.cmake
@@ -9,10 +9,10 @@ vcpkg_from_github(
     PATCHES lmdb_45a88275d2a410e683bae4ef44881e0f55fa3c4d.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake/ DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake/ DESTINATION ${SOURCE_PATH}/libraries/liblmdb)
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH ${SOURCE_PATH}/libraries/liblmdb
     OPTIONS_DEBUG
         -DLMDB_INSTALL_HEADERS=OFF
 )
@@ -21,6 +21,6 @@ vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH share/lmdb)
 
-file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/lmdb RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/libraries/liblmdb/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/lmdb RENAME copyright)
 
 vcpkg_copy_pdbs()
