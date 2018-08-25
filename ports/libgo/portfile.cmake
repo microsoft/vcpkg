62ca5cbedea36fffefdf3eec1d99734c6a3b4d70
Robert Schumacher <roschuma@microsoft.com>
2018-08-24 22:10:38 -0700

    [libgo] Partially restore windows fixes

--- ports/libgo/portfile.cmake
+++ ports/libgo/portfile.cmake
@@ -8,7 +8,16 @@ vcpkg_from_github(
     REF v3.0-beta
     SHA512 b60803a8dcaac942fce23d101e8563ec4d850c9d477dc3d116db7f104404acc89a589ac7a0577c2425f2f65a79da211343fa5491cb3ee11c2b34af6883b4e4ba
     HEAD_REF master
+    PATCHES
+        remove-compiler-flags.patch
+        out-of-source-configure.patch
 )
+
+file(REMOVE_RECURSE ${SOURCE_PATH}/third_party/gtest ${SOURCE_PATH}/third_party/xhook ${SOURCE_PATH}/third_party/boost.context)
+file(GLOB EXTRACTED_STAMPS ${SOURCE_PATH}/third_party/*.extracted)
+if(EXTRACTED_STAMPS)
+    file(REMOVE ${EXTRACTED_STAMPS})
+endif()
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/XBased/xhook/archive/e18c450541892212ca4f11dc91fa269fabf9646f.tar.gz"
@@ -16,9 +25,8 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 1bcf320f50cff13d92013a9f0ab5c818c2b6b63e9c1ac18c5dd69189e448d7a848f1678389d8b2c08c65f907afb3909e743f6c593d9cfb21e2bb67d5c294a166
 )
 
-#vcpkg_extract_source_archive(${ARCHIVE} ${SOURCE_PATH}/third_party/xhook)
-#file(REMOVE_RECURSE ${SOURCE_PATH}/third_party/xhook)
-#file(RENAME ${SOURCE_PATH}/third_party/xhook-e18c450541892212ca4f11dc91fa269fabf9646f ${SOURCE_PATH}/third_party/xhook)
+vcpkg_extract_source_archive(${ARCHIVE} ${SOURCE_PATH}/third_party)
+file(RENAME ${SOURCE_PATH}/third_party/xhook-e18c450541892212ca4f11dc91fa269fabf9646f ${SOURCE_PATH}/third_party/xhook)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
