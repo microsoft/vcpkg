66f09a0b609e0a118902ca603d51172eb788fff9
Cheney-Wang <v-xincwa@microsoft.com>
2018-08-28 00:43:56 -0700

    [Live555] Update to latest

--- ports/live555/portfile.cmake
+++ ports/live555/portfile.cmake
@@ -4,14 +4,14 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
 endif()
 
 # The current Live555 version from http://www.live555.com/liveMedia/public/
-set(LIVE_VERSION 2018.08.05)
-set(LIVE_SHA 2d7de75d91567a7a8a8982ea414f4bf403cb3d3efcc5183c2b55c5ac6b7b18f8f93f50c4fe762078c6e799b66835d5436be99f32db8becb735507ccf559c544e)
+set(LIVE_VERSION latest)
+set(LIVE_SHA 225f27c8e188d41d8b81dc6d27c918cbbb0399330362683600ed02cc8f50e7dac10e6a9e0cf6c23a0153c750ddf1f20213922c426507f40a5e63950d46683fd5)
 
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION}/live)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.live555.com/liveMedia/public/live.${LIVE_VERSION}.tar.gz"
-    FILENAME "live.${LIVE_VERSION}.tar.gz"
+    URLS "http://www.live555.com/liveMedia/public/live555-${LIVE_VERSION}.tar.gz"
+    FILENAME "live555-${LIVE_VERSION}.tar.gz"
     SHA512 ${LIVE_SHA}
 )
 vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION})
