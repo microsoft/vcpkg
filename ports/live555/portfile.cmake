cfbd6d93d47fe288bdb19fa600047378346be868
Cheney-Wang <v-xincwa@microsoft.com>
2018-08-29 00:03:55 -0700

    [live555] update live555

--- ports/live555/portfile.cmake
+++ ports/live555/portfile.cmake
@@ -3,36 +3,41 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     set(VCPKG_LIBRARY_LINKAGE "static")
 endif()
 
-# The current Live555 version from http://www.live555.com/liveMedia/public/
-set(LIVE_VERSION latest)
-set(LIVE_SHA 225f27c8e188d41d8b81dc6d27c918cbbb0399330362683600ed02cc8f50e7dac10e6a9e0cf6c23a0153c750ddf1f20213922c426507f40a5e63950d46683fd5)
+if(NOT VCPKG_USE_HEAD_VERSION)
+	message(FATAL_ERROR "Please re-run the installation with --head.")
+else()	
+	# The current Live555 version from http://www.live555.com/liveMedia/public/
+	set(LIVE_VERSION latest)
 
-include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION}/live)
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.live555.com/liveMedia/public/live555-${LIVE_VERSION}.tar.gz"
-    FILENAME "live555-${LIVE_VERSION}.tar.gz"
-    SHA512 ${LIVE_SHA}
-)
-vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION})
+	include(vcpkg_common_functions)	
+	set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION}/live)
+	vcpkg_download_distfile(ARCHIVE
+		URLS "http://www.live555.com/liveMedia/public/live555-${LIVE_VERSION}.tar.gz"
+		FILENAME "live555-${LIVE_VERSION}.tar.gz"
+		SKIP_SHA512
+	)
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+	vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION})
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-)
+	file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_install_cmake()
+	vcpkg_configure_cmake(
+		SOURCE_PATH ${SOURCE_PATH}
+		PREFER_NINJA
+	)
 
-file(GLOB HEADERS
-    "${SOURCE_PATH}/BasicUsageEnvironment/include/*.h*"
-    "${SOURCE_PATH}/groupsock/include/*.h*"
-    "${SOURCE_PATH}/liveMedia/include/*.h*"
-    "${SOURCE_PATH}/UsageEnvironment/include/*.h*"
-)
+	vcpkg_install_cmake()
 
-file(COPY ${HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/live555 RENAME copyright)
+	file(GLOB HEADERS
+		"${SOURCE_PATH}/BasicUsageEnvironment/include/*.h*"
+		"${SOURCE_PATH}/groupsock/include/*.h*"
+		"${SOURCE_PATH}/liveMedia/include/*.h*"
+		"${SOURCE_PATH}/UsageEnvironment/include/*.h*"
+	)
 
-vcpkg_copy_pdbs()
+	file(COPY ${HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+	file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/live555 RENAME copyright)
+
+	vcpkg_copy_pdbs()
+endif()
+
