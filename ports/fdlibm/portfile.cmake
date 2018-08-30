378c917a1a5b734e0e94d5f18ce7a4e7fffd3c27
atkawa7 <atkawa7@yahoo.com>
2018-08-30 09:47:34 +0200

    Fix different hashes creation with google host

--- ports/fdlibm/portfile.cmake
+++ ports/fdlibm/portfile.cmake
@@ -1,11 +1,25 @@
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://android.googlesource.com/platform/external/fdlibm/+archive/59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz"
-    FILENAME "fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz"
-    SHA512 bb998648429c8977a7d574e01689ef6ee04b4b25c0850a61087a5c79fa91e0e43da99061fd38bcff56c8e7a8347cf8c176c873bc8b05c3d3b036943c0734c7dd
-)
-vcpkg_extract_source_archive(${ARCHIVE})
+if(NOT EXISTS "${DOWNLOADS}/fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz")
+  find_program(GIT NAMES git git.cmd)
+  # Note: git init is safe to run multiple times
+  vcpkg_execute_required_process(
+    COMMAND ${GIT} init git-tmp
+    WORKING_DIRECTORY ${DOWNLOADS}
+    LOGNAME git-init
+  )
+  vcpkg_execute_required_process(
+    COMMAND ${GIT} fetch https://android.googlesource.com/platform/external/fdlibm 59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac --depth 1 -n
+    WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+    LOGNAME git-fetch
+  )
+  vcpkg_execute_required_process(
+    COMMAND ${GIT} archive 59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac -o ../fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz
+    WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+    LOGNAME git-archive
+  )
+endif()
+vcpkg_extract_source_archive(${DOWNLOADS}/fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/libm5.def DESTINATION ${SOURCE_PATH})
