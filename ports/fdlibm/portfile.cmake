31ebfc943fee8378e5880c05533aa9c7ead37a3d
atkawa7 <atkawa7@yahoo.com>
2018-08-30 11:02:19 +0200

    Move functions to script

--- ports/fdlibm/portfile.cmake
+++ ports/fdlibm/portfile.cmake
@@ -1,25 +1,10 @@
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
-if(NOT EXISTS "${DOWNLOADS}/fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz")
-  find_program(GIT NAMES git git.cmd)
-  # Note: git init is safe to run multiple times
-  vcpkg_execute_required_process(
-    COMMAND ${GIT} init git-tmp
-    WORKING_DIRECTORY ${DOWNLOADS}
-    LOGNAME git-init
-  )
-  vcpkg_execute_required_process(
-    COMMAND ${GIT} fetch https://android.googlesource.com/platform/external/fdlibm 59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac --depth 1 -n
-    WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
-    LOGNAME git-fetch
-  )
-  vcpkg_execute_required_process(
-    COMMAND ${GIT} archive 59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac -o ../fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz
-    WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
-    LOGNAME git-archive
-  )
-endif()
-vcpkg_extract_source_archive(${DOWNLOADS}/fdlibm-59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac.tar.gz)
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://android.googlesource.com/platform/external/fdlibm
+    REF  59f7335e4dd8275a7dc2f8aeb4fd00758fde37ac
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/libm5.def DESTINATION ${SOURCE_PATH})
---
+++ scripts/cmake/vcpkg_from_git.cmake
@@ -0,0 +1,89 @@
+## # vcpkg_from_git
+##
+## Download and extract a project from git
+##
+## ## Usage:
+## ```cmake
+## vcpkg_from_git(
+##     OUT_SOURCE_PATH <SOURCE_PATH>
+##     URL https://android.googlesource.com/platform/external/fdlibm
+##     [REF <59f7335e4d...>]
+##     [PATCHES <patch1.patch> <patch2.patch>...]
+## )
+## ```
+##
+## ## Parameters:
+## ### OUT_SOURCE_PATH
+## Specifies the out-variable that will contain the extracted location.
+##
+## This should be set to `SOURCE_PATH` by convention.
+##
+## ### URL
+## The url of the git repository.
+##
+## ### REF
+## The full commit id of the current latest master.
+
+## ### PATCHES
+## A list of patches to be applied to the extracted sources.
+##
+## Relative paths are based on the port directory.
+##
+## ## Notes:
+## `REF` and `URL` must be specified.
+##
+## ## Examples:
+##
+## * [fdlibm](https://github.com/Microsoft/vcpkg/blob/master/ports/fdlibm/portfile.cmake)
+
+function(vcpkg_from_git)
+  set(oneValueArgs OUT_SOURCE_PATH URL REF)
+  set(multipleValuesArgs PATCHES)
+  cmake_parse_arguments(_vdud "" "${oneValueArgs}" "${multipleValuesArgs}" ${ARGN})
+
+  if(NOT DEFINED _vdud_OUT_SOURCE_PATH)
+    message(FATAL_ERROR "OUT_SOURCE_PATH must be specified.")
+  endif()
+
+  if(NOT DEFINED _vdud_URL)
+    message(FATAL_ERROR "The git url must be specified")
+  endif()
+
+  if(NOT DEFINED _vdud_REF)
+    message(FATAL_ERROR "The git ref must be specified.")
+  endif()
+
+  string(REPLACE "/" "-" SANITIZED_REF "${_vdud_REF}")
+  set(FILENAME "${PORT}-${SANITIZED_REF}.tar.gz")
+  set(ARCHIVE "${DOWNLOADS}/${FILENAME}")
+  set(TEMP_SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/${SANITIZED_REF}")
+
+  if(NOT EXISTS "${ARCHIVE}")
+    find_program(GIT NAMES git git.cmd)
+    # Note: git init is safe to run multiple times
+    vcpkg_execute_required_process(
+      COMMAND ${GIT} init git-tmp
+      WORKING_DIRECTORY ${DOWNLOADS}
+      LOGNAME git-init
+    )
+    vcpkg_execute_required_process(
+      COMMAND ${GIT} fetch ${_vdud_URL} ${_vdud_REF} --depth 1 -n
+      WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+      LOGNAME git-fetch
+    )
+    vcpkg_execute_required_process(
+      COMMAND ${GIT} archive ${_vdud_REF} -o "${ARCHIVE}"
+      WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+      LOGNAME git-archive
+    )
+  endif()
+
+  vcpkg_extract_source_archive("${ARCHIVE}" "${TEMP_SOURCE_PATH}")
+
+  vcpkg_apply_patches(
+    SOURCE_PATH "${TEMP_SOURCE_PATH}"
+    PATCHES ${_vdud_PATCHES}
+  )
+  set(${_vdud_OUT_SOURCE_PATH} "${TEMP_SOURCE_PATH}" PARENT_SCOPE)
+  message(STATUS "Using source at ${TEMP_SOURCE_PATH}")
+endfunction()
