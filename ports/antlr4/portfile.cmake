d6b075c3f94e38fef4f46a424d135c82a3b619e0
icedream2linxi <icedream2linxi@qq.com>
2018-08-30 20:13:29 +0800

    [antlr4] Support build on Linux and MacOS.

--- ports/antlr4/portfile.cmake
+++ ports/antlr4/portfile.cmake
@@ -4,13 +4,12 @@ endif()
 
 include(vcpkg_common_functions)
 
-set(VERSION 4.7)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/runtime)
+set(VERSION 4.7.1)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.antlr.org/download/antlr4-cpp-runtime-4.7-source.zip"
+    URLS "http://www.antlr.org/download/antlr4-cpp-runtime-${VERSION}-source.zip"
     FILENAME "antlr4-cpp-runtime-${VERSION}-source.zip"
-    SHA512 a14fd3320537075a8d4c1cfa81d416bad6257d238608e2428f4930495072cce984c707126e3777ffd3849dd6b6cdf1bf43624bd6d318b1fa5dd6749a7304f808
+    SHA512 24d53278db56b199e6787242f22339f74e07d2cd3ed56f851ad905b110c2ba3cb001e1e2fcbc8624f0e93e00ba1fe1b23630dd1a736558c694655aeb1c3129da
 )
 
 # license not exist in antlr folder.
@@ -20,62 +19,75 @@ vcpkg_download_distfile(LICENSE
     SHA512 c72ae3d5c9f3f07160405b5ca44f01116a9602d82291d6cd218fcc5ec6e8baf985e4baa2acf3d621079585385708bd171c96ef44dd808e60c40a48bc1f56c9ae
 )
 
-vcpkg_extract_source_archive(${ARCHIVE})
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${VERSION})
+vcpkg_extract_source_archive(${ARCHIVE} ${SOURCE_PATH})
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src
-    PATCHES     ${CMAKE_CURRENT_LIST_DIR}/crt_mt.patch
-                ${CMAKE_CURRENT_LIST_DIR}/Fix-building-in-Visual-Studio-2017.patch
+vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
+                    PATCHES ${CMAKE_CURRENT_LIST_DIR}/fixed_build.patch
 )
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    set(DEBUG_CONFIG   "Debug Static")
-    set(RELEASE_CONFIG "Release Static")
-else()
-    set(DEBUG_CONFIG   "Debug DLL")
-    set(RELEASE_CONFIG "Release DLL")
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL Linux)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(UUID uuid)
+    if (NOT UUID_FOUND)
+        message(FATAL_ERROR "Unable to find uuid library. You can execute 'sudo apt-get install uuid-dev'.")
+    endif()
 endif()
 
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/antlr4cpp-vs2015.vcxproj
-    DEBUG_CONFIGURATION ${DEBUG_CONFIG}
-    RELEASE_CONFIGURATION ${RELEASE_CONFIG}
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS_DEBUG -DLIB_OUTPUT_DIR=${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/dist
+    OPTIONS_RELEASE -DLIB_OUTPUT_DIR=${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/dist
 )
 
-file (MAKE_DIRECTORY
-    ${CURRENT_PACKAGES_DIR}/include)
-FILE(COPY            ${SOURCE_PATH}/src/
-     DESTINATION     ${CURRENT_PACKAGES_DIR}/include
-     FILES_MATCHING PATTERN "*.h")
+vcpkg_install_cmake()
 
-file (MAKE_DIRECTORY
-    ${CURRENT_PACKAGES_DIR}/lib
-    ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/doc
+                    ${CURRENT_PACKAGES_DIR}/debug/share
+                    ${CURRENT_PACKAGES_DIR}/debug/include
+)
 
-file(COPY       ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(COPY       ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+if (NOT VCPKG_CMAKE_SYSTEM_NAME)
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime-static.lib
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime-static.lib
+        )
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_apply_patches(
-        SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-        PATCHES     ${CMAKE_CURRENT_LIST_DIR}/static.patch
-    )
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime.dll ${CURRENT_PACKAGES_DIR}/bin/antlr4-runtime.dll)
+        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime.dll ${CURRENT_PACKAGES_DIR}/debug/bin/antlr4-runtime.dll)
+    else()
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime.lib
+                    ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime.dll
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime.lib
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime.dll
+        )
+
+        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime-static.lib ${CURRENT_PACKAGES_DIR}/lib/antlr4-runtime.lib)
+        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime-static.lib ${CURRENT_PACKAGES_DIR}/debug/lib/antlr4-runtime.lib)
+    endif()
 else()
-    file (MAKE_DIRECTORY
-        ${CURRENT_PACKAGES_DIR}/bin
-        ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-    file(COPY
-        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.dll
-        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${DEBUG_CONFIG}/antlr4-runtime.pdb
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(COPY
-        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.dll
-        ${SOURCE_PATH}/bin/vs-2015/${TRIPLET_SYSTEM_ARCH}/${RELEASE_CONFIG}/antlr4-runtime.pdb
-        DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libantlr4-runtime.a
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/libantlr4-runtime.a
+        )
+    elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL Linux)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libantlr4-runtime.so
+                    ${CURRENT_PACKAGES_DIR}/lib/libantlr4-runtime.so.${VERSION}
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/libantlr4-runtime.so
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/libantlr4-runtime.so.${VERSION}
+        )
+    else()
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libantlr4-runtime.dylib
+                    ${CURRENT_PACKAGES_DIR}/lib/libantlr4-runtime.${VERSION}.dylib
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/libantlr4-runtime.dylib
+                    ${CURRENT_PACKAGES_DIR}/debug/lib/libantlr4-runtime.${VERSION}.dylib
+        )
+    endif()
 endif()
+
+vcpkg_copy_pdbs()
 
 file(INSTALL ${LICENSE} DESTINATION ${CURRENT_PACKAGES_DIR}/share/antlr4 RENAME copyright)
 
