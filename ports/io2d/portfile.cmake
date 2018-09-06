f223e147f4d3df6ebbee922835456b5a95d873b0
David Ludwig <dludwig@pobox.com>
2018-09-06 14:03:03 -0400

    [io2d] point at io2d's main Github repo

--- ports/io2d/portfile.cmake
+++ ports/io2d/portfile.cmake
@@ -15,18 +15,11 @@ if ("${SOURCE_PATH}" STREQUAL "")
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
 
-        ## TODO: point at cpp-io2d/(whatever), if and as needed
-        # REPO cpp-io2d/P0267_RefImpl
-        # REF a805fc8dfbbab29de171a45899b74e3af4889089
-        # SHA512 99ca75d421d7375ec501a702360660e849bae4d728d08e6f93beded3c84944423a00f350d4234e71175d4e3d1426efa68dfae87c841e20eb23ccbabf9047c7b3
-        # HEAD_REF master
-
-        ## TODO: remove reference to 'DavidLudwig'-owned repo
-        REPO DavidLudwig/P0267_Modified
-        REF cc2aeef42d702a694f7dc8d27f9f6b2245b2c52d
-        SHA512 f0f609d147a21c188a20c7dbb71ed36989683b6fba2dea5f8c8ac92b521504d49d29ecf13990a4502a75b1b451a6f3150226ae4369446fe85d06441107fded72
-        HEAD_REF vcpkg
-
+        # TODO: point at cpp-io2d/(whatever), if and as needed
+        REPO cpp-io2d/P0267_RefImpl
+        REF 21ae92c8be6916034e6e18f08aa57899a975dfb0
+        SHA512 5b674f98ca7705d6901af339a4189d5ce4f2c3118bfb99430734f355159602f177bc8d1b345c3a2f17947a62547553f7b91747a1f16da063707a4da7f990391d
+        HEAD_REF master
     )
 endif()
 
