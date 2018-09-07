7c0a9adac33d9282afe75708da059f608fd5ce6e
Roger Leigh <rleigh@codelibre.net>
2018-09-07 12:43:23 +0100

    [xerces-c] Update to 3.2.2

--- ports/xerces-c/portfile.cmake
+++ ports/xerces-c/portfile.cmake
@@ -19,37 +19,49 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apache/xerces-c
-    REF Xerces-C_3_1_4
-    SHA512 6dc3e4bb68bc32a0e8ec6dcc7ec67e21239a79c909d08ccc16c96dc5de4e73800993d1c09f589606925507baf0b2a9bf6037d28c84dae826935bf1f7a151071e
-    HEAD_REF master
+    REF Xerces-C_3_2_2
+    SHA512 66f60fe9194376ac0ca99d13ea5bce23ada86e0261dde30686c21ceb5499e754dab8eb0a98adadd83522bda62709377715501f6dac49763e3a686f9171cc63ea
+    HEAD_REF trunk
 )
 
-if (VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
-    set(BUILD_ARCH "Win32")
-    set(OUTPUT_DIR "Win32")
-elseif (VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
-    set(BUILD_ARCH "x64")
-    set(OUTPUT_DIR "Win64")
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+if(CMAKE_HOST_WIN32)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CURRENT_PACKAGES_DIR}/cmake"
+        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CURRENT_PACKAGES_DIR}/debug/cmake"
+        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/cmake"
+        "${CURRENT_PACKAGES_DIR}/debug/cmake"
+    )
+    file(GLOB release_exe "${CURRENT_PACKAGES_DIR}/bin/*.exe")
+    file(GLOB debug_exe "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe")
+    file(REMOVE ${release_exe} ${debug_exe})
 else()
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CURRENT_PACKAGES_DIR}/lib/cmake/XercesC"
+        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CURRENT_PACKAGES_DIR}/debug/lib/cmake/XercesC"
+        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/lib/cmake"
+        "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
+    )
 endif()
 
-vcpkg_build_msbuild(
-    #PROJECT_PATH ${SOURCE_PATH}/projects/Win32/VC14/xerces-all/xerces-all.sln
-    PROJECT_PATH ${SOURCE_PATH}/projects/Win32/VC14/xerces-all/xercesLib/xercesLib.vcxproj
-    PLATFORM ${BUILD_ARCH})
-
-file(COPY ${SOURCE_PATH}/Build/${OUTPUT_DIR}/VC14/Debug/xerces-c_3_1D.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(COPY ${SOURCE_PATH}/Build/${OUTPUT_DIR}/VC14/Debug/xerces-c_3D.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(COPY ${SOURCE_PATH}/Build/${OUTPUT_DIR}/VC14/Release/xerces-c_3_1.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/Build/${OUTPUT_DIR}/VC14/Release/xerces-c_3.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-
-file(COPY ${SOURCE_PATH}/src/xercesc DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.hpp)
-# Certain headers under xercesc/util include .c files, so we need these copied over as well
-file(COPY ${SOURCE_PATH}/src/xercesc DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.c)
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/xercesc/NLS)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/xercesc/util/MsgLoaders/ICU/resources)
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/xerces-c)
