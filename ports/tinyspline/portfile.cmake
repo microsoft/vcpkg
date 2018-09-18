a5f43b89df1de8046d893d912b67bf6e2e6fe863
Robert Schumacher <roschuma@microsoft.com>
2018-09-18 01:35:21 -0700

    [tinyspline] Simplify patching, remove extra comments and unused directives

--- ports/tinyspline/portfile.cmake
+++ ports/tinyspline/portfile.cmake
@@ -1,43 +1,17 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
-#set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/tinyspline-0.2.0)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO msteinbeck/tinyspline
     REF 0.2.0
     SHA512 50cf4927b311eeca6de7954f1b8d585cbf71355f5e5b0aac2f92f5f4ba37986df16eb3251f94a2304d27dab27d4f6b838b410f53e30de28bab53facf194eb640
     HEAD_REF master
-)
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}/src
     PATCHES
-    ${CMAKE_CURRENT_LIST_DIR}/cmake.patch
+        cmake.patch
 )
-    
-#vcpkg_download_distfile(ARCHIVE
-#    URLS "https://github.com/msteinbeck/tinyspline/archive/0.2.0.tar.gz"
-#    FILENAME "0.2.0.tar.gz"
-#    SHA512 50cf4927b311eeca6de7954f1b8d585cbf71355f5e5b0aac2f92f5f4ba37986df16eb3251f94a2304d27dab27d4f6b838b410f53e30de28bab53facf194eb640
-#)
-#vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}/src
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS
-	${OPTIONS}
+    PREFER_NINJA
 )
 
 vcpkg_install_cmake()
@@ -48,7 +22,4 @@ endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/tinyspline RENAME copyright)
-
-# Handle copyright
-# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/tinyspline RENAME copyright)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/tinyspline/copyright COPYONLY)
