18e1e595711206e3f889ca9035a6e06e45ade891
Robert Schumacher <roschuma@microsoft.com>
2018-09-18 02:03:05 -0700

    [fruit] Use vcpkg_from_github() and configure_file()

--- ports/fruit/portfile.cmake
+++ ports/fruit/portfile.cmake
@@ -1,22 +1,22 @@
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/fruit-3.4.0)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/google/fruit/archive/v3.4.0.zip"
-    FILENAME "fruit-3.4.0.zip"
-    SHA512 29b85954f6157bd09e531a0a7283253682da547f1328f565844641a3b6e72c822bf4bd781807dc91d77ea86d137eb2a937f9a93783a8f30ff91e0ad3c4a4d5e2
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/fruit
+    REF v3.4.0
+    SHA512 d78c76432c77acc4cc6ccf3fd9627a3fb2a0aa55d1baf7346422e9f1c1e048237d136588b44cfa943b542b43adbbb62fcd524e4a1cb870e9ffe8b7cf4dadb35d
+    HEAD_REF master
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 # TODO: Make boost an optional dependency?
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-    	-DFRUIT_USES_BOOST=False
+        -DFRUIT_USES_BOOST=False
 )
 
 vcpkg_install_cmake()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/fruit RENAME copyright)
+configure_file(${SOURCE_PATH}/COPYING ${CURRENT_PACKAGES_DIR}/share/fruit/copyright COPYONLY)
