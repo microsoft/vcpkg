d47d7cb3ca667d7a8d3be85f4914400dc6cd89ca
Robert Schumacher <roschuma@microsoft.com>
2018-09-20 20:51:29 -0700

    [xerces-c] Use vcpkg_fixup_cmake_targets

--- ports/xerces-c/portfile.cmake
+++ ports/xerces-c/portfile.cmake
@@ -1,20 +1,4 @@
-# Common Ambient Variables:
-#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
-#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
-#   PORT is the current port name (zlib, etc)
-#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#
-
 include(vcpkg_common_functions)
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    message(STATUS "Static libraries not supported; building dynamic instead")
-    set(VCPKG_LIBRARY_LINKAGE "dynamic")
-endif()
-if (VCPKG_CRT_LINKAGE STREQUAL "static")
-    message(STATUS "Static linking against the CRT not supported; building dynamic instead")
-    set(VCPKG_CRT_LINKAGE "dynamic")
-endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -31,32 +15,26 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+if(EXISTS ${CURRENT_PACKAGES_DIR}/cmake)
+    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/xercesc)
+else()
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/XercesC TARGET_PATH share/xercesc)
+endif()
+
 if(CMAKE_HOST_WIN32)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${CURRENT_PACKAGES_DIR}/cmake"
-        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${CURRENT_PACKAGES_DIR}/debug/cmake"
-        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/cmake"
-        "${CURRENT_PACKAGES_DIR}/debug/cmake"
-    )
     file(GLOB release_exe "${CURRENT_PACKAGES_DIR}/bin/*.exe")
     file(GLOB debug_exe "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe")
     file(REMOVE ${release_exe} ${debug_exe})
-else()
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${CURRENT_PACKAGES_DIR}/lib/cmake/XercesC"
-        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-        "${CURRENT_PACKAGES_DIR}/debug/lib/cmake/XercesC"
-        "${CURRENT_PACKAGES_DIR}/share/xerces-c/cmake")
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/lib/cmake"
-        "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
-    )
 endif()
+
+file(READ ${CURRENT_PACKAGES_DIR}/share/xercesc/XercesCConfigInternal.cmake _contents)
+string(REPLACE
+    "get_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)"
+    "get_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)\nget_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)"
+    _contents
+    "${_contents}"
+)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/xercesc/XercesCConfigInternal.cmake "${_contents}")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
@@ -64,7 +42,9 @@ file(REMOVE_RECURSE
 )
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/xerces-c)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/xerces-c)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/xerces-c/LICENSE ${CURRENT_PACKAGES_DIR}/share/xerces-c/copyright)
 
 vcpkg_copy_pdbs()
+
+vcpkg_test_cmake(PACKAGE_NAME XercesC)
