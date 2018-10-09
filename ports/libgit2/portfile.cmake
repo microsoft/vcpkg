8bb061a20cc1389b76d3c9771254f48b0c4cdf28
kjpus <35301243+kjpus@users.noreply.github.com>
2018-10-09 03:27:45 -0400

    Build static libgit2 with dynamic CRT on Windows (#4432)
    * Build static libgit2 with dynamic CRT on Windows
    
    * [vcpkg-triplets] Revert changes to built-in triplets
    
    * [libgit2] Simplify computation of STATIC_CRT

--- ports/libgit2/portfile.cmake
+++ ports/libgit2/portfile.cmake
@@ -10,13 +10,17 @@ vcpkg_from_github(
     REPO libgit2/libgit2
     REF v0.27.4
     SHA512 d27db86eb1b9f0d4057f8538ba1985ee76c3ca106e57d417fa9bff79d575f91a07ad28693112b58dc1d61d68116a82e6a145f12276158f2806b6c4964d741f61
-    HEAD_REF master)
+    HEAD_REF master
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DBUILD_CLAR=OFF
+        -DSTATIC_CRT=${STATIC_CRT}
 )
 
 vcpkg_install_cmake()
