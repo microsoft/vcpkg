bd4976053f8fda2b76f3e9c514fdbe6eec13442c
Robert Schumacher <roschuma@microsoft.com>
2018-10-12 05:45:02 -0700

    [asyncplusplus] Fixup cmake targets

--- ports/asyncplusplus/portfile.cmake
+++ ports/asyncplusplus/portfile.cmake
@@ -17,14 +17,18 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+file(RENAME ${CURRENT_PACKAGES_DIR}/cmake/Async++.cmake ${CURRENT_PACKAGES_DIR}/cmake/Async++Targets.cmake)
+
 vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/async++)
+
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/async++/Async++Targets.cmake ${CURRENT_PACKAGES_DIR}/share/async++/Async++.cmake)
+
+file(READ ${CURRENT_PACKAGES_DIR}/share/async++/Async++Config.cmake _contents)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/async++/Async++Config.cmake "include(CMakeFindDependencyMacro)\n${_contents}")
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/asyncplusplus)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/asyncplusplus/LICENSE ${CURRENT_PACKAGES_DIR}/share/asyncplusplus/copyright)
-
-file(READ ${CURRENT_PACKAGES_DIR}/share/async++/Async++Config.cmake _contents)
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/async++/Async++Config.cmake "include(CMakeFindDependencyMacro)\n${_contents}")
 
 vcpkg_test_cmake(PACKAGE_NAME Async++)
