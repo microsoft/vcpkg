80e912a3e0165f90b6517ea70d959d450b03ae4f
Robert Schumacher <roschuma@microsoft.com>
2018-10-12 16:20:54 -0700

    [restbed] Disable tests to simplify build. Only build one flavor.

--- ports/restbed/portfile.cmake
+++ ports/restbed/portfile.cmake
@@ -1,5 +1,6 @@
 include(vcpkg_common_functions)
 
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,13 +8,15 @@ vcpkg_from_github(
     REF 0f8af8d8ed183a88e208adeb22da0080d5d74d1e
     SHA512 f0175a10c88f1ad4f16c8e4cff7ceea7b80c56b0724b59791c23e91f1ecf146dfdbda9e9238d31a35f21d8cdcc413b586cc633725dd0ba87de6b599a7087916f
     HEAD_REF master
+    PATCHES cmake.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-	OPTIONS
-	    -DBUILD_SSL=OFF
+    OPTIONS
+        -DBUILD_SSL=OFF
+        -DDISABLE_TESTS=ON
 )
 
 vcpkg_install_cmake()
@@ -22,5 +25,4 @@ vcpkg_install_cmake()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/restbed)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/restbed/LICENSE ${CURRENT_PACKAGES_DIR}/share/restbed/copyright)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/restbed/copyright COPYONLY)
