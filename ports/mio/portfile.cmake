f2ec67647203857438bf0ba7cc41ca279ff5d8eb
myd7349 <myd7349@gmail.com>
2018-10-19 07:49:17 +0800

    [mio] Clean

--- ports/mio/portfile.cmake
+++ ports/mio/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/mio RENAME copyright)
