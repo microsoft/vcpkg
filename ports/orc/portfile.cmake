c612d4ebbd41763a9a44d47b2042af28911e1213
Robert Schumacher <roschuma@microsoft.com>
2018-10-19 01:45:56 -0700

    [orc] Simplify patch. Note in usage that no targets are available. Don't build tools on windows because they are not supported.

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -1,4 +1,3 @@
-
 include(vcpkg_common_functions)
 
 vcpkg_from_github(
@@ -7,12 +6,8 @@ vcpkg_from_github(
     REF f47e02cfbf346f14d7f38c3ddd45d39e3b515847
     SHA512 5a389f4ab3b0ce4e7c8869493cf9e91feb4917a42bf2740abd71602fa03a2a53217b572e60af7328b7568dab084c07275ea275438ec8ae87f230a87fb60f2601
     HEAD_REF master
-)
-
-vcpkg_apply_patches(
-  SOURCE_PATH ${SOURCE_PATH}
-  PATCHES
-    "${CMAKE_CURRENT_LIST_DIR}/0001-dependencies-from-vcpkg.patch"
+    PATCHES
+      0001-dependencies-from-vcpkg.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/cmake_modules/FindGTest.cmake")
@@ -21,14 +16,28 @@ file(REMOVE "${SOURCE_PATH}/cmake_modules/FindProtobuf.cmake")
 file(REMOVE "${SOURCE_PATH}/cmake_modules/FindSnappy.cmake")
 file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZLIB.cmake")
 
+if(CMAKE_HOST_WIN32)
+  set(PROTOBUF_EXECUTABLE ${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc.exe)
+else()
+  set(PROTOBUF_EXECUTABLE ${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc)
+endif()
+
+if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+  set(BUILD_TOOLS OFF)
+else()
+  set(BUILD_TOOLS ON)
+endif()
+
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
   PREFER_NINJA
   OPTIONS
-  -DBUILD_TOOLS=ON
+  -DBUILD_TOOLS=${BUILD_TOOLS}
   -DBUILD_CPP_TESTS=OFF
   -DBUILD_JAVA=OFF
-  -DPROTOBUF_PROTOC_EXECUTABLE:FILEPATH=${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc
+  -DINSTALL_VENDORED_LIBS=OFF
+  -DBUILD_LIBHDFSPP=OFF
+  -DPROTOBUF_EXECUTABLE:FILEPATH=${PROTOBUF_EXECUTABLE}
 )
 
 vcpkg_install_cmake()
