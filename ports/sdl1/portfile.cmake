45087d29bc18c2f9291cf4ffc471138c228b75c9
Robert Schumacher <roschuma@microsoft.com>
2018-10-22 09:46:53 -0700

    [sdl1] Modernize

--- ports/sdl1/portfile.cmake
+++ ports/sdl1/portfile.cmake
@@ -1,28 +1,21 @@
 include(vcpkg_common_functions)
 
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SDL-Mirror/SDL
     REF release-1.2.15
     SHA512 38b94a650ec205377ae1503d0ec8a5254ef6d50ed0acac8d985b57b64bc16ea042cfa41e19e5ef8317980c4afb83186829f5bc3da9433d0a649dfd10554801b5
     HEAD_REF master
+    PATCHES export-symbols-only-in-shared-build.patch
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/export-symbols-only-in-shared-build.patch
-)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/SDL1_2017.sln DESTINATION ${SOURCE_PATH}/VisualC/ )
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/SDL.vcxproj DESTINATION ${SOURCE_PATH}/VisualC/SDL )
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/SDLmain.vcxproj DESTINATION ${SOURCE_PATH}/VisualC/SDLmain )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL_STATIC)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SDL_SHARED)
-string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" FORCE_STATIC_VCRT)
-
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/SDL1_2017.sln DESTINATION ${SOURCE_PATH}/VisualC/ )
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/SDL.vcxproj DESTINATION ${SOURCE_PATH}/VisualC/SDL )
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/SDLmain.vcxproj DESTINATION ${SOURCE_PATH}/VisualC/SDLmain )
-
-file(INSTALL ${SOURCE_PATH}/include/SDL_config.h.default DESTINATION ${SOURCE_PATH}/include/ RENAME SDL_config.h)
+configure_file(${SOURCE_PATH}/include/SDL_config.h.default ${SOURCE_PATH}/include/SDL_config.h COPYONLY)
 
 # This text file gets copied as a library, and included as one in the package 
 file(REMOVE_RECURSE ${SOURCE_PATH}/src/hermes/COPYING.LIB)
@@ -32,55 +25,16 @@ vcpkg_install_msbuild(
     PROJECT_SUBPATH VisualC/SDL1_2017.sln
     INCLUDES_SUBPATH include
     LICENSE_SUBPATH COPYING
-    PLATFORM Win32
-    OPTIONS
-        /p:SDL_STATIC=${SDL_STATIC}
-        /p:SDL_SHARED=${SDL_SHARED}
-        /p:VIDEO_VULKAN=OFF
-        /p:FORCE_STATIC_VCRT=${FORCE_STATIC_VCRT}
-        /p:LIBC=ON
     ALLOW_ROOT_INCLUDES
-    USE_VCPKG_INTEGRATION
 )
 
-if(EXISTS "${CURRENT_PACKAGES_DIR}/cmake")
-    vcpkg_fixup_cmake_targets(CONFIG_PATH "cmake")
-elseif(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/SDL")
-    vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/SDL")
-elseif(EXISTS "${CURRENT_PACKAGES_DIR}/SDL.framework/Resources")
-    vcpkg_fixup_cmake_targets(CONFIG_PATH "SDL.framework/Resources")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/doxyfile)
+
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib/manual-link)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/SDLmain.lib ${CURRENT_PACKAGES_DIR}/lib/manual-link/SDLmain.lib)
 endif()
-
-file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
-)
-
-file(GLOB BINS ${CURRENT_PACKAGES_DIR}/debug/bin/* ${CURRENT_PACKAGES_DIR}/bin/*)
-if(NOT BINS)
-    file(REMOVE_RECURSE
-        ${CURRENT_PACKAGES_DIR}/bin
-        ${CURRENT_PACKAGES_DIR}/debug/bin
-    )
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/SDLmain.lib ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link/SDLmaind.lib)
 endif()
-
-if(NOT VCPKG_CMAKE_SYSTEM_NAME)
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib/manual-link)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/SDLmain.lib ${CURRENT_PACKAGES_DIR}/lib/manual-link/SDLmain.lib)
-    endif()
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/SDLmain.lib ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link/SDLmaind.lib)
-    endif()
-
-    file(GLOB SHARE_FILES ${CURRENT_PACKAGES_DIR}/share/sdl/*.cmake)
-    foreach(SHARE_FILE ${SHARE_FILES})
-        file(READ "${SHARE_FILE}" _contents)
-        string(REPLACE "lib/SDLmain" "lib/manual-link/SDLmain" _contents "${_contents}")
-        file(WRITE "${SHARE_FILE}" "${_contents}")
-    endforeach()
-endif()
-
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/sdl1 RENAME copyright)
-vcpkg_copy_pdbs()
