ab83b167fc0f136dc257fa96705c12a0dc3a2b8b
Agustín Rodríguez <me@agurodriguez.net>
2018-10-24 12:23:44 -0300

    [websocketpp] update to 0.8.1

--- ports/websocketpp/portfile.cmake
+++ ports/websocketpp/portfile.cmake
@@ -4,8 +4,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zaphoyd/websocketpp
-    REF 0.7.0
-    SHA512 91a86d4f5120db3f474169bb146f865f82167b1e9eedabec8793b31005e4ce3d22083283bc1b9f9e37fa0da835addcb2b68260a27c753852c06b3b1bb2f3c12e
+    REF 0.8.1
+    SHA512 35e0261ed0285acf77d300768819bd380197de8acdf68223e2d7598481b9bfd69cb1653b435139771b1db6c16530c8d8cf9a887a8a6bba3fea126d0da4dbc13c
     HEAD_REF master
     PATCHES
         openssl_110.patch
@@ -23,4 +23,3 @@ file(COPY "${SOURCE_PATH}/websocketpp" DESTINATION "${CURRENT_PACKAGES_DIR}/incl
 set(INSTALL_INCLUDE_DIR "\${CMAKE_CURRENT_LIST_DIR}/../../include")
 set(WEBSOCKETPP_VERSION 0.7.0)
 configure_file(${SOURCE_PATH}/websocketpp-config.cmake.in "${CURRENT_PACKAGES_DIR}/share/websocketpp/websocketpp-config.cmake" @ONLY)
-configure_file(${SOURCE_PATH}/websocketpp-configVersion.cmake.in "${CURRENT_PACKAGES_DIR}/share/websocketpp/websocketpp-configVersion.cmake" @ONLY)
