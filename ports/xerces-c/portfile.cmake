cc3fae8e5ca3e47ff2d7e1de428aba2d515bec54
Robert Schumacher <roschuma@microsoft.com>
2018-11-01 19:47:20 -0700

    [xerces-c] Avoid building executables (#4576)

--- ports/xerces-c/portfile.cmake
+++ ports/xerces-c/portfile.cmake
@@ -6,11 +6,16 @@ vcpkg_from_github(
     REF Xerces-C_3_2_2
     SHA512 66f60fe9194376ac0ca99d13ea5bce23ada86e0261dde30686c21ceb5499e754dab8eb0a98adadd83522bda62709377715501f6dac49763e3a686f9171cc63ea
     HEAD_REF trunk
+    PATCHES disable-tests.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DDISABLE_TESTS=ON
+        -DDISABLE_DOC=ON
+        -DDISABLE_SAMPLES=ON
 )
 
 vcpkg_install_cmake()
@@ -19,16 +24,6 @@ if(EXISTS ${CURRENT_PACKAGES_DIR}/cmake)
     vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/xercesc)
 else()
     vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/XercesC TARGET_PATH share/xercesc)
-endif()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-else()
-    file(GLOB release_exe "${CURRENT_PACKAGES_DIR}/bin/*.exe")
-    file(GLOB debug_exe "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe")
-    if(release_exe OR debug_exe)
-        file(REMOVE ${release_exe} ${debug_exe})
-    endif()
 endif()
 
 file(READ ${CURRENT_PACKAGES_DIR}/share/xercesc/XercesCConfigInternal.cmake _contents)
