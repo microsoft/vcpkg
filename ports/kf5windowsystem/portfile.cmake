a6688c1d3698968ff1b3adcb6cce9a2cc475619e
Christian Meurin <cmmeurin@gmail.com>
2018-11-05 16:32:24 -0800

    KF5WindowSystem port

---
+++ ports/kf5windowsystem/portfile.cmake
@@ -0,0 +1,31 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kwindowsystem
+    REF v5.51.0
+    SHA512 a583614bf07d8465da1f000f930443f4feb81f43a49f0a966a9f4e5c873daf14f7bd4153c098162a4bb78f8715c516864ba0e3513330501bd1f9bd8ce5f04695
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5WindowSystem)
+vcpkg_copy_pdbs()
+
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/plugins ${CURRENT_PACKAGES_DIR}/plugins)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/plugins ${CURRENT_PACKAGES_DIR}/debug/plugins)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/plugins)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/plugins)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5windowsystem RENAME copyright)
