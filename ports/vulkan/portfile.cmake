9585e02af206c0fad07e18eee1308cc259ac12d5
Mark Holland <markianholland@live.co.uk>
2018-11-06 19:09:48 +0000

    Add VULKAN_SDK env variable to whitelist

--- ports/vulkan/portfile.cmake
+++ ports/vulkan/portfile.cmake
@@ -3,7 +3,7 @@
 # Other packages can depend on this package to declare a dependency on Vulkan.
 include(vcpkg_common_functions)
 
-if(DEFINED $ENV{VULKAN_SDK})
+if(EXISTS "$ENV{VULKAN_SDK}Include/vulkan/vulkan.h")
     message(FATAL_ERROR "Could not find Vulkan SDK. Before continuing, please download and install Vulkan from:"
                         "\n    https://vulkan.lunarg.com/sdk/home\n")
 endif()
