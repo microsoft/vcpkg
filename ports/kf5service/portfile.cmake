c1b0f03111ba9f13b892490f2f9837082403416d
Christian Meurin <cmmeurin@gmail.com>
2018-11-06 15:39:15 -0800

    KF5Service port

---
+++ ports/kf5service/portfile.cmake
@@ -0,0 +1,36 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kservice
+    REF v5.51.0
+    SHA512 27078f0d4e34923ec94f4af1750d31593c8637a66f0b8b11823f376e4b725952497332308774b75627d64415969889e65b8e828a1d94064d5f92b27124de6437
+    HEAD_REF master
+)
+
+vcpkg_find_acquire_program(FLEX)
+get_filename_component(FLEX_EXE_PATH ${FLEX} DIRECTORY)
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+set(ENV{PATH} "$ENV{PATH};${GETTEXT_PATH};${FLEX_EXE_PATH}")
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5Service)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/kbuildsycoca5.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/kbuildsycoca5.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5service RENAME copyright)
