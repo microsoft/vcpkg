086655a6dc50159208590e2ca2c407e1ab3e3670
Christian Meurin <cmmeurin@gmail.com>
2018-11-06 15:15:01 -0800

    KF5I18n port

---
+++ ports/kf5i18n/portfile.cmake
@@ -0,0 +1,33 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/ki18n
+    REF v5.51.0
+    SHA512 bdef33146e59e194767546831b42bfa124ac26712ac61218481f519b8b3688a40fee528c86251ed6a270bdf11d5ba4864543b008d9f36c43545b9b6cf8a23147
+    HEAD_REF master
+)
+
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+set(ENV{PATH} "$ENV{PATH};${GETTEXT_PATH}")
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+            -DPLUGIN_INSTALL_DIR=plugins
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5I18n)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5i18n RENAME copyright)
