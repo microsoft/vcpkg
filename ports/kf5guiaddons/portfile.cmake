9c7e6fdc8ce927747b25017520c99a4bb2ba6903
Christian Meurin <cmmeurin@gmail.com>
2018-11-06 14:46:46 -0800

    KF5GuiAddons port

---
+++ ports/kf5guiaddons/portfile.cmake
@@ -0,0 +1,28 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kguiaddons
+    REF v5.51.0
+    SHA512 f2e8bc19c472b26674a840879220b553764d51fe48105dabf2646f53e29f38fb9a5b67a78d5b2d4f70b319c4dcc628777ee72726f19397d3ab6b6bc2b9fe0679
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5GuiAddons)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5guiaddons RENAME copyright)
