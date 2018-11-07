ccf9033aa3992176fd53136f71e4fc8b9a142201
Christian Meurin <cmmeurin@gmail.com>
2018-11-06 20:36:14 -0800

    KF5UnitConversion port

---
+++ ports/kf5unitconversion/portfile.cmake
@@ -0,0 +1,32 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kunitconversion
+    REF v5.51.0
+    SHA512 7548c1312ea9087e14d00245f4abac0bfd4a93f1083cdd4ed0e09d03d8b33fc950b521b9294a19bfeb619cbb34b7bbb06550ef96c44ec4e7fd2217acd21c42d6
+    HEAD_REF master
+)
+
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+set(ENV{PATH} "$ENV{PATH};${GETTEXT_PATH}")
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5UnitConversion)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5unitconversion RENAME copyright)
