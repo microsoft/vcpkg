88c1df131f4b97e57de822940c3d0eef38dd53ca
Christian Meurin <cmmeurin@gmail.com>
2018-11-08 02:47:29 -0800

    KF5XmlGui port

---
+++ ports/kf5xmlgui/portfile.cmake
@@ -0,0 +1,35 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kxmlgui
+    REF v5.51.0
+    SHA512 89cd339f940913d75dbc5a548ee99b3465902c53d415ebb68346646a7907b91b3151e2834ce83b455ca88b74a5327b14ccd2ae3fdb915e79e8cf566e2c761de6
+    HEAD_REF master
+)
+
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+vcpkg_add_to_path(${GETTEXT_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5XmlGui)
+vcpkg_copy_pdbs()
+
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/ksendbugmail.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/ksendbugmail.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5xmlgui RENAME copyright)
