f68163142aea688b007a3c56e0576152a1ed66ca
Christian Meurin <cmmeurin@gmail.com>
2018-11-08 02:39:03 -0800

    KF5TextWidgets port

---
+++ ports/kf5textwidgets/portfile.cmake
@@ -0,0 +1,34 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/ktextwidgets
+    REF v5.51.0
+    SHA512 6afa0801e2f10f47616bb7f06b42a838acb6738244b91ff5733aa4efa6ea2db7856252130d83f0c552f11a6f41148bfbfcc0b4ac80553dcdcb7ecd2d48a6cd15
+    HEAD_REF master
+)
+
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+vcpkg_add_to_path(${GETTEXT_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5TextWidgets)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/data/kservicetypes5)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/data/kservicetypes5)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5textwidgets RENAME copyright)
