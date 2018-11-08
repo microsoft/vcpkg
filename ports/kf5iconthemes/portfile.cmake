9c306fd24e7547f7b223f60ef98a400fc53d9eab
Christian Meurin <cmmeurin@gmail.com>
2018-11-07 17:13:48 -0800

    KF5IconThemes port

---
+++ ports/kf5iconthemes/portfile.cmake
@@ -0,0 +1,35 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kiconthemes
+    REF v5.51.0
+    SHA512 dd8d34f6cc8bc78c08c30dc5ad6a5d892ae6a13e4024754dfdb2c44553f5ac1ec3cfddb9356f7962799589c01c2bd39a922e7d83af33c2d40fc2c6c9d13b4a09
+    HEAD_REF master
+)
+
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES mingw-w64-i686-gettext)
+set(GETTEXT_PATH ${MSYS_ROOT}/mingw32/bin)
+set(ENV{PATH} "$ENV{PATH};${GETTEXT_PATH}")
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+            -DKDE_INSTALL_PLUGINDIR=plugins
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5IconThemes)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/kiconfinder5.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/kiconfinder5.exe)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/kf5iconthemes RENAME copyright)
