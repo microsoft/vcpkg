0d9fac6f6a47b6d5df1578009b6b4cbb1602feb1
Robert Schumacher <roschuma@microsoft.com>
2018-11-08 19:17:08 -0800

    [vcpkg_find_acquire_program] Fix PYTHON3 on non-Windows

--- ports/xmsh/portfile.cmake
+++ ports/xmsh/portfile.cmake
@@ -1,5 +1,6 @@
 include(vcpkg_common_functions)
-find_program(PYTHON3 /usr/bin/python3)
+
+vcpkg_find_acquire_program(PYTHON3)
 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
@@ -7,14 +8,14 @@ vcpkg_from_github(
   REF v0.4.1
   SHA512 7bd9fe9e565b33722fec37a7e3d9bd8b7b132692add5d26e31954367fb284b49a26a21532ddcb0e425af7f8208e755f21f2d8de81b33ed2a1149724f4ccd2c38
   HEAD_REF master
-  )
+)
 
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-   -DPYTHON3_EXECUTABLE=${PYTHON3}
   PREFER_NINJA
-  )
+  OPTIONS
+    -DPYTHON3_EXECUTABLE=${PYTHON3}
+)
 
 vcpkg_install_cmake()
 
@@ -24,4 +25,5 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
+
 vcpkg_copy_pdbs()
