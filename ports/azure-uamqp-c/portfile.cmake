13e7f60bcab978ba65a929ab2c8bbf1e1e92328c
Rajeev Massand <rajeev.massand@microsoft.com>
2018-11-16 15:45:32 -0800

    [azure-iot-sdk-c] Update sdk to 1.2.10 and update dependencies (#4717)
    * new versions for azure-iot-sdk-c, azure-c-shared-utility, azure-uamqp-c, azure-umqtt-c, azure-uhttp-c
    
    * [azure-iot-sdk-c] Modernize & Cleanup
    
    * fix for x64-windows

--- ports/azure-uamqp-c/portfile.cmake
+++ ports/azure-uamqp-c/portfile.cmake
@@ -1,23 +1,16 @@
 include(vcpkg_common_functions)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    message("azure-uamqp-c only supports static linkage")
-    set(VCPKG_LIBRARY_LINKAGE "static")
-endif()
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-uamqp-c
-    REF 1.2.3
-    SHA512 18fc978517371fcb19e1c078f07d06b3bf8ec046c5cba955dd3cfe0a364d8775542acc970d81fa42384942ea4db7fb60d8939e80e90baf582c9d9e6ff0b577b5
+    REF ec107011716bc797d3d7a4a4a1141df4367038a3
+    SHA512 db25018a6f93a29da4bae8426f6e021a0d823075375f69f617c6aba776f5ac6ed5107e055f0a436f22ede2057045d28fab15607a58703c6c43937abec87ab076
     HEAD_REF master
 )
 
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH} PATCHES ${CMAKE_CURRENT_LIST_DIR}/glob-headers.patch)
-
 file(COPY ${CURRENT_INSTALLED_DIR}/share/azure-c-shared-utility/azure_iot_build_rules.cmake DESTINATION ${SOURCE_PATH}/deps/azure-c-shared-utility/configs/)
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_AS_DYNAMIC)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -25,7 +18,7 @@ vcpkg_configure_cmake(
     OPTIONS
         -Dskip_samples=ON
         -Duse_installed_dependencies=ON
-        -Dbuild_as_dynamic=${BUILD_AS_DYNAMIC}
+        -Dbuild_as_dynamic=OFF
 )
 
 vcpkg_install_cmake()
@@ -34,8 +27,6 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/uamqp)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
-file(INSTALL
-    ${SOURCE_PATH}/LICENSE
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/azure-uamqp-c RENAME copyright)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/azure-uamqp-c/copyright COPYONLY)
 
 vcpkg_copy_pdbs()
