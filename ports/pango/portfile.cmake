c8caa2667bf47e769d0b792367b65ca57ca876fa
Angel Sánchez <angelsanchez@inedit.com>
2018-11-21 11:41:16 +0100

    fix pango for macOS: added an already parsed config.h.unix and moved win32 code conditionally for platform

--- ports/pango/portfile.cmake
+++ ports/pango/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_download_distfile(ARCHIVE
 
 vcpkg_extract_source_archive(${ARCHIVE})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/config.h.unix DESTINATION ${SOURCE_PATH})
 
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
