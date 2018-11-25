70206574732976848c60c27eb2d9df0071210c73
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2018-11-26 03:12:06 +0900

    [ilmbase] Update to IlmBase v2.3.0
    Update IlmBase port to IlmBase v2.3.0.

--- ports/ilmbase/portfile.cmake
+++ ports/ilmbase/portfile.cmake
@@ -2,38 +2,38 @@ if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
     message(FATAL_ERROR "UWP build not supported")
 endif()
 
-set(ILM_VERSION 2.2.1)
-set(ILM_HASH a08ddd9069b34a93612445a445a2ddf80c0e22349bcf221a3cc6e9f5575180b08a8b597009dacabf072360e7162e15964988bc79e8ec82cf3da6507148a75320)
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ilmbase-${ILM_VERSION})
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://download.savannah.nongnu.org/releases/openexr/ilmbase-${ILM_VERSION}.tar.gz"
-    FILENAME "ilmbase-${ILM_VERSION}.tar.gz"
-    SHA512 ${ILM_HASH})
-
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/fix-parallel-build.patch)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO openexr/openexr
+    REF v2.3.0
+    SHA512 268ae64b40d21d662f405fba97c307dad1456b7d996a447aadafd41b640ca736d4851d9544b4741a94e7b7c335fe6e9d3b16180e710671abfc0c8b2740b147b2
+    HEAD_REF develop
+    PATCHES 001-26c86195935f5365685bfefe046f0ac7e98fa231.patch
+            002-c2557b73c97c5dfb9e3eeff6b7622566edcfb54b.patch
+            003-ef3ca9e2303fe5f74263e9ac4f7a068baf3ed01f.patch
+            004-027a323e375ebe46a6a74863c3c6306dda4427aa.patch
+            005-eb2392c88152c05c3b554cc14bc8a1fafb344340.patch
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA)
+    PREFER_NINJA
+    OPTIONS
+        -DOPENEXR_BUILD_ILMBASE=ON
+        -DOPENEXR_BUILD_OPENEXR=OFF
+        -DOPENEXR_BUILD_PYTHON_LIBS=OFF
+        -DOPENEXR_BUILD_SHARED=ON
+        -DOPENEXR_BUILD_STATIC=ON
+        -DOPENEXR_BUILD_TESTS=OFF
+        -DOPENEXR_BUILD_UTILS=OFF
+        -DOPENEXR_BUILD_VIEWERS=OFF
+)
+
 vcpkg_install_cmake()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    foreach(SUBDIR "" "/debug")
-        file(GLOB DLLS ${CURRENT_PACKAGES_DIR}${SUBDIR}/lib/*.dll)
-        file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}${SUBDIR}/bin)
-        file(REMOVE ${DLLS})
-    endforeach()
-endif()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
 vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 foreach(HEADER halfExport.h IexExport.h IexMathFloatExc.h IlmThreadExport.h ImathExport.h)
     file(READ ${CURRENT_PACKAGES_DIR}/include/OpenEXR/${HEADER} HEADER_FILE)
