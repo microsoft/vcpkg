b28fb3f9fb3e27315a3624caa711979bfb878c01
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2018-11-26 03:13:09 +0900

    [openexr] Update to OpenEXR v2.3.0
    Update OpenEXR port to OpenEXR v2.3.0.

--- ports/openexr/portfile.cmake
+++ ports/openexr/portfile.cmake
@@ -1,61 +1,51 @@
-set(OPENEXR_VERSION 2.2.1)
-set(OPENEXR_HASH 192100c6ac47534f3a93c55327d2ab90b07a8265156855086b326184328c257dcde12991b3f3f1831e2df4226fe884adcfe481c2f02a157c715aee665e89a480)
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/openexr-${OPENEXR_VERSION})
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://download.savannah.nongnu.org/releases/openexr/openexr-${OPENEXR_VERSION}.tar.gz"
-    FILENAME "openexr-${OPENEXR_VERSION}.tar.gz"
-    SHA512 ${OPENEXR_HASH})
 
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-    "${CMAKE_CURRENT_LIST_DIR}/add-missing-export.patch"
-    "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.patch"
-    "${CMAKE_CURRENT_LIST_DIR}/fix-static-linking.patch"
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO openexr/openexr
+    REF v2.3.0
+    SHA512 268ae64b40d21d662f405fba97c307dad1456b7d996a447aadafd41b640ca736d4851d9544b4741a94e7b7c335fe6e9d3b16180e710671abfc0c8b2740b147b2
+    HEAD_REF develop
+    PATCHES 001-26c86195935f5365685bfefe046f0ac7e98fa231.patch
+            002-c2557b73c97c5dfb9e3eeff6b7622566edcfb54b.patch
+            003-ef3ca9e2303fe5f74263e9ac4f7a068baf3ed01f.patch
+            004-027a323e375ebe46a6a74863c3c6306dda4427aa.patch
+            005-eb2392c88152c05c3b554cc14bc8a1fafb344340.patch
+            FindIlmBase.patch
 )
 
-# Ensure helper executables can run during build
-set(ENV{PATH} "$ENV{PATH};${CURRENT_INSTALLED_DIR}/bin")
+# Copy OpenEXR/OpenEXRConfig.h for FindIlmBase.cmake
+file(COPY ${CURRENT_PORT_DIR}/OpenEXRConfig.h DESTINATION ${CURRENT_INSTALLED_DIR}/include/OpenEXR)
 
-# In debug build buildsystem cannot locate IlmBase headers
-set(VCPKG_C_FLAGS_DEBUG "${VCPKG_C_FLAGS_DEBUG}")
-set(VCPKG_CXX_FLAGS_DEBUG "${VCPKG_CXX_FLAGS_DEBUG} -I\"${CURRENT_INSTALLED_DIR}/include/OpenEXR\"")
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" OPENEXR_SHARED_LIBS)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" OPENEXR_STATIC_LIBS)
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH}
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DOPENEXR_BUILD_ILMBASE=OFF
+        -DOPENEXR_BUILD_OPENEXR=ON
+        -DOPENEXR_BUILD_PYTHON_LIBS=OFF
+        -DOPENEXR_BUILD_SHARED=${OPENEXR_SHARED_LIBS}
+        -DOPENEXR_BUILD_STATIC=${OPENEXR_STATIC_LIBS}
+        -DOPENEXR_BUILD_TESTS=OFF
+        -DOPENEXR_BUILD_UTILS=OFF
+        -DOPENEXR_BUILD_VIEWERS=OFF
+        -DOPENEXR_LOCATION=${CURRENT_INSTALLED_DIR}
+    OPTIONS_RELEASE
+        -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}
+        -DOPENEXR_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}
     OPTIONS_DEBUG
         -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}/debug
-    OPTIONS_RELEASE
-        -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR})
+        -DOPENEXR_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}/debug
+)
 
 vcpkg_install_cmake()
-
-# if you need to have OpenEXR tools, edit CMakeLists.txt.patch and remove the part that disables building executables,
-# then remove the following line which deletes them and finally use vcpkg_copy_tool_dependencies() to save them
-# (may require additional patching to the OpenEXR toolchain which is really broken)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    foreach(SUBDIR "" "/debug")
-        file(GLOB DLLS ${CURRENT_PACKAGES_DIR}${SUBDIR}/lib/*.dll)
-        file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}${SUBDIR}/bin)
-        file(REMOVE ${DLLS})
-    endforeach()
-endif()
-
 vcpkg_copy_pdbs()
 
-file(READ ${CURRENT_PACKAGES_DIR}/include/OpenEXR/ImfExport.h HEADER_FILE)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    string(REPLACE "defined(OPENEXR_DLL)" "1" HEADER_FILE "${HEADER_FILE}")
-else()
-    string(REPLACE "defined(OPENEXR_DLL)" "0" HEADER_FILE "${HEADER_FILE}")
-endif()
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/OpenEXR/ImfExport.h "${HEADER_FILE}")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/openexr)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/openexr/LICENSE ${CURRENT_PACKAGES_DIR}/share/openexr/copyright)
