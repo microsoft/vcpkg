40dde0dad1cdd03bfc700d16262410398d0dd4d0
myd7349 <myd7349@gmail.com>
2018-11-27 08:09:03 +0800

    [rapidstring] Rename package to be unofficial-rapidstring

--- ports/rapidstring/portfile.cmake
+++ ports/rapidstring/portfile.cmake
@@ -19,6 +19,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/rapidstring TARGET_PATH share/unofficial-rapidstring)
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
 # Handle copyright
