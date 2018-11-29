33fe515d9d762ae4a8ae519e36a06f0bff35a867
Roland <roland.waltersson@king.com>
2018-11-29 16:08:03 +0100

    Angle updated to master, UWP fix for Angle

--- ports/angle/portfile.cmake
+++ ports/angle/portfile.cmake
@@ -5,15 +5,11 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "ANGLE currently only supports being built for desktop")
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/angle
-    REF 8d471f907d8d4ec1d46bc9366493bd76c11c1870 
-    SHA512 b4670caeeaa5d662bc82702eb5f620123812ea6b5d82f57a65df54ae25cdaa5c9ff0fdb592448b07569d9c09af3d3c51b0b2f135c5800d1845b425009656bf18
+    REF efe061bd4f9de0f8da3135bcabff7f40e05e91f7 
+    SHA512 0fdd8d4149fb0b75edd9d408732176bc8f7a2c2000abb9d3d783e96e88bf1ec52fe9aa0bf1d24a7fdb3a07f00975a2a7058e724b0fd70b71baf495d45ca492a2
     HEAD_REF master
 )
 vcpkg_apply_patches(
@@ -25,15 +21,14 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/commit.h DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+    SOURCE_PATH ${SOURCE_PATH}    
     OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=1
 )
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-angle)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/angle ${CURRENT_PACKAGES_DIR}/share/unofficial-angle)
+#vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-angle)
+#file(RENAME ${CURRENT_PACKAGES_DIR}/share/angle ${CURRENT_PACKAGES_DIR}/share/unofficial-angle)
 
 vcpkg_copy_pdbs()
 
