5edd66af9b4105561816c595616a4445520b7c8f
Phil Christensen <philc@microsoft.com>
2018-12-04 13:33:50 -0800

    Add local FILENAME

--- ports/ace/portfile.cmake
+++ ports/ace/portfile.cmake
@@ -10,6 +10,7 @@ include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ACE_wrappers/ace)
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_3/ACE-6.5.3.zip"
+    FILENAME ACE-6.5.3.zip
     SHA512 1c24c7a138b6580e0f2a71d3e797ed846a3c046854c19d10175198dc519b610b2ac684d6e0e3999705319304b6eee3f09405cb43ce48a869d2f1446342da469d
 )
 vcpkg_extract_source_archive(${ARCHIVE})
