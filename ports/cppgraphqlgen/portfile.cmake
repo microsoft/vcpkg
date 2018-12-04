d53b60f01bb596cefbfc98914a2f7b634e3f4a2b
Bill Avery <billav@microsoft.com>
2018-12-03 21:22:46 -0800

    Upgrade to cppgraphqlgen v0.6

--- ports/cppgraphqlgen/portfile.cmake
+++ ports/cppgraphqlgen/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/cppgraphqlgen
-    REF 47e90d658bb7853163a01f39c2d9cc0e7a02768d # updated v0.5
-    SHA512 b6a8785bc4e4880afd6a7feb15c23086531b8c521676afae3f114706df7ea035137e7da35241146c41b1add654b24aca7a90632c48917949bf976e5030611cc9
+    REF v0.6
+    SHA512 cc2596f5cd975377fcc9432eee11973e2ff044c244c3ac13c3f45549874c2fe0ac5c48bccb96813b48a1b7c940bb54decc194d9eb358c6ae39d1288b9ce58b01
     HEAD_REF master
 )
 
