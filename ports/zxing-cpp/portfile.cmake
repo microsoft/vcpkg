86b9bb6a009cbf425a80849cd6eaa7ba1b6e5ceb
Robert Schumacher <roschuma@microsoft.com>
2018-12-04 19:13:33 -0800

    [zxing-cpp] Rename from zxing. Use full ref. Only require opencv[core].

--- ports/zxing-cpp/portfile.cmake
+++ ports/zxing-cpp/portfile.cmake
@@ -1,10 +1,12 @@
 #header-only library
 include(vcpkg_common_functions)
 
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO glassechidna/zxing-cpp
-    REF 5aad474
+    REF 5aad4744a3763d814df98a18886979893e638274
     SHA512 a079ad47171224de4469e76bf0779b6ebc9c6dfb3604bd5dbf5e6e5f321d9e6255f689daa749855f8400023602f1773214013c006442e9b32dd4b8146c888c02
     HEAD_REF master
 )
@@ -16,13 +18,13 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/zxing/cmake")
+vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/zxing/cmake" TARGET_PATH share/zxing)
 
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/zxing)
 if (WIN32)
-file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/zxing)
+    file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/zxing)
 else(WIN32)
-file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing DESTINATION ${CURRENT_PACKAGES_DIR}/tools/zxing)
+    file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing DESTINATION ${CURRENT_PACKAGES_DIR}/tools/zxing)
 endif(WIN32)
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/zxing)
 
@@ -36,6 +38,6 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/zxing)
 
 # Handle copyright
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/zxing)
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/zxing)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/zxing/COPYING ${CURRENT_PACKAGES_DIR}/share/zxing/copyright)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/zxing-cpp)
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/zxing-cpp)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/zxing-cpp/COPYING ${CURRENT_PACKAGES_DIR}/share/zxing-cpp/copyright)
