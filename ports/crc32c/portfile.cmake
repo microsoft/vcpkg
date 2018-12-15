bc79cf9a7c39067fcbd7765ec8129b232cb35da4
Carlos O'Ryan <coryan@google.com>
2018-12-14 23:12:44 -0500

    Apply patch to export all symbols.

--- ports/crc32c/portfile.cmake
+++ ports/crc32c/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
   REF 1.0.6
   SHA512 c30f6510d6348f15dcdddc06e375f21a69681cd615483d67628b32de747e5e98200fa49faf7e3fc30a1302991fd1f9c9a706c9eb4e13c9c6c09e74066474ea7b
   HEAD_REF master
+  PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001_export_symbols.patch
 )
 
 vcpkg_configure_cmake(
