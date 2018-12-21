64d702880d85fd98a81ff61f7f8baffcc964df07
Bill Avery <wravery@gmail.com>
2018-12-21 09:09:38 -0800

    [cppgraphqlgen] Update to version 0.7

--- ports/cppgraphqlgen/portfile.cmake
+++ ports/cppgraphqlgen/portfile.cmake
@@ -3,17 +3,17 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/cppgraphqlgen
-    REF v0.6
-    SHA512 cc2596f5cd975377fcc9432eee11973e2ff044c244c3ac13c3f45549874c2fe0ac5c48bccb96813b48a1b7c940bb54decc194d9eb358c6ae39d1288b9ce58b01
+    REF v0.7
+    SHA512 503c3fdd7d5aed32acaeacc5b3be26ec795cd58948229a83c62740938833384166aa750980a305da7f2b19294726d0b19b95bf8e4d4889a40293185469703012
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    PREFER_NINJA
     OPTIONS -DBUILD_TESTS=OFF -DUPDATE_SAMPLES=OFF
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    OPTIONS_RELEASE -DCMAKE_INSTALL_CONFIGDIR=${CURRENT_PACKAGES_DIR}/share/cppgraphqlgen -DCMAKE_INSTALL_TOOLSDIR=${CURRENT_PACKAGES_DIR}/tools/cppgraphqlgen
+    OPTIONS_DEBUG -DCMAKE_INSTALL_CONFIGDIR=${CURRENT_PACKAGES_DIR}/debug/share/cppgraphqlgen -DCMAKE_INSTALL_TOOLSDIR=${CURRENT_PACKAGES_DIR}/debug/tools/cppgraphqlgen
 )
 
 vcpkg_install_cmake()
