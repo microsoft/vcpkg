1aa7d6a4fc8388a4f3066100757db2483b916c46
Lennart Trunk <lennart.trunk@outlook.de>
2019-01-05 02:55:38 +0100

    add qt5-x11extras

---
+++ ports/qt5-x11extras/portfile.cmake
@@ -0,0 +1,5 @@
+include(vcpkg_common_functions)
+
+include(${CURRENT_INSTALLED_DIR}/share/qt5modularscripts/qt_modular_library.cmake)
+
+qt_modular_library(qtwebview 84a52aedac59f1bc4372538dc0810c742f87f04b2ee599e7ed3f8f0cfe0e13d652e80d3cf7de95395bbe1285a60cc15791760e17f0975cf8399527cdadbd7c1b)
