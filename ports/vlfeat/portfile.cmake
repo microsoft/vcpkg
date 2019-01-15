3f9c6b0596de1aa65b167157b70ad62f94bf7276
cDc <cdc.seacave@gmail.com>
2019-01-15 15:57:53 +0200

    [vlfeat] Linux fix for 1.0.1

--- ports/vlfeat/portfile.cmake
+++ ports/vlfeat/portfile.cmake
@@ -22,7 +22,11 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH "cmake")
+if(WIN32)
+	vcpkg_fixup_cmake_targets(CONFIG_PATH "CMake")
+else()
+	vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/vlfeat")
+endif()
 
 vcpkg_copy_pdbs()
 
