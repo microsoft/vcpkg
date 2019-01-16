92a8da2224ada02689139a2bc910f212f48b5d57
Victor Romero <viromer@microsoft.com>
2019-01-16 15:44:51 -0800

    [tinyexif] Use SHA as download source code ref

--- ports/tinyexif/portfile.cmake
+++ ports/tinyexif/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cdcseacave/TinyEXIF
-    REF 1.0.2
-    SHA512 95a88c45c9f4812266cc6b7cf57e93be1229513274b446f5da6b9c4d5dc07c07985bf6ad6ffc31de673e88ef6e428cb29b1b58ad78dee17cb36fc2b859a55f98
+    REF c57a5fec1a847ec0e7b482b2da72e1f04fc2e147
+    SHA512 a968e79b4b7a6f5f189328a8dc493da5c0d568526b5dca46b9bfc0cf85295fb348bd0b38828fedbb7b6e4467fc8932d0d7b435b4acbfe662e954cbed1d0ea914
     HEAD_REF master
 )
 
