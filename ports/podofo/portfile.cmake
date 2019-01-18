91dcfb92dfa4bbde0816fa2102b2b943b2068db0
Alexej Harm <alexej.h@xiphos.de>
2019-01-18 08:06:04 +0100

    [podofo] Change auto_ptr to unique_ptr.

--- ports/podofo/portfile.cmake
+++ ports/podofo/portfile.cmake
@@ -11,6 +11,10 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     REF ${PODOFO_VERSION}
 )
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES ${CMAKE_CURRENT_LIST_DIR}/unique_ptr.patch
+)
 
 set(PODOFO_NO_FONTMANAGER ON)
 if("fontconfig" IN_LIST FEATURES)
