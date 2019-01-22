1ed05f9265d01a55b80bdf6e5ee3163bdb6686c3
Victor Romero <viromer@microsoft.com>
2019-01-22 14:33:34 -0800

    [tinyspline] Supress undefined macro warning treated as error

--- ports/tinyspline/portfile.cmake
+++ ports/tinyspline/portfile.cmake
@@ -6,7 +6,8 @@ vcpkg_from_github(
     SHA512 50cf4927b311eeca6de7954f1b8d585cbf71355f5e5b0aac2f92f5f4ba37986df16eb3251f94a2304d27dab27d4f6b838b410f53e30de28bab53facf194eb640
     HEAD_REF master
     PATCHES
-        cmake.patch
+        "cmake.patch"
+        "ignore-preprocessor-symbol-not-defined-warning.patch"
 )
 
 vcpkg_configure_cmake(
