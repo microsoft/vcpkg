a9ab72726f09bfbe52e0d7e4260ef770b571a32e
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-01-24 13:40:49 +0100

    [OpenEXR] Update to v2.3.0, enable tools, improve cmake module while still maintaining backwards compatibility (there's an official one distributed with the package but it is completely different, for now it is avoided)

--- ports/openexr/portfile.cmake
+++ ports/openexr/portfile.cmake
@@ -1,51 +1,46 @@
-set(OPENEXR_VERSION 2.2.1)
-set(OPENEXR_HASH 192100c6ac47534f3a93c55327d2ab90b07a8265156855086b326184328c257dcde12991b3f3f1831e2df4226fe884adcfe481c2f02a157c715aee665e89a480)
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/openexr-${OPENEXR_VERSION})
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://download.savannah.nongnu.org/releases/openexr/openexr-${OPENEXR_VERSION}.tar.gz"
-    FILENAME "openexr-${OPENEXR_VERSION}.tar.gz"
-    SHA512 ${OPENEXR_HASH})
 
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-    "${CMAKE_CURRENT_LIST_DIR}/add-missing-export.patch"
-    "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.patch"
-    "${CMAKE_CURRENT_LIST_DIR}/fix-static-linking.patch"
+set(OPENEXR_VERSION 2.3.0)
+set(OPENEXR_HASH 268ae64b40d21d662f405fba97c307dad1456b7d996a447aadafd41b640ca736d4851d9544b4741a94e7b7c335fe6e9d3b16180e710671abfc0c8b2740b147b2)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO openexr/openexr
+  REF v${OPENEXR_VERSION}
+  SHA512 ${OPENEXR_HASH}
+  HEAD_REF master
 )
 
-# Ensure helper executables can run during build
-set(ENV{PATH} "$ENV{PATH};${CURRENT_INSTALLED_DIR}/bin")
-
-# In debug build buildsystem cannot locate IlmBase headers
-set(VCPKG_C_FLAGS_DEBUG "${VCPKG_C_FLAGS_DEBUG}")
-set(VCPKG_CXX_FLAGS_DEBUG "${VCPKG_CXX_FLAGS_DEBUG} -I\"${CURRENT_INSTALLED_DIR}/include/OpenEXR\"")
-
 vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS_DEBUG
-        -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}/debug
-    OPTIONS_RELEASE
-        -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR})
+  PREFER_NINJA
+  OPTIONS
+    -DOPENEXR_BUILD_PYTHON_LIBS:BOOL=FALSE
+  OPTIONS_DEBUG
+    -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR}/debug
+  OPTIONS_RELEASE
+    -DILMBASE_PACKAGE_PREFIX=${CURRENT_INSTALLED_DIR})
 
 vcpkg_install_cmake()
 
-# if you need to have OpenEXR tools, edit CMakeLists.txt.patch and remove the part that disables building executables,
-# then remove the following line which deletes them and finally use vcpkg_copy_tool_dependencies() to save them
-# (may require additional patching to the OpenEXR toolchain which is really broken)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    foreach(SUBDIR "" "/debug")
-        file(GLOB DLLS ${CURRENT_PACKAGES_DIR}${SUBDIR}/lib/*.dll)
-        file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}${SUBDIR}/bin)
-        file(REMOVE ${DLLS})
-    endforeach()
-endif()
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrenvmap.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrheader.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrmakepreview.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrmaketiled.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrmultipart.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrmultiview.exe)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/exrstdattr.exe)
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/openexr/)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrenvmap.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrenvmap.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrheader.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrheader.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrmakepreview.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrmakepreview.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrmaketiled.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrmaketiled.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrmultipart.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrmultipart.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrmultiview.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrmultiview.exe)
+file(RENAME ${CURRENT_PACKAGES_DIR}/bin/exrstdattr.exe ${CURRENT_PACKAGES_DIR}/tools/openexr/exrstdattr.exe)
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/openexr)
 
 vcpkg_copy_pdbs()
 
@@ -57,7 +52,7 @@ else()
 endif()
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/OpenEXR/ImfExport.h "${HEADER_FILE}")
 
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/openexr)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/openexr/LICENSE ${CURRENT_PACKAGES_DIR}/share/openexr/copyright)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/OpenEXR)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/OpenEXR/LICENSE ${CURRENT_PACKAGES_DIR}/share/OpenEXR/copyright)
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindOpenEXR.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/openexr)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindOpenEXR.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/OpenEXR)
