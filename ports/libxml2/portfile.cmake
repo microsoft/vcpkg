94ae0a477771d56fa637b90268e2678977a32305
myd7349 <myd7349@gmail.com>
2019-01-26 16:31:49 +0800

    [libxml2] Modernize

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -2,14 +2,16 @@ include(vcpkg_common_functions)
 
 set(_libxml2_version 2.9.9)
 
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libxml2-${_libxml2_version})
-
 vcpkg_download_distfile(ARCHIVE
     URLS "ftp://xmlsoft.org/libxml2/libxml2-${_libxml2_version}.tar.gz" "http://xmlsoft.org/sources/libxml2-${_libxml2_version}.tar.gz"
     FILENAME "libxml2-${_libxml2_version}.tar.gz"
     SHA512 cb7784ba4e72e942614e12e4f83f4ceb275f3d738b30e3b5c1f25edf8e9fa6789e854685974eed95b362049dbf6c8e7357e0327d64c681ed390534ac154e6810
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${_libxml2_version}
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
