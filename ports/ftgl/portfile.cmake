f67fca06353df9354347d78fa885f4524ba087ed
Victor Romero <viromer@microsoft.com>
2019-01-29 11:24:30 -0800

    [ftgl] Fix targets import path

--- ports/ftgl/portfile.cmake
+++ ports/ftgl/portfile.cmake
@@ -16,6 +16,16 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+# Fix relative paths that vcpkg_fixup_cmake_targets didn't pick up
+set(TARGETS_CMAKE ${CURRENT_PACKAGES_DIR}/share/ftgl/FTGL-targets.cmake)
+file(READ ${TARGETS_CMAKE} _contents)
+string(REGEX REPLACE
+    "get_filename_component\\(_IMPORT_PREFIX \"\\\${CMAKE_CURRENT_LIST_FILE}\" PATH\\)(\nget_filename_component\\(_IMPORT_PREFIX \"\\\${_IMPORT_PREFIX}\" PATH\\))*"
+    "get_filename_component(_IMPORT_PREFIX \"\${CMAKE_CURRENT_LIST_FILE}\" PATH)\nget_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)\nget_filename_component(_IMPORT_PREFIX \"\${_IMPORT_PREFIX}\" PATH)"
+    _contents "${_contents}")
+file(WRITE ${TARGETS_CMAKE} "${_contents}")
+
+
 vcpkg_test_cmake(PACKAGE_NAME FTGL)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
