66c1a74ed864fa1a5ba1cf30bda00144d4ffe24a
Victor Romero <viromer@microsoft.com>
2019-01-29 10:06:02 -0800

    [anax] Fix apply patches

--- ports/anax/portfile.cmake
+++ ports/anax/portfile.cmake
@@ -14,11 +14,8 @@ vcpkg_from_github(
     REF v2.1.0
     SHA512 b573733b5f9634bf8cfc5b0715074f9a8ee29ecb48dc981d9371254a1f6ff8afbbb9ba6aa0877d53e518e5486ecc398a6d331fb9b5dbfd17d8707679216e11a3
     HEAD_REF master
-)
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/Add-bin-output.patch
+    PATCHES 
+        ${CMAKE_CURRENT_LIST_DIR}/Add-bin-output.patch
 )
 
 vcpkg_configure_cmake(
