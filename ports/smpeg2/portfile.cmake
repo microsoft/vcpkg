7a70c0b989fe461bdc9ebd311db373c1163977cc
Victor Romero <viromer@microsoft.com>
2019-01-29 10:20:11 -0800

    [smpeg2] Fix apply patch

--- ports/smpeg2/portfile.cmake
+++ ports/smpeg2/portfile.cmake
@@ -1,15 +1,16 @@
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/smpeg2-2.0.0)
+
+set(SMPEG2_VERSION 2.0.0)
+
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.libsdl.org/projects/smpeg/release/smpeg2-2.0.0.tar.gz"
-    FILENAME "smpeg2-2.0.0.tar.gz"
+    URLS "https://www.libsdl.org/projects/smpeg/release/smpeg2-${SMPEG2_VERSION}.tar.gz"
+    FILENAME "smpeg2-${SMPEG2_VERSION}.tar.gz"
     SHA512 80a779d01e7aa76778ef6ceea8041537db9e4b354df413214c4413c875cb98551891cef98fa0f057cc6a3222e4983da9ae65b86bdad2f87f9e2a6751837e2baf)
 
-vcpkg_extract_source_archive(${ARCHIVE})
-
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${SMPEG2_VERSION}
     PATCHES 
         ${CMAKE_CURRENT_LIST_DIR}/correct-sdl-headers-dir.patch)
 
