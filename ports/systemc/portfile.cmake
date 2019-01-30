69ff0762d113e4273f57c6e10e7975639fbf0af9
Robert Schumacher <roschuma@microsoft.com>
2019-01-29 17:49:17 -0800

    [systemc] Use vcpkg_extract_source_archive_ex()

--- ports/systemc/portfile.cmake
+++ ports/systemc/portfile.cmake
@@ -1,30 +1,28 @@
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/systemc-2.3.3)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+set(SYSTEMC_VERSION 2.3.3)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.zip"
-    FILENAME "systemc-2.3.3.zip"
+    URLS "https://www.accellera.org/images/downloads/standards/systemc/systemc-${SYSTEMC_VERSION}.zip"
+    FILENAME "systemc-${SYSTEMC_VERSION}.zip"
     SHA512 f4df172addf816a1928d411dcab42c1679dc4c9d772f406c10d798a2c174d89cdac7a83947fa8beea1e3aff93da522d2d2daf61a4841ec456af7b7446c5c4a14
 )
-vcpkg_extract_source_archive(${ARCHIVE})
-
-set(VCPKG_LIBRARY_LINKAGE static)
-
-vcpkg_apply_patches(
-  SOURCE_PATH ${SOURCE_PATH}
-  PATCHES
-  ${CMAKE_CURRENT_LIST_DIR}/install.patch
-  ${CMAKE_CURRENT_LIST_DIR}/tlm_correct_dependency.patch
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${SYSTEMC_VERSION}
+    PATCHES
+        install.patch
+        tlm_correct_dependency.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    PREFER_NINJA
     OPTIONS
         -DCMAKE_CXX_STANDARD=17
         -DDISABLE_COPYRIGHT_MESSAGE=ON
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
 
 vcpkg_install_cmake()
