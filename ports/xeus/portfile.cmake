fca07467ce2bcb02c38794ba6a5ae7041863657f
myd7349 <myd7349@gmail.com>
2019-02-05 10:04:14 +0800

    [xeus] Update to 2019-02-04

--- ports/xeus/portfile.cmake
+++ ports/xeus/portfile.cmake
@@ -1,11 +1,15 @@
 include(vcpkg_common_functions)
 
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO QuantStack/xeus
-    REF 7541d476e054bf2f22f76a443e19a248e758ca48
-    SHA512 87d49f42067b07871f9365966b00d70f15b9597aa14b2b9fdd266f5755ad7303a307354434be37db0dfd62f3edd97726a3e1a33c9e7e16577c8ebad6468b7876
+    REF 24ad09ef6cfa251b842462e2eb84cb00987b1327
+    SHA512 59a3ec921f5ee8e5347630fb478fb06e20f61fa1bc3d1d833ba545c6d39e9b06de316bb6118c7b9bae2b337057f9d00b08da0b6aa5f37fd907e95ed90cb757c8
     HEAD_REF master
+    PATCHES
+        no-override-debug-flags.patch
 )
 
 vcpkg_configure_cmake(
@@ -21,12 +25,18 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-#vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
-#file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/debug/include
+    ${CURRENT_PACKAGES_DIR}/debug/share
+)
 
 # Handle copyright
-#configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+
+# Install usage
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 
 # CMake integration test
 #vcpkg_test_cmake(PACKAGE_NAME ${PORT})
