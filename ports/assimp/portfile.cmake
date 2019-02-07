140cc013a442b372f0d82ca2aae9ba64e73c349f
Lassi Lääti <lassilaati@gmail.com>
2019-02-07 08:16:51 +0200

    added remove debug/share

--- ports/assimp/portfile.cmake
+++ ports/assimp/portfile.cmake
@@ -22,17 +22,19 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS -DASSIMP_BUILD_TESTS=OFF
             -DASSIMP_BUILD_ASSIMP_VIEW=OFF
+            -DBUILD_FRAMEWORK=OFF
             -DASSIMP_BUILD_ZLIB=OFF
+            -DASSIMP_BUILD_SHARED_LIBS=OFF
             -DASSIMP_BUILD_ASSIMP_TOOLS=OFF
             -DASSIMP_INSTALL_PDB=OFF
             -DZLIB_INCLUDE_DIR=${CURRENT_INSTALLED_DIR}/include
             -DZLIB_FOUND=1
     OPTIONS_RELEASE
-        -DZLIB_LIBRARIES=${CURRENT_INSTALLED_DIR}/lib/zlib.lib
-        -DZLIB_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/zlib.lib
+            -DZLIB_LIBRARIES=${CURRENT_INSTALLED_DIR}/lib/zlib.lib
+            -DZLIB_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/zlib.lib
     OPTIONS_DEBUG
-        -DZLIB_LIBRARIES=${CURRENT_INSTALLED_DIR}/debug/lib/zlibd.lib
-        -DZLIB_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/zlibd.lib
+            -DZLIB_LIBRARIES=${CURRENT_INSTALLED_DIR}/debug/lib/zlibd.lib
+            -DZLIB_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/zlibd.lib
 )
 
 vcpkg_install_cmake()
@@ -43,6 +45,7 @@ vcpkg_copy_pdbs()
 
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(READ ${CURRENT_PACKAGES_DIR}/share/assimp/assimp-config.cmake ASSIMP_CONFIG)
 string(REPLACE "get_filename_component(ASSIMP_ROOT_DIR \"\${_PREFIX}\" PATH)"
