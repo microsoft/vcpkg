256cecc5abd41630e2a9f2aea7c55007a3d341a7
luncliff <luncliff@gmail.com>
2019-02-12 02:48:15 +0900

    [coroutine] x64 support
    * library linkage on windows
    * disabled x86
    * build on linux: ubuntu xenial

--- ports/coroutine/portfile.cmake
+++ ports/coroutine/portfile.cmake
@@ -3,10 +3,23 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO luncliff/coroutine
-    REF b65dc006892e11cd4b0ada543927514b6945cfe2
-    SHA512 b3e58b024232a2c47755a50db8dce02dbabca57a107e67bc09c0829a4e4ca9c7bd652045159afc1a410ba0989e905116c17b834dd7469f21aa8731a5bb635907
+    REF be75a5dd2b2b85233b7260bc6aabeb924d8ebeb8
+    SHA512 edd8f9384ba75f10038892e55f0937edfdb8987affbee04d8f5680306fc3f72c69db4f84f31142b18edb24bb057f0c984d0b90de0adb0ca0521b0fece6dae523
     HEAD_REF vcpkg
 )
+
+if(${VCPKG_TARGET_ARCHITECTURE} MATCHES x86)
+    message(FATAL_ERROR "This library doesn't support x86 arch. Please use x64 instead or contact maintainer")
+endif()
+
+# package: 'ms-gsl'
+message(STATUS "Using GSL with header path: ${CURRENT_INSTALLED_DIR}/include")
+
+set(DLL_LINKAGE false)
+if(${VCPKG_LIBRARY_LINKAGE} MATCHES dynamic)
+    message(STATUS "Using DLL linkage")
+    set(DLL_LINKAGE true)
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -15,6 +28,7 @@ vcpkg_configure_cmake(
         # package: 'ms-gsl'
         -DGSL_INCLUDE_DIR=${CURRENT_INSTALLED_DIR}/include
         -DTEST_DISABLED=True
+        -DBUILD_SHARED_LIBS=${DLL_LINKAGE}
 )
 
 vcpkg_install_cmake()
@@ -24,6 +38,21 @@ file(
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/coroutine
     RENAME      copyright
 )
+
+if(WIN32) # for win32, move dll files to bin
+    file(INSTALL        ${CURRENT_PACKAGES_DIR}/debug/lib/coroutine.dll
+         DESTINATION    ${CURRENT_PACKAGES_DIR}/debug/bin
+    )
+    file(REMOVE         ${CURRENT_PACKAGES_DIR}/debug/lib/coroutine.dll)
+
+    file(INSTALL        ${CURRENT_PACKAGES_DIR}/lib/coroutine.dll
+         DESTINATION    ${CURRENT_PACKAGES_DIR}/bin
+    )
+    file(REMOVE         ${CURRENT_PACKAGES_DIR}/lib/coroutine.dll)
+endif()
 # removed duplicates in debug
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# unset used variables
+unset(DLL_LINKAGE)
