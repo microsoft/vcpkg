7b1a1226f358da9e373c1c4357df4f57c64335ac
Victor Romero <viromer@microsoft.com>
2019-02-13 12:19:13 -0800

    [ompl] Disable extra components

--- ports/ompl/portfile.cmake
+++ ports/ompl/portfile.cmake
@@ -11,23 +11,39 @@
 #
 
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/ompl-1.4.1)
+
+set(OMPL_VERSION 1.4.1)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/ompl/ompl/archive/1.4.1.zip"
-    FILENAME "ompl-1.4.1.zip"
+    URLS "https://github.com/ompl/ompl/archive/${OMPL_VERSION}.zip"
+    FILENAME "ompl-${OMPL_VERSION}.zip"
     SHA512 2552476e86fdd4d61a5663d401c18f873424127d04d2342f1e0e5d4a15212b952c9de59f1a2483c2c71ee3cc3aadbe1b5912ae7fa262c218dd0b22daadfcd95c
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${OMPL_VERSION}
+    PATCHES 
+        "001-disable-extra-components.patch"
+)
+# For some reason, demos and tests are being installed 
+# even when disabling the correct OMPL options.
+# The "001-disable-extra-components.patch" avoids that issue.
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DOMPL_REGISTRATION=OFF
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA
+    OPTIONS 
+        -DOMPL_VERSIONED_INSTALL=OFF
+        -DOMPL_REGISTRATION=OFF
+        -DOMPL_BUILD_DEMOS=OFF
+        -DOMPL_BUILD_TESTS=OFF
+        -DOMPL_BUILD_PYBINDINGS=OFF
+        -DOMPL_BUILD_PYTESTS=OFF
 )
 
 vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/ompl/cmake)
 
 # Remove debug distribution
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
@@ -37,4 +53,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/ompl RENAME copyright)
 
 # Post-build test for cmake libraries
-# vcpkg_test_cmake(PACKAGE_NAME ompl)
+vcpkg_test_cmake(PACKAGE_NAME ompl)
