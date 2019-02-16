2f3058ef6bbc76e7b3b0b2fd5e1b0a748ae992dc
myd7349 <myd7349@gmail.com>
2019-02-16 17:36:47 +0800

    [xeus] Static building support

--- ports/xeus/portfile.cmake
+++ ports/xeus/portfile.cmake
@@ -1,13 +1,13 @@
 include(vcpkg_common_functions)
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO QuantStack/xeus
     REF f78c60c7ce28baecb2479f2b82e4e8d1a6c35188
     SHA512 9d83f32f641bcad4ac96e263c465d46bdfa7d18d41f1e201309244c95587ce08ff2426f7cdd3a4399563d46064ed9bedd4d0babf4840f65e95c6a2c6f23ac9bb
     HEAD_REF master
+    PATCHES
+        static-lib.patch
 )
 
 vcpkg_configure_cmake(
@@ -29,6 +29,13 @@ file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
     ${CURRENT_PACKAGES_DIR}/debug/share
 )
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/xeus/xeus.hpp
+        "#ifdef XEUS_STATIC_LIB"
+        "#if 1 // #ifdef XEUS_STATIC_LIB"
+    )
+endif()
 
 # Handle copyright
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
