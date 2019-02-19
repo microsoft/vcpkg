a3cbe96f93824aad21a96d330e893bfce5b0c2c8
Victor Romero <viromer@microsoft.com>
2019-02-19 10:00:38 -0800

    [tinyspline] Do not treat warnings as errors

--- ports/tinyspline/portfile.cmake
+++ ports/tinyspline/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     SHA512 50cf4927b311eeca6de7954f1b8d585cbf71355f5e5b0aac2f92f5f4ba37986df16eb3251f94a2304d27dab27d4f6b838b410f53e30de28bab53facf194eb640
     HEAD_REF master
     PATCHES
-         001-ignore-warnings-treated-as-errors.patch
+        "001-do-not-treat-warnings-as-errors.patch"
 )
 
 
