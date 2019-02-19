2aafb890b37481b0729d6d518aeabd7c1956932a
Cody Miller <codym@microsoft.com>
2019-02-19 12:16:51 -0800

    fix: update hash for lest

--- ports/lest/portfile.cmake
+++ ports/lest/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO martinmoene/lest
     REF v1.34.1
-    SHA512 e2e2764f4095ff95b9faab01c2cb4bd2b527d3e582eba10a699d8e44d194c90409eabbc842cf7627fd6f1f1eaa4c45cf314959e79aea858346fb844962009f92
+    SHA512 7f4b0e49c1cf4c55d21752259ee45f9265aba254b9c15f84e77f9ae3e5ef3443abcb43fafe8e16d84bbdffee72dae842de0ed661c2caeb9607fcb188eb3ec7d1
 )
 
 file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
