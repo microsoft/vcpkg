23393370757cd7f67c6fac5446b8d0c8eb984a06
Victor Romero <viromer@microsoft.com>
2019-02-19 10:55:02 -0800

    [smpeg2] Fix double* to int comparison

--- ports/smpeg2/portfile.cmake
+++ ports/smpeg2/portfile.cmake
@@ -3,9 +3,10 @@ include(vcpkg_common_functions)
 
 set(VERSION 2.0.0)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.libsdl.org/projects/smpeg/release/smpeg2-{VERSION}.tar.gz"
+    URLS "https://www.libsdl.org/projects/smpeg/release/smpeg2-${VERSION}.tar.gz"
     FILENAME "smpeg2-${VERSION}.tar.gz"
-    SHA512 80a779d01e7aa76778ef6ceea8041537db9e4b354df413214c4413c875cb98551891cef98fa0f057cc6a3222e4983da9ae65b86bdad2f87f9e2a6751837e2baf)
+    SHA512 80a779d01e7aa76778ef6ceea8041537db9e4b354df413214c4413c875cb98551891cef98fa0f057cc6a3222e4983da9ae65b86bdad2f87f9e2a6751837e2baf
+)
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -13,7 +14,9 @@ vcpkg_extract_source_archive_ex(
     REF ${VERSION}
     PATCHES 
         "001-correct-sdl-headers-dir.patch"
-        "002-use-SDL2-headers.patch")
+        "002-use-SDL2-headers.patch"
+        "003-fix-double-ptr-to-int-comparison.patch"
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
