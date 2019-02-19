bd4dfe4b99e67d3e2b147a59041148c6c469cc19
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2019-02-19 03:42:01 +0000

    Update libmodplug to build on linux

--- ports/libmodplug/portfile.cmake
+++ ports/libmodplug/portfile.cmake
@@ -1,11 +1,11 @@
 
-set(MODPLUG_HASH 5a39f5913d07ba3e61d8d5afdba00b70165da81d)
+set(MODPLUG_HASH c855db2a0938aaac4bd686a345e0d1b09564f181)
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libmodplug-${MODPLUG_HASH})
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/Konstanty/libmodplug/archive/${MODPLUG_HASH}.zip"
+    URLS "https://github.com/JackBister/libmodplug/archive/${MODPLUG_HASH}.zip"
     FILENAME "libmodplug-${MODPLUG_HASH}.zip"
-    SHA512 71b1314c44c98694c66ac17b638e997b99abc1ad61f7ac2e971000bdd4276d50d538259f4ee4dd39a3f672d28d3d322a32c83a9be0b1ffe5099ecc81273b5b55)
+    SHA512 68ceb84e891e076d673d1886169d0c926e833a56dc5371f8012a4e2a9aa2de44dabdddf2df021c5498f61a951e52f3a2b77f2c7f487b79ab9e3dac483609f4af)
 
 vcpkg_extract_source_archive(${ARCHIVE})
 vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH} PREFER_NINJA)
