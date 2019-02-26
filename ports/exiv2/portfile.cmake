9e0a46b0af5e3c79f45e1e25b3ff03d83afdf179
Sean Warren <s.warren@garvan.org.au>
2019-02-26 15:29:57 +1100

    [exiv2] Update exiv2 to 0.27 release, optionally support unicode on windows

--- ports/exiv2/portfile.cmake
+++ ports/exiv2/portfile.cmake
@@ -3,15 +3,22 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Exiv2/exiv2
-    REF 274ef04f7ebbff567135e732434c9c8353265d6c
-    SHA512 8e8d32a4c51da2a61026a4541885da72df88c3e30452944c5e2d2d8e6d8d03dcdea72c4e3bfe9673554453ddc547651d160cfba2bfab1450a1770f4af2be740a
+    REF 0.27
+    SHA512 ec605db73abcf3cc2df78c1fc3aae5335a51192f660668e39a4f20fc7f372b18c3cec9b704e1c71c356315fd75e791622de1dffe576432ee0fb12bf63a98a423
     HEAD_REF master
-    PATCHES iconv.patch
 )
+
+if(WIN32 AND ("unicode" IN_LIST FEATURES))
+    set(enable_win_unicode TRUE)
+elseif()
+    set(enable_win_unicode FALSE)
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DEXIV2_ENABLE_WIN_UNICODE:BOOL=${enable_win_unicode}
 )
 
 vcpkg_install_cmake()
