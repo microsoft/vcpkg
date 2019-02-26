9b88931fc8a762a849ca6d3955279f6bf182d430
Victor Romero <romerosanchezv@gmail.com>
2019-02-26 15:20:30 -0800

    [libmodplug] Patch instead of changing downloading source

--- ports/libmodplug/portfile.cmake
+++ ports/libmodplug/portfile.cmake
@@ -1,14 +1,30 @@
+set(MODPLUG_HASH 5a39f5913d07ba3e61d8d5afdba00b70165da81d)
 
-set(MODPLUG_HASH c855db2a0938aaac4bd686a345e0d1b09564f181)
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libmodplug-${MODPLUG_HASH})
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/JackBister/libmodplug/archive/${MODPLUG_HASH}.zip"
-    FILENAME "libmodplug-${MODPLUG_HASH}.zip"
-    SHA512 68ceb84e891e076d673d1886169d0c926e833a56dc5371f8012a4e2a9aa2de44dabdddf2df021c5498f61a951e52f3a2b77f2c7f487b79ab9e3dac483609f4af)
 
-vcpkg_extract_source_archive(${ARCHIVE})
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    vcpkg_from_github(ARCHIVE
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO JackBister/libmodplug
+        REF ${MODPLUG_HASH}
+        SHA512 c43bb3190b62c3a4e3636bba121b5593bbf8e6577ca9f2aa04d90b03730ea7fb590e640cdadeb565758b92e81187bc456e693fe37f1f4deace9b9f37556e3ba1
+        PATCHES
+            "001-automagically-define-modplug-static.patch"
+            "002-detect_sinf.patch"
+    )
+else()
+    vcpkg_from_github(ARCHIVE
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO JackBister/libmodplug
+        REF ${MODPLUG_HASH}
+        SHA512 c43bb3190b62c3a4e3636bba121b5593bbf8e6577ca9f2aa04d90b03730ea7fb590e640cdadeb565758b92e81187bc456e693fe37f1f4deace9b9f37556e3ba1
+        PATCHES
+            "002-detect_sinf.patch"
+    )
+endif()
+
 vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH} PREFER_NINJA)
+
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
@@ -19,11 +35,6 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
     file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/modplug.dll ${CURRENT_PACKAGES_DIR}/debug/bin/modplug.dll)
     vcpkg_copy_pdbs()
-else()
-    vcpkg_apply_patches(
-        SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-        PATCHES
-            ${CMAKE_CURRENT_LIST_DIR}/automagically-define-modplug-static.patch)
 endif()
 
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libmodplug)
