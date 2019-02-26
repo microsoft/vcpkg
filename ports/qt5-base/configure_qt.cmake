f55689173a839c041a5de402cf4e022fcc8bda35
Claudio Fantacci <claudio.fantacci@gmail.com>
2019-02-26 11:50:38 +0100

    Fix Qt5 hardcoded absolute paths within DLLs

--- ports/qt5-base/configure_qt.cmake
+++ ports/qt5-base/configure_qt.cmake
@@ -32,11 +32,11 @@ function(configure_qt)
             -debug
             -prefix ${CURRENT_INSTALLED_DIR}/debug
             -extprefix ${CURRENT_PACKAGES_DIR}/debug
-            -hostbindir ${CURRENT_PACKAGES_DIR}/debug/tools/qt5
-            -archdatadir ${CURRENT_PACKAGES_DIR}/share/qt5/debug
-            -datadir ${CURRENT_PACKAGES_DIR}/share/qt5/debug
-            -plugindir ${CURRENT_PACKAGES_DIR}/debug/plugins
-            -qmldir ${CURRENT_PACKAGES_DIR}/debug/qml
+            -hostbindir ${CURRENT_INSTALLED_DIR}/debug/tools/qt5
+            -archdatadir ${CURRENT_INSTALLED_DIR}/share/qt5/debug
+            -datadir ${CURRENT_INSTALLED_DIR}/share/qt5/debug
+            -plugindir ${CURRENT_INSTALLED_DIR}/debug/plugins
+            -qmldir ${CURRENT_INSTALLED_DIR}/debug/qml
             -headerdir ${CURRENT_PACKAGES_DIR}/include
             -I ${CURRENT_INSTALLED_DIR}/include
             -L ${CURRENT_INSTALLED_DIR}/debug/lib
@@ -53,11 +53,11 @@ function(configure_qt)
             -release
             -prefix ${CURRENT_INSTALLED_DIR}
             -extprefix ${CURRENT_PACKAGES_DIR}
-            -hostbindir ${CURRENT_PACKAGES_DIR}/tools/qt5
-            -archdatadir ${CURRENT_PACKAGES_DIR}/share/qt5
-            -datadir ${CURRENT_PACKAGES_DIR}/share/qt5
-            -plugindir ${CURRENT_PACKAGES_DIR}/plugins
-            -qmldir ${CURRENT_PACKAGES_DIR}/qml
+            -hostbindir ${CURRENT_INSTALLED_DIR}/tools/qt5
+            -archdatadir ${CURRENT_INSTALLED_DIR}/share/qt5
+            -datadir ${CURRENT_INSTALLED_DIR}/share/qt5
+            -plugindir ${CURRENT_INSTALLED_DIR}/plugins
+            -qmldir ${CURRENT_INSTALLED_DIR}/qml
             -I ${CURRENT_INSTALLED_DIR}/include
             -L ${CURRENT_INSTALLED_DIR}/lib
             -platform ${_csc_PLATFORM}
