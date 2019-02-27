a0d47c552e6407eb2ca0a0fcd61d0f40740b8bb9
Sean Warren <s.warren@garvan.org.au>
2019-02-28 09:52:57 +1100

    [libxml2] Link to iconv correctly on mac,linux

--- ports/libxml2/vcpkg-cmake-wrapper.cmake
+++ ports/libxml2/vcpkg-cmake-wrapper.cmake
@@ -1,13 +1,16 @@
 _find_package(${ARGS})
 if(LibXml2_FOUND)
     find_package(LibLZMA)
-    list(APPEND LIBXML2_LIBRARIES ${LIBLZMA_LIBRARIES})
+    find_package(unofficial-iconv REQUIRED)
+    set_property(TARGET LibXml2::LibXml2 APPEND PROPERTY INTERFACE_LINK_LIBRARIES 
+        unofficial::iconv::libcharset 
+        unofficial::iconv::libiconv
+        ${LIBLZMA_LIBRARIES})
+    list(APPEND LIBXML2_LIBRARIES
+        unofficial::iconv::libcharset 
+        unofficial::iconv::libiconv
+        ${LIBLZMA_LIBRARIES})
     if(CMAKE_SYSTEM_NAME STREQUAL "Windows" OR CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-        list(APPEND LIBXML2_LIBRARIES
-            debug ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/libiconv.lib
-            optimized ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/libiconv.lib
-            debug ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/libcharset.lib
-            optimized ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/libcharset.lib
-            ws2_32)
+        list(APPEND LIBXML2_LIBRARIES ws2_32)
     endif()
 endif()
