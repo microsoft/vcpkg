0802bebd545dae2bf0ad69b54a8007f2017d0ebb
Hans L <thehans@gmail.com>
2019-02-27 20:18:35 -0600

    Add patch for illegal character, fix line endings

--- ports/opencsg/portfile.cmake
+++ ports/opencsg/portfile.cmake
@@ -16,11 +16,11 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO thehans/OpenCSG
-    #REF "opencsg-1-4-2-release"
-    #SHA512 df117a1b7153a95332d236918d1547b0afe6f3ead46af2733c5feee6e25cec984b21affc41fd8320a45be9292bd3b32e21ed8bb3d08371ddd657f659b9bb932a
+    REPO floriankirsch/OpenCSG
+    REF "opencsg-1-4-2-release"
+    SHA512 df117a1b7153a95332d236918d1547b0afe6f3ead46af2733c5feee6e25cec984b21affc41fd8320a45be9292bd3b32e21ed8bb3d08371ddd657f659b9bb932a
     HEAD_REF master
-    #PATCHES illegal_char.patch
+    PATCHES illegal_char.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
