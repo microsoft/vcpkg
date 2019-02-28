a58126d7cfd12a8d7dafbad0db12946f5978882e
myd7349 <myd7349@gmail.com>
2019-02-28 17:38:12 +0800

    [nanopb] Copy pdb file

--- ports/nanopb/portfile.cmake
+++ ports/nanopb/portfile.cmake
@@ -24,6 +24,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_copy_pdbs()
+
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
