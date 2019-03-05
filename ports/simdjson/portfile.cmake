4327f4d82cc1e402794b145eb0d95f921277a71c
myd7349 <myd7349@gmail.com>
2019-03-05 08:23:35 +0800

    [simdjson] Update to 2019-03-05

--- ports/simdjson/portfile.cmake
+++ ports/simdjson/portfile.cmake
@@ -7,11 +7,9 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lemire/simdjson
-    REF 352dd5e7faf3000004c6ad5852c119ce3e679939
-    SHA512 29c578952d7aa117fe5808ceb2cb686895185d5f53bee3fff6636ac6fe6e50e1cc802499710eda4f233e3c5ff57ebf187ff6658fd5048a59cee8cfd8fca64c1d
+    REF 5040840578de7eb5c80c0250585d03b2f096a4ff
+    SHA512 be1d07b12a6ed2fdd61862cf8f049209467f1cfd40ad4a73d4da9a31147b83f1e60fc205b64bb311e15fa97432a42aad8a6e3eb5aa0dcc8a0c3afb47b55b6aac
     HEAD_REF master
-    PATCHES
-        fix-cmake.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SIMDJSON_BUILD_STATIC)
@@ -27,10 +25,7 @@ vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(
-    CONFIG_PATH lib/cmake/unofficial-${PORT}
-    TARGET_PATH share/unofficial-${PORT}
-)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
