08a15e5de37715a67f3e03dc57bd5deb52e19a02
JackBoosY <yuzaiyang@beyondsoft.com>
2019-03-05 09:40:53 +0000

    [usbmuxd]fix dependence port pthreads library name to "pthreadVC2".

--- ports/usbmuxd/portfile.cmake
+++ ports/usbmuxd/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 1.1.1.133
     SHA512 1a5f9abc239deeb15e2aab419ba9e88ef41ffa80396546fb65bc06b0f419cbabc80cdf95995caf71d5628d1537fb0329a73d923202e91ea43fcc7c32b840d047
     HEAD_REF master-msvc
+    PATCHES
+        fix-dependence-pthreads.patch
 )
 
 vcpkg_install_msbuild(
