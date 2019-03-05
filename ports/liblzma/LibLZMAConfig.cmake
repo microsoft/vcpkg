e84215a73c6692e414d1e49bc472901ac726f27d
Sean Warren <s.warren@garvan.org.au>
2019-03-05 11:15:21 +1100

    [liblzma] Use config file to correctly distinguish debug/release libraries

---
+++ ports/liblzma/LibLZMAConfig.cmake
@@ -0,0 +1,22 @@
+include("${CMAKE_CURRENT_LIST_DIR}/LibLZMATargets.cmake")
+
+set(LibLZMA_LIBRARIES LibLZMA::LibLZMA)
+get_filename_component(LIBLZMA_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../../include" ABSOLUTE)
+
+if(LIBLZMA_INCLUDE_DIR AND EXISTS "${LIBLZMA_INCLUDE_DIR}/lzma/version.h")
+    file(STRINGS "${LIBLZMA_INCLUDE_DIR}/lzma/version.h" LIBLZMA_HEADER_CONTENTS REGEX "#define LZMA_VERSION_[A-Z]+ [0-9]+")
+
+    string(REGEX REPLACE ".*#define LZMA_VERSION_MAJOR ([0-9]+).*" "\\1" LIBLZMA_VERSION_MAJOR "${LIBLZMA_HEADER_CONTENTS}")
+    string(REGEX REPLACE ".*#define LZMA_VERSION_MINOR ([0-9]+).*" "\\1" LIBLZMA_VERSION_MINOR "${LIBLZMA_HEADER_CONTENTS}")
+    string(REGEX REPLACE ".*#define LZMA_VERSION_PATCH ([0-9]+).*" "\\1" LIBLZMA_VERSION_PATCH "${LIBLZMA_HEADER_CONTENTS}")
+
+    set(LIBLZMA_VERSION_STRING "${LIBLZMA_VERSION_MAJOR}.${LIBLZMA_VERSION_MINOR}.${LIBLZMA_VERSION_PATCH}")
+    unset(LIBLZMA_HEADER_CONTENTS)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibLZMA  REQUIRED_VARS  LibLZMA_LIBRARIES
+                                                          LIBLZMA_INCLUDE_DIR
+                                           VERSION_VAR    LIBLZMA_VERSION_STRING
+                                 )
+mark_as_advanced( LIBLZMA_INCLUDE_DIR LibLZMA_LIBRARIES )
--- ports/liblzma/portfile.cmake
+++ ports/liblzma/portfile.cmake
@@ -32,7 +32,12 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
             ${CMAKE_CURRENT_LIST_DIR}/auto-define-lzma-api-static.patch)
 endif()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/LibLZMA TARGET_PATH share/LibLZMA)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/LibLZMAConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/LibLZMA)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/liblzma)
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/liblzma)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/liblzma/COPYING ${CURRENT_PACKAGES_DIR}/share/liblzma/copyright)
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/liblzma)
+#file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/liblzma)
