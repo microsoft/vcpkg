0d7f4c1b89a13b4548b1b9329019545661596e8c
myd7349 <myd7349@gmail.com>
2019-03-08 10:22:44 +0800

    [simdjson] Update to 2019-03-09 (fix UWP build)

--- ports/simdjson/portfile.cmake
+++ ports/simdjson/portfile.cmake
@@ -1,14 +1,17 @@
 include(vcpkg_common_functions)
 
-if(TARGET_TRIPLET MATCHES "^x86")
-    message(FATAL_ERROR "simdjson doesn't support x86 architecture.")
+# https://github.com/Microsoft/vcpkg/issues/5418#issuecomment-470519894
+if(TARGET_TRIPLET MATCHES "^(x86|arm-)")
+    message(FATAL_ERROR "simdjson doesn't support x86 or 32-bit ARM architecture.")
+elseif(TARGET_TRIPLET MATCHES "^arm64")
+    message(FATAL_ERROR "simdjson doesn't support ARM64 architecture currently.")
 endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lemire/simdjson
-    REF 5040840578de7eb5c80c0250585d03b2f096a4ff
-    SHA512 be1d07b12a6ed2fdd61862cf8f049209467f1cfd40ad4a73d4da9a31147b83f1e60fc205b64bb311e15fa97432a42aad8a6e3eb5aa0dcc8a0c3afb47b55b6aac
+    REF d2fa086198b77df44e7fa48b25200e118fa81eb0
+    SHA512 fe92b65e44502381d286b6a7c949055d185e56e7c244a5ab3086b2fe7da76ce81a966daa2d8459794ff0a911b426b1c77e1fc9ef0d616e20868621b1bb30cf67
     HEAD_REF master
 )
 
