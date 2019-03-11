4c2bc1f0743dc1645d828648522dfebda5e699ab
Thomas Arcila <thomas.arcila@gmail.com>
2019-03-11 04:32:25 -0400

    Fix tools/ and add support vcpkg_test_cmake()

--- ports/capnproto/portfile.cmake
+++ ports/capnproto/portfile.cmake
@@ -14,10 +14,6 @@ vcpkg_from_github(
     REF v0.7.0
     SHA512 a3ea278ded6a866759c0517d16b99bd38ffea1c163ce63a3604b752d8bdaafbc38a600de94afe12db35e7f7f06e29cc94c911dc2e0ecec6fe1185452df2a2bd3
     HEAD_REF master
-)
-
-vcpkg_apply_patches(
-	SOURCE_PATH ${SOURCE_PATH}
 	PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-fix-capnpc-extension-handling-on-Windows.patch"
 )
 
@@ -28,7 +24,7 @@ vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/CapnProto")
 
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools")
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools/CapnProto")
+file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools/capnproto")
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/capnproto)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
@@ -40,3 +36,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/capnproto)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/capnproto/LICENSE ${CURRENT_PACKAGES_DIR}/share/capnproto/copyright)
 
+vcpkg_test_cmake(PACKAGE_NAME CapnProto)
