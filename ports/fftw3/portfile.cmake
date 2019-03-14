68a540d84c9096a252239ad3e248b9873709a5d0
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2019-03-14 23:24:31 +0100

    fix find_package for fftw3 and add arch features (#4783)
    * fix find_package for fftw3
    
    * the patch took care of moving the files
    
    * Additional Patch to add Suffix to LibraryDepends
    
    * fixed minsizerel and relwithdebinfo for vs cmake configs
    
    * remove fix for minsizerel and relwithdeb info. Use global cmake mappings of these targets instead!
    
    * added feature packages. unfortunally avx and avx2 create an ICE with Visual Studio 2017 (maybe fixed with 2019?)
    
    * removed sse2 as default (forgot about arm...)

--- ports/fftw3/portfile.cmake
+++ ports/fftw3/portfile.cmake
@@ -21,6 +21,8 @@ vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES
         ${CMAKE_CURRENT_LIST_DIR}/omp_test.patch
+        ${CMAKE_CURRENT_LIST_DIR}/patch_targets.patch
+        ${CMAKE_CURRENT_LIST_DIR}/fftw3_arch_fix.patch
 )
 
 if ("openmp" IN_LIST FEATURES)
@@ -29,34 +31,88 @@ else()
     set(ENABLE_OPENMP OFF)
 endif()
 
+if ("avx" IN_LIST FEATURES)
+    set(HAVE_AVX ON)
+    set(HAVE_SSE ON)
+    set(HAVE_SSE2 ON)
+else()
+    set(HAVE_AVX OFF)
+endif()
+
+if ("avx2" IN_LIST FEATURES)
+    set(HAVE_AVX2 ON)
+    set(HAVE_FMA ON)
+    set(HAVE_SSE ON)
+    set(HAVE_SSE2 ON)
+else()
+    set(HAVE_AVX2 OFF)
+    set(HAVE_FMA OFF)
+endif()
+
+if ("sse" IN_LIST FEATURES)
+    set(HAVE_SSE ON)
+else()
+    set(HAVE_SSE OFF)
+endif()
+
+if ("sse2" IN_LIST FEATURES)
+    set(HAVE_SSE2 ON)
+    set(HAVE_SSE ON)
+else()
+    set(HAVE_SSE2 OFF)
+endif()
+
+if ("threads" IN_LIST FEATURES)
+    set(HAVE_THREADS ON)
+else()
+    set(HAVE_THREADS OFF)
+endif()
+
 foreach(PRECISION ENABLE_DEFAULT_PRECISION ENABLE_FLOAT ENABLE_LONG_DOUBLE)
-	vcpkg_configure_cmake(
-		SOURCE_PATH ${SOURCE_PATH}
-		PREFER_NINJA
-		OPTIONS 
-			-D${PRECISION}=ON
-			-DENABLE_OPENMP=${ENABLE_OPENMP}
-	)
+    if(${PRECISION} MATCHES "ENABLE_LONG_DOUBLE")
+        vcpkg_configure_cmake(
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS 
+            -D${PRECISION}=ON
+            -DENABLE_OPENMP=${ENABLE_OPENMP}
+            -DENABLE_THREADS=${HAVE_THREADS}
+        )
+    else()
+        vcpkg_configure_cmake(
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS 
+            -D${PRECISION}=ON
+            -DENABLE_OPENMP=${ENABLE_OPENMP}
+            -DHAVE_SSE=${HAVE_SSE}
+            -DHAVE_SSE2=${HAVE_SSE2}
+            -DHAVE_AVX=${HAVE_AVX}
+            -DHAVE_AVX2=${HAVE_AVX2}
+            -DHAVE_FMA=${HAVE_FMA}
+            -DENABLE_THREADS=${HAVE_THREADS}
+        )
+    endif()
 
-	vcpkg_install_cmake()
-	vcpkg_copy_pdbs()
+    vcpkg_install_cmake()
+    vcpkg_copy_pdbs()
 
-	file(COPY ${SOURCE_PATH}/api/fftw3.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+    file(COPY ${SOURCE_PATH}/api/fftw3.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
-	vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
 
-	if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
-		vcpkg_apply_patches(
-			   SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-			   PATCHES
-					   ${CMAKE_CURRENT_LIST_DIR}/fix-dynamic.patch)
-	endif()
+    if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
+        vcpkg_apply_patches(
+               SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
+               PATCHES
+                       ${CMAKE_CURRENT_LIST_DIR}/fix-dynamic.patch)
+    endif()
 
-	# Cleanup
-	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+    # Cleanup
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 endforeach()
-	
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/fftw3)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/fftw3/COPYING ${CURRENT_PACKAGES_DIR}/share/fftw3/copyright)
