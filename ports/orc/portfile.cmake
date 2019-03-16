40e81a640d1245fde124320c5f1eb6121b9715eb
Erwan BERNARD <bernard.erwan@gmail.com>
2019-03-16 18:04:45 +0100

    [orc] bump to 1.5.5

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apache/orc
-    REF bf5b7800930bfa030db83aba925d9d3b75852839
-    SHA512 42a1fd0fd2b8dec19a5019a42e3a85fe6d6cb2fe922d9bf7d47669a36801a9c65a1edccf01e08f0da8d01c79e329d8069d2b6200c4005d1bfe3628c598f24ee5
+    REF 47a490f083bd411bf04bfed8131eef42606d7789
+    SHA512 c2650d9fd367a5ec04c79c16434728e5c20608131f21bfc89d412cbaf8dd4ae5900b03e59df21d3617d8c6a9504e4b14b1f788157afa90b57d733499d2995e39
     HEAD_REF master
     PATCHES
       0003-dependencies-from-vcpkg.patch
