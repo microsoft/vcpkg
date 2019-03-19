083ae70b66945e58cf8f7e54753c240c8511e326
Cheney-Wang <v-xincwa@microsoft.com>
2019-03-18 23:54:41 -0700

    [rxqt] Remove SHA512 verification

--- ports/rxqt/portfile.cmake
+++ ports/rxqt/portfile.cmake
@@ -1,20 +1,22 @@
 #header-only library
+if(NOT VCPKG_USE_HEAD_VERSION)
+    message(FATAL_ERROR "rxqt does not have persistent releases. Please re-run the installation with --head.")
+else()
+    include(vcpkg_common_functions)
 
-include(vcpkg_common_functions)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO tetsurom/rxqt
+        SKIP_SHA512
+        HEAD_REF master
+    )
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO tetsurom/rxqt
-    REF master
-    SHA512 3f25570ab98c8d3959c35b0d99d5c35486339aa8cd752d8140bb064239265ecb8c5002e3a5e2b46324bb79b558cfe0f17e23a59ad51e6d3a0764acdce493b12c
-    HEAD_REF master
-)
+    file(INSTALL
+        ${SOURCE_PATH}/include
+        DESTINATION ${CURRENT_PACKAGES_DIR}
+    )
 
-file(INSTALL
-	${SOURCE_PATH}/include
-    DESTINATION ${CURRENT_PACKAGES_DIR}
-)
-
-file(INSTALL
-	${SOURCE_PATH}/LICENSE
-	DESTINATION ${CURRENT_PACKAGES_DIR}/share/rxqt RENAME copyright)
+    file(INSTALL
+        ${SOURCE_PATH}/LICENSE
+        DESTINATION ${CURRENT_PACKAGES_DIR}/share/rxqt RENAME copyright)
+endif()
