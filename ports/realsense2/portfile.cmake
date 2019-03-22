00e36598293a592911ede82520b9483cb2d6fa9a
Claudio Fantacci <claudio.fantacci@gmail.com>
2019-03-22 16:11:14 +0100

    [realsense2] Update to v2.19.0

--- ports/realsense2/portfile.cmake
+++ ports/realsense2/portfile.cmake
@@ -3,48 +3,53 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO IntelRealSense/librealsense
-    REF v2.16.1
-    SHA512 e030f7b1833db787b8976ead734535fb2209a19317d74d4f68bd8f8cae38abe2343d584e88131a1a66bf6f9f1c0a17bc2c64540841a74cf6300fecf3e69f9dff
+    REF v2.19.0
+    SHA512 c1fcb2b11827a5518b1e5196b7d1d2406447c6b2301809d3c66aaf69363a36f9789fd33595f581846809eba2c2540d3add964da03ecd96c804c7ca2b2df85180
     HEAD_REF development
+    PATCHES
+        libtm.patch
+        windowsconfig.patch
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_LIBRARY_LINKAGE)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" BUILD_CRT_LINKAGE)
 
 set(BUILD_EXAMPLES OFF)
 set(BUILD_GRAPHICAL_EXAMPLES OFF)
 if("tools" IN_LIST FEATURES)
-  set(BUILD_EXAMPLES ON)
-  set(BUILD_GRAPHICAL_EXAMPLES ON)
+    set(BUILD_EXAMPLES ON)
+    set(BUILD_GRAPHICAL_EXAMPLES ON)
 endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
-        #Ungrouped Entries
+        # Ungrouped Entries
         -DENFORCE_METADATA=ON
+        # BUILD
+        -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
+        -DBUILD_WITH_TM2=OFF
+    OPTIONS_RELEASE
         # BUILD
         -DBUILD_EXAMPLES=${BUILD_EXAMPLES}
         -DBUILD_GRAPHICAL_EXAMPLES=${BUILD_GRAPHICAL_EXAMPLES}
-        -DBUILD_SHARED_LIBS=${BUILD_LIBRARY_LINKAGE}
-        -DBUILD_UNIT_TESTS=OFF
-        -DBUILD_WITH_OPENMP=OFF
-        -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
     OPTIONS_DEBUG
         # BUILD
         -DBUILD_EXAMPLES=OFF
         -DBUILD_GRAPHICAL_EXAMPLES=OFF
         # CMAKE
-        -DCMAKE_PDB_OUTPUT_DIRECTORY=${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg
-        -DCMAKE_DEBUG_POSTFIX=_d
+        -DCMAKE_DEBUG_POSTFIX=d
 )
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/realsense2)
 vcpkg_copy_pdbs()
 
+file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/libtm_util.exe)
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/libtm_util.exe)
 
 if(BUILD_EXAMPLES)
     file(GLOB EXEFILES_RELEASE ${CURRENT_PACKAGES_DIR}/bin/rs-* ${CURRENT_PACKAGES_DIR}/bin/realsense-*)
@@ -52,10 +57,7 @@ if(BUILD_EXAMPLES)
         file(COPY ${EXEFILES_RELEASE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/realsense2)
         file(REMOVE ${EXEFILES_RELEASE})
     endif()
-    file(GLOB EXEFILES_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/*)
-    if (EXEFILES_DEBUG)
-        file(REMOVE ${EXEFILES_RELEASE} ${EXEFILES_DEBUG})
-    endif()
+
     vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/realsense2)
 
     file(GLOB BINS ${CURRENT_PACKAGES_DIR}/bin/*)
@@ -64,5 +66,5 @@ if(BUILD_EXAMPLES)
     endif()
 endif()
 
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/realsense2)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/realsense2/COPYING ${CURRENT_PACKAGES_DIR}/share/realsense2/copyright)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/realsense2)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/realsense2/LICENSE ${CURRENT_PACKAGES_DIR}/share/realsense2/copyright)
