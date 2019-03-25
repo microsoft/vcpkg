ebb82ee80c5f2fe841624be5d473ed9cc77a848c
JackBoosY <yuzaiyang@beyondsoft.com>
2019-03-25 02:43:51 +0000

    [libnice]Fix build error in windows.

--- ports/libnice/portfile.cmake
+++ ports/libnice/portfile.cmake
@@ -8,6 +8,11 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 c9bb81e8cd0b4e3673dba07ce08a16dd8821831339b44f1006510cdc09f9ae4c6eb7d43230711a2509867acb8d7df71821c411830dbf71c5a5d7e802f14a32c1
 )
 vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        ${CMAKE_CURRENT_LIST_DIR}/fix-build_in_windows.patch
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
