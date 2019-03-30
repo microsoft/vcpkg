eff755b7f0c0baf3d274523dc64b32a682b81b61
myd7349 <myd7349@gmail.com>
2019-03-30 16:33:14 +0800

    [brunocodutra-metal] Rename

--- ports/brunocodutra-metal/portfile.cmake
+++ ports/brunocodutra-metal/portfile.cmake
@@ -17,7 +17,10 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_cmake_targets(
+    CONFIG_PATH lib/cmake/Metal
+    TARGET_PATH share/metal
+)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
 
