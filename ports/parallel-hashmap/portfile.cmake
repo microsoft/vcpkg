81448a3035c506064c2bd2f26764505af02795c1
Gregory Popovitch <greg7mdp@gmail.com>
2019-04-01 14:01:59 -0400

    [parallel hashmap] increment release version (#5898)
    * update version of package

--- ports/parallel-hashmap/portfile.cmake
+++ ports/parallel-hashmap/portfile.cmake
@@ -4,8 +4,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO greg7mdp/parallel-hashmap
-    REF 1.0.0
-    SHA512 8e0e6da3f809b177fbeaa30916bd8fed8d4b896e8d8526d10e74169078050e91dc52c48b4ca29847fac669be5fcfb1216a706fe242a2330999273e10975d9439
+    REF 1.1.0
+    SHA512 ff9497d2a8009c9aa955f50e66269e5963a86d8593e3eb07ef968a8ea5e162fea7e145d6d4d9e7aa91380b49f22166d1a08445fa40d02f43327e4c39612f52d9
     HEAD_REF master
 )
 
@@ -22,4 +22,3 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/debug ${
 # Put the licence file where vcpkg expects it
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/parallel-hashmap)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/parallel-hashmap/LICENSE ${CURRENT_PACKAGES_DIR}/share/parallel-hashmap/copyright)
-
