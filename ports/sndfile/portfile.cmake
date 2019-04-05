a2dcd516dd392cb27f858290456a0496a84bb3d7
evpobr <evpobr@gmail.com>
2019-04-05 17:40:02 +0500

    [sndfile] Fix configuration on non-Win32 platforms

--- ports/sndfile/portfile.cmake
+++ ports/sndfile/portfile.cmake
@@ -33,7 +33,11 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
++if(WIN32)
+    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+else()
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/SndFile)
+endif()
 
 vcpkg_copy_pdbs()
 
