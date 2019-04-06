c44f21b13280233f2f9df61fc108f0a55d02c9ef
kiwixz <kiwixz@outlook.com>
2019-04-06 22:36:06 +0100

    [ebml] fix installation on linux

--- ports/ebml/portfile.cmake
+++ ports/ebml/portfile.cmake
@@ -22,7 +22,11 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+if (WIN32)
+    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+else ()
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
+endif ()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
