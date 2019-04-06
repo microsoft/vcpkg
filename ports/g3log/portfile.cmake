86ee4b4401051b67d1820497f018c6b10b6e83cc
myd7349 <myd7349@gmail.com>
2019-04-06 12:15:42 +0800

    [g3log] Use vcpkg_install_cmake

--- ports/g3log/portfile.cmake
+++ ports/g3log/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 9c5e7f3bcbe2c18984a7de7351da2451b23e45ed
     SHA512 3d989ec76e95a2a6863bd301bf00600b42d3fcf8ab2963385d79008bf3ec2621daa37638d17543736eaddf30d06979b24ae31ce0a37fe5eb842e14a15c1341b2
     HEAD_REF master
+    PATCHES
+        fix-cmake.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" G3_SHARED_LIB)
@@ -25,53 +27,18 @@ vcpkg_configure_cmake(
         -DVERSION=${VERSION}
 )
 
-vcpkg_build_cmake()
-
-set(DEBUG_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
-set(RELEASE_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-
-file(GLOB HEADER_FILES
-    ${SOURCE_PATH}/src/g3log/*.hpp
-    ${RELEASE_DIR}/include/g3log/generated_definitions.hpp
-)
-
-file(COPY ${HEADER_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include/g3log
-)
-
-macro(install_if_exists source_dir target_dir filename)
-    if (EXISTS ${source_dir}/${filename})
-        file(INSTALL
-            ${source_dir}/${filename}
-            DESTINATION ${target_dir}
-        )
-    endif ()
-endmacro()
-
-install_if_exists(${DEBUG_DIR} ${CURRENT_PACKAGES_DIR}/debug/lib g3logger.lib)
-install_if_exists(${RELEASE_DIR} ${CURRENT_PACKAGES_DIR}/lib g3logger.lib)
-
-install_if_exists(${DEBUG_DIR} ${CURRENT_PACKAGES_DIR}/debug/bin g3logger.dll)
-install_if_exists(${RELEASE_DIR} ${CURRENT_PACKAGES_DIR}/bin g3logger.dll)
-
-install_if_exists(${DEBUG_DIR} ${CURRENT_PACKAGES_DIR}/debug/lib libg3logger.a)
-install_if_exists(${RELEASE_DIR} ${CURRENT_PACKAGES_DIR}/lib libg3logger.a)
+vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-configure_file(
-    ${SOURCE_PATH}/cmake/g3loggerConfig.cmake
-    ${CURRENT_PACKAGES_DIR}/share/g3logger/g3loggerConfig.cmake
-    COPYONLY
-)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/g3logger TARGET_PATH share/g3logger)
 
 file(COPY
     ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake
     DESTINATION ${CURRENT_PACKAGES_DIR}/share/g3logger
 )
 
-# Install usage
-configure_file(${CMAKE_CURRENT_LIST_DIR}/usage ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage @ONLY)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
--- ports/g3log/vcpkg-cmake-wrapper.cmake
+++ ports/g3log/vcpkg-cmake-wrapper.cmake
@@ -1,18 +1,13 @@
 _find_package(${ARGS})
 
-find_package(Threads REQUIRED)
+if (G3LOG_FOUND)
+    find_package(Threads REQUIRED)
 
-set(G3LOG_LIBRARIES Threads::Threads)
+    list(APPEND G3LOG_LIBRARIES Threads::Threads)
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-    list(APPEND G3LOG_LIBRARIES
-        debug ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/g3logger.lib
-        optimized ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/g3logger.lib
-    )
-elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux" OR
-       CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    list(APPEND G3LOG_LIBRARIES
-        debug ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib/libg3logger.a
-        optimized ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/libg3logger.a
-    )
-endif()
+    if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+        list(APPEND G3LOG_LIBRARIES
+            DbgHelp.lib
+        )
+    endif()
+endif ()
