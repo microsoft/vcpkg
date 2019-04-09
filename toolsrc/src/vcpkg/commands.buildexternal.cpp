b39b0899cb403aa1f2e86f9e111d16d6f1328737
Robert Schumacher <roschuma@microsoft.com>
2019-04-08 23:26:18 -0700

    [vcpkg] Synchronize vcpkg-base with external repo (#5934)

--- toolsrc/src/vcpkg/commands.buildexternal.cpp
+++ toolsrc/src/vcpkg/commands.buildexternal.cpp
@@ -20,7 +20,7 @@ namespace vcpkg::Commands::BuildExternal
         const ParsedArguments options = args.parse_arguments(COMMAND_STRUCTURE);
 
         const FullPackageSpec spec = Input::check_and_get_full_package_spec(
-            args.command_arguments.at(0), default_triplet, COMMAND_STRUCTURE.example_text);
+            std::string(args.command_arguments.at(0)), default_triplet, COMMAND_STRUCTURE.example_text);
         Input::check_triplet(spec.package_spec.triplet(), paths);
 
         const fs::path port_dir = args.command_arguments.at(1);
