b888180381deaa00d979214d8bf1661fa33e4e9a
Sean Warren <s.warren@garvan.org.au>
2019-04-11 13:08:56 +1000

    Update build script

--- build-flimfit-env.py
+++ build-flimfit-env.py
@@ -18,7 +18,7 @@ else:
    exec_ext = ""
 
 # Bootstrap vcpkg
-subprocess.run([vcpkg_dir + "bootstrap-vcpkg" + script_ext], check=True)
+subprocess.run([os.path.join(vcpkg_dir, "bootstrap-vcpkg" + script_ext)], check=True)
 
 # Read configuration
 json_file = open(os.path.join(vcpkg_dir, 'flimfit-env-config.json'))
@@ -29,7 +29,7 @@ triplet_ext = ":" + config["default_triplet"][system]
 ports = [port + triplet_ext for port in config["ports"]]
 
 # Build ports
-subprocess.run([vcpkg_dir + "vcpkg" + exec_ext, "install", "--clean-after-build"] + ports, 
+subprocess.run([os.path.join(vcpkg_dir, "vcpkg" + exec_ext), "install", "--clean-after-build"] + ports, 
    cwd=vcpkg_dir, check=True)
 
 shutil.rmtree(os.path.join(vcpkg_dir, "downloads"))
