c95512aaf6cb590a5fd809075d012e617ad33525
Robert Schumacher <roschuma@microsoft.com>
2019-04-15 22:30:11 -0700

    [many ports] Updates 2019.03.29 (#5698)
    * Updates 2019.03.29
    
    * Undo changes to 4 ports(rocksdb tbb thrift uwebsockets) due to failures
    
    * [folly] Fix target files in debug mode
    
    * Undo glog changes since it caused sophus openmvg and cartographer failures
    
    * Undo Folly changes since it cause wangle failed
    
    * Undo Folly portfile

--- ports/catch2/portfile.cmake
+++ ports/catch2/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO catchorg/Catch2
-    REF v2.6.0
-    SHA512 8d693cce413421ca747a0a3864d72c20f30fb8e432eb1f13e69605a71cc4e536d6710561f989cce6783d28f8b667b8da42c624056c4d412852885a8cf0df1e5d
+    REF v2.7.0
+    SHA512 ab6c816642a92418c76c8ddabf17eb0c19bef1dc6ac2405c820280d670ef1d78c9ae89909f5a36bf063f0939d253b7c6b5e845c1ac4e63b338927e125dee950d
     HEAD_REF master
 )
 
