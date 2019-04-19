488a7fbba3e1da305b922391740984a02a469683
Robert Schumacher <roschuma@microsoft.com>
2019-04-19 16:47:42 -0700

    [vcpkg-bootstrap] Avoid using bash extensions and keep ./bootstrap-vcpkg.sh as a simple forwarder.

--- bootstrap-vcpkg.sh
+++ bootstrap-vcpkg.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -e
 
 vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
-unixName=$(uname -s)
-if [[ $unixName == MINGW*_NT* ]]; then
-  vcpkgRootDir=$(cygpath -aw "$vcpkgRootDir")
-  cmd "/C $vcpkgRootDir\\bootstrap-vcpkg.bat"
-else
-  . "$vcpkgRootDir/scripts/bootstrap.sh"
-fi
+. "$vcpkgRootDir/scripts/bootstrap.sh"
