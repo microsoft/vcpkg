41623d8e4e5d0d8618045f8d67e79bb620696649
Cheney Wang <38240633+Cheney-W@users.noreply.github.com>
2019-04-26 03:41:33 +0800

    [devicenameresolver] Add missing <string> (#6207)

--- ports/devicenameresolver/portfile.cmake
+++ ports/devicenameresolver/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_bitbucket(
     REF 0850d88fa6a759d79b3c859933870d9aa602aa79
     SHA512 9161411d3c8c17f49f5ff9482a007a6608872c948ef856aa7076a45c246e8d777e4cd6b54169d9c1b9e99e7b383436e1a084e168fafff1ca5f2b28260bac1452
     HEAD_REF master
+    PATCHES add-string-headfile.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
