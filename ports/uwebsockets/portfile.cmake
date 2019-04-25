69605fcb698d1f61138835439c3f00cc05bf8339
Phoebe <v-phma@microsoft.com>
2019-04-24 19:12:34 -0700

    Support Usockets

--- ports/uwebsockets/portfile.cmake
+++ ports/uwebsockets/portfile.cmake
@@ -2,7 +2,7 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO uWebSockets/uWebSockets
+    REPO uNetworking/uWebSockets
     REF v0.15.6
     SHA512 ba5dc18412ecceadb48e3c0f9b6f6d9ea920b76c36b12456bc96198346149010257c0f7807a1e1cc262ae0eca07e1994d3f1e3be0b3c815ce455d778c5375311
     HEAD_REF master
