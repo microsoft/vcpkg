229a23486bf9ce231adf8ac4010d397472b9657e
Victor Romero <viromer@microsoft.com>
2019-04-25 20:11:41 -0700

    [live555] Update to 2019.04.24

--- ports/live555/portfile.cmake
+++ ports/live555/portfile.cmake
@@ -3,14 +3,14 @@ include(vcpkg_common_functions)
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY) 
 
 # The current Live555 version from http://www.live555.com/liveMedia/public/live.2019.03.06
-set(LIVE_VERSION 2019.03.06)
+set(LIVE_VERSION 2019.04.24)
 
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION}/live)
 vcpkg_download_distfile(ARCHIVE
 	URLS "http://www.live555.com/liveMedia/public/live.${LIVE_VERSION}.tar.gz"
 	FILENAME "live555-${LIVE_VERSION}.tar.gz"
-	SHA512 cf3cbf57ec43d392fa82f06bd02f6d829208c9a9ec1c505d9eb6c5e2dd3393bbd8829b6216163deb8ea8356c180f30f610a639044a6941df5c9a92f29d4f1a75
+	SHA512 b92b8b515f8d6a6e5e92bc074aa77278668b4aa9d00d6f3cdc14fdfdc3f3a55ef0f9464b18532eed47ed5eabb604e8b03ccffb2502a7047b3b47969325d21ead
 )
 
 vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/src/${LIVE_VERSION})
