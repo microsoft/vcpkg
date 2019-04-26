d4aab996ded901f10c954e1b7505698bbfdcc2ba
Qing-Dong Wang <ioadong@126.com>
2019-04-26 14:37:20 +0800

    [freeglut] fix download URLs (#6035)

--- ports/freeglut/portfile.cmake
+++ ports/freeglut/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/freeglut-3.0.0)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://downloads.sourceforge.net/project/freeglut/freeglut/3.0.0/freeglut-3.0.0.tar.gz"
+    URLS "https://sourceforge.net/projects/freeglut/files/freeglut/3.0.0/freeglut-3.0.0.tar.gz/download"
     FILENAME "freeglut-3.0.0.tar.gz"
     SHA512 9c45d5b203b26a7ff92331b3e080a48e806c92fbbe7c65d9262dd18c39cd6efdad8a795a80f499a2d23df84b4909dbd7c1bab20d7dd3555d3d88782ce9dd15b0
 )
