35009e4966e4b23b6a1a60a2d238309f0e36ae11
myd7349 <myd7349@gmail.com>
2019-05-01 17:43:48 -0500

    [libsodium] Migrate build system to CMake (#5600)

--- ports/libsodium/portfile.cmake
+++ ports/libsodium/portfile.cmake
@@ -6,73 +6,52 @@ vcpkg_from_github(
     REF 1.0.17
     SHA512 faf6ab57d113b6b1614b51390823a646f059018327b6f493e9e918a908652d0932a75a1a6683032b7a3869f516f387d67acdf944568387feddff7b2f5b6e77d6
     HEAD_REF master
-    PATCHES
-        "disable-tests.patch"
 )
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(LIBSODIUM_RELEASE_CONFIGURATION ReleaseDLL)
-    set(LIBSODIUM_DEBUG_CONFIGURATION DebugDLL)
-else()
-    set(LIBSODIUM_RELEASE_CONFIGURATION Release)
-    set(LIBSODIUM_DEBUG_CONFIGURATION Debug)
-endif()
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
+    ${SOURCE_PATH}/CMakeLists.txt
+    COPYONLY
+)
 
-IF(VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
-    SET(BUILD_ARCH "Win32")
-ELSE()
-    SET(BUILD_ARCH ${VCPKG_TARGET_ARCHITECTURE})
-ENDIF()
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/sodiumConfig.cmake.in
+    ${SOURCE_PATH}/sodiumConfig.cmake.in
+    COPYONLY
+)
 
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/libsodium.vcxproj
-    RELEASE_CONFIGURATION ${LIBSODIUM_RELEASE_CONFIGURATION}
-    DEBUG_CONFIGURATION ${LIBSODIUM_DEBUG_CONFIGURATION}
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
-        /p:ForceImportBeforeCppTargets=${SOURCE_PATH}/builds/msvc/properties/${BUILD_ARCH}.props
+        -DBUILD_TESTING=OFF
 )
 
-file(INSTALL
-    ${SOURCE_PATH}/src/libsodium/include/sodium.h
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-)
-
-file(GLOB LIBSODIUM_HEADERS "${SOURCE_PATH}/src/libsodium/include/sodium/*.h")
-file(INSTALL
-    ${LIBSODIUM_HEADERS}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include/sodium
-)
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(READ ${CURRENT_PACKAGES_DIR}/include/sodium/export.h _contents)
-    string(REPLACE "#ifdef SODIUM_STATIC" "#if 1 //#ifdef SODIUM_STATIC" _contents "${_contents}")
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/sodium/export.h "${_contents}")
-endif ()
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    file(INSTALL
-        ${SOURCE_PATH}/Build/${LIBSODIUM_RELEASE_CONFIGURATION}/${BUILD_ARCH}/libsodium.dll
-        DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-    )
-    file(INSTALL
-    ${SOURCE_PATH}/Build/${LIBSODIUM_DEBUG_CONFIGURATION}/${BUILD_ARCH}/libsodium.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-    )
-endif()
-
-file(INSTALL
-    ${SOURCE_PATH}/Build/${LIBSODIUM_RELEASE_CONFIGURATION}/${BUILD_ARCH}/libsodium.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(INSTALL
-    ${SOURCE_PATH}/Build/${LIBSODIUM_DEBUG_CONFIGURATION}/${BUILD_ARCH}/libsodium.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
+vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-file(INSTALL
-    ${SOURCE_PATH}/LICENSE
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/libsodium
-    RENAME copyright
+vcpkg_fixup_cmake_targets(
+    CONFIG_PATH lib/cmake/unofficial-sodium
+    TARGET_PATH share/unofficial-sodium
 )
+
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/debug/include
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string(
+        ${CURRENT_PACKAGES_DIR}/include/sodium/export.h
+        "#ifdef SODIUM_STATIC"
+        "#if 1 //#ifdef SODIUM_STATIC"
+    )
+endif ()
+
+configure_file(
+    ${SOURCE_PATH}/LICENSE
+    ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright
+    COPYONLY
+)
+
+#vcpkg_test_cmake(PACKAGE_NAME unofficial-sodium)
