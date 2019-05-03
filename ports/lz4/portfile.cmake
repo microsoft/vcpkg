1df74189f3d209cec5682af99b32c3f5158914fc
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-05-03 12:14:14 +0200

    [lz4, flann] port patches from #5169

--- ports/lz4/portfile.cmake
+++ ports/lz4/portfile.cmake
@@ -2,9 +2,10 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lz4/lz4
-    REF v1.9.0
-    SHA512 f9e78df262818192800157d6ed64d42c06e918062afc93e3098d00f5f49fd3279b5709486a7d8841708a4ce1c539381225f0813e6a342f49d13b576eb61eb444
-    HEAD_REF dev)
+    REF v1.8.3
+    SHA512 5d284f75a0c4ad11ebc4abb4394d98c863436da0718d62f648ef2e2cda8e5adf47617a4b43594375f7b0b673541a9ccfaf73880a55fd240986594558214dbf9f
+    HEAD_REF dev
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
@@ -12,21 +13,33 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS_DEBUG
-        -DLZ4_SKIP_INCLUDES=ON
-        -DCMAKE_DEBUG_POSTFIX=d)
+        -DCMAKE_DEBUG_POSTFIX=d
+)
 
 vcpkg_install_cmake()
-vcpkg_copy_pdbs()
 
-foreach(FILE lz4.h lz4frame.h)
-    file(READ ${CURRENT_PACKAGES_DIR}/include/${FILE} LZ4_HEADER)
+file(GLOB_RECURSE LZ4_HEADERS "${SOURCE_PATH}/*.h")
+
+foreach(FILE ${LZ4_HEADERS})
+    file(READ ${FILE} LZ4_HEADER)
     if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
         string(REPLACE "defined(LZ4_DLL_IMPORT) && (LZ4_DLL_IMPORT==1)" "1" LZ4_HEADER "${LZ4_HEADER}")
     else()
         string(REPLACE "defined(LZ4_DLL_IMPORT) && (LZ4_DLL_IMPORT==1)" "0" LZ4_HEADER "${LZ4_HEADER}")
     endif()
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/${FILE} "${LZ4_HEADER}")
+    file(WRITE ${FILE} "${LZ4_HEADER}")
 endforeach()
+
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/share/lz4/lz4Config-debug.cmake ${CURRENT_PACKAGES_DIR}/share/lz4/lz4Config-debug.cmake)
+file(READ ${CURRENT_PACKAGES_DIR}/share/lz4/lz4Config-debug.cmake LZ4_TARGET_DEBUG)
+string(REPLACE "/lib/"
+               "/debug/lib/" LZ4_TARGET_DEBUG "${LZ4_TARGET_DEBUG}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/lz4/lz4Config-debug.cmake "${LZ4_TARGET_DEBUG}")
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+vcpkg_copy_pdbs()
 
 file(COPY ${SOURCE_PATH}/lib/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/lz4)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/lz4/LICENSE ${CURRENT_PACKAGES_DIR}/share/lz4/copyright)
