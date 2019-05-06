5bdc101dd18638bbc44ea8fa81a0ef481ee6e2d3
myd7349 <myd7349@gmail.com>
2019-05-06 18:19:34 +0800

    [xeus] Update usage

--- ports/xeus/portfile.cmake
+++ ports/xeus/portfile.cmake
@@ -27,6 +27,11 @@ vcpkg_copy_pdbs()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
+file(COPY
+    ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
     ${CURRENT_PACKAGES_DIR}/debug/share
