1ad88f402ebfffb998da552f6215fd33419dea82
myd7349 <myd7349@gmail.com>
2019-05-14 19:55:08 +0800

    [json-c] Add new port

---
+++ ports/json-c/portfile.cmake
@@ -0,0 +1,23 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO json-c/json-c
+    REF 3e81b4abe359c8128bb2b4127f4e8c8c057fb004
+    SHA512 a2cd6d71d72d0dcacf2056466f3f414df180aacc9c2ee93b85f047683a88671590089535d7cecf71ef1bf0844b5ab35535e64022854d2fbc7f82d889aefcd730
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+# Handle copyright
+configure_file(${SOURCE_PATH}/COPYING ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
