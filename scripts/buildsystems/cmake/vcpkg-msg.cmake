1d08e8dc741dbbe50f7db2f86dd994800fa803d9
Alexander Neumann <alexander.neumann@hamburg.de>
2019-05-16 14:39:32 +0200

    fixing syntax errors

--- scripts/buildsystems/cmake/vcpkg-msg.cmake
+++ scripts/buildsystems/cmake/vcpkg-msg.cmake
@@ -1,7 +1,7 @@
 option(VCPKG_VERBOSE "Enables messages from the VCPKG toolchain for debugging purposes." ON)
 mark_as_advanced(VCPKG_VERBOSE)
 
-function(vcpkg-msg _mode _function _message)
+function(vcpkg_msg _mode _function _message)
     cmake_parse_arguments(PARSE_ARGV 3 vcpkg-msg "ALWAYS" "" "")
     if(VCPKG_VERBOSE OR vcpkg-msg_ALWAYS)
         message(${_mode} "VCPKG-${_function}: ${_message}")
