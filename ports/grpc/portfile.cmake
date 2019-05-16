395e0570b0bce1fb7db656d199aca88217b6eea1
Darrin W. Cullop <v-dacull@microsoft.com>
2019-05-16 16:54:34 -0700

    [grpc] #6485 - Fix ARM/ARM64 on Windows Build
    This PR includes the following changes that are required to get gRPC to build for ARM/ARM64 on Windows
    1) Updated to v1.20.1
    2) Disables Codegen Build for ARM/ARM64 builds
    3) Rebased the "Fix UWP" patch so that it will apply properly
    4) Fixed the "Static Linking in Linux" patch because it specified an invalid option on Windows builds
    5) Added patch to "#undef" a macro name collision
    6) Added GDI32.lib to the list of standard libraries linked to Windows

--- ports/grpc/portfile.cmake
+++ ports/grpc/portfile.cmake
@@ -11,15 +11,17 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO grpc/grpc
-    REF v1.19.1
-    SHA512 4bb127d946fc16887fd4cf75215f0bc9f6d17dbd36fc4f1b191a64914f96c49dddb41f1b6c72fd24ea0a40f242b4398248f32fcb1fe9a764367be1c2edda9142
+    REF v1.20.1
+    SHA512 e0dd0318d2b4ec07e0eafffa218938d91b1440c5053a557460ea7fceaab3d76f0cccc1d595abe7de9fa79f068b71cfbc5a28a3b688bc9c1e2737086928149583
     HEAD_REF master
     PATCHES 
         00001-fix-uwp.patch
         00002-static-linking-in-linux.patch
+        00003-undef-base64-macro.patch
+        00004-link-gdi32-on-windows.patch
 )
 
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
     set(gRPC_BUILD_CODEGEN OFF)
 else()
     set(gRPC_BUILD_CODEGEN ON)
