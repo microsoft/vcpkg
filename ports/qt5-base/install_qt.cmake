d75c3eb4504c65534e98acd42a23acfb8e74b3c7
Andreas Mack <andreas.mack@konsec.com>
2019-05-17 14:30:35 +0000

    Rename flex and bison so qt angle finds them

--- ports/qt5-base/install_qt.cmake
+++ ports/qt5-base/install_qt.cmake
@@ -25,7 +25,25 @@ function(install_qt)
     endif()
     vcpkg_find_acquire_program(PYTHON3)
     get_filename_component(PYTHON3_EXE_PATH ${PYTHON3} DIRECTORY)
+    vcpkg_find_acquire_program(FLEX)
+    get_filename_component(FLEX_EXE_PATH ${FLEX} DIRECTORY)
+    #    vcpkg_find_acquire_program(BISON)
+    #get_filename_component(BISON_EXE_PATH ${BISON} DIRECTORY)
+
+    file(COPY ${FLEX} DESTINATION "${FLEX_EXE_PATH}/bin" )
+    file(COPY "${FLEX_EXE_PATH}/win_bison.exe" DESTINATION "${FLEX_EXE_PATH}/bin" )
+    file(RENAME "${FLEX_EXE_PATH}/bin/win_bison.exe" "${FLEX_EXE_PATH}/bin/bison.exe")
+    file(RENAME "${FLEX_EXE_PATH}/bin/win_flex.exe" "${FLEX_EXE_PATH}/bin/flex.exe")
+    file(COPY "${FLEX_EXE_PATH}/bin/flex.exe" DESTINATION "${FLEX_EXE_PATH}" )
+    file(COPY "${FLEX_EXE_PATH}/bin/bison.exe" DESTINATION "${FLEX_EXE_PATH}" )
+    vcpkg_add_to_path("${FLEX_EXE_PATH}")
+
     vcpkg_add_to_path(PREPEND "${PYTHON3_EXE_PATH}")
+
+
+    message(STATUS "PATH: $ENV{PATH}")
+
+
     set(_path "$ENV{PATH}")
 
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
