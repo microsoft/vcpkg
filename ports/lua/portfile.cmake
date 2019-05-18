3754e5389702c6b14d7de2919df13c245f3c1bbc
TitanSnow <sweeto@live.cn>
2019-05-18 10:50:57 +0800

    don't use c89 on windows
    vs2015 already supports c99

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -13,7 +13,11 @@ vcpkg_download_distfile(ARCHIVE
     FILENAME "lua-5.3.5.tar.gz"
     SHA512 4f9516acc4659dfd0a9e911bfa00c0788f0ad9348e5724fe8fb17aac59e9c0060a64378f82be86f8534e49c6c013e7488ad17321bafcc787831d3d67406bd0f4
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH ${SOURCE_PATH}
+    ARCHIVE ${ARCHIVE}
+    PATCHES vs2015-impl-c99.patch
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
