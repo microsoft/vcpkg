3579b5b2f0a6363e6dea7a0feaef9823f03834c9
Robert Schumacher <roschuma@microsoft.com>
2019-05-17 17:27:35 -0700

    [azure-iot-sdk-c] Bump CONTROL versions and normalize tabs/spaces

--- ports/azure-macro-utils-c/portfile.cmake
+++ ports/azure-macro-utils-c/portfile.cmake
@@ -3,18 +3,18 @@ include(vcpkg_common_functions)
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO Azure/azure-macro-utils-c
-	REF 38729b4b7ac3ea8b7d71e394782b861ecb25193e
-	SHA512 c9c820e74aee403d45f257359318d3435e5d6534afe821da5679bc462e26ad256dd01ed253a80ba1c58343f850ef1026280533a152c4b0465527f6537b3092d3
-	HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-macro-utils-c
+    REF 38729b4b7ac3ea8b7d71e394782b861ecb25193e
+    SHA512 c9c820e74aee403d45f257359318d3435e5d6534afe821da5679bc462e26ad256dd01ed253a80ba1c58343f850ef1026280533a152c4b0465527f6537b3092d3
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-		-Drun_int_tests=OFF
+        -Drun_int_tests=OFF
 )
 
 vcpkg_install_cmake()
--- ports/umock-c/portfile.cmake
+++ ports/umock-c/portfile.cmake
@@ -3,11 +3,11 @@ include(vcpkg_common_functions)
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO Azure/umock-c
-	REF 92772d9d8317a37dd0b656e95877ffb03bc67e92
-	SHA512 4dd738c7b2c7e1237ad874a7ad90bf81b864aa242af335dcc82d0cfea51bc33fe84de4eebedb6e00944c70d01d1ade4827716dbcf95754165b35981bde4147e7
-	HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/umock-c
+    REF 92772d9d8317a37dd0b656e95877ffb03bc67e92
+    SHA512 4dd738c7b2c7e1237ad874a7ad90bf81b864aa242af335dcc82d0cfea51bc33fe84de4eebedb6e00944c70d01d1ade4827716dbcf95754165b35981bde4147e7
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
@@ -15,7 +15,7 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         -Drun_unittests=OFF
-		-Drun_int_tests=OFF
+        -Drun_int_tests=OFF
         -Duse_installed_dependencies=ON
 )
 
