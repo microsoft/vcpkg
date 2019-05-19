36c18d8da8d2cbdaf492078aea29bd256be8788b
evpobr <evpobr@gmail.com>
2019-05-19 21:15:40 +0500

    [ebml, matroska] Updates
    * ebml: use standard CMake project
    * ebml: no patches needed (merged in upstream)
    * matroska: update to 1.5.0
    * matroska: use standard CMake project
    
    Breaking changes from upstream:
    
    * ebml target is now in EBML:: namespace
    * matroska target is now in Matroska:: namespace

--- ports/ebml/portfile.cmake
+++ ports/ebml/portfile.cmake
@@ -7,10 +7,9 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Matroska-Org/libebml
-    REF release-1.3.7
-    SHA512 754dee128db2eb6f0ba09962312ddda79f3178238464dd6161cce50bd08fd4193490a48bb537c4e2a388dc339951909a8857617cb30500d76d5689da4f855b47
+    REF 366fe3c34639aa68caa6af6d8ca78a62337ffd75
+    SHA512 6025bcdee1adfb32c86c286448c70eb4f08bd7bbbafbbb12a2b6e1e751f42dc360105c040ccf892b19c5a6f9660bd3456b3bdee9e2dc48cb389007dd860fa4ea
     HEAD_REF master
-    PATCHES export-endofstreamx.patch
 )
 
 vcpkg_configure_cmake(
@@ -21,11 +20,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-if (WIN32)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
-else ()
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/ebml)
-endif ()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/ebml)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
