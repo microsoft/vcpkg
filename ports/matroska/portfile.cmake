7762a9d1b98e2f8b029888a9dfed6dc18a379112
Victor Romero <romerosanchezv@gmail.com>
2019-05-22 16:06:50 -0700

    [matroska] Bump CONTROL version

--- ports/matroska/portfile.cmake
+++ ports/matroska/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "${PORT} does not currently support UWP")
+    message(FATAL_ERROR "matroska does not currently support UWP")
 endif()
 
 vcpkg_from_github(
