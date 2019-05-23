5a38b62ad376da277cf0a0300bc4e961427b41cb
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-05-22 18:39:17 -0600

    [gdk-pixbuf,gettext,glib,libcroco,libiconv] Fix for gdk-pixbuf Linux build
    This commit includes changes and updates to properly build
    dependencies for gdk-pixbuf on Linux.
    
    Of particular note is the generation of actual archive files
    for both gettext and libiconv.
    
    This is preliminary work to introduce a librsvg port.

--- ports/glib/cmake/unofficial-glib-config.in.cmake
+++ ports/glib/cmake/unofficial-glib-config.in.cmake
@@ -1,7 +1,8 @@
 if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
     include(CMakeFindDependencyMacro)
     find_dependency(Threads)
-    find_dependency(unofficial-iconv)
+    find_dependency(unofficial-iconv CONFIG)
+    find_dependency(unofficial-gettext CONFIG)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/unofficial-glib-targets.cmake")
