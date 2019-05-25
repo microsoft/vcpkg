2f967f476fdb9c4c80ec4fb4a1ed6026c4364b95
driver1998 <driver1998@foxmail.com>
2019-05-25 18:40:18 +0800

    ffmpeg: remove outdated patch

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -15,7 +15,6 @@ vcpkg_extract_source_archive_ex(
         configure_opencv.patch
         fix_windowsinclude-in-ffmpegexe-1.patch
         fix_windowsinclude-in-ffmpegexe-2.patch
-        fix_windowsinclude-in-ffmpegexe-3.patch
         fixed-debug-bzip2-link.patch
 )
 
