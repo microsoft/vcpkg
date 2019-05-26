7947825ff97fce5ac495f3dc3b652d93735ee443
evpobr <evpobr@gmail.com>
2019-05-26 18:55:41 +0500

    [matroska] Upgrade to 1.5.1

--- ports/matroska/portfile.cmake
+++ ports/matroska/portfile.cmake
@@ -7,7 +7,7 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Matroska-Org/libmatroska
-    REF 51648bb73e4f6a81be754c9a271fc681a1c78f7a
+    REF release-1.5.1
     SHA512 f0369680dfc071837230ff409659c243563d341ebda55b475eee749e8abf305d149c8f8db5d649e87edf03d9c2b186ba8d452ec996eff402e5d1812e01ff32ae
     HEAD_REF master
 )
