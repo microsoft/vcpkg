cab4b38d2c57489c899cd5f8063feee27685f318
Martin Moene <martin.moene@gmail.com>
2019-05-27 10:55:17 +0200

    [byte-lite] Perform CMake install

--- ports/byte-lite/portfile.cmake
+++ ports/byte-lite/portfile.cmake
@@ -7,5 +7,25 @@ vcpkg_from_github(
     SHA512 eefc730a39453dfc367a4478a84d4825cd85721d3c332a57321d0a5c3040a4d45921603ff24220f968dd21df61acea856ae30db8bae6c1e835a1755fb03c84b7
 )
 
-file(INSTALL ${SOURCE_PATH}/include/nonstd/byte.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include/nonstd)
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/byte-lite RENAME copyright)
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBYTE_LITE_OPT_BUILD_TESTS=OFF
+        -DBYTE_LITE_OPT_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(
+    CONFIG_PATH lib/cmake/byte-lite
+)
+
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/debug
+    ${CURRENT_PACKAGES_DIR}/lib
+)
+
+file(INSTALL
+    ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/byte-lite RENAME copyright
+)
