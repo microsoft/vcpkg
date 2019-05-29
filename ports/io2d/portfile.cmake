9910a6f99112e09544e2198b2b8943725fa0a27a
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-05-29 22:24:38 +0200

    [io2d] remove broken sintax

--- ports/io2d/portfile.cmake
+++ ports/io2d/portfile.cmake
@@ -5,23 +5,14 @@ include(vcpkg_common_functions)
 # For now, io2d is always a static library.
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-# Optionally, uncomment and modify one of the 'set(...)' calls below
-# to use io2d sources from a local directory, rather than Github.
-# set(SOURCE_PATH "C:\\Path\\To\\P0267_RefImpl\\")
-# set(SOURCE_PATH "/Path/To/P0267_RefImpl")
-
-# Retrieve and validate io2d source code, as-needed
-if ("${SOURCE_PATH}" STREQUAL "")
-    # TODO: point at cpp-io2d/(whatever), if and as needed
-    vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO cpp-io2d/P0267_RefImpl
-        REF 21ae92c8be6916034e6e18f08aa57899a975dfb0
-        SHA512 5b674f98ca7705d6901af339a4189d5ce4f2c3118bfb99430734f355159602f177bc8d1b345c3a2f17947a62547553f7b91747a1f16da063707a4da7f990391d
-        HEAD_REF master
-        PATCHES find-package.patch
-    )
-endif()
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cpp-io2d/P0267_RefImpl
+    REF 21ae92c8be6916034e6e18f08aa57899a975dfb0
+    SHA512 5b674f98ca7705d6901af339a4189d5ce4f2c3118bfb99430734f355159602f177bc8d1b345c3a2f17947a62547553f7b91747a1f16da063707a4da7f990391d
+    HEAD_REF master
+    PATCHES find-package.patch
+)
 
 # Configure the library, using CMake
 if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL Darwin)
