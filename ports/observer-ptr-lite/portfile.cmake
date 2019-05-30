516431de82a566e559e70c2e7742a072f8b035e6
Martin Moene <martin.moene@gmail.com>
2019-05-30 10:39:20 +0200

    Use dash in unit name for vcpkg_fixup_cmake_targets()

--- ports/observer-ptr-lite/portfile.cmake
+++ ports/observer-ptr-lite/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO martinmoene/observer-ptr-lite
-    REF v0.3.0
-    SHA512 929a08946f3fe13de0456c241900d9924762bc4b963daf7f677dcc7ab2d860f5276742230079b99bd59cc320ff91f2e2eedbd56dfaa20ec35483bd56f2fc104c
+    REF v0.4.0
+    SHA512 4e53d8e0ce595604880bda423440071e7c207dd63e7b6bfa09cc7a870a010f09c51c31e640142c565ce261c4911acab13c6e9f5970853ad8fc2da3e4034ab7d7
 )
 
 vcpkg_configure_cmake(
@@ -17,9 +17,9 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-#vcpkg_fixup_cmake_targets(
-#    CONFIG_PATH lib/cmake/${PORT}
-#)
+vcpkg_fixup_cmake_targets(
+    CONFIG_PATH lib/cmake/${PORT}
+)
 
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug
