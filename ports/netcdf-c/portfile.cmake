6142254f8e2da9fcbe23235a46b13c4f390b0116
JackBoosY <yuzaiyang@beyondsoft.com>
2019-05-31 00:36:15 -0700

    [netcdf-c]Fix adding patch error.

--- ports/netcdf-c/portfile.cmake
+++ ports/netcdf-c/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
         no-install-deps.patch
         config-pkg-location.patch
         transitive-hdf5.patch
+        fix_curl_linkage.patch
         fix-build-error-in-linux.patch # Wait for update and remove this patch.
 )
 
