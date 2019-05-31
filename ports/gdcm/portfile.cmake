2f6140867ad92befaf66339245fa5e9ba808c7f7
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-05-31 13:46:42 +0200

    [gdcm,jbig2dec] minor fixes

--- ports/gdcm/portfile.cmake
+++ ports/gdcm/portfile.cmake
@@ -7,22 +7,18 @@ vcpkg_from_github(
     SHA512 2ac076dd49011234f4431ffe67fcba84a1ca9042ec5fc4dfc8aed2ed16bec5f499fa7aa666e5630796afc266ce76741d931cca333534b55fdc477e25a9189d33
     HEAD_REF master
     PATCHES
-        find-openjpeg.patch
+        use-openjpeg-config.patch
         fix-share-path.patch
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    set(GDCM_BUILD_SHARED_LIBS ON)
-else()
-    set(GDCM_BUILD_SHARED_LIBS OFF)
-endif()
+file(REMOVE ${SOURCE_PATH}/CMake/FindOpenJPEG.cmake)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    PREFER_NINJA
     OPTIONS
         -DGDCM_BUILD_DOCBOOK_MANPAGES=OFF
-        -DGDCM_BUILD_SHARED_LIBS=${GDCM_BUILD_SHARED_LIBS}
+        -DGDCM_BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
         -DGDCM_INSTALL_INCLUDE_DIR=include
         -DGDCM_USE_SYSTEM_EXPAT=ON
         -DGDCM_USE_SYSTEM_ZLIB=ON
@@ -31,8 +27,8 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/gdcm TARGET_PATH share/gdcm)
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
@@ -43,7 +39,4 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
 
-# Handle copyright
 file(INSTALL ${SOURCE_PATH}/Copyright.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/gdcm RENAME copyright)
-
-vcpkg_copy_pdbs()
