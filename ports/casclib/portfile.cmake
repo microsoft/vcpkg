828e6031de3ddfc1229b383b4be2a22d1d072a01
Stijn Herfst <stijnherfst@outlook.com>
2019-06-03 23:38:02 +0200

    [casclib] Add new port

---
+++ ports/casclib/portfile.cmake
@@ -0,0 +1,22 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ladislav-zezula/CascLib
+    REF 3ba98b86885d595da516d5b7fff6c47d993b967a
+    SHA512 f17784267df2ea13aa3d0f95fd410dd68c33d23c3fc6e0d4aaba67f601ae61327020b8a50fc33d630efcb98c7dcb3c5b3ef7df962f000ed28e3c8808cb07f1cc
+    HEAD_REF master
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -STORM_BUILD_TESTS=OFF
+)
+
+vcpkg_install_cmake()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/casclib RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
