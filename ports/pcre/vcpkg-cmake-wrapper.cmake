a7fbc504ea169c9c8518d0ffd379d1b49c2c1f55
Alexander Neumann <Alexander.Neumann@hamburg.de>
2019-06-04 22:52:57 +0200

    fix poco include dir

--- ports/pcre/vcpkg-cmake-wrapper.cmake
+++ ports/pcre/vcpkg-cmake-wrapper.cmake
@@ -1,6 +1,7 @@
 
 include(SelectLibraryConfigurations)
 find_path(PCRE_INCLUDE_DIR pcre.h)
+set(PCRE_INCLUDE_DIRS ${PCRE_INCLUDE_DIR})
 find_library(PCRE_LIBRARY_DEBUG NAMES pcred pcre NAMES_PER_DIR)
 find_library(PCRE_LIBRARY_RELEASE pcre NAMES_PER_DIR)
 select_library_configurations(PCRE)
