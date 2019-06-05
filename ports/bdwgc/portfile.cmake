d0173a276586fad08c7354f58805791457354d0d
Victor Romero <romerosanchezv@gmail.com>
2019-06-05 16:26:01 -0700

    [bdwgc] Update patch

--- ports/bdwgc/portfile.cmake
+++ ports/bdwgc/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_download_distfile(
     PATCH_FILE_PATH
     URLS https://patch-diff.githubusercontent.com/raw/ivmai/bdwgc/pull/281.diff
     FILENAME "install-cmake.diff"
-    SHA512 e94343f77f0e83816c215a9c7b572fcbed57a162b5c20ccf87e2169013aedd8041748be700959184aab6e11e54fad817fe8545d25cda22f7e966dc5542bb92d0
+    SHA512 7c618d21ad730ed49994ba0a639ded98d72e69d910141fb7a1d92f962686db763171690aa596b8d98e9c252a6bd68fb6a871f6306c40a18f9a19c06e933a6d54
 )
 
 vcpkg_from_github(
@@ -13,8 +13,8 @@ vcpkg_from_github(
     REPO ivmai/bdwgc
     # REF v8.0.4
     # SHA512 f3c178c9cab9d9df9ecdad5ac5661c916518d29b0eaca24efe569cb757c386c118ad4389851107597d99ff1bbe99b46383cce73dfd01be983196aa57c9626a4a
-    REF 2b7003769e1ecc941ef80b603cba16a121f37997
-    SHA512 ef2438780cd2bc0d938663c75244dc876aeacb5e1b1c5d30a46c4710608505481ff9769fe9ab142fa03c494ec1853333c873c8a89354b3b363279ed8b8cb1b2a
+    REF 0c8905e84d16bd5e14ed91e21904fd7ab9d197e2
+    SHA512 b38fe86d0dfaacd502971e39ea7df83a3dbf5542711f6b0462b7a6d48dbcf43da07a41a60ee96bca6403a2d2adaac0815a64667f3c80549ca57c5ebbe0e9672d
     HEAD_REF master
     PATCHES
         ${PATCH_FILE_PATH} # 001-install-libraries.patch 
