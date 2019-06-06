4863ffdd0db6906606003fa6a35f5bd70a0a6e1c
Victor Romero <romerosanchezv@gmail.com>
2019-06-05 17:46:27 -0700

    [bdwgc] Use local patch

--- ports/bdwgc/portfile.cmake
+++ ports/bdwgc/portfile.cmake
@@ -1,13 +1,5 @@
 include(vcpkg_common_functions)
 
-
-vcpkg_download_distfile(
-    PATCH_FILE_PATH
-    URLS https://patch-diff.githubusercontent.com/raw/ivmai/bdwgc/pull/281.diff
-    FILENAME "install-cmake.diff"
-    SHA512 7c618d21ad730ed49994ba0a639ded98d72e69d910141fb7a1d92f962686db763171690aa596b8d98e9c252a6bd68fb6a871f6306c40a18f9a19c06e933a6d54
-)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ivmai/bdwgc
@@ -17,7 +9,7 @@ vcpkg_from_github(
     SHA512 b38fe86d0dfaacd502971e39ea7df83a3dbf5542711f6b0462b7a6d48dbcf43da07a41a60ee96bca6403a2d2adaac0815a64667f3c80549ca57c5ebbe0e9672d
     HEAD_REF master
     PATCHES
-        ${PATCH_FILE_PATH} # 001-install-libraries.patch 
+        001-install-libraries.patch 
 )
 
 
