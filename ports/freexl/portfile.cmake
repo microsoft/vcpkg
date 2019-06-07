c0034e565830e8cdff47ac01cce838052c96d883
Jackie Ng <jumpinjackie@gmail.com>
2019-06-07 20:46:09 +1000

    [freexl]: Add linux build support

--- ports/freexl/portfile.cmake
+++ ports/freexl/portfile.cmake
@@ -7,84 +7,171 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/fix-makefiles.patch
-        ${CMAKE_CURRENT_LIST_DIR}/fix-sources.patch
-)
-find_program(NMAKE nmake)
+if (NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
 
-set(LIBS_ALL_DBG 
-	"\"${CURRENT_INSTALLED_DIR}/debug/lib/libiconv.lib\" \
-	\"${CURRENT_INSTALLED_DIR}/debug/lib/libcharset.lib\""
-   )
-set(LIBS_ALL_REL 
-	"\"${CURRENT_INSTALLED_DIR}/lib/libiconv.lib\" \
-	\"${CURRENT_INSTALLED_DIR}/lib/libcharset.lib\""
-   )
+  vcpkg_apply_patches(
+      SOURCE_PATH ${SOURCE_PATH}
+      PATCHES
+          ${CMAKE_CURRENT_LIST_DIR}/fix-makefiles.patch
+          ${CMAKE_CURRENT_LIST_DIR}/fix-sources.patch
+  )
+  find_program(NMAKE nmake)
 
-
-if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
-	set(CL_FLAGS_DBG "/MDd /Zi")
-	set(CL_FLAGS_REL "/MD /Ox")
-else()
-	set(CL_FLAGS_DBG "/MTd /Zi")
-	set(CL_FLAGS_REL "/MT /Ox")
-endif()
-
-
-################
-# Debug build
-################
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    message(STATUS "Building ${TARGET_TRIPLET}-dbg")
-
-    file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}/debug" INST_DIR_DBG)
-    vcpkg_execute_required_process(
-        COMMAND ${NMAKE} -f makefile.vc clean install
-        INST_DIR="${INST_DIR_DBG}" INSTALLED_ROOT="${CURRENT_INSTALLED_DIR}" "LINK_FLAGS=/debug" "CL_FLAGS=${CL_FLAGS_DBG}" "LIBS_ALL=${LIBS_ALL_DBG}"
-        WORKING_DIRECTORY ${SOURCE_PATH}
-        LOGNAME nmake-build-${TARGET_TRIPLET}-debug
+  set(LIBS_ALL_DBG 
+    "\"${CURRENT_INSTALLED_DIR}/debug/lib/libiconv.lib\" \
+    \"${CURRENT_INSTALLED_DIR}/debug/lib/libcharset.lib\""
     )
-    message(STATUS "Building ${TARGET_TRIPLET}-dbg done")
-    vcpkg_copy_pdbs()
-endif()
-
-################
-# Release build
-################
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    message(STATUS "Building ${TARGET_TRIPLET}-rel")
-
-    file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}" INST_DIR_REL)
-    vcpkg_execute_required_process(
-        COMMAND ${NMAKE} -f makefile.vc clean install
-        INST_DIR="${INST_DIR_REL}" INSTALLED_ROOT="${CURRENT_INSTALLED_DIR}" "LINK_FLAGS=" "CL_FLAGS=${CL_FLAGS_REL}" "LIBS_ALL=${LIBS_ALL_REL}"
-        WORKING_DIRECTORY ${SOURCE_PATH}
-        LOGNAME nmake-build-${TARGET_TRIPLET}-release
+  set(LIBS_ALL_REL 
+    "\"${CURRENT_INSTALLED_DIR}/lib/libiconv.lib\" \
+    \"${CURRENT_INSTALLED_DIR}/lib/libcharset.lib\""
     )
-    message(STATUS "Building ${TARGET_TRIPLET}-rel done")
-endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/freexl RENAME copyright)
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
-  file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/freexl_i.lib)
-  file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/freexl_i.lib)
-else()
-  file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/freexl.lib)
-  file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/freexl.lib)
-  if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    file(RENAME ${CURRENT_PACKAGES_DIR}/lib/freexl_i.lib ${CURRENT_PACKAGES_DIR}/lib/freexl.lib)
+  if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
+    set(CL_FLAGS_DBG "/MDd /Zi")
+    set(CL_FLAGS_REL "/MD /Ox")
+  else()
+    set(CL_FLAGS_DBG "/MTd /Zi")
+    set(CL_FLAGS_REL "/MT /Ox")
   endif()
+
+
+  ################
+  # Debug build
+  ################
   if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/freexl_i.lib ${CURRENT_PACKAGES_DIR}/debug/lib/freexl.lib)
-  endif()
-endif()
+      message(STATUS "Building ${TARGET_TRIPLET}-dbg")
 
+      file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}/debug" INST_DIR_DBG)
+      vcpkg_execute_required_process(
+          COMMAND ${NMAKE} -f makefile.vc clean install
+          INST_DIR="${INST_DIR_DBG}" INSTALLED_ROOT="${CURRENT_INSTALLED_DIR}" "LINK_FLAGS=/debug" "CL_FLAGS=${CL_FLAGS_DBG}" "LIBS_ALL=${LIBS_ALL_DBG}"
+          WORKING_DIRECTORY ${SOURCE_PATH}
+          LOGNAME nmake-build-${TARGET_TRIPLET}-debug
+      )
+      message(STATUS "Building ${TARGET_TRIPLET}-dbg done")
+      vcpkg_copy_pdbs()
+  endif()
+
+  ################
+  # Release build
+  ################
+  if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+      message(STATUS "Building ${TARGET_TRIPLET}-rel")
+
+      file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}" INST_DIR_REL)
+      vcpkg_execute_required_process(
+          COMMAND ${NMAKE} -f makefile.vc clean install
+          INST_DIR="${INST_DIR_REL}" INSTALLED_ROOT="${CURRENT_INSTALLED_DIR}" "LINK_FLAGS=" "CL_FLAGS=${CL_FLAGS_REL}" "LIBS_ALL=${LIBS_ALL_REL}"
+          WORKING_DIRECTORY ${SOURCE_PATH}
+          LOGNAME nmake-build-${TARGET_TRIPLET}-release
+      )
+      message(STATUS "Building ${TARGET_TRIPLET}-rel done")
+  endif()
+
+  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+  file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/freexl RENAME copyright)
+
+  if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/freexl_i.lib)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/freexl_i.lib)
+  else()
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/freexl.lib)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/freexl.lib)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+      file(RENAME ${CURRENT_PACKAGES_DIR}/lib/freexl_i.lib ${CURRENT_PACKAGES_DIR}/lib/freexl.lib)
+    endif()
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+      file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/freexl_i.lib ${CURRENT_PACKAGES_DIR}/debug/lib/freexl.lib)
+    endif()
+  endif()
+
+elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Darwin") # Build in UNIX
+
+    # Check build system first
+    find_program(MAKE make)
+    if (NOT MAKE)
+        message(FATAL_ERROR "MAKE not found")
+    endif()
+
+    set(SOURCE_PATH_RELEASE ${SOURCE_PATH})
+    set(SOURCE_PATH_DEBUG ${SOURCE_PATH})
+
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        ################
+        # Release build
+        ################
+        message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
+        set(OUT_PATH_RELEASE ${SOURCE_PATH_RELEASE}/../../make-build-${TARGET_TRIPLET}-release)
+        file(MAKE_DIRECTORY ${OUT_PATH_RELEASE})
+        vcpkg_execute_required_process(
+            COMMAND "${SOURCE_PATH_RELEASE}/configure" --prefix=${OUT_PATH_RELEASE} "${FREEXL_CONFIGURE_ARGS_REL}"
+            WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
+            LOGNAME config-${TARGET_TRIPLET}-rel
+        )
+
+        message(STATUS "Building ${TARGET_TRIPLET}-rel")
+        vcpkg_execute_required_process(
+            COMMAND make
+            WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
+            LOGNAME make-build-${TARGET_TRIPLET}-release
+        )
+
+        message(STATUS "Installing ${TARGET_TRIPLET}-rel")
+        vcpkg_execute_required_process(
+            COMMAND make install
+            WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
+            LOGNAME make-install-${TARGET_TRIPLET}-release
+        )
+
+        #file(REMOVE_RECURSE ${OUT_PATH_RELEASE}/lib/gdalplugins)
+        #file(COPY ${OUT_PATH_RELEASE}/lib/pkgconfig DESTINATION ${OUT_PATH_RELEASE}/share/gdal)
+        #file(REMOVE_RECURSE ${OUT_PATH_RELEASE}/lib/pkgconfig)
+        file(COPY ${OUT_PATH_RELEASE}/lib DESTINATION ${CURRENT_PACKAGES_DIR})
+        file(COPY ${OUT_PATH_RELEASE}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+        #file(COPY ${OUT_PATH_RELEASE}/share DESTINATION ${CURRENT_PACKAGES_DIR})
+        file(COPY ${CURRENT_BUILDTREES_DIR}/src/freexl-1.0.4/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/freexl)
+        file(RENAME ${CURRENT_PACKAGES_DIR}/share/freexl/COPYING ${CURRENT_PACKAGES_DIR}/share/freexl/copyright)
+        message(STATUS "Installing ${TARGET_TRIPLET}-rel done")
+    endif()
+
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        ################
+        # Debug build
+        ################
+        message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
+        set(OUT_PATH_DEBUG ${SOURCE_PATH_DEBUG}/../../make-build-${TARGET_TRIPLET}-debug)
+        file(MAKE_DIRECTORY ${OUT_PATH_DEBUG})
+        vcpkg_execute_required_process(
+            COMMAND "${SOURCE_PATH_DEBUG}/configure" --prefix=${OUT_PATH_DEBUG} "${FREEXL_CONFIGURE_ARGS_DBG}"
+            WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
+            LOGNAME config-${TARGET_TRIPLET}-debug
+        )
+
+        message(STATUS "Building ${TARGET_TRIPLET}-dbg")
+            vcpkg_execute_required_process(
+            COMMAND make
+            WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
+            LOGNAME make-build-${TARGET_TRIPLET}-debug
+        )
+
+        message(STATUS "Installing ${TARGET_TRIPLET}-dbg")
+            vcpkg_execute_required_process(
+            COMMAND make -j install
+            WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
+            LOGNAME make-install-${TARGET_TRIPLET}-debug
+        )
+
+        #file(REMOVE_RECURSE ${OUT_PATH_DEBUG}/lib/gdalplugins)
+        #file(REMOVE_RECURSE ${OUT_PATH_DEBUG}/lib/pkgconfig)
+        file(COPY ${OUT_PATH_DEBUG}/lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug)
+        message(STATUS "Installing ${TARGET_TRIPLET}-dbg done")
+    endif()
+
+else()# Other build system
+    message(FATAL_ERROR "Unsupported build system.")
+endif()
 
 message(STATUS "Packaging ${TARGET_TRIPLET} done")
