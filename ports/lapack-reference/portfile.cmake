38ddfc5ba1d7fcdf9e35887f1c300902554bd7ed
Alexander Neumann <Alexander.Neumann@hamburg.de>
2019-06-09 14:10:13 +0200

    [lapack] new port build with user supplied fortran

---
+++ ports/lapack-reference/portfile.cmake
@@ -0,0 +1,57 @@
+#TODO: Features to add:
+# USE_XBLAS??? extended precision blas. needs xblas
+
+# LAPACKE should be its own PORT
+# USE_OPTIMIZED_LAPACK (Probably not what we want. Does a find_package(LAPACK): probably for LAPACKE only builds _> own port?)
+# LAPACKE Builds LAPACKE
+# LAPACKE_WITH_TMG Build LAPACKE with tmglib routines
+
+SET(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+include(vcpkg_common_functions)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_get_fortran_compiler()
+
+set(ENV{PATH} "${CMAKE_CURRENT_LIST_DIR}/../../installed/${TARGET_TRIPLET}/tools;$ENV{PATH}") # Put vcpkg built ninja with fortran support into path 
+
+set(lapack_ver 3.8.0)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO  Reference-LAPACK/lapack
+    REF ${lapack_ver}
+    SHA512 17786cb7306fccdc9b4a242de7f64fc261ebe6a10b6ec55f519deb4cb673cb137e8742aa5698fd2dc52f1cd56d3bd116af3f593a01dcf6770c4dcc86c50b2a7f
+    HEAD_REF master
+)
+
+
+set(USE_OPTIMIZED_BLAS OFF)
+if("noblas" IN_LIST FEATURES)
+    set(USE_OPTIMIZED_BLAS ON)
+endif()
+
+set(CBLAS OFF)
+if("cblas" IN_LIST FEATURES)
+    set(CBLAS ON)
+endif()
+
+vcpkg_configure_cmake(
+        PREFER_NINJA
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
+            "-DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}"
+            "-DUSE_OPTIMIZED_BLAS=${USE_OPTIMIZED_BLAS}"
+            "-DCBLAS=${CBLAS}"
+        )
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/lapack-${lapack_ver}) #Should the target path be lapack and not lapack-reference?
+vcpkg_copy_pdbs()
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+# remove debug includs
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
---
+++ scripts/cmake/vcpkg_get_fortran_compiler.cmake
@@ -0,0 +1,50 @@
+## # vcpkg_get_fortran_compiler
+##
+## Tries to detect a fortran compiler using CMakeDetermineFortranCompiler with some additional error checking.
+##
+##
+## If the variable CMAKE_Fortran_COMPILER gets not set an error will be raised.
+## You can use that variable or VCPKG_FORTRAN_COMPILER
+##
+## ## Usage:
+## ```cmake
+## vcpkg_get_fortran_compiler()
+## ```
+##
+## ## Examples:
+##
+## * [lapack](https://github.com/Microsoft/vcpkg/blob/master/ports/lapack-reference/portfile.cmake)
+
+function(vcpkg_get_fortran_compiler)
+
+    if(WIN32)
+        file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
+        file(GLOB PGI_PATHS LIST_DIRECTORIES true "${_programfiles}/PGI/win64/*") # find possible PGI default paths
+        message(STATUS "${PGI_PATHS}")
+        foreach(_pgi_path ${PGI_PATHS})
+            if(IS_DIRECTORY ${_pgi_path})
+                if(EXISTS "${_pgi_path}/bin")
+                    list(APPEND CMAKE_PROGRAM_PATH "${_pgi_path}/bin") # Should probably do a search for folders in this 
+                endif()
+            endif()
+        endforeach()
+    endif()
+
+    include(CMakeDetermineFortranCompiler)  # Find a FORTRAN compiler for us. Can be overwritten by setting CMAKE_Fortran_COMPILER
+
+    if(${CMAKE_Fortran_COMPILER} MATCHES "NOTFOUND")
+        message(FATAL_ERROR "LAPACK requires a FORTRAN compiler! Examples: Windows: PGI Compiler & Tools/Intel; Linux: gFortran")
+    else()
+        message(STATUS "Used FORTRAN Compiler: ${CMAKE_Fortran_COMPILER}")
+    endif()
+
+    if(${CMAKE_Fortran_COMPILER} MATCHES "$pg")
+        if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64") 
+            # Support for 32-bit development was deprecated in PGI 2016 and is no longer available as of the PGI 2017 release. 
+            # PGI 2017 is only available for 64-bit operating systems and does not include the ability to compile 32-bit applications
+            message(FATAL_ERROR "lapack can only be built for x64 systems")
+        endif()
+    endif()
+    
+    set(VCPKG_FORTRAN_COMPILER "${CMAKE_Fortran_COMPILER}")
+endfunction()
