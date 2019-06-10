5e9d36bc08fbb2eeb5d712f6ea2653f5eb19a260
Robert Schumacher <roschuma@microsoft.com>
2019-06-09 18:30:30 -0700

    [sqlite-modern-cpp] Track master due to infrequent release

--- ports/sqlite-modern-cpp/portfile.cmake
+++ ports/sqlite-modern-cpp/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO aminroosta/sqlite_modern_cpp
-    REF 56af65d2c5085dd34a2c1a4ad20cf8b93ad1024f
+    REPO SqliteModernCpp/sqlite_modern_cpp
+    REF 936cd0c86aacac87a3dab32906397944ae5f6c3d
     SHA512 06a2ded49c397bc5863b08e662a4c980b2bb6ffeaade9ba8ffa34f63e22a95fe2138ba2412b51c6ef5429e2d1110c8dfb085af7e97b2548c2293319caea77bae
     HEAD_REF master
 )
