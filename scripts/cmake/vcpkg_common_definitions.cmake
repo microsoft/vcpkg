310b41c1c96bcbfb0a62f14b135c2756f0dac790
Alexander Neumann <Alexander.Neumann@hamburg.de>
2019-06-10 22:31:42 +0200

    fix missing ()

--- scripts/cmake/vcpkg_common_definitions.cmake
+++ scripts/cmake/vcpkg_common_definitions.cmake
@@ -7,7 +7,7 @@ elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     set(VCPKG_TARGET_IS_OSX 1)
 elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(VCPKG_TARGET_IS_LINUX 1)
-else
+else()
     message(WARNING "Unknown system target for VCPKG_CMAKE_SYSTEM_NAME:${VCPKG_CMAKE_SYSTEM_NAME}! Please define VCPKG_TARGET_IS_<target> in vcpkg_common_definitions.cmake")
 endif()
 
