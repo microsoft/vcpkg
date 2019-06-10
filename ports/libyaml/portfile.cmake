e1d4ec0380686b27feac29deac366e5f72406136
Robert Schumacher <roschuma@microsoft.com>
2019-06-09 18:24:17 -0700

    [libyaml] Switch to non-dist tag

--- ports/libyaml/portfile.cmake
+++ ports/libyaml/portfile.cmake
@@ -3,17 +3,16 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO yaml/libyaml
-    REF dist-0.2.2
-    SHA512 0728c89ba43af3cdec22bccaf18c9ad7b07e13cdebed9d8919e2c62cf90bb5e23d2c746fd250320b2827dfcd9f1ce442d3bf8a3fe18b61f9a8d1d7770561e610
+    REF 0.2.2
+    SHA512 455494591014a97c4371a1f372ad09f0d6e487e4f1d3419c98e9cd2f16d43a0cf9a0787d7250bebee8b8d400df4626f5acd81e90139e54fa574a66ec84964c06
     HEAD_REF master
-    PATCHES fix-build-error.patch
 )
-
-file(COPY "${CURRENT_PORT_DIR}/yamlConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DBUILD_TESTING=OFF
 )
 
 vcpkg_install_cmake()
