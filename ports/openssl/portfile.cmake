8e05f9453cfbd5080c4c06ecea47c621dccff348
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-06-10 16:57:10 +0200

    [libressl,openssl] do not try to build one if the other is already installed

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -1,2 +1,8 @@
+include(vcpkg_common_functions)
+
+if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h")
+  message(WARNING "Can't build openssl if libressl is installed. Please remove libressl, and try install openssl again if you need it.")
+endif()
+
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/openssl/)
