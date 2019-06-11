6f373db41b9f9a97430f1d953e716ba3c22051c2
Alexander Neumann <alexander.neumann@hamburg.de>
2019-06-11 14:14:06 +0200

    remove warning message

--- scripts/cmake/vcpkg_common_definitions.cmake
+++ scripts/cmake/vcpkg_common_definitions.cmake
@@ -7,8 +7,4 @@ elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Darwin")
     set(VCPKG_TARGET_IS_OSX 1)
 elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(VCPKG_TARGET_IS_LINUX 1)
-else()
-    message(WARNING "Unknown system target for VCPKG_CMAKE_SYSTEM_NAME:${VCPKG_CMAKE_SYSTEM_NAME}! Please define VCPKG_TARGET_IS_<target> in vcpkg_common_definitions.cmake")
 endif()
-
-
