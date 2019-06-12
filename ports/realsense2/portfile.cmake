c7a4fe7a169fcf2c6fb1e3efd8eace3ccc624bbd
Victor Romero <romerosanchezv@gmail.com>
2019-06-12 11:27:31 -0700

    [realsense2] Update to version 2.22.0

--- ports/realsense2/portfile.cmake
+++ ports/realsense2/portfile.cmake
@@ -3,12 +3,9 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO IntelRealSense/librealsense
-    REF v2.19.0
-    SHA512 c1fcb2b11827a5518b1e5196b7d1d2406447c6b2301809d3c66aaf69363a36f9789fd33595f581846809eba2c2540d3add964da03ecd96c804c7ca2b2df85180
+    REF v2.22.0
+    SHA512 7595780c1955a2d4a91df5b70ab6366c672f389bc3d2dcb9f2e78a2ea1fc875c65f878103df483205e17f62dfd024ee5f7ccb15afc5d18978d3c25aa071652ab
     HEAD_REF development
-    PATCHES
-        libtm.patch
-        windowsconfig.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" BUILD_CRT_LINKAGE)
@@ -24,38 +21,35 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        # Ungrouped Entries
         -DENFORCE_METADATA=ON
-        # BUILD
-        -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
         -DBUILD_WITH_TM2=OFF
+        -DBUILD_WITH_OPENMP=OFF
+        -DBUILD_UNIT_TESTS=OFF
+        -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
     OPTIONS_RELEASE
-        # BUILD
         -DBUILD_EXAMPLES=${BUILD_EXAMPLES}
         -DBUILD_GRAPHICAL_EXAMPLES=${BUILD_GRAPHICAL_EXAMPLES}
-        -DBUILD_UNIT_TESTS=OFF
-        -DBUILD_WITH_OPENMP=OFF
-        -DBUILD_WITH_STATIC_CRT=${BUILD_CRT_LINKAGE}
     OPTIONS_DEBUG
-        # BUILD
         -DBUILD_EXAMPLES=OFF
         -DBUILD_GRAPHICAL_EXAMPLES=OFF
-        # CMAKE
-        -DCMAKE_DEBUG_POSTFIX=d
 )
 
 vcpkg_install_cmake()
+
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/realsense2)
+
 vcpkg_copy_pdbs()
 
-file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/libtm_util.exe)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/libtm_util.exe)
 
 if(BUILD_EXAMPLES)
-    file(GLOB EXEFILES_RELEASE ${CURRENT_PACKAGES_DIR}/bin/rs-* ${CURRENT_PACKAGES_DIR}/bin/realsense-*)
+    file(GLOB EXEFILES_RELEASE 
+        ${CURRENT_PACKAGES_DIR}/bin/rs-* 
+        ${CURRENT_PACKAGES_DIR}/bin/realsense-*
+    )
+
     if (EXEFILES_RELEASE)
         file(COPY ${EXEFILES_RELEASE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/realsense2)
         file(REMOVE ${EXEFILES_RELEASE})
