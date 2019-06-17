fedd4bbc2e083fcc67a3deef28936e5dfa978a3e
et-nik <nikita.hldm@gmail.com>
2019-06-17 20:36:16 +0300

    Add cmakelists patch

--- ports/restclient-cpp/portfile.cmake
+++ ports/restclient-cpp/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 0.5.1
     SHA512 d5e17a984af44f863bc7cdc7307c2b06cae9252f86c6c6c2377cdb317f61b6419d8e9aedc5e5ccdb08fd1ee13848ec3b9ef8067a8d26dcf438a5c8793b5a2ce3
     HEAD_REF master
+    PATCHES
+        0001_fix_cmake_linking.patch
 )
 
 vcpkg_configure_cmake(
