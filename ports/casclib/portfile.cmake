cc52260e5f9ec406a4b83968b0bec3e7cdc9bb32
Phil Christensen <philc@microsoft.com>
2019-06-18 15:44:54 -0700

    [casclib] add patch for Mac and use 1.50 tag

--- ports/casclib/portfile.cmake
+++ ports/casclib/portfile.cmake
@@ -3,9 +3,11 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ladislav-zezula/CascLib
-    REF 3ba98b86885d595da516d5b7fff6c47d993b967a
-    SHA512 f17784267df2ea13aa3d0f95fd410dd68c33d23c3fc6e0d4aaba67f601ae61327020b8a50fc33d630efcb98c7dcb3c5b3ef7df962f000ed28e3c8808cb07f1cc
+    REF 1.50
+    SHA512 7e95e314f09e504566e6fa2b1742f986d17526fb0283af8ffb77681338c9a852d369cbd863512a20ddd3a277d10a9bf701d745f500a717826dd08e201eb6d80e
     HEAD_REF master
+    PATCHES
+        ctype_for_mac.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
