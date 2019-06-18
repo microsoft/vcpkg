d91e8133a4e973e32ae505f9644d4c60b6099406
Daniel <51385773+dan-shaw@users.noreply.github.com>
2019-06-18 11:32:34 -0700

    [spirv-tools] comment python distutils

--- ports/spirv-tools/portfile.cmake
+++ ports/spirv-tools/portfile.cmake
@@ -7,6 +7,8 @@ vcpkg_from_github(
     REPO KhronosGroup/SPIRV-Tools
     REF d0a1f5a05a2b0f8315e5b3f17b8e34c730861b31
     SHA512 7179751b0216368b4a4bf8c9b0c1c1e3b17d6aa4788b4aeaa7fbb2b6d9d50b34cf209082f3531a2e0994b5fc02416373666d4d12cee282cec2c3d02c13a640a8
+    PATCHES
+        comment-distutils.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
