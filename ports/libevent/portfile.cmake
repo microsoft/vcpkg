74dadd1421c1388366c43920adcb9d8b45fdd07f
wangli28 <wangli28@beyondsoft.com>
2019-06-20 09:35:19 +0000

    Fix build error

--- ports/libevent/portfile.cmake
+++ ports/libevent/portfile.cmake
@@ -14,11 +14,18 @@ vcpkg_from_github(
         fix-arm_build.patch
 )
 
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    set(LIBEVENT_LIB_TYPE SHARED)
+else()
+    set(LIBEVENT_LIB_TYPE STATIC)
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DEVENT_INSTALL_CMAKE_DIR:PATH=share/libevent
+        -DEVENT__LIBRARY_TYPE=${LIBEVENT_LIB_TYPE}
         -DEVENT__DISABLE_BENCHMARK=ON
         -DEVENT__DISABLE_TESTS=ON
         -DEVENT__DISABLE_REGRESS=ON
@@ -28,11 +35,15 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+if (NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "windows" OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore") #Windows
+    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/libevent)
+elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux") #Linux/Unix
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/libevent)
+elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Darwin") #OsX
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/libevent)
 endif()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/libevent)
 vcpkg_copy_pdbs()
 
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libevent)
