82b6877c0585a5fa50327de041ac92d50354d559
JackBoosY <yuzaiyang@beyondsoft.com>
2019-06-25 00:20:55 -0700

    [qt5-base]Use print messages instead of installing dependencies directly.

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -87,7 +87,8 @@ if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore
     )
 
 elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    message("qt5 requires libgl1-mesa-dev and libglu1-mesa-dev, please use command: \"apt-get install libgl1-mesa-dev\" and \"apt-get install libglu1-mesa-dev\" to install them.")
+    message("qt5 requires libgl1-mesa-dev and libglu1-mesa-dev, please use your distribution's package manager to install them.")
+    message("Example: \"apt-get install libgl1-mesa-dev\" and \"apt-get install libglu1-mesa-dev\"")
 
     configure_qt(
         SOURCE_PATH ${SOURCE_PATH}
