39b4c6ad041f18a433500ad9358991362cba9dae
Daniel Marshall <xandan@gmail.com>
2019-06-26 13:02:45 +0100

    Update version and homepage

--- ports/scnlib/portfile.cmake
+++ ports/scnlib/portfile.cmake
@@ -5,8 +5,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO eliaskosunen/scnlib
-    REF 21001af7f21ddedc329a698d1a6514ee80a813ae
-    SHA512 41a426fa94e1b2f31f5a1959ef97c59f48d0d7f0fd7369aab31a131aac71a173509e9354ef19e32211a1aba03597dbdfde131c8bb03fb234a093631716051d96
+    REF v0.1.2
+    SHA512 bb2aa176517d6547d62800839b7e9d86821a4006f7d09a8b3731b67f6fd7130f5cf8d73e7c8331c7c50f1dd5ca19bd3759bb0e181fcaed2f0bfb7fd8276ef141
     HEAD_REF master
 )
 
@@ -18,11 +18,12 @@ vcpkg_configure_cmake(
       -DSCN_EXAMPLES=OFF
       -DSCN_BENCHMARKS=OFF
       -DSCN_DOCS=OFF
-      -DCMAKE_DISABLE_FIND_PACKAGE_range-v3=ON
+      -DSCN_RANGES=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/scn)
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share ${CURRENT_PACKAGES_DIR}/share/scn)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/scnlib RENAME copyright)
 
