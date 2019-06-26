7d334942d77fb0e9ecc4fc98571c473941e5bd75
Robert Schumacher <roschuma@microsoft.com>
2019-06-25 21:58:24 -0700

    [tl-expected][tl-function-ref][tl-optional] Always prepopulate FetchContent

--- ports/tl-optional/portfile.cmake
+++ ports/tl-optional/portfile.cmake
@@ -8,9 +8,20 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+vcpkg_from_github(
+    OUT_SOURCE_PATH TL_CMAKE_SOURCE_DIR
+    REPO TartanLlama/tl-cmake
+    REF 284c6a3f0f61823cc3871b0f193e8df699e2c4ce
+    SHA512 f611326d75d6e87e58cb05e91f9506b1d83e6fd3b214fe311c4c15604feabfb7a18bbf9c4b4c389a39d615eb468b1f4b15802ab9f44f334a12310cb183fa77a7
+    HEAD_REF master
+)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
+        -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+        -DFETCHCONTENT_SOURCE_DIR_TL_CMAKE=${TL_CMAKE_SOURCE_DIR}
         -DOPTIONAL_ENABLE_TESTS=OFF
 )
 
