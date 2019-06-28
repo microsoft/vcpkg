02fab5f043ac954d648fdb6cc537921d73b1bab9
JackBoosY <yuzaiyang@beyondsoft.com>
2019-06-27 22:37:52 -0700

    [qt5]Fix tools/quickcontrols2 build errors in linux and add activeqt/macextras/winextras error messages.

--- ports/qt5-tools/portfile.cmake
+++ ports/qt5-tools/portfile.cmake
@@ -2,6 +2,20 @@ include(vcpkg_common_functions)
 
 include(${CURRENT_INSTALLED_DIR}/share/qt5modularscripts/qt_modular_library.cmake)
 
+# Copy qt5-declarative tools and library
+# This is a temporary workaround and hope to fix and remove it after the version update.
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    if (EXISTS ${CURRENT_INSTALLED_DIR}/debug/tools/qt5-declarative/qmlimportscanner)
+        file(COPY ${CURRENT_INSTALLED_DIR}/debug/tools/qt5-declarative/qmlimportscanner DESTINATION ${CURRENT_INSTALLED_DIR}/debug/tools/qt5/)
+    endif()
+    if (EXISTS ${CURRENT_INSTALLED_DIR}/tools/qt5-declarative/qmlimportscanner)
+        file(COPY ${CURRENT_INSTALLED_DIR}/tools/qt5-declarative/qmlimportscanner DESTINATION ${CURRENT_INSTALLED_DIR}/tools/qt5/)
+    endif()
+    if (EXISTS ${CURRENT_INSTALLED_DIR}/lib/libQt5QmlDevTools.a)
+        file(COPY ${CURRENT_INSTALLED_DIR}/lib/libQt5QmlDevTools.a DESTINATION ${CURRENT_INSTALLED_DIR}/debug/lib/)
+    endif()
+endif()
+
 qt_modular_library(qttools d37c0e11a26a21aa60f29f3b17ddc9895385d848692956e4481e49003cbe9c227daf8fda1c40a2ab70ac8e7e56d3771c1b2964524589eb77ac1f2362c269162e)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/tools/qt5-tools/plugins/platforminputcontexts)
