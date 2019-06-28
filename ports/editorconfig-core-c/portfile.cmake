16269339760ba9c7dd7f84ba3bc200e32572cb3a
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-06-28 10:20:20 +0200

    [editorconfig-core-c] remove files in a portable way

--- ports/editorconfig-core-c/portfile.cmake
+++ ports/editorconfig-core-c/portfile.cmake
@@ -15,9 +15,24 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/editorconfig.exe)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/editorconfig.exe)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(EXECUTABLE_SUFFIX ".exe")
+else()
+    set(EXECUTABLE_SUFFIX "")
+endif()
+
+if(EXISTS "${CURRENT_PACKAGES_DIR}/bin/editorconfig${EXECUTABLE_SUFFIX}")
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/editorconfig${EXECUTABLE_SUFFIX})
+endif()
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/bin/editorconfig${EXECUTABLE_SUFFIX}")
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/editorconfig${EXECUTABLE_SUFFIX})
+endif()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/man)
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/editorconfig-core-c RENAME copyright)
