eb5ac77df59ebd502bac968619a48f405edafd76
myd7349 <myd7349@gmail.com>
2019-06-29 13:05:58 +0800

    [mongoose] About UWP

--- ports/mongoose/portfile.cmake
+++ ports/mongoose/portfile.cmake
@@ -1,5 +1,9 @@
 include(vcpkg_common_functions)
 
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    message(FATAL_ERROR "${PORT} does not currently support UWP")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cesanta/mongoose
