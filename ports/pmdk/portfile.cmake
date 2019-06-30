b25129e4dd3655f991ed803ffef2d8ee30fa6ab6
Robert Schumacher <roschuma@microsoft.com>
2019-06-30 02:28:59 -0700

    [pmdk][libpmemobj-cpp] Updates and improvements

--- ports/pmdk/portfile.cmake
+++ ports/pmdk/portfile.cmake
@@ -2,10 +2,11 @@ include(vcpkg_common_functions)
 
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 
-if (TRIPLET_SYSTEM_ARCH MATCHES "arm")
-    message(FATAL_ERROR "ARM is currently not supported")
-elseif (TRIPLET_SYSTEM_ARCH MATCHES "x86")
-    message(FATAL_ERROR "x86 is not supported. Please use pmdk:x64-windows instead.")
+if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    message(FATAL_ERROR "${VCPKG_TARGET_ARCHITECTURE} is currently not supported; only x64 is available.")
+endif()
+if(VCPKG_CMAKE_SYSTEM_NAME)
+    message(FATAL_ERROR "This port currently can only be built for Windows Desktop")
 endif()
 
 # Download source
@@ -16,51 +17,37 @@ vcpkg_from_github(
     SHA512 f66e4edf1937d51abfa7c087b65a64109cd3d2a8d9587d6c4fc28a1003d67ec1f35a0011c9a9d0bfe76ad7227be83e86582f8405c988eac828d8ae5d0a399483
     HEAD_REF master
     PATCHES
-        "${CMAKE_CURRENT_LIST_DIR}/addPowerShellExecutionPolicy.patch"
-        "${CMAKE_CURRENT_LIST_DIR}/v141.patch"
+        addPowerShellExecutionPolicy.patch
+        v141.patch
 )
 
 get_filename_component(PMDK_VERSION "${SOURCE_PATH}" NAME)
 string(REPLACE "pmdk-" "" PMDK_VERSION "${PMDK_VERSION}")
 
 # Build only the selected projects
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/src/PMDK.sln
+vcpkg_install_msbuild(
+    SOURCE_PATH ${SOURCE_PATH}
+    PROJECT_SUBPATH src/PMDK.sln
+    INCLUDES_SUBPATH src/include
     TARGET "Solution Items\\libpmem,Solution Items\\libpmemlog,Solution Items\\libpmemblk,Solution Items\\libpmemobj,Solution Items\\libpmempool,Solution Items\\libvmem,Solution Items\\Tools\\pmempool"
     OPTIONS /p:SRCVERSION=${PMDK_VERSION}
+    ALLOW_ROOT_INCLUDES
 )
 
-set(DEBUG_ARTIFACTS_PATH ${SOURCE_PATH}/src/x64/Debug)
-set(RELEASE_ARTIFACTS_PATH ${SOURCE_PATH}/src/x64/Release)
-
-# Install header files
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/include/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/include/libpmemobj/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libpmemobj)
-
-# Remove unneeded header files
-file(REMOVE ${CURRENT_PACKAGES_DIR}/include/libvmmalloc.h)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/include/librpmem.h)
-
-# Install libraries (debug)
-file(GLOB LIB_DEBUG_FILES ${DEBUG_ARTIFACTS_PATH}/libs/libpmem*.lib)
-file(INSTALL ${LIB_DEBUG_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/libpmemcommon.lib)
-file(GLOB LIB_DEBUG_FILES ${DEBUG_ARTIFACTS_PATH}/libs/libpmem*.dll)
-file(INSTALL ${LIB_DEBUG_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-# Install libraries (release)
-file(GLOB LIB_RELEASE_FILES ${RELEASE_ARTIFACTS_PATH}/libs/libpmem*.lib)
-file(INSTALL ${LIB_RELEASE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libpmemcommon.lib)
-file(GLOB LIB_RELEASE_FILES ${RELEASE_ARTIFACTS_PATH}/libs/libpmem*.dll)
-file(INSTALL ${LIB_RELEASE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-
-# Install tools (release only)
-file(INSTALL ${RELEASE_ARTIFACTS_PATH}/libs/pmempool.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/pmdk)
-
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/pmdk)
+# Remove unneeded files
+file(REMOVE_RECURSE
+    ${CURRENT_PACKAGES_DIR}/include/libvmmalloc.h
+    ${CURRENT_PACKAGES_DIR}/include/librpmem.h
+    ${CURRENT_PACKAGES_DIR}/include/.cstyleignore
+    ${CURRENT_PACKAGES_DIR}/include/README
+    ${CURRENT_PACKAGES_DIR}/include/libpmemobj++
+    ${CURRENT_PACKAGES_DIR}/lib/getopt.lib
+    ${CURRENT_PACKAGES_DIR}/lib/jemalloc.lib
+    ${CURRENT_PACKAGES_DIR}/lib/libpmemcommon.lib
+    ${CURRENT_PACKAGES_DIR}/debug/lib/getopt.lib
+    ${CURRENT_PACKAGES_DIR}/debug/lib/jemalloc.lib
+    ${CURRENT_PACKAGES_DIR}/debug/lib/libpmemcommon.lib
+)
 
 vcpkg_copy_pdbs()
 
