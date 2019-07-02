4ce83bb9d1a8a4fd60a2bf09f94c289332940009
JackBoosY <yuzaiyang@beyondsoft.com>
2019-07-01 18:40:17 -0700

    [aws-checksums]Fix build error with arm-uwp.

--- ports/aws-checksums/portfile.cmake
+++ ports/aws-checksums/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     REF v0.1.2
     SHA512 d924918fce5179e2f42c0aeb86cb3bfda22f92bf3950e179f248a8d3e72c05a4f1015982970fd4075bb687a0a6e03120eee2134c0f017ec8ef69ae066881aa0d
     HEAD_REF master
+    PATCHES fix-arm-build.patch
 )
 
 vcpkg_configure_cmake(
