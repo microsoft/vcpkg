fdb411587c3224eed779b01f0ff08339b21e7ee9
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-07-03 11:11:28 +0200

    [OpenMPI] fix prefix

--- ports/openmpi/portfile.cmake
+++ ports/openmpi/portfile.cmake
@@ -4,6 +4,8 @@ if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore" OR NOT VCPKG_CMAKE_SYSTEM_NAM
   message(FATAL_ERROR "This port is only for openmpi on Unix-like systems")
 endif()
 
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
 set(OpenMPI_FULL_VERSION "4.0.1")
 set(OpenMPI_SHORT_VERSION "4.0")
 
@@ -19,8 +21,14 @@ endif()
 if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
     list(APPEND BUILD_TYPES "debug")
 endif()
+
 set(SOURCE_PATH_DEBUG   ${CURRENT_BUILDTREES_DIR}/src-${TARGET_TRIPLET}-debug/openmpi-${OpenMPI_FULL_VERSION})
 set(SOURCE_PATH_RELEASE ${CURRENT_BUILDTREES_DIR}/src-${TARGET_TRIPLET}-release/openmpi-${OpenMPI_FULL_VERSION})
+set(OUT_PATH_DEBUG      ${SOURCE_PATH_RELEASE}/../../make-build-${TARGET_TRIPLET}-debug)
+set(OUT_PATH_RELEASE    ${SOURCE_PATH_RELEASE}/../../make-build-${TARGET_TRIPLET}-release)
+file(MAKE_DIRECTORY     ${OUT_PATH_DEBUG})
+file(MAKE_DIRECTORY     ${OUT_PATH_RELEASE})
+
 foreach(BUILD_TYPE IN LISTS BUILD_TYPES)
     vcpkg_extract_source_archive(${ARCHIVE} ${CURRENT_BUILDTREES_DIR}/src-${TARGET_TRIPLET}-${BUILD_TYPE})
     #vcpkg_apply_patches(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src-${TARGET_TRIPLET}-${BUILD_TYPE}/openmpi-${OpenMPI_FULL_VERSION} PATCHES patch.file)
@@ -33,14 +41,9 @@ vcpkg_add_to_path(${PERL_PATH})
 set(BASH bash)
 
 if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-  ################
-  # Debug build
-  ################
   message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
-  set(OUT_PATH_DEBUG ${SOURCE_PATH_RELEASE}/../../make-build-${TARGET_TRIPLET}-debug)
-  file(MAKE_DIRECTORY ${OUT_PATH_DEBUG})
   vcpkg_execute_required_process(
-    COMMAND ${BASH} --noprofile --norc -c "${SOURCE_PATH_DEBUG}/configure" --prefix=${OUT_PATH_DEBUG} --enable-debug
+    COMMAND ${BASH} --noprofile --norc -c "${SOURCE_PATH_DEBUG}/configure --prefix=${OUT_PATH_DEBUG} --enable-debug"
     WORKING_DIRECTORY "${SOURCE_PATH_DEBUG}"
     LOGNAME "config-${TARGET_TRIPLET}-dbg"
   )
@@ -62,16 +65,11 @@ if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
 endif()
 
 if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-  ################
-  # Release build
-  ################
   message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
-  set(OUT_PATH_RELEASE ${SOURCE_PATH_RELEASE}/../../make-build-${TARGET_TRIPLET}-release)
-  file(MAKE_DIRECTORY ${OUT_PATH_RELEASE})
   vcpkg_execute_required_process(
-    COMMAND ${BASH} --noprofile --norc -c "${SOURCE_PATH_RELEASE}/configure" --prefix=${OUT_PATH_RELEASE}
-    WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
-    LOGNAME config-${TARGET_TRIPLET}-rel
+    COMMAND ${BASH} --noprofile --norc -c "${SOURCE_PATH_RELEASE}/configure --prefix=${OUT_PATH_RELEASE}"
+    WORKING_DIRECTORY "${SOURCE_PATH_RELEASE}"
+    LOGNAME "config-${TARGET_TRIPLET}-rel"
   )
   message(STATUS "Building ${TARGET_TRIPLET}-rel")
   vcpkg_execute_required_process(
@@ -89,6 +87,7 @@ if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
   file(COPY ${OUT_PATH_RELEASE}/lib DESTINATION ${CURRENT_PACKAGES_DIR})
   file(COPY ${OUT_PATH_RELEASE}/include DESTINATION ${CURRENT_PACKAGES_DIR})
   file(COPY ${OUT_PATH_RELEASE}/share DESTINATION ${CURRENT_PACKAGES_DIR})
+  file(COPY ${OUT_PATH_RELEASE}/bin DESTINATION ${CURRENT_PACKAGES_DIR})
   message(STATUS "Installing ${TARGET_TRIPLET}-rel done")
 endif()
 
