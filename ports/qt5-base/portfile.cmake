904c3eea1f70d4d02f36819dc00ad836c03375f2
JackBoosY <yuzaiyang@beyondsoft.com>
2019-07-04 00:02:22 -0700

    [qt5-base]Do not move Qt5Bootstrap.lib to tools path(Caused qt5-tools build failure).

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -214,10 +214,6 @@ if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/qtmain.lib)
         file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/Qt5Bootstrap.lib)
         file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/Qt5Bootstrap.prl)
     endif()
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/Qt5Bootstrap.lib ${CURRENT_PACKAGES_DIR}/tools/qt5/Qt5Bootstrap.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/Qt5Bootstrap.prl ${CURRENT_PACKAGES_DIR}/tools/qt5/Qt5Bootstrap.prl)
-    endif()
     #---------------------------------------------------------------------------
 endif()
 
