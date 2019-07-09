39356dec4ddff1224d1a0912387c9597382909cf
Igor Kostenko <ikostenko@perforce.com>
2019-07-09 15:07:18 +0100

    pkgconfig for zlib

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,10 +1,10 @@
 include(vcpkg_common_functions)
 
-set(VERSION 1.2.11)
+set(VERSION ${PORT_VERSION})
 
 vcpkg_download_distfile(ARCHIVE_FILE
     URLS "http://www.zlib.net/zlib-${VERSION}.tar.gz" "https://downloads.sourceforge.net/project/libpng/zlib/${VERSION}/zlib-${VERSION}.tar.gz"
-    FILENAME "zlib1211.tar.gz"
+    FILENAME "zlib-${VERSION}.tar.gz"
     SHA512 73fd3fff4adeccd4894084c15ddac89890cd10ef105dd5e1835e1e9bbb6a49ff229713bd197d203edfa17c2727700fce65a2a235f07568212d820dca88b528ae
 )
 
@@ -48,3 +48,4 @@ vcpkg_copy_pdbs()
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 
 vcpkg_test_cmake(PACKAGE_NAME ZLIB MODULE)
+vcpkg_pkgconfig(COMMON -lz)
