8c459a338acf0399f0a551ae8eb324a3a696e7c9
JackBoosY <yuzaiyang@beyondsoft.com>
2019-07-15 23:38:14 -0700

    [xmsh]Add copyright.

--- ports/xmsh/portfile.cmake
+++ ports/xmsh/portfile.cmake
@@ -36,4 +36,6 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
 
+file(INSTALL ${SOURCE_PATH}/copyright.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/xmsh RENAME copyright)
+
 vcpkg_copy_pdbs()
