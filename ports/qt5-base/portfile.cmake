314a6ea745de480a0b72fd2359d3325b7d407e11
JackBoosY <yuzaiyang@beyondsoft.com>
2019-07-16 20:03:34 -0700

    [qt5-base]Print error message if glu.h check failed.

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -87,8 +87,9 @@ if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore
     )
 
 elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    message("qt5 requires libgl1-mesa-dev and libglu1-mesa-dev, please use your distribution's package manager to install them.")
-    message("Example: \"apt-get install libgl1-mesa-dev\" and \"apt-get install libglu1-mesa-dev\"")
+    if (NOT EXISTS "/usr/include/GL/glu.h")
+        message(FATAL_ERROR "qt5 requires libgl1-mesa-dev and libglu1-mesa-dev, please use your distribution's package manager to install them.\nExample: \"apt-get install libgl1-mesa-dev\" and \"apt-get install libglu1-mesa-dev\"")
+    endif()
 
     configure_qt(
         SOURCE_PATH ${SOURCE_PATH}
