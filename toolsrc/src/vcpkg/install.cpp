618fa203c13c30bd19826988cff66481bca0562f
Curtis J Bezault <curtbezault@gmail.com>
2019-07-19 08:01:38 -0700

    [vcpkg] Portfile Settings (#7292)

--- toolsrc/src/vcpkg/install.cpp
+++ toolsrc/src/vcpkg/install.cpp
@@ -331,9 +331,8 @@ namespace vcpkg::Install
 
             auto result = [&]() -> Build::ExtendedBuildResult {
                 const auto& scfl = action.source_control_file_location.value_or_exit(VCPKG_LINE_INFO);
-                const Build::BuildPackageConfig build_config{*scfl.source_control_file,
+                const Build::BuildPackageConfig build_config{scfl,
                                                              action.spec.triplet(),
-                                                             static_cast<fs::path>(scfl.source_location),
                                                              action.build_options,
                                                              action.feature_list};
                 return Build::build_package(paths, build_config, status_db);
