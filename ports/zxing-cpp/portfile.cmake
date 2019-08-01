afa44bc48cfa331546f68c277b88622d4cbf7c17
Victor Romero <romerosanchezv@gmail.com>
2019-08-01 15:12:42 -0700

    [zxing-cpp] Revert unnecessary changes

--- ports/zxing-cpp/portfile.cmake
+++ ports/zxing-cpp/portfile.cmake
@@ -1,7 +1,3 @@
-# if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-#     message(FATAL_ERROR "The required Video IO module from OpenCV is not supported in UWP.")
-# endif()
-
 include(vcpkg_common_functions)
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
