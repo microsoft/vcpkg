bdcb4a2e7b1fa85b556672b42c183de1ce0b5ea4
Stijn Herfst <stijnherfst@outlook.com>
2019-08-02 13:52:21 +0200

    Added CMake targets

--- ports/casclib/portfile.cmake
+++ ports/casclib/portfile.cmake
@@ -10,11 +10,18 @@ vcpkg_from_github(
         ctype_for_mac.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY 
+	${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt 
+	${CMAKE_CURRENT_LIST_DIR}/casclibConfig.cmake.in
+    ${CMAKE_CURRENT_LIST_DIR}/casclibConfigVersion.cmake.in
+
+	DESTINATION ${SOURCE_PATH}
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS_DEBUG -DINSTALL_HEADERS=OFF
 )
 
 vcpkg_install_cmake()
