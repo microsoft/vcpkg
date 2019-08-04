cdf998c6c53fd8e03582a67d2b826ec49cd7b60b
Francesco Bertolaccini <francesco@bertolaccini.dev>
2019-08-04 11:07:32 +0200

    Update riffcpp

--- ports/riffcpp/portfile.cmake
+++ ports/riffcpp/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libdmusic/riffcpp
-    REF  v2.1.0
-    SHA512 bf41876b2d2305d5ec83dfb2739becf4c9d584248022123c985f5a49ccf051a53f2453b715052ae4fb96e57ab1191001572139803c7c36f37f97c6e21c59d1ba
+    REF  v2.2.0
+    SHA512 e595a74aa4b49d6948beeaddaa2c51313f006482bc88afebf5a7ece4be5c864ec798169047e913545e9a4850b7de4c0a2b6b73d4d3a56a1ada7edf946e78ceef
     HEAD_REF master
 )
 
@@ -16,6 +16,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
