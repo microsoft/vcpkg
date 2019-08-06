c9acaed977fee393f6c4ec93dd494aee64f84b8d
Victor Romero <romerosanchezv@gmail.com>
2019-08-06 01:30:10 -0700

    Do not create variables for each feature only set OUT_FEATURE_OPTIONS

--- docs/maintainers/vcpkg_check_features.md
+++ docs/maintainers/vcpkg_check_features.md
@@ -1,15 +1,15 @@
 # vcpkg_check_features
-Check if one or more features are a part of the package installation.
+Check if one or more features are a part of a package installation.
 
 ## Usage
 ```cmake
 vcpkg_check_features(
-  [OUT_EXPAND_OPTIONS <output_variable>] 
-  [CHECK_FEATURES
+  OUT_FEATURE_OPTIONS <output_variable>  
+  [FEATURES
     <feature1> <output_variable1>
     [<feature2> <output_variable2>]
     ...]
-  [UNCHECK_FEATURES
+  [INVERTED_FEATURES
     <feature3> <output_variable3>
     [<feature4> <output_variable4>]
     ...]
@@ -17,70 +17,107 @@ vcpkg_check_features(
 ```
 `vcpkg_check_features()` accepts these parameters: 
 
-* (Optional) `OUT_EXPAND_OPTIONS`:  
-  An optional output variable that will be set to contain the definitions (`-D<FEATURE_VAR>=ON|OFF`) for each ## checked ## feature.
+* `OUT_FEATURE_OPTIONS`:  
+  An output variable that will contain a list of definitions for each feature.  
+  This is a required parameter.
   
-* `CHECK_FEATURES`:  
-  A list of (feature, output variable) pairs. If a feature is specified for installation, the corresponding output 
-  variable will be set as `ON`, or `OFF` otherwise.  
-  
-  The syntax is similar to the `PROPERTIES` argument of `set_target_properties`.
+* `FEATURES`:  
+  A list of (`FEATURE_NAME`, `OPTION_NAME`) pairs.  
+  For each `FEATURE_NAME` a definition is added to `OUT_FEATURE_OPTIONS` in the form of:   
+    
+    * `-D<OPTION_NAME>=ON`, if a feature is specified for installation,
+    * `-D<OPTION_NAME>=OFF`, otherwise. 
 
-* `UNCHECK_FEATURES`:  
-  Reverse logic version of `CHECK_FEATURES`, sets the output variable to `OFF` is the feature is checked for ## installation, and `ON` otherwise.
-
-Both `CHECK_FEATURES` and `UNCHECK_FEATURES` are optional, however at least one of them must be used.
-
-When using `CHECK_FEATURES` or `UNCHECK_FEATURES` parameter, at least a pair of arguments must be provided.
-
-The output variable set in `OUT_EXPAND_OPTIONS` can be passed as a part of the `OPTIONS` argument when calling ## functions like `vcpkg_config_cmake`:
-
-```cmake
-vcpkg_check_features(OUT_EXPAND_OPTIONS port_OPTIONS)
-vcpkg_config_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        ${port_OPTIONS}
-)
-```
+* `INVERTED_FEATURES`:  
+  A list of (`FEATURE_NAME`, `OPTION_NAME`) pairs, uses reversed logic from `FEATURES`.  
+  For each `FEATURE_NAME` a definition is added to `OUT_FEATURE_OPTIONS` in the form of:   
+    
+    * `-D<OPTION_NAME>=OFF`, if a feature is specified for installation,
+    * `-D<OPTION_NAME>=ON`, otherwise. 
 
 ## Notes
 
-The following code
+The `FEATURES` name parameter can be omitted if no `INVERTED_FEATURES` are used.
 
-```cmake
-if(<feature> IN_LIST FEATURES)
-    set(<output_variable> ON)
-else()
-    set(<output_variable> OFF)
-endif()
-```
-can be replaced by
-```cmake
-vcpkg_check_features(CHECK_FEATURES <feature> <output_variable>)
-```
+At least one (`FEATURE_NAME`, `OPTION_NAME`) pair must be passed to the function call.
 
-If reverse logic is required:
+Features passed to `FEATURES` and `INVERTED_FEATURES` are not validated against duplication.  
+If the same (`FEATURE_NAME`, `OPTION_NAME`) is passed to both lists, two conflicting ## definitions are added to `OUT_FEATURE_OPTIONS`
 
-```cmake
-if(<feature> IN_LIST FEATURES)
-    set(<output_variable> OFF)
-else()
-    set(<output_variable> ON)
-endif()
-```
-
-use the `UNCHECK_FEATURES` parameter.
-```cmake
-vcpkg_check_features(UNCHECK_FEATURES non-posix ENABLE_POSIX_API)
-```
 
 ## Examples
+
+### Example 1: Regular features
+
+```cmake
+$ ./vcpkg install mimalloc[asm,secure]
+
+# ports/mimalloc/portfile.cmake
+vcpkg_check_features(OUT_FEATURE_OPTIONS mimalloc_FEATURE_OPTIONS
+  # Keyword FEATURES is optional if INVERTED_FEATURES are not used
+    asm       MI_SEE_ASM
+    override  MI_OVERRIDE
+    secure    MI_SECURE
+)
+
+vcpkg_config_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS
+    # Expands to "-DMI_SEE_ASM=ON; -DMI_OVERRIDE=OFF; -DMI_SECURE=ON"
+    ${mimalloc_FEATURE_OPTIONS}
+)
+```
+
+### Example 2: Inverted features
+
+```cmake
+$ ./vcpkg install cpprestsdk[websockets]
+
+# ports/cpprestsdk/portfile.cmake
+vcpkg_check_features(OUT_FEATURE_OPTIONS cpprestsdk_FEATURE_OPTIONS
+  INVERTED_FEATURES # <- Keyword INVERTED_FEATURES required
+    brotli      CPPREST_EXCLUDE_BROTLI
+    websockets  CPPREST_EXCLUDE_WEBSOCKETS
+)
+
+vcpkg_config_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS
+    # Expands to "-DCPPREST_EXCLUDE_BROTLI=ON; -DCPPREST_EXCLUDE_WEBSOCKETS=OFF"
+    ${cpprestsdk_FEATURE_OPTIONS}
+)
+```
+
+### Example 3: Mixed regular and inverted features
+
+```cmake
+$ ./vcpkg install rocksdb[tbb]
+
+# ports/rocksdb/portfile.cmake
+vcpkg_check_features(OUT_FEATURE_OPTIONS rocksdb_FEATURE_OPTIONS
+  FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
+    tbb   WITH_TBB
+  INVERTED_FEATURES
+    tbb   ROCKSDB_IGNORE_PACKAGE_TBB
+)
+
+vcpkg_config_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS
+    # Expands to "-DWITH_TBB=ON; -DROCKSDB_IGNORE_PACKAGE_TBB=OFF"
+    ${rocksdb_FEATURE_OPTIONS}
+)
+``` 
+
+## Examples in portfiles
+
 * [czmq](https://github.com/microsoft/vcpkg/blob/master/ports/czmq/portfile.cmake)
 * [xsimd](https://github.com/microsoft/vcpkg/blob/master/ports/xsimd/portfile.cmake)
 * [xtensor](https://github.com/microsoft/vcpkg/blob/master/ports/xtensor/portfile.cmake)
- 
+
 
 ## Source
 [scripts/cmake/vcpkg_check_features.cmake](https://github.com/Microsoft/vcpkg/blob/master/scripts/cmake/vcpkg_check_features.cmake)
--- scripts/cmake/vcpkg_check_features.cmake
+++ scripts/cmake/vcpkg_check_features.cmake
@@ -1,15 +1,15 @@
 ## # vcpkg_check_features
-## Check if one or more features are a part of the package installation.
+## Check if one or more features are a part of a package installation.
 ## 
 ## ## Usage
 ## ```cmake
 ## vcpkg_check_features(
-##   [OUT_EXPAND_OPTIONS <output_variable>] 
-##   [CHECK_FEATURES
+##   OUT_FEATURE_OPTIONS <output_variable>  
+##   [FEATURES
 ##     <feature1> <output_variable1>
 ##     [<feature2> <output_variable2>]
 ##     ...]
-##   [UNCHECK_FEATURES
+##   [INVERTED_FEATURES
 ##     <feature3> <output_variable3>
 ##     [<feature4> <output_variable4>]
 ##     ...]
@@ -17,75 +17,112 @@
 ## ```
 ## `vcpkg_check_features()` accepts these parameters: 
 ## 
-## * (Optional) `OUT_EXPAND_OPTIONS`:  
-##   An optional output variable that will be set to contain the definitions (`-D<FEATURE_VAR>=ON|OFF`) for each ## checked ## feature.
+## * `OUT_FEATURE_OPTIONS`:  
+##   An output variable that will contain a list of definitions for each feature.  
+##   This is a required parameter.
 ##   
-## * `CHECK_FEATURES`:  
-##   A list of (feature, output variable) pairs. If a feature is specified for installation, the corresponding output 
-##   variable will be set as `ON`, or `OFF` otherwise.  
-##   
-##   The syntax is similar to the `PROPERTIES` argument of `set_target_properties`.
+## * `FEATURES`:  
+##   A list of (`FEATURE_NAME`, `OPTION_NAME`) pairs.  
+##   For each `FEATURE_NAME` a definition is added to `OUT_FEATURE_OPTIONS` in the form of:   
+##     
+##     * `-D<OPTION_NAME>=ON`, if a feature is specified for installation,
+##     * `-D<OPTION_NAME>=OFF`, otherwise. 
 ## 
-## * `UNCHECK_FEATURES`:  
-##   Reverse logic version of `CHECK_FEATURES`, sets the output variable to `OFF` is the feature is checked for ## installation, and `ON` otherwise.
-## 
-## Both `CHECK_FEATURES` and `UNCHECK_FEATURES` are optional, however at least one of them must be used.
-## 
-## When using `CHECK_FEATURES` or `UNCHECK_FEATURES` parameter, at least a pair of arguments must be provided.
-## 
-## The output variable set in `OUT_EXPAND_OPTIONS` can be passed as a part of the `OPTIONS` argument when calling ## functions like `vcpkg_config_cmake`:
-## 
-## ```cmake
-## vcpkg_check_features(OUT_EXPAND_OPTIONS port_OPTIONS)
-## vcpkg_config_cmake(
-##     SOURCE_PATH ${SOURCE_PATH}
-##     PREFER_NINJA
-##     OPTIONS
-##         ${port_OPTIONS}
-## )
-## ```
+## * `INVERTED_FEATURES`:  
+##   A list of (`FEATURE_NAME`, `OPTION_NAME`) pairs, uses reversed logic from `FEATURES`.  
+##   For each `FEATURE_NAME` a definition is added to `OUT_FEATURE_OPTIONS` in the form of:   
+##     
+##     * `-D<OPTION_NAME>=OFF`, if a feature is specified for installation,
+##     * `-D<OPTION_NAME>=ON`, otherwise. 
 ## 
 ## ## Notes
 ## 
-## The following code
+## The `FEATURES` name parameter can be omitted if no `INVERTED_FEATURES` are used.
 ## 
-## ```cmake
-## if(<feature> IN_LIST FEATURES)
-##     set(<output_variable> ON)
-## else()
-##     set(<output_variable> OFF)
-## endif()
-## ```
-## can be replaced by
-## ```cmake
-## vcpkg_check_features(CHECK_FEATURES <feature> <output_variable>)
-## ```
+## At least one (`FEATURE_NAME`, `OPTION_NAME`) pair must be passed to the function call.
 ## 
-## If reverse logic is required:
+## Features passed to `FEATURES` and `INVERTED_FEATURES` are not validated against duplication.  
+## If the same (`FEATURE_NAME`, `OPTION_NAME`) is passed to both lists, two conflicting ## definitions are added to `OUT_FEATURE_OPTIONS`
 ## 
-## ```cmake
-## if(<feature> IN_LIST FEATURES)
-##     set(<output_variable> OFF)
-## else()
-##     set(<output_variable> ON)
-## endif()
-## ```
-## 
-## use the `UNCHECK_FEATURES` parameter.
-## ```cmake
-## vcpkg_check_features(UNCHECK_FEATURES non-posix ENABLE_POSIX_API)
-## ```
 ## 
 ## ## Examples
+## 
+## ### Example 1: Regular features
+## 
+## ```cmake
+## $ ./vcpkg install mimalloc[asm,secure]
+## 
+## # ports/mimalloc/portfile.cmake
+## vcpkg_check_features(OUT_FEATURE_OPTIONS mimalloc_FEATURE_OPTIONS
+##   # Keyword FEATURES is optional if INVERTED_FEATURES are not used
+##     asm       MI_SEE_ASM
+##     override  MI_OVERRIDE
+##     secure    MI_SECURE
+## )
+## 
+## vcpkg_config_cmake(
+##   SOURCE_PATH ${SOURCE_PATH}
+##   PREFER_NINJA
+##   OPTIONS
+##     # Expands to "-DMI_SEE_ASM=ON; -DMI_OVERRIDE=OFF; -DMI_SECURE=ON"
+##     ${mimalloc_FEATURE_OPTIONS}
+## )
+## ```
+## 
+## ### Example 2: Inverted features
+## 
+## ```cmake
+## $ ./vcpkg install cpprestsdk[websockets]
+## 
+## # ports/cpprestsdk/portfile.cmake
+## vcpkg_check_features(OUT_FEATURE_OPTIONS cpprestsdk_FEATURE_OPTIONS
+##   INVERTED_FEATURES # <- Keyword INVERTED_FEATURES required
+##     brotli      CPPREST_EXCLUDE_BROTLI
+##     websockets  CPPREST_EXCLUDE_WEBSOCKETS
+## )
+## 
+## vcpkg_config_cmake(
+##   SOURCE_PATH ${SOURCE_PATH}
+##   PREFER_NINJA
+##   OPTIONS
+##     # Expands to "-DCPPREST_EXCLUDE_BROTLI=ON; -DCPPREST_EXCLUDE_WEBSOCKETS=OFF"
+##     ${cpprestsdk_FEATURE_OPTIONS}
+## )
+## ```
+## 
+## ### Example 3: Mixed regular and inverted features
+## 
+## ```cmake
+## $ ./vcpkg install rocksdb[tbb]
+## 
+## # ports/rocksdb/portfile.cmake
+## vcpkg_check_features(OUT_FEATURE_OPTIONS rocksdb_FEATURE_OPTIONS
+##   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
+##     tbb   WITH_TBB
+##   INVERTED_FEATURES
+##     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
+## )
+## 
+## vcpkg_config_cmake(
+##   SOURCE_PATH ${SOURCE_PATH}
+##   PREFER_NINJA
+##   OPTIONS
+##     # Expands to "-DWITH_TBB=ON; -DROCKSDB_IGNORE_PACKAGE_TBB=OFF"
+##     ${rocksdb_FEATURE_OPTIONS}
+## )
+## ``` 
+## 
+## ## Examples in portfiles
+## 
 ## * [czmq](https://github.com/microsoft/vcpkg/blob/master/ports/czmq/portfile.cmake)
 ## * [xsimd](https://github.com/microsoft/vcpkg/blob/master/ports/xsimd/portfile.cmake)
 ## * [xtensor](https://github.com/microsoft/vcpkg/blob/master/ports/xtensor/portfile.cmake)
-##  
+## 
 function(vcpkg_check_features)
-    cmake_parse_arguments(_vcf "" "OUT_EXPAND_OPTIONS" "CHECK_FEATURES;UNCHECK_FEATURES" ${ARGN})
+    cmake_parse_arguments(_vcf "" "OUT_FEATURE_OPTIONS" "FEATURES;INVERTED_FEATURES" ${ARGN})
 
-    if (NOT DEFINED _vcf_CHECK_FEATURES AND NOT DEFINED _vcf_UNCHECK_FEATURES)
-        message(FATAL_ERROR "No features to check/uncheck are provided. You must use either or both CHECK_FEATURES and UNCHECK_FEATURES arguments.")
+    if (NOT DEFINED _vcf_OUT_FEATURE_OPTIONS)
+        message(FATAL_ERROR "OUT_FEATURE_OPTIONS is not defined")
     endif()
 
     macro(_check_features _vcf_ARGUMENT _set_if _set_else)
@@ -101,17 +138,15 @@ function(vcpkg_check_features)
             if(_vcf_IS_FEATURE_NAME_ARG)
                 set(_vcf_FEATURE_NAME ${_vcf_ARG})
                 if(NOT ${_vcf_FEATURE_NAME} IN_LIST ALL_FEATURES)
-                    message(FATAL_ERROR "Unknown feature: ${_vcf_FEATURE}")
+                    message(FATAL_ERROR "Unknown feature: ${_vcf_FEATURE_NAME}")
                 endif()
                 set(_vcf_IS_FEATURE_NAME_ARG OFF)
             else()
                 set(_vcf_FEATURE_VARIABLE ${_vcf_ARG})
                 if(${_vcf_FEATURE_NAME} IN_LIST FEATURES)
-                    set(${_vcf_FEATURE_VARIABLE} ${_set_if} PARENT_SCOPE)
-                    list(APPEND _vcf_FEATURE_OPTIONS " -D${_vcf_FEATURE_VARIABLE}=${_set_if}")
+                    list(APPEND _vcf_FEATURE_OPTIONS "-D${_vcf_FEATURE_VARIABLE}=${_set_if}")
                 else()
-                    set(${_vcf_FEATURE_VARIABLE} ${_set_else} PARENT_SCOPE)
-                    list(APPEND _vcf_FEATURE_OPTIONS " -D${_vcf_FEATURE_VARIABLE}=${_set_else}")
+                    list(APPEND _vcf_FEATURE_OPTIONS "-D${_vcf_FEATURE_VARIABLE}=${_set_else}")
                 endif()
                 set(_vcf_IS_FEATURE_NAME_ARG ON)
             endif()
@@ -119,9 +154,14 @@ function(vcpkg_check_features)
     endmacro()
 
     set(_vcf_FEATURE_OPTIONS)
-    _check_features(_vcf_CHECK_FEATURES ON OFF)
-    _check_features(_vcf_UNCHECK_FEATURES OFF ON)
-    if (DEFINED _vcf_OUT_EXPAND_OPTIONS)
-        set(${_vcf_OUT_EXPAND_OPTIONS} "${_vcf_FEATURE_OPTIONS}" PARENT_SCOPE)
+
+    if (DEFINED _vcf_FEATURES OR DEFINED _vcf_INVERTED_FEATURES)
+        _check_features(_vcf_FEATURES ON OFF)
+        _check_features(_vcf_INVERTED_FEATURES OFF ON)
+    else() 
+        # Skip the first two parameters that correspond to OUT_FEATURE_OPTIONS and its value 
+        list(SUBLIST ARGN 2 -1 _vcf_ARGN)
+        _check_features(_vcf_ARGN ON OFF)
     endif()
+    set(${_vcf_OUT_FEATURE_OPTIONS} "${_vcf_FEATURE_OPTIONS}" PARENT_SCOPE)
 endfunction()
