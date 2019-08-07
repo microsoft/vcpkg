a2f5df9c8526f0be9db24989b3d269a39ad0f54a
Daniel <51385773+dan-shaw@users.noreply.github.com>
2019-08-07 15:46:14 -0700

    [stormlib] update cmakelists

--- ports/stormlib/portfile.cmake
+++ ports/stormlib/portfile.cmake
@@ -9,10 +9,7 @@ vcpkg_from_github(
 )
 
 file(COPY 
-    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt 
-    ${CMAKE_CURRENT_LIST_DIR}/stormlibConfig.cmake.in
-    ${CMAKE_CURRENT_LIST_DIR}/stormlibConfigVersion.cmake.in
-
+    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
     DESTINATION ${SOURCE_PATH}
 )
 
