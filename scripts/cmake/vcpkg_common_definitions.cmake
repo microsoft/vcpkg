6eaf7f0f977e62b07515cb17676dcf5d383d357d
Alexander Neumann <alexander.neumann@hamburg.de>
2019-08-08 14:18:47 +0200

    add definition for host specific path separators

--- scripts/cmake/vcpkg_common_definitions.cmake
+++ scripts/cmake/vcpkg_common_definitions.cmake
@@ -1,3 +1,4 @@
+#Helper variable to identify the Target system. VCPKG_TARGET_IS_<targetname>
 if (NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
     set(VCPKG_TARGET_IS_WINDOWS 1)
     if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
@@ -11,4 +12,13 @@ elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Android")
     set(VCPKG_TARGET_IS_ANDROID 1)
 elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
     set(VCPKG_TARGET_IS_FREEBSD 1)
+endif()
+
+#Helper variable to identify the host path seperator. 
+if(WIN32)
+    set(VCPKG_HOST_PATH_SEPARATOR ";")
+    set(VCPKG_HOST_PATH_SEPARATOR_ESCAPED "\\;") #sometimes needed to differentiate between the cmake list separator
+elseif(UNIX)
+    set(VCPKG_HOST_PATH_SEPARATOR ":")
+    set(VCPKG_HOST_PATH_SEPARATOR_ESCAPED ":")
 endif()
