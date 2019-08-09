cdc51c3246ca7872e88a5d32cfb71830a44fbd6d
Robert Schumacher <roschuma@microsoft.com>
2019-08-09 15:41:02 -0700

    [bitsery] Fix indentation

--- ports/bitsery/portfile.cmake
+++ ports/bitsery/portfile.cmake
@@ -3,7 +3,7 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO fraillt/bitsery
- REF f85dff74151530fb05241877e3c80afab5891467
+	REF f85dff74151530fb05241877e3c80afab5891467
 	SHA512 27dc79e80224f1429fccbbff75d9562e19db60a50567cb66c7999587cbf4a0b8344e3865ec804c63d05e041377dcc30de6fdc5ddfb99c779894ddd811f0d4a71
 	HEAD_REF master
 	PATCHES fix-install-paths.patch
