bca31831f1d868d5cd6e4c7b5fa1dbe89ff10b9c
unknown <mohammadi.ehsan1994@gmail.com>
2019-08-09 15:15:55 +0430

    Add port nsync

---
+++ ports/nsync/portfile.cmake
@@ -0,0 +1,19 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/nsync
+    REF 1.21.0
+    SHA512 39df3820d22f0c1c4dd0322601e82cb1feacc811ff3e5269c1339254fc87514046492a8565d634613b52459e8953e814786632c6d382a4d3f5e0cbaf25af09ad
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
