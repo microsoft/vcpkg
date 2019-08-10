aa0c1f962efc92ce106d1942a442df34655d1ce2
Elijah Insua <tmpvar@gmail.com>
2019-08-10 13:25:07 -0700

    [openvdb] add barebones usage file

--- ports/openvdb/portfile.cmake
+++ ports/openvdb/portfile.cmake
@@ -71,6 +71,8 @@ endif()
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/openvdb/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/openvdb RENAME copyright)
 
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/openvdb)
+
 # Handle FindOpenVDB
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/FindOpenVDB.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
