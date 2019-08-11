de4c394593b7aa31c631a7e73024c15f2db01fc6
Victor Romero <romerosanchezv@gmail.com>
2019-08-11 01:43:38 -0700

    [depend-info] Follow same rules as vcpkg install

--- toolsrc/src/vcpkg/commands.dependinfo.cpp
+++ toolsrc/src/vcpkg/commands.dependinfo.cpp
@@ -5,14 +5,19 @@
 #include <vcpkg/base/util.h>
 #include <vcpkg/commands.h>
 #include <vcpkg/help.h>
-#include <vcpkg/paragraphs.h>
+#include <vcpkg/input.h>
+#include <vcpkg/install.h>
 #include <vcpkg/packagespec.h>
+#include <vcpkg/paragraphs.h>
 
-#include <vector>
 #include <memory>
 #include <vcpkg/dependencies.h>
+#include <vector>
 
+using vcpkg::Dependencies::AnyAction;
+using vcpkg::Dependencies::create_feature_install_plan;
 using vcpkg::Dependencies::PathsPortFileProvider;
+using vcpkg::Dependencies::InstallPlanAction;
 
 namespace vcpkg::Commands::DependInfo
 {
@@ -35,13 +40,6 @@ namespace vcpkg::Commands::DependInfo
         nullptr,
     };
 
-    std::string replace_dashes_with_underscore(const std::string& input)
-    {
-        std::string output = input;
-        std::replace(output.begin(), output.end(), '-', '_');
-        return output;
-    }
-
     std::string create_dot_as_string(const std::vector<const SourceControlFile*>& source_control_files)
     {
         int empty_node_count = 0;
@@ -58,11 +56,11 @@ namespace vcpkg::Commands::DependInfo
                 continue;
             }
 
-            const std::string name = replace_dashes_with_underscore(source_paragraph.name);
+            const std::string name = Strings::replace_all(std::string{source_paragraph.name}, "-", "_");
             s.append(Strings::format("%s;", name));
             for (const Dependency& d : source_paragraph.depends)
             {
-                const std::string dependency_name = replace_dashes_with_underscore(d.depend.name);
+                const std::string dependency_name = Strings::replace_all(std::string{d.depend.name}, "-", "_");
                 s.append(Strings::format("%s -> %s;", name, dependency_name));
             }
         }
@@ -129,122 +127,56 @@ namespace vcpkg::Commands::DependInfo
         return "";
     }
 
-    void build_dependencies_list(std::set<std::string>& packages_to_keep,
-                                 const std::string& requested_package,
-                                 const std::vector<const SourceControlFile*>& source_control_files,
-                                 const std::unordered_set<std::string>& switches)
-    {
-        auto maybe_requested_spec = ParsedSpecifier::from_string(requested_package);
-        // TODO: move this check to the top-level invocation of this function since
-        // argument `requested_package` shall always be valid in inner-level invocation.
-        if (!maybe_requested_spec.has_value())
-        {
-            System::print2(System::Color::warning,
-                           "'",
-                           requested_package,
-                           "' is not a valid package specifier: ",
-                           vcpkg::to_string(maybe_requested_spec.error()),
-                           "\n");
-            return;
-        }
-        auto requested_spec = maybe_requested_spec.get();
-
-        const auto source_control_file =
-            Util::find_if(source_control_files, [&requested_spec](const auto& source_control_file) {
-                return source_control_file->core_paragraph->name == requested_spec->name;
-            });
-
-        if (source_control_file != source_control_files.end())
-        {
-            const auto new_package = packages_to_keep.insert(requested_spec->name).second;
-
-            if (new_package && !Util::Sets::contains(switches, OPTION_NO_RECURSE))
-            {
-                for (const auto& dependency : (*source_control_file)->core_paragraph->depends)
-                {
-                    build_dependencies_list(packages_to_keep, dependency.depend.name, source_control_files, switches);
-                }
-
-                // Collect features with `*` considered
-                std::set<const FeatureParagraph*> collected_features;
-                for (const auto& requested_feature_name : requested_spec->features)
-                {
-                    if (requested_feature_name == "*")
-                    {
-                        for (auto &&feature_paragraph : (*source_control_file)->feature_paragraphs)
-                        {
-                            collected_features.insert(std::addressof(Util::as_const(*feature_paragraph)));
-                        }
-                        continue;
-                    }
-                    auto maybe_feature = (*source_control_file)->find_feature(requested_feature_name);
-                    if (auto &&feature_paragraph = maybe_feature.get())
-                    {
-                        collected_features.insert(std::addressof(Util::as_const(*feature_paragraph)));
-                    }
-                    else
-                    {
-                        System::print2(System::Color::warning,
-                                       "dependency '",
-                                       requested_feature_name,
-                                       "' of package '",
-                                       requested_spec->name,
-                                       "' does not exist\n");
-                        continue;
-                    }
-                }
-                for (auto feature_paragraph : collected_features)
-                {
-                    for (const auto& dependency : feature_paragraph->depends)
-                    {
-                        build_dependencies_list(packages_to_keep, dependency.depend.name, source_control_files, switches);
-                    }
-                }
-            }
-        }
-        else
-        {
-            System::print2(System::Color::warning, "package '", requested_package, "' does not exist\n");
-        }
-    }
-
-    void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths)
+    void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths, const Triplet& default_triplet)
     {
         const ParsedArguments options = args.parse_arguments(COMMAND_STRUCTURE);
 
-        // TODO: Optimize implementation, current implementation needs to load all ports from disk which is too slow.
-        PathsPortFileProvider provider(paths, args.overlay_ports.get());
-        auto source_control_files = Util::fmap(provider.load_all_control_files(), [](auto&& scfl) -> const SourceControlFile * {
-            return scfl->source_control_file.get();
+        const std::vector<FullPackageSpec> specs = Util::fmap(args.command_arguments, [&](auto&& arg) {
+            return Input::check_and_get_full_package_spec(
+                std::string{arg}, default_triplet, COMMAND_STRUCTURE.example_text);
         });
 
-        if (args.command_arguments.size() >= 1)
+        for (auto&& spec : specs)
         {
-            std::set<std::string> packages_to_keep;
-            for (const auto& requested_package : args.command_arguments)
-            {
-                build_dependencies_list(packages_to_keep, requested_package, source_control_files, options.switches);
-            }
-
-            Util::erase_remove_if(source_control_files, [&packages_to_keep](const auto& source_control_file) {
-                return !Util::Sets::contains(packages_to_keep, source_control_file->core_paragraph->name);
-            });
+            Input::check_triplet(spec.package_spec.triplet(), paths);
         }
 
-        if (Util::Sets::contains(options.switches, OPTION_DOT) || Util::Sets::contains(options.switches, OPTION_DGML))
+        PathsPortFileProvider provider(paths, args.overlay_ports.get());
+
+        // By passing an empty status_db, we should get a plan containing all dependencies.
+        // All actions in the plan should be install actions, as there's no installed packages to remove.
+        StatusParagraphs status_db;
+        std::vector<AnyAction> action_plan =
+            create_feature_install_plan(provider, FullPackageSpec::to_feature_specs(specs), status_db);
+        std::vector<const InstallPlanAction*> install_actions =
+            Util::fmap(action_plan, [&](const AnyAction& action) {
+                if (auto install_action = action.install_action.get())
+                {
+                    return install_action;
+                }
+                Checks::exit_with_message(VCPKG_LINE_INFO, "Only install actions should exist in the plan");
+            });
+
+        /*if (Util::Sets::contains(options.switches, OPTION_DOT) || Util::Sets::contains(options.switches, OPTION_DGML))
         {
             const std::string graph_as_string = create_graph_as_string(options.switches, source_control_files);
             System::print2(graph_as_string, '\n');
             Checks::exit_success(VCPKG_LINE_INFO);
-        }
+        }*/
 
-        for (auto&& source_control_file : source_control_files)
+        for (auto* install_action : install_actions)
         {
-            const SourceParagraph& source_paragraph = *source_control_file->core_paragraph.get();
-            const auto s = Strings::join(", ", source_paragraph.depends, [](const Dependency& d) { return d.name(); });
-            System::print2(source_paragraph.name, ": ", s, "\n");
-        }
+            Checks::check_exit(VCPKG_LINE_INFO, install_action != nullptr);
+            const std::string portname = install_action->spec.name();
+            const std::string features = Strings::join(", ", install_action->feature_list);
 
+            const std::vector<std::string> deps = 
+                Util::fmap(install_action->computed_dependencies, 
+                    [](const PackageSpec& spec) -> std::string { return spec.name(); });
+            const std::string dependencies = Strings::join(", ", deps);
+
+            System::printf("%s[%s]: %s\n", portname, features, dependencies);
+        }
         Checks::exit_success(VCPKG_LINE_INFO);
     }
 }
