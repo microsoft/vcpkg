9949b1ce3eba1b8f6fdc8b1af323bbe1666e13d8
Alexander Neumann <alexander.neumann@hamburg.de>
2019-08-14 01:29:20 +0200

    cleanup

--- ports/qt5-declarative/portfile.cmake
+++ ports/qt5-declarative/portfile.cmake
@@ -8,13 +8,4 @@ qt_modular_library(
   953b0dac76b73a7a21b393ab88718da12d77dfc688dc07c55c96ea1658bc14acd9097bef60df4a95d2923d3fb1e02b46499c032aa53844d4fd344b0037514671
   )
 
-file(
-  REMOVE_RECURSE
-  ${CURRENT_PACKAGES_DIR}/tools/qt5-declarative/plugins/platforminputcontexts)
-
-# if (NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-# set(qt5decpath ${CURRENT_PACKAGES_DIR}/share/qt5/debug/mkspecs/modules/qt_lib_
-# qmldevtools_private.pri) file(READ "${qt5decpath}" _contents) string(REPLACE
-# [[QT.qmldevtools_private.libs = $$QT_MODULE_HOST_LIB_BASE]]
-# [[QT.qmldevtools_private.libs = $$QT_MODULE_LIB_BASE]]  _contents
-# "${_contents}") file(WRITE "${qt5decpath}" "${_contents}") endif()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/tools/qt5-declarative/plugins/platforminputcontexts)
