cdcb4eaaa8a708ff7bf946809571e5752e884d4c
Stijn Herfst <stijnherfst@outlook.com>
2019-08-14 11:09:22 +0200

    fixed static build

--- ports/qt-advanced-docking-system/portfile.cmake
+++ ports/qt-advanced-docking-system/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO githubuser0xFFFF/Qt-Advanced-Docking-System
-    REF 6cc7d7983201596a3f579adbe09417fe8c73c409
-    SHA512 fc508ffb69a265ac52a9be72854140717e7c61925c7275e44fd2f76d0df03637b8d7fd61b7ce86e03fa1e9bf0a0e4a9c2ed9477b9d758a3b0e4760839288e431
+    REF a2b07fd97f0fac63fd7a0ed7b1eb0692b3efab71
+    SHA512 a44babd6100f299aea7fcf2d730874e204151ce363e1f58a2be938f70d28b07f3cb39adfbf46527fdacc3b12a630e7d97851e4a6fcd04e750a007ee06d06d3b5
     HEAD_REF master
     PATCHES
         config_changes.patch
