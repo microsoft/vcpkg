55d170f59d04891d87654fd981493d4301d9c7ed
Alexander Neumann <alexander.neumann@hamburg.de>
2019-08-14 16:38:10 +0200

    fix qt5-script regression

--- ports/qt5-script/portfile.cmake
+++ ports/qt5-script/portfile.cmake
@@ -1,5 +1,4 @@
-
-include(vcpkg_common_functions)
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 include(${CURRENT_INSTALLED_DIR}/share/qt5modularscripts/qt_modular_library.cmake)
 
 qt_modular_library(qtscript 6d3b802c7cdfb270318b9210df4ad9dbd2d29f485d9194051dbf278a48c25cc5c6311adeaf56b4fe2a7746dbc10234544e75e61a2e9c86f653aed0a92e07e717)
