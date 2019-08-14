64661d984d5437236f46fac64c6169022a595b22
Alexander Neumann <alexander.neumann@hamburg.de>
2019-08-14 13:42:47 +0200

    since i am at it also test 5.13 in x86-windows and use x64-windows for 5.12.4

--- triplets/x86-windows.cmake
+++ triplets/x86-windows.cmake
@@ -1,3 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
+if(NOT ${PORT} MATCHES (qt5|zlib|libjpeg-turbo|libpng|freetype|pcre2|harfbuzz|sqlite3|libpq|double-conversion|openssl)
+	message(FATAL_ERROR "Qt5 5.13 test build. Don't build everything stupid CI and just fail early!")
+endif()
+set(VCPKG_QT_MAJOR_MINOR_VER 5.13)
+set(VCPKG_QT_PATCH_VER 0)
