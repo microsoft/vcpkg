c97de4c858e80ef15c1537bfdb66e63ce2360e4f
Alexander Neumann <alexander.neumann@hamburg.de>
2019-08-16 08:14:17 +0200

    Revert "allow uwp ci"
    This reverts commit 280e2d7e12eb79673fa238ce179733d29d77a77f.

--- triplets/x86-windows.cmake
+++ triplets/x86-windows.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-if(NOT ${PORT} MATCHES "(protobuf|boost-build|qt5|zlib|libjpeg-turbo|libpng|freetype|pcre2|harfbuzz|sqlite3|libpq|double-conversion|openssl)" )
+if(NOT ${PORT} MATCHES "(qt5|zlib|libjpeg-turbo|libpng|freetype|pcre2|harfbuzz|sqlite3|libpq|double-conversion|openssl)" )
 	message(FATAL_ERROR "Qt5 5.13 test build. Don't build everything stupid CI and just fail early!")
 endif()
 set(VCPKG_QT_MAJOR_MINOR_VER 5.13)
