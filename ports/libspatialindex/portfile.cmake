d3544296cc8e99cb4f48c4202bb2325e15e8bd54
Long Huan <8551701+longhuan2018@users.noreply.github.com>
2019-08-20 17:10:30 +0800

    support static build

--- ports/libspatialindex/portfile.cmake
+++ ports/libspatialindex/portfile.cmake
@@ -1,18 +1,18 @@
 include(vcpkg_common_functions)
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libspatialindex/libspatialindex
     REF 1.9.0
     SHA512   368537e9bfe52db96486a1febfabe035f9f7714fd1cb50450e3ab89d51c5ffffb0e2ea219e08bee34f772ba9813a3a7f9e63d8b8946887ce83811ef68d17d1cc
     HEAD_REF master
+	PATCHES
+        static.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-	OPTIONS -DCMAKE_DEBUG_POSTFIX=d
+	OPTIONS -DCMAKE_DEBUG_POSTFIX=d -DSIDX_BUILD_TESTS:BOOL=OFF
 )
 
 vcpkg_install_cmake()
