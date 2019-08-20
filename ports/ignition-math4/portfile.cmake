183061a9410afad6ccedc81f3874b597ee92065b
Silvio Traversaro <silvio.traversaro@iit.it>
2019-08-20 14:35:14 +0200

    Add ignition-math4 port

---
+++ ports/ignition-math4/portfile.cmake
@@ -0,0 +1,7 @@
+include(vcpkg_common_functions)
+
+include(${CURRENT_INSTALLED_DIR}/share/ignitionmodularscripts/ignition_modular_library.cmake)
+
+ignition_modular_library(NAME math
+                         VERSION "4.0.0"
+                         SHA512 09023b559e7e544e628131189f3a7f57a9b73868f66f81e5ce1a353092940949973e1753f18ead8f655ad88c0e1d1bf51bbf63163760694aab7a97a4c0f6d519)
