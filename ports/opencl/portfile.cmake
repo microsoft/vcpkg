da3db910a38a28dce88ec9eff1ca4b6d06d6c400
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2019-08-22 15:23:50 +0200

    OpenCL builds on linux

--- ports/opencl/portfile.cmake
+++ ports/opencl/portfile.cmake
@@ -1,13 +1,13 @@
 include(vcpkg_common_functions)
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+#vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 # OpenCL C headers
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/OpenCL-Headers
-    REF a749dc6b85b3dcb57a54b17fcbf279c4f7198648
-    SHA512 dffa1a26641fcb4fa8040971c603deeae111d0615d18e6205e35fe4cb1c19b4b0f5b331e9de28f6dc6aa21d9a549bc707e16768bb1cc0f5b6cfaec918e6ac465
+    REF 0d5f18c6e7196863bc1557a693f1509adfcee056
+    SHA512 7e8fa6c8e73c660d8e9e31ddea3bfef887ed827fc21a1da559bde9dd4af6c52a91f609401bb718528b5c96d21e4c01aee7b8027bdf3dec4b0aa326270788a4b0
     HEAD_REF master
 )
 
@@ -21,8 +21,8 @@ file(INSTALL
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/OpenCL-CLHPP
-    REF bbccc50adcd667d4f7c58960b586bdc60c13f7f0
-    SHA512 1bfa4461d339586bf9f32498237631f20740e3cff29242b37db96ce98e9b3fad20878c983ddc37a21ff54c4025c79e19c1c6764f71d29376498d47d569e81784
+    REF d62a02090625655e5b2d791d6a58618b043c989c
+    SHA512 837bbe914931d2f18a468f21634dbd4d088eda0a2f22eea23304c0323b9ee064c3ee76db7ebf28ba67fbe07c44129241f8dca62512d89bc7a6b35c2b4b316ed7
     HEAD_REF master
 )
 
@@ -49,26 +49,26 @@ message(STATUS "Generating OpenCL C++ headers done")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/OpenCL-ICD-Loader
-    REF b342ff7b7f70a4b3f2cfc53215af8fa20adc3d86
-    SHA512 6e7620a4a971fb292fd4079a2c011c7b31f553ec14bf5503462ac3fe6769931acf549e9c8a2d5aafd6307dc246432006ecf65e5d8dcbc14235bec72cc858b618
+    REF e6e30ab9c7a61c171cf68d2e7f5c0ce28e2a4eae
+    SHA512 f3563c0a4c094d3795d8386ec0db41189d350ab8136d80ae5de611ee3db87fbb0ab851bad2b33e111eddf135add5dbfef77d96979473ca5a23c036608d443378
     HEAD_REF master
 )
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/do-not-enforce-dynamic-library.patch"
-)
+#vcpkg_apply_patches(
+#    SOURCE_PATH ${SOURCE_PATH}
+#    PATCHES "${CMAKE_CURRENT_LIST_DIR}/do-not-enforce-dynamic-library.patch"
+#)
 
-if(NOT VCPKG_CMAKE_SYSTEM_NAME)
-    message(STATUS "Building the ICD loader as a static library on Windows is not supported. Building as DLLs instead.")
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
+#if(NOT VCPKG_CMAKE_SYSTEM_NAME)
+#    message(STATUS "Building the ICD loader as a static library on Windows is not supported. Building as DLLs instead.")
+#    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+#endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DOPENCL_INCLUDE_DIRS=${CURRENT_PACKAGES_DIR}/include
+        -DOPENCL_ICD_LOADER_HEADERS_DIR=${CURRENT_PACKAGES_DIR}/include
 )
 
 vcpkg_build_cmake(TARGET OpenCL)
@@ -101,7 +101,7 @@ if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
 endif()
 
 file(INSTALL
-        "${SOURCE_PATH}/LICENSE.txt"
+        "${SOURCE_PATH}/LICENSE"
     DESTINATION
         ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright
 )
