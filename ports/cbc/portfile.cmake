49e0da07231be0bd0a616eb30dc105aa4b4f1229
NancyLi1013 <lirui09@beyondsoft.com>
2019-08-22 23:28:46 -0700

    [cbc] Add new port

---
+++ ports/cbc/portfile.cmake
@@ -0,0 +1,28 @@
+include(vcpkg_common_functions)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO coin-or/Cbc
+    REF releases/2.10.3
+    SHA512 4e645d85ed2481090d9fff0f5b43c1538935d38177e7efab6ca90cc5aa53e6dead72bef3b9ce6652f81fa57803254d22ce6316894ee8f2ace3e54c11d7e2ffe0
+    PATCHES fix-c1083-error.patch
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
