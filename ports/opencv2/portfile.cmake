f59e0fcf3d42972c132b4824cb8df186e093b81e
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-08-24 17:37:58 +0200

    [OpenCV2] re-enable parallel build also with CUDA

--- ports/opencv2/portfile.cmake
+++ ports/opencv2/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 if (EXISTS "${CURRENT_INSTALLED_DIR}/share/opencv4")
   message(FATAL_ERROR "OpenCV 4 is installed, please uninstall and try again:\n    vcpkg remove opencv4")
 endif()
@@ -8,19 +6,21 @@ if (EXISTS "${CURRENT_INSTALLED_DIR}/share/opencv3")
   message(FATAL_ERROR "OpenCV 3 is installed, please uninstall and try again:\n    vcpkg remove opencv3")
 endif()
 
+include(vcpkg_common_functions)
+
 if (VCPKG_TARGET_IS_UWP)
   # - opengl feature is broken on UWP
   # - jasper and openexr are not available on UWP due to missing dependencies
-  # - opencv2 code itself fails if previous conditions are avoided
+  # - opencv2 code itself fails even if previous conditions are avoided
   message(FATAL_ERROR "${PORT} doesn't support UWP")
 endif()
 
-set(OPENCV_PORT_VERSION "2.4.13.7")
+set(OPENCV_VERSION "2.4.13.7")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO opencv/opencv
-    REF ${OPENCV_PORT_VERSION}
+    REF ${OPENCV_VERSION}
     SHA512 de7d24ac7ed78ac14673011cbecc477cae688b74222a972e553c95a557b5cb8e5913f97db525421d6a72af30998ca300112fa0b285daed65f65832eb2cf7241a
     HEAD_REF master
     PATCHES
@@ -31,6 +31,8 @@ vcpkg_from_github(
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+ "cuda"     WITH_CUDA
+ "cuda"     WITH_CUBLAS
  "eigen"    WITH_EIGEN
  "ffmpeg"   WITH_FFMPEG
  "jasper"   WITH_JASPER
@@ -40,66 +42,71 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
  "png"      WITH_PNG
  "qt"       WITH_QT
  "tiff"     WITH_TIFF
- "vtk"      WITH_VTK
  "world"    BUILD_opencv_world
 )
 
-set(WITH_CUDA OFF)
-if("cuda" IN_LIST FEATURES)
-  set(WITH_CUDA ON)
-  set(FLAG_DISABLE_PARALLEL "DISABLE_PARALLEL") #otherwise CUDA fails to build!
+set(WITH_VTK OFF)
+if("vtk" IN_LIST FEATURES)
+  set(WITH_VTK ON)
 endif()
 
-if(NOT VCPKG_CMAKE_SYSTEM_NAME MATCHES "Linux" AND NOT VCPKG_CMAKE_SYSTEM_NAME MATCHES "Darwin")
-  set(WITH_MSMF ON)
-  if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    set(WITH_MSMF OFF)
-  endif()
-else()
+set(WITH_MSMF ON)
+if(NOT VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
   set(WITH_MSMF OFF)
+endif()
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+  if (WITH_VTK)
+    message(WARNING "VTK is currently unsupported in this build configuration, turning it off")
+    set(WITH_VTK OFF)
+  endif()
+endif()
+
+if("ffmpeg" IN_LIST FEATURES)
+  if(VCPKG_TARGET_IS_UWP)
+    set(VCPKG_C_FLAGS "/sdl- ${VCPKG_C_FLAGS}")
+    set(VCPKG_CXX_FLAGS "/sdl- ${VCPKG_CXX_FLAGS}")
+  endif()
 endif()
 
 vcpkg_configure_cmake(
     PREFER_NINJA
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        ###### OpenCV options
+        ###### ocv_options
+        -DCMAKE_DEBUG_POSTFIX=d
+        # Do not build docs/examples
         -DBUILD_DOCS=OFF
         -DBUILD_EXAMPLES=OFF
-        -DBUILD_TESTS=OFF
-        -DBUILD_PERF_TESTS=OFF
-        -DBUILD_PACKAGE=OFF
-        -DBUILD_WITH_DEBUG_INFO=ON
-        -DCMAKE_DEBUG_POSTFIX=d
-        ###### CMake options
-        -DCMAKE_DISABLE_FIND_PACKAGE_JNI=ON
-        # Do not build integrated libraries, use external ones
-        -DBUILD_ZLIB=OFF
-        -DBUILD_TIFF=OFF
+        ###### Disable build 3rd party libs
         -DBUILD_JASPER=OFF
         -DBUILD_JPEG=OFF
-        -DBUILD_PNG=OFF
         -DBUILD_OPENEXR=OFF
+        -DBUILD_PNG=OFF
+        -DBUILD_TIFF=OFF
         -DBUILD_TBB=OFF
-        # Select which OpenCV modules should be built
+        -DBUILD_ZLIB=OFF
+        ###### OpenCV Build components
         -DBUILD_opencv_apps=OFF
-        -DBUILD_opencv_python=OFF
-        -DBUILD_opencv_world=${BUILD_opencv_world}
-        # WITH
+        -DBUILD_PACKAGE=OFF
+        -DBUILD_PERF_TESTS=OFF
+        -DBUILD_TESTS=OFF
+        -DBUILD_WITH_DEBUG_INFO=ON
+        # CMAKE
+        -DCMAKE_DISABLE_FIND_PACKAGE_JNI=ON
+        ###### customized properties
+        ## Options from vcpkg_check_features()
         ${FEATURE_OPTIONS}
-        -DWITH_CUDA=${WITH_CUDA}
-        -DWITH_CUBLAS=${WITH_CUDA}
         -DWITH_1394=OFF
         -DWITH_IPP=OFF
         -DWITH_LAPACK=OFF
         -DWITH_MSMF=${WITH_MSMF}
         -DWITH_OPENCLAMDBLAS=OFF
         -DWITH_OPENMP=OFF
-        -DWITH_PROTOBUF=ON
         -DWITH_ZLIB=ON
 )
 
-vcpkg_install_cmake(${FLAG_DISABLE_PARALLEL})
+vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH "share/opencv" TARGET_PATH "share/opencv")
 vcpkg_copy_pdbs()
 
@@ -107,30 +114,7 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(READ ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules.cmake OPENCV_MODULES)
   string(REPLACE "set(CMAKE_IMPORT_FILE_VERSION 1)"
                  "set(CMAKE_IMPORT_FILE_VERSION 1)
-find_package(TIFF REQUIRED)
-find_package(Protobuf REQUIRED)
-if(Protobuf_FOUND)
-  if(TARGET protobuf::libprotobuf)
-    add_library(libprotobuf INTERFACE IMPORTED)
-    set_target_properties(libprotobuf PROPERTIES
-      INTERFACE_LINK_LIBRARIES protobuf::libprotobuf
-    )
-  else()
-    add_library(libprotobuf UNKNOWN IMPORTED)
-    set_target_properties(libprotobuf PROPERTIES
-      IMPORTED_LOCATION \"${Protobuf_LIBRARY}\"
-      INTERFACE_INCLUDE_DIRECTORIES \"${Protobuf_INCLUDE_DIR}\"
-      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES \"${Protobuf_INCLUDE_DIR}\"
-    )
-  endif()
-endif()
-find_package(HDF5 QUIET)
-find_package(Freetype QUIET)
-find_package(Ogre QUIET)
-find_package(gflags QUIET)
-find_package(Ceres QUIET)
-find_package(VTK QUIET)
-find_package(GDCM QUIET)" OPENCV_MODULES "${OPENCV_MODULES}")
+find_package(TIFF QUIET)" OPENCV_MODULES "${OPENCV_MODULES}")
 
   file(WRITE ${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules.cmake "${OPENCV_MODULES}")
 
