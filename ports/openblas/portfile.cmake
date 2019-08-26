0a0c33ca6b721f6919f56036acc0fc5d261be9a4
JackBoosY <yuzaiyang@beyondsoft.com>
2019-08-25 22:49:28 -0700

    [openblas]Enable x86 build.

--- ports/openblas/portfile.cmake
+++ ports/openblas/portfile.cmake
@@ -1,9 +1,5 @@
 include(vcpkg_common_functions)
 
-if(NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    message(FATAL_ERROR "openblas can only be built for x64 currently")
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xianyi/OpenBLAS
