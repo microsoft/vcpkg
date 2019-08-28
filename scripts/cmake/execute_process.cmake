8bb2b5f72157d2fc1b2842829ba06e9fef78f09f
Victor Romero <romerosanchezv@gmail.com>
2019-08-28 13:25:15 -0700

    Fix calls to _execute_process() when not in Download Mode

--- scripts/cmake/execute_process.cmake
+++ scripts/cmake/execute_process.cmake
@@ -6,14 +6,15 @@
 ## In order to execute a process in Download Mode call `_execute_process()` instead.
 ##
 if (NOT DEFINED OVERRIDEN_EXECUTE_PROCESS)
-set(OVERRIDEN_EXECUTE_PROCESS ON)
+  set(OVERRIDEN_EXECUTE_PROCESS ON)
 
-function(execute_process)
-        message(FATAL_ERROR 
-[[
-  This command cannot be executed in Download Mode.
-  Halting portfile execution.
-]])
-endfunction()
-
+  if (DEFINED VCPKG_DOWNLOAD_MODE)
+    function(execute_process)
+      message(FATAL_ERROR "This command cannot be executed in Download Mode.\nHalting portfile execution.\n")
+    endfunction()
+  else()
+    function(execute_process)
+      _execute_process(${ARGV})
+    endfunction()
+  endif()
 endif()
