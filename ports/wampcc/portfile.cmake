7f88f82cdd83531a8da771c5ad0445c61e5a42b0
Sergey Lukashevich <sergey.lukashevi4@gmail.com>
2019-08-28 22:47:22 +0300

    Allow shared build for non-Windows systems; Add utils and tools features; Add messages about broken Windows build;

--- ports/wampcc/portfile.cmake
+++ ports/wampcc/portfile.cmake
@@ -1,9 +1,10 @@
 include(vcpkg_common_functions)
 
-# Shared build is broken under Windows. See https://github.com/darrenjs/wampcc/issues/57
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if (VCPKG_TARGET_IS_WINDOWS)
+    message("Shared build is broken under Windows. See https://github.com/darrenjs/wampcc/issues/57")
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
-# Check architecture:
 if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
     set(BUILD_ARCH "Win32")
 elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
@@ -25,30 +26,31 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-if(VCPKG_CRT_LINKAGE STREQUAL static)
-    set(USE_STATIC_RUNTIME ON)
+# Utils build is broken under Windows
+if ("utils" IN_LIST FEATURES)
+    if (VCPKG_TARGET_IS_WINDOWS)
+        message(FATAL_ERROR "'utils' build is broken under Windows")
+    endif()
+
+    set(ENABLE_UTILS ON)
 else()
-    set(USE_STATIC_RUNTIME OFF)
+    set(ENABLE_UTILS OFF)
 endif()
 
-if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
-    set(USE_SHARED_RUNTIME ON)
+if ("examples" IN_LIST FEATURES)
+    set(ENABLE_EXAMPLES ON)
 else()
-    set(USE_SHARED_RUNTIME OFF)
+    set(ENABLE_EXAMPLES OFF)
 endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DBUILD_STATIC_LIBS:BOOL=${USE_STATIC_RUNTIME}
-        -DBUILD_SHARED_LIBS:BOOL=${USE_SHARED_RUNTIME}
-        -DBUILD_UTILS:BOOL=OFF
-        -DBUILD_EXAMPLES:BOOL=OFF
-        -DBUILD_TESTS:BOOL=OFF
+        -DBUILD_UTILS:BOOL=${ENABLE_UTILS}
+        -DBUILD_EXAMPLES:BOOL=${ENABLE_EXAMPLES}
+        -DBUILD_TESTS:BOOL=OFF # Tests build is broken
 )
 vcpkg_install_cmake()
 
-
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/wampcc RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
