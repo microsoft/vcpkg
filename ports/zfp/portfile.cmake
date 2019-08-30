2daca3800e748a392fece4b5c15387e59246714c
NancyLi1013 <lirui09@beyondsoft.com>
2019-08-29 19:37:03 -0700

    Fix build static error

--- ports/zfp/portfile.cmake
+++ ports/zfp/portfile.cmake
@@ -33,7 +33,12 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+   vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+elseif(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
+   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
+endif()
 
 vcpkg_copy_pdbs()
 
