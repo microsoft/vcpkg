05b05ca3fcb36650433b9b604d013c26a6554834
Phoebe <v-phma@microsoft.com>
2019-09-01 21:27:51 -0700

    Update the code to handle copyright

--- ports/libsquish/portfile.cmake
+++ ports/libsquish/portfile.cmake
@@ -25,4 +25,4 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
