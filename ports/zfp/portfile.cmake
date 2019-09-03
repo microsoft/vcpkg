de8caad14b010704c6fa4ecb160283fefda83f6f
NancyLi1013 <lirui09@beyondsoft.com>
2019-09-03 02:19:00 -0700

    Add features and fix build error.

--- ports/zfp/portfile.cmake
+++ ports/zfp/portfile.cmake
@@ -7,38 +7,35 @@ vcpkg_from_github(
     SHA512 c043cee73f6e972e047452552ab2ceb9247a6747fdb7e5f863aeab3a05208737c0bcabbe29f3c10e5c1aba961ec47aa6a0abdb395486fa0d5fb16a4ad45733c4
     HEAD_REF master
     PATCHES
-       fix-cfp-install.patch
-       fix-test-install.patch
+       fix-build-error.patch
+       fix-install-tools.patch
 )
 
-set(BUILD_CFP OFF)
-if("cfp" IN_LIST FEATURES)
-   set(BUILD_CFP ON)
-endif()
-
-set(BUILD_TESTING OFF)
-if("test" IN_LIST FEATURES)
-   set(BUILD_TESTING ON)
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    all     BUILD_ALL
+    cfp     BUILD_CFP
+    test    BUILD_TESTING
+    example BUILD_EXAMPLES
+    utility BUILD_UTILITIES
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
-      -DBUILD_CFP=${BUILD_CFP} 
-      -DBUILD_TESTING=${BUILD_TESTING} 
+    OPTIONS ${FEATURE_OPTIONS}
+      -DBUILD_ZFPY=OFF
+      -DBUILD_ZFORP=OFF
 )
 
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-   vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
-elseif(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
-   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 vcpkg_copy_pdbs()
 
