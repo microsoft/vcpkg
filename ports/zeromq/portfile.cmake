3079eb1dd4a8d6f0691f6ea5bafe39c6feee72b2
myd7349 <myd7349@gmail.com>
2019-09-10 19:05:03 +0800

    [zeromq] Update to 2019-09-13

--- ports/zeromq/portfile.cmake
+++ ports/zeromq/portfile.cmake
@@ -3,13 +3,27 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeromq/libzmq
-    REF b5db4b4421c8b2766321e70e5e9ca07c197fac47
-    SHA512 e1166b9eec3c0613c2b085bf995e6d711554ba77e180f7b86aa2d04157f2ab7dca8131b855568f8fc931ac256628f6018e6bc67bc9fbecfd498776b4151d6e63
+    REF 3ecaf9fe6e246d5f6d9d0038f5deb253cf66457c
+    SHA512 c5cf378e921adb489d6243802f92d1c2f39b8c3f9f51ea9764f6f2be98585c74af1d6371564d10b0d3cf6458b581a1658d39cc4eaf16d431c90e1b6b5df40082
     HEAD_REF master
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
+
+if("nss" IN_LIST FEATURES)
+    if("sha1" IN_LIST FEATURES)
+        message(WARNING "Both sha1 and nss are specified, will use NSS instead.")
+    endif()
+
+    set(DISABLE_WS OFF)
+    set(WITH_NSS ON)
+elseif("sha1" IN_LIST FEATURES)
+    set(DISABLE_WS OFF)
+    set(WITH_NSS OFF)
+else()
+    set(DISABLE_WS ON)
+endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     sodium WITH_LIBSODIUM
@@ -24,6 +38,9 @@ vcpkg_configure_cmake(
         -DBUILD_STATIC=${BUILD_STATIC}
         -DBUILD_SHARED=${BUILD_SHARED}
         -DWITH_PERF_TOOL=OFF
+        -DWITH_DOCS=OFF
+        -DDISABLE_WS=${DISABLE_WS}
+        -DWITH_NSS=${WITH_NSS}
         ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         "-DCMAKE_PDB_OUTPUT_DIRECTORY=${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg"
