a9e0621c329b84b54b2782b5f0a126798f9e2e46
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-09-11 23:45:50 +0200

    [lcms] use modern patching routine

--- ports/lcms/portfile.cmake
+++ ports/lcms/portfile.cmake
@@ -1,13 +1,14 @@
 include(vcpkg_common_functions)
 
-vcpkg_from_github( 
-    OUT_SOURCE_PATH SOURCE_PATH 
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
     REPO mm2/Little-CMS
     REF lcms2.9
     SHA512 b565ed3714c9beaf13e15b3798abbc6c295443357c8db3299cecd9794620bb1d7c50ad258cf887c7bbf66efacb8d8699a7ee579f8c73598740915caa3044ae70
     HEAD_REF master
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/remove_library_directive.patch"
-) 
+    PATCHES
+        remove_library_directive.patch
+)
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
@@ -24,9 +25,7 @@ file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/lc
 
 vcpkg_copy_pdbs()
 
-#patch header files to fix import/export issues
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  vcpkg_apply_patches(
-    SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/shared.patch")
-endif()
+file(READ "${CURRENT_PACKAGES_DIR}/include/lcms2.h" _contents)
+string(REPLACE " #endif  // CMS_USE_BIG_ENDIAN" " #endif  // CMS_USE_BIG_ENDIAN
++#define CMS_DLL" _contents "${_contents}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/include/lcms2.h" "${_contents}")
