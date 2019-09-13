4d983be9088fb4f9c9bc062a743b4df0c1bd0065
Alexander Neumann <alexander.neumann@hamburg.de>
2019-09-13 09:14:42 +0200

    Revert "add update capability to mqtt"
    This reverts commit 4118a184b55d54b09fc9b1a3fbd62664e309931c.

--- ports/qt5-mqtt/portfile.cmake
+++ ports/qt5-mqtt/portfile.cmake
@@ -8,20 +8,13 @@ vcpkg_from_github(
     SHA512 ${QT_HASH_${PORT}}
 )
 
-if(QT_UPDATE_VERSION)
-    file(SHA512 "${ARCHIVE_FILE}" ARCHIVE_HASH)
-    message(STATUS "${PORT} new hash is ${ARCHIVE_HASH}")
-    file(APPEND "${VCPKG_ROOT_DIR}/ports/qt5-base/cmake/qt_new_hashes.cmake" "set(QT_HASH_${PORT} ${ARCHIVE_HASH})\n")
-    SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-else()
-    # qt module builds from a git repository require a .git entry to invoke syncqt
-    file(WRITE "${SOURCE_PATH}/.git" "repocontent")
+# qt module builds from a git repository require a .git entry to invoke syncqt
+file(WRITE "${SOURCE_PATH}/.git" "repocontent")
 
-    # syncqt is a perl script
-    vcpkg_find_acquire_program(PERL)
-    get_filename_component(perl_exe_path ${PERL} DIRECTORY)
-    vcpkg_add_to_path("${perl_exe_path}")
+# syncqt is a perl script
+vcpkg_find_acquire_program(PERL)
+get_filename_component(perl_exe_path ${PERL} DIRECTORY)
+vcpkg_add_to_path("${perl_exe_path}")
 
-    qt_build_submodule(${SOURCE_PATH})
-    qt_install_copyright(${SOURCE_PATH})
-endif()
+qt_build_submodule(${SOURCE_PATH})
+qt_install_copyright(${SOURCE_PATH})
