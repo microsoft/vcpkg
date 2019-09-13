5575552f1bb9f81b406aa99b49e9382cfd47fbdf
Sutou Kouhei <kou@clear-code.com>
2019-09-13 18:35:58 +0900

    [libffi] Add support for CMake config

--- ports/libffi/portfile.cmake
+++ ports/libffi/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/libffiConfig.cmake.in DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
