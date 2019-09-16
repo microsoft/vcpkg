5bada438c1b30d17095a6df9b8a6ab7ee5e6a557
Alexander Neumann <alexander.neumann@hamburg.de>
2019-09-16 11:01:21 +0200

    make feature work correctly

--- ports/qt5-base/cmake/qt_port_functions.cmake
+++ ports/qt5-base/cmake/qt_port_functions.cmake
@@ -2,7 +2,11 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
 #Basic setup
 include(qt_port_hashes)
-include(qt_port_hashes_latest OPTIONAL)
+if(QT_BUILD_LATEST) # only set in qt5-base
+    include(qt_port_hashes_latest)
+else()
+    include(qt_port_hashes_latest OPTIONAL) # will only be available for the other qt ports if qt5-base was build with latest
+endif()
 #Fixup scripts
 include(qt_fix_makefile_install)
 include(qt_fix_cmake)
