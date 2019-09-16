ca12944bb33a6b5c68f7bbf68e58d6053b9a1060
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2019-09-16 05:23:27 -0700

    [rabit] Fix file conflict with dmlc (#8042)
    * [rabit] Add new port.
    
    * [rabit] Fix file conflict with dmlc

--- ports/rabit/portfile.cmake
+++ ports/rabit/portfile.cmake
@@ -10,11 +10,17 @@ vcpkg_from_github(
     REF v0.1
     SHA512 145fd839898cb95eaab9a88ad3301a0ccac0c8b672419ee2b8eb6ba273cc9a26e069e5ecbc37a3078e46dc64d11efb3e5ab10e5f8fed714e7add85b9e6ac2ec7
     HEAD_REF master
+    PATCHES fix-file-conflict.patch
 )
+
+file(REMOVE_RECURSE ${SOURCE_PATH}/include/dmlc)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+    PREFER_NINJA 
+    OPTIONS
+      -DRABIT_BUILD_TESTS=OFF
+      -DRABIT_BUILD_MPI=OFF
 )
 
 vcpkg_install_cmake()
@@ -28,4 +34,3 @@ vcpkg_copy_pdbs()
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/${PORT}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
-
