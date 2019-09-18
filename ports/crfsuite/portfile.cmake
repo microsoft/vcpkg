2ee7b94b2c8e8b5835d53272f5ef37e57a82a808
Li Yang <t-liyan1@microsoft.com>
2019-09-18 11:24:43 +0800

    [crfsuite] Add new port

---
+++ ports/crfsuite/portfile.cmake
@@ -0,0 +1,25 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO chokkan/crfsuite
+    REF 0.12
+    SHA512   7cb02c1ac72613bafd639e1ef9a7ce7ddb0d36b2e7038c45f9745ad177344c3f20c7df48dfc1bdce364af1d0a85957a918ed8fee078232d5a2c91ab3796ee7f0
+    HEAD_REF master
+    PATCHES  fix_msc_ver.patch
+)
+
+message(STATUS "source path is : ${SOURCE_PATH}")
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFIX_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/crfsuite RENAME copyright)
