4e24eda03f495ed1f431fdd6080cc026ed4dfbe1
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-09-19 21:52:53 +0200

    [spaceland] use install instead of copy for copyright

--- ports/spaceland/portfile.cmake
+++ ports/spaceland/portfile.cmake
@@ -28,4 +28,4 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME license)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
