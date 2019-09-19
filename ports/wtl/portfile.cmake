d54be2e5b4cd9b7cb5b83e4c7cbcd1144c7bc339
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-09-19 23:46:08 +0200

    [many ports] use modern routines

--- ports/wtl/portfile.cmake
+++ ports/wtl/portfile.cmake
@@ -1,11 +1,16 @@
 #header-only library
 include(vcpkg_common_functions)
+
 vcpkg_download_distfile(ARCHIVE
     URLS "https://downloads.sourceforge.net/project/wtl/WTL%2010/WTL%2010.0.9163/WTL10_9163.zip"
     FILENAME "WTL10_9163.zip"
     SHA512 feb7fb1c456e44ad05610f31f8c0f964eb6ce3eadf65a389219051f0ea2547069727666616622631cd90e25ea4a682a7c88c7089a374181870717246ad44e035
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
 
 file(INSTALL ${CURRENT_BUILDTREES_DIR}/src/Include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/wtl FILES_MATCHING PATTERN "*.h")
 
