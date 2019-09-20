279e25aecfe30f55296881ea9b0236c1d6ee030a
Alexander Neumann <alexander.neumann@hamburg.de>
2019-09-20 12:20:21 +0200

    revert changes to vcpkg_build_qmake file

--- scripts/cmake/vcpkg_build_qmake.cmake
+++ scripts/cmake/vcpkg_build_qmake.cmake
@@ -11,9 +11,11 @@ function(vcpkg_build_qmake)
     cmake_parse_arguments(_csc "SKIP_MAKEFILES" "BUILD_LOGNAME" "TARGETS;RELEASE_TARGETS;DEBUG_TARGETS" ${ARGN})
 
     if(CMAKE_HOST_WIN32)
+        set(_PATHSEP ";")
         vcpkg_find_acquire_program(JOM)
         set(INVOKE "${JOM}")
     else()
+        set(_PATHSEP ":")
         find_program(MAKE make)
         set(INVOKE "${MAKE}")
     endif()
@@ -56,15 +58,9 @@ function(vcpkg_build_qmake)
 
     #First generate the makefiles so we can modify them
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-        set(_BUILD_PREFIX "/debug")
-        set(_int_build_type dbg)
-
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/tools/qt5")
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}${_BUILD_PREFIX}/bin")
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}${_BUILD_PREFIX}/lib")
-
+        set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/debug/lib${_PATHSEP}${CURRENT_INSTALLED_DIR}/debug/bin${_PATHSEP}${CURRENT_INSTALLED_DIR}/tools/qt5${_PATHSEP}${ENV_PATH_BACKUP}")
         if(NOT _csc_SKIP_MAKEFILES)
-            run_jom(qmake_all makefiles ${_int_build_type})
+            run_jom(qmake_all makefiles dbg)
 
             #Store debug makefiles path
             file(GLOB_RECURSE DEBUG_MAKEFILES ${DEBUG_DIR}/*Makefile*)
@@ -83,18 +79,13 @@ function(vcpkg_build_qmake)
             endforeach()
         endif()
 
-        run_jom("${_csc_DEBUG_TARGETS}" ${_csc_BUILD_LOGNAME} ${_int_build_type})
+        run_jom("${_csc_DEBUG_TARGETS}" ${_csc_BUILD_LOGNAME} dbg)
     endif()
 
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        set(_int_build_type rel)
-
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/tools/qt5")
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}${_BUILD_PREFIX}/bin")
-        vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}${_BUILD_PREFIX}/lib")
-
+        set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/lib${_PATHSEP}${CURRENT_INSTALLED_DIR}/bin${_PATHSEP}${CURRENT_INSTALLED_DIR}/tools/qt5${_PATHSEP}${ENV_PATH_BACKUP}")
         if(NOT _csc_SKIP_MAKEFILES)
-            run_jom(qmake_all makefiles ${_int_build_type})
+            run_jom(qmake_all makefiles rel)
 
             #Store release makefile path
             file(GLOB_RECURSE RELEASE_MAKEFILES ${RELEASE_DIR}/*Makefile*)
@@ -111,7 +102,7 @@ function(vcpkg_build_qmake)
             endforeach()
         endif()
 
-        run_jom("${_csc_RELEASE_TARGETS}" ${_csc_BUILD_LOGNAME} ${_int_build_type})
+        run_jom("${_csc_RELEASE_TARGETS}" ${_csc_BUILD_LOGNAME} rel)
     endif()
     
     # Restore the original value of ENV{PATH}
