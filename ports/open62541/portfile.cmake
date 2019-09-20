891b8f2cb49086b39d573160d3b7ea5ca6d8a0a2
Phoebe <v-phma@microsoft.com>
2019-09-19 23:04:52 -0700

    Add the regex back to unblock --head revision

--- ports/open62541/portfile.cmake
+++ ports/open62541/portfile.cmake
@@ -14,9 +14,20 @@ vcpkg_from_github(
     REF v0.3.0
     SHA512 67766d226e1b900c0c37309099ecdbe987d10888ebf43f9066b21cf79f64d34e6ac30c2671a4901892f044859da4e8dbaa9fed5a49c633f73fef3bec75774050
     HEAD_REF master
-    PATCHES
-        fix-dynamic-build.patch
 )
+
+file(READ ${SOURCE_PATH}/CMakeLists.txt OPEN62541_CMAKELISTS)
+string(REPLACE
+               "RUNTIME DESTINATION \${CMAKE_INSTALL_PREFIX}"
+               "RUNTIME DESTINATION \${BIN_INSTALL_DIR}"
+       OPEN62541_CMAKELISTS "${OPEN62541_CMAKELISTS}")
+file(WRITE ${SOURCE_PATH}/CMakeLists.txt "${OPEN62541_CMAKELISTS}")
+
+if(CMAKE_HOST_WIN32)
+    set(EXECUTABLE_SUFFIX ".exe")
+else()
+    set(EXECUTABLE_SUFFIX "")
+endif()
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
@@ -24,25 +35,27 @@ vcpkg_add_to_path("${PYTHON3_DIR}")
 
 if(CMAKE_HOST_WIN32)
     # Must not modify system copy of python3 -- on CMAKE_HOST_WIN32, we have our own private copy
-    if(NOT EXISTS ${PYTHON3_DIR}/easy_install.exe)
-        if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip.exe)
+    if(NOT EXISTS ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX})
+        if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX})
             get_filename_component(PYTHON3_DIR_NAME "${PYTHON3_DIR}" NAME)
             vcpkg_download_distfile(GET_PIP
                 URLS "https://bootstrap.pypa.io/3.3/get-pip.py"
                 FILENAME "tools/python/${PYTHON3_DIR_NAME}/get-pip.py"
                 SHA512 92e68525830bb23955a31cb19ebc3021ef16b6337eab83d5db2961b791283d2867207545faf83635f6027f2f7b7f8fee2c85f2cfd8e8267df25406474571c741
             )
-            execute_process(COMMAND ${PYTHON3_DIR}/python.exe ${GET_PIP})
+            execute_process(COMMAND ${PYTHON3_DIR}/python${EXECUTABLE_SUFFIX} ${GET_PIP})
         endif()
-        execute_process(COMMAND ${PYTHON3_DIR}/Scripts/pip.exe install six)
+        execute_process(COMMAND ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX} install six)
     else()
-        execute_process(COMMAND ${PYTHON3_DIR}/easy_install.exe six)
+        execute_process(COMMAND ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX} six)
     endif()
 endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DBIN_INSTALL_DIR:STRING=bin
     OPTIONS_DEBUG
         -DCMAKE_DEBUG_POSTFIX=d
 )
