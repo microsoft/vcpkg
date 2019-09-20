4aa9193d33ec148f82cf2788a10cff03f9f073f7
TsukasaSugiura <t.sugiura0204@gmail.com>
2019-09-21 04:05:19 +0900

    [libsoundio] WindowsStore not supported

--- ports/libsoundio/portfile.cmake
+++ ports/libsoundio/portfile.cmake
@@ -1,5 +1,9 @@
 include(vcpkg_common_functions)
 
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    message(FATAL_ERROR "WindowsStore not supported")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO andrewrk/libsoundio
