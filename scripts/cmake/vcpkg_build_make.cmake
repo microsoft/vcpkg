e64c918b5501232169ea82917722d64959e42bcd
JackBoosY <yuzaiyang@beyondsoft.com>
2019-09-24 00:09:32 -0700

    Add nmake support.

--- scripts/cmake/vcpkg_build_make.cmake
+++ scripts/cmake/vcpkg_build_make.cmake
@@ -25,6 +25,9 @@ function(vcpkg_build_make)
         set(_bc_LOGFILE_ROOT "build")
     endif()
     
+    set(MAKE )
+    set(MAKE_OPTS )
+    set(INSTALL_OPTS )
     if (_VCPKG_MAKE_GENERATOR STREQUAL "make")
         if (CMAKE_HOST_WIN32)
             vcpkg_find_acquire_program(YASM)
@@ -35,95 +38,153 @@ function(vcpkg_build_make)
             get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
             set(ENV{PATH} "${YASM_EXE_PATH};${MSYS_ROOT}/usr/bin;$ENV{PATH};${PERL_EXE_PATH}")
             set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
-            
-            set(MAKE_CMD "${BASH} make --noprofile --norc")
-            set(INSTALL_CMD "${BASH} make install --noprofile --norc")
+            # Set make command and install command
+            set(MAKE "${BASH} make --noprofile --norc")
+            set(_VCPKG_MAKE_GENERATOR install)
         else()
             find_program(MAKE make)
-            set(MAKE_CMD make)
-            set(INSTALL_CMD make install)
+            # Set make command and install command
+            set(MAKE make)
+            set(INSTALL_OPTS install)
+        endif()
+        if (_VCPKG_PROJECT_SUBPATH)
+            set(_VCPKG_PROJECT_SUBPATH /${_VCPKG_PROJECT_SUBPATH})
+        else()
+            set(_VCPKG_PROJECT_SUBPATH )
         endif()
     elseif (_VCPKG_MAKE_GENERATOR STREQUAL "nmake")
         find_program(NMAKE nmake)
-        set(MAKE_CMD "${NMAKE}")
-        set(INSTALL_CMD "${NMAKE} install")
+        get_filename_component(NMAKE_EXE_PATH ${NMAKE} DIRECTORY)
+        set(ENV{PATH} "$ENV{PATH};${NMAKE_EXE_PATH}")
+        # Set make command and install command
+        set(MAKE ${NMAKE} /NOLOGO /G /U)
+        set(MAKE_OPTS -f makefile.vc all)
+        set(INSTALL_OPTS install)
+        # Add subpath to work directory
+        if (_VCPKG_NMAKE_PROJECT_SUBPATH)
+            set(_VCPKG_NMAKE_PROJECT_SUBPATH /${_VCPKG_NMAKE_PROJECT_SUBPATH})
+        else()
+            set(_VCPKG_NMAKE_PROJECT_SUBPATH )
+        endif()
     else()
         message(FATAL_ERROR "${_VCPKG_MAKE_GENERATOR} not supported.")
     endif()
     
     set(ENV{INCLUDE} "${CURRENT_INSTALLED_DIR}/include;$ENV{INCLUDE}")
-
-    foreach(BUILDTYPE "debug" "release")
-        if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL BUILDTYPE)
-            if(BUILDTYPE STREQUAL "debug")
-                # Skip debug generate
-                if (_VCPKG_NO_DEBUG)
-                    continue()
-                endif()
-                set(SHORT_BUILDTYPE "-dbg")
-                set(CONFIG "Debug")
-            else()
-                if (_VCPKG_NO_DEBUG)
-                    set(SHORT_BUILDTYPE "")
-                else()
-                    set(SHORT_BUILDTYPE "-rel")
-                endif()
-                set(CONFIG "Release")
-            endif()
-
-            message(STATUS "Building ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
-
-            if(_bc_ADD_BIN_TO_PATH)
-                set(_BACKUP_ENV_PATH "$ENV{PATH}")
-                if(CMAKE_HOST_WIN32)
-                    set(_PATHSEP ";")
-                else()
-                    set(_PATHSEP ":")
-                endif()
-                if(BUILDTYPE STREQUAL "debug")
-                    set(ENV{LIB} "${CURRENT_INSTALLED_DIR}/debug/lib;$ENV{LIB}")
-                    set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/bin${_PATHSEP}$ENV{PATH}")
-                else()
-                    set(ENV{LIB} "${CURRENT_INSTALLED_DIR}/lib;$ENV{LIB}")
-                    set(ENV{PATH} "${CURRENT_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin${_PATHSEP}$ENV{PATH}")
-                endif()
-            endif()
-
-            vcpkg_execute_build_process(
-                COMMAND ${MAKE_CMD} #${CONFIG}
-                WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}
-                LOGNAME "${_bc_LOGFILE_ROOT}-${TARGET_TRIPLET}${SHORT_BUILDTYPE}"
-            )
-
-            if(_bc_ADD_BIN_TO_PATH)
-                set(ENV{PATH} "${_BACKUP_ENV_PATH}")
-            endif()
-        endif()
-    endforeach()
     
-    foreach(BUILDTYPE "debug" "release")
-        if(BUILDTYPE STREQUAL "debug")
-            # Skip debug generate
-            if (_VCPKG_NO_DEBUG)
-                continue()
+    if (NOT _VCPKG_MAKE_GENERATOR STREQUAL "nmake")
+        # For make or other generator
+        foreach(BUILDTYPE "debug" "release")
+            if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL BUILDTYPE)
+                if(BUILDTYPE STREQUAL "debug")
+                    # Skip debug generate
+                    if (_VCPKG_NO_DEBUG)
+                        continue()
+                    endif()
+                    set(SHORT_BUILDTYPE "-dbg")
+                    set(CONFIG "Debug")
+                else()
+                    # In NO_DEBUG mode, we only use ${TARGET_TRIPLET} directory.
+                    if (_VCPKG_NO_DEBUG)
+                        set(SHORT_BUILDTYPE "")
+                    else()
+                        set(SHORT_BUILDTYPE "-rel")
+                    endif()
+                    set(CONFIG "Release")
+                endif()
+    
+                message(STATUS "Building ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
+    
+                
+                vcpkg_execute_required_process(
+                    COMMAND ${MAKE} ${MAKE_OPTS}
+                    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}${_VCPKG_PROJECT_SUBPATH}
+                    LOGNAME "${_bc_LOGFILE_ROOT}-${TARGET_TRIPLET}${SHORT_BUILDTYPE}"
+                )
+    
+                if(_bc_ADD_BIN_TO_PATH)
+                    set(ENV{PATH} "${_BACKUP_ENV_PATH}")
+                endif()
             endif()
-            set(SHORT_BUILDTYPE "-dbg")
-            set(CONFIG "Debug")
-        else()
-            if (_VCPKG_NO_DEBUG)
-                set(SHORT_BUILDTYPE "")
-            else()
-                set(SHORT_BUILDTYPE "-rel")
-            endif()
-            set(CONFIG "Release")
-        endif()
+        endforeach()
+        
         if (_bc_ENABLE_INSTALL)
-            message(STATUS "Installing ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
-            vcpkg_execute_build_process(
-                COMMAND ${INSTALL_CMD} #${CONFIG}
-                WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}
-                LOGNAME "${_bc_LOGFILE_ROOT}-${TARGET_TRIPLET}${SHORT_BUILDTYPE}"
-            )
+            foreach(BUILDTYPE "debug" "release")
+                if(BUILDTYPE STREQUAL "debug")
+                    # Skip debug generate
+                    if (_VCPKG_NO_DEBUG)
+                        continue()
+                    endif()
+                    set(SHORT_BUILDTYPE "-dbg")
+                    set(CONFIG "Debug")
+                else()
+                    # In NO_DEBUG mode, we only use ${TARGET_TRIPLET} directory.
+                    if (_VCPKG_NO_DEBUG)
+                        set(SHORT_BUILDTYPE "")
+                    else()
+                        set(SHORT_BUILDTYPE "-rel")
+                    endif()
+                    set(CONFIG "Release")
+                endif()
+                message(STATUS "Installing ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
+                vcpkg_execute_required_process(
+                    COMMAND ${MAKE} ${INSTALL_OPTS}
+                    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}${_VCPKG_PROJECT_SUBPATH}
+                    LOGNAME "install-${TARGET_TRIPLET}${SHORT_BUILDTYPE}"
+                )
+            endforeach()
         endif()
-    endforeach()
+    else()
+        # For nmake
+        set(EXTRA_OPT OPTS=pdbs OPTS=symbols)
+        foreach(BUILDTYPE "debug" "release")
+            if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL BUILDTYPE)
+                if(BUILDTYPE STREQUAL "debug")
+                    # Skip debug generate
+                    if (_VCPKG_NO_DEBUG)
+                        continue()
+                    endif()
+                    # Generate obj dir suffix
+                    set(SHORT_BUILDTYPE "-dbg")
+                    set(CONFIG "Debug")
+                    # Add install command and arguments
+                    if (_bc_ENABLE_INSTALL)
+                        set(INSTALL_OPTS ${INSTALL_OPTS} INSTALLDIR=${CURRENT_PACKAGES_DIR}/debug)
+                        set(MAKE_OPTS ${MAKE_OPTS} ${INSTALL_OPTS})
+                    endif()
+                    set(MAKE_OPTS ${MAKE_OPTS} ${_VCPKG_NMAKE_OPTION_DEBUG} ${EXTRA_OPT})
+                else()
+                    # In NO_DEBUG mode, we only use ${TARGET_TRIPLET} directory.
+                    if (_VCPKG_NO_DEBUG)
+                        set(SHORT_BUILDTYPE "")
+                    else()
+                        set(SHORT_BUILDTYPE "-rel")
+                    endif()
+                    set(CONFIG "Release")
+                    # Add install command and arguments
+                    if (_bc_ENABLE_INSTALL)
+                        set(INSTALL_OPTS ${INSTALL_OPTS} INSTALLDIR=${CURRENT_PACKAGES_DIR})
+                        set(MAKE_OPTS ${MAKE_OPTS} ${INSTALL_OPTS})
+                    endif()
+                    set(MAKE_OPTS ${MAKE_OPTS} ${_VCPKG_NMAKE_OPTION_RELEASE} ${EXTRA_OPT})
+                endif()
+
+                if (NOT _bc_ENABLE_INSTALL)
+                    message(STATUS "Building ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
+                else()
+                    message(STATUS "Building and installing ${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
+                endif()
+
+                vcpkg_execute_required_process(
+                    COMMAND ${MAKE} ${MAKE_OPTS}
+                    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}${_VCPKG_NMAKE_PROJECT_SUBPATH}
+                    LOGNAME "${_bc_LOGFILE_ROOT}-${TARGET_TRIPLET}${SHORT_BUILDTYPE}"
+                )
+
+                if(_bc_ADD_BIN_TO_PATH)
+                    set(ENV{PATH} "${_BACKUP_ENV_PATH}")
+                endif()
+            endif()
+        endforeach()
+    endif()
 endfunction()
--- scripts/cmake/vcpkg_configure_make.cmake
+++ scripts/cmake/vcpkg_configure_make.cmake
@@ -1,4 +1,4 @@
-## # vcpkg_configure_cmake
+## # vcpkg_configure_make
 ##
 ## Configure configure for Debug and Release builds of a project.
 ##
@@ -17,66 +17,80 @@
 ##
 ## ## Parameters
 ## ### SOURCE_PATH
-## Specifies the directory containing the `CMakeLists.txt`.
+## Specifies the directory containing the `configure`/`configure.ac`.
 ## By convention, this is usually set in the portfile as the variable `SOURCE_PATH`.
+##
+## ### PROJECT_SUBPATH
+## Specifies the directory containing the ``configure`/`configure.ac`.
+## By convention, this is usually set in the portfile as the variable `SOURCE_PATH`.
+## Should use `GENERATOR NMake` first.
+##
+## ### NMAKE_PROJECT_SUBPATH
+## Specifies the directory containing the ``makefile.vc`.
+## By convention, this is usually set in the portfile as the variable `SOURCE_PATH`.
+## Should use `GENERATOR NMake` first.
 ##
 ## ### NO_DEBUG
 ## This port doesn't support debug mode.
 ##
 ## ### AUTOCONFIG
-## Need to use autoconfig to generate configure.
+## Need to use autoconfig to generate configure file.
 ##
 ## ### GENERATOR
 ## Specifies the precise generator to use.
-##
-## This is useful if some project-specific buildsystem has been wrapped in a cmake script that won't perform an actual build.
-## If used for this purpose, it should be set to "NMake Makefiles".
+## NMake: nmake(windows) make(unix)
+## MAKE: make(windows) make(unix)
 ##
 ## ### PRERUN_SHELL
 ## Script that needs to be called before configuration
 ##
 ## ### OPTIONS
-## Additional options passed to CMake during the configuration.
+## Additional options passed to configure during the configuration.
 ##
 ## ### OPTIONS_RELEASE
-## Additional options passed to CMake during the Release configuration. These are in addition to `OPTIONS`.
+## Additional options passed to configure during the Release configuration. These are in addition to `OPTIONS`.
 ##
 ## ### OPTIONS_DEBUG
-## Additional options passed to CMake during the Debug configuration. These are in addition to `OPTIONS`.
+## Additional options passed to configure during the Debug configuration. These are in addition to `OPTIONS`.
+##
+## ### NMAKE_OPTION
+## Additional options passed to generate during the generation.
+## Only for msvc makefile
+##
+## ### NMAKE_OPTION_RELEASE
+## Additional options passed to generate during the Release generation. These are in addition to `NMAKE_OPTION`.
+## Only for msvc makefile
+##
+## ### NMAKE_OPTION_DEBUG
+## Additional options passed to generate during the Debug generation. These are in addition to `NMAKE_OPTION`.
+## Only for msvc makefile
 ##
 ## ## Notes
-## This command supplies many common arguments to CMake. To see the full list, examine the source.
+## This command supplies many common arguments to configure. To see the full list, examine the source.
 function(vcpkg_configure_make)
     cmake_parse_arguments(_csc
         "AUTOCONFIG;NO_DEBUG"
-        "SOURCE_PATH;GENERATOR;PRERUN_SHELL"
-        "OPTIONS;OPTIONS_DEBUG;OPTIONS_RELEASE"
+        "SOURCE_PATH;PROJECT_SUBPATH;NMAKE_PROJECT_SUBPATH;GENERATOR;PRERUN_SHELL"
+        "OPTIONS;OPTIONS_DEBUG;OPTIONS_RELEASE;NMAKE_OPTION;NMAKE_OPTION_DEBUG;NMAKE_OPTION_RELEASE"
         ${ARGN}
     )
-
+    
     if(NOT VCPKG_PLATFORM_TOOLSET)
         message(FATAL_ERROR "Vcpkg has been updated with VS2017 support; "
             "however, vcpkg.exe must be rebuilt by re-running bootstrap-vcpkg.bat\n")
     endif()
-
-    if(CMAKE_HOST_WIN32)
-        set(_PATHSEP ";")
-        if(DEFINED ENV{PROCESSOR_ARCHITEW6432})
-            set(_csc_HOST_ARCHITECTURE $ENV{PROCESSOR_ARCHITEW6432})
-        else()
-            set(_csc_HOST_ARCHITECTURE $ENV{PROCESSOR_ARCHITECTURE})
-        endif()
-    else()
-        set(_PATHSEP ":")
+    
+    if (_csc_OPTIONS_DEBUG STREQUAL _csc_OPTIONS_RELEASE OR NMAKE_OPTION_RELEASE STREQUAL NMAKE_OPTION_DEBUG)
+        message(FATAL_ERROR "Detected debug configuration is equal to release configuration, please use NO_DEBUG for vcpkg_configure_make")
     endif()
 
-    if(_csc_GENERATOR STREQUAL NMAKE)
+    if(_csc_GENERATOR MATCHES "NMake")
         if (CMAKE_HOST_WIN32)
             set(GENERATOR "nmake")
         else()
             set(GENERATOR "make")
         endif()
-    elseif(_csc_GENERATOR STREQUAL MAKE OR NOT _csc_GENERATOR)
+    elseif(NOT _csc_GENERATOR OR _csc_GENERATOR MATCHES "MAKE")
         if (CMAKE_HOST_WIN32)
             set(GENERATOR "make")
         else()
@@ -93,13 +107,16 @@ function(vcpkg_configure_make)
         endif()
     endif()
 
-    # If we use Ninja, make sure it's on PATH
+    set(SKIP_CONFIGURE OFF)
     if (GENERATOR STREQUAL "nmake")
+        message(STATUS "Using generator NMAKE")
         find_program(NMAKE nmake)
-        if (NOT NMAKE_FOUND)
-            message(FATAL_ERROR "nmake not found.")
+        if (NOT NMAKE)
+            message(FATAL_ERROR "NMAKE not found.")
         endif()
+        set(SKIP_CONFIGURE ON)
     elseif (GENERATOR STREQUAL "make")
+        message(STATUS "Using generator make")
         if (CMAKE_HOST_WIN32)
             vcpkg_find_acquire_program(YASM)
             vcpkg_find_acquire_program(PERL)
@@ -120,21 +137,36 @@ function(vcpkg_configure_make)
     else()
         message(FATAL_ERROR "${GENERATOR} not supported.")
     endif()
-
-    file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
+    
+    if (NOT _csc_NO_DEBUG)
+        file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
+    else()
+        file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}")
+    endif()
 
     set(_csc_OPTIONS_RELEASE ${_csc_OPTIONS_RELEASE} --bindir=${CURRENT_PACKAGES_DIR}/bin --sbindir=${CURRENT_PACKAGES_DIR}/bin --libdir=${CURRENT_PACKAGES_DIR}/lib --includedir=${CURRENT_PACKAGES_DIR}/include)
     set(_csc_OPTIONS_DEBUG ${_csc_OPTIONS_DEBUG} --bindir=${CURRENT_PACKAGES_DIR}/debug/bin --sbindir=${CURRENT_PACKAGES_DIR}/debug/bin --libdir=${CURRENT_PACKAGES_DIR}/debug/lib --includedir=${CURRENT_PACKAGES_DIR}/debug/include)
     set(base_cmd )
     if(CMAKE_HOST_WIN32)
-        set(base_cmd ${BASH} --noprofile --norc )
-        set(rel_command
-            ${base_cmd} "configure" "${_csc_OPTIONS}" "${_csc_OPTIONS_RELEASE}"
-        )
-        set(dbg_command
-            ${base_cmd} "configure" "${_csc_OPTIONS}" "${_csc_OPTIONS_DEBUG}"
-        )
+        if (GENERATOR STREQUAL "nmake")
+            set(base_cmd ${NMAKE} /NOLOGO /G /U /f "${_csc_NMAKE_PROJECT_SUBPATH}")
+            set(rel_command
+                ${base_cmd} "${_csc_OPTIONS}" "${_csc_OPTIONS_RELEASE}"
+            )
+            set(dbg_command
+                ${base_cmd} "${_csc_OPTIONS}" "${_csc_OPTIONS_DEBUG}"
+            )
+        else()
+            set(base_cmd ${BASH} --noprofile --norc )
+            set(rel_command
+                ${base_cmd} "configure" "${_csc_OPTIONS}" "${_csc_OPTIONS_RELEASE}"
+            )
+            set(dbg_command
+                ${base_cmd} "configure" "${_csc_OPTIONS}" "${_csc_OPTIONS_DEBUG}"
+            )
+        endif()
     else()
+        set(_csc_NMAKE_PROJECT_SUBPATH )
         set(base_cmd ./)
         set(rel_command
             ${base_cmd}configure "${_csc_OPTIONS}" "${_csc_OPTIONS_RELEASE}"
@@ -147,35 +179,43 @@ function(vcpkg_configure_make)
     # Configure debug
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug" AND NOT _csc_NO_DEBUG)
         set(OBJ_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
+        if (GENERATOR STREQUAL "nmake")
+            set(PRJ_DIR ${OBJ_DIR}/${_csc_NMAKE_PROJECT_SUBPATH})
+        else()
+            set(PRJ_DIR ${OBJ_DIR}/${_csc_PROJECT_SUBPATH})
+        endif()
         file(MAKE_DIRECTORY ${OBJ_DIR})
         file(GLOB SOURCE_FILES ${_csc_SOURCE_PATH}/*)
         foreach(ONE_SOUCRCE_FILE ${SOURCE_FILES})
             file(COPY ${ONE_SOUCRCE_FILE} DESTINATION ${OBJ_DIR})
         endforeach()
-        if (_csc_PRERUN_SHELL)
-            message(STATUS "Prerun shell with ${TARGET_TRIPLET}-dbg")
+        
+        if (NOT SKIP_CONFIGURE)
+            if (_csc_PRERUN_SHELL)
+                message(STATUS "Prerun shell with ${TARGET_TRIPLET}-dbg")
+                vcpkg_execute_required_process(
+                    COMMAND ${base_cmd}${_csc_PRERUN_SHELL}
+                    WORKING_DIRECTORY ${PRJ_DIR}
+                    LOGNAME prerun-${TARGET_TRIPLET}-dbg
+                )
+            endif()
+            
+            if (_csc_AUTOCONFIG)
+                message(STATUS "Generating configure with ${TARGET_TRIPLET}-dbg")
+                vcpkg_execute_required_process(
+                    COMMAND autoreconf -v --install
+                    WORKING_DIRECTORY ${PRJ_DIR}
+                    LOGNAME prerun-${TARGET_TRIPLET}-dbg
+                )
+            endif()
+            
+            message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
             vcpkg_execute_required_process(
-                COMMAND ${base_cmd}${_csc_PRERUN_SHELL}
-                WORKING_DIRECTORY ${OBJ_DIR}
-                LOGNAME prerun-${TARGET_TRIPLET}-dbg
+                COMMAND ${dbg_command}
+                WORKING_DIRECTORY ${PRJ_DIR}
+                LOGNAME config-${TARGET_TRIPLET}-dbg
             )
         endif()
-        
-        if (_csc_AUTOCONFIG)
-            message(STATUS "Generating configure with ${TARGET_TRIPLET}-dbg")
-            vcpkg_execute_required_process(
-                COMMAND autoreconf -v --install
-                WORKING_DIRECTORY ${OBJ_DIR}
-                LOGNAME prerun-${TARGET_TRIPLET}-dbg
-            )
-        endif()
-        
-        message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
-        vcpkg_execute_required_process(
-            COMMAND ${dbg_command}
-            WORKING_DIRECTORY ${OBJ_DIR}
-            LOGNAME config-${TARGET_TRIPLET}-dbg
-        )
     endif()
 
     # Configure release
@@ -187,37 +227,48 @@ function(vcpkg_configure_make)
             set(TAR_TRIPLET_DIR ${TARGET_TRIPLET}-rel)
             set(OBJ_DIR ${CURRENT_BUILDTREES_DIR}/${TAR_TRIPLET_DIR})
         endif()
+        if (GENERATOR STREQUAL "nmake")
+            set(PRJ_DIR ${OBJ_DIR}/${_csc_NMAKE_PROJECT_SUBPATH})
+        else()
+            set(PRJ_DIR ${OBJ_DIR}/${_csc_PROJECT_SUBPATH})
+        endif()
         file(MAKE_DIRECTORY ${OBJ_DIR})
         file(GLOB SOURCE_FILES ${_csc_SOURCE_PATH}/*)
         foreach(ONE_SOUCRCE_FILE ${SOURCE_FILES})
             file(COPY ${ONE_SOUCRCE_FILE} DESTINATION ${OBJ_DIR})
         endforeach()
-        if (_csc_PRERUN_SHELL)
-            message(STATUS "Prerun shell with ${TAR_TRIPLET_DIR}")
+        
+        if (NOT SKIP_CONFIGURE)
+            if (_csc_PRERUN_SHELL)
+                message(STATUS "Prerun shell with ${TAR_TRIPLET_DIR}")
+                vcpkg_execute_required_process(
+                    COMMAND ${base_cmd}${_csc_PRERUN_SHELL}
+                    WORKING_DIRECTORY ${PRJ_DIR}
+                    LOGNAME prerun-${TAR_TRIPLET_DIR}
+                )
+            endif()
+            
+            if (_csc_AUTOCONFIG)
+                message(STATUS "Generating configure with ${TAR_TRIPLET_DIR}")
+                vcpkg_execute_required_process(
+                    COMMAND autoreconf -v --install
+                    WORKING_DIRECTORY ${PRJ_DIR}
+                    LOGNAME prerun-${TAR_TRIPLET_DIR}
+                )
+            endif()
+            
+            message(STATUS "Configuring ${TAR_TRIPLET_DIR}")
             vcpkg_execute_required_process(
-                COMMAND ${base_cmd}${_csc_PRERUN_SHELL}
-                WORKING_DIRECTORY ${OBJ_DIR}
-                LOGNAME prerun-${TAR_TRIPLET_DIR}
+                COMMAND ${rel_command}
+                WORKING_DIRECTORY ${PRJ_DIR}
+                LOGNAME config-${TAR_TRIPLET_DIR}
             )
         endif()
-        
-        if (_csc_AUTOCONFIG)
-            message(STATUS "Generating configure with ${TAR_TRIPLET_DIR}")
-            vcpkg_execute_required_process(
-                COMMAND autoreconf -v --install
-                WORKING_DIRECTORY ${OBJ_DIR}
-                LOGNAME prerun-${TAR_TRIPLET_DIR}
-            )
-        endif()
-        
-        message(STATUS "Configuring ${TAR_TRIPLET_DIR}")
-        vcpkg_execute_required_process(
-            COMMAND ${rel_command}
-            WORKING_DIRECTORY ${OBJ_DIR}
-            LOGNAME config-${TAR_TRIPLET_DIR}
-        )
     endif()
-    
     set(_VCPKG_MAKE_GENERATOR "${GENERATOR}" PARENT_SCOPE)
     set(_VCPKG_NO_DEBUG ${_csc_NO_DEBUG} PARENT_SCOPE)
+    set(_VCPKG_NMAKE_OPTION_RELEASE ${_csc_NMAKE_OPTION} ${_csc_NMAKE_OPTION_RELEASE} PARENT_SCOPE)
+    set(_VCPKG_NMAKE_OPTION_DEBUG ${_csc_NMAKE_OPTION} ${_csc_NMAKE_OPTION_DEBUG} PARENT_SCOPE)
+    set(_VCPKG_PROJECT_SUBPATH ${_csc_PROJECT_SUBPATH} PARENT_SCOPE)
+    set(_VCPKG_NMAKE_PROJECT_SUBPATH ${_csc_NMAKE_PROJECT_SUBPATH} PARENT_SCOPE)
 endfunction()
