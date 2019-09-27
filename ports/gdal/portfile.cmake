15e95b0bf13a0b7a5ad89071439c0cf8541d8fda
Lakin Wecker <lakin.wecker@ucalgary.ca>
2019-09-27 12:44:39 -0600

    Addresses the review comments.
    Use VCPKG_CONCURRENCY so the normal vcpkg concurrency controls apply.
    Use vcpkg_execute_build_process and NO_PARALLEL_COMMAND which allows
    for builds to be retried in some cases where parallel builds might fail.

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -332,9 +332,6 @@ elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STRE
       message(FATAL_ERROR "MAKE not found")
   endif()
 
-  include(ProcessorCount)
-  ProcessorCount(NUM_CPUS)
-
   if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
     ################
     # Release build
@@ -349,8 +346,9 @@ elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STRE
     )
 
     message(STATUS "Building ${TARGET_TRIPLET}-rel")
-    vcpkg_execute_required_process(
-      COMMAND make "-j${NUM_CPUS}"
+    vcpkg_execute_build_process(
+      COMMAND make -j ${VCPKG_CONCURRENCY}
+      NO_PARALLEL_COMMAND make
       WORKING_DIRECTORY ${SOURCE_PATH_RELEASE}
       LOGNAME make-build-${TARGET_TRIPLET}-release
     )
@@ -385,8 +383,9 @@ elseif (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux" OR VCPKG_CMAKE_SYSTEM_NAME STRE
     )
 
     message(STATUS "Building ${TARGET_TRIPLET}-dbg")
-    vcpkg_execute_required_process(
-      COMMAND make "-j${NUM_CPUS}"
+    vcpkg_execute_build_process(
+      COMMAND make -j ${VCPKG_CONCURRENCY}
+      NO_PARALLEL_COMMAND make
       WORKING_DIRECTORY ${SOURCE_PATH_DEBUG}
       LOGNAME make-build-${TARGET_TRIPLET}-debug
     )
