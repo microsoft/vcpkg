ae25d6e828e0f15e806cd4a32d7e401148eda13b
Joel Höner <athre0z@zyantific.com>
2019-10-02 17:22:17 +0200

    [zydis] Update to v3.0.0
    Since v3.0.0 now depends on the internal zycore library via a submodule, I had to replace `vcpkg_from_github` with `vcpkg_download_distfile`

--- ports/zydis/portfile.cmake
+++ ports/zydis/portfile.cmake
@@ -1,16 +1,39 @@
 include(vcpkg_common_functions)
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO zyantific/zydis
-    REF v2.0.3
-    SHA512 254aee734f93ee51a8b963404f79d6edfd0831867763243b8020c44ec2d7dd4cb7e445248df4a9af7cd2743c020674df482661d59d3278a44d2ad9a2e0611a39
-    HEAD_REF master
+vcpkg_download_distfile(ZYDIS_ARCHIVE
+  URLS "https://github.com/zyantific/zydis/archive/v3.0.0.zip"
+  FILENAME "zydis-3.0.0.zip"
+  SHA512 a9a75903553793a19164124e6b6b4f202ef928558ba094ce9e6a9290e4bce4a28a8bb1696b552560a9c093f2774e1f68310ea278b6533867712ba7b3e283d367
 )
+
+vcpkg_download_distfile(ZYCORE_ARCHIVE
+  URLS "https://github.com/zyantific/zycore-c/archive/ffcc3663200984e6cb54c2879f7b4de3f6112227.zip"
+  FILENAME "zycore-c-ffcc3663200984e6cb54c2879f7b4de3f611222.zip"
+  SHA512 157ad277462bf71fd111a367c9b192f58f1f8ba364bfdf17cfdec956d14b5b32545299cdd355fd4b7bd7ab07f83b3219c2bb2e9c7b958b857613ed2c5f26afe2
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ZYDIS_ARCHIVE}
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH ZYCORE_PATH
+    ARCHIVE ${ZYCORE_ARCHIVE}
+)
+
+file(RENAME ${ZYCORE_PATH} ${SOURCE_PATH}/dependencies/zycore)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    set(ZYDIS_BUILD_SHARED_LIB OFF)
+else()
+    set(ZYDIS_BUILD_SHARED_LIB ON)
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS "-DZYDIS_BUILD_SHARED_LIB=${ZYDIS_BUILD_SHARED_LIB}"
 )
 
 vcpkg_install_cmake()
