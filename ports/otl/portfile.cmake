0154d7403b5328de7925353ed8fd13c45299bd9c
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-10-07 13:29:19 +0200

    [otl] bump CONTROL

--- ports/otl/portfile.cmake
+++ ports/otl/portfile.cmake
@@ -2,15 +2,14 @@ include(vcpkg_common_functions)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "http://otl.sourceforge.net/otlv4_h2.zip"
-    FILENAME "otl-4.0.443.zip"
-    SHA512 ee866971a7546c5fbfcdde07c63eb2bbfc2c2a681174eb58b77e7ccb55af08e80674c40267ca7762136aa422eca2d8456e668d7ee495a43d8378fdfad084184b
+    FILENAME "otlv4_h2-4.0.443.zip"
+    SHA512 90a90d909586aae2088c87b5899244c01eef58aed7183d73b9be647f9c7a7bdcdc2f50f5c3cb564330b0061fb63e85f1b5522b4cf9390bc9baa5e2cb97ea3f3e
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     NO_REMOVE_ONE_LEVEL
-    REF 4.0.443
 )
 
 file(INSTALL ${SOURCE_PATH}/otlv4.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/otl)
