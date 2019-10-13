0892bf4518a268603a963abcc2ffe551c3922456
Stephen Webb <stephen.webb@sabreautonomous.com.au>
2019-10-13 12:39:20 +1100

    Allow the apr-util port to be used on non-windows systems

--- ports/apr-util-unix/portfile.cmake
+++ ports/apr-util-unix/portfile.cmake
@@ -3,7 +3,7 @@ include(vcpkg_common_functions)
 vcpkg_download_distfile(ARCHIVE
     URLS "http://archive.apache.org/dist/apr/apr-util-1.6.1.tar.bz2"
     FILENAME "apr-util-1.6.1.tar.bz2"
-    SHA512 0
+    SHA512 40eff8a37c0634f7fdddd6ca5e596b38de15fd10767a34c30bbe49c632816e8f3e1e230678034f578dd5816a94f246fb5dfdf48d644829af13bf28de3225205d
 )
 
 vcpkg_extract_source_archive_ex(
@@ -13,7 +13,7 @@ vcpkg_extract_source_archive_ex(
 
 message(STATUS "Configuring apr-util")
 vcpkg_execute_required_process(
-    COMMAND "./configure" --prefix=${CURRENT_INSTALLED_DIR}
+    COMMAND "./configure" --prefix=${CURRENT_INSTALLED_DIR} --with-apr=${CURRENT_INSTALLED_DIR} --with-openssl=${CURRENT_INSTALLED_DIR} --with-expat=${CURRENT_INSTALLED_DIR}
     WORKING_DIRECTORY "${SOURCE_PATH}"
     LOGNAME "autotools-config-${TARGET_TRIPLET}"
 )
