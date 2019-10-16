d7c8d72236e1fe2bacfb5295c2c269591edebc14
JackBoosY <yuzaiyang@beyondsoft.com>
2019-10-15 23:42:59 -0700

    [lipng/libpng-apng]Remove port libpng-apng and add apng as a feature with libpng.

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -1,15 +1,44 @@
 include(vcpkg_common_functions)
 
+set(LIBPNG_VER 1.6.37)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO glennrp/libpng
-    REF v1.6.37
+    REF v${LIBPNG_VER}
     SHA512 ccb3705c23b2724e86d072e2ac8cfc380f41fadfd6977a248d588a8ad57b6abe0e4155e525243011f245e98d9b7afbe2e8cc7fd4ff7d82fcefb40c0f48f88918
     HEAD_REF master
     PATCHES
         use-abort-on-all-platforms.patch
         fix-libm-unix.patch
 )
+
+# Download the apng patch
+set(LIBPNG_APNG_OPTION )
+if ("apng" IN_LIST FEATURES AND NOT EXISTS ${SOURCE_PATH}/../libpng-${LIBPNG_VER}-apng.patch)
+    vcpkg_download_distfile(LIBPNG_APNG_PATCH_ARCHIVE
+        URLS "https://downloads.sourceforge.net/project/libpng-apng/libpng16/${LIBPNG_VER}/libpng-${LIBPNG_VER}-apng.patch.gz"
+        FILENAME "libpng-${LIBPNG_VER}-apng.patch.gz"
+        SHA512 226adcb3a8c60f2267fe2976ab531329ae43c2603dab4d0cf8f16217d64069936b879f3d6516b75d259c47d6f5c5b1f24f887602206c8e46abde0fb7f5c7946b
+    )
+    
+    vcpkg_find_acquire_program(7Z)
+
+    vcpkg_execute_required_process(
+        COMMAND ${7Z} x ${LIBPNG_APNG_PATCH_ARCHIVE} -aoa
+        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src
+        LOGNAME extract-patch.log
+    )
+    
+    vcpkg_apply_patches(
+        SOURCE_PATH ${SOURCE_PATH}
+        PATCHES
+            skip-install-symlink.patch
+            ${SOURCE_PATH}/../libpng-${LIBPNG_VER}-apng.patch
+    )
+    
+    set(LIBPNG_APNG_OPTION "-DPNG_PREFIX=a")
+endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     set(PNG_STATIC_LIBS OFF)
@@ -23,6 +52,7 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
+        ${LIBPNG_APNG_OPTION}
         -DPNG_STATIC=${PNG_STATIC_LIBS}
         -DPNG_SHARED=${PNG_SHARED_LIBS}
         -DPNG_TESTS=OFF
