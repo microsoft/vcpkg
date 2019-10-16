23c83a4f8591b63e4c33785f8fc3b43134a205a4
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-10-16 17:27:25 +0200

    [gettext] add tool on windows

--- ports/aravis/portfile.cmake
+++ ports/aravis/portfile.cmake
@@ -1,17 +1,39 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
-message(STATUS "${PORT} currently requires the following tools from the system package manager:
-    glib-2.0
-    libxml2
-This can be installed on Ubuntu systems via apt-get install libglib2.0-dev libxml2-dev")
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AravisProject/aravis
     REF e49cfe86dfa5e526de1047db876315340cefce2b
     SHA512 1b93197031f1a4912911c320e88eda67f7c08db1570c22213f5f2ec41f17e17b6f04c513b8990871e07864137da3cf725389da2aceca4035303560f472ea2a83
     HEAD_REF master
+    PATCHES
+        allow_better_dependencies_search.patch
 )
+
+if(CMAKE_HOST_WIN32)
+    set(EXECUTABLE_SUFFIX ".exe")
+else()
+    set(EXECUTABLE_SUFFIX "")
+endif()
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
+vcpkg_add_to_path("${PYTHON3_DIR}")
+
+if(CMAKE_HOST_WIN32)
+    if(NOT EXISTS ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX})
+        if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX})
+            get_filename_component(PYTHON3_DIR_NAME "${PYTHON3_DIR}" NAME)
+            vcpkg_download_distfile(GET_PIP
+                URLS "https://bootstrap.pypa.io/3.3/get-pip.py"
+                FILENAME "tools/python/${PYTHON3_DIR_NAME}/get-pip.py"
+                SHA512 92e68525830bb23955a31cb19ebc3021ef16b6337eab83d5db2961b791283d2867207545faf83635f6027f2f7b7f8fee2c85f2cfd8e8267df25406474571c741
+            )
+            execute_process(COMMAND ${PYTHON3_DIR}/python${EXECUTABLE_SUFFIX} ${GET_PIP})
+        endif()
+        execute_process(COMMAND ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX} install python-gettext --user)
+    else()
+        execute_process(COMMAND ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX} python-gettext)
+    endif()
+endif()
 
 vcpkg_configure_meson(
     SOURCE_PATH ${SOURCE_PATH}
