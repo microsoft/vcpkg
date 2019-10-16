5c8bff1b9bc86035993cfeb7691728ede2c42787
Igor Sandler <isandler@topcon.com>
2019-10-16 13:25:29 -0700

    fix in port KDE Marble library

--- ports/marble/portfile.cmake
+++ ports/marble/portfile.cmake
@@ -35,12 +35,109 @@
 
 include(vcpkg_common_functions)
 
-vcpkg_from_git(
+function(vcpkg_from_git_1)
+  set(oneValueArgs OUT_SOURCE_PATH URL REF SHA)
+  set(multipleValuesArgs PATCHES)
+  cmake_parse_arguments(_vdud "" "${oneValueArgs}" "${multipleValuesArgs}" ${ARGN})
+
+  if(NOT DEFINED _vdud_OUT_SOURCE_PATH)
+    message(FATAL_ERROR "OUT_SOURCE_PATH must be specified.")
+  endif()
+
+  if(NOT DEFINED _vdud_URL)
+    message(FATAL_ERROR "The git url must be specified")
+  endif()
+
+  if( NOT _vdud_URL MATCHES "^https:")
+    # vcpkg_from_git does not support a SHA256 parameter because hashing the git archive is
+    # not stable across all supported platforms.  The tradeoff is to require https to download
+    # and the ref to be the git sha (i.e. not things that can change like a label)
+    message(FATAL_ERROR "The git url must be https")
+  endif()
+
+  if(NOT DEFINED _vdud_REF)
+    message(FATAL_ERROR "The git ref must be specified.")
+  endif()
+
+  # using .tar.gz instead of .zip because the hash of the latter is affected by timezone.
+  string(REPLACE "/" "-" SANITIZED_REF "${_vdud_REF}")
+  set(TEMP_ARCHIVE "${DOWNLOADS}/temp/${PORT}-${SANITIZED_REF}.tar.gz")
+  set(ARCHIVE "${DOWNLOADS}/${PORT}-${SANITIZED_REF}.tar.gz")
+  set(TEMP_SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/${SANITIZED_REF}")
+
+  if(NOT EXISTS "${ARCHIVE}")
+    if(_VCPKG_NO_DOWNLOADS)
+        message(FATAL_ERROR "Downloads are disabled, but '${ARCHIVE}' does not exist.")
+    endif()
+    message(STATUS "Fetching ${_vdud_URL}...")
+    find_program(GIT NAMES git git.cmd)
+    # Note: git init is safe to run multiple times
+    vcpkg_execute_required_process(
+      ALLOW_IN_DOWNLOAD_MODE
+      COMMAND ${GIT} init git-tmp
+      WORKING_DIRECTORY ${DOWNLOADS}
+      LOGNAME git-init-${TARGET_TRIPLET}
+    )
+    vcpkg_execute_required_process(
+      ALLOW_IN_DOWNLOAD_MODE
+      COMMAND ${GIT} fetch ${_vdud_URL} ${_vdud_REF} --depth 1 -n
+      WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+      LOGNAME git-fetch-${TARGET_TRIPLET}
+    )
+    _execute_process(
+      COMMAND ${GIT} rev-parse FETCH_HEAD
+      OUTPUT_VARIABLE REV_PARSE_HEAD
+      ERROR_VARIABLE REV_PARSE_HEAD
+      RESULT_VARIABLE error_code
+      WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+    )
+    if(error_code)
+        message(FATAL_ERROR "unable to determine FETCH_HEAD after fetching git repository")
+    endif()
+    string(REGEX REPLACE "\n$" "" REV_PARSE_HEAD "${REV_PARSE_HEAD}")
+    if(NOT REV_PARSE_HEAD STREQUAL _vdud_REF)
+		if(DEFINED _vdud_SHA)
+			if(NOT REV_PARSE_HEAD STREQUAL _vdud_SHA)
+				message(FATAL_ERROR "REF (${_vdud_SHA}) does not match FETCH_HEAD (${REV_PARSE_HEAD})")
+			endif()
+		else()
+			message(STATUS "SHA not defined; not sure if correct code was fetched")
+		endif()
+    endif()
+
+    file(MAKE_DIRECTORY "${DOWNLOADS}/temp")
+    vcpkg_execute_required_process(
+      ALLOW_IN_DOWNLOAD_MODE
+      COMMAND ${GIT} archive FETCH_HEAD -o "${TEMP_ARCHIVE}"
+      WORKING_DIRECTORY ${DOWNLOADS}/git-tmp
+      LOGNAME git-archive
+    )
+
+    get_filename_component(downloaded_file_dir "${ARCHIVE}" DIRECTORY)
+    file(MAKE_DIRECTORY "${downloaded_file_dir}")
+    file(RENAME "${TEMP_ARCHIVE}" "${ARCHIVE}")
+  else()
+    message(STATUS "Using cached ${ARCHIVE}")
+  endif()
+
+  vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    REF "${SANITIZED_REF}"
+    PATCHES ${_vdud_PATCHES}
+    NO_REMOVE_ONE_LEVEL
+  )
+
+  set(${_vdud_OUT_SOURCE_PATH} "${SOURCE_PATH}" PARENT_SCOPE)
+endfunction()
+
+vcpkg_from_git_1(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://anongit.kde.org/marble
-    REF 77e47155f186d03222c407d3967137d41cd33dae # should disscuss this with Marble community
-   PATCHES
-        md32.patch
+    REF refs/heads/Applications/17.04 # should disscuss this with Marble community
+	SHA 016b072716ed61ea7bd3a0ccffd1ab8a0d09de17
+   #PATCHES
+   #     md32.patch
 )
 
 set(POSTFIX d)
