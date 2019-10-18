db07879bae0cc0e6567bb676a6e21e20f0409afe
AlvinZhangH <260137639@qq.com>
2019-10-18 09:18:47 +0800

    [dataframe]Use a macro to distinguish the patch.

--- ports/dataframe/portfile.cmake
+++ ports/dataframe/portfile.cmake
@@ -2,25 +2,19 @@ include(vcpkg_common_functions)
 
 vcpkg_fail_port_install(MESSAGE "dataframe currently only supports Windows, Linux and Mac platforms" ON_TARGET "WindowsStore")
 
-if(NOT VCPKG_CMAKE_SYSTEM_NAME)
-    vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO hosseinmoein/DataFrame
-        REF V-1.5.0
-        SHA512 2eaef420d6b6d7eeeb0e78ec7c9e0bb06d21f8a2c6416c641def567d340b2fdd21f7981df65be2da54117b440afbbc8fa4c7e4d106d954e2b5a17d717322dc02
-        HEAD_REF master
-        PATCHES
-            fix-cmakelists.patch
-    )
-elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO hosseinmoein/DataFrame
-        REF V-1.5.0
-        SHA512 2eaef420d6b6d7eeeb0e78ec7c9e0bb06d21f8a2c6416c641def567d340b2fdd21f7981df65be2da54117b440afbbc8fa4c7e4d106d954e2b5a17d717322dc02
-        HEAD_REF master
-    )
-endif()
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(SOURCE_PATCH fix-cmakelists.patch)
+endif()	
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO hosseinmoein/DataFrame
+    REF V-1.5.0
+    SHA512 2eaef420d6b6d7eeeb0e78ec7c9e0bb06d21f8a2c6416c641def567d340b2fdd21f7981df65be2da54117b440afbbc8fa4c7e4d106d954e2b5a17d717322dc02
+    HEAD_REF master
+    PATCHES
+        ${SOURCE_PATCH}
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -31,9 +25,9 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-if(NOT VCPKG_CMAKE_SYSTEM_NAME)
+if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/dataframe)
-elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+elseif(VCPKG_TARGET_IS_LINUX)
     vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/dataframe)
 endif()
 
