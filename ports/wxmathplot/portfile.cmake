87cf1774f3e6b0e98a6d0f52546f6c2bfb4e322b
zi-m <zi.m.github.com@gmail.com>
2019-10-18 20:34:06 +0200

    wxmathplot

---
+++ ports/wxmathplot/portfile.cmake
@@ -0,0 +1,48 @@
+include(vcpkg_common_functions)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://sourceforge.net/code-snapshots/svn/w/wx/wxmathplot/svn/wxmathplot-svn-r95-trunk-mathplot.zip"
+    FILENAME "wxmathplot-svn-r95-trunk-mathplot.zip"
+    SHA512 2cd4353ee99d08b6070d865ce6d2f174ff586825d9cda9f407188a503e767be1d0fb624ddb5eb13754c499708a4c809f9b935c6293612ad8af613767fea11663
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH VCPKG_WX_FIND_SOURCE_PATH
+    REPO CaeruleusAqua/vcpkg-wx-find
+    REF 17993e942f677799b488a06ca659a8e46ff272c9
+    SHA512 0fe07d3669f115c9b6a761abd7743f87e67f24d1eae3f3abee4715fa4d6b76af0d1ea3a4bd82dbdbed430ae50295e1722615ce0ee7d46182125f5048185ee153
+    HEAD_REF master
+)
+
+file(COPY ${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake DESTINATION ${VCPKG_WX_FIND_SOURCE_PATH})
+file(COPY ${CMAKE_ROOT}/Modules/FindPackageMessage.cmake DESTINATION ${VCPKG_WX_FIND_SOURCE_PATH})
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  set(BUILD_SHARED_LIBS ON)
+else()
+  set(BUILD_SHARED_LIBS OFF)
+endif()
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+        -DCMAKE_MODULE_PATH=${VCPKG_WX_FIND_SOURCE_PATH}
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/wxmathplot)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+configure_file(${SOURCE_PATH}/debian/copyright ${CURRENT_PACKAGES_DIR}/share/wxmathplot/copyright COPYONLY)
