cb34f1cadd0df4fa1b19b47e16576d8f60b0cb5a
NancyLi1013 <lirui09@beyondsoft.com>
2019-10-17 22:59:57 -0700

    Update

--- ports/libbase58/portfile.cmake
+++ ports/libbase58/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_fail_port_install(MESSAGE "${PORT} currently only supports Linux platform"
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO luke-jr/libbase58
-    REF v0.1.4
-    SHA512 9b36fd7308a1a5486d77cb9baeeac49669a2c823998ff0885fee0757d6fca374f796b743e69af087e20a58b95556faf6c48106e034c09879937d96dae5fc7ac9
+    REF 16c2527608053d2cc2fa05b2e3b5ae96065d1410
+    SHA512 c5473ab33cff9cd242e980c77fa8fb9da7b33e7e50011f356ea98b692b23a24abf5dcade5fc09528e759cf0ea84a8fce75c28f7f6a9f409cbdeeaf49241a51da
     HEAD_REF mater
 )
 
@@ -19,4 +19,4 @@ vcpkg_install_make()
 vcpkg_copy_pdbs()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
