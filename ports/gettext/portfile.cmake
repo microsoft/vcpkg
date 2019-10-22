bfcb830cf2412a9fd5a000f7aa40cfba59497752
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-10-22 22:14:33 +0200

    update gettext

--- ports/gettext/portfile.cmake
+++ ports/gettext/portfile.cmake
@@ -1,28 +1,17 @@
-if(VCPKG_TARGET_IS_LINUX)
-    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-    if (NOT EXISTS "/usr/include/libintl.h")
-        message(FATAL_ERROR "Please use command \"sudo apt-get install gettext\" to install gettext on linux.")
-    endif()
-    file(COPY ${CMAKE_CURRENT_LIST_DIR}/unofficial-gettext-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/unofficial-gettext)
-    return()
-endif()
-
-#Based on https://github.com/winlibs/gettext
-
-set(GETTEXT_VERSION 0.19)
+set(GETTEXT_VERSION 0.20.1)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://ftp.gnu.org/pub/gnu/gettext/gettext-${GETTEXT_VERSION}.tar.gz" "https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gettext/gettext-${GETTEXT_VERSION}.tar.gz"
     FILENAME "gettext-${GETTEXT_VERSION}.tar.gz"
-    SHA512 a5db035c582ff49d45ee6eab9466b2bef918e413a882019c204a9d8903cb3770ddfecd32c971ea7c7b037c7b69476cf7c56dcabc8b498b94ab99f132516c9922
+    SHA512 af6d74986da285df0bdd59524bdf01bb12db448e5ea659dda3b60b660c4a9063c80e8c74cc8751334e065e98348ee0db0079e43c67d485a15e86ae236115fe06
 )
+
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     REF ${GETTEXT_VERSION}
     PATCHES
         0001-Fix-macro-definitions.patch
-        0002-Fix-uwp-build.patch
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)
@@ -43,9 +32,12 @@ if(VCPKG_TARGET_IS_WINDOWS)
     )
 
     vcpkg_install_cmake()
+    vcpkg_copy_pdbs()
 
     vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-gettext TARGET_PATH share/unofficial-gettext)
 else()
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
     vcpkg_configure_make(
         SOURCE_PATH ${SOURCE_PATH}
     )
@@ -54,8 +46,4 @@ else()
     file(COPY ${CMAKE_CURRENT_LIST_DIR}/unofficial-gettext-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/unofficial-gettext)
 endif()
 
-# Handle copyright
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/gettext)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/gettext/COPYING ${CURRENT_PACKAGES_DIR}/share/gettext/copyright)
-
-vcpkg_copy_pdbs()
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
