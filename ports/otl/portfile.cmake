871d15f1aeb4e7a84238aa04a4a550111966174b
JackBoosY <yuzaiyang@beyondsoft.com>
2019-10-22 19:22:39 -0700

    [otl]Revert https changes.

--- ports/otl/portfile.cmake
+++ ports/otl/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://otl.sourceforge.net/otlv4_h2.zip"
+    URLS "http://otl.sourceforge.net/otlv4_h2.zip"
     FILENAME "otlv4_h2-4.0.447.zip"
     SHA512 07663442272a327a7d9154f6e817c0166ed254cfe3b9d043762179e96180a70d3ba4b3762e5ef1ebdb18492e3425467c9ddad3a2c68aa93bb7d51d54e9712008
 )
