1ebe0933ab1148658837b73613e4afe4ac0e9348
NancyLi1013 <lirui09@beyondsoft.com>
2019-10-24 01:51:00 -0700

    Add feature and update

--- ports/polyhook2/portfile.cmake
+++ ports/polyhook2/portfile.cmake
@@ -6,23 +6,27 @@ vcpkg_from_github(
     REF  00709c8621af8a6f9e91200088178e6d9f751097
     SHA512 c6fe9ef9e21de440556cbeb8269e13ef0daafcbc760b04a06e1689d181b6a097c4de9a0f364f7e10f8b0b2f3e419e0ede62aaf4a2a9b16eb2fb57d24eb1b9b5c
     HEAD_REF master
-    PATCHES fix-build-error.patch
+    PATCHES 
+        fix-build-error.patch
+        fix-build-tests-error.patch
 )
 
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(BUILD_DLL ON)
-    set(BUILD_STATIC OFF)
-else()
-    set(BUILD_DLL OFF)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    tool BUILD_TOOLS
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     set(BUILD_STATIC ON)
+else()
+    set(BUILD_STATIC OFF)
 endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA  
-    OPTIONS
+    OPTIONS ${FEATURE_OPTIONS}
       -DFEATURE_INLINENTD=OFF
-      -DBUILD_DLL=${BUILD_DLL}
+      -DBUILD_DLL=ON
       -DBUILD_STATIC=${BUILD_STATIC}
 )
 
@@ -32,17 +36,8 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_copy_pdbs()
 
-file(GLOB EXE ${CURRENT_PACKAGES_DIR}/bin/*.exe)
-file(GLOB DEBUG_EXE ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-if(EXE OR DEBUG_EXE)
-    file(COPY ${EXE} ${DEBUG_EXE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools )
-    file(REMOVE ${EXE} ${DEBUG_EXE})
-endif()
-
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
-    
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)    
 endif()
 
 # Handle copyright
