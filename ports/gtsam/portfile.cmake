4d1e7799fcf6ebf10233b045fe21cc28d243b9d2
NancyLi1013 <lirui09@beyondsoft.com>
2019-10-25 00:37:44 -0700

    Update

--- ports/gtsam/portfile.cmake
+++ ports/gtsam/portfile.cmake
@@ -1,10 +1,8 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO borglab/gtsam
-    REF 3.2.3
-    SHA512 4224b22e30f1b15328bc3a6fbbc079470a71ac2e62abb23806b89ebe40d0c795538d73f6cc173cba44b3e2cec85e6e974c0fed5dc23b4556aa09c85bce57bbd4
+    REF be74732a091ca3ed8a95bb6f98331b7a52b19161
+    SHA512 4519195254e34e66e5aae6bb4eb423b80128284317eb24ecf26e11dadb6656ab8a99bc57c0dc8ff5f518a573d989189af435b4cad1e9414171b176c23af1e1c8
     HEAD_REF master
     PATCHES 
         fix-c2280-error.patch
