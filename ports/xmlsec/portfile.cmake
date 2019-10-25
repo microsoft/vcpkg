4b50ddd040758c0ca640f1e566fca01bc13c691a
SvenPStarFinanz <spa@starfinanz.de>
2019-10-25 23:52:51 +0200

    [xmlsec] Update to 1.2.29 (#8721)

--- ports/xmlsec/portfile.cmake
+++ ports/xmlsec/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lsh123/xmlsec
-    REF xmlsec-1_2_28
-    SHA512 fb0c775f6455ce5a5579a69bb91d60fe90c023e538c32bdf2a70aa413a53b22ef938349a3ce6b42bb23f8f70b4e00f1b9917f877487bb1507c927ec70c3d95f5
+    REF xmlsec-1_2_29
+    SHA512 b53eeac801cbcf4b18aa7f4fcec5fea5cae26e9cbef923d6e807ee98b988840943f00781ec6194b300403bd7f656c76ba96efce29c8b49920c9f0d67ba14ea57
     HEAD_REF master
     PATCHES 
         0001-uwp-fix.patch
