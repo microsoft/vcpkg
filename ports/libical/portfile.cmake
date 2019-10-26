4d45f0913a8f3a99f4e2c962d2c6ae9283a0b5cf
Allen Winter <allen.winter@kdab.com>
2019-10-26 08:35:18 -0400

    ports/libical/portfile.cmake - cleanup

--- ports/libical/portfile.cmake
+++ ports/libical/portfile.cmake
@@ -7,24 +7,12 @@ vcpkg_from_github(
     SHA512 624b70b54da1ff30085fd16bf136496f179088dd089a3bea7e9cfcf3ad07588907d99ff07782b33f5fdfd4fbfc0bae9daa42bb965380b28bf1c9ce4481b698cb
 )
 
-# Only build the requested library
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    SET(LIBICAL_BUILD_STATIC_ONLY ON)
-    SET(LIBICAL_BUILD_SHARED_ONLY OFF)
-else()
-    #By default, build both static and dynamic
-    SET(LIBICAL_BUILD_STATIC_ONLY OFF)
-    SET(LIBICAL_BUILD_SHARED_ONLY OFF)
-endif()
-#TODO: at this time glib cannot build statically -- keep that in mind when/if adding glib features
-
 #Perl is required by the buildsystem.
 vcpkg_find_acquire_program(PERL)
 
 set(BUILD_ICAL_GLIB OFF)
 #TODO need pkgconfig, glib and libxml2 to build libical-glib
-#TODO pkgconfig is also needed but there is no ports yet.
-#if("glib" IN_LIST FEATURES AND "libxml2" IN_LIST FEATURES AND "pkgconfig" IN_LIST FEATURES)
+#if("glib" IN_LIST FEATURES AND "libxml2" IN_LIST FEATURES)
 #set(BUILD_ICAL_GLIB ON)
 #endif()
 
@@ -32,8 +20,8 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DSHARED_ONLY=${LIBICAL_BUILD_SHARED_ONLY}
-        -DSTATIC_ONLY=${LIBICAL_BUILD_STATIC_ONLY}
+        -DSHARED_ONLY=False
+        -DSTATIC_ONLY=False
         -DUSE_BUILTIN_TZDATA=True
         -DICAL_BUILD_DOCS=False
         -DICAL_GLIB=${BUILD_ICAL_GLIB}
