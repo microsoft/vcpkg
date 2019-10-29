73ec3cc30ce1079ced9c777b6261f7c354106724
JackBoosY <yuzaiyang@beyondsoft.com>
2019-10-29 02:39:37 -0700

    [imgui]Set imgui as a header-only library.

--- ports/imgui/portfile.cmake
+++ ports/imgui/portfile.cmake
@@ -1,7 +1,3 @@
-include(vcpkg_common_functions)
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ocornut/imgui
@@ -15,31 +11,38 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS_DEBUG
-        -DIMGUI_SKIP_HEADERS=ON
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+vcpkg_copy_pdbs()
 
 if ("example" IN_LIST FEATURES)
+    vcpkg_check_linkage(ONLY_STATIC_CRT)
+    
     if (NOT VCPKG_TARGET_IS_WINDOWS)
         message(FATAL_ERROR "Feature example only support windows.")
     endif()
+    
+    if (VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
+        set(BUILD_ARCH "Win32")
+    elseif (VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
+        set(BUILD_ARCH "x64")
+    else()
+        message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+    endif()
+    
     vcpkg_build_msbuild(
         USE_VCPKG_INTEGRATION
         PROJECT_PATH ${SOURCE_PATH}/examples/imgui_examples.sln
+        PLATFORM ${BUILD_ARCH}
     )
-    
-    # Install headers
-    file(GLOB IMGUI_EXAMPLE_INCLUDES ${SOURCE_PATH}/examples/*.h)
-    file(INSTALL ${IMGUI_EXAMPLE_INCLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
     
     # Install tools
     file(GLOB_RECURSE IMGUI_EXAMPLE_BINARIES ${SOURCE_PATH}/examples/*${VCPKG_TARGET_EXECUTABLE_SUFFIX})
     file(INSTALL ${IMGUI_EXAMPLE_BINARIES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
 endif()
 
-vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
-configure_file(${SOURCE_PATH}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/imgui/copyright COPYONLY)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
