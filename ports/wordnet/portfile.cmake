6ee560d2ac5aa4ebd1606be695102a127d443788
Chao Jia <chao.cj.jia@gmail.com>
2019-10-30 18:11:01 +0100

    [wordnet]fix-uwp

--- ports/wordnet/portfile.cmake
+++ ports/wordnet/portfile.cmake
@@ -1,15 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
@@ -25,13 +13,9 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE} 
-    # (Optional) A friendly name to use instead of the filename of the archive (e.g.: a version number or tag).
     REF ${VERSION}
-    # (Optional) Read the docs for how to generate patches at: 
-    # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
     PATCHES
-        extern_c_global_vars.patch
-    #   002_more_port_fixes.patch
+        fix_gobal_vars_uninit_local_ptr.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
@@ -57,10 +41,8 @@ set (WORDNET_DICT_PATH "${CURRENT_PACKAGES_DIR}/tools/${PORT}/dict")
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    PREFER_NINJA
     OPTIONS -DWORDNET_DICT_PATH=${WORDNET_DICT_PATH}
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
 
 vcpkg_install_cmake()
@@ -70,5 +52,3 @@ vcpkg_fixup_cmake_targets()
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/wordnet RENAME copyright)
 
-# Post-build test for cmake libraries
-# vcpkg_test_cmake(PACKAGE_NAME wordnet)
