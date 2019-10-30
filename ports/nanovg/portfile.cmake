13c7067f6946d65801d85fd20d5e328be1dbcdf1
JackBoosY <yuzaiyang@beyondsoft.com>
2019-10-30 02:59:38 -0700

    [nanovg]Add depends port stb. Do not install stb headers.

--- ports/nanovg/portfile.cmake
+++ ports/nanovg/portfile.cmake
@@ -1,4 +1,3 @@
-include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO memononen/nanovg
@@ -12,10 +11,7 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/nanovgConfig.cmake DESTINATION ${SOURCE_PATH
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA
 )
 
 vcpkg_install_cmake()
