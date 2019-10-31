06bca4b258c27fdf81d37c7918bb7325b85a8834
JackBoosY <yuzaiyang@beyondsoft.com>
2019-10-31 01:34:26 -0700

    [aws-c-common]Fix cmake target path.

--- ports/aws-c-common/portfile.cmake
+++ ports/aws-c-common/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
     PATCHES
         disable-error-4068.patch # This patch fixes dependency port compilation failure
         disable-internal-crt-option.patch # Disable internal crt option because vcpkg contains crt processing flow
+        fix-cmake-target-path.patch # Shared libraries and static libraries are not built at the same time
 )
 
 vcpkg_configure_cmake(
