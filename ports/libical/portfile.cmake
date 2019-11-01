5cf6587e10ffd2aed9ffcc1d7f4afa42c997e13f
Allen Winter <allen.winter@kdab.com>
2019-11-01 10:51:02 -0400

    [libical] implement fixes from pull request

--- ports/libical/portfile.cmake
+++ ports/libical/portfile.cmake
@@ -1,14 +1,14 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libical/libical
-    REF v3.0.6
-    SHA512 624b70b54da1ff30085fd16bf136496f179088dd089a3bea7e9cfcf3ad07588907d99ff07782b33f5fdfd4fbfc0bae9daa42bb965380b28bf1c9ce4481b698cb
+    REF 0595c7d45ef5e75705f2d80e03e5310d9f78438c #v3.0.6 tag
+    SHA512 611dcb28500b9db23262926e6869f3e3920ece585f059be4e09c356d949014770b7f1f95d1e3e75e770b6c38f91c4ae8a014df8dc86d5191f12bc5d9b0549216
 )
 
 #Perl is required by the buildsystem.
 vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_PATH})
 
 set(BUILD_ICAL_GLIB OFF)
 #TODO need pkgconfig, glib and libxml2 to build libical-glib
@@ -28,19 +28,12 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
-file(COPY ${CURRENT_PACKAGES_DIR}/lib/cmake/LibICal
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/libical/cmake
-    FILES_MATCHING PATTERN "*.cmake" )
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/cmake)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/LibIcal TARGET_PATH share/LibIcal)
 
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libical)
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libical/LICENSE.txt)
-file(COPY ${SOURCE_PATH}/LICENSE.LGPL21.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/libical)
-file(COPY ${SOURCE_PATH}/LICENSE.MPL2.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/libical)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/libical/COPYING ${CURRENT_PACKAGES_DIR}/share/libical/copyright)
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
