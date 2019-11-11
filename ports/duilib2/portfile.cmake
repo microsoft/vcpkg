da424164737a67045cc047850d18b8b8af05eb7f
winsoft666 <winsoft666@outlook.com>
2019-11-11 16:36:58 +0800

    duilib2 version

--- ports/duilib2/portfile.cmake
+++ ports/duilib2/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO winsoft666/duilib2
-    REF v1.27
-    SHA512 e107a495337dc2c13eb1dafb7b167bfb111c67a755c6198bade418952fb0f42b7ed07c6c696c7a3aec32b1cf46f77ae327f19902b3210e493604da8e4a386d86
+    REF v1.30
+    SHA512 3900df0c2a40e99f964a9ee0b31b5e54933d5dfa153253320257f46d5dd9b5e949511bf5cd41d2d3a6f21fd1374a4d0827f54f7401fad4006b90c889745d876e
     HEAD_REF master
 )
 
