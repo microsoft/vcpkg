ae514db3ad67f6e27ed38f55d7e4a2648f761722
JackBoosY <yuzaiyang@beyondsoft.com>
2019-11-11 21:23:11 -0800

    [libsodium]Change build mode to vcpkg_configure_make/vcpkg_install_make in Linux.

--- ports/libsodium/portfile.cmake
+++ ports/libsodium/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jedisct1/libsodium
@@ -8,47 +6,59 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
-    ${SOURCE_PATH}/CMakeLists.txt
-    COPYONLY
-)
-
-configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/sodiumConfig.cmake.in
-    ${SOURCE_PATH}/sodiumConfig.cmake.in
-    COPYONLY
-)
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DBUILD_TESTING=OFF
-)
-
-vcpkg_install_cmake()
-
-vcpkg_copy_pdbs()
-
-vcpkg_fixup_cmake_targets(
-    CONFIG_PATH lib/cmake/unofficial-sodium
-    TARGET_PATH share/unofficial-sodium
-)
+if (VCPKG_TARGET_IS_WINDOWS)
+    configure_file(
+        ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
+        ${SOURCE_PATH}/CMakeLists.txt
+        COPYONLY
+    )
+    
+    vcpkg_configure_cmake(
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS
+            -DBUILD_TESTING=OFF
+    )
+    
+    vcpkg_install_cmake()
+    
+    vcpkg_copy_pdbs()
+    
+    vcpkg_fixup_cmake_targets(
+        CONFIG_PATH lib/cmake/unofficial-sodium
+        TARGET_PATH share/unofficial-sodium
+    )
+    
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+        vcpkg_replace_string(
+            ${CURRENT_PACKAGES_DIR}/include/sodium/export.h
+            "#ifdef SODIUM_STATIC"
+            "#if 1 //#ifdef SODIUM_STATIC"
+        )
+    endif ()
+else()
+    set(EXTRA_OPTS )
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL dymaic)
+        set(EXTRA_OPTS ${EXTRA_OPTS} --enable-shared)
+    else()
+        set(EXTRA_OPTS ${EXTRA_OPTS} --enable-static)
+    endif()
+    
+    vcpkg_configure_make(
+        SOURCE_PATH ${SOURCE_PATH}
+        AUTOCONFIG
+        OPTIONS ${EXTRA_OPTS}
+        OPTIONS_DEBUG --enable-debug 
+    )
+    
+    vcpkg_install_make()
+endif()
 
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/include
 )
 
 file(REMOVE ${CURRENT_PACKAGES_DIR}/include/Makefile.am)
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string(
-        ${CURRENT_PACKAGES_DIR}/include/sodium/export.h
-        "#ifdef SODIUM_STATIC"
-        "#if 1 //#ifdef SODIUM_STATIC"
-    )
-endif ()
 
 configure_file(
     ${SOURCE_PATH}/LICENSE
