b576ab4996ffc07049dfdef3140b18b414faafbd
Matthew Albrecht <mattparks5855@gmail.com>
2019-11-12 21:13:59 -0600

    [glslang] update to stable release November 1, 2019

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -3,23 +3,24 @@ include(vcpkg_common_functions)
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO KhronosGroup/glslang
-  REF f88e5824d2cfca5edc58c7c2101ec9a4ec36afac
-  SHA512 92dc287e8930db6e00bde23b770f763dc3cf8a405a37b682bbd65e1dbde1f1f5161543fcc70b09eef07a5ce8bbe8f368ef84ac75003c122f42d1f6b9eaa8bd50
-  HEAD_REF master
-  PATCHES
-    CMakeLists-targets.patch
-    CMakeLists-windows.patch
-)
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO KhronosGroup/glslang
+	REF 7.13.3496
+	SHA512 5c096ed340b03150c3bef16b17cbd44ae7fa88ca55a206ac4a0879ac10895d7025561669ccea33c703c37b9bcca9b8d64ff604a6cdc175fcd3af06051f721367
+	HEAD_REF master
+    PATCHES
+		CMakeLists-targets.patch
+	)
 
 vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
-  OPTIONS
-    -DCMAKE_DEBUG_POSTFIX=d
-    -DSKIP_GLSLANG_INSTALL=OFF
-)
+	SOURCE_PATH ${SOURCE_PATH}
+	PREFER_NINJA
+	OPTIONS
+		-DCMAKE_DEBUG_POSTFIX=d
+		-DSKIP_GLSLANG_INSTALL=OFF
+		-DENABLE_AMD_EXTENSIONS=OFF
+		-DENABLE_NV_EXTENSIONS=OFF
+	)
 
 vcpkg_install_cmake()
 
@@ -31,7 +32,6 @@ file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools")
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
 
-# Handle copyright
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/glslang)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 vcpkg_test_cmake(PACKAGE_NAME glslang)
