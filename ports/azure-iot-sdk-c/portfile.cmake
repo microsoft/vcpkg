1f8cc53772eb72cb149c46bd001ec20f773445cf
John Spaith <jspaith@windows.microsoft.com>
2019-11-14 13:24:23 -0800

    Updating to latest code

--- ports/azure-iot-sdk-c/portfile.cmake
+++ ports/azure-iot-sdk-c/portfile.cmake
@@ -6,8 +6,8 @@ if("public-preview" IN_LIST FEATURES)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Azure/azure-iot-sdk-c
-        REF abb906e01060f7c837cbb6522f75f31c7b10fa4a
-        SHA512 9549a799de741964cea2c436a8990b78a04715c43237bb8c3837237e66aa43ada25d74001eada160cad3f5fe71cd32fc054584fade686d931cd51f4ae44d69bd
+        REF bda4b775b11ee3baea7ed7bb37b0e1354b513919
+        SHA512 cf658ff4da328c7fcfa552fb5abd8d1e73cce8b8bfc1e67fa898bd1f72f290a48c61c6333241c8a75bbce98ed1f7eb4397c8af9447fabeb4c08b5da8a84fcc40
         HEAD_REF public-preview
         PATCHES improve-external-deps.patch
     )
