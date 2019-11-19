aa1f04fdc109b69877a48ccce368156ed21c7327
Alexander Neumann <Alexander.Neumann@hamburg.de>
2019-11-19 21:51:19 +0100

    forgot to commit the triplet changes

--- triplets/x64-windows-clang.cmake
+++ triplets/x64-windows-clang.cmake
@@ -3,15 +3,16 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_PLATFORM_TOOLSET v142) 
 set(VCPKG_SKIP_POST_BUILD_LIB_ARCH_CHECK "ON") # Skips post build architecture checks of libs and dlls. Only works with VS link.exe
-set(LLVM_BINDIR "C:/Program Files/LLVM/bin")
-set(VCPKG_C_COMPILER "${LLVM_BINDIR}/clang.exe")
-set(VCPKG_CXX_COMPILER "${LLVM_BINDIR}/clang++.exe")
-set(VCPKG_LINKER "${LLVM_BINDIR}/lld-link.exe")
-set(VCPKG_AR "${LLVM_BINDIR}/llvm-ar.exe")
+set(LLVM_BINDIR "C:\\Program Files\\LLVM\\bin")
+set(ENV{Path} "${LLVM_BINDIR};$ENV{Path}")
+set(VCPKG_C_COMPILER "clang")
+set(VCPKG_CXX_COMPILER "clang++")
+set(VCPKG_LINKER "lld-link")
+set(VCPKG_AR "llvm-ar.exe")
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchain/win-clang-toolchain.cmake")
-message(STATUS "${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}")
 set(VCPKG_DEFAULT_CMAKE_GENERATOR Ninja)
-set(ENV{VCPKG_KEEP_ENV_VARS} "Path;INCLUDE;LIB") # start building this triplet from a dev cmd with vcvars loaded. Needs to be set manually for some reason 
+set(VCPKG_LOAD_ENVIROMNENT_BATCH "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat")
+#set(ENV{VCPKG_KEEP_ENV_VARS} "Path;INCLUDE;LIB") # start building this triplet from a dev cmd with vcvars loaded. Needs to be set manually for some reason 
 #set(VCPKG_C_FLAGS) # Injects additional C build flags
 #set(VCPKG_CXX_FLAGS) # Injects additional C++ build flags
 #set(VCPKG_C_FLAGS_DEBUG) # Injects additional C build flags in debug mode
