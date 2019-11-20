1d5a3d222c0aa5e4bdfc3f7c8e3902ff1bd0f4dc
Yauheni Akhotnikau <eao197@gmail.com>
2019-11-20 18:56:19 +0300

    json-dto updated to v.0.2.9.

--- ports/json-dto/portfile.cmake
+++ ports/json-dto/portfile.cmake
@@ -1,10 +1,10 @@
 include(vcpkg_common_functions)
 
-vcpkg_from_bitbucket(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO sobjectizerteam/json_dto-0.2
-    REF v.0.2.8
-    SHA512 50a2d8d31f4cf67bdf84a58bae5f95642f4be571e8e052a48830be119d5e3c4ddbb19c5ac97fc0f8383c9958d64ec9be4ce23019c1da4f2cbf4b8ddbf23f5ad7
+    REPO stiffstream/json_dto
+    REF v.0.2.9
+    SHA512 6a34e24c784f002bb3b025af93dc24c485090bc1f9ce55472a54929fe58fa4b1bf6f1ff5d2fea9fb33c8bc87aeae6a926cbb2d196f1e7172bb1a356935f3a7b6
 )
 
 vcpkg_configure_cmake(
@@ -26,3 +26,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/debug)
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/json-dto)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/json-dto/LICENSE ${CURRENT_PACKAGES_DIR}/share/json-dto/copyright)
+
