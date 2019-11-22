76f1a13347e9f9a00c32254b21b7a3da3bcdb14e
ggainant <ggainant@gmail.com>
2019-11-21 19:48:23 -0600

    [libyaml] Preserve strdup() on Mac OSX

--- ports/libyaml/portfile.cmake
+++ ports/libyaml/portfile.cmake
@@ -6,9 +6,15 @@ vcpkg_from_github(
     REF 0.2.2
     SHA512 455494591014a97c4371a1f372ad09f0d6e487e4f1d3419c98e9cd2f16d43a0cf9a0787d7250bebee8b8d400df4626f5acd81e90139e54fa574a66ec84964c06
     HEAD_REF master
-	PATCHES
-		fix-POSIX_name.patch
 )
+
+if (VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_apply_patches(
+        SOURCE_PATH ${SOURCE_PATH}
+        PATCHES
+            fix-POSIX_name.patch
+    )
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
