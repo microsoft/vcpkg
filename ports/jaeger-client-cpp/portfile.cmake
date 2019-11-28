c8f52d4b7887e51c6873b393964589c4717d5361
xgcssch <soenke.schau@alte-leipziger.de>
2019-11-28 09:29:25 +0100

    Add patchfile

--- ports/jaeger-client-cpp/portfile.cmake
+++ ports/jaeger-client-cpp/portfile.cmake
@@ -1,7 +1,3 @@
-# CURRENT_PACKAGES_DIR = Root of installed Target files
-# SOURCE_PATH = Root of unpacked Sources
-# CMAKE_CURRENT_SOURCE_DIR = Root of this vcpkg instance
-
 include(vcpkg_common_functions)
 
 # Get jaeger-idl from github
@@ -37,13 +33,15 @@ vcpkg_from_github(
     REF v0.5.0
     SHA512 d855ecfbb1dadffc8c21ff390717a732e743a1e751759bb85e800a0057d5ec5885080ca5ab9d89761b05d68d71817bb5780f42411a92cb5152a2a0f84b0b035a
     HEAD_REF master
+    PATCHES 
+       "fix-CMakeLists.patch"
 )
 
 # Do not use hunter, not testtools and build opentracing plugin
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DHUNTER_ENABLED=0 -DBUILD_TESTING=0 -DJAEGERTRACING_PLUGIN=1
+    OPTIONS -DHUNTER_ENABLED=0 -DBUILD_TESTING=0 -DJAEGERTRACING_PLUGIN=0 -DJAEGERTRACING_BUILD_EXAMPLES=0
 )
 
 # Copy generated files over to jaeger-client-cpp 
@@ -53,23 +51,15 @@ file(COPY ${IDL_SOURCE_FILES} DESTINATION ${SOURCE_PATH}/src/jaegertracing/thrif
 # Generate Jaeger client
 vcpkg_install_cmake()
 
-# Copy includefiles
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/include/jaegertracing)
-file(GLOB JCCPP_INCLUDE_FILES LIST_DIRECTORIES true ${SOURCE_PATH}/src/jaegertracing/*.h)
-file(COPY ${JCCPP_INCLUDE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/jaegertracing)
-file(COPY ${CURRENT_PACKAGES_DIR}/src/jaegertracing/Constants.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/jaegertracing)
-
-# Copy binaries
-file(COPY ${CURRENT_PACKAGES_DIR}/jaegertracing_plugin.dll DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
-# Copy Libraries
-file(COPY ${CURRENT_PACKAGES_DIR}/jaegertracing_plugin.dll ${CURRENT_PACKAGES_DIR}/jaegertracing-static.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
-# Copy CMake files
-#file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp)
-#file(GLOB JCCPP_CMAKE_FILES LIST_DIRECTORIES true ${SOURCE_PATH}/src/jaegertracing/*.h)
-#file(COPY ${CURRENT_PACKAGES_DIR}/jaegertracing_plugin.dll ${CURRENT_PACKAGES_DIR}/jaegertracing-static.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib/)
+# Integrate Debug CMake file
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/share/jaeger-client-cpp/jaegertracingTargets-debug.cmake ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp/jaegertracingTargets-debug.cmake)
+# Cleanup unused Debug files
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp/copyright)
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp/LICENSE ${CURRENT_PACKAGES_DIR}/share/jaeger-client-cpp/copyright)
 
 # Cleanup
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/jaegertracing/testutils)
