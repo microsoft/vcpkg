b4cd163dc68b5a68a3522a3153f906009bb15a99
AlvinZhangH <260137639@qq.com>
2019-11-28 14:56:33 +0800

    [imgui] Add feature internal to add imgui_internal.h

--- ports/imgui/portfile.cmake
+++ ports/imgui/portfile.cmake
@@ -12,11 +12,18 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    internal   ENABLE_INTERNAL
+)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS_DEBUG
         -DIMGUI_SKIP_HEADERS=ON
+        ${FEATURE_OPTIONS}
+    OPTIONS_RELEASE	
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_install_cmake()
