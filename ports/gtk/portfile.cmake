12b10de593d488a9d158fc1c5c449d5a58bf24f6
Danny Angelo Carminati Grein <fungos@gmail.com>
2019-11-29 22:34:28 -0500

    [gtk] Bump to gtk+-3.24.10 which is the first version using meson build
    - Fixes #7412 by using gtk 3.24.10.
    
    - Possibly fixes #6554, #7827, #8103 as now all the missing assets are
    correctly copied into installed/<triplet>/share/ and should be deployed
    with the final application.
    
    This simplify the gtk port and correctly generate the missing files.
    
    The source of the problem is with the underlying build system, as these
    files were installed by the demo `icon-browser`, but the
    gtk-update-icon-cache were executed by the main gtk project, failing if
    the demo wasn't built or installed.
    
    - This also fixes #7641 as now the meson build will correctly generate
    the missing .pc files.
    
    - This also fixes #9035, #7150 by fixing `vcpkg_configure_meson.cmake`
    which fails on Linux/OSX because it indiscriminately try to pass msvc
    cl arguments to the cmake c/xx flags. Although this works, it is not
    the ideal fix. The correct fix would be to do as suggested in
    issue #8271. Also see issue #8630.

--- ports/gtk/portfile.cmake
+++ ports/gtk/portfile.cmake
@@ -1,11 +1,11 @@
 
 include(vcpkg_common_functions)
-set(GTK_VERSION 3.22.19)
+set(GTK_VERSION 3.24.10)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/gnome/sources/gtk+/3.22/gtk+-${GTK_VERSION}.tar.xz"
+    URLS "https://ftp.gnome.org/pub/gnome/sources/gtk+/3.24/gtk+-${GTK_VERSION}.tar.xz"
     FILENAME "gtk+-${GTK_VERSION}.tar.xz"
-    SHA512 c83198794433ee6eb29f8740d59bd7056cd36808b4bff1a99563ab1a1742e6635dab4f2a8be33317f74d3b336f0d1adc28dd91410da056b50a08c215f184dce2
+    SHA512 1f7980189f522fd3646fb480b965c21801cc30b3316eb8bad8ded1efd25d3054f62160ddbe9ea241628c11b24f746024fbc3d22b17b9bd61fa6c301ab91d6498
 )
 
 vcpkg_extract_source_archive_ex(
@@ -13,30 +13,22 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
+vcpkg_configure_meson(
+	SOURCE_PATH ${SOURCE_PATH}
+	OPTIONS
+		-Dexamples=false
+		-Dtests=false
+		--backend=ninja
+		#-Ddemos=false # WE NEED demos, because the icons come from here, otherwise the update icon cache will fail to build!
+)
 
-# generate sources using python script installed with glib
-if(NOT EXISTS ${SOURCE_PATH}/gtk/gtkdbusgenerated.h OR NOT EXISTS ${SOURCE_PATH}/gtk/gtkdbusgenerated.c)
-    vcpkg_find_acquire_program(PYTHON3)
-    set(GLIB_TOOL_DIR ${CURRENT_INSTALLED_DIR}/tools/glib)
-
-    vcpkg_execute_required_process(
-        COMMAND ${PYTHON3} ${GLIB_TOOL_DIR}/gdbus-codegen --interface-prefix org.Gtk. --c-namespace _Gtk --generate-c-code gtkdbusgenerated ./gtkdbusinterfaces.xml
-        WORKING_DIRECTORY ${SOURCE_PATH}/gtk
-        LOGNAME source-gen)
-endif()
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DGTK_VERSION=${GTK_VERSION}
-    OPTIONS_DEBUG
-        -DGTK_SKIP_HEADERS=ON)
-
-vcpkg_install_cmake()
+vcpkg_install_meson()
 vcpkg_copy_pdbs()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/gtk)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/gtk/COPYING ${CURRENT_PACKAGES_DIR}/share/gtk/copyright)
