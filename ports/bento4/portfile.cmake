0aef91e21725a32c0cb8154860b62f21612f92af
Ramadan <ramadan.ahmedr93@gmail.com>
2019-11-30 14:13:18 +0200

    [bento4] Fix install

--- ports/bento4/portfile.cmake
+++ ports/bento4/portfile.cmake
@@ -18,6 +18,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake/Bento4)
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_copy_pdbs()
