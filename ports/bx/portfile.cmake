0d9e7cfa415c1fc45689525080d743d2047a3a9e
Outurnate <joseph@outurnate.com>
2019-11-30 16:29:26 -0500

    Move GENie common functions to common script

--- ports/bx/portfile.cmake
+++ ports/bx/portfile.cmake
@@ -24,69 +24,7 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_DIR
 )
 
 # Set up GENie (custom project generator)
-
-if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --with-dynamic-runtime)
-endif()
-
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=x32)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=x64)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL arm OR VCPKG_TARGET_ARCHITECTURE STREQUAL arm64)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=ARM)
-else()
-    message(WARNING "Architecture may be not supported: ${VCPKG_TARGET_ARCHITECTURE}")
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --platform=${VCPKG_TARGET_ARCHITECTURE})
-endif()
-
-if(TARGET_TRIPLET MATCHES osx)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=macosx)
-elseif(TARGET_TRIPLET MATCHES linux)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=linux)
-elseif(TARGET_TRIPLET MATCHES windows)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --os=windows)
-elseif(TARGET_TRIPLET MATCHES uwp)
-    set(GENIE_OPTIONS ${GENIE_OPTIONS} --vs=winstore100)
-endif()
-
-# GENie does not allow cmake+msvc, so we use msbuild in windows
-if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-    if(VCPKG_PLATFORM_TOOLSET STREQUAL "v140")
-        set(GENIE_ACTION vs2015)
-    elseif(VCPKG_PLATFORM_TOOLSET STREQUAL "v141")
-        set(GENIE_ACTION vs2017)
-    elseif(VCPKG_PLATFORM_TOOLSET STREQUAL "v142")
-        set(GENIE_ACTION vs2019)
-    else()
-        message(FATAL_ERROR "Unsupported Visual Studio toolset: ${VCPKG_PLATFORM_TOOLSET}")
-    endif()
-    set(PROJ_FOLDER ${GENIE_ACTION})
-    if(TARGET_TRIPLET MATCHES uwp)
-        set(PROJ_FOLDER ${PROJ_FOLDER}-winstore100)
-    endif()
-else()
-    set(GENIE_ACTION cmake)
-    set(PROJ_FOLDER ${GENIE_ACTION})
-endif()
-
-if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
-    set(GENIE "${SOURCE_DIR}/tools/bin/windows/genie.exe")
-elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
-    set(GENIE "${SOURCE_DIR}/tools/bin/darwin/genie")
-elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
-    set(GENIE "${SOURCE_DIR}/tools/bin/linux/genie")
-else()
-    message(FATAL_ERROR "Unsupported host platform: ${CMAKE_HOST_SYSTEM_NAME}")
-endif()
-
-# Run GENie
-
-vcpkg_execute_required_process(
-    COMMAND ${GENIE} ${GENIE_OPTIONS} ${GENIE_ACTION}
-    WORKING_DIRECTORY "${SOURCE_DIR}"
-    LOGNAME "genie-${TARGET_TRIPLET}"
-)
+vcpkg_configure_genie("${SOURCE_DIR}/tools")
 
 if(GENIE_ACTION STREQUAL cmake)
     # Run CMake
@@ -121,9 +59,6 @@ else()
         INCLUDES_SUBPATH "include"
     )
 endif()
-
-#file(INSTALL
-#     "${SOURCE_DIR}/include/
 
 # Post-build test for cmake libraries
 vcpkg_test_cmake(PACKAGE_NAME ${PORT})
