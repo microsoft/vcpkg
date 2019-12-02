30153eaad372f1bf40b67965cab71d19cc839a4e
Marc Bertola <5149167+marcbertola@users.noreply.github.com>
2019-12-01 21:24:10 -0500

    Add NVIDIA Codec support for Windows, Linux and Linux for Tegra

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ffmpeg/ffmpeg
@@ -129,6 +127,12 @@ if("avresample" IN_LIST FEATURES)
     set(OPTIONS "${OPTIONS} --enable-avresample")
 endif()
 
+if("nvcodec" IN_LIST FEATURES)
+    set(OPTIONS "${OPTIONS} --enable-cuda --enable-nvenc --enable-cuvid --disable-libnpp")
+else()
+    set(OPTIONS "${OPTIONS} --disable-cuda --disable-nvenc --disable-cuvid --disable-libnpp")
+endif()
+
 set(OPTIONS_CROSS "")
 
 if (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
@@ -175,12 +179,13 @@ if(VCPKG_TARGET_IS_WINDOWS)
 endif()
 
 set(ENV_LIB_PATH "$ENV{${LIB_PATH_VAR}}")
+set(ENV{PKG_CONFIG_PATH} "${CURRENT_PACKAGES_DIR}/../ffnvcodec_${TARGET_TRIPLET}/lib/pkgconfig")
 
 message(STATUS "Building Options: ${OPTIONS}")
 
-# Relase build
+# Release build
 if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL release)
-    message(STATUS "Building Relase Options: ${OPTIONS_RELEASE}")
+    message(STATUS "Building Release Options: ${OPTIONS_RELEASE}")
     set(ENV{${LIB_PATH_VAR}} "${CURRENT_INSTALLED_DIR}/lib${SEP}${ENV_LIB_PATH}")
     set(ENV{CFLAGS} "${VCPKG_C_FLAGS} ${VCPKG_C_FLAGS_RELEASE}")
     set(ENV{LDFLAGS} "${VCPKG_LINKER_FLAGS}")
--- ports/ffnvcodec/portfile.cmake
+++ ports/ffnvcodec/portfile.cmake
@@ -1,13 +1,50 @@
-# Header-only-library
+# Get nvcodec
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO FFmpeg/nv-codec-headers
-    REF n9.1.23.0 
-    SHA512 d9cb1ad496d971da31165e643c6c4f433561a856050503783051604f24ea5f9997859b05695632ea94ce9659966915789e6d7f7d536764804c9f673d1c8c63e4
+    REF n9.0.18.1
+    SHA512 4306ee3c6e72e9e3172b28c5e6166ec3fb9dfdc32578aebda0588afc682f56286dd6f616284c9892907cd413f57770be3662572207a36d6ac65c75a03d381f6f
     HEAD_REF master
 )
 
-file(COPY ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+# ====================================================
+# Install the pkgconfig info for the the `nvcodec` package
+# ====================================================
+if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    set(BUILD_SCRIPT ${CMAKE_CURRENT_LIST_DIR}\\build.sh)
+    vcpkg_acquire_msys(MSYS_ROOT PACKAGES make pkg-config)
+    set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)
 
-#Handle copyright
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+    message(STATUS "Building ${_csc_PROJECT_PATH} for Release")
+    file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET})
+
+    # Build script parameters:
+    # source root
+    # root of where the package would be installed on a linux system (here this is the package dir)
+    vcpkg_execute_required_process(
+        COMMAND ${BASH} --noprofile --norc "${BUILD_SCRIPT}"
+            "${SOURCE_PATH}" # SOURCE DIR
+            "${CURRENT_PACKAGES_DIR}" # PACKAGE DIR
+        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}
+        LOGNAME build-${TARGET_TRIPLET}
+    )
+
+else()
+    FIND_PROGRAM(MAKE make)
+    IF (NOT MAKE)
+        MESSAGE(FATAL_ERROR "MAKE not found")
+    ENDIF ()
+    
+    vcpkg_execute_required_process(
+        COMMAND make PREFIX=$${CURRENT_PACKAGES_DIR}
+        WORKING_DIRECTORY ${SOURCE_PATH}
+        LOGNAME make-${TARGET_TRIPLET}
+    )
+
+    # Deploy a copy of the ffnvcodec.pc file where ffmpeg's pkgconfig call expects to find it
+    file(INSTALL ${SOURCE_PATH}/ffnvcodec.pc DESTINATION ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
+endif()
+
+# Install the files to their generic location as well
+file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+file(INSTALL ${CURRENT_PORT_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/ffnvcodec)
