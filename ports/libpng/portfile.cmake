7c77cf7f5a7d2d376bc3e6cfaee0cabe5949167f
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-12-03 22:15:44 +0100

    [libpng] strong cleanup of the port

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -13,17 +13,17 @@ if ("apng" IN_LIST FEATURES)
             FILENAME "${LIBPNG_APG_PATCH_NAME}.gz"
             SHA512 226adcb3a8c60f2267fe2976ab531329ae43c2603dab4d0cf8f16217d64069936b879f3d6516b75d259c47d6f5c5b1f24f887602206c8e46abde0fb7f5c7946b
         )
-        
+
         vcpkg_find_acquire_program(7Z)
-    
+
         vcpkg_execute_required_process(
             COMMAND ${7Z} x ${LIBPNG_APNG_PATCH_ARCHIVE} -aoa
             WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/src
             LOGNAME extract-patch.log
         )
     endif()
-    
-    set(APNG_EXTRA_PATCH ${LIBPNG_APG_PATCH_PATH})    
+
+    set(APNG_EXTRA_PATCH ${LIBPNG_APG_PATCH_PATH})
     set(LIBPNG_APNG_OPTION "-DPNG_PREFIX=a")
 endif()
 
@@ -34,8 +34,8 @@ vcpkg_from_github(
     SHA512 ccb3705c23b2724e86d072e2ac8cfc380f41fadfd6977a248d588a8ad57b6abe0e4155e525243011f245e98d9b7afbe2e8cc7fd4ff7d82fcefb40c0f48f88918
     HEAD_REF master
     PATCHES
-        use-abort-on-all-platforms.patch
-        fix-libm-unix.patch
+        use_abort.patch
+        cmake.patch
         ${APNG_EXTRA_PATCH}
 )
 
@@ -55,40 +55,16 @@ vcpkg_configure_cmake(
         -DPNG_STATIC=${PNG_STATIC_LIBS}
         -DPNG_SHARED=${PNG_SHARED_LIBS}
         -DPNG_TESTS=OFF
-        -DSKIP_INSTALL_PROGRAMS=ON
         -DSKIP_INSTALL_EXECUTABLES=ON
         -DSKIP_INSTALL_FILES=ON
-        -DSKIP_INSTALL_SYMLINK=ON
-    OPTIONS_DEBUG
-        -DSKIP_INSTALL_HEADERS=ON
+        OPTIONS_DEBUG
+            -DSKIP_INSTALL_HEADERS=ON
 )
 
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/libpng)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/libpng16_static.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/lib/libpng16_static.lib ${CURRENT_PACKAGES_DIR}/lib/libpng16.lib)
-    endif()
-    if(EXISTS ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16_staticd.lib)
-        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16_staticd.lib ${CURRENT_PACKAGES_DIR}/debug/lib/libpng16d.lib)
-    endif()
-
-    foreach(FILE ${CURRENT_PACKAGES_DIR}/share/libpng/libpng16-release.cmake ${CURRENT_PACKAGES_DIR}/share/libpng/libpng16-debug.cmake)
-        file(READ ${FILE} _contents)
-        string(REGEX REPLACE "libpng16_static.lib" "libpng16.lib" _contents "${_contents}")
-        string(REGEX REPLACE "libpng16_staticd.lib" "libpng16d.lib" _contents "${_contents}")
-        file(WRITE ${FILE} "${_contents}")
-    endforeach()
-endif()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share/)
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/libpngConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/png)
-endif()
-
 vcpkg_copy_pdbs()
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
