bc226c1ee8d45e585de22355a2e2e50d2dc7f132
Karol Trojanowski <karol.trojanowski@displaylink.com>
2019-12-04 11:52:28 +0100

    [xtensor-python] Add new port

---
+++ ports/xtensor-python/portfile.cmake
@@ -0,0 +1,32 @@
+# header-only library
+
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO QuantStack/xtensor-python
+    REF e33ad393dac606e513fb92b60d81545373b9b916
+    SHA512 f7eec109cd4ae15a28fb4572fbb46ad96e3371ee2e52634c889756884a6be7479943515d59ca2f2e9e23efd433ce46b41106b5599cb1de75e77f54bc57fa9fc1
+    HEAD_REF master
+)
+
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+    	-DBUILD_TESTS=OFF
+	-DDOWNLOAD_GTEST=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+
+# Handle copyright
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+
+# CMake integration test
+vcpkg_test_cmake(PACKAGE_NAME ${PORT})
