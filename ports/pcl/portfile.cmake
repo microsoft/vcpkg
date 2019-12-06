7cd9a2494bcfaae52e5555217f99495c1a808a08
NancyLi1013 <lirui09@beyondsoft.com>
2019-12-05 22:58:09 -0800

    [pcl] Add compile options on unix platform

--- ports/pcl/portfile.cmake
+++ ports/pcl/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO PointCloudLibrary/pcl
@@ -14,6 +12,7 @@ vcpkg_from_github(
         cuda_10_1.patch
 		# Patch for https://github.com/microsoft/vcpkg/issues/7660
 		use_target_link_libraries_in_pclconfig.patch
+        add_compile_option_for_unix.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/Modules/FindFLANN.cmake)
@@ -66,5 +65,4 @@ if("tools" IN_LIST FEATURES)
     vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/pcl)
 endif()
 
-file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/pcl)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/pcl/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/pcl/copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
