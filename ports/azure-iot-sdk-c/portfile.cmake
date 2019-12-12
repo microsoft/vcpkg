ff5c85e03f49c06ab8ed46abdcba01fffbbfebda
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2019-12-12 10:59:13 -0800

    [azure-iot-sdk-c] C SDK release 2019-12-11

--- ports/azure-iot-sdk-c/portfile.cmake
+++ ports/azure-iot-sdk-c/portfile.cmake
@@ -15,8 +15,8 @@ else()
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Azure/azure-iot-sdk-c
-        REF f8d260df190f90c04114ca8ff7d83dd03d4dd80d
-        SHA512 111331293cfbdbdac4a6460d293ec8650bee31940829852c27afc88cc6e742e96f71c996aa275dc5ed1f13e9fe19452d7b2685dde47bb7d6c135ebee58c50d21
+        REF 509228d0641878a714b91771aae9c3990808079f
+        SHA512 951781978d149f48184c7baaeb67650a2f766d3c197f87d7c4c59831dffc9240a6106c64763cc9c6e84e291e1ea7ce88cc319d4545839c68bc80f483def0b1df
         HEAD_REF master
         PATCHES improve-external-deps.patch
     )
