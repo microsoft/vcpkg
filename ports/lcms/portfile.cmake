3c2060198e8710bbfcdd3d824555147579768496
Angel Sánchez <angelsanchez@inedit.com>
2019-12-19 13:29:48 +0100

    revert lcms to an old/stable version

--- ports/lcms/portfile.cmake
+++ ports/lcms/portfile.cmake
@@ -7,8 +7,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mm2/Little-CMS
-    REF lcms2.9
-    SHA512 b565ed3714c9beaf13e15b3798abbc6c295443357c8db3299cecd9794620bb1d7c50ad258cf887c7bbf66efacb8d8699a7ee579f8c73598740915caa3044ae70
+    REF b5b36cd062c968fd3e1c5c0c37f9392bc7a47ddf
+    SHA512 094e3d0f87f85943c3aade31eadb8b456252260ccb91fad1b8628164c7084b593e57a283ab265a8cfcb81f8b10e2a285cdb3c4b4246d39bc19cb95945d01f5f5
     HEAD_REF master
     PATCHES
         remove_library_directive.patch
