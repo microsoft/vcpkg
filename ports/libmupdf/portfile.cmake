c41b9a7e1af21f447c5678f11fd0764e95ad6226
JackBoosY <yuzaiyang@beyondsoft.com>
2019-12-23 01:06:23 -0800

    [libmupdf] Update to 1.16.1-epub-prerelease and build with msbuild instead

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -1,28 +1,50 @@
-include(vcpkg_common_functions)
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_fail_port_install(ON_TARGET "UWP")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ArtifexSoftware/mupdf
-    REF 1.15.0
-    SHA512 9f47a79a2040ff3da885f54d143a7c44712f8a08650fca1d2be21199a7103364a35e28a1832708c2b7752b11c95bf0755ae6c922afc35ee8ae639da7a6ac03b0
+    REF 75dbbd97826e7191ee539f9934e607eb93247a42 #1.16.1-epub-prerelease
+    SHA512 6ef83cadd0fd0d681ffe7f377b795046da533cb4d2a33f0165a0c77d29cd764f8c68fc8ff69bab957bc599e72241f402cf8fbfc31c46bc5cfd2783c3511c414d
     HEAD_REF master
 	PATCHES
         Fix-error-C2169.patch
+        fix-win-build.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+if (VCPKG_TARGET_IS_WINDOWS)
+    if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(BUILD_ARCH "Win32")
+    elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(BUILD_ARCH "x64")
+    else()
+        message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+    endif()
+    
+    message(STATUS "Upgrading solution")
+    vcpkg_execute_required_process(
+        COMMAND "devenv.exe"
+                "mupdf.sln"
+                /Upgrade
+        WORKING_DIRECTORY ${SOURCE_PATH}/platform/win32
+        LOGNAME upgrade-Packet-${TARGET_TRIPLET}
+    )
+    vcpkg_install_msbuild(
+        SOURCE_PATH ${SOURCE_PATH}
+        PROJECT_SUBPATH platform/win32/mupdf.sln
+        INCLUDES_SUBPATH include/mupdf
+        TARGET libmupdf 
+        PLATFORM ${BUILD_ARCH}
+        USE_VCPKG_INTEGRATION
+    )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA
-)
-
-vcpkg_install_cmake()
-
-file(COPY ${SOURCE_PATH}/include/mupdf DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+else()
+    vcpkg_configure_make(
+        SOURCE_PATH ${SOURCE_PATH}
+        SKIP_CONFIGURE
+    )
+    
+    vcpkg_install_make()
+endif()
 
 vcpkg_copy_pdbs()
 
