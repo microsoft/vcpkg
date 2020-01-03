cd70d2cf015d16aa44810e329233ba06d0aab7ff
JackBoosY <yuzaiyang@beyondsoft.com>
2020-01-02 22:18:54 -0800

    [libmupdf] Fix static build in Windows

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -20,14 +20,30 @@ if (VCPKG_TARGET_IS_WINDOWS)
         message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
     endif()
     
-    message(STATUS "Upgrading solution")
-    vcpkg_execute_required_process(
-        COMMAND "devenv.exe"
-                "mupdf.sln"
-                /Upgrade
-        WORKING_DIRECTORY ${SOURCE_PATH}/platform/win32
-        LOGNAME upgrade-Packet-${TARGET_TRIPLET}
-    )
+    if (NOT EXISTS ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj AND NOT EXISTS ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj.bak)
+        message(STATUS "Upgrading solution")
+        vcpkg_execute_required_process(
+            COMMAND "devenv.exe"
+                    "mupdf.sln"
+                    /Upgrade
+            WORKING_DIRECTORY ${SOURCE_PATH}/platform/win32
+            LOGNAME upgrade-Packet-${TARGET_TRIPLET}
+        )
+    endif()
+    
+    # Bakup libmupdf project file to include gdcmjpeg include path
+    if (NOT EXISTS ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj.bak)
+        configure_file(${SOURCE_PATH}/platform/win32/libmupdf.vcxproj ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj.bak COPYONLY)
+        file(REMOVE ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj)
+    endif()
+    configure_file(${SOURCE_PATH}/platform/win32/libmupdf.vcxproj.bak ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj COPYONLY)
+    # Add include/gdcmjpeg/8 to include path
+    file(READ ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj LIBMUPDF_CFG)
+    file(TO_NATIVE_PATH "${CURRENT_INSTALLED_DIR}/include/gdcmjpeg/8" GDCMJPEG_INC_PATH)
+    string(REPLACE "<AdditionalIncludeDirectories>"
+                "<AdditionalIncludeDirectories>${GDCMJPEG_INC_PATH};" LIBMUPDF_CFG "${LIBMUPDF_CFG}")
+    file(WRITE ${SOURCE_PATH}/platform/win32/libmupdf.vcxproj "${LIBMUPDF_CFG}")
+    
     vcpkg_install_msbuild(
         SOURCE_PATH ${SOURCE_PATH}
         PROJECT_SUBPATH platform/win32/mupdf.sln
