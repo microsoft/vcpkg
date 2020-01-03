dd860a89920d1df08d38cddeb56abb6809768493
treeshateorcs <treeshateorcs@pm.me>
2020-01-04 01:18:56 +0300

    add tdlib

---
+++ ports/tdlib/portfile.cmake
@@ -0,0 +1,14 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO tdlib/td
+    REF v1.5.0
+    SHA512 57f7dcaacd253d06852d95e210c6ecefa3d477f36d74f1f934a9affb621e99258bd631b7c3573df84812f9f44e6ceb090fbbc62e9f541c5d539fbab0ee51a222
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_install_cmake()
+file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/tdlib RENAME copyright)
