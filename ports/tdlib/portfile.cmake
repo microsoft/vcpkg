bcb5d755bdf191e51d5e360581ac68e4ee8edb9e
treeshateorcs <treeshateorcs@pm.me>
2020-01-06 20:18:19 +0300

    Update portfile.cmake

--- ports/tdlib/portfile.cmake
+++ ports/tdlib/portfile.cmake
@@ -11,6 +11,17 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/tdlib RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Td)
+
+if (VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_find_acquire_program(GPERF)
+    get_filename_component(GPERF_PATH ${GPERF} DIRECTORY)
+    vcpkg_add_to_path(${GPERF_PATH})
+else()
+    # gperf only have windows package in vcpkg now.
+    if (NOT EXISTS /usr/bin/gperf)
+        message(FATAL_ERROR "${PORT} requires gperf, these can be installed on Ubuntu systems via apt-get install gperf.")
+    endif()
+endif()
