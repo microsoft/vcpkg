68a89f25d9593ac0d48da061c637056c2dbe775e
cnbatch <cnbatch@gmail.com>
2020-01-10 16:35:00 +0800

    Upgrade to 1.4.0

--- ports/xlnt/portfile.cmake
+++ ports/xlnt/portfile.cmake
@@ -1,4 +1,8 @@
-include(vcpkg_common_functions)
+if(VCPKG_TARGET_IS_WINDOWS) 
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+       set(win_patch "static-linking-for-windows.patch")
+    endif()
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -6,6 +10,8 @@ vcpkg_from_github(
     REF v1.4.0
     SHA512 74abbee97994098fb7d8fd0839929db74fe01b8428f8bdb8edd28340d3b3ed04d4c7d6dd5d886ae766054ff1b0fe9a8275098a1462e7a5146ff09f1cdb063360
     HEAD_REF master
+    PATCHES
+        ${win_patch}
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
@@ -22,20 +28,11 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-if(CMAKE_SYSTEM_NAME STREQUAL "Windows" OR (NOT CMAKE_SYSTEM_NAME AND CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows"))
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-        vcpkg_apply_patches(
-            SOURCE_PATH ${CURRENT_PACKAGES_DIR}
-            PATCHES ${CMAKE_CURRENT_LIST_DIR}/static-linking-for-windows.patch
-        )
-    endif()
-endif()
-
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/xlnt)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/man)
-file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/xlnt RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 vcpkg_copy_pdbs()
