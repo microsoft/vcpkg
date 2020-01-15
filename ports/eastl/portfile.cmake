bd377f9fd92976d278dc1ebcb94e8de38ed818fc
c0rp3n <ojhitchcock@gmail.com>
2020-01-15 15:42:15 +0000

    Improve EASTL
    Still need to fix
    The /debug/lib/cmake folder should be merged with /lib/cmake into /share/eastl
    Found 3 error(s). Please correct the portfile:
        E:\vcpkg\ports\eastl\portfile.cmake
    -- Performing post-build validation done
    Error: Building package eastl:x64-windows-static failed with: POST_BUILD_CHECKS_FAILED
    Please ensure you're using the latest portfiles with `.\vcpkg update`, then
    submit an issue at https://github.com/Microsoft/vcpkg/issues including:
      Package: eastl:x64-windows-static
      Vcpkg version: 2019.09.12-nohash

--- ports/eastl/portfile.cmake
+++ ports/eastl/portfile.cmake
@@ -1,16 +1,16 @@
-include(vcpkg_common_functions)
-
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO electronicarts/EASTL
-    REF 3.14.01
-    SHA512 efc03bdd4b6371b3fb4b75cac31ce3081290e9177453914a4fbb601d1ba44d65a86f2e98b7b27efbd985f37bd59a80169cf58beb3a32e5b3672ea2a2d6dd78d1
+    REF 3.16.01
+    SHA512 c5c4b6637da5257e6dbbccd293a07691d509bf1310a67aa8b85777a1163f0034874e0638728a54deaa0839320fd919b67a8c9e22461fee6612a76637bb3086bc
     HEAD_REF master
     PATCHES 
     fix_cmake_install.patch
 )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/EASTLConfig.cmake.in DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
