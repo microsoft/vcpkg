732f59aee3fd3a97b9f485132a8c8cff5ed72c28
wangli28 <wangli28@beyondsoft.com>
2020-01-15 07:19:21 +0000

    [libgpod] Fix build error

--- ports/libgpod/portfile.cmake
+++ ports/libgpod/portfile.cmake
@@ -1,8 +1,4 @@
-include(vcpkg_common_functions)
-
-if (VCPKG_TARGET_IS_WINDOWS)
-	message(FATAL_ERROR "libgpod only support unix.")
-endif()
+vcpkg_fail_port_install(MESSAGE "${PORT} currently only supports Linux platform" ON_TARGET "Windows" "OSX")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -10,13 +6,10 @@ vcpkg_from_github(
     REF 4a8a33ef4bc58eee1baca6793618365f75a5c3fa
     SHA512 b7a120c1106c1205e8de2808de5ac4ff1cf189943017939a5ea4eded4e1ceef44557587e69a8591cc5249f8c8dbf0cbdcce1dd309d33a0e9207b0560abe3ae39
     HEAD_REF master
+    PATCHES fix-glibconfigpath.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS	
-	gchecksum WITH_INTERNAL_GCHECKSUM
-)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -27,13 +20,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
 # Handle copyright
-file(COPY 
-	${SOURCE_PATH}/README 
-	${SOURCE_PATH}/README.SysInfo 
-	${SOURCE_PATH}/README.overview 
-	${SOURCE_PATH}/README.sqlite 
-	DESTINATION 
-	${CURRENT_PACKAGES_DIR}/share/${PORT}
-)
+file(COPY ${SOURCE_PATH}/README DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
