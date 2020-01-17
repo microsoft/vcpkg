2d9f03954f7da9d059ff683a3bc8bdcb4cf10c52
unknown <rene.winchenbach@gmail.com>
2020-01-17 23:46:57 +0100

    Update Alembic version to 1.7.12

--- ports/alembic/portfile.cmake
+++ ports/alembic/portfile.cmake
@@ -7,8 +7,8 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO alembic/alembic
-    REF 1.7.11
-    SHA512 94b9c218a2fe6e2e24205aff4a2f6bab784851c2aa15592fb60ea91f0e8038b0c0656a118f3a5cba0d3de8917dd90b74d0e2d1c4ac034b9ee3f5d0741d9f6b70
+    REF 1.7.12
+    SHA512 e05e0b24056c17f01784ced1f9606a269974de195f1aca8a6fce2123314e7ee609f70df77ac7fe18dc7f0c04fb883d38cc7de9b963caacf9586aaa24d4ac6210
     HEAD_REF master
     PATCHES
         fix-C1083.patch
