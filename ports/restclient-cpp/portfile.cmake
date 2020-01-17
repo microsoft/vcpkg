12395876b5a5219801a14b6b349ad63eb84e8d86
et-nik <nikita.hldm@gmail.com>
2020-01-17 11:40:22 +0300

    [restclient-cpp] Changes and fixes Update version Update handling copiright Remove no longer needed include Move COMPILE_TYPE parameter

--- ports/restclient-cpp/portfile.cmake
+++ ports/restclient-cpp/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mrtazz/restclient-cpp
@@ -8,15 +6,13 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-set(COMPILE_TYPE STATIC)
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DCMAKE_DISABLE_FIND_PACKAGE_GTest=TRUE
         -DCMAKE_DISABLE_FIND_PACKAGE_jsoncpp=TRUE
-        -DCOMPILE_TYPE=${COMPILE_TYPE}
+        -DCOMPILE_TYPE=STATIC
 )
 
 vcpkg_install_cmake()
@@ -27,8 +23,7 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/restclient-cpp)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/restclient-cpp)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/restclient-cpp/LICENSE ${CURRENT_PACKAGES_DIR}/share/restclient-cpp/copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 # Copy pdb files
 vcpkg_copy_pdbs()
