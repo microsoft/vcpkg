3bb066b646ab0ea20a05160a868dea49a4aa11cd
jsmolka <jsmolka@pm.me>
2020-01-17 22:24:23 +0100

    [asmjit] Update to newest version

--- ports/asmjit/portfile.cmake
+++ ports/asmjit/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO asmjit/asmjit
-  REF 761130b1d8f32b5d3d612d285664fcfef5258149
-  SHA512 a86fd58ba0c8bc81ec575e86a9acdf4a11f2acc9c2facd2a0a8512cffa9ee6fc0bd877a1f33fb52f8f510eff1de654b45cd4f5f5a18c5252ecae22a92db6e93e
+  REF 7e164e3edeefb76d8a2da4fbe84957ece0d07a13
+  SHA512 19d0a7f7a7cb4e6bd6c03f2e29ab012edf67ba4ba92789fd81e71a4937f1271a5124fa9c02d40d202bad7785b9e5ae21a3a72048cdeb8781c1b927c9717669c8
   HEAD_REF master
 )
 
