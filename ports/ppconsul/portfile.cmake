96b86613a83a7fbd386dd517fddfe53ce15176ef
SeekingMeaning <meaningseeking@protonmail.com>
2020-01-18 06:48:23 -0800

    [ppconsul] Possible fix for CI

--- ports/ppconsul/portfile.cmake
+++ ports/ppconsul/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO oliora/ppconsul
@@ -15,14 +13,13 @@ file(REMOVE_RECURSE ${SOURCE_PATH}/ext/catch)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
 )
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 
 
-file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ppconsul RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 
