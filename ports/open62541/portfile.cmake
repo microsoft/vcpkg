b6dc5854b33f222e2c2f752f8fd00a39a99e3dcd
yurybura <yurybura@gmail.com>
2020-01-20 19:02:05 +0300

    Remove dependency on python-six

--- ports/open62541/portfile.cmake
+++ ports/open62541/portfile.cmake
@@ -1,9 +1,5 @@
 include(vcpkg_common_functions)
 
-if(NOT CMAKE_HOST_WIN32)
-    message("${PORT} currently requires the following tools from the system package manager:\n    python-six\n\nThis can be installed on Ubuntu systems via apt-get install python-six python3-six (depending on your current python default interpreter)")
-endif()
-
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
     message(FATAL_ERROR "${PORT} does not currently support UWP")
 endif()
@@ -18,38 +14,15 @@ vcpkg_from_github(
 
 file(READ ${SOURCE_PATH}/CMakeLists.txt OPEN62541_CMAKELISTS)
 string(REPLACE
-               "RUNTIME DESTINATION \${CMAKE_INSTALL_PREFIX}"
-               "RUNTIME DESTINATION \${BIN_INSTALL_DIR}"
-       OPEN62541_CMAKELISTS "${OPEN62541_CMAKELISTS}")
+    "RUNTIME DESTINATION \${CMAKE_INSTALL_PREFIX}"
+    "RUNTIME DESTINATION \${BIN_INSTALL_DIR}"
+    OPEN62541_CMAKELISTS "${OPEN62541_CMAKELISTS}"
+)
 file(WRITE ${SOURCE_PATH}/CMakeLists.txt "${OPEN62541_CMAKELISTS}")
-
-if(CMAKE_HOST_WIN32)
-    set(EXECUTABLE_SUFFIX ".exe")
-else()
-    set(EXECUTABLE_SUFFIX "")
-endif()
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
-
-if(CMAKE_HOST_WIN32)
-    # Must not modify system copy of python3 -- on CMAKE_HOST_WIN32, we have our own private copy
-    if(NOT EXISTS ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX})
-        if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX})
-            get_filename_component(PYTHON3_DIR_NAME "${PYTHON3_DIR}" NAME)
-            vcpkg_download_distfile(GET_PIP
-                URLS "https://bootstrap.pypa.io/3.3/get-pip.py"
-                FILENAME "tools/python/${PYTHON3_DIR_NAME}/get-pip.py"
-                SHA512 92e68525830bb23955a31cb19ebc3021ef16b6337eab83d5db2961b791283d2867207545faf83635f6027f2f7b7f8fee2c85f2cfd8e8267df25406474571c741
-            )
-            execute_process(COMMAND ${PYTHON3_DIR}/python${EXECUTABLE_SUFFIX} ${GET_PIP})
-        endif()
-        execute_process(COMMAND ${PYTHON3_DIR}/Scripts/pip${EXECUTABLE_SUFFIX} install six)
-    else()
-        execute_process(COMMAND ${PYTHON3_DIR}/easy_install${EXECUTABLE_SUFFIX} six)
-    endif()
-endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -61,17 +34,14 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-
-if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT})
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
-else()
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
-endif()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/open62541/tools)
 
-vcpkg_copy_pdbs()
-
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+# CMake integration test
+vcpkg_test_cmake(PACKAGE_NAME ${PORT})
