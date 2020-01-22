0982dfd79d3762133bb43925f8ce4069d20c57bb
Alexander Neumann <alexander.neumann@hamburg.de>
2020-01-22 17:18:16 +0100

    fix cmake config path

--- ports/pcl/portfile.cmake
+++ ports/pcl/portfile.cmake
@@ -51,7 +51,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
