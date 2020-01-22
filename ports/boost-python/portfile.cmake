e92c203378161b6352325cd037b752134ab56a0f
yurybura <yurybura@gmail.com>
2020-01-22 18:26:28 +0300

    Fix user-config.jam configuration on Linux

--- ports/boost-python/portfile.cmake
+++ ports/boost-python/portfile.cmake
@@ -10,11 +10,14 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Find Python. Can't use find_package here, but we already know where everything is
-file(GLOB PYTHON_INCLUDE_PATH "${CURRENT_INSTALLED_DIR}/include/python3.*")
-set(PYTHONLIBS_RELEASE "${CURRENT_INSTALLED_DIR}/lib")
-set(PYTHONLIBS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib")
-string(REGEX REPLACE ".*python([0-9\.]+)$" "\\1" PYTHON_VERSION "${PYTHON_INCLUDE_PATH}")
+# Find Python3 executable
+vcpkg_find_acquire_program(PYTHON3)
+set(VCPKG_PYTHON_EXECUTABLE "${PYTHON3}")
+# Find Python3 libraries. Can't use find_package here, but we already know where everything is
+file(GLOB VCPKG_PYTHON_INCLUDE "${CURRENT_INSTALLED_DIR}/include/python3.*")
+set(VCPKG_PYTHON_LIBS_RELEASE "${CURRENT_INSTALLED_DIR}/lib")
+set(VCPKG_PYTHON_LIBS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib")
+string(REGEX REPLACE ".*python([0-9\.]+).*" "\\1" VCPKG_PYTHON_VERSION "${VCPKG_PYTHON_INCLUDE}")
 include(${CURRENT_INSTALLED_DIR}/share/boost-build/boost-modular-build.cmake)
 boost_modular_build(SOURCE_PATH ${SOURCE_PATH})
 include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)
