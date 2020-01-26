ef0fcf755bf1ef9a7bf3ad4951264f932bed9edd
Silvio Traversaro <silvio@traversaro.it>
2020-01-26 15:42:42 +0100

    Update eigen3's portfile to use new gitlab repo
    The official GitHub mirror at https://github.com/eigenteam/eigen-git-mirror is deprecated and soon will be removed.

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -2,9 +2,10 @@ include(vcpkg_common_functions)
 
 vcpkg_buildpath_length_warning(37)
 
-vcpkg_from_github(
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO eigenteam/eigen-git-mirror
+    REPO libeigen/eigen
     REF 3.3.7
     SHA512 270ab9b5c22e09aa0e70d1a26995523c5c21fb0f09da45c137c11ab4c7700fe2bdb2b343c1e063bea4be5ae61d2313ff29ebbcad519dc355a568792b4a6e9e48
     HEAD_REF master
