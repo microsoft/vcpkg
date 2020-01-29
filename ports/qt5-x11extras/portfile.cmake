b7cde1ecac839fc48e9c12a7b624f9aa301913f6
Alexander Neumann <alexander.neumann@hamburg.de>
2020-01-29 11:15:22 +0100

    still trying to get log info

--- ports/qt5-x11extras/portfile.cmake
+++ ports/qt5-x11extras/portfile.cmake
@@ -1,4 +1,4 @@
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
-qt_submodule_installation(OPTIONS -verbose)
+qt_submodule_installation(BUILD_OPTIONS -verbose)
 file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/config.log" DESTINATION "${CURRENT_BUILDTREES_DIR}/config-dbg.log")
 file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/config.log" DESTINATION "${CURRENT_BUILDTREES_DIR}/config-rel.log")
