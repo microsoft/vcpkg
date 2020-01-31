d8ad806e72b7a0f24e0d11ee89be9f8d0d316429
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-01-31 21:38:29 +0100

    remove most of the wrapper

--- ports/qt5-base/vcpkg-cmake-wrapper.cmake
+++ ports/qt5-base/vcpkg-cmake-wrapper.cmake
@@ -17,63 +17,25 @@ endfunction()
 
 get_target_property(_target_type Qt5::Core TYPE)
 if("${_target_type}" STREQUAL "STATIC_LIBRARY")
-    find_package(ZLIB)
-    find_package(JPEG)
-    find_package(PNG)
-    find_package(Freetype)
-    find_package(sqlite3 CONFIG)
-    find_package(PostgreSQL MODULE REQUIRED)
-    find_package(double-conversion CONFIG)
-    find_package(OpenSSL)
-    find_package(harfbuzz CONFIG)
-
-    set_property(TARGET Qt5::Core APPEND PROPERTY INTERFACE_LINK_LIBRARIES
-        ZLIB::ZLIB JPEG::JPEG PNG::PNG Freetype::Freetype sqlite3 harfbuzz::harfbuzz
-        double-conversion::double-conversion OpenSSL::SSL OpenSSL::Crypto PostgreSQL::PostgreSQL
-    )
-
-    add_qt_library(Qt5::Core
-        pcre2-16
-        icuin icui18n
-        icutu icuuc icuio
-        icudt icudata
-        Qt5ThemeSupport
-        Qt5EventDispatcherSupport
-        Qt5PlatformCompositorSupport
-        Qt5FontDatabaseSupport)
-
-    if(MSVC)
-       set_property(TARGET Qt5::Core APPEND PROPERTY INTERFACE_LINK_LIBRARIES
-           Netapi32.lib Ws2_32.lib Mincore.lib Winmm.lib Iphlpapi.lib Wtsapi32.lib Dwmapi.lib Imm32.lib)
-
-      add_qt_library(Qt5::Core Qt5WindowsUIAutomationSupport qwindows qdirect2d)
+    if(WIN32)
     elseif(UNIX AND NOT APPLE)
-      add_qt_library(Qt5::Core
-            Qt5GraphicsSupport
-            Qt5ClipboardSupport
-            Qt5AccessibilitySupport)
     elseif(APPLE)
-       set_property(TARGET Qt5::Core APPEND PROPERTY INTERFACE_LINK_LIBRARIES
-            "-weak_framework DiskArbitration" "-weak_framework IOKit" "-weak_framework Foundation" "-weak_framework CoreServices"
-            "-weak_framework AppKit" "-weak_framework Security" "-weak_framework ApplicationServices"
-            "-weak_framework CoreFoundation" "-weak_framework SystemConfiguration"
-            "-weak_framework Carbon"
-            "-weak_framework QuartzCore"
-            "-weak_framework CoreVideo"
-            "-weak_framework Metal"
-            "-weak_framework CoreText"
-            "-weak_framework ApplicationServices"
-            "-weak_framework CoreGraphics"
-            "-weak_framework OpenGL"
-            "-weak_framework AGL"
-            "-weak_framework ImageIO"
-            "z" "m"
-            cups)
-        add_qt_library(Qt5::Core
-            Qt5GraphicsSupport
-            Qt5ClipboardSupport
-            Qt5AccessibilitySupport
-            qcocoa)
+       # set_property(TARGET Qt5::Core APPEND PROPERTY INTERFACE_LINK_LIBRARIES
+            # "-weak_framework DiskArbitration" "-weak_framework IOKit" "-weak_framework Foundation" "-weak_framework CoreServices"
+            # "-weak_framework AppKit" "-weak_framework Security" "-weak_framework ApplicationServices"
+            # "-weak_framework CoreFoundation" "-weak_framework SystemConfiguration"
+            # "-weak_framework Carbon"
+            # "-weak_framework QuartzCore"
+            # "-weak_framework CoreVideo"
+            # "-weak_framework Metal"
+            # "-weak_framework CoreText"
+            # "-weak_framework ApplicationServices"
+            # "-weak_framework CoreGraphics"
+            # "-weak_framework OpenGL"
+            # "-weak_framework AGL"
+            # "-weak_framework ImageIO"
+            # "z" "m"
+            # cups)
     endif()
 
 endif()
