76a340e3db4246fdb8c94dcbbf2f8c94fe13d9e7
Stephen Kyne <xskyne@gmail.com>
2020-02-02 16:11:08 +0100

    Added in debug logging to debug builds only. Still removed from release

--- ports/portaudio/portfile.cmake
+++ ports/portaudio/portfile.cmake
@@ -12,12 +12,19 @@ vcpkg_from_git(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
+    OPTIONS_RELEASE
         -DPA_USE_DS=ON
         -DPA_USE_WASAPI=ON
         -DPA_USE_WDMKS=ON
         -DPA_USE_WMME=ON
         -DPA_LIBNAME_ADD_SUFFIX=OFF
+    OPTIONS_DEBUG
+    	-DPA_USE_DS=ON
+        -DPA_USE_WASAPI=ON
+        -DPA_USE_WDMKS=ON
+        -DPA_USE_WMME=ON
+        -DPA_LIBNAME_ADD_SUFFIX=OFF
+        -DPA_ENABLE_DEBUG_OUTPUT:BOOL=ON
 )
 
 vcpkg_install_cmake()
