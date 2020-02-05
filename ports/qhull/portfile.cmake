948695e132b56be7327b47a4e0442ed6f86c43cd
NancyLi1013 <lirui09@beyondsoft.com>
2020-02-05 04:47:06 -0800

    [qhull] Fix library cannot be found by cmake

--- ports/qhull/portfile.cmake
+++ ports/qhull/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO qhull/qhull
@@ -9,21 +7,9 @@ vcpkg_from_github(
     PATCHES
         uwp.patch
         mac-fix.patch
+        fix-target-cannot-found.patch
 )
-if(${TARGET_TRIPLET} STREQUAL "x64-windows-static") 
-# workaround for visual studio toolset regression LNK1201 (remove if solved)
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS 
-        -DINCLUDE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/include
-        -DMAN_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/doc/qhull
-        -DDOC_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/doc/qhull
-    OPTIONS_RELEASE
-        -DLIB_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib
-    OPTIONS_DEBUG
-        -DLIB_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib
-)
-else()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
@@ -36,7 +22,6 @@ vcpkg_configure_cmake(
     OPTIONS_DEBUG
         -DLIB_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib
 )
-endif()
 
 vcpkg_install_cmake()
 
@@ -47,23 +32,11 @@ file(GLOB_RECURSE HTMFILES ${CURRENT_PACKAGES_DIR}/include/*.htm)
 file(REMOVE ${HTMFILES})
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/doc)
 
-file(GLOB EXEFILES_RELEASE ${CURRENT_PACKAGES_DIR}/bin/*.exe)
-file(GLOB EXEFILES_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-file(COPY ${EXEFILES_RELEASE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/qhull)
-if(EXEFILES_RELEASE OR EXEFILES_DEBUG)
-    file(REMOVE ${EXEFILES_RELEASE} ${EXEFILES_DEBUG})
-endif()
-
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull_d.lib)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull_p.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull_pd.lib)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull_r.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull_rd.lib)
-else()
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhullcpp.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhullcpp_d.lib)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhullstatic.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhullstatic_d.lib)
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhullstatic_r.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhullstatic_rd.lib)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull.lib)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull_p.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull_p.lib)
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/qhull_r.lib ${CURRENT_PACKAGES_DIR}/debug/lib/qhull_r.lib)
 endif()
 
-file(COPY ${SOURCE_PATH}/README.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/qhull)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/qhull/README.txt ${CURRENT_PACKAGES_DIR}/share/qhull/copyright)
+file(INSTALL ${SOURCE_PATH}/README.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
