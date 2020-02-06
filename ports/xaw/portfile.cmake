5c3b0da9bdba1cd154434893fc3fe9198d4f03f6
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-06 16:34:47 +0100

    addded xaw patch

--- ports/xaw/portfile.cmake
+++ ports/xaw/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_gitlab(
     REF 8c82c47a770274c50944f002b97369c4e30872d8 # 1.0.13 
     SHA512  15b7afc706fe50c55391846892961e3286165e43513009664cfca7c443f9f9eafa32e17e8ba8089a0ae2d568aab4117f7e5ff696052ee58f6da78e3d9ad7e721
     HEAD_REF master # branch name
-    #PATCHES example.patch #patch name
+    PATCHES getpagesize.patch #patch name
 ) 
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
