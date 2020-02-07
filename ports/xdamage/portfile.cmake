77e6260b94fb2b1688d6b17008b21260e365f3b9
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:38:44 +0100

    addd xdamage

---
+++ ports/xdamage/portfile.cmake
@@ -0,0 +1,38 @@
+message(STATUS "----- ${PORT} requires autoconf, libtool and pkconf from the system package manager! -----")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxdamage
+    REF 977b04cd69738806e0b48fcf5c725763d065f06d # 1.1.5
+    SHA512  79c3a4c63f6c50c39d324183b98ad7e70235aed1c8385acf2f593739c71e7929119448be3e15dffd276b32e4fbb056508deeb35f450f74b85c101047f68d4339
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
+    #OPTIONS
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
