4cff1e9c2cfa1453c89c940fbaca9d82100af7b6
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:37:14 +0100

    add xcb-cursor

---
+++ ports/xcb-cursor/portfile.cmake
@@ -0,0 +1,43 @@
+## requires AUTOCONF, LIBTOOL, PKCONF, GPERF
+vcpkg_find_acquire_program(GPERF)
+get_filename_component(GPERF_DIR "${GPERF}" DIRECTORY)
+vcpkg_add_to_path("${GPERF_DIR}")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxcb-cursor
+    REF  95b9a8fd876fdbbc854cdf3d90317be3846c7417 #0.1.3
+    SHA512 cca7bf1f2aeaab8d256052a676098d7c600b90dc47cf9bc84d11229e59fbf5c83f7f877b8538f7cc662983807566d28c87b3501abc7cab76cc553d9db29eceb9
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+file(TOUCH ${SOURCE_PATH}/m4/dummy)
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL ${SHELL_PATH}
+    #OPTIONS
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
+file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
+
+
