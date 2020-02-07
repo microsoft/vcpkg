c7105942023e2dce5a90685b7f318837777eb9d5
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:35:18 +0100

    add fontenc

---
+++ ports/fontenc/portfile.cmake
@@ -0,0 +1,39 @@
+## requires AUTOCONF, LIBTOOL and PKCONF
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libfontenc
+    REF  2baea13978759d1a011fc6d739465893b554d30a #1.1.4
+    SHA512 5ebef8b516a2377b004894b53d56ad960bc5179d9f9a36e18bc2228ea04e8f87e9baffd4883c21783dd1f4c57d7f521cdfa42c1e9facae60c6fc2c9f5472230e
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+#file(TOUCH ${SOURCE_PATH}/m4/dummy)
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL ${SHELL_PATH}
+    #OPTIONS 
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
+file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
+
+
