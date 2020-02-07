70970cacf729947aa1c14080cdccd2a74304b6c4
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:39:35 +0100

    add xkbcomp

---
+++ ports/xkbcomp/portfile.cmake
@@ -0,0 +1,43 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+message(STATUS "----- ${PORT} requires autoconf, libtool and pkconf from the system package manager! -----")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO app/xkbcomp
+    REF 2abe23d23d3755335c10ff573e4e1f93b682e9d9 # 1.4.2 
+    SHA512  43b47775afa0be884ec66446d3f12728225e83bbacdf56b6a00790672f783a351c779bbbf2482bd0276dc48347adca8db2e3dc061e56d262713e32a8251c66a7
+    HEAD_REF master # branch name
+    PATCHES configure.patch #patch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
+    OPTIONS
+       #"XKBCOMP_LIBS=\"-L${CURRENT_INSTALLED_DIR}/debug/lib -lX11 -lX11-xcb -lxkbfile\""
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+file(RENAME "${CURRENT_PACKAGES_DIR}/bin/xkbcomp${VCPKG_TARGET_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/xkbcomp${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
