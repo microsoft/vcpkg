69c62ee7a003a41fe10cc24c8c08113e114dba57
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:39:56 +0100

    add xp

---
+++ ports/xp/portfile.cmake
@@ -0,0 +1,39 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+message(STATUS "----- ${PORT} requires autoconf, libtool and pkconf from the system package manager! -----")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxp
+    REF 57ce065b3c4329838f82de7e791caa52345a8c6f # 1.0.3
+    SHA512  112a0bbde4d5036507e2ff2484fe9f48dc4e4a9e989efac73423001fa9f6ff3ec2b7a375ddd8721224beb68b080198a7fca5f5af17d154b12bf807951cac4779
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
+    #OPTIONS
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
