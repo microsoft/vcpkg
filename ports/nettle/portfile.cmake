c69eb803bb060ec020ea4842375022a3567f2801
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:35:32 +0100

    add nettle

---
+++ ports/nettle/portfile.cmake
@@ -0,0 +1,47 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+## requires AUTOCONF, LIBTOOL and PKCONF
+message(STATUS "----- ${PORT} requires autoconf, libtool and pkconf from the system package manager! \n ----- sudo apt-get install autogen autoconf libtool-----")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://git.lysator.liu.se/
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO nettle/nettle
+    REF  ee5d62898cf070f08beedc410a8d7c418588bd95 #v3.5.1 
+    SHA512 881912548f4abb21460f44334de11439749c8a055830849a8beb4332071d11d9196d9eecaeba5bf822819d242356083fba91eb8719a64f90e41766826e6d75e1
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL ${SHELL_PATH}
+    OPTIONS
+        --disable-documentation
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share/")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYINGv3" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+# # Post-build test for cmake libraries
+# vcpkg_test_cmake(PACKAGE_NAME Xlib)
+
+set(TOOLS nettle-hash nettle-lfib-stream nettle-pbkdf2 sexp-conv)
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+foreach(tool ${TOOLS})
+    file(RENAME "${CURRENT_PACKAGES_DIR}/bin/${tool}${VCPKG_TARGET_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/${tool}${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
+endforeach()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" OR VCPKG_TARGET_IS_LINUX)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
