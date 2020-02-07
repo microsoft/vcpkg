8b4a1b55e6e3a4701cf6bff77e668eb266f24c95
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:37:07 +0100

    add xaw3d

---
+++ ports/xaw3d/portfile.cmake
@@ -0,0 +1,38 @@
+message(STATUS "----- ${PORT} requires autoconf, libtool, Yacc, lex and pkconf from the system package manager! -----") #Lex and Yacc
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxaw3d
+    REF 62e18b8884efd39e8d764442f7b354d3e34fefbd # 1.6.3 
+    SHA512  c4cfbf3e6085c1b9e0fae3a15b1ec2a27af20c5f14c20a47f4c0fc96512feded888c7152484667135880bf93a12b0837730f29be416ea290795b385cbe511c5e
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
+    #OPTIONS
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
