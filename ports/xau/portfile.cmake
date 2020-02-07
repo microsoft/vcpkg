60eeaeba3121628ae07a0b04155f992b9c13aa18
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:36:54 +0100

    add xau

---
+++ ports/xau/portfile.cmake
@@ -0,0 +1,44 @@
+message(STATUS "----- ${PORT} requires autoconf, libtool and pkconf from the system package manager! -----")
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxau
+    REF d9443b2c57b512cfb250b35707378654d86c7dea # 1.0.9 
+    SHA512  d46d2be838b8ea7116ee0e312adafa80f6551762b5f7b5e503bd40e74fc0b43b45255e8135d03f831d5c483b98aac992fcd91a7e22119261e76778571a72ef07
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+if(WIN32)
+    SET(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled) # dumpbin will fail on GCC
+    set(OPTIONS --disable-dependency-tracking)
+    #string(APPEND VCPKG_C_FLAGS "/DX_NOT_POSIX")
+endif()
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
+    OPTIONS ${OPTIONS}
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
