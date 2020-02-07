8a606a029a132842b5114db74bb00c3dc4b022f8
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-07 13:43:08 +0100

    add xcb-keysyms

---
+++ ports/xcb-keysyms/portfile.cmake
@@ -0,0 +1,39 @@
+## requires AUTOCONF, LIBTOOL and PKCONF
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxcb-keysyms
+    REF  0e51ee5570a6a80bdf98770b975dfe8a57f4eeb1 #v 0.4.0
+    SHA512 5fe01c8902ed09582c6c5d98a24dc2a19e16f507200e7ee4a75889d0fec4e2faea910a1994b0c85e3139a28b563b1edf5db86284e5d19e14760f39eb893f623c
+    HEAD_REF master # branch name
+    #PATCHES example.patch #patch name
+) 
+file(TOUCH ${SOURCE_PATH}/m4/dummy)
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    #SKIP_CONFIGURE
+    #NO_DEBUG
+    #AUTO_HOST
+    #AUTO_DST
+    #PRERUN_SHELL ${SHELL_PATH}
+    #OPTIONS
+    #OPTIONS_DEBUG
+    #OPTIONS_RELEASE
+    PKG_CONFIG_PATHS_RELEASE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig"
+    PKG_CONFIG_PATHS_DEBUG "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig"
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
+file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
+
+
