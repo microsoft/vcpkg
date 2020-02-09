d338342b45835af540467b3c37b81f275f86a0ee
Robert Schumacher <roschuma@microsoft.com>
2020-02-08 21:10:00 -0800

    [vcpkg] Fix various uses of >NUL across the code

--- toolsrc/src/vcpkg/build.cpp
+++ toolsrc/src/vcpkg/build.cpp
@@ -301,7 +301,7 @@ namespace vcpkg::Build
         const auto arch = to_vcvarsall_toolchain(pre_build_info.target_architecture, toolset);
         const auto target = to_vcvarsall_target(pre_build_info.cmake_system_name);
 
-        return Strings::format(R"("%s" %s %s %s %s 2>&1 <NUL)",
+        return Strings::format(R"(cmd /c ""%s" %s %s %s %s 2>&1 <NUL")",
                                toolset.vcvarsall.u8string(),
                                Strings::join(" ", toolset.vcvarsall_options),
                                arch,
@@ -750,7 +750,9 @@ namespace vcpkg::Build
         return nullopt;
     }
 
-    static int decompress_archive(const VcpkgPaths& paths, const PackageSpec& spec, const fs::path& archive_path)
+    static System::ExitCodeAndOutput decompress_archive(const VcpkgPaths& paths,
+                                                        const PackageSpec& spec,
+                                                        const fs::path& archive_path)
     {
         auto& fs = paths.get_filesystem();
 
@@ -763,14 +765,12 @@ namespace vcpkg::Build
 
 #if defined(_WIN32)
         auto&& seven_zip_exe = paths.get_tool_exe(Tools::SEVEN_ZIP);
-
-        int result = System::cmd_execute_clean(Strings::format(
-            R"("%s" x "%s" -o"%s" -y >nul)", seven_zip_exe.u8string(), archive_path.u8string(), pkg_path.u8string()));
+        auto cmd = Strings::format(
+            R"("%s" x "%s" -o"%s" -y)", seven_zip_exe.u8string(), archive_path.u8string(), pkg_path.u8string());
 #else
-        int result = System::cmd_execute_clean(
-            Strings::format(R"(unzip -qq "%s" "-d%s")", archive_path.u8string(), pkg_path.u8string()));
+        auto cmd = Strings::format(R"(unzip -qq "%s" "-d%s")", archive_path.u8string(), pkg_path.u8string());
 #endif
-        return result;
+        return System::cmd_execute_and_capture_output(cmd, System::get_clean_environment());
     }
 
     // Compress the source directory into the destination file.
@@ -786,8 +786,10 @@ namespace vcpkg::Build
 #if defined(_WIN32)
         auto&& seven_zip_exe = paths.get_tool_exe(Tools::SEVEN_ZIP);
 
-        System::cmd_execute_clean(Strings::format(
-            R"("%s" a "%s" "%s\*" >nul)", seven_zip_exe.u8string(), destination.u8string(), source.u8string()));
+        System::cmd_execute_and_capture_output(
+            Strings::format(
+                R"("%s" a "%s" "%s\*")", seven_zip_exe.u8string(), destination.u8string(), source.u8string()),
+            System::get_clean_environment());
 #else
         System::cmd_execute_clean(
             Strings::format(R"(cd '%s' && zip --quiet -r '%s' *)", source.u8string(), destination.u8string()));
@@ -866,7 +868,7 @@ namespace vcpkg::Build
             {
                 System::print2("Using cached binary package: ", archive_path.u8string(), "\n");
 
-                int archive_result = decompress_archive(paths, spec, archive_path);
+                int archive_result = decompress_archive(paths, spec, archive_path).exit_code;
 
                 if (archive_result != 0)
                 {
