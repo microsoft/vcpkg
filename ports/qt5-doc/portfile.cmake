aee74045651f3920d6b11ba5bd7397ebe8079114
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-02-09 23:45:51 +0100

    qtdoc does not have any include files

--- ports/qt5-doc/portfile.cmake
+++ ports/qt5-doc/portfile.cmake
@@ -1,3 +1,4 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 if(VCPKG_TARGET_IS_WINDOWS)
     set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
     message(STATUS "${PORT} will not build any artifacts on Windows!")
