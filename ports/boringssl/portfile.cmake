81dbd4cc7619fe64f513682c78e6c398063f8419
Alexej Harm <alexej.h@xiphos.de>
2020-02-11 15:23:13 +0100

    applied requested changes

--- ports/boringssl/portfile.cmake
+++ ports/boringssl/portfile.cmake
@@ -8,20 +8,19 @@ endif()
 
 vcpkg_find_acquire_program(PERL)
 get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-set(ENV{PATH} "$ENV{PATH};${PERL_EXE_PATH}")
+vcpkg_add_to_path(${PERL_EXE_PATH})
 
 vcpkg_find_acquire_program(NASM)
 get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
-set(ENV{PATH} "${NASM_EXE_PATH};$ENV{PATH}")
+vcpkg_add_to_path(${NASM_EXE_PATH})
 
 vcpkg_find_acquire_program(GO)
 get_filename_component(GO_EXE_PATH ${GO} DIRECTORY)
-set(ENV{PATH} "${GO_EXE_PATH};$ENV{PATH}")
+vcpkg_add_to_path(${GO_EXE_PATH})
 
-set(INSTALL_TOOLS OFF)
-if("tools" IN_LIST FEATURES)
-  set(INSTALL_TOOLS ON)
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  tools INSTALL_TOOLS
+)
 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
@@ -37,28 +36,16 @@ vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
   PREFER_NINJA
   OPTIONS
-    -DINSTALL_TOOLS=${INSTALL_TOOLS}
+    ${FEATURE_OPTIONS}
   OPTIONS_DEBUG
+    -DINSTALL_HEADERS=OFF
     -DINSTALL_TOOLS=OFF
 )
 
 vcpkg_install_cmake()
 
-if(INSTALL_TOOLS)
-  if(NOT VCPKG_CMAKE_SYSTEM_NAME)
-    set(EXECUTABLE_SUFFIX .exe)
-  endif()
-  file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/boringssl")
-  file(RENAME "${CURRENT_PACKAGES_DIR}/bin/bssl${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/boringssl/bssl${EXECUTABLE_SUFFIX}")
+if(IS_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/boringssl)
   vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/boringssl")
 endif()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/bin"
-    "${CURRENT_PACKAGES_DIR}/debug/bin"
-  )
-endif()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
