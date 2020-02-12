80a68cd70afc5972884ed598504786cb0ef357c0
Michael Konečný <konecny.mike@gmail.com>
2020-02-12 12:56:26 +0100

    add mirror for mpfr at gnu.org

--- ports/mpfr/portfile.cmake
+++ ports/mpfr/portfile.cmake
@@ -1,7 +1,7 @@
 include(vcpkg_common_functions)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz"
+    URLS "http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.xz" "https://ftp.gnu.org/gnu/mpfr/mpfr-4.0.2.tar.xz"
     FILENAME "mpfr-4.0.2.tar.xz"
     SHA512 d583555d08863bf36c89b289ae26bae353d9a31f08ee3894520992d2c26e5683c4c9c193d7ad139632f71c0a476d85ea76182702a98bf08dde7b6f65a54f8b88
 )
