c8fe6951409ac89e30ff4c121c33666bb42ff946
Alexander Neumann <alexander.neumann@hamburg.de>
2020-02-14 15:45:47 +0100

    make xcb-util build with cl

--- ports/xcb-util/portfile.cmake
+++ ports/xcb-util/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_gitlab(
     REF  acf790d7752f36e450d476ad79807d4012ec863b #v0.4.0
     SHA512 d1ef49c1e16b7643a7afeca1495a96ab9ab9c537ea7669a13b3adda400a204626714afc8ed7fcc3d7532ebe1f89a3aa31e3ca0ee9617330d4df5b65b0c8e6dbc
     HEAD_REF master # branch name
-    #PATCHES example.patch #patch name
+    PATCHES ssize.patch #patch name
 ) 
 
 file(TOUCH "${SOURCE_PATH}/m4/dummy")
