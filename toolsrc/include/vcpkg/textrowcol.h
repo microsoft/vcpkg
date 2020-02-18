e130ff1c69407c79832db2ee538f13c093679b52
Victor Romero <romerosanchezv@gmail.com>
2020-02-18 12:53:50 -0800

    Add missing constructors for TexRowCol required by VS2015

--- toolsrc/include/vcpkg/textrowcol.h
+++ toolsrc/include/vcpkg/textrowcol.h
@@ -4,6 +4,8 @@ namespace vcpkg::Parse
 {
     struct TextRowCol
     {
+        TextRowCol() = default;
+        TextRowCol(int row, int column) : row(row), column(column) {}
         /// '0' indicates uninitialized; '1' is the first row.
         int row = 0;
         /// '0' indicates uninitialized; '1' is the first column.
