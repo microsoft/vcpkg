f2a7ec0a91bcc530651566bca4261579fee7caaf
Victor Romero <romerosanchezv@gmail.com>
2020-02-19 12:58:14 -0800

    Make TextRowCol() constexpr and noexcept

--- toolsrc/include/vcpkg/textrowcol.h
+++ toolsrc/include/vcpkg/textrowcol.h
@@ -4,8 +4,8 @@ namespace vcpkg::Parse
 {
     struct TextRowCol
     {
-        TextRowCol() = default;
-        TextRowCol(int row, int column) : row(row), column(column) {}
+        constexpr TextRowCol() noexcept = default;
+        constexpr TextRowCol(int row, int column) noexcept : row(row), column(column) {}
         /// '0' indicates uninitialized; '1' is the first row.
         int row = 0;
         /// '0' indicates uninitialized; '1' is the first column.
