000330686c32cb6e5f8abff58c1d08671b17c57f
wangli28 <wangli28@beyondsoft.com>
2020-02-19 04:17:28 +0000

    [cli11] Update to version 1.9.0

--- ports/cli11/portfile.cmake
+++ ports/cli11/portfile.cmake
@@ -1,11 +1,10 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CLIUtils/CLI11
-    REF v1.8.0
-    SHA512 80071573e3eee524df1ed8cd1fc23c0e197f3ba113db361cb3c2cb843109ddb1c35a65bcb989364bc53d872a80f180e4859f9598da443b870a6260f991705182
+    REF dd0d8e4fe729e5b1110232c7a5c9566dad884686 #version 1.9.0
+    SHA512 dccee89de994d17537b31db717d0f42cae8827a192067718641a6e9e3188f468047a86ce329781142b9c7a1216d5eedcfe975fdbd001f40005a8ab50e08470d9
     HEAD_REF master
+    PATCHES fix-GtestSubmodule.patch
 )
 
 vcpkg_configure_cmake(
@@ -14,6 +13,7 @@ vcpkg_configure_cmake(
     OPTIONS
         -DCLI11_TESTING=OFF
         -DCLI11_EXAMPLES=OFF
+        -DENABLE_DOCS=OFF
 )
 
 vcpkg_install_cmake()
@@ -23,7 +23,4 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/CLI11)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cli11 RENAME copyright)
-
-# Post-build test for cmake libraries
-# vcpkg_test_cmake(PACKAGE_NAME cli11)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
