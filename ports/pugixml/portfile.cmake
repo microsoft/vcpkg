681d19886de556f83cf9c0f5459814a9170c495f
Greg Williamson <fundies@users.noreply.github.com>
2020-02-20 07:20:08 -0500

    use option

--- ports/pugixml/portfile.cmake
+++ ports/pugixml/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DCMAKE_DEBUG_POSTFIX=d
+    OPTIONS -DUSE_POSTFIX=ON
 )
 
 vcpkg_install_cmake()
