6d0bb3681c8efabd7da52e26c448b783b2cf7a7f
DDoSolitary <DDoSolitary@gmail.com>
2020-02-21 15:04:24 +0800

    Add "pthreads" feature to mbedtls.

--- ports/mbedtls/portfile.cmake
+++ ports/mbedtls/portfile.cmake
@@ -8,12 +8,22 @@ vcpkg_from_github(
     REF mbedtls-2.16.3
     SHA512 3d798f7de9c33325585d5d7c8608cc16acdcf42c246d283b2fb8a29f5e419f2899342965ff297432ef2ab20c91eaee28d6ca53349f5a68b0a4fd29d6905fc64c
     HEAD_REF master
+    PATCHES
+        enable-pthreads4w.patch
 )
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        pthreads ENABLE_PTHREADS4W
+)
+
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
+        ${FEATURE_OPTIONS}
         -DENABLE_TESTING=OFF
         -DENABLE_PROGRAMS=OFF
 )
