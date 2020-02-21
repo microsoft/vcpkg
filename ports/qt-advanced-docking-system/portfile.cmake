5153a4de4691675e88c08626e82ec400e5b2a44c
Stijn Herfst <stijnherfst@outlook.com>
2020-02-21 23:58:31 +0100

    fixed source hash

--- ports/qt-advanced-docking-system/portfile.cmake
+++ ports/qt-advanced-docking-system/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO githubuser0xFFFF/Qt-Advanced-Docking-System
     REF 3ffbbfb6d01ff211d8349027221a19b1419296b5
-    SHA512 0f71a9015f6f500ef5749d6ea3e1d5311d7e892fe5d21fc59f65736484c95d37eb7a03354ab34f8c3801813fa6bcbdcc1746cdbdc262b3870f5b323dc3acca5d
+    SHA512 b705d6a125d4e16805e2cc8dda11f157066dc3039decfb058e37e5c9307bdaaa229e448f1d7fe6de0086bd9a2abeac73a64dac93b28bd45bba5e093240e54193
     HEAD_REF master
     PATCHES
         config_changes.patch
