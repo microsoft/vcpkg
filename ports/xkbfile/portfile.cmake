676fa007dfc42cdade2e17adb97ff74f6dcb6f3b
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-02-24 23:59:37 +0100

    fix xkbfile

--- ports/xkbfile/portfile.cmake
+++ ports/xkbfile/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_gitlab(
     REF  261992d42905f209cd5bf6afcf8a7ae3aa30b3ff #1.1.0
     SHA512 5be520e408d25331c9a97648f2a6fa832f0d4f49f93f71490b89746da0fbbda404eaab3797c5fbe195287dc94581a6703fa4ecc2511e046127af057eab60378f
     HEAD_REF master # branch name
-    #PATCHES example.patch #patch name
+    PATCHES fix_u_char.patch #patch name
 ) 
 #file(TOUCH ${SOURCE_PATH}/m4/dummy)
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
