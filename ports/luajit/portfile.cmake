313776b689b74a94060084663770f749f938e493
JackBoosY <yuzaiyang@beyondsoft.com>
2020-02-23 22:48:13 -0800

    [luajit] Fix incorrect merge

--- ports/luajit/portfile.cmake
+++ ports/luajit/portfile.cmake
@@ -30,6 +30,9 @@ if (VCPKG_TARGET_IS_WINDOWS)
             LOGNAME build-${TARGET_TRIPLET}-dbg
         )
         
+        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/luajit.exe DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools)
+        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    
         if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
             file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
             file(COPY ${CURRENT_PACKAGES_DIR}/debug/bin/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools)
@@ -47,29 +50,40 @@ if (VCPKG_TARGET_IS_WINDOWS)
             COMMAND "${SOURCE_PATH}/src/msvcbuild.bat" ${SOURCE_PATH}/src ${VCPKG_CRT_LINKAGE} ${LJIT_STATIC}
             WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
             LOGNAME build-${TARGET_TRIPLET}-rel
-    )
+        )
+    
+        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/luajit.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
+        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+
+        if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+            file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+            vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools)
+        endif()
+        vcpkg_copy_pdbs()
+    endif()
 else()
     vcpkg_configure_make(
         SOURCE_PATH ${SOURCE_PATH}
         SKIP_CONFIGURE
         NO_DEBUG
-)
+    )
     
     vcpkg_build_make()
     
-    if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-        vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools)
+    file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}/src/libluajit.a DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}/src/libluajit.so DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
     endif()
+    
     file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}/src/luajit DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
 endif()
 
-file(INSTALL ${SOURCE_PATH}/src/lua.h 			    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/luajit.h 	    	DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/luaconf.h 		    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lualib.h 		    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lauxlib.h 		    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lua.hpp 		    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/lua.h     DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/luajit.h  DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/luaconf.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/lualib.h  DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/lauxlib.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL ${SOURCE_PATH}/src/lua.hpp   DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
