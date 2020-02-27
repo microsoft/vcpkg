dc407a476cb3daea1420048844f647276867312c
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-02-27 09:45:23 +0100

    Use modern vcpkg functions and remove deprecated code

--- ports/cgal/portfile.cmake
+++ ports/cgal/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_buildpath_length_warning(37)
 
 vcpkg_from_github(
@@ -11,9 +9,10 @@ vcpkg_from_github(
 )
 
 set(WITH_CGAL_Qt5  OFF)
-if("qt" IN_LIST FEATURES)
-  set(WITH_CGAL_Qt5 ON)
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    qt WITH_CGAL_Qt5
+    )
+
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -21,7 +20,7 @@ vcpkg_configure_cmake(
     OPTIONS
         -DCGAL_HEADER_ONLY=ON
         -DCGAL_INSTALL_CMAKE_DIR=share/cgal
-        -DWITH_CGAL_Qt5=${WITH_CGAL_Qt5}
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_install_cmake()
@@ -46,8 +45,8 @@ endif()
 
 file(WRITE ${CURRENT_PACKAGES_DIR}/lib/cgal/CGALConfig.cmake "include (\$\{CMAKE_CURRENT_LIST_DIR\}/../../share/cgal/CGALConfig.cmake)")
 
-file(COPY ${SOURCE_PATH}/Installation/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cgal)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/cgal/LICENSE ${CURRENT_PACKAGES_DIR}/share/cgal/copyright)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 file(
     COPY
@@ -59,4 +58,3 @@ file(
 )
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/cgal)
-vcpkg_test_cmake(PACKAGE_NAME CGAL)
