a7f0892ca286a0b52f94202b5c4e3672525cf526
Bill Avery <billav@microsoft.com>
2020-03-09 08:34:15 -0700

    Remove deprecated vcpkg functions

--- ports/cppgraphqlgen/portfile.cmake
+++ ports/cppgraphqlgen/portfile.cmake
@@ -1,10 +1,8 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/cppgraphqlgen
-    REF c286c837223069a41ba3130dbee42314fd48de66 # v3.1.0
-    SHA512 57a0923655976c9749794f4e4b8bf5397cfc974cd373f9dabb60b208052bb85d27f063f9456a2d0270eab1e040450c410e906d897bd7318c23572ecce6b60b18
+    REF 766d3b857b871b135e0974cb84ee51e6f0860da0 # v3.1.0
+    SHA512 2d9dc8cd1c1df592812a49724a3a29fef1b57d3dc278207becad3c3dcbd278a5e343719d6727b28dee19ca287cc1126783f981580e26a3faaa2a278562db14f3
     HEAD_REF master
 )
 
@@ -25,5 +23,3 @@ vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/cppgraphqlgen)
 vcpkg_copy_pdbs()
 
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/cppgraphqlgen/copyright COPYONLY)
-
-vcpkg_test_cmake(PACKAGE_NAME cppgraphqlgen)
