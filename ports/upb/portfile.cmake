670a2656df7ac34937ad4eb7c14eeba772954c9d
Robert Schumacher <roschuma@microsoft.com>
2020-03-09 16:37:52 -0700

    [grpc][openssl][upb] Fix install on uwp

--- ports/upb/portfile.cmake
+++ ports/upb/portfile.cmake
@@ -3,10 +3,12 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO protocolbuffers/upb
-    REF  9effcbcb27f0a665f9f345030188c0b291e32482
+    REF 9effcbcb27f0a665f9f345030188c0b291e32482
     SHA512 416ff26ec21181d53be23e94203205072152ab3a8e4b3b28d16263a601995fd2d2f8afe5d8cfbecdac8543249482287b9fe6129314f7c9a7880660f5508bb85e
     HEAD_REF master
-    PATCHES add-cmake-install.patch
+    PATCHES
+        add-cmake-install.patch
+        fix-uwp.patch
 )
 
 vcpkg_configure_cmake(
