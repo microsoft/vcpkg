d1e261b03a25bb54e81a8aa9016400201a4c7d20
Alexander Neumann <alexander.neumann@hamburg.de>
2020-03-17 20:08:02 +0100

    cleanup docs

--- scripts/cmake/vcpkg_build_make.cmake
+++ scripts/cmake/vcpkg_build_make.cmake
@@ -4,15 +4,25 @@
 ##
 ## ## Usage:
 ## ```cmake
-## vcpkg_build_make([TARGET <target>])
+## vcpkg_build_make([BUILD_TARGET <target>]
+##                  [ADD_BIN_TO_PATH]
+##                  [ENABLE_INSTALL])
+##                  [LOGFILE_ROOT <logfileroot>])
 ## ```
 ##
-## ### TARGET
-## The target passed to the configure/make build command (`./configure/make/make install`). If not specified, no target will
+## ### BUILD_TARGET
+## The target passed to the make build command (`./make <target>`). If not specified, the 'all' target will
 ## be passed.
 ##
 ## ### ADD_BIN_TO_PATH
 ## Adds the appropriate Release and Debug `bin\` directories to the path during the build such that executables can run against the in-tree DLLs.
+##
+## ### ENABLE_INSTALL
+## IF the port supports the install target use vcpkg_install_make() instead of vcpkg_build_make()
+##
+## ### BUILD_TARGET
+## The target passed to the make build command (`./make <target>`). If not specified, the 'all' target will
+## be passed.
 ##
 ## ## Notes:
 ## This command should be preceeded by a call to [`vcpkg_configure_make()`](vcpkg_configure_make.md).
