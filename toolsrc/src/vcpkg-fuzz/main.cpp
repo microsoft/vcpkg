2ab79702973b8908df52b7fd35aea6baf5473145
Nicole Mazzuca <mazzucan@outlook.com>
2020-03-23 13:24:27 -0700

    [vcpkg] Add initial JSON support
    This adds a JSON parser, as well as the amount of unicode support
    required for JSON parsing to work according to the specification. In the
    future, I hope to rewrite our existing XML files into JSON.
    
    Additionally, as a drive-by, we've added the following:
    
    * add /wd4800 to pragmas.h -- this is a "performance warning", for when
      you implicitly convert pointers or integers to bool, and shouldn't be
      an issue for us.
    * Switched Parse::ParserBase to read unicode (as utf-8), as opposed to
      ASCII
    * Building again under VCPKG_DEVELOPMENT_WARNINGS, yay!

---
+++ toolsrc/src/vcpkg-fuzz/main.cpp
@@ -0,0 +1,175 @@
+#include <vcpkg/base/checks.h>
+#include <vcpkg/base/json.h>
+#include <vcpkg/base/stringview.h>
+#include <vcpkg/base/system.print.h>
+
+#include <iostream>
+#include <string.h>
+#include <utility>
+
+using namespace vcpkg;
+
+namespace
+{
+    enum class FuzzKind
+    {
+        None,
+        Utf8Decoder,
+        JsonParser,
+    };
+
+    struct FuzzArgs
+    {
+        FuzzArgs(int argc, char** argv)
+        {
+            if (argc <= 1)
+            {
+                print_help_and_exit();
+            }
+
+            char** it = argv + 1; // skip the name of the program
+            char** last = argv + argc;
+
+            for (; it != last; ++it)
+            {
+                auto arg = StringView(*it, strlen(*it));
+                if (arg == "/?")
+                {
+                    print_help_and_exit();
+                }
+
+                auto pr = split_arg(arg);
+                auto key = pr.first;
+                auto value = pr.second;
+                if (key == "h" || key == "help")
+                {
+                    print_help_and_exit();
+                }
+
+                if (key == "kind")
+                {
+                    if (value == "json")
+                    {
+                        kind = FuzzKind::JsonParser;
+                    }
+                    else if (value == "utf-8")
+                    {
+                        kind = FuzzKind::Utf8Decoder;
+                    }
+                    else
+                    {
+                        System::print2(System::Color::error, "Invalid kind: ", value, "\n");
+                        System::print2(System::Color::error, "  Expected one of: utf-8, json\n\n");
+                        print_help_and_exit(true);
+                    }
+                }
+                else
+                {
+                    System::print2("Unknown option: ", key, "\n\n");
+                    print_help_and_exit(true);
+                }
+            }
+        }
+
+        // returns {arg, ""} when there isn't an `=`
+        // skips preceding `-`s
+        std::pair<StringView, StringView> split_arg(StringView arg)
+        {
+            auto start = arg.begin();
+            const auto last = arg.end();
+
+            // skip preceding `-`s
+            while (start != last)
+            {
+                if (*start == '-')
+                {
+                    ++start;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            auto it = start;
+            for (; it != last; ++it)
+            {
+                if (*it == '=')
+                {
+                    return std::make_pair(StringView(start, it), StringView(it + 1, last));
+                }
+            }
+
+            return std::make_pair(StringView(start, last), StringView(last, last));
+        }
+
+        [[noreturn]] void print_help_and_exit(bool invalid = false)
+        {
+            constexpr auto help =
+                R"(
+Usage: vcpkg-fuzz <options>
+
+Accepts input on stdin.
+
+Options:
+  --kind=...                One of {utf-8, json}
+)";
+
+            auto color = invalid ? System::Color::error : System::Color::success;
+
+            System::print2(color, help);
+            if (invalid)
+            {
+                Checks::exit_fail(VCPKG_LINE_INFO);
+            }
+            else
+            {
+                Checks::exit_success(VCPKG_LINE_INFO);
+            }
+        }
+
+        FuzzKind kind;
+    };
+
+    std::string read_all_of_stdin()
+    {
+        constexpr std::size_t block_size = 1000;
+        std::string res;
+        std::size_t current_size = 0;
+        for (;;)
+        {
+            res.resize(current_size + block_size);
+            std::cin.read(&res[current_size], block_size);
+            current_size += std::cin.gcount();
+            if (std::cin.eof())
+            {
+                break;
+            }
+        }
+        res.resize(current_size);
+
+        return res;
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+    auto args = FuzzArgs(argc, argv);
+
+    if (args.kind == FuzzKind::None)
+    {
+        args.print_help_and_exit(true);
+    }
+
+    auto text = read_all_of_stdin();
+    auto res = Json::parse(text);
+    if (!res)
+    {
+        System::print2(System::Color::error, res.error()->format());
+    }
+    else
+    {
+        System::print2(System::Color::success, "success!");
+    }
+}
