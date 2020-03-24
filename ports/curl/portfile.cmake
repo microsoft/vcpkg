3954072837e79636eeb307e66bf992f4aad82784
wangli28 <wangli28@beyondsoft.com>
2020-03-24 13:07:16 +0000

    [curl] Fix line alignment

--- ports/curl/portfile.cmake
+++ ports/curl/portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_from_github(
         0007_disable_tool_export_curl_target.patch
         0008_fix_tools_path.patch
         0009_fix_openssl_config.patch
-		0010_fix_ZlibFeature.patch
+        0010_fix_ZlibFeature.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" CURL_STATICLIB)
