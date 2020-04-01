203f826756a22b268a9cbd9f564d67f6861a4284
Simone Gasparini <simone.gasparini@gmail.com>
2020-04-01 17:08:58 +0200

    [ports] add popsift

---
+++ ports/popsift/portfile.cmake
@@ -0,0 +1,40 @@
+# TODO
+# 1. change:
+#   - HEAD_REF to master
+#   - REF and SHA512 to the release version (remember to push to master the develop with release)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO alicevision/popsift	
+    REF v1.0.0-rc1
+    SHA512 df4732082504186a751c7941ef51c9bd8d7c4035db47d465af6b622a08c0a4ab5c59604783e073c32b7490a15c854836221e336b02a28637abf04283635c7204
+    HEAD_REF develop
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    apps       PopSift_BUILD_EXAMPLES
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS ${FEATURE_OPTIONS}
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/PopSift)
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
+
+ # move the bin direcory to tools
+ if ("apps" IN_LIST FEATURES)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools)
+    file(RENAME "${CURRENT_PACKAGES_DIR}/bin" ${CURRENT_PACKAGES_DIR}/tools/popsift)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/popsift)
+ endif()
+
+file(INSTALL ${SOURCE_PATH}/COPYING.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/popsift RENAME copyright)
