9a84f487e6f735eabff11acb3ca4d15a94ce4989
Glyn Matthews <glyn.matthews@gmail.com>
2020-04-04 17:24:16 +0200

    Bumped version of skyr-url to 1.5.1

--- ports/skyr-url/portfile.cmake
+++ ports/skyr-url/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO cpp-netlib/url
-        REF v1.4.5
-        SHA512 39501217e331a904daf928a5874f81808a9665a9e8debe39c15a3fb7607ab293a5a1b335062cc7ac8f4fe239d4233a2c5ed0e9b45dbec7edcc267eb3d25509d3
+        REF v1.5.1
+        SHA512 0259b6eed43e480779d35990bdebd32bdbd3c30a548fba46a6b03afd742c3fd862d2a2cba6f49a6baf669763e7701c12b457530e424f2b7175533911e911ae3b
         HEAD_REF master
 )
 
@@ -22,6 +22,7 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE_1_0.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/skyr-url RENAME copyright)
