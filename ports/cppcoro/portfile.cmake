dfdb2095fbef21060df6205bd2875ede4a585a7d
Park DongHa <luncliff@gmail.com>
2020-04-07 23:47:29 +0900

    [cppcoro] update with PR review
    * update reference to tag and hash value
    * ${PORT} for destination
    
    Linux build support should marked unavailable?

--- ports/cppcoro/portfile.cmake
+++ ports/cppcoro/portfile.cmake
@@ -1,11 +1,11 @@
-include(vcpkg_common_functions)
-
+#
 # the repo is fork of https://github.com/lewissbaker/cppcoro to support CMake / VcPkg
+#
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            luncliff/cppcoro
-    REF             20b4217a65165d0ec7849fbe7970310cdf31b6a5
-    SHA512          ab1545da09bb7053d95afd4ea9d5ec17cc42824d047e6887632c1d14c1b02a77cb8b99b73b7e51293a252e13ef615eaa89a31cdb6aa88148f9f8f6a450fa0599
+    REF             2020.04
+    SHA512          bd60a97911f4ee6c692e99817e9691cc5063be816c3a09a6100bc8f590f5b1e7b13a372a87e1710993b1bc177bbd99d779b2ebf16a367851b256e396bfd038da
     HEAD_REF        master
 )
 
@@ -18,9 +18,10 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 file(INSTALL     ${SOURCE_PATH}/LICENSE.txt
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/cppcoro
+     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
      RENAME      copyright
 )
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
