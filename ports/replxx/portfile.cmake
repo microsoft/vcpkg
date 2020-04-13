2001dc94f58bd659fa91e4532f81c32504d34416
JackBoosY <yuzaiyang@beyondsoft.com>
2020-04-12 21:59:39 -0700

    [replxx] Re-trigger ci test

--- ports/replxx/portfile.cmake
+++ ports/replxx/portfile.cmake
@@ -18,5 +18,6 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/replxxConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
