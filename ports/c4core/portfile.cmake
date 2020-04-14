edf9f20856b657dcdeb8135063a9ef50867f959d
vvandrounik <vvandrounik@gmail.com>
2020-04-14 10:56:00 +0300

    Mark port as unsupport OSX

--- ports/c4core/portfile.cmake
+++ ports/c4core/portfile.cmake
@@ -1,5 +1,9 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-vcpkg_fail_port_install(ON_ARCH "arm" "arm64")
+
+vcpkg_fail_port_install(
+    ON_ARCH "arm" "arm64"
+    ON_TARGET "OSX"
+)
 
 # Get c4core src
 vcpkg_from_github(
