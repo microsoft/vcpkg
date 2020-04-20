d9442fdf6d597bb7a253448e8765b867b584f209
Travis Drake <tdrake@qsrautomations.com>
2020-04-20 12:29:54 -0400

    Add patch for CMakeLists.txt add install target for CrashRpt and CrashSender

--- ports/crashrpt/portfile.cmake
+++ ports/crashrpt/portfile.cmake
@@ -12,6 +12,8 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://git.code.sf.net/p/crashrpt/code
     REF 4616504670be5a425a525376648d912a72ce18f2
+    PATCHES
+        001-cmake-install.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" CRASHRPT_BUILD_SHARED_LIBS)
@@ -25,40 +27,14 @@ vcpkg_configure_cmake(
     OPTIONS
         -DCRASHRPT_BUILD_SHARED_LIBS=${CRASHRPT_BUILD_SHARED_LIBS}
         -DCRASHRPT_LINK_CRT_AS_DLL=${CRASHRPT_LINK_CRT_AS_DLL}
+        -DCRASHRPT_BUILD_DEMOS=OFF
+        -DCRASHRPT_BUILD_PROBE=OFF
+        -DCRASHRPT_BUILD_TESTS=OFF
 )
 
-# CrashRpt does not have an install target, so build the two targets that are needed and copy the output
-vcpkg_build_cmake(TARGET CrashSender)
-vcpkg_build_cmake(TARGET CrashRpt)
-
-file(INSTALL ${SOURCE_PATH}/include/CrashRpt.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-
-set(DEBUG_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
-set(RELEASE_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    file(INSTALL ${DEBUG_DIR}/lib/${ARCH_DIR}CrashRpt1403d.exp DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-    file(INSTALL ${DEBUG_DIR}/lib/${ARCH_DIR}CrashRpt1403d.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-    file(INSTALL ${DEBUG_DIR}/bin/${ARCH_DIR}CrashRpt1403d.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    
-    file(INSTALL ${RELEASE_DIR}/lib/${ARCH_DIR}CrashRpt1403.exp DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${RELEASE_DIR}/lib/${ARCH_DIR}CrashRpt1403.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${RELEASE_DIR}/bin/${ARCH_DIR}CrashRpt1403.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-else()
-    file(INSTALL ${DEBUG_DIR}/lib/${ARCH_DIR}CrashRptLIBd.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-    
-    file(INSTALL ${RELEASE_DIR}/lib/${ARCH_DIR}CrashRptLIB.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-endif()
-
-file(INSTALL ${DEBUG_DIR}/bin/${ARCH_DIR}CrashSender1403d.exe DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT})
-file(INSTALL ${RELEASE_DIR}/bin/${ARCH_DIR}CrashSender1403.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
-file(GLOB LANG_FILES "${SOURCE_PATH}/lang_files/crashrpt_lang_*.ini")
-file(INSTALL ${LANG_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
-file(INSTALL ${SOURCE_PATH}/thirdparty/dbghelp/bin/dbghelp.dll DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
-
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/debug/tools/${PORT})
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
-
+vcpkg_install_cmake()
 vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
