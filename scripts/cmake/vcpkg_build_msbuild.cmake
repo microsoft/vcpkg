9e8dc47341d83ed3a111e032429e2776bb8dc0e7
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-04-21 01:46:56 +0800

    Update vcpkg_build_msbuild.cmake

--- scripts/cmake/vcpkg_build_msbuild.cmake
+++ scripts/cmake/vcpkg_build_msbuild.cmake
@@ -93,7 +93,7 @@ function(vcpkg_build_msbuild)
         /t:${_csc_TARGET}
         /p:Platform=${_csc_PLATFORM}
         /p:PlatformToolset=${_csc_PLATFORM_TOOLSET}
-        /p:BasePlatformToolset=${_csc_PLATFORM_TOOLSET}
+#        /p:BasePlatformToolset=${_csc_PLATFORM_TOOLSET}
         /p:UseEnv=True
         /p:VCPkgLocalAppDataDisabled=true
         /p:UseIntelMKL=No
