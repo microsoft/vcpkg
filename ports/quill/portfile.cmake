4ea4947f7215164ab7c8a3122f07104b23afa067
odygrd <odygrd@gmail.com>
2020-04-21 12:48:24 +0100

    use vcpkg_from_github and simplify feature options

--- ports/quill/portfile.cmake
+++ ports/quill/portfile.cmake
@@ -1,25 +1,21 @@
 vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "uwp")
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/odygrd/quill/archive/v1.2.0.tar.gz"
-    FILENAME "v1.2.0.tar.gz"
-    SHA512 e86f2c986740fd814067caf9d50a26214ac0681d98cd1b48e84daff97a754fb733266a45cd22d530bb5ffa186b1e90af4f9771a35ad6fa76f267980b9fe91a74
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    REF 1.2.0
+    REPO odygrd/quill
+    REF 18271f251ccd5244c0052ed0565a1ada517f5c25 #v1.2.1
+    SHA512 87e3ff15a5033f3a76702e42403490e6fc4417712f166756c1e8a179451a0cb46a9d58555bf71d3cd87905a0befa027b6f26a28c2656e6589382879c4ca28ea5
+    HEAD_REF master
 )
 
-if("external_lib_fmt" IN_LIST FEATURES)
+if("fmt" IN_LIST FEATURES)
     # use vcpkg-provided fmt library (see also option QUILL_FMT_EXTERNAL)
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/quill/quill/include/quill/bundled/fmt)
 	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/quill/quill/src/bundled/fmt)
 endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    external_lib_fmt QUILL_FMT_EXTERNAL
+    fmt QUILL_FMT_EXTERNAL
 )
 
 vcpkg_configure_cmake(
