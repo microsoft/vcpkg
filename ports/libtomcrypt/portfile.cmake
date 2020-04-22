665430b5141f58acd743ac3d9ceaaabc4a570868
Christopher Moore <crmoore@gmail.com>
2020-04-22 14:06:06 -0400

    [libtomcrypt] make build for non-windows

--- ports/libtomcrypt/portfile.cmake
+++ ports/libtomcrypt/portfile.cmake
@@ -6,44 +6,74 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Make sure we start from a clean slate
-vcpkg_execute_build_process(
-    COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc clean
-    WORKING_DIRECTORY ${SOURCE_PATH}
-)
+if(VCPKG_TARGET_IS_WINDOWS)
+    # Make sure we start from a clean slate
+    vcpkg_execute_build_process(
+        COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc clean
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
 
-#Debug Build
-vcpkg_execute_build_process(
-    COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc CFLAGS="/MTd /DUSE_LTM /DLTM_DESC /I${CURRENT_PACKAGES_DIR}/.."
-    WORKING_DIRECTORY ${SOURCE_PATH}
-)
+    #Debug Build
+    vcpkg_execute_build_process(
+        COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc CFLAGS="/MTd /DUSE_LTM /DLTM_DESC /I${CURRENT_PACKAGES_DIR}/.."
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
 
-file(INSTALL
-    ${SOURCE_PATH}/tomcrypt.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/Debug/lib/
-)
+    file(INSTALL
+        ${SOURCE_PATH}/tomcrypt.lib
+        DESTINATION ${CURRENT_PACKAGES_DIR}/Debug/lib/
+    )
 
-# Clean up necessary to rebuild without debug symbols
-vcpkg_execute_build_process(
-    COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc clean
-    WORKING_DIRECTORY ${SOURCE_PATH}
-)
+    # Clean up necessary to rebuild without debug symbols
+    vcpkg_execute_build_process(
+        COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc clean
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
 
-#Release Build
-vcpkg_execute_build_process(
-    COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc CFLAGS="/Ox /DUSE_LTM /DLTM_DESC /I${CURRENT_PACKAGES_DIR}/.."
-    WORKING_DIRECTORY ${SOURCE_PATH}
-)
+    #Release Build
+    vcpkg_execute_build_process(
+        COMMAND nmake -f ${SOURCE_PATH}/makefile.msvc CFLAGS="/Ox /DUSE_LTM /DLTM_DESC /I${CURRENT_PACKAGES_DIR}/.."
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
 
-file(INSTALL
-    ${SOURCE_PATH}/tomcrypt.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib/
-)
+    file(INSTALL
+        ${SOURCE_PATH}/tomcrypt.lib
+        DESTINATION ${CURRENT_PACKAGES_DIR}/lib/
+    )
 
-file(INSTALL
-    ${SOURCE_PATH}/src/headers/
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-)
+    file(INSTALL
+        ${SOURCE_PATH}/src/headers/
+        DESTINATION ${CURRENT_PACKAGES_DIR}/include
+    )
+else()
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+        set(MAKE_FILE "makefile.shared")
+    else()
+        set(MAKE_FILE "makefile")
+    endif()
+
+    vcpkg_execute_build_process(
+        COMMAND make -f ${MAKE_FILE} clean
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+
+    vcpkg_execute_build_process(
+        COMMAND make -j4 -f ${MAKE_FILE} PREFIX=${CURRENT_PACKAGES_DIR}/Debug LTC_DEBUG=1 CFLAGS="-DUSE_LTM -DLTM_DESC " install
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/Debug/include")
+
+    vcpkg_execute_build_process(
+        COMMAND make -f ${MAKE_FILE} clean
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+
+    vcpkg_execute_build_process(
+        COMMAND make -j4 -f ${MAKE_FILE} PREFIX=${CURRENT_PACKAGES_DIR} CFLAGS="-DUSE_LTM -DLTM_DESC " install
+        WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+    
+endif()
 
 #Copy license
 file(
