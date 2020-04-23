3a9ba55ea8dfe5216b6b95d93df130127017e647
Christopher Moore <crmoore@gmail.com>
2020-04-23 09:48:01 -0400

    [libtomcrypt] set HEAD_REF to upstream dev branch

--- ports/libtomcrypt/portfile.cmake
+++ ports/libtomcrypt/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO libtom/libtomcrypt
     REF v1.18.2
     SHA512 53accb4f92077ff1c52102bece270e77c497e599c392aa0bf4dbc173b6789e7e4f1012d8b5f257c438764197cb7bac8ba409a9d4e3a70e69bec5863b9495329e
-    HEAD_REF master
+    HEAD_REF develop
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)
