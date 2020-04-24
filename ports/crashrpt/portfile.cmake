50d9ba739a3b6d639ee122bb9ab5984326962862
Travis Drake <tdrake@qsrautomations.com>
2020-04-24 12:27:55 -0400

    Add vcpkg_copy_tool_dependencies so that dlls for CrashSender .exe will be copied to tools folder

--- ports/crashrpt/portfile.cmake
+++ ports/crashrpt/portfile.cmake
@@ -41,6 +41,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
