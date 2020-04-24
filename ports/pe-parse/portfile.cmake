6c423d0974944ba645785b99fa7238a7184f6681
William Woodruff <william@trailofbits.com>
2020-04-24 13:54:17 -0400

    pe-parse/portfile: Formatting

--- ports/pe-parse/portfile.cmake
+++ ports/pe-parse/portfile.cmake
@@ -9,8 +9,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
-        -DBUILD_COMMAND_LINE_TOOLS=OFF
+    OPTIONS -DBUILD_COMMAND_LINE_TOOLS=OFF
 )
 
 vcpkg_install_cmake()
