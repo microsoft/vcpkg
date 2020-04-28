3eee48add0a610ecf202443f99f17456701f63f1
L <zover.v@gmail.com>
2020-04-28 16:05:25 +0800

    [gainput] update portfile

--- ports/gainput/portfile.cmake
+++ ports/gainput/portfile.cmake
@@ -10,10 +10,9 @@ vcpkg_from_github(
         "install_as_cmake_package.patch"
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(GAINPUT_BUILD_SHARED ON)
-    set(GAINPUT_BUILD_STATIC OFF)
-elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+set(GAINPUT_BUILD_SHARED ON)
+set(GAINPUT_BUILD_STATIC OFF)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     set(GAINPUT_BUILD_SHARED OFF)
     set(GAINPUT_BUILD_STATIC ON)
 endif()
