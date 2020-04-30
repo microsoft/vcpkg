856f6774bb51ab0d4ac41c5f611879b63ce108d9
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-04-30 17:56:34 +0200

    add pkgconfig to openblas

--- ports/openblas/portfile.cmake
+++ ports/openblas/portfile.cmake
@@ -84,6 +84,11 @@ endif()
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/OpenBLAS TARGET_PATH share/openblas)
+vcpkg_fixup_pkgconfig()
+file(COPY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/openblas.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/blas.pc")
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/")
+    file(COPY "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/openblas.pc" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/blas.pc")
+endif()
 #maybe we need also to write a wrapper inside share/blas to search implicitly for openblas, whenever we feel it's ready for its own -config.cmake file
 
 # openblas do not make the config file , so I manually made this
