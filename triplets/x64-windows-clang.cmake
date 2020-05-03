8e7f6b873f339906b502d109fb38c41dc1164c42
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-03 16:55:03 +0800

    Update x64-windows-clang.cmake

--- triplets/x64-windows-clang.cmake
+++ triplets/x64-windows-clang.cmake
@@ -6,7 +6,7 @@ set(VCPKG_PLATFORM_TOOLSET v142)
 
 set(VS_PLATFORM_TOOLSET "ClangCL,host=x64")
 #or
-##set(VS_PLATFORM_TOOLSET "Intel C++ Compiler 19.1")#ClangCL #LLVM_v142,host=x64 #"v142,cuda=10.1,host=x64" #"v142,host=x64,v=14.16#version=14.11
+##set(VS_PLATFORM_TOOLSET "LLVM_v142,host=x64" #"v142,cuda=10.1,host=x64" #"v142,host=x64,v=14.16#version=14.11
 
 if(DEFINED VS_PLATFORM_TOOLSET)
     set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
