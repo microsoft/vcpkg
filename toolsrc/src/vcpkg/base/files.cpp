d3e83b0964db500eb8eda5c0e92590a17d652ae4
Nicole Mazzuca <nicole@strega-nil.co>
2020-05-04 13:53:30 -0700

    flip the conditional for billy

--- toolsrc/src/vcpkg/base/files.cpp
+++ toolsrc/src/vcpkg/base/files.cpp
@@ -724,12 +724,12 @@ namespace vcpkg::Files
             // absolute was called system_complete in experimental filesystem
             return fs::stdfs::system_complete(path, ec);
 #else // ^^^ _WIN32 / !_WIN32 vvv
-            if (!path.is_absolute()) {
+            if (path.is_absolute()) {
+                return path;
+            } else {
                 auto current_path = this->current_path(ec);
                 if (ec) return fs::path();
                 return std::move(current_path) / path;
-            } else {
-                return path;
             }
 #endif
 #endif
