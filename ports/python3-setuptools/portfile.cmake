b65a86a23c8a9db7532b9502b9bf6f1c56f4bc53
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-08 09:31:32 +0800

    Update portfile.cmake

--- ports/python3-setuptools/portfile.cmake
+++ ports/python3-setuptools/portfile.cmake
@@ -24,12 +24,23 @@ set(PYTHON_EXECUTABLE ${PYTHON3})
 # install default pip
 message(STATUS "Installing built-in pip wheel..")
 #vcpkg_execute_required_process(
-#    COMMAND ${PYTHON_DEBUG_EXECUTABLE} -m ensurepip --default-pip
+#    COMMAND ${PYTHON_DEBUG_EXECUTABLE} -m ensurepip --upgrade --default-pip
 #    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
 #    LOGNAME install-pip-${TARGET_TRIPLET}-dbg
 #)
+
+if(NOT EXISTS ${PYTHON3_DIR}/Scripts/pip)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH PYFILE_PATH
+        REPO pypa/get-pip
+        REF 1fe530e9e3d800be94e04f6428460fc4fb94f5a9
+        SHA512 e1b03b89418572c1e52e6aa9740333877af7145b080d70d1ffe63bd94871f61ea057457ae589fa55d172fd6e0511e0de021b02c693abd17289310e36e6d8a618
+        HEAD_REF master
+    )
+endif()
+
 vcpkg_execute_required_process(
-    COMMAND ${PYTHON_EXECUTABLE} -m ensurepip --default-pip
+    COMMAND ${PYTHON_EXECUTABLE} ${PYFILE_PATH}/get-pip.py
     WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
     LOGNAME install-pip-${TARGET_TRIPLET}-rel
 )
