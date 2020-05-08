7d01ac8b9a52724e8371c4312cf287548db9261a
William Woodruff <william@trailofbits.com>
2020-05-07 23:32:03 -0400

    ports/uthenticode: Use ${PORT}

--- ports/uthenticode/portfile.cmake
+++ ports/uthenticode/portfile.cmake
@@ -23,6 +23,6 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(
     INSTALL
     ${SOURCE_PATH}/LICENSE
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/uthenticode
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
     RENAME copyright
 )
