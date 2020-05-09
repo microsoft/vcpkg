352911d864ba1bed41f6c48306e483e833330e16
NancyLi1013 <lirui09@beyondsoft.com>
2020-05-09 01:22:32 -0700

    [libgo] Update to 3.1

--- ports/libgo/portfile.cmake
+++ ports/libgo/portfile.cmake
@@ -1,16 +1,9 @@
-include(vcpkg_common_functions)
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO yyzybb537/libgo
-    REF v2.8
-    SHA512 44784de4aec36ea321195c11c99a73de4f6f51285febdf6980e8aaced1fdfc0a34c6b1a8acc8c6b424e747310a1d7fb1604f722084c28ab91f8ebee15667d59b
+    REF 5d4f36508e8eb2d5aa17cf37cd951dc91da23096 #v3.1
+    SHA512 0f281f58116148ba1dd3904febbc391d47190f8e148b70bed7c4b7e6cb3efa5e41e2b7be4832ceeb805996e085f4c2d89fd0cf3b0651e037b32758d6a441411b
     HEAD_REF master
-    PATCHES
-        cmake.patch
-        boost-168.patch
 )
 
 vcpkg_from_github(
@@ -28,36 +21,45 @@ file(RENAME ${XHOOK_SOURCE_PATH} ${SOURCE_PATH}/third_party/xhook)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS
-        -DDISABLE_ADJUST_COMMAND_LINE_FLAGS=ON
-        -DDISABLE_DYNAMIC_LIB=ON
-        -DENABLE_BOOST_CONTEXT=ON
-        -DFORCE_UNIX_TARGETS=ON
-        -DDISABLE_SYSTEMWIDE=ON
 )
 
-vcpkg_install_cmake()
-
-# remove duplicated include files
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/libgo/disable_hook)
-
-file(GLOB REL_MAIN ${CURRENT_PACKAGES_DIR}/lib/libgo_main.lib ${CURRENT_PACKAGES_DIR}/lib/liblibgo_main.a)
-if(REL_MAIN)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib/manual-link)
-    file(COPY ${REL_MAIN} DESTINATION ${CURRENT_PACKAGES_DIR}/lib/manual-link)
-    file(REMOVE ${REL_MAIN})
-endif()
-
-file(GLOB DBG_MAIN ${CURRENT_PACKAGES_DIR}/debug/lib/libgo_main.lib ${CURRENT_PACKAGES_DIR}/debug/lib/liblibgo_main.a)
-if(DBG_MAIN)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link)
-    file(COPY ${DBG_MAIN} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link)
-    file(REMOVE ${DBG_MAIN})
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_build_cmake()
+    file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/libgo.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/libgo.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    # Install headers
+    file(GLOB HEADERS ${SOURCE_PATH}/libgo/*.h)   
+    file(INSTALL ${HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo)
+    file(INSTALL ${SOURCE_PATH}/libgo/cls/co_local_storage.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/cls)
+    file(GLOB COMMOM_HEADERS ${SOURCE_PATH}/libgo/common/*.h)
+    file(INSTALL ${COMMOM_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/common)
+    file(INSTALL ${SOURCE_PATH}/libgo/context/fiber/context.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/context/fiber)
+    file(GLOB CONTXET_HEADERS ${SOURCE_PATH}/libgo/context/*.h)
+    file(INSTALL ${CONTXET_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/context)
+    file(INSTALL ${SOURCE_PATH}/libgo/defer/defer.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/defer)
+    file(GLOB WINDOWS_HEADERS ${SOURCE_PATH}/libgo/netio/windows/*.h)
+    file(INSTALL ${WINDOWS_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/netio/windows)
+    file(INSTALL ${SOURCE_PATH}/libgo/netio/windows/arpa/inet.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/netio/windows/arpa)
+    file(GLOB WINDOWS_SYS ${SOURCE_PATH}/libgo/netio/windows/sys/*.h)
+    file(INSTALL ${WINDOWS_SYS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/netio/windows/sys)
+    file(INSTALL ${SOURCE_PATH}/libgo/netio/windows/xhook/xhook.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/netio/windows/xhook/)
+    file(GLOB POOL_HEADERS ${SOURCE_PATH}/libgo/pool/*.h)
+    file(INSTALL ${POOL_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/pool)
+    file(GLOB SCHEDULER_HEADERS ${SOURCE_PATH}/libgo/scheduler/*.h)
+    file(INSTALL ${SCHEDULER_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/scheduler) 
+    file(GLOB SYNC_HEADERS ${SOURCE_PATH}/libgo/sync/*.h)
+    file(INSTALL ${SYNC_HEADERS} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/sync)
+    file(INSTALL ${SOURCE_PATH}/libgo/task/task.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/task)
+    file(INSTALL ${SOURCE_PATH}/libgo/timer/timer.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/libgo/timer)
+else()
+    vcpkg_install_cmake()
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/libgo/netio/disable_hook)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/libgo/netio/unix/static_hook)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/libgo/netio/unix/static_hook)
 endif()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libgo RENAME copyright)
-file(INSTALL ${CURRENT_PORT_DIR}/libgo-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libgo)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${CURRENT_PORT_DIR}/libgo-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
