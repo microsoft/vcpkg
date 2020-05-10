a07e7f153762883be2554fd2ba52433e78ff5a0b
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-10 13:42:32 +0800

    Update portfile.cmake

--- ports/python3-cython/portfile.cmake
+++ ports/python3-cython/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cython/cython
-    REF 0.28.1
+    REF 0.29.17
     SHA512 3346ebe01049ff6628f74ee1904d440680ccc7fc09c51afd26d6e05264318678c9fb64da4d98703d3e687662e98125e0b182d01cb9276cbb4fcb014ecb35be63 
     HEAD_REF master
 )
