dca5990abd5cfddfc951f788746574d4c6eac3b0
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-10 21:24:18 +0800

    Update portfile.cmake

--- ports/python3-numexpr/portfile.cmake
+++ ports/python3-numexpr/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/pydata/numexpr/archive/d274455bc6a092cb49c0606f61a67c61afb86f72.zip"
     FILENAME "numexpr.zip"
-    SHA512 2e248bbed52ebaa2b12aa35dc45dcaed716453599882fa51d95574a5dda8af92b3f3a6dcd4c059f762978e619c423291ac1ea24c82ad9e23dfc7322c28767ba0
+    SHA512 79def09bf2c64569be479e84a0556b4fa2cd6afedc1ba6f0963fe48aa3e6c208c6aadb2d03c1e816f0123ac13a9ee1e55256880330d00fed7003faaa9ac9dcf6
 )
 
 vcpkg_extract_source_archive_ex(OUT_SOURCE_PATH SOURCE_PATH ARCHIVE ${ARCHIVE})
