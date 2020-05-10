168b7ac8488fe328f5ce1110114efb97274495a9
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-10 11:51:42 +0800

    Update vcpkg_build_python.cmake

--- scripts/cmake/vcpkg_build_python.cmake
+++ scripts/cmake/vcpkg_build_python.cmake
@@ -5,23 +5,14 @@ function(vcpkg_build_python)
     vcpkg_find_acquire_program(PYTHON3)
     get_filename_component(PYTHON_PREFIX ${PYTHON3} DIRECTORY)
     vcpkg_add_to_path(PREPEND ${PYTHON_PREFIX})
-    find_program(Python3_EXECUTABLE NAMES python python3 python3.7 PATHS ${PYTHON_PREFIX} NO_DEFAULT_PATHS)
-#    set(Python3_ROOT_DIR ${PYTHON_PREFIX})
+
       set(PYTHONPATH "${PYTHON_PREFIX}/DLLs;${PYTHON_PREFIX}/Lib;${PYTHON_PREFIX}/Lib/site-packages")#;${PYTHON_PREFIX}/Lib/lib-tk
       set(PYTHONSCRIPT "${PYTHON_PREFIX}/Scripts")
       set(PYTHON_PACKAGES_PATH "${PYTHON_PREFIX}/Lib/site-packages")
-      set(PYTHON3_PACKAGES_PATH ${PYTHON_PACKAGES_PATH})
-      set(PYTHON3_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
-      set(PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
+	  set(PYTHONHOME "${PYTHON_PREFIX}")
       set(PYTHON_INCLUDE_DIR "${CURRENT_INSTALLED_DIR}/include/python3.7")
-      set(PYTHON3_LIBRARY ${PYTHON_LIBRARY})
-      set(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
       set(PYTHON_LIBRARY "${CURRENT_INSTALLED_DIR}/lib/python37.lib;${CURRENT_INSTALLED_DIR}/lib")#;${PYTHON_PREFIX}/libs
 
-      find_package_handle_standard_args(Python3 DEFAULT_MSG Python3_EXECUTABLE)
-      find_package_handle_standard_args(Python3Interp DEFAULT_MSG Python3_EXECUTABLE)
-    mark_as_advanced(Python3_EXECUTABLE)
-    
     if(NOT DEFINED _ppi_SOURCE_PATH)
         message(FATAL_ERROR "SOURCE_PATH is a required argument to vcpkg_install_python.")
     endif()
@@ -34,11 +25,9 @@ function(vcpkg_build_python)
     configure_file(${SCRIPTS}/templates/distutils-rel.cfg.in ${PYTHON_PREFIX}/Lib/distutils/distutils.cfg)
     file(REMOVE ${CURRENT_BUILDTREES_DIR}/pip-install-${TARGET_TRIPLET}-rel-detailed.log)
     vcpkg_execute_required_process(
-        COMMAND ${Python3_EXECUTABLE} -m pip install .
+        COMMAND ${PYTHON3} -m pip install .
             --prefix ${PYTHON_PREFIX}
             --ignore-installed --compile --no-deps
-#            compiler=msvc | compiler=bcpp | compiler=cygwin | compiler=mingw32
-###            regulated in the file, #system 	prefix\Lib\distutils\distutils.cfg, #personal 	%HOME%\pydistutils.cfg, #local 	setup.cfg
             --log ${CURRENT_BUILDTREES_DIR}/pip-install-${TARGET_TRIPLET}-rel-detailed.log
         WORKING_DIRECTORY ${_ppi_SOURCE_PATH}
         LOGNAME pip-install-${TARGET_TRIPLET}-rel
