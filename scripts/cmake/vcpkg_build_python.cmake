ffb3974b58fc4534e07e1c9d62f2a9799d7a14e5
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-10 11:12:43 +0800

    Update vcpkg_build_python.cmake

--- scripts/cmake/vcpkg_build_python.cmake
+++ scripts/cmake/vcpkg_build_python.cmake
@@ -1,29 +1,23 @@
 include(FindPackageHandleStandardArgs)
 function(vcpkg_build_python)
     cmake_parse_arguments(_ppi "" "SOURCE_PATH" "" ${ARGN})
-
-    vcpkg_find_acquire_program(PYTHON3)
-    get_filename_component(PYTHON_PREFIX ${PYTHON3} PATH)
-#    set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
+    
+    find_program(Python3_EXECUTABLE NAMES python python3 python3.7 NAMES_PER_DIR HINTS ${PYTHON_PREFIX} NO_DEFAULT_PATHS)
 #    set(Python3_ROOT_DIR ${PYTHON_PREFIX})
-    set(PYTHONPATH "${PYTHON_PREFIX}/Lib;${PYTHON_PREFIX}/Lib/site-packages")#;${PYTHON_PREFIX}/DLLs;${PYTHON_PREFIX}/Lib/lib-tk
-    set(PYTHONSCRIPT "${PYTHON_PREFIX}/Scripts")
-    set(PYTHON_PACKAGES_PATH "${PYTHON_PREFIX}/Lib/site-packages")
-    set(PYTHON3_PACKAGES_PATH ${PYTHON_PACKAGES_PATH})
-    set(PYTHON3_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
-    set(PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
-    set(PYTHON_INCLUDE_DIR "${CURRENT_INSTALLED_DIR}/include/python3.7")
-    set(PYTHON3_LIBRARY ${PYTHON_LIBRARY})
-    set(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
-    set(PYTHON_LIBRARY "${CURRENT_INSTALLED_DIR}/lib/python37.lib;${CURRENT_INSTALLED_DIR}/lib")#;${PYTHON_PREFIX}/libs
+      set(PYTHONPATH "${PYTHON_PREFIX}/DLLs;${PYTHON_PREFIX}/Lib;${PYTHON_PREFIX}/Lib/site-packages")#;${PYTHON_PREFIX}/Lib/lib-tk
+      set(PYTHONSCRIPT "${PYTHON_PREFIX}/Scripts")
+      set(PYTHON_PACKAGES_PATH "${PYTHON_PREFIX}/Lib/site-packages")
+      set(PYTHON3_PACKAGES_PATH ${PYTHON_PACKAGES_PATH})
+      set(PYTHON3_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
+      set(PYTHON_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
+      set(PYTHON_INCLUDE_DIR "${CURRENT_INSTALLED_DIR}/include/python3.7")
+      set(PYTHON3_LIBRARY ${PYTHON_LIBRARY})
+      set(PYTHON_LIBRARIES ${PYTHON_LIBRARY})
+      set(PYTHON_LIBRARY "${CURRENT_INSTALLED_DIR}/lib/python37.lib;${CURRENT_INSTALLED_DIR}/lib")#;${PYTHON_PREFIX}/libs
 
-    find_program(Python3_EXECUTABLE NAMES python python3 python3.7 PATHS ${PYTHON_PREFIX} NO_DEFAULT_PATHS)
       find_package_handle_standard_args(Python3 DEFAULT_MSG Python3_EXECUTABLE)
       find_package_handle_standard_args(Python3Interp DEFAULT_MSG Python3_EXECUTABLE)
     mark_as_advanced(Python3_EXECUTABLE)
-    
-    vcpkg_add_to_path(PREPEND ${PYTHON_PREFIX})
-    vcpkg_add_to_path(PREPEND "${PYTHON_PREFIX}/DLLs")
     
     if(NOT DEFINED _ppi_SOURCE_PATH)
         message(FATAL_ERROR "SOURCE_PATH is a required argument to vcpkg_install_python.")
