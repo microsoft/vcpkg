360bf554b1733613aa4a9d9f1c323967d6f32b01
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-10 22:57:28 +0800

    Update portfile.cmake

--- ports/python3-mako/portfile.cmake
+++ ports/python3-mako/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://files.pythonhosted.org/packages/0a/af/a6d8aa7b8909a36074f517b15222e3a2fbd5ef3452c0a686e3d43043dd3b/Mako-1.0.12.tar.gz"
-    FILENAME "Mako-1.0.12.tar.gz"
-    SHA512 aa295880c07938425e661b8865fed7cf398ac257d6b84bdbef5a2c542bbae79cd27df363e81c10450c9b6c8e038360166480b06e701d5a5affeb7bd03a7f5d83
+    URLS "https://files.pythonhosted.org/packages/42/64/fc7c506d14d8b6ed363e7798ffec2dfe4ba21e14dda4cfab99f4430cba3a/Mako-1.1.2.tar.gz"
+    FILENAME "Mako-1.1.2.tar.gz"
+    SHA512 73b7a32ceb6e39dc2491734546c83a752bf0c8bc7546bb236801c02a5bccc5b59f2440333a328ff747eee936612dd12279f08d27d76f065fd589081d232692a8
 )
 
 vcpkg_extract_source_archive_ex(OUT_SOURCE_PATH SOURCE_PATH ARCHIVE ${ARCHIVE})
