984ed3451a37178de877da0ae6af7d7048cc81d5
Angel Sánchez <angelsanchez@inedit.com>
2020-05-11 13:53:03 +0200

    glib building in macOS using meson

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -17,16 +17,7 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     REF ${GLIB_VERSION}
     PATCHES
-        #use-libiconv-on-windows.patch
-        #arm64-defines.patch
-        #fix-arm-builds.patch
 )
-
-#file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-#file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
-#file(REMOVE_RECURSE ${SOURCE_PATH}/glib/pcre)
-#file(WRITE ${SOURCE_PATH}/glib/pcre/Makefile.in)
-#file(REMOVE ${SOURCE_PATH}/glib/win_iconv.c)
 
 if (selinux IN_LIST FEATURES AND NOT VCPKG_TARGET_IS_WINDOWS AND NOT EXISTS "/usr/include/selinux")
     message("Selinux was not found in its typical system location. Your build may fail. You can install Selinux with \"apt-get install selinux\".")
@@ -38,7 +29,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_configure_meson(
     SOURCE_PATH ${SOURCE_PATH}
-    #PREFER_NINJA
+    PREFER_NINJA
     OPTIONS ${FEATURE_OPTIONS}
         -DGLIB_VERSION=${GLIB_VERSION}
     OPTIONS_DEBUG
@@ -47,11 +38,14 @@ vcpkg_configure_meson(
 )
 
 vcpkg_install_meson()
-#vcpkg_fixup_meson_targets(CONFIG_PATH share/unofficial-glib TARGET_PATH share/unofficial-glib)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 vcpkg_copy_pdbs()
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
