1c06ba4e55f22cffb4cf53b00891c59f724348a0
Mohammed Alyousef <may642_2000@hotmail.com>
2020-05-12 19:22:22 +0300

    add x86-wasm.cmake to community triplets
    The EMSDK enviroment variable is normally defined when first installing Emscripten.

---
+++ triplets/community/x86-wasm.cmake
@@ -0,0 +1,9 @@
+ if(NOT DEFINED ENV{EMSDK})
+   message(FATAL_ERROR "The EMSDK environment variable must be defined")
+ endif()
+
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE $ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
