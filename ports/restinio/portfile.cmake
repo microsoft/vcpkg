17ab5a15f82de8872ae63f9ed6a8896a685100e9
Yauheni Akhotnikau <eao197@gmail.com>
2020-05-12 12:35:25 +0300

    RESTinio updated to v.0.6.7.1.

--- ports/restinio/portfile.cmake
+++ ports/restinio/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stiffstream/restinio
-    REF d170b6af5aacfc64d0ed25b4e25c5057d5dd2130 # v.0.6.6
-    SHA512 017aeadd8b4171d7e7e11d631eb5b57d75272456948808e7dc8b988cba3cfbbd18cba5a95deccb7440bc4918f6daf17904358179f45a5158cf1b9ca687afc0c8
+    REF 00a6e8782ee0a818a52ce00f6ace4efc90dbc892 # v.0.6.7.1
+    SHA512 93185dd590dd785141f47064572bcc53d32cc063fb08f9dce1e03ef5228561dba3a3ce29b45752b74eb2f299d1219ad022aa735344e8ed9fe14ffcaa6f82c6df
 )
 
 vcpkg_configure_cmake(
