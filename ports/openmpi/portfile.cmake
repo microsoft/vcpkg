4cab392ec21b36690bcbc9005fb8469be40a82ef
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-05-12 19:54:50 +0200

    [openmpi] remove unnecessary build artifacts

--- ports/openmpi/portfile.cmake
+++ ports/openmpi/portfile.cmake
@@ -35,4 +35,7 @@ vcpkg_configure_make(
 
 vcpkg_install_make()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
