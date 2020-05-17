f8262fe8a06e040ff6186ed576af8fa3284200da
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-18 06:08:16 +0800

    Enable Developer Mode
    https://github.com/reupen/azure-pipelines-image-generation/blob/master/images/win/scripts/Installers/Enable-DeveloperMode.ps1

--- scripts/azure-pipelines/windows/initialize-environment.ps1
+++ scripts/azure-pipelines/windows/initialize-environment.ps1
@@ -86,3 +86,12 @@ Remove-Item buildtrees\* -Recurse -Force -errorAction silentlycontinue
 
 Write-Host 'Cleaning packages'
 Remove-Item packages\* -Recurse -Force -errorAction silentlycontinue
+
+# Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
+if (-not(Test-Path -Path $RegistryKeyPath)) {
+    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
+}
+
+# Add registry value to enable Developer Mode
+New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
