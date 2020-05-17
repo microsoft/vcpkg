257b6780ade17d83044913bd1dd8b1c813841271
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-18 06:52:44 +0800

    Update initialize-environment.ps1

--- scripts/azure-pipelines/windows/initialize-environment.ps1
+++ scripts/azure-pipelines/windows/initialize-environment.ps1
@@ -89,9 +89,6 @@ Remove-Item packages\* -Recurse -Force -errorAction silentlycontinue
 
 # Create AppModelUnlock if it doesn't exist, required for enabling Developer Mode
 $RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock"
-if (-not(Test-Path -Path $RegistryKeyPath)) {
-    New-Item -Path $RegistryKeyPath -ItemType Directory -Force
-}
 
 # Add registry value to enable Developer Mode
 New-ItemProperty -Path $RegistryKeyPath -Name AllowDevelopmentWithoutDevLicense -PropertyType DWORD -Value 1
