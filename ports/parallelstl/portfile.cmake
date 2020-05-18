e3945e14137148507b39aa9fb54bc8337df4e50d
nicole mazzuca <mazzucan@outlook.com>
2020-05-18 11:08:07 -0700

    code review

--- ports/parallelstl/portfile.cmake
+++ ports/parallelstl/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO intel/parallelstl #20200330
-    REF  0241743d73bb405b16d9c4a24b693e4533dc34a7
+    REPO intel/parallelstl
+    REF  0241743d73bb405b16d9c4a24b693e4533dc34a7 # 20200330
     SHA512 cd2f1b60639e9da35a722bdef0bc6420ddca064e3bff979d8a6ea591fb43865b7614c811bced642fd5ff1fab659da1dfaa248dc2b321db7f27d0e74544a2f21e
     HEAD_REF master
     PATCHES 
