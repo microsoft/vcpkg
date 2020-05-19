69a24b712b69d812ea6413003a989de4d92d95a3
JackBoosY <yuzaiyang@beyondsoft.com>
2020-05-18 22:48:31 -0700

    [dbghelp] Improve portfile.cmake

--- ports/dbghelp/portfile.cmake
+++ ports/dbghelp/portfile.cmake
@@ -1,27 +1,21 @@
 vcpkg_fail_port_install(ON_TARGET "OSX" "Linux")
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-vcpkg_get_program_files_32_bit(PROGRAM_FILES_32_BIT)
 vcpkg_get_windows_sdk(WINDOWS_SDK)
 
 if (WINDOWS_SDK MATCHES "10.")
-    set(LIBFILEPATH "${PROGRAM_FILES_32_BIT}\\Windows Kits\\10\\Debuggers\\lib\\${TRIPLET_SYSTEM_ARCH}\\dbghelp.lib")
-    set(DLLFILEPATH "${PROGRAM_FILES_32_BIT}\\Windows Kits\\10\\Debuggers\\${TRIPLET_SYSTEM_ARCH}\\dbghelp.dll")
-    set(HEADERPATH "${PROGRAM_FILES_32_BIT}\\Windows Kits\\10\\Debuggers\\inc\\dbghelp.h")
+    set(LIBFILEPATH "$ENV{WindowsSdkDir}Debuggers\\lib\\${TRIPLET_SYSTEM_ARCH}\\dbghelp.lib")
+    message("LIBFILEPATH: ${LIBFILEPATH}")
+    set(DLLFILEPATH "$ENV{WindowsSdkDir}Debuggers\\${TRIPLET_SYSTEM_ARCH}\\dbghelp.dll")
+    message("DLLFILEPATH: ${DLLFILEPATH}")
+    set(HEADERPATH "$ENV{WindowsSdkDir}Debuggers\\inc\\dbghelp.h")
+    message("HEADERPATH: ${HEADERPATH}")
 else()
     message(FATAL_ERROR "Portfile not yet configured for Windows SDK with version: ${WINDOWS_SDK}")
 endif()
 
-if (NOT EXISTS "${LIBFILEPATH}")
-    message(FATAL_ERROR "Cannot find Windows ${WINDOWS_SDK} SDK. File does not exist: ${LIBFILEPATH}")
-endif()
-
-if (NOT EXISTS "${DLLFILEPATH}")
-    message(FATAL_ERROR "Cannot find Windows ${WINDOWS_SDK} SDK. File does not exist: ${DLLFILEPATH}")
-endif()
-
-if (NOT EXISTS "${HEADERPATH}")
-    message(FATAL_ERROR "Cannot find Windows ${WINDOWS_SDK} SDK. File does not exist: ${HEADERPATH}")
+if (NOT EXISTS "${LIBFILEPATH}" OR NOT EXISTS "${DLLFILEPATH}" OR NOT EXISTS "${HEADERPATH}")
+    message(FATAL_ERROR "Cannot find debugging tools in Windows SDK ${WINDOWS_SDK}. Please reinstall the Windows SDK and select \"Debugging Tool\".")
 endif()
 
 file(INSTALL ${LIBFILEPATH} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
