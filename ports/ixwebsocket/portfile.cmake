6a388f363ba8092b8b32497e4d86e83c028cdcda
Benjamin Sergeant <bsergeant@mz.com>
2020-05-18 20:33:08 -0700

    portfile will handles openssl, mbedtls and sectransp options to choose the tls backend library

--- ports/ixwebsocket/portfile.cmake
+++ ports/ixwebsocket/portfile.cmake
@@ -5,10 +5,23 @@ vcpkg_from_github(
     SHA512 22a97f0c74d5789fd265627bbadfce02116a41fca888d684b756cbb1c070d626517855e265c484870d02db7084b6c1107d500acc1c13e327ede0c23228a54f7b
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    openssl   USE_OPEN_SSL
+    mbedtls   USE_MBED_TLS
+    sectransp USE_SECURE_TRANSPORT
+)
+
+if("sectransp" IN_LIST FEATURES AND NOT VCPKG_TARGET_IS_OSX)
+    message(FATAL_ERROR "sectransp is not supported on non-Apple platforms")
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DUSE_TLS=1
+    PREFER_NINJA
+    OPTIONS
+	${FEATURE_OPTIONS}
+	-DUSE_TLS=1
 )
 
 vcpkg_install_cmake()
