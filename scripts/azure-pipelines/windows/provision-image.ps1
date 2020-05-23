d38d5cf1e685cf6a15422bc987b255917062f5fe
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-23 14:34:36 +0800

    Update provision-image.ps1

--- scripts/azure-pipelines/windows/provision-image.ps1
+++ scripts/azure-pipelines/windows/provision-image.ps1
@@ -340,9 +340,9 @@ Function InstallWindowsWDK {
     Write-Host 'Installing Windows WDK...'
     $proc = Start-Process -FilePath $installerPath -ArgumentList @('/features', '+', '/q') -Wait -PassThru
     $exitCode = $proc.ExitCode
-    $vsPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Enterprise"
-    $proc = Start-Process "$vsPath\Common7\IDE\VSIXInstaller.exe" "/q /a `"${env:ProgramFiles(x86)}\Windows Kits\10\Vsix\VS2019\WDK.vsix`"" -Wait
-    $exitCode = $proc.ExitCode
+#    $vsPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Enterprise"
+#    $proc = Start-Process "$vsPath\Common7\IDE\VSIXInstaller.exe" "/q /a `"${env:ProgramFiles(x86)}\Windows Kits\10\Vsix\VS2019\WDK.vsix`"" -Wait
+#    $exitCode = $proc.ExitCode
     if ($exitCode -eq 0) {
       Write-Host 'Installation successful!'
     }
