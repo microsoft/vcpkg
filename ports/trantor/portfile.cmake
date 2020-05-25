9e8dd7d16455469f234ca5db9ad54b131479060d
An Tao <antao2002@gmail.com>
2020-05-25 17:09:48 +0800

    Apply suggestions from code review
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/trantor/portfile.cmake
+++ ports/trantor/portfile.cmake
@@ -15,8 +15,6 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    # OPTIONS 
-    #     -DBUILD_TRANTOR_SHARED=${TRANTOR_SHARED}
 )
 
 vcpkg_install_cmake()
@@ -28,8 +26,7 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Trantor)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 # Handle copyright
-file(COPY ${SOURCE_PATH}/License DESTINATION ${CURRENT_PACKAGES_DIR}/share/trantor)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/trantor/License ${CURRENT_PACKAGES_DIR}/share/trantor/copyright)
+file(INSTALL ${SOURCE_PATH}/License DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 # Copy pdb files
 vcpkg_copy_pdbs()
