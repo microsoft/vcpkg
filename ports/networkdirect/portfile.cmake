40ee43393e71a662ded1a34131e3b2e1743e0cc9
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-26 11:26:42 +0800

    Update portfile.cmake

--- ports/networkdirect/portfile.cmake
+++ ports/networkdirect/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+#vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 find_program(GIT NAMES git git.cmd)
 set(GIT_URL "https://github.com/microsoft/NetworkDirect")
@@ -24,32 +24,29 @@ vcpkg_apply_patches(
     PATCHES patch_v2.patch
 )
 
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/global.json DESTINATION ${CURRENT_BUILDTREES_DIR})
+
 # MSBuild - TARGET Restore #not work /t:Restore or /Restore and /t:Build
 vcpkg_find_acquire_program(NUGET)
-set(ENV{RestoreUseSkipNonexistentTargets} "false")
-# set(ENV{NUGET_RESTORE_MSBUILD_ARGS} /p:TreatWarningsAsErrors="true")
-set(ENV{NUGET_RESTORE_MSBUILD_VERBOSITY "diag")
-set(ENV{NUGET_SHOW_STACK} "true")
-execute_process(
+vcpkg_execute_required_process(
     COMMAND ${NUGET} restore ${SOURCE_PATH}/dirs.proj -Force -NonInteractive -Verbosity detailed
-#    WORKING_DIRECTORY ${SOURCE_PATH}
-#    LOGNAME nuget_error_output
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME nuget_error_output
 )
 
 vcpkg_install_msbuild(
     SOURCE_PATH ${SOURCE_PATH}
     PROJECT_SUBPATH dirs.proj
-    SKIP_CLEAN
+#    SKIP_CLEAN
     OPTIONS
         "/p:PlatformToolset=WindowsUserModeDriver10.0"
-        "/p:BasePlatformToolset=v142"
+        "/p:BasePlatformToolset=${VCPKG_PLATFORM_TOOLSET}"
         "/p:VCToolsVersion=$ENV{VCToolsVersion}"
-        "/P:RestoreUseSkipNonexistentTargets=false"
     LICENSE_SUBPATH LICENSE.txt
 )
 
-file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}/out/Release-${PLATFORM}/include" DESTINATION ${CURRENT_PACKAGES_DIR})
-file(REMOVE ${CURRENT_INSTALLED_DIR}/tools/${PORT}/nuget.exe)
+file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}/out/Release-${TRIPLET_SYSTEM_ARCH}/include" DESTINATION ${CURRENT_PACKAGES_DIR})
+file(REMOVE_RECURSE ${CURRENT_INSTALLED_DIR}/tools/${PORT}/nuget.exe)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 
 vcpkg_copy_pdbs() # automatic templates
