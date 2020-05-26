5650dc57f9f96abafff7c90c43eb6782aabb7412
Ankush Singh <ankush.1996@gmail.com>
2020-05-27 02:54:50 +0530

    [json-schema-validator] Fix string ambiguous assignment error

--- ports/json-schema-validator/portfile.cmake
+++ ports/json-schema-validator/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 27fc1d094503623dfe39365ba82581507524545c
     SHA512 4fd05087743f43871586a53d119acd1a19d0bdec8a5620f62b6eee7a926d285842e8439127eec52eeb11069c92b8d9af28558897d48e2422ecafca39d9f23cdb
     HEAD_REF master
+    PATCHES
+        "${CMAKE_CURRENT_LIST_DIR}/fix-ambiguous-assignment.patch"
 )
 
 vcpkg_configure_cmake(
