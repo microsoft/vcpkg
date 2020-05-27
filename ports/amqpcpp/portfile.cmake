02fff681a21f89f14fff62111763f71ea69a4537
JonLiu1993 <13720414433@163.com>
2020-05-27 19:34:56 +0800

    [amqpcpp] Update to 4.1.7

--- ports/amqpcpp/portfile.cmake
+++ ports/amqpcpp/portfile.cmake
@@ -29,6 +29,6 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/amqpcpp RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
