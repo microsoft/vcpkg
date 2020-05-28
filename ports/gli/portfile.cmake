87254f761c197146084ce5688562a0bf9e55b7de
NancyLi1013 <lirui09@beyondsoft.com>
2020-05-28 03:40:18 -0700

    Disable test

--- ports/gli/portfile.cmake
+++ ports/gli/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF dd17acf9cc7fc6e6abe9f9ec69949eeeee1ccd82
     SHA512 9e3a4ab9ee73d5c271b8346cf81339cd3cd0c20d20991524b816313b6a99e8d3a01863316a38cf1a52ef9c5b31d689ecccf6248b12d1d270460c048bf904650b
     HEAD_REF master
+    PATCHES
+        disable-test.patch
 )
 
 vcpkg_configure_cmake(
