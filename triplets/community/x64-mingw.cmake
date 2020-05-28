4390dae1c5d57b4fba63f386399da994fd0fb2b6
Todor Prokopov <koprok@gmail.com>
2020-05-28 17:18:27 +0300

    [vcpkg] Missing import libs check does not work with MinGW

--- triplets/community/x64-mingw.cmake
+++ triplets/community/x64-mingw.cmake
@@ -4,3 +4,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_ENV_PASSTHROUGH PATH)
 
 set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
+
+# Missing import libs check does not work with MinGW.
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
