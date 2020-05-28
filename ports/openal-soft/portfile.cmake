0ad324d382ff01f9cf6d0c9325a58bfa95320c9f
Todor Prokopov <koprok@gmail.com>
2020-05-28 17:18:42 +0300

    [openal-soft] MinGW build fixes

--- ports/openal-soft/portfile.cmake
+++ ports/openal-soft/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         dont-export-symbols-in-static-build.patch
+        fix-mingw-build.patch
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
