5f451b590c4ac03ea056d490b82cb288ff68850f
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-05-28 20:11:44 +0800

    fix BUILD_ARCH

--- ports/networkdirect/portfile.cmake
+++ ports/networkdirect/portfile.cmake
@@ -1,4 +1,3 @@
-#vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "UWP" "Linux" "OSX")
 
 find_program(GIT NAMES git git.cmd)
@@ -28,9 +27,16 @@ vcpkg_apply_patches(
 file(REMOVE_RECURSE ${SOURCE_PATH}/.build/Local/CBTModules/CBTModules.proj)
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/packages.config DESTINATION ${SOURCE_PATH}/.build/Local/CBTModules)
 
+if(TRIPLET_SYSTEM_ARCH MATCHES "x86")
+    set(BUILD_ARCH "Win32")
+else()
+    set(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
+endif()
+
 vcpkg_install_msbuild(
     SOURCE_PATH ${SOURCE_PATH}
     PROJECT_SUBPATH dirs.proj
+    SKIP_CLEAN
     OPTIONS
         "/p:PlatformToolset=WindowsUserModeDriver10.0"
         "/p:BasePlatformToolset=${VCPKG_PLATFORM_TOOLSET}"
@@ -38,8 +44,10 @@ vcpkg_install_msbuild(
     LICENSE_SUBPATH LICENSE.txt
 )
 
-file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}/out/Release-${TRIPLET_SYSTEM_ARCH}/include" DESTINATION ${CURRENT_PACKAGES_DIR})
+file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}/out/Release-${BUILD_ARCH}/include" DESTINATION ${CURRENT_PACKAGES_DIR})
 file(REMOVE_RECURSE 
+#  ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}/packages
+#  ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}/packages
   ${CURRENT_INSTALLED_DIR}/tools/${PORT}/nuget.exe
   ${CURRENT_PACKAGES_DIR}/bin 
   ${CURRENT_PACKAGES_DIR}/debug/bin
