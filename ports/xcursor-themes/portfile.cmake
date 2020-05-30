c44d8d4c9ea5212666a942d721da3af0756de34c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-05-30 22:23:11 +0200

    xcursor fixes

--- ports/xcursor-themes/portfile.cmake
+++ ports/xcursor-themes/portfile.cmake
@@ -1,3 +1,4 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org/xorg
     OUT_SOURCE_PATH SOURCE_PATH
@@ -10,24 +11,17 @@ vcpkg_from_gitlab(
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
 
+set(ENV{XCURSORGEN} "${CURRENT_INSTALLED_DIR}/tools/xcursorgen/bin/xcursorgen${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
     AUTOCONFIG
-    #SKIP_CONFIGURE
-    #NO_DEBUG
-    #AUTO_HOST
-    #AUTO_DST
-    #PRERUN_SHELL ${SHELL_PATH}
-    #OPTIONS
-    #OPTIONS_DEBUG
-    #OPTIONS_RELEASE
 )
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+
 
 # # Handle copyright
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
--- ports/xcursorgen/portfile.cmake
+++ ports/xcursorgen/portfile.cmake
@@ -1,3 +1,5 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org/xorg
     OUT_SOURCE_PATH SOURCE_PATH
@@ -5,29 +7,23 @@ vcpkg_from_gitlab(
     REF 291d9a052aec0ad4a315c09a9af8b451c94ed57a 
     SHA512  eb834227020d2158d33ca2c9683592572bdba2ca9b6e5f940171ad13a4c3d893447cf261e679fc1e67f46e1fc86804e14abf3275d29055aa6996d502fc367970
     HEAD_REF master # branch name
-    #PATCHES example.patch #patch name
+    PATCHES configure.patch #this patch is incomplete which is why we need to set LIBS here
 ) 
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    set(ENV{LIBS} "$ENV{LIBS} -lxcb -lxdmcp -lxau -lm -Wl,--no-as-needed -ldl")
+endif()
 
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
     AUTOCONFIG
-    #SKIP_CONFIGURE
-    #NO_DEBUG
-    #AUTO_HOST
-    #AUTO_DST
-    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
-    #OPTIONS
-    #OPTIONS_DEBUG
-    #OPTIONS_RELEASE
 )
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
 # # Handle copyright
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
