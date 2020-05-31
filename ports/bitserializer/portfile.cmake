5383ee6ad84242fb732a3aec8649a18be38bb25f
Pavel_Kisliak <real@tut.by>
2020-05-31 22:53:33 +0300

    [bitserializer] Update to version 0.10

--- ports/bitserializer/portfile.cmake
+++ ports/bitserializer/portfile.cmake
@@ -1,17 +1,26 @@
 vcpkg_from_bitbucket(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Pavel_Kisliak/BitSerializer
-	REF 0.9
-	SHA512 74515a1a8338935c52752200d96abc0c0ee2c2b0b42880b1d10c8f56b5f8eb30680bd96b64eb2b352764128ad6a1fcd59749f481883c48a06230d652bee05902
+    REF 0.10
+    SHA512 518edee6b9cfc44ab5a28128aff82c4f84d5484aa4ffb609ebb286cd46f451f24a0823156692e5d8e7de7bbb40d70fcdd0d246791a45237966bc06775047488a
     HEAD_REF master
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    "cpprestjson-archive"  BUILD_CPPRESTJSON_ARCHIVE
+    "rapidjson-archive"    BUILD_RAPIDJSON_ARCHIVE
+    "pugixml-archive"      BUILD_PUGIXML_ARCHIVE
+    "rapidyaml-archive"    BUILD_RAPIDYAML_ARCHIVE
+)
+
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/core
+    SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/bitserializer/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/bitserializer)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/debug)
 
