ded6b139a80d26010d996a903de4794c7a515c92
NancyLi1013 <lirui09@beyondsoft.com>
2020-06-01 00:00:17 -0700

    [geographiclib] Update to 1.50.1

--- ports/geographiclib/portfile.cmake
+++ ports/geographiclib/portfile.cmake
@@ -1,35 +1,30 @@
+set(GEOGRAPHICLIB_VERSION 1.50.1)
+
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://jaist.dl.sourceforge.net/project/geographiclib/distrib/archive/GeographicLib-1.47-patch1.zip"
-    FILENAME "geographiclib-1.47-patch1.zip"
-    SHA512 d8fdfd7ae093057ec1a4ab922457fe71a3fb9975df5b673c276d62a0e9c4f212dc63652830b9d89e3890bc96aafd335992943cf6a1bce8260acf932d1eb7abfd
+    URLS "https://sourceforge.net/projects/geographiclib/files/distrib/GeographicLib-${GEOGRAPHICLIB_VERSION}.tar.gz"
+    FILENAME "geographiclib-${GEOGRAPHICLIB_VERSION}.tar.gz"
+    SHA512 1db874f30957a0edb8a1df3eee6db73cc993629e3005fe912e317a4ba908e7d7580ce483bb0054c4b46370b8edaec989609fb7e4eb6ba00c80182db43db436f1
 )
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     PATCHES
-           remove-tools-and-fix-version.patch
-           fix-usage.patch
+        remove-tools-and-fix-version.patch
+        fix-usage.patch
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS -DGEOGRAPHICLIB_LIB_TYPE=SHARED
-        PREFER_NINJA # Disable this option if project cannot be built with Ninja
-        # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-        # OPTIONS_RELEASE -DOPTIMIZE=1
-        # OPTIONS_DEBUG -DDEBUGGABLE=1
-    )
+    set(GEOGRAPHICLIB_LIB_TYPE SHARED)
 else()
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS -DGEOGRAPHICLIB_LIB_TYPE=STATIC
-        PREFER_NINJA # Disable this option if project cannot be built with Ninja
-        # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-        # OPTIONS_RELEASE -DOPTIMIZE=1
-        # OPTIONS_DEBUG -DDEBUGGABLE=1
-    )
+    set(GEOGRAPHICLIB_LIB_TYPE STATIC)
 endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS 
+        -DGEOGRAPHICLIB_LIB_TYPE=${GEOGRAPHICLIB_LIB_TYPE}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+)
 
 vcpkg_install_cmake()
 
