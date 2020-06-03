16a87999353791796b17b7a6790f74a73a5d7438
Eric Huff <thegreaterici@gmail.com>
2020-06-03 09:01:15 -0600

    Change version and update outdated vcpkg functions

--- ports/librabbitmq/portfile.cmake
+++ ports/librabbitmq/portfile.cmake
@@ -1,9 +1,7 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO alanxz/rabbitmq-c
-  REF d416b8b16d196085106cfe137a0ff6919a9f6752 # v0.10.0
+  REF d416b8b16d196085106cfe137a0ff6919a9f6752 
   SHA512 3fc137893fc18509a3e583cc8d40a8e91f219063237b9fd018a65cf14da188914ddba3a031c4bc033a886fed19fc6291d1b28b55458b9163eb6d20425b0474dc
   HEAD_REF master
   PATCHES
@@ -29,4 +27,4 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/rabbitmq-c TARGET_PATH share/rab
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
-file(INSTALL ${SOURCE_PATH}/LICENSE-MIT DESTINATION ${CURRENT_PACKAGES_DIR}/share/librabbitmq RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE-MIT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${port} RENAME copyright)
