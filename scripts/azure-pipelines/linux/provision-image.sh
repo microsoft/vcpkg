e2c6a74015a9981c5743a44f0c93a7499b60149c
Billy Robert O'Neal III <bion@microsoft.com>
2020-06-03 20:15:14 -0700

    Combine some apt installs, remove /etc/debian_version workaround, defend against no StorageAccountName et al. set.

--- scripts/azure-pipelines/linux/provision-image.sh
+++ scripts/azure-pipelines/linux/provision-image.sh
@@ -5,17 +5,41 @@
 
 sudo apt -y update
 sudo apt -y dist-upgrade
-# Install common build dependencies and partitioning tools
-sudo apt -y install at curl unzip tar libxt-dev gperf libxaw7-dev cifs-utils build-essential g++ gfortran zip libx11-dev libxkbcommon-x11-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxinerama-dev libxcursor-dev yasm libnuma1 libnuma-dev python-six python3-six python-yaml flex libbison-dev autoconf libudev-dev libncurses5-dev libtool libxrandr-dev xutils-dev dh-autoreconf libgles2-mesa-dev ruby-full pkg-config
-# Required by qt5-base
-sudo apt -y install libx11-dev libxext-dev libxfixes-dev libxi-dev libxrender-dev libxcb1-dev libx11-xcb-dev libxcb-glx0-dev libxkbcommon-x11-dev
-# Required by qt5-base for qt5-x11extras
-sudo apt -y install libxkbcommon-dev libxcb-keysyms1-dev libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev
-# Required by libhdfs3
-sudo apt -y install libkrb5-dev
-# Required by mesa
-sudo apt -y install python3-setuptools
+# Install common build dependencies
+APT_PACKAGES="at curl unzip tar libxt-dev gperf libxaw7-dev cifs-utils \
+  build-essential g++ gfortran zip libx11-dev libxkbcommon-x11-dev libxi-dev \
+  libgl1-mesa-dev libglu1-mesa-dev mesa-common-dev libxinerama-dev \
+  libxcursor-dev yasm libnuma1 libnuma-dev python-six python3-six python-yaml \
+  flex libbison-dev autoconf libudev-dev libncurses5-dev libtool libxrandr-dev \
+  xutils-dev dh-autoreconf libgles2-mesa-dev ruby-full pkg-config"
 
+# Additionally required by qt5-base
+APT_PACKAGES="$APT_PACKAGES libxext-dev libxfixes-dev libxrender-dev \
+  libxcb1-dev libx11-xcb-dev libxcb-glx0-dev"
+
+# Additionally required by qt5-base for qt5-x11extras
+APT_PACKAGES="$APT_PACKAGES libxkbcommon-dev libxcb-keysyms1-dev \
+  libxcb-image0-dev libxcb-shm0-dev libxcb-icccm4-dev libxcb-sync0-dev \
+  libxcb-xfixes0-dev libxcb-shape0-dev libxcb-randr0-dev libxcb-render-util0-dev"
+
+# Additionally required by libhdfs3
+APT_PACKAGES="$APT_PACKAGES libkrb5-dev"
+
+# Additionally required by mesa
+APT_PACKAGES="$APT_PACKAGES python3-setuptools"
+
+# Additionally required/installed by Azure DevOps Scale Set Agents
+APT_PACKAGES="$APT_PACKAGES liblttng-ust0 libkrb5-3 zlib1g libicu60"
+
+sudo apt -y install $APT_PACKAGES
+
+# Delete /etc/debian_version to prevent Azure Pipelines Scale Set Agents from
+# removing some of the above
+sudo apt-mark hold libcurl4
+sudo apt-mark hold liblttng-ust0
+sudo apt-mark hold libkrb5-3
+sudo apt-mark hold zlib1g
+sudo apt-mark hold libicu60
 
 # Install newer version of nasm than the apt package, required by intel-ipsec
 mkdir /tmp/nasm
@@ -47,6 +71,11 @@ sudo apt update
 sudo add-apt-repository universe
 sudo apt install -y powershell
 
+if [ -z "$StorageAccountName" ]; then
+echo "No storage account supplied, skipping."
+else
+echo "Mapping storage account"
+
 # Write SMB credentials
 sudo mkdir /etc/smbcredentials
 smbCredentialFile=/etc/smbcredentials/$StorageAccountName.cred
@@ -57,11 +86,4 @@ sudo chmod 600 $smbCredentialFile
 # Mount the archives SMB share to /archives
 sudo mkdir /archives -m=777
 echo "//$StorageAccountName.file.core.windows.net/archives /archives cifs nofail,vers=3.0,credentials=$smbCredentialFile,serverino,dir_mode=0777,file_mode=0777 0 0" | sudo tee -a /etc/fstab
-
-# Delete /etc/debian_version to prevent Azure Pipelines Scale Set Agents from removing some of the above
-sudo rm /etc/debian_version
-
-# Install dependencies that the Azure Pipelines agent will want later to make launching VMs faster
-# https://docs.microsoft.com/en-us/dotnet/core/install/dependencies?tabs=netcore31&pivots=os-linux
-# (we assume libssl1.0.0 or equivalent is already installed to not accidentially change SSL certs)
-apt install -y liblttng-ust0 libkrb5-3 zlib1g libicu60
+fi
