ac69c3721f8cc22a84769a4afdead59bc734365e
yurybura <yurybura@gmail.com>
2020-06-04 13:46:30 +0300

    [sqlpp11] update to v0.59 [sqlpp11-connector-mysql] support MySQL connector

--- ports/sqlpp11/portfile.cmake
+++ ports/sqlpp11/portfile.cmake
@@ -1,10 +1,8 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rbock/sqlpp11
-    REF 0.58
-    SHA512 c391e72638a748e0e25b53176dc371ba468bc14bdcb6dda2f2418c4ab4d620ebc5507ee284ff81c3104888d0d959703c6c91b55ccd69a8641b07dcb20cd56209
+    REF 0.59
+    SHA512 9da05e7a5163200040205b9740d6bf4ad1faa94b2bf031c16d896865b3f10e0fe95a0532a2c2e89adc051250a7f76c550a239916fdd700828d4fb1da566a4fe3
     HEAD_REF master
     PATCHES ddl2cpp_path.patch
 )
@@ -21,7 +19,6 @@ vcpkg_install_cmake()
 
 # Move CMake config files to the right place
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Sqlpp11 TARGET_PATH share/sqlpp11)
-
 
 # Delete redundant and unnecessary directories
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
