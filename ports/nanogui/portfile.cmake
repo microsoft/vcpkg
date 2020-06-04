cd2906cf846a5cc37a0d637903bb51992a075ac1
PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>
2020-06-04 02:56:08 -0700

    [nanogui] Update to 2020-06-04

--- ports/nanogui/portfile.cmake
+++ ports/nanogui/portfile.cmake
@@ -1,14 +1,10 @@
-include(vcpkg_common_functions)
-
-if(VCPKG_TARGET_IS_UWP)
-    message(FATAL_ERROR "nanogui doesn't support UWP.")
-endif()
+vcpkg_fail_port_install(ON_TARGET "uwp")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO wjakob/nanogui
-    REF e9ec8a1a9861cf578d9c6e85a6420080aa715c03 #Commits on Sep 23, 2019 
-    SHA512 36c93bf977862ced2df4030211e2b83625e60a11fc9fdb6c1f2996bb234758331d3f41a7fbafd25a5bca0239ed9bac9c93446a4a7fac4c5e6d7943af2be3e14a
+    REPO mitsuba-renderer/nanogui
+    REF 21e5cbc880b2e26b28b2a35085a9e6706da1e2a8
+    SHA512 7eb356f0b589137f542db4b5b631071459ba367cd9db962e72edb19c1dbdbf37b4c93216152563bc23aae74fe6af2c2c859409a05234b26f2d29149622119d42
     HEAD_REF master
     PATCHES
       fix-cmakelists.patch
@@ -16,11 +12,12 @@ vcpkg_from_github(
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTION
+        -DNANOGUI_BUILD_EXAMPLES=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-# Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
