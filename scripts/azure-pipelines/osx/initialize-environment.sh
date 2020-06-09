a22665f581831ff1319cb0267b8f96666cab2c05
Billy O'Neal <bion@microsoft.com>
2020-06-08 22:46:18 -0700

    [vcpkg] Fix OSX CI by ensuring the downloads directory exists (#11839)
    * [vcpkg] Remove do-nothing Set-Content from Windows azure-pipelines.yml.
    * [vcpkg] Fix OSX CI by ensuring the downloads directory exists in advance, and extract common command line parameters with powershell splatting.
    * [tensorflow-cc] Prevent hang building tensorflow-cc asking to configure iOS.
    * Skip ignition-msgs5:x64-osx

--- scripts/azure-pipelines/osx/initialize-environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: MIT
-
-# Sets up the environment for MacOS runs of vcpkg CI
-
-#delete downloaded files that have not been used in 7 days
-find ~/Data/downloads/  -maxdepth 1 -type f ! -atime 7  -exec rm -f {} \;
