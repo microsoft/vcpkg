6acefc5a3c9204237098bfa673f82235ab3bb6f9
am2222 <asd56yu@gmail.com>
2020-06-10 10:43:49 -0400

    minor fixes

--- ports/mapnik/portfile.cmake
+++ ports/mapnik/portfile.cmake
@@ -1,36 +1,13 @@
-# find_program(GIT git)
-# set(GIT_URL "https://github.com/am2222/mapnik-windows.git")
-# set(GIT_REV "fdba45cd95b66576e47de2e56d30196bfc9de99d")
-
-# set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
-
-# if(NOT EXISTS "${SOURCE_PATH}/.git")
-# 	message(STATUS "Cloning and fetching submodules")
-# 	vcpkg_execute_required_process(
-# 	  COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
-# 	  WORKING_DIRECTORY ${SOURCE_PATH}
-# 	  LOGNAME clone
-# 	)
-
-# 	message(STATUS "Checkout revision ${GIT_REV}")
-# 	vcpkg_execute_required_process(
-# 	  COMMAND ${GIT} checkout ${GIT_REV}
-# 	  WORKING_DIRECTORY ${SOURCE_PATH}
-# 	  LOGNAME checkout
-# 	)
-# endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mapnik/mapnik
     REF v3.0.23
     SHA512 004429defd3047601b63c28bedd57d701ff88cdfb910a9958f4a10a1ac032d4d13316372122aef06eda60f2e4f67600b8842577db455f8c970009a0e86c5c1cf
     HEAD_REF master
-    PATCHES 0001-CMakeFiles.patch 0002-sqlitepixelwidth.patch
+    PATCHES
+      0001-CMakeFiles.patch 
+      0002-sqlitepixelwidth.patch
 )
-
-message(STATUS "Adding worktree done")
-
 
 vcpkg_find_acquire_program(PYTHON2)
 get_filename_component(PYTHON2_DIR "${PYTHON2}" DIRECTORY)
@@ -46,30 +23,27 @@ endif()
 
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  # Keyword FEATURES is optional if INVERTED_FEATURES are not used
-    CAIRO   WITH_CAIRO
-    DEMO  WITH_DEMO
-    INPUT_CSV WITH_INPUT_CSV
-    INPUT_GDAL WITH_INPUT_GDAL
-    INPUT_GEOBUF  WITH_INPUT_GEOBUF
-    INPUT_GEOJSON   WITH_INPUT_GEOJSON
-    INPUT_OGR WITH_INPUT_OGR
-    INPUT_PGRASTER  WITH_INPUT_PGRASTER
-    INPUT_POSTGIS  WITH_INPUT_POSTGIS
-    INPUT_RASTER  WITH_INPUT_RASTER
-    INPUT_SHAPE WITH_INPUT_SHAPE
-    INPUT_SQLITE WITH_INPUT_SQLITE
-    INPUT_TOPOJSON  WITH_INPUT_TOPOJSON
-    PROJ4 WITH_PROJ4
-    UTILS WITH_UTILS
-
+    cairo   WITH_CAIRO
+    demo  WITH_DEMO
+    input_csv WITH_INPUT_CSV
+    input_gdal WITH_INPUT_GDAL
+    input_geobuf  WITH_INPUT_GEOBUF
+    input_geojson   WITH_INPUT_GEOJSON
+    input_ogr WITH_INPUT_OGR
+    input_pgraster  WITH_INPUT_PGRASTER
+    input_postgis  WITH_INPUT_POSTGIS
+    input_raster  WITH_INPUT_RASTER
+    input_shape WITH_INPUT_SHAPE
+    input_sqlite WITH_INPUT_SQLITE
+    input_topojson  WITH_INPUT_TOPOJSON
+    proj4 WITH_PROJ4
+    utils WITH_UTILS
 )
 
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT}
- #    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-#    NO_CHARSET_FLAG # automatic templates
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
   OPTIONS   
       ${FEATURE_OPTIONS}
     	-DMAPNIK_STATIC_LIB=${MAPNIK_STATIC_LIB}
@@ -78,8 +52,6 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-
-
 
 file(COPY ${SOURCE_PATH}/include/${PORT} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
@@ -90,15 +62,13 @@ if ("UTILS" IN_LIST FEATURES)
       TOOL_NAMES mapnik-demo mapnik-render shapeindex
       AUTO_CLEAN
     )
+    
     file(COPY ${SOURCE_PATH}/demo/data DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik/demo)
     file(COPY ${SOURCE_PATH}/fonts DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
     file(COPY ${CURRENT_PACKAGES_DIR}/bin/plugins DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
 
     file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/mapnik-demo ${CURRENT_PACKAGES_DIR}/debug/bin/mapnik-render ${CURRENT_PACKAGES_DIR}/debug/bin/shapeindex)
-
-
 endif()
-
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(INSTALL ${SOURCE_PATH}/fonts/unifont_license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME fonts_copyright)
