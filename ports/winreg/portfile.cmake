984794ef534326e8624948a3829536e65d57897d
JonLiu1993 <13720414433@163.com>
2020-06-11 13:54:36 +0800

    [WinReg] Update to 3.0.1

--- ports/winreg/portfile.cmake
+++ ports/winreg/portfile.cmake
@@ -4,8 +4,8 @@ vcpkg_fail_port_install(ON_TARGET "linux" "osx")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GiovanniDicanio/WinReg
-    REF c1c2a9ce26295cd3f0ca69e7383319813d46574f #v2.4.0
-    SHA512 99d52bb90784c5918816f2f843b75001a817cab25470a5a8a09bd8e6189a1631c098c5da9935abb94cf21461e9b58f431619d943efaa0750f216fcf1ba96c427 
+    REF ffb5ae9eb6609847c3a3a98d8da6192e2f67be79 #v3.0.1
+    SHA512 6a9fecb09769a3d859d09421dd9c894fb1fe1ed90ede3eb899dba0a4c7a01f69192bd5b640bba6aea675de9e025a8121230f1e36d2354ed9fb79f1f2167299b4 
     HEAD_REF master
 )
 
