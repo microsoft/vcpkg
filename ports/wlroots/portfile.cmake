9482c96b3d4b093895a8cb6d14280b4b5cd796d4
Nickolay Ilyushin <nickolay02@inbox.ru>
2020-06-13 13:35:24 +0300

    Added wlroots port

---
+++ ports/wlroots/portfile.cmake
@@ -0,0 +1,27 @@
+include(vcpkg_common_functions)
+
+vcpkg_fail_port_install(
+  MESSAGE "Only Linux is supported by wlroots"
+  ON_ARCH x86 arm arm64
+  ON_TARGET WINDOWS UWP ANDROID FREEBSD OSX
+)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO swaywm/wlroots
+    REF 0c7c562482575cacaecadcd7913ef25aeb21711f # 0.10.1
+    SHA512 3b464041331e0f61386200bb89ba3c603f5bd37545bf1c3413ba2be03d684e5187466b4fa4821a701c2148191a7f933ec087bc148467f7f5b2a04c23f21a0bcf
+    HEAD_REF master
+    PATCHES use-system-wayland-data.patch
+)
+
+vcpkg_configure_meson(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        --backend=ninja
+        "-Dvcpkg_installed_dir=${CURRENT_INSTALLED_DIR}"
+)
+
+vcpkg_install_meson()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
