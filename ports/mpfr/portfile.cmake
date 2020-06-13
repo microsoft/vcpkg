805e70a502974ac171e083b403bb6a7ef546aaae
Voskrese <10104740+Voskrese@users.noreply.github.com>
2020-06-14 02:34:00 +0800

    Update portfile.cmake

--- ports/mpfr/portfile.cmake
+++ ports/mpfr/portfile.cmake
@@ -3,10 +3,12 @@ vcpkg_download_distfile(ARCHIVE
     FILENAME "mpfr-4.0.2.tar.xz"
     SHA512 d583555d08863bf36c89b289ae26bae353d9a31f08ee3894520992d2c26e5683c4c9c193d7ad139632f71c0a476d85ea76182702a98bf08dde7b6f65a54f8b88
 )
+
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
 )
+
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_configure_cmake(
