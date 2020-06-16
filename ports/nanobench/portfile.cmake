f0704e64e4ac982f056395883773de51d756d8a4
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2020-06-16 10:21:09 +0800

    Update ports/nanobench/portfile.cmake

--- ports/nanobench/portfile.cmake
+++ ports/nanobench/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO martinus/nanobench
-    REF c534992696b9341274c6714931d0064d74239fcb
+    REF c534992696b9341274c6714931d0064d74239fcb #v4.0.0
     SHA512 09078f1100c6f843e7646b8aaab687c32e71ba2dc05a2a5b282c72ab064ceffbf4aeb5ad14c4a2bdbe2ea66dca9cd207dee5eade44f77844cddfa490b4c09c32
     HEAD_REF master
 )
