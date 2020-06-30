f05103afa1c2e0d7cdfd6b785deb75c8a5cb0cf7
JackBoosY <yuzaiyang@beyondsoft.com>
2020-06-29 22:35:38 -0700

    Add error message when building paraview[tools]:x64-osx

--- ports/paraview/portfile.cmake
+++ ports/paraview/portfile.cmake
@@ -1,5 +1,9 @@
 set(VERSION 5.8)
 
+if ("tools" IN_LIST FEATURES AND VCPKG_TARGET_IS_OSX)
+    mesage(FATAL_ERROR "Feature tools are currently not supported.")
+endif()
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     "cuda"         PARAVIEW_USE_CUDA            #untested; probably only affects internal VTK build so it does nothing here
     "all_modules"  PARAVIEW_BUILD_ALL_MODULES   #untested
