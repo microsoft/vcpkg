123300e1ac054a405665f29985431c51a0fc9390
Leo Yao <t-yunyao@microsoft.com>
2020-07-01 09:16:01 -0700

    fixed an issue that causes some leading slashes were being removed

--- scripts/file_script.py
+++ scripts/file_script.py
@@ -19,7 +19,7 @@ def gen_all_file_strings(path, files, headers, output):
                 if idx < 0:
                     continue
                 headers.write(package + ":" + line[idx + len(keyword):])
-                output.write(package + ":" + line[idx:])
+                output.write(package + ":" + line[idx-1:])
             elif line.strip()[-1] != "/":
                 output.write(package + ":" + line[line.find("/"):])
         f.close()
