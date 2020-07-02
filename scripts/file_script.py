a4f8ba3c6a8489073a56ad394f78bbf7b6dbdf02
Leo Yao <t-yunyao@microsoft.com>
2020-07-02 08:37:43 -0700

    Changed Header Database to include all files under \include\ folder

--- scripts/file_script.py
+++ scripts/file_script.py
@@ -14,10 +14,8 @@ def gen_all_file_strings(path, files, headers, output):
         package = file[:file.find("_")]
         f = open(path + file)
         for line in f:
-            if (line.strip()[-1] == "h" and line.strip()[-2] == '.') or line.strip()[-4:] == '.hpp':
-                idx = line.strip().find(keyword)
-                if idx < 0:
-                    continue
+            idx = line.strip().find(keyword)
+            if idx >= 0 and line.strip()[-1] != "/":
                 headers.write(package + ":" + line[idx + len(keyword):])
                 output.write(package + ":" + line[idx-1:])
             elif line.strip()[-1] != "/":
