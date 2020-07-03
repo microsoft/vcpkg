e4e62557e8f2abe3ee2048016425ff4a33714c51
Johnny Willemsen <jwillemsen@remedy.nl>
2020-07-03 08:31:33 +0200

    Add version macros to make the port file easier to update
        * ports/ace/portfile.cmake:

--- ports/ace/portfile.cmake
+++ ports/ace/portfile.cmake
@@ -1,19 +1,22 @@
 # Using zip archive under Linux would cause sh/perl to report "No such file or directory" or "bad interpreter"
 # when invoking `prj_install.pl`.
 # So far this issue haven't yet be triggered under WSL 1 distributions. Not sure the root cause of it.
+set(VERSION 6.5.10)
+set(VERSION_DIRECTORY 6_5_10)
+
 if("tao" IN_LIST FEATURES)
   if(VCPKG_TARGET_IS_WINDOWS)
       # Don't change to vcpkg_from_github! This points to a release and not an archive
       vcpkg_download_distfile(ARCHIVE
-          URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_10/ACE%2BTAO-src-6.5.10.zip"
-          FILENAME ACE-TAO-6.5.10.zip
+          URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-${VERSION_DIRECTORY}/ACE%2BTAO-src-${VERSION}.zip"
+          FILENAME ACE-TAO-${VERSION}.zip
           SHA512 1b6453bb692dde6a51090a1ea57677d9241a54ebb7edf32838dbf413f7515b83ae77f407998609f6dcd7cc37b2d4973f2b5cb1ad2f92f75caa686efd08c3a0b5
       )
     else()
       # VCPKG_TARGET_IS_LINUX
       vcpkg_download_distfile(ARCHIVE
-          URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_10/ACE%2BTAO-src-6.5.10.tar.gz"
-          FILENAME ACE-TAO-6.5.10.tar.gz
+          URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-${VERSION_DIRECTORY}/ACE%2BTAO-src-${VERSION}.tar.gz"
+          FILENAME ACE-TAO-${VERSION}.tar.gz
           SHA512 d0b8c6b398fba62dce75daa2f8759113c1235be3dc3d1c88ecce668d804a1acd6f40bc7e38eede3000c7f85e9da2123b84da1357375f5ee29b1a002cd9d6e0f8
       )
     endif()
@@ -21,15 +24,15 @@ else()
   if(VCPKG_TARGET_IS_WINDOWS)
     # Don't change to vcpkg_from_github! This points to a release and not an archive
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_10/ACE-src-6.5.10.zip"
-        FILENAME ACE-src-6.5.10.zip
+        URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-${VERSION_DIRECTORY}/ACE-src-${VERSION}.zip"
+        FILENAME ACE-src-${VERSION}.zip
         SHA512 d17b4bf41ea6804594a8363d2b8776b5981d305df75b99cda5fe05dd021261a424e1038a522c8dc175f8ea2ee15e064676cbd615bd6c3c6339ef174a0e8a0914
     )
   else(VCPKG_TARGET_IS_WINDOWS)
     # VCPKG_TARGET_IS_LINUX
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_10/ACE-src-6.5.10.tar.gz"
-        FILENAME ACE-src-6.5.10.tar.gz
+        URLS "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-${VERSION_DIRECTORY}/ACE-src-${VERSION}.tar.gz"
+        FILENAME ACE-src-${VERSION}.tar.gz
         SHA512 a92edae9e120f2220272f138907fc25e53753d3b21a446460e8682d0cae6f5c6e5fa08d398630f6d1e189bc47ce8e69a52143635f459292522753549fd7ad9ab
     )
   endif()
