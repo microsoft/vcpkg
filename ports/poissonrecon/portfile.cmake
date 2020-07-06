2dd6293873908bd1291bd1c54b66244fbc0ee64e
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-07-06 04:43:05 +0200

    [PoissonRecon] fix build

--- ports/poissonrecon/portfile.cmake
+++ ports/poissonrecon/portfile.cmake
@@ -1,11 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mkazhdan/PoissonRecon
-    REF a5c7d359a70c6e560feb5a8543bc38fc71d5a72f
-    SHA512 6190666a4f8f3c1b5e3efd0af47af2dbfcff535b3c5a60323301b29b3628d7918a2cb6da1f50e1bb5cf81c3ef1c4db623ef09f4d8d72eae8fb4a1284a55b93d1
+    REF 03f73754e994eb388de63285d3a2772493168e8a
+    SHA512 be1d6842952d2b27860b5a82d9cc536da213fd2e44d946c512e04881af66a4c7c039930347fe9db5b168cc356e55167c9bdbdb39eab9cea68882dc01a9482867
     HEAD_REF master
     PATCHES
         use-external-libs.patch
+        intel-isl.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
