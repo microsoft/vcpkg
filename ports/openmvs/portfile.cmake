3c848595ae7d80a7168a2bc16380b93e10288595
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-07-06 21:54:19 +0200

    [OpenMVS] fixes when using cmake targets

--- ports/openmvs/portfile.cmake
+++ ports/openmvs/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_github(
     PATCHES
        fix-tool-paths.patch
        fix-linux-build.patch
+       fix-tool-build.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -24,6 +25,10 @@ vcpkg_configure_cmake(
         -DINSTALL_CMAKE_DIR:STRING=share/openmvs
         -DINSTALL_BIN_DIR:STRING=bin
         -DINSTALL_LIB_DIR:STRING=lib
+    OPTIONS_RELEASE
+        -DOpenMVS_BUILD_TOOLS=ON
+    OPTIONS_DEBUG
+        -DOpenMVS_BUILD_TOOLS=OFF
 )
 
 vcpkg_install_cmake()
@@ -31,6 +36,15 @@ vcpkg_install_cmake()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_fixup_cmake_targets()
+file(READ ${CURRENT_PACKAGES_DIR}/share/openmvs/OpenMVSTargets-release.cmake TARGETS_CMAKE)
+string(REPLACE "bin/InterfaceCOLMAP.exe" "tools/openmvs/InterfaceCOLMAP.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/InterfaceVisualSFM.exe" "tools/openmvs/InterfaceVisualSFM.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/DensifyPointCloud.exe" "tools/openmvs/DensifyPointCloud.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/ReconstructMesh.exe" "tools/openmvs/ReconstructMesh.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/RefineMesh.exe" "tools/openmvs/RefineMesh.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/TextureMesh.exe" "tools/openmvs/TextureMesh.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+string(REPLACE "bin/Viewer.exe" "tools/openmvs/Viewer.exe" TARGETS_CMAKE "${TARGETS_CMAKE}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/openmvs/OpenMVSTargets-release.cmake "${TARGETS_CMAKE}")
 
 vcpkg_copy_tools(AUTO_CLEAN TOOL_NAMES
     DensifyPointCloud
