b37b1bae589d25b21458bbe530161f308350c60c
ras0219 <533828+ras0219@users.noreply.github.com>
2020-07-06 11:53:25 -0700

    [vcpkg] Revert change which causes sources to be purged by default in `vcpkg build` (#12272)
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

--- toolsrc/src/vcpkg/build.cpp
+++ toolsrc/src/vcpkg/build.cpp
@@ -126,6 +126,8 @@ namespace vcpkg::Build
         Checks::check_exit(VCPKG_LINE_INFO, action != nullptr);
         ASSUME(action != nullptr);
         action->build_options = default_build_package_options;
+        action->build_options.clean_buildtrees = CleanBuildtrees::NO;
+        action->build_options.clean_packages = CleanPackages::NO;
 
         const auto build_timer = Chrono::ElapsedTimer::create_started();
         const auto result = Build::build_package(paths, *action, binaryprovider, build_logs_recorder, status_db);
