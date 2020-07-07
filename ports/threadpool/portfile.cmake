f19d378fb7d3cd465f4bc64ebe71bff6c670b00b
Robert Schumacher <roschuma@microsoft.com>
2020-07-07 15:55:18 -0700

    [threadpool] Use vcpkg_from_sourceforge

--- ports/threadpool/portfile.cmake
+++ ports/threadpool/portfile.cmake
@@ -1,13 +1,9 @@
-# header-only library
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://downloads.sourceforge.net/project/threadpool/threadpool/0.2.5%20%28Stable%29/threadpool-0_2_5-src.zip"
-    FILENAME "threadpool-0_2_5-src.zip"
-    SHA512 961576b619e5227098fa37a3c8d903128b3c2a9cf1e55c057c6f9126062bcccfa6fe2510b4e8ee5d1a0e3d0425f0077c50eccad2120a423f69e2705460780e7c
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_sourceforge(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    REPO threadpool/threadpool
+    REF "0.2.5 (Stable)"
+    SHA512 961576b619e5227098fa37a3c8d903128b3c2a9cf1e55c057c6f9126062bcccfa6fe2510b4e8ee5d1a0e3d0425f0077c50eccad2120a423f69e2705460780e7c
+    FILENAME "threadpool-0_2_5-src.zip"
 )
 
 file(INSTALL ${SOURCE_PATH}/threadpool/boost
