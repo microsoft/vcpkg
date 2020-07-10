07f098687835688cb767e4ad779b8934d122c0e2
ig-or <19777752+ig-or@users.noreply.github.com>
2020-07-10 14:49:35 -0700

    marble: patch to move exe to tools

--- ports/marble/portfile.cmake
+++ ports/marble/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     REF a7b7e23113a8a87ae2206caed969ab557137c282 #v19.08.2
     SHA512 f808bfbb118f509bd7939e8ae514a72cb9eec30b3a42f2bfedffcbeab7a1cbd658e35b5cd3e90ebeeacee6402c11ae9d293c12aaa5f2d70908e451b174a58e8e
     HEAD_REF master
+    PATCHES "move-exe-to-tools.patch"
 )
  
 vcpkg_configure_cmake(
@@ -18,20 +19,14 @@ vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
 
 # Install  plugins and data files
-file(GLOB  EXE "${CURRENT_PACKAGES_DIR}/*.exe")
-file(GLOB  DLL "${CURRENT_PACKAGES_DIR}/*.dll")
-file(GLOB  DEXE "${CURRENT_PACKAGES_DIR}/debug/*.exe")
-file(GLOB  DDLL "${CURRENT_PACKAGES_DIR}/debug/*.dll")
 file(GLOB_RECURSE  PLUGINS "${CURRENT_PACKAGES_DIR}/plugins/*")
 file(GLOB_RECURSE  PLUGINS_DESIGNER "${CURRENT_PACKAGES_DIR}/lib/plugins/*")
 file(GLOB_RECURSE  PLUGINS_DEBUG "${CURRENT_PACKAGES_DIR}/debug/lib/plugins/*")
 file(GLOB_RECURSE  MKSPECS "${CURRENT_PACKAGES_DIR}/mkspecs/*")
 
 file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/${PORT})
-file(COPY ${EXE} ${DLL} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
-file(COPY ${DDLL} ${DEXE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/debug)
 file(COPY ${PLUGINS} ${PLUGINS_DESIGNER} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/plugins)
-file(COPY ${PLUGINS_DEBUG} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/debug/plugins)
+file(COPY ${PLUGINS_DEBUG} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}/plugins)
 file(COPY "${CURRENT_PACKAGES_DIR}/data" DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}) # have to keep folder structure here
 file(COPY ${MKSPECS} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT}/mkspecs)
 
@@ -39,12 +34,11 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # remove plugin folder
 file(REMOVE_RECURSE
-    ${EXE} ${DEXE} ${DLL} ${DDLL}
     ${CURRENT_PACKAGES_DIR}/plugins ${CURRENT_PACKAGES_DIR}/debug/plugins
     ${CURRENT_PACKAGES_DIR}/data    ${CURRENT_PACKAGES_DIR}/debug/data
     ${CURRENT_PACKAGES_DIR}/mkspecs ${CURRENT_PACKAGES_DIR}/debug/mkspecs
     ${CURRENT_PACKAGES_DIR}/debug/share
-	${CURRENT_PACKAGES_DIR}/debug/lib/plugins   ${CURRENT_PACKAGES_DIR}/lib/plugins
+    ${CURRENT_PACKAGES_DIR}/debug/lib/plugins   ${CURRENT_PACKAGES_DIR}/lib/plugins
 )
 
 vcpkg_copy_pdbs()
