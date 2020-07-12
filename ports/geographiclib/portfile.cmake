de697151d5b361cdc30c9e986a39f8d3208b506c
Charles Karney <charles.karney@sri.com>
2020-07-12 15:23:05 -0400

    geographiclib: fix up build of tools

--- ports/geographiclib/portfile.cmake
+++ ports/geographiclib/portfile.cmake
@@ -7,31 +7,42 @@ vcpkg_from_sourceforge (
   PATCHES cxx-library-only.patch
 )
 
-if (VCPKG_TARGET_TRIPLET MATCHES ".*-uwp")
-  set (SKIP_TOOLS_OPTION "-DSKIP_TOOLS=ON")
+if (TARGET_TRIPLET MATCHES ".*-uwp")
+  set (SKIP_TOOLS ON)
 else ()
-  set (SKIP_TOOLS_OPTION "-DSKIP_TOOLS=OFF")
+  set (SKIP_TOOLS OFF)
 endif ()
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  set (LIB_TYPE "-DGEOGRAPHICLIB_LIB_TYPE=SHARED")
+  set (LIB_TYPE "SHARED")
 else ()
-  set (LIB_TYPE "-DGEOGRAPHICLIB_LIB_TYPE=STATIC")
+  set (LIB_TYPE "STATIC")
 endif ()
 
 vcpkg_configure_cmake (
   SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS ${LIB_TYPE} ${SKIP_TOOLS_OPTION}
+  OPTIONS
+  "-DGEOGRAPHICLIB_LIB_TYPE=${LIB_TYPE}"
+  "-DSKIP_TOOLS=${SKIP_TOOLS}"
   PREFER_NINJA # Disable this option if project cannot be built with Ninja
   )
 
 vcpkg_install_cmake ()
 vcpkg_fixup_cmake_targets (CONFIG_PATH share/geographiclib)
 vcpkg_copy_pdbs ()
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
+
+file (GLOB TOOL_LIST LIST_DIRECTORIES false
+  ${CURRENT_PACKAGES_DIR}/tools/*)
+if (TOOL_LIST)
+  file (INSTALL ${TOOL_LIST}
+    DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+  vcpkg_copy_tool_dependencies (${CURRENT_PACKAGES_DIR}/tools/${PORT})
+  file (REMOVE ${TOOL_LIST})
+endif ()
 
 file (REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file (REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file (REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/tools)
 file (INSTALL ${SOURCE_PATH}/LICENSE.txt
   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
   RENAME copyright)
