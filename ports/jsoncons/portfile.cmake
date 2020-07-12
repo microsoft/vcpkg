9052643f09d20b37e11e396ac4885f1bb255f095
danielaparker <danielaparker@yahoo.com>
2020-07-12 12:02:01 -0400

    [jsoncons] Update to v0.154.1

--- ports/jsoncons/portfile.cmake
+++ ports/jsoncons/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO danielaparker/jsoncons
-    REF 75f2fde8cd18592c3ef317eff2832cedd3a5c0e6 # v0.154.0
-    SHA512 64660e89217f0520b41d646d5f6306897dfd6c3e99de0908a2c27fa0e5b4296a45d54515ad582c4def9e32cdbd17c8066639b64d0bfcae693b3756f941626432
+    REF b51fdde4b51b048b4a86aa8d7f1b17fac1ca1c85 # v0.154.1
+    SHA512 aa77bf2d9c6e8f21f0b04f4edcfb6bad486f5f1a7b7ed296101ca4d09da4e916b0425ce7d63408a7ebca238aeb695ac5248da7b94600eec3e7c42d7ddceffb1c
     HEAD_REF master
 )
 
