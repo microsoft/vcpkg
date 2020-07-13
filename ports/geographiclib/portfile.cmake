a1abc980f04cba6d9fdf569a8d883abca9aba9be
Charles Karney <charles.karney@sri.com>
2020-07-13 07:47:45 -0400

    geographiclib: Minor clean up

--- ports/geographiclib/portfile.cmake
+++ ports/geographiclib/portfile.cmake
@@ -1,10 +1,10 @@
 vcpkg_from_sourceforge (
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO geographiclib
-  REF distrib
-  FILENAME "GeographicLib-1.50.1.tar.gz"
-  SHA512 1db874f30957a0edb8a1df3eee6db73cc993629e3005fe912e317a4ba908e7d7580ce483bb0054c4b46370b8edaec989609fb7e4eb6ba00c80182db43db436f1
-  PATCHES cxx-library-only.patch
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO geographiclib
+    REF distrib
+    FILENAME "GeographicLib-1.50.1.tar.gz"
+    SHA512 1db874f30957a0edb8a1df3eee6db73cc993629e3005fe912e317a4ba908e7d7580ce483bb0054c4b46370b8edaec989609fb7e4eb6ba00c80182db43db436f1
+    PATCHES cxx-library-only.patch
 )
 
 if (TARGET_TRIPLET MATCHES ".*-uwp")
@@ -20,11 +20,11 @@ else ()
 endif ()
 
 vcpkg_configure_cmake (
-  SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-  "-DGEOGRAPHICLIB_LIB_TYPE=${LIB_TYPE}"
-  "-DSKIP_TOOLS=${SKIP_TOOLS}"
-  PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        "-DGEOGRAPHICLIB_LIB_TYPE=${LIB_TYPE}"
+        "-DSKIP_TOOLS=${SKIP_TOOLS}"
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
   )
 
 vcpkg_install_cmake ()
@@ -50,6 +50,3 @@ file (INSTALL ${SOURCE_PATH}/LICENSE.txt
 # Install usage
 configure_file (${CMAKE_CURRENT_LIST_DIR}/usage
   ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage @ONLY)
-
-# Post-build test for cmake libraries
-vcpkg_test_cmake(PACKAGE_NAME GeographicLib)
