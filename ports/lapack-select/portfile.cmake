8babd6610085cd696e490610b6150e04aff4db5d
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-07-17 16:07:52 +0200

    [lapack-select] add port lapack-select to select the lapack implementation

---
+++ ports/lapack-select/portfile.cmake
@@ -0,0 +1,14 @@
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+list(LENGTH FEATURES NUMBER_ELEMS)
+if(NUMBER_ELEMS GREATER 2)
+    message(FATAL_ERROR "Cannot select more than one LAPACK implementation!")
+    # Ports cannot depend on more than one LAPACK implementation. Always use Build-Depends: lapack-select[core]
+endif()
+if(NUMBER_ELEMS LESS 2)
+    message(FATAL_ERROR "Need to select at least one LAPACK implementation! Only using feature [core] is not supported")
+    # Ports should only depend on lapack-select[core] so that they don't implicitly depend on the selected default feature
+endif()
+
+# In the future this port might also be used to install a vcpkg-cmake-wrapper or 
+# a FindLAPACK module to make sure all dependent ports use the same LAPACK implementation
