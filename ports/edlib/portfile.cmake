8e320a1b4f88acca6f8409dbf29d653ff1579c05
Remi Bedard-Couture <remi.bc@outlook.com>
2020-07-19 15:58:40 -0400

    Fix cmake installation

--- ports/edlib/portfile.cmake
+++ ports/edlib/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF v1.2.6
     SHA512 75b470c1403113e5f0895b3c1bb4163e65c6e04ccf41a75297a5b4cc915a567567ebcc79f3b9ea74b5e7188adfab2eceda5ac75e2d861aef8b3fefc6d4f39200
     HEAD_REF master
+	PATCHES
+		fix-cmake-install.patch
 )
 
 vcpkg_configure_cmake(
@@ -14,6 +16,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/edlib)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
