e57089112d9e4df64d5740205ec4fe88d2b2ad94
Matt Oliver <protogonoi@gmail.com>
2020-07-20 19:54:17 +1000

    [opus] Use vcpkg_fixup_pkgconfig.

--- ports/opus/portfile.cmake
+++ ports/opus/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Opus)
 vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 file(INSTALL
      ${SOURCE_PATH}/COPYING
