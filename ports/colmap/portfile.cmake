40fff4c6d6bba966eeab64167697dd88753cd445
Pablo Speciale <Pablo.Speciale@microsoft.com>
2020-07-20 16:21:14 +0200

    Changes proposed from @ahojnne to handle the internal colmap variables `DGIT_COMMIT_ID` and `DGIT_COMMIT_DATE`

--- ports/colmap/portfile.cmake
+++ ports/colmap/portfile.cmake
@@ -1,10 +1,25 @@
+set(COLMAP_REF "3.6-dev.3")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO colmap/colmap
-    REF 3.6-dev.3
+    REF ${COLMAP_REF}
     SHA512 7eec27fced20f43547e67e9824d33c8412b17c2c80d29d8346d583cef3f7598b59c7c10a0556b857e31106c9312aace54c5dee65b8465974930f35b58331304a
     HEAD_REF dev
 )
+
+set(CUDA_ENABLED OFF)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    cuda CUDA_ENABLED
+)
+
+if(DEFINED VCPKG_HEAD_VERSION)
+    set(COLMAP_GIT_COMMIT_ID "${VCPKG_HEAD_VERSION}")
+else()
+    set(COLMAP_GIT_COMMIT_ID "${COLMAP_REF}")
+endif()
+
+string(TIMESTAMP COLMAP_GIT_COMMIT_DATE "%Y-%m-%d")
 
 if("cuda-redist" IN_LIST FEATURES)
     set(CUDA_ENABLED ON)
@@ -17,6 +32,8 @@ vcpkg_configure_cmake(
     OPTIONS
         -DCUDA_ENABLED=${CUDA_ENABLED}
         -DCUDA_ARCHS=${CUDA_ARCHS}
+        -DGIT_COMMIT_ID=${COLMAP_GIT_COMMIT_ID}
+        -DGIT_COMMIT_DATE=${COLMAP_GIT_COMMIT_DATE}
 )
 
 vcpkg_install_cmake()
