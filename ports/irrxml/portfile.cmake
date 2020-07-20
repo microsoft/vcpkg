d48999b3010682fa8e38f7cdbbe514fcf5fc2ba9
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-07-20 23:52:07 +0200

    [irrxml] fixes for windows

--- ports/irrxml/portfile.cmake
+++ ports/irrxml/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_sourceforge(
     REF 1.2
     FILENAME irrxml-1.2.zip
     SHA512 b6a7f76305c6d1e74a66bc182bd260428b9aa0b1db444f79de56095a7d39e320429756329202b44d3159f6b4d9ff13b7ebb6b88ca3d087f09c3a4a3a0ce08995
+    PATCHES
+        disable_asm_calls.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
