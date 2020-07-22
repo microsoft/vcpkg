5864b3b5d10c8b44f5a40dda12feb157f20afedb
wangli28 <wangli28@beyondsoft.com>
2020-07-22 08:10:36 +0000

    [evpp] Add vcpkg-cmake-wrapper.cmake and vcpkg.json

--- ports/evpp/portfile.cmake
+++ ports/evpp/portfile.cmake
@@ -25,5 +25,7 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
+configure_file(${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake @ONLY)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
