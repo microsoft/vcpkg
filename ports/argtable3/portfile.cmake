1d0e23ffe59bdd681cab35a93891ceaa5891cee2
PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>
2020-07-21 20:10:14 -0700

    Remove bin folder on static build

--- ports/argtable3/portfile.cmake
+++ ports/argtable3/portfile.cmake
@@ -37,4 +37,8 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     )
 endif()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
