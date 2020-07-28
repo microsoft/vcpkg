20b51cadd3a185376c06dd0c7bd5f7c6f6935296
JackBoosY <yuzaiyang@beyondsoft.com>
2020-07-27 23:23:33 -0700

    [qt5] change build method to vcpkg_execute_build_process

--- ports/qt5-connectivity/portfile.cmake
+++ ports/qt5-connectivity/portfile.cmake
@@ -1,5 +1,2 @@
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
-
-# Disable parallel build
-set(VCPKG_CONCURRENCY 1)
 qt_submodule_installation()
--- ports/qt5-location/portfile.cmake
+++ ports/qt5-location/portfile.cmake
@@ -1,6 +1,3 @@
 message(STATUS "${PORT} has a spurious failure in which it is unable to create a parent directory! Just retry.")
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
-
-# Disable parallel build
-set(VCPKG_CONCURRENCY 1)
 qt_submodule_installation(PATCHES clipper.patch)
