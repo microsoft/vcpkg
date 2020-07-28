8246dec49a62ebc1663c7cd41ffb86b2d5485d32
Nicole Mazzuca <mazzucan@outlook.com>
2020-07-28 09:38:14 -0700

    Add ContactCommand

--- toolsrc/src/vcpkg-test/commands.cpp
+++ toolsrc/src/vcpkg-test/commands.cpp
@@ -1,9 +1,11 @@
 #include <catch2/catch.hpp>
 
 #include <vcpkg/commands.h>
+#include <vcpkg/commands.contact.h>
 #include <vcpkg/commands.version.h>
 
 TEST_CASE ("test commands are constructible", "[commands]")
 {
-	vcpkg::Commands::Version::VersionCommand version{};
+    vcpkg::Commands::Contact::ContactCommand contact{};
+    vcpkg::Commands::Version::VersionCommand version{};
 }
