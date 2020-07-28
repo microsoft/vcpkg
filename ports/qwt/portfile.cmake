6b468b39390ddac91d4fc3e59910604577d2e5ce
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2020-07-27 19:39:42 -0700

    Update ports/qwt/portfile.cmake
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

--- ports/qwt/portfile.cmake
+++ ports/qwt/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_sourceforge(
     REPO qwt/qwt
     REF 6.1.4
     FILENAME "qwt-6.1.4.zip"
-    SHA512 711256ec4d1d54d201650e9c63b584b17a61dbedde5a581e0b6227fdbee149cdbe2a889aa58f67777125b6471fc1fe248992ec845eb6481a2dfc2c4276701d7f 
+    SHA512 711256ec4d1d54d201650e9c63b584b17a61dbedde5a581e0b6227fdbee149cdbe2a889aa58f67777125b6471fc1fe248992ec845eb6481a2dfc2c4276701d7f
     PATCHES fix-dynamic-static.patch
             build.patch
 )
