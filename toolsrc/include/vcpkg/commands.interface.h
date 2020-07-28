6291002cd98bfcdf48fcd2e576edcbe05fb902b7
Nicole Mazzuca <mazzucan@outlook.com>
2020-07-28 10:22:34 -0700

    Add TripletCommand & InstallCommand

--- toolsrc/include/vcpkg/commands.interface.h
+++ toolsrc/include/vcpkg/commands.interface.h
@@ -16,4 +16,10 @@ namespace vcpkg::Commands
         virtual void perform_and_exit(const VcpkgCmdArguments& args, Files::Filesystem& fs) const = 0;
         virtual ~BasicCommand() = default;
     };
+
+    struct TripletCommand
+    {
+        virtual void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths, Triplet default_triplet) const = 0;
+        virtual ~TripletCommand() = default;
+    };
 }
