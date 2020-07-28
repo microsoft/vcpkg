f948bb4c0ea9f53ba2812998390b93dbab6d8c74
Nicole Mazzuca <mazzucan@outlook.com>
2020-07-28 10:56:21 -0700

    Add tests for commands_type_a

--- toolsrc/src/vcpkg-test/commands.cpp
+++ toolsrc/src/vcpkg-test/commands.cpp
@@ -20,3 +20,22 @@ TEST_CASE ("get_available_commands_type_c works", "[commands]")
     CHECK(Commands::find("contact", commands_list) != nullptr);
     CHECK(Commands::find("aang", commands_list) == nullptr);
 }
+
+TEST_CASE ("get_available_commands_type_a works", "[commands]")
+{
+    auto commands_list = Commands::get_available_commands_type_a();
+    CHECK(commands_list.size() == 10);
+
+    CHECK(Commands::find("install", commands_list) != nullptr);
+    CHECK(Commands::find("x-set-installed", commands_list) != nullptr);
+    CHECK(Commands::find("ci", commands_list) != nullptr);
+    CHECK(Commands::find("remove", commands_list) != nullptr);
+    CHECK(Commands::find("upgrade", commands_list) != nullptr);
+    CHECK(Commands::find("build", commands_list) != nullptr);
+    CHECK(Commands::find("env", commands_list) != nullptr);
+    CHECK(Commands::find("build-external", commands_list) != nullptr);
+    CHECK(Commands::find("export", commands_list) != nullptr);
+    CHECK(Commands::find("depend-info", commands_list) != nullptr);
+
+    CHECK(Commands::find("mai", commands_list) == nullptr);
+}
