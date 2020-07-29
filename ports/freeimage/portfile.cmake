44df21a44213cc1364b81e8d399723fd4c83266b
wangli28 <wangli28@beyondsoft.com>
2020-07-29 06:04:11 +0000

    [freeimage] Add vcpkg-cmake-wrapper.cmake

--- ports/freeimage/portfile.cmake
+++ ports/freeimage/portfile.cmake
@@ -48,5 +48,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
-file(INSTALL ${SOURCE_PATH}/license-fi.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
+configure_file(${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake @ONLY)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL ${SOURCE_PATH}/license-fi.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
