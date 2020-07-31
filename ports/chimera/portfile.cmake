707800c6ca9a2c1bb73f98da79659d03953497f7
JackBoosY <yuzaiyang@beyondsoft.com>
2020-07-31 00:13:48 -0700

    [chimera] Fix unix build

--- ports/chimera/portfile.cmake
+++ ports/chimera/portfile.cmake
@@ -7,7 +7,9 @@ vcpkg_from_github(
     REF c00683d73916e39f01b0d418f686c8b5c379159c
     SHA512 3c4d52706901acc9ef4c3d12b0e5b2956f4e6bce13f6828a4ba3b736c05ffacb1d733ef9c226988ca80220584525f9cb6dcfe4914ced6cc34ae6a0a45975afb5
     HEAD_REF master
-    PATCHES fix-cmakelists.patch
+    PATCHES
+        fix-cmakelists.patch
+        fix-unix-build.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
