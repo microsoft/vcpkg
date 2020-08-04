b9caf550e07fe4370ed16d5cd9857526acea95c5
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2020-08-04 02:14:34 -0700

    Update ports/libmariadb/portfile.cmake

--- ports/libmariadb/portfile.cmake
+++ ports/libmariadb/portfile.cmake
@@ -33,7 +33,7 @@ vcpkg_configure_cmake(
         -DWITH_UNITTEST=OFF
         -DWITH_SSL=${WITH_SSL}
         -DWITH_CURL=OFF
-		-DWITH_EXTERNAL_ZLIB=${WITH_ZLIB}
+        -DWITH_EXTERNAL_ZLIB=${WITH_ZLIB}
 )
 
 vcpkg_install_cmake()
