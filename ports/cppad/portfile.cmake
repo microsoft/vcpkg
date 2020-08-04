3edc2aa5a44217401edbfaf9d359bd15b2fc7ebf
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-08-04 12:12:08 +0200

    [CppAD] add DISABLE_PARALLEL_CONFIGURE as option of vcpkg_configure_cmake() command

--- ports/cppad/portfile.cmake
+++ ports/cppad/portfile.cmake
@@ -16,6 +16,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -Dcppad_prefix=${CURRENT_PACKAGES_DIR}
     OPTIONS_RELEASE
