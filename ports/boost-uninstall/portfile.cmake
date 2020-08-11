3c750bba88f4abf0fb9c3000665aa57c2100e0e3
JackBoosY <yuzaiyang@beyondsoft.com>
2020-08-10 19:58:17 -0700

    [boost-uninstall] Add notice message

--- ports/boost-uninstall/portfile.cmake
+++ ports/boost-uninstall/portfile.cmake
@@ -1,1 +1,4 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+message(STATUS "\nPlease using the following command when you need to remove all boost ports/components:\n\
+    \"./vcpkg remove boost-uninstall --recurse\"\n")
