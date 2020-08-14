278305ebab840304fd9db098075c899c2f5204ee
root <root@ci.lxd>
2020-08-14 16:11:43 +0000

    update_mupdf

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -5,9 +5,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO simon987/mupdf
-    REF 75f6261fc5769470249ec01f40447da69ed6a68b # simon987/release
+    REF  e8e4cd42074cfe7e399b029ce475eed8b6d46159# simon987/release
     SHA512 13cfb7e4dbfd392c02cef89cc554810bf01689fe23713bba718fd21ea662d7615f0eeacbd0738b9ac6370428b3e1323f286d1f6cfc23f7e089e710d435e44af5
-    HEAD_REF master
+    HEAD_REF release
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
