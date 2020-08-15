dd6d89f51c8eb4e8332c6ff3b28c6810ea4bd290
jgehw <Joachim_Gehweiler@McAfee.com>
2020-08-15 23:02:46 +0200

    fix execute process macro The existing implementation totally screwed up commands if the command's arguments contained semicolons (this is the case, e.g., in the FindPython modules of the cmake distribution).

--- scripts/cmake/execute_process.cmake
+++ scripts/cmake/execute_process.cmake
@@ -13,8 +13,45 @@ if (NOT DEFINED OVERRIDEN_EXECUTE_PROCESS)
       message(FATAL_ERROR "This command cannot be executed in Download Mode.\nHalting portfile execution.\n")
     endmacro()
   else()
-    macro(execute_process)
-      _execute_process(${ARGV})
-    endmacro()
+    function(execute_process)
+      # check that COMMAND is not supplied more than once; if there's ever a use case for this, the implementation below needs to be extended
+      set(command_count 0)
+      foreach(arg ${ARGV})
+        if(arg STREQUAL COMMAND)
+          math(EXPR command_count "${command_count} + 1")
+        endif()
+      endforeach()
+      if(NOT command_count EQUAL 1)
+        message(FATAL_ERROR "Overriden execute_process() function only supports 1 COMMAND parameter.\n")
+      endif()
+
+      # parse parameters such that semicolons in options arguments to COMMAND don't get erased
+      cmake_parse_arguments(PARSE_ARGV 0 overriden_execute_process
+                            "OUTPUT_QUIET;ERROR_QUIET;OUTPUT_STRIP_TRAILING_WHITESPACE;ERROR_STRIP_TRAILING_WHITESPACE"
+                            "WORKING_DIRECTORY;TIMEOUT;RESULT_VARIABLE;RESULTS_VARIABLE;OUTPUT_VARIABLE;ERROR_VARIABLE;INPUT_FILE;OUTPUT_FILE;ERROR_FILE;ENCODING"
+                            "COMMAND")
+
+      # collect all other present parameters
+      set(other_args "")
+      foreach(arg OUTPUT_QUIET ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_STRIP_TRAILING_WHITESPACE)
+        if(overriden_execute_process_${arg})
+          list(APPEND other_args ${arg})
+        endif()
+      endforeach()
+      foreach(arg WORKING_DIRECTORY TIMEOUT RESULT_VARIABLE RESULTS_VARIABLE OUTPUT_VARIABLE ERROR_VARIABLE INPUT_FILE OUTPUT_FILE ERROR_FILE ENCODING)
+        if(overriden_execute_process_${arg})
+          list(APPEND other_args ${arg} ${overriden_execute_process_${arg}})
+        endif()
+      endforeach()
+
+      _execute_process(COMMAND ${overriden_execute_process_COMMAND} ${other_args})
+
+      # pass output parameters back to caller's scope
+      foreach(arg RESULT_VARIABLE RESULTS_VARIABLE OUTPUT_VARIABLE ERROR_VARIABLE)
+        if(overriden_execute_process_${arg})
+          set(${overriden_execute_process_${arg}} ${${overriden_execute_process_${arg}}} PARENT_SCOPE)
+        endif()
+      endforeach()
+    endfunction()
   endif()
 endif()
