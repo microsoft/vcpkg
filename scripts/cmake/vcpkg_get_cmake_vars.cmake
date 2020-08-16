b5f28f723714d1d2f951fffbdc1d1be44a71cbb1
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-08-16 14:41:27 +0200

    fine tuning.

--- scripts/cmake/vcpkg_get_cmake_vars.cmake
+++ scripts/cmake/vcpkg_get_cmake_vars.cmake
@@ -26,31 +26,36 @@
 ## * [libimobiledevice](https://github.com/Microsoft/vcpkg/blob/master/ports/libimobiledevice/portfile.cmake)
 
 function(vcpkg_get_cmake_vars)
-    cmake_parse_arguments(PARSE_ARGV 0 _gcv "OUTPUT_VAR" "OPTIONS" ${ARGN})
+    cmake_parse_arguments(PARSE_ARGV 0 _gcv "" "OUTPUT_FILE" "OPTIONS")
 
     if(_gcv_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "vcpkg_get_cmake_vars was passed unparsed arguments: '${_gcv_UNPARSED_ARGUMENTS}'")
     endif()
 
-    if(NOT _gcv_OUTPUT_VAR)
-        message(FATAL_ERROR "vcpkg_get_cmake_vars requires parameter OUTPUT_VAR!")
+    if(NOT _gcv_OUTPUT_FILE)
+        message(FATAL_ERROR "vcpkg_get_cmake_vars requires parameter OUTPUT_FILE!")
     endif()
 
-    if(${_gcv_OUTPUT_VAR})
-        list(APPEND "-DVCPKG_OUTPUT_FILE=${${_gcv_OUTPUT_VAR}}")
+    if(${_gcv_OUTPUT_FILE})
+        debug_message("OUTPUT_FILE ${${_gcv_OUTPUT_FILE}}")
+        list(APPEND _gcv_OPTIONS "-DVCPKG_OUTPUT_FILE=${${_gcv_OUTPUT_FILE}}")
     else()
         set(DEFAULT_OUT "${CURRENT_BUILDTREES_DIR}/cmake-vars-${TARGET_TRIPLET}.cmake.log") # So that the file gets included in CI artifacts.
-        list(APPEND "-DVCPKG_OUTPUT_FILE=${CURRENT_BUILDTREES_DIR}/${DEFAULT_OUT}")
-        set(${_gcv_OUTPUT_VAR} ${DEFAULT_OUT} PARENT_SCOPE)
+        list(APPEND _gcv_OPTIONS "-DVCPKG_OUTPUT_FILE:PATH=${DEFAULT_OUT}")
+        set(${_gcv_OUTPUT_FILE} "${DEFAULT_OUT}" PARENT_SCOPE)
     endif()
 
     set(VCPKG_BUILD_TYPE release)
     vcpkg_configure_cmake(
-        SOURCE_PATH ${CURRENT_PORT_DIR}
+        SOURCE_PATH "${SCRIPTS}/get_cmake_vars"
         OPTIONS ${_gcv_OPTIONS}
         PREFER_NINJA
     )
 
+    file(REMOVE "${CURRENT_BUILDTREES_DIR}/get-cmake-vars-${TARGET_TRIPLET}-out.log")
+    file(REMOVE "${CURRENT_BUILDTREES_DIR}/get-cmake-vars-${TARGET_TRIPLET}-err.log")
+    file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-get-cmake-vars")
     file(RENAME "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-get-cmake-vars")
-    file(RENAME "${CURRENT_BUILDTREES_DIR}/config-${TARGET_TRIPLET}-rel.log" "${CURRENT_BUILDTREES_DIR}/get-cmake-vars-${TARGET_TRIPLET}.log")
+    file(RENAME "${CURRENT_BUILDTREES_DIR}/config-${TARGET_TRIPLET}-out.log" "${CURRENT_BUILDTREES_DIR}/get-cmake-vars-${TARGET_TRIPLET}-out.log")
+    file(RENAME "${CURRENT_BUILDTREES_DIR}/config-${TARGET_TRIPLET}-err.log" "${CURRENT_BUILDTREES_DIR}/get-cmake-vars-${TARGET_TRIPLET}-err.log")
 endfunction()
