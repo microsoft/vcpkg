3d43435d722ab28c6c2749f560d498d14d093af1
myd7349 <myd7349@gmail.com>
2020-08-19 09:42:24 +0800

    [numcpp] Fix linux build

--- ports/numcpp/portfile.cmake
+++ ports/numcpp/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/NumCpp/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/NumCpp/cmake TARGET_PATH share/NumCpp)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
