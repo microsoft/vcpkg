a66b922b8eca2bca369d4b4a12f518549f2d69cb
Billy Robert O'Neal III <bion@microsoft.com>
2020-08-21 09:52:56 -0700

    Remove redundant capture group, add missing $?| escapes, use more descriptive variable name.

--- scripts/cmake/vcpkg_escape_regex_control_characters.cmake
+++ scripts/cmake/vcpkg_escape_regex_control_characters.cmake
@@ -1,4 +1,4 @@
 function(vcpkg_escape_regex_control_characters out_var string_with_regex_characters)
-  string(REGEX REPLACE "([][+.*()^\\])" "\\\\\\1" _vercc_out "${string_with_regex_characters}")
-  set(${out_var} "${_vercc_out}" PARENT_SCOPE)
+  string(REGEX REPLACE "[][+.*()^\\$?|]" "\\\\\\0" _escaped_content "${string_with_regex_characters}")
+  set(${out_var} "${_escaped_content}" PARENT_SCOPE)
 endfunction()
