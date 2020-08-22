aa47a35d038dcbf9ae4237b44d0153f8cb0f6cfa
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-08-22 21:26:18 +0200

    fix mpfr regression on !windows

--- ports/mpfr/portfile.cmake
+++ ports/mpfr/portfile.cmake
@@ -11,7 +11,9 @@ vcpkg_extract_source_archive_ex(
     PATCHES gmpd.patch
             dll.patch
 )
-vcpkg_acquire_msys(MSYS_ROOT PACKAGES texinfo)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_acquire_msys(MSYS_ROOT PACKAGES texinfo)
+endif()
 file(REMOVE_RECURSE "${SOURCE_PATH}/m4")
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
