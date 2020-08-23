28ec78fe88a74ed7386f2709c8b0b4d3db152742
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-08-23 23:26:15 +0200

    try actual fixing io2d

--- ports/io2d/portfile.cmake
+++ ports/io2d/portfile.cmake
@@ -6,19 +6,17 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cpp-io2d/P0267_RefImpl
-    REF add3c9792dcd3f08c497ae3adafb2a3b5b5fc338
-    SHA512 2727342fbb31523583374ab6df6ff7542e80b4f94319cf0f293e8c085711fa10ed312b4fc4b91391112b5e27eaaae519cb4141ea9d4108ffb5b7383a043b38b8
+    REF caa0ba0cb5a421a38bc26afaf3505bee206c44dd
+    SHA512 f8e5a708f6cbda913a0492a843e1502b8d3cc615a6abda50e850be944e1484ec9087b787c54cc25d513172a7d5ab789be41a761c97df94266df4d1bcf14db17c
     HEAD_REF master
     PATCHES
-        fix-linux-build.patch
-        Fix-FindCairo.patch
-        fix-expat.patch
+        cmake.dep.patch
 )
 
 if (VCPKG_TARGET_IS_OSX)
     set(IO2D_DEFAULT_OPTION "-DIO2D_DEFAULT=COREGRAPHICS_MAC")
 endif()
-
+vcpkg_find_acquire_program(PKGCONFIG)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
@@ -27,6 +25,7 @@ vcpkg_configure_cmake(
         -DIO2D_WITHOUT_TESTS=1
         -DCMAKE_INSTALL_INCLUDEDIR:STRING=include
         ${IO2D_DEFAULT_OPTION}
+        #"-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
 )
 
 vcpkg_install_cmake()
@@ -35,15 +34,15 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/io2d)
 
-if (NOT VCPKG_TARGET_IS_OSX)
-    file(RENAME ${CURRENT_PACKAGES_DIR}/share/io2d/io2dConfig.cmake ${CURRENT_PACKAGES_DIR}/share/io2d/io2dTargets.cmake)
-    file(WRITE ${CURRENT_PACKAGES_DIR}/share/io2d/io2dConfig.cmake "
-    include(CMakeFindDependencyMacro)
-    find_dependency(unofficial-cairo CONFIG)
-    find_dependency(unofficial-graphicsmagick CONFIG)
+# There probably nees to be more fixed in the config.... Would be better if this port would be using targets for ext. deps. 
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/io2d/io2dConfig.cmake ${CURRENT_PACKAGES_DIR}/share/io2d/io2dTargets.cmake)
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/io2d/io2dConfig.cmake "
+include(CMakeFindDependencyMacro)
+find_dependency(unofficial-cairo CONFIG)
+find_dependency(unofficial-graphicsmagick CONFIG)
 
-    include(\${CMAKE_CURRENT_LIST_DIR}/io2dTargets.cmake)
-    ")
-endif()
+include(\${CMAKE_CURRENT_LIST_DIR}/io2dTargets.cmake)
+")
+
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
