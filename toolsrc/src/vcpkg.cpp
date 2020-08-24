e03e8047028f2cb4dd66f4fe5e4248254d888a33
JackBoosY <yuzaiyang@beyondsoft.com>
2020-08-24 00:28:18 -0700

    check error message

--- toolsrc/src/vcpkg.cpp
+++ toolsrc/src/vcpkg.cpp
@@ -6,6 +6,7 @@
 #include <vcpkg/base/strings.h>
 #include <vcpkg/base/system.debug.h>
 #include <vcpkg/base/system.print.h>
+
 #include <vcpkg/base/system.process.h>
 
 #include <vcpkg/commands.contact.h>
