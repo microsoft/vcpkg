352ca7e7eb69b265b457bc79752631a143e85292
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-08-24 15:32:04 +0200

    [qhull] use 2020.2 prerelease version

--- ports/qhull/portfile.cmake
+++ ports/qhull/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO qhull/qhull
-    REF e8bcf2fafd7938205e4e5b8b9b921cb7609ff394
-    SHA512 c7028a1d2d4d3b8c2d143610c75073c39ce404154d41e2da6e11484e9bcc517f7e770dbad9572c708d87cbd1a0462c915d1d5bdf95e2f0359e9839681334e7bd
+    REF f7aff465101711ae4cee3f501a528d6d53e75185 #2020.2 Prerelease (8.0.1 + PR#76)
+    SHA512 51e3fbf05c8d65b4c0bd9b5c88745abccd6a1fadd685b13b2cb9fd7e0c13c10d75ee7e79adf99567a31172839e90f7886fb0fea860df311546c168bc1f1582d5
     HEAD_REF master
     PATCHES
         mac-fix.patch
@@ -27,8 +27,6 @@ vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Qhull)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(GLOB_RECURSE HTMFILES ${CURRENT_PACKAGES_DIR}/include/*.htm)
-file(REMOVE ${HTMFILES})
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/doc)
 
 vcpkg_copy_tools(TOOL_NAMES
