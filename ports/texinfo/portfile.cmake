6398496fbc28da190fcc021f9d3b13f1794f810c
Vitaly Petrov <v31337@gmail.com>
2020-08-26 10:17:50 +0300

    Added missing dependency

--- ports/texinfo/portfile.cmake
+++ ports/texinfo/portfile.cmake
@@ -1,10 +1,12 @@
 vcpkg_fail_port_install(ON_TARGET "OSX" "Windows" "UWP")
 
-set(TARGET_VERSION 2.34)
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+set(TARGET_VERSION 6.7)
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://ftp.gnu.org/gnu/binutils/binutils-${TARGET_VERSION}.tar.xz"
+    URLS "https://ftp.gnu.org/gnu/texinfo/texinfo-${TARGET_VERSION}.tar.xz"
     FILENAME "binutils-${TARGET_VERSION}.tar.xz"
-    SHA512 2c7976939dcf5e8c5b7374cccd39bfe803b1bec73c6abfa0eb17c24e1942574c6bdb874c66a092a82adc443182eacd8a5a8001c19a76101f0c7ba40c27de0bbd
+    SHA512 da55a0d0a760914386393c5e8e864540265d8550dc576f784781a6d72501918e8afce716ff343e5c2a0ce09cf921bfaf0a48ecb49f6182a7d10e920ae3ea17e7
 )
 
 vcpkg_extract_source_archive_ex(
@@ -13,16 +15,13 @@ vcpkg_extract_source_archive_ex(
     REF ${TARGET_VERSION}
 )
 
-set(ENV{C_INCLUDE_PATH} "/")
-set(ENV{MAKEINFO} "${CURRENT_INSTALLED_DIR}/tools/texinfo/bin/makeinfo")
-
 vcpkg_configure_make(
-    AUTOCONFIG
+    #AUTOCONFIG
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS --disable-nls --disable-werror --disable-docs
 )
 
 vcpkg_install_make()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
