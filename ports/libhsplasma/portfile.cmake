d8193cdad7f9d13e75ca01d7de32c0a770c106d1
Adam Johnson <AdamJohnso@gmail.com>
2020-08-26 20:44:29 -0400

    [libhsplasma] Add new port.

---
+++ ports/libhsplasma/portfile.cmake
@@ -0,0 +1,39 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO H-uru/libhsplasma
+    REF afd9c46fa72afbbaf5260800ecfde0704ba2a475
+    SHA512 21774b465ede7fe5e44df4470ed6867977ac9249400ba6d0bed33fd0e856d467de1afe79afb8a4dc6145eef21c5a3f79590b5cfa3ea1337cc355ca8f3bf1a11c
+    HEAD_REF master
+)
+
+if(net IN_LIST FEATURES)
+    set(HSPLASMA_NET ON)
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DENABLE_NET=${HSPLASMA_NET}
+        -DENABLE_PHYSX=OFF
+        -DENABLE_PYTHON=OFF
+        -DENABLE_TOOLS=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(
+    CONFIG_PATH share/cmake/HSPlasma
+    TARGET_PATH share/HSPlasma
+)
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+vcpkg_test_cmake(HSPlasma)
+if(HSPLASMA_NET)
+    vcpkg_test_cmake(HSPlasmaNet)
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
