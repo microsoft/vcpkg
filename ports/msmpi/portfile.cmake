75012ccec14c2872f5a73c9e8b3056ebe6543e36
Albert Ziegenhagel <albert.ziegenhagel@outlook.com>
2020-08-29 12:52:35 +0200

    [msmpi] build from source

--- ports/msmpi/portfile.cmake
+++ ports/msmpi/portfile.cmake
@@ -1,127 +1,115 @@
 vcpkg_fail_port_install(ON_TARGET "Linux" "OSX" "UWP")
 
-set(MSMPI_VERSION "10.1.12498")
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/msmpi-${MSMPI_VERSION})
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO microsoft/Microsoft-MPI
+    REF v10.1.1
+    SHA512 c5aef7c15e815dab22a46bdc7ad14fea20e6ed4324f560c3d9df2dd236338b282ec2d4a45522eb04801e3733a0d3db8017ce0ed9f18c3844a452c182296b9e59
+    HEAD_REF master
+    PATCHES
+        # PlatformToolset and WindowsTargetPlatformVersion are explicitly set by `vcpkg_install_msbuild`
+        # and VCToolsVersion is automatically selected
+        no-toolsversion.patch
 
-vcpkg_download_distfile(SDK_ARCHIVE
-    URLS "https://download.microsoft.com/download/a/5/2/a5207ca5-1203-491a-8fb8-906fd68ae623/msmpisdk.msi"
-    FILENAME "msmpisdk-${MSMPI_VERSION}.msi"
-    SHA512 330fad53c1979dfae786abed228d82c82207f0d61148e2efc1f37c8931838b806eb50554e7f006a56962f748a21f7017c540bd70444b7a93a72c313a64e9254c
+        # Some symbols referenced from Basestd.h end up unresolved, so we just use the macro version
+        # of them directly.
+        fix-external-symbols.patch
+
+        # Disable building with CFG enabled to make it usable from gfortran.
+        # See https://github.com/microsoft/Microsoft-MPI/issues/7
+        disable-control-flow-guard.patch
+
+        # mpif.h uses invalid BOZ integer constants, which will not be accepted without `-fallow-invalid-boz`
+        # by gfortran >= 10.0, so we convert them to regular integer constants.
+        fix-invalid-boz-literals.patch
 )
 
+# Replace CBT project by packages.config
+# See https://github.com/CommonBuildToolset/CBT.Modules/issues/292
+file(REMOVE "${SOURCE_PATH}/.build/Local/CBTModules/CBTModules.proj")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/packages.config" DESTINATION "${SOURCE_PATH}/.build/Local/CBTModules/")
 
-#to enable CI, you should modify the following URL also in ${VCPKG_ROOT}/scripts/azure-pipelines/windows/provision-image.ps1
-macro(download_msmpi_redistributable_package)
-    vcpkg_download_distfile(REDIST_ARCHIVE
-        URLS "https://download.microsoft.com/download/a/5/2/a5207ca5-1203-491a-8fb8-906fd68ae623/msmpisetup.exe"
-        FILENAME "msmpisetup-${MSMPI_VERSION}.exe"
-        SHA512 1ee463e7dfc3e55a7ac048fdfde13fef09a5eea4b74d8fd7c22a7aad667a025b467ce939e5de308e25bbc186c3fe66e0e24ac03a3741656fc7558f2af2fa132a
-    )
-endmacro()
-
-### Check for correct version of installed redistributable package
-
-# We always want the ProgramFiles folder even on a 64-bit machine (not the ProgramFilesx86 folder)
-vcpkg_get_program_files_platform_bitness(PROGRAM_FILES_PLATFORM_BITNESS)
-set(SYSTEM_MPIEXEC_FILEPATH "${PROGRAM_FILES_PLATFORM_BITNESS}/Microsoft MPI/Bin/mpiexec.exe")
-
-if(EXISTS "${SYSTEM_MPIEXEC_FILEPATH}")
-    set(MPIEXEC_VERSION_LOGNAME "mpiexec-version")
-    vcpkg_execute_required_process(
-        COMMAND ${SYSTEM_MPIEXEC_FILEPATH}
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-        LOGNAME ${MPIEXEC_VERSION_LOGNAME}
-    )
-    file(READ ${CURRENT_BUILDTREES_DIR}/${MPIEXEC_VERSION_LOGNAME}-out.log MPIEXEC_OUTPUT)
-
-    if(MPIEXEC_OUTPUT MATCHES "\\[Version ([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+\\]")
-        if(NOT CMAKE_MATCH_1 STREQUAL MSMPI_VERSION)
-            download_msmpi_redistributable_package()
-
-            message(FATAL_ERROR
-                "  The version of the installed MSMPI redistributable packages does not match the version to be installed\n"
-                "    Expected version: ${MSMPI_VERSION}\n"
-                "    Found version: ${CMAKE_MATCH_1}\n"
-                "  Please upgrade the installed version on your system.\n"
-                "  The appropriate installer for the expected version has been downloaded to:\n"
-                "    ${REDIST_ARCHIVE}\n")
-        endif()
-    else()
-        message(FATAL_ERROR
-            "  Could not determine installed MSMPI redistributable package version.\n"
-            "  See logs for more information:\n"
-            "    ${CURRENT_BUILDTREES_DIR}\\${MPIEXEC_VERSION_LOGNAME}-out.log\n"
-            "    ${CURRENT_BUILDTREES_DIR}\\${MPIEXEC_VERSION_LOGNAME}-err.log\n")
-    endif()
+# Acquire gfortran
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(MINGW_PATH mingw32)
+    set(MSYS_TARGET i686)
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(MINGW_PATH mingw64)
+    set(MSYS_TARGET x86_64)
 else()
-    download_msmpi_redistributable_package()
-
-    message(FATAL_ERROR
-        "  Could not find:\n"
-        "    ${SYSTEM_MPIEXEC_FILEPATH}\n"
-        "  Please install the MSMPI redistributable package before trying to install this port.\n"
-        "  The appropriate installer has been downloaded to:\n"
-        "    ${REDIST_ARCHIVE}\n")
+    message(FATAL_ERROR "Unknown architecture '${VCPKG_TARGET_ARCHITECTURE}' for MinGW Fortran build!")
 endif()
 
-file(TO_NATIVE_PATH "${SDK_ARCHIVE}" SDK_ARCHIVE)
-file(TO_NATIVE_PATH "${SOURCE_PATH}/sdk" SDK_SOURCE_DIR)
-file(TO_NATIVE_PATH "${CURRENT_BUILDTREES_DIR}/msiexec-${TARGET_TRIPLET}.log" MSIEXEC_LOG_PATH)
+vcpkg_acquire_msys(MSYS_ROOT PACKAGES "mingw-w64-${MSYS_TARGET}-gcc-fortran")
+set(MINGW_BIN "${MSYS_ROOT}/${MINGW_PATH}/bin")
+vcpkg_add_to_path(PREPEND "${MINGW_BIN}")
 
-set(PARAM_MSI "/a \"${SDK_ARCHIVE}\"")
-set(PARAM_LOG "/log \"${MSIEXEC_LOG_PATH}\"")
-set(PARAM_TARGET_DIR "TARGETDIR=\"${SDK_SOURCE_DIR}\"")
-set(SCRIPT_FILE ${CURRENT_BUILDTREES_DIR}/msiextract-msmpi.bat)
-# Write the command out to a script file and run that to avoid weird escaping behavior when spaces are present
-file(WRITE ${SCRIPT_FILE} "msiexec ${PARAM_MSI} /qn ${PARAM_LOG} ${PARAM_TARGET_DIR}")
+# Acquire Perl
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_PATH})
 
+# Force a NuGet restore
+message(STATUS "Performing nuget restore")
 vcpkg_execute_required_process(
-    COMMAND ${SCRIPT_FILE}
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-    LOGNAME extract-sdk
+    COMMAND msbuild "/t:Clean" "/restore" # We use the "Clean" target to make sure nothing gets build here
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    LOGNAME nuget-restore
 )
 
-set(SOURCE_INCLUDE_PATH "${SOURCE_PATH}/sdk/PFiles/Microsoft SDKs/MPI/Include")
-set(SOURCE_LIB_PATH "${SOURCE_PATH}/sdk/PFiles/Microsoft SDKs/MPI/Lib")
-
-# Install include files
-file(INSTALL
-        "${SOURCE_INCLUDE_PATH}/mpi.h"
-        "${SOURCE_INCLUDE_PATH}/mpif.h"
-        "${SOURCE_INCLUDE_PATH}/mpi.f90"
-        "${SOURCE_INCLUDE_PATH}/mpio.h"
-        "${SOURCE_INCLUDE_PATH}/mspms.h"
-        "${SOURCE_INCLUDE_PATH}/pmidbg.h"
-        "${SOURCE_INCLUDE_PATH}/${TRIPLET_SYSTEM_ARCH}/mpifptr.h"
-    DESTINATION
-        ${CURRENT_PACKAGES_DIR}/include
-)
-
-# NOTE: since the binary distribution does not include any debug libraries we always install the release libraries
-SET(VCPKG_POLICY_ONLY_RELEASE_CRT enabled)
-
-file(GLOB STATIC_LIBS
-    ${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpifec.lib
-    ${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpifmc.lib
-    ${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpifes.lib
-    ${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpifms.lib
-)
-
-file(INSTALL
-        "${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpi.lib"
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(INSTALL
-        "${SOURCE_LIB_PATH}/${TRIPLET_SYSTEM_ARCH}/msmpi.lib"
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-
-if(VCPKG_CRT_LINKAGE STREQUAL "static")
-    file(INSTALL ${STATIC_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(INSTALL ${STATIC_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+# Build driver projects by devenv instead of msbuild in advance.
+# This makes sure that the message compiler (mc.exe) tasks get executed correctly
+if(TRIPLET_SYSTEM_ARCH MATCHES "x86")
+    set(MSBUILD_PLATFORM "Win32")
+else ()
+    set(MSBUILD_PLATFORM ${TRIPLET_SYSTEM_ARCH})
 endif()
 
-# Handle copyright
-file(COPY "${SOURCE_PATH}/sdk/PFiles/Microsoft SDKs/MPI/License/MicrosoftMPI-SDK-EULA.rtf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/msmpi)
-file(COPY "${SOURCE_PATH}/sdk/PFiles/Microsoft SDKs/MPI/License/MPI-SDK-TPN.txt" DESTINATION ${CURRENT_PACKAGES_DIR}/share/msmpi)
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/msmpi/copyright "See the accompanying MicrosoftMPI-SDK-EULA.rtf and MPI-SDK-TPN.txt")
+set(CONFIGURATIONS)
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+    list(APPEND CONFIGURATIONS Release)
+endif()
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    list(APPEND CONFIGURATIONS Debug)
+endif()
+
+set(DRIVER_PROJECTS traceManifest msmpiLaunchSvcMc)
+
+foreach(PROJECT IN LISTS DRIVER_PROJECTS)
+    foreach(CONFIGURATION IN LISTS CONFIGURATIONS)
+        message(STATUS "Building ${PROJECT} for ${CONFIGURATION}")
+        vcpkg_execute_required_process(
+            COMMAND devenv "./src/msmpi.sln" /Build "${CONFIGURATION}|${MSBUILD_PLATFORM}" /Project "${PROJECT}"
+            WORKING_DIRECTORY ${SOURCE_PATH}
+            LOGNAME build-driver-project-${PROJECT}-${CONFIGURATION}-${TARGET_TRIPLET}
+        )
+    endforeach()
+endforeach()
+
+# Build the project
+list(GET CONFIGURATIONS 0 HEADER_CONFIGURATION)
+vcpkg_install_msbuild(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH .
+    BINARIES_SUBPATH out/*/bin
+    LICENSE_SUBPATH LICENSE.txt
+    SKIP_CLEAN
+    OPTIONS
+      "/p:GFORTRAN_BIN=${MINGW_BIN}"
+)
+
+# The headers to install are located in the build directories
+get_filename_component(SOURCE_PATH_SUFFIX "${SOURCE_PATH}" NAME)
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+    set(INCLUDES_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${SOURCE_PATH_SUFFIX}/out/Release-${MSBUILD_PLATFORM}/bin/sdk/inc)
+elseif(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    set(INCLUDES_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-deb/${SOURCE_PATH_SUFFIX}/out/Debug-${MSBUILD_PLATFORM}/bin/sdk/inc)
+endif()
+
+file(COPY ${INCLUDES_DIR}/${TRIPLET_SYSTEM_ARCH}/mpifptr.h DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
+file(REMOVE_RECURSE ${INCLUDES_DIR}/x64)
+file(REMOVE_RECURSE ${INCLUDES_DIR}/x86)
+
+file(COPY ${INCLUDES_DIR}/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
+
+vcpkg_clean_msbuild()
