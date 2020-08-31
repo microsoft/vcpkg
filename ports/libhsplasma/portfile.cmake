fac2a96f5c96f2c15cec6a4c9526bfe1641962e4
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2020-08-30 19:40:21 -0700

    Update ports/libhsplasma/portfile.cmake

--- ports/libhsplasma/portfile.cmake
+++ ports/libhsplasma/portfile.cmake
@@ -31,10 +31,6 @@ vcpkg_fixup_cmake_targets(
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-vcpkg_test_cmake(HSPlasma)
-if(HSPLASMA_NET)
-    vcpkg_test_cmake(HSPlasmaNet)
-endif()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
