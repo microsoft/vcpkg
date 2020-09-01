f5843c49a69858a8b6f44c834eeda06d7e2d33d0
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-01 00:25:32 -0700

    Reduce include files

--- toolsrc/include/vcpkg/commands.mirror.h
+++ toolsrc/include/vcpkg/commands.mirror.h
@@ -1,19 +1,9 @@
 #pragma once
 
-#include <vcpkg/base/chrono.h>
-
-#include <vcpkg/binaryparagraph.h>
 #include <vcpkg/build.h>
-#include <vcpkg/dependencies.h>
-#include <vcpkg/vcpkgcmdarguments.h>
-#include <vcpkg/vcpkgpaths.h>
-
-#include <vector>
 
 namespace vcpkg::Commands::Mirror
 {
-    extern const CommandStructure COMMAND_STRUCTURE;
-
     void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths, Triplet default_triplet);
 
     struct MirrorCommand : Commands::TripletCommand
