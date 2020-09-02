324022c59e18da433b190df7d5c0e2a5c147ec87
Lily <47812810+LilyWangL@users.noreply.github.com>
2020-09-02 10:05:43 +0800

    Update portfile.cmake

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -1,5 +1,3 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GNOME/libxml2
@@ -31,5 +29,5 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage
 vcpkg_copy_pdbs()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/libxml2)
+    file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 endif()
