1da9b92de254c2abe1cbf8d9b34660f3afc513fd
jgehw <kontakt@joachim-gehweiler.de>
2020-09-03 16:49:01 +0200

    fix linux postbuild script

--- ports/tensorflow-cc/convert_lib_params_linux.py
+++ ports/tensorflow-cc/convert_lib_params_linux.py
@@ -1,9 +1,12 @@
 for sub in ("cc", "framework"):
     with open("libtensorflow_" + sub + ".so.2.3.0-2.params", "r") as f_in:
         with open("libtensorflow_" + sub + ".a.2.3.0-2.params", "w") as f_out:
+            skip_next = False
             for line in f_in:
-                if line.startswith("-o "):
-                    line = line.replace(".so", ".a")
-                    f_out.write(line)
+                if skip_next:
+                    skip_next = False
+                    continue
+                if line.startswith("-o"):
+                    skip_next = True
                 elif line.startswith("bazel-out"):
                     f_out.write(line)
