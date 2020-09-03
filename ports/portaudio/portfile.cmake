66a7291a506ea3d2cea7f375900a509ea8a81c44
Stephen Kyne <xskyne@gmail.com>
2020-09-03 10:53:13 +0200

    Update ports/portaudio/portfile.cmake
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/portaudio/portfile.cmake
+++ ports/portaudio/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_from_git(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-    	-DPA_USE_DS=ON
+        -DPA_USE_DS=ON
         -DPA_USE_WASAPI=ON
         -DPA_USE_WDMKS=ON
         -DPA_USE_WMME=ON
