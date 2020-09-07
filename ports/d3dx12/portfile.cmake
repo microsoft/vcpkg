9eec6965a5d056f05150d4cf38231311b69dc911
Mérill Téterel <59962715+mteterel@users.noreply.github.com>
2020-09-07 10:01:04 +0200

    Update portfile.cmake
    Use the correct d3dx12.h that does not require the very latest Windows SDK

--- ports/d3dx12/portfile.cmake
+++ ports/d3dx12/portfile.cmake
@@ -1,8 +1,8 @@
 # Header-only library
 vcpkg_download_distfile(D3DX12_H
-    URLS "https://raw.githubusercontent.com/microsoft/DirectX-Graphics-Samples/master/Libraries/D3DX12/d3dx12.h"
+    URLS "https://raw.githubusercontent.com/walbourn/directx-vs-templates/master/d3d12game_win32_dr/d3dx12.h"
     FILENAME "d3dx12.h"
-    SHA512 "e41936bec06db9e22e953b31de418981343d5a4df4979d359a4c427d90f58ccee41fba73b9b0b8251d957dbdf4f9b7730ee11846f59f23636b862685a0fc21dc"
+    SHA512 "58a1f5060c82c2769987592d6f20d8c8d86032505740492b24f69c88b75f0814d3f18d5c3ae7c71c54bfe5eeb7f844ee623ce73376a676986db0643589aab62b"
 )
 
 file(INSTALL ${D3DX12_H} DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
