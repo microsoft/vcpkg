d1b153c4f0ed98ab69fd82556c8829e5b7e9cd64
JonLiu1993 <13720414433@163.com>
2020-09-07 11:22:16 +0800

    [libpqxx] Update to 7.1.1

--- ports/libpqxx/portfile.cmake
+++ ports/libpqxx/portfile.cmake
@@ -1,13 +1,11 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jtv/libpqxx
-    REF a6b1d60e74c1427c8ac2324b85cd4a0dc2068332
-    SHA512 990083f738322283dc9c98b138a676e5ba04ab77794d5a51d672557e0562d2366b5085ad5571dd91af8ba4dea56baa94e8c1e4e6fe571341c95e92eb28d2b15a
+    REF 29f8c73492a1bfdf87fda77351c7356ebee5ecde  #v7.1.2
+    SHA512 64fb6f62475c593430e3c6b6ad45f03b172b5efe3f51e2714334ca19303c436fab0ae70e1c5b462c3fcabbc74fc43da396ec1464c695c1fe1c89c904f3940848
     HEAD_REF master
-    PATCHES
-        fix-deprecated-bug.patch
+	PATCHES
+	    fix-connect.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
@@ -22,4 +20,4 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libpqxx RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
