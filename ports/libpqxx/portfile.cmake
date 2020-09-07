903da1f841c324af4dd13016cafb2d7a0c8563af
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2020-09-07 15:11:32 +0800

    Update portfile.cmake

--- ports/libpqxx/portfile.cmake
+++ ports/libpqxx/portfile.cmake
@@ -4,8 +4,8 @@ vcpkg_from_github(
     REF 29f8c73492a1bfdf87fda77351c7356ebee5ecde  #v7.1.2
     SHA512 64fb6f62475c593430e3c6b6ad45f03b172b5efe3f51e2714334ca19303c436fab0ae70e1c5b462c3fcabbc74fc43da396ec1464c695c1fe1c89c904f3940848
     HEAD_REF master
-	PATCHES
-	    fix-connect.patch
+    PATCHES
+        fix-connect.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
