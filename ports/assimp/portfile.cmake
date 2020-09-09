2b7a08af36118dc17bc41a46e2a69eb80da54409
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-09-09 13:49:55 -0700

    [assimp][irrxml][kubazip][poly2tri][polyclipping] Extract vendored dependencies

--- ports/assimp/portfile.cmake
+++ ports/assimp/portfile.cmake
@@ -5,18 +5,33 @@ vcpkg_from_github(
     SHA512 59b213428e2f7494cb5da423e6b2d51556318f948b00cea420090d74d4f5f0f8970d38dba70cd47b2ef35a1f57f9e15df8597411b6cd8732b233395080147c0f
     HEAD_REF master
     PATCHES
-        uninitialized-variable.patch
-        fix-static-build-error.patch
-        cmake-policy.patch
-        fix_minizip.patch
-        config.patch
+        build_fixes.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake-modules/FindZLIB.cmake)
-file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/zlib ${SOURCE_PATH}/contrib/gtest ${SOURCE_PATH}/contrib/rapidjson)
+file(REMOVE ${SOURCE_PATH}/cmake-modules/FindIrrXML.cmake)
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/clipper) # https://github.com/assimp/assimp/issues/788
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/poly2tri)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/zlib)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/gtest)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/irrXML)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/rapidjson)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/stb_image)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/zip)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/unzip)
+file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/utf8cpp)
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/Open3DGC)      #TODO
+#file(REMOVE_RECURSE ${SOURCE_PATH}/contrib/openddlparser) #TODO
+
 
 set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
 set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+  set(VCPKG_BUILD_SHARED_LIBS ON)
+else()
+  set(VCPKG_BUILD_SHARED_LIBS OFF)
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -24,27 +39,30 @@ vcpkg_configure_cmake(
     OPTIONS -DASSIMP_BUILD_TESTS=OFF
             -DASSIMP_BUILD_ASSIMP_VIEW=OFF
             -DASSIMP_BUILD_ZLIB=OFF
+            -DASSIMP_BUILD_SHARED_LIBS=${VCPKG_BUILD_SHARED_LIBS}
             -DASSIMP_BUILD_ASSIMP_TOOLS=OFF
             -DASSIMP_INSTALL_PDB=OFF
+            -DSYSTEM_IRRXML=ON
             -DIGNORE_GIT_HASH=ON
-            -DCMAKE_DISABLE_FIND_PACKAGE_RT=ON
-            -DHUNTER_ENABLED=OFF
-            #-DSYSTEM_IRRXML=ON # Assimp is not compatible with the latest irrlicht
 )
 
 vcpkg_install_cmake()
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Assimp)
-# The pkgconfig files have hardcoded library names that do not match cmake's output
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
-file(READ ${CURRENT_PACKAGES_DIR}/share/assimp/AssimpConfig.cmake _contents)
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/assimp/AssimpConfig.cmake "${_contents}
-add_library (assimp::assimp INTERFACE IMPORTED)
-set_target_properties(assimp::assimp PROPERTIES INTERFACE_LINK_LIBRARIES Assimp::assimp)
-set(ASSIMP_LIBRARIES Assimp::assimp)
-")
-
+vcpkg_fixup_cmake_targets()
 vcpkg_copy_pdbs()
 
+file(READ ${CURRENT_PACKAGES_DIR}/share/assimp/AssimpConfig.cmake ASSIMP_CONFIG)
+string(REPLACE "set(CMAKE_IMPORT_FILE_VERSION 1)"
+               "set(CMAKE_IMPORT_FILE_VERSION 1)
+find_package(irrXML QUIET)
+find_package(polyclipping QUIET)
+find_package(minizip QUIET)
+find_package(kubazip QUIET)
+find_package(poly2tri QUIET)" ASSIMP_CONFIG "${ASSIMP_CONFIG}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/assimp/AssimpConfig.cmake "${ASSIMP_CONFIG}")
+
+
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
