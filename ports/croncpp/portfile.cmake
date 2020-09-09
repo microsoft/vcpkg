ab11a2282488037abe8037991e7557af0c7c0655
Robert Kesteson <rkesters@gmail.com>
2020-09-09 18:12:14 -0400

    Update ports/croncpp/portfile.cmake
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

--- ports/croncpp/portfile.cmake
+++ ports/croncpp/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-#vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/croncpp)
+vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
