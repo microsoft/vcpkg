a782694677c684260d49947c7506daedba66357a
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-10 02:49:43 -0700

    [vcpkg_build_make] Add option MAKEFILE

--- scripts/cmake/vcpkg_build_make.cmake
+++ scripts/cmake/vcpkg_build_make.cmake
@@ -7,6 +7,7 @@
 ## vcpkg_build_make([BUILD_TARGET <target>]
 ##                  [ADD_BIN_TO_PATH]
 ##                  [ENABLE_INSTALL])
+##                  [MAKEFILE <makefileName>]
 ##                  [LOGFILE_ROOT <logfileroot>])
 ## ```
 ##
@@ -19,6 +20,9 @@
 ##
 ## ### ENABLE_INSTALL
 ## IF the port supports the install target use vcpkg_install_make() instead of vcpkg_build_make()
+##
+## ### MAKEFILE
+## Specific the name of the makefile
 ##
 ## ### BUILD_TARGET
 ## The target passed to the make build command (`./make <target>`). If not specified, the 'all' target will
@@ -39,7 +43,7 @@
 ## * [freexl](https://github.com/Microsoft/vcpkg/blob/master/ports/freexl/portfile.cmake)
 ## * [libosip2](https://github.com/Microsoft/vcpkg/blob/master/ports/libosip2/portfile.cmake)
 function(vcpkg_build_make)
-    cmake_parse_arguments(_bc "ADD_BIN_TO_PATH;ENABLE_INSTALL;DISABLE_PARALLEL" "LOGFILE_ROOT;BUILD_TARGET" "" ${ARGN})
+    cmake_parse_arguments(_bc "ADD_BIN_TO_PATH;ENABLE_INSTALL;DISABLE_PARALLEL;MAKEFILE" "LOGFILE_ROOT;BUILD_TARGET" "" ${ARGN})
 
     if(NOT _bc_LOGFILE_ROOT)
         set(_bc_LOGFILE_ROOT "build")
@@ -47,6 +51,10 @@ function(vcpkg_build_make)
 
     if(NOT _bc_BUILD_TARGET)
         set(_bc_BUILD_TARGET "all")
+    endif()
+
+    if (NOT _bc_MAKEFILE)
+        set(_bc_MAKEFILE Makefile)
     endif()
 
     if(WIN32)
@@ -66,20 +74,20 @@ function(vcpkg_build_make)
         vcpkg_acquire_msys(MSYS_ROOT)
         find_program(MAKE make REQUIRED)
         set(MAKE_COMMAND "${MAKE}")
-        set(MAKE_OPTS ${_bc_MAKE_OPTIONS} -j ${VCPKG_CONCURRENCY} --trace -f Makefile ${_bc_BUILD_TARGET})
-        set(NO_PARALLEL_MAKE_OPTS ${_bc_MAKE_OPTIONS} -j 1 --trace -f Makefile ${_bc_BUILD_TARGET})
+        set(MAKE_OPTS ${_bc_MAKE_OPTIONS} -j ${VCPKG_CONCURRENCY} --trace -f ${_bc_MAKEFILE} ${_bc_BUILD_TARGET})
+        set(NO_PARALLEL_MAKE_OPTS ${_bc_MAKE_OPTIONS} -j 1 --trace -f ${_bc_MAKEFILE} ${_bc_BUILD_TARGET})
 
         string(REPLACE " " "\\\ " _VCPKG_PACKAGE_PREFIX ${CURRENT_PACKAGES_DIR})
         string(REGEX REPLACE "([a-zA-Z]):/" "/\\1/" _VCPKG_PACKAGE_PREFIX "${_VCPKG_PACKAGE_PREFIX}")
-        set(INSTALL_OPTS -j ${VCPKG_CONCURRENCY} --trace -f Makefile install DESTDIR=${_VCPKG_PACKAGE_PREFIX})
+        set(INSTALL_OPTS -j ${VCPKG_CONCURRENCY} --trace -f ${_bc_MAKEFILE} install DESTDIR=${_VCPKG_PACKAGE_PREFIX})
         #TODO: optimize for install-data (release) and install-exec (release/debug)
     else()
         # Compiler requriements
         find_program(MAKE make REQUIRED)
         set(MAKE_COMMAND "${MAKE}")
         # Set make command and install command
-        set(MAKE_OPTS ${_bc_MAKE_OPTIONS} V=1 -j ${VCPKG_CONCURRENCY} -f Makefile ${_bc_BUILD_TARGET})
-        set(NO_PARALLEL_MAKE_OPTS ${_bc_MAKE_OPTIONS} V=1 -j 1 -f Makefile ${_bc_BUILD_TARGET})
+        set(MAKE_OPTS ${_bc_MAKE_OPTIONS} V=1 -j ${VCPKG_CONCURRENCY} -f ${_bc_MAKEFILE} ${_bc_BUILD_TARGET})
+        set(NO_PARALLEL_MAKE_OPTS ${_bc_MAKE_OPTIONS} V=1 -j 1 -f ${_bc_MAKEFILE} ${_bc_BUILD_TARGET})
         set(INSTALL_OPTS -j ${VCPKG_CONCURRENCY} install DESTDIR=${CURRENT_PACKAGES_DIR})
     endif()
 
