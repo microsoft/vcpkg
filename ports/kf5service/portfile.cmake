7f4cb4da0de3333ced56d85e5e6e0d52dd56c6f6
Dawid Wróbel <me@dawidwrobel.com>
2020-09-11 00:33:18 -0400

    Add kf5service port

--- ports/kf5service/portfile.cmake
+++ ports/kf5service/portfile.cmake
@@ -4,7 +4,22 @@ vcpkg_from_github(
     REF v5.73.0
     SHA512 02329f8aca3a3fc7ffc8fa4565aecab32e01938ab60bd14d5c3379aacecd0e2f252110c76b4cee37639d2182b1f1af10d4b33a1c815cc8c8b4d8190581d4341f
     HEAD_REF master
+    PATCHES
+        "add-missing-kf5windowsystem-dependency.patch"
 )
+
+if (VCPKG_TARGET_IS_OSX)
+     message(WARNING "${PORT} requires bison version greater than one provided by macOS, please use \`brew install bison\` to install a newer bison.")
+endif()
+
+vcpkg_find_acquire_program(BISON)
+vcpkg_find_acquire_program(FLEX)
+
+get_filename_component(FLEX_DIR "${FLEX}" DIRECTORY )
+get_filename_component(BISON_DIR "${BISON}" DIRECTORY )
+
+vcpkg_add_to_path(PREPEND "${FLEX_DIR}")
+vcpkg_add_to_path(PREPEND "${BISON_DIR}")
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
