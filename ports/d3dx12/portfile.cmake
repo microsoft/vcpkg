677b80aadc28bc1018a453a2af9a90f341038151
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2020-09-11 11:04:04 +0800

    Update ports/d3dx12/portfile.cmake

--- ports/d3dx12/portfile.cmake
+++ ports/d3dx12/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_download_distfile(D3DX12_H
     URLS "https://raw.githubusercontent.com/walbourn/directx-vs-templates/may2020/d3d12game_win32_dr/d3dx12.h"
     FILENAME "d3dx12.h"
-    SHA512 "58a1f5060c82c2769987592d6f20d8c8d86032505740492b24f69c88b75f0814d3f18d5c3ae7c71c54bfe5eeb7f844ee623ce73376a676986db0643589aab62b"
+    SHA512 "829b72ddf861652bd96518b7d54f7a103c95b4434ec022e1551fb085e4dfc8f804e01ccdb4677e3f64367553c56d35291b305e10c2ea6186ddadaaa071c6d7a2"
 )
 
 file(INSTALL ${D3DX12_H} DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
