54a303747408b367e0a939f1f5c4fe90598183e3
Dawid Wróbel <me@dawidwrobel.com>
2020-09-13 02:18:52 -0400

    Add kf5package port

---
+++ ports/kf5package/portfile.cmake
@@ -0,0 +1,36 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kpackage
+    REF v5.73.0
+    SHA512 b9fe2179ea0c6b52f1e451631dc3384836c6f241c46102bebc0d218c13fe6505d2ffc7c53c0f0b3805f71fc1fac5ca8e6b98bed26a6463e3b46b0645420bd823
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_HTML_DOCS=OFF
+            -DBUILD_MAN_DOCS=OFF
+            -DBUILD_QTHELP_DOCS=OFF
+            -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KPackage)
+
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/vcp kg-cmake-wrapper.cmake
+    ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    @ONLY
+)
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/qml ${CURRENT_PACKAGES_DIR}/debug/qml )
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/qml ${CURRENT_PACKAGES_DIR}/qml )
+
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
