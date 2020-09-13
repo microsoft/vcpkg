f09a701921df8cb67fab026b5dd59366f580accc
Dawid Wróbel <me@dawidwrobel.com>
2020-09-13 03:37:27 -0400

    Add kf5solid port

--- ports/kf5i18n/portfile.cmake
+++ ports/kf5i18n/portfile.cmake
@@ -39,6 +39,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5I18n)
+
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/data)
