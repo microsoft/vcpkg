3bc8f25dffbbbf4e197b526da9f94cf5a1e73b18
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-14 00:02:14 -0700

    [utfcpp] Use vcpkg_*_cmake and add features

--- ports/utfcpp/portfile.cmake
+++ ports/utfcpp/portfile.cmake
@@ -4,8 +4,26 @@ vcpkg_from_github(
     REF v3.1.1
     SHA512 720e2eba5c04f0bc4903a287138149a9cd432bc68bb163fe36b2e0d26d8bf616b4665f389b4a9c97af6ae7869e78973d97db976a4745512a241eebf774608997
     HEAD_REF master
+    PATCHES fix-test.patch
 )
 
-file(COPY ${SOURCE_PATH}/source/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    sample UTF8_SAMPLES
+    test UTF8_TESTS
+)
 
-file(INSTALL ${SOURCE_PATH}/source/utf8.h DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS ${FEATURE_OPTIONS}
+        -DUTF8_INSTALL=ON
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/utf8cpp)
+
+# Header only
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
