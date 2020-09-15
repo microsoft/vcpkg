3a6eb149bb4ad86eb748ab4b2cf5c2bc8caaf508
alek-d <25618925+alek-d@users.noreply.github.com>
2020-09-15 20:10:50 +0200

    Split library linkage from crt linkage

--- ports/scintilla/portfile.cmake
+++ ports/scintilla/portfile.cmake
@@ -5,19 +5,21 @@ vcpkg_download_distfile(ARCHIVE
   FILENAME "scintilla423.zip"
   SHA512 82a595782119ce5bb48c39f4cb9b29605c4cdc276f605ebd3e3b3ecae003ef2132102e21be8943c8b36ec40957e2e50f4ebc0086a5096901fa0e8e5e178db750
 )
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  list(APPEND PATCHES 0001-static-lib.patch)
+endif()
+
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+  list(APPEND PATCHES 0002-static-crt.patch)
+endif()
+
 vcpkg_extract_source_archive_ex(
   OUT_SOURCE_PATH SOURCE_PATH
   ARCHIVE ${ARCHIVE}
   REF 4.2.3
+  PATCHES ${PATCHES}
 )
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-  vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES
-      ${CMAKE_CURRENT_LIST_DIR}/0001-static-build.patch
-  )
-endif()
 
 vcpkg_install_msbuild(
   SOURCE_PATH ${SOURCE_PATH}
