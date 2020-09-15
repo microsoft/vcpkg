1dfb89963a67ebc8e70b1f2a734c2895ea2b75b2
dmillard <dmillard10@gmail.com>
2020-09-15 12:39:51 -0400

    Patch PAGMO_BUILD_STATIC_LIBRARY back to 2.15

--- ports/pagmo2/portfile.cmake
+++ ports/pagmo2/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF v2.15.0
     SHA512 992c8a00018a2e84ccdc0f1f5ef46e97f3ec5c3167366a8b836016f3a13d6f915b2a6ae7271e220e3c7ce89372cab5c69aa52bd9b416f8c88b68396b15d49231
     HEAD_REF master
+    PATCHES static-build-support.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
