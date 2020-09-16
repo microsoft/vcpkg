cad9b66759547e28b1a00976b9ac20ce5a089743
Adam Johnson <AdamJohnso@gmail.com>
2020-09-16 13:13:53 -0400

    [pybind11] Partially revert the assumption of an automated finder.
    It seems pybind11 has its own python find module. The current upstream
    head allows using CMake's new FindPython module with the definition
    `-DPYBIND11_FINDPYTHON`, but there has been no upstream release with
    this feature. In the meantime, we correctly find the vcpkg python3
    interpreter and libraries.

--- ports/pybind11/portfile.cmake
+++ ports/pybind11/portfile.cmake
@@ -6,11 +6,28 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+# pybind master allows opting into the builtin cmake find module with -DPYBIND11_FINDPYTHON-ON.
+# When the upstream port is updated, remove all of this and change the options to just:
+# -DPYBIND11_TEST=OFF -DPYBIND11_FINDPYTHON=ON
+find_program(PYTHON_EXECUTABLE NAMES python python3.8 PATHS "${CURRENT_INSTALLED_DIR}/tools/python3" NO_DEFAULT_PATH)
+find_library(PYTHON_LIBRARY_DEBUG NAMES python38_d python3.8_d PATHS "${CURRENT_INSTALLED_DIR}/debug/lib" NO_DEFAULT_PATH)
+find_library(PYTHON_LIBRARY_RELEASE NAMES python38 python3.8 PATHS "${CURRENT_INSTALLED_DIR}/lib" NO_DEFAULT_PATH)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DPYBIND11_TEST=OFF
+        -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}
+        -DPYTHONLIBS_FOUND=ON
+        -DPYTHON_INCLUDE_DIRS=${CURRENT_INSTALLED_DIR}/include
+        -DPYTHON_MODULE_EXTENSION=.dll
+    OPTIONS_RELEASE
+        -DPYTHON_IS_DEBUG=OFF
+        -DPYTHON_LIBRARIES=${PYTHON_LIBRARY_DEBUG}
+    OPTIONS_DEBUG
+        -DPYTHON_IS_DEBUG=ON
+        -DPYTHON_LIBRARIES=${PYTHON_LIBRARY_RELEASE}
 )
 
 vcpkg_install_cmake()
