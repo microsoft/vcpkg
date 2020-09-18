e4a14583bd116dffd365c8b974287abee14f30df
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-09-18 14:25:03 +0200

    [libctl] add new port

---
+++ ports/libctl/portfile.cmake
@@ -0,0 +1,55 @@
+message(STATUS "${PORT} currently requires the following library from the system package manager:
+    guile-2.2-dev
+This can be installed on Ubuntu systems via sudo apt install guile-2.2-dev")
+
+#vcpkg_from_github(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    REPO NanoComp/libctl
+#    REF aa56a410f33fb2fd80605faf35dfa7906785edef
+#    SHA512 0351f35e433089bf8437ab17eaa0cf13d9d578776806011e1ecfe48cda769ebc21801f0e5b31bf3f80a62ceae170f0d5e9c9b817e01e39bffd8c062b3ff1731c
+#    HEAD_REF master
+#)
+
+#if(CMAKE_HOST_WIN32)
+#	vcpkg_acquire_msys(MSYS_ROOT PACKAGES autoconf)
+#endif()
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/NanoComp/libctl/archive/v4.5.0.tar.gz"
+    FILENAME "libctl-4.5.0.tar.gz"
+    SHA512 afa25e8dbc4b4652e45e7235937a8fe93f75e5e8f812afcca047c165f08ee4077e3cb215579c2f8b0716556ba4216f23ed2f2c69ad954083dde02c1261e02771
+)
+
+vcpkg_extract_source_archive_ex(
+     ARCHIVE ${ARCHIVE}
+     OUT_SOURCE_PATH SOURCE_PATH
+)
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS_DEBUG
+        --enable-debug
+)
+
+vcpkg_install_make()
+
+
+# remove debug include folder
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include/)
+
+# remove debug bin folder
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/)
+
+# remove bin folder
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/)
+
+vcpkg_copy_pdbs()
+
+# remove debug share folder
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share/)
+
+# remove share folder
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
--- ports/x264/portfile.cmake
+++ ports/x264/portfile.cmake
@@ -1,6 +1,6 @@
 set(X264_VERSION 157)
 
-vcpkg_fail_port_install(ON_TARGET "OSX") 
+vcpkg_fail_port_install(ON_TARGET "OSX")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -8,7 +8,7 @@ vcpkg_from_github(
     REF 303c484ec828ed0d8bfe743500e70314d026c3bd
     SHA512 faf210a3f9543028ed882c8348b243dd7ae6638e7b3ef43bec1326b717f23370f57c13d0ddb5e1ae94411088a2e33031a137b68ae9f64c18f8f33f601a0da54d
     HEAD_REF master
-    PATCHES 
+    PATCHES
         "uwp-cflags.patch"
 )
 
@@ -38,6 +38,7 @@ vcpkg_configure_make(
         --disable-gpac
         --disable-lsmash
         --disable-asm
+    OPTIONS_DEBUG
         --enable-debug
 
 )
