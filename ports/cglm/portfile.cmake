a36d5bcadac4bd71b3d4d4ff26a6ba5dec8c1b00
Suyash Mohan <suyashatweb@gmail.com>
2020-09-19 13:16:04 +0800

    build only static version

--- ports/cglm/portfile.cmake
+++ ports/cglm/portfile.cmake
@@ -9,9 +9,9 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    #    OPTIONS 
-    #  -DCGLM_SHARED=OFF
-    #  -DCGLM_STATIC=ON 
+	OPTIONS 
+		-DCGLM_SHARED=OFF
+		-DCGLM_STATIC=ON 
     #  -DCGLM_USE_C99=ON
     #  -DCGLM_USE_TEST=OFF
 )
@@ -25,23 +25,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cglm")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cglm")
 configure_file(${CMAKE_CURRENT_LIST_DIR}/cglm-config.cmake  ${CURRENT_PACKAGES_DIR}/share/cglm COPYONLY)
-
-file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
-if(DLLS)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
-    foreach(DLL ${DLLS})
-        get_filename_component(N "${DLL}" NAME)
-        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/bin/${N})
-    endforeach()
-endif()
-file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
-if(DLLS)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
-    foreach(DLL ${DLLS})
-        get_filename_component(N "${DLL}" NAME)
-        file(RENAME ${DLL} ${CURRENT_PACKAGES_DIR}/debug/bin/${N})
-    endforeach()
-endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
