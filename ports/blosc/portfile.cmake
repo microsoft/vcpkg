f8d2c94449248f08e3a4ed92d02af3cd9c82d7e5
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-22 02:47:36 -0700

    [blosc] Fix win static build

--- ports/blosc/portfile.cmake
+++ ports/blosc/portfile.cmake
@@ -1,12 +1,18 @@
+
+set(BLOSC_PATHCES 0001-find-deps.patch
+    0002-export-blosc-config.patch
+)
+if (VCPKG_TARGET_IS_WINDOWS)
+    list(APPEND BLOSC_PATHCES 0003-fix-win-static-build.patch)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Blosc/c-blosc
     REF a44e961498cdca2eb893fa897cd15dd007fad496 # v1.20.1
     SHA512 588fddbfe6e72aad857ca0845aa4a0f72513cabe89d6989699ee2d7c74e72d0afdab5ad26dd94a508208c99afbdb47f791572b7adcf369b7a62f3aa5fba0bdd5
     HEAD_REF master
-    PATCHES
-      0001-find-deps.patch
-      0002-export-blosc-config.patch
+    PATCHES ${BLOSC_PATHCES}
 )
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
@@ -40,4 +46,4 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/blosc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSES/BLOSC.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/blosc RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSES/BLOSC.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
