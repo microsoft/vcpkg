fe1144595a5cc0b25c970596a9c5ae4604976524
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-22 03:03:46 -0700

    [blosc] fix unix build

--- ports/blosc/portfile.cmake
+++ ports/blosc/portfile.cmake
@@ -1,18 +1,13 @@
-
-set(BLOSC_PATHCES 0001-find-deps.patch
-    0002-export-blosc-config.patch
-)
-if (VCPKG_TARGET_IS_WINDOWS)
-    list(APPEND BLOSC_PATHCES 0003-fix-win-static-build.patch)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Blosc/c-blosc
     REF a44e961498cdca2eb893fa897cd15dd007fad496 # v1.20.1
     SHA512 588fddbfe6e72aad857ca0845aa4a0f72513cabe89d6989699ee2d7c74e72d0afdab5ad26dd94a508208c99afbdb47f791572b7adcf369b7a62f3aa5fba0bdd5
     HEAD_REF master
-    PATCHES ${BLOSC_PATHCES}
+    PATCHES
+        0001-find-deps.patch
+        0002-export-blosc-config.patch
+        0003-fix-static-build.patch
 )
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
