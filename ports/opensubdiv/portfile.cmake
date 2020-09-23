e1dfde4d08cc2b4dac5791459907e321cdefdc1e
ousttrue <ousttrue@gmail.com>
2020-09-24 08:59:29 +0900

    new port opensubdiv

---
+++ ports/opensubdiv/portfile.cmake
@@ -0,0 +1,44 @@
+# # Specifies if the port install should fail immediately given a condition
+# vcpkg_fail_port_install(MESSAGE "opensubdiv currently only supports Linux and Mac platforms" ON_TARGET "Windows")
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO PixarAnimationStudios/OpenSubdiv
+    REF 82ab1b9f54c87fdd7e989a3470d53e137b8ca270 # 3.4.3
+    SHA512 607cb9aa05d83a24bc2102bfd28abfec58f5723b1c56f6f431111ebf98f105ff7ca2a77610953acd21f73cb74d8d8ec68db3aeb11be1f9ca56d87c36c58dd095
+    HEAD_REF master
+    PATCHES
+        fix_compile-option.patch
+)
+
+vcpkg_find_acquire_program(PYTHON2)
+get_filename_component(PYTHON2_DIR "${PYTHON2}" DIRECTORY)
+vcpkg_add_to_path("${PYTHON2_DIR}")
+
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    OPTIONS
+        -DNO_DX=ON
+        -DNO_EXAMPLES=ON
+        -DNO_TUTORIALS=ON
+        -DNO_REGRESSION=ON
+        -DNO_TESTS=ON
+)
+
+vcpkg_install_cmake()
+
+# # Moves all .cmake files from /debug/share/opensubdiv/ to /share/opensubdiv/
+# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
+# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/opensubdiv)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# # Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/opensubdiv RENAME copyright)
+
+# # Post-build test for cmake libraries
+# vcpkg_test_cmake(PACKAGE_NAME opensubdiv)
