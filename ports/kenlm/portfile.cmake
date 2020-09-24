89db2cc5cc8478406334c49515e73982767b4267
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-24 00:58:15 -0700

    Correct cmake

--- ports/kenlm/portfile.cmake
+++ ports/kenlm/portfile.cmake
@@ -33,7 +33,7 @@ vcpkg_install_cmake()
 
 set(KENLM_TOOLS count_ngrams filter fragment kenlm_benchmark lmplz phrase_table_vocab)
 if (NOT VCPKG_TARGET_IS_WINDOWS)
-    list(APPEND ${KENLM_TOOLS} probing_hash_table_benchmark query build_binary)
+    list(APPEND KENLM_TOOLS probing_hash_table_benchmark query build_binary)
 endif()
 vcpkg_copy_tools(TOOL_NAMES ${KENLM_TOOLS} AUTO_CLEAN)
 
