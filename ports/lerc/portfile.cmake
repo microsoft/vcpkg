da3afcfaf5942ff8ec6642723ec3f165585a2565
wangli28 <wangli28@beyondsoft.com>
2020-09-25 09:27:33 +0000

    [lerc] Update CONTROL

--- ports/lerc/portfile.cmake
+++ ports/lerc/portfile.cmake
@@ -16,9 +16,9 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
