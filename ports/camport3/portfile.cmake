5d0a7d0b51bfeca24f7d54f44ac011c1a218eb95
Ivan Bravo <tecnic@robinteg.com>
2020-09-26 21:02:04 +0200

    [camport3] add new port: https://github.com/percipioxyz/camport3

---
+++ ports/camport3/portfile.cmake
@@ -0,0 +1,67 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO percipioxyz/camport3
+    REF v1.5.3
+    SHA512 efa41e75b4ed7147f94270765138aa226a92ec51c99157776e916ec178ad2a9fe55aa6e6e746be46e2f2178852f4c4f9323b515f5a1b151ac70c21f8f923d901
+    HEAD_REF master
+)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/include)
+file(COPY 
+    ${SOURCE_PATH}/include/TYApi.h
+    ${SOURCE_PATH}/include/TYCoordinateMapper.h
+    ${SOURCE_PATH}/include/TYImageProc.h
+    ${SOURCE_PATH}/include/TyIsp.h
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
+if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+        file(COPY
+            ${SOURCE_PATH}/lib/win/hostapp/${VCPKG_TARGET_ARCHITECTURE}/tycam.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+        )
+        file(COPY
+            ${SOURCE_PATH}/lib/win/hostapp/${VCPKG_TARGET_ARCHITECTURE}/tycam.dll
+            DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+        )
+    endif()
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+        file(COPY
+            ${SOURCE_PATH}/lib/win/hostapp/${VCPKG_TARGET_ARCHITECTURE}/tycam.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        )
+        file(COPY
+            ${SOURCE_PATH}/lib/win/hostapp/${VCPKG_TARGET_ARCHITECTURE}/tycam.dll
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+        )
+    endif()
+
+elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
+        file(COPY
+            ${SOURCE_PATH}/lib/linux/lib_${VCPKG_TARGET_ARCHITECTURE}/libtycam.so
+            DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+        )
+    endif()
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib)
+        file(COPY
+            ${SOURCE_PATH}/lib/linux/lib_${VCPKG_TARGET_ARCHITECTURE}/libtycam.so
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        )
+    endif()
+
+else()
+    message(FATAL_ERROR "Unsupported OS")
+endif()
+
+file(INSTALL
+    ${SOURCE_PATH}/LICENSE
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    RENAME copyright
+)
