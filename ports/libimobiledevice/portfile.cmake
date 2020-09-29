df06ea6b48102eb2e8f5fc2cecdb7fcfe7b9114d
JackBoosY <yuzaiyang@beyondsoft.com>
2020-09-29 02:12:02 -0700

    [libimobiledevice] Fix build

--- ports/libimobiledevice/portfile.cmake
+++ ports/libimobiledevice/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF b30267ac1fb46f2b1d2d5e585aaa73c0f4ce8ad8 # v1.3.6
     SHA512 47912571726c38fe3c306a5e7c76b4042994b53a30794432a5af7eae5a30855d39828c52048b1a90b837306e6d5c447fc11d8521669258e76231cfdd6aef17d9
     HEAD_REF msvc-master
+    PATCHES fix-functions-declaration.patch
 )
 
 configure_file(${CURRENT_PORT_DIR}/CMakeLists.txt ${SOURCE_PATH}/CMakeLists.txt COPYONLY)
