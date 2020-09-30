33714728cfbf5ce5fbe96e8e17dd0fba8ccd1065
Johannes Schönberger <joschonb@microsoft.com>
2020-09-30 14:05:57 +0200

    Add new portfiles for FAISS library

---
+++ ports/faiss/portfile.cmake
@@ -0,0 +1,30 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebookresearch/faiss
+    REF ef6e53f8ba6b719a053bee513d3b60151925eef2
+    SHA512 78b85aa14d183e7e65bf79e70c2c13195728055957ca6c7618081097446aa9b8418d032ca6fc34d18461b8e9f080df88fe4eb62723f7044a794862c3e9efff56
+    HEAD_REF master
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    gpu FAISS_ENABLE_GPU
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DFAISS_ENABLE_PYTHON=OFF  # Requires SWIG
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets()
+
+vcpkg_copy_pdbs()
+
+file(INSTALL     ${SOURCE_PATH}/LICENSE
+     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+     RENAME copyright
+)
