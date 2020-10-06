fffd057b4f2de1eeb600206bd3c37e28a4858fe2
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-10-06 14:03:24 +0200

    [meep] move to autoconfig also on linux

--- ports/meep/portfile.cmake
+++ ports/meep/portfile.cmake
@@ -1,30 +1,19 @@
-#vcpkg_from_github(
-#    OUT_SOURCE_PATH SOURCE_PATH
-#    REPO NanoComp/meep
-#    REF ad8986b622631232324fc7a100fb726a297f22f3
-#    SHA512 2546a0e5c40d6532119279809571b68e9fd2d21af719f9b49cae315e3810c0adbf9fe5fe62e9bc7726ede3487dd7ad6fca5c49bd3adbe842f6598f29dd303ca4
-#    HEAD_REF master
-#)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NanoComp/meep
+    REF ad8986b622631232324fc7a100fb726a297f22f3
+    SHA512 2546a0e5c40d6532119279809571b68e9fd2d21af719f9b49cae315e3810c0adbf9fe5fe62e9bc7726ede3487dd7ad6fca5c49bd3adbe842f6598f29dd303ca4
+    HEAD_REF master
+)
 
 if(CMAKE_HOST_WIN32)
-#	vcpkg_acquire_msys(MSYS_ROOT PACKAGES autoconf)
+	vcpkg_acquire_msys(MSYS_ROOT PACKAGES autoconf)
     vcpkg_acquire_msys(MSYS_ROOT PACKAGES make)
     vcpkg_add_to_path("${MSYS_ROOT}/usr/bin")
 endif()
 
 include(vcpkg_find_fortran)
 vcpkg_find_fortran(FORTRAN_CMAKE)
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/NanoComp/meep/releases/download/v1.15.0/meep-1.15.0.tar.gz"
-    FILENAME "meep-1.15.0.tar.gz"
-    SHA512 d31e6eda92c1dc8d9d99898e154428660e364c4b065ecc872fd358d2512a72d670e98ee9c6be43e33d92a282c5eb0c89de516361c413fd05a371cfebbe942d7c
-)
-
-vcpkg_extract_source_archive_ex(
-     ARCHIVE ${ARCHIVE}
-     OUT_SOURCE_PATH SOURCE_PATH
-)
 
 vcpkg_configure_make(
     AUTOCONFIG
@@ -38,7 +27,6 @@ vcpkg_configure_make(
 )
 
 vcpkg_install_make()
-
 
 # remove debug include folder
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include/)
