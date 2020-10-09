32365795cf010a16b929a70969d8882cc1bac046
Billy Robert O'Neal III <bion@microsoft.com>
2020-10-08 22:24:00 -0700

    Remove default TARGET_PATH.

--- ports/fastcdr/portfile.cmake
+++ ports/fastcdr/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/fastcdr TARGET_PATH share/fastcdr)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/fastcdr)
 
 vcpkg_copy_pdbs()
 
