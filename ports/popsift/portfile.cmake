e7d56ecd74a5ed54293570a71f991e542020c326
Simone Gasparini <simone.gasparini@gmail.com>
2020-10-13 11:47:07 +0200

    [popsift] using vcpkg_find_cuda
    and v1.0.0-rc2

--- ports/popsift/portfile.cmake
+++ ports/popsift/portfile.cmake
@@ -6,14 +6,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO alicevision/popsift	
-#    REF v1.0.0-rc1
-    # REF 318ae82
-    REF 73e5e08a23b351a5f4c69f59d28bfda8496f1598
-    # SHA512 5e9ee211d2f35189a7a284b5fcd47fca56d69ecaef5a13ffa9babd4d9e91eb02b596df10846366417031e2291bf468c5160dd7b622bb51fdfab672ee4f1517f2
-    SHA512 e9a21b48450efe2abbcdb6ca1be1e11f3d3575fa3085f463cdaca5196cb2999a2da4bb7ed2a0a438fbc47e3ba64592a3e49f2f901eef1a29ce69f711185eb702
-#    HEAD_REF develop
-    HEAD_REF ci/win/addDebug
+    REF v1.0.0-rc2
+    SHA512 510912f8085c017ef4d8400f9b64c79d5da2ceb887d57917c3b7f7d56257b9741c94980227cdd1fff1ed761b446315f917f153fa5f9ab667b2af3f0ddac4ab86
+    HEAD_REF develop
 )
+
+include(${CURRENT_INSTALLED_DIR}/share/vcpkg_find_cuda/vcpkg_find_cuda.cmake)
+vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT CUDA_TOOLKIT_ROOT)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     apps       PopSift_BUILD_EXAMPLES
@@ -22,7 +21,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS ${FEATURE_OPTIONS} -DCUDA_TOOLKIT_ROOT=${CUDA_TOOLKIT_ROOT}
 )
 
 vcpkg_install_cmake()
