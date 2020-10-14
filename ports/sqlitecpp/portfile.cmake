2d59ff0e70b1bad7394c4b763e39e5fb32b793a6
SvenPStarFinanz <spa@starfinanz.de>
2020-10-14 11:04:42 +0200

    Build sqlitecpp.dll instead of static lib. Feature sqlcipher for encrypted use of sqlite database (sqlcipher). Changed port control to vcpkg.json format.

--- ports/sqlitecpp/portfile.cmake
+++ ports/sqlitecpp/portfile.cmake
@@ -1,19 +1,23 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
     REPO "SRombauts/SQLiteCpp"
     REF 3.1.1
     HEAD_REF master
     SHA512 9030b5249c149db8a5b2fe350f71613e4ee91061765a771640ed3ffa7c24aada4000ba884ef91790fdc0f13dc4519038c1edeba64b85b85ac09c3e955a7988a1
     PATCHES
-        0001-Find-external-sqlite3.patch
+        0001-unofficial-sqlite3-and-sqlcipher.patch
         fix_dependency.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+	sqlcipher SQLITE_HAS_CODEC
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
+		${FEATURE_OPTIONS}
         -DSQLITECPP_RUN_CPPLINT=OFF
         -DSQLITECPP_RUN_CPPCHECK=OFF
         -DSQLITECPP_INTERNAL_SQLITE=OFF
