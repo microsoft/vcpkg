b2bffb05f5461782c102e738dba144a10d7f31b3
jimmy hu <60071jimmy@gmail.com>
2020-10-14 15:43:44 +0800

    [jansson] Update portfile.cmake
    [jansson] Update portfile.cmake

--- ports/jansson/portfile.cmake
+++ ports/jansson/portfile.cmake
@@ -1,9 +1,7 @@
-include(vcpkg_common_functions)
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO akheron/jansson
-  REF e9ebfa7e77a6bee77df44e096b100e7131044059
+  REF e9ebfa7e77a6bee77df44e096b100e7131044059 # v2.13.1
   SHA512 88a59c1cf5150699def17c86192ca9bacdfe6669319f770c3fbf14fa8edc48b4bb015a4a634a09db40fba9054320ac7c133c4d156f813af540a636f7825f0610
   HEAD_REF master
 )
@@ -45,6 +43,6 @@ vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/jansson RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 vcpkg_copy_pdbs()
