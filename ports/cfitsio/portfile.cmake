e41f7285341686a91dbafbf640534c2867ad648f
JackBoosY <yuzaiyang@beyondsoft.com>
2020-10-15 02:42:33 -0700

    [cfitsio] Fix file paths, export cmake targets

--- ports/cfitsio/portfile.cmake
+++ ports/cfitsio/portfile.cmake
@@ -9,13 +9,35 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     PATCHES
         0001-correct-headers-for-getcwd.patch
-        0002-fix-dependency-zlib.patch
+        0002-fix-dependencies.patch
         0003-export-cmake-targets.patch
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    curl        UseCurl
+)
+
+if ("curl" IN_LIST FEATURES)
+    set(FIND_CURL_DEPENDENCY "find_dependency(CURL CONFIG)")
+endif()
+
+if ("pthreads" IN_LIST FEATURES)
+    if (VCPKG_TARGET_IS_WINDOWS)
+        set(WITH_PTHREADS ON)
+        set(FIND_PTHREADS_DEPENDENCY "find_dependency(pthreads)")
+    else()
+        message(WARNING "Feature pthreads only support Windows, disable it now.")
+        set(WITH_PTHREADS OFF)
+    endif()
+else()
+    
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS ${FEATURE_OPTIONS}
+        -DUSE_PTHREADS=${WITH_PTHREADS}
 )
 
 vcpkg_install_cmake()
@@ -25,24 +47,12 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-cfitsio TARGET_PATH share
 file(READ ${CURRENT_PACKAGES_DIR}/share/unofficial-cfitsio/unofficial-cfitsio-config.cmake ASSIMP_CONFIG)
 file(WRITE ${CURRENT_PACKAGES_DIR}/share/unofficial-cfitsio/unofficial-cfitsio-config.cmake "
 include(CMakeFindDependencyMacro)
+${FIND_CURL_DEPENDENCY}
+${FIND_PTHREADS_DEPENDENCY}
 find_dependency(ZLIB)
-${ASSIMP_CONFIG}")
+${ASSIMP_CONFIG}
+")
 
-# Remove duplicate include files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/include/unistd.h ${CURRENT_PACKAGES_DIR}/debug/share)
 
-# cfitsio uses very common names for its headers, so they must be moved to a subdirectory
-file(RENAME ${CURRENT_PACKAGES_DIR}/include ${CURRENT_PACKAGES_DIR}/cfitsio)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/include)
-file(RENAME ${CURRENT_PACKAGES_DIR}/cfitsio ${CURRENT_PACKAGES_DIR}/include/cfitsio)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    # move DLLs to bin directories for dynamic builds
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
-    file(RENAME  ${CURRENT_PACKAGES_DIR}/lib/cfitsio.dll ${CURRENT_PACKAGES_DIR}/bin/cfitsio.dll)
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(RENAME  ${CURRENT_PACKAGES_DIR}/debug/lib/cfitsio.dll ${CURRENT_PACKAGES_DIR}/debug/bin/cfitsio.dll)
-endif()
-
-# Handle copyright
-file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/cfitsio RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
