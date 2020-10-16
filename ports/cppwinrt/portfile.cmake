03f8323b99a62b5027d8d85f435dbac5e57ac589
Jared Irwin <jrairwin@sympatico.ca>
2020-10-16 15:20:32 -0400

    Update cppwinrt

--- ports/cppwinrt/portfile.cmake
+++ ports/cppwinrt/portfile.cmake
@@ -1,21 +1,29 @@
-include(vcpkg_common_functions)
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    message(FATAL_ERROR "\n${PORT} does not support your system, only Windows for now. Please open a ticket issue on github.com/microsoft/vcpkg if necessary\n")
+endif()
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO Microsoft/cppwinrt
-    REF fall_2017_creators_update_for_vs_15.3
-    SHA512 e3f987ed3f3dce019b8bf9f5451e53b42357473a003b8c14f9009e1848ee0463286bd46fdc3c739c8f7c2d232707e8018f5c087ffae784c745d51a8143f9a294
-    HEAD_REF master
+set(PACKAGE_NAME Microsoft.Windows.CppWinRT)
+set(VERSION 2.0.201008.2)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
+
+vcpkg_find_acquire_program(NUGET)
+vcpkg_execute_required_process(
+    ALLOW_IN_DOWNLOAD_MODE
+    COMMAND ${NUGET} install ${PACKAGE_NAME} -ExcludeVersion -Version ${VERSION} -NonInteractive -OutputDirectory ${SOURCE_PATH}
+    LOGNAME nuget-${TARGET_TRIPLET}
 )
 
-# Put the licence file where vcpkg expects it
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cppwinrt)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/cppwinrt/LICENSE ${CURRENT_PACKAGES_DIR}/share/cppwinrt/copyright)
+set(SOURCE_PATH ${SOURCE_PATH}/${PACKAGE_NAME})
 
-# Copy the cppwinrt header files
-file(GLOB HEADER_FILES ${SOURCE_PATH}/10.0.16299.0/winrt/*)
-file(
-    COPY ${HEADER_FILES}
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include/winrt
-    REGEX "\.(gitattributes|gitignore)$" EXCLUDE
+vcpkg_execute_required_process(
+    COMMAND ${SOURCE_PATH}/bin/cppwinrt.exe -input sdk -output ${SOURCE_PATH}/include
 )
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
