8ede7c834a70b6649d3ece3dfb34f6f5adb1e343
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2020-10-19 09:57:30 +0630

    [oatpp-sqlite] port added

---
+++ ports/oatpp-sqlite/portfile.cmake
@@ -0,0 +1,32 @@
+set(OATPP_VERSION "1.2.0")
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO oatpp/oatpp-sqlite
+    REF 932a79f1db5a0dfd6955c8fd84dc7bd2e5962be1 # 1.2.0
+    SHA512 12004d9339d6debc86513f78d17f9a41bf29a60d41c084e0698044b49c549b10bd3ddea35a6b2eabf8de06da54394695797bb7972888f57fc2938f1596fe03a0
+    HEAD_REF master
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(OATPP_BUILD_SHARED_LIBRARIES_OPTION "ON")
+else()
+    set(OATPP_BUILD_SHARED_LIBRARIES_OPTION "OFF")
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS
+        "-DOATPP_BUILD_TESTS:BOOL=OFF"
+        "-DCMAKE_CXX_FLAGS=-D_CRT_SECURE_NO_WARNINGS"
+        "-DBUILD_SHARED_LIBS:BOOL=${OATPP_BUILD_SHARED_LIBRARIES_OPTION}"
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/oatpp-sqlite-${OATPP_VERSION})
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
