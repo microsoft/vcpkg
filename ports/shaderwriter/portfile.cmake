afe4e196dd972b44af03a6ddf6ff880292ed2901
JackBoosY <yuzaiyang@beyondsoft.com>
2020-10-19 22:57:58 -0700

    [shaderwriter] Use vcpkg_from_github

--- ports/shaderwriter/portfile.cmake
+++ ports/shaderwriter/portfile.cmake
@@ -1,28 +1,25 @@
 
-find_program(GIT git)
+vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+    REPO DragonJoker/ShaderWriter
+    REF e978c96c959e8aa41eedaef322dcc6a0ec00ad49
+    HEAD_REF master
+    SHA512 4f1ea1fd97392edc2b3dd9b6453fa395772e75a52f28d7bbd5a63d2fe8eca40c8a0d4016e2017e126f22fc03acf28ab29db6de037d64c7687f95884b8435b7c7
+)
 
-set(GIT_URL "https://github.com/DragonJoker/ShaderWriter.git")
-set(GIT_BRANCH "master")
-set(GIT_REV "e978c96c959e8aa41eedaef322dcc6a0ec00ad49")
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${PORT}-${GIT_REV})
+vcpkg_from_github(OUT_SOURCE_PATH CMAKE_SOURCE_PATH
+    REPO DragonJoker/CMakeUtils
+    REF 6b76e6873afe33baeb59c233d3a70bd391f01769
+    HEAD_REF master
+    SHA512 d7834cf8eeff12575acc526d9dc39471ededad1ff25bec0b85e27a36381157f797c1eb168983410a40b60c001b52b787baea498aef80ffa08c9b7d9a9f5eb7ae
+)
 
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} clone ${GIT_URL} ${SOURCE_PATH}/ -b ${GIT_BRANCH}
-        WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-        LOGNAME clone
-    )
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} checkout ${GIT_REV}
-        WORKING_DIRECTORY ${SOURCE_PATH}
-        LOGNAME checkout
-    )
-    vcpkg_execute_required_process(
-        COMMAND ${GIT} submodule update --init CMake
-        WORKING_DIRECTORY ${SOURCE_PATH}
-        LOGNAME submodule
-    )
+get_filename_component(SRC_PATH ${CMAKE_SOURCE_PATH} DIRECTORY)
+if (EXISTS ${SRC_PATH}/CMake)
+    file(REMOVE_RECURSE ${SRC_PATH}/CMake)
 endif()
+file(RENAME ${CMAKE_SOURCE_PATH} ${SRC_PATH}/CMake)
+set(CMAKE_SOURCE_PATH ${SRC_PATH}/CMake)
+file(COPY ${CMAKE_SOURCE_PATH} DESTINATION ${SOURCE_PATH})
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     vcpkg_configure_cmake(
