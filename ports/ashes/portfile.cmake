de2d0975ae1a610e91d35bfb512078283be57a4a
DragonJoker <dragonjoker59@hotmail.com>
2020-10-21 12:30:51 +0200

    Taken care of vulkan-headers new package.

--- ports/ashes/portfile.cmake
+++ ports/ashes/portfile.cmake
@@ -2,36 +2,19 @@ vcpkg_fail_port_install(ON_TARGET "UWP" "iOS" "Android")
 vcpkg_fail_port_install(ON_ARCH "x86" "arm")
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
-find_program(GIT git)
-
-set(GIT_URL "https://github.com/DragonJoker/Ashes.git")
-set(GIT_BRANCH "master")
-set(GIT_REV "752a6fef9971e43744884300617ad99d78d1d631")
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/${PORT}-${GIT_REV})
-
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-	vcpkg_execute_required_process(
-		COMMAND ${GIT} clone ${GIT_URL} ${SOURCE_PATH}/ -b ${GIT_BRANCH}
-		WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-		LOGNAME clone
-	)
-	vcpkg_execute_required_process(
-		COMMAND ${GIT} checkout ${GIT_REV}
-		WORKING_DIRECTORY ${SOURCE_PATH}
-		LOGNAME checkout
-	)
-	vcpkg_execute_required_process(
-		COMMAND ${GIT} submodule update --init external/Vulkan-Headers
-		WORKING_DIRECTORY ${SOURCE_PATH}
-		LOGNAME submodule
-	)
-endif()
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO DragonJoker/Ashes
+	REF 01066539d667b21a20c97c2a70d9bf17555c8cc0
+	SHA512 f335d99ff044b388494cbbf7cbde3c9ee46eaecdc9339fc7f45fa781d23635dde3d3eaa237ed466ce50e5c1ea6e2729d8b5a6cc3930535d1252e564f32d6b778
+)
 vcpkg_configure_cmake(
 	SOURCE_PATH ${SOURCE_PATH}
 	PREFER_NINJA
 	OPTIONS
 		-DCMAKE_INSTALL_PREFIX=${CURRENT_INSTALLED_DIR}
 		-DPROJECTS_USE_PRECOMPILED_HEADERS=OFF
+		-DVCPKG_PACKAGE_BUILD=ON
 		-DASHES_BUILD_TEMPLATES=OFF
 		-DASHES_BUILD_TESTS=OFF
 		-DASHES_BUILD_INFO=OFF
@@ -39,5 +22,6 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/ashes RENAME copyright)
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/ashes)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/ashes RENAME copyright)
