30fedb3c1c1c7ab9fd821c396dfbed85312c030f
JonLiu1993 <13720414433@163.com>
2020-10-21 16:44:32 +0800

    [efsw] Fix share location

--- ports/efsw/portfile.cmake
+++ ports/efsw/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
