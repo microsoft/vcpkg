b168dd1f196b5660f96bad5029dacddc2dd7346f
Mathis Logemann <mathis.logemann.external@rheinmetall.com>
2020-10-22 21:00:24 +0200

    [restbed] patch for asio 1.18.0 and fix build by removing prefer ninja

--- ports/restbed/portfile.cmake
+++ ports/restbed/portfile.cmake
@@ -1,14 +1,8 @@
-include(vcpkg_common_functions)
-
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     openssl     BUILD_SSL 
 )
-
-if(BUILD_SSL)
-    set(ADDITIONAL_PATCH "use-FindOpenSSL-cmake.patch")
-endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -17,12 +11,12 @@ vcpkg_from_github(
     SHA512 f8aaa89ae5c862253f9a480fefa8827927e3a6c13c51938bbcd6c28ac4e6d54496ecf51610343e9a3efe3e969314f643a487506c606a65f56125b51ab8478652
     HEAD_REF master
     PATCHES
-        ${ADDITIONAL_PATCH}
+        use-FindOpenSSL-cmake.patch
+        asio-1-18-0-deprecations.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         ${FEATURE_OPTIONS}
         -DBUILD_TESTS=OFF
