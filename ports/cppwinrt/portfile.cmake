e42075afae8c43cdf588f38651bb6958e53e2767
Jared Irwin <jrairwin@sympatico.ca>
2020-10-22 19:23:16 -0400

    Convert cppwinrt to an empty port

--- ports/cppwinrt/portfile.cmake
+++ ports/cppwinrt/portfile.cmake
@@ -1,27 +1,10 @@
-vcpkg_fail_port_install(ON_TARGET linux osx)
-
-set(PACKAGE_NAME Microsoft.Windows.CppWinRT)
-set(VERSION 2.0.201008.2)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
-
-vcpkg_find_acquire_program(NUGET)
-vcpkg_execute_required_process(
-    ALLOW_IN_DOWNLOAD_MODE
-    COMMAND ${NUGET} install ${PACKAGE_NAME} -ExcludeVersion -Version ${VERSION} -NonInteractive -OutputDirectory "${SOURCE_PATH}"
-    LOGNAME nuget-${TARGET_TRIPLET}
+find_path(CPPWINRT_BASE_H
+    NAMES winrt/base.h
+    PATHS $ENV{INCLUDE}
 )
 
-set(PACKAGE_PATH ${SOURCE_PATH}/${PACKAGE_NAME})
+if(NOT CPPWINRT_BASE_H)
+    message(FATAL_ERROR "Unable to locate cppwinrt. Please install Windows SDK version 10.0.17134.0 or newer.")
+endif()
 
-vcpkg_execute_required_process(
-    COMMAND "${PACKAGE_PATH}/bin/cppwinrt.exe" -input sdk -output "${PACKAGE_PATH}/include"
-)
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${PACKAGE_PATH})
-vcpkg_configure_cmake(
-    SOURCE_PATH ${PACKAGE_PATH}
-    PREFER_NINJA
-)
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
-
-file(INSTALL ${PACKAGE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
