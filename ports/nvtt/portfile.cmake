microsoft/vcpkg

commit a97e32fa2de760252ec401f21d82d0cd3dd402c1

[netcdf-c/nvtt] Update to the latest version (#13674)

ports/nvtt/CONTROL
@@ -1,3 +1,5 @@
 Source: nvtt
-Version: 2.1.1
+Version: 2.1.2
+Homepage: https://github.com/castano/nvidia-texture-tools
 Description: Texture processing tools with support for Direct3D 10 and 11 formats.
+Supports: !x86

ports/nvtt/portfile.cmake
@@ -1,12 +1,12 @@
-include(vcpkg_common_functions)
+vcpkg_fail_port_install(ON_ARCH "x86")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO castano/nvidia-texture-tools
-    REF 2.1.1
-    SHA512 3e6fef5977ca29daa7dc97afe11d61de57a8556c9caf30902db8c5c167d9c38f736bcb62eebdaaf7558299b39975bc269d41ab980c813b67dd1fc85064c853c9
+    REF b1a90f36013522b9e5a3a2197859b72188752e3f  # 2.1.2
+    SHA512 13cf04a9856f150f7569c8c256c42fc6a5bc40586c9ca2b3ae553edf5bfcbccbba5b8538924079ed35effdd07b9e3ef4bfdb9733a2ec51f5a95f958885cc6cca
     HEAD_REF master
     PATCHES
         001-define-value-for-HAVE_UNISTD_H-in-mac-os.patch
@@ -14,6 +14,7 @@ vcpkg_from_github(
         bc7.patch
         squish.patch
         fix-build-error.patch
+        add-compile-options-for-osx.patch
 )
 
 vcpkg_configure_cmake(
@@ -37,5 +38,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
 file(REMOVE ${CURRENT_PACKAGES_DIR}/share/doc/nvtt/LICENSE)
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/nvtt)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/nvtt/LICENSE ${CURRENT_PACKAGES_DIR}/share/nvtt/copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)

ports/netcdf-c/CONTROL
@@ -1,6 +1,5 @@
 Source: netcdf-c
-Version: 4.7.3
-Port-Version: 4
+Version: 4.7.4
 Build-Depends: hdf5, curl
 Homepage: https://github.com/Unidata/netcdf-c
 Description: a set of self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data.

ports/netcdf-c/portfile.cmake
@@ -1,14 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Unidata/netcdf-c
-    REF b7cd387bee8c661141fabb490f4969587c008c55 # v4.7.3
-    SHA512 a55391620fac61e4975fe62907ca21049911afce6190fc12d183d24133a32aae8cd223b97a3fe57fc82d8bdca1a7db451046e3be3c379051624d48b1f56c0332
+    REF 26fba54a58fa02af92d84441ed90b417c1d08161 # v4.7.4
+    SHA512 7144374b5bd3574ea422de07ffb30fecc4e5f560f9b46f62762cc0cce511dd33068b8df9244fe94ae3cc7b3a9bb9fe398c7e67c3e5ac2109768e5a9b984f24fb
     HEAD_REF master
     PATCHES
         no-install-deps.patch
         config-pkg-location.patch
         use_targets.patch
-        mpi.patch
         fix-dependency-libmath.patch
 )
 

ports/netcdf-c/config-pkg-location.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 46892ee..803ff46 100644
+index bd7c4f8..e4e92c6 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -1742,7 +1742,9 @@ FILE(COPY ${netCDF_SOURCE_DIR}/include/netcdf_meta.h
+@@ -2050,7 +2050,9 @@ configure_file(${CMAKE_CURRENT_SOURCE_DIR}/ncdap_test/pingurl.c ${CMAKE_CURRENT_
  # Create CMake package configuration files. With these, other packages using
  # cmake should be able to find netcdf using find_package and find_library.
  # The EXPORT call is paired with one in liblib.

ports/netcdf-c/fix-dependency-libmath.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 35c694f..e2c03e5 100644
+index bd7c4f8..03b7ccf 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -839,7 +839,7 @@ ENDIF()
+@@ -866,7 +866,7 @@ ENDIF()
  
  # Check for the math library so it can be explicitly linked.
  IF(NOT WIN32)

ports/netcdf-c/mpi.patch
@@ -1,14 +1,0 @@
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 101f97719..b070121b8 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -1004,6 +1004,8 @@ IF(ENABLE_PARALLEL4 AND ENABLE_NETCDF_4)
-     IF(MSVC)
-       FIND_PACKAGE(MPI REQUIRED)
-       INCLUDE_DIRECTORIES(${MPI_C_INCLUDE_PATH})
-+      list(APPEND CMAKE_REQUIRED_LIBRARIES MPI::MPI_C)
-+      list(APPEND EXTRA_DEPS MPI::MPI_C)
-     ENDIF()
-     SET(HDF5_PARALLEL ON CACHE BOOL "")
-     SET(USE_PARALLEL ON CACHE BOOL "")
-

ports/netcdf-c/no-install-deps.patch
@@ -1,8 +1,8 @@
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index e87f3de..46892ee 100644
+index bd7c4f8..95fb01c 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -1570,7 +1570,7 @@ ADD_SUBDIRECTORY(docs)
+@@ -1830,7 +1830,7 @@ ADD_SUBDIRECTORY(docs)
  # install them in the binary dir. Grab all of the .libs, put them
  # in the libdir.
  ##

commit 9a227b6c944b34c0a3cbbad55dbec656b5461afc

[nvtt] Devendor libsquish
This was regressed by https://github.com/microsoft/vcpkg/pull/13674

REGRESSION: nvtt:x64-linux. If expected, add nvtt:x64-linux=fail to .\scripts\ci.baseline.txt.
REGRESSION: nvtt:x64-osx. If expected, add nvtt:x64-osx=fail to .\scripts\ci.baseline.txt.
REGRESSION: nvtt:x64-windows-static. If expected, add nvtt:x64-windows-static=fail to .\scripts\ci.baseline.txt.
REGRESSION: nvtt:x64-windows. If expected, add nvtt:x64-windows=fail to .\scripts\ci.baseline.txt.

Installing package nvtt[core]:x64-linux...
The following files are already installed in /mnt/vcpkg-ci/installed/x64-linux and are in conflict with nvtt:x64-linux

Installed by libsquish:x64-linux
    debug/lib/libsquishd.a
    include/squish.h
    lib/libsquish.a

ports/nvtt/CONTROL
@@ -1,5 +1,7 @@
 Source: nvtt
 Version: 2.1.2
+Port-Version: 1
 Homepage: https://github.com/castano/nvidia-texture-tools
 Description: Texture processing tools with support for Direct3D 10 and 11 formats.
 Supports: !x86
+Build-Depends: libsquish

ports/nvtt/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_from_github(
         squish.patch
         fix-build-error.patch
         add-compile-options-for-osx.patch
+        skip-building-libsquish.patch
 )
 
 vcpkg_configure_cmake(

ports/nvtt/skip-building-libsquish.patch
@@ -1,0 +1,13 @@
+diff --git a/extern/CMakeLists.txt b/extern/CMakeLists.txt
+index 911c19e..6f13de4 100644
+--- a/extern/CMakeLists.txt
++++ b/extern/CMakeLists.txt
+@@ -5,7 +5,6 @@ ADD_SUBDIRECTORY(EtcLib)
+ ADD_SUBDIRECTORY(rg_etc1_v104)
+ #ADD_SUBDIRECTORY(etcpack)
+ 
+-ADD_SUBDIRECTORY(libsquish-1.15)
++#ADD_SUBDIRECTORY(libsquish-1.15)
+ 
+ ADD_SUBDIRECTORY(CMP_Core)
+-