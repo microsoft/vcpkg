0dabcff73fc207310590d2e65705b518676ab743
Dawid Wróbel <me@dawidwrobel.com>
2020-10-27 15:21:24 -0400

    [libtasn1] fix building

--- ports/libtasn1/portfile.cmake
+++ ports/libtasn1/portfile.cmake
@@ -1,18 +1,24 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO gnutls/libtasn1
-    REF 4.16.0
-    SHA512 e268aaddc3972b4e89db6b470a9b132ea89a33a0b4b61fadd4cfa149349ce7080a3f42d1cb0a18949330c9c0f14151ba666c6ae64babd44e6eb8a2c004500b27
-    HEAD_REF master
+set(VERSION 4.16.0)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://ftp.gnu.org/gnu/libtasn1/libtasn1-${VERSION}.tar.gz"
+    FILENAME "libtasn1-${VERSION}.tar.gz"
+    SHA512 b356249535d5d592f9b59de39d21e26dd0f3f00ea47c9cef292cdd878042ea41ecbb7c8d2f02ac5839f5210092fe92a25acd343260ddf644887b031b167c2e71
 )
 
+vcpkg_extract_source_archive_ex(
+   OUT_SOURCE_PATH SOURCE_PATH
+   ARCHIVE ${ARCHIVE} 
+   REF ${VERSION}
+)
+
+# restore the default ac_cv_prog_cc_g flags, otherwise it fails to compile
+set(VCPKG_C_FLAGS "-g -O2") 
+
 vcpkg_configure_make(
-    AUTOCONFIG
-    COPY_SOURCE
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         --disable-doc
-        --disable-silent-rules
 )
 
 vcpkg_install_make()
@@ -20,4 +26,4 @@ vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
