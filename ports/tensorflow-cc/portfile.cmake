e2b95d1c39b0f3856186fca25e6a56a87c54bda4
jgehw <kontakt@joachim-gehweiler.de>
2020-10-27 16:41:28 +0100

    fix include file

--- ports/tensorflow-cc/portfile.cmake
+++ ports/tensorflow-cc/portfile.cmake
@@ -3,6 +3,7 @@ set(TF_PORT_SUFFIX "-cc")
 set(TF_PORT_SUFFIX_INVERSE "")
 set(TF_PATCHES_PREFIX "")
 set(TF_INCLUDE_DIRS "${INSTALL_PREFIX}/${TARGET_TRIPLET}/include/tensorflow-external;${INSTALL_PREFIX}/${TARGET_TRIPLET}/include/tensorflow-external/src")
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 include(tensorflow-common)
 
 file(COPY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/bazel-bin/tensorflow/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/tensorflow-external)
