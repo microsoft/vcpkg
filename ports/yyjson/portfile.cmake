9fef028ad446142d2540978ffa99d1e2540e5a3f
myd7349 <myd7349@gmail.com>
2020-10-28 14:34:51 +0800

    [yyjson] Add new port

---
+++ ports/yyjson/portfile.cmake
@@ -0,0 +1,21 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ibireme/yyjson
+    REF c3a1f1f54a517dc15ba8670d969c0acdfabcd039
+    SHA512 57d75ee51826f6cf8e583038ec39642e469558bdc3485d6ac5173d6dc293c48055af11fcb0c9c0be3ce782af50bb8c8453f3fec89d4450e3003c2a62237d76de
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+	OPTIONS
+	    -DYYJSON_BUILD_TESTS=OFF
+		-DYYJSON_BUILD_MISC=OFF
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
