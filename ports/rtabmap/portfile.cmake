d637c176ac1dfdfa1b966222ca2a652f2649d623
seanyen <seanyen@microsoft.com>
2020-10-29 14:45:09 -0700

    Adding rtabmap.

---
+++ ports/rtabmap/portfile.cmake
@@ -0,0 +1,18 @@
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/introlab/rtabmap/archive/0.20.3.tar.gz"
+    FILENAME "rtabmap"
+    SHA512 2f8837e00f89210b270dbd863e1088d9786774a8fcf3db593efe35384251c2bb92bd97b261f823e4aee90312e30c886b42241ed22042a540091a218a2d1819f7
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DWITH_QT=OFF
+)
+
+vcpkg_install_cmake()
