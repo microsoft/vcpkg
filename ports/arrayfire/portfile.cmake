0f186176473e8dc0635a7008fdcb398b97407d5f
jacobkahn <jacobkahn1@gmail.com>
2020-10-30 15:09:35 -0700

    Actually, don't change cmake targets in non-Windows systems

--- ports/arrayfire/portfile.cmake
+++ ports/arrayfire/portfile.cmake
@@ -63,8 +63,6 @@ vcpkg_install_cmake()
 
 if (VCPKG_TARGET_IS_WINDOWS)
   vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
-else()
-  vcpkg_fixup_cmake_targets(CONFIG_PATH share/ArrayFire/cmake)
 endif()
 
 vcpkg_copy_pdbs()
