f2f0a3ada182a9b8a06a5ca5c1a2528f41dc8f29
Silvio Traversaro <silvio@traversaro.it>
2020-10-31 15:34:38 +0100

    [ignition-transport9] Enable installation of .pc files

--- ports/ignition-transport9/portfile.cmake
+++ ports/ignition-transport9/portfile.cmake
@@ -2,6 +2,4 @@ include(${CURRENT_INSTALLED_DIR}/share/ignitionmodularscripts/ignition_modular_l
 
 ignition_modular_library(NAME transport
                          VERSION "9.0.0"
-                         SHA512 c3edb7a8a063b4aa5826838ae08c8ec2b3d14563492022df632a719409c95272f4f6a43d91f0c317e44b85921b5aedc1685670b81a7baa949f01af3b3534d76e
-                         # This can  be removed when the pc file of libuuid on Windows is fixed
-                         DISABLE_PKGCONFIG_INSTALL)
+                         SHA512 c3edb7a8a063b4aa5826838ae08c8ec2b3d14563492022df632a719409c95272f4f6a43d91f0c317e44b85921b5aedc1685670b81a7baa949f01af3b3534d76e)
