9973073c539514e7c752ffd799c3d9e5bea54a2b
Antoine Hoarau <antoine@flr.io>
2020-10-31 13:05:55 +0100

    remove common3

--- ports/ignition-common3/portfile.cmake
+++
@@ -1,6 +0,0 @@
-include(${CURRENT_INSTALLED_DIR}/share/ignitionmodularscripts/ignition_modular_library.cmake)
-
-ignition_modular_library(NAME common
-                         VERSION "1.1.1"
-                         REF ignition-common_1.1.1
-                         SHA512 e96e82dc401281cd31843f4b0ae2c1d23589170869621ea62eb6d6b31b11bd622c14da7046b1993c8fc67a1d39bae9a96b9f8efc8923e305823f963d864975f7)
