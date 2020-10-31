1525b4961429b1648e3c0eda494bad0bb312e23e
Silvio Traversaro <silvio@traversaro.it>
2020-10-31 15:34:01 +0100

    Update portfile.cmake

--- ports/ignition-transport8/portfile.cmake
+++ ports/ignition-transport8/portfile.cmake
@@ -2,6 +2,4 @@ include(${CURRENT_INSTALLED_DIR}/share/ignitionmodularscripts/ignition_modular_l
 
 ignition_modular_library(NAME transport
                          VERSION "8.1.0"
-                         SHA512 eb64f18721190fcb79a5b45746fd44fa24274c6fe6c5021dd9306c15a327873377d07d4aa770633982038b84da650d3d0c8a56169222c0c88fa1318314fc4529
-                         # This can  be removed when the pc file of libuuid on Windows is fixed
-                         DISABLE_PKGCONFIG_INSTALL)
+                         SHA512 eb64f18721190fcb79a5b45746fd44fa24274c6fe6c5021dd9306c15a327873377d07d4aa770633982038b84da650d3d0c8a56169222c0c88fa1318314fc4529)
