4f24ca1b6a4468029962c50f407af92e385a92e6
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-11-01 22:41:32 +0100

    add features to qt5compat.

--- ports/qt5compat/portfile.cmake
+++ ports/qt5compat/portfile.cmake
@@ -3,13 +3,22 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
 set(${PORT}_PATCHES)
 
-# General features:
-# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    # "appstore-compliant"  QT_FEATURE_appstore-compliant
-    # )
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+FEATURES
+    "textcodec"     QT_FEATURE_textcodec
+    "codecs"        QT_FEATURE_codecs
+    "big-codecs"    QT_FEATURE_big_codecs
+    "iconv"         QT_FEATURE_iconv
+    "iconv"         CMAKE_DISABLE_FIND_PACKAGE_ICU
+INVERTED_FEATURES
+    "iconv"         CMAKE_DISABLE_FIND_PACKAGE_WrapIconv
+    )
 
- set(TOOL_NAMES)
+#For iconv feature to work the following must be true:
+#CONDITION NOT QT_FEATURE_icu AND QT_FEATURE_textcodec AND NOT WIN32 AND NOT QNX AND NOT ANDROID AND NOT APPLE AND WrapIconv_FOUND
+# TODO check if qtbase was built with ICU and fail if iconv is given here. 
 
+set(TOOL_NAMES)
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
                      CONFIGURE_OPTIONS
