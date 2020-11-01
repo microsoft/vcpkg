b807855d19e4e1eb7da5ef91fbebea9efd7f3787
heymamd1 <Michael.Heyman@jhuapl.edu>
2020-11-01 08:52:37 -0500

    [oatpp-zlib]: has v1.2.0

---
+++ ports/oatpp-zlib/portfile.cmake
@@ -0,0 +1,33 @@
+set(OATPP_VERSION "1.2.0")
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO oatpp/oatpp-zlib
+    REF b3a0c1d140175d8ec43ebbe1da9ac46e2baa142c # 1.2.0
+    SHA512 a6a012ad5294f994a5a65a57ccfd8b9af77515d0c203d04193a2202996b3a831f314c0847a3f5ebc1867835fbd0c438284142c070998ed26881fdc1110a3c5ad
+    HEAD_REF master
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(OATPP_BUILD_SHARED_LIBRARIES_OPTION "ON")
+else()
+    set(OATPP_BUILD_SHARED_LIBRARIES_OPTION "OFF")
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS
+        "-DOATPP_BUILD_TESTS:BOOL=OFF"
+        "-DCMAKE_CXX_FLAGS=-D_CRT_SECURE_NO_WARNINGS"
+        "-DBUILD_SHARED_LIBS:BOOL=${OATPP_BUILD_SHARED_LIBRARIES_OPTION}"
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/oatpp-zlib-${OATPP_VERSION})
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
