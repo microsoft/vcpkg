2b43c7b15320d537a7dc584b6581126b28481904
Shimon Doodkin <helpmepro1@gmail.com>
2020-11-02 21:58:38 +0200

    add linux support to ports/dataframe

--- ports/dataframe/portfile.cmake
+++ ports/dataframe/portfile.cmake
@@ -18,6 +18,11 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/dataframe)
+else()
+    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/dataframe)
+endif()
 
 file( REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/cmake
