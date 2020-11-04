0b5483f5c670c6abf5db9c2614194d31895dcb0a
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-11-04 21:01:19 +0100

    [vcpkg] copy tools pdb if they exist.

--- scripts/cmake/vcpkg_clean_executables_in_bin.cmake
+++ scripts/cmake/vcpkg_clean_executables_in_bin.cmake
@@ -30,6 +30,8 @@ function(vcpkg_clean_executables_in_bin)
         file(REMOVE
             "${CURRENT_PACKAGES_DIR}/bin/${file_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
             "${CURRENT_PACKAGES_DIR}/debug/bin/${file_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
+            "${CURRENT_PACKAGES_DIR}/bin/${file_name}.pdb"
+            "${CURRENT_PACKAGES_DIR}/debug/bin/${file_name}.pdb"
         )
     endforeach()
 
