42278e0a72815ca907325d777ffa9439cb01f03d
victor <victor.burckel@gmail.com>
2020-11-04 09:22:12 +0100

    Backport fix for llvm-mingw build

--- ports/boost-iostreams/portfile.cmake
+++ ports/boost-iostreams/portfile.cmake
@@ -6,7 +6,9 @@ vcpkg_from_github(
     REF boost-1.74.0
     SHA512 3d1899ed258dff8cc20c7b725159f3fd40550f8e47b450dc6009a49499e209c07ee71598803eff1f7daca30629a288af4b286f629dfbb72677dc5d23eb2c6efb
     HEAD_REF master
-    PATCHES Removeseekpos.patch
+    PATCHES
+        0001-Fix-build-on-windows-with-libc-106.patch
+        Removeseekpos.patch
 )
 
 include(${CURRENT_INSTALLED_DIR}/share/boost-build/boost-modular-build.cmake)
