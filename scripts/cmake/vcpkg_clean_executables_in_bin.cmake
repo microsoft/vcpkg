60d70ba26662010e831e0746b76b4a7bb37992df
JackBoosY <yuzaiyang@beyondsoft.com>
2020-11-03 22:00:44 -0800

    [vcpkg_clean_executables_in_bin] clean up the pdbs

--- scripts/cmake/vcpkg_clean_executables_in_bin.cmake
+++ scripts/cmake/vcpkg_clean_executables_in_bin.cmake
@@ -31,6 +31,12 @@ function(vcpkg_clean_executables_in_bin)
             "${CURRENT_PACKAGES_DIR}/bin/${file_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
             "${CURRENT_PACKAGES_DIR}/debug/bin/${file_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
         )
+        if (EXISTS ${CURRENT_PACKAGES_DIR}/bin/${file_name}.pdb)
+            file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/${file_name}.pdb)
+        endif()
+        if (EXISTS ${CURRENT_PACKAGES_DIR}/debug/bin/${file_name}.pdb)
+            file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/${file_name}.pdb)
+        endif()
     endforeach()
 
     function(try_remove_empty_directory directory)
