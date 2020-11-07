2f39e3c3e0fca2c7dfdbe0e383164e0c9a372949
Seeker <meaningseeking@protonmail.com>
2020-11-07 06:48:15 -0800

    Update portfile.cmake

--- ports/alembic/portfile.cmake
+++ ports/alembic/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO alembic/alembic
     REF 7e5cf9b896f4299117457f36a7bf47d962cd0ebf # 1.7.16
-    SHA512 0
+    SHA512 aeb449890874fa3a89a72245f3e63a3370332d6becdf20bc77bd9c216bbe1e4578018bbe559c06df69db199799d071399f925a91c8fa816e0eec2d2420f091e9
     HEAD_REF master
     PATCHES
         fix-find-openexr-ilmbase.patch
