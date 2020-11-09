4ef10dc924ad43cd685619a184562bccb43344e1
JonLiu1993 <13720414433@163.com>
2020-11-09 17:30:23 +0800

    [libheif] update to 1.9.1

--- ports/libheif/portfile.cmake
+++ ports/libheif/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 2e218ef40440e258b5da1e513f72c7f3b1611c7c # v1.9.1
     SHA512 78fc62813f088133dfc12799d8e1989580630e80865e33e43450ae4bba0d9ef03fe250dcc734f7905ea1d02dcb7ae77a9b461b25da27fcb2ef98562c69ab0b87 
     HEAD_REF master
+    PATCHES
+        dont_build_examples_and_gdk_pixbuf.patch
 )
 
 vcpkg_configure_cmake(
