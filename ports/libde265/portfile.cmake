36d9ee7c3478720a0455a5e14d6f3621efcc8bf8
JonLiu1993 <13720414433@163.com>
2020-11-12 15:26:36 +0800

    Updated libde265 to v1.0.8

--- ports/libde265/portfile.cmake
+++ ports/libde265/portfile.cmake
@@ -18,13 +18,12 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/libde265/)
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/libde265)
-file(RENAME ${CURRENT_PACKAGES_DIR}/bin/dec265.exe ${CURRENT_PACKAGES_DIR}/tools/libde265/dec265)
-file(RENAME ${CURRENT_PACKAGES_DIR}/bin/enc265.exe ${CURRENT_PACKAGES_DIR}/tools/libde265/enc265)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/dec265.exe ${CURRENT_PACKAGES_DIR}/tools/libde265/dec265)
-file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/enc265.exe ${CURRENT_PACKAGES_DIR}/tools/libde265/enc265)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    vcpkg_copy_tools(TOOL_NAMES dec265 enc265 AUTO_CLEAN)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
