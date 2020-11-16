8d162708306f848c9c04593b1ae02a322afe10a7
Nemirtingas <nanaki89@hotmail.fr>
2020-11-16 10:36:01 +0100

    Update libdatachannel to v0.10.1

--- ports/libdatachannel/portfile.cmake
+++ ports/libdatachannel/portfile.cmake
@@ -1,19 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO paullouisageneau/libdatachannel
-    REF d59c209c2044b74b8a60f1de68aadea28298e6b3
-    SHA512 35a01349e7038a7d1776d154ee2c111d431d29a426b4948a6288017d30a0baa71f497dc694996085811d7860e40c5e6906a3fb07150bda87f4d9ed56db9fec70
+    REF v0.10.1
+    SHA512 195f564776b2a7ab6fe3a838d55be7fa03b72a5f08faaba5ddefac59ae1fff28f3ee0b9ed5dc17a7ef9155a1d30a61f8c593f2a108f60e415ffd213188e081bd
     HEAD_REF master
     PATCHES
         fix-for-vcpkg.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    srtp USE_SRTP
-
     INVERTED_FEATURES
     ws NO_WEBSOCKET
+    srtp NO_MEDIA
 )
 
 vcpkg_configure_cmake(
@@ -21,6 +19,7 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DUSE_SYSTEM_SRTP=ON
         -DNO_EXAMPLES=ON
         -DNO_TESTS=ON
 )
