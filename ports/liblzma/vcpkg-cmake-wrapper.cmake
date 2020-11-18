54299f533fb494cb532e1e17865a37f9009756e6
Alexander Neumann <alexander.neumann@hamburg.de>
2020-11-18 13:19:23 +0100

    fix wrapper

--- ports/liblzma/vcpkg-cmake-wrapper.cmake
+++ ports/liblzma/vcpkg-cmake-wrapper.cmake
@@ -4,5 +4,5 @@ list(REMOVE_ITEM ARGS "MODULE") # Always call CONFIG.
 _find_package(${ARGS} CONFIG)
 if(NOT TARGET LibLZMA::LibLZMA AND TARGET liblzma::liblzma)
     add_library(LibLZMA::LibLZMA INTERFACE IMPORTED) # Too lazy to fix wrong target usage all over vcpkg. 
-    set_target_properties(LibLZMA::LibLZMA INTERFACE_LINK_LIBRARIES liblzma::liblzma)
+    set_target_properties(LibLZMA::LibLZMA PROPERTIES INTERFACE_LINK_LIBRARIES liblzma::liblzma)
 endif()
