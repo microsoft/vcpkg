cf5a972622a47c64f484dfd0e646ee88770f4542
Mathis Logemann <mathis.logemann.external@rheinmetall.com>
2020-11-18 14:09:19 +0100

    fix typo in cmake...

--- ports/mapnik/portfile.cmake
+++ ports/mapnik/portfile.cmake
@@ -70,7 +70,7 @@ endif()
 
 if("viewer" IN_LIST FEATURES)
   # copy the ini file to reference the plugins correctly
-  file(COPY $${CMAKE_CURRENT_LIST_DIR}/viewer.ini DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+  file(COPY ${CMAKE_CURRENT_LIST_DIR}/viewer.ini DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
   vcpkg_copy_tools(TOOL_NAMES mapnik-viewer AUTO_CLEAN)
 endif()
 
