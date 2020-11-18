693f82301e99de00874b1e2128e9fcb1c2fd9277
Mathis Logemann <mathis.logemann.external@rheinmetall.com>
2020-11-18 11:42:20 +0100

    functional build with mapnik master

--- ports/mapnik/portfile.cmake
+++ ports/mapnik/portfile.cmake
@@ -1,27 +1,16 @@
-find_program(GIT git)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mapnik/mapnik
+    REF c6fc956a779123e3735c16ba28aa85922b924cac
+    SHA512 004429defd3047601b63c28bedd57d701ff88cdfb910a9958f4a10a1ac032d4d13316372122aef06eda60f2e4f67600b8842577db455f8c970009a0e86c5c1cf
+    HEAD_REF master
+    PATCHES
+      fix-config.patch
+      fix-box2d.patch
+      fix-constructor-inheritance.patch
+)
 
-set(GIT_URL "https://github.com/am2222/mapnik-windows.git")
-set(GIT_REV "cfa268fa6ab438568b9b5320f6fce4317f0e3d8e")
-
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
-
-
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-	message(STATUS "Cloning and fetching submodules")
-	vcpkg_execute_required_process(
-	  COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
-	  WORKING_DIRECTORY ${SOURCE_PATH}
-	  LOGNAME clone
-	)
-
-	message(STATUS "Checkout revision ${GIT_REV}")
-	vcpkg_execute_required_process(
-	  COMMAND ${GIT} checkout ${GIT_REV}
-	  WORKING_DIRECTORY ${SOURCE_PATH}
-	  LOGNAME checkout
-	)
-endif()
-
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake/ DESTINATION ${SOURCE_PATH})
 
 vcpkg_find_acquire_program(PYTHON2)
 get_filename_component(PYTHON2_DIR "${PYTHON2}" DIRECTORY)
@@ -34,26 +23,27 @@ elseif (VCPKG_LIBRARY_LINKAGE STREQUAL static)
 endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    cairo   WITH_CAIRO
-    demo  WITH_DEMO
-    input_csv WITH_INPUT_CSV
-    input_gdal WITH_INPUT_GDAL
-    input_geobuf  WITH_INPUT_GEOBUF
-    input_geojson   WITH_INPUT_GEOJSON
-    input_ogr WITH_INPUT_OGR
-    input_pgraster  WITH_INPUT_PGRASTER
-    input_postgis  WITH_INPUT_POSTGIS
-    input_raster  WITH_INPUT_RASTER
-    input_shape WITH_INPUT_SHAPE
-    input_sqlite WITH_INPUT_SQLITE
-    input_topojson  WITH_INPUT_TOPOJSON
-    proj4 WITH_PROJ4
-    utils WITH_UTILS
+    cairo             WITH_CAIRO
+    demo              WITH_DEMO
+    viewer            WITH_VIEWER
+    "input-csv"       WITH_INPUT_CSV
+    "input-gdal"      WITH_INPUT_GDAL
+    "input-geobuf"    WITH_INPUT_GEOBUF
+    "input-geojson"   WITH_INPUT_GEOJSON
+    "input-ogr"       WITH_INPUT_OGR
+    "input-pgraster"  WITH_INPUT_PGRASTER
+    "input-postgis"   WITH_INPUT_POSTGIS
+    "input-raster"    WITH_INPUT_RASTER
+    "input-shape"     WITH_INPUT_SHAPE
+    "input-sqlite"    WITH_INPUT_SQLITE
+    "input-topojson"  WITH_INPUT_TOPOJSON
+    proj4             WITH_PROJ4
+    utils             WITH_UTILS
 )
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS   
         ${FEATURE_OPTIONS}
         -DMAPNIK_STATIC_LIB=${MAPNIK_STATIC_LIB}
@@ -67,17 +57,22 @@ file(COPY ${SOURCE_PATH}/include/${PORT} DESTINATION ${CURRENT_PACKAGES_DIR}/inc
 
 vcpkg_copy_pdbs()
 
-if ("utils" IN_LIST FEATURES)
-    vcpkg_copy_tools(
-      TOOL_NAMES mapnik-demo mapnik-render shapeindex
-      AUTO_CLEAN
-    )
-    
-    file(COPY ${SOURCE_PATH}/demo/data DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik/demo)
-    file(COPY ${SOURCE_PATH}/fonts DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
-    file(COPY ${CURRENT_PACKAGES_DIR}/bin/plugins DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
+if("demo" IN_LIST FEATURES)
+  file(COPY ${SOURCE_PATH}/demo/data DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik/demo)
+  vcpkg_copy_tools(TOOL_NAMES mapnik-demo AUTO_CLEAN)
+endif()
 
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/mapnik-demo ${CURRENT_PACKAGES_DIR}/debug/bin/mapnik-render ${CURRENT_PACKAGES_DIR}/debug/bin/shapeindex)
+if("viewer" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES mapnik-viewer AUTO_CLEAN)
+endif()
+
+if ("utils" IN_LIST FEATURES)
+  vcpkg_copy_tools(
+    TOOL_NAMES mapnik-render shapeindex
+    AUTO_CLEAN
+  )
+  file(COPY ${SOURCE_PATH}/fonts DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
+  #file(COPY ${CURRENT_PACKAGES_DIR}/bin/plugins DESTINATION ${CURRENT_PACKAGES_DIR}/tools/mapnik)
 endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
