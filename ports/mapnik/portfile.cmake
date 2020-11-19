f4091b14364a2554207e8c62efc0797432d81b05
Mathis Logemann <mathisloge@gmail.com>
2020-11-19 10:57:42 +0100

    only fail on windows static

--- ports/mapnik/portfile.cmake
+++ ports/mapnik/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_fail_port_install(ON_LIBRARY_LINKAGE "static")
+vcpkg_fail_port_install(ON_TARGET Windows ON_LIBRARY_LINKAGE "static")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mapnik/mapnik
