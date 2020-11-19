4a1d11d9471fcd15b455273e3d9d154eda8b3a59
JackBoosY <yuzaiyang@beyondsoft.com>
2020-11-19 02:01:21 -0800

    [gdal] add json-c as a dependency on non-windows

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -185,7 +185,7 @@ else()
     endif()
     
     set(CONF_OPTS --enable-shared=${BUILD_DYNAMIC} --enable-static=${BUILD_STATIC})
-    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR})
+    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR} --with-libjson-c=${CURRENT_INSTALLED_DIR})
     
     vcpkg_configure_make(
         SOURCE_PATH ${SOURCE_PATH}
