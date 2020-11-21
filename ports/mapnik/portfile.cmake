c6fd59b4094b46afd99abb4aedfe97a75c3e559d
Mathis Logemann <mathisloge@gmail.com>
2020-11-21 13:49:06 +0100

    run a first test with upstream cmake

--- ports/mapnik/portfile.cmake
+++ ports/mapnik/portfile.cmake
@@ -1,40 +1,40 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO mapnik/mapnik
-    REF 4227fd5d698d069a92917cfea9ce9139435d3f12
-    SHA512 913e725a05e315b8630928fec7635ccddac833428800cd3f1b5c25226952f00883dbe9b007eb36bfd0330eac001608596fb53d08aa3df2fce297a3aab3bbf935
+    REPO mathisloge/mapnik
+    REF cmake-support
+    SHA512 69aab419d74e9bf70a54f3a900e28a8f2721cad13c167fb2ce44dd8d548a9d54fd2983417b8757e29cacf79efbaf11cfe405e000f8fdeb399aa3bf5ab1c9c213
     HEAD_REF master
-    PATCHES
-        fix-box2d.patch
-        fix-constructor-inheritance.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake/ DESTINATION ${SOURCE_PATH})
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  set(MAPNIK_STATIC_LIB OFF)
-elseif (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-  set(MAPNIK_STATIC_LIB ON)
-endif()
-
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    cairo             WITH_CAIRO
-    demo              WITH_DEMO
-    viewer            WITH_VIEWER
-    "input-csv"       WITH_INPUT_CSV
-    "input-gdal"      WITH_INPUT_GDAL
-    "input-geobuf"    WITH_INPUT_GEOBUF
-    "input-geojson"   WITH_INPUT_GEOJSON
-    "input-ogr"       WITH_INPUT_OGR
-    "input-pgraster"  WITH_INPUT_PGRASTER
-    "input-postgis"   WITH_INPUT_POSTGIS
-    "input-raster"    WITH_INPUT_RASTER
-    "input-shape"     WITH_INPUT_SHAPE
-    "input-sqlite"    WITH_INPUT_SQLITE
-    "input-topojson"  WITH_INPUT_TOPOJSON
-    proj4             WITH_PROJ4
-    utils             WITH_UTILS
+    jpeg              USE_JPEG
+    png               USE_PNG
+    tiff              USE_TIFF
+    webp              USE_WEBP
+    libxml2           USE_LIBXML2
+    cairo             USE_CAIRO
+    proj4             USE_PROJ4
+    "input-csv"       USE_PLUGIN_INPUT_CSV
+    "input-gdal"      USE_PLUGIN_INPUT_GDAL
+    "input-geobuf"    USE_PLUGIN_INPUT_GEOBUF
+    "input-geojson"   USE_PLUGIN_INPUT_GEOJSON
+    "input-ogr"       USE_PLUGIN_INPUT_OGR
+    "input-pgraster"  USE_PLUGIN_INPUT_PGRASTER
+    "input-postgis"   USE_PLUGIN_INPUT_POSTGIS
+    "input-raster"    USE_PLUGIN_INPUT_RASTER
+    "input-shape"     USE_PLUGIN_INPUT_SHAPE
+    "input-sqlite"    USE_PLUGIN_INPUT_SQLITE
+    "input-topojson"  USE_PLUGIN_INPUT_TOPOJSON
+    viewer            BUILD_DEMO_VIEWER
+    demo              BUILD_DEMO_CPP
+    "utility-geometry-to-wkb" BUILD_UTILITY_GEOMETRY_TO_WKB
+    "utility-mapnik-index" BUILD_UTILITY_MAPNIK_INDEX
+    "utility-mapnik-render" BUILD_UTILITY_MAPNIK_RENDER
+    "utility-ogrindex" BUILD_UTILITY_OGRINDEX
+    "utility-pgsql2sqlite" BUILD_UTILITY_PGSQL2SQLITE
+    "utility-shapeindex" BUILD_UTILITY_SHAPEINDEX
+    "utility-svg2png" BUILD_UTILITY_SVG2PNG
 )
 
 vcpkg_configure_cmake(
@@ -42,10 +42,12 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS   
         ${FEATURE_OPTIONS}
-        -DMAPNIK_STATIC_LIB=${MAPNIK_STATIC_LIB}
-        -DBOOST_PREFIX=${CURRENT_INSTALLED_DIR}/include
-        -DFREE_TYPE_INCLUDE=${CURRENT_INSTALLED_DIR}/include/freetype2
-        -DCMAKE_INSTALL_INCLUDEDIR=${CURRENT_PACKAGES_DIR}/include
+        -DBUILD_SHARED_LIBS=ON
+        -DBUILD_TEST=OFF
+        -DUSE_EXTERNAL_MAPBOX_GEOMETRY=ON
+        -DUSE_EXTERNAL_MAPBOX_POLYLABEL=ON
+        -DUSE_EXTERNAL_MAPBOX_PROTOZERO=ON
+        -DUSE_EXTERNAL_MAPBOX_VARIANT=ON
 )
 
 vcpkg_install_cmake()
@@ -53,6 +55,7 @@ vcpkg_install_cmake()
 # copy plugins into tool path, if any plugin is installed
 if(IS_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin/plugins)
   file(COPY ${CURRENT_PACKAGES_DIR}/bin/plugins DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+  #file(COPY ${SOURCE_PATH}/fonts DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
 endif()
 vcpkg_copy_pdbs()
 
@@ -67,12 +70,28 @@ if("viewer" IN_LIST FEATURES)
   vcpkg_copy_tools(TOOL_NAMES mapnik-viewer AUTO_CLEAN)
 endif()
 
-if ("utils" IN_LIST FEATURES)
-  vcpkg_copy_tools(
-    TOOL_NAMES mapnik-render shapeindex
-    AUTO_CLEAN
-  )
-  file(COPY ${SOURCE_PATH}/fonts DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+if("utility-geometry-to-wkb" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES geometry_to_wkb AUTO_CLEAN)
+endif()
+
+if("utility-mapnik-index" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES mapnik-index AUTO_CLEAN)
+endif()
+if("utility-mapnik-render" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES mapnik-render AUTO_CLEAN)
+endif()
+if("utility-ogrindex" IN_LIST FEATURES)
+  # build is currently not supported
+  # vcpkg_copy_tools(TOOL_NAMES ogrindex AUTO_CLEAN)
+endif()
+if("utility-pgsql2sqlite" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES pgsql2sqlite AUTO_CLEAN)
+endif()
+if("utility-shapeindex" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES shapeindex AUTO_CLEAN)
+endif()
+if("utility-svg2png" IN_LIST FEATURES)
+  vcpkg_copy_tools(TOOL_NAMES svg2png AUTO_CLEAN)
 endif()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mapnik)
