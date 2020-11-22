a0f0cccad723e430c3f64d8940030f839e441fd8
yurybura <yurybura@gmail.com>
2020-11-22 13:12:28 +0300

    [llvm] add /Zc:__cplusplus

--- ports/llvm/portfile.cmake
+++ ports/llvm/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_github(
     SHA512 b6d38871ccce0e086e27d35e42887618d68e57d8274735c59e3eabc42dee352412489296293f8d5169fe0044936345915ee7da61ebdc64ec10f7737f6ecd90f2
     HEAD_REF master
     PATCHES
-        0001-add-bigobj-option.patch    # fixed in master
+        0001-add-msvc-options.patch     # fixed in master
         0003-fix-openmp-debug.patch
         0004-fix-dr-1734.patch
         0005-fix-tools-path.patch
