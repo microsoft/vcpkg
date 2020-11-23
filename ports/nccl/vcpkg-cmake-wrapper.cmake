53fac8de1c517dcc63f419974c5c85f5560a728c
jacobkahn <jacobkahn1@gmail.com>
2020-11-23 15:30:18 -0800

    Move usage to usage file

--- ports/nccl/vcpkg-cmake-wrapper.cmake
+++ ports/nccl/vcpkg-cmake-wrapper.cmake
@@ -1,14 +1,3 @@
-# Try to find NCCL on the local system.
-#
-# Usage:
-# ```cmake
-# find_package(NCCL [version] [EXACT] [QUIET] MODULE [REQUIRED])
-#
-# target_link_libraries(main PRIVATE ${NCCL_LIBRARIES}
-# target_include_directories(main PRIVATE ${NCCL_INCLUDE_DIRS})
-# ```
-#
-
 set(NCCL_PREV_MODULE_PATH ${CMAKE_MODULE_PATH})
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
