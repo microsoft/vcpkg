13bdd9bccc96007c48fc7cb4875a970100f74440
jacobkahn <jacobkahn1@gmail.com>
2020-11-23 20:54:21 -0800

    [flashlight-cuda] New Port

---
+++ ports/flashlight-cuda/portfile.cmake
@@ -0,0 +1,55 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebookresearch/flashlight
+    REF ed1dc8bf4855ae7288494aa77da7f6cbe3f995e3
+    SHA512 de41546e19b9c3388236a1a26f560271b4c4259042ed8846edd52b29a3ab4d9fe4b015b6638ccc9f2dbee322fed0d165398e3faff805895e2e58e5ca2e711fc3
+    HEAD_REF master
+)
+
+################################### Build ###################################
+# Default flags
+set(FL_DEFAULT_VCPKG_CMAKE_FLAGS
+  -DFL_BUILD_TESTS=OFF
+  -DFL_BUILD_EXAMPLES=OFF
+  -DFL_BACKEND=CUDA # this port is CUDA-backend only
+  -DFL_BUILD_STANDALONE=OFF
+  -DFL_INSTALL_CMAKE_DIR=${CURRENT_PACKAGES_DIR}/share/${PORT} # for CMake configs/targets
+  )
+
+# Determine which backend to build via specified feature
+vcpkg_check_features(
+  OUT_FEATURE_OPTIONS FL_BACKEND_FEATURE_OPTIONS
+  FEATURES
+    lib FL_BUILD_LIBRARIES
+    fl FL_BUILD_CORE
+    asr FL_BUILD_APP_ASR
+    imgclass FL_BUILD_APP_IMG_CLASS
+)
+
+# Build and install
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS ${FL_DEFAULT_VCPKG_CMAKE_FLAGS} ${FL_BACKEND_FEATURE_OPTIONS}
+)
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Binaries/tools
+set(FLASHLIGHT_TOOLS "")
+if ("imgclass" IN_LIST FEATURES)
+  list(APPEND FLASHLIGHT_TOOLS fl_imageNetResnet34)
+endif()
+if ("asr" IN_LIST FEATURES)
+  list(APPEND FLASHLIGHT_TOOLS fl_asr_train fl_asr_test fl_asr_decode)
+endif()
+list(LENGTH FLASHLIGHT_TOOLS NUM_TOOLS)
+if (NUM_TOOLS GREATER 0)
+  vcpkg_copy_tools(TOOL_NAMES ${FLASHLIGHT_TOOLS} AUTO_CLEAN)
+endif()
+
+# Copyright and license
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/flashlight-cuda RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/flashlight-cuda RENAME license)
