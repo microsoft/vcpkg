e14dbb3924902ef437ca7cebbfe82eb7cd163d85
JonLiu1993 <13720414433@163.com>
2020-11-26 11:14:53 +0800

    Modify feature options

--- ports/redis-plus-plus/portfile.cmake
+++ ports/redis-plus-plus/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sewenew/redis-plus-plus
-    REF a9f9c301f8de1c181e6d45c573b5d1fe7b8200b1 # 1.2.1
+    REF a9f9c301f8de1c181e6d45c573b5d1fe7b8200b1 # v1.2.1
     SHA512 038641a91a2d62f8b07b548fb16f03e87a014fc3a53e59d95e9d31d707e800de4922838bc1bc181d642bb6ac23eae877e0a8724548d9d8f113ebc8d744384abf
     HEAD_REF master
     PATCHES
