e9e479eb8ceebdd900f545ed88c7d28c9b50f4c8
Rémy Tassoux <contact@rt2.fr>
2020-11-27 13:34:53 +0100

    [pugixml] Update to 1.11
    Changelog : https://github.com/zeux/pugixml/releases/tag/v1.11
    
    I removed the patch because it was fixed upsteam : https://github.com/zeux/pugixml/pull/363

--- ports/pugixml/portfile.cmake
+++ ports/pugixml/portfile.cmake
@@ -1,10 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeux/pugixml
-    REF v1.10
-    SHA512 0634053d4f757b9293997763bb2e51595197c192f3974e954975d6d6ff91e4a6cb7c194efa530e0eef1a2a93db16592c5630010b6482430dff5ffc6879e84b6a
+    REF v1.11
+    SHA512 9b902d158ef044b49638e42e769045f42559f42963fdca50151143e6857fa58040e6be0e449b643bfd00fb267ab443fbc5af5e3fc9925274cedd4f586b12ae85
     HEAD_REF master
-    PATCHES pugixml-v1.10_fix_debug_pkgconfig.patch # Upstream: https://github.com/zeux/pugixml/pull/363
 )
 
 vcpkg_configure_cmake(
