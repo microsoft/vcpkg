f318219e8ba2b6afedcc1b8cb31a082bfa4cc763
JonLiu1993 <13720414433@163.com>
2020-11-30 18:59:34 +0800

    Add feature Cxx17

--- ports/redis-plus-plus/portfile.cmake
+++ ports/redis-plus-plus/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sewenew/redis-plus-plus
-    REF a9f9c301f8de1c181e6d45c573b5d1fe7b8200b1 # v1.2.1
+    REF a9f9c301f8de1c181e6d45c573b5d1fe7b8200b1 # 1.2.1
     SHA512 038641a91a2d62f8b07b548fb16f03e87a014fc3a53e59d95e9d31d707e800de4922838bc1bc181d642bb6ac23eae877e0a8724548d9d8f113ebc8d744384abf
     HEAD_REF master
     PATCHES
@@ -9,9 +9,12 @@ vcpkg_from_github(
         fix-conversion.patch
 )
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    cxx17 ENABLE_CXX17
-)
+if("cxx17" IN_LIST FEATURES)
+    set(REDIS_PLUS_PLUS_CXX_STANDARD 17)
+else()
+    set(REDIS_PLUS_PLUS_CXX_STANDARD 11)
+endif()
+
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
     set(REDIS_PLUS_PLUS_BUILD_STATIC ON)
@@ -30,6 +33,7 @@ vcpkg_configure_cmake(
         -DREDIS_PLUS_PLUS_BUILD_SHARED=${REDIS_PLUS_PLUS_BUILD_SHARED}
         -DREDIS_PLUS_PLUS_BUILD_TEST=OFF
         -DREDIS_PLUS_PLUS_CXX_STANDARD=17
+        -DREDIS_PLUS_PLUS_CXX_STANDARD=${REDIS_PLUS_PLUS_CXX_STANDARD}
 )
 
 vcpkg_install_cmake()
