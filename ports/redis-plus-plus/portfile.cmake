aed06edfd953535dabe46df14a6df8b7c23413ea
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2020-11-30 17:52:11 -0800

    Update ports/redis-plus-plus/portfile.cmake

--- ports/redis-plus-plus/portfile.cmake
+++ ports/redis-plus-plus/portfile.cmake
@@ -41,4 +41,4 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright )
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright )
