9fef9c5a9c5a3097bc11b87b2c549e679a4a97c2
Bruno Carvalho <b.camara@live.com>
2020-12-01 19:08:18 -0300

    Remove msbuild Back ARM builds to baseline

--- ports/chipmunk/portfile.cmake
+++ ports/chipmunk/portfile.cmake
@@ -6,94 +6,28 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-    #architecture detection
-    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(CHIPMUNK_ARCH Win32)
-    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(CHIPMUNK_ARCH x64)
-    else()
-    message(FATAL_ERROR "unsupported architecture")
-    endif()
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" KEYSTONE_BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" KEYSTONE_BUILD_SHARED)
 
-    #linking
-    if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-        set(CHIPMUNK_CONFIGURATION_SUFFIX " DLL")
-    else()
-        if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
-            set(CHIPMUNK_CONFIGURATION_SUFFIX "")
-        else()
-            set(CHIPMUNK_CONFIGURATION_SUFFIX " SCRT")
-        endif()
-    endif()
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS -DBUILD_DEMOS=OFF
+            -DBUILD_SHARED=${KEYSTONE_BUILD_SHARED}
+            -DBUILD_STATIC=${KEYSTONE_BUILD_STATIC}
+            -DINSTALL_STATIC=${KEYSTONE_BUILD_STATIC}
+)
 
-    vcpkg_build_msbuild(
-        PROJECT_PATH ${SOURCE_PATH}/msvc/VS2015/chipmunk/chipmunk.vcxproj
-        RELEASE_CONFIGURATION "Release${CHIPMUNK_CONFIGURATION_SUFFIX}"
-        DEBUG_CONFIGURATION "Debug${CHIPMUNK_CONFIGURATION_SUFFIX}"
-    )
+vcpkg_install_cmake()
 
-    message(STATUS "Installing")
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-        file(INSTALL
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.dll"
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-        )
-        file(INSTALL
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.dll"
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-            DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-        )
-    else()
-        file(INSTALL
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-        )
-        file(INSTALL
-            "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.pdb"
-            DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-        )
-    endif()
+if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL debug)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+endif()
 
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Debug${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.lib"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-    )
-    file(INSTALL
-        "${SOURCE_PATH}/msvc/VS2015/chipmunk/${CHIPMUNK_ARCH}/Release${CHIPMUNK_CONFIGURATION_SUFFIX}/chipmunk.lib"
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-    )
-else()
-    string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" KEYSTONE_BUILD_STATIC)
-    string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" KEYSTONE_BUILD_SHARED)
-
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS -DBUILD_DEMOS=OFF
-                -DBUILD_SHARED=${KEYSTONE_BUILD_SHARED}
-                -DBUILD_STATIC=${KEYSTONE_BUILD_STATIC}
-                -DINSTALL_STATIC=${KEYSTONE_BUILD_STATIC}
-    )
-
-    vcpkg_install_cmake()
-
-    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL debug)
-        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-        if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-            file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/src/libchipmunk.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-            file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/libchipmunk.dll")
-        endif()
-    endif()
-
-    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL release)
-        if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-            file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/src/libchipmunk.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-            file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/libchipmunk.dll")
-        endif()
-    endif()
+file(GLOB DLLS ${CURRENT_PACKAGES_DIR}/lib/*.dll)
+if(DLLS)
+    file(COPY ${DLLS} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE ${DLLS})
 endif()
 
 file(INSTALL
@@ -102,7 +36,3 @@ file(INSTALL
 )
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-    message(STATUS "Installing done")
-endif()
