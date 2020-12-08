234a0faf43c9977f02cfaef7315e4a9fc7fc1651
Pashupati Kumar <paskumar@microsoft.com>
2020-12-08 01:57:00 -0800

    Fix for Linux

--- ports/onnxruntime/portfile.cmake
+++ ports/onnxruntime/portfile.cmake
@@ -1,20 +1,14 @@
-if(NOT TARGET_TRIPLET MATCHES "(x|X)64-(windows|linux)-static-md")
-  vcpkg_fail_port_install(MESSAGE "${PORT} doesn't support ${TARGET_TRIPLET} currently" ALWAYS)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_fail_port_install(ON_LIBRARY_LINKAGE "dynamic")
+    vcpkg_fail_port_install(ON_TARGET "uwp")
+    vcpkg_fail_port_install(ON_ARCH "arm" "arm64" "wasm32" "x86")
+elseif(VCPKG_TARGET_IS_LINUX)
+    vcpkg_fail_port_install(ON_ARCH "x86")
+else()
+    vcpkg_fail_port_install(ALWAYS)
 endif()
 
 set(ORT_REVISION "v1.5.3")
-
-# check out the `https://github.com/microsoft/onnxruntime/archive/${ORT_REVISION}.tar.gz`
-# hash checksum can be obtained with `curl -L -o tmp.tgz ${URL} && vcpkg hash tmp.tgz`
-#vcpkg_from_github(
-#  OUT_SOURCE_PATH SOURCE_PATH
-#  REPO microsoft/onnxruntime
-#  REF aec4cb489e49a63fc8523ee3d22e406c2af9c22b
-#  SHA512  965046f76083c36f728d74275b7db091abb43e8eb514b45fce4223a43c1d8a0825db79f8528f0a7a68e12e3419e3467a949177a9d8e54b8a853baa0394e6f4d7
-#  HEAD_REF master
-#  PATCHES
-#    "CMakeLists.patch"
-#)
 
 vcpkg_find_acquire_program(GIT)
 get_filename_component(GIT_PATH ${GIT} DIRECTORY)
@@ -25,28 +19,20 @@ get_filename_component(NINJA_PATH ${NINJA} DIRECTORY)
 set(ONNXRUNTIME "onnxruntime.git")
 set(ONNX_GITHUB_URL "https://github.com/microsoft/${ONNXRUNTIME}")
 
-#vcpkg_from_git(
-#  OUT_SOURCE_PATH SOURCE_PATH
-#  URL "${ONNX_GITHUB_URL}"
-#  REF aec4cb489e49a63fc8523ee3d22e406c2af9c22b
-#  PATCHES
-#    "CMakeLists.patch"
-#)
-
 if(EXISTS "${CURRENT_BUILDTREES_DIR}/${ONNXRUNTIME}")
   # Purge any local changes
   vcpkg_execute_required_process(
     COMMAND ${GIT} reset --hard
     WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${ONNXRUNTIME}
     LOGNAME build-${TARGET_TRIPLET})
-  
-  # 
+
+  #
   vcpkg_execute_required_process(
     COMMAND ${GIT} submodule sync --recursive
     WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${ONNXRUNTIME}
     LOGNAME build-${TARGET_TRIPLET})
 
-  # 
+  #
   vcpkg_execute_required_process(
     COMMAND ${GIT} submodule update --init --recursive
     WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${ONNXRUNTIME}
@@ -60,16 +46,6 @@ else()
 endif()
 
 
-
-if(VCPKG_TARGET_IS_WINDOWS)
-#  list(APPEND FEATURE_OPTIONS  "Visual Studio 16 2019")
-endif()
-
-if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-#  list(APPEND FEATURE_OPTIONS -x64)
-#  list(APPEND FEATURE_OPTIONS "host=x64")
-endif()
-
 set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/${ONNXRUNTIME}")
 
 vcpkg_configure_cmake(
@@ -80,9 +56,7 @@ vcpkg_configure_cmake(
     -Donnxruntime_BUILD_WINML_TESTS=ON
     -Donnxruntime_GENERATE_TEST_REPORTS=ON
     -Donnxruntime_USE_CUDA=OFF
-    -Donnxruntime_CUDNN_HOME=
     -Donnxruntime_USE_FEATURIZERS=OFF
-    -Donnxruntime_CUDA_HOME=
     -Donnxruntime_USE_JEMALLOC=OFF
     -Donnxruntime_USE_MIMALLOC_STL_ALLOCATOR=OFF
     -Donnxruntime_USE_MIMALLOC_ARENA_ALLOCATOR=OFF
@@ -90,8 +64,6 @@ vcpkg_configure_cmake(
     -Donnxruntime_USE_EIGEN_FOR_BLAS=ON
     -Donnxruntime_USE_OPENBLAS=OFF
     -Donnxruntime_USE_DNNL=OFF
-    -Donnxruntime_DNNL_GPU_RUNTIME=
-    -Donnxruntime_DNNL_OPENCL_ROOT=
     -Donnxruntime_USE_MKLML=OFF
     -Donnxruntime_USE_NNAPI_BUILTIN=OFF
     -Donnxruntime_USE_RKNPU=OFF
@@ -131,79 +103,99 @@ vcpkg_configure_cmake(
     -Donnxruntime_USE_NCCL=ON
     -Donnxruntime_BUILD_BENCHMARKS=OFF
     -Donnxruntime_USE_ROCM=OFF
-    -Donnxruntime_ROCM_HOME=
     -Donnxruntime_PYBIND_EXPORT_OPSCHEMA=OFF
     -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
     -DCMAKE_BUILD_TYPE=RelWithDebInfo
     ${FEATURE_OPTIONS}
-  OPTIONS_RELEASE
-
-  OPTIONS_DEBUG
-
 )
 
 vcpkg_install_cmake()
 
+message(STATUS "Copy libs and additional header files!")
+# Copy lib and PDB files
 if(VCPKG_TARGET_IS_WINDOWS)
-  if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug") 
-    # copy the onnxruntime_*.libs
-    message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
-    file(GLOB DEBUG_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/*.lib)
-    file(COPY
-        ${DEBUG_LIBS}
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-        )
-    # copy their corresponding *.pdbs
-    file(GLOB DEBUG_LIB_PDBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/*.pdb)
-    file(COPY
-        ${DEBUG_LIB_PDBS}
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-        )
-  endif()
-
-  if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    # copy the onnxruntime_*.libs
-    message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-    file(GLOB DEBUG_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/*.lib)
-    file(COPY
-        ${DEBUG_LIBS}
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-        )
-
-    # copy their corresponding *.pdbs
-    file(GLOB DEBUG_LIB_PDBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-release/*.pdb)
-    file(COPY
-        ${DEBUG_LIB_PDBS}
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-        )
-  endif()
-
-  # copy library from external modules
-  foreach(BUILD_TYPE rel dbg)
-    if(${BUILD_TYPE} STREQUAL "dbg")
-      set(Destination "${CURRENT_PACKAGES_DIR}/debug/lib")
-    else()
-      set(Destination "${CURRENT_PACKAGES_DIR}/lib")
-    endif()  
-    foreach(EXTERNAL_MODULE flatbuffers onnx protobuf re2)
-      message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${BUILD_TYPE}/external/${EXTERNAL_MODULE}")
-      file(GLOB_RECURSE EXT_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${BUILD_TYPE}/external/${EXTERNAL_MODULE}/*.lib)
-      file(COPY
-          ${EXT_LIBS}
-          DESTINATION ${Destination}
-          )
-    endforeach ()
-  endforeach()
-
+  set(STATIC_LIB_EXTN "*.lib")
+  set(DYN_LIB_EXTN "*.dll")
+  set(SYM_FILE_EXTN "*.pdb")
 elseif(VCPKG_TARGET_IS_LINUX) # !Winddows
-message(STATUS "Copy libs and pdbs on Linux!")
+  set(STATIC_LIB_EXTN "*.a")
+  set(DYN_LIB_EXTN "*.(s|S)(o|O)")
+  set(SYM_FILE_EXTN "*.pdb")
 endif()
 
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug") 
+  # copy the onnxruntime_*.libs
+  message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
+  file(GLOB DEBUG_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${STATIC_LIB_EXTN})
+  file(COPY
+      ${DEBUG_LIBS}
+      DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+      )
+  # copy their corresponding *.pdbs
+  if(VCPKG_TARGET_IS_WINDOWS)
+    file(GLOB DEBUG_LIB_PDBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${SYM_FILE_EXTN})
+    file(COPY
+        ${DEBUG_LIB_PDBS}
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        )
+  endif()
+endif()
+
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+  # copy the onnxruntime_*.libs
+  message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
+  file(GLOB DEBUG_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${STATIC_LIB_EXTN})
+  file(COPY
+      ${DEBUG_LIBS}
+      DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+      )
+
+  # copy their corresponding *.pdbs
+  if(VCPKG_TARGET_IS_WINDOWS)
+    file(GLOB DEBUG_LIB_PDBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${SYM_FILE_EXTN})
+    file(COPY
+        ${DEBUG_LIB_PDBS}
+        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+        )
+  endif()
+endif()
+
+# copy library from external modules
+foreach(BUILD_TYPE rel dbg)
+  if(${BUILD_TYPE} STREQUAL "dbg")
+    set(Destination "${CURRENT_PACKAGES_DIR}/debug/lib")
+  else()
+    set(Destination "${CURRENT_PACKAGES_DIR}/lib")
+  endif()
+  foreach(EXTERNAL_MODULE flatbuffers onnx protobuf re2)
+    message(STATUS "Copying from ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${BUILD_TYPE}/external/${EXTERNAL_MODULE}")
+    file(GLOB_RECURSE EXT_LIBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${BUILD_TYPE}/external/${EXTERNAL_MODULE}/${STATIC_LIB_EXTN})
+    file(COPY
+        ${EXT_LIBS}
+        DESTINATION ${Destination}
+        )
+    file(GLOB_RECURSE EXT_LIB_PDBS LIST_DIRECTORIES false ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-${BUILD_TYPE}/external/${EXTERNAL_MODULE}/${SYM_FILE_EXTN})
+    file(COPY
+        ${EXT_LIB_PDBS}
+        DESTINATION ${Destination}
+        )
+  endforeach () # for all externam modules
+endforeach() # for different linkage types
+
+message(STATUS "Copy libs done, copying additional header files!")
+# Copy additional header files
+# 1. Copy missing orttraining header files
+# TODO: Make changes in ORT souce repo to put all training header files under top level include dir
+file(GLOB_RECURSE TRAINING_HEADERS LIST_DIRECTORIES false ${SOURCE_PATH}/orttraining/*.h)
+file(COPY ${SOURCE_PATH}/orttraining/orttraining/core DESTINATION ${CURRENT_PACKAGES_DIR}/include/orttraining FOLLOW_SYMLINK_CHAIN FILES_MATCHING PATTERN "*.h")
+# file(INSTALL ${SOURCE_PATH}/orttraining/orttraining/models DESTINATION ${CURRENT_PACKAGES_DIR}/include/orttraining FOLLOW_SYMLINK_CHAIN PATTERN "*.h")
+
+# Copy the license file
 file(INSTALL
   ${SOURCE_PATH}/LICENSE
   DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
-message(STATUS "Copy libs and pdbs done!")
+message(STATUS "Copy libs and headers done!")
 
 # Remove bin directories for static build, both debug and release build
 # onnx_test_runner.exe, onnxruntime.dll
