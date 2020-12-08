d297ec7ab00bbaf1c16a3d4ca03e2eaf54f60069
Victor Romero <romerosanchezv@gmail.com>
2020-12-08 01:06:30 -0800

    Code cleanup

--- toolsrc/include/vcpkg/commands.civerifyversions.h
+++ toolsrc/include/vcpkg/commands.civerifyversions.h
@@ -4,8 +4,6 @@
 
 namespace vcpkg::Commands::CIVerifyVersions
 {
-    extern const CommandStructure COMMAND_STRUCTURE;
-
     void perform_and_exit(const VcpkgCmdArguments& args, const VcpkgPaths& paths);
 
     struct CIVerifyVersionsCommand : PathsCommand
