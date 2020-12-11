1a7ca9930e3ceb3162ee6267ea320c9f47ae9ec3
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-12-11 22:33:16 +0100

    fix missing limits include

--- ports/qtopcua/portfile.cmake
+++ ports/qtopcua/portfile.cmake
@@ -1,7 +1,8 @@
 set(SCRIPT_PATH "${CURRENT_INSTALLED_DIR}/share/qtbase")
 include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
-set(${PORT}_PATCHES build.patch)
+set(${PORT}_PATCHES build.patch
+                    limits.patch)
 # General features:
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 FEATURES
