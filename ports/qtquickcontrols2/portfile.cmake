e8a8aaebb06899d7d1a82a67898d39ae0588d5ad
Alexander Neumann <Alexander.Neumann@hamburg.de>
2020-12-11 22:06:24 +0100

    make comment in qtquickcontrols2

--- ports/qtquickcontrols2/portfile.cmake
+++ ports/qtquickcontrols2/portfile.cmake
@@ -3,6 +3,10 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
 set(${PORT}_PATCHES)
 
+#make sure your <vcpkg_root> path is really short! Otherwise the build will probably fail. 
+# or maybe try to switch the build to nmake. 
+#set(ENV{CMAKE_BUILD_PARALLEL_LEVEL} 1)
+
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS
                      CONFIGURE_OPTIONS_RELEASE
