82e292051f8a470d37ce9818dace7fdb9c46305b
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2020-12-16 18:31:47 +0800

    Update ports/mdns/portfile.cmake

--- ports/mdns/portfile.cmake
+++ ports/mdns/portfile.cmake
@@ -1,11 +1,11 @@
 #header-only library
 
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO mjansson/mdns
-	REF 1.1
-	SHA512 03e4682d87e9c1157bba04e04a3dfbb2ed7e25df31f00834fbc7bf4275e5c7f7406e590c8bdc386a4e6fbe6a5667f700e146d39758aa8ee0a47f735547cacd31
-	HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mjansson/mdns
+    REF 1.1
+    SHA512 03e4682d87e9c1157bba04e04a3dfbb2ed7e25df31f00834fbc7bf4275e5c7f7406e590c8bdc386a4e6fbe6a5667f700e146d39758aa8ee0a47f735547cacd31
+    HEAD_REF master
 )
 
 vcpkg_configure_cmake(
