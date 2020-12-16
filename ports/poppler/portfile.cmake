4b48605198e6dc99a316dc61acbf5ff2c6ee5f55
abc <a@b.com>
2020-12-16 22:46:56 +0800

    Use Options in vcpkg_configure_cmake instead of patches

--- ports/poppler/portfile.cmake
+++ ports/poppler/portfile.cmake
@@ -1,37 +1,3 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
-#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#   VCPKG_TOOLCHAIN           = ON OFF
-#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
-#   BUILD_ARCH                = "Win32" "x64" "ARM"
-#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
-#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
-#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
-#   VCPKG_TARGET_IS_WINDOWS
-#   VCPKG_TARGET_IS_UWP
-#   VCPKG_TARGET_IS_LINUX
-#   VCPKG_TARGET_IS_OSX
-#   VCPKG_TARGET_IS_FREEBSD
-#   VCPKG_TARGET_IS_ANDROID
-#   VCPKG_TARGET_IS_MINGW
-#   VCPKG_TARGET_EXECUTABLE_SUFFIX
-#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
-#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
-#
-# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
-
-# # Specifies if the port install should fail immediately given a condition
-# vcpkg_fail_port_install(MESSAGE "poppler currently only supports Linux and Mac platforms" ON_TARGET "Windows")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO freedesktop/poppler
@@ -39,11 +5,7 @@ vcpkg_from_github(
     SHA512 f692682689c0b0fcc3953a1cc157b6e1d2ce3ccab185189d6dc0807f1dd3ea2d1a9773d0b805079a30b3c8a3b0cf3ee83239ed48d7b08dc7762eba29c2033674
     HEAD_REF master
     PATCHES
-        0001-remove-tests.patch
-        0002-remove-utils.patch
-        0003-remove-glib-and-GObject-introspection.patch
-        0004-remove-Qt5-and-Qt6-support.patch
-        0005-remove-CMAKE_CXX_STANDARD.patch
+        0001-remove-CMAKE_CXX_STANDARD.patch
 )
 
 if("cpp17" IN_LIST FEATURES)
@@ -52,16 +14,20 @@ else()
     set(CXX_STANDARD 14)
 endif()
 
-message(${CXX_STANDARD})
-
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DCMAKE_CXX_STANDARD=${CXX_STANDARD}
-    #OPTIONS -DCMAKE_CXX_STANDARD=${CXX_STANDARD}
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
+    PREFER_NINJA
+    OPTIONS
+        -DCMAKE_CXX_STANDARD=${CXX_STANDARD}
+        -DBUILD_GTK_TESTS=OFF
+        -DBUILD_QT5_TESTS=OFF
+        -DBUILD_QT6_TESTS=OFF
+        -DBUILD_CPP_TESTS=OFF
+        -DENABLE_UTILS=OFF
+        -DENABLE_GLIB=OFF
+        -DENABLE_GLOBJECT_INTROSPECTION=OFF
+        -DENABLE_QT5=OFF
+        -DENABLE_QT6=OFF
 )
 
 vcpkg_install_cmake()
