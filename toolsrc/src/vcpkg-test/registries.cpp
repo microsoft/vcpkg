260d25eeff06c5b1f4de349ff8110938b786314e
Nicole Mazzuca <mazzucan@outlook.com>
2020-12-21 12:02:04 -0800

    add registry parsing unit tests

--- toolsrc/src/vcpkg-test/registries.cpp
+++ toolsrc/src/vcpkg-test/registries.cpp
@@ -1,5 +1,7 @@
 #include <catch2/catch.hpp>
 
+#include <vcpkg/base/jsonreader.h>
+
 #include <vcpkg/registries.h>
 
 using namespace vcpkg;
@@ -36,6 +38,8 @@ namespace
         }
     }
 
+    // test functions which parse string literals, so no concerns about failure
+    Json::Value parse_json(StringView sv) { return Json::parse(sv).value_or_exit(VCPKG_LINE_INFO).first; }
 }
 
 TEST_CASE ("registry_set_selects_registry", "[registries]")
@@ -66,4 +70,63 @@ TEST_CASE ("registry_set_selects_registry", "[registries]")
     CHECK(get_tri_num(*reg) == 2);
     reg = set.registry_for_port("a");
     CHECK_FALSE(reg);
+}
+
+TEST_CASE ("registry_parsing", "[registries]")
+{
+    Json::Reader r;
+    auto registry_impl_des = get_registry_implementation_deserializer({});
+
+    auto test_json = parse_json(R"json(
+{
+    "kind": "builtin"
+}
+    )json");
+    auto registry_impl = r.visit(test_json, *registry_impl_des);
+    REQUIRE(registry_impl);
+    CHECK(*registry_impl.get());
+    CHECK(r.errors().empty());
+
+    test_json = parse_json(R"json(
+{
+    "kind": "builtin",
+    "baseline": "hi"
+}
+    )json");
+    registry_impl = r.visit(test_json, *registry_impl_des);
+    REQUIRE(registry_impl);
+    CHECK(*registry_impl.get());
+    CHECK(r.errors().empty());
+
+    test_json = parse_json(R"json(
+{
+    "kind": "builtin",
+    "path": "a/b"
+}
+    )json");
+    registry_impl = r.visit(test_json, *registry_impl_des);
+    CHECK_FALSE(r.errors().empty());
+    r.errors().clear();
+
+    test_json = parse_json(R"json(
+{
+    "kind": "filesystem",
+    "path": "a/b/c"
+}
+    )json");
+    registry_impl = r.visit(test_json, *registry_impl_des);
+    REQUIRE(registry_impl);
+    CHECK(*registry_impl.get());
+    CHECK(r.errors().empty());
+
+    test_json = parse_json(R"json(
+{
+    "kind": "filesystem",
+    "path": "/a/b/c"
+}
+    )json");
+    registry_impl = r.visit(test_json, *registry_impl_des);
+    REQUIRE(registry_impl);
+    CHECK(*registry_impl.get());
+    CHECK(r.errors().empty());
 }
