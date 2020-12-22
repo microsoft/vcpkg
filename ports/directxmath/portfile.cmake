788d373917f34792595ee25bc502ed6241930b5f
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-12-21 21:21:15 -0800

    Updated for OSX & Linux

--- ports/directxmath/portfile.cmake
+++ ports/directxmath/portfile.cmake
@@ -14,6 +14,19 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
 
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_download_distfile(
+        SAL_HEADER
+        URLS "https://raw.githubusercontent.com/dotnet/corert/master/src/Native/inc/unix/sal.h"
+        FILENAME "sal.h"
+        SHA512 1643571673195d9eb892d2f2ac76eac7113ef7aa0ca116d79f3e4d3dc9df8a31600a9668b7e7678dfbe5a76906f9e0734ef8d6db0903ccc68fc742dd8238d8b0
+    )
+
+    file(INSTALL
+      ${DOWNLOADS}/sal.h
+      DESTINATION ${CURRENT_PACKAGES_DIR}/include/)
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
