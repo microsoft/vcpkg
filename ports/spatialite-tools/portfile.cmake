3a20b14180af32263cc4e485f0c22c2a37504e5a
Long Huan <8551701+longhuan2018@users.noreply.github.com>
2020-12-22 09:52:36 +0800

    Update portfile.cmake

--- ports/spatialite-tools/portfile.cmake
+++ ports/spatialite-tools/portfile.cmake
@@ -104,17 +104,17 @@ if (VCPKG_TARGET_IS_WINDOWS)
   vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT}/debug)
 elseif (VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX) # Build in UNIX
   if(VCPKG_TARGET_IS_LINUX)
-      set(STDLIB stdc++)
+      set(LIBS "-lpthread -ldl -lm -lz -lstdc++")
   else()
-      set(STDLIB c++)
+      set(LIBS "-lpthread -ldl -lm -lz -lc++ -iconv -lc")
   endif()
 
   list(APPEND OPTIONS_RELEASE
-      "LIBXML2_LIBS=-lxml2 -llzma -liconv"
+      "LIBXML2_LIBS=-lxml2 -llzma"
       "GEOS_LDFLAGS=-lgeos_c -lgeos"
   )
   list(APPEND OPTIONS_DEBUG
-      "LIBXML2_LIBS=-lxml2 -llzmad -liconv"
+      "LIBXML2_LIBS=-lxml2 -llzmad"
       "GEOS_LDFLAGS=-lgeos_cd -lgeosd"
   )
 
@@ -124,7 +124,7 @@ elseif (VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX) # Build in UNIX
     OPTIONS
         "CFLAGS=-DACCEPT_USE_OF_DEPRECATED_PROJ_API_H"
         "LIBXML2_CFLAGS=-I\"${CURRENT_INSTALLED_DIR}/include\""
-        "LIBS=-lpthread -ldl -lm -lz -l${STDLIB}"
+        "LIBS=${LIBS}"
         "--disable-minizip"
     OPTIONS_DEBUG
         ${OPTIONS_DEBUG}
