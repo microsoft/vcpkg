32d05d7956662ebae7ee8cda8c9fc07d99536059
JackBoosY <yuzaiyang@beyondsoft.com>
2020-12-28 22:46:49 -0800

    [quirc] Fix license file name

--- ports/quirc/portfile.cmake
+++ ports/quirc/portfile.cmake
@@ -16,10 +16,9 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-# Handle copyright
-file(COPY ${SOURCE_PATH}/license DESTINATION ${CURRENT_PACKAGES_DIR}/share/quirc)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/quirc/license ${CURRENT_PACKAGES_DIR}/share/quirc/copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/quirc/copyright)
