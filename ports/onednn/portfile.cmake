df3306f6ed03b4fe166e0081f9fb164c8a76529d
jacobkahn <jacobkahn1@gmail.com>
2020-12-29 08:36:09 -0800

    [onednn] Fix share path infix to match find_package() name

--- ports/onednn/portfile.cmake
+++ ports/onednn/portfile.cmake
@@ -17,7 +17,9 @@ vcpkg_configure_cmake(
 )
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake)
+# The port name and the find_package() name are different (onednn versus dnnl)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/dnnl TARGET_PATH share/dnnl)
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
