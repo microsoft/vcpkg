82f96cb9e5d5a1d9de0e2cd17e10a35668dfd17d
Gerald Combs <gerald@zing.org>
2020-12-30 09:34:44 -0800

    [libilbc] More packaging cleanups.
    Add vcpkg_fixup_pkgconfig after vcpkg_install_cmake.
    
    Install docs into $PREFIX/share/libilbc.
    
    Remove ilbc_test after a dynamic build and the bin and debug/bin dirs
    after a static build.
    
    Fixup include/ilbc_export.h for static builds.

--- ports/libilbc/portfile.cmake
+++ ports/libilbc/portfile.cmake
@@ -9,15 +9,23 @@ vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     REF ${ILBC_VERSION}
+    PATCHES do-not-build-ilbc_test.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DCMAKE_INSTALL_DOCDIR=share/${PORT}
 )
+
 vcpkg_install_cmake()
 
-vcpkg_copy_tools(TOOL_NAMES ilbc_test AUTO_CLEAN)
+vcpkg_fixup_pkgconfig()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/ilbc_export.h "#ifdef ILBC_STATIC_DEFINE" "#if 1")
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
