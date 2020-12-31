d35b78e98a0d6a655d3831012167447d32dda2d0
Adam Johnson <AdamJohnso@gmail.com>
2020-12-31 18:57:38 -0500

    [python3] Be more selective about when to unleash the nukes.

--- ports/python3/portfile.cmake
+++ ports/python3/portfile.cmake
@@ -181,18 +181,14 @@ else()
 endif()
 
 function(_generate_finder)
-    cmake_parse_arguments(PythonFinder "NEW_FINDER;LIBS;INTERP" "DIRECTORY;PREFIX" "" ${ARGN})
+    cmake_parse_arguments(PythonFinder "" "DIRECTORY;PREFIX" "" ${ARGN})
     configure_file(
-        "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in"
+        "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake"
         "${CURRENT_PACKAGES_DIR}/share/${PythonFinder_DIRECTORY}/vcpkg-cmake-wrapper.cmake"
         @ONLY
     )
 endfunction()
 
 message(STATUS "Installing cmake wrappers")
-# CMake 3.12+ FindPython-backed find modules
-_generate_finder(DIRECTORY "python" PREFIX "Python" NEW_FINDER LIBS INTERP)
-_generate_finder(DIRECTORY "python3" PREFIX "Python3" NEW_FINDER LIBS INTERP)
-# Deprecated find modules
-_generate_finder(DIRECTORY "pythoninterp" PREFIX "PYTHON" INTERP)
-_generate_finder(DIRECTORY "pythonlibs" PREFIX "PYTHON" LIBS)
+_generate_finder(DIRECTORY "python" PREFIX "Python")
+_generate_finder(DIRECTORY "python3" PREFIX "Python3")
