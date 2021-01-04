ace0e80907220df146b9c09ba09d33be975f5bb8
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-01-04 15:41:57 -0800

    Update and verify updates for Azure SDK for C++ using VcPkg's CI/CD pipeline

---
+++ ports/azure-identity-cpp/portfile.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO antkmsft/azure-sdk-for-cpp
+    REF azure-sdk-for-cpp_1.0.0-test.0
+    SHA512 9f60751d6baa5517383225c90f992105b78dfa2a73f10788a547228e9f9a95b15f5b168bfc8beaed3cefe276581cfabf6cfabe83d807880e2271c899e1cea941
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/sdk/identity/azure-identity/
+    PREFER_NINJA
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+)
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
