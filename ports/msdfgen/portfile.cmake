11f762c12003cde7841ecf6f39c40c9d2bdf5341
Haeri <Haeri@users.noreply.github.com>
2021-01-04 05:14:44 +0100

    Update ports/msdfgen/portfile.cmake
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/msdfgen/portfile.cmake
+++ ports/msdfgen/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/msdfgen TARGET_PATH share/msdfgen)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/msdfgen)
 
 # move exe to tools
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
