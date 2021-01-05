46f18c86073a730bf9ee4d271e24744efb81e1f5
Silvio Traversaro <silvio@traversaro.it>
2021-01-05 08:17:54 +0100

    Update ports/ignition-common3/portfile.cmake
    Co-authored-by: Phoebe <20694052+PhoebeHui@users.noreply.github.com>

--- ports/ignition-common3/portfile.cmake
+++ ports/ignition-common3/portfile.cmake
@@ -5,7 +5,7 @@ ignition_modular_library(NAME common
                          SHA512 8d052850cbb125e334494c9ad9b234c371fe310327dba997515651f29479d747dffa55b0aa822f2a78e6317a4df2d41389c7a07165cdc08894fdfb116e4d9756)
 
 # Remove non-relocatable helper scripts (see https://github.com/ignitionrobotics/ign-common/issues/82)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/ign_remotery_vis" "${CURRENT_PACKAGES_DIR}/debug/bin'/ign_remotery_vis")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/ign_remotery_vis" "${CURRENT_PACKAGES_DIR}/debug/bin/ign_remotery_vis")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
