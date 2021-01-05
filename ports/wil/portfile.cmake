043c7852402fbd952f99609e5fb6afde03058b8c
JackBoosY <yuzaiyang@beyondsoft.com>
2021-01-04 19:05:08 -0800

    [wil] Fix install headers on Linux

--- ports/wil/portfile.cmake
+++ ports/wil/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(
     REF 3c00e7f1d8cf9930bbb8e5be3ef0df65c84e8928
     SHA512 c9c3b4a41f7523a6da6378def4a6b868e9f66438998d04ae8489b9784db91664af7af3ab6ef73c104b9ac100c0dc5ae6a13e9cb9f679ba428a4abc07b32a7dce
     HEAD_REF master
+    PATCHES fix-install-headers.patch
 )
 
 vcpkg_configure_cmake(
