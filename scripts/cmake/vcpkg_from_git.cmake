9dfcf66296844ec568b0d8977bdc9e28f4861693
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2021-01-07 12:18:36 +0100

    Apply suggestions from code review
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

--- scripts/cmake/vcpkg_from_git.cmake
+++ scripts/cmake/vcpkg_from_git.cmake
@@ -26,16 +26,15 @@ The url of the git repository.
 The git sha of the commit to download.
 
 ### TAG
-The git tag to download.
+An optional git tag to be verified against the `REF`. If the remote repository's tag does not match the specified `REF`, the build will fail.
 
 ### PATCHES
 A list of patches to be applied to the extracted sources.
 
 Relative paths are based on the port directory.
 
-### OUT_REF
-Maintainers only. Used for automatic REF updates for ports using a versioned TAG.
-Will return early!
+### X_OUT_REF (internal only)
+This parameter is used for automatic REF updates for certain ports in the central vcpkg catalog. It should not be used by any ports outside the central catalog and within the central catalog it should not be used on any user path. This parameter may change behavior incompatibly or be removed at any time.
 
 ## Notes:
 `OUT_SOURCE_PATH`, `REF`, and `URL` must be specified.
@@ -48,7 +47,7 @@ Will return early!
 include(vcpkg_execute_in_download_mode)
 
 function(vcpkg_from_git)
-  set(oneValueArgs OUT_SOURCE_PATH URL REF TAG OUT_REF)
+  set(oneValueArgs OUT_SOURCE_PATH URL REF TAG X_OUT_REF)
   set(multipleValuesArgs PATCHES)
   # parse parameters such that semicolons in options arguments to COMMAND don't get erased
   cmake_parse_arguments(PARSE_ARGV 0 _vdud "" "${oneValueArgs}" "${multipleValuesArgs}")
