52174bc6979729d0f654654b089e835223c1d904
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-01-08 15:10:57 +0100

    add fortran flags so that find_package finds blas

--- triplets/community/x64-windows-ifort.toolchain.cmake
+++ triplets/community/x64-windows-ifort.toolchain.cmake
@@ -69,6 +69,14 @@ set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} ${VCPKG_CXX_FLAGS_RELEASE}
 set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_MINSIZEREL} ${VCPKG_CXX_FLAGS_RELEASE}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_C_FLAGS_RELWITHDEBINFO} ${VCPKG_CXX_FLAGS_RELEASE}" CACHE STRING "")
 
+# Make sure the name mangling of Intel Fortran generated symbols is all lowercase with underscore suffix
+# because this is assumed by many libraries (that e.g. consume BLAS/LAPACK)
+set(CMAKE_Fortran_FLAGS " /fpp /W0 /nologo /names:lowercase /assume:underscore /threads ")
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+    string(APPEND CMAKE_Fortran_FLAGS " /libs:static")
+elseif(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
+    string(APPEND CMAKE_Fortran_FLAGS " /libs:dll")
+endif()
 # Set linker flags.
 foreach(LINKER SHARED_LINKER MODULE_LINKER EXE_LINKER)
   set(CMAKE_${LINKER}_FLAGS_INIT "${VCPKG_LINKER_FLAGS}")
