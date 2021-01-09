0f583079895a181112df6695390c94c5c5eb9edb
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-09 20:58:22 +0100

    force blas vendor with wrapper

--- ports/blas/portfile.cmake
+++ ports/blas/portfile.cmake
@@ -1,6 +1,7 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-
+set(BLA_VENDOR OpenBLAS)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
 # Make sure BLAS can be found
 vcpkg_configure_cmake(SOURCE_PATH ${CURRENT_PORT_DIR}
                       OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
-                              -DBLA_VENDOR=OpenBLAS)
+                              -DBLA_VENDOR=${BLA_VENDOR})
