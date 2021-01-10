e13a5461eda9dbea7947abb34c78be2cfe0e2b66
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-10 23:41:36 +0100

    update intel-mkl port

--- ports/intel-mkl/portfile.cmake
+++ ports/intel-mkl/portfile.cmake
@@ -4,16 +4,18 @@
 
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-set(MKL_REQUIRED_VERSION "20200000")
+set(MKL_REQUIRED_VERSION "2020.00.00")
 
 set(ProgramFilesx86 "ProgramFiles(x86)")
-set(INTEL_ROOT $ENV{${ProgramFilesx86}}/IntelSWTools/compilers_and_libraries/windows)
+set(INTEL_ROOT "$ENV{${ProgramFilesx86}}/IntelSWTools/compilers_and_libraries/windows")
+set(INTEL_ONEAPI_MKL_ROOT "$ENV{${ProgramFilesx86}}/Intel/oneAPI/mkl/latest")
 
-find_path(MKL_ROOT include/mkl.h PATHS $ENV{MKLROOT} ${INTEL_ROOT}/mkl DOC "Folder contains MKL")
+message(STATUS )
+find_path(MKL_ROOT mkl.h PATHS "$ENV{MKLROOT}" "${INTEL_ROOT}/mkl" "${INTEL_ONEAPI_MKL_ROOT}" PATH_SUFFIXES include DOC "Folder contains MKL")
 
 if (MKL_ROOT STREQUAL "MKL_ROOT-NOTFOUND")
     message(FATAL_ERROR "Could not find MKL. Before continuing, please download and install MKL  (${MKL_REQUIRED_VERSION} or higher) from:"
-                        "\n    https://registrationcenter.intel.com/en/products/download/3178/\n"
+                        "\n    https://software.intel.com/content/www/us/en/develop/tools/oneapi/base-toolkit.html\n"
                         "\nAlso ensure vcpkg has been rebuilt with the latest version (v0.0.104 or later)")
 endif()
 
@@ -23,10 +25,10 @@ endif()
 # list(GET MKL_VERSION 1 MKL_VERSION_MINOR)
 # list(GET MKL_VERSION 2 MKL_VERSION_UPDATE)
 
-file(STRINGS ${MKL_ROOT}/include/mkl_version.h MKL_VERSION_DEFINITION REGEX "INTEL_MKL_VERSION")
+file(STRINGS ${MKL_ROOT}/mkl_version.h MKL_VERSION_DEFINITION REGEX "INTEL_MKL_VERSION")
 string(REGEX MATCH "([0-9]+)" MKL_VERSION ${MKL_VERSION_DEFINITION})
 
-if (MKL_VERSION LESS MKL_REQUIRED_VERSION)
+if (MKL_VERSION VERSION_LESS MKL_REQUIRED_VERSION)
     message(FATAL_ERROR "MKL ${MKL_VERSION} is found but ${MKL_REQUIRED_VERSION} is required. Please download and install a more recent version of MKL from:"
-                        "\n    https://registrationcenter.intel.com/en/products/download/3178/\n")
+                        "\n    https://software.intel.com/content/www/us/en/develop/tools/oneapi/base-toolkit.html\n")
 endif()
