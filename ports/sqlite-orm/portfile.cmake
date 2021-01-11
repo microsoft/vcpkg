478d905fc15de8c914a25d71aa8e54dc3cbe8951
NancyLi1013 <lirui09@beyondsoft.com>
2021-01-10 21:56:01 -0800

    Add vcpkg_fixup_cmake_targets()

--- ports/sqlite-orm/portfile.cmake
+++ ports/sqlite-orm/portfile.cmake
@@ -24,6 +24,8 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/SqliteOrm TARGET_PATH share/SqliteOrm)
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
 
