c2dbe7a1ead31df8dc018271c515c0d0d79d185c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-11 18:41:54 +0100

    fix cpp wrapper to work.

--- ports/xlib/portfile.cmake
+++ ports/xlib/portfile.cmake
@@ -8,8 +8,8 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org/xorg
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lib/libx11
-    REF  db7cca17ad7807e92a928da9d4c68a00f4836da2 #x11 v 1.6.9  
-    SHA512 63106422bf74071f73e47a954607472a7df6f4094c197481a100fa10676a22e81ece0459108790d3ebda6a1664c5cba6809bdb80cd5bc4befa1a76bd87188616
+    REF  ca8115186f810eccb7d86b0979980eff3ba95f0b #x11 v 1.7.0
+    SHA512 fcb65ecc3055f5c696de5e0d320b6e51ca3e5a1eed9ac1e19db84e409df25c878def884fcc5eb575bb767f1d3cdc736f63d6d88abb5f36d6bfd15bebe9f0d6c4
     HEAD_REF master # branch name
     PATCHES cl.build.patch #patch name
             ${PATCHES}
@@ -60,18 +60,11 @@ vcpkg_find_acquire_program(PERL)
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
     AUTOCONFIG
-    #SKIP_CONFIGURE
-    #NO_DEBUG
-    #AUTO_HOST
-    #AUTO_DST
-    #PRERUN_SHELL "export ACLOCAL=\"aclocal -I ${CURRENT_INSTALLED_DIR}/share/xorg-macros/aclocal/\""
     OPTIONS ${OPTIONS}
-    #OPTIONS_DEBUG
-    #OPTIONS_RELEASE
 )
 
 vcpkg_install_make()
-vcpkg_fixup_pkgconfig(SYSTEM_LIBRARIES Ws2_32 pthread)
+vcpkg_fixup_pkgconfig()
 
 if(EXISTS "${CURRENT_INSTALLED_DIR}/include/X11/extensions/XKBgeom.h")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/X11/extensions/") #XKBgeom.h should be the only file in there
