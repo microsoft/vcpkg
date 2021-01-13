ea400ee6a5b3a7bdb3e5f45bd39b77fded48151d
JackBoosY <yuzaiyang@beyondsoft.com>
2021-01-12 23:00:50 -0800

    [soci] Correct cmake config file name

--- ports/soci/portfile.cmake
+++ ports/soci/portfile.cmake
@@ -48,7 +48,9 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/SOCI)
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake)
+# Correct the config file name
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/${PORT}/SOCI.cmake ${CURRENT_PACKAGES_DIR}/share/${PORT}/SOCI-config.cmake)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
