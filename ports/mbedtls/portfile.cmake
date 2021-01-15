a83f5d8e42e9a1ae368614b6ef6a4d0d430bc6be
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2021-01-15 20:45:21 -0300

    Fix Mbed TLS build for Android

--- ports/mbedtls/portfile.cmake
+++ ports/mbedtls/portfile.cmake
@@ -3,28 +3,33 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 set(VCPKG_LIBRARY_LINKAGE static)
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO ARMmbed/mbedtls
-    REF 523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8 # mbedtls-2.24.0
-    SHA512 1ce78f34e8d87c2ce0454e0a08f4c6e5b3129d4b24cfa44162af21c2e8b5dc7feabf849e4fa547ce3781b5ce11aaf675cfed47412bae40091fbdd87bbcdbee07
-    HEAD_REF master
-    PATCHES
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO ARMmbed/mbedtls
+        REF 523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8 # mbedtls-2.24.0
+        SHA512 1ce78f34e8d87c2ce0454e0a08f4c6e5b3129d4b24cfa44162af21c2e8b5dc7feabf849e4fa547ce3781b5ce11aaf675cfed47412bae40091fbdd87bbcdbee07
+        HEAD_REF master
+        PATCHES
         enable-pthread.patch
 )
 
 vcpkg_check_features(
-    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    pthreads ENABLE_PTHREAD
+        OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+        pthreads ENABLE_PTHREAD
 )
 
+if (VCPKG_TARGET_IS_ANDROID)
+    set(EXTRA_ARGS "-DMBEDTLS_FATAL_WARNINGS=FALSE")
+endif ()
+
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
+        SOURCE_PATH ${SOURCE_PATH}
+        PREFER_NINJA
+        OPTIONS
         ${FEATURE_OPTIONS}
         -DENABLE_TESTING=OFF
         -DENABLE_PROGRAMS=OFF
+        ${EXTRA_ARGS}
 )
 
 vcpkg_install_cmake()
@@ -33,8 +38,8 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
-if(WIN32 AND pthreads IN_LIST FEATURES)
+if (WIN32 AND pthreads IN_LIST FEATURES)
     file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-endif()
+endif ()
 
 vcpkg_copy_pdbs()
