5a9e3acbf7c43cee5f04108d4e91424068c79c13
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2021-01-15 18:53:44 -0300

    Fix Android builds for AWS C++ SDK
    (cherry picked from commit 55b2c4c3a1df2f834cefffae4c90d86965c738ff)

--- ports/aws-sdk-cpp/portfile.cmake
+++ ports/aws-sdk-cpp/portfile.cmake
@@ -1,14 +1,15 @@
 vcpkg_buildpath_length_warning(37)
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO aws/aws-sdk-cpp
-    REF e98e5732ec7319051f162f7314ae361c85d0a8c9 # 1.8.83
-    SHA512 da540db60551be833ea0315dd93241f9740ab953ed5657c1c7a8c401ae52a4e75b116758420b0a8a4ebb79358dff8377f5e052b180b36f0af27a36003f28bd56
-    HEAD_REF master
-    PATCHES
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO aws/aws-sdk-cpp
+        REF e98e5732ec7319051f162f7314ae361c85d0a8c9 # 1.8.83
+        SHA512 da540db60551be833ea0315dd93241f9740ab953ed5657c1c7a8c401ae52a4e75b116758420b0a8a4ebb79358dff8377f5e052b180b36f0af27a36003f28bd56
+        HEAD_REF master
+        PATCHES
         patch-relocatable-rpath.patch
         fix-AWSSDKCONFIG.patch
+        allow-vcpkg-chaining.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" FORCE_SHARED_CRT)
@@ -17,16 +18,27 @@ set(BUILD_ONLY core)
 
 include(${CMAKE_CURRENT_LIST_DIR}/compute_build_only.cmake)
 
-if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
+if (VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
     set(rpath "@loader_path")
-else()
+elseif (VCPKG_TARGET_IS_ANDROID)
+    find_package(Git)
+
+    set(EXTRA_ARGS "-DTARGET_ARCH=ANDROID"
+            "-DGIT_EXECUTABLE=${GIT_EXECUTABLE}"
+            "-DGIT_FOUND=${GIT_FOUND}"
+            "-DGIT_VERSION_STRING=${GIT_VERSION_STRING}"
+            "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}"
+            )
+else ()
     set(rpath "\$ORIGIN")
-endif()
+endif ()
+
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA
-    OPTIONS
+        SOURCE_PATH ${SOURCE_PATH}
+        DISABLE_PARALLEL_CONFIGURE
+        PREFER_NINJA
+        OPTIONS
+        ${EXTRA_ARGS}
         -DENABLE_UNITY_BUILD=ON
         -DENABLE_TESTING=OFF
         -DFORCE_SHARED_CRT=${FORCE_SHARED_CRT}
@@ -43,45 +55,45 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
 vcpkg_copy_pdbs()
 
 file(GLOB_RECURSE AWS_TARGETS "${CURRENT_PACKAGES_DIR}/share/*/*-targets-*.cmake")
-foreach(AWS_TARGET IN LISTS AWS_TARGETS)
+foreach (AWS_TARGET IN LISTS AWS_TARGETS)
     file(READ ${AWS_TARGET} _contents)
     string(REGEX REPLACE
-        "bin\\/([A-Za-z0-9_.-]+\\.lib)"
-        "lib/\\1"
-        _contents "${_contents}")
+            "bin\\/([A-Za-z0-9_.-]+\\.lib)"
+            "lib/\\1"
+            _contents "${_contents}")
     file(WRITE ${AWS_TARGET} "${_contents}")
-endforeach()
+endforeach ()
 
 file(GLOB AWS_CONFIGS "${CURRENT_PACKAGES_DIR}/share/*/aws-cpp-sdk-*-config.cmake")
 list(FILTER AWS_CONFIGS EXCLUDE REGEX "aws-cpp-sdk-core-config\\.cmake\$")
-foreach(AWS_CONFIG IN LISTS AWS_CONFIGS)
+foreach (AWS_CONFIG IN LISTS AWS_CONFIGS)
     file(READ "${AWS_CONFIG}" _contents)
     file(WRITE "${AWS_CONFIG}" "include(CMakeFindDependencyMacro)\nfind_dependency(aws-cpp-sdk-core)\n${_contents}")
-endforeach()
+endforeach ()
 
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
-    ${CURRENT_PACKAGES_DIR}/lib/pkgconfig
-    ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
-    ${CURRENT_PACKAGES_DIR}/nuget
-    ${CURRENT_PACKAGES_DIR}/debug/nuget
-)
+        ${CURRENT_PACKAGES_DIR}/debug/include
+        ${CURRENT_PACKAGES_DIR}/debug/share
+        ${CURRENT_PACKAGES_DIR}/lib/pkgconfig
+        ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
+        ${CURRENT_PACKAGES_DIR}/nuget
+        ${CURRENT_PACKAGES_DIR}/debug/nuget
+        )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     file(GLOB LIB_FILES ${CURRENT_PACKAGES_DIR}/bin/*.lib)
-    if(LIB_FILES)
+    if (LIB_FILES)
         file(COPY ${LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
         file(REMOVE ${LIB_FILES})
-    endif()
+    endif ()
     file(GLOB DEBUG_LIB_FILES ${CURRENT_PACKAGES_DIR}/debug/bin/*.lib)
-    if(DEBUG_LIB_FILES)
+    if (DEBUG_LIB_FILES)
         file(COPY ${DEBUG_LIB_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
         file(REMOVE ${DEBUG_LIB_FILES})
-    endif()
+    endif ()
 
     file(APPEND ${CURRENT_PACKAGES_DIR}/include/aws/core/SDKConfig.h "#ifndef USE_IMPORT_EXPORT\n#define USE_IMPORT_EXPORT\n#endif")
-endif()
+endif ()
 
 configure_file(${CURRENT_PORT_DIR}/usage ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage @ONLY)
 
