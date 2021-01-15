a46ccac5f6ab5b3e78953fb350ce542101d1601e
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-01-15 11:52:36 +0100

    remove old libsigcpp

--- ports/atkmm/portfile.cmake
+++ ports/atkmm/portfile.cmake
@@ -3,57 +3,74 @@ if (VCPKG_TARGET_IS_WINDOWS)
 endif()
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/atkmm/2.24/atkmm-2.24.2.tar.xz"
-    FILENAME "atkmm-2.24.2.tar.xz"
-    SHA512 427714cdf3b10e3f9bc36df09c4b05608d295f5895fb1e079b9bd84afdf7bf1cfdec6794ced7f1e35bd430b76f87792df4ee63c515071a2ea6e3e51e672cdbe2
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/atkmm/2.36/atkmm-2.36.0.tar.xz"
+    FILENAME "atkmm-2.36.0.tar.xz"
+    SHA512 8527dfa50191919a7dcf6db6699767352cb0dac800d834ee39ed21694eee3136a41a7532d600b8b3c0fcea52da6129b623e8e61ada728d806aa61fdc8dc8dedf
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     PATCHES
-        fix_properties.patch
-        fix_charset.patch
+        #fix_properties.patch
+        #fix_charset.patch
 )
 
-if (VCPKG_TARGET_IS_WINDOWS)
-    file(COPY ${CMAKE_CURRENT_LIST_DIR}/msvc_recommended_pragmas.h DESTINATION ${SOURCE_PATH}/MSVC_Net2013)
+# if (VCPKG_TARGET_IS_WINDOWS)
+    # file(COPY ${CMAKE_CURRENT_LIST_DIR}/msvc_recommended_pragmas.h DESTINATION ${SOURCE_PATH}/MSVC_Net2013)
 
-    set(VS_PLATFORM ${VCPKG_TARGET_ARCHITECTURE})
-    if(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-        set(VS_PLATFORM "Win32")
-    endif(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-    vcpkg_build_msbuild(
-        PROJECT_PATH ${SOURCE_PATH}/MSVC_Net2013/atkmm.sln
-        TARGET atkmm
-        PLATFORM ${VS_PLATFORM}
-        USE_VCPKG_INTEGRATION
-    )
+    # set(VS_PLATFORM ${VCPKG_TARGET_ARCHITECTURE})
+    # if(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
+        # set(VS_PLATFORM "Win32")
+    # endif(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
+    # vcpkg_build_msbuild(
+        # PROJECT_PATH ${SOURCE_PATH}/MSVC_Net2013/atkmm.sln
+        # TARGET atkmm
+        # PLATFORM ${VS_PLATFORM}
+        # USE_VCPKG_INTEGRATION
+    # )
 
-    # Handle headers
-    file(COPY ${SOURCE_PATH}/MSVC_Net2013/atkmm/atkmmconfig.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-    file(COPY ${SOURCE_PATH}/atk/atkmm.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-    file(COPY ${SOURCE_PATH}/atk/atkmm
-        DESTINATION ${CURRENT_PACKAGES_DIR}/include
-        FILES_MATCHING PATTERN *.h)
+    # # Handle headers
+    # file(COPY ${SOURCE_PATH}/MSVC_Net2013/atkmm/atkmmconfig.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+    # file(COPY ${SOURCE_PATH}/atk/atkmm.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+    # file(COPY ${SOURCE_PATH}/atk/atkmm
+        # DESTINATION ${CURRENT_PACKAGES_DIR}/include
+        # FILES_MATCHING PATTERN *.h)
 
-    # Handle libraries
-    file(COPY ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.dll
-        DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-    file(COPY ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-    file(COPY ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.dll
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-    file(COPY ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+    # # Handle libraries
+    # file(COPY ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.dll
+        # DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+    # file(COPY ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.lib
+        # DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+    # file(COPY ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.dll
+        # DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+    # file(COPY ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.lib
+        # DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 
-    vcpkg_copy_pdbs()
-else()
-    set(ENV{CONFIG_SHELL} bash)
-    vcpkg_configure_make(SOURCE_PATH ${SOURCE_PATH} OPTIONS ${ATKMM_OPTIONS})
+    # vcpkg_copy_pdbs()
+# else()
 
-    vcpkg_install_make()
-endif()
+vcpkg_configure_meson(SOURCE_PATH ${SOURCE_PATH} 
+                      OPTIONS -Dbuild-documentation=false
+                              -Dmsvc14x-parallel-installable=false)
+vcpkg_install_meson()
+# endif()
+
+vcpkg_fixup_pkgconfig()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+
+# option('maintainer-mode', type: 'combo', choices: ['false', 'if-git-build', 'true'],
+  # value: 'if-git-build', description: 'Generate source code from .hg and .ccg files')
+# option('warnings', type: 'combo', choices: ['no', 'min', 'max', 'fatal'], value: 'min',
+  # description: 'Compiler warning level')
+# option('dist-warnings', type: 'combo', choices: ['no', 'min', 'max', 'fatal'], value: 'fatal',
+  # description: 'Compiler warning level when a tarball is created')
+# option('build-deprecated-api', type: 'boolean', value: true,
+  # description: 'Build deprecated API and include it in the library')
+# option('build-documentation', type: 'combo', choices: ['false', 'if-maintainer-mode', 'true'],
+  # value: 'if-maintainer-mode', description: 'Build and install the documentation')
+# option('msvc14x-parallel-installable', type: 'boolean', value: true,
+  # description: 'Use separate DLL and LIB filenames for Visual Studio 2017 and 2019')
