83055de998746461706f405256572dd1f5e60ea9
Billy Robert O'Neal III <bion@microsoft.com>
2021-01-15 23:17:48 -0800

    Add missing formatted versions.

--- toolsrc/include/vcpkg/base/checks.h
+++ toolsrc/include/vcpkg/base/checks.h
@@ -32,4 +32,29 @@ namespace vcpkg::Checks
                               Strings::format(error_message_template, error_message_arg1, error_message_args...));
         }
     }
+
+    template<class Arg1, class... Args>
+    [[noreturn]] void exit_maybe_upgrade(const LineInfo& line_info,
+                                         const char* error_message_template,
+                                         const Arg1& error_message_arg1,
+                                         const Args&... error_message_args)
+    {
+        exit_maybe_upgrade(line_info,
+                           Strings::format(error_message_template, error_message_arg1, error_message_args...));
+    }
+
+    template<class Conditional, class Arg1, class... Args>
+    void check_maybe_upgrade(const LineInfo& line_info,
+                             Conditional&& expression,
+                             const char* error_message_template,
+                             const Arg1& error_message_arg1,
+                             const Args&... error_message_args)
+    {
+        if (!expression)
+        {
+            // Only create the string if the expression is false
+            exit_maybe_upgrade(line_info,
+                               Strings::format(error_message_template, error_message_arg1, error_message_args...));
+        }
+    }
 }
