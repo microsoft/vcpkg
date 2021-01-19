118cab8ad90e824ac1bf9883bddfdb20f3ee2213
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-19 19:07:59 +0100

    update gtkmm

--- ports/gtkmm/portfile.cmake
+++ ports/gtkmm/portfile.cmake
@@ -2,96 +2,52 @@ if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL WindowsStore)
     message(FATAL_ERROR "Error: UWP builds are currently not supported.")
 endif()
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/gtkmm/3.22/gtkmm-3.22.2.tar.xz"
-    FILENAME "gtkmm-3.22.2.tar.xz"
-    SHA512 6e96b543e459481145ee0f56f31a7ad2466bd8ccdd2abf3205998aecede73d235149ca6e5ba6e8d20a4fd5345e310870d81ac2a716d4f78d1460ed685badbdc2
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/gtkmm/4.0/gtkmm-4.0.0.tar.xz"
+    FILENAME "gtkmm-4.0.0.tar.xz"
+    SHA512 16893b6caa39f1b65a4140296d8d25c0d5e5f8a6ab808086783e7222bc1f5e8b94d17d48e4b718a12f0e0291010d445f4da9f88b7f494ec36adb22752d932743
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    PATCHES
-        fix_properties.patch
-        fix_treeviewcolumn.patch
+    PATCHES build.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/msvc_recommended_pragmas.h DESTINATION ${SOURCE_PATH}/MSVC_Net2013)
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} /DGTKMM_BUILD /DGDKMM_BUILD")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} /DGTKMM_BUILD /DGDKMM_BUILD")
 
-set(VS_PLATFORM ${VCPKG_TARGET_ARCHITECTURE})
-if(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-    set(VS_PLATFORM "Win32")
-endif(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/MSVC_Net2013/gtkmm.sln
-    TARGET gtkmm
-    PLATFORM ${VS_PLATFORM}
-    USE_VCPKG_INTEGRATION
-)
-
-# Handle headers
-file(COPY ${SOURCE_PATH}/MSVC_Net2013/gdkmm/gdkmmconfig.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(COPY ${SOURCE_PATH}/gdk/gdkmm.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(
-    COPY
-    ${SOURCE_PATH}/gdk/gdkmm
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-    FILES_MATCHING PATTERN *.h
-)
-file(COPY ${SOURCE_PATH}/MSVC_Net2013/gtkmm/gtkmmconfig.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(COPY ${SOURCE_PATH}/gtk/gtkmm.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(
-    COPY
-    ${SOURCE_PATH}/gtk/gtkmm
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-    FILES_MATCHING PATTERN *.h
+vcpkg_configure_meson(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -Dmsvc14x-parallel-installable=false
+        -Dbuild-tests=false
+        -Dbuild-demos=false
+    ADDITIONAL_NATIVE_BINARIES glib-compile-resources='${CURRENT_INSTALLED_DIR}/tools/glib/glib-compile-resources${VCPKG_HOST_EXECUTABLE_SUFFIX}'
+    ADDITIONAL_CROSS_BINARIES  glib-compile-resources='${CURRENT_INSTALLED_DIR}/tools/glib/glib-compile-resources${VCPKG_HOST_EXECUTABLE_SUFFIX}'
 )
 
-# Handle libraries
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/gdkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/gdkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/gtkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/gtkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/gdkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/gdkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/gtkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/gtkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-)
+vcpkg_install_meson()
 
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/gtkmm RENAME copyright)
+vcpkg_fixup_pkgconfig()
+
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/gtkmm" RENAME copyright)
+
+
+# option('maintainer-mode', type: 'combo', choices: ['false', 'if-git-build', 'true'],
+  # value: 'if-git-build', description: 'Generate source code from .hg and .ccg files')
+# option('warnings', type: 'combo', choices: ['no', 'min', 'max', 'fatal'], value: 'min',
+  # description: 'Compiler warning level')
+# option('dist-warnings', type: 'combo', choices: ['no', 'min', 'max', 'fatal'], value: 'fatal',
+  # description: 'Compiler warning level when a tarball is created')
+# option('build-deprecated-api', type: 'boolean', value: true,
+  # description: 'Build deprecated API and include it in the library')
+# option('build-documentation', type: 'combo', choices: ['false', 'if-maintainer-mode', 'true'],
+  # value: 'if-maintainer-mode', description: 'Build and install the documentation')
+# option('build-demos', type: 'boolean', value: true, description: 'Build demo programs')
+# option('build-tests', type: 'boolean', value: true, description: 'Build test programs')
+# option('msvc14x-parallel-installable', type: 'boolean', value: true,
+  # description: 'Use separate DLL and LIB filenames for Visual Studio 2017 and 2019')
