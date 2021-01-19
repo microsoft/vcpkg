f14cac729cc416428b44c52e03ae835fcb4a23c5
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-19 13:37:58 +0100

    fix wrapper install

--- ports/lapack/portfile.cmake
+++ ports/lapack/portfile.cmake
@@ -1,14 +1,14 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-set(BLA_VENDOR Generic)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
-
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     set(BLA_STATIC ON)
 else()
     set(BLA_STATIC OFF)
 endif()
-
+set(BLA_VENDOR Generic)
+if(VCPKG_TARGET_IS_WINDOWS) # The other wrapper is in lapack-reference
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/lapack/vcpkg-cmake-wrapper.cmake" @ONLY)
+endif()
 # Make sure LAPACK can be found
 vcpkg_configure_cmake(SOURCE_PATH ${CURRENT_PORT_DIR}
                       OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
