eba8ae568844a4279ff00ae61ee49821d5a79853
Matt Oliver <protogonoi@gmail.com>
2021-01-20 22:32:31 +1100

    [libxml2] Correct pkgconfig lib name.

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -19,6 +19,10 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libxml-2.0.pc" "-lxml2" "-llibxml2")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libxml-2.0.pc" "-lxml2" "-llibxml2")
+endif ()
 vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_pdbs()
