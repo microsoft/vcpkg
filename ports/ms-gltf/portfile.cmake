e009ac886a412eed7e0f5bc93198e6f6615d60f1
PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>
2021-01-21 18:43:03 -0800

    Address the review suggestions

--- ports/ms-gltf/portfile.cmake
+++ ports/ms-gltf/portfile.cmake
@@ -5,9 +5,9 @@ endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO     microsoft/glTF-SDK
-    REF      9428f114b540fb93e6533d5ec460fc123efe0c86 # todo: r1.9.6.0
-    SHA512   900caf6d72d360bae4c7af769a8879507f7f727b40f61723ffed679ad22877fb37daed2a0dfcbf27e15ab3adc8afe3249530b95691ce489b3446e5d9a35f205a
+    REPO microsoft/glTF-SDK
+    REF 9428f114b540fb93e6533d5ec460fc123efe0c86 # todo: r1.9.6.0
+    SHA512 900caf6d72d360bae4c7af769a8879507f7f727b40f61723ffed679ad22877fb37daed2a0dfcbf27e15ab3adc8afe3249530b95691ce489b3446e5d9a35f205a
     HEAD_REF master
     PATCHES
         fix-install.patch
@@ -15,7 +15,6 @@ vcpkg_from_github(
 
 # note: Test/Sample executables won't be installed
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
         test    ENABLE_UNIT_TESTS
         samples ENABLE_SAMPLES
 )
@@ -25,11 +24,13 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 #       For Windows, we rely on /MP. The other platforms should be able to build with PREFER_NINJA.
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
-file(INSTALL    ${SOURCE_PATH}/LICENSE
+file(INSTALL ${SOURCE_PATH}/LICENSE
      DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
 )
 
