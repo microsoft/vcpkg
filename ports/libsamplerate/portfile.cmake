7c23028ebca54e8c117a9469ea6efc03906f9c3c
evpobr <evpobr@gmail.com>
2021-01-22 13:39:45 +0500

    Remove SYSTEM_LIBRARIES m argument
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/libsamplerate/portfile.cmake
+++ ports/libsamplerate/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/SampleRate TARGET_PATH share/SampleRate)
-vcpkg_fixup_pkgconfig(SYSTEM_LIBRARIES m)
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
