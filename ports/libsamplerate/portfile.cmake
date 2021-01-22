ec43cfb9aade376fa4b0fe3805bdf3b0e54e5050
evpobr <evpobr@gmail.com>
2021-01-22 23:29:41 +0500

    [libsamplerate] Upgrade to 0.2.0 (#15797)
    * [libsamplerate] Upgrade to 0.2.0
    
    * Add missiong OPTIONS
    
    * Use share/${PORT} instead of share/libsamplerate
    
    * Remove SYSTEM_LIBRARIES m argument
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * Update version
    
    * Update version
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    Co-authored-by: Jonliu1993 <13720414433@163.com>

--- ports/libsamplerate/portfile.cmake
+++ ports/libsamplerate/portfile.cmake
@@ -1,24 +1,27 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libsndfile/libsamplerate
-    REF f6730d03c3e7660bb6ecad8816f1b09c5825142a # v0.1.9
-    SHA512 6a349c9144d024212fc78dc0d9e39bdc1a43abaf590fcfbf84396af22834545962d5ef10176b48b21fcae2ce62d12277b682059383805d059f4dd2b9f6708478
+    REF 0.2.0
+    SHA512 6a12506ad391a557fcc354c7bb5767ae71349da26bf31753b806a36449cfc879ef97e4d25adab5722a9540ec1539203c6155b4581e42bdb8b6708dca1f089d11
     HEAD_REF master
-    PATCHES Use-the-lrintf-intrinsic.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH}/src)
-file(COPY ${SOURCE_PATH}/Win32/config.h DESTINATION ${SOURCE_PATH}/src)
-
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/src
+    SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DLIBSAMPLERATE_EXAMPLES=OFF
 )
 
 vcpkg_install_cmake()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/SampleRate TARGET_PATH share/SampleRate)
+vcpkg_fixup_pkgconfig()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libsamplerate RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 vcpkg_copy_pdbs()
