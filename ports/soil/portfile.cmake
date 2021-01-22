c86eb645d7410212d31732ab403fec8ba22efe04
JackBoosY <yuzaiyang@beyondsoft.com>
2021-01-22 00:11:23 -0800

    [soil] Use cache source address temporary

--- ports/soil/portfile.cmake
+++ ports/soil/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.lonesock.net/files/soil.zip"
-    FILENAME "soil-2008.07.07.zip"
+    URLS "https://web.archive.org/web/20200104042737/http://www.lonesock.net/files/soil.zip" #"http://www.lonesock.net/files/soil.zip"
+    FILENAME "soil.zip"
     SHA512 a575a84aa65b7556320779d635561341f5cf156418d0462473e5d1eb082829be3bcb30600b4887af75aeddd3715de16bdb3ca1668ebaa93eea62bacf22b79548
 )
 
