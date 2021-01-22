250658dfeb5dd2ec4332900da369392df9a0679a
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-01-22 16:42:52 +0100

    fix xres on windows

--- ports/xres/portfile.cmake
+++ ports/xres/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_gitlab(
     REF 84b9156c7833dfd91c65d33542420ff4fe226948 # 1.2.0
     SHA512  3c3ba4b82fbc4856a827022b5b0a774eb39cb1bd53a7d6fdb3056c58648f2a38ea33efee9154611c29291983753dbf775ef74d1cba6aea59c5d8b98527abdecd
     HEAD_REF master # branch name
+    PATCHES build.patch
 ) 
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
