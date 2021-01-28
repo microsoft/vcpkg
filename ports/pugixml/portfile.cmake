c22f555b9352a04a706cb5cef797d48ba6d6b1e3
NancyLi1013 <lirui09@beyondsoft.com>
2021-01-27 23:12:23 -0800

    [pugixml] Update to latest version 1.11.4

--- ports/pugixml/portfile.cmake
+++ ports/pugixml/portfile.cmake
@@ -1,15 +1,16 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeux/pugixml
-    REF v1.11.1
-    SHA512 94a79a28d96e763cdd8951c4d0b20aefb43cd1b32cbf5a5354b09f2636710e960e2dbfa56534b61c8d6ddecb2126a006e5f485c1465a97376e1cc077df25f16e
+    REF 08b3433180727ea2f78fe02e860a08471db1e03c #v1.11.4
+    SHA512 998f54203fbf81ad08d21964acb5b9ad00bf5b9eff6a0e88b93d97d0c6eda4bb75e12bb5f6a937c81b4582a415662f9ede77abe2ab3c48188d1a3768053b9c08
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DUSE_POSTFIX=ON
+    OPTIONS 
+        -DUSE_POSTFIX=ON
 )
 
 vcpkg_install_cmake()
