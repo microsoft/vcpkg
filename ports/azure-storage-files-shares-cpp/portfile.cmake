f2993fbed1671c9e90e633280e8743edb30ad54b
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-02-01 08:15:29 -0800

    [IGNORE PLEASE] Azure SDK for C++: verify on VcPkg's CI/CD

---
+++ ports/azure-storage-files-shares-cpp/portfile.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO antkmsft/azure-sdk-for-cpp
+    REF azure-sdk-for-cpp_1.0.0-beta.1
+    SHA512 920490f427dfc836ba19693901f95ccba777aeda39f3a0d55cb8a6bf3cec259b4df30accd9b0223b317834f644daaf340d7e335a6f4f42ad1a455cd6aeb91f50
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/sdk/storage/azure-storage-files-shares/
+    PREFER_NINJA
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+)
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
