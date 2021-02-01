4d43d79a4de98c2ae1ff944f612c4b72e62ec267
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-02-01 08:56:51 -0800

    Update versions

--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO antkmsft/azure-sdk-for-cpp
-    REF azure-sdk-for-cpp_1.0.0-beta.1
-    SHA512 920490f427dfc836ba19693901f95ccba777aeda39f3a0d55cb8a6bf3cec259b4df30accd9b0223b317834f644daaf340d7e335a6f4f42ad1a455cd6aeb91f50
+    REF azure-sdk-for-cpp_1.0.0-beta.2
+    SHA512 465777618b36f405c74446216149162f28a9de579b7f43545eae4d1657388d561f2b849aa2644a2ae785f5fd1f01177cb0ead311faae9f1806dc2c7e7f4dc872
 )
 
 vcpkg_check_features(
