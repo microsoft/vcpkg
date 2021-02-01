584dbb1b63d05f26ac248230c9e2f48081f5de7d
DragonJoker <dragonjoker59@hotmail.com>
2021-02-01 13:35:24 +0100

    Fixed license file install script.

--- ports/shaderwriter/portfile.cmake
+++ ports/shaderwriter/portfile.cmake
@@ -39,4 +39,4 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/shaderwriter)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/shaderwriter RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
