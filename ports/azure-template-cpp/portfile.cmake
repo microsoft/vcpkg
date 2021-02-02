bcfb3100d285cc09f4642b565281c14f90ee506d
azure-sdk <azuresdk@microsoft.com>
2021-02-02 17:07:18 +0000

    Update port for azure-template-cpp

---
+++ ports/azure-template-cpp/portfile.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF azure-template_1.0.0-beta.25
+    SHA512 6216b30d5c3f060cc4219016c646f347b4be4534dc77a93cf1647817550538dd96b63e05b832b46ac96bced6ec2fc639ee3c2b026a8ac0596347a840e9695e37
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/sdk/template/azure-template/
+    PREFER_NINJA
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+)
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
+
