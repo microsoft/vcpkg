1fee31da614e9ff298d7f4be6a64024479e38764
JackBoosY <yuzaiyang@beyondsoft.com>
2021-02-02 01:11:30 -0800

    [zbar] Fix build

--- ports/zbar/portfile.cmake
+++ ports/zbar/portfile.cmake
@@ -1,25 +1,33 @@
+vcpkg_fail_port_install(ON_TARGET "Windows" "OSX")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ZBar/ZBar
-    REF xxx
-    SHA512 xxx
+    REF 67003d2a985b5f9627bee2d8e3e0b26d0c474b57
+    SHA512 680fba4674d610e1eb5e5f746dba13c08befc555bb53cd2b0fa2654225ad3c7bf32c9cd15235c0b3eb5b96fc8cbecf2da721f85b074a025802ad23191847e168
     HEAD_REF master
+    PATCHES
+        fix-build-error.patch # https://github.com/ZBar/ZBar/pull/9/files
+        disable-warnings.patch
+        fix-install-doc.patch
 )
 
-vcpkg_configure_cmake(
+vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
-        -Ddiscreture_INSTALL_CMAKE_DIR=share
+        --with-zlib-prefix=${CURRENT_INSTALLED_DIR}
+        --with-libpng-prefix=${CURRENT_INSTALLED_DIR}
+        --without-qt
+        --without-gtk
+        --without-imagemagick
+        --without-python
 )
 
-vcpkg_install_cmake()
+vcpkg_install_make()
 
-file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug
-    ${CURRENT_PACKAGES_DIR}/share/zbar-config-version.cmake)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/zbar
-    RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/zbar" RENAME copyright)
