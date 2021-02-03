microsoft/vcpkg

commit 4d136ef25f4fab5b744c7ae6acfa04d44f254f2b

[vcpkg] Add vcpkg_minimum_required as a replacement for VERSION.txt. (#15638)

toolsrc/include/vcpkg/base/expected.h
@@ -3,6 +3,7 @@
 #include <vcpkg/base/checks.h>
 #include <vcpkg/base/lineinfo.h>
 #include <vcpkg/base/stringliteral.h>
+#include <vcpkg/base/system.print.h>
 
 #include <system_error>
 #include <type_traits>
@@ -225,10 +226,10 @@ namespace vcpkg
     private:
         void exit_if_error(const LineInfo& line_info) const
         {
-            // This is used for quick value_or_exit() calls, so always put line_info in the error message.
             if (m_s.has_error())
             {
-                Checks::exit_with_message(line_info, "Failed at [%s] with message:\n%s", line_info, m_s.to_string());
+                System::print2(System::Color::error, m_s.to_string(), "\n");
+                Checks::unreachable(line_info);
             }
         }
 

toolsrc/src/vcpkg.cpp
@@ -79,12 +79,6 @@ static void inner(vcpkg::Files::Filesystem& fs, const VcpkgCmdArguments& args)
     paths.track_feature_flag_metrics();
 
     fs.current_path(paths.root, VCPKG_LINE_INFO);
-    if ((args.command == "install" || args.command == "remove" || args.command == "export" ||
-         args.command == "update") &&
-        !args.output_json())
-    {
-        Commands::Version::warn_if_vcpkg_version_mismatch(paths);
-    }
 
     if (const auto command_function = find_command(Commands::get_available_paths_commands()))
     {

toolsrc/src/vcpkg/build.cpp
@@ -104,11 +104,12 @@ namespace vcpkg::Build
         const PackageSpec& spec = full_spec.package_spec;
         const SourceControlFile& scf = *scfl.source_control_file;
 
-        Checks::check_exit(VCPKG_LINE_INFO,
-                           spec.name() == scf.core_paragraph->name,
-                           "The Source field inside the CONTROL file does not match the port directory: '%s' != '%s'",
-                           scf.core_paragraph->name,
-                           spec.name());
+        Checks::check_maybe_upgrade(
+            VCPKG_LINE_INFO,
+            spec.name() == scf.core_paragraph->name,
+            Strings::format("The Source field inside the CONTROL file does not match the port directory: '%s' != '%s'",
+                            scf.core_paragraph->name,
+                            spec.name()));
 
         compute_all_abis(paths, action_plan, var_provider, status_db);
 
@@ -181,7 +182,7 @@ namespace vcpkg::Build
         const auto port_name = spec.package_spec.name();
         const auto* scfl = provider.get_control_file(port_name).get();
 
-        Checks::check_exit(VCPKG_LINE_INFO, scfl != nullptr, "Error: Couldn't find port '%s'", port_name);
+        Checks::check_maybe_upgrade(VCPKG_LINE_INFO, scfl != nullptr, "Error: Couldn't find port '%s'", port_name);
         ASSUME(scfl != nullptr);
 
         return perform_ex(args,
@@ -298,13 +299,13 @@ namespace vcpkg::Build
         if (cmake_system_name == "Windows") return "";
         if (cmake_system_name == "WindowsStore") return "store";
 
-        Checks::exit_with_message(VCPKG_LINE_INFO, "Unsupported vcvarsall target %s", cmake_system_name);
+        Checks::exit_maybe_upgrade(VCPKG_LINE_INFO, "Unsupported vcvarsall target %s", cmake_system_name);
     }
 
     static CStringView to_vcvarsall_toolchain(const std::string& target_architecture, const Toolset& toolset)
     {
         auto maybe_target_arch = System::to_cpu_architecture(target_architecture);
-        Checks::check_exit(
+        Checks::check_maybe_upgrade(
             VCPKG_LINE_INFO, maybe_target_arch.has_value(), "Invalid architecture string: %s", target_architecture);
         auto target_arch = maybe_target_arch.value_or_exit(VCPKG_LINE_INFO);
         auto host_architectures = System::get_supported_host_architectures();
@@ -317,12 +318,12 @@ namespace vcpkg::Build
             if (it != toolset.supported_architectures.end()) return it->name;
         }
 
-        Checks::exit_with_message(VCPKG_LINE_INFO,
-                                  "Unsupported toolchain combination. Target was: %s but supported ones were:\n%s",
-                                  target_architecture,
-                                  Strings::join(",", toolset.supported_architectures, [](const ToolsetArchOption& t) {
-                                      return t.name.c_str();
-                                  }));
+        Checks::exit_maybe_upgrade(VCPKG_LINE_INFO,
+                                   "Unsupported toolchain combination. Target was: %s but supported ones were:\n%s",
+                                   target_architecture,
+                                   Strings::join(",", toolset.supported_architectures, [](const ToolsetArchOption& t) {
+                                       return t.name.c_str();
+                                   }));
     }
 
 #if defined(_WIN32)
@@ -515,11 +516,12 @@ namespace vcpkg::Build
         Util::Vectors::append(&out_vars,
                               std::initializer_list<System::CMakeVariable>{
                                   {"CMD", "BUILD"},
+                                  {"DOWNLOADS", paths.downloads},
                                   {"TARGET_TRIPLET", triplet.canonical_name()},
                                   {"TARGET_TRIPLET_FILE", fs::u8string(paths.get_triplet_file_path(triplet))},
-                                  {"VCPKG_PLATFORM_TOOLSET", toolset.version.c_str()},
-                                  {"DOWNLOADS", paths.downloads},
+                                  {"VCPKG_BASE_VERSION", Commands::Version::base_version()},
                                   {"VCPKG_CONCURRENCY", std::to_string(get_concurrency())},
+                                  {"VCPKG_PLATFORM_TOOLSET", toolset.version.c_str()},
                               });
         if (!System::get_environment_variable("VCPKG_FORCE_SYSTEM_BINARIES").has_value())
         {
@@ -626,14 +628,14 @@ namespace vcpkg::Build
         }
 
         std::vector<System::CMakeVariable> variables{
-            {"PORT", scf.core_paragraph->name},
+            {"ALL_FEATURES", all_features},
             {"CURRENT_PORT_DIR", scfl.source_location},
+            {"FEATURES", Strings::join(";", action.feature_list)},
+            {"PORT", scf.core_paragraph->name},
             {"VCPKG_USE_HEAD_VERSION", Util::Enum::to_bool(action.build_options.use_head_version) ? "1" : "0"},
-            {"_VCPKG_NO_DOWNLOADS", !Util::Enum::to_bool(action.build_options.allow_downloads) ? "1" : "0"},
             {"_VCPKG_DOWNLOAD_TOOL", to_string(action.build_options.download_tool)},
             {"_VCPKG_EDITABLE", Util::Enum::to_bool(action.build_options.editable) ? "1" : "0"},
-            {"FEATURES", Strings::join(";", action.feature_list)},
-            {"ALL_FEATURES", all_features},
+            {"_VCPKG_NO_DOWNLOADS", !Util::Enum::to_bool(action.build_options.allow_downloads) ? "1" : "0"},
         };
 
         for (auto cmake_arg : args.cmake_args)
@@ -726,10 +728,11 @@ namespace vcpkg::Build
         }
         else
         {
-            Checks::exit_with_message(VCPKG_LINE_INFO,
-                                      "Unable to determine toolchain to use for triplet %s with CMAKE_SYSTEM_NAME %s",
-                                      triplet,
-                                      cmake_system_name);
+            Checks::exit_maybe_upgrade(VCPKG_LINE_INFO,
+                                       "Unable to determine toolchain to use for triplet %s with CMAKE_SYSTEM_NAME %s; "
+                                       "maybe you meant to use VCPKG_CHAINLOAD_TOOLCHAIN_FILE?",
+                                       triplet,
+                                       cmake_system_name);
         }
     }
 
@@ -1061,13 +1064,11 @@ namespace vcpkg::Build
                         auto status_it = status_db.find(pspec);
                         if (status_it == status_db.end())
                         {
-                            Checks::exit_with_message(
+                            Checks::exit_maybe_upgrade(
                                 VCPKG_LINE_INFO, "Failed to find dependency abi for %s -> %s", action.spec, pspec);
                         }
-                        else
-                        {
-                            dependency_abis.emplace_back(AbiEntry{pspec.name(), status_it->get()->package.abi});
-                        }
+
+                        dependency_abis.emplace_back(AbiEntry{pspec.name(), status_it->get()->package.abi});
                     }
                     else
                     {
@@ -1248,9 +1249,13 @@ namespace vcpkg::Build
 
             auto crtlinkage = to_linkage_type(crt_linkage_as_string);
             if (const auto p = crtlinkage.get())
+            {
                 build_info.crt_linkage = *p;
+            }
             else
+            {
                 Checks::exit_with_message(VCPKG_LINE_INFO, "Invalid crt linkage type: [%s]", crt_linkage_as_string);
+            }
         }
 
         {
@@ -1258,11 +1263,16 @@ namespace vcpkg::Build
             parser.required_field(BuildInfoRequiredField::LIBRARY_LINKAGE, library_linkage_as_string);
             auto liblinkage = to_linkage_type(library_linkage_as_string);
             if (const auto p = liblinkage.get())
+            {
                 build_info.library_linkage = *p;
+            }
             else
+            {
                 Checks::exit_with_message(
                     VCPKG_LINE_INFO, "Invalid library linkage type: [%s]", library_linkage_as_string);
+            }
         }
+
         std::string version = parser.optional_field("Version");
         if (!version.empty()) build_info.version = std::move(version);
 
@@ -1276,7 +1286,7 @@ namespace vcpkg::Build
             else if (setting == "disabled")
                 policies.emplace(policy, false);
             else
-                Checks::exit_with_message(
+                Checks::exit_maybe_upgrade(
                     VCPKG_LINE_INFO, "Unknown setting for policy '%s': %s", to_string(policy), setting);
         }
 
@@ -1294,7 +1304,7 @@ namespace vcpkg::Build
     BuildInfo read_build_info(const Files::Filesystem& fs, const fs::path& filepath)
     {
         const ExpectedS<Parse::Paragraph> pghs = Paragraphs::get_single_paragraph(fs, filepath);
-        Checks::check_exit(
+        Checks::check_maybe_upgrade(
             VCPKG_LINE_INFO, pghs.get() != nullptr, "Invalid BUILD_INFO file for package: %s", pghs.error());
         return inner_create_buildinfo(*pghs.get());
     }
@@ -1394,16 +1404,22 @@ namespace vcpkg::Build
                     else if (Strings::case_insensitive_ascii_equals(variable_value, "1") ||
                              Strings::case_insensitive_ascii_equals(variable_value, "on") ||
                              Strings::case_insensitive_ascii_equals(variable_value, "true"))
+                    {
                         load_vcvars_env = true;
+                    }
                     else if (Strings::case_insensitive_ascii_equals(variable_value, "0") ||
                              Strings::case_insensitive_ascii_equals(variable_value, "off") ||
                              Strings::case_insensitive_ascii_equals(variable_value, "false"))
+                    {
                         load_vcvars_env = false;
+                    }
                     else
+                    {
                         Checks::exit_with_message(VCPKG_LINE_INFO,
                                                   "Unknown boolean setting for VCPKG_LOAD_VCVARS_ENV: %s. Valid "
                                                   "settings are '', '1', '0', 'ON', 'OFF', 'TRUE', and 'FALSE'.",
                                                   variable_value);
+                    }
                     break;
             }
         }

commit 5205184322047349b92b691f09324f327a271c1b

[vcpkg] Apply fixes needed to extract vcpkg-tool
utilities.cmake: Disable warnings that are firing on Azure Pipelines CI machines due to different Clang version.

expected.h: Fix a bug I introduced in https://github.com/microsoft/vcpkg/pull/15638/ because I was under the impression expected worked like optional (in that value_or_exit for it should be treated as a program bug)

build.cpp: Add (void)s to silence warnings.

binarycaching.cpp: Repair assumption that the current directory is C: which isn't true on the Hosted Azure Pipelines agents.

others: Make unit tests respect %VCPKG_ROOT%, as necessary in the vcpkg_tool repo. Note that this required splitting vcpkgcmdarguments::ImbueFromEnvironment into the once-only process modifying part and the just imbue from environment part.

toolsrc/cmake/utilities.cmake
@@ -204,7 +204,13 @@ function(vcpkg_target_add_warning_options TARGET)
         if(VCPKG_DEVELOPMENT_WARNINGS)
             target_compile_options(${TARGET} PRIVATE -W4)
             if(VCPKG_COMPILER STREQUAL "clang")
-                target_compile_options(${TARGET} PRIVATE -Wmissing-prototypes -Wno-missing-field-initializers)
+                # -Wno-range-loop-analysis is due to an LLVM bug which will be fixed in a
+                # future version of clang https://reviews.llvm.org/D73007
+                target_compile_options(${TARGET} PRIVATE
+                    -Wmissing-prototypes
+                    -Wno-missing-field-initializers
+                    -Wno-range-loop-analysis
+                    )
             else()
                 target_compile_options(${TARGET} PRIVATE -analyze)
             endif()
@@ -219,13 +225,21 @@ function(vcpkg_target_add_warning_options TARGET)
         if(VCPKG_DEVELOPMENT_WARNINGS)
             target_compile_options(${TARGET} PRIVATE
                 -Wall -Wextra -Wpedantic
-                -Wno-unknown-pragmas -Wno-missing-field-initializers -Wno-redundant-move)
+                -Wno-unknown-pragmas
+                -Wno-missing-field-initializers
+                -Wno-redundant-move
+                )
 
             # GCC and clang have different names for the same warning
             if(VCPKG_COMPILER STREQUAL "gcc")
-                target_compile_options(${TARGET} PRIVATE -Wmissing-declarations)
+                target_compile_options(${TARGET} PRIVATE
+                    -Wmissing-declarations
+                    )
             elseif(VCPKG_COMPILER STREQUAL "clang")
-                target_compile_options(${TARGET} PRIVATE -Wmissing-prototypes)
+                target_compile_options(${TARGET} PRIVATE
+                    -Wmissing-prototypes
+                    -Wno-range-loop-analysis
+                    )
             endif()
         endif()
 

toolsrc/include/vcpkg/base/expected.h
@@ -229,7 +229,7 @@ namespace vcpkg
             if (m_s.has_error())
             {
                 System::print2(System::Color::error, m_s.to_string(), "\n");
-                Checks::unreachable(line_info);
+                Checks::exit_fail(line_info);
             }
         }
 

toolsrc/include/vcpkg/vcpkgcmdarguments.h
@@ -212,7 +212,6 @@ namespace vcpkg
         }
 
         bool output_json() const { return json.value_or(false); }
-        bool is_recursive_invocation() const { return m_is_recursive_invocation; }
 
         std::string command;
         std::vector<std::string> command_arguments;
@@ -221,13 +220,16 @@ namespace vcpkg
 
         void imbue_from_environment();
 
+        // Applies recursive settings from the environment or sets a global environment variable
+        // to be consumed by subprocesses; may only be called once per process.
+        static void imbue_or_apply_process_recursion(VcpkgCmdArguments& args);
+
         void check_feature_flag_consistency() const;
 
         void debug_print_feature_flags() const;
         void track_feature_flag_metrics() const;
 
     private:
-        bool m_is_recursive_invocation = false;
         std::unordered_set<std::string> command_switches;
         std::unordered_map<std::string, std::vector<std::string>> command_options;
     };

toolsrc/src/vcpkg.cpp
@@ -226,6 +226,7 @@ int main(const int argc, const char* const* const argv)
     VcpkgCmdArguments args = VcpkgCmdArguments::create_from_command_line(fs, argc, argv);
     if (const auto p = args.debug.get()) Debug::g_debugging = *p;
     args.imbue_from_environment();
+    VcpkgCmdArguments::imbue_or_apply_process_recursion(args);
     args.check_feature_flag_consistency();
 
     {

toolsrc/src/vcpkg/build.cpp
@@ -344,6 +344,9 @@ namespace vcpkg::Build
                        "for more information.\n");
         Checks::exit_maybe_upgrade(VCPKG_LINE_INFO);
 #else
+        (void)target_architecture;
+        (void)toolset;
+        (void)all_toolsets;
         Checks::exit_with_message(VCPKG_LINE_INFO,
                                   "Error: vcvars-based toolchains are only usable on Windows platforms.");
 #endif

toolsrc/src/vcpkg/vcpkgcmdarguments.cpp
@@ -648,13 +648,6 @@ namespace vcpkg
 
     void VcpkgCmdArguments::imbue_from_environment()
     {
-        static bool s_reentrancy_guard = false;
-        Checks::check_exit(VCPKG_LINE_INFO,
-                           !s_reentrancy_guard,
-                           "VcpkgCmdArguments::imbue_from_environment() modifies global state and thus may only be "
-                           "called once per process.");
-        s_reentrancy_guard = true;
-
         if (!disable_metrics)
         {
             const auto vcpkg_disable_metrics_env = System::get_environment_variable(DISABLE_METRICS_ENV);
@@ -701,46 +694,52 @@ namespace vcpkg
                 parse_feature_flags(flags, *this);
             }
         }
+    }
 
+    void VcpkgCmdArguments::imbue_or_apply_process_recursion(VcpkgCmdArguments& args)
+    {
+        static bool s_reentrancy_guard = false;
+        Checks::check_exit(
+            VCPKG_LINE_INFO,
+            !s_reentrancy_guard,
+            "VcpkgCmdArguments::imbue_or_apply_process_recursion() modifies global state and thus may only be "
+            "called once per process.");
+        s_reentrancy_guard = true;
+
+        auto maybe_vcpkg_recursive_data = System::get_environment_variable(RECURSIVE_DATA_ENV);
+        if (auto vcpkg_recursive_data = maybe_vcpkg_recursive_data.get())
         {
-            auto maybe_vcpkg_recursive_data = System::get_environment_variable(RECURSIVE_DATA_ENV);
-            if (auto vcpkg_recursive_data = maybe_vcpkg_recursive_data.get())
+            auto rec_doc = Json::parse(*vcpkg_recursive_data).value_or_exit(VCPKG_LINE_INFO).first;
+            const auto& obj = rec_doc.object();
+
+            if (auto entry = obj.get(DOWNLOADS_ROOT_DIR_ENV))
             {
-                m_is_recursive_invocation = true;
-
-                auto rec_doc = Json::parse(*vcpkg_recursive_data).value_or_exit(VCPKG_LINE_INFO).first;
-                const auto& obj = rec_doc.object();
-
-                if (auto entry = obj.get(DOWNLOADS_ROOT_DIR_ENV))
-                {
-                    downloads_root_dir = std::make_unique<std::string>(entry->string().to_string());
-                }
-
-                if (obj.get(DISABLE_METRICS_ENV))
-                {
-                    disable_metrics = true;
-                }
-
-                // Setting the recursive data to 'poison' prevents more than one level of recursion because
-                // Json::parse() will fail.
-                System::set_environment_variable(RECURSIVE_DATA_ENV, "poison");
+                args.downloads_root_dir = std::make_unique<std::string>(entry->string().to_string());
             }
-            else
+
+            if (obj.get(DISABLE_METRICS_ENV))
             {
-                Json::Object obj;
-                if (downloads_root_dir)
-                {
-                    obj.insert(DOWNLOADS_ROOT_DIR_ENV, Json::Value::string(*downloads_root_dir.get()));
-                }
-
-                if (disable_metrics)
-                {
-                    obj.insert(DISABLE_METRICS_ENV, Json::Value::boolean(true));
-                }
-
-                System::set_environment_variable(RECURSIVE_DATA_ENV,
-                                                 Json::stringify(obj, Json::JsonStyle::with_spaces(0)));
+                args.disable_metrics = true;
             }
+
+            // Setting the recursive data to 'poison' prevents more than one level of recursion because
+            // Json::parse() will fail.
+            System::set_environment_variable(RECURSIVE_DATA_ENV, "poison");
+        }
+        else
+        {
+            Json::Object obj;
+            if (args.downloads_root_dir)
+            {
+                obj.insert(DOWNLOADS_ROOT_DIR_ENV, Json::Value::string(*args.downloads_root_dir.get()));
+            }
+
+            if (args.disable_metrics)
+            {
+                obj.insert(DISABLE_METRICS_ENV, Json::Value::boolean(true));
+            }
+
+            System::set_environment_variable(RECURSIVE_DATA_ENV, Json::stringify(obj, Json::JsonStyle::with_spaces(0)));
         }
     }
 

toolsrc/src/vcpkg-test/binarycaching.cpp
@@ -58,7 +58,11 @@ TEST_CASE ("generate_nuspec", "[generate_nuspec]")
 {
     auto& fsWrapper = Files::get_real_filesystem();
     VcpkgCmdArguments args = VcpkgCmdArguments::create_from_arg_sequence(nullptr, nullptr);
+    args.imbue_from_environment();
     args.packages_root_dir = std::make_unique<std::string>("/");
+    auto pkgPath = fsWrapper.absolute(VCPKG_LINE_INFO, fs::u8path("/zlib2_x64-windows")) / fs::u8path("**");
+    pkgPath.make_preferred();
+    const auto pkgPathStr = fs::u8string(pkgPath);
     VcpkgPaths paths(fsWrapper, args);
 
     auto pghs = Paragraphs::parse_paragraphs(R"(
@@ -101,11 +105,6 @@ Build-Depends: bzip
 
     {
         auto nuspec = generate_nuspec(paths, ipa, ref, {});
-#ifdef _WIN32
-#define PKGPATH "C:\\zlib2_x64-windows\\**"
-#else
-#define PKGPATH "/zlib2_x64-windows/**"
-#endif
         std::string expected = R"(<package>
   <metadata>
     <id>zlib2_x64-windows</id>
@@ -125,7 +124,8 @@ Dependencies:
 </description>
     <packageTypes><packageType name="vcpkg"/></packageTypes>
   </metadata>
-  <files><file src=")" PKGPATH R"(" target=""/></files>
+  <files><file src=")" + pkgPathStr +
+                               R"(" target=""/></files>
 </package>
 )";
         REQUIRE_EQUAL_TEXT(nuspec, expected);
@@ -133,11 +133,6 @@ Dependencies:
 
     {
         auto nuspec = generate_nuspec(paths, ipa, ref, {"urlvalue"});
-#ifdef _WIN32
-#define PKGPATH "C:\\zlib2_x64-windows\\**"
-#else
-#define PKGPATH "/zlib2_x64-windows/**"
-#endif
         std::string expected = R"(<package>
   <metadata>
     <id>zlib2_x64-windows</id>
@@ -158,18 +153,14 @@ Dependencies:
     <packageTypes><packageType name="vcpkg"/></packageTypes>
     <repository type="git" url="urlvalue"/>
   </metadata>
-  <files><file src=")" PKGPATH R"(" target=""/></files>
+  <files><file src=")" + pkgPathStr +
+                               R"(" target=""/></files>
 </package>
 )";
         REQUIRE_EQUAL_TEXT(nuspec, expected);
     }
     {
         auto nuspec = generate_nuspec(paths, ipa, ref, {"urlvalue", "branchvalue", "commitvalue"});
-#ifdef _WIN32
-#define PKGPATH "C:\\zlib2_x64-windows\\**"
-#else
-#define PKGPATH "/zlib2_x64-windows/**"
-#endif
         std::string expected = R"(<package>
   <metadata>
     <id>zlib2_x64-windows</id>
@@ -190,7 +181,8 @@ Dependencies:
     <packageTypes><packageType name="vcpkg"/></packageTypes>
     <repository type="git" url="urlvalue" branch="branchvalue" commit="commitvalue"/>
   </metadata>
-  <files><file src=")" PKGPATH R"(" target=""/></files>
+  <files><file src=")" + pkgPathStr +
+                               R"(" target=""/></files>
 </package>
 )";
         REQUIRE_EQUAL_TEXT(nuspec, expected);

toolsrc/src/vcpkg-test/commands.build.cpp
@@ -20,6 +20,7 @@ TEST_CASE ("build smoke test", "[commands-build]")
 
     auto& fs_wrapper = Files::get_real_filesystem();
     VcpkgCmdArguments args = VcpkgCmdArguments::create_from_arg_sequence(std::begin(args_raw), std::end(args_raw));
+    args.imbue_from_environment();
     args.binary_caching = false;
     args.buildtrees_root_dir =
         std::make_unique<std::string>(fs::u8string(Test::base_temporary_directory() / fs::u8path("buildtrees")));

toolsrc/src/vcpkg-test/manifests.cpp
@@ -681,6 +681,7 @@ TEST_CASE ("Serialize all the ports", "[manifests]")
     std::vector<std::string> args_list = {"format-manifest"};
     auto& fs = Files::get_real_filesystem();
     auto args = VcpkgCmdArguments::create_from_arg_sequence(args_list.data(), args_list.data() + args_list.size());
+    args.imbue_from_environment();
     VcpkgPaths paths{fs, args};
 
     std::vector<SourceControlFile> scfs;