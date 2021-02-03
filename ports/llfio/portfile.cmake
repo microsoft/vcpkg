f3fc75e2488dd1fd2c835a2fd708395673800105
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2021-02-03 10:16:38 +0000

    Have LLFIO not build static or shared libs according to VCPKG_LIBRARY_LINKAGE.

--- ports/llfio/portfile.cmake
+++ ports/llfio/portfile.cmake
@@ -52,8 +52,12 @@ vcpkg_configure_cmake(
 )
 
 # Build the shared library and static library editions
-vcpkg_build_cmake(TARGET _dl)
-vcpkg_build_cmake(TARGET _sl)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_build_cmake(TARGET _sl)
+endif()
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "shared")
+    vcpkg_build_cmake(TARGET _dl)
+endif()
 
 if("-DLLFIO_ENABLE_DEPENDENCY_SMOKE_TEST=ON" IN_LIST OUTCOME_FEATURE_OPTIONS)
     vcpkg_build_cmake(TARGET test)
@@ -63,8 +67,10 @@ vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/llfio)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/Licence.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
