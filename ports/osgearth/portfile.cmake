038397f17c772663497d9cebc1e6ec8c16495483
JackBoosY <yuzaiyang@beyondsoft.com>
2021-02-03 22:47:53 -0800

    [osgearth] Fix dependencies

--- ports/osgearth/portfile.cmake
+++ ports/osgearth/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_from_github(
         StaticOSG.patch # Fix port compilation in static-md module
         deprecated_cpp_fix.patch # Fix port headers to not use classes deprecated in c++17. Gives errors when using the installed port headers
         make-all-find-packages-required.patch
-        fix-tinyxml.patch
+        fix-dependencies.patch
         remove-tool-debug-suffix.patch
 )
 
