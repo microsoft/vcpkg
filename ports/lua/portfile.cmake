b0cfe41eb3831acd2c5049e68e077d00082a765a
luncliff <luncliff@gmail.com>
2021-02-08 15:30:36 +0900

    [lua] support iOS triplets
    * separate interpreter/compiler to lua[tools] feature
    * update git-tree SHA

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -6,41 +6,36 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    PATCHES vs2015-impl-c99.patch
+    PATCHES
+        vs2015-impl-c99.patch
+        fix-ios-system.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    cpp COMPILE_AS_CPP
+  INVERTED_FEATURES
+    tools SKIP_INSTALL_TOOLS
+)
+if(VCPKG_TARGET_IS_IOS)
+    if(FEATURE_OPTIONS MATCHES "-DSKIP_INSTALL_TOOLS=OFF")
+        message(FATAL_ERROR "lua[tools] is not supported for iOS platform build")
+    endif()
+endif()
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DCOMPILE_AS_CPP=OFF
+        ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         -DSKIP_INSTALL_HEADERS=ON
-        -DSKIP_INSTALL_TOOLS=ON
 )
 
 vcpkg_install_cmake()
-
-set(ENABLE_LUA_CPP 0)
-if("cpp" IN_LIST FEATURES)
-    set(ENABLE_LUA_CPP 1)
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS
-            -DCOMPILE_AS_CPP=ON
-        OPTIONS_DEBUG
-            -DSKIP_INSTALL_HEADERS=ON
-            -DSKIP_INSTALL_TOOLS=ON
-    )
-
-    vcpkg_install_cmake()
-endif()
-
 vcpkg_copy_pdbs()
-
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/lua)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
