da788f0deea100a8269356fa816af10a94665e27
azure-sdk <azuresdk@microsoft.com>
2021-02-09 17:27:38 +0000

    [azure-template-cpp] Update to 1.0.0-beta.28

---
+++ ports/azure-template-cpp/portfile.cmake
@@ -0,0 +1,23 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF azure-template_1.0.0-beta.28
+    SHA512 642c7c1b313926cf5c969021b556369fa52f295bcb2352cee3a89f46029e28440c073ee14a81b13cb3d40c6abba9260759f15b7fb324339f5a1e3a7ea2ea2b8b
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/sdk/template/azure-template/
+    PREFER_NINJA
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+)
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
+
