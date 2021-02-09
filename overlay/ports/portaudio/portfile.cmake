3eaba950a58ae815a7a58935815a1b532e3a56ca
Be <be@mixxx.org>
2021-02-09 13:07:08 -0600

    [portaudio] make ASIO an optional feature of the port

--- overlay/ports/portaudio/portfile.cmake
+++ overlay/ports/portaudio/portfile.cmake
@@ -2,15 +2,20 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Be-ing/portaudio
-    REF 5905aff5f72fa72dc1da97bfcfaea39ffe11c547
-    SHA512 3698e8948ef53410cd2875a8909d2e057721f3c05be033af1df0fbd19bff299f29fdef92fa9ef8b631f3f461b1ebe0c663e578da213c7d8b49b5b4c2235a62a7
+    REF 2d4d7d5f0fe29f34d30b5365411a2ff7587e94f5
+    SHA512 5932b764bff2cc06fcff417c655d86d894541ced08afcd352cfc4c731d66052562d3987f1870b106d91e366b700e01fb01c58ec654566efce8b9d5247a1b87ee
     HEAD_REF cmake_rewrite
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    asio ASIO
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS_DEBUG
-        -DDEBUG_OUTPUT:BOOL=ON
+    OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS_DEBUG -DDEBUG_OUTPUT:BOOL=ON
+    PREFER_NINJA
 )
 
 vcpkg_install_cmake()
