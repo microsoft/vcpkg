2601d4052da790e68c1b0ce179fa2218b6c2d4a3
Matt Oliver <protogonoi@gmail.com>
2021-02-13 01:04:47 +1100

    [libssh,libxml2,openjpeg,fontconfig] Fix pkg-config for release only triplets.

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -20,7 +20,9 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libxml-2.0.pc" "-lxml2" "-llibxml2")
+    if(NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libxml-2.0.pc" "-lxml2" "-llibxml2")
+    endif()
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libxml-2.0.pc" "-lxml2" "-llibxml2")
 endif ()
 vcpkg_fixup_pkgconfig()
