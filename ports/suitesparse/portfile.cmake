67d04365e8d0f7ed9bb2c4d97c2ed7fd830c199d
Vincent Traag <vincent@traag.net>
2021-02-13 22:53:58 +0100

    Suitesparse: Copy shared library to build directory.

--- ports/suitesparse/portfile.cmake
+++ ports/suitesparse/portfile.cmake
@@ -46,6 +46,8 @@ vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/suitesparse)
 
+vcpkg_copy_pdbs()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
