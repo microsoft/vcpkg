e64346ea8f2fbf7b4dd06768cde039e17f128877
Robert Schumacher <roschuma@microsoft.com>
2021-02-19 12:56:28 -0800

    [hazelcast-cpp-client] Avoid checking in large patch file

--- ports/hazelcast-cpp-client/portfile.cmake
+++ ports/hazelcast-cpp-client/portfile.cmake
@@ -1,20 +1,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hazelcast/hazelcast-cpp-client
-    REF v4.0.0
-    SHA512 fe58db4356eded6b433c21314586de4284d538e29cbcd979da141377432756d71f13224be1e07a12317dc8bf202791ad68c02c730dfd066a178d33278620dd26
+    REF 57ece8e82c4380472b4acff948db6a86ff0648b1 # v4.0.0 + fixes
+    SHA512 eb11677883f237e27562dfcd0f0e7e19ed439f8b0e13ca02942d2351b99ec54d4496cbaf8c3aaa08304b799f921641d51286bb7b7d4c6a0f1ff58cf954f3596f
     HEAD_REF master
-    PATCHES
-        fix-cmake-targets.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    openssl     WITH_OPENSSL
+    openssl WITH_OPENSSL
     example BUILD_EXAMPLES
 )
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
     OPTIONS ${FEATURE_OPTIONS}
 )
