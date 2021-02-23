0e68ad3bda109c64fd6a0605641176b827ec2059
Rene Stein <rene@renestein.net>
2021-02-23 20:35:14 +0100

    [rsasyncpp] Fix ci.baseline.txt, portfile.cmake, baseline.json, sasynccpp.json

--- ports/rsasynccpp/portfile.cmake
+++ ports/rsasynccpp/portfile.cmake
@@ -3,15 +3,15 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO 			renestein/Rstein.AsyncCpp
-    REF  			0.0.6
-    SHA512  		ba49d244eb294a907a7dab087c27120c7b2c36c89b150314b456b504eaaf3cc01125e66418ecf9f21b58468282f32dd637e252c5146c6e31fd8f87e98c87d7ab
-	HEAD_REF        master
+    OUT_SOURCE_PATH  SOURCE_PATH
+    REPO             renestein/Rstein.AsyncCpp
+    REF              0.0.6
+    SHA512           ba49d244eb294a907a7dab087c27120c7b2c36c89b150314b456b504eaaf3cc01125e66418ecf9f21b58468282f32dd637e252c5146c6e31fd8f87e98c87d7ab
+    HEAD_REF         master
 )
 
 set(RELEASE_CONFIGURATION "Release")
-set(DEBUG_CONFIGURATION "Debug")
+set(DEBUG_CONFIGURATION   "Debug")
 
 if (TRIPLET_SYSTEM_ARCH MATCHES "x86")
     set(MSBUILD_PLATFORM "x86")
@@ -20,19 +20,19 @@ else ()
 endif()
 
 if("lib-cl-win-legacy-await" IN_LIST FEATURES)
-     set(RELEASE_CONFIGURATION "Release_VSAWAIT")
-     set(DEBUG_CONFIGURATION "Debug_VSAWAIT")
+     set(RELEASE_CONFIGURATION  "Release_VSAWAIT")
+     set(DEBUG_CONFIGURATION    "Debug_VSAWAIT")
 endif()
 
 vcpkg_install_msbuild(
-    SOURCE_PATH 			${SOURCE_PATH}
-    PROJECT_SUBPATH 		RStein.AsyncCppLib.sln
-    LICENSE_SUBPATH 		LICENSE    
-	PLATFORM				${MSBUILD_PLATFORM}
-	DEBUG_CONFIGURATION 	${DEBUG_CONFIGURATION}
-	RELEASE_CONFIGURATION 	${RELEASE_CONFIGURATION}	
+    SOURCE_PATH             ${SOURCE_PATH}
+    PROJECT_SUBPATH         RStein.AsyncCppLib.sln
+    LICENSE_SUBPATH         LICENSE    
+    PLATFORM                ${MSBUILD_PLATFORM}
+    DEBUG_CONFIGURATION     ${DEBUG_CONFIGURATION}
+    RELEASE_CONFIGURATION   ${RELEASE_CONFIGURATION}    
 )
-	
-file(COPY 		 			"${SOURCE_PATH}/RStein.AsyncCpp/"
-	DESTINATION 			"${CURRENT_PACKAGES_DIR}/include/asynccpp"
-    FILES_MATCHING PATTERN 	"*.h")
+
+file(COPY                      "${SOURCE_PATH}/RStein.AsyncCpp/"
+    DESTINATION                "${CURRENT_PACKAGES_DIR}/include/asynccpp"
+    FILES_MATCHING PATTERN     "*.h")
