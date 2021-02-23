39c3fb2d21e8b06e54ef12c340adb44b913c0531
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2021-02-23 20:29:43 +1030

    Delete unnecessary comments.

--- ports/erfa/portfile.cmake
+++ ports/erfa/portfile.cmake
@@ -17,5 +17,3 @@ vcpkg_fixup_pkgconfig()
 
 configure_file("${SOURCE_PATH}/LICENSE" "${CURRENT_PACKAGES_DIR}/share/erfa/copyright" COPYONLY)
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/erfaConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-#file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-# file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libogg RENAME copyright)
