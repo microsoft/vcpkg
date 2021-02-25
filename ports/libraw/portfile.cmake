31386733b61188f18665d8e92efab013053c3228
Jonliu1993 <13720414433@163.com>
2021-02-25 16:58:20 +0800

    [libraw] update to snapshot 202101

--- ports/libraw/portfile.cmake
+++ ports/libraw/portfile.cmake
@@ -1,19 +1,19 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO LibRaw/LibRaw
-    REF d4f05dd1b9b2d44c8f7e82043cbad3c724db2416
-    SHA512 5794521f535163afd7815ad005295301c5e0e2f8b2f34ef0a911d9dd1572c1f456b292777548203f9767957a55782b5bc9041c033190d25d1e9b4240d7df32b9
+    REF aeb6a2b6f1e0f752340580e60a7ed56099b4ff21 #2021-2-24
+    SHA512 7eeccdc1cd1e35b4463ce3c2097a2e097de9aac49dccfeaa695024264d35a8f974b8e830b4f5b39c1a8130905fb6341edaccde6889bf125a65f8d2f20adcf90e
     HEAD_REF master
 )
 
 vcpkg_from_github(
     OUT_SOURCE_PATH LIBRAW_CMAKE_SOURCE_PATH
     REPO LibRaw/LibRaw-cmake
-    REF a71f3b83ee3dccd7be32f9a2f410df4d9bdbde0a
-    SHA512 607e6f76bcb57534da4f0c864b7a421f1ed49244468b1b52abe77f65aa599cae80715520b3a951294321b812deffd4f163757c9949f337571aa54f414ccc58a5
+    REF 5d54a89d1e3229b313b2cf3db8bf493dfc153cdb #2021-2-24
+    SHA512 546814981fa80f3befbf96f264fbd08092e83e2f4f42fe206a7bc997c19e35a4d8904689eaafc5fccacb083a518039bf73a507d705c4a73eaa93eb28b2de8a6f
     HEAD_REF master
     PATCHES
-        findlibraw_debug_fix.patch
+        fix-incorrect-pdb.patch
         lcms2_debug_fix.patch
 )
 
