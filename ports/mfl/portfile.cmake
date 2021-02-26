9a9ccd00855009e306016245efbe541821fb503f
cpp-niel <cpp.niel@gmail.com>
2021-02-26 15:26:23 +0100

    Check for static linkage and add message about C++20 requirements.

--- ports/mfl/portfile.cmake
+++ ports/mfl/portfile.cmake
@@ -1,3 +1,9 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+message("Warning: `mfl` is a C++20 library and requires a corresponding compiler. GCC 10, Clang 10 and MSVC 2019 16.8 are known to work.")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cpp-niel/mfl
