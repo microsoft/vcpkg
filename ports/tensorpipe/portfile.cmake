80b749ad60a91b8d755de85235cbffa9b842d7a2
luncliff <luncliff@gmail.com>
2021-02-26 15:45:04 +0900

    [tensorpipe] create a new port

---
+++ ports/tensorpipe/portfile.cmake
@@ -0,0 +1,35 @@
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO pytorch/tensorpipe
+    REF a9aa71a2fe49a8e8475cd5ff16cbd0de13b67c2b
+    SHA512 8b2679d4325acc6cd669326504bb49a645c126586b1d61d873d5a0423ed27e7c513574ccd2e74bdd792869167d3daba0804f127e21cfd5831d37a6d1ef7a79ba
+    # PATCHES
+    #     use-packages.patch # invoke find_package in CMakeLists.txt
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    cuda        TP_USE_CUDA
+    cuda        TP_ENABLE_CUDA_IPC
+    shm         TP_ENABLE_SHM
+    ibv         TP_ENABLE_IBV
+    cma         TP_ENABLE_CMA
+    pybind11    TP_BUILD_PYTHON
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DTP_BUILD_LIBUV=OFF
+        -DTP_BUILD_TESTING=OFF -DTP_BUILD_BENCHMARK=OFF
+)
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
+                    ${CURRENT_PACKAGES_DIR}/debug/bin
+)
