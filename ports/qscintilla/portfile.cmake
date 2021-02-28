a8d686334683a0b0af7b743d9af2a375782fdb80
Matthias Kuhn <matthias@opengis.ch>
2021-02-28 08:36:04 +0100

    Bump qscintilla to 2.12

--- ports/qscintilla/portfile.cmake
+++ ports/qscintilla/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.riverbankcomputing.com/static/Downloads/QScintilla/2.11.4/QScintilla-2.11.4.tar.gz"
-    FILENAME "QScintilla-2.11.4.tar.gz"
-    SHA512 90fc2427121ca9ae55e34cf636460099bbdadd844318d9ef05f86790a36e25fb64528264bb7bb99e46b7add96378eff0cc69bb692940c6a1bddfadf86a9abdbd
+    URLS "https://www.riverbankcomputing.com/static/Downloads/QScintilla/2.12.0/QScintilla_src-2.12.0.zip"
+    FILENAME "QScintilla-2.12.0.zip"
+    SHA512 94e826a68cfc313f7fe6caf47ca43fb43070869e698a9a4f266e0b472393c1dcaeedf33a2ecc6c7687af3f12a3b564ec160b580207311672368f9c8c28b0308e
 )
 
 vcpkg_extract_source_archive_ex(
@@ -18,7 +18,7 @@ get_filename_component(PYTHON3_PATH ${PYTHON3} DIRECTORY)
 vcpkg_add_to_path(${PYTHON3_PATH})
 
 vcpkg_configure_qmake(
-    SOURCE_PATH ${SOURCE_PATH}/Qt4Qt5
+    SOURCE_PATH ${SOURCE_PATH}/src
     OPTIONS
         CONFIG+=build_all
         CONFIG-=hide_symbols
@@ -34,7 +34,7 @@ else()
     vcpkg_install_qmake()
 endif()
 
-file(GLOB HEADER_FILES ${SOURCE_PATH}/Qt4Qt5/Qsci/*)
+file(GLOB HEADER_FILES ${SOURCE_PATH}/src/Qsci/*)
 file(COPY ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/Qsci)
 
 vcpkg_copy_pdbs()
