fb39f315054b3d25d645ee3283d01bb5a3f7b90a
Sam Clegg <sbc@chromium.org>
2021-03-04 08:45:39 -0800

    [triplets][wasm32-emscripten] Find emscripten installation based on PATH
    Not all emscripten users are emsdk users so `$EMSDK` won't always be
    present.  However its resonable to assume that all emscripten users
    (including emsdk users) will have `emcc` (the esmcripten compiler) in
    their `$PATH`
    
    Keep the old method of locating emscripten (which is fragile since it
    depends on the internal layout of emsdk) around in case there are any
    users that set `$EMSDK` themselves without adding emcc to the `$PATH`.
    This is unlikely since emsdk, when used in the normal way, takes care
    of adding the emscripten directory to `$PATH`.

--- triplets/community/wasm32-emscripten.cmake
+++ triplets/community/wasm32-emscripten.cmake
@@ -1,15 +1,20 @@
 set(VCPKG_ENV_PASSTHROUGH EMSDK PATH)
 
-if(NOT DEFINED ENV{EMSDK})
-   message(FATAL_ERROR "The EMSDK environment variable must be defined")
+find_path(EMSCRIPTEN_ROOT "emcc")
+if(NOT EMSCRIPTEN_ROOT)
+    # Old-lookup method, try to infer emscripten direcotyr based on $EMSDK environment variable
+    if(NOT DEFINED ENV{EMSDK})
+        message(FATAL_ERROR "emcc compiler not found in PATH")
+    endif()
+    set(EMSCRIPTEN_ROOT "ENV{EMSDK}//upstream/emscripten")
 endif()
 
-if(NOT EXISTS $ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
-   message(FATAL_ERROR "Emscripten.cmake toolchain file not found")
+if(NOT EXISTS ${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Emscripten.cmake)
+    message(FATAL_ERROR "Emscripten.cmake toolchain file not found")
 endif()
 
 set(VCPKG_TARGET_ARCHITECTURE wasm32)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE $ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE ${EMSCRIPTEN_ROOT}/cmake/Modules/Platform/Emscripten.cmake)
