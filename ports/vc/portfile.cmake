95277f8e73295a90780b35365942a7a4090b133e
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-03-05 12:47:09 +0100

    fix options passes to cmake configure

--- ports/vc/portfile.cmake
+++ ports/vc/portfile.cmake
@@ -13,8 +13,7 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    -DBUILD_TESTING=OFF
-    -DBUILD_EXAMPLES=OFF
+    OPTIONS -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
 )
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Vc/)
