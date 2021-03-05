03c2ff58bbb3470cb8a976fa21707f1c90bdac70
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2021-03-05 12:25:34 +0100

    use 1.4 branch of Vc as HEAD_REF

--- ports/vc/portfile.cmake
+++ ports/vc/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     REPO  VcDevel/Vc 
     REF 1.4.1
     SHA512 dd17e214099796c41d70416d365ea038c00c5fda285b05e48d7ee4fe03f4db2671d2be006ca7b98b0d4133bfcb57faf04cecfe35c29c3b006cd91c9a185cc04a
-    HEAD_REF master
+    HEAD_REF 1.4
     PATCHES
         "correct_cmake_config_path.patch"
 )
