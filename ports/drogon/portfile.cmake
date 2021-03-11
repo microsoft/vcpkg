bba4f50c6c486575b647652ef6a5b1eca4595c23
an-tao <antao2002@gmail.com>
2021-03-11 19:30:51 +0800

    [drogon] Update to 1.4.1

--- ports/drogon/portfile.cmake
+++ ports/drogon/portfile.cmake
@@ -1,46 +1,42 @@
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO an-tao/drogon
-    REF v1.3.0
-    SHA512 cddda4b90d28c15319b9cd1dea561c429b804508fc40678b9906fb70153cb36d7a4fc1c13fee01ec1f49d747a722856ceee364aba4118d922afabc3629ba2115
-    HEAD_REF master
-    PATCHES
-        vcpkg.patch
-        resolv.patch
-)
+  OUT_SOURCE_PATH
+  SOURCE_PATH
+  REPO
+  an-tao/drogon
+  REF
+  v1.4.1
+  SHA512
+  8611c18e65229095f5443f10c87e91593d619bc3a2d47da6d19d501a64be1aba12a813e44ccfc1c10179e5b0fa10121e8806955c0091258992501b88fa50d939
+  HEAD_REF
+  master
+  PATCHES
+  vcpkg.patch
+  resolv.patch)
 
-vcpkg_check_features(
-    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    ctl BUILD_CTL
-)
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DBUILD_EXAMPLES=OFF
-        ${FEATURE_OPTIONS}
-)
+vcpkg_configure_cmake(SOURCE_PATH
+                      ${SOURCE_PATH}
+                      PREFER_NINJA
+                      OPTIONS
+                      -DBUILD_EXAMPLES=OFF)
 
 vcpkg_install_cmake()
 
 # Fix CMake files
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Drogon)
 # Copy drogon_ctl
-if("ctl" IN_LIST FEATURES)
-    message("copying tools")
-    vcpkg_copy_tools(TOOL_NAMES drogon_ctl
-                     AUTO_CLEAN)
-endif()
+vcpkg_copy_tools(TOOL_NAMES drogon_ctl AUTO_CLEAN)
+
 # # Remove includes in debug
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin"
+       "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE
+     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+     RENAME copyright)
 
 # Copy pdb files
 vcpkg_copy_pdbs()
-
