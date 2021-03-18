f31830dadfbcb1b45524709b353228c6953cba50
luncliff <luncliff@gmail.com>
2021-03-18 14:44:00 +0900

    [fbgemm] update to latest code
    * removed asmjit related patch files

--- ports/fbgemm/portfile.cmake
+++ ports/fbgemm/portfile.cmake
@@ -6,11 +6,10 @@ vcpkg_find_acquire_program(PYTHON3)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/fbgemm
-    REF 975e41df8170115129abc524c225ddb88ec686dd
-    SHA512 2ee8b1464b29cd7ceb36665188f1b03e20d2c476b74fd81d2e3104a8e8f87196eca4497e7cb507eac576e717df8f306d05cc81a7122ed046814a171f6cfbe7b1
+    REF 51bc2ecc7086980fd9e6190163b04486cd37ac2a
+    SHA512 43e59ff25ec9365061be6a8db7ec592f096e74897e338dd3e17401f2188986cf3b7e26f82f7c1e2adf34496fb32cb4b50d40731bee3bf9fa5b1f2ac92a09e671
     PATCHES
         fix-cmakelists.patch
-        update-asmjit-usage.patch
 )
 
 vcpkg_configure_cmake(
@@ -20,7 +19,7 @@ vcpkg_configure_cmake(
         -DUSE_SANITIZER=OFF
         -DFBGEMM_BUILD_TESTS=OFF
         -DFBGEMM_BUILD_BENCHMARKS=OFF
-        -DPYTHON_EXECUTABLE=${PYTHON3} # inject the path instead of find_package(Python) while configuration
+        -DPYTHON_EXECUTABLE=${PYTHON3} # inject the path instead of find_package(Python)
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
