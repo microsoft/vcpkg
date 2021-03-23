d39e9f4ce7ee1660e40b42ad3cd4f71c5422d983
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-03-23 10:28:37 +0100

    [aubio] add suggestions

--- ports/aubio/portfile.cmake
+++ ports/aubio/portfile.cmake
@@ -11,7 +11,12 @@ vcpkg_from_github(
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    tools    WITH_DEPENDENCIES
+    tools WITH_DEPENDENCIES
+    tools HAVE_SNDFILE
+    tools HAVE_WAVWRITE
+    tools HAVE_WAVREAD
+    tools LIBAV
+    tools SWRESAMPLE
 )
 
 vcpkg_configure_cmake(
