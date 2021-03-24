dd6be869463c482a0c0057428b03fa4685670775
NancyLi1013 <lirui09@beyondsoft.com>
2021-03-23 23:37:42 -0700

    [bde] Update to 3.61.0.0 and support cmake build

--- ports/bde/portfile.cmake
+++ ports/bde/portfile.cmake
@@ -1,136 +1,48 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-set(CONFIGURE_COMMON_ARGS ${CONFIGURE_COMMON_ARGS} --library-type=static)
-
-set(BDE_VERSION 3.2.0.0)
-set(BDE_TOOLS_VERSION 1.x)
-
-# Paths used in build
-set(SOURCE_PATH_DEBUG ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/bde-${BDE_VERSION})
-set(SOURCE_PATH_RELEASE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/bde-${BDE_VERSION})
-
 # Acquire Python 2 and add it to PATH
 vcpkg_find_acquire_program(PYTHON2)
 get_filename_component(PYTHON2_EXE_PATH ${PYTHON2} DIRECTORY)
+vcpkg_add_to_path(${PYTHON2_EXE_PATH})
 
 # Acquire BDE Tools and add them to PATH
 vcpkg_from_github(
     OUT_SOURCE_PATH TOOLS_PATH
     REPO "bloomberg/bde-tools"
-    REF d4b1a7670829228f4ec81ecdccc598ce03ae8e80
-    SHA512 80af734c080adb225d5369157301ae0af18e02b1912351e34d23f5f2ba4e19f9ae2b5a367923f036330c9f9afd11a90cdf12680eb3e59b4297a312a1b713f17f
+    REF ee80556db9afbc4107bf9cdcc51333829f251d9f # 3.61.0.0
+    SHA512 fa8f0741a174f8d1f08c00d94e04db5a1afca4eeec3d1dec673ab0a0bc19bd5eb7d3f5d1cb8b532ae53c90fb1e1362dbc5ae78e5294637b7318fac8018af80d5
     HEAD_REF master
 )
-message(STATUS "Configure bde-tools-v${BDE_TOOLS_VERSION}")
-if(VCPKG_CMAKE_SYSTEM_NAME)
-    set(ENV{PATH} "$ENV{PATH}:${PYTHON2_EXE_PATH}")
-    set(ENV{PATH} "$ENV{PATH}:${TOOLS_PATH}/bin")
-else()
-    set(ENV{PATH} "$ENV{PATH};${PYTHON2_EXE_PATH}")
-    set(ENV{PATH} "$ENV{PATH};${TOOLS_PATH}/bin")
-endif()
+
+vcpkg_add_to_path(${TOOLS_PATH}/bin)
 
 # Acquire BDE sources
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "bloomberg/bde"
-    REF 3720d132d0879f19b9084cca62ebc75f1f24e1a3
-    SHA512 234ebb71997f5b7d3951584235ead10f977689cef323ae1c198629a6b1995b1481d8a1515d827c46df10209bdc66e1f3cc7780dafee9ca0ff4172be47c460d78
+    REF a8e484bcb405f14ea76159ceb12b61dc9ba3455f # 3.61.0.0
+    SHA512 66b89273ad8bc1c9ef62bb1d548e501cd40d8f94be8b8bce9ae68d1b0fb74958dd6bfbd10c4f42da264ba145e22da546a672f7bead57b1c8daad4c43ac223de5
     HEAD_REF master
 )
 
-# Clean up previous builds
-file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-                    ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
-
-# Identify waf executable and calculate configure args
-if(VCPKG_CMAKE_SYSTEM_NAME)
-    set(WAF_COMMAND waf)
-else()
-    set(WAF_COMMAND waf.bat)
-endif()
-set(CONFIGURE_COMMON_ARGS ${CONFIGURE_COMMON_ARGS} --use-flat-include-dir)
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(CONFIGURE_COMMON_ARGS ${CONFIGURE_COMMON_ARGS} --abi-bits=32)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(CONFIGURE_COMMON_ARGS ${CONFIGURE_COMMON_ARGS} --abi-bits=64)
-else()
-    message(FATAL_ERROR "Unsupported target architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-endif()
+#https://bloomberg.github.io/bde/library_information/build.html
 if(VCPKG_TARGET_IS_WINDOWS)
-    set(CONFIGURE_COMMON_ARGS ${CONFIGURE_COMMON_ARGS} --msvc-runtime-type=static)
-else()
-    set(ENV{CFLAGS} "$ENV{CFLAGS} -Wno-error=implicit-function-declaration")
+    set(CMAKE_TOOLCHAIN_FILE ${TOOLS_PATH}/cmake/toolchains/win32/cl-default.cmake)
+elseif(VCPKG_TARGET_IS_LINUX)
+    set(CMAKE_TOOLCHAIN_FILE ${TOOLS_PATH}/cmake/toolchains/linux/gcc-default.cmake)
+elseif(VCPKG_TARGET_IS_OSX)
+    set(CMAKE_TOOLCHAIN_FILE ${TOOLS_PATH}/cmake/toolchains/darwin/gcc-default.cmake)    
 endif()
 
-# Configure debug
-message(STATUS "Configuring ${TARGET_TRIPLET}-dbg")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} configure ${CONFIGURE_COMMON_ARGS}
-            --prefix=${CURRENT_PACKAGES_DIR}/debug --out=${SOURCE_PATH_DEBUG}
-            --build-type=debug
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME configure-${TARGET_TRIPLET}--dbg
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DCMAKE_MODULE_PATH=${TOOLS_PATH}/cmake
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
 )
-message(STATUS "Configuring ${TARGET_TRIPLET}-dbg done")
 
-# Build debug
-message(STATUS "Building ${TARGET_TRIPLET}-dbg")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} build
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME build-${TARGET_TRIPLET}--dbg
-)
-message(STATUS "Building ${TARGET_TRIPLET}-dbg done")
+vcpkg_install_cmake()    
 
-# Install debug
-message(STATUS "Installing ${TARGET_TRIPLET}-dbg")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} install
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME install-${TARGET_TRIPLET}--dbg
-)
-# Include files should not be duplicated
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-# pkg-config files should point to correct include directory
-file(GLOB PC_FILES "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/*.pc")
-foreach(PC_FILE_NAME ${PC_FILES})
-    file(READ "${PC_FILE_NAME}" _contents)
-    string(REPLACE "includedir=\${prefix}/include" "includedir=\${prefix}/../include" _contents "${_contents}")
-    file(WRITE "${PC_FILE_NAME}" "${_contents}")
-endforeach()
-message(STATUS "Installing ${TARGET_TRIPLET}-dbg done")
-
-# Configure release
-message(STATUS "Configuring ${TARGET_TRIPLET}-rel")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} configure ${CONFIGURE_COMMON_ARGS}
-            --prefix=${CURRENT_PACKAGES_DIR} --out=${SOURCE_PATH_RELEASE}
-            --build-type=release
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME configure-${TARGET_TRIPLET}--rel
-)
-message(STATUS "Configuring ${TARGET_TRIPLET}-rel done")
-
-# Build release
-message(STATUS "Building ${TARGET_TRIPLET}-rel")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} build
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME build-${TARGET_TRIPLET}--rel
-)
-message(STATUS "Building ${TARGET_TRIPLET}-rel done")
-
-# Install release
-message(STATUS "Installing ${TARGET_TRIPLET}-rel")
-vcpkg_execute_required_process(
-    COMMAND ${WAF_COMMAND} install
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME install-${TARGET_TRIPLET}--rel
-)
-message(STATUS "Installing ${TARGET_TRIPLET}-rel done")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/bde
-     RENAME copyright
-)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
