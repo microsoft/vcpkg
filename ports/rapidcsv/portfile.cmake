6eff85a63aa4a56fbae6a00b5d589b0524dea65a
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-03-27 01:09:23 +0100

    [rapidcsv] apply suggestions

--- ports/rapidcsv/portfile.cmake
+++ ports/rapidcsv/portfile.cmake
@@ -6,10 +6,10 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
 )
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
