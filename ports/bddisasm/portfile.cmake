5c162bc55834ef80e64e6ae428cefea11575df09
Ionel-Cristinel ANICHITEI <ianichitei@bitdefender.com>
2021-03-31 10:03:15 +0300

    bddisasm: Don't use vcpkg_install_msbuild

--- ports/bddisasm/portfile.cmake
+++ ports/bddisasm/portfile.cmake
@@ -27,26 +27,3 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
-
-if (VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${SOURCE_PATH}
-        PROJECT_SUBPATH bddisasm/bddisasm.vcxproj
-        USE_VCPKG_INTEGRATION
-    )
-
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${SOURCE_PATH}
-        PROJECT_SUBPATH bdshemu/bdshemu.vcxproj
-        USE_VCPKG_INTEGRATION
-    )
-
-    file(COPY ${SOURCE_PATH}/inc/
-        DESTINATION ${CURRENT_PACKAGES_DIR}/include/bddisasm
-        FILES_MATCHING PATTERN *.h
-    )
-
-    # disasmtool.exe will be placed here, but it shouldn't. We can't delete only disasmtool.exe because that will
-    # leave us with two empty directories, and that's an error.
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/" "${CURRENT_PACKAGES_DIR}/debug/bin/")
-endif ()
