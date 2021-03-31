f3b2836041d66003bc88e281607998137de7b664
Florian Schaeffeler <florian.schaeffeler@avira.com>
2021-03-31 11:53:17 +0200

    Fix formatting and usage of vcpkg_fail_port.

--- ports/keychain/portfile.cmake
+++ ports/keychain/portfile.cmake
@@ -1,35 +1,24 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "${PORT} does not currently support UWP")
-endif()
-
+vcpkg_fail_port_install(ON_TARGET "uwp")
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-set(BUILD_TESTS OFF)
 
+set(BUILD_TESTS OFF)
 vcpkg_from_github(
-    OUT_SOURCE_PATH
-    SOURCE_PATH
-    REPO
-    hrantzsch/keychain
-    REF
-    v1.2.0
-    SHA512
-    8faed892e6d84ad3d31056682dc4bb18ff8c12a3eababfa58e3c01ad369da1d9b0772198e15196b49b4de895a44ff7e96a59b56b87011f95ec88bcae819fe6ff
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO hrantzsch/keychain
+    REF v1.2.0
+    SHA512 8faed892e6d84ad3d31056682dc4bb18ff8c12a3eababfa58e3c01ad369da1d9b0772198e15196b49b4de895a44ff7e96a59b56b87011f95ec88bcae819fe6ff
 )
 
 vcpkg_configure_cmake(
-    SOURCE_PATH
-    ${SOURCE_PATH}
-    PREFER_NINJA
-    DISABLE_PARALLEL_CONFIGURE
-    OPTIONS
-    -DBUILD_TESTS:BOOL=${BUILD_TESTS}
+    SOURCE_PATH ${SOURCE_PATH} 
+    PREFER_NINJA 
+    DISABLE_PARALLEL_CONFIGURE 
+    OPTIONS -DBUILD_TESTS:BOOL=${BUILD_TESTS}
 )
 
 vcpkg_install_cmake()
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(
-    INSTALL ${SOURCE_PATH}/LICENSE
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/keychain
-    RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
