47b40ebb52195a64f3cd39b3ff48bf82df5859f1
NancyLi1013 <lirui09@beyondsoft.com>
2021-04-01 03:48:00 -0700

    [soil] Update repository

--- ports/soil/portfile.cmake
+++ ports/soil/portfile.cmake
@@ -1,19 +1,15 @@
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.lonesock.net/files/soil.zip"
-    FILENAME "soil-2008.07.07.zip"
-    SHA512 a575a84aa65b7556320779d635561341f5cf156418d0462473e5d1eb082829be3bcb30600b4887af75aeddd3715de16bdb3ca1668ebaa93eea62bacf22b79548
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    REPO littlstar/soil
+    REF 475f3d83433582618aae8a3c89eb978b01e263a3 # 0.1.0
+    SHA512 89da87da9c9f21ba7ccfbff6f6c46f2463f672d2ce13a37a9b161eeef471f5a31fa751c80dba9378c77d878148ca77012dec861461d3a7b6966a34951157ae2f
+    HEAD_REF master
 )
 
 file(COPY
     ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
     ${CMAKE_CURRENT_LIST_DIR}/SOILConfig.cmake.in
     ${CMAKE_CURRENT_LIST_DIR}/SOILConfigVersion.cmake.in
-    ${CMAKE_CURRENT_LIST_DIR}/LICENSE
     DESTINATION ${SOURCE_PATH}
 )
 
@@ -25,4 +21,4 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
