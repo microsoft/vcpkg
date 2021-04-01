f6e217947cc4c9cd2d9d0acb4c6b0036a885f3e4
Florian Schaeffeler <florian.schaeffeler@avira.com>
2021-04-01 16:21:17 +0200

    Throw error if libsecret-1-dev isn't installed.

--- ports/keychain/portfile.cmake
+++ ports/keychain/portfile.cmake
@@ -9,6 +9,15 @@ vcpkg_from_github(
     SHA512 8faed892e6d84ad3d31056682dc4bb18ff8c12a3eababfa58e3c01ad369da1d9b0772198e15196b49b4de895a44ff7e96a59b56b87011f95ec88bcae819fe6ff
 )
 
+if(VCPKG_TARGET_IS_LINUX)
+    if (NOT EXISTS "/usr/include/libsecret-1/libsecret/secret.h")
+        message(FATAL_ERROR "keychain requires libsecret-1-dev, please use your distribution's package manager to install them.\n"
+                            "Debian/Ubuntu: sudo apt-get install libsecret-1-dev\n"
+                            "Red Hat/CentOS/Fedora: sudo yum install libsecret-devel\n"
+                            "Arch Linux: sudo pacman -Sy libsecret\n")
+    endif()
+endif()
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH} 
     PREFER_NINJA 
