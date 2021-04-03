237b9c458436cb6710e2751135f6f7751522b5a3
luncliff <luncliff@gmail.com>
2021-04-04 00:30:53 +0900

    [xnnpack] create a patch

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -1,19 +1,18 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/XNNPACK
-    REF a5e242c633af4e577fa206411082f1aba276602d
-    SHA512 6b864018ae6689811a9a198fbd995330ab168b284c78907b170bdedd0e9dc2f99d1ff1b4fdd100a01e604ff83c6481d7845a533e37d0893288f41c896e91cace
-    # PATCHES
-    #     use-packages.patch # invoke find_package in CMakeLists.txt
+    REF 07feec8df3927f3c150dd0cf7db9a54927bd2569
+    SHA512 19db1c75ee580864126b785f75bcc714cbae5fe1ddfc71122883c84d5fcae51b14815e1eed57ff15fd88d75ac13990728d3af286204cef3c15af8edfd0b3224f
+    HEAD_REF master
+    PATCHES
+        use-packages.patch
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
-        -DXNNPACK_LIBRARY_TYPE=default
-        -DXNNPACK_USE_SYSTEM_LIBS=OFF
+        -DXNNPACK_USE_SYSTEM_LIBS=ON
         -DXNNPACK_ENABLE_ASSEMBLY=ON
         -DXNNPACK_ENABLE_MEMOPT=ON
         -DXNNPACK_ENABLE_SPARSE=ON
