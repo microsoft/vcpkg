80c8795da674474f432e3ddbabed406ebc35543b
aggieNick02 <nick@pcpartpicker.com>
2021-04-05 14:00:06 -0500

    CMake-based port for presentmon, based on source provided by JackBoosY

--- ports/presentmon/portfile.cmake
+++ ports/presentmon/portfile.cmake
@@ -1,5 +1,9 @@
+# The upstream doesn't export any symbols
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_fail_port_install(ON_TARGET "linux" "osx" "uwp" "ios" "android" "freebsd")
 
+set(PRESENTMON_VERSION 1.6.0)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GameTechDev/PresentMon
@@ -7,21 +11,26 @@ vcpkg_from_github(
     SHA512 2522b0e3218d4a6588531a09bc82631f14ad05c20f4560fe0574f00f2f5eece114ae04320f920eb52ba64173cea5cdf15bb223b7395c3782e4a6465afb5d9bec
     HEAD_REF main
 )
-set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
-# A bit silly - this is not honored, but we make it dynamic so that vcpkg_install_msbuild does not disable whole program optimization (which it does if static)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-set(PLATFORM ${VCPKG_TARGET_ARCHITECTURE})
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(PLATFORM x86)
+
+file(COPY ${CURRENT_PORT_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    tools BUILD_TOOLS
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS_DEBUG
+        -DINSTALL_HEADERS=OFF
+)
+
+vcpkg_install_cmake()
+
+if("tools" IN_LIST FEATURES)
+    vcpkg_copy_tools(TOOL_NAMES presentmon AUTO_CLEAN)
 endif()
 
-vcpkg_install_msbuild(
-    SOURCE_PATH ${SOURCE_PATH}
-    PROJECT_SUBPATH PresentMon.sln
-    PLATFORM ${PLATFORM}
-)
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(GLOB PRESENTMON_EXE ${CURRENT_PACKAGES_DIR}/tools/${PORT}/PresentMon-*.exe)
-file(GLOB PRESENTMONTESTS_EXE ${CURRENT_PACKAGES_DIR}/tools/${PORT}/PresentMonTests-*.exe)
-file(RENAME ${PRESENTMON_EXE} ${CURRENT_PACKAGES_DIR}/tools/${PORT}/PresentMon.exe)
-file(REMOVE ${PRESENTMONTESTS_EXE})
