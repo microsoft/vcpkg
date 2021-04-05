b6acb380f54d8c9402127dcbc4dbb04115ea5880
veggiesaurus <accomrie@gmail.com>
2021-04-05 20:59:05 +0200

    use static linking

--- ports/starlink-ast/portfile.cmake
+++ ports/starlink-ast/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_fail_port_install(MESSAGE "starlink-ast currently only supports Windows and Mac platforms" ON_TARGET "Linux" "OSX")
+vcpkg_fail_port_install(MESSAGE "starlink-ast currently only supports Windows" ON_TARGET "Linux" "OSX")
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/Starlink/ast/releases/download/v9.2.3/ast-9.2.3.tar.gz"
@@ -14,25 +14,26 @@ vcpkg_extract_source_archive_ex(
         "patch-avoid-gcc-specifics.patch"
 )
 
-
-set(CONFIGURE_OPTIONS "CFLAGS=-DCMINPACK_NO_DLL --without-fortran --without-pthreads --without-yaml star_cv_cnf_trail_type=long star_cv_cnf_f2c_compatible=no")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --disable-static --enable-shared")
-else()
-    set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --enable-static --disable-shared")
-endif()
-
+set(CONFIGURE_OPTIONS "CFLAGS=-DCMINPACK_NO_DLL --disable-shared --without-fortran --without-pthreads --without-yaml star_cv_cnf_trail_type=long star_cv_cnf_f2c_compatible=no")
+set(CONFIGURE_OPTIONS_RELEASE "--disable-debug --enable-release --prefix=${CURRENT_PACKAGES_DIR}")
+set(CONFIGURE_OPTIONS_DEBUG  "--enable-debug --disable-release --prefix=${CURRENT_PACKAGES_DIR}/debug")
 set(RELEASE_TRIPLET ${TARGET_TRIPLET}-rel)
 set(DEBUG_TRIPLET ${TARGET_TRIPLET}-dbg)
+
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --host=x86_64-w64-mingw32")
+else()
+    set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --host=i686-w64-mingw32")
+endif()
 
 vcpkg_acquire_msys(MSYS_ROOT PACKAGES make automake1.16 perl)    
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     USE_WRAPPERS
     OPTIONS ${CONFIGURE_OPTIONS}
+    OPTIONS_RELEASE ${CONFIGURE_OPTIONS_RELEASE}
+    OPTIONS_DEBUG ${CONFIGURE_OPTIONS_DEBUG}
 )
-
 
 file(COPY ${SOURCE_PATH}/ast_par.source DESTINATION ${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET})
 file(COPY ${SOURCE_PATH}/ast_par.source DESTINATION ${CURRENT_BUILDTREES_DIR}/${DEBUG_TRIPLET})
