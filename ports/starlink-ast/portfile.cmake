ceb4b38594845984225fa2de8d4cdf337b4567d6
veggiesaurus <accomrie@gmail.com>
2021-04-05 14:41:25 +0200

    almost working...

--- ports/starlink-ast/portfile.cmake
+++ ports/starlink-ast/portfile.cmake
@@ -45,13 +45,15 @@ vcpkg_extract_source_archive_ex(
     # REF 1.0.0
     # (Optional) Read the docs for how to generate patches at:
     # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
-    # PATCHES
+    PATCHES
+        "patch-use-exeetc-envar.patch"
+        "patch-avoid-gcc-specifics.patch"
     #   001_port_fixes.patch
     #   002_more_port_fixes.patch
 )
 
 
-set(CONFIGURE_OPTIONS "--without-fortran --without-stardocs --without-pthreads --without-yaml")
+set(CONFIGURE_OPTIONS "--without-fortran --without-pthreads --without-yaml")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --disable-static --enable-shared")
@@ -67,107 +69,32 @@ set(DEBUG_TRIPLET ${TARGET_TRIPLET}-dbg)
 if(NOT VCPKG_TARGET_IS_WINDOWS)
     mesage(ERROR "TODO!")
 else()
-
+    #set(ENV{AR_FLAGS} "-verbose")
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
         set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --host=x86_64-w64-mingw32")
     else()
         set(CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS} --host=i686-w64-mingw32")
-    endif()
+    endif()    
 
     # Acquire tools
-    vcpkg_acquire_msys(MSYS_ROOT PACKAGES make automake1.16)
-    # Insert msys into the path between the compiler toolset and windows system32. This prevents masking of "link.exe" but DOES mask "find.exe".
-    string(REPLACE ";$ENV{SystemRoot}\\system32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\system32;" NEWPATH "$ENV{PATH}")
-    string(REPLACE ";$ENV{SystemRoot}\\System32;" ";${MSYS_ROOT}/usr/bin;$ENV{SystemRoot}\\System32;" NEWPATH "${NEWPATH}")
-    set(ENV{PATH} "${NEWPATH}")
-    set(ENV{LD} "link.exe")
-    set(BASH ${MSYS_ROOT}/usr/bin/bash.exe)    
+    vcpkg_acquire_msys(MSYS_ROOT PACKAGES make automake1.16 perl)    
+    vcpkg_configure_make(
+        SOURCE_PATH "${SOURCE_PATH}"
+        USE_WRAPPERS
+        OPTIONS ${CONFIGURE_OPTIONS}
+    )
+    
 
-    if(VCPKG_CRT_LINKAGE STREQUAL static)
-        set(AST_CRT_LINKAGE --enable-static-msvcrt)
-        set(LIBVPX_CRT_SUFFIX mt)
-    else()
-        set(LIBVPX_CRT_SUFFIX md)
-    endif()
+    file(COPY ${SOURCE_PATH}/ast_par.source DESTINATION ${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET})
+    file(COPY ${SOURCE_PATH}/ast_par.source DESTINATION ${CURRENT_BUILDTREES_DIR}/${DEBUG_TRIPLET})
+    file(COPY ${SOURCE_PATH}/makeh DESTINATION ${CURRENT_BUILDTREES_DIR}/${DEBUG_TRIPLET})
+    file(COPY ${SOURCE_PATH}/makeh DESTINATION ${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET})
 
-    if(NOT VCPKG_TARGET_IS_MINGW)
-        set(PLATFORM "MSYS/MSVC")
-        if(VCPKG_CRT_LINKAGE STREQUAL static)
-            set(ICU_RUNTIME "-MT")
-        else()
-            set(ICU_RUNTIME "-MD")
-        endif()
-        set(EXTRA_RELEASE_FLAGS "${ICU_RUNTIME} -O2 -Oi -Zi -FS")
-        set(RELEASE_LDFLAGS "-DEBUG -INCREMENTAL:NO -OPT:REF -OPT:ICF")
-        set(EXTRA_DEBUG_FLAGS "${ICU_RUNTIME}d -Od -Zi -FS -RTC1")
-        set(DEBUG_LDFLAGS "-DEBUG")
-    else()
-        set(PLATFORM "MinGW")
-        set(ENV{CC} "${CMAKE_C_COMPILER}")
-        set(ENV{CXX} "${CMAKE_CXX_COMPILER}")
-    endif()
-
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        # Configure release
-        message(STATUS "Configuring ${RELEASE_TRIPLET}")
-        file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET})
-        file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET})
-        set(ENV{CFLAGS} "${EXTRA_RELEASE_FLAGS} ${VCPKG_C_FLAGS} ${VCPKG_C_FLAGS_RELEASE}")
-        set(ENV{CXXFLAGS} "${EXTRA_RELEASE_FLAGS} ${VCPKG_CXX_FLAGS} ${VCPKG_CXX_FLAGS_RELEASE}")
-        set(ENV{LDFLAGS} "${RELEASE_LDFLAGS}")
-        vcpkg_execute_required_process(
-            COMMAND ${BASH} --noprofile --norc -c
-                "${SOURCE_PATH}/configure CFLAGS=-DCMINPACK_NO_DLL --without-pthreads --without-fortran --without-stardocs --enable-shared=no"
-            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET}"
-            LOGNAME "configure-${RELEASE_TRIPLET}")
-        message(STATUS "Configuring ${RELEASE_TRIPLET} done")
-    endif()
-
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-        # Configure debug
-        message(STATUS "Configuring ${DEBUG_TRIPLET}")
-        file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${DEBUG_TRIPLET})
-        file(MAKE_DIRECTORY ${CURRENT_BUILDTREES_DIR}/${DEBUG_TRIPLET})
-        set(ENV{CFLAGS} "${EXTRA_DEBUG_FLAGS} ${VCPKG_C_FLAGS} ${VCPKG_C_FLAGS_DEBUG}")
-        set(ENV{CXXFLAGS} "${EXTRA_DEBUG_FLAGS} ${VCPKG_CXX_FLAGS} ${VCPKG_CXX_FLAGS_DEBUG}")
-        set(ENV{LDFLAGS} "${DEBUG_LDFLAGS}")
-        vcpkg_execute_required_process(
-            COMMAND ${BASH} --noprofile --norc -c
-                "${SOURCE_PATH}/configure ${AST_CRT_LINKAGE} ${CONFIGURE_OPTIONS} ${CONFIGURE_OPTIONS_DEBUG}"
-            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${RELEASE_TRIPLET}"
-            LOGNAME "configure-${RELEASE_TRIPLET}")
-        message(STATUS "Configuring ${DEBUG_TRIPLET} done")
-    endif()
+    vcpkg_install_make()
 endif()
 
-unset(ENV{CFLAGS})
-unset(ENV{CXXFLAGS})
-unset(ENV{LDFLAGS})
-
-endif()
-
-# # Check if one or more features are a part of a package installation.
-# # See /docs/maintainers/vcpkg_check_features.md for more details
-# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-#   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
-#     tbb   WITH_TBB
-#   INVERTED_FEATURES
-#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
-# )
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
-)
-
-vcpkg_install_cmake()
-
-# # Moves all .cmake files from /debug/share/starlink-ast/ to /share/starlink-ast/
-# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
-# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/starlink-ast)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
 
 # # Handle copyright
-# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/starlink-ast RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/starlink-ast RENAME copyright)
