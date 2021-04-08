fbc05afad0e76620ecebd5b7b09654a20205a717
electron <66094410+electron271@users.noreply.github.com>
2021-04-08 18:53:02 -0500

    Create portfile.cmake

---
+++ ports/ncurses/portfile.cmake
@@ -0,0 +1,7 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mirror/ncurses
+    REF v6.2
+    SHA512 89eea695388837b5a6c51b3e8aa984ec51afdeba7652448992f1a0c83acc7a537fc8f9fef7f8ae438aadc06d6810f09add4b129c46a61b1bc5d4c0e4bf5f767e
+    HEAD_REF master
+)
