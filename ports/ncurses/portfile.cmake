a1e83acd88a60944ff6e44a12e545ed7673f9b7a
electron <66094410+electron271@users.noreply.github.com>
2021-04-09 18:17:22 -0500

    Update ports/ncurses/portfile.cmake
    Co-authored-by: Adam Johnson <AdamJohnso@gmail.com>

--- ports/ncurses/portfile.cmake
+++ ports/ncurses/portfile.cmake
@@ -1,4 +1,5 @@
 vcpkg_download_distfile(
+    SOURCE_PATH
     URLS "https://invisible-mirror.net/archives/ncurses/current/ncurses-6.2-20210403.tgz"
     FILENAME "ncurses-6.2-20210403.tgz"
     SHA512 45d55a99cc4090d7f6ec58dd0a8f1b2839a0949f8073d73ed13c98989542dfda0ecf8fb8652ff2a952a8b7fcbbebac8a2070adb84522fc0084d6404c5bd1d6ad
