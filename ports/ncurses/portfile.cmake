a3fe2bd9d722b0b67c5d946ee5112233ce8735c8
electron <66094410+electron271@users.noreply.github.com>
2021-04-09 08:24:55 -0500

    Update portfile.cmake

--- ports/ncurses/portfile.cmake
+++ ports/ncurses/portfile.cmake
@@ -1,9 +1,7 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO mirror/ncurses
-    REF v6.2
+vcpkg_download_distfile(
+    URLS "https://invisible-mirror.net/archives/ncurses/current/ncurses-6.2-20210403.tgz"
+    FILENAME "ncurses-6.2-20210403.tgz"
     SHA512 45d55a99cc4090d7f6ec58dd0a8f1b2839a0949f8073d73ed13c98989542dfda0ecf8fb8652ff2a952a8b7fcbbebac8a2070adb84522fc0084d6404c5bd1d6ad
-    HEAD_REF master
 )
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
