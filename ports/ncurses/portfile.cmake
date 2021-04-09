f05c5e696129a68772e99f76d3e61c49dc4ca25a
Ahmedbot <ahmedanzaldua@gmail.com>
2021-04-09 17:48:45 -0500

    Update portfile.cmake

--- ports/ncurses/portfile.cmake
+++ ports/ncurses/portfile.cmake
@@ -3,9 +3,12 @@ vcpkg_download_distfile(
     FILENAME "ncurses-6.2-20210403.tgz"
     SHA512 45d55a99cc4090d7f6ec58dd0a8f1b2839a0949f8073d73ed13c98989542dfda0ecf8fb8652ff2a952a8b7fcbbebac8a2070adb84522fc0084d6404c5bd1d6ad
 )
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
 )
+
 vcpkg_install_cmake()
+
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/ncurses RENAME copyright)
