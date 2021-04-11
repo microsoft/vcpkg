66cebe0baaa04606f943e2fff15bb94cf128cbbf
myd7349 <myd7349@gmail.com>
2021-04-11 16:53:20 +0800

    [yyjson] Patch CMakeLists.txt

--- ports/yyjson/portfile.cmake
+++ ports/yyjson/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF c3a1f1f54a517dc15ba8670d969c0acdfabcd039
     SHA512 57d75ee51826f6cf8e583038ec39642e469558bdc3485d6ac5173d6dc293c48055af11fcb0c9c0be3ce782af50bb8c8453f3fec89d4450e3003c2a62237d76de
     HEAD_REF master
+    PATCHES
+        cmake-install.patch
 )
 
 vcpkg_configure_cmake(
@@ -15,6 +17,8 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
