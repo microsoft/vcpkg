1ce819eb40c941336a6c85ec08fdb362398cd81b
myd7349 <myd7349@gmail.com>
2021-04-12 06:55:34 +0800

    [yyjson] Update to 2021-04-12

--- ports/yyjson/portfile.cmake
+++ ports/yyjson/portfile.cmake
@@ -1,12 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ibireme/yyjson
-    REF c3a1f1f54a517dc15ba8670d969c0acdfabcd039
-    SHA512 57d75ee51826f6cf8e583038ec39642e469558bdc3485d6ac5173d6dc293c48055af11fcb0c9c0be3ce782af50bb8c8453f3fec89d4450e3003c2a62237d76de
+    REF 31313e8c15e3c221c7452fd99bf7fdf89b6d92c1
+    SHA512 05e9cf3e5db5e79188f0207f865744ef411ffef50824783e1d9f4be7ad49fc67aa7490723c44f38c5de336840bbeb948c160bdef9312b97119a6733ff3c1ae04
     HEAD_REF master
-    PATCHES
-        cmake-install.patch
-        fix-uwp-build.patch
 )
 
 vcpkg_configure_cmake(
