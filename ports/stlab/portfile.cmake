0b14291479231a12051fbfdd63e2763939d87b45
ian <ian>
2021-04-12 16:21:44 +0100

    fix comment in typo

--- ports/stlab/portfile.cmake
+++ ports/stlab/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stlab/libraries
-    REF 0a7232a4120c2daf8ddb6621ec13f313a029e495 # V1.5.2
+    REF 0a7232a4120c2daf8ddb6621ec13f313a029e495 # V1.6.2
     SHA512 6e03a5370d02accd798fc14fd256ab593b9a33b4a9b9cda8f2233eeafacf70c389c2999d1834b7ffef6968008921d28d88bcf728a322ba7943106ddc9d8e6f16
     HEAD_REF develop
 )
