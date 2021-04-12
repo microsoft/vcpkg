bc96cd54bf3afe85e4c1f201d6475f5ce0a4763c
Dawid Wróbel <me@dawidwrobel.com>
2021-04-12 13:15:11 -0400

    [many ports] Update existing KF5 frameworks to 5.75 (#13467)
    * Update KF5 framework to 5.73
    
    kf5crash
      - make x11 patch exclusive to linux
      - enable win/macOS builds
    kf5syntaxhighlighting
      - fix building on non-Win by removing the cli tool from all platforms
    
    * Add kf5auth port
    
    * Revert "Add kf5auth port"
    
    This reverts commit 8703c2423667f62075c34496ef273394a5b8d538.
    
    * Add kf5globalaccel port
    
    * Revert "Add kf5globalaccel port"
    
    This reverts commit f1ac3db8e2019f474a09d6964aea8e21bc9da166.
    
    * [ecm] use  where possible
    
    * [kf5holidays] use PORT placeholder where possible
    
    * [kf5syntaxhighlighting] use PORT placeholder where possible
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5plotting] use PORT placeholder where possible
    
    * [kf5crash] fix logical condition in the patch
    
    * [kf5crash] logical condition again, include other BSDs
    
    * [kf5i18n] acquire python3
    
    * [kf5i18n] remove vcpk cmake wrapper added mistakenly
    
    * Update KF5 framework to 5.73
    
    kf5crash
      - make x11 patch exclusive to linux
      - enable win/macOS builds
    kf5syntaxhighlighting
      - fix building on non-Win by removing the cli tool from all platforms
    
    * Add kf5auth port
    
    * Revert "Add kf5auth port"
    
    This reverts commit 8703c2423667f62075c34496ef273394a5b8d538.
    
    * Add kf5globalaccel port
    
    * Revert "Add kf5globalaccel port"
    
    This reverts commit f1ac3db8e2019f474a09d6964aea8e21bc9da166.
    
    * [ecm] use  where possible
    
    * [kf5holidays] use PORT placeholder where possible
    
    * [kf5syntaxhighlighting] use PORT placeholder where possible
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5plotting] use PORT placeholder where possible
    
    * [kf5crash] fix logical condition in the patch
    
    * [kf5crash] logical condition again, include other BSDs
    
    * [kf5i18n] acquire python3
    
    * [kf5i18n] remove vcpk cmake wrapper added mistakenly
    
    * [ecm] Remove 'skip' from windows baseline
    
    * Fixed KF5ItemModel for windows
    
    * [kf5syntaxhighlighting] fix Windows build
    
    * Fixed k5syntaxhighlighting for windows
    
    * [kf5plotting] fix license files
    
    * [kf5archive] fix license files
    
    * [kf5*] update to 5.75.0
    
    * [kf5holidays] fix sha sum
    
    * [kf5syntaxhighlighting] fix licensing
    
    * [kf5itemviews] fix windows static build
    
    * [kf5widgetsaddons] fix windows static build
    
    * [kf5completion] fix windows static build
    
    * [kf5*] fix windows static build
    
    * [kf5i18n] fix windows static build
    
    * [kf5windowsystem] require libxcb-res0-dev
    
    * [kf5windowsystem] depend on qt5-x11extras
    
    * [qt5] fix brotli reference
    
    * Delete duplicate apt entries.
    
    * kf5crash: fix linux build
    
    * kf5i18n: fix Linux build
    
    * ecm: do not override custom clang format files
    
    * kf5i18n: fix Windows build
    
    * [kf5*] update versions
    
    * [kf5i18n] fix logical NOT
    
    * [kf5syntaxhighlighting] disable parallel configure
    
    * [kf5*] update versions
    
    * [kf5*] convert CONTROL to manifest
    
    * [kf5*] update versions
    
    * [kf5*] update versioning scheme
    
    * [ecm] syntax formatting
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5syntaxhighlighting] syntax formatting
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5holidays] syntax formatting
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5archive] restore original line order
    
    * [kf5holidays] restore CRLF
    
    * [kf5archive] restore CRLF
    
    * [kf5syntaxhighlighting] syntax formatting
    
    * [kf5syntaxhighlighting] quotations
    
    * [kf5syntaxhighlighting] fix \bin removal redundancy
    
    * [kf5*] update versions
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>
    Co-authored-by: Kuntal Majumder <hellozee@disroot.org>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/ecm/portfile.cmake
+++ ports/ecm/portfile.cmake
@@ -1,9 +1,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/extra-cmake-modules
-    REF 5c191fa1d2383c69ddc075395e04f84b7679613a # v5.74.0
-    SHA512 9a520e2cb45f2891cb5b463409063549904574e2ad0c4f77987555fc9aac68589695344a3acaddcb95d2739458031ece489ff9635c7d713c6309e4f9027f6ef7
+    REF v5.75.0
+    SHA512 486845a91967c89dec42f9120757c7b883006713d0957e9bd21dfacb2911225fe45680590ef3dc5e0d45a9dbc81cf99e1514fb4922b1a027bcba3864da883076
     HEAD_REF master
+    PATCHES
+        "do-not-override-custom-clang-format-files.patch"
 )
 
 vcpkg_configure_cmake(
@@ -21,7 +23,7 @@ vcpkg_install_cmake()
 # Remove debug files
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
-file(COPY ${CURRENT_PORT_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/ecm)
+file(COPY ${CURRENT_PORT_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/COPYING-CMAKE-SCRIPTS DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
--- ports/kf5widgetsaddons/portfile.cmake
+++ ports/kf5widgetsaddons/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/kwidgetsaddons
-    REF v5.64.0
-    SHA512 f49bade85b97468ad4c2faa34b71c6d04ff95c720c27e5b7adfb9a7b5acf8ff30b91d2beca7472dec6b8241f918b4fb15bddc1429be86a62bcefefe918311307
+    REF v5.75.0
+    SHA512 8854ddc56d8a30081d69d952d035ef303b3a30760fdee3ba8889029b373cb793807f984ce3fc06bd1d377fa3d5a768d3dbf99cb44a3b29a7589b2c4f77767360
     HEAD_REF master
 )
 
@@ -21,10 +21,15 @@ vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5WidgetsAddons)
 vcpkg_copy_pdbs()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/data)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/data)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
-file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
