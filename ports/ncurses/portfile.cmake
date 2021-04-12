72aacfa74dc6e09516beaaf93ce9ffa8753f7c1b
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2021-04-12 09:57:36 +0800

    Update ports/ncurses/portfile.cmake

--- ports/ncurses/portfile.cmake
+++ ports/ncurses/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mirror/ncurses
     REF  47d2fb4537d9ad5bb14f4810561a327930ca4280 # v6.2
-    SHA512 776558fdd911f0cc9e8d467bf8e00a1930d2e51bb8ccd5f36f95955fefecab65faf575a80fdaacfe83fd32808f8b9c2e0323b16823e0431300df7bc0c1dfde12
+    SHA512 e468904d3a10f7edfc060becbe261f46d9dd5f51eef3821dfeb3459e123a6af2dcb6c7d1399e914b29866ca8456c1e95d4ca9f61ca82ab9a3a2e2bc879cbcdca 
     HEAD_REF master
 )
 
