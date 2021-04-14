473fc2c226084f3b6fdea4a54d90c4a059a8285d
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2021-04-14 12:23:08 +0800

    Update ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake

--- ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake
+++ ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.cmake
@@ -230,15 +230,22 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)]]
         foreach(targets_file IN LISTS targets_files)
             file(READ "${targets_file}" targets_content)
             
+            string(REGEX MATCHALL "INTERFACE_LINK_LIBRARIES[^\n]*" library_contents "${targets_content}")
             string(REGEX REPLACE
                 [[/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/[^/]+/System/Library/Frameworks/([_a-zA-Z]+)\.framework]]
                 [[-framework \1]]
-                targets_content
-                "${targets_content}"
+                fixed_contents
+                "${library_contents}"
             )
             string(REGEX REPLACE
                 [[/Library/Developer/CommandLineTools/SDKs/[^/]+/System/Library/Frameworks/([_a-zA-Z]+)\.framework]]
                 [[-framework \1]]
+                fixed_contents
+                "${fixed_contents}"
+            )
+            string(REGEX REPLACE
+               "${library_contents}"
+               "${fixed_contents}"
                 targets_content
                 "${targets_content}"
             )
@@ -258,4 +265,3 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)]]
         file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
     endif()
 endfunction()
-
