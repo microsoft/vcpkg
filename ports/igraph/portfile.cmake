5dacdfb0c5c7e0b3e22b0a32242ab3c97b106072
Szabolcs Horvát <szhorvat@gmail.com>
2021-04-15 21:12:31 +0200

    Update ports/igraph/portfile.cmake
    Co-authored-by: Adam Johnson <AdamJohnso@gmail.com>

--- ports/igraph/portfile.cmake
+++ ports/igraph/portfile.cmake
@@ -42,5 +42,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/igraph TARGET_PATH share/igraph)
 
-# # Handle copyright
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/igraph RENAME copyright)
