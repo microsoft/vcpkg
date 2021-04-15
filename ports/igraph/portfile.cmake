448b72efb024819c797fa00813c5c4e4dd57f61a
Szabolcs Horvát <szhorvat@gmail.com>
2021-04-15 18:14:00 +0200

    [igraph] create new port

---
+++ ports/igraph/portfile.cmake
@@ -0,0 +1,52 @@
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/igraph/igraph/releases/download/0.9.2/igraph-0.9.2.tar.gz"
+    FILENAME "igraph-0.9.2.tar.gz"
+    SHA512 8feb0c23c28e62f1e538fc41917e941f45421060b6240653ee03153b13551c454be019343a314b7913edb9c908518a131034c8e2098d9dd8e5c923fb84d195b3
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    # (Optional) A friendly name to use instead of the filename of the archive (e.g.: a version number or tag).
+    # REF 1.0.0
+    # (Optional) Read the docs for how to generate patches at:
+    # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
+    # PATCHES
+    #   001_port_fixes.patch
+    #   002_more_port_fixes.patch
+)
+
+# # Check if one or more features are a part of a package installation.
+# # See /docs/maintainers/vcpkg_check_features.md for more details
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    graphml   IGRAPH_GRAPHML_SUPPORT
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DIGRAPH_ENABLE_LTO=AUTO
+        -DIGRAPH_USE_INTERNAL_ARPACK=ON
+        -DIGRAPH_USE_INTERNAL_BLAS=ON
+        -DIGRAPH_USE_INTERNAL_CXSPARSE=ON
+        -DIGRAPH_USE_INTERNAL_GLPK=ON
+        -DIGRAPH_USE_INTERNAL_GMP=ON
+        -DIGRAPH_USE_INTERNAL_LAPACK=ON
+        ${FEATURE_OPTIONS}
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# # Moves all .cmake files from /debug/share/igraph/ to /share/igraph/
+# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/igraph TARGET_PATH share/igraph)
+
+# # Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/igraph RENAME copyright)
