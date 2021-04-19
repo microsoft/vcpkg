8477e310724bce745ba89b2180601f312b9a4119
Dmitry Baryshev <dmitrymq@gmail.com>
2021-04-19 17:32:16 +0300

    [aom] Fixes after review

--- ports/aom/portfile.cmake
+++ ports/aom/portfile.cmake
@@ -15,13 +15,12 @@ vcpkg_from_git(
     URL "https://aomedia.googlesource.com/aom"
     REF 307ce06ed82d93885ee8ed53e152c9268ac0d98d
     PATCHES
-        aom-rename-static.diff
+        aom-rename-static.diff # to prevent ninja failures on duplicate target
         aom-install.diff
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DENABLE_DOCS=OFF
         -DENABLE_EXAMPLES=OFF
@@ -30,15 +29,16 @@ vcpkg_configure_cmake(
         -DENABLE_TOOLS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
 # Move cmake configs
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/aom)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 # Remove duplicate files
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
+                    ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle usage
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
