559bc829014e1d3b65c0b67e06981a0f4758634c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-04-19 10:06:24 +1000

    [tinyfiledialogs] Use git rather than explicit snapshot

--- ports/tinyfiledialogs/portfile.cmake
+++ ports/tinyfiledialogs/portfile.cmake
@@ -2,25 +2,29 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_download_distfile(
-    OUT_SOURCE_PATH tinyfiledialogs.zip
-    URLS "https://sourceforge.net/code-snapshots/git/t/ti/tinyfiledialogs/code.git/tinyfiledialogs-code-ab6f4f916aaa95d05247ffa66a30867e7f55e875.zip"
-    FILENAME "tinyfiledialogs.zip"
-    SHA512 0b4bd0f9388a5b11c75b8d4128ced8855281a2a27778035556de5a1812015da1
+set(TINYFILEDIALOGS_VERSION "3.8.7")
+set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/tinyfiledialogs-${TINYFILEDIALOGS_VERSION}")
+
+vcpkg_from_git(
+    OUT_SOURCE_PATH "${SOURCE_PATH}"
+    URL "https://git.code.sf.net/p/tinyfiledialogs/code"
+    REF "ab6f4f916aaa95d05247ffa66a30867e7f55e875"
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${${SOURCE_PATH}}/tinyfiledialogs.h" DESTINATION "${SOURCE_PATH}")
+file(COPY "${${SOURCE_PATH}}/tinyfiledialogs.c" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
 )
 
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(READ ${CURRENT_PACKAGES_DIR}/include/tinyfiledialogs/tinyfiledialogs.h _contents)
+file(READ "${CURRENT_PACKAGES_DIR}/include/tinyfiledialogs/tinyfiledialogs.h" _contents)
 string(SUBSTRING "${_contents}" 0 1024 _contents)
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright "${_contents}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "${_contents}")
