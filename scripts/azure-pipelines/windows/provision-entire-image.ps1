a3fe1a5d0d5ebf0ced0e3eb57d36c8c57c34b982
Billy Robert O'Neal III <bion@microsoft.com>
2021-04-19 19:06:40 -0700

    Add provision-entire-image script.

---
+++ scripts/azure-pipelines/windows/provision-entire-image.ps1
@@ -0,0 +1,16 @@
+# This script runs all the scripts we run on Azure machines to deploy prerequisites,
+# and assumes it is being run as an admin user.
+
+. "$PSScriptRoot\utility-prefix.ps1"
+
+. "$PSScriptRoot\deploy-visual-studio.ps1"
+. "$PSScriptRoot\deploy-windows-wdk.ps1"
+. "$PSScriptRoot\deploy-mpi.ps1"
+. "$PSScriptRoot\deploy-cuda.ps1"
+. "$PSScriptRoot\deploy-pwsh.ps1"
+try {
+    Copy-Item "$PSScriptRoot\deploy-settings.txt" "$PSScriptRoot\deploy-settings.ps1"
+    . "$PSScriptRoot\deploy-settings.ps1"
+} finally {
+    Remove-Item "$PSScriptRoot\deploy-settings.ps1"
+}
