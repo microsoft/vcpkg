4fd36e17f00ad297de3ae58cfa24261009de11ab
jacobkahn <jacobkahn1@gmail.com>
2021-04-21 15:37:30 -0700

    Update origin

--- ports/flashlight-cpu/portfile.cmake
+++ ports/flashlight-cpu/portfile.cmake
@@ -6,7 +6,7 @@ endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO facebookresearch/flashlight
+    REPO flashlight/flashlight
     REF 76f7fa7f5a162c73d6bf8befdb8e197a4dc7515d # 0.3 branch tip
     SHA512 87786f9443d27ac9b513cf582caea13dccfa344e55a4970c0c2c7df7530260ad38cc578690ebf2fa256e8ea943abea547e0e6d5ee0ba090b336c4f7af8d2f53f
     HEAD_REF master
