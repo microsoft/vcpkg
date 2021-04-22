0b2a7b46a44b488d6da284b6363a1b47879553f3
NancyLi1013 <lirui09@beyondsoft.com>
2021-04-21 23:37:34 -0700

    Update to another repository

--- ports/soil/portfile.cmake
+++ ports/soil/portfile.cmake
@@ -2,14 +2,14 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO littlstar/soil
-    REF 475f3d83433582618aae8a3c89eb978b01e263a3 # 0.1.0
-    SHA512 89da87da9c9f21ba7ccfbff6f6c46f2463f672d2ce13a37a9b161eeef471f5a31fa751c80dba9378c77d878148ca77012dec861461d3a7b6966a34951157ae2f
+    REPO paralin/soil
+    REF 8bb18a909f94e58afbc0bda941ffc6eee58b4066 # committed on 2014-03-06
+    SHA512 6cbaa10d8b2a274c389fda723db53a3f1ba7d25a7367df40efec4d0553c09f0d67fb16f927bba2ff0aed4234e3a83922edcc574ffac72dd7e05d6cec768b561b
     HEAD_REF master
+    PATCHES fix-cmakelists.patch
 )
 
 file(COPY
-    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt
     ${CMAKE_CURRENT_LIST_DIR}/SOILConfig.cmake.in
     ${CMAKE_CURRENT_LIST_DIR}/SOILConfigVersion.cmake.in
     DESTINATION ${SOURCE_PATH}
@@ -18,9 +18,11 @@ file(COPY
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS_DEBUG -DINSTALL_HEADERS=OFF
 )
 
 vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
