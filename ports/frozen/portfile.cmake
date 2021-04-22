215444b6926773f80b53fbd4cc66e444ea3be3dc
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-04-22 15:59:12 -0700

    cleanup deprecated functions

--- ports/frozen/portfile.cmake
+++ ports/frozen/portfile.cmake
@@ -6,18 +6,17 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
       -Dfrozen.benchmark=OFF
       -Dfrozen.coverage=OFF
       -Dfrozen.installation=ON
       -Dfrozen.tests=ON
 )
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/frozen)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/frozen)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
