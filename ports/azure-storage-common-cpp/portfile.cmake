02b65a73ac1490ac514668dd263d54fa9d7b2857
JackBoosY <yuzaiyang@beyondsoft.com>
2021-04-23 05:47:12 -0700

    [azure-storage-common-cpp] Fix dependency libxml2

--- ports/azure-storage-common-cpp/portfile.cmake
+++ ports/azure-storage-common-cpp/portfile.cmake
@@ -3,6 +3,7 @@ vcpkg_from_github(
     REPO Azure/azure-sdk-for-cpp
     REF azure-storage-common_12.0.0-beta.10
     SHA512 f49f74c9a6d7b80b0f7c8a9b9d2e4595dc304439d30c8c83f520833f81ef7d3480b858ada61c12f76e82eea7960eb155453b0acdcea143f163a12d567adbf18d
+    PATCHES fix-dependency-libxml2.patch
 )
 
 vcpkg_configure_cmake(
