e0fddfde167b767e9891b1d1ee5e3b89e8390506
Robert Schumacher <roschuma@microsoft.com>
2021-04-23 15:36:01 -0700

    [tinyexr] Disable building sample

--- ports/tinyexr/portfile.cmake
+++ ports/tinyexr/portfile.cmake
@@ -13,6 +13,8 @@ vcpkg_from_github(
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        -DTINYEXR_BUILD_SAMPLE=OFF
 )
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
