03d46a92691008b020667ddcc4d58d5273a3f449
Jonliu1993 <13720414433@163.com>
2021-04-26 15:24:44 +0800

    [oatpp] update to 1.2.5

--- ports/oatpp/portfile.cmake
+++ ports/oatpp/portfile.cmake
@@ -1,13 +1,15 @@
-set(OATPP_VERSION "1.2.0")
+set(OATPP_VERSION "1.2.5")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO oatpp/oatpp
-    REF 8f1c11ae534e1f03646de8efcc9b2505fb1881cc  # 1.2.0
-    SHA512 4661aefe89be8153b08be0eb95ce688d5af3abd80e8f92fe3b2f1ac9dc76228383b05f7b33314de5f25d433013d6d287650ed156b69244b0d9ba8b604df8aaa3
+    REF 9899786b235b2d987c8b131cdb01909727b98d4f  # 1.2.5
+    SHA512 b5d1d6e2e2927f929aa3c7ac7fbc8afd491c28391d54d77647700982217ed17a00b96886cd314058003e103d3d7992f30fd0ab26f6cfce4ed26e12e0b6c9fd4c
     HEAD_REF master
+    PATCHES
+        fix-identifier-not-found.patch
 )
 
 vcpkg_configure_cmake(
