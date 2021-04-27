3cc5dbf6531b79478d8cb19b77ed5c2e809121cc
Long Huan <8551701+longhuan2018@users.noreply.github.com>
2021-04-27 11:12:22 +0800

    Update ports/libspatialindex/portfile.cmake
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/libspatialindex/portfile.cmake
+++ ports/libspatialindex/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libspatialindex/libspatialindex
     REF 1.9.3
-    SHA512   d4c608abbd631dc163b7b4fb6bf09dee3e85ce692a5f8875d51f05a26e09c75cd17dff1ed9d2c232a071f0f5864d21d877b4cbc252f3416896db24dfa3fa18cb
+    SHA512 d4c608abbd631dc163b7b4fb6bf09dee3e85ce692a5f8875d51f05a26e09c75cd17dff1ed9d2c232a071f0f5864d21d877b4cbc252f3416896db24dfa3fa18cb
     HEAD_REF master
     PATCHES
         static.patch
