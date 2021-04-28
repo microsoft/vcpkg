98aa353e6096346bc6db0c8ce7053a6d55503150
Mathis Logemann <mathisloge@gmail.com>
2021-04-28 21:53:49 +0200

    update fplus and fix cmake target. new port frugally-deep

---
+++ ports/frugally-deep/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Dobiasd/frugally-deep
+    REF v0.15.2-p0
+    SHA512 2237c139c217cc9e338c854505009e85cea6658888e0d97c0c7957b58e0e53e2add555b81fa276c2ec9f794d5356bdb267c1e0b05090c83627916d954d2a11ba
+    HEAD_REF master
+)
+
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+    -DFDEEP_BUILD_UNITTEST=OFF
+    -DFDEEP_USE_TOOLCHAIN=ON
+)
+# feature FDEEP_USE_DOUBLE
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/frugally-deep TARGET_PATH share/${PORT})
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
