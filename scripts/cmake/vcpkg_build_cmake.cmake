4c7e5b4f9f0f0737a04027a70101a1e304de9e3b
Kai Pastor <dg0yt@darc.de>
2021-04-28 21:52:37 +0200

    [doc] Fix spelling errors in scripts

--- scripts/cmake/vcpkg_build_cmake.cmake
+++ scripts/cmake/vcpkg_build_cmake.cmake
@@ -22,7 +22,7 @@ be passed.
 Adds the appropriate Release and Debug `bin\` directories to the path during the build such that executables can run against the in-tree DLLs.
 
 ## Notes:
-This command should be preceeded by a call to [`vcpkg_configure_cmake()`](vcpkg_configure_cmake.md).
+This command should be preceded by a call to [`vcpkg_configure_cmake()`](vcpkg_configure_cmake.md).
 You can use the alias [`vcpkg_install_cmake()`](vcpkg_configure_cmake.md) function if your CMake script supports the
 "install" target
 
--- scripts/cmake/vcpkg_build_nmake.cmake
+++ scripts/cmake/vcpkg_build_nmake.cmake
@@ -64,7 +64,7 @@ be passed.
 Adds the appropriate Release and Debug `bin\` directories to the path during the build such that executables can run against the in-tree DLLs.
 
 ## Notes:
-This command should be preceeded by a call to [`vcpkg_configure_nmake()`](vcpkg_configure_nmake.md).
+This command should be preceded by a call to [`vcpkg_configure_nmake()`](vcpkg_configure_nmake.md).
 You can use the alias [`vcpkg_install_nmake()`](vcpkg_install_nmake.md) function if your makefile supports the
 "install" target
 
