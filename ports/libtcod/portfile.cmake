ffc4cf4a92f5d5ebfd50d305c7cd73a8d79b3c42
nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>
2021-04-30 11:31:58 -0700

    Apply suggestions from code review

--- ports/libtcod/portfile.cmake
+++ ports/libtcod/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libtcod/libtcod
-    REF 1.16.7-1-g2311b47
+    REF 2311b47028f0879c1d3fc44e11d8352052b8d8b0
     SHA512  7b3ab6daf6847d2d0993a1aa73d6d4b9b56684ac5f44cdf7d4ae551600c5354c72afdfa40abd837dda66b3bb1e4c0d50c8c9a7bf5a7e4915cbdc703211c7c917
     HEAD_REF develop
 )
