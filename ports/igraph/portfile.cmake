f60469f43703159d07821818160d5e125b92fb56
Szabolcs Horvát <szhorvat@gmail.com>
2021-05-05 16:10:27 +0200

    igraph: update to 0.9.3

--- ports/igraph/portfile.cmake
+++ ports/igraph/portfile.cmake
@@ -4,20 +4,20 @@
 #  - The release tarball contains pre-generated parser sources, which eliminates the dependency on bison/flex.
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/igraph/igraph/releases/download/0.9.2/igraph-0.9.2.tar.gz"
-    FILENAME "igraph-0.9.2.tar.gz"
-    SHA512 8feb0c23c28e62f1e538fc41917e941f45421060b6240653ee03153b13551c454be019343a314b7913edb9c908518a131034c8e2098d9dd8e5c923fb84d195b3
+    URLS "https://github.com/igraph/igraph/releases/download/0.9.3/igraph-0.9.3.tar.gz"
+    FILENAME "igraph-0.9.3.tar.gz"
+    SHA512 69c7a217a27a34763750fe1bb3a981d9ce5aceb9027fe98ebdd2b2cbf4558734f55128ff8d9e46d066020d5bc942f6df761861bf7b103025015d3f0e336a7e7c
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    PATCHES 001_add_crt_secure_no_warnings.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
     graphml   IGRAPH_GRAPHML_SUPPORT
+    openmp    IGRAPH_OPENMP_SUPPORT
 )
 
 # Allow cross-compilation. See https://igraph.org/c/doc/igraph-Installation.html#idm207877354096
