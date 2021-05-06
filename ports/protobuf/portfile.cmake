microsoft/vcpkg

commit 6d9ed568117dd958c543b3ab8d3ed692965cac34

[vcpkg baseline][osgearth/gdal] Re-fix dependency gdal (#16904)
* [osgearth] Re-fix dependency gdal

* [gdal] Add dependency freexl on UNIX

* update version record

* fix patch

* convert to manifest

* update version record

* re-fix

* update version record

* add dependency freexl

* update version record

* Disable freexl

Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

* Don't require freexl on Windows

Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

* Revert osgearth change

* Fix the baseline version

* Fix osgearth baseline version

* [protouf] Automatic set protobuf_MODULE_COMPATIBLE to include the module file

* Set protobuf_MODULE_COMPATIBLE in wrapper

* Re-fix protobuf

* update version record

Co-authored-by: Victor Romero <romerosanchezv@gmail.com>
Co-authored-by: Robert Schumacher <roschuma@microsoft.com>
Co-authored-by: PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>

ports/protobuf/portfile.cmake
@@ -83,6 +83,11 @@ else()
     file(COPY ${CURRENT_HOST_INSTALLED_DIR}/tools/${PORT} DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
 endif()
 
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-config.cmake
+    "if(protobuf_MODULE_COMPATIBLE)"
+    "if(ON)"
+)
+
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/bin)
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/debug/bin)

ports/gdal/portfile.cmake
@@ -185,7 +185,7 @@ else()
     endif()
     
     set(CONF_OPTS --enable-shared=${BUILD_DYNAMIC} --enable-static=${BUILD_STATIC})
-    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR} --with-libjson-c=${CURRENT_INSTALLED_DIR})
+    list(APPEND CONF_OPTS --with-proj=${CURRENT_INSTALLED_DIR} --with-libjson-c=${CURRENT_INSTALLED_DIR} --without-freexl)
     list(APPEND CONF_OPTS --without-jasper)
     
     vcpkg_configure_make(

ports/gdal/CONTROL
@@ -1,15 +1,0 @@
-Source: gdal
-Version: 3.1.3
-Port-Version: 2
-Homepage: https://gdal.org/
-Description: The Geographic Data Abstraction Library for reading and writing geospatial raster and vector data.
-Build-Depends: proj4, libpng, geos, sqlite3, curl, expat, libpq, openjpeg, libwebp, libxml2, liblzma, netcdf-c, hdf5, zlib, libgeotiff, cfitsio, json-c (!windows)
-Supports: !arm
-
-Feature: mysql-libmariadb
-Build-Depends: libmariadb
-Description: Add mysql support using libmariadb
-
-Feature: libspatialite
-Build-Depends: libspatialite
-Description: Create or update SpatiaLite databases using libspatialite

commit 3b245c593b5e794d60a8a3d93637f5de368c8db3

[protobuf] Fix regression introduced in #16904

ports/protobuf/portfile.cmake
@@ -88,6 +88,12 @@ vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-config.cmake
     "if(protobuf_MODULE_COMPATIBLE)"
     "if(ON)"
 )
+if(NOT protobuf_BUILD_LIBPROTOC)
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/${PORT}/protobuf-module.cmake
+        "_protobuf_find_libraries(Protobuf_PROTOC protoc)"
+        ""
+    )
+endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     protobuf_try_remove_recurse_wait(${CURRENT_PACKAGES_DIR}/bin)