8166ef0412c79e4a9da0b19d0458a683cac0b2b7
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-05-10 00:08:47 +0200

    fix debug windeployqt install but somebody is linking release icudt in debug

--- ports/qttools/windeployqt.debug.bat
+++ ports/qttools/windeployqt.debug.bat
@@ -1,1 +1,5 @@
-"%0\..\windeployqt.exe" --qmake "%0\..\qmake.debug.bat" %*
+set mypath=%~dp0
+set mypath=%mypath:~0,-1%
+cd %mypath%\..\..\..\debug\bin
+set PATH=%CD%;%PATH%
+"%mypath%\windeployqt.exe" --qmake "%mypath%\qmake.debug.bat" %*
