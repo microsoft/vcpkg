cff973e2c4037f866e202200b1983224790996aa
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-05-10 23:18:04 +0200

    some batch improvements

--- ports/qttools/windeployqt.debug.bat
+++ ports/qttools/windeployqt.debug.bat
@@ -1,5 +1,9 @@
+setlocal
 set mypath=%~dp0
 set mypath=%mypath:~0,-1%
 cd %mypath%\..\..\..\debug\bin
+set BAKCD=%CD%
 set PATH=%CD%;%PATH%
 "%mypath%\windeployqt.exe" --qmake "%mypath%\qmake.debug.bat" %*
+cd %BAKCD%
+endlocal
