828b64e654b3ca151317c2ef0d9692036a1950b2
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-05-11 14:15:15 +0200

    fix qtactiveqt

--- ports/qtactiveqt/portfile.cmake
+++ ports/qtactiveqt/portfile.cmake
@@ -3,12 +3,19 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
 set(${PORT}_PATCHES)
 
-set(TOOL_NAMES 
-        dumpcpp
-        dumpdoc
-        idc
-        testcon
-   )
+set(NO_BIN_AND_TOOLS FALSE)
+if(VCPKG_TARGET_ARCHITECTURE MATCHES "^arm" AND VCPKG_TARGET_IS_WINDOWS)
+    set(NO_BIN_AND_TOOLS TRUE)
+endif()
+
+if(NOT NO_BIN_AND_TOOLS)
+    set(TOOL_NAMES 
+            dumpcpp
+            dumpdoc
+            idc
+            testcon
+       )
+endif()
 
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
@@ -17,3 +24,7 @@ qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS_DEBUG
                     )
                     
+
+if(NO_BIN_AND_TOOLS)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/tools")
+endif()
