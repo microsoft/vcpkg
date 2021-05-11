0056e5fd7d71fdfcfd7ee962534e30ee1ff2ceb6
duzhongwei <duzhongwei@querycap.com>
2021-05-11 11:56:43 +0800

    add the port of bsio library

---
+++ ports/bsio/portfile.cmake
@@ -0,0 +1,12 @@
+#header-only library
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO IronsDu/bsio
+    REF v1.0.0
+    SHA512 28b895b823d84203f4cec531ddd5bb49dc915e9a4eb26e064834d1e999b98e512b37d361e59029eb6d7e44fe99ba81f9c5729f119eab7eb928de1a1374f0b7df
+    HEAD_REF master
+)
+
+file(INSTALL ${SOURCE_PATH}/include/bsio DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
