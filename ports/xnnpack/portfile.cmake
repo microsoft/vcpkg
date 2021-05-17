1b97130626d45b987aa8695377c0a9f1a994e5bd
luncliff <luncliff@gmail.com>
2021-05-17 14:50:05 +0900

    [xnnpack] update code version
    * snapshot: 21/04/10 -> 21/05/17

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -5,8 +5,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/XNNPACK
-    REF 2e42787663028a83c30dfaaa1c6f1d1a9f08d80a
-    SHA512 368046fb8d9386fa60bca97387857cfd7cda4cd07bfbd7f394e927df0b35fc1027981152da8827f282505279d50c6e421a430b59f6357621bc4e140452eca51a
+    REF 5223fd1922d9d965ddb07865d1d13293069fab65 # 2021-05-17
+    SHA512 5c1eb9171a046c683d5a96ae70b6dab36c415c981e8f29857d16ad67afa22681f0fe6f7623527200fd954403d50b036bb2892ba44e397d639860b9ec4873e7c6
     HEAD_REF master
     PATCHES
         use-packages.patch
@@ -29,4 +29,5 @@ vcpkg_cmake_config_fixup()
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/debug/bin
+                    ${CURRENT_PACKAGES_DIR}/debug/share
 )
