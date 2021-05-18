f19402d9568e43b50b284c68b8086a425dd62ff0
Yury Bura <yurybura@gmail.com>
2021-05-18 16:14:47 +0300

    [llvm] fix libxml2 (depends on #17945)

--- ports/llvm/portfile.cmake
+++ ports/llvm/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
         0008-fix-libcxx-install.patch
         0009-fix-tools-install-path.patch
         0010-fix-libffi.patch
+        0011-fix-libxml2.patch
 )
 
 vcpkg_check_features(
