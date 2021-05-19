36ea493f5b8f8d040c304c777fe7c60b77f82b40
azure-sdk <azuresdk@microsoft.com>
2021-05-19 06:51:16 +0000

    [azure-identity-cpp] Update to 1.0.0-beta.6 ## 1.0.0-beta.6 (2021-05-18)
    ### Breaking Changes
    
    - Added `final` specifier to classes and structures that are are not expected to be inheritable at the moment.

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-identity_1.0.0-beta.5
-    SHA512 9ff56d719d77c7b0db3054788dc69aee18105e27d0732d79b1eb7b86fd8e568dd52631aaf329c4b0f4c65699c2d8bda0a050586bcd8052cb1e74cb46f3f2c85a
+    REF azure-identity_1.0.0-beta.6
+    SHA512 0ed151a322049dfd2df8da43539ad5a81478866edb421c394e6937d886039d7d24f0b483cfd5e50655b590eb75ed4c5c42ed5a1760c51ea2eda81f7bf3b3114e
 )
 
 vcpkg_cmake_configure(
