a1b00fbcb6278ed0b0c482c2fb6d31fb11680168
Park DongHa <luncliff@gmail.com>
2021-05-24 00:08:19 +0900

    [onnx] fix windows build
    * rewrite cmake patch
    * provide CRT related options

--- ports/onnx/portfile.cmake
+++ ports/onnx/portfile.cmake
@@ -1,24 +1,31 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO onnx/onnx
     REF v1.9.0
     SHA512 a3eecc74ce4f22524603fb86367d21c87a143ba27eef93ef4bd2e2868c2cadeb724b84df58a429286e7824adebdeba7fa059095b7ab29df8dcea8777bd7f4101
     PATCHES
-        fix-apple.patch
+        fix-cmakelists.patch
         fix-pybind11.patch
 )
-
-vcpkg_find_acquire_program(PYTHON3)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         pybind11 BUILD_ONNX_PYTHON
 )
-if(NOT VCPKG_TARGET_IS_LINUX AND "pybind11" IN_LIST FEATURES)
-    message(FATAL_ERROR "Feature 'pybind11' is for Linux platform")
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" USE_PROTOBUF_SHARED)
+list(APPEND FEATURE_OPTIONS 
+    -DONNX_USE_PROTOBUF_SHARED_LIBS=${USE_PROTOBUF_SHARED}
+)
+if(VCPKG_TARGET_IS_WINDOWS)
+    string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
+    list(APPEND FEATURE_OPTIONS 
+        -DONNX_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
+    )
 endif()
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_find_acquire_program(PYTHON3)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
@@ -29,7 +36,6 @@ vcpkg_cmake_configure(
         -DONNX_USE_LITE_PROTO=OFF
         -DONNX_BUILD_TESTS=OFF
         -DONNX_BUILD_BENCHMARKS=OFF
-        -DONNX_VERIFY_PROTO3=ON
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
@@ -49,11 +55,6 @@ file(INSTALL ${CODEGEN_DIR}/onnx-data.proto
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/debug/share
-                    # these are for python scripts
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_ml
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_data
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_operators_ml
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_cpp2py_export
                     # the others are empty
                     ${CURRENT_PACKAGES_DIR}/include/onnx/backend
                     ${CURRENT_PACKAGES_DIR}/include/onnx/tools
@@ -74,3 +75,11 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/include/onnx/defs/rnn
                     ${CURRENT_PACKAGES_DIR}/include/onnx/defs/nn
 )
+if(NOT "pybind11" IN_LIST FEATURES)
+    # these are for python scripts
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_ml
+                        ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_data
+                        ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_operators_ml
+                        ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_cpp2py_export
+    )
+endif()
