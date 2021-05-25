a6fd0955babf160d907cb0f6bc034ee22362bdaa
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-05-26 00:38:32 +0200

    fix wrong suggestion

--- ports/boost-json/portfile.cmake
+++ ports/boost-json/portfile.cmake
@@ -10,7 +10,7 @@ vcpkg_from_github(
 
 # see https://github.com/boostorg/json/issues/556 fore more details
 vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "import ../../config/checks/config" "import config/checks/config")
-vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "\n      <library>/boost//container/<warnings-as-errors>off")
+vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "\n      <library>/boost//container/<warnings-as-errors>off" "")
 
 vcpkg_replace_string("${SOURCE_PATH}/Jamfile" "import ../config/checks/config" "import build/config/checks/config")
 
--- scripts/boost/post-source-stubs/json.cmake
+++ scripts/boost/post-source-stubs/json.cmake
@@ -1,6 +1,6 @@
 # see https://github.com/boostorg/json/issues/556 fore more details
 vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "import ../../config/checks/config" "import config/checks/config")
-vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "\n      <library>/boost//container/<warnings-as-errors>off")
+vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "\n      <library>/boost//container/<warnings-as-errors>off" "")
 
 vcpkg_replace_string("${SOURCE_PATH}/Jamfile" "import ../config/checks/config" "import build/config/checks/config")
 
