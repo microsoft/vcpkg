a06d58a291f7e5b0e44cc51a4e5111015a8a8fff
JackBoosY <yuzaiyang@beyondsoft.com>
2021-05-27 22:56:35 -0700

    install

--- ports/vcpkg-cmake-test/portfile.cmake
+++ ports/vcpkg-cmake-test/portfile.cmake
@@ -1,5 +1,6 @@
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_test_cmake_config.cmake"
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/copyright"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
