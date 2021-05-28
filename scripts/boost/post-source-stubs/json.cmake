47e4c4390dcffabd5d20a1b5199abc546453520b
autoantwort <41973254+autoantwort@users.noreply.github.com>
2021-05-28 20:20:52 +0200

    [Boost] fix not header only libs (#17372)
    * [boost] don't handle compiled libs as header only ones (json/python)
    
    * add version files
    
    * [boost script] wait for lock to not get failure messages
    
    * [boost-json] fix build
    
    * add version files
    
    * remove unnecessary changes
    
    * Trigger Build because of #17612
    
    * update boost asio and concept-check as requested by ci
    
    * add version files
    
    * Trigger Build because of #17612
    
    * apply code review
    
    * add version files
    
    * fix version stuff
    
    * Apply suggestions from code review
    
    Co-authored-by: nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>
    
    * fixes
    
    * add version file
    
    * fix wrong suggestion
    
    * add version files
    
    * Update versions/b-/boost.json
    
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>
    
    * Address the review suggestion
    
    Co-authored-by: nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>
    Co-authored-by: PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>

---
+++ scripts/boost/post-source-stubs/json.cmake
@@ -0,0 +1,7 @@
+# see https://github.com/boostorg/json/issues/556 fore more details
+vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "import ../../config/checks/config" "import config/checks/config")
+vcpkg_replace_string("${SOURCE_PATH}/build/Jamfile" "\n      <library>/boost//container/<warnings-as-errors>off" "")
+
+vcpkg_replace_string("${SOURCE_PATH}/Jamfile" "import ../config/checks/config" "import build/config/checks/config")
+
+file(COPY "${CURRENT_INSTALLED_DIR}/share/boost-config/checks" DESTINATION "${SOURCE_PATH}/build/config")
