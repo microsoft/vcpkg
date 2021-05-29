70bd69df5a4d57192502c2bfd1a96febddc42e4e
Kai Pastor <dg0yt@darc.de>
2021-05-29 13:24:52 +0200

    Update to 4.3.0

--- ports/tiff/portfile.cmake
+++ ports/tiff/portfile.cmake
@@ -1,9 +1,9 @@
-set(LIBTIFF_VERSION 4.1.0)
+set(LIBTIFF_VERSION 4.3.0)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://download.osgeo.org/libtiff/tiff-${LIBTIFF_VERSION}.tar.gz"
+    URLS "https://download.osgeo.org/libtiff/tiff-${LIBTIFF_VERSION}.tar.gz"
     FILENAME "tiff-${LIBTIFF_VERSION}.tar.gz"
-    SHA512 fd541dcb11e3d5afaa1ec2f073c9497099727a52f626b338ef87dc93ca2e23ca5f47634015a4beac616d4e8f05acf7b7cd5797fb218758cc2ad31b390491c5a6
+    SHA512 e04a4a6c542e58a174c1e9516af3908acf1d3d3e1096648c5514f4963f73e7af27387a76b0fbabe43cf867a18874088f963796a7cd6e45deb998692e3e235493
 )
 
 vcpkg_extract_source_archive_ex(
@@ -11,17 +11,23 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
     REF ${LIBTIFF_VERSION}
     PATCHES
-        fix-stddef.patch
         cmakelists.patch
 )
 
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    set (TIFF_CXX_TARGET -Dcxx=OFF)
+set(EXTRA_OPTIONS "")
+if(VCPKG_TARGET_IS_UWP)
+    list(APPEND EXTRA_OPTIONS "-DUSE_WIN32_FILEIO=OFF")  # On UWP we use the unix I/O api.
 endif()
 
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+    list(APPEND EXTRA_OPTIONS "-Dcxx=OFF")
+endif()
+
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    tool BUILD_TOOLS
-	zstd WITH_ZSTD
+    FEATURES
+        tool    BUILD_TOOLS
+        zstd    zstd
 )
 
 vcpkg_configure_cmake(
@@ -29,16 +35,17 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         ${FEATURE_OPTIONS}
+        ${EXTRA_OPTIONS}
         -DBUILD_DOCS=OFF
         -DBUILD_CONTRIB=OFF
         -DBUILD_TESTS=OFF
+        -DCMAKE_DEBUG_POSTFIX=d # tiff sets "d" for MSVC only.
+        -DVERSION=${LIBTIFF_VERSION} # Needed for pc file
         -Djbig=OFF # This is disabled by default due to GPL/Proprietary licensing.
         -Djpeg12=OFF
         -Dwebp=OFF
-        -Dzstd=${WITH_ZSTD}
         -DCMAKE_DISABLE_FIND_PACKAGE_OpenGL=ON
         -DCMAKE_DISABLE_FIND_PACKAGE_GLUT=ON
-        ${TIFF_CXX_TARGET}
 )
 
 vcpkg_install_cmake()
@@ -50,11 +57,11 @@ endif()
 # Fix dependencies:
 set(_file "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libtiff-4.pc")
 if(EXISTS "${_file}")
-    vcpkg_replace_string("${_file}" "Version: 4.1.0" "Version: 4.1.0\nRequires.private: liblzma libjpeg")
+    vcpkg_replace_string("${_file}" "Version: ${LIBTIFF_VERSION}" "Version: ${LIBTIFF_VERSION}\nRequires.private: liblzma libjpeg")
 endif() 
 set(_file "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libtiff-4.pc")
 if(EXISTS "${_file}")
-    vcpkg_replace_string("${_file}" "Version: 4.1.0" "Version: 4.1.0\nRequires.private: liblzma libjpeg")
+    vcpkg_replace_string("${_file}" "Version: ${LIBTIFF_VERSION}" "Version: ${LIBTIFF_VERSION}\nRequires.private: liblzma libjpeg")
 endif()
 
 vcpkg_fixup_pkgconfig()
@@ -69,16 +76,29 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake DESTINATION ${CURR
 file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
 if ("tool" IN_LIST FEATURES)
-    file(GLOB TIFF_TOOLS ${CURRENT_PACKAGES_DIR}/bin/*.exe)
-    file(INSTALL ${TIFF_TOOLS} DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
-    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
-    file(REMOVE ${TIFF_TOOLS})
-    file(GLOB TIFF_TOOLS ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-    file(REMOVE ${TIFF_TOOLS})
-
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-    endif()
+    set(_tools
+        fax2ps
+        fax2tiff
+        pal2rgb
+        ppm2tiff
+        raw2tiff
+        tiff2bw
+        tiff2pdf
+        tiff2ps
+        tiff2rgba
+        tiffcmp
+        tiffcp
+        tiffcrop
+        tiffdither
+        tiffdump
+        tiffinfo
+        tiffmedian
+        tiffset
+        tiffsplit
+    )
+    vcpkg_copy_tools(TOOL_NAMES ${_tools} AUTO_CLEAN)
+elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
 
 vcpkg_copy_pdbs()
