8e02cc37724f77e31bca18931494421ae3f60fa5
luncliff <luncliff@gmail.com>
2021-05-30 23:20:54 +0900

    [liburing] configure with ENABLE_SHARED
    * update git-tree SHA

--- ports/liburing/portfile.cmake
+++ ports/liburing/portfile.cmake
@@ -7,8 +7,8 @@ vcpkg_from_github(
     SHA512 8a7b37600246d44a94d3fed1ca4bb60e76f9ddc60bd3c237e600b60e77961a1125c8a1f230cb7569f959acf10b68b91aafb4935c1c2fd13d5df7373b374e47f5
     HEAD_REF master
     PATCHES
-        fix-configure.patch     # ignore unsupported options
         fix-spec-version.patch  # update version value for pkgconfig(.pc) files
+        fix-configure.patch     # ignore unsupported options, handle ENABLE_SHARED
 )
 
 # note: check ${SOURCE_PATH}/liburing.spec before updating configure options
@@ -17,12 +17,6 @@ vcpkg_configure_make(
     COPY_SOURCE
     NO_DEBUG
 )
-# note: {SOURCE_PATH}/src/Makefile makes liburing.so from liburing.a.
-#   For static, disable .so using the environment variable ENABLE_SHARED.
-#   For dynamic, remove intermediate file liburing.a when install is finished.
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    set(ENV{ENABLE_SHARED} "0")
-endif()
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
@@ -31,6 +25,8 @@ file(INSTALL ${SOURCE_PATH}/LICENSE
 file(INSTALL ${CURRENT_PORT_DIR}/usage 
      DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 
+# note: {SOURCE_PATH}/src/Makefile makes liburing.so from liburing.a.
+#   For dynamic, remove intermediate file liburing.a when install is finished.
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/liburing.a
                 ${CURRENT_PACKAGES_DIR}/lib/liburing.a
