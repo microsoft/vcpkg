5c7f6fa458a7ccdf98f3acbd65044e9763dc510b
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-05-31 19:27:39 +0200

    Don't apply patch.

--- ports/d3d12-memory-allocator/portfile.cmake
+++ ports/d3d12-memory-allocator/portfile.cmake
@@ -4,7 +4,6 @@ vcpkg_from_github(
     REF 10f148cef0dfd34ae1a9373b9396beb1581c992a
     SHA512 c99934a606ce5a5c9c59e05faf2e659bfad2e485b58aaf00f38219a6c89f17b62033f4a69935915f0d5269a4f0ecba41037b044913ae6f4077fa981eaab470c8
     HEAD_REF master
-    PATCHES single_header.patch
 )
 
 file(COPY "${SOURCE_PATH}/src/D3D12MemAlloc.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
