7e0c80d81a9d836397f0dce13d22244c0abf3049
Carsten Rudolph <18394207+crud89@users.noreply.github.com>
2021-05-31 19:43:00 +0200

    Properly set interface sources.

--- ports/d3d12-memory-allocator/unofficial-d3d12-memory-allocator-config.cmake
+++ ports/d3d12-memory-allocator/unofficial-d3d12-memory-allocator-config.cmake
@@ -5,5 +5,5 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 
 add_library(unofficial::d3d12-memory-allocator::d3d12-memory-allocator INTERFACE IMPORTED)
 set_target_properties(unofficial::d3d12-memory-allocator::d3d12-memory-allocator PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include")
-set_target_properties(unofficial::d3d12-memory-allocator::d3d12-memory-allocator PROPERTIES INTERFACE_SOURCES "${_IMPORT_PREFIX}/src")
+set_target_properties(unofficial::d3d12-memory-allocator::d3d12-memory-allocator PROPERTIES INTERFACE_SOURCES "${_IMPORT_PREFIX}/src/D3D12MemAlloc.cpp")
 set(_IMPORT_PREFIX)
