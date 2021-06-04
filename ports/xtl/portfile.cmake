4cde6d9750dc378be31c9990b99ad9a15d07523b
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2021-06-04 16:19:23 +0800

    Update ports/xtl/portfile.cmake

--- ports/xtl/portfile.cmake
+++ ports/xtl/portfile.cmake
@@ -21,8 +21,9 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
-vcpkg_fixup_pkgconfig()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
