ef1fa67bd213da0f8625bde18ef409458f65a762
Carlos O'Ryan <coryan@google.com>
2021-06-04 02:21:35 +0000

    Revert "Address review comments"
    This reverts commit db47320dda0d15a02d9fc4e177f4ee355a5991b5.

--- ports/google-cloud-cpp/portfile.cmake
+++ ports/google-cloud-cpp/portfile.cmake
@@ -28,7 +28,6 @@ vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
-vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
