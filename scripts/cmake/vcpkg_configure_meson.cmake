3b115cde5249f1292a22593d1ab5b62d9df58461
Vitaly Petrov <v31337@gmail.com>
2021-06-05 10:54:19 +0300

    Update vcpkg_configure_meson.cmake

--- scripts/cmake/vcpkg_configure_meson.cmake
+++ scripts/cmake/vcpkg_configure_meson.cmake
@@ -220,7 +220,7 @@ function(vcpkg_internal_meson_generate_cross_file _additional_binaries) #https:/
             OUTPUT_VARIABLE MACHINE
             COMMAND_ERROR_IS_FATAL ANY)
         
-        # show real machine architecture to visual understand, does we now in native Apple Silicon Terminal or inside the Rosetta
+        # show real machine architecture to visual understand, do we now in native Apple Silicon Terminal or inside the Rosetta
         debug_message("Machine: ${MACHINE}")
 
         if(MACHINE MATCHES "arm64")
