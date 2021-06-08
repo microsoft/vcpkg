9893f75b7e9f0e9b28f6927410285d3a333c0d68
Jonliu1993 <13720414433@163.com>
2021-06-08 16:07:42 +0800

    [clickhouse-cpp] update to 1.2.2

--- ports/clickhouse-cpp/portfile.cmake
+++ ports/clickhouse-cpp/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO artpaul/clickhouse-cpp
-    REF 1634d8b9e2f3183de1574344563d90557be3b305
-    SHA512 bef74f624c2a777f8ec6b7e87ac3cc564e914bcecc639b3889695da56be4c90531309a8fd87054c2777580c36bc3b2d6e9c5690a6345018bf65a5294eeeb3390
+    REF 08ad35895f732a27fea82590dc9d05017e312146 #v1.2.2
+    SHA512 4bdbb0d701cab03d39036c75b817bc5e43b7444d7874d156891cf746178fc2c6ef350bf72ac83e2a398357bb9ca263f85f449579cd9aae7d2126e92d3ddc4da2
     HEAD_REF master
     PATCHES 00001-fix-build.patch
 )
@@ -23,4 +23,4 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/clickhouse-cpp)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
-configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/clickhouse-cpp/copyright COPYONLY)
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
