3a2259c41ffd202324ebaedb94e01715da51c7ae
Phoebe <20694052+PhoebeHui@users.noreply.github.com>
2021-06-12 00:58:36 +0800

    [vcpkg baseline][otl] Update to 40463 (#18383)
    * [vcpkg baseline][otl] Update to 40463
    
    * Update the version
    
    * Upate the baseline

--- ports/otl/portfile.cmake
+++ ports/otl/portfile.cmake
@@ -1,9 +1,9 @@
-set(OTL_VERSION 40455)
+set(OTL_VERSION 40463)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "http://otl.sourceforge.net/otlv4_${OTL_VERSION}.zip"
-    FILENAME "otlv4_${OTL_VERSION}-1.zip"
-    SHA512 d1f777dea53f4451616ef628680d070db1fe53683571bd509f58dba9c1e64fe6caa8a81d2f47876c2128af701ba19dc2e9c5612a57232efca3ad084843f272b6
+    FILENAME "otlv4_${OTL_VERSION}-9485a0fe15a7.zip"
+    SHA512 9485a0fe15a737d55b0746a7e289b1a20e9435ed5c69bda7010705f8cde0a456163d83221d0103236a723837596613b578edc6d3d0007ce80a6cc76b4ed83888
 )
 
 vcpkg_extract_source_archive_ex(
