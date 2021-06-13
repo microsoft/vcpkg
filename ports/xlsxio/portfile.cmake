64917727577a13a4054d5656675560cdab0bd46f
abc <a@b.com>
2021-06-13 13:20:35 +0800

    add port: xlsxio

---
+++ ports/xlsxio/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
+                  REPO brechtsanders/xlsxio
+                  REF e3acace39e5fb153f5ce3500a4952c2bf93175bd
+                  SHA512 8148b89c43cf45653c583d51fb8050714d3cd0a76ab9a05d46604f3671a06487e4fc58d3f6f9f2a9f9b57a9f9fe1863ef07017c74197f151390576c5aac360ea
+                  HEAD_REF master
+                  PATCHES
+                      0001-fix-build-type.patch)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+                     FEATURES
+                        libzip WITH_LIBZIP
+                        wide WITH_WIDE)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DBUILD_DOCUMENTATION=OFF
+        -DBUILD_EXAMPLES=OFF
+        -DBUILD_PC_FILES=OFF
+        -DBUILD_TOOLS=OFF
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
