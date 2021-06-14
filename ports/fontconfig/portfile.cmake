7fab96b89d6612d070d86abcbf8e037bb0ec525c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-06-14 14:04:32 +0200

    fix fontconfig broken links

--- ports/fontconfig/portfile.cmake
+++ ports/fontconfig/portfile.cmake
@@ -24,7 +24,7 @@ get_filename_component(GPERF_PATH ${GPERF} DIRECTORY)
 vcpkg_add_to_path(${GPERF_PATH})
 
 if(NOT VCPKG_TARGET_IS_WINDOWS)
-    list(APPEND OPTIONS "LN_S='ln -sr'")
+    list(APPEND OPTIONS "as_ln_s=ln -sr")
 endif()
 
 vcpkg_configure_make(
