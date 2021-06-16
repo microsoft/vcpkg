7d7564be59e44baca601cf1d3eefc4cae7444023
Leonid Pospelov <pospelovlm@yandex.ru>
2021-06-16 10:38:28 +0300

    [ChakraCore] Add Linux support

--- ports/chakracore/portfile.cmake
+++ ports/chakracore/portfile.cmake
@@ -1,20 +1,25 @@
-vcpkg_fail_port_install(ON_TARGET osx linux uwp ON_CRT_LINKAGE static ON_LIBRARY_LINKAGE static)
+vcpkg_fail_port_install(ON_TARGET osx uwp)
 
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+if (WIN32)
+    vcpkg_fail_port_install(ON_CRT_LINKAGE static ON_LIBRARY_LINKAGE static)
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/ChakraCore
-    REF 63c5099027ebb4547c802d62c2d2a6a39ee7eff6 # v1.11.22
-    SHA512 5de915db48f5a125d4e0e112671ad7447212e6c0165d6c634a855a1d334f0bd2f7c015ba8c58d55225dd75d4c6687e6807987b8354b82405eb87944b46313062
+    REF 385409ee4b634b860e090606a28acbc99f4d2567
+    SHA512 ef47db988c4ddd77fa87f4c5e1ac91d9f6b31b35aa6934d8b2863ee1274776d90a2b85dbad51eef069c96777d3cd7729349b89f23eda8c61b4cb637150bead71
     HEAD_REF master
 )
 
-find_path(COR_H_PATH cor.h)
-if(COR_H_PATH MATCHES "NOTFOUND")
-    message(FATAL_ERROR "Could not find <cor.h>. Ensure the NETFXSDK is installed.")
+if(WIN32)
+    find_path(COR_H_PATH cor.h)
+    if(COR_H_PATH MATCHES "NOTFOUND")
+        message(FATAL_ERROR "Could not find <cor.h>. Ensure the NETFXSDK is installed.")
+    endif()
+    get_filename_component(NETFXSDK_PATH "${COR_H_PATH}/../.." ABSOLUTE)
 endif()
-get_filename_component(NETFXSDK_PATH "${COR_H_PATH}/../.." ABSOLUTE)
 
 set(BUILDTREE_PATH ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET})
 file(REMOVE_RECURSE ${BUILDTREE_PATH})
@@ -22,51 +27,92 @@ file(COPY ${SOURCE_PATH}/ DESTINATION ${BUILDTREE_PATH})
 
 set(CHAKRA_RUNTIME_LIB "static_library") # ChakraCore only supports static CRT linkage
 
-vcpkg_build_msbuild(
-    PROJECT_PATH ${BUILDTREE_PATH}/Build/Chakra.Core.sln
-    OPTIONS
-        "/p:DotNetSdkRoot=${NETFXSDK_PATH}/"
-        "/p:CustomBeforeMicrosoftCommonTargets=${CMAKE_CURRENT_LIST_DIR}/no-warning-as-error.props"
-        "/p:RuntimeLib=${CHAKRA_RUNTIME_LIB}"
-)
+if(WIN32)
+    vcpkg_build_msbuild(
+        PROJECT_PATH ${BUILDTREE_PATH}/Build/Chakra.Core.sln
+        OPTIONS
+            "/p:DotNetSdkRoot=${NETFXSDK_PATH}/"
+            "/p:CustomBeforeMicrosoftCommonTargets=${CMAKE_CURRENT_LIST_DIR}/no-warning-as-error.props"
+            "/p:RuntimeLib=${CHAKRA_RUNTIME_LIB}"
+    )
+else()
+    if(VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
+        set(CHAKRACORE_TARGET_ARCH amd64)
+    elseif(VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
+        set(CHAKRACORE_TARGET_ARCH x86)
+    endif()
+
+    vcpkg_find_acquire_program(CLANG)
+    if(CLANG MATCHES "-NOTFOUND")
+        message(FATAL_ERROR "Clang is required.")
+    endif()
+
+    execute_process(
+        COMMAND bash "build.sh" "--arch=${CHAKRACORE_TARGET_ARCH}"
+        WORKING_DIRECTORY ${BUILDTREE_PATH}
+        OUTPUT_VARIABLE CHAKRA_BUILD_SH_OUT
+        ERROR_VARIABLE CHAKRA_BUILD_SH_ERR
+        RESULT_VARIABLE CHAKRA_BUILD_SH_RES
+        ECHO_OUTPUT_VARIABLE
+        ECHO_ERROR_VARIABLE
+    )
+endif()
+
+if (WIN32)
+    set(JSRT_DIRECTORY_NAME jsrt)
+else()
+    set(JSRT_DIRECTORY_NAME Jsrt)
+endif()
 
 file(INSTALL
-    ${BUILDTREE_PATH}/lib/jsrt/ChakraCore.h
-    ${BUILDTREE_PATH}/lib/jsrt/ChakraCommon.h
-    ${BUILDTREE_PATH}/lib/jsrt/ChakraCommonWindows.h
-    ${BUILDTREE_PATH}/lib/jsrt/ChakraDebug.h
+    ${BUILDTREE_PATH}/lib/${JSRT_DIRECTORY_NAME}/ChakraCore.h
+    ${BUILDTREE_PATH}/lib/${JSRT_DIRECTORY_NAME}/ChakraCommon.h
+    ${BUILDTREE_PATH}/lib/${JSRT_DIRECTORY_NAME}/ChakraDebug.h
     DESTINATION ${CURRENT_PACKAGES_DIR}/include
 )
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+if(WIN32)
     file(INSTALL
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.dll
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.pdb
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+        ${BUILDTREE_PATH}/lib/${JSRT_DIRECTORY_NAME}/ChakraCommonWindows.h
+        DESTINATION ${CURRENT_PACKAGES_DIR}/include
     )
-    file(INSTALL
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/Chakracore.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-    )
-endif()
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    file(INSTALL
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.dll
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.pdb
-        DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-    )
-    file(INSTALL
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/Chakracore.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-    )
-    file(INSTALL
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ch.exe
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/GCStress.exe
-        ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/rl.exe
-        DESTINATION ${CURRENT_PACKAGES_DIR}/tools/chakracore)
-    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/chakracore)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(INSTALL
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.dll
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/ChakraCore.pdb
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+        )
+        file(INSTALL
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_debug/Chakracore.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        )
+    endif()
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(INSTALL
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.dll
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ChakraCore.pdb
+            DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+        )
+        file(INSTALL
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/Chakracore.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+        )
+        file(INSTALL
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/ch.exe
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/GCStress.exe
+            ${BUILDTREE_PATH}/Build/VcBuild/bin/${TRIPLET_SYSTEM_ARCH}_release/rl.exe
+            DESTINATION ${CURRENT_PACKAGES_DIR}/tools/chakracore)
+        vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/chakracore)
+    endif()
+    vcpkg_copy_pdbs()
+else()
+
 endif()
 
-vcpkg_copy_pdbs()
+if(WIN32)
+    set(LICENSE_DESTINATION ${CURRENT_PACKAGES_DIR}/share/ChakraCore)
+else()
+    set(LICENSE_DESTINATION ${CURRENT_PACKAGES_DIR}/share/chakracore)
+endif()
 file(INSTALL
     ${SOURCE_PATH}/LICENSE.txt
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/ChakraCore RENAME copyright)
+    DESTINATION ${LICENSE_DESTINATION} RENAME copyright)
