98f21114d2b8232f149643d9d1f70968ddd57d63
Lukas Lipp <llipp@cg.tuwien.ac.at>
2021-06-16 20:48:00 +0200

    [glslang] portfile apply changes

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -3,13 +3,17 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO KhronosGroup/glslang
-  REF f88e5824d2cfca5edc58c7c2101ec9a4ec36afac
-  SHA512 92dc287e8930db6e00bde23b770f763dc3cf8a405a37b682bbd65e1dbde1f1f5161543fcc70b09eef07a5ce8bbe8f368ef84ac75003c122f42d1f6b9eaa8bd50
+  REF 18eef33bd7a4bf5ad8c69f99cb72022608cf6e73
+  SHA512 1e3c034b27ce4fa335ec0789e744eb2d260e032dc429ed0877e1c2a691656eac6f2e9e03c00dec3b115b249690a38f155e4456fad0fd803d3c229b09e5a4ee3b
   HEAD_REF master
   PATCHES
     CMakeLists-targets.patch
-    CMakeLists-windows.patch
+    CMakeLists-os.patch
 )
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON_PATH ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path("${PYTHON_PATH}")
 
 if(VCPKG_TARGET_IS_IOS)
   # this case will report error since all executable will require BUNDLE DESTINATION
@@ -29,7 +33,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/glslang)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/glslang)
 
 vcpkg_copy_pdbs()
 
@@ -42,4 +46,4 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/debug/bin)
 
 # Handle copyright
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/glslang)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
