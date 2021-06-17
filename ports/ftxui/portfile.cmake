63fda376d65e3489f4a65b7973220432e59d65fb
myd7349 <myd7349@gmail.com>
2021-06-18 00:05:25 +0800

    [ftxui] Remove debug/share

--- ports/ftxui/portfile.cmake
+++ ports/ftxui/portfile.cmake
@@ -23,6 +23,6 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
