58dfadbbee03186eb0e23df42b023045bb964b5d
Michael Ragazzon <michael.ragazzon@gmail.com>
2021-06-19 13:36:59 +0200

    - Update to RmlUi 4.1, fixes some issues with missing dependencies. - Add robin-hood-hashing as dependency, replacing built-in copy. - Only need FreeType core. - Static library definition now replaced in-source.

--- ports/rmlui/portfile.cmake
+++ ports/rmlui/portfile.cmake
@@ -1,32 +1,51 @@
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO mikke89/RmlUi
-    REF 4.0
-    SHA512 b1c8dc48ce3c1ef8e53c7e8d0aa830eec5b968bb3ff7cd778067627de649b45c1a72adfcd168d0ca36018455b6b2a740324cec63304ec4e66dd859ef9d64f674
-    HEAD_REF master
-	PATCHES 
-       fix-uwp.patch
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO mikke89/RmlUi
+	REF 4.1
+	SHA512 f79bd30104c42469142e4c79a81f120c61f5bd3ae918df9847fa42d05fcda372d3adb5f6884c81c8517a440a81235e70ffcdde8d98751a14d2e4265fc2051a01
+	HEAD_REF master
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES 
-        lua             BUILD_LUA_BINDINGS
-    INVERTED_FEATURES
-        freetype        NO_FONT_INTERFACE_DEFAULT
+	FEATURES 
+		lua             BUILD_LUA_BINDINGS
+	INVERTED_FEATURES
+		freetype        NO_FONT_INTERFACE_DEFAULT
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        ${FEATURE_OPTIONS}
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+	vcpkg_replace_string(${SOURCE_PATH}/Include/RmlUi/Core/Header.h
+		"#if !defined RMLUI_STATIC_LIB"
+		"#if 0"
+	)
+	vcpkg_replace_string(${SOURCE_PATH}/Include/RmlUi/Debugger/Header.h
+		"#if !defined RMLUI_STATIC_LIB"
+		"#if 0"
+	)
+	if ("lua" IN_LIST FEATURES)
+		vcpkg_replace_string(${SOURCE_PATH}/Include/RmlUi/Lua/Header.h
+			"#if !defined RMLUI_STATIC_LIB"
+			"#if 0"
+		)
+	endif()
+endif()
+
+# Replace built-in thirdparty header
+file(COPY
+	${CURRENT_INSTALLED_DIR}/include/robin_hood.h
+	DESTINATION ${SOURCE_PATH}/Include/RmlUi/Core/Containers
+)
+
+vcpkg_cmake_configure(
+	SOURCE_PATH ${SOURCE_PATH}
+	OPTIONS
+		${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(
-    CONFIG_PATH lib/${PORT}/cmake
+	CONFIG_PATH  lib/${PORT}/cmake
 )
-
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE 
