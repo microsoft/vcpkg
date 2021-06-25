25c454b8468b6188ca056797a047982edd9ad115
Robert Schumacher <roschuma@microsoft.com>
2021-06-25 15:02:04 -0700

    [mpg123] Separate UWP patches

--- ports/mpg123/portfile.cmake
+++ ports/mpg123/portfile.cmake
@@ -1,6 +1,15 @@
 set(MPG123_VERSION 1.28.0)
 set(MPG123_HASH 4e333ee4f3bbebcfff280cf286265e969a8da93b9043d03c0189e22cd40918b07bf12181bd06141d4479c78bc0d0ed472e0d3bb61b2fdb96fe9f7cd48f9a6b77)
 
+set(PATCHES "")
+if(VCPKG_TARGET_IS_UWP)
+    set(PATCHES
+        0002-fix-libmpg123-uwp-build.patch
+        0003-fix-libout123-uwp-build.patch
+        0004-fix-libsyn123-uwp-build.patch
+    )
+endif()
+
 vcpkg_from_sourceforge(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mpg123/mpg123
@@ -9,9 +18,7 @@ vcpkg_from_sourceforge(
     SHA512 ${MPG123_HASH}
     PATCHES
         0001-fix-checkcpuarch-path.patch
-        0002-fix-libmpg123-uwp-build.patch
-        0003-fix-libout123-uwp-build.patch
-        0004-fix-libsyn123-uwp-build.patch
+        ${PATCHES}
 )
 
 include(${CURRENT_INSTALLED_DIR}/share/yasm-tool-helper/yasm-tool-helper.cmake)
