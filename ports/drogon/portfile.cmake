e55431c6f100c02a0a4bbc6c4958dbe2ddcaa86d
Greisby <greisberger@zenon-media.com>
2021-06-29 12:07:53 +0200

    [drogon] Added database access features
    * Replaced CONTROL with vcpkg.json manifest file
    
    * Patched finding brotli libraries for static triplets (brotlicommon-static.lib and co.)
    
    * Added features for database access, disabled by default.

--- ports/drogon/portfile.cmake
+++ ports/drogon/portfile.cmake
@@ -9,12 +9,17 @@ vcpkg_from_github(
         resolv.patch
         drogon_config.patch
         ssize_t.patch
+        static-brotli.patch
 )
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-    ctl BUILD_CTL
+        ctl      BUILD_CTL
+        mysql    BUILD_MYSQL
+        orm      BUILD_ORM
+        postgres BUILD_POSTGRESQL
+        sqlite3  BUILD_SQLITE
 )
 
 vcpkg_configure_cmake(
