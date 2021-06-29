f2e80167877825e7982f9fbb7a5a738128dfcaa2
Xing Ji <jixingcn@gmail.com>
2021-06-29 21:46:52 +0800

    update gli

--- ports/gli/portfile.cmake
+++ ports/gli/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/gli TARGET_PATH share/gli)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/gli)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
