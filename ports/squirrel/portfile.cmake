cebc22b0277bcb37e86dfffe95c6ca79f60beadb
Artem Shubovych <ashubovych@atlassian.com>
2021-06-30 18:32:12 +1000

    Smart remove bin directory for static vs dynamic build; add note on the binaries there

--- ports/squirrel/portfile.cmake
+++ ports/squirrel/portfile.cmake
@@ -3,10 +3,28 @@ vcpkg_from_github(
     REPO albertodemichelis/squirrel
     HEAD_REF master
     REF cf0516720e1fa15c8cbd649aebd1924f6e7084cc
-    SHA512 6127d25e40217188abe14e30943f131f0e03923cf095f3df276a9c36b48495cf5d84bb1e30b39fa23bd630d905b6a6b4c70685dfb7a999b8b0c12e28c2e3b902 # 0462bf5a4347ad9c24d8372122ac1b4474d9c80cbcac8523a08a671232688f7193ea2c8b7f05088ad9d6a0dbb8c893b1e8e640185a326be75efdd86e813c5016
+    SHA512 6127d25e40217188abe14e30943f131f0e03923cf095f3df276a9c36b48495cf5d84bb1e30b39fa23bd630d905b6a6b4c70685dfb7a999b8b0c12e28c2e3b902
 )
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_DYNAMIC)
+
+# TODO: currently, squirrel builds samples (see the `sq/` dir) no matter what; it should be an optional build, but there is no option for it and it fails if either DISABLE_STATIC or DISABLE_DYNAMIC option is provided
+
+# if(BUILD_STATIC)
+#     set(DISABLE_DYNAMIC ON)
+#     set(DISABLE_STATIC OFF)
+# elseif(BUILD_DYNAMIC)
+#     set(DISABLE_DYNAMIC OFF)
+#     set(DISABLE_STATIC ON)
+# endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    # OPTIONS
+    #     -DDISABLE_DYNAMIC=${DISABLE_DYNAMIC}
+    #     -DDISABLE_STATIC=${DISABLE_STATIC}
+)
 
 vcpkg_cmake_install()
 
@@ -15,20 +33,17 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/squirrel)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-if((VCPKG_LIBRARY_LINKAGE STREQUAL "static") OR (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic"))
-    # on Windows DLLs are put under the bin/ directory
-    if(WIN32)
-        file(REMOVE_RECURSE
-            "${CURRENT_PACKAGES_DIR}/debug/bin/sq.exe"
-            "${CURRENT_PACKAGES_DIR}/debug/bin/sq_static.exe")
+if(BUILD_STATIC)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+elseif(BUILD_DYNAMIC)
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/debug/bin/sq.exe"
+        "${CURRENT_PACKAGES_DIR}/debug/bin/sq_static.exe")
 
-        file(REMOVE_RECURSE
-            "${CURRENT_PACKAGES_DIR}/bin/sq.exe"
-            "${CURRENT_PACKAGES_DIR}/bin/sq_static.exe")
-    else()
-        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
-        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-    endif()
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/bin/sq.exe"
+        "${CURRENT_PACKAGES_DIR}/bin/sq_static.exe")
 endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
