6c74fa3d372d7e3aa8060795139d836bc60d21d6
Kai Pastor <dg0yt@darc.de>
2021-06-30 08:19:19 +0200

    Revise cmake wrapper

--- ports/gdal/vcpkg-cmake-wrapper.cmake
+++ ports/gdal/vcpkg-cmake-wrapper.cmake
@@ -1,12 +1,28 @@
-include(FindPackageHandleStandardArgs)
 include(SelectLibraryConfigurations)
 
-find_path(GDAL_INCLUDE_DIR NAMES gdal.h HINTS ${CURRENT_INSTALLED_DIR})
-
-find_library(GDAL_LIBRARY_DEBUG NAMES gdal_d gdal_i_d gdal NAMES_PER_DIR PATH_SUFFIXES lib PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug" NO_DEFAULT_PATH REQUIRED)
-find_library(GDAL_LIBRARY_RELEASE NAMES gdal_i gdal NAMES_PER_DIR PATH_SUFFIXES lib PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}" NO_DEFAULT_PATH REQUIRED)
-
+find_path(GDAL_INCLUDE_DIR
+    NAMES gdal.h
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/../../include"
+    NO_DEFAULT_PATH
+)
+find_library(GDAL_LIBRARY_DEBUG
+    NAMES gdal_d gdal_i_d gdal
+    NAMES_PER_DIR
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/../../debug/lib"
+    NO_DEFAULT_PATH
+)
+find_library(GDAL_LIBRARY_RELEASE
+    NAMES gdal_i gdal
+    NAMES_PER_DIR
+    PATHS "${CMAKE_CURRENT_LIST_DIR}/../../lib"
+    NO_DEFAULT_PATH
+)
 select_library_configurations(GDAL)
 
-set(GDAL_INCLUDE_DIRS ${GDAL_INCLUDE_DIR})
-set(GDAL_LIBRARIES ${GDAL_LIBRARY})
+if(NOT GDAL_INCLUDE_DIR OR NOT GDAL_LIBRARY)
+    message(FATAL_ERROR "Installation of vcpkg port gdal is broken.")
+endif()
+
+set(FindGDAL_SKIP_GDAL_CONFIG TRUE)
+
+_find_package(${ARGS})
