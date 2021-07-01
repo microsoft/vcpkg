cc5a5106d7f5f12c49f6b3325549464963bab4cf
aggieNick02 <nick@pcpartpicker.com>
2021-07-01 09:27:37 -0500

    [tbb] Remove ability to disable exception support in TBB runtime (#18740)
    * After filing an issue with TBB (https://github.com/oneapi-src/oneTBB/issues/414), I found out that building the library with exceptions explicitly disabled as I had added support for had gone from not officially supported to more explicitly disallowed
    
    * update tbb port-version
    
    * run x-add-version

--- ports/tbb/portfile.cmake
+++ ports/tbb/portfile.cmake
@@ -14,22 +14,11 @@ vcpkg_from_github(
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-if (TBB_DISABLE_EXCEPTIONS)
-    message(STATUS "Building TBB with exception-handling constructs disabled because TBB_DISABLE_EXCEPTIONS is set to ON.")
-else()
-    message(STATUS "TBB uses exception-handling constructs by default (if supported by the compiler). This use can be disabled with 'SET(TBB_DISABLE_EXCEPTIONS ON)' in your custom triplet.")
-endif()
 
 if (NOT VCPKG_TARGET_IS_WINDOWS)
-    if (TBB_DISABLE_EXCEPTIONS)
-        set(DISABLE_EXCEPTIONS ON)
-    else()
-        set(DISABLE_EXCEPTIONS OFF)
-    endif()
     vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
         PREFER_NINJA
-        OPTIONS -DDISABLE_EXCEPTIONS=${DISABLE_EXCEPTIONS}
     )
 
     vcpkg_install_cmake()
@@ -72,10 +61,6 @@ else()
         else()
             string(REPLACE "\/D_CRT_SECURE_NO_DEPRECATE"
                         "\/D_CRT_SECURE_NO_DEPRECATE \/DIN_CILK_RUNTIME" SLN_CONFIGURE "${SLN_CONFIGURE}")
-        endif()
-        if (TBB_DISABLE_EXCEPTIONS)
-            string(REPLACE "<PreprocessorDefinitions>%(PreprocessorDefinitions)<\/PreprocessorDefinitions>"
-                        "<PreprocessorDefinitions>TBB_USE_EXCEPTIONS=0;%(PreprocessorDefinitions)<\/PreprocessorDefinitions>" SLN_CONFIGURE "${SLN_CONFIGURE}")
         endif()
         file(WRITE ${CONFIGURE_FILE_NAME} "${SLN_CONFIGURE}")
     endmacro()
