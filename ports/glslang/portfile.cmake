afe89cb24f8636fdca6a22aa80bba8e974820518
Robert Schumacher <ras0219@outlook.com>
2021-07-01 14:04:38 +0000

    [glslang][shaderc] Update and fix targets to match upstream

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -7,8 +7,8 @@ vcpkg_from_github(
   SHA512 1c5a91e60c1809c6c26f11649659090a75501b0570f3147e5b27ac65c539b591967f258d14c399d33019317864ede823353ea44e0015bc3f7afc5a787f046cc7
   HEAD_REF master
   PATCHES
-    CMakeLists-targets.patch
-    CMakeLists-os.patch
+    ignore-crt.patch
+    always-install-resource-limits.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
@@ -26,13 +26,12 @@ vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
   PREFER_NINJA
   OPTIONS
-    -DCMAKE_DEBUG_POSTFIX=d
     -DSKIP_GLSLANG_INSTALL=OFF
+    -DBUILD_EXTERNAL=OFF
     -DENABLE_GLSLANG_BINARIES=${BUILD_BINARIES}
 )
 
 vcpkg_install_cmake()
-
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/glslang)
 
 vcpkg_copy_pdbs()
@@ -40,10 +39,19 @@ vcpkg_copy_pdbs()
 if(NOT BUILD_BINARIES)
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
 else()
-  file(RENAME ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/tools)
+  file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools)
+  file(RENAME ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/tools/glslang)
 endif()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
                     ${CURRENT_PACKAGES_DIR}/debug/bin)
 
-# Handle copyright
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+if(EXISTS ${CURRENT_PACKAGES_DIR}/share/glslang/glslang-config.cmake OR EXISTS ${CURRENT_PACKAGES_DIR}/share/glslang/glslangConfig.cmake)
+  message(FATAL_ERROR "glslang has been updated to provide a -config file -- please remove the vcpkg provided version from the portfile")
+endif()
+
+file(COPY
+  ${CMAKE_CURRENT_LIST_DIR}/copyright
+  ${CMAKE_CURRENT_LIST_DIR}/glslang-config.cmake
+  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
