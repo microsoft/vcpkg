1611fdcb6144381569207d207b48a7bf4cf8211b
Kai Pastor <dg0yt@darc.de>
2021-07-06 21:01:31 +0200

    Modernize portfile

--- ports/giflib/portfile.cmake
+++ ports/giflib/portfile.cmake
@@ -2,8 +2,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 set(GIFLIB_VERSION 5.2.1)
 
+set(EXTRA_PATCHES "")
 if (VCPKG_TARGET_IS_WINDOWS)
-    set(ADDITIONAL_PATCH "fix-compile-error.patch")
+    list(APPEND EXTRA_PATCHES fix-compile-error.patch)
 endif()
 
 vcpkg_from_sourceforge(
@@ -13,19 +14,18 @@ vcpkg_from_sourceforge(
     SHA512 4550e53c21cb1191a4581e363fc9d0610da53f7898ca8320f0d3ef6711e76bdda2609c2df15dc94c45e28bff8de441f1227ec2da7ea827cb3c0405af4faa4736
     PATCHES
         msvc-guard-unistd-h.patch
-        ${ADDITIONAL_PATCH}
+        ${EXTRA_PATCHES}
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG
         -DGIFLIB_SKIP_HEADERS=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/giflib RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
