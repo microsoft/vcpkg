55b235225f9ecf57790e04fdf1afb26af2d495b1
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-07-06 23:25:37 +0200

    remove patch

--- ports/qtbase/portfile.cmake
+++ ports/qtbase/portfile.cmake
@@ -9,7 +9,8 @@ set(${PORT}_PATCHES
         allow_outside_prefix.patch 
         dont_force_cmakecache.patch
         fix_cmake_build.patch
-        promotion.patch
+        #promotion.patch
+        #promotion.patch
         #buildcmake.patch # <-probably required
         #fix_find_dep.patch # <-MAybe required
         clang-cl_source_location.patch
