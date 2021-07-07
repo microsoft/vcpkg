4d84cae199fc2ef7431b574cfe11579c500ebbfe
JackBoosY <yuzaiyang@beyondsoft.com>
2021-07-06 20:05:10 -0700

    update version

--- scripts/cmake/vcpkg_install_msbuild.cmake
+++ scripts/cmake/vcpkg_install_msbuild.cmake
@@ -185,7 +185,7 @@ function(vcpkg_install_msbuild)
         endif()
         if(NOT exes STREQUAL "")
             file(COPY ${exes} DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
-            vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+            vcpkg_copy_tool_dependencies(TOOL_DIR "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
         endif()
     endif()
 
