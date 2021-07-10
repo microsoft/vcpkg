32b83a3ffe49fa51463a992a0982dd69f8b8bf20
myd7349 <myd7349@gmail.com>
2021-07-10 10:56:54 +0800

    [czmq] Fix dependencies

--- ports/czmq/Findlibcurl.cmake
+++ ports/czmq/Findlibcurl.cmake
@@ -1,10 +1,11 @@
-find_package(CURL CONFIG REQUIRED)
+find_package(CURL CONFIG QUIET)
 
-set(LIBCURL_INCLUDE_DIRS ${CURL_INCLUDE_DIRS})
-set(LIBCURL_LIBRARIES CURL::libcurl)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-    LIBCURL
-    REQUIRED_VARS LIBCURL_LIBRARIES LIBCURL_INCLUDE_DIRS
-)
+if(CURL_FOUND)
+    set(LIBCURL_INCLUDE_DIRS ${CURL_INCLUDE_DIRS})
+    set(LIBCURL_LIBRARIES CURL::libcurl)
+    set(LIBCURL_FOUND TRUE)
+    message(STATUS "Found libcurl: ${LIBCURL_LIBRARIES}")
+else()
+    set(LIBCURL_FOUND FALSE)
+    message(STATUS "Could not find libcurl")
+endif()
