0850cf3808aa0859300cbdca988b087545edfce7
Billy Robert O'Neal III <bion@microsoft.com>
2021-07-12 20:52:43 +0000

    Fix typo in deployment of CUDA.

--- scripts/azure-pipelines/linux/provision-image.sh
+++ scripts/azure-pipelines/linux/provision-image.sh
@@ -59,7 +59,7 @@ curl -sSL https://get.haskellstack.org/ | sudo sh
 
 # Install CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt -y update
