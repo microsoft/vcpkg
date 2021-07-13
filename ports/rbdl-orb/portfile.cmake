b4c2a688b12bf47897390e9724a2b49d35a1aeb6
ju6ge <judge@felixrichter.tech>
2021-07-13 11:51:20 +0200

    only use vcpkg_from_github for source download

--- ports/rbdl-orb/portfile.cmake
+++ ports/rbdl-orb/portfile.cmake
@@ -1,45 +1,32 @@
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" RBDL_STATIC)	
 
-find_program(GIT git)
-
-set(GIT_URL "https://github.com/orb-hd/rbdl-orb.git")
-set(GIT_REV "b56291e4ccd8e84b54252aceac7a48aa017cc0a1")
-
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
-set(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled)
-
-
-if(NOT EXISTS "${SOURCE_PATH}/.git")
-	message(STATUS "Cloning and fetching submodules")
-	vcpkg_execute_required_process(
-	  COMMAND ${GIT} clone --recursive ${GIT_URL} ${SOURCE_PATH}
-	  LOGNAME clone
-	)
-endif()		
-
-message(STATUS "Pull latests commits")
-vcpkg_execute_required_process(
-  COMMAND ${GIT} pull origin master
-  WORKING_DIRECTORY ${SOURCE_PATH}
-  LOGNAME checkout
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO ORB-HD/rbdl-orb
+  REF  b56291e4ccd8e84b54252aceac7a48aa017cc0a1
+  SHA512 569555afcf07caa2569af73788c32c3190ce0042fe04e79f206376779452d1baab786f6b6d657ad3a17e1e43fae749f73877556914a869cb3919ba2822f98135
+  HEAD_REF master
 )	
 
-message(STATUS "Checkout revision ${GIT_REV}")
-vcpkg_execute_required_process(
-  COMMAND ${GIT} checkout ${GIT_REV}
-  WORKING_DIRECTORY ${SOURCE_PATH}
-  LOGNAME checkout
+vcpkg_from_github(
+  OUT_SOURCE_PATH PARSER_SOURCE_PATH
+  REPO ORB-HD/URDF_Parser
+  REF  0f3310d766c658b72d54560833012c8fe63ce9d7
+  SHA512 6cd8e300cc47b5a5370efb5a4cd843a1621e2832b790daedc1e260ba5bbcaaabdbcddce239f93c3900258093d483d332110ba7e9f0b4b6cda64ce51b6cf2365d
 )
+if(NOT EXISTS "${SOURCE_PATH}/addons/urdfreader/thirdparty")
+    file(RENAME "${PARSER_SOURCE_PATH}" "${SOURCE_PATH}/addons/urdfreader/thirdparty")
+endif()		
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA # Disable this option if project cannot be built with Ninja
     OPTIONS
         -DRBDL_BUILD_STATIC=${RBDL_STATIC}
-	-DRBDL_BUILD_ADDON_LUAMODEL=ON
-	-DRBDL_BUILD_ADDON_GEOMETRY=ON
-	-DRBDL_BUILD_ADDON_URDFREADER=ON
-	-DRBDL_BUILD_EXECUTABLES=OFF
+        -DRBDL_BUILD_ADDON_LUAMODEL=ON
+        -DRBDL_BUILD_ADDON_GEOMETRY=ON
+        -DRBDL_BUILD_ADDON_URDFREADER=ON
+        -DRBDL_BUILD_EXECUTABLES=OFF
 )
 
 vcpkg_cmake_install()
