42aa3a3c83401e4ecf68d50518fc9c6ab59e2cc8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-07-14 16:43:53 +1000

    [ports/rexo/portfile.cmake] Update version; use vcpkg_configure_cmake

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -1,11 +1,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO christophercrouzet/rexo
-    REF fcfb35d341aa5c7921a1e9e3350d4768104617e0
-    SHA512 9ab931c38c0d29a3c7fcb105bce9d4b9d47db08a13cd3fa6c7c7c4fac5bce94583dddb8480e423b80bd2e7191e0f73dbd3bfda62ca36cbd2c4ca164b7705fb6a
-    HEAD_REF master
+    REPO "christophercrouzet/rexo"
+    REF "v0.2.0"
+    SHA512 "f22ca5be1b5d1a5159377609d2b666eaa69643281294202d2c37b8ab5c9d28acd5b67c2a3d5a0c4e6bae487b9ceff00a11dae1847e7a1e5f72193d9ad34b1457"
+    HEAD_REF "main"
 )
 
-file(COPY ${SOURCE_PATH}/include/rexo.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PREFER_NINJA
+    OPTIONS
+        -DREXO_BUILD_TESTS=OFF
+)
 
-configure_file(${SOURCE_PATH}/UNLICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+file(COPY "${SOURCE_PATH}/include/rexo.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+configure_file("${SOURCE_PATH}/UNLICENSE" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
