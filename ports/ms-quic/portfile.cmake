2ecb24d265c5fa36385e4c5509837a359c112118
dong-ha-park <luncliff@gmail.com>
2021-07-16 12:58:45 +0900

    [ms-quic] make quictls only
    * reduce feature list for future updates
    * update version SHA

--- ports/ms-quic/portfile.cmake
+++ ports/ms-quic/portfile.cmake
@@ -12,48 +12,37 @@ vcpkg_from_github(
         fix-install.patch   # Adjust install path of build outputs
 )
 
-if("quictls" IN_LIST FEATURES)
-    vcpkg_from_github(
-        OUT_SOURCE_PATH OPENSSL_SOURCE_PATH
-        REPO quictls/openssl
-        REF a6e9d76db343605dae9b59d71d2811b195ae7434
-        SHA512 23510a11203b96476c194a1987c7d4e758375adef0f6dfe319cd8ec4b8dd9b12ea64c4099cf3ba35722b992dad75afb1cfc5126489a5fa59f5ee4d46bdfbeaf6
-        HEAD_REF OpenSSL_1_1_1k+quic
-    )
-    file(REMOVE_RECURSE ${QUIC_SOURCE_PATH}/submodules)
-    file(MAKE_DIRECTORY ${QUIC_SOURCE_PATH}/submodules)
-    file(RENAME ${OPENSSL_SOURCE_PATH} ${QUIC_SOURCE_PATH}/submodules/openssl)
+vcpkg_from_github(
+    OUT_SOURCE_PATH OPENSSL_SOURCE_PATH
+    REPO quictls/openssl
+    REF a6e9d76db343605dae9b59d71d2811b195ae7434
+    SHA512 23510a11203b96476c194a1987c7d4e758375adef0f6dfe319cd8ec4b8dd9b12ea64c4099cf3ba35722b992dad75afb1cfc5126489a5fa59f5ee4d46bdfbeaf6
+    HEAD_REF OpenSSL_1_1_1k+quic
+)
+file(REMOVE_RECURSE ${QUIC_SOURCE_PATH}/submodules)
+file(MAKE_DIRECTORY ${QUIC_SOURCE_PATH}/submodules)
+file(RENAME ${OPENSSL_SOURCE_PATH} ${QUIC_SOURCE_PATH}/submodules/openssl)
 
-    vcpkg_find_acquire_program(PERL)
-    get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-    vcpkg_add_to_path(${PERL_EXE_PATH})
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
+vcpkg_add_to_path(${PERL_EXE_PATH})
 
-    if(NOT VCPKG_HOST_IS_WINDOWS)
-        find_program(MAKE make)
-        get_filename_component(MAKE_EXE_PATH ${MAKE} DIRECTORY)
-        vcpkg_add_to_path(PREPEND ${MAKE_EXE_PATH})
-    endif()
-
-    if(VCPKG_TARGET_IS_WINDOWS)
-        vcpkg_find_acquire_program(NASM)
-        get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
-        vcpkg_add_to_path(PREPEND ${NASM_EXE_PATH})
-
-    endif()
+if(NOT VCPKG_HOST_IS_WINDOWS)
+    find_program(MAKE make)
+    get_filename_component(MAKE_EXE_PATH ${MAKE} DIRECTORY)
+    vcpkg_add_to_path(PREPEND ${MAKE_EXE_PATH})
 endif()
+
+ if(VCPKG_TARGET_IS_WINDOWS)
+     vcpkg_find_acquire_program(NASM)
+     get_filename_component(NASM_EXE_PATH ${NASM} DIRECTORY)
+     vcpkg_add_to_path(PREPEND ${NASM_EXE_PATH})
+ endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         tools QUIC_BUILD_TOOLS
 )
-
-if("quictls" IN_LIST FEATURES)
-    set(TLS_OPTION "openssl")
-elseif(VCPKG_TARGET_IS_WINDOWS)
-    set(TLS_OPTION "schannel")
-else()
-    set(TLS_OPTION "stub")
-endif()
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
 
@@ -62,7 +51,7 @@ vcpkg_cmake_configure(
     OPTIONS
         ${FEATURE_OPTIONS}
         -DQUIC_SOURCE_LINK=OFF
-        -DQUIC_TLS=${TLS_OPTION}
+        -DQUIC_TLS=openssl
         -DQUIC_TLS_SECRETS_SUPPORT=ON
         -DQUIC_USE_SYSTEM_LIBCRYPTO=OFF
         -DQUIC_BUILD_PERF=OFF
@@ -71,9 +60,7 @@ vcpkg_cmake_configure(
         -DQUIC_UWP_BUILD=${VCPKG_TARGET_IS_UWP}
 )
 
-if("quictls" IN_LIST FEATURES)
-    vcpkg_cmake_build(TARGET OpenSSL_Build) # separate build log
-endif()
+vcpkg_cmake_build(TARGET OpenSSL_Build) # separate build log for quictls/openssl
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME msquic CONFIG_PATH lib/cmake/msquic)
