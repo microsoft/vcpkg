6e712a1df1cb22ce923a9f9fba41523610dc566f
Jonliu1993 <13720414433@163.com>
2021-07-16 15:22:24 +0800

    print a message

--- ports/azure-storage-cpp/portfile.cmake
+++ ports/azure-storage-cpp/portfile.cmake
@@ -1,3 +1,4 @@
+message(WARNING "azure-storage-cpp is no longer actively developed. Instead, users should migrate to the new sdk: https://github.com/Azure/azure-sdk-for-cpp/releases")
 vcpkg_fail_port_install(ON_TARGET "UWP")
 
 vcpkg_from_github(
