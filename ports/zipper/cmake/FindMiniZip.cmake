da397338573dc04d05be8696e618fa0bd69a1c09
BullyWiiPlaza <bullywiihacks@gmail.com>
2021-07-17 13:28:06 +0200

    Add draft port zipper

---
+++ ports/zipper/cmake/FindMiniZip.cmake
@@ -0,0 +1,30 @@
+# - Find minizip
+# Find the native MINIZIP includes and library
+#
+#  MINIZIP_INCLUDE_DIR - where to find minizip.h, etc.
+#  MINIZIP_LIBRARIES   - List of libraries when using minizip.
+#  MINIZIP_FOUND       - True if minizip found.
+
+
+IF (MINIZIP_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(MINIZIP_FIND_QUIETLY TRUE)
+ENDIF (MINIZIP_INCLUDE_DIR)
+
+FIND_PATH(MINIZIP_INCLUDE_DIR zip.h PATH_SUFFIXES minizip)
+
+SET(MINIZIP_NAMES minizip)
+FIND_LIBRARY(MINIZIP_LIBRARY NAMES ${MINIZIP_NAMES} )
+
+# handle the QUIETLY and REQUIRED arguments and set MINIZIP_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MiniZip DEFAULT_MSG MINIZIP_LIBRARY MINIZIP_INCLUDE_DIR)
+
+IF(MINIZIP_FOUND)
+  SET( MINIZIP_LIBRARIES ${MINIZIP_LIBRARY} )
+ELSE(MINIZIP_FOUND)
+  SET( MINIZIP_LIBRARIES )
+ENDIF(MINIZIP_FOUND)
+
+MARK_AS_ADVANCED( MINIZIP_LIBRARY MINIZIP_INCLUDE_DIR )
---
+++ ports/zipper/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO sebastiandev/zipper
+  REF 201627bd3dbfbf1b53f69af29b8b079a9f3ffb1c
+  SHA512 cd8362849a36945701e1f4b7305518b05e5d57df38fb578871182853e8757d582be839786387dac959c4b15f1a12172a5d870dd237a3e1361d8ec5dbcdb1616c
+  HEAD_REF master
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+# Post-build test for cmake libraries
+vcpkg_test_cmake(PACKAGE_NAME zipper)
