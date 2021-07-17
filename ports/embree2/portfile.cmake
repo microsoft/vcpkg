abe8ccf96b1fdc2a2bd63868306658351e7927e7
Billy Robert ONeal III <bion@microsoft.com>
2021-07-16 22:13:02 -0700

    [embree2] Fix static linking
    Recently attempting to build embree2 is causing our osx workers to die. Example recent CIs:
    
    https://dev.azure.com/vcpkg/public/_build/results?buildId=56146
    https://dev.azure.com/vcpkg/public/_build/results?buildId=55886
    https://dev.azure.com/vcpkg/public/_build/results?buildId=55802
    
    Notably, we don't see this problem in PRs because it is marked "fail" in ci.baseline.txt. Initially, I was going to change it to skip, but observed that the project uses an ordinary cmake build and all the failing triplets were static ones.
    
    If this PR passes, the hope is that CI with this change will pass too.

--- ports/embree2/portfile.cmake
+++ ports/embree2/portfile.cmake
@@ -8,7 +8,13 @@ vcpkg_from_github(
         cmake_policy.patch
 )
 
-file(REMOVE ${SOURCE_PATH}/common/cmake/FindTBB.cmake)
+file(REMOVE "${SOURCE_PATH}/common/cmake/FindTBB.cmake")
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    set(EMBREE_STATIC_LIB ON)
+else()
+    set(EMBREE_STATIC_LIB OFF)
+endif()
 
 if(VCPKG_CRT_LINKAGE STREQUAL static)
     set(EMBREE_STATIC_RUNTIME ON)
@@ -17,12 +23,13 @@ else()
 endif()
 
 vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    PREFER_NINJA
     OPTIONS
         -DEMBREE_ISPC_SUPPORT=OFF
         -DEMBREE_TUTORIALS=OFF
+        -DEMBREE_STATIC_LIB=${EMBREE_STATIC_LIB}
         -DEMBREE_STATIC_RUNTIME=${EMBREE_STATIC_RUNTIME}
         "-DTBB_LIBRARIES=TBB::tbb"
         "-DTBB_INCLUDE_DIRS=${CURRENT_INSTALLED_DIR}/include"
@@ -34,19 +41,22 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 # these cmake files do not seem to contain helpful configuration for find libs, just remove them
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/embree-config.cmake)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/embree-config-version.cmake)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/embree-config.cmake)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/embree-config-version.cmake)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/embree-config.cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/embree-config-version.cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/embree-config.cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/embree-config-version.cmake")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/models)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/models)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/models")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/models")
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/embree2)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/doc ${CURRENT_PACKAGES_DIR}/share/embree2/doc)
+if("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/embree2")
+file(RENAME "${CURRENT_PACKAGES_DIR}/share/doc" "${CURRENT_PACKAGES_DIR}/share/embree2/doc")
 
 # Handle copyright
-file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/embree2)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/embree2/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/embree2/copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
