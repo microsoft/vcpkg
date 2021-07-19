bcd13951721fb074fdbc7962c1a137316b3ef1eb
Matthias Kuhn <matthias@opengis.ch>
2021-07-20 01:42:44 +0200

    [exiv2] Make add nls feature (#18964)

--- ports/exiv2/vcpkg-cmake-wrapper.cmake
+++ ports/exiv2/vcpkg-cmake-wrapper.cmake
@@ -2,11 +2,15 @@ _find_package(${ARGS})
 
 if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
     find_package(Iconv REQUIRED)
-    find_package(Intl REQUIRED)
+    if(@EXIV2_ENABLE_NLS@)
+        find_package(Intl REQUIRED)
+    endif()
     if(TARGET exiv2lib)
         set_property(TARGET exiv2lib APPEND PROPERTY INTERFACE_LINK_LIBRARIES 
             Iconv::Iconv 
             )
-        target_link_libraries(exiv2lib INTERFACE ${Intl_LIBRARIES})
+        if(@EXIV2_ENABLE_NLS@)
+            target_link_libraries(exiv2lib INTERFACE ${Intl_LIBRARIES})
+        endif()
     endif()
 endif()
