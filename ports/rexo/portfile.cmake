8d7055cd53d7a11a67f768e80243f787bfe058b0
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-07-20 23:05:59 +1000

    Update ports/rexo/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v0.2.0"
     SHA512 "f22ca5be1b5d1a5159377609d2b666eaa69643281294202d2c37b8ab5c9d28acd5b67c2a3d5a0c4e6bae487b9ceff00a11dae1847e7a1e5f72193d9ad34b1457"
     HEAD_REF "main"
+    PATCHES
+        fix-include-path.patch
 )
 
 vcpkg_cmake_configure(
