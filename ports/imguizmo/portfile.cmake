d535c79b2bf5fb1a111e7527666032ada786ee21
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2021-07-20 17:18:28 +0100

    [imguizmo] update to use v1.83 tag

--- ports/imguizmo/portfile.cmake
+++ ports/imguizmo/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CedricGuillemet/ImGuizmo
-    REF 7daa4b24ffe08b906040079bf297e8b920f86f47
+    REF v1.83
     SHA512 dd4d5ee47753f7f70519f94dd63ab19badc962d02f05e847bc1b9fc9798a5fa617daf9d801dcc9856ffbb989c3070defe71f6d3706eefa72c165327a0d577d22
     HEAD_REF master
 )
