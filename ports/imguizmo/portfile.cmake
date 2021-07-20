37c33cd2d61ab1cf03cefbe23d1566440eb3f121
Jonathan Higgs <jonathan.higgs.11@mail.wbs.ac.uk>
2021-07-20 10:21:31 +0100

    [imguizmo] apply suggestions from code review
    Co-authored-by: Phoebe <20694052+PhoebeHui@users.noreply.github.com>

--- ports/imguizmo/portfile.cmake
+++ ports/imguizmo/portfile.cmake
@@ -10,11 +10,8 @@ vcpkg_from_github(
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-
     OPTIONS_DEBUG
         -DIMGUIZMO_SKIP_HEADERS=ON
 )
@@ -24,4 +21,4 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
