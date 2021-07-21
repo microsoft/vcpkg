b19670ba73f7092f550d2654d216c9db63bbe546
Kai Pastor <dg0yt@darc.de>
2021-07-21 19:04:22 +0200

    Don't use absl::string_view

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -6,7 +6,8 @@ set(GDAL_PATCHES
     0004-Fix-cfitsio.patch
     0005-Fix-configure.patch
     0007-Control-tools.patch
-    )
+    0008-Fix-absl-string_view.patch
+)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     list(APPEND GDAL_PATCHES 0006-Fix-mingw-dllexport.patch)
 endif()
