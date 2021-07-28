bcfc5b0025d2ea4a7ec8b1432a8eb309b79c59d5
Dawid Wróbel <me@dawidwrobel.com>
2021-07-28 12:04:13 +0200

    [kf5package] update to 5.84.0

--- ports/kf5package/portfile.cmake
+++ ports/kf5package/portfile.cmake
@@ -1,24 +1,22 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/kpackage
-    REF v5.75.0
-    SHA512 8498fbd6bb2ed2fc37feab2e248f9d56800dafda27d993e6eefc66e383e74f58aa237c8813f92f03f5aec0afe03c4e0ca76d1dc71e9f0c9d69e0a08627b5ecd2
+    REF v5.84.0
+    SHA512 ad6a7083d461b2e782c612a544a8f6c0493c68c98a915e3f8fd4fd7de4ce636002383869476f877ace407fdc5013ad58e297ed3fd46dcac055ce79251704bf95
     HEAD_REF master
     PATCHES
-        "fix_libiconv.patch"
+        fix_libiconv.patch
 )
-
-vcpkg_find_acquire_program(GETTEXT_MSGMERGE)
-get_filename_component(GETTEXT_MSGMERGE_EXE_PATH ${GETTEXT_MSGMERGE} DIRECTORY)
-vcpkg_add_to_path(${GETTEXT_MSGMERGE_EXE_PATH})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DBUILD_HTML_DOCS=OFF
-            -DBUILD_MAN_DOCS=OFF
-            -DBUILD_QTHELP_DOCS=OFF
-            -DBUILD_TESTING=OFF
+    OPTIONS 
+        -DBUILD_TESTING=OFF
+    MAYBE_UNUSED_VARIABLES
+        -DBUILD_HTML_DOCS=OFF
+        -DBUILD_MAN_DOCS=OFF
+        -DBUILD_QTHELP_DOCS=OFF
 )
 
 vcpkg_install_cmake()
@@ -33,7 +31,6 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
