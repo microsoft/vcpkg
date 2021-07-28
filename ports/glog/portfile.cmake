5ab5bda897eb3844667f0f1c343a8c5e9dcb8a19
Jonliu1993 <13720414433@163.com>
2021-07-28 14:00:15 +0800

    add patch

--- ports/glog/portfile.cmake
+++ ports/glog/portfile.cmake
@@ -4,22 +4,23 @@ vcpkg_from_github(
     REF 8f9ccfe770add9e4c64e9b25c102658e3c763b73 #v0.5.0
     SHA512 93dab1041ef4e3eb758c76325531ae32208dcf92c296c8c3404cfa249b3849cc24d5423e087478b17f79ccbae9dbcabe049e509d47c4ec6d7135ca8ca80dada5
     HEAD_REF master
+    PATCHES
+       glog_disable_debug_postfix.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/glog)
-vcpkg_fixup_pkgconfig()
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glog)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
