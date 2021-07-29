bf22df199efa83566f593e185e3a3cb4c1a467ae
Jonliu1993 <13720414433@163.com>
2021-07-29 18:05:33 +0800

    [pe-parse] update to 1.3.0

--- ports/pe-parse/portfile.cmake
+++ ports/pe-parse/portfile.cmake
@@ -1,27 +1,22 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO trailofbits/pe-parse
-    REF ff4f8449bf9a04710507b75e6dcef6f156f2ec4c   #v1.3.0
-    SHA512 4687e0ffdb84537fa9a2a78a4584d0f8e7cd39ea797d7bbdff2aa40e80e1f87f4230fba0e30c662062a98db6d70f08c524f0203aa86d00bc02088d432b7eacc5
+    REF v1.3.0
+    SHA512 b723e90821e0ac67b4d6b15e5a46cf13544f21380a4d2add013eedcaa309e1be2cff6789247397c6fb4a938e4a240a835cbe21c8221bd558a4fccf8e93ce1548
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS -DBUILD_COMMAND_LINE_TOOLS=OFF
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS 
+        -DBUILD_COMMAND_LINE_TOOLS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/pe-parse TARGET_PATH share/pe-parse)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/pe-parse)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(
-    INSTALL
-    "${SOURCE_PATH}/LICENSE"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/pe-parse"
-    RENAME copyright
-)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
