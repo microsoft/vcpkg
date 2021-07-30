87a043e00e24794feb812632f0793c5392adc513
Jonliu1993 <13720414433@163.com>
2021-07-30 14:28:06 +0800

    [fmt] update to 8.0.1

--- ports/fmt/portfile.cmake
+++ ports/fmt/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO fmtlib/fmt
-    REF 9e8b86fd2d9806672cc73133d21780dd182bfd24 # v8.0.0
-    SHA512 1acf9936a559839b5605452cc8ae26632614a5936e6504d2f8f23cfa0715f39238d0f8b664e3a4962a2df97191cba15330af4f6ee358018800feb07b50e56f18
+    REF d141cdbeb0fb422a3fb7173b285fd38e0d1772dc # v8.0.1
+    SHA512 1171f0305bed07497d2957b4053e8802030f448921c5bc1b066345dc1e77fbdb96d2c7f323452a32278ae4adb01e69e80b816119b0821e37d50980800e7406f8
     HEAD_REF master
     PATCHES
         fix-write-batch.patch
@@ -22,24 +22,24 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     if(VCPKG_TARGET_IS_WINDOWS)
         if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
             if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/lib/fmtd.dll")
-                file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/bin)
+                file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin")
                 file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/fmtd.dll ${CURRENT_PACKAGES_DIR}/debug/bin/fmtd.dll)
             endif()
         endif()
         if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
             if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/fmt.dll")
-                file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/bin)
+                file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin")
                 file(RENAME ${CURRENT_PACKAGES_DIR}/lib/fmt.dll ${CURRENT_PACKAGES_DIR}/bin/fmt.dll)
             endif()
         endif()
     endif()
 
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/fmt/core.h"
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/fmt/core.h
         "defined(FMT_SHARED)"
         "1"
     )
 endif()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
@@ -52,7 +52,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
         )
     endif()
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/fmt/fmt-targets-release.cmake"
+        vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/share/fmt/fmt-targets-release.cmake
             "lib/fmt.dll"
             "bin/fmt.dll"
         )
