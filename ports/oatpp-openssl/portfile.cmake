cc7c12d1d6c162b443a183240451769e10faa732
Michael Heyman <mheyman@gmail.com>
2021-08-02 14:36:12 -0400

    [oatpp-version] has paramters with arguments surrounded by quotes

--- ports/oatpp-openssl/portfile.cmake
+++ ports/oatpp-openssl/portfile.cmake
@@ -13,15 +13,14 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS
         "-DOATPP_BUILD_TESTS:BOOL=OFF"
         "-DCMAKE_CXX_FLAGS=-D_CRT_SECURE_NO_WARNINGS"
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/oatpp-openssl-${OATPP_VERSION})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/oatpp-openssl-${OATPP_VERSION}")
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
