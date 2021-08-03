fb5def55f1e5b24a7910415021056947c4775c49
Kuntal Majumder <hellozee@disroot.org>
2021-08-03 20:19:14 +0530

    use copy_tools and add quotes

--- ports/libsnoretoast/portfile.cmake
+++ ports/libsnoretoast/portfile.cmake
@@ -6,24 +6,24 @@ vcpkg_from_github(
     REF v0.8.0
     SHA512 233751b6cc3f8099c742e4412a3c9ba8707a2f3c69b57bab93dd83b028aa0c0656cade8de1ece563843ace576fd0d8e5f3a29c254a07ed939d0a69cd2d4f6c2a
     HEAD_REF master
-    PATCHES
-        install_location.patch
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS 
         -DBUILD_EXAMPLES=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libsnoretoast)
-vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/libsnoretoast")
+vcpkg_copy_tools(
+    TOOL_NAMES "snoretoast"
+    SEARCH_DIR "${CURRENT_PACKAGES_DIR}/bin"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
+    AUTO_CLEAN
+)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(INSTALL "${SOURCE_PATH}/COPYING.LGPL-3" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/etc")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+file(INSTALL "${SOURCE_PATH}/COPYING.LGPL-3" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
