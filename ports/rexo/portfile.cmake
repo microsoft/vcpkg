7e3d4844bc02efac08377f5ad65a405f133830cb
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-08-03 18:28:44 +1000

    [rexo] Upgrade to 0.2.1; use new vcpkg cmake function names

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -1,20 +1,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "christophercrouzet/rexo"
-    REF "v0.2.0"
-    SHA512 "f22ca5be1b5d1a5159377609d2b666eaa69643281294202d2c37b8ab5c9d28acd5b67c2a3d5a0c4e6bae487b9ceff00a11dae1847e7a1e5f72193d9ad34b1457"
+    REF "v0.2.1"
+    SHA512 "746072d691adade7fa69a7ee35becd812164a4698048b06824ca801c175801f78dda3c4b986d68c4ff060636603c353f90ee10eb58e8ea53ac5467c87b65e593"
     HEAD_REF "main"
-    PATCHES
-        fix-include-path.patch
 )
 
-vcpkg_cmake_configure(
+vcpkg_configure_cmake(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DREXO_BUILD_TESTS=OFF
 )
 
-vcpkg_cmake_install()
+vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Rexo)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
