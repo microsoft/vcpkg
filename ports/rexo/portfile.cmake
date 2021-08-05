b77b9aaa10008cff81c420df7ab5ed2750900473
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-08-05 11:54:57 +1000

    [rexo] Upgrade to 0.2.2

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "christophercrouzet/rexo"
-    REF "v0.2.1"
-    SHA512 "746072d691adade7fa69a7ee35becd812164a4698048b06824ca801c175801f78dda3c4b986d68c4ff060636603c353f90ee10eb58e8ea53ac5467c87b65e593"
+    REF "v0.2.2"
+    SHA512 "0"
     HEAD_REF "main"
 )
 
