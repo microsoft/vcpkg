b1daed24040553bce6151b12ec7021069cd2babe
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-08-04 21:21:00 -0700

    apply cmake guidelines for quoting arguments

--- ports/pybind11/portfile.cmake
+++ ports/pybind11/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO pybind/pybind11
-    REF v2.7.1
+    REPO "pybind/pybind11"
+    REF "v2.7.1"
     SHA512 f09f46622b394d3990ab82aa7ea15a06e298df109cd2df263ba9d6ac7fb248217df7450e1954a9679a8360335d5bbf662926a34c8b7c61b6e4c396bbdfd88305
     HEAD_REF master
 )
@@ -9,21 +9,21 @@ vcpkg_from_github(
 vcpkg_find_acquire_program(PYTHON3)
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DPYBIND11_TEST=OFF
-        -DPYBIND11_FINDPYTHON=ON
-        -DPython3_EXECUTABLE=${PYTHON3}
+        "-DPYBIND11_TEST=OFF"
+        "-DPYBIND11_FINDPYTHON=ON"
+        "-DPython3_EXECUTABLE=${PYTHON3}"
     OPTIONS_RELEASE
-        -DPYTHON_IS_DEBUG=OFF
+        "-DPYTHON_IS_DEBUG=OFF"
     OPTIONS_DEBUG
-        -DPYTHON_IS_DEBUG=ON
+        "-DPYTHON_IS_DEBUG=ON"
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/pybind11)
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/pybind11")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/")
 
 # copy license
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
