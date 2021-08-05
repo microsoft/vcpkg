e20cf7db81bc0e4670453297ce4e8e11457ceab4
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-08-05 14:04:17 +1000

    [rexo] Upgrade to 0.2.2 SHA hash

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "christophercrouzet/rexo"
     REF "v0.2.2"
-    SHA512 "0"
+    SHA512 "c7b093920bb23d1b8ecb905c8d3eb281e46607890c071c079df4c194215fc007d672ce3524848a1f0376188869f51fd9955e3fe027c10f3d286a003adfd78d09"
     HEAD_REF "main"
 )
 
