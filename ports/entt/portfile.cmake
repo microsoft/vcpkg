2d1de59d219b0b0b4024fb6cf8433da3f985953c
David <skaarj1989@gmail.com>
2021-08-06 11:30:43 +0200

    update to 3.8.1

--- ports/entt/portfile.cmake
+++ ports/entt/portfile.cmake
@@ -8,8 +8,8 @@ else()
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO skypjack/entt
-        REF 2d5a3f24aa6dc38ed0c4eee9d4d625586d8e921e #v3.8.0
-        SHA512 36b7be2550be5a919548e5c91db218ed9b062804d4f43073d6fc6b432fa2d358b5d36098490359ca2392370b2bf1b99d20e564504eaac619f5cd7f9612d91e2e
+        REF dd6863f71da1b360ec09c25912617a3423f08149 #v3.8.1
+        SHA512 4ed0e18f7b7d4934a9573fdd4749898273a53847ccc8c0c9b1bffa97f33a8c8a5be6a96b52620e6b83153d5fb6beeff64ff8fea4d96eb6def335f12dbec3a9d6
         HEAD_REF master
     )
 endif()
