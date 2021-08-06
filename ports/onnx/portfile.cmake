d1d8dcdc0367ba84063d6d3956d886234eccee0d
luncliff <luncliff@gmail.com>
2021-08-07 03:02:03 +0900

    [onnx] rewrite portfile, patch
    * add host dependencies for protoc/python3
    * rewrite patch files to work with FindPython3

--- ports/onnx/portfile.cmake
+++ ports/onnx/portfile.cmake
@@ -1,5 +1,6 @@
 # uwp: LOAD_LIBRARY_SEARCH_DEFAULT_DIRS undefined identifier
 vcpkg_fail_port_install(ON_TARGET "uwp")
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -8,7 +9,6 @@ vcpkg_from_github(
     SHA512 a3eecc74ce4f22524603fb86367d21c87a143ba27eef93ef4bd2e2868c2cadeb724b84df58a429286e7824adebdeba7fa059095b7ab29df8dcea8777bd7f4101
     PATCHES
         fix-cmakelists.patch
-        fix-pybind11.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -16,27 +16,27 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         pybind11 BUILD_ONNX_PYTHON
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" USE_PROTOBUF_SHARED)
-list(APPEND FEATURE_OPTIONS 
-    -DONNX_USE_PROTOBUF_SHARED_LIBS=${USE_PROTOBUF_SHARED}
-)
 if(VCPKG_TARGET_IS_WINDOWS)
     string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
-    list(APPEND FEATURE_OPTIONS 
+    list(APPEND PLATFORM_OPTIONS
         -DONNX_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
     )
 endif()
 
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-vcpkg_find_acquire_program(PYTHON3)
+# ONNX_USE_PROTOBUF_SHARED_LIBS: find the library and check its suffix
+find_library(PROTOBUF_LIBPATH NAMES protobuf PATHS ${CURRENT_INSTALLED_DIR}/bin ${CURRENT_INSTALLED_DIR}/lib REQUIRED)
+get_filename_component(PROTOBUF_LIBPATH ${PROTOBUF_LIBPATH} NAME)
+string(COMPARE EQUAL "${PROTOBUF_LIBPATH}" "${CMAKE_SHARED_LIBRARY_SUFFIX}" USE_PROTOBUF_SHARED)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DPython_EXECUTABLE=${PYTHON3} # for Windows
-        -DPYTHON_EXECUTABLE=${PYTHON3} # for configure_file of protoc-gen-mypy.py
+        -DPython3_ROOT_DIR=${CURRENT_INSTALLED_DIR}
+        -DPY_VERSION=3.9 # see version of the 'python3' package
+        -DONNX_ML=ON
         -DONNX_GEN_PB_TYPE_STUBS=ON
+        -DONNX_USE_PROTOBUF_SHARED_LIBS=${USE_PROTOBUF_SHARED}
         -DONNX_USE_LITE_PROTO=OFF
         -DONNX_BUILD_TESTS=OFF
         -DONNX_BUILD_BENCHMARKS=OFF
@@ -51,50 +51,30 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ONNX)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-# install protobuf files together
-get_filename_component(CODEGEN_DIR ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/onnx ABSOLUTE)
-file(INSTALL ${CODEGEN_DIR}/onnx-data.proto
-             ${CODEGEN_DIR}/onnx-data.proto3
-             ${CODEGEN_DIR}/onnx-ml.proto
-             ${CODEGEN_DIR}/onnx-ml.proto3
-             ${CODEGEN_DIR}/onnx-operators-ml.proto
-             ${CODEGEN_DIR}/onnx-operators-ml.proto3
-     DESTINATION ${CURRENT_PACKAGES_DIR}/include/onnx
-)
-if("pybind11" IN_LIST FEATURES)
-    file(INSTALL ${CODEGEN_DIR}/onnx_data_pb.py
-                 ${CODEGEN_DIR}/onnx_data_pb2.py
-                 ${CODEGEN_DIR}/onnx_ml_pb2.py
-                 ${CODEGEN_DIR}/onnx_operators_ml_pb2.py
-                 ${CODEGEN_DIR}/onnx_operators_pb.py
-                 ${CODEGEN_DIR}/onnx_pb.py
-         DESTINATION ${CURRENT_PACKAGES_DIR}/include/onnx
-    )
-endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
-                    ${CURRENT_PACKAGES_DIR}/debug/share
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_ml
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_data
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_operators_ml
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/onnx_cpp2py_export
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share"
                     # the others are empty
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/backend
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/tools
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/test
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/bin
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/examples
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/frontend
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/training
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/math
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/quantization
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/generator
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/reduction
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/logical
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/object_detection
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/traditionalml
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/sequence
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/controlflow
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/rnn
-                    ${CURRENT_PACKAGES_DIR}/include/onnx/defs/nn
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_ml"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_data"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_operators_ml"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/onnx_cpp2py_export"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/backend"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/tools"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/test"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/bin"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/examples"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/frontend"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/controlflow"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/generator"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/logical"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/math"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/nn"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/object_detection"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/quantization"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/reduction"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/rnn"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/sequence"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/traditionalml"
+                    "${CURRENT_PACKAGES_DIR}/include/onnx/defs/training"
 )
