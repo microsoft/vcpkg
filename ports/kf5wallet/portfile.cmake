c57679ae0b1910595778cf1afd41686c22481f2e
Dawid Wróbel <me@dawidwrobel.com>
2021-08-08 23:44:07 +0000

    [kf5wallet] update deprecated functions

--- ports/kf5wallet/portfile.cmake
+++ ports/kf5wallet/portfile.cmake
@@ -6,17 +6,16 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS 
         -DBUILD_TESTING=OFF
         -DBUILD_KWALLETD=OFF
         -DBUILD_KWALLET_QUERY=OFF
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5Wallet)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Wallet)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
