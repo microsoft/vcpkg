33279aa106664a74ba534ed3e5853a6d0c14d44c
Dawid Wróbel <me@dawidwrobel.com>
2021-08-08 23:34:45 +0000

    [kf5wallet] only remove what's required

--- ports/kf5wallet/portfile.cmake
+++ ports/kf5wallet/portfile.cmake
@@ -1,29 +1,25 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/kwallet
-    REF v5.75.0
-    SHA512 2284e9dec15aee897592f3b0c67d86130d6ce486552d16f8461fbd80bb9828ce2f48142b246dbda80b2e18cd43fe5599f72a64b1afb37efb2b78faf19ef4cc3a
+    REF v5.84.0
+    SHA512 5bea4c7f46d127de1bc530e03e1033e9e133197cc36bb6963c9154783ef7bb9920bec62443b5bba9b55d3adcc1f7284ce4376422915d93d6df387587fd7ab8e0
     HEAD_REF master
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DBUILD_HTML_DOCS=OFF
-            -DBUILD_MAN_DOCS=OFF
-            -DBUILD_QTHELP_DOCS=OFF
-            -DBUILD_TESTING=OFF
-            -DBUILD_KWALLETD=OFF
-            -DBUILD_KWALLET_QUERY=OFF
+    OPTIONS 
+        -DBUILD_TESTING=OFF
+        -DBUILD_KWALLETD=OFF
+        -DBUILD_KWALLET_QUERY=OFF
 )
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5Wallet)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
