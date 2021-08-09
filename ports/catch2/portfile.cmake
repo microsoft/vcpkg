6fa09d27c20cfb62331ae948b8b27cdceac697bf
NancyLi1013 <lirui09@beyondsoft.com>
2021-08-08 23:31:14 -0700

    Fix install path for lib and pkgconfig

--- ports/catch2/portfile.cmake
+++ ports/catch2/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF 5c88067bd339465513af4aec606bd2292f1b594a # v2.13.6
     SHA512 62ab120ef9cbbcf7320a96654bda60c766dbbcc0d9cbb2b0b36dd04e828315b627caf51e390dcea915efa266655fe0f28058b972c0d6e0e3e457c565d26e1fd3
     HEAD_REF master
+    PATCHES fix-install-path.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC) 
@@ -25,12 +26,10 @@ file(RENAME "${CURRENT_PACKAGES_DIR}/debug/share/catch2_" "${CURRENT_PACKAGES_DI
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Catch2)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
-else()
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-endif()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/include/catch2/catch.hpp")
     message(FATAL_ERROR "Main includes have moved. Please update the forwarder.")
