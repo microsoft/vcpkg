3d662e8b91c273488dbe6f8ad083a294c52a3c0d
GelilaSeifu <gelila.seifu@intel.com>
2021-08-09 09:32:55 -0700

    update HEXL v1.2

--- ports/intel-hexl/portfile.cmake
+++ ports/intel-hexl/portfile.cmake
@@ -6,11 +6,10 @@ vcpkg_from_github(
     REPO intel/hexl
     REF 2dc1db6824be3fc89d13740efd0270ec9afec77e
     SHA512 aaa80dc53a21586d763a2b84b40d60062020470988422601bc5e9c2b31c6263847a97ea8f956d002a95e2d5e843cafa96fabdfd8b8ee892c7a7b9747133adebb
-    HEAD_REF main
-    PATCHES
-        disable_downloading_cpu_features.patch
-        find_dependency_cpu_features.patch
+    HEAD_REF 1.2.0
 )
+
+set(VCPKG_BUILD_TYPE release)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     set(HEXL_SHARED OFF)
@@ -22,17 +21,14 @@ vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
-        "-DHEXL_DEBUG=OFF"
         "-DHEXL_BENCHMARK=OFF"
-        "-DHEXL_EXPORT=ON"
         "-DHEXL_COVERAGE=OFF"
         "-DHEXL_TESTING=OFF"
         "-DHEXL_SHARED_LIB=${HEXL_SHARED}"
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME "HEXL" CONFIG_PATH "lib/cmake")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "HEXL" CONFIG_PATH "lib/cmake/hexl-1.2.0")
 
 vcpkg_fixup_pkgconfig()
 
