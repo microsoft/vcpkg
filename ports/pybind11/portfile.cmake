7210b67559de8c1cb15b88b15097de423c9d9c92
Ryan <ryan__mckenzie@hotmail.com>
2021-08-10 01:00:44 -0700

    [pybind11] Update to 2.7.1 (#19375)
    * update to 2.7.1
    
    * use `semver` version field
    
    * fixup deprecated commands
    
    * apply cmake guidelines for quoting arguments
    
    * set `Python_EXECUTABLE` instead
    
    pybind11 uses the `FindPython` module instead of `FindPython3`
    
    * update baseline
    
    * skip passing python executable on command line
    
    * update baseline
    
    * remove extraneous quotes
    
    * remove unused function call
    
    * update baseline

--- ports/pybind11/portfile.cmake
+++ ports/pybind11/portfile.cmake
@@ -1,30 +1,26 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pybind/pybind11
-    REF 8de7772cc72daca8e947b79b83fea46214931604 # v2.6.2
-    SHA512 9bb688209791bd5f294fa316ab9a8007f559673a733b796e76e223fe8653d048d3f01eb045b78aa1843f7eacf97f6e2ee090ac68fed2b43856eb0c4813583204
+    REF v2.7.1
+    SHA512 f09f46622b394d3990ab82aa7ea15a06e298df109cd2df263ba9d6ac7fb248217df7450e1954a9679a8360335d5bbf662926a34c8b7c61b6e4c396bbdfd88305
     HEAD_REF master
 )
 
-vcpkg_find_acquire_program(PYTHON3)
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DPYBIND11_TEST=OFF
         -DPYBIND11_FINDPYTHON=ON
-        -DPython3_EXECUTABLE=${PYTHON3}
     OPTIONS_RELEASE
         -DPYTHON_IS_DEBUG=OFF
     OPTIONS_DEBUG
         -DPYTHON_IS_DEBUG=ON
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/pybind11)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/pybind11")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/")
 
 # copy license
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
