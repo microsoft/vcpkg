83026aa48f5aafcaf3c14f264a61fe70b2fd2572
9cvele3 <9cvele3@gmail.com>
2021-08-10 14:34:29 +0200

    Update ports/b64/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/b64/portfile.cmake
+++ ports/b64/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     PATCHES "windows-fix.patch"
 )
 
-file(COPY ports/b64/CMakeLists.txt DESTINATION ${SOURCE_PATH}/)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
