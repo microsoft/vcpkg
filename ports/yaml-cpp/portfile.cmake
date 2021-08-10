51a4174faf0a71800ed899d5e1cdb48669181053
autoantwort <41973254+autoantwort@users.noreply.github.com>
2021-08-10 10:11:41 +0200

    [yaml-cpp] update to 0.7.0 (#19272)
    * [yaml-cpp] update to 0.7.0
    
    * [opencolorio] update to 2.0.1
    
    * [openimageio] Update to 2.2.17.0
    
    * [pystring] move header to pystring subfolder
    
    * add version files

--- ports/yaml-cpp/portfile.cmake
+++ ports/yaml-cpp/portfile.cmake
@@ -1,11 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jbeder/yaml-cpp
-    REF 9a3624205e8774953ef18f57067b3426c1c5ada6 #v0.6.3
-    SHA512 9bd0f05b882beed748eddb5d615bf356b7d1f31c4e3a4bbf80a6bdeb30b33fa1e0ccf596161a489169e6a111a3112e371d8d00514a0bfd02e6a6a11513904bed
+    REF 0579ae3d976091d7d664aa9d2527e0d0cff25763 # yaml-cpp-0.7.0
+    SHA512 930f13737c23faf06be3fa9821492d6c677359e532212ced495173367a8aec45f87fbf3a5da47d4d1b61a95c25e0101bc7f8d175752434c63b25e097186e1745
     HEAD_REF master
-    PATCHES
-        fix-include-path.patch
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
@@ -14,34 +12,34 @@ else()
     set(YAML_BUILD_SHARED_LIBS OFF)
 endif()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DYAML_CPP_BUILD_TOOLS=OFF
         -DYAML_CPP_BUILD_TESTS=OFF
         -DYAML_BUILD_SHARED_LIBS=${YAML_BUILD_SHARED_LIBS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-if(EXISTS ${CURRENT_PACKAGES_DIR}/CMake)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH CMake)
+if(EXISTS "${CURRENT_PACKAGES_DIR}/cmake")
+    vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
 endif()
-if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/cmake/yaml-cpp)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/yaml-cpp)
+if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/yaml-cpp")
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/yaml-cpp)
 endif()
 
 # Remove debug include files
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(READ ${CURRENT_PACKAGES_DIR}/include/yaml-cpp/dll.h DLL_H)
+file(READ "${CURRENT_PACKAGES_DIR}/include/yaml-cpp/dll.h" DLL_H)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic" AND VCPKG_TARGET_IS_WINDOWS)
     string(REPLACE "#ifdef YAML_CPP_DLL" "#if 1" DLL_H "${DLL_H}")
 else()
     string(REPLACE "#ifdef YAML_CPP_DLL" "#if 0" DLL_H "${DLL_H}")
 endif()
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/yaml-cpp/dll.h "${DLL_H}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/include/yaml-cpp/dll.h" "${DLL_H}")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
