23d2aae1d93a1ef27af472df4a060cfdc30fed98
Guekka <39066502+Guekka@users.noreply.github.com>
2021-08-13 09:26:12 +0000

    Apply suggestions from code review
    Co-authored-by: Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>

--- ports/bsa/portfile.cmake
+++ ports/bsa/portfile.cmake
@@ -1,3 +1,5 @@
+vcpkg_fail_port_install(ON_TARGET "Linux" "OSX" ON_ARCH "x86")
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ryan-rsm-McKenzie/bsa
