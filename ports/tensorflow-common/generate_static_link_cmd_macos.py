bc65d1237b816948e442b87f58b7313cd86aa324
jgehw <kontakt@joachim-gehweiler.de>
2021-08-16 13:59:52 +0200

    fix regex for macOS linker command

--- ports/tensorflow-common/generate_static_link_cmd_macos.py
+++ ports/tensorflow-common/generate_static_link_cmd_macos.py
@@ -4,8 +4,8 @@ import sys
 lib_suffix = "" if len(sys.argv) < 3 else sys.argv[2]
 with open(sys.argv[1], "r") as f_in:
     with open("static_link.sh", "w") as f_out:
-        p_cd = re.compile("^\\((cd .*) && \\\\$")
-        p_linker = re.compile(fr"^\s*(.+)libtool.+(@bazel-out\S+libtensorflow{lib_suffix}\.\d\.\d\.\d\.dylib-2\.params).*")
+        p_cd = re.compile(r"^\((cd .*) && \\$")
+        p_linker = re.compile(fr"^\s*.+cc_wrapper.sh.+(@bazel-out\S+libtensorflow{lib_suffix}\.\d\.\d\.\d\.dylib-2\.params).*")
         f_out.write("#!/bin/bash\n# note: ar/binutils version 2.27 required to support output files > 4GB\n")
         env = []
         for line in f_in:
@@ -17,7 +17,7 @@ with open(sys.argv[1], "r") as f_in:
                 if m1:
                     m2 = p_cd.match(env[0])
                     f_out.write(m2.group(1) + "\n")
-                    line = f'"{m1.group(1)}libtool" -static -o {m1.group(2)[1:-9].replace(".dylib", ".a")} {m1.group(2).replace(".dylib", ".a")}\n'
+                    line = f'"/usr/bin/libtool" -static -o {m1.group(1)[1:-9].replace(".dylib", ".a")} {m1.group(1).replace(".dylib", ".a")}\n'
                     f_out.write(line)
                 else:
                     env.append(line)
