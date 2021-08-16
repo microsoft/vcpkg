cab35af9d9e89079189ad3cc83d46fefc4b45f14
nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>
2021-08-16 13:42:31 -0700

    [rollup:2021-08-09] Rollup PR (#19469)
    * [rollup:2021-08-09] PR #16706 (@JackBoosY)
    
    [vcpkg_fixup_cmake_targets] Fix up OSX system development path
    
    * [rollup:2021-08-09] PR #19238 (@strega-nil)
    
    [scripts-audit] vcpkg_download_distfile
    
    * [rollup:2021-08-09] PR #19239 (@strega-nil)
    
    [scripts-audit] vcpkg_find_fortran
    
    * [rollup:2021-08-09] PR #19338 (@strega-nil)
    
    [tinyfiledialogs] Fix for good
    
    * [rollup:2021-08-09] PR #19348 (@strega-nil)
    
    [scripts-audit] vcpkg_fixup_pkgconfig
    
    * fix ports.cmake with newer vcpkg_download_distfile
    
    * fix vcpkg create
    
    * move vcpkg_common_definitions down so that it's not incorrect
    
    * fix vcpkg_internal_get_cmake_vars
    
    Co-authored-by: nicole mazzuca <mazzucan@outlook.com>
    Co-authored-by: Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/tinyfiledialogs/portfile.cmake
+++ ports/tinyfiledialogs/portfile.cmake
@@ -2,11 +2,12 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_sourceforge(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO tinyfiledialogs
-    SHA512 d63d6dd847d6bbd1f252c8fbd228086381af7189ac71afc97bd57e06badd1d3f4d90c6aab6207191ae7253d5a71fc44636cf8e33714089d5b478dd2714f59376
-    FILENAME "tinyfiledialogs-current.zip"
+    FETCH_REF master
+    HEAD_REF master
+    URL https://git.code.sf.net/p/tinyfiledialogs/code
+    REF e11f94cd7887b101d64f74892d769f0139b5e166
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
@@ -26,6 +27,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(READ "${CURRENT_PACKAGES_DIR}/include/tinyfiledialogs/tinyfiledialogs.h" _contents)
 # reads between the line "- License -" and a closing "*/"
 if (NOT _contents MATCHES [[- License -(([^*]|\*[^/])*)\*/]])
-	message(FATAL_ERROR "Failed to parse license from tinyfiledialogs.h")
+    message(FATAL_ERROR "Failed to parse license from tinyfiledialogs.h")
 endif()
 file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "${CMAKE_MATCH_1}")
