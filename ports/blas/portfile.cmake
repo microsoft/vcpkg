11d5f6dcfae146e74aa3514a7166b1f85dfdfc41
JackBoosY <yuzaiyang@beyondsoft.com>
2021-08-17 00:23:33 -0700

    [blas] Re-add port blas

--- ports/blas/portfile.cmake
+++ ports/blas/portfile.cmake
@@ -1,5 +1,48 @@
-SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+SET(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
-# Make sure BLAS can be found
-vcpkg_configure_cmake(SOURCE_PATH ${CURRENT_PORT_DIR}
-                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}")
+vcpkg_download_distfile(ARCHIVE
+  URLS "http://www.netlib.org/blas/blas-3.10.0.tgz"
+  FILENAME "blas-3.10.0.tgz"
+  SHA512 1f243ce4f7e0974e62c03c49da2741509662e20e82d0ebb73e10a940cff6458739b9dc238125d5e1ae7c179eaba06155bb502327bd58eaf246c29a16e554eeb0
+)
+
+vcpkg_extract_source_archive_ex(
+  OUT_SOURCE_PATH SOURCE_PATH
+  ARCHIVE ${ARCHIVE}
+  PATCHES
+)
+
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        single      BUILD_SINGLE
+        double      BUILD_DOUBLE
+        complex     BUILD_COMPLEX
+        complex16   BUILD_COMPLEX16
+)
+
+if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(BUILD_X64 ON)
+else()
+    set(BUILD_X64 OFF)
+endif()
+
+include(vcpkg_find_fortran)
+vcpkg_find_fortran(FORTRAN_CMAKE)
+
+vcpkg_configure_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS
+    ${FEATURE_OPTIONS}
+    ${FORTRAN_CMAKE}
+    -DBUILD_INDEX64=${BUILD_X64}
+    -DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_pkgconfig()
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
