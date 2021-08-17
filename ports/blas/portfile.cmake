1e19f464f2cc5ffc14a8afb26eedf9a971aa030a
JackBoosY <yuzaiyang@beyondsoft.com>
2021-08-17 00:33:08 -0700

    Add cmake usage

--- ports/blas/portfile.cmake
+++ ports/blas/portfile.cmake
@@ -45,4 +45,5 @@ vcpkg_copy_pdbs()
 
 vcpkg_fixup_pkgconfig()
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
