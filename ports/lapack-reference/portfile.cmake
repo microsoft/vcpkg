fa284badef3564637974de9fb5c6edb66cbbf51c
JackBoosY <yuzaiyang@beyondsoft.com>
2021-08-18 00:01:58 -0700

    Restore the changes about vcpkg-cmake-configure / vcpkg-cmake-install

--- ports/lapack-reference/portfile.cmake
+++ ports/lapack-reference/portfile.cmake
@@ -60,15 +60,18 @@ if(VCPKG_USE_INTERNAL_Fortran)
 else()
     set(USE_OPTIMIZED_BLAS ON)
 endif()
-vcpkg_cmake_configure(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            "-DUSE_OPTIMIZED_BLAS=${USE_OPTIMIZED_BLAS}"
-            "-DCBLAS=${CBLAS}"
-            ${FORTRAN_CMAKE}
-        )
 
-vcpkg_cmake_install()
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        "-DUSE_OPTIMIZED_BLAS=${USE_OPTIMIZED_BLAS}"
+        "-DCBLAS=${CBLAS}"
+        ${FORTRAN_CMAKE}
+)
+
+vcpkg_install_cmake()
+
 vcpkg_cmake_config_fixup(PACKAGE_NAME lapack-${lapack_ver} CONFIG_PATH lib/cmake/lapack-${lapack_ver}) #Should the target path be lapack and not lapack-reference?
 
 set(pcfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/lapack.pc")
