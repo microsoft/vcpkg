ebe763ff8a13cf4f6b952ad5d870b9a70b11edeb
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-08-18 10:42:58 +0200

    [skip actions] switch to version-date and revert changes to the portfile

--- ports/lapack/portfile.cmake
+++ ports/lapack/portfile.cmake
@@ -1,8 +1,5 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-#x_vcpkg_find_fortran(FORTRAN_CMAKE)
 # Make sure LAPACK can be found
 vcpkg_configure_cmake(SOURCE_PATH ${CURRENT_PORT_DIR}
-                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
-                              #--trace-expand
-                              )
+                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}")
