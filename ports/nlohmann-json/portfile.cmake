f507a60bf31723a2b23cbbe284b273f951e6e441
Mathis Logemann <mathisloge@gmail.com>
2021-08-22 11:41:37 +0200

    fix package naming

--- ports/nlohmann-json/portfile.cmake
+++ ports/nlohmann-json/portfile.cmake
@@ -15,18 +15,18 @@ vcpkg_configure_cmake(
         -DJSON_BuildTests=OFF
 )
 vcpkg_install_cmake()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/nlohmann_json")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "nlohmann_json" CONFIG_PATH "lib/cmake/nlohmann_json")
 vcpkg_fixup_pkgconfig()
 
 vcpkg_replace_string(
-    "${CURRENT_PACKAGES_DIR}/share/nlohmann-json/nlohmann_jsonTargets.cmake"
+    "${CURRENT_PACKAGES_DIR}/share/nlohmann_json/nlohmann_jsonTargets.cmake"
     "{_IMPORT_PREFIX}/nlohmann_json.natvis"
-    "{_IMPORT_PREFIX}/share/nlohmann-json/nlohmann_json.natvis"
+    "{_IMPORT_PREFIX}/share/nlohmann_json/nlohmann_json.natvis"
 )
 if(EXISTS ${CURRENT_PACKAGES_DIR}/nlohmann_json.natvis)
     file(RENAME
         "${CURRENT_PACKAGES_DIR}/nlohmann_json.natvis"
-        "${CURRENT_PACKAGES_DIR}/share/nlohmann-json/nlohmann_json.natvis"
+        "${CURRENT_PACKAGES_DIR}/share/nlohmann_json/nlohmann_json.natvis"
     )
 endif()
 
