0f7b801d98cc777c47b88b371d68a117a84801ae
mheyman <mheyman@localdomain>
2021-08-22 08:16:41 -0400

    [activemq-cpp] activemq-cppConfig.cmake works (I hope)

---
+++ ports/activemq-cpp/activemq-cppConfig.cmake
@@ -0,0 +1,147 @@
+set(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}")
+foreach(i RANGE 1 2)
+    get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+    if (_IMPORT_PREFIX STREQUAL "/")
+        set(_IMPORT_PREFIX "")
+        break()
+    endif()
+endforeach()
+
+
+#
+# Since this is a CMake config file for a non-CMake project, and one that is
+# for vcpkg to as well, the config file has to cover the various products of
+# the builds on the various platforms.
+#
+# Below, Windows and Linux are covered for static and shared libraries.
+#
+if (EXISTS "${_IMPORT_PREFIX}/bin/activemq-cpp.dll")
+    #
+    # Windows shared install
+    #
+    if (EXISTS "${_IMPORT_PREFIX}/lib/activemq-cpp.lib")
+        if (EXISTS "${_IMPORT_PREFIX}/debug/bin/activemq-cppd.dll")
+            if (EXISTS "${_IMPORT_PREFIX}/debug/lib/activemq-cppd.lib")
+                find_library(APR_LIB_RELEASE libapr-1.lib PATHS "${_IMPORT_PREFIX}/lib" NO_DEFAULT_PATH)
+                find_library(APR_DLL_RELEASE libapr-1.dll PATHS "${_IMPORT_PREFIX}/bin" NO_DEFAULT_PATH)
+                find_library(APR_LIB_DEBUG libapr-1.lib PATHS "${_IMPORT_PREFIX}/debug/lib" NO_DEFAULT_PATH)
+                find_library(APR_DLL_DEBUG libapr-1.dll PATHS "${_IMPORT_PREFIX}/debug/bin" NO_DEFAULT_PATH)
+                if (APR_LIB_RELEASE AND APL_DLL_RELEASE AND APR_LIB_DEBUG AND APR_DLL_DEBUG)
+                    add_library(activemq-cpp::activemq-cpp SHARED IMPORTED)
+                    set_target_properties(activemq-cpp::activemq-cpp
+                                          PROPERTIES
+                                              MAP_IMPORTED_CONFIG_MINSIZEREL Release
+                                              MAP_IMPORTED_CONFIG_RELWITHDEBINFO Release
+                                              IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/activemq-cpp.lib"
+                                              IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/activemq-cpp.dll"
+                                              IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/activemq-cppd.lib"
+                                              IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/activemq-cppd.dll"
+                                              IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+                                              INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+                    )
+                    set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
+                else()
+                    set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install dependency failure: apr vcpkg port not found in ${_IMPORT_PREFIX}")
+                    set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+                    set(activemq-cppConfig_FOUND TRUE)
+                endif()
+            else()
+                set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}debug/bin/activemq-cppd.dll but not ${_IMPORT_PREFIX}/debug/lib/activemq-cppd.lib")
+                set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+            endif()
+        else()
+            set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}/bin/activemq-cpp.dll but not ${_IMPORT_PREFIX}/debug/bin/activemq-cppd.dll")
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+        endif()
+    else()
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}/bin/activemq-cpp.dll but not ${_IMPORT_PREFIX}/lib/activemq-cpp.lib")
+        set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+    endif()
+elseif (EXISTS "${_IMPORT_PREFIX}/lib/libactivemq-cpp.lib")
+    #
+    # Windows static install
+    #
+    if (EXISTS "${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.lib")
+        find_library(APR_LIB_RELEASE apr-1.lib PATHS "${_IMPORT_PREFIX}/lib" NO_DEFAULT_PATH)
+        find_library(APR_LIB_DEBUG apr-1.lib PATHS "${_IMPORT_PREFIX}/debug/lib" NO_DEFAULT_PATH)
+        if (APR_LIB_RELEASE AND APR_LIB_DEBUG)
+            add_library(activemq-cpp::activemq-cpp STATIC IMPORTED)
+            set_target_properties(activemq-cpp::activemq-cpp
+                                  PROPERTIES
+                                      MAP_IMPORTED_CONFIG_MINSIZEREL Release
+                                      MAP_IMPORTED_CONFIG_RELWITHDEBINFO Release
+                                      IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.lib"
+                                      IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libactivemq-cpp.lib"
+                                      IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+                                      INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+            )
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
+        else()
+            set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install dependency failure: apr vcpkg port not found in ${_IMPORT_PREFIX}")
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+        endif()
+    else()
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}/lib/libactivemq-cpp.lib but not ${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.lib")
+        set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+    endif()
+elseif (EXISTS "${_IMPORT_PREFIX}/lib/libactivemq-cpp.so.19.0.5")
+    #
+    # Linux shared install  (this may pick up some other Unix-like installs)
+    #
+    if (EXISTS "${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.so.19.0.5")
+        find_library(APR_LIB_RELEASE libapr-1.so PATHS "${_IMPORT_PREFIX}/lib" NO_DEFAULT_PATH)
+        find_library(APR_LIB_DEBUG libapr-1.so PATHS "${_IMPORT_PREFIX}/debug/lib" NO_DEFAULT_PATH)
+        if (APR_LIB_RELEASE AND APR_LIB_DEBUG)
+            add_library(activemq-cpp::activemq-cpp SHARED IMPORTED)
+            set_target_properties(activemq-cpp::activemq-cpp
+                                  PROPERTIES
+                                      MAP_IMPORTED_CONFIG_MINSIZEREL Release
+                                      MAP_IMPORTED_CONFIG_RELWITHDEBINFO Release
+                                      IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libactivemq-cpp.so.19.0.5"
+                                      IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.so.19.0.5"
+                                      IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+                                      INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+            )
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
+        else()
+            set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install dependency failure: apr vcpkg port not found in ${_IMPORT_PREFIX}")
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+        endif()
+    else()
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}/lib/libactivemq-cpp.so but not ${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.so")
+        set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+    endif()
+elseif (EXISTS "${_IMPORT_PREFIX}/lib/libactivemq-cpp.a")
+    #
+    # Linux static install (this may pick up some other Unix-like installs)
+    #
+    if (EXISTS ${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.a)
+        find_library(APR_LIB_RELEASE libapr-1.a PATHS "${_IMPORT_PREFIX}/lib" NO_DEFAULT_PATH)
+        find_library(APR_LIB_DEBUG libapr-1.a PATHS "${_IMPORT_PREFIX}/debug/lib" NO_DEFAULT_PATH)
+        if (APR_LIB_RELEASE AND APR_LIB_DEBUG)
+            add_library(activemq-cpp::activemq-cpp STATIC IMPORTED)
+            set_target_properties(activemq-cpp::activemq-cpp
+                                  PROPERTIES
+                                      MAP_IMPORTED_CONFIG_MINSIZEREL Release
+                                      MAP_IMPORTED_CONFIG_RELWITHDEBINFO Release
+                                      IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libactivemq-cpp.a"
+                                      IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.a"
+                                      IMPORTED_CONFIGURATIONS "RELEASE;DEBUG"
+                                      INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+            )
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
+        else()
+            set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install dependency failure: apr vcpkg port not found in ${_IMPORT_PREFIX}")
+            set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+        endif()
+    else()
+        set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg install error: Found ${_IMPORT_PREFIX}/lib/libactivemq-cpp.so but not ${_IMPORT_PREFIX}/debug/lib/libactivemq-cpp.so")
+        set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+    endif()
+else()
+    #
+    # Some other configuration...
+    #
+    set(${CMAKE_FIND_PACKAGE_NAME}_NOT_FOUND_MESSAGE "Activemq-cpp vcpkg unexpected install: could not find any expected activemq-cpp libraries under ${_IMPORT_PREFIX}. The CMake configuration file only understands Windows and Linux static and shared installs from vcpkg.")
+    set(${CMAKE_FIND_PACKAGE_NAME}_FOUND FALSE)
+endif()
--- ports/activemq-cpp/portfile.cmake
+++ ports/activemq-cpp/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_fail_port_install( ON_TARGET "uwp" "linux" "osx")
+vcpkg_fail_port_install( ON_TARGET "uwp" "osx")
 
 set(VERSION 3.9.5)
 
@@ -8,92 +8,126 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 83692d3dfd5ecf557fc88d204a03bf169ce6180bcff27be41b09409b8f7793368ffbeed42d98ef6374c6b6b477d9beb8a4a9ac584df9e56725ec59ceceaa6ae2
 )
 
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    PATCHES
-        FunctionLevelLinkingOn.diff
-        fix-crt-linkage.patch
-)
-
-set(ACTIVEMQCPP_MSVC_PROJ ${SOURCE_PATH}/vs2010-build/activemq-cpp.vcxproj)
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ACTIVEMQCPP_SHARED_LIB)
-
-if (ACTIVEMQCPP_SHARED_LIB)
-    set(RELEASE_CONF "ReleaseDLL")
-    set(DEBUG_CONF   "DebugDLL")
-    
-    set(ACTIVEMQCPP_LIB_PREFFIX )
-    set(ACTIVEMQCPP_LIB_SUFFIX d)
-    vcpkg_replace_string(${ACTIVEMQCPP_MSVC_PROJ} ";apr-1.lib" ";libapr-1.lib")
-else()
-    set(RELEASE_CONF "Release")
-    set(DEBUG_CONF   "Debug")
-    
-    set(ACTIVEMQCPP_LIB_PREFFIX lib)
-    set(ACTIVEMQCPP_LIB_SUFFIX )
-    vcpkg_replace_string(${ACTIVEMQCPP_MSVC_PROJ} ";libapr-1.lib" ";apr-1.lib")
-endif()
-
-if (VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
-    set(BUILD_ARCH "Win32")
-    set(OUTPUT_DIR "Win32")
-elseif (VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
-    set(BUILD_ARCH "x64")
-    set(OUTPUT_DIR "Win64")
-else()
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-endif()
-
-vcpkg_build_msbuild(
-     PROJECT_PATH ${ACTIVEMQCPP_MSVC_PROJ}
-     RELEASE_CONFIGURATION ${RELEASE_CONF}
-     DEBUG_CONFIGURATION   ${DEBUG_CONF}
-     PLATFORM ${BUILD_ARCH}
-     USE_VCPKG_INTEGRATION
-)
-
-vcpkg_copy_pdbs()
-
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    file(COPY
-        ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/${ACTIVEMQCPP_LIB_PREFFIX}activemq-cpp.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    vcpkg_extract_source_archive_ex(
+        OUT_SOURCE_PATH SOURCE_PATH
+        ARCHIVE ${ARCHIVE}
     )
-    
-    if (ACTIVEMQCPP_SHARED_LIB)
-        file(COPY
-            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/activemq-cpp.dll
-            DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+
+    vcpkg_configure_make(
+        SOURCE_PATH ${SOURCE_PATH}
+        AUTOCONFIG
+	OPTIONS 
+	    "--with-openssl=${CURRENT_INSTALLED_DIR}"
+		"--with-apr=${CURRENT_INSTALLED_DIR}/tools/apr"
+    )
+
+    vcpkg_install_make()
+    file(RENAME ${CURRENT_PACKAGES_DIR}/include/activemq-cpp-${VERSION}/activemq ${CURRENT_PACKAGES_DIR}/include/activemq)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/include/activemq-cpp-${VERSION}/cms ${CURRENT_PACKAGES_DIR}/include/cms)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/include/activemq-cpp-${VERSION}/decaf ${CURRENT_PACKAGES_DIR}/include/decaf)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/activemq-cpp-${VERSION})
+
+    vcpkg_copy_pdbs()
+else()
+    if(VCPKG_CRT_LINKAGE STREQUAL "static")
+        vcpkg_extract_source_archive_ex(
+            OUT_SOURCE_PATH SOURCE_PATH
+            ARCHIVE ${ARCHIVE}
+            PATCHES
+                FunctionLevelLinkingOn.diff
+                fix-crt-linkage.patch
         )
-        file(COPY
-            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/activemq-cpp.pdb
-            DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+    else()
+        vcpkg_extract_source_archive_ex(
+            OUT_SOURCE_PATH SOURCE_PATH
+            ARCHIVE ${ARCHIVE}
+            PATCHES
+                FunctionLevelLinkingOn.diff
+                fix-crt-linkage-dyn.patch
         )
     endif()
-endif()
 
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    file(COPY
-        ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/${ACTIVEMQCPP_LIB_PREFFIX}activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.lib
-        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
-    )
-    
+    set(ACTIVEMQCPP_MSVC_PROJ ${SOURCE_PATH}/vs2010-build/activemq-cpp.vcxproj)
+
+    string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ACTIVEMQCPP_SHARED_LIB)
+
     if (ACTIVEMQCPP_SHARED_LIB)
-        file(COPY
-            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.dll
-            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-        )
-        file(COPY
-            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.pdb
-            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-        )
+        set(RELEASE_CONF "ReleaseDLL")
+        set(DEBUG_CONF   "DebugDLL")
+    
+        set(ACTIVEMQCPP_LIB_PREFFIX )
+        set(ACTIVEMQCPP_LIB_SUFFIX d)
+        vcpkg_replace_string(${ACTIVEMQCPP_MSVC_PROJ} ";apr-1.lib" ";libapr-1.lib")
+    else()
+        set(RELEASE_CONF "Release")
+        set(DEBUG_CONF   "Debug")
+    
+        set(ACTIVEMQCPP_LIB_PREFFIX lib)
+        set(ACTIVEMQCPP_LIB_SUFFIX )
+        vcpkg_replace_string(${ACTIVEMQCPP_MSVC_PROJ} ";libapr-1.lib" ";apr-1.lib")
     endif()
+
+    if (VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
+        set(BUILD_ARCH "Win32")
+        set(OUTPUT_DIR "Win32")
+    elseif (VCPKG_TARGET_ARCHITECTURE MATCHES "x64")
+        set(BUILD_ARCH "x64")
+        set(OUTPUT_DIR "Win64")
+    else()
+        message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+    endif()
+
+    vcpkg_build_msbuild(
+         PROJECT_PATH ${ACTIVEMQCPP_MSVC_PROJ}
+         RELEASE_CONFIGURATION ${RELEASE_CONF}
+         DEBUG_CONFIGURATION   ${DEBUG_CONF}
+         PLATFORM ${BUILD_ARCH}
+         USE_VCPKG_INTEGRATION
+    )
+
+    vcpkg_copy_pdbs()
+
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(COPY
+            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/${ACTIVEMQCPP_LIB_PREFFIX}activemq-cpp.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+        )
+    
+        if (ACTIVEMQCPP_SHARED_LIB)
+            file(COPY
+                ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/activemq-cpp.dll
+                DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+            )
+            file(COPY
+                ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${RELEASE_CONF}/activemq-cpp.pdb
+                DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+            )
+        endif()
+    endif()
+
+    if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(COPY
+            ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/${ACTIVEMQCPP_LIB_PREFFIX}activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.lib
+            DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        )
+    
+        if (ACTIVEMQCPP_SHARED_LIB)
+            file(COPY
+                ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.dll
+                DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+            )
+            file(COPY
+                ${SOURCE_PATH}/vs2010-build/${BUILD_ARCH}/${DEBUG_CONF}/activemq-cpp${ACTIVEMQCPP_LIB_SUFFIX}.pdb
+                DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+            )
+        endif()
+    endif()
+
+    file(COPY ${SOURCE_PATH}/src/main/activemq DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
+    file(COPY ${SOURCE_PATH}/src/main/cms      DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
+    file(COPY ${SOURCE_PATH}/src/main/decaf    DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
 endif()
 
-file(COPY ${SOURCE_PATH}/src/main/activemq DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
-file(COPY ${SOURCE_PATH}/src/main/cms      DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
-file(COPY ${SOURCE_PATH}/src/main/decaf    DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
-
+file(INSTALL ${CURRENT_PORT_DIR}/activemq-cppConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/activemq-cpp)
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
