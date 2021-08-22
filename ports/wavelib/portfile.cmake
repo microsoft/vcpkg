ae42def7a8d55dec1d95f7ac9bf1ad252283a52e
myd7349 <myd7349@gmail.com>
2021-08-22 16:45:44 +0800

    [wavelib] Clean

--- ports/wavelib/portfile.cmake
+++ ports/wavelib/portfile.cmake
@@ -19,6 +19,6 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${SOURCE_PATH}/COPYRIGHT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
