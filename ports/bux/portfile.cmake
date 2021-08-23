09f1bbd91a25f4b1e61c2dde2369f32d1dae94f0
Buck Yeh <buck-yeh@users.noreply.github.com>
2021-08-23 16:54:57 +0800

    Update ports/bux/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/bux/portfile.cmake
+++ ports/bux/portfile.cmake
@@ -6,9 +6,8 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_install_cmake()
