67ae8591077e87f4da060a2013521e76fee76969
Xing Ji <jixingcn@gmail.com>
2021-08-25 21:46:55 +0800

    rename `libpystring` to `pystring`

--- ports/pystring/portfile.cmake
+++ ports/pystring/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/pystring RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
