f59510a29441e9c8c8abc81177c96d189283d515
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2021-08-26 10:26:35 +0800

    Update ports/comms/portfile.cmake

--- ports/comms/portfile.cmake
+++ ports/comms/portfile.cmake
@@ -31,7 +31,7 @@ vcpkg_cmake_configure(
         -DCC_INSTALL_DEMO_PROTOCOL=OFF
 )
 vcpkg_cmake_install()
-
+vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME "LibComms" CONFIG_PATH "lib/LibComms/cmake" )
 
