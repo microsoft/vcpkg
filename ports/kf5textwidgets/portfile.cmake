3db7c45d8a7c851b4339fc585661dac3b89a01c6
Dawid Wróbel <me@dawidwrobel.com>
2021-08-27 16:08:38 +0200

    [kf5textwidgets] remove /bin when building static

--- ports/kf5textwidgets/portfile.cmake
+++ ports/kf5textwidgets/portfile.cmake
@@ -20,6 +20,10 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5TextWidgets)
 vcpkg_copy_pdbs()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
