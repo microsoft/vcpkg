092d7cfe6991f999b2fe8abd167ed76788e00161
Stefano Sinigardi <stesinigardi@hotmail.com>
2021-08-27 16:37:49 +0200

    [libgo] fail fast on unsupported archs

--- ports/libgo/portfile.cmake
+++ ports/libgo/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "uwp")
+vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "uwp")
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
