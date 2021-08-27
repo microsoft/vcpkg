ac386f0c1f5b34690bce7dcb4feb8bc34e5b63e1
Dawid Wróbel <me@dawidwrobel.com>
2021-08-27 14:12:46 +0200

    [kf5service] copy kbuildsycoca5 tool

--- ports/kf5service/portfile.cmake
+++ ports/kf5service/portfile.cmake
@@ -42,6 +42,11 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Service)
 vcpkg_copy_pdbs()
 
+vcpkg_copy_tools(
+     TOOL_NAMES kbuildsycoca5
+     AUTO_CLEAN
+)
+
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")	
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
