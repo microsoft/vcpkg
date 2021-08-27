0b048e3b91436dcd7c48f12887dacae4a541a3db
JackBoosY <yuzaiyang@beyondsoft.com>
2021-08-27 01:03:21 -0700

    Re-fix the dependencies

--- ports/hexl/portfile.cmake
+++ ports/hexl/portfile.cmake
@@ -7,24 +7,26 @@ vcpkg_from_github(
     REF 2dc1db6824be3fc89d13740efd0270ec9afec77e
     SHA512 aaa80dc53a21586d763a2b84b40d60062020470988422601bc5e9c2b31c6263847a97ea8f956d002a95e2d5e843cafa96fabdfd8b8ee892c7a7b9747133adebb
     HEAD_REF 1.2.0
-    PATCHES fix-git-path.patch
+    PATCHES fix-dependencies.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" HEXL_SHARED)
-
-vcpkg_find_acquire_program(GIT)
-get_filename_component(GIT_PATH ${GIT} DIRECTORY)
-vcpkg_add_to_path(${GIT_PATH})
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
-        "-DHEXL_BENCHMARK=OFF"
-        "-DHEXL_COVERAGE=OFF"
-        "-DHEXL_TESTING=OFF"
-        "-DHEXL_SHARED_LIB=${HEXL_SHARED}"
-        -DGIT_EXECUTABLE=${GIT}
+        -DHEXL_SHARED_LIB=${HEXL_SHARED}
+        -DHEXL_ENABLE_ADDRESS_SANITIZER=OFF
+        -DHEXL_ENABLE_THREAD_SANITIZER=OFF
+        -DHEXL_ENABLE_UB_SANITIZER=OFF
+        -DHEXL_BENCHMARK=OFF
+        -DHEXL_COVERAGE=OFF
+        -DHEXL_DEBUG=OFF
+        -DHEXL_DOCS=OFF
+        -DHEXL_TESTING=OFF
+        -DHEXL_EXPORT=ON
+        -DHEXL_TREAT_WARNING_AS_ERROR=OFF`
 )
 
 vcpkg_cmake_install()
