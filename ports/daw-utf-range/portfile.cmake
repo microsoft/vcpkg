80468fe4514ee1521d3ea9f9cd2dffa9a2904e25
JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
2021-08-27 10:12:42 +0800

    Update ports/daw-utf-range/portfile.cmake
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/daw-utf-range/portfile.cmake
+++ ports/daw-utf-range/portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 # remove empty lib and debug/lib directories (and duplicate files from debug/include)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
