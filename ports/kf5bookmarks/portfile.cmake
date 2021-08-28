b22de54abc837074c73f4bb0e3ff732787ec29bf
Dawid Wróbel <me@dawidwrobel.com>
2021-08-28 15:01:51 +0200

    [kf5bookmarks] only remove what's needed

--- ports/kf5bookmarks/portfile.cmake
+++ ports/kf5bookmarks/portfile.cmake
@@ -19,9 +19,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Bookmarks)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
