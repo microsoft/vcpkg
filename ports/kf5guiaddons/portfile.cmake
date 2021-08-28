b3da805f1d31c721c6dcb7a645d133eba63285a9
Dawid Wróbel <me@dawidwrobel.com>
2021-08-28 17:10:50 +0200

    [kf5guiaddons] Add missing static dependency

--- ports/kf5guiaddons/portfile.cmake
+++ ports/kf5guiaddons/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(
     SHA512 e5905c0aa5343ce3d4cd3765cb81390fc89fb78aec3c8de8b31d1dada8074d04f549ff785f3988498d2e274d7cb08a35a83ba031d18562049e6ca41d18ea52ee
     HEAD_REF master
     PATCHES
+        25.patch # https://invent.kde.org/frameworks/kguiaddons/-/merge_requests/25
         fix_cmake.patch # see https://github.com/microsoft/vcpkg/issues/17607#issuecomment-831518812
 )
 
