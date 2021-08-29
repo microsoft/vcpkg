f8f52b924ec42b678fdbd0ae00446c4187d94b8f
cmarois <cmarois@coveo.com>
2021-08-29 15:12:05 -0400

    [aws-checksums] Update to 0.1.11

--- ports/aws-checksums/portfile.cmake
+++ ports/aws-checksums/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO awslabs/aws-checksums
-    REF fb96b3e964fe9bde2625c3ac9547e54d6c802211 # v0.1.9
-    SHA512 2cf63a462c900fae8ad101ecac8be6fd6ce3f796e3cecc1b3d19ffba364030d7468a7c7beab91594d2521cab5e99765c7b67fa2fef6f772457e04f75f59962cc
+    REF 99bb0ad4b89d335d638536694352c45e0d2188f5 # v0.1.11
+    SHA512 cb9c249496fd41fda1efb9330e823d8b965adca6c8f372a50fe97eda821e277780bf9af8f5977102c44121568993cca55edbb750967b41f323e07e06a93c50a8
     HEAD_REF master
     PATCHES fix-cmake-target-path.patch
 )
@@ -36,4 +36,4 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
