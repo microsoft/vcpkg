06429b65b1311ca8d94e2ea32cde685ddeee34fb
azure-sdk <azuresdk@microsoft.com>
2021-08-30 21:51:46 +0000

    [azure-template-cpp] Update to 1.0.0-beta.1072920 ## 1.0.0-beta.1072920 (2021-08-30)
    * Validate template project ahead of release

--- ports/azure-template-cpp/portfile.cmake
+++ ports/azure-template-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-template_1.0.0-beta.1072881
-    SHA512 42c9bfc1627d38ab7c353b03a32d2bc245064d4dd8a6eace2c1ea38a247cb6278d2ee02a622d035082880c9359da0fd24d1ebd8bd9c8c98795029d8ecc673064
+    REF azure-template_1.0.0-beta.1072920
+    SHA512 5d586a8fe360f0778a0dfc97bef1e9fa5f5840ceece25c386db900497d92d26a2990c05b25cf5932630721dae92cad0611d7891a01df58cef9e3c2b20cbf5153
 )
 
 vcpkg_cmake_configure(
