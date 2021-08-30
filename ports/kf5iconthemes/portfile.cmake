ddf9395fec7d0e25155c40be45bb072714170178
Dawid Wróbel <me@dawidwrobel.com>
2021-08-30 16:35:50 +0200

    [kf5iconthemes] add PACKAGE_NAME to vcpkg_cmake_config_fixup

--- ports/kf5iconthemes/portfile.cmake
+++ ports/kf5iconthemes/portfile.cmake
@@ -26,7 +26,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5IconThemes)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5IconThemes CONFIG_PATH lib/cmake/KF5IconThemes)
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools(
