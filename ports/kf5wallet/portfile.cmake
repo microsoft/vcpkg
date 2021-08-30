a42f5a0b645f0abc90e9181e59fbb081467916c0
Dawid Wróbel <me@dawidwrobel.com>
2021-08-30 16:38:27 +0200

    [kf5wallet] add PACKAGE_NAME to vcpkg_cmake_config_fixup

--- ports/kf5wallet/portfile.cmake
+++ ports/kf5wallet/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Wallet)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Wallet CONFIG_PATH lib/cmake/KF5Wallet)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
