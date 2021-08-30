0004ef6599c804c5959b5fa9bf32fb759868102d
Dawid Wróbel <me@dawidwrobel.com>
2021-08-30 20:10:48 +0200

    [kf5bookmarks] remove static bin files

--- ports/kf5bookmarks/portfile.cmake
+++ ports/kf5bookmarks/portfile.cmake
@@ -17,6 +17,10 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Bookmarks CONFIG_PATH lib/cmake/KF5Bookmarks)
 vcpkg_copy_pdbs()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
