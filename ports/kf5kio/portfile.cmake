f6737db45320feae5e25a48eb523feb0e6e164d6
Dawid Wróbel <me@dawidwrobel.com>
2021-08-30 23:31:09 +0200

    [kf5kio] libiconv patches only for windows static builds

--- ports/kf5kio/portfile.cmake
+++ ports/kf5kio/portfile.cmake
@@ -1,12 +1,16 @@
+set(PATCHES fix_dbusmetatypes.patch)
+
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    list(APPEND PATCHES fix_libiconv.patch)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/kio
     REF v5.84.0
     SHA512 6c2d57a31e64ff1475b21d7fb2556d37b061dae319ddfe57a36f0bfc9627db624b1ac8fa4b2851681cb90d218255d0444c1403329d88f34a23e8ddffe99ca5b4
     HEAD_REF master
-    PATCHES
-        fix_dbusmetatypes.patch
-        fix_libiconv.patch
+    PATCHES ${PATCHES}
 )
 
 vcpkg_cmake_configure(
