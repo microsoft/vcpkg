13da815b0fea54f8d3f767de7c8aef3ea85a1525
Dawid Wróbel <me@dawidwrobel.com>
2021-08-30 16:31:08 +0200

    [kf5crash] add PACKAGE_NAME to vcpkg_cmake_config_fixup

--- ports/kf5crash/portfile.cmake
+++ ports/kf5crash/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Crash)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Crash CONFIG_PATH lib/cmake/KF5Crash)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/data")
