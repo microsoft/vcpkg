232b9b3713aa42c149a5f66bb13b6e5343605923
Dawid Wróbel <me@dawidwrobel.com>
2021-08-31 03:58:59 +0200

    [kf5declarative] add PACKAGE_NAME to vcpkg_cmake_config_fixup

--- ports/kf5declarative/portfile.cmake
+++ ports/kf5declarative/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Declarative)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Declarative CONFIG_PATH lib/cmake/KF5Declarative)
 vcpkg_copy_pdbs()
 
 if(VCPKG_TARGET_IS_WINDOWS)
