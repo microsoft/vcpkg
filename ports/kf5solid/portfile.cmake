1008419ee308ec04704a27f2a8898172930791e7
Dawid Wróbel <me@dawidwrobel.com>
2021-08-31 02:32:02 +0200

    [kf5solid] add PACKAGE_NAME to vcpkg_cmake_config_fixup

--- ports/kf5solid/portfile.cmake
+++ ports/kf5solid/portfile.cmake
@@ -41,7 +41,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Solid)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Solid CONFIG_PATH lib/cmake/KF5Solid)
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools(
