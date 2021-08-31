c15357e5b4719d09e2c37918fed0c852ac39c67a
G'k <39066502+Guekka@users.noreply.github.com>
2021-08-31 08:58:53 +0200

    Rename to rsm-bsa

--- ports/rsm-bsa/portfile.cmake
+++ ports/rsm-bsa/portfile.cmake
@@ -1,12 +1,13 @@
 vcpkg_fail_port_install(ON_TARGET "OSX" "UWP" ON_ARCH "x86")
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-vcpkg_from_github(
+#[[vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ryan-rsm-McKenzie/bsa
     REF 2.0.3
     SHA512 b54f1cbc88e55b98b0c25ee81007dc42df736836784b2a1e2e91652b46a73054671d798cc0007ea79b7cf0c8181021bb0cbc95ae407d4a538298d668b4c8fe69
     HEAD_REF master
-)
+)]]
+set(SOURCE_PATH "C:/IT/Code_perso/bsa)
 
 if (VCPKG_TARGET_IS_LINUX)
     message(WARNING "Build ${PORT} requires at least gcc 10.")
