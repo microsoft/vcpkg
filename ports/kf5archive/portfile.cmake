2103d5083140dfb2c7c5833fe7760280559d6b83
Dawid Wróbel <me@dawidwrobel.com>
2021-09-01 20:19:07 +0200

    [kf5archive] fix cmake.in to allow consumption by static builds (#19194)
    * [kf5archive] fix cmake.in to allow consumption by static builds
    
    * [kf5archive] update versions
    
    * [kf5archive] add lzma, zstd features
    
    * [kf5archive] update versions
    
    * [kf5archive] fix MAYBE_UNUSED_VARIABLES usage
    
    * [kf5archive] replace deprecated functions
    
    * [kf5archive] use semVer
    
    * [kf5archive] update versions
    
    * [kf5archive] add vcpkg_check_features
    
    * [kf5archive] update versions
    
    * [kf5archive] remove redundant option
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    * [kf5archive] wrap all paths in quotes
    
    * [kf5archive] update versions
    
    * [kf5archive] lookup ZSTD using CMake
    
    * [kf5archive] update versions
    
    * [kf5archive] fix vcpkg_cmake_config_fixup() usage
    
    * [kf5archive] update versions
    
    * [kf5archive] rename patch files
    
    * [kf5archive] update versions
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/kf5archive/portfile.cmake
+++ ports/kf5archive/portfile.cmake
@@ -4,30 +4,38 @@ vcpkg_from_github(
     REF v5.84.0
     SHA512 82926f62424446df0f4fc300f57ae9bd5baf8e13da2ce4135ac56c0c52a0307bffb06f84ac7e8e658e96ace2ae3d530f27e232061284ac87271404f218e9fdd4
     HEAD_REF master
+    PATCHES
+        only_pkg_check_modules_if_pkgconfig_found.patch # https://invent.kde.org/frameworks/karchive/-/commit/9ab5f2bfbe59038b0d0b6ca7f1b22d1c9229c67e
+        add_zstd_to_cmake_config.patch # https://invent.kde.org/frameworks/karchive/-/commit/5a79756f381e1a1843cb2171bdc151dad53fb7db
+        add_support_for_static_builds.patch # https://invent.kde.org/frameworks/karchive/-/merge_requests/23
+        use_cmake_to_find_zstd.patch # https://invent.kde.org/frameworks/karchive/-/merge_requests/24
 )
 
-vcpkg_configure_cmake(
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    INVERTED_FEATURES
+        "lzma"  CMAKE_DISABLE_FIND_PACKAGE_LibLZMA
+        "zstd"  CMAKE_DISABLE_FIND_PACKAGE_ZSTD
+)
+
+vcpkg_cmake_configure(
     DISABLE_PARALLEL_CONFIGURE
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS 
-        -DBUILD_HTML_DOCS=OFF
-        -DBUILD_MAN_DOCS=OFF
-        -DBUILD_QTHELP_DOCS=OFF
         -DBUILD_TESTING=OFF
+        ${FEATURE_OPTIONS}
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5Archive)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Archive CONFIG_PATH lib/cmake/KF5Archive)
 vcpkg_copy_pdbs()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/etc")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/etc")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSES/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
