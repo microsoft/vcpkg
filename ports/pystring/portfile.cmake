35c762f7283940fd29859e3e10feee30dc80361d
Robert Schumacher <roschuma@microsoft.com>
2021-09-01 03:19:54 +0000

    [pystring] Use target name as per opencolorio

--- ports/pystring/portfile.cmake
+++ ports/pystring/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-pystring)
+vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
