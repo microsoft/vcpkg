ac87ec35700bcc7e4091422b5d095075b8230751
Dawid Wróbel <me@dawidwrobel.com>
2021-09-01 20:29:13 +0200

    [kf5coreaddons] fix Windows static builds & CMake Config (#19439)
    * [kf5coreaddons] fix Windows static builds
    
    https://invent.kde.org/frameworks/kcoreaddons/-/merge_requests/122
    
    Also:
    - replace deprecated functions
    - embrace paths in quotes
    - actually remove the folders that need to be removed
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] delete redundant REMOVE
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] save data files to default /share location
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] fix CMake Config for static builds
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] fix vcpkg_cmake_config_fixup() usage
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] workaround Linux Inotify issue
    
    * [kf5coreaddons] update versions
    
    * [kf5coreaddons] rename patch file
    
    * [kf5coreaddons] update versions

--- ports/kf5coreaddons/portfile.cmake
+++ ports/kf5coreaddons/portfile.cmake
@@ -3,22 +3,20 @@ vcpkg_from_github(
     REPO KDE/kcoreaddons
     REF v5.84.0
     SHA512 58a802d03bea135c192265651540f9f87066b20ee4af620a94a874ec3992c5e734d1ab1e4ccb082de97389fa3479fd3672ec586c3dd36c9b4a5422c7be98d119
+    PATCHES
+        fix_static_build.patch # https://invent.kde.org/frameworks/kcoreaddons/-/merge_requests/122
+        fix_cmake_config.patch # https://invent.kde.org/frameworks/kcoreaddons/-/merge_requests/129
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA
     OPTIONS 
-        -DBUILD_HTML_DOCS=OFF
-        -DBUILD_MAN_DOCS=OFF
-        -DBUILD_QTHELP_DOCS=OFF
         -DBUILD_TESTING=OFF
-        -DKDE_INSTALL_DATAROOTDIR=data
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5CoreAddons)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5CoreAddons CONFIG_PATH lib/cmake/KF5CoreAddons)
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools( 
@@ -26,18 +24,13 @@ vcpkg_copy_tools(
     AUTO_CLEAN
 )
 
-file(APPEND ${CURRENT_PACKAGES_DIR}/tools/${PORT}/qt.conf "Data = ../../data")
+file(APPEND "${CURRENT_PACKAGES_DIR}/tools/${PORT}/qt.conf" "Data = ../../share")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/data)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/data)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/desktoptojson${EXECUTABLE_SUFFIX})
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")	
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSES/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
