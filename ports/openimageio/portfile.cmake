f36bf6b760a7e2e72b9b8efcfe8b0561f3ed2e7a
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2021-09-02 17:15:01 +0800

    Update ports/openimageio/portfile.cmake

--- ports/openimageio/portfile.cmake
+++ ports/openimageio/portfile.cmake
@@ -84,7 +84,7 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/doc"
 
 file(COPY "${SOURCE_PATH}/src/cmake/modules/FindOpenImageIO.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/OpenImageIO")
 file(COPY "${SOURCE_PATH}/src/cmake/modules/FindLibsquish.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/OpenImageIO")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/openimageio")
 
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
