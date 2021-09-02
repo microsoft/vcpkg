1ab02dfd870c7166d56b172176237d38d4792618
JackBoosY <yuzaiyang@beyondsoft.com>
2021-09-02 02:07:19 -0700

    Re-fix the build issues

--- ports/ecal/portfile.cmake
+++ ports/ecal/portfile.cmake
@@ -1,3 +1,7 @@
+if (VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO continental/ecal
@@ -5,13 +9,9 @@ vcpkg_from_github(
     SHA512 a483568b16ae191410d7a8cd8bfba59570be2a2f912f3fa9718fe74a4a5f3de910bed5831781e38617924da69c1cc625836df2e507a9ef7dea04e03c597de8fe
     HEAD_REF master
     PATCHES
-        CMakeLists.txt.patch
-        cmake.Modules.Findtclap.cmake.patch
-        cmake.Modules.Findsimpleini.cmake.patch
-        thirdparty.cmake_functions.cmake.CMakeFunctionsConfig.cmake.in.patch
-        thirdparty.cmake_functions.cmake_functions.cmake.patch
-        thirdparty.cmake_functions.CMakeLists.txt.patch
-        no-find-package-cmakefunctions.patch
+        fix-build.patch
+        disable-app-plugins.patch
+        fix-dependencies.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/cmake/Modules/Findasio.cmake")
@@ -19,32 +19,49 @@ file(REMOVE "${SOURCE_PATH}/cmake/Modules/Findasio.cmake")
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DHAS_HDF5=ON
         -DHAS_QT5=OFF
+        -DHAS_CURL=ON
+        -DHAS_CAPNPROTO=OFF
+        -DBUILD_DOCS=OFF
         -DBUILD_APPS=OFF
         -DBUILD_SAMPLES=OFF
+        -DBUILD_TIME=ON
+        -DBUILD_PY_BINDING=OFF
+        -DBUILD_CSHARP_BINDING=OFF
+        -DBUILD_ECAL_TESTS=OFF
+        -DECAL_LAYER_ICEORYX=OFF
+        -DECAL_INCLUDE_PY_SAMPLES=OFF
         -DECAL_INSTALL_SAMPLE_SOURCES=OFF
+        -DECAL_JOIN_MULTICAST_TWICE=OFF
+        -DECAL_NPCAP_SUPPORT=OFF
+        -DECAL_THIRDPARTY_BUILD_CMAKE_FUNCTIONS=ON
         -DECAL_THIRDPARTY_BUILD_SPDLOG=OFF 
         -DECAL_THIRDPARTY_BUILD_TINYXML2=OFF
         -DECAL_THIRDPARTY_BUILD_FINEFTP=OFF
         -DECAL_THIRDPARTY_BUILD_TERMCOLOR=OFF
+        -DECAL_THIRDPARTY_BUILD_GTEST=OFF
         -DECAL_THIRDPARTY_BUILD_PROTOBUF=OFF
         -DECAL_THIRDPARTY_BUILD_CURL=OFF
         -DECAL_THIRDPARTY_BUILD_HDF5=OFF
-        -DECAL_LINK_HDF5_SHARED=OFF
+        #-DECAL_LINK_HDF5_SHARED=OFF Don't need it since it was fixed in the dependecies patch 
         -DCPACK_PACK_WITH_INNOSETUP=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cmake")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/cmake")
+# Install the helper_functions scripts first
+file(INSTALL "${CURRENT_PACKAGES_DIR}/lib/cmake/eCAL/helper_functions" DESTINATION "${CURRENT_PACKAGES_DIR}/share/eCAL")
+vcpkg_cmake_config_fixup(PACKAGE_NAME eCAL CONFIG_PATH lib/cmake/eCAL)
+
+# Remove unnecessary cmake functions
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cmake" "${CURRENT_PACKAGES_DIR}/debug/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cfg" "${CURRENT_PACKAGES_DIR}/debug/cfg")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
