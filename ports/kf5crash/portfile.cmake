e906d682415d8ec0733994e944946b052e195efe
Dawid Wróbel <me@dawidwrobel.com>
2021-09-04 01:41:20 +0200

    [kf5crash] fix cmake.in to allow consumption by static builds (#19179)
    * [kf5crash] fix cmake.in to allow consumption by static builds
    
    * [kf5crash] update versions
    
    * [kf5crash] kcrash is supported on Win and macOS, too
    
    * [kf5crash] update versions
    
    * [kf5crash] replace deprecated functions
    
    * [kf5crash] update versions
    
    * [kf5crash] don't overzealously delete folders
    
    * [kf5crash] update versions
    
    * [kf5crash] proactively disable docs
    
    * [kf5crash] use semVer
    
    * [kf5crash] update versions
    
    * [kf5crash] remove extraneous options
    
    * [kf5crash] update versions
    
    * [kf5crash] wrap paths in quotes
    
    * [kf5crash] update versions
    
    * [kf5crash] remove bin folders in static build
    
    * [kf5crash] update versions
    
    * [kf5crash] rename patch file
    
    * [kf5crash] update versions
    
    * [kf5crash] add PACKAGE_NAME to vcpkg_cmake_config_fixup
    
    * [kf5crash] update versions

--- ports/kf5crash/portfile.cmake
+++ ports/kf5crash/portfile.cmake
@@ -4,25 +4,28 @@ vcpkg_from_github(
     REF v5.84.0
     SHA512 db37a481e0d34e0024cabd77c22680171fa383ea300c56c73f807380fd818263d27f36fb012fc6604b462dbf9374db39bab94c4dc2c3bff488ac9812d01eb2dd
     HEAD_REF master
+    PATCHES
+        support_static_builds.patch # https://invent.kde.org/frameworks/kcrash/-/merge_requests/23
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA
     OPTIONS
         -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/KF5Crash)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Crash CONFIG_PATH lib/cmake/KF5Crash)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/data)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/data)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/data")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/etc")
 
-file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(INSTALL "${SOURCE_PATH}/LICENSES/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
