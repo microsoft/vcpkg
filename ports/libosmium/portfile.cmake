dd5fab7c11abd1f4e95009c2b93832210c24d41b
NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
2021-09-04 05:14:39 +0800

    [libosmium/libxlsxwriter] Update to the latest version (#19935)
    * [libosmium/libxlsxwriter] Update to the latest version
    
    * Update version files
    
    * Update versions/l-/libosmium.json
    
    * Attempt to fix version database again.
    
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/libosmium/portfile.cmake
+++ ports/libosmium/portfile.cmake
@@ -2,20 +2,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO osmcode/libosmium
-    REF v2.15.5
-    SHA512 a4972901db8ed89302e6ba15fd104543b5e36a41bc83daf8f6f6fb29ce73b0dbd8596de801d099a33df413b26eec1b3a6f4f0d669936ecc6d25f88d783468a59
+    REF b263ba5e85c9ac254fa4090c855ec6f0556795e2 #v2.17.0
+    SHA512 fd2955af6153ef58d76cca1e5b83cb70cd33cb616a3e221a80df94ee1256eeeaa5f15f4727cd1023f0335e55d7f3f36e3f9f5490bcd78ba9d267b2075480d1ba
 )
-set(BOOST_ROOT ${CURRENT_INSTALLED_DIR})
+set(BOOST_ROOT "${CURRENT_INSTALLED_DIR}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DBUILD_EXAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
--- ports/libxlsxwriter/portfile.cmake
+++ ports/libxlsxwriter/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jmcnamara/libxlsxwriter
-    REF RELEASE_0.9.4
-    SHA512 d7bc319e6b9cd2ad6aaa2f3eb6fdce1c5bcc1d5af23ffb3413e29760191f6aed41f836aaa71a322efe7966f3753a6d8a01cb0b403d682b13a6a3734a87cc12ba
-	HEAD_REF master
+    REF 576d169463c7f9990045fd9223f5cf688150def0 #RELEASE_1.1.3
+    SHA512 376db117df3ab48a6471d7004fc77fb8bd9b5d9dfaff53675f1bd99c8bc9bec7cadcefbd7116b206ef4703b9146cf097ad3b8aadff36b66302f1c82e8e1fa988
+    HEAD_REF master
 )
 
 if (VCPKG_TARGET_IS_UWP)
@@ -12,16 +12,16 @@ else()
   set(USE_WINDOWSSTORE OFF)
 endif()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-	PREFER_NINJA
-	OPTIONS -DWINDOWSSTORE=${USE_WINDOWSSTORE}
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS 
+        -DWINDOWSSTORE=${USE_WINDOWSSTORE}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/License.txt" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
