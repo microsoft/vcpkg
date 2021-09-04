3f1898ce70d97373a9895096974d891116a6a134
Billy O'Neal <bion@microsoft.com>
2021-09-03 22:16:43 -0700

    [simdutf] Disable benchmarks and tests. (#19987)

--- ports/simdutf/portfile.cmake
+++ ports/simdutf/portfile.cmake
@@ -2,8 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO simdutf/simdutf
     REF v1.0.0
-    SHA512 3ab09aa61cb9765bf1a77de59c5b823ee58ae5e4badfd5dd70e511fd4f378f8d3917a5b577e7f275720b975740344968132ce0b3f628452bde67f2ab6cc82337 
+    SHA512 3ab09aa61cb9765bf1a77de59c5b823ee58ae5e4badfd5dd70e511fd4f378f8d3917a5b577e7f275720b975740344968132ce0b3f628452bde67f2ab6cc82337
     HEAD_REF master
+    PATCHES
+        disable-tests-and-benchmarks.patch
 )
 
 vcpkg_cmake_configure(
