d548ec3f3fd2f1b701d7e5de6dfb816eeccf0580
JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
2021-09-04 08:37:20 +0800

    [libmupdf/liblinear/libkeyfinder] Update to latest version (#19959)
    * [libmupdf/liblinear/libkeyfinder] Update to latest version
    
    * update version
    
    * update vcpkg.json format
    
    * update version

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -5,25 +5,24 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ArtifexSoftware/mupdf
-    REF ea5799e01730c4aa15cddd1023700e4f7b78cc27 # 1.18.0
-    SHA512 4904565e900970939f93bf4326df86851e699699c8e49df7abdbedf6ba3e9d26b74691710b2019d04bba2dea11c7880fe4418b643866128828e388500aa666c2
+    REF af0e25cec567868a04eaacf6410c395712fe4b90 #1.18.1-so-3.11.14
+    SHA512 3dc6b75964d93af84921ee30a5b14e0ab84d16afa31f97a0fbf62e2006ace62f9c0366d1c3872cd678dab71eb23a422daeca0eb0b5db58e434f27657bbf9b5bc
     HEAD_REF master
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
-    PREFER_NINJA
     OPTIONS
         -DBUILD_EXAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-file(COPY ${SOURCE_PATH}/include/mupdf DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(COPY "${SOURCE_PATH}/include/mupdf" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
