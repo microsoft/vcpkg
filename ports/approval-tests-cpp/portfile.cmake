9f22d5d7db9ebee004f7404cd68c39aed1ff6a4a
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-09-04 00:07:24 +0000

    [approval-tests-cpp] Update to 10.11.0 (#19941)
    * [approval-tests-cpp] Update to 10.11.0
    
    * add version files
    
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/approval-tests-cpp/portfile.cmake
+++ ports/approval-tests-cpp/portfile.cmake
@@ -1,12 +1,12 @@
 vcpkg_download_distfile(single_header
-    URLS https://github.com/approvals/ApprovalTests.cpp/releases/download/v.10.10.0/ApprovalTests.v.10.10.0.hpp
-    FILENAME ApprovalTests.v.10.10.0.hpp
-    SHA512 909302175fcaa23aea9839a8276a1c20b654e8544600bfb05d6bb76bc6a635f51ef6efe437ac07e01391724b2dc0e31cbf16bcb688fbec1ef3e378b027a6eb64
+    URLS https://github.com/approvals/ApprovalTests.cpp/releases/download/v.10.11.0/ApprovalTests.v.10.11.0.hpp
+    FILENAME ApprovalTests.v.10.11.0.hpp
+    SHA512 2b43792b28a1dd44d76584c4d3bb9cb99563f92fdeafefd747516a8cfe0baf118b27ad235e1b023b3f5f3a34ecf920c12ab8a6d337776efc036456f4277142ae
 )
 
 vcpkg_download_distfile(license_file
-    URLS https://raw.githubusercontent.com/approvals/ApprovalTests.cpp/v.10.10.0/LICENSE
-    FILENAME ApprovalTestsLicense.v.10.10.0
+    URLS https://raw.githubusercontent.com/approvals/ApprovalTests.cpp/v.10.11.0/LICENSE
+    FILENAME ApprovalTestsLicense.v.10.11.0
     SHA512 dc6b68d13b8cf959644b935f1192b02c71aa7a5cf653bd43b4480fa89eec8d4d3f16a2278ec8c3b40ab1fdb233b3173a78fd83590d6f739e0c9e8ff56c282557
 )
 
