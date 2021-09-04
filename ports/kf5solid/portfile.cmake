0cde558415e1027284c0d98e71d59e4d5839efb6
Dawid Wróbel <me@dawidwrobel.com>
2021-09-04 07:20:10 +0200

    [kf5solid] Fix CMake config; some extra fixes (#19810)
    * [kf5solid] require bison >= 3.0
    
    * [kf5solid] fix CMake config file
    
    * [kf5solid] some apps actually use solid-hardware5
    
    diff --git a/ports/kf5solid/portfile.cmake b/ports/kf5solid/portfile.cmake
    index b876b00c0..53cf1841b 100644
    --- a/ports/kf5solid/portfile.cmake
    +++ b/ports/kf5solid/portfile.cmake
    @@ -44,6 +44,11 @@ vcpkg_cmake_install()
     vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Solid)
     vcpkg_copy_pdbs()
    
    +vcpkg_copy_tools(
    +      TOOL_NAMES solid-hardware5
    +      AUTO_CLEAN
    + )
    +
     if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
         file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
     endif()
    @@ -51,9 +56,6 @@ endif()
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/etc")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
    -# solid-hardware and solid-power are a non-dev tools allowing to list hardware and power managament status of one's system. No need to keep them.
    -file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/solid-hardware5${VCPKG_HOST_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/debug/bin/solid-hardware5${VCPKG_HOST_EXECUTABLE_SUFFIX}")
    -file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/solid-power${VCPKG_HOST_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/debug/bin/solid-power${VCPKG_HOST_EXECUTABLE_SUFFIX}")
     file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/qml" "${CURRENT_PACKAGES_DIR}/debug/qml")
     file(RENAME "${CURRENT_PACKAGES_DIR}/lib/qml" "${CURRENT_PACKAGES_DIR}/qml")
    
    * [kf5solid] /etc is empty, no need to remove it
    
    * [kf5solid] add PACKAGE_NAME to vcpkg_cmake_config_fixup
    
    * [kf5solid] update versions

--- ports/kf5solid/portfile.cmake
+++ ports/kf5solid/portfile.cmake
@@ -4,23 +4,24 @@ vcpkg_from_github(
     REF v5.84.0
     SHA512 b6452e56c6029289450850c1fcfff96da0005f8dfa03f1817754910945e3ccadd8502e330a4484a5c5e9a8d5525838c8090268bb083639062dfca7176852c159
     HEAD_REF master
+    PATCHES
+        fix_config_cmake.patch # https://invent.kde.org/frameworks/solid/-/merge_requests/53
 )
 
 if(VCPKG_TARGET_IS_OSX)
-     # In Darwin platform, there can be an old version of `bison`, 
-     # Which can't be used for `gst-build`. It requires 2.4+
-     vcpkg_find_acquire_program(BISON)
-     execute_process(
-         COMMAND ${BISON} --version
-         OUTPUT_VARIABLE BISON_OUTPUT
-     )
-     string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" BISON_VERSION "${BISON_OUTPUT}")
-     set(BISON_MAJOR ${CMAKE_MATCH_1})
-     set(BISON_MINOR ${CMAKE_MATCH_2})
-     message(STATUS "Using bison: ${BISON_MAJOR}.${BISON_MINOR}.${CMAKE_MATCH_3}")
-     if(NOT (BISON_MAJOR GREATER_EQUAL 2 AND BISON_MINOR GREATER_EQUAL 4))
-         message(WARNING "${PORT} requires bison version greater than one provided by macOS, please use \`brew install bison\` to install a newer bison.")
-     endif()
+    # On Darwin platform, the bundled version of 'bison' may be too old (< 3.0).
+    vcpkg_find_acquire_program(BISON)
+    execute_process(
+        COMMAND ${BISON} --version
+        OUTPUT_VARIABLE BISON_OUTPUT
+    )
+    string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" BISON_VERSION "${BISON_OUTPUT}")
+    set(BISON_MAJOR ${CMAKE_MATCH_1})
+    set(BISON_MINOR ${CMAKE_MATCH_2})
+    message(STATUS "Using bison: ${BISON_MAJOR}.${BISON_MINOR}.${CMAKE_MATCH_3}")
+    if(NOT (BISON_MAJOR GREATER_EQUAL 3 AND BISON_MINOR GREATER_EQUAL 0))
+        message(WARNING "${PORT} requires bison version greater than one provided by macOS, please use \`brew install bison\` to install a newer bison.")
+    endif()
 endif()
 
 vcpkg_find_acquire_program(BISON)
@@ -40,19 +41,21 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Solid)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5Solid CONFIG_PATH lib/cmake/KF5Solid)
 vcpkg_copy_pdbs()
+
+vcpkg_copy_tools(
+      TOOL_NAMES solid-hardware5
+      AUTO_CLEAN
+ )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/etc")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-# solid-hardware and solid-power are a non-dev tools allowing to list hardware and power managament status of one's system. No need to keep them.
-file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/solid-hardware5${VCPKG_HOST_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/debug/bin/solid-hardware5${VCPKG_HOST_EXECUTABLE_SUFFIX}")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/solid-power${VCPKG_HOST_EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/debug/bin/solid-power${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/qml" "${CURRENT_PACKAGES_DIR}/debug/qml")
 file(RENAME "${CURRENT_PACKAGES_DIR}/lib/qml" "${CURRENT_PACKAGES_DIR}/qml")
 
