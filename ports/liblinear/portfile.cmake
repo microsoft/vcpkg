68f09e2a31aaa50b21b04eca9550e1ac996ba7bb
JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
2021-09-04 08:37:20 +0800

    [libmupdf/liblinear/libkeyfinder] Update to latest version (#19959)
    * [libmupdf/liblinear/libkeyfinder] Update to latest version
    
    * update version
    
    * update vcpkg.json format
    
    * update version

--- ports/liblinear/portfile.cmake
+++ ports/liblinear/portfile.cmake
@@ -3,27 +3,26 @@ vcpkg_fail_port_install(ON_TARGET "uwp")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cjlin1/liblinear
-    REF 2381122d05bbb1e4ee24b522298dd548f0ec0d24 #v241
-    SHA512 ee784b6325681b3d9e3dc0b59f4a703d87be35fb898cc16df93e4a814a959d530736a8451be4f0f2c856769d81e3f5acbcd6f0f8677425e700597e3502f9f36d
+    REF 60f1adf6f35d6f3e031c334b33dfe8399d6f8a9d #v243
+    SHA512 0f88f8dd768313d0a9b3bb82e7b878d7173ea43ef609e993dc79e94398897373faf2688249b17111e2b6e06e78e26a50570a1b746dc4e73fb7416ccc24936c66
     HEAD_REF master
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG
         -DDISABLE_INSTALL_HEADERS=ON
         -DDISABLE_INSTALL_TOOLS=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 if(NOT DISABLE_INSTALL_TOOLS)
-    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/liblinear)
+    vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/liblinear")
 endif()
 
-file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/README DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${SOURCE_PATH}/COPYRIGHT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
