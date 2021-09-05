bfc0615cc709da900cc3c5dd017022bbc465a92e
Reno Raksi <adriano.raksi@gmail.com>
2021-09-05 00:33:55 -0500

    Update SHA512 and fix duplicated include files

--- ports/libspng/portfile.cmake
+++ ports/libspng/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO randy408/libspng
     REF v0.6.3
-    SHA512 0
+    SHA512 857ff6ba51d8e338b1c96a0c016aaea3aea807aaea935cc14959d3d0c337229dfb328279d042ddf937152dd0c496e5bcddbc9fa50ac167e4b31847950cc043da
     HEAD_REF master
 )
 
@@ -13,3 +13,4 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libspng RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
