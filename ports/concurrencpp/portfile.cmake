60bf7273f47630baf9e477352b3b8af795fe8ed5
chausner <15180557+chausner@users.noreply.github.com>
2021-09-06 19:11:12 +0200

    Fix whitespace

--- ports/concurrencpp/portfile.cmake
+++ ports/concurrencpp/portfile.cmake
@@ -21,6 +21,6 @@ vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/concurrencpp-0.1.2 )
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/concurrencpp-0.1.2)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/concurrencpp RENAME copyright)
