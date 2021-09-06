12f6dff66913be769f2f0969d83046d27460eacc
JafarAbdi <cafer.abdi@gmail.com>
2021-09-06 19:10:19 +0300

    [gflags]: Fix targets gflags::* not globally visible

--- ports/gflags/vcpkg-cmake-wrapper.cmake
+++ ports/gflags/vcpkg-cmake-wrapper.cmake
@@ -4,6 +4,7 @@ _find_package(${ARGS})
 
 foreach(tgt gflags gflags_shared gflags_static)
     if (NOT TARGET ${tgt} AND TARGET "gflags::${tgt}")
+        set_target_properties("gflags::${tgt}" PROPERTIES IMPORTED_GLOBAL TRUE)
         add_library(${tgt} ALIAS "gflags::${tgt}")
     endif() 
 endforeach(tgt)
