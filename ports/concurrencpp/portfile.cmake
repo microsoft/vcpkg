bcda25009f2c38dd09865aa93967cec0862e16d5
chausner <chausner@users.noreply.github.com>
2021-09-08 21:03:03 +0200

    Adapt for version 0.1.3, address suggestions

--- ports/concurrencpp/portfile.cmake
+++ ports/concurrencpp/portfile.cmake
@@ -1,15 +1,10 @@
-if (VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO David-Haim/concurrencpp
-  REF v.0.1.2
-  SHA512 48e3acca73c7b06940d41e84334cec56e7657c45f862d53e8e4ca1fb76b792c5b136ce4eeaf8afb8db95e746442fb45031c2c695ce60fcbe92ce91e5e1e80b25
+  REF v.0.1.3
+  SHA512 2f4530ba93d768a7a1ae14c532c8ef443745e48cceeca8a0e9da9f91633876ae4971caad70eeff9e18c7a45e8cf7c0b7bb79720a62026850244fb2377ad10df7
   HEAD_REF master
   PATCHES
-    make-linkage-configurable.patch
     fix-include-path.patch
 )
 
@@ -21,6 +16,6 @@ vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/concurrencpp-0.1.2)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/concurrencpp-0.1.3)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/concurrencpp RENAME copyright)
