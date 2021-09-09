f80fc82ff61ea19621f4ac4603d342d1344d5b9b
Jonliu1993 <13720414433@163.com>
2021-09-09 17:48:43 +0800

    [libssh/libssh2/libssh2] update to the latest version

--- ports/libssh2/portfile.cmake
+++ ports/libssh2/portfile.cmake
@@ -1,14 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libssh2/libssh2
-    REF 42d37aa63129a1b2644bf6495198923534322d64
+    REF 635caa90787220ac3773c1d5ba11f1236c22eae8 #v1.10.0
     SHA512 e86c0787e2aa7be5e9f19356e543493e53c7d1b51b585c46facfb05f769e6491209f820b207bf594348f4760c492c32dda3fcc94fc0af93cb09c736492a8e231
     HEAD_REF master
-    PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-Fix-UWP.patch"
+    PATCHES 
+        0001-Fix-UWP.patch
+        fix-dellexport.patch
+        fix-pkgconfig.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_EXAMPLES=OFF
         -DBUILD_TESTING=OFF
@@ -17,16 +20,14 @@ vcpkg_configure_cmake(
         -DENABLE_DEBUG_LOGGING=OFF
 )
 
-vcpkg_install_cmake()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share)
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/libssh2)
-
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libssh2 RENAME copyright)
-
+vcpkg_cmake_install()
+vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libssh2)
 vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
