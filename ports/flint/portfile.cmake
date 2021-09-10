b098fe4639d62169f3fbad36f39cb59f1d5bf217
NancyLi1013 <lirui09@beyondsoft.com>
2021-09-09 19:50:26 -0700

    [flint] Handle dllimport macro in header files

--- ports/flint/portfile.cmake
+++ ports/flint/portfile.cmake
@@ -23,11 +23,17 @@ vcpkg_cmake_configure(
     OPTIONS
         -DPYTHON_EXECUTABLE=${PYTHON3}
         -DWITH_NTL=OFF
-        -DWITH_CBLAS=OFF     
+        -DWITH_CBLAS=OFF 
 )
 
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic" AND VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/flint/flint-config.h"
+        "#elif defined(MSC_USE_DLL)" "#elif 1"
+    )
+endif()
 
 file(INSTALL "${SOURCE_PATH}/gpl-2.0.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
