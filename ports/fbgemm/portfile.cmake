c534c19c0cb3c30cbb06653263bcb4f1172dfa7b
luncliff <luncliff@gmail.com>
2021-09-11 19:19:29 +0900

    [fbgemm] fix find_package
    * install internal header for pytorch

--- ports/fbgemm/portfile.cmake
+++ ports/fbgemm/portfile.cmake
@@ -12,18 +12,20 @@ vcpkg_from_github(
         fix-cmakelists.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DUSE_SANITIZER=OFF
         -DFBGEMM_BUILD_TESTS=OFF
         -DFBGEMM_BUILD_BENCHMARKS=OFF
         -DPYTHON_EXECUTABLE=${PYTHON3} # inject the path instead of find_package(Python)
 )
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/${PORT})
+vcpkg_cmake_config_fixup(PACKAGE_NAME fbgemmLibrary CONFIG_PATH share/cmake/${PORT})
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+# this internal header is required by pytorch
+file(INSTALL     "${SOURCE_PATH}/src/RefImplementations.h"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/include/fbgemm/src")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
