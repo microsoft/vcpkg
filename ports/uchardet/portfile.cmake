895d7305b8e040d1f85de41fd62189f0ae028d20
myd7349 <myd7349@gmail.com>
2021-09-12 21:04:44 +0800

    [uchardet] Fix UWP build

--- ports/uchardet/portfile.cmake
+++ ports/uchardet/portfile.cmake
@@ -2,21 +2,17 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://gitlab.freedesktop.org/uchardet/uchardet
     REF 6f38ab95f55afd45ee6ccefcb92d21034b4a2521
+    PATCHES
+        fix-uwp-build.patch
 )
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-	FEATURES
+    FEATURES
         tool BUILD_BINARY
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
-
-if(VCPKG_TARGET_IS_UWP)
-    # uchardet calls `fopen` and `strdup`, which makes UWP unhappy.
-    set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE")
-    set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE")
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
