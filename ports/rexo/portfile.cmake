11dd894f52ef38379b5ba4d9dde7749fc318d452
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-09-12 23:46:31 -0400

    [ports/rexo] Use new format

--- ports/rexo/portfile.cmake
+++ ports/rexo/portfile.cmake
@@ -6,14 +6,14 @@ vcpkg_from_github(
     HEAD_REF "main"
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DREXO_BUILD_TESTS=OFF
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Rexo)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Rexo)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 configure_file("${SOURCE_PATH}/UNLICENSE" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
