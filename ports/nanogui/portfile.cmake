b9440ef03eac8fa9ace7505ec4ef4c2dd7c7a824
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2021-09-14 08:33:29 -0400

    [ports/nanogui] Bring in changes from @JackBoosY

--- ports/nanogui/portfile.cmake
+++ ports/nanogui/portfile.cmake
@@ -7,20 +7,22 @@ vcpkg_from_github(
     SHA512 0b0d0d077079e1a7186ec2cc640dbd48aebcefa0e1e0caa476128c6396b33d236cb5e8aaada4e1c8868a5d4aaefd90178206dd1569abab63c84af26d3309e2a5
     HEAD_REF master
     PATCHES
-         fix-package-seeking.diff
+        fix-dependencies.patch
+#         fix-package-seeking.diff
 #        fix-cmakelists.patch
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DNANOGUI_EIGEN_INCLUDE_DIR=${CURRENT_INSTALLED_DIR}/include/eigen3
-        -DEIGEN_INCLUDE_DIR=${CURRENT_INSTALLED_DIR}/include/eigen3
+        -DNANOGUI_BUILD_EXAMPLES=OFF
+        -DNANOGUI_BUILD_PYTHON=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
