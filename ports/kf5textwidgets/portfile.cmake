f446c96825700be67555d1e9e25191938161c9bf
Dawid Wróbel <me@dawidwrobel.com>
2021-09-14 03:46:28 +0100

    [kf5textwidgets] new port (#19766)
    * Add kf5textwidgets port
    
    * Fixed kf5textwidgets for windows
    
    * [kf5*] update to 5.75.0
    
    * [kf5textwidgets] convert manifest to JSON
    
    * [kf5textwidgets] use gettext port tools
    
    * [kf5textwidgets] use current spec syntax
    
    * [kf5textwidgets] remove redundant options
    
    * [kf5textwidgets] update to 5.84
    
    * [kf5textwidgets] only remove what's needed
    
    * [kf5textwidgets] wrap paths in quotes
    
    * [kf5textwidgets] default data location is OK
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] DISABLE_PARALLEL_CONFIGURE
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] fix Windows static builds
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] remove /bin when building static
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] add PACKAGE_NAME to vcpkg_cmake_config_fixup
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] libiconv patches only for windows static builds
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] cleanup, iconv was fixed in kf5i18n
    
    * [kf5textwidgets] update versions
    
    * [kf5textwidgets] use generic, non-semver versioning
    
    * [kf5textwidgets] update versions
    
    Co-authored-by: Kuntal Majumder <hellozee@disroot.org>

---
+++ ports/kf5textwidgets/portfile.cmake
@@ -0,0 +1,28 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/ktextwidgets
+    REF v5.84.0
+    SHA512 39afc3265c8aed26f78c836691548cafca05f31238e11f6d29e497c78b6e809d9dba5d3f6cbb9425cfe84d2a1d0910165e77c7841d833cccee3c7398e39bfc68
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    DISABLE_PARALLEL_CONFIGURE
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DKDE_INSTALL_QTPLUGINDIR=plugins
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5TextWidgets CONFIG_PATH lib/cmake/KF5TextWidgets)
+vcpkg_copy_pdbs()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(INSTALL "${SOURCE_PATH}/LICENSES/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
