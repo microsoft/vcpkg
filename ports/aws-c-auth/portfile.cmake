92af1ff59a651c1cf43aca48f54369a86d40d81c
Charles-Auguste Marois <cmarois@coveo.com>
2021-09-14 22:06:34 -0400

    [aws-sdk-cpp] update to 1.9.96 (#20033)
    * [aws-c-common] Update to 0.6.9
    
    * [aws-checksums] Update to 0.1.11
    
    * [aws-c-event-stream] Upgrade to 0.2.7
    
    * Add aws-c-cal port
    
    * Add s2n port
    
    * Add port aws-c-io
    
    * [aws-c-event-stream] Update port to 0.2.7
    
    * Update aws-sdk-cpp port
    
    * Add aws-c-compression port
    
    * Add aws-c-auth port
    
    * Add aws-c-http port
    
    * Add aws-c-mqtt port
    
    * Add aws-c-s3 port
    
    * Add aws-crt-cpp port
    
    * [aws-sdk-cpp] Update port to 1.9.91 version
    
    * Fix port issues.
    
    * Upgrade aws-sdk-cpp to 1.9.96
    Upgrade aws-crt-cpp to 0.15.1
    
    * Support x64-windows
    
    * Fix format issues
    
    * Specify supported triplets for aws libs
    
    * Specify platforms for more aws libs
    
    * Support x64-windows-static triplet.
    
    * Fix format issue for aws-c-common
    
    * Update port versions file
    
    * Use new vcpkg cmake methods and format
    
    * Use new vcpkg cmake methods and format
    
    * update version
    
    * Fix s2n port typo
    
    * Fix s2n port typo
    
    Co-authored-by: Jonliu1993 <13720414433@163.com>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/aws-c-auth/portfile.cmake
+++ ports/aws-c-auth/portfile.cmake
@@ -1,35 +1,35 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO awslabs/aws-checksums
-    REF 99bb0ad4b89d335d638536694352c45e0d2188f5 # v0.1.11
-    SHA512 cb9c249496fd41fda1efb9330e823d8b965adca6c8f372a50fe97eda821e277780bf9af8f5977102c44121568993cca55edbb750967b41f323e07e06a93c50a8
+    REPO awslabs/aws-c-auth
+    REF 61b6524960ad5e0c7aa2e38b343425d5941781bf # v0.6.3
+    SHA512 b5dda92e4a8796f3f1b8e2d326f57979a673f57325c921cdbc9c44273ada2f2a8eb6723f0292d223175ba4cca24508d2b635fad2af5ec7dd9e7b06db9588ede6
     HEAD_REF master
     PATCHES fix-cmake-target-path.patch
 )
-
-if (VCPKG_CRT_LINKAGE STREQUAL static)
-    set(STATIC_CRT_LNK ON)
-else()
-    set(STATIC_CRT_LNK OFF)
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
     OPTIONS
-        "-DSTATIC_CRT=${STATIC_CRT_LNK}"
         "-DCMAKE_MODULE_PATH=${CURRENT_INSTALLED_DIR}/share/aws-c-common" # use extra cmake files
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/aws-checksums/cmake)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/aws-c-auth/cmake)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE 
+        "${CURRENT_PACKAGES_DIR}/bin" 
+        "${CURRENT_PACKAGES_DIR}/debug/bin"
+    )
+endif()
 
 file(REMOVE_RECURSE
 	"${CURRENT_PACKAGES_DIR}/debug/include"
-	"${CURRENT_PACKAGES_DIR}/debug/lib/aws-checksums"
-	"${CURRENT_PACKAGES_DIR}/lib/aws-checksums"
-)
+	"${CURRENT_PACKAGES_DIR}/debug/lib/aws-c-auth"
+	"${CURRENT_PACKAGES_DIR}/lib/aws-c-auth"
+	)
 
 vcpkg_copy_pdbs()
 
