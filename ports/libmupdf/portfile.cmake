0989cd73dfe9804e5e19e209faeee2a2339b546d
JackBoosY <yuzaiyang@beyondsoft.com>
2021-09-16 00:54:33 -0700

    Add features

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -1,4 +1,4 @@
-vcpkg_fail_port_install(ON_TARGET "osx")
+vcpkg_fail_port_install(ON_TARGET "osx" ON_ARCH "arm")
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
@@ -9,20 +9,63 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+vcpkg_from_github(
+    OUT_SOURCE_PATH MUJS_SOURCE_PATH
+    REPO ArtifexSoftware/mujs
+    REF 6871e5b41c07558b17340f985f2af39717d3ba77
+    SHA512 d2d294099c66f36acabd6ccb49238253752c8e7a60cf34c86f34c21cc86a8bc8a64a79042fa9dd28db072fe8142eccf105ff75fad2b808e881f6a47cec80d933
+    HEAD_REF master
+    PATCHES fix-win-build.patch
+)
+
+file(REMOVE_RECURSE "${SOURCE_PATH}/thirdparty/mujs")
+file(RENAME "${MUJS_SOURCE_PATH}" "${SOURCE_PATH}/thirdparty/mujs")
+
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/gen_font_objs.bat" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        gentool     INSTALL_GEN_TOOLS
+        mudraw      BUILD_MUDRAW
+        mujs        WITH_MUJS
+        mupdf       BUILD_MUPDF
+        muthreads   BUILD_MUTHREADS
+        mutool      BUILD_MUTOOL
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
+        ${FEATURE_OPTIONS}
         -DBUILD_EXAMPLES=OFF
+        -DTARGET_TRIPLET=${TRIPLET_SYSTEM_ARCH}
+        -DBIN2COFF_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/${PORT}/bin2coff${VCPKG_HOST_EXECUTABLE_SUFFIX}
+        -DHEXDUMP_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/${PORT}/hexdump${VCPKG_HOST_EXECUTABLE_SUFFIX}
 )
 
-vcpkg_cmake_install()
-
-file(COPY "${SOURCE_PATH}/include/mupdf" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_copy_pdbs()
+vcpkg_cmake_install()
+
+set(EXTRA_TOOLS)
+if ("gentool" IN_LIST FEATURES)
+    list(APPEND EXTRA_TOOLS bin2coff hexdump)
+endif()
+if ("mudraw" IN_LIST FEATURES)
+    list(APPEND EXTRA_TOOLS mudraw)
+endif()
+if ("mupdf" IN_LIST FEATURES)
+    list(APPEND EXTRA_TOOLS mupdf)
+endif()
+if ("mutool" IN_LIST FEATURES)
+    list(APPEND EXTRA_TOOLS mutool)
+endif()
+
+if (EXTRA_TOOLS)
+    vcpkg_copy_tools(TOOL_NAMES ${EXTRA_TOOLS} AUTO_CLEAN)
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
