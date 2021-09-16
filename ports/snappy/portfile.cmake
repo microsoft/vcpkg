651b9538cf8a60e4d3750bc0177cbdcfb146ef28
NancyLi1013 <lirui09@beyondsoft.com>
2021-09-16 01:55:04 -0700

    [snappy] Update to 1.1.9

--- ports/snappy/portfile.cmake
+++ ports/snappy/portfile.cmake
@@ -1,22 +1,28 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/snappy
-    REF 537f4ad6240e586970fe554614542e9717df7902 # 1.1.8
-    SHA512 555d3b69a6759592736cbaae8f41654f0cf14e8be693b5dde37640191e53daec189f895872557b173e905d10681ef502f3e6ed8566811add963ffef96ce4016d
+    REF 2b63814b15a2aaae54b7943f0cd935892fae628f # 1.1.9
+    SHA512 1494596d472de5fbee086ab6ab0586c83b14357fa735cf15ebdba63838a30e1183d33cd089dda8e7c9ee680535c2527ee1c22b5de6a2156e6c86ec8f748454a5
     HEAD_REF master
+    PATCHES
+        fix-cmakelists.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DSNAPPY_BUILD_TESTS=OFF
-        -DCMAKE_DEBUG_POSTFIX=d)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        test        SNAPPY_BUILD_TESTS
+        benchmark   SNAPPY_BUILD_BENCHMARKS
+)
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Snappy)
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Snappy)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/snappy)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/snappy/COPYING ${CURRENT_PACKAGES_DIR}/share/snappy/copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
