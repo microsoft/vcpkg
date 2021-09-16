ea85a8b814d7174374f0a26ce803651cbea6b9ce
Be <be@mixxx.org>
2021-09-15 19:16:46 -0500

    [qtkeychain] add features to build with either Qt5 or Qt6
    and update to 0.12.0

--- ports/qtkeychain/portfile.cmake
+++ ports/qtkeychain/portfile.cmake
@@ -1,12 +1,28 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO frankosterfeld/qtkeychain
-    REF 6743abd98586fbabd01da9839f53f61ccfb7f83c # v0.11.1
-    SHA512 0ad6b82b972ca1cc5f1f8318899637ce0a6786f912b7f9efc1b7eea132ccefbe9a5dc0eb82d0dc9a020bcd55cd538d9e962fc40eb5c828142a7f2186b19633b1
+    REF v0.12.0
+    SHA512 ad8f7b3e8f59894a09892aeb78118f5ed93aa4593eece782c1879a4f3c37d9d63e8d40ad4b2e6a2e286e0da39f45cd4ed46181a1a05c078a59134114b2456a03
     HEAD_REF master
 )
 
-list(APPEND QTKEYCHAIN_OPTIONS -DCMAKE_DEBUG_POSTFIX=d)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    qt5 QT5
+    qt6 QT6
+)
+
+# Force the user to explicitly specify whether they want to link Qt5 or Qt6 rather
+# than setting a default because accidentally pulling in the wrong dependency could waste
+# a lot of time building.
+if(NOT QT5 AND NOT QT6)
+  message(FATAL_ERROR "Either qt5 or qt6 feature must be selected for qtkeychain.")
+endif()
+
+if(QT5 AND QT6)
+  message(FATAL_ERROR "qt5 and qt6 features cannot both be enabled for qtkeychain. Pick one or the other.")
+endif()
+
 list(APPEND QTKEYCHAIN_OPTIONS -DBUILD_TEST_APPLICATION:BOOL=OFF)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     list(APPEND QTKEYCHAIN_OPTIONS -DQTKEYCHAIN_STATIC:BOOL=ON)
@@ -20,16 +36,23 @@ else()
     list(APPEND QTKEYCHAIN_OPTIONS -DBUILD_TRANSLATIONS:BOOL=OFF)
 endif()
 
-vcpkg_configure_cmake(
+if(QT6)
+    list(APPEND QTKEYCHAIN_OPTIONS -DBUILD_WITH_QT6:BOOL=ON)
+endif()
+
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
 	OPTIONS ${QTKEYCHAIN_OPTIONS}
 )
-
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Qt5Keychain TARGET_PATH share/Qt5Keychain)
+if (QT5)
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Qt5Keychain)
+else()
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Qt6Keychain)
+endif()
 # Remove unneeded dirs
 file(REMOVE_RECURSE 
 	${CURRENT_PACKAGES_DIR}/debug/include
