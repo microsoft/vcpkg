d38ce66130386f3e5c88766bf0f4fac59ed7e482
Cheney Wang <38240633+Cheney-W@users.noreply.github.com>
2021-09-16 10:50:15 -0700

    [amqpcpp/leptonica/glibmm] Update to latest version (#20069)
    * [amqpcpp/leptonica/glibmm] Update to latest version
    
    * Modify the include dir path
    
    * Modify version schemes
    
    Co-authored-by: Cheney-Wang <v-xincwa@microsoft.com>

--- ports/glibmm/portfile.cmake
+++ ports/glibmm/portfile.cmake
@@ -2,9 +2,9 @@
 vcpkg_fail_port_install(ON_TARGET "UWP")
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/glibmm/2.68/glibmm-2.68.0.tar.xz"
-    FILENAME "glibmm-2.68.0.tar.xz"
-    SHA512 a13121052315e949acf2528e226079f1a2cf7853080aec770dcb269e422997e5515ed767c7a549231fb3fa5f913b3fd9ef083080589283824b6a218d066b253e
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/glibmm/2.68/glibmm-2.68.1.tar.xz"
+    FILENAME "glibmm-2.68.1.tar.xz"
+    SHA512 ca164f986da651e66bb5b98a760853e73d57ff84e035809d4c3b2c0a1b6ddf8ca68ffc49a71d0e0b2e14eca1c00e2e727e3bf3821e0b2b3a808397c3d33c6d5c
 )
 
 vcpkg_extract_source_archive_ex(
@@ -24,5 +24,5 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 # Handle copyright and readme
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/README DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME readme.txt)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME readme.txt)
