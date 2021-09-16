66533880b75f1ea59456805e01004450c8fa988e
Cheney Wang <38240633+Cheney-W@users.noreply.github.com>
2021-09-16 10:45:05 -0700

    [cppad/cpuid] Update to latest version (#20187)
    * [cppad/cpuid] Update to latest version
    
    * Add quotes to SOURCE_PATH
    
    Co-authored-by: Cheney-Wang <v-xincwa@microsoft.com>

--- ports/cpuid/portfile.cmake
+++ ports/cpuid/portfile.cmake
@@ -4,25 +4,23 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO anrieff/libcpuid
-    REF f2ab8b7ef2c286f619d96c3ce8902cb76b801bf0
-    SHA512 fcd2d35994554eed80c04315f1cf3bc91f272a5051dde040fe2266d71af902b60ecfd74b6f9dc8284a22f222208c6789bfb94cc12d61de17d605265d3cd2c43d
+    REF 179fbcb713566c2084a1903729b6eebba68a4424 #0.5.1
+    SHA512 76eef8147a9ed2f116ddfd07ab95f76126bee0e23d0e6b5be54d157a1456b769fb698a4799a43c5f81ac12368d76fe8904bff735a698342ad8ee41d43e72645b
     HEAD_REF master
-    PATCHES fix-install-headers.patch
 )
 
-vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        PREFER_NINJA
-        OPTIONS
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
         -DENABLE_DOCS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/cpuid)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cpuid)
 
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
