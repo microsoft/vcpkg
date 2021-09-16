3adc3af3a15492a90e31ef0e960c14120abf3438
Park DongHa <luncliff@gmail.com>
2021-09-16 21:28:54 +0900

    [gemmlowp] create a new port
    * update baseline JSON

---
+++ ports/gemmlowp/portfile.cmake
@@ -0,0 +1,20 @@
+if(NOT VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/gemmlowp
+    REF f9959600daa42992baace8a49544a00a743ce1b6
+    SHA512 017966e3cb23cf27097e16a648e42aa7daa7032a4a62723ef86c66f51371d325680740e1fa54b54b207ad8de68525bf1384e42ab640daefd81b012a36ade2ea7
+    PATCHES
+        support-install.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/contrib"
+)
+vcpkg_cmake_install()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
