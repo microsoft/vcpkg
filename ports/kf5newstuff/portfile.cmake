3e5d5a36a4e0505033e73978f7f899f68e1a892e
Dawid Wróbel <me@dawidwrobel.com>
2021-09-17 23:16:48 +0200

    [kf5newstuff] fix static builds - continued

--- ports/kf5newstuff/portfile.cmake
+++ ports/kf5newstuff/portfile.cmake
@@ -5,7 +5,8 @@ vcpkg_from_github(
     SHA512 107f8c7915f5a7656ed729d03b59e5f53f8751d771487b96e517358f1149801981e27d5cafdf86f759be539305fc8f847c51a28a35947766200cc912072f5c8d
     HEAD_REF master
     PATCHES
-        fix_static_builds.patch
+        fix_static_builds.patch     # https://invent.kde.org/frameworks/knewstuff/-/merge_requests/139
+        fix_static_builds2.patch    # https://invent.kde.org/frameworks/knewstuff/-/merge_requests/140
 )
 
 vcpkg_cmake_configure(
@@ -19,7 +20,8 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME KF5NewStuff CONFIG_PATH lib/cmake/KF5NewStuff DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(PACKAGE_NAME KF5NewStuffCore CONFIG_PATH lib/cmake/KF5NewStuffCore)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5NewStuffCore CONFIG_PATH lib/cmake/KF5NewStuffCore DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF5NewStuffQuick CONFIG_PATH lib/cmake/KF5NewStuffQuick)
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools(
