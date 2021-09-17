66fc2038ac8eb9c2fb3dd53ab8b61d3a8fc13d95
Dawid Wróbel <me@dawidwrobel.com>
2021-09-17 19:06:02 +0200

    [kf5kcmutils] fix cmake config for static builds

--- ports/kf5kcmutils/portfile.cmake
+++ ports/kf5kcmutils/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v5.84.0
     SHA512 e5f6347416143775e660430d582db3a60153b75063e7079bb3743043132f2e2f0d01234229f5eb1b4678e29d6981d03bd826622924ec7e385900df9067676f5b
     HEAD_REF master
+    PATCHES
+        fix_cmake_config.patch
 )
 
 vcpkg_cmake_configure(
@@ -18,8 +20,8 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME KF5KCMUtils CONFIG_PATH lib/cmake/KF5KCMUt
 
 vcpkg_copy_pdbs()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")	
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")	
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
