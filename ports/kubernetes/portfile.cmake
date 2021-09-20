39a80abe0737b8d1e0470761df3379daafda5535
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2021-09-20 08:56:20 -0300

    Use vcpkg_from_github with a valid hash Remove usage file

--- ports/kubernetes/portfile.cmake
+++ ports/kubernetes/portfile.cmake
@@ -1,9 +1,11 @@
 vcpkg_fail_port_install(ON_TARGET "UWP")
 
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL https://github.com/kubernetes-client/c.git
+    REPO kubernetes-client/c
     REF f6465c8fb099f2c8fcf1f84edd5e880610600f10
+    SHA512 e95a843028a352b1db881be483de5838d8795491e3125598432c4fcc10650aa9c733f96f6a63ec7fe0b1612274df46241b05cc59d0aa569a6c46b7c9db56221c
+    HEAD_REF master
     PATCHES
         001-fix-destination.patch
 )
@@ -23,4 +25,3 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_copy_pdbs()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-file(INSTALL ${CURRENT_PORT_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
