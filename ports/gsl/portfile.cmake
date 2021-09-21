a4bff21b3337aa7fe405f803fc596178db0fb1ce
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2021-09-20 19:32:27 -0500

    Updating portfile to remove deprecated functions
    These were the github-actions bot recommendations

--- ports/gsl/portfile.cmake
+++ ports/gsl/portfile.cmake
@@ -16,13 +16,12 @@ vcpkg_extract_source_archive_ex(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS_DEBUG -DINSTALL_HEADERS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
