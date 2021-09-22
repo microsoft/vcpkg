37629682b0ec9274dd4ed327761ac5b51693b650
Spirrwell <Spirrwell@gmail.com>
2021-09-22 01:37:41 -0400

    [enet] Export config, support BUILD_SHARED_LIBS, and add usage

--- ports/enet/portfile.cmake
+++ ports/enet/portfile.cmake
@@ -16,9 +16,11 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_copy_pdbs()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
