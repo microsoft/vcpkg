1abd32bd671b245687e285740261fb4256319133
Carlos O'Ryan <coryan@users.noreply.github.com>
2021-09-22 13:09:55 -0700

    [protobuf] upgrade to latest release (v3.18.0) (#20208)
    * [protobuf] upgrade to latest release (v3.18.0)
    
    This requires patching OpenCV as it uses a function removed in the
    latest version of protobuf (FWIW, upstream OpenCV has a similar patch).
    
    * The arcus port also needs a patch
    
    * The caffe2 port also needs a patch
    
    * The brpc port also needs a patch

--- ports/arcus/portfile.cmake
+++ ports/arcus/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 617f6f71572090f73cb44592b12f49567b539e5b #v4.10.0
     SHA512 cf0954d8b10d9f94165aa5c086d0e58c2925464f9fbe4252535c36d7e6bb12b767d89efb816c9e642f9cd7f0ec0d66d61ca21c5121a05340499d38d5d851f73b
     HEAD_REF master
+    PATCHES
+        0001-fix-protobuf-deprecated.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ENABLE_STATIC)
