4bf439b1cec76bb146b205204c2c42ae3d11c1ac
Kai Pastor <dg0yt@darc.de>
2021-09-23 19:36:37 +0200

    [qt5-declarative] Fix native mingw build

--- ports/qt5-declarative/portfile.cmake
+++ ports/qt5-declarative/portfile.cmake
@@ -1,2 +1,11 @@
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
-qt_submodule_installation(PATCHES limits_include.patch)
+set(BUILD_OPTIONS "")
+if(CMAKE_HOST_WIN32 AND VCPKG_TARGET_IS_MINGW)
+    set(ENV{MSYS2_ARG_CONV_EXCL} "--foreign-types=")
+    list(APPEND BUILD_OPTIONS "-no-d3d12")
+endif()
+qt_submodule_installation(
+    # PATCHES must be first, or vcpkg_configure_qmake will take it as BUILD_OPTIONS.
+    PATCHES limits_include.patch
+    BUILD_OPTIONS ${BUILD_OPTIONS}
+)
