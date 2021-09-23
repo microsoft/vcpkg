75380bc455b198599d16266aec2ae31ea1468c6a
Marco Correia <marco.v.correia@gmail.com>
2021-09-23 08:40:21 +0100

    Update ports/argparse/portfile.cmake
    Co-authored-by: nicole mazzuca <83086508+strega-nil-ms@users.noreply.github.com>

--- ports/argparse/portfile.cmake
+++ ports/argparse/portfile.cmake
@@ -3,7 +3,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO p-ranav/argparse
-    REF b0cb28ab177cdbd19d439c02cdc217bdfc508ba9 #2021-09-14
+    REF b0cb28ab177cdbd19d439c02cdc217bdfc508ba9 # v2.2
     SHA512 247f27aa45994ee4629b947e3fe1b1df8fae4e773992c740a7c3d544e45ce83a68ecdcfdedf38c461ccb81cf93d88dfee21dc4f7d027677a3c7d930170446971
     HEAD_REF master
 )
