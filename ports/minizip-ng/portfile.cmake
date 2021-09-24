6342b74ef400be29c54ca3c77966a3352cc25c05
Christian Fersch <christian.fersch@ptvgroup.com>
2021-09-24 11:11:19 +0200

    [minizip-ng] Make dependencies optional

--- ports/minizip-ng/portfile.cmake
+++ ports/minizip-ng/portfile.cmake
@@ -8,18 +8,30 @@ vcpkg_from_github(
         Modify-header-file-path.patch
 )
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        zlib MZ_ZLIB
+        bzip2 MZ_BZIP2
+        lzma MZ_LZMA
+        zstd MZ_ZSTD
+        openssl MZ_OPENSSL
+)
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA 
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS 
+        ${FEATURE_OPTIONS}
+        -DMZ_FETCH_LIBS=OFF
         -DMZ_PROJECT_SUFFIX:STRING=-ng
 )
 
 vcpkg_cmake_install()
 
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
