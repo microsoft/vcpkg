bd5b4cb6142b0f99eb820e0658b8d919aa08d6a0
chausner <chausner@users.noreply.github.com>
2021-09-27 21:48:14 +0200

    Switch to new CMake portfile commands

--- ports/libssh2/portfile.cmake
+++ ports/libssh2/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-Fix-UWP.patch"
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_EXAMPLES=OFF
@@ -17,7 +17,7 @@ vcpkg_configure_cmake(
         -DENABLE_DEBUG_LOGGING=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
@@ -25,10 +25,8 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/libssh2)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libssh2)
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libssh2" RENAME copyright)
 
 vcpkg_copy_pdbs()
-
-
