c3094da18c187957c6aafea370aaccd52cb22686
Dawid Wróbel <me@dawidwrobel.com>
2021-09-28 01:39:57 +0300

    [libopensp] initial port

---
+++ ports/libopensp/portfile.cmake
@@ -0,0 +1,28 @@
+set(OPENSP_VERSION 1.5.2)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://downloads.sourceforge.net/project/openjade/opensp/${OPENSP_VERSION}/OpenSP-${OPENSP_VERSION}.tar.gz"
+    FILENAME "OpenSP-${OPENSP_VERSION}.tar.gz"
+    SHA512 a7dcc246ba7f58969ecd6d107c7b82dede811e65f375b7aa3e683621f2c6ff3e7dccefdd79098fcadad6cca8bb94c2933c63f4701be2c002f9a56f1bbe6b047e
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    REF ${OPENSP_VERSION}
+)
+
+vcpkg_configure_make(
+    AUTOCONFIG
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        --disable-doc-build
+        --enable-http
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
