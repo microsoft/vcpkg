b154e5eeec8a4dbc7f469e8d0b9ae93dbe45d9a6
luncliff <dong-ha-park@linecorp.com>
2021-09-28 13:11:57 +0900

    [gemmlowp] support find_pacakge
    * export `unofficial::` targets

--- ports/gemmlowp/portfile.cmake
+++ ports/gemmlowp/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/contrib"
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
