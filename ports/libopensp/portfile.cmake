6d92c5a3c2f3e036be40319f62742cc737e4720a
Dawid Wróbel <me@dawidwrobel.com>
2021-09-28 03:41:02 +0300

    [libopensp] also remove debug/include

--- ports/libopensp/portfile.cmake
+++ ports/libopensp/portfile.cmake
@@ -25,4 +25,5 @@ vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
