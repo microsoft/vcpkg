591ccaa01c792558c49402523d3139b97e5e847b
luncliff <luncliff@gmail.com>
2021-09-28 22:07:37 +0900

    [gemmlowp] use latest sources
    * remove patches to support find_package

--- ports/gemmlowp/portfile.cmake
+++ ports/gemmlowp/portfile.cmake
@@ -5,15 +5,15 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/gemmlowp
-    REF f9959600daa42992baace8a49544a00a743ce1b6
-    SHA512 017966e3cb23cf27097e16a648e42aa7daa7032a4a62723ef86c66f51371d325680740e1fa54b54b207ad8de68525bf1384e42ab640daefd81b012a36ade2ea7
+    REF daf630d3d0c439dbe22229838a5ba1bc5f82908d
+    SHA512 8d1a422bbf2495dcec1c30147bc96d0a2ca494220a45050937f84567c3352cd8916f07d1bbab68c8d36ae7ce3e9b8bee8b518900e21967a3185bf7ff25812785
     HEAD_REF master
-    PATCHES
-        support-install.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/contrib"
+    OPTIONS
+        -DBUILD_TESTING=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
