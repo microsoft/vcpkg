87c52d958809a1806ca7df1d3d54e18aaa9eee5c
NancyLi1013 <lirui09@beyondsoft.com>
2021-09-28 02:20:00 -0700

    [arb] Update to 2.21.0

--- ports/arb/portfile.cmake
+++ ports/arb/portfile.cmake
@@ -3,24 +3,23 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO fredrik-johansson/arb
-    REF ae6009e3e19bd309a2433467d1b2ddb7001cd1eb # 2.18.1
-    SHA512 78e149f0d51ef8ab29afbad99fd24e3b59acfc509f626e89bdcd57d4a8478b84c3aa51e92f5e26f8a10a20c66d72d2eed50f0dfbfda4a5f5277988f9bac3fa48
+    REF bd68cc0219c5b4bf4a3466b5b6c190203d7dc7b0 # 2.21.0
+    SHA512 5e09ece4855906206c0f5aff9f56d13e1b2432863921af235682b9c45b35e03b1a7db308315bd329e9ed93be36d5e5aac914b1276b9412a551f5f2a189884404
     HEAD_REF master
 )
 
-file(REMOVE ${SOURCE_PATH}/CMakeLists.txt)
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(REMOVE "${SOURCE_PATH}/CMakeLists.txt")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 # Remove duplicate headers
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
