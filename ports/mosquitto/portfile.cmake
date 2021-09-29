microsoft/vcpkg

commit 5aec72b4f3f1ccda1176549f58abc24c7ee36ffe

[mosquitto] Update to 2.0.12 (#20148)
* update mosquitto to 2.0.12

* update json

* Update ports/mosquitto/vcpkg.json

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

* Update ports/libwebsockets/vcpkg.json

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

* add head ref

* remove plugins

* update jsons

* update libwebsockets

* Update versions/m-/mosquitto.json

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

* Update versions/l-/libwebsockets.json

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

* Update ports/mosquitto/portfile.cmake

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

* update version

Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>
Co-authored-by: Jonliu1993 <13720414433@163.com>

ports/mosquitto/portfile.cmake
@@ -1,16 +1,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO eclipse/mosquitto
-    REF e55f7facce7628b33e57d6b44cc8e9dd1042e624 # v1.6.8
-    SHA512 c192b53f52ce9dc8e02d31acd9e93c00cafbe543b038d7619e6b653f102126872bbd485c94604bca9287e71a5dfe0de2f4d8d3f51cdd5c37f90fd2a6535bd89b
     HEAD_REF master
+    REF v2.0.12
+    SHA512  bf502f4f8c5f8d9ae9ee6e6a417e3f9deea9933c69176903d460a1c91132658ebe093c8e9ed000cb579fe93f15626c15d8041ceb6438e15caca1d153a67975f9
     PATCHES
         0001-add-archive-destination-to-install.patch
         0002-win64-support.patch
         0003-add-find_package-libwebsockets.patch
         0004-support-static-build.patch
-        0005-add-mach.h-include.patch
+        0005-websocket-shared-lib-name.patch
 )
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" STATIC_WEBSOCKETS)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -18,14 +20,12 @@ vcpkg_configure_cmake(
     OPTIONS
         -DWITH_SRV=OFF
         -DWITH_WEBSOCKETS=ON
+        -DSTATIC_WEBSOCKETS=${STATIC_WEBSOCKETS}
         -DWITH_TLS=ON
         -DWITH_TLS_PSK=ON
         -DWITH_THREADING=ON
         -DDOCUMENTATION=OFF
-    OPTIONS_RELEASE
-        -DENABLE_DEBUG=OFF
-    OPTIONS_DEBUG
-        -DENABLE_DEBUG=ON
+        -DWITH_PLUGINS=OFF
 )
 
 vcpkg_install_cmake()

ports/libwebsockets/portfile.cmake
@@ -145,6 +145,7 @@ vcpkg_cmake_configure(
         -DLWS_WITH_HTTP2=ON
         -DLWS_WITH_HTTP_STREAM_COMPRESSION=ON # Since zlib is already a dependency
         -DLWS_WITH_LIBUV=ON
+        -DLWS_WITH_EXTERNAL_POLL=ON
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
 )

ports/mosquitto/0002-win64-support.patch
@@ -1,18 +1,8 @@
-From 675ba1de6c01b4050ae3cc60916d3f0a72b03105 Mon Sep 17 00:00:00 2001
-From: Nicole Mazzuca <mazzucan@outlook.com>
-Date: Tue, 22 Sep 2020 15:40:55 -0700
-Subject: [PATCH 2/5] win64 support
-
----
- CMakeLists.txt | 7 +------
- config.h       | 2 +-
- 2 files changed, 2 insertions(+), 7 deletions(-)
-
 diff --git a/CMakeLists.txt b/CMakeLists.txt
-index e11959c..1a92fe3 100644
+index 07eff849..3edc7efa 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
-@@ -57,12 +57,7 @@ option(WITH_THREADING "Include client library threading support?" ON)
+@@ -67,12 +67,7 @@ option(WITH_THREADING "Include client library threading support?" ON)
  if (WITH_THREADING)
  	add_definitions("-DWITH_THREADING")
  	if (WIN32)
@@ -23,22 +13,6 @@ index e11959c..1a92fe3 100644
 -		endif (CMAKE_CL_64)
 -		set (PTHREAD_INCLUDE_DIR C:\\pthreads\\Pre-built.2\\include)
 +		find_package(pthread REQUIRED)
- 	else (WIN32)
- 		find_library(LIBPTHREAD pthread)
- 		if (LIBPTHREAD)
-diff --git a/config.h b/config.h
-index 5cce39e..c673bf4 100644
---- a/config.h
-+++ b/config.h
-@@ -31,7 +31,7 @@
- #  define EPROTO ECONNABORTED
- #endif
- 
--#ifdef WIN32
-+#ifdef _WIN32
- #  ifndef strcasecmp
- #    define strcasecmp strcmpi
- #  endif
--- 
-2.24.3 (Apple Git-128)
-
+ 	elseif (ANDROID)
+ 		set (PTHREAD_LIBRARIES "")
+ 		set (PTHREAD_INCLUDE_DIR "")

ports/mosquitto/0004-support-static-build.patch
@@ -1,43 +1,11 @@
-From 4bb19e77e95bc505d32e098190ffbf056810017c Mon Sep 17 00:00:00 2001
-From: Nicole Mazzuca <mazzucan@outlook.com>
-Date: Tue, 22 Sep 2020 15:44:42 -0700
-Subject: [PATCH 4/5] support static build
-
----
- lib/CMakeLists.txt | 2 +-
- src/CMakeLists.txt | 8 ++++++--
- 2 files changed, 7 insertions(+), 3 deletions(-)
-
-diff --git a/lib/CMakeLists.txt b/lib/CMakeLists.txt
-index bd60b75..cc01a2a 100644
---- a/lib/CMakeLists.txt
-+++ b/lib/CMakeLists.txt
-@@ -60,7 +60,7 @@ if (UNIX AND NOT APPLE)
- endif (UNIX AND NOT APPLE)
+diff --git a/apps/mosquitto_passwd/CMakeLists.txt b/apps/mosquitto_passwd/CMakeLists.txt
+index 13a7d826..31069306 100644
+--- a/apps/mosquitto_passwd/CMakeLists.txt
++++ b/apps/mosquitto_passwd/CMakeLists.txt
+@@ -13,6 +13,10 @@ if (WITH_TLS)
+ 		)
  
- if (WIN32)
--	set (LIBRARIES ${LIBRARIES} ws2_32)
-+	set (LIBRARIES ${LIBRARIES} ws2_32 crypt32)
- endif (WIN32)
  
- if (WITH_SRV)
-diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
-index 7898ff5..f066aea 100644
---- a/src/CMakeLists.txt
-+++ b/src/CMakeLists.txt
-@@ -158,7 +158,7 @@ if (UNIX)
- endif (UNIX)
- 
- if (WIN32)
--	set (MOSQ_LIBS ${MOSQ_LIBS} ws2_32)
-+	set (MOSQ_LIBS ${MOSQ_LIBS} ws2_32 crypt32)
- endif (WIN32)
- 
- if (WITH_WEBSOCKETS)
-@@ -189,6 +189,10 @@ install(FILES mosquitto_broker.h mosquitto_plugin.h DESTINATION "${CMAKE_INSTALL
- 
- if (WITH_TLS)
- 	add_executable(mosquitto_passwd mosquitto_passwd.c)
 -	target_link_libraries(mosquitto_passwd ${OPENSSL_LIBRARIES})
 +	if(WIN32)
 +		target_link_libraries(mosquitto_passwd ${OPENSSL_LIBRARIES} ws2_32 crypt32)
@@ -46,6 +14,29 @@ index 7898ff5..f066aea 100644
 +	endif()
  	install(TARGETS mosquitto_passwd RUNTIME DESTINATION "${CMAKE_INSTALL_BINDIR}")
  endif (WITH_TLS)
--- 
-2.24.3 (Apple Git-128)
-
+diff --git a/lib/CMakeLists.txt b/lib/CMakeLists.txt
+index 5da221dc..c3e9fce0 100644
+--- a/lib/CMakeLists.txt
++++ b/lib/CMakeLists.txt
+@@ -69,7 +69,7 @@ if (UNIX AND NOT APPLE AND NOT ANDROID)
+ endif (UNIX AND NOT APPLE AND NOT ANDROID)
+ 
+ if (WIN32)
+-	set (LIBRARIES ${LIBRARIES} ws2_32)
++	set (LIBRARIES ${LIBRARIES} ws2_32 crypt32)
+ endif (WIN32)
+ 
+ if (WITH_SRV)
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 9380a046..62ce99e5 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -189,7 +189,7 @@ if (UNIX)
+ endif (UNIX)
+ 
+ if (WIN32)
+-	set (MOSQ_LIBS ${MOSQ_LIBS} ws2_32)
++	set (MOSQ_LIBS ${MOSQ_LIBS} ws2_32 crypt32)
+ endif (WIN32)
+ 
+ if (WITH_WEBSOCKETS)

ports/mosquitto/0005-add-mach.h-include.patch
@@ -1,27 +1,0 @@
-From af7d62dceb064b5a8458616536bc27329ad236b1 Mon Sep 17 00:00:00 2001
-From: Nicole Mazzuca <mazzucan@outlook.com>
-Date: Tue, 22 Sep 2020 15:46:06 -0700
-Subject: [PATCH 5/5] add mach.h include
-
----
- lib/mosquitto.c | 4 ++++
- 1 file changed, 4 insertions(+)
-
-diff --git a/lib/mosquitto.c b/lib/mosquitto.c
-index 795013e..cd00513 100644
---- a/lib/mosquitto.c
-+++ b/lib/mosquitto.c
-@@ -33,6 +33,10 @@ Contributors:
- #include "packet_mosq.h"
- #include "will_mosq.h"
- 
-+#if defined(__APPLE__)
-+#include <mach/mach_time.h>
-+#endif
-+
- 
- void mosquitto__destroy(struct mosquitto *mosq);
- 
--- 
-2.24.3 (Apple Git-128)
-

ports/mosquitto/0005-websocket-shared-lib-name.patch
@@ -1,0 +1,20 @@
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 9380a046..f2929e19 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -194,13 +194,13 @@ endif (WIN32)
+ 
+ if (WITH_WEBSOCKETS)
+ 	if (STATIC_WEBSOCKETS)
+-		set (MOSQ_LIBS ${MOSQ_LIBS} websockets_static)
++		set (MOSQ_LIBS ${MOSQ_LIBS} websockets)
+ 		if (WIN32)
+ 			set (MOSQ_LIBS ${MOSQ_LIBS} iphlpapi)
+ 			link_directories(${mosquitto_SOURCE_DIR})
+ 		endif (WIN32)
+ 	else (STATIC_WEBSOCKETS)
+-		set (MOSQ_LIBS ${MOSQ_LIBS} websockets)
++		set (MOSQ_LIBS ${MOSQ_LIBS} websockets_shared)
+ 	endif (STATIC_WEBSOCKETS)
+ endif (WITH_WEBSOCKETS)
+ 

commit 473f586acc6fd2e94dd7e61d284f839ad398c53c

Build fixes from 2021-09-28's nightly build.
https://dev.azure.com/vcpkg/public/_build/results?buildId=60445

PASSING, REMOVE FROM FAIL LIST: soundtouch:x64-windows-static
Probably fixed by: https://github.com/microsoft/vcpkg/pull/19377

REGRESSION: mosquitto:x86-windows
REGRESSION: mosquitto:x64-windows
Probably broken by: https://github.com/microsoft/vcpkg/pull/20148/

```
The following EXEs were found in /bin or /debug/bin. EXEs are not valid distribution targets.

    D:/packages/mosquitto_x86-windows/bin/mosquitto_ctrl.exe

The following EXEs were found in /bin or /debug/bin. EXEs are not valid distribution targets.

    D:/packages/mosquitto_x86-windows/debug/bin/mosquitto_ctrl.exe

Found 2 error(s). Please correct the portfile:
    C:\a\1\s\ports\mosquitto\portfile.cmake
-- Performing post-build validation done
```

The upstream build system has this:

```
option(WITH_CJSON "Build with cJSON support (required for dynamic security plugin and useful for mosquitto_sub)?" ON)
if (WITH_CJSON)
    FIND_PACKAGE(cJSON)
    if (CJSON_FOUND)
	    message(STATUS ${CJSON_FOUND})
    else (CJSON_FOUND)
	    message(STATUS "Optional dependency cJSON not found. Some features will be disabled.")
    endif(CJSON_FOUND)
endif()
```

and indeed, I repro the problem if cjson is installed first. Disable WITH_CJSON as a fix.

OSX never finishing: Hopefully fixed by https://github.com/microsoft/vcpkg/pull/20388

ports/mosquitto/portfile.cmake
@@ -26,6 +26,7 @@ vcpkg_configure_cmake(
         -DWITH_THREADING=ON
         -DDOCUMENTATION=OFF
         -DWITH_PLUGINS=OFF
+        -DWITH_CJSON=OFF
 )
 
 vcpkg_install_cmake()