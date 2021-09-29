9ebe56e994e209f88786d646193971a5bfd37ad2
chausner <15180557+chausner@users.noreply.github.com>
2021-09-29 17:46:03 +0200

    [cli11] Update to 2.1.0 (#20304)
    * Update cli11 to 2.1.0
    
    * Update CI baseline
    
    * Change version-semver to version for cli11
    
    * Update git-tree version
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>

--- ports/cli11/portfile.cmake
+++ ports/cli11/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CLIUtils/CLI11
-    REF 0d06d21b56eadef9dcb24907ee2866b2b6f0babe #version 2.0.0
-    SHA512 1952ce0e9fd3bc6646f00abdd7f0f7bae541c383b691a985b0f70ca53fe80a9acbcf68c07c59e021c92aae7b1e062fd7ffe4c9f9b198b95525dde3a1ce83c2f5
+    REF v2.1.0
+    SHA512 22c75e6b55d5320656ca2f65bf7c1dc82546fa65458f66a3d6401d002e83f12a0ee93a572637f5daa8daed9e0ce11fbf4aabf864caadc851f75bced63814ba10
     HEAD_REF master
 )
 
@@ -16,9 +16,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/CLI11)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/CLI11)
 vcpkg_fixup_pkgconfig()
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
