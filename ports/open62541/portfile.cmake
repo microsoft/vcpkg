5cd5a443114546977698144102033b4789bc8638
chausner <15180557+chausner@users.noreply.github.com>
2021-09-29 17:38:23 +0200

    [open62541] Update to 1.2.2 (#20399)
    * Update open62541 to 1.2.2
    
    * Update CI baseline
    
    * delete double quotes
    
    * update version
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>
    Co-authored-by: Jonliu1993 <13720414433@163.com>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/open62541/portfile.cmake
+++ ports/open62541/portfile.cmake
@@ -1,26 +1,27 @@
-set(VERSION v1.1)
+set(VERSION v1.2.2)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open62541/open62541
-    REF 8ad84962e67987af91d7f746425d7b26be47a0cb # v1.1.2
-    SHA512 7cca9458cdfbc386c0a7012373c91e9ab3a4876303db95a513f62cf26e751446bd73c0fa1f76e0821d2a47747bfb4612e8822389bc9002775636db8f8093f94c
+    REF "${VERSION}"
+    SHA512 E6A1EC2208EC29D8685D2A957FAE6F3FEDC0E847D6AB1BB8AC5C7980223BC377692334C87575956B53BB37A9B71C5DEDD1B5C4F19F122561543D04661FEFE1D5
     HEAD_REF master
 )
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    openssl UA_ENABLE_ENCRYPTION_OPENSSL
-    mbedtls UA_ENABLE_ENCRYPTION_MBEDTLS
-    amalgamation UA_ENABLE_AMALGAMATION
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        openssl UA_ENABLE_ENCRYPTION_OPENSSL
+        mbedtls UA_ENABLE_ENCRYPTION_MBEDTLS
+        amalgamation UA_ENABLE_AMALGAMATION
 )
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DOPEN62541_VERSION=${VERSION}
@@ -28,12 +29,12 @@ vcpkg_configure_cmake(
         -DCMAKE_DEBUG_POSTFIX=d
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/open62541/tools)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/open62541/tools")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
