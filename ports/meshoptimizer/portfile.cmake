79505da4877a996ae5a3746416f26f9b9086949a
chausner <15180557+chausner@users.noreply.github.com>
2021-09-29 17:36:24 +0200

    [meshoptimizer] Update to 0.16 (#20395)
    * Update meshoptimizer to 0.16
    
    * Update CI baseline
    
    * Address PR feedback
    
    * Update git-tree hash
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/meshoptimizer/portfile.cmake
+++ ports/meshoptimizer/portfile.cmake
@@ -1,29 +1,26 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeux/meshoptimizer
-    REF v0.14
-    SHA512 303b3bf1bed7cba8f89bce1c2782e3718fc8f4ec01f7ffd64f5ca23406130097f07d234b142916b16fe586db97c7deaa0ae9135b4e558543cc1664e7db85de67
+    REF v0.16
+    SHA512 CC6D28359FB99A615E1046A4AF1B247CBC6EA0266D9E7F41EA0516C1FC09FA1E67376071DAF138A126C77BCA3BAF9D565636ED0E3ADF045F1A08498C38B7A7E7
     HEAD_REF master
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIBS)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DMESHOPT_BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/meshoptimizer)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/meshoptimizer)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE.md
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
-    RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 vcpkg_copy_pdbs()
