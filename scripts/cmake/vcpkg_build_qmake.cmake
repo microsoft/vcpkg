378c76bc3d48094ee8d6d1cafbeb734d53391a93
JackBoosY <yuzaiyang@beyondsoft.com>
2021-09-28 23:15:53 -0700

    Apply suggestions

--- scripts/cmake/vcpkg_build_qmake.cmake
+++ scripts/cmake/vcpkg_build_qmake.cmake
@@ -26,13 +26,20 @@ function(vcpkg_build_qmake)
         "TARGETS;RELEASE_TARGETS;DEBUG_TARGETS"
     )
 
+    # Make sure that the linker finds the libraries used
+    set(env_path_backup "$ENV{PATH}")
+
+    # This fixes issues on machines with default codepages that are not ASCII compatible, such as some CJK encodings
+    set(env_cl_backup "$ENV{_CL_}")
+    set(ENV{_CL_} "/utf-8")
+
     if(CMAKE_HOST_WIN32)
         if (VCPKG_QMAKE_USE_NMAKE)
             find_program(nmake_executable nmake)
             set(invoke_command "${nmake_executable}")
-            get_filename_component(NMAKE_EXE_PATH "${nmake_executable}" DIRECTORY)
-            set(ENV{PATH} "$ENV{PATH};${NMAKE_EXE_PATH}")
-            set(ENV{CL} "$ENV{CL} /MP${VCPKG_CONCURRENCY}")
+            get_filename_component(nmake_exe_path "${nmake_executable}" DIRECTORY)
+            vcpkg_host_path_list(APPEND ENV{PATH} "${nmake_exe_path}")
+            vcpkg_host_path_list(APPEND ENV{CL} "/MP${VCPKG_CONCURRENCY}")
         else()
             vcpkg_find_acquire_program(JOM)
             set(invoke_command "${JOM}")
@@ -42,62 +49,47 @@ function(vcpkg_build_qmake)
         set(invoke_command "${make_executable}")
     endif()
 
-    # Make sure that the linker finds the libraries used
-    set(env_path_backup "$ENV{PATH}")
-
     file(TO_NATIVE_PATH "${CURRENT_INSTALLED_DIR}" NATIVE_INSTALLED_DIR)
 
     if(NOT arg_BUILD_LOGNAME)
         set(arg_BUILD_LOGNAME build)
     endif()
 
-    # This fixes issues on machines with default codepages that are not ASCII compatible, such as some CJK encodings
-    set(env_cl_backup "$ENV{_CL_}")
-    set(ENV{_CL_} "/utf-8")
-
     #Replace with VCPKG variables if PR #7733 is merged
-    vcpkg_list(SET buildtypes)
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-        set(build_type_name "DEBUG")
-        vcpkg_list(APPEND buildtypes "${build_type_name}")
-        set(short_name_${build_type_name} "dbg")
-        set(path_suffix_${build_type_name} "/debug")
-    endif()
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        set(build_type_name "RELEASE")
-        vcpkg_list(APPEND buildtypes "${build_type_name}")
-        set(short_name_${build_type_name} "rel")
-        set(path_suffix_${build_type_name} "")
-    endif()
+    set(short_name_debug "dbg")
+    set(path_suffix_debug "/debug")
+    set(targets_debug "${arg_DEBUG_TARGETS}")
+    
+    set(short_name_debug "rel")
+    set(path_suffix_debug "")
+    set(targets_debug "${arg_RELEASE_TARGETS}")
 
-    foreach(build_type IN LISTS buildtypes)
+    foreach(build_type IN ITEMS debug release)
         set(current_installed_prefix "${CURRENT_INSTALLED_DIR}${path_suffix_${build_type}}")
         set(current_installed_libpath "${current_installed_prefix}/lib/${VCPKG_HOST_PATH_SEPARATOR}${current_installed_prefix}/lib/manual-link/")
 
-        vcpkg_add_to_path(PREPEND "${current_installed_prefix}/bin")
-        vcpkg_add_to_path(PREPEND "${current_installed_prefix}/lib")
+        vcpkg_add_to_path(PREPEND "${current_installed_prefix}/lib" "${current_installed_prefix}/bin")
 
         # We set LD_LIBRARY_PATH ENV variable to allow executing Qt tools (rcc,...) even with dynamic linking
         if(CMAKE_HOST_UNIX)
             if(DEFINED ENV{LD_LIBRARY_PATH})
-                set(is_ld_library_path_defined TRUE)
-                set(ld_library_path_backup $ENV{LD_LIBRARY_PATH})
-                set(ENV{LD_LIBRARY_PATH} "${current_installed_libpath}${VCPKG_HOST_PATH_SEPARATOR}${ld_library_path_backup}")
+                set(ld_library_path_backup "$ENV{LD_LIBRARY_PATH}")
+                vcpkg_host_path_list(SET ENV{LD_LIBRARY_PATH} "${current_installed_libpath}" "${ld_library_path_backup}")
             else()
-                set(is_ld_library_path_defined FALSE)
-                set(ENV{LD_LIBRARY_PATH} "${current_installed_libpath}")
+                unset(ld_library_path_backup)
+                vcpkg_host_path_list(SET ENV{LD_LIBRARY_PATH} "${current_installed_libpath}")
             endif()
         endif()
         
-        vcpkg_list(APPEND arg_${build_type}_TARGETS ${arg_TARGETS})
+        vcpkg_list(SET targets ${targets_${build_type}} ${arg_TARGETS})
         if(NOT arg_SKIP_MAKEFILES)
-            z_run_jom_build(qmake_all makefiles ${short_name_${build_type}})
+            z_run_jom_build(qmake_all makefiles "${short_name_${build_type}}")
         endif()
-        z_run_jom_build("${arg_${build_type}_TARGETS}" ${arg_BUILD_LOGNAME} ${short_name_${build_type}})
+        z_run_jom_build("${targets}" "${arg_BUILD_LOGNAME}" "${short_name_${build_type}}")
 
         # Restore backup
         if(CMAKE_HOST_UNIX)
-            if(is_ld_library_path_defined)
+            if(DEFINED ld_library_path_backup)
                 set(ENV{LD_LIBRARY_PATH} "${ld_library_path_backup}")                
             else()
                 unset(ENV{LD_LIBRARY_PATH})
