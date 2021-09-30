670c013f8f49e34c19065ea774d483ed3484520e
Deniz Kilic <d.kilic@fz-juelich.de>
2021-09-30 12:37:43 +0200

    Update ezc3d-port to 1.4.6

--- ports/ezc3d/portfile.cmake
+++ ports/ezc3d/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(ARCHIVE
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pyomeca/ezc3d
-    REF Release_1.3.7
-    SHA512 5beb0909a4ddc56f5965b5f2edcfd2c8d68d473b172778ebe21bc134e1b4931cac1e6529676866d4238b41041658041a72ccd44879b9685d85f857a4e0df23ec
+    REF Release_1.4.6
+    SHA512 f63da7e715c09c6a757fe923fd397c09e1cbd0a58a78b1d8fa52bd1a41230ecab2cbb17ecc3d4f66656f3234bfe4c8588164f1d4964dcce729da091e99daab2d
     HEAD_REF dev
 )
 
@@ -11,15 +11,13 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         -DBUILD_EXAMPLE=OFF
+        -Dezc3d_LIB_FOLDER="lib"
+        -Dezc3d_BIN_FOLDER="bin"
 )
 
 vcpkg_install_cmake()
 
-if (VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/ezc3d/cmake")
-else()
-    vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake")
-endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH "CMake")
 
 # # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
