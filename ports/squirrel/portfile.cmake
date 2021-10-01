3fcef0e91adec736cb500488993706cbb4196f3f
Artem Shubovych <artem.shubovych@thetradedesk.com>
2021-10-01 10:48:23 +1000

    Define 'interpreter' feature which controls building sq and sq_static binaries

--- ports/squirrel/portfile.cmake
+++ ports/squirrel/portfile.cmake
@@ -9,21 +9,16 @@ vcpkg_from_github(
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_DYNAMIC)
 
-# TODO: currently, squirrel builds samples (see the `sq/` dir) no matter what; it should be an optional build, but there is no option for it and it fails if either DISABLE_STATIC or DISABLE_DYNAMIC option is provided
-
-# if(BUILD_STATIC)
-#     set(DISABLE_DYNAMIC ON)
-#     set(DISABLE_STATIC OFF)
-# elseif(BUILD_DYNAMIC)
-#     set(DISABLE_DYNAMIC OFF)
-#     set(DISABLE_STATIC ON)
-# endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    INVERTED_FEATURES
+        "interpreter" BUILD_SQ
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    # OPTIONS
-    #     -DDISABLE_DYNAMIC=${DISABLE_DYNAMIC}
-    #     -DDISABLE_STATIC=${DISABLE_STATIC}
+    OPTIONS
+        -DDISABLE_DYNAMIC=${DISABLE_DYNAMIC}
+        -DDISABLE_STATIC=${DISABLE_STATIC}
 )
 
 vcpkg_cmake_install()
@@ -33,17 +28,18 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/squirrel)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-if(BUILD_STATIC)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-elseif(BUILD_DYNAMIC)
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/debug/bin/sq.exe"
-        "${CURRENT_PACKAGES_DIR}/debug/bin/sq_static.exe")
-
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/bin/sq.exe"
-        "${CURRENT_PACKAGES_DIR}/bin/sq_static.exe")
+if(BUILD_SQ)
+    if(BUILD_STATIC)
+        vcpkg_copy_tools(
+            TOOL_NAMES sq
+            AUTO_CLEAN
+        )
+    elseif(BUILD_DYNAMIC)
+        vcpkg_copy_tools(
+            TOOL_NAMES sq sq_static
+            AUTO_CLEAN
+        )
+    endif()
 endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
