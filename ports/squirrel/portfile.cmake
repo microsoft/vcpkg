5fb80f2f9dd2426e820c9fae3d599973db850295
Artem Shubovych <artem.shubovych@thetradedesk.com>
2021-10-01 10:58:28 +1000

    Remove bin/ directory for static build

--- ports/squirrel/portfile.cmake
+++ ports/squirrel/portfile.cmake
@@ -42,4 +42,8 @@ if(BUILD_SQ)
     endif()
 endif()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
