0ee16ce8a2ba157ffca188012d36695ee6c4af10
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-09-30 21:06:57 -0700

    fixup deprecated functions

--- ports/expected-lite/portfile.cmake
+++ ports/expected-lite/portfile.cmake
@@ -5,17 +5,16 @@ vcpkg_from_github(
     SHA512 6dd8974d518db0c79fe7bd0e407a85436c6ad222f8e5ed84efb34925d9a665b5b83ff08529b3e985034ed9a9201c80575f6a956132408ef6577c9c47cac55eae
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DEXPECTED_LITE_OPT_BUILD_TESTS=OFF
         -DEXPECTED_LITE_OPT_BUILD_EXAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(
+vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/${PORT}
 )
 
