3c35e055a918226abf298db8da091e8b0c950112
Carlos O'Ryan <coryan@google.com>
2021-10-01 18:23:33 +0000

    Undo migration to `vcpkg_cmake_config_fixup()`
    I could not get this to work. I will ask in the discord channel because
    I must be using it wrong.

--- ports/google-cloud-cpp/portfile.cmake
+++ ports/google-cloud-cpp/portfile.cmake
@@ -31,7 +31,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
