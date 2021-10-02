7608bf52a9fe0f069579d3faa3e53c512427ef3f
Kai Pastor <dg0yt@darc.de>
2021-10-02 17:24:29 +0200

    Modernize portfile

--- ports/libspatialite/portfile.cmake
+++ ports/libspatialite/portfile.cmake
@@ -7,14 +7,14 @@ vcpkg_download_distfile(ARCHIVE
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    ARCHIVE "${ARCHIVE}"
     PATCHES
         fix-makefiles.patch
         fix-linux-configure.patch
 )
 
 if (VCPKG_TARGET_IS_WINDOWS)
-  if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
+  if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
       set(GEOS_LIBS_REL "${CURRENT_INSTALLED_DIR}/lib/geos_c.lib")
       set(GEOS_LIBS_DBG "${CURRENT_INSTALLED_DIR}/debug/lib/geos_cd.lib")
       set(LIBXML2_LIBS_REL "${CURRENT_INSTALLED_DIR}/lib/libxml2.lib")
@@ -53,7 +53,7 @@ if (VCPKG_TARGET_IS_WINDOWS)
       ${CURRENT_INSTALLED_DIR}/lib/proj.lib ole32.lib shell32.lib"
   )
 
-  string(REPLACE "/" "\\\\" INST_DIR ${CURRENT_PACKAGES_DIR})
+  string(REPLACE "/" "\\\\" INST_DIR "${CURRENT_PACKAGES_DIR}")
   list(APPEND OPTIONS_RELEASE
       "INST_DIR=${INST_DIR}" "LIBS_ALL=${LIBS_ALL_REL}"
   )
@@ -62,7 +62,7 @@ if (VCPKG_TARGET_IS_WINDOWS)
    )
 
   vcpkg_install_nmake(
-      SOURCE_PATH ${SOURCE_PATH}
+      SOURCE_PATH "${SOURCE_PATH}"
       OPTIONS
           "CL_FLAGS=/DACCEPT_USE_OF_DEPRECATED_PROJ_API_H"
       OPTIONS_RELEASE
@@ -71,19 +71,20 @@ if (VCPKG_TARGET_IS_WINDOWS)
           ${OPTIONS_DEBUG}
   )
 
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+  vcpkg_copy_pdbs()
 
-  if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-      file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
-      file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
-      file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/spatialite_i.lib)
-      file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/spatialite_i.lib)
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+  if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+      file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+      file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+      file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/spatialite_i.lib")
+      file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/lib/spatialite_i.lib")
   else()
-      file(COPY ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/spatialite.pdb DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-      file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/spatialite.lib)
-      file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/spatialite.lib)
-      file(RENAME ${CURRENT_PACKAGES_DIR}/lib/spatialite_i.lib ${CURRENT_PACKAGES_DIR}/lib/spatialite.lib)
-      file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/spatialite_i.lib ${CURRENT_PACKAGES_DIR}/debug/lib/spatialite.lib)
+      file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/spatialite.lib")
+      file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/lib/spatialite.lib")
+      file(RENAME "${CURRENT_PACKAGES_DIR}/lib/spatialite_i.lib" "${CURRENT_PACKAGES_DIR}/lib/spatialite.lib")
+      file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/spatialite_i.lib" "${CURRENT_PACKAGES_DIR}/debug/lib/spatialite.lib")
   endif()
 else () # Build in UNIX
   if(VCPKG_TARGET_IS_LINUX)
@@ -104,11 +105,11 @@ else () # Build in UNIX
   )
 
   vcpkg_configure_make(
-      SOURCE_PATH ${SOURCE_PATH}
+      SOURCE_PATH "${SOURCE_PATH}"
       AUTOCONFIG
       OPTIONS
           "LIBS=${EXTRALIBS} -ldl -lm -l${STDLIB}"
-          "LIBXML2_CFLAGS=-I\"${CURRENT_INSTALLED_DIR}/include\""
+          "LIBXML2_CFLAGS=-I${CURRENT_INSTALLED_DIR}/include"
           "--enable-rttopo"
           "--enable-gcp"
           "--enable-geocallbacks"
@@ -125,4 +126,4 @@ else () # Build in UNIX
 endif()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
