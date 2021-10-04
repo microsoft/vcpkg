ff55aa634d4abb5197eadec8c530aff4d2e60405
idealvin <idealvin@qq.com>
2021-10-04 09:58:02 +0800

    update fail port

--- ports/co/portfile.cmake
+++ ports/co/portfile.cmake
@@ -1,7 +1,4 @@
-if(VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
-    vcpkg_fail_port_install(ON_ARCH "arm" ON_TAREGT "uwp")
-endif()
-vcpkg_fail_port_install(ON_TAREGT "OSX")
+vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "uwp" "OSX")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
