413226d68663567ff139344e77d895e415870dea
chausner <15180557+chausner@users.noreply.github.com>
2021-10-06 01:16:05 +0200

    [date] Update to 3.0.1 (#20481)
    * Update date to 3.0.1
    
    * Update CI baseline
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/date/portfile.cmake
+++ ports/date/portfile.cmake
@@ -8,37 +8,36 @@ endif()
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO HowardHinnant/date
-  REF cac99da8dc88be719a728dc1b597b0ac307c1800 #3.0.0
-  SHA512 07bac40c9d92ed92f05ab71b07c203fc341cd35999f1eab16d584bf77ff69e2cdc106931b2faf0dcfc5a311ee55e8445a81fd97c62f4672957b6aac1b24a08fd
+  REF v3.0.1
+  SHA512 6BDC7CBA821D66E17A559250CC0CE0095808E9DB81CEC9E16EAA4C31ABDFA705299C67B72016D9B06B302BC306D063E83A374EB00728071B83A5AD650D59034F
   HEAD_REF master
   PATCHES
     0001-fix-uwp.patch
     0002-fix-cmake-3.14.patch
-    0003-find-dependency-pthread.patch
 )
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     INVERTED_FEATURES
     remote-api USE_SYSTEM_TZ_DB
 )
 
-vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
-     ${FEATURE_OPTIONS}
+    ${FEATURE_OPTIONS}
     -DBUILD_TZ_LIB=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 if(VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_fixup_cmake_targets(CONFIG_PATH CMake TARGET_PATH share/date)
+  vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
 else()
-  vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/date TARGET_PATH share/date)
+  vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/date)
 endif()
 
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/date RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
