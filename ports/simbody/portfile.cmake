e3ce0e8a4005bb3fd30871b366aa57194c13ad3d
Akash Munagala <akash.munagala@gmail.com>
2021-10-06 21:58:44 -0500

    Fix cmake config path for unix

--- ports/simbody/portfile.cmake
+++ ports/simbody/portfile.cmake
@@ -17,7 +17,13 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+
+if(WIN32)
+    set(SIMBODY_CMAKE_DIR cmake)
+elseif(UNIX)
+    set(SIMBODY_CMAKE_DIR ${CMAKE_INSTALL_LIBDIR}/cmake/simbody/)
+endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH ${SIMBODY_CMAKE_DIR})
 
 vcpkg_copy_tools(
     TOOL_NAMES simbody-visualizer
