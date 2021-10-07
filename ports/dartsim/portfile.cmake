a01e54db97c6761a399b27cdaf9a0676f3bed38d
Akash <Ace314159@users.noreply.github.com>
2021-10-07 12:11:10 -0500

    Update ports/dartsim/portfile.cmake
    Co-authored-by: Robert Schumacher <roschuma@microsoft.com>

--- ports/dartsim/portfile.cmake
+++ ports/dartsim/portfile.cmake
@@ -26,7 +26,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH share/dart/cmake)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/dart/cmake PACKAGE_NAME dart)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/doc)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
