c107bc02da4a1aeb570e9d66089019764870399b
Yauheni Akhotnikau <eao197@gmail.com>
2021-10-07 10:59:27 +0300

    Change deprecated commands.

--- ports/json-dto/portfile.cmake
+++ ports/json-dto/portfile.cmake
@@ -5,9 +5,8 @@ vcpkg_from_github(
     SHA512 6c8a664148e65e347cd793f4f03be4d01612691cc4a69a4f84b906e582ea50a42db606c04e33cedb431f4ac45bf112460f109ab581ff2f34e97c2257534b9b40
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}/dev
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
     OPTIONS
         -DJSON_DTO_INSTALL=ON
         -DJSON_DTO_TEST=OFF
@@ -15,9 +14,9 @@ vcpkg_configure_cmake(
         -DJSON_DTO_INSTALL_SAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/json-dto)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/json-dto)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib ${CURRENT_PACKAGES_DIR}/debug)
 
