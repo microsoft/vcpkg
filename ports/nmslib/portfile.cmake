9fc6645b8e8445fd94ef1229305b343a9d10908a
chausner <chausner@users.noreply.github.com>
2021-10-10 17:55:47 +0200

    Update nmslib to 2.1.1

--- ports/nmslib/portfile.cmake
+++ ports/nmslib/portfile.cmake
@@ -5,41 +5,37 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nmslib/nmslib
-    REF 5482e077d1c8637499f86231bcd3979cb7fa6aef # v2.0.6
-    SHA512 e529c8d1d97e972f8314be9837e10f4ebab57d4a5f19a66341bb8e163dfe53d1d640a3909a708b021a52d0e6c2537954d749cb80e71757469700a3e9e173ceca
+    REF v2.1.1
+    SHA512 62BBB965EA4BF1D416ED78231B1BA4B41C0F46327D7BE16D1F98095DB63EF0E0D893B70040009711BC9C68555B1B8C4038F5032ABD66B759E955E2CBB0553EC3
     HEAD_REF master
-    PATCHES
-        fix-headers.patch
 )
 
-# TODO: check SSE and AVX avability and set corresponding tags
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/similarity_search
-    PREFER_NINJA
+# TODO: check SSE and AVX availability and set corresponding tags
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/similarity_search"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 # Move headers into separate folder
 set(SUBFOLDERS factory method space)
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/include/nmslib)
-foreach(SUBFOLER ${SUBFOLDERS})
-    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/include/nmslib/${SUBFOLER})
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/nmslib")
+foreach(SUBFOLDER ${SUBFOLDERS})
+    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include/nmslib/${SUBFOLDER}")
 endforeach()
 
-file(GLOB HEADERS ${CURRENT_PACKAGES_DIR}/include/*.h ${CURRENT_PACKAGES_DIR}/include/*/*.h)
+file(GLOB HEADERS "${CURRENT_PACKAGES_DIR}/include/*.h" "${CURRENT_PACKAGES_DIR}/include/*/*.h")
 foreach(HEADER ${HEADERS})
-    string(REPLACE "${CURRENT_PACKAGES_DIR}/include" "${CURRENT_PACKAGES_DIR}/include/nmslib"
-                   MOVED_HEADER ${HEADER})
-    file(RENAME ${HEADER} ${MOVED_HEADER})
+    string(REPLACE "${CURRENT_PACKAGES_DIR}/include" "${CURRENT_PACKAGES_DIR}/include/nmslib" MOVED_HEADER "${HEADER}")
+    file(RENAME "${HEADER}" "${MOVED_HEADER}")
 endforeach(HEADER ${HEADERS})
 
-foreach(SUBFOLER ${SUBFOLDERS})
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/${SUBFOLER}/)
+foreach(SUBFOLDER ${SUBFOLDERS})
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/${SUBFOLDER}/")
 endforeach()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Put the license file where vcpkg expects it
-file(INSTALL ${SOURCE_PATH}/README.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
