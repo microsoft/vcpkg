4eeec39d198d76e5ea4a891b18cdc421b3be9a66
chausner <15180557+chausner@users.noreply.github.com>
2021-10-11 20:02:24 +0200

    [msdfgen] Update to 1.9.1 (#20639)
    * Update msdfgen to 1.9.1
    
    * Update CI baseline
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>

--- ports/msdfgen/portfile.cmake
+++ ports/msdfgen/portfile.cmake
@@ -1,44 +1,42 @@
 # No symbols are exported in msdfgen source
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+if ("tools" IN_LIST FEATURES AND VCPKG_TARGET_IS_UWP)
+    message(FATAL_ERROR "Tools cannot be built on UWP.")
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Chlumsky/msdfgen
-    REF 9af250c7d6780a41dcaf536c05e3e1987a1bdcd7
-    SHA512 6b1dadd386aedf1e2de927dc83fe1f7fd7e053b0e9829ea0609a193ab8d9f92ecf08d2a6225b76a4f7bf9344b2935f38bbd00c4cc0c6627c1d95f67d2db728fe
+    REF v1.9.1
+    SHA512 7A721B38027548F66D1C3EB862C1F27EA0A431C325119504D072BA6D7ED87539BA4D6020C663354B7B82BA48725D77CC642DB05FB596749292A2A12BE21CE528
     HEAD_REF master
-    PATCHES
-        compatibility.patch
 )
 
-set(BUILD_TOOLS OFF)
-if ("tools" IN_LIST FEATURES)
-    if (VCPKG_TARGET_IS_UWP)
-        message("Tools couldn't be built on UWP, disable it automatically.")
-    else()
-        set(BUILD_TOOLS ON)
-    endif()
-endif()
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools MSDFGEN_BUILD_MSDFGEN_STANDALONE
+        openmp MSDFGEN_USE_OPENMP
+)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DMSDFGEN_BUILD_MSDFGEN_STANDALONE=${BUILD_TOOLS}
+        ${FEATURE_OPTIONS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/msdfgen)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/msdfgen)
 
 # move exe to tools
-if(BUILD_TOOLS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+if("tools" IN_LIST FEATURES AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     vcpkg_copy_tools(TOOL_NAMES msdfgen AUTO_CLEAN)
 endif()
 
-
 # cleanup
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # license
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
