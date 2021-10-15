e4491ec2e761108cb09238c4cafcac1107a6e522
Bruk Tedla <40060643+brukted@users.noreply.github.com>
2021-10-15 11:30:25 +0300

    Update ports/imnodes/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/imnodes/portfile.cmake
+++ ports/imnodes/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG
         -DIMNODES_SKIP_HEADERS=ON
 )
