bd13ff273cd721fb6676d25adb1a71491915911c
chausner <chausner@users.noreply.github.com>
2021-10-17 01:17:43 +0200

    Convert tabs to spaces

--- ports/soxr/portfile.cmake
+++ ports/soxr/portfile.cmake
@@ -3,16 +3,16 @@ vcpkg_from_sourceforge(
     REPO soxr
     FILENAME "soxr-0.1.3-Source.tar.xz"
     SHA512 f4883ed298d5650399283238aac3dbe78d605b988246bea51fa343d4a8ce5ce97c6e143f6c3f50a3ff81795d9c19e7a07217c586d4020f6ced102aceac46aaa8
-	PATCHES
-		001_initialize-resampler.patch
-		002_disable_warning.patch
-		003_detect_arm_on_windows.patch
+    PATCHES
+        001_initialize-resampler.patch
+        002_disable_warning.patch
+        003_detect_arm_on_windows.patch
 )
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-	    openmp WITH_OPENMP
+        openmp WITH_OPENMP
         lsr-bindings WITH_LSR_BINDINGS
 )
 
@@ -23,12 +23,12 @@ else()
 endif()
 
 vcpkg_cmake_configure(
-	SOURCE_PATH "${SOURCE_PATH}"
-	OPTIONS
-		-DBUILD_TESTS=OFF
-		-DBUILD_EXAMPLES=OFF
-		-DBUILD_SHARED_RUNTIME=${BUILD_SHARED_RUNTIME}
-		${FEATURE_OPTIONS}
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_EXAMPLES=OFF
+        -DBUILD_SHARED_RUNTIME=${BUILD_SHARED_RUNTIME}
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
