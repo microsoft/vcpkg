19cd3d166c8650f3795ea4bd7af91618ca240059
Dominik Wernberger <dominik.wernberger@gmx.de>
2021-10-18 08:49:50 +0200

    Integrate suggestion
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/compoundfilereader/portfile.cmake
+++ ports/compoundfilereader/portfile.cmake
@@ -5,6 +5,6 @@ vcpkg_from_github(
     SHA512 0ebb3ad02e4723d5e00a553d608dd8760614a3d83785c0e96a6d9a04df7e92486f6a4ecc0f7327a593776865abe697d21125554d547e89c838c69ab2da24d906
 )
 
-file(COPY ${SOURCE_PATH}/src/include/compoundfilereader.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(COPY ${SOURCE_PATH}/src/include/utf.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(COPY "${SOURCE_PATH}/src/include/compoundfilereader.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(COPY "${SOURCE_PATH}/src/include/utf.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
