a714f07e901370d2d4f936467f8df6a5700a6b6c
Dawid Wróbel <me@dawidwrobel.com>
2021-10-19 18:15:53 +0200

    [libofx] apply a patch instead of using getopt port on Windows

--- ports/libofx/portfile.cmake
+++ ports/libofx/portfile.cmake
@@ -6,23 +6,26 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 a46662bc425a4d343c0ac8e7885feb62f6854096b5cb2667e0f7419b380fcd8409f82bf287373bb23827b5abbee79b191edd871e434b9266e6aa7c35f2578097
 )
 
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    list(APPEND PATCHES
+        getopt.diff         # https://github.com/libofx/libofx/pull/50
+    )
+endif()
+
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
     REF ${OFX_VERSION}
     PATCHES
-        msvc.diff   		    # https://github.com/libofx/libofx/pull/47
-        pkgconfig.patch             # https://github.com/libofx/libofx/pull/49
+        msvc.diff           # https://github.com/libofx/libofx/pull/47
+        pkgconfig.patch     # https://github.com/libofx/libofx/pull/49
+        ${PATCHES}
 )
 
 # libopensp requirements
 if(VCPKG_TARGET_IS_OSX)
     list(APPEND EXTRA_OPTS "LIBS=-lintl -liconv \$LIBS")
-    list(APPEND EXTRA_OPTS "LDFLAGS=-framework CoreFoundation \$LDFLAGS") 
-endif()
-
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-    list(APPEND EXTRA_OPTS "LIBS=-lgetopt \$LIBS")
+    list(APPEND EXTRA_OPTS "LDFLAGS=-framework CoreFoundation \$LDFLAGS")
 endif()
 
 vcpkg_configure_make(
@@ -32,6 +35,7 @@ vcpkg_configure_make(
         --enable-doxygen=no
         --enable-html-docs=no
         --enable-latex-docs=no
+        --disable-gengetopt
         "--with-opensp-includes=${CURRENT_INSTALLED_DIR}/include/OpenSP"
         ${EXTRA_OPTS}
     OPTIONS_RELEASE
