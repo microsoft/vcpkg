516a5e3d097389dbe446413180e49e6f6a8e2a94
NancyLi1013 <lirui09@beyondsoft.com>
2021-10-19 00:57:54 -0700

    [glog] Update to 0.5.0

--- ports/glog/portfile.cmake
+++ ports/glog/portfile.cmake
@@ -1,26 +1,30 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/glog
-    REF v0.4.0
-    SHA512 b585f1819ade2075f6b61dc5aaca5c3f9d25601dba2bd08b6c49b96ac5f79db23c6b7f2042df003f7130497dd7241fcaa8b107d1f97385cb66ce52d3c554b176
+    REF 8f9ccfe770add9e4c64e9b25c102658e3c763b73 #v0.5.0
+    SHA512 93dab1041ef4e3eb758c76325531ae32208dcf92c296c8c3404cfa249b3849cc24d5423e087478b17f79ccbae9dbcabe049e509d47c4ec6d7135ca8ca80dada5
     HEAD_REF master
-    PATCHES
-       glog_disable_debug_postfix.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS 
         -DBUILD_TESTING=OFF
+        -DWITH_PKGCONFIG=ON
+        -DWITH_GTEST=OFF
+        -DWITH_UNWIND=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/glog)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glog)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_pdbs()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
