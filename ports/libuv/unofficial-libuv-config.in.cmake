a5c6e5d9c35ec465cd44ac8b283dff58be17f382
NancyLi1013 <lirui09@beyondsoft.com>
2021-10-19 23:59:43 -0700

    Only remove the limit for WIN32

--- ports/libuv/unofficial-libuv-config.in.cmake
+++ ports/libuv/unofficial-libuv-config.in.cmake
@@ -1,4 +1,7 @@
-include(CMakeFindDependencyMacro)
-find_dependency(Threads)
+
+if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
+    include(CMakeFindDependencyMacro)
+    find_dependency(Threads)
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/unofficial-libuv-targets.cmake)
