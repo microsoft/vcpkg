7a9bb77fee80f8fd37a2b968e7573cf05781efa2
Charles-Auguste Marois <cmarois@coveo.com>
2021-10-20 09:43:06 -0400

    [s2n] Fix remove libcrpyto messages patch.

--- ports/s2n/portfile.cmake
+++ ports/s2n/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     REF 4513f8d707a68388990886d353e7cfe46cc6454b # v1.1.1
     SHA512 6586e330733982ed3b70fd320e63575639d5793d69ffa06b2a46ed487d55d8271b46df611d62cc48410654b394d599de65edd9cdc860dea13255161d843f1f48
     PATCHES fix-cmake-target-path.patch
-            silence-libcrypto-messages.patch
+            remove-libcrypto-messages.patch
 )
 
 vcpkg_cmake_configure(
