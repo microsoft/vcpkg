65e08d6574ac4dab3495317bb35c740b9ae30512
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-10-21 23:04:24 +0200

    hwloc fix build.

--- ports/hwloc/portfile.cmake
+++ ports/hwloc/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO open-mpi/hwloc
     REF 5e185ccfff2728fa351cea41f6d9fefebfb88078 # hwloc-2.5.0
     SHA512 96f6421c40eede3a3c273a1ffa06accc43767421d5fb7b402a83caea1ef1a3bb8282c08ed94bc696296f37f3df80cd86403dac1012f2218b674569c8afcf3de9
+    PATCHES fix_wrong_ifdef.patch
+            fix_shared_win_build.patch
 )
 
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
@@ -34,20 +36,19 @@ vcpkg_configure_make(
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
-
-if(VCPKG_TARGET_IS_WINDOWS) # hardcode import specifiers. 
-    file(READ ${CURRENT_PACKAGES_DIR}/include/hwloc/autogen/config.h PUBLIC_CONFIG_H)
-    string(REPLACE "defined( DECLSPEC_EXPORTS )" "0" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-        string(REPLACE "defined( _USRDLL )" "0" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    else()
-        string(REPLACE "defined( _USRDLL )" "1" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    endif()
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/hwloc/autogen/config.h "${PUBLIC_CONFIG_H}")
-endif()
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
+
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CURRENT_PACKAGES_DIR}/COPYING.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/COPYING.txt"
+            "${CURRENT_PACKAGES_DIR}/debug/README.txt"
+            "${CURRENT_PACKAGES_DIR}/debug/NEWS.txt"
+            "${CURRENT_PACKAGES_DIR}/COPYING.txt"
+            "${CURRENT_PACKAGES_DIR}/README.txt"
+            "${CURRENT_PACKAGES_DIR}/NEWS.txt"
+    )
