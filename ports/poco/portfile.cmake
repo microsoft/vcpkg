99b20b7b45221cb371bf7a636ff77e3fe71e4e36
sundar <sundar@mcruncher.com>
2021-10-21 15:30:02 +0800

    Remove win netssl to prevents the library conflicts

--- ports/poco/portfile.cmake
+++ ports/poco/portfile.cmake
@@ -28,10 +28,8 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" POCO_STATIC)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" POCO_MT)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-   FEATURES
         pdf         ENABLE_PDF
         netssl      ENABLE_NETSSL
-        netssl      ENABLE_NETSSL_WIN
         netssl      ENABLE_CRYPTO
         sqlite3     ENABLE_DATA_SQLITE
         postgresql  ENABLE_DATA_POSTGRESQL
