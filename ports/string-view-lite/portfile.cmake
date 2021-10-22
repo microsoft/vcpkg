db149c14bb62ba38ead6920176411c175cb6d785
chausner <15180557+chausner@users.noreply.github.com>
2021-10-22 07:37:40 +0200

    [string-view-lite] Update to 1.6.0 (#20912)
    * Update string-view-lite to 1.6.0
    
    * Update CI baseline
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>

--- ports/string-view-lite/portfile.cmake
+++ ports/string-view-lite/portfile.cmake
@@ -1,29 +1,28 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO martinmoene/string-view-lite
-    REF v1.3.0
-    SHA512 52fb76198249ade5352d95af4a4e305b3e22b464a5d0a702e4b2228b1ca30df98b90bb01d5bfd16ae6ebb7bab5aecac5bd4a867898c362e82e57c2aaf938e07a
+    REF v1.6.0
+    SHA512 a21c4c956360b76cf6f530ae7c26d97777d4c37164e6fc0da0dc931d41378aa020a235e40d7f8e8160c1b9dab552c6d7bf3aa7697e9048effef2b3cee8573553
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DSTRING_VIEW_LITE_OPT_BUILD_TESTS=OFF
         -DSTRING_VIEW_LITE_OPT_BUILD_EXAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(
-    CONFIG_PATH lib/cmake/${PORT}
+vcpkg_cmake_config_fixup(
+    CONFIG_PATH "lib/cmake/${PORT}"
 )
 
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug
-    ${CURRENT_PACKAGES_DIR}/lib
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
 )
 
 file(INSTALL
-    ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
+    "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright
 )
