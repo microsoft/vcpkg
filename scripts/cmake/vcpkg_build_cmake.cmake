7e3b3d802ce7a307be1753ae87bf238e957e5817
nicole mazzuca <mazzucan@outlook.com>
2021-10-22 12:27:42 -0700

    fix x86 configure, fix `if` guidelines

--- scripts/cmake/vcpkg_build_cmake.cmake
+++ scripts/cmake/vcpkg_build_cmake.cmake
@@ -52,17 +52,17 @@ function(vcpkg_build_cmake)
     vcpkg_list(SET parallel_param)
     vcpkg_list(SET no_parallel_param)
 
-    if(Z_VCPKG_CMAKE_GENERATOR STREQUAL "Ninja")
+    if("${Z_VCPKG_CMAKE_GENERATOR}" STREQUAL "Ninja")
         vcpkg_list(SET build_param "-v") # verbose output
         vcpkg_list(SET parallel_param "-j${VCPKG_CONCURRENCY}")
         vcpkg_list(SET no_parallel_param "-j1")
-    elseif(Z_VCPKG_CMAKE_GENERATOR MATCHES "^Visual Studio")
+    elseif("${Z_VCPKG_CMAKE_GENERATOR}" MATCHES "^Visual Studio")
         vcpkg_list(SET build_param
             "/p:VCPkgLocalAppDataDisabled=true"
             "/p:UseIntelMKL=No"
         )
         vcpkg_list(SET parallel_param "/m")
-    elseif(Z_VCPKG_CMAKE_GENERATOR STREQUAL "NMake Makefiles")
+    elseif("${Z_VCPKG_CMAKE_GENERATOR}" STREQUAL "NMake Makefiles")
         # No options are currently added for nmake builds
     else()
         message(FATAL_ERROR "Unrecognized GENERATOR setting from vcpkg_configure_cmake(). Valid generators are: Ninja, Visual Studio, and NMake Makefiles")
@@ -75,8 +75,8 @@ function(vcpkg_build_cmake)
     endif()
 
     foreach(build_type IN ITEMS debug release)
-        if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL build_type)
-            if(build_type STREQUAL "debug")
+        if(NOT DEFINED VCPKG_BUILD_TYPE OR "${VCPKG_BUILD_TYPE}" STREQUAL "${build_type}")
+            if("${build_type}" STREQUAL "debug")
                 set(short_build_type "dbg")
                 set(config "Debug")
             else()
@@ -88,14 +88,14 @@ function(vcpkg_build_cmake)
 
             if(arg_ADD_BIN_TO_PATH)
                 vcpkg_backup_env_variables(VARS PATH)
-                if(build_type STREQUAL "debug")
+                if("${build_type}" STREQUAL "debug")
                     vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/debug/bin")
                 else()
                     vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/bin")
                 endif()
             endif()
 
-            if (arg_DISABLE_PARALLEL)
+            if(arg_DISABLE_PARALLEL)
                 vcpkg_execute_build_process(
                     COMMAND
                         "${CMAKE_COMMAND}" --build . --config "${config}" ${target_param}
