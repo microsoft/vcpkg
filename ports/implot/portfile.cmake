0f5d91b13d976c56f2fc258f5b35c310f52ac7e6
Rémy Tassoux <contact@rt2.fr>
2021-10-22 08:55:18 +0200

    [implot] Update to 0.12 (#20873)
    * [implot] Update to 0.12
    
    * [implot] Update version files
    
    * [implot] Replace deprecated vcpkg functions
    
    * [implot] Fix port formatting
    
    * [implot] Update version files
    
    * [implot] Update implot to 0.12
    
    * [implot] Update version files
    
    * [implot] Update version database
    
    * update portfile.cmake
    
    * update version
    
    Co-authored-by: Rémy Tassoux <rt2@rasterizedworld.com>
    Co-authored-by: Jonliu1993 <13720414433@163.com>

--- ports/implot/portfile.cmake
+++ ports/implot/portfile.cmake
@@ -3,23 +3,22 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO epezent/implot
-    REF v0.11
-    SHA512 161e709fcd872abd414129915501261085df531a0b5a9b33ed40422a082603627242467de59b11116948352fdb62b5d28131658b603f48692edd43429513ba29
+    REF v0.12
+    SHA512 972bb831150a8b7c5fddb828c9d6928b493186ae587c8c5ea679c4fe499459892f175181c529e37195f72e09854ec8c698b77ae39e60295f831ad67e4a5f3358
     HEAD_REF master
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG
         -DIMPLOT_SKIP_HEADERS=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_config_fixup()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
