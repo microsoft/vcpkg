5ba18a325c3fb8c493511ec4274ed1416ecc902e
chausner <chausner@users.noreply.github.com>
2021-10-25 00:55:38 +0200

    Fix file name casing

--- ports/imath/portfile.cmake
+++ ports/imath/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/imath)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Imath)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
