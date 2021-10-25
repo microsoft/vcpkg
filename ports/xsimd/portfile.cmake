1377b2a80e06064bc0ec74cb711e61a21f18932f
Jonliu1993 <13720414433@163.com>
2021-10-25 10:08:42 +0800

    Remove double quotes

--- ports/xsimd/portfile.cmake
+++ ports/xsimd/portfile.cmake
@@ -23,7 +23,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
