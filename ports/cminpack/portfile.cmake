1eaf2a36ae52790e13aad145de980c483e619eec
veggiesaurus <accomrie@gmail.com>
2021-10-25 15:08:16 +0200

    fixed reviewer comments

--- ports/cminpack/portfile.cmake
+++ ports/cminpack/portfile.cmake
@@ -1,16 +1,12 @@
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/devernay/cminpack/archive/refs/tags/v1.3.8.tar.gz"
-    FILENAME "v1.3.8.tar.gz"
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO devernay/cminpack
+    REF v1.3.8
     SHA512 0cab275074a31af69dbaf3ef6d41b20184c7cf9f33c78014a69ae7a022246fa79e7b4851341c6934ca1e749955b7e1096a40b4300a109ad64ebb1b2ea5d1d8ae
 )
 
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-)
-
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_EXAMPLES=OFF
 )
@@ -20,4 +16,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # # Handle copyright
-file(INSTALL ${SOURCE_PATH}/CopyrightMINPACK.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/cminpack RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/CopyrightMINPACK.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
