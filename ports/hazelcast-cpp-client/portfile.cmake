8e2a728fd70b3c54761e4d2733baec1c8c216751
Yunus Inci <yemreinci0@gmail.com>
2021-10-26 21:43:52 +0300

    fix deprecated functions

--- ports/hazelcast-cpp-client/portfile.cmake
+++ ports/hazelcast-cpp-client/portfile.cmake
@@ -13,19 +13,18 @@ vcpkg_check_features(
         example BUILD_EXAMPLES
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS ${FEATURE_OPTIONS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/hazelcast-cpp-client)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/hazelcast-cpp-client)
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
