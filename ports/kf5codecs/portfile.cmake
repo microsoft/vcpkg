1a0c3037b3c570830c07ccd6b4af5f3e05856370
Dawid Wróbel <me@dawidwrobel.com>
2021-10-27 07:27:18 +0200

    [kf5codecs] add explicit gperf dependency (#20266)
    * [kf5codecs] add explicit gperf dependency
    
    * [kf5codecs] wrap paths in quotes; no need to remove /etc
    
    * [kf5codecs] update versions
    
    * [kf5codecs] make gperf host dependency
    
    Co-authored-by: Billy O'Neal <bion@microsoft.com>
    
    * [kf5codecs] pass the gperf host executable path
    
    * [kf5codecs] update versions
    
    * [kf5codecs] fix a typo in find_program()
    
    * [kf5codecs] quote the Gperf_EXECUTABLE path
    
    * [kf5codecs] update versions
    
    Co-authored-by: Billy O'Neal <bion@microsoft.com>

--- ports/kf5codecs/portfile.cmake
+++ ports/kf5codecs/portfile.cmake
@@ -6,17 +6,16 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_find_acquire_program(GPERF)
-get_filename_component(GPERF_EXE_PATH ${GPERF} DIRECTORY)
-vcpkg_add_to_path(${GPERF_EXE_PATH})
-
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
 file(WRITE ${SOURCE_PATH}/.clang-format "DisableFormat: true\nSortIncludes: false\n")
 
+find_program(GPERF_EXE NAMES gperf PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools" NO_DEFAULT_PATH)
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
+        "-DGperf_EXECUTABLE=${GPERF_EXE}"
 )
 
 vcpkg_cmake_install()
@@ -28,9 +27,8 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/data)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/etc)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/data")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/LICENSES/ DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSES/" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
