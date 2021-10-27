a6c4fd372d8508cbdd12bc5b31b3503e593a35c9
Jonliu1993 <13720414433@163.com>
2021-10-27 14:23:01 +0800

    [WinReg] update to v4.1.1

--- ports/winreg/portfile.cmake
+++ ports/winreg/portfile.cmake
@@ -4,13 +4,13 @@ vcpkg_fail_port_install(ON_TARGET "linux" "osx")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GiovanniDicanio/WinReg
-    REF 023ad61dc77c83407e7ae061f177a3ba3d3941e6 #v4.1.0
-    SHA512 e62bf4a7926c720ad2c9a56b71b19ff48f566d56ddd5c858c25cec3fc6d8fd829267d3d1789b4841140b95d4e7ed0718af55317f6b4f76c1094bd1c69dda24f1
+    REF 6b61514683a185d207f1952723a3be405ef0c4bd #v4.1.1
+    SHA512 e85eec1496d52988063567f4e9ceceda55e167ebefb6ea2ff90b8dc7670573ca4084d64dea952f5f439282b078d7b5d6f4aad9e2d3cda8424c745d81d3cad7ae
     HEAD_REF master
 )
 
 # Copy the single reusable library header
-file(COPY ${SOURCE_PATH}/WinReg/WinReg.hpp DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(COPY "${SOURCE_PATH}/WinReg/WinReg.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
