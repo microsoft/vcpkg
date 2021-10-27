1a48fe707c8d9e4fb78b158cc041cb8a47f6544e
chausner <15180557+chausner@users.noreply.github.com>
2021-10-27 08:34:56 +0200

    [socket-io-client] Update to 3.1.0 (#20883)
    * Update socket-io-client to 3.1.0
    
    * Update CI baseline
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>

--- ports/socket-io-client/portfile.cmake
+++ ports/socket-io-client/portfile.cmake
@@ -3,21 +3,19 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO socketio/socket.io-client-cpp
-    REF 3.0.0
-    SHA512 42735d73d24546b37332d649a2633f4a1b6e004b016c45d53bd8e230a157753bb319c80a59721865b9c3dcc588b4eec3cdf4ae9f7fc2cdf290b6bb07c866552c
+    REF 3.1.0
+    SHA512 a0adaa06ddb24297686a40b1e71a55bbab437093f828f76040c376b4adccb7d8b06eff4d8569dbde9b2e071257b3290e7e2bffd6354b33ecf67378ffa1d0cc13
     HEAD_REF master
     PATCHES
         fix-file-not-found.patch
-        fix-error-C3321.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
