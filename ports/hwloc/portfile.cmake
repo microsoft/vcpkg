52d72e7c2975740b454bf7402408298bd01d2918
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2021-10-28 08:05:10 +0200

    [hwloc] Update to 2.5 and switch to make (#20905)
    * hwloc try and error
    
    * hwloc fix build.
    
    * format manifest
    
    * version stuff
    
    * remove supports expression. Copy COPYING from src dir
    
    * format manifest
    
    * overwrite version
    
    * update ci baseline
    hpx uses _mm_pause which seems to be undefined on arm64
    maybe hpx requires a supports field?
    
    * switch to version in manifest
    
    * version stuff
    
    * add maintainer
    
    Co-authored-by: Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
    
    * version stuff
    
    Co-authored-by: Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>

--- ports/hwloc/portfile.cmake
+++ ports/hwloc/portfile.cmake
@@ -1,54 +1,54 @@
-vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "uwp")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-mpi/hwloc
-    REF 263908a2c1f21c0e221a8d1f6472daf3a1fc07b9 # hwloc-2.2.0
-    SHA512 87f3d267781fd1f8907b0c080868b56943c7c2caecae5c0fbe9a55f8c5e9453bb6b7892834ba37696c1ebadd8d7bfdd5e513ea72a075211b808a1d5803ea4b8e
+    REF 5e185ccfff2728fa351cea41f6d9fefebfb88078 # hwloc-2.5.0
+    SHA512 96f6421c40eede3a3c273a1ffa06accc43767421d5fb7b402a83caea1ef1a3bb8282c08ed94bc696296f37f3df80cd86403dac1012f2218b674569c8afcf3de9
+    PATCHES fix_wrong_ifdef.patch
+            fix_shared_win_build.patch
 )
 
-if (VCPKG_TARGET_IS_WINDOWS)
-    file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH} 
-        PREFER_NINJA
-    )
-    
-    vcpkg_install_cmake()
-    
-    file(READ ${CURRENT_PACKAGES_DIR}/include/hwloc/autogen/config.h PUBLIC_CONFIG_H)
-    string(REPLACE "defined( DECLSPEC_EXPORTS )" "0" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-        string(REPLACE "defined( _USRDLL )" "0" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    else()
-        string(REPLACE "defined( _USRDLL )" "1" PUBLIC_CONFIG_H "${PUBLIC_CONFIG_H}")
-    endif()
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/hwloc/autogen/config.h "${PUBLIC_CONFIG_H}")
-    
-    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/tools)
-else()
-    message(WARNING "${PORT} currently requires the following tool from the system package manager:\n    libtool")
-
-    vcpkg_configure_make(
-        SOURCE_PATH ${SOURCE_PATH}
-        AUTOCONFIG
-        OPTIONS
-            --disable-libxml2
-            --disable-opencl
-    )
-    
-    vcpkg_install_make()
-    
-    file(GLOB HWLOC_EXEC ${CURRENT_PACKAGES_DIR}/bin)
-    message("HWLOC_EXEC: ${HWLOC_EXEC}")
-    if (HWLOC_EXEC)
-        file(COPY ${HWLOC_EXEC} DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
-        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-    endif()
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    set(OPTIONS ac_cv_prog_cc_c99= # To avoid the compiler check for C99 which will fail for MSVC
+                --disable-plugin-dlopen) 
 endif()
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    list(APPEND OPTIONS "HWLOC_LDFLAGS=-no-undefined")
+endif()
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+    OPTIONS
+        ${OPTIONS} 
+        --disable-libxml2
+        --disable-opencl
+        --disable-cairo
+        --disable-cuda
+        --disable-libudev
+        --disable-levelzero
+        --disable-nvml
+        --disable-rsmi
+        --disable-pci
+        #--disable-cpuid
+        #--disable-picky
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/COPYING.txt"
+            "${CURRENT_PACKAGES_DIR}/debug/README.txt"
+            "${CURRENT_PACKAGES_DIR}/debug/NEWS.txt"
+            "${CURRENT_PACKAGES_DIR}/COPYING.txt"
+            "${CURRENT_PACKAGES_DIR}/README.txt"
+            "${CURRENT_PACKAGES_DIR}/NEWS.txt"
+    )
