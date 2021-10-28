bf52b5cf3d3cbee237a2552510e613bffff5b461
chausner <chausner@users.noreply.github.com>
2021-10-29 01:42:32 +0200

    Various improvements

--- ports/alembic/portfile.cmake
+++ ports/alembic/portfile.cmake
@@ -1,3 +1,5 @@
+vcpkg_fail_port_install(ON_ARCH "x86") # see https://github.com/alembic/alembic/issues/372
+
 vcpkg_buildpath_length_warning(37)
 
 vcpkg_from_github(
@@ -8,10 +10,14 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ALEMBIC_SHARED_LIBS)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DALEMBIC_SHARED_LIBS=${ALEMBIC_SHARED_LIBS}
         -DUSE_HDF5=ON
+        -DUSE_TESTS=OFF
 )
 
 vcpkg_cmake_install()
@@ -23,37 +29,21 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-if(VCPKG_TARGET_IS_WINDOWS)
-
+if(VCPKG_TARGET_IS_WINDOWS AND ALEMBIC_SHARED_LIBS)
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-
-        file(GLOB EXE "${CURRENT_PACKAGES_DIR}/bin/*.exe")
-        file(REMOVE ${EXE})
         file(RENAME "${CURRENT_PACKAGES_DIR}/lib/Alembic.dll" "${CURRENT_PACKAGES_DIR}/bin/Alembic.dll")
-        file(READ "${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-release.cmake" RELEASE_CONFIG)
-        string(REPLACE "\${_IMPORT_PREFIX}/lib/Alembic.dll"
-                    "\${_IMPORT_PREFIX}/bin/Alembic.dll" RELEASE_CONFIG "${RELEASE_CONFIG}")
-        file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-release.cmake" "${RELEASE_CONFIG}")
-
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-release.cmake" "\${_IMPORT_PREFIX}/lib/Alembic.dll" "\${_IMPORT_PREFIX}/bin/Alembic.dll")
     endif()
-
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-
-        file(GLOB DEBUG_EXE "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe")
-        file(REMOVE ${DEBUG_EXE})
         file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/Alembic.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/Alembic.dll")
-        file(READ "${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-debug.cmake" DEBUG_CONFIG)
-        string(REPLACE "\${_IMPORT_PREFIX}/debug/lib/Alembic.dll"
-                    "\${_IMPORT_PREFIX}/debug/bin/Alembic.dll" DEBUG_CONFIG "${DEBUG_CONFIG}")
-        file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-debug.cmake" "${DEBUG_CONFIG}")
-        
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/AlembicTargets-debug.cmake" "\${_IMPORT_PREFIX}/debug/lib/Alembic.dll" "\${_IMPORT_PREFIX}/debug/bin/Alembic.dll")
     endif()
-
-else()
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
+vcpkg_copy_tools(
+    TOOL_NAMES abcconvert abcdiff abcecho abcechobounds abcls abcstitcher abctree
+    AUTO_CLEAN
+)
 vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
