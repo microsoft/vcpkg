7b3ff6818b891f041de12a38ec2c5eac10f75ca9
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-10-28 02:03:09 -0700

    support pkgconfig file

--- ports/args/portfile.cmake
+++ ports/args/portfile.cmake
@@ -16,10 +16,11 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_pkgconfig()
 
 # Put the licence file where vcpkg expects it
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_copy_pdbs()
