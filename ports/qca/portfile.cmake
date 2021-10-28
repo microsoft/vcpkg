4a4b45744d10cc7dd4ee676b2540d2d29bef6fed
Jack·Boos·Yu <47264268+JackBoosY@users.noreply.github.com>
2021-10-29 03:35:04 +0800

    [qca] Fix usage (#20977)
    * [qca] Fix usage
    
    * version
    
    * Add double quotes to paths
    
    * version
    
    * Remove some double quotes
    
    * version
    
    * Remove depreciate code
    
    * version
    
    * Try to remove the out-dated code
    
    * version
    
    * Apply suggestions
    
    * version

--- ports/qca/portfile.cmake
+++ ports/qca/portfile.cmake
@@ -6,11 +6,7 @@
 #
 vcpkg_find_acquire_program(PERL)
 get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-vcpkg_add_to_path(${PERL_EXE_PATH})
-
-if(EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
-    file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
-endif()
+vcpkg_add_to_path("${PERL_EXE_PATH}")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -21,24 +17,6 @@ vcpkg_from_github(
         0001-fix-path-for-vcpkg.patch
         0002-fix-build-error.patch
 )
-
-# Remove these files on the cmake next update
-vcpkg_download_distfile(ARCHIVE_PKGCONFIG
-    URLS "https://gitlab.kitware.com/cmake/cmake/-/raw/ab8bd48352df060c4ead210fe30dc4736646206b/Modules/FindPkgConfig.cmake?inline=false"
-    FILENAME "FindPkgConfig.cmake"
-    SHA512 d9aada8107eac9ada197d0b1e9040cd3707de7f5838c90bca80321e8eb35931f520635800bea0e2aafeca7fafb08b8e4752d5c6c2d6b1a5d5b4e3225d7818aa6
-)
-vcpkg_download_distfile(ARCHIVE_PKGHSA
-    URLS "https://gitlab.kitware.com/cmake/cmake/-/raw/ab8bd48352df060c4ead210fe30dc4736646206b/Modules/FindPackageHandleStandardArgs.cmake?inline=false"
-    FILENAME "FindPackageHandleStandardArgs.cmake"
-    SHA512 61a459a9e0797f976fae36ce4fd5a18a30bd13e8bc9d65d165ca760e13ddc27a8c8ad371ad4b41cc36fe227425424fe9bc6e4dfa5cfdc68bd59a3c10005cf3b4
-)
-vcpkg_download_distfile(ARCHIVE_PKGMSG
-    URLS "https://gitlab.kitware.com/cmake/cmake/-/raw/ab8bd48352df060c4ead210fe30dc4736646206b/Modules/FindPackageMessage.cmake?inline=false"
-    FILENAME "FindPackageMessage.cmake"
-    SHA512 44af652038ecd98c1e54f440e67994759345290530b36f687b7e6d5c310caa55597f3718901fe7a3f8816b560f03b8f238d90aab6ce9b1b24391ab0bb2aa44a8
-)
-file(COPY ${ARCHIVE_PKGCONFIG} ${ARCHIVE_PKGHSA} ${ARCHIVE_PKGMSG} DESTINATION ${SOURCE_PATH}/cmake/modules)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
   set(QCA_FEATURE_INSTALL_DIR_DEBUG ${CURRENT_PACKAGES_DIR}/debug/bin/Qca)
@@ -53,30 +31,28 @@ endif()
 # it is up to developers or admins to maintain CAs.
 # So we do it here:
 message(STATUS "Importing certstore")
-file(REMOVE ${SOURCE_PATH}/certs/rootcerts.pem)
+file(REMOVE "${SOURCE_PATH}/certs/rootcerts.pem")
 # Using file(DOWNLOAD) to use https
 file(DOWNLOAD https://raw.githubusercontent.com/mozilla/gecko-dev/master/security/nss/lib/ckfw/builtins/certdata.txt
-    ${CURRENT_BUILDTREES_DIR}/cert/certdata.txt
+    "${CURRENT_BUILDTREES_DIR}/cert/certdata.txt"
     TLS_VERIFY ON
 )
 vcpkg_execute_required_process(
-    COMMAND ${PERL} ${CMAKE_CURRENT_LIST_DIR}/mk-ca-bundle.pl -n ${SOURCE_PATH}/certs/rootcerts.pem
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}/cert
+    COMMAND "${PERL}" "${CMAKE_CURRENT_LIST_DIR}/mk-ca-bundle.pl" -n "${SOURCE_PATH}/certs/rootcerts.pem"
+    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/cert"
     LOGNAME ca-bundle
 )
 message(STATUS "Importing certstore done")
 
 # Configure and build
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DUSE_RELATIVE_PATHS=ON
-        -DQT4_BUILD=OFF
         -DBUILD_TESTS=OFF
         -DBUILD_TOOLS=OFF
         -DQCA_SUFFIX=OFF
-        -DQCA_FEATURE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/qca/mkspecs/features
+        -DQCA_FEATURE_INSTALL_DIR=share/qca/mkspecs/features
         -DOSX_FRAMEWORK=OFF
     OPTIONS_DEBUG
         -DQCA_PLUGINS_INSTALL_DIR=${QCA_FEATURE_INSTALL_DIR_DEBUG}
@@ -84,36 +60,23 @@ vcpkg_configure_cmake(
         -DQCA_PLUGINS_INSTALL_DIR=${QCA_FEATURE_INSTALL_DIR_RELEASE}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-# Patch and copy cmake files
-message(STATUS "Patching files")
-file(READ 
-    ${CURRENT_PACKAGES_DIR}/debug/share/qca/cmake/QcaTargets-debug.cmake
-    QCA_DEBUG_CONFIG
+vcpkg_cmake_config_fixup(CONFIG_PATH share/qca/cmake)
+file(READ "${CURRENT_PACKAGES_DIR}/share/${PORT}/QcaConfig.cmake" QCA_CONFIG_FILE)
+string(REGEX REPLACE "PACKAGE_PREFIX_DIR \"(.*)\" ABSOLUTE"
+                     [[PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE]]
+       QCA_CONFIG_FILE "${QCA_CONFIG_FILE}"
 )
-string(REPLACE "\${_IMPORT_PREFIX}" "\${_IMPORT_PREFIX}/debug" QCA_DEBUG_CONFIG "${QCA_DEBUG_CONFIG}")
-file(WRITE 
-    ${CURRENT_PACKAGES_DIR}/share/qca/cmake/QcaTargets-debug.cmake
-    "${QCA_DEBUG_CONFIG}"
-)
-
-file(READ ${CURRENT_PACKAGES_DIR}/share/qca/cmake/QcaTargets.cmake
-    QCA_TARGET_CONFIG
-)
-string(REPLACE "packages/qca_" "installed/" QCA_TARGET_CONFIG "${QCA_TARGET_CONFIG}")
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/qca/cmake/QcaTargets.cmake
-    "${QCA_TARGET_CONFIG}"
-)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/QcaConfig.cmake" "${QCA_CONFIG_FILE}")
 
 # Remove unneeded dirs
 file(REMOVE_RECURSE 
-    ${CURRENT_BUILDTREES_DIR}/share/man
-    ${CURRENT_PACKAGES_DIR}/share/man
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+    "${CURRENT_BUILDTREES_DIR}/share/man"
+    "${CURRENT_PACKAGES_DIR}/share/man"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
-message(STATUS "Patching files done")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
