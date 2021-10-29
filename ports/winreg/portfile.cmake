c913392fb280231ea6ef404d7175289c493e0bc4
Jonliu1993 <13720414433@163.com>
2021-10-29 14:54:00 +0800

    change version

--- ports/winreg/portfile.cmake
+++ ports/winreg/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_fail_port_install(ON_TARGET "linux" "osx")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GiovanniDicanio/WinReg
-    REF 2594342b7fe6065430bc377961c37d30614cb4ff #v4.1.1
+    REF 2594342b7fe6065430bc377961c37d30614cb4ff #v4.1.2
     SHA512 924cdb77518c3f0843e95cd7e7d4626d4c0c466444cd79fdfa6943975154a54f4eb0d4bd45b8d37d73c650467b1d2728543176688f356c5100d98810e95c9fe8
     HEAD_REF master
 )
