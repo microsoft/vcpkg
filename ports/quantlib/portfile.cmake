3f6d5fd11aa254dfd58579356a27ff594e9cf85a
Jonathan Sweemer <sweemer@gmail.com>
2021-10-31 15:02:46 +0900

    [quantlib] update to 1.24

--- ports/quantlib/portfile.cmake
+++ ports/quantlib/portfile.cmake
@@ -1,3 +1,8 @@
+if (VCPKG_TARGET_IS_WINDOWS)
+    # This can (and should) be removed if QuantLib ever supports dynamically linking on Windows
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY ONLY_STATIC_CRT)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lballabio/QuantLib
@@ -5,8 +10,6 @@ vcpkg_from_github(
     SHA512 1a63d90363cda69e20e40a60a2d6357f632fac997df520b949e73feb256dac8a999395d0ebe94734ac630e9104e512b0a545f6a2111b287a41e9128127aa5bb0
     HEAD_REF master
 )
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
