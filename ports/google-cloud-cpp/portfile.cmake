4abfcf0a727f6e1f56ae3bd9f7a548ce79cfdf09
Carlos O'Ryan <coryan@google.com>
2021-11-02 14:56:27 -0400

    [google-cloud-cpp] update to latest release (v1.33.0) (#21118)

--- ports/google-cloud-cpp/portfile.cmake
+++ ports/google-cloud-cpp/portfile.cmake
@@ -5,8 +5,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO googleapis/google-cloud-cpp
-    REF v1.32.1
-    SHA512 385adb0e39ed0b3474609d90524bb3fe8c42d92d34626cde7b70b1eb4cd76dd22e9b5b9d0933d600ce7a585c63382d764a038fd8152a39bbbd8010524eb40e9b
+    REF v1.33.0
+    SHA512 fda083368db3b31dbfa6b6d6b02254a2c2b71a61389c94d8f62a5fcd729691bbe54745e1887fe36b244411af09fcaf7968e535ea9a3dc4833892859e7201bf67
     HEAD_REF main
 )
 
