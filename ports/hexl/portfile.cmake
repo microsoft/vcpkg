259180b72cfd61e3430b1fd9b9ae94cf0cbabd25
GelilaSeifu <gelila.seifu@intel.com>
2021-11-03 12:31:24 -0700

    update HEXL to v1.2.3

--- ports/hexl/portfile.cmake
+++ ports/hexl/portfile.cmake
@@ -4,9 +4,9 @@ vcpkg_fail_port_install(ON_ARCH "x86" "arm" "arm64")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO intel/hexl
-    REF 343acab47f7612c76cc09bfe39ec9435283fb0e6
-    SHA512 afaf0c68559547d6617cb75ff465f5c4998f00bda1e5edb399e4a80f8a67628fbbbee6b3b484ec96944c8686b9b2180e6523c1329619454b7752a46ffa2a913c
-    HEAD_REF 1.2.2
+    REF 0858760dc957280e8eb8953af4b4b83879d7b8a4
+    SHA512 f2400c4bf32d22904f5917a396fdf6fc625a8832455437429abd54cae70da51cfc42a57dc176d1faeb76f7cd4569dd7499a8f302aef2ea8463d3e8ddc3132050
+    HEAD_REF 1.2.3
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
@@ -29,7 +29,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "HEXL" CONFIG_PATH "lib/cmake/hexl-1.2.2")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "HEXL" CONFIG_PATH "lib/cmake/hexl-1.2.3")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
