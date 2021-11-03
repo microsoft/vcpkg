3f2e27432ecfd2b130a9c33b467cc42241f92201
Yury Bura <yurybura@gmail.com>
2021-11-04 00:22:57 +0300

    [halide] update to 13.0.0

--- ports/halide/portfile.cmake
+++ ports/halide/portfile.cmake
@@ -3,9 +3,9 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO halide/Halide
-    REF 5dabcaa9effca1067f907f6c8ea212f3d2b1d99a  # v12.0.1
-    SHA512 5ab44703850885561337e23d8b538a5adfe1611e24e8daa4a1313756b4f9dfeb54e89bf8400d46a3340c00234402681b4f44ba3ed5322027fd6cb5dfbd525acd
-    HEAD_REF release/12.x
+    REF c3641b6850d156aff6bb01a9c01ef475bd069a31 # v13.0.0
+    SHA512 9a8e8a1f922babce992e73e7b6567bd338a651a4cc0d411a53ddab6087fa3a4fb4e864be4b36891edefaa80d85750f6c25760f61de75f7e642d1d8ce80d88cbe
+    HEAD_REF release/13.x
 )
 
 vcpkg_check_features(
@@ -67,9 +67,6 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME HalideHelpers)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/tutorial)
-
-file(GLOB readmes "${CURRENT_PACKAGES_DIR}/share/${PORT}/*.md")
-file(REMOVE ${readmes})
 
 configure_file(${SOURCE_PATH}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/usage ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage COPYONLY)
