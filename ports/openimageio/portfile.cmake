7678fa940527fa675b3a1eabf3fb77814d0f81e7
Fabien Castan <fabcastan@gmail.com>
2021-11-03 15:25:53 +0100

    [openimageio] do not install deprecated FindOpenImageIO
    OpenImageIO provides cmake Config/Target files, so there is no more need for a FindOpenImageIO.cmake.
    This hides openimageio modules like OpenImageIO_Util.

--- ports/openimageio/portfile.cmake
+++ ports/openimageio/portfile.cmake
@@ -83,9 +83,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/doc"
                     "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(COPY "${SOURCE_PATH}/src/cmake/modules/FindOpenImageIO.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/OpenImageIO")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
