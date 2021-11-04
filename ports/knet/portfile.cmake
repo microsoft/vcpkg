a94707ba0add55fcfd75760072396f85fa89d459
Kiba Amor <5093911+kibaamor@users.noreply.github.com>
2021-11-05 04:00:46 +0800

    [knet] Update to v1.0.3 (#21076)
    * [knet] Update to v1.0.3
    
    * [knet] Update to v1.0.3
    
    * update ci.baseline.txt
    
    * Update scripts/ci.baseline.txt
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>
    
    Co-authored-by: NancyLi1013 <46708020+NancyLi1013@users.noreply.github.com>

--- ports/knet/portfile.cmake
+++ ports/knet/portfile.cmake
@@ -5,24 +5,25 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kibaamor/knet
-    REF v1.0.2
-    SHA512 fb101d10d3bb08e565618923a10bc39586a934ae562ba7241c079f781baaafbc511e0bd2de13db6263246dede5e602d578e1fbdf45c84a6fecc4d3ddad4735e9
+    REF v1.0.3
+    SHA512 8bc4fb43b456d86ecae939cfae539432bde1f7ad2261abf9a7654d7476eb6502f53cd314f21dd23f77eb8d2969d293f6cb886729c688ea25911157c7fa3c8b7c
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
-        -DKNET_BUILD_EXAMPLE:BOOL=OFF
-        -DKNET_BUILD_TEST:BOOL=OFF
+        -DKNET_BUILD_EXAMPLE=OFF
+        -DKNET_BUILD_TEST=OFF
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/knet TARGET_PATH share/knet)
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/knet)
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
