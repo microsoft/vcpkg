f8417562a909b6347e28c44efd4d5f4b3586eb03
Matthias Kuhn <matthias@opengis.ch>
2021-11-05 20:56:10 +0100

    [pcre2] fetch from github (#21193)
    * [pcre2] fetch from github
    
    * version

--- ports/pcre2/portfile.cmake
+++ ports/pcre2/portfile.cmake
@@ -1,32 +1,14 @@
 set(PCRE2_VERSION 10.37)
 set(EXPECTED_SHA f91760a8e0747f52211612fb0e134d685e224d16bd884eb574718d077a586b1fd7b6435d4e3b75c879b12e02b252467ecc28cdc4bc2903c783dacab089f99c99)
-set(PATCHES
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO PhilipHazel/pcre2
+    REF pcre2-${PCRE2_VERSION}
+    SHA512   ${EXPECTED_SHA}
+    HEAD_REF master
+    PATCHES
         pcre2-10.35_fix-uwp.patch
 )
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.pcre.org/pub/pcre/pcre2-${PCRE2_VERSION}.zip"
-    FILENAME "pcre2-${PCRE2_VERSION}.zip"
-    SHA512 ${EXPECTED_SHA}
-    SILENT_EXIT
-)
-
-if (EXISTS "${ARCHIVE}")
-    vcpkg_extract_source_archive_ex(
-        OUT_SOURCE_PATH SOURCE_PATH
-        ARCHIVE ${ARCHIVE}
-        PATCHES ${PATCHES}
-    )
-else()
-    vcpkg_from_sourceforge(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO pcre/pcre2
-        REF ${PCRE2_VERSION}
-        FILENAME "pcre2-${PCRE2_VERSION}.zip"
-        SHA512 ${EXPECTED_SHA}
-        PATCHES ${PATCHES}
-    )
-endif()
 
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Emscripten" OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "iOS")
     set(JIT OFF)
