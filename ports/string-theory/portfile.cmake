1ae9e35142bff768d1d1d4700475227dcba1788f
chausner <chausner@users.noreply.github.com>
2021-11-05 21:31:45 +0100

    Fix CMake config

--- ports/string-theory/portfile.cmake
+++ ports/string-theory/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/string_theory)
+vcpkg_cmake_config_fixup(PACKAGE_NAME string_theory CONFIG_PATH lib/cmake/string_theory)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
