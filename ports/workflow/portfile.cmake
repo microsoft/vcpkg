0c12bfed86eee0534059b9fd71962c75376957dc
dengjun <dengjun@useful.plus>
2021-11-08 16:54:30 +0800

    fix function of fixup

--- ports/workflow/portfile.cmake
+++ ports/workflow/portfile.cmake
@@ -23,7 +23,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_fixup_targets(CONFIG_PATH lib/cmake TARGET_PATH share)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake TARGET_PATH share)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
