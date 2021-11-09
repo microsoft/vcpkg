88ceafb6a8ddf97117ca5a50a00626f3bafaf808
chausner <chausner@users.noreply.github.com>
2021-11-09 22:21:23 +0100

    Use asio package from vcpkg

--- ports/liblsl/portfile.cmake
+++ ports/liblsl/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF a9a06c78d4218b76e1c906c04452eec697f02f8e # NOTE: when updating version, also change it in the parameter to vcpkg_cmake_configure
     SHA512 b58665e836e6a1fc6b7669f7ba1208e14468b5f6b8ba3fdd06936cc47c8b32a027de49a4e252c8b6fabc13f846af30b7d209c6ba79842d165049f4baad7e4da9    
     HEAD_REF master
+    PATCHES
+        use-find-package-asio.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" LSL_BUILD_STATIC)
@@ -12,7 +14,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DLSL_BUILD_STATIC=${LSL_BUILD_STATIC}
-        -DLSL_BUNDLED_BOOST=OFF # we use the boost vcpkg package instead
+        -DLSL_BUNDLED_BOOST=OFF # we use the boost vcpkg packages instead
         -DLSL_BUNDLED_PUGIXML=OFF # we use the pugixml vcpkg package instead
         -Dlslgitrevision=v1.15.2-a9a06c
         -Dlslgitbranch=master
