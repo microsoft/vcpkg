4c76bd8a922838612165975205b9e4d0d43aa0ab
Ian Cook <ianmcook@gmail.com>
2021-11-11 15:35:20 -0500

    Remove quotes from empty DZSTD_MSVC_LIB_PREFIX

--- ports/arrow/portfile.cmake
+++ ports/arrow/portfile.cmake
@@ -83,7 +83,7 @@ vcpkg_cmake_configure(
         -DARROW_WITH_SNAPPY=ON
         -DARROW_WITH_ZLIB=ON
         -DARROW_WITH_ZSTD=ON
-        -DZSTD_MSVC_LIB_PREFIX=""
+        -DZSTD_MSVC_LIB_PREFIX=
     MAYBE_UNUSED_VARIABLES
         ZSTD_MSVC_LIB_PREFIX
 )
