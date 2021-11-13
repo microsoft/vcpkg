300ba452d2914a03ec5095a47ea0a91174e5ff6a
chausner <chausner@users.noreply.github.com>
2021-11-13 22:11:14 +0100

    Fix build on 64 bit platforms

--- ports/font-chef/portfile.cmake
+++ ports/font-chef/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v1.1.0
     SHA512 3df1e31e4405bcbb05ffed8fe618eb953498389adef3d83d337ac570644008bee031e08cd64382443ad123c4abf7e0acca5e3e16288caf6225672d6796a9494f
     HEAD_REF master
+    PATCHES
+        disable-warnings-as-errors.patch
 )
 
 vcpkg_cmake_configure(
