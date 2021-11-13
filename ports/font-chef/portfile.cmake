78cdbd20198e641ebc3bf00e69034febbc387035
chausner <chausner@users.noreply.github.com>
2021-11-13 22:17:10 +0100

    Add comment

--- ports/font-chef/portfile.cmake
+++ ports/font-chef/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 3df1e31e4405bcbb05ffed8fe618eb953498389adef3d83d337ac570644008bee031e08cd64382443ad123c4abf7e0acca5e3e16288caf6225672d6796a9494f
     HEAD_REF master
     PATCHES
-        disable-warnings-as-errors.patch
+        disable-warnings-as-errors.patch # to workaround https://github.com/mobius3/font-chef/issues/3
 )
 
 vcpkg_cmake_configure(
