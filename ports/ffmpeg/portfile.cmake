a2c26da78c7daba29131d0a38da04d5d58a42281
Andy Li <andy@onthewings.net>
2021-11-13 08:39:29 +0800

    [ffmpeg] update version to 4.4.1 (#21049)
    * [ffmpeg] update to 4.4.1
    
    * Update version database
    
    * update version
    
    * update version
    
    Co-authored-by: Jonliu1993 <13720414433@163.com>

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -133,8 +133,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ffmpeg/ffmpeg
-    REF n4.4
-    SHA512 ae7426ca476df9fa9dba52cab06c38e484f835653fb2da57e7c06f5589d887c0854ee17df93a2f57191d498c1264cb1c69312cf0a8214b476800382e2d260c4b
+    REF n4.4.1
+    SHA512 a53e617937f9892c5cfddb00896be9ad8a3e398dc7cf3b6c893b52ff38aff6ff0cbc61a44cd5f93d9a28f775e71ae82996a5e2b699a769c1de8f882aab34c797
     HEAD_REF master
     PATCHES
         0001-create-lib-libraries.patch
