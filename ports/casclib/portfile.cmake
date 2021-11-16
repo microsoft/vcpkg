85a562f1b587aa6c71c9508f1bfdfdaeaa37c4e3
chausner <chausner@users.noreply.github.com>
2021-11-16 22:29:52 +0100

    Add patch for shared Windows builds

--- ports/casclib/portfile.cmake
+++ ports/casclib/portfile.cmake
@@ -1,13 +1,11 @@
-if (VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ladislav-zezula/CascLib
     REF 07bfe85fd0804cba49f072819af716274d82b855
     SHA512 b333fe6318aa95629aa67613287ac71b1931c6443985e7e52412066d769f7996bffca1c19e890851eba976f572c7a11f14a8ff5fb95433577e1c9af8de0f34af
     HEAD_REF master
+    PATCHES
+        fix-shared-windows-builds.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" CASC_BUILD_SHARED_LIB)
@@ -21,6 +19,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/CascLib)
 
