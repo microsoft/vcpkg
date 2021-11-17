fc04b718e04320a10031cc14579eaf92afed06c6
autoantwort <41973254+autoantwort@users.noreply.github.com>
2021-11-17 01:25:28 +0100

    [teemo] use supports expression (#21449)

--- ports/teemo/portfile.cmake
+++ ports/teemo/portfile.cmake
@@ -10,32 +10,25 @@ vcpkg_from_github(
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" TEEMO_STATIC)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DTEEMO_STATIC:BOOL=${TEEMO_STATIC}
         -DBUILD_TESTS:BOOL=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/cmake/teemo)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/teemo)
-elseif(EXISTS ${CURRENT_PACKAGES_DIR}/share/teemo)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH share/teemo)
+if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/teemo")
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/teemo)
+elseif(EXISTS "${CURRENT_PACKAGES_DIR}/share/teemo")
+    vcpkg_cmake_config_fixup(CONFIG_PATH share/teemo)
 endif()
 
-file(READ ${CURRENT_PACKAGES_DIR}/include/teemo/teemo.h TEEMO_H)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    string(REPLACE "#ifdef TEEMO_STATIC" "#if 1" TEEMO_H "${TEEMO_H}")
-else()
-    string(REPLACE "#ifdef TEEMO_STATIC" "#if 0" TEEMO_H "${TEEMO_H}")
-endif()
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/teemo/teemo.h "${TEEMO_H}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/teemo/teemo.h" "#ifdef TEEMO_STATIC" "#if $<STREQUAL:${VCPKG_LIBRARY_LINKAGE},static>")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_copy_pdbs()
