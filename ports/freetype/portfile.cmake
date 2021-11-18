e7c4d82b91303c6706d04ca038c44282cc166602
JackBoosY <yuzaiyang@beyondsoft.com>
2021-11-17 23:01:25 -0800

    [freetype] Switch build system to meson

--- ports/freetype/portfile.cmake
+++ ports/freetype/portfile.cmake
@@ -8,65 +8,36 @@ vcpkg_from_sourceforge(
     SHA512 bf1991f3c382832586be1d21ae73c20840ee8546807ba60d0eb0215134545656c0c8de488f27357d4a4f6497d7cb540998cda98ec59061a3e640036fb209147d
     PATCHES
         0003-Fix-UWP.patch
-        fix-bzip2-pc.patch  # we have a bzip2 file that we can use - https://gitlab.freedesktop.org/freetype/freetype/-/commit/b2aeca5fda870751f3c9d645e0dca4c80fa1ae5a
-        brotli-static.patch
-        fix-exports.patch
         fix-2.11-msvc-build.patch
+        fix-find-bzip2.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        zlib        FT_WITH_ZLIB
-        bzip2       FT_WITH_BZIP2
-        png         FT_WITH_PNG
-        brotli      FT_WITH_BROTLI
-    INVERTED_FEATURES
-        zlib        CMAKE_DISABLE_FIND_PACKAGE_ZLIB
-        bzip2       CMAKE_DISABLE_FIND_PACKAGE_BZip2
-        png         CMAKE_DISABLE_FIND_PACKAGE_PNG
-        brotli      CMAKE_DISABLE_FIND_PACKAGE_BrotliDec
+        zlib        zlib
+        bzip2       bzip2
+        png         png
+        brotli      brotli
 )
 
-vcpkg_cmake_configure(
+list(TRANSFORM FEATURE_OPTIONS REPLACE "ON" "enabled")
+list(TRANSFORM FEATURE_OPTIONS REPLACE "OFF" "disabled")
+
+vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCMAKE_DISABLE_FIND_PACKAGE_HarfBuzz=ON
         ${FEATURE_OPTIONS}
+        -Dharfbuzz=disabled
 )
 
-vcpkg_cmake_install()
+vcpkg_install_meson()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/freetype)
 
 # Rename for easy usage (VS integration; CMake and autotools will not care)
 file(RENAME "${CURRENT_PACKAGES_DIR}/include/freetype2/freetype" "${CURRENT_PACKAGES_DIR}/include/freetype")
 file(RENAME "${CURRENT_PACKAGES_DIR}/include/freetype2/ft2build.h" "${CURRENT_PACKAGES_DIR}/include/ft2build.h")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/freetype2")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-# Fix the include dir [freetype2 -> freetype]
-file(READ "${CURRENT_PACKAGES_DIR}/share/freetype/freetype-targets.cmake" CONFIG_MODULE)
-string(REPLACE "\${_IMPORT_PREFIX}/include/freetype2" "\${_IMPORT_PREFIX}/include" CONFIG_MODULE "${CONFIG_MODULE}")
-string(REPLACE "\${_IMPORT_PREFIX}/lib/brotlicommon-static.lib" [[\$<\$<NOT:\$<CONFIG:DEBUG>>:${_IMPORT_PREFIX}/lib/brotlicommon-static.lib>;\$<\$<CONFIG:DEBUG>:${_IMPORT_PREFIX}/debug/lib/brotlicommon-static.lib>]] CONFIG_MODULE "${CONFIG_MODULE}")
-string(REPLACE "\${_IMPORT_PREFIX}/lib/brotlidec-static.lib" [[\$<\$<NOT:\$<CONFIG:DEBUG>>:${_IMPORT_PREFIX}/lib/brotlidec-static.lib>;\$<\$<CONFIG:DEBUG>:${_IMPORT_PREFIX}/debug/lib/brotlidec-static.lib>]] CONFIG_MODULE "${CONFIG_MODULE}")
-string(REPLACE "\${_IMPORT_PREFIX}/lib/brotlidec.lib" [[\$<\$<NOT:\$<CONFIG:DEBUG>>:${_IMPORT_PREFIX}/lib/brotlidec.lib>;\$<\$<CONFIG:DEBUG>:${_IMPORT_PREFIX}/debug/lib/brotlidec.lib>]] CONFIG_MODULE "${CONFIG_MODULE}")
-string(REPLACE "\${_IMPORT_PREFIX}/lib/brotlidec.lib" [[\$<\$<NOT:\$<CONFIG:DEBUG>>:${_IMPORT_PREFIX}/lib/brotlidec.lib>;\$<\$<CONFIG:DEBUG>:${_IMPORT_PREFIX}/debug/lib/brotlidec.lib>]] CONFIG_MODULE "${CONFIG_MODULE}")
-file(WRITE ${CURRENT_PACKAGES_DIR}/share/freetype/freetype-targets.cmake "${CONFIG_MODULE}")
-
-find_library(FREETYPE_DEBUG NAMES freetyped PATHS "${CURRENT_PACKAGES_DIR}/debug/lib/" NO_DEFAULT_PATH)
-if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/freetype2.pc")
-    file(READ "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/freetype2.pc" _contents)
-    if(FREETYPE_DEBUG)
-        string(REPLACE "-lfreetype" "-lfreetyped" _contents "${_contents}")
-    endif()
-    string(REPLACE "-I\${includedir}/freetype2" "-I\${includedir}" _contents "${_contents}")
-    file(WRITE "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/freetype2.pc" "${_contents}")
-endif()
-if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/freetype2.pc")
-    file(READ "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/freetype2.pc" _contents)
-    string(REPLACE "-I\${includedir}/freetype2" "-I\${includedir}" _contents "${_contents}")
-    file(WRITE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/freetype2.pc" "${_contents}")
-endif()
 
 vcpkg_fixup_pkgconfig()
 
