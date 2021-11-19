8da4e63ea1a186eefbc2a36c8629f1c848cca184
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-11-19 15:05:50 +0100

    fix subtle bugs in vcpkg_find_acquire_program make the port install meson if the system does not provide it.

--- ports/vcpkg-tool-meson/portfile.cmake
+++ ports/vcpkg-tool-meson/portfile.cmake
@@ -3,5 +3,52 @@
 # Currently is used in ports that call vcpkg_find_acquire_program(MESON) in order to force rebuilds.
 
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-vcpkg_find_acquire_program(MESON)
+
+set(program MESON)
+set(program_version 0.60.1)
+set(program_name meson)
+set(search_names meson meson.py)
+set(interpreter PYTHON3)
+set(apt_package_name "meson")
+set(brew_package_name "meson")
+set(ref 4859d7039b05d31216ddeda3fbc528991bc95cac)
+set(paths_to_search "${CURRENT_PACKAGES_DIR}/tools/meson")
+set(download_urls "https://github.com/mesonbuild/meson/archive/${ref}.tar.gz")
+set(download_filename "meson-${ref}.tar.gz")
+set(download_sha512 1208f0eae94038f3dd2b68468dcf0f7be78f36efffc945e0ae6ece5930b4da01e16d3bb0df6cd37d6cf6dd652880ec91ee4ed951fd0d4685a10064818fdaeb09)
+set(supported_on_unix ON)
+set(version_command --version)
+set(extra_search_args EXACT_VERSION_MATCH)
+
+vcpkg_find_acquire_program(PYTHON3)
+
+z_vcpkg_find_acquire_program_find_external("${program}"
+    ${extra_search_args}
+    PROGRAM_NAME "${program_name}"
+    MIN_VERSION "${program_version}"
+    INTERPRETER "${interpreter}"
+    NAMES ${search_names}
+    VERSION_COMMAND ${version_command}
+)
+
+if(NOT "${program}")
+    vcpkg_download_distfile(archive_path
+        URLS ${download_urls}
+        SHA512 "${download_sha512}"
+        FILENAME "${download_filename}"
+    )
+    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools")
+    vcpkg_execute_in_download_mode(
+                        COMMAND "${CMAKE_COMMAND}" -E tar xzf "${archive_path}"
+                        WORKING_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools"
+                    )
+    file(RENAME "${CURRENT_PACKAGES_DIR}/tools/meson-${ref}" "${CURRENT_PACKAGES_DIR}/tools/meson")
+endif()
+
+z_vcpkg_find_acquire_program_find_internal("${program}"
+    INTERPRETER "${interpreter}"
+    PATHS ${paths_to_search}
+    NAMES ${search_names}
+)
+#vcpkg_find_acquire_program(MESON)
 message(STATUS "Using meson: ${MESON}")
