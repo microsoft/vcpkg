a130233df33779b9293752882e74bff82b40ee91
Christopher Schwartz <71826935+ChristopherSchwartzXRITE@users.noreply.github.com>
2021-11-19 06:28:02 +0100

    [openblas] make openblas build on MinGW (#20986)
    * [openblas] make openblas build on MinGW
    
    * Portfile cleanup: Join separate vcpkg_check_feature calls and add double quotes according to maintainer guidelines.
    
    * Portfile cleanup: Migrate vcpkg cmake commands from deprecated to new functions.
    
    * Portfile cleanup: Migrate vcpkg cmake commands from deprecated to new functions (2) - missed a spot.
    
    * Re-factory code
    
    * version
    
    * [shogun] Fix include cblas path
    
    * version
    
    Co-authored-by: JackBoosY <yuzaiyang@beyondsoft.com>

--- ports/openblas/portfile.cmake
+++ ports/openblas/portfile.cmake
@@ -10,17 +10,18 @@ vcpkg_from_github(
         fix-redefinition-function.patch
         fix-uwp-build.patch
         fix-marco-conflict.patch
+        install-tools.patch
 )
 
 find_program(GIT NAMES git git.cmd)
 
 # sed and awk are installed with git but in a different directory
-get_filename_component(GIT_EXE_PATH ${GIT} DIRECTORY)
+get_filename_component(GIT_EXE_PATH "${GIT}" DIRECTORY)
 set(SED_EXE_PATH "${GIT_EXE_PATH}/../usr/bin")
 
 # openblas require perl to generate .def for exports
 vcpkg_find_acquire_program(PERL)
-get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
+get_filename_component(PERL_EXE_PATH "${PERL}" DIRECTORY)
 set(PATH_BACKUP "$ENV{PATH}")
 vcpkg_add_to_path("${PERL_EXE_PATH}")
 vcpkg_add_to_path("${SED_EXE_PATH}")
@@ -29,14 +30,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         threads         USE_THREAD
         simplethread    USE_SIMPLE_THREADED_LEVEL3
+        "dynamic-arch"  DYNAMIC_ARCH
 )
 
 set(COMMON_OPTIONS -DBUILD_WITHOUT_LAPACK=ON)
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    	"dynamic-arch"      DYNAMIC_ARCH
-)
 
 if(VCPKG_TARGET_IS_OSX)
     if("dynamic-arch" IN_LIST FEATURES)
@@ -45,67 +42,41 @@ if(VCPKG_TARGET_IS_OSX)
     endif()
 endif()
 
+set(OPENBLAS_EXTRA_OPTIONS)
 # for UWP version, must build non uwp first for helper
 # binaries.
-if(VCPKG_TARGET_IS_UWP)
-    message(STATUS "Building Windows helper files")
-    set(TEMP_CMAKE_SYSTEM_NAME "${VCPKG_CMAKE_SYSTEM_NAME}")
-    set(TEMP_CMAKE_SYSTEM_VERSION "${VCPKG_CMAKE_SYSTEM_VERSION}")
-    set(TEMP_TARGET_TRIPLET "${TARGET_TRIPLET}")
-    unset(VCPKG_CMAKE_SYSTEM_NAME)
-    unset(VCPKG_CMAKE_SYSTEM_VERSION)
-    set(TARGET_TRIPLET "x64-windows")
-
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS ${FEATURE_OPTIONS}
-            ${COMMON_OPTIONS}
-            -DTARGET=NEHALEM
-    )
-
-    # add just built path to environment for gen_config_h.exe,
-    # getarch.exe and getarch_2nd.exe
-    vcpkg_add_to_path("${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-
-    # restore target build information
-    set(VCPKG_CMAKE_SYSTEM_NAME "${TEMP_CMAKE_SYSTEM_NAME}")
-    set(VCPKG_CMAKE_SYSTEM_VERSION "${TEMP_CMAKE_SYSTEM_VERSION}")
-    set(TARGET_TRIPLET "${TEMP_TARGET_TRIPLET}")
-
-    message(STATUS "Finished building Windows helper files")
-
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            -DCMAKE_SYSTEM_PROCESSOR=AMD64
-            -DVS_WINRT_COMPONENT=TRUE
-            "-DBLASHELPER_BINARY_DIR=${CURRENT_BUILDTREES_DIR}/x64-windows-rel")
-
-elseif(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_configure_cmake(
-        PREFER_NINJA
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            ${FEATURE_OPTIONS}
-    )
-else()
+if(VCPKG_TARGET_IS_UWP)    
+    list(APPEND OPENBLAS_EXTRA_OPTIONS -DCMAKE_SYSTEM_PROCESSOR=AMD64
+                "-DBLASHELPER_BINARY_DIR=${CURRENT_HOST_INSTALLED_DIR}/tools/${PORT}")
+elseif(NOT (VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW))
     string(APPEND VCPKG_C_FLAGS " -DNEEDBUNDERSCORE") # Required to get common BLASFUNC to append extra _
     string(APPEND VCPKG_CXX_FLAGS " -DNEEDBUNDERSCORE")
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            ${FEATURE_OPTIONS}
-            -DNOFORTRAN=ON
-            -DBU=_  #required for all blas functions to append extra _ using NAME
-            )
+    list(APPEND OPENBLAS_EXTRA_OPTIONS
+                -DNOFORTRAN=ON
+                -DBU=_  #required for all blas functions to append extra _ using NAME
+    )
 endif()
 
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        ${COMMON_OPTIONS}
+        ${OPENBLAS_EXTRA_OPTIONS}
+)
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/OpenBLAS TARGET_PATH share/openblas)
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/OpenBLAS)
+
+if (EXISTS "${CURRENT_PACKAGES_DIR}/bin/getarch${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+    vcpkg_copy_tools(TOOL_NAMES getarch AUTO_CLEAN)
+endif()
+if (EXISTS "${CURRENT_PACKAGES_DIR}/bin/getarch_2nd${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+    vcpkg_copy_tools(TOOL_NAMES getarch_2nd AUTO_CLEAN)
+endif()
+
 set(ENV{PATH} "${PATH_BACKUP}")
 
 set(pcfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/openblas.pc")
@@ -128,14 +99,14 @@ vcpkg_fixup_pkgconfig()
 # openblas do not make the config file , so I manually made this
 # but I think in most case, libraries will not include these files, they define their own used function prototypes
 # this is only to quite vcpkg
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/openblas_common.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/openblas_common.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-file(READ ${SOURCE_PATH}/cblas.h CBLAS_H)
-string(REPLACE "#include \"common.h\"" "#include \"openblas_common.h\"" CBLAS_H "${CBLAS_H}")
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/cblas.h "${CBLAS_H}")
+vcpkg_replace_string(
+    "${SOURCE_PATH}/cblas.h"
+    "#include \"common.h\""
+    "#include \"openblas_common.h\""
+)
 
-vcpkg_copy_pdbs()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
-
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
