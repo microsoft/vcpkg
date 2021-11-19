b56024295d0604f917f02d22ae5b611fbc9e8aa9
Kai Pastor <dg0yt@darc.de>
2021-11-19 08:24:48 +0100

    Patch and use pc file installation from upstream

--- ports/proj4/portfile.cmake
+++ ports/proj4/portfile.cmake
@@ -1,8 +1,7 @@
-set(VERSION 8.1.1) # Variable used for pc file
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OSGeo/PROJ
-    REF "${VERSION}"
+    REF 8.1.1
     SHA512 a26d4191905ac01ce8052290dbd065038bb59bdf5ee4ead0b8ba948de7fcc9d7bffd897533a07ffc2e9824b59210fa2a6cec652e512794a9ef9b07ce40e7e213
     HEAD_REF master
     PATCHES
@@ -10,6 +9,7 @@ vcpkg_from_github(
         fix-win-output-name.patch
         fix-proj4-targets-cmake.patch
         tools-cmake.patch
+        pkgconfig.patch
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
@@ -63,46 +63,10 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_copy_pdbs()
 
-set(PACKAGE "${PORT}")
-set(requires_private "sqlite3")
-if(ENABLE_CURL)
-    string(APPEND requires_private " libcurl")
-endif()
-if(ENABLE_TIFF)
-    string(APPEND requires_private " libtiff-4")
-endif()
-set(libs_private "")
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-    string(APPEND libs_private " -lole32 -lshell32")
-elseif(VCPKG_TARGET_IS_OSX)
-    string(APPEND libs_private " -lc++ -lm")
-else()
-    string(APPEND libs_private " -lstdc++ -lm")
-endif()
-set(libdir [[${prefix}/lib]])
-set(exec_prefix [[${prefix}]])
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    set(includedir [[${prefix}/include]])
-    set(datarootdir [[${prefix}/share]])
-    set(datadir [[${prefix}/share]])
-    set(outfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/proj.pc")
-    configure_file("${SOURCE_PATH}/proj.pc.in" "${outfile}" @ONLY)
-    vcpkg_replace_string("${outfile}" "Requires:" "Requires.private: ${requires_private}")
-    vcpkg_replace_string("${outfile}" " -lstdc++" "${libs_private}")
-endif()
-if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    set(includedir [[${prefix}/../include]])
-    set(datarootdir [[${prefix}/../share]])
-    set(datadir [[${prefix}/../share]])
-    set(outfile "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/proj.pc")
-    configure_file("${SOURCE_PATH}/proj.pc.in" "${outfile}" @ONLY)
-    vcpkg_replace_string("${outfile}" "Requires:" "Requires.private: ${requires_private}")
-    vcpkg_replace_string("${outfile}" " -lstdc++" "${libs_private}")
-    if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
-        vcpkg_replace_string("${outfile}" " -lproj" " -lproj_d")
-    endif()
-endif()
 vcpkg_fixup_pkgconfig()
+if(NOT DEFINED VCPKG_BUILD_TYPE AND VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/proj.pc" " -lproj" " -lproj_d")
+endif()
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
