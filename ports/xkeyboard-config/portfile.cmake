46a80eeeaa0ac3fd952ae6eaca501cf7ae896c90
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-20 00:25:17 +0100

    [xkeyboard-config] add port

---
+++ ports/xkeyboard-config/portfile.cmake
@@ -0,0 +1,27 @@
+# Get source code:
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO xkeyboard-config/xkeyboard-config
+    REF  b5f7adf3fd85b90c1fd5d85186ea5cb0d5db69be
+    SHA512 2a4cc13507401858e469333805471367a75697716ecae542a212696cc5c556999f6f18bc0fd3ec436065ed7419b1cd40251bcb8acb6932e75adb70de046fa776
+    HEAD_REF master # branch name
+) 
+
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS -Dxorg-rules-symlinks=false
+)
+vcpkg_find_acquire_program(PERL)
+get_filename_component(PERL_PATH "${PERL}" DIRECTORY)
+vcpkg_add_to_path("${PERL_PATH}")
+vcpkg_install_meson()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
