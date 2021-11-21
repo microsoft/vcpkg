c0194b3be9c46c021cdf0cf666821458bb1bf5ad
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-21 23:21:37 +0100

    [xcb-keysms] add port

---
+++ ports/xcb-keysyms/portfile.cmake
@@ -0,0 +1,31 @@
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+endif()
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxcb-keysyms
+    REF  0e51ee5570a6a80bdf98770b975dfe8a57f4eeb1 #v 0.4.0
+    SHA512 5fe01c8902ed09582c6c5d98a24dc2a19e16f507200e7ee4a75889d0fec4e2faea910a1994b0c85e3139a28b563b1edf5db86284e5d19e14760f39eb893f623c
+    HEAD_REF master # branch name
+) 
+file(TOUCH "${SOURCE_PATH}/m4/dummy")
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Handle copyright
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
+file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
+
