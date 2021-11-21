1bf2145f16997f3b1c815dd1ef512af4814e2a8e
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-21 23:21:07 +0100

    [xcb-image] add port

---
+++ ports/xcb-image/portfile.cmake
@@ -0,0 +1,31 @@
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+endif()
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxcb-image
+    REF  d882052fb2ce439c6483fce944ba8f16f7294639 #v 0.4.0
+    SHA512 60d933d60351071fa47540a7943a34fd57bd46ebcd7441020c8f3f0ef4abc44fbb8e86e72e328d64992254ce20d5b718bede9f4994dbd0674fbf5ce88050c307
+    HEAD_REF master # branch name
+) 
+file(TOUCH "${SOURCE_PATH}/m4/dummy")
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Handle copyright
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
+file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
+
