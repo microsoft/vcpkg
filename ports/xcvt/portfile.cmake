3f11337314adafc1311a43ed4ae6d370342cccf6
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-22 18:12:00 +0100

    [xcvt] add new port

---
+++ ports/xcvt/portfile.cmake
@@ -0,0 +1,29 @@
+set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON)
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+else()
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxcvt
+    REF 6fe840b9295cfdc41bd734586c5b8756f6af6f9b # 0.1.1
+    SHA512  81472455e3ce5645b8b0aa79222e9fc5de92f09429bf9a1641de259b8152253fda59abbcde97c99f59f313e9a571f2545e9420d7ba5ee9a38f70302fdce09f53
+    HEAD_REF master # branch name
+) 
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_install_meson()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_tools(TOOL_NAMES cvt AUTO_CLEAN)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+endif()
