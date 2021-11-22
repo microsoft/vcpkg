6dc42785057de4c9e6602eaddf69e0605d2a9329
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-22 22:30:15 +0100

    fix xcb-image on osx.

--- ports/xcb-image/portfile.cmake
+++ ports/xcb-image/portfile.cmake
@@ -1,7 +1,8 @@
+set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON)
 if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
     message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
     set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-endif()
+else()
 
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org/xorg
@@ -10,6 +11,7 @@ vcpkg_from_gitlab(
     REF  d882052fb2ce439c6483fce944ba8f16f7294639 #v 0.4.0
     SHA512 60d933d60351071fa47540a7943a34fd57bd46ebcd7441020c8f3f0ef4abc44fbb8e86e72e328d64992254ce20d5b718bede9f4994dbd0674fbf5ce88050c307
     HEAD_REF master # branch name
+    PATCHES add_return_value.patch
 ) 
 file(TOUCH "${SOURCE_PATH}/m4/dummy")
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
@@ -28,4 +30,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 # Handle copyright
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
 file(TOUCH "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright") #already installed by xproto
-
+endif()
