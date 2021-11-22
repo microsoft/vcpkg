4e9e8a60732027ec30686e1b671a4e1b99b46b4c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-22 13:53:51 +0100

    [xxf86dga] add port

---
+++ ports/xxf86dga/portfile.cmake
@@ -0,0 +1,31 @@
+set(X_VCPKG_FORCE_VCPKG_X_LIBRARIES ON) # For Testing 
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+endif()
+
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxxf86dga
+    REF e605c540995554af99d8b728c7d1d4e82e514199 # 1.1.5 
+    SHA512  910b90d3ea8f6267f90cd4c0d717a661e8181e5caecf119adbd866f8a478d9b84e24d7da41c6bd2591363909ca9421e56631cdd557a8e5043d2ee3645146db3f
+    HEAD_REF master # branch name
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
