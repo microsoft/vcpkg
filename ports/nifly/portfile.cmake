02e5ba3377cc98fb876a7155457ce61f7aa5343c
Guekka <39066502+Guekka@users.noreply.github.com>
2021-11-24 18:47:06 +0000

    [nifly] Update ports/nifly/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/nifly/portfile.cmake
+++ ports/nifly/portfile.cmake
@@ -17,8 +17,9 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "cmake/")
 
 file(REMOVE_RECURSE
-	${CURRENT_PACKAGES_DIR}/debug/include
-	${CURRENT_PACKAGES_DIR}/debug/share
+	"${CURRENT_PACKAGES_DIR}/debug/include"
+	"${CURRENT_PACKAGES_DIR}/debug/share"
+
 )
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
