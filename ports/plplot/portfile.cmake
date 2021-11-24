microsoft/vcpkg

commit a5ff5cdf2f6611358c9cfdf9c8460d0eed683a90

[plplot] no absolute paths (#21303)

ports/plplot/portfile.cmake
@@ -15,14 +15,14 @@ vcpkg_from_sourceforge(
       fix-pkg-config.patch
 )
 
-set(BUILD_with_wxwidgets OFF)
-if("wxwidgets" IN_LIST FEATURES)
-  set(BUILD_with_wxwidgets ON)
-endif()
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        wxwidgets BUILD_with_wxwidgets
+)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DENABLE_tcl=OFF
         -DENABLE_d=OFF
@@ -31,41 +31,45 @@ vcpkg_configure_cmake(
         -DPL_HAVE_QHULL=OFF
         -DPLPLOT_USE_QT5=OFF
         -DPL_DOUBLE=ON
-        -DPLD_wxwidgets=${BUILD_with_wxwidgets}
         -DENABLE_DYNDRIVERS=OFF
         -DDATA_DIR=${CURRENT_PACKAGES_DIR}/share/plplot
+        ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         -DDRV_DIR=${CURRENT_PACKAGES_DIR}/debug/bin
     OPTIONS_RELEASE
         -DDRV_DIR=${CURRENT_PACKAGES_DIR}/bin
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/plplot)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/plplot)
+
+if(EXISTS "${CURRENT_PACKAGES_DIR}/share/plplot/examples/plplot-test-interactive.sh")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/plplot/examples/plplot-test-interactive.sh" "${CURRENT_PACKAGES_DIR}/share/plplot" "../")
+endif()
 
 # Remove unnecessary tool
 file(REMOVE
-    ${CURRENT_PACKAGES_DIR}/debug/bin/pltek.exe
-    ${CURRENT_PACKAGES_DIR}/bin/pltek.exe
-    ${CURRENT_PACKAGES_DIR}/debug/bin/wxPLViewer.exe
-    ${CURRENT_PACKAGES_DIR}/bin/wxPLViewer.exe
+    "${CURRENT_PACKAGES_DIR}/debug/bin/pltek.exe"
+    "${CURRENT_PACKAGES_DIR}/bin/pltek.exe"
+    "${CURRENT_PACKAGES_DIR}/debug/bin/wxPLViewer.exe"
+    "${CURRENT_PACKAGES_DIR}/bin/wxPLViewer.exe"
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     message(STATUS "Static build: Removing the full bin directory.")
     file(REMOVE_RECURSE
-        ${CURRENT_PACKAGES_DIR}/debug/bin
-        ${CURRENT_PACKAGES_DIR}/bin
+        "${CURRENT_PACKAGES_DIR}/debug/bin"
+        "${CURRENT_PACKAGES_DIR}/bin"
     )
 endif()
 
 # Remove unwanted and duplicate directories
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/Copyright DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/Copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 vcpkg_fixup_pkgconfig()

commit 4502f70650ba658e741f9d9dde991f7c3323c41f

[plplot] Correct control of wxwidgets probably broken by https://github.com/microsoft/vcpkg/pull/21303/
This caused wxwidgets to get set on as far as the upstream build system cares if wxwidgets was built first regardless of the feature setting, which broke in CI with unresolved external symbol errors in wxwidgets' internals.

ports/plplot/portfile.cmake
@@ -18,7 +18,8 @@ vcpkg_from_sourceforge(
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        wxwidgets BUILD_with_wxwidgets
+        wxwidgets PLD_wxwidgets
+        wxwidgets ENABLE_wxwidgets
 )
 
 vcpkg_cmake_configure(