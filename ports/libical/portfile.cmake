1b387cc470c9c1e59235a6d564b0e49e460262fc
Dawid Wróbel <me@dawidwrobel.com>
2021-11-25 16:52:59 +0100

    [libical] Add 'glib' feature, make TZ automatic
    - add glib feauture, which enables compilation of libical-glib
    - don't explicitly enable BUILTIN_TZDATA, this gets enabled
      automatically depending on the system (currently on Windows only)

--- ports/libical/portfile.cmake
+++ ports/libical/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO libical/libical
     REF v3.0.11
     SHA512 cdee86c50edc2373ab2024d7d4ae26dd4b9a728dbc13083472c4923c67f61ff3cef7d43edca762c6a11979d2040fc1576a033eaa23a19e58af8f14a7d67fc139
+    PATCHES
+        glib.patch # https://github.com/libical/libical/pull/530
 )
 
 vcpkg_find_acquire_program(PERL)
@@ -10,6 +12,8 @@ get_filename_component(PERL_PATH ${PERL} DIRECTORY)
 vcpkg_add_to_path(${PERL_PATH})
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        "glib"      ICAL_GLIB
     INVERTED_FEATURES
         "rscale"    CMAKE_DISABLE_FIND_PACKAGE_ICU
 )
@@ -24,8 +28,6 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCMAKE_DISABLE_FIND_PACKAGE_BDB=ON
-        -DUSE_BUILTIN_TZDATA=ON
-        -DICAL_GLIB=OFF
         -DICAL_BUILD_DOCS=OFF
         -DLIBICAL_BUILD_TESTING=OFF
         ${FEATURE_OPTIONS}
