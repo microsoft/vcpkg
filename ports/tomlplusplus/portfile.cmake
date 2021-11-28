631f1dd837aa1d905367dcc897261ee17212db8b
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2021-11-28 14:48:13 +0100

    [tomlplusplus] no absolute paths

--- ports/tomlplusplus/portfile.cmake
+++ ports/tomlplusplus/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_github(
 )
 
 vcpkg_configure_meson(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -Dgenerate_cmake_config=true
         -Dbuild_tests=false
@@ -17,7 +17,9 @@ vcpkg_configure_meson(
 )
 
 vcpkg_install_meson()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+cmake_path(NATIVE_PATH SOURCE_PATH native_source_path)
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/tomlplusplus/tomlplusplusConfig.cmake" "${native_source_path}" "")
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug"
