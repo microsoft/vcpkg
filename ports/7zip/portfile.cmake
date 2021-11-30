210c0afed9225ee1dc1356d128c323578c00374c
Frank xie <v-frankxie@microsoft.com>
2021-11-29 17:57:03 -0800

    update to 21.06

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -1,10 +1,9 @@
-vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "uwp" "linux" "osx")
 
-set(7ZIP_VERSION 19.00)
+set(7ZIP_VERSION 21.06)
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.7-zip.org/a/7z1900-src.7z"
-    FILENAME "7z1900-src.7z"
-    SHA512 d68b308e175224770adc8b1495f1ba3cf3e7f67168a7355000643d3d32560ae01aa34266f0002395181ed91fb5e682b86e0f79c20625b42d6e2c62dd24a5df93
+    URLS "https://www.7-zip.org/a/7z2106-src.7z"
+    FILENAME "7z2106-src.7z"
+    SHA512 2ad05eaf14770584d7e6111734c7c239ab0163f9fd9b0cc5473fa33d5c882b0493331dd0f851af5e8be2b7d1eaede2f96f30f8e777b05d17c46a0121ff8211ec 
 )
 
 vcpkg_extract_source_archive_ex(
@@ -14,19 +13,18 @@ vcpkg_extract_source_archive_ex(
     NO_REMOVE_ONE_LEVEL
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
 file(
-    INSTALL ${CMAKE_CURRENT_LIST_DIR}/License.txt
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    INSTALL "${CMAKE_CURRENT_LIST_DIR}/License.txt"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     RENAME copyright
 )
