dc4097c04c5886ca8f54038533da2d8a43c13a4e
Jonliu1993 <13720414433@163.com>
2021-12-01 15:51:27 +0800

    [xxhash] update to version 0.8.1

--- ports/xxhash/portfile.cmake
+++ ports/xxhash/portfile.cmake
@@ -1,30 +1,30 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Cyan4973/xxHash
-    REF 94e5f23e736f2bb67ebdf90727353e65344f9fc0 # v0.8.0
-    SHA512 367c82b37fd188890574446a7c237294a4a9ba038aff9a19a5d5d119058df74c089c682e9abad3281d5e14020c08834666343b2ea18378f400c8a0ee640098b8
+    REF 35b0373c697b5f160d3db26b1cbb45a0d5ba788c # v0.8.1
+    SHA512 0d30230d1bd25c56e74e668cff0a1f5b923f9cbb2001cf6c77749cf2580be7af4c3e99f073c1b956dec97c0ac2e29ac6240730c708329640ad6b4b1b9ded2b57
     HEAD_REF dev
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    xxhsum XXHASH_BUILD_XXHSUM
+    FEATURES
+        xxhsum XXHASH_BUILD_XXHSUM
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}/cmake_unofficial
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/cmake_unofficial"
     OPTIONS ${FEATURE_OPTIONS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/xxHash TARGET_PATH share/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/xxHash)
 
 if("xxhsum" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES xxhsum AUTO_CLEAN)
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
