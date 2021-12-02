040a6b4c4430d947f6bdf9c9e047c1b948a51295
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-12-02 22:36:59 +0100

    fix qtapplication?

--- ports/qtapplicationmanager/portfile.cmake
+++ ports/qtapplicationmanager/portfile.cmake
@@ -1,13 +1,17 @@
 set(SCRIPT_PATH "${CURRENT_INSTALLED_DIR}/share/qtbase")
 include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
-set(${PORT}_PATCHES)
+set(${PORT}_PATCHES bump-cmake-version.patch)
 
 set(TOOL_NAMES)
 
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
                      CONFIGURE_OPTIONS
+                        -DINPUT_libarchive=system
+                        -DINPUT_libyaml=system
+                        -DFEATURE_am_system_libyaml=ON
+                        -DFEATURE_am_system_libarchive=ON
                      CONFIGURE_OPTIONS_RELEASE
                      CONFIGURE_OPTIONS_DEBUG
                     )
