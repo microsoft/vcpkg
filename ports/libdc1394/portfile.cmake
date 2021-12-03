1d0376093c3dd0e68fe2c82e5258a8413885dae9
Zhiqiang Niu <nzqpeace@gmail.com>
2021-12-03 14:18:00 +0800

    [libdc1394] fetch source code with vcpkg_from_sourceforge instead

--- ports/libdc1394/portfile.cmake
+++ ports/libdc1394/portfile.cmake
@@ -1,14 +1,11 @@
 set(LIBDC1394_VER "2.2.6")
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://udomain.dl.sourceforge.net/project/libdc1394/libdc1394-2/${LIBDC1394_VER}/libdc1394-${LIBDC1394_VER}.tar.gz"
+vcpkg_from_sourceforge(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO libdc1394/libdc1394-2
+    REF "${LIBDC1394_VER}"
     FILENAME "libdc1394-${LIBDC1394_VER}.tar.gz"
     SHA512 2d60ed1054da67d8518e870193b60c1d79778858f48cc6487e252de00cc57a08548515d41914a37d0227d29e158d68892c290f83930ffd95f4a483dce5aa3d25
-)
-
-vcpkg_extract_source_archive_ex(
-    ARCHIVE "${ARCHIVE}"
-    OUT_SOURCE_PATH SOURCE_PATH
 )
 
 vcpkg_configure_make(
