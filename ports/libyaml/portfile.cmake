e1d2851c299e80ec4268c06630d8d490f6ae7064
JackBoosY <yuzaiyang@beyondsoft.com>
2021-12-03 01:51:43 -0800

    Fix cmake config file path

--- ports/libyaml/portfile.cmake
+++ ports/libyaml/portfile.cmake
@@ -24,7 +24,7 @@ vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH share/yaml)
+vcpkg_cmake_config_fixup(PACKAGE_NAME yaml CONFIG_PATH share/yaml)
 
 vcpkg_fixup_pkgconfig()
 
