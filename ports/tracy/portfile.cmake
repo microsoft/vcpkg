4e994287f02bd64480cc33920aca70f26fc58326
Honeybunch <amt3824@g.rit.edu>
2021-12-02 18:08:39 -0800

    Responding to feedback and double checking which builds will work

--- ports/tracy/portfile.cmake
+++ ports/tracy/portfile.cmake
@@ -24,11 +24,11 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Tracy)
 vcpkg_fixup_pkgconfig()
 
 # Need to manually copy headers
-file(GLOB INCLUDES ${CURRENT_PACKAGES_DIR}/include/*)
+file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/*")
 file(COPY ${INCLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 # Cleanup
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
