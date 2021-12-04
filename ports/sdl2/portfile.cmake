76d80a501f6f8ebffa30194943716fcd0a027741
crackedmind <digital.stream.of.mind@gmail.com>
2021-12-04 16:22:52 +0500

    [sdl2] Update to 2.0.18

--- ports/sdl2/portfile.cmake
+++ ports/sdl2/portfile.cmake
@@ -1,16 +1,14 @@
-set(SDL2_VERSION 2.0.16)
+set(SDL2_VERSION 2.0.18)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libsdl-org/SDL
-    REF release-2.0.16
-    SHA512 45ce71f77b01f5fd886f92e5b3d96f1f72c7e0f70c09e615384a900533b941cad65bf6b54a125a9eeb8499e2056e9a8e54d4e654bccfca9730584792a2b18fbc
+    REF release-2.0.18
+    SHA512 a98114f339c6610935bc611652c4a07262f6a3eeb9fa45502fdecd656dbf43302799809194ac8b7dbea08f00132329b807931c533d87d63cc6ef82178a7cb317
     HEAD_REF master
     PATCHES
         0001-sdl2-Enable-creation-of-pkg-cfg-file-on-windows.patch
         0002-sdl2-skip-ibus-on-linux.patch
         0003-sdl2-disable-sdlmain-target-search-on-uwp.patch
-        0004-sdl2-alias-on-static-build.patch
-        0005-sdl2-fix-mingw-checks.patch # Can be removed once >= SDL 2.0.18
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL_STATIC)
