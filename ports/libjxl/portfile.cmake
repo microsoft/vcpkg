e04662bfc05e924b30ea6150603e53dfbfaeef30
Kai Pastor <dg0yt@darc.de>
2021-12-04 10:29:31 +0100

    [libjxl] Fix link flags

--- ports/libjxl/portfile.cmake
+++ ports/libjxl/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
     PATCHES
         fix-install-directories.patch
         fix-dependencies.patch
+        fix-link-flags.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
