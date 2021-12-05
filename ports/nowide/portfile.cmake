0be9f021fb7ee86e4f6e9862ed0a549f5123bda0
Tatsunori Uchino <tats.u@live.jp>
2021-12-05 23:23:40 +0900

    [nowide] Don't use deprecated functions anymore

--- ports/nowide/portfile.cmake
+++ ports/nowide/portfile.cmake
@@ -9,15 +9,14 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/nowide TARGET_PATH share/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/nowide)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
