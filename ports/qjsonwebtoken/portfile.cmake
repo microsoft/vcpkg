170ae18035c56b9a26db1a5349e25837d4223d15
Vitaly Petrov <v31337@gmail.com>
2021-12-06 12:08:09 +0300

    Changes by review

--- ports/qjsonwebtoken/portfile.cmake
+++ ports/qjsonwebtoken/portfile.cmake
@@ -1,13 +1,9 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO juangburgos/QJsonWebToken
     REF master
     SHA512 20f30d264ca5f761fcba27ddd05ed022bfef47a0e584683750a6eda23e3db12cdb4e8c8f95699e805328a42588e15aff8e5abe4554b4d99eb6c596cfa32b922e
     HEAD_REF master
-    #PATCHES 
-    #    allow-build-static.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
