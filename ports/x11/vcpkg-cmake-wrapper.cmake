8460ea35bc6006d312e873e9e3ebe8fa0c1f6610
Alexander Neumann <neumann@imt.uni-luebeck.de>
2021-12-07 10:20:53 +0100

    add xcb to x11

--- ports/x11/vcpkg-cmake-wrapper.cmake
+++ ports/x11/vcpkg-cmake-wrapper.cmake
@@ -1,5 +1,8 @@
 _find_package(${ARGS})
 if(TARGET X11::xcb)
+    if(TARGET X11:X11)
+        target_link_libraries(X11::X11 INTERFACE X11:xcb)
+    endif()
     if(TARGET X11:xdmcp)
         target_link_libraries(X11::xcb INTERFACE X11:xdmcp)
     endif()
