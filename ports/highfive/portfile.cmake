a4102b1031b63d889bf890744e7325d7ddb14203
Jonliu1993 <13720414433@163.com>
2021-12-07 18:50:37 +0800

    [highfive] Add eigen3 feature option

--- ports/highfive/portfile.cmake
+++ ports/highfive/portfile.cmake
@@ -4,7 +4,9 @@ vcpkg_from_github(
     REF v2.3
     SHA512 5bf8bc6d3a57be39a4fd15f28f8c839706e2c8d6e2270f45ea39c28a2ac1e3c7f31ed2f48390a45a868c714c85f03f960a0bc8fad945c80b41f495e6f4aca36a
     HEAD_REF master
-    PATCHES fix-dependency-hdf5.patch
+    PATCHES 
+        fix-dependency-hdf5.patch
+        fix-error-C1128.patch
 )
 
 vcpkg_check_features(
@@ -13,9 +15,8 @@ vcpkg_check_features(
         boost   HIGHFIVE_USE_BOOST
         tests   HIGHFIVE_UNIT_TESTS
         xtensor HIGHFIVE_USE_XTENSOR
+        eigen3  HIGHFIVE_USE_EIGEN
 )
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" HDF5_USE_STATIC_LIBRARIES)
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
@@ -23,7 +24,6 @@ vcpkg_cmake_configure(
         ${FEATURE_OPTIONS}
         -DHIGHFIVE_EXAMPLES=OFF
         -DHIGHFIVE_BUILD_DOCS=OFF
-        -DHDF5_USE_STATIC_LIBRARIES=${HDF5_USE_STATIC_LIBRARIES}
 )
 
 vcpkg_cmake_install()
