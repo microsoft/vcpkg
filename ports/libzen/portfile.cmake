474df63a05bd2ccb392285316af30263f7c3d4d4
PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>
2021-12-09 15:52:21 -0800

    Fix up the package name

--- ports/libzen/portfile.cmake
+++ ports/libzen/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share/zenlib)
+vcpkg_cmake_config_fixup(PACKAGE_NAME zenlib CONFIG_PATH share/zenlib)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
