e98f80650570b4ebd203ff0db9d04309fac3e33a
Kai Pastor <dg0yt@darc.de>
2021-12-12 12:26:58 +0100

    [pdal-c] Update portfile

--- ports/pdal-c/portfile.cmake
+++ ports/pdal-c/portfile.cmake
@@ -7,26 +7,28 @@ vcpkg_from_github(
 	SHA512 6a5f4cb3d36b419f3cd195028c3e6dc17abf3cdb7495aa3df638bc1f842ba98243c73e051e9cfcd3afe22787309cb871374b152ded92e6e06f404cd7b1ae50bf
 	HEAD_REF master
 	PATCHES
-		${CMAKE_CURRENT_LIST_DIR}/fix-docs-version.patch
-		${CMAKE_CURRENT_LIST_DIR}/preserve-install-dir.patch
-		${CMAKE_CURRENT_LIST_DIR}/remove-tests.patch
+		fix-docs-version.patch
+		preserve-install-dir.patch
+		remove-tests.patch
 )
 
-vcpkg_configure_cmake(
-	SOURCE_PATH ${SOURCE_PATH}
-	PREFER_NINJA
+if(VCPKG_TARGET_IS_WINDOWS)
+    string(APPEND VCPKG_C_FLAGS " -DNOMINMAX")
+    string(APPEND VCPKG_CXX_FLAGS " -DNOMINMAX")
+endif()
+
+vcpkg_cmake_configure(
+	SOURCE_PATH "${SOURCE_PATH}"
 	OPTIONS
 		-DPDALC_ENABLE_CODE_COVERAGE:BOOL=OFF
+		-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen:BOOL=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 # Remove headers from debug
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Install copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE.md
-	DESTINATION ${CURRENT_PACKAGES_DIR}/share/pdal-c
-	RENAME copyright
-)
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
