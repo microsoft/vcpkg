f2fca9aaec958a33bb74e030d519d8b31d3ef050
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-12-12 22:07:06 +0100

    update port

--- ports/qt-advanced-docking-system/portfile.cmake
+++ ports/qt-advanced-docking-system/portfile.cmake
@@ -1,26 +1,31 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO githubuser0xFFFF/Qt-Advanced-Docking-System
-    REF 487e23e190b8130de3b60e6e801f404edbedbf2f #v3.7.2
-    SHA512 da6d813267f242d4460d77808588f3bf11a8ea91183e2d8aa1040228cf5176556153d3c741c2f1c22f0a098de15ecc6c336f0b078fd0365c81cc27aa54868572 
+    REF 2afe62ec77c558c6c81435b479a99c0a188c5113 #v3.8.1
+    SHA512 2580f93901a72adc63151f25983a0414d732cf7e40a9255ca8ff2f6fbcecde886ab43a09a19dca16d19ec76ee8b4ecfd7af8f673012a04c0fc6c577721891715 
     HEAD_REF master
     PATCHES
         hardcode_version.patch
-        config_changes.patch
+        qt.patch
 )
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS 
         -DBUILD_EXAMPLES=OFF
-        -DVERSION_SHORT=3.7.2
+        -DVERSION_SHORT=3.8.1
+        -DQT_VERSION_MAJOR=6
+        -DCMAKE_DISABLE_FIND_PACKAGE_Qt5=TRUE
+        -DBUILD_STATIC=${BUILD_STATIC}
+    MAYBE_UNUSED_VARIABLES
+        CMAKE_DISABLE_FIND_PACKAGE_Qt5
 )
-
 vcpkg_cmake_install()
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/gnu-lgpl-v2.1.md DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/license)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-vcpkg_cmake_config_fixup(PACKAGE_NAME qtadvanceddocking CONFIG_PATH lib/cmake/qtadvanceddocking)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
+file(INSTALL "${SOURCE_PATH}/gnu-lgpl-v2.1.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/license")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "qtadvanceddocking" CONFIG_PATH "lib/cmake/qtadvanceddocking")
