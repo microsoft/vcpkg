80e8866bf11af82f608ab444509d33b8251111cc
Vitaly Petrov <v31337@gmail.com>
2021-12-15 12:01:10 +0300

    Review changes and update lib version

--- ports/optimus-cpp/portfile.cmake
+++ ports/optimus-cpp/portfile.cmake
@@ -1,17 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kafeg/optimus-cpp
-    REF 0.1.0
-    SHA512 387b1da32a653d2026d884749a69932ed47929afa92c66843a654735cdc7572f033bb64abd6553db24f03f9566e225f054234e1ec9ff177051aff3bb9ceea1ed
+    REF 0.3.0
+    SHA512 93abd13d4552a896f61e32dfebcc9037e7255f3fa86b230c03905df3148b9cc91cec772ec733e83fbcad574fd93fa4dadca9ec88b5836c5a4137d01e16580d6f
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
