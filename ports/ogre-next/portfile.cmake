67eee33cbc53ee6337add7041063e3cb08decbb6
FrankXie <v-frankxie@microsoft.com>
2021-12-16 18:28:36 -0800

    [ogre-next] update to v2.2.5

--- ports/ogre-next/portfile.cmake
+++ ports/ogre-next/portfile.cmake
@@ -11,13 +11,11 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OGRECave/ogre-next
-    REF 8083067c1835147de5d82015347d95c710e36bc0
-    SHA512 2660f739def7076b42009ba932a27532f2a20e7be8ec2674aff29116c7dcd7f2d75564d8ce07bef3dba13ece5293d604573ad4f8734f6086c1750127f52834cd
+    REF 0e0c47ed70091e7bdead5fb1ca01e1cae5857ef4 #v2.2.5
+    SHA512 25b9903b0c30e7c1baa01bfc126c9fc8397ab74d40c21695c88d886df5db503c3239490143b7ff64ec6c8c3da89b90b5d3846bbe49219ca8778c2a6f234dbffc 
     HEAD_REF master
     PATCHES
         toolchain_fixes.patch
-        rename_config_cmake.patch
-        fix_find_package_sdl2.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/CMake/Packages/FindOpenEXR.cmake")
@@ -28,19 +26,12 @@ else()
     set(OGRE_STATIC OFF)
 endif()
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    d3d9    OGRE_BUILD_RENDERSYSTEM_D3D9
-    java    OGRE_BUILD_COMPONENT_JAVA
-    python  OGRE_BUILD_COMPONENT_PYTHON
-    csharp  OGRE_BUILD_COMPONENT_CSHARP
-)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DOGRE_BUILD_DEPENDENCIES=OFF
-        -DOGRE_BUILD_SAMPLES=OFF
+        -DOGRE_BUILD_SAMPLES=ON
         -DOGRE_BUILD_TESTS=OFF
         -DOGRE_BUILD_TOOLS=OFF
         -DOGRE_BUILD_MSVC_MP=ON
@@ -50,72 +41,45 @@ vcpkg_configure_cmake(
         -DOGRE_INSTALL_PDB=OFF
         -DOGRE_INSTALL_SAMPLES=OFF
         -DOGRE_INSTALL_TOOLS=OFF
-        -DOGRE_INSTALL_CMAKE=ON
         -DOGRE_INSTALL_VSPROPS=OFF
         -DOGRE_STATIC=${OGRE_STATIC}
-        -DOGRE_CONFIG_THREAD_PROVIDER=std
+        -DOGRE_CONFIG_THREAD_PROVIDER=0
         -DOGRE_BUILD_RENDERSYSTEM_D3D11=ON
         -DOGRE_BUILD_RENDERSYSTEM_GL=ON
         -DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON
         -DOGRE_BUILD_RENDERSYSTEM_GLES=OFF
         -DOGRE_BUILD_RENDERSYSTEM_GLES2=OFF
-# Optional stuff
-        ${FEATURE_OPTIONS}
-# vcpkg specific stuff
+        -DOGRE_BUILD_PLUGIN_PFX=ON
+
         -DOGRE_CMAKE_DIR=share/ogre-next
-)
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+        -DCMAKE_CXX_FLAGS=\"/DWIN32 /D_WINDOWS /W3 /GR /EHsc\"
+        -DCMAKE_C_FLAGS=\"/DWIN32 /D_WINDOWS /W3\"
+        -DCMAKE_EXE_LINKER_FLAGS=/machine:x64
+		)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-file(GLOB REL_CFGS ${CURRENT_PACKAGES_DIR}/bin/*.cfg)
+file(GLOB REL_CFGS "${CURRENT_PACKAGES_DIR}/bin/*.cfg")
 if(REL_CFGS)
-  file(COPY ${REL_CFGS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-  file(REMOVE ${REL_CFGS})
+  file(COPY "${REL_CFGS}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+  file(REMOVE "${REL_CFGS}")
 endif()
 
-file(GLOB DBG_CFGS ${CURRENT_PACKAGES_DIR}/debug/bin/*.cfg)
+file(GLOB DBG_CFGS "${CURRENT_PACKAGES_DIR}/debug/bin/*.cfg")
 if(DBG_CFGS)
-  file(COPY ${DBG_CFGS} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-  file(REMOVE ${DBG_CFGS})
+  file(COPY "${DBG_CFGS}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+  file(REMOVE "${DBG_CFGS}")
 endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
-
-#Remove OgreMain*.lib from lib/ folder, because autolink would complain, since it defines a main symbol
-#manual-link subfolder is here to the rescue!
-if(VCPKG_TARGET_IS_WINDOWS)
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "Release")
-        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib/manual-link)
-        if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-            file(RENAME ${CURRENT_PACKAGES_DIR}/lib/OgreMain.lib ${CURRENT_PACKAGES_DIR}/lib/manual-link/OgreMain.lib)
-        else()
-            file(RENAME ${CURRENT_PACKAGES_DIR}/lib/OgreMainStatic.lib ${CURRENT_PACKAGES_DIR}/lib/manual-link/OgreMainStatic.lib)
-        endif()
-    endif()
-    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "Debug")
-        file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link)
-        if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-            file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/OgreMain_d.lib ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link/OgreMain_d.lib)
-        else()
-            file(RENAME ${CURRENT_PACKAGES_DIR}/debug/lib/OgreMainStatic_d.lib ${CURRENT_PACKAGES_DIR}/debug/lib/manual-link/OgreMainStatic_d.lib)
-        endif()
-    endif()
-
-    file(GLOB SHARE_FILES ${CURRENT_PACKAGES_DIR}/share/ogre-next/*.cmake)
-    foreach(SHARE_FILE ${SHARE_FILES})
-        file(READ "${SHARE_FILE}" _contents)
-        string(REPLACE "lib/OgreMain" "lib/manual-link/OgreMain" _contents "${_contents}")
-        file(WRITE "${SHARE_FILE}" "${_contents}")
-    endforeach()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_copy_pdbs()
