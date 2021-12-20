1ecb93e8ef5c1e5cc0efb20f7c72a2a7f946e1ed
Jonliu1993 <13720414433@163.com>
2021-12-20 15:50:12 +0800

    [LuaBridge] update to 3.x

--- ports/luabridge/portfile.cmake
+++ ports/luabridge/portfile.cmake
@@ -1,19 +1,20 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO vinniefalco/LuaBridge
-    REF 12d51bdd992a22b43adb9cc5fb83ebf8b1f2be7f # 2.6
-    SHA512 18593b2fda5be0b7cd9701feed53820002f93bba883cfa9fcdfa3e25ead02fb9b2f16f30d1603ae5d43ee3b199925071260723d4ebb79eb581888496d422f42d
+    REPO kunitoki/LuaBridge3
+    REF 5c064d66d621fe7f23b3bcdfd3b7be452c984e8a #v3.0-beta1
+    SHA512 b9abd953756ac115ceef5fb01cc710ae011aaa61a71213d5af9e5377a4a625c6a46d866d27f100dcbfd6591216955686503d199958237a396eb53b9ec7185a3f
     HEAD_REF master
+    PATCHES
+        fix-find_package.patch
 )
 
-file(
-    COPY ${SOURCE_PATH}/Source/LuaBridge
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DLUABRIDGE_TESTING=ON
 )
+
+file(INSTALL "${SOURCE_PATH}/Source/LuaBridge" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 # Handle copyright
-configure_file(
-    ${SOURCE_PATH}/README.md
-    ${CURRENT_PACKAGES_DIR}/share/luabridge/copyright
-    COPYONLY
-)
+configure_file("${SOURCE_PATH}/README.md" "${CURRENT_PACKAGES_DIR}/share/luabridge/copyright" COPYONLY)
