18929e468b467204affb60b08b27c5334d955c3e
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2021-12-20 17:00:23 +0800

    [mvfst] new port

--- ports/mvfst/portfile.cmake
+++ ports/mvfst/portfile.cmake
@@ -11,7 +11,6 @@ vcpkg_from_github(
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DBUILD_TESTS=OFF
         -DBUILD_EXAMPLES=OFF
@@ -19,9 +18,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mvfst)
-file(REMOVE_RECURSE
-${CURRENT_PACKAGES_DIR}/debug/include
-)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
