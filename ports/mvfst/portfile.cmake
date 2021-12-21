f7101c6048731c5a27b4faa758385ce9f78b9669
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2021-12-21 14:25:45 +0800

    [mvfst]: new port

--- ports/mvfst/portfile.cmake
+++ ports/mvfst/portfile.cmake
@@ -3,14 +3,14 @@ vcpkg_fail_port_install(ON_TARGET "Windows")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebookincubator/mvfst
-    REF 497f6e4732bf6e95553d70e24f2e6e1a20c26397 
-    SHA512 41bb998f8183839f532c32755572df4a8d38a84ad164011675703a4de73347292b77134fe53359a5622b72c5b1f3fea4b295b8740eda754c860b41f75ba7f751
+    REF 531b935a  
+    SHA512  47683907e5c19aa7392bffc0f23378636e73e03bbbd598640d20042234a9385ac7ca2fb8202905e1c101713bfb3467df78b12fd1629ca83a3ccf2bae2f58a2e1
     HEAD_REF main
 )
 
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTS=OFF
         -DBUILD_EXAMPLES=OFF
@@ -18,7 +18,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mvfst)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
