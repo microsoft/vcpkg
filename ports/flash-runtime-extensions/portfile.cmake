0d77ed3d6764ddc174019fcbe53c975db145e70b
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2021-12-23 09:37:46 +0100

    Initial commit with a working port on Windows.

---
+++ ports/flash-runtime-extensions/portfile.cmake
@@ -0,0 +1,21 @@
+#set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO al-sabr/FlashRuntimeExtensions
+    REF v2.3
+    SHA512 461e6bb7ec49cb7d687d6504f9b6261eca407c2a79ba29b59abfaf48625e4e077640b43b933fdefe3bdaa76a283cbf2c4495220534c41ca982b7c370ea73f9e5
+    HEAD_REF main
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
