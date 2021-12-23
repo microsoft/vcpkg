a86b65e692ecfd65615e17434e67bc833a234bc5
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2021-12-23 12:12:06 +0800

    [s2geometry] new port

--- ports/s2geometry/portfile.cmake
+++ ports/s2geometry/portfile.cmake
@@ -7,7 +7,8 @@ vcpkg_from_github(
     SHA512 854ec84a54aff036b3092a6233be0f5fc0e4846ac5f882326bbb3f2b9ce88bd5c866a80ae352d8e7d5ae00b9c9a8ab1cff6a95412f990b7bc1fdc5ca3d632b9c
     HEAD_REF main
     PATCHES
-       config.patch
+        CMakeLists.txt.patch
+        Config.cmake.in.patch
 )
 
 
@@ -20,9 +21,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/s2geometry/" RENAME copyright)
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/s2geometry/")
