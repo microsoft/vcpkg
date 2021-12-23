9f743ac4d6b3cb2c240aeefb8b0ab0e3587042e9
Lily Wang <v-lilywang@microsoft.com>
2021-12-22 23:23:05 -0800

    [entt] Add feature natvis

--- ports/entt/portfile.cmake
+++ ports/entt/portfile.cmake
@@ -11,14 +11,21 @@ else()
         REF v3.9.0
         SHA512 b318ea06deb69350a00b3e824462a22fe443f4c778d0934857b68e43f0e6f1fe30c281889c14e3456067629e62a2bbb54491458c43d52ef543b2db8903133922
         HEAD_REF master
+		PATCHES fix-featureNatvis.patch
     )
 endif()
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        natvis ENTT_INCLUDE_NATVIS
+)
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
-        -DENTT_BUILD_TESTING=OFF
+        ${FEATURE_OPTIONS}
+		-DENTT_BUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
@@ -29,7 +36,7 @@ else()
     vcpkg_cmake_config_fixup(CONFIG_PATH lib/EnTT/cmake)
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
