965f87142eb7968477d35f959a89896aa5e03c94
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2021-12-29 16:42:54 +0800

    [glog] update to v0.5.0

--- ports/glog/portfile.cmake
+++ ports/glog/portfile.cmake
@@ -11,15 +11,14 @@ vcpkg_from_github(
        fix_glog_config.h.patch
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
 )
 
-vcpkg_install_cmake()
-
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/glog)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glog)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
