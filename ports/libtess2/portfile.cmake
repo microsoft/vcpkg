d1d390ac73a174328fb06ef56b3a97a7d4fb3aae
Billy Robert O'Neal III <bion@microsoft.com>
2021-12-28 18:50:57 -0800

    Modernize with target_sources, add missing quotes, and add vcpkg_check_linkage since the port only makes a static output.

--- ports/libtess2/portfile.cmake
+++ ports/libtess2/portfile.cmake
@@ -1,3 +1,5 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO memononen/libtess2
@@ -6,18 +8,18 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(
-    PACKAGE_NAME unofficial-${PORT}
-    CONFIG_PATH lib/cmake/unofficial-${PORT}
+    PACKAGE_NAME "unofficial-${PORT}"
+    CONFIG_PATH "lib/cmake/unofficial-${PORT}"
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
