08189e852ef5e1e8de9d731989c708866530e6bf
xuan.guo@atomdatatech.com <xuan.guo@atomdatatech.com>
2021-12-29 13:19:59 +0800

    fix deprecated functions

--- ports/minio-cpp/portfile.cmake
+++ ports/minio-cpp/portfile.cmake
@@ -6,13 +6,13 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_EXAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
