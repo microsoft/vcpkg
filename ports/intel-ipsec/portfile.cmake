74259e90fc2838a08d83cef0d7352814e06e6e5b
Billy Robert O'Neal III <bion@microsoft.com>
2021-12-30 10:33:41 -0800

    [intel-ipsec] Overhaul/modernize port.
    * Remove CMakeLists.txt replacement that was a fancy way to say vcpkg_from_make/vcpkg_from_nmake.
    * Convert support messages into a "supports" expression.
    * Fix Windows.
    * Correct CMake config handling of release/debug static/shared and Windows.

--- ports/intel-ipsec/portfile.cmake
+++ ports/intel-ipsec/portfile.cmake
@@ -1,57 +1,107 @@
-if (VCPKG_TARGET_IS_WINDOWS)
-    set(EXEC_ENV "Windows")
-else ()
-    set(EXEC_ENV "${VCPKG_CMAKE_SYSTEM_NAME}")
-endif ()
-
-if (NOT EXEC_ENV STREQUAL "Linux")
-    message(FATAL_ERROR "Intel(R) Multi-Buffer Crypto for IPsec Library currently only supports Linux/Windows platforms")
-    message(STATUS "Well, it is not true, but I didnt manage to get it working on Windows")
-endif ()
-
-if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    message(FATAL_ERROR "Intel(R) Multi-Buffer Crypto for IPsec Library currently only supports x64 architecture")
-elseif (NOT VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-endif ()
-
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO intel/intel-ipsec-mb
-        REF bde82c8737edc04d80549f0a68225ede7e5cefd #v1.1
-        SHA512 f41dcde88b062e8ec2327987c6d36cd4f74a5e4fea386cc1ef8364f1dc432a2db02ca7d3312c0471b443cf93e815af6d74a4819c249afd6777aa91693b2546e5
-        HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO intel/intel-ipsec-mb
+    REF bde82c8737edc04d80549f0a68225ede7e5cefd #v1.1
+    SHA512 f41dcde88b062e8ec2327987c6d36cd4f74a5e4fea386cc1ef8364f1dc432a2db02ca7d3312c0471b443cf93e815af6d74a4819c249afd6777aa91693b2546e5
+    HEAD_REF master
+    PATCHES
+        always-generate-pdb.patch # https://github.com/intel/intel-ipsec-mb/pull/93
 )
 
 vcpkg_find_acquire_program(NASM)
+get_filename_component(NASM_DIR "${NASM}" DIRECTORY)
+vcpkg_add_to_path(PREPEND "${NASM_DIR}")
 
-exec_program(${NASM}
-             ARGS -v
-             OUTPUT_VARIABLE NASM_OUTPUT
-             )
-string(REGEX REPLACE "NASM version ([0-9]+\\.[0-9]+\\.[0-9]+).*" "\\1"
-       NASM_VERSION
-       ${NASM_OUTPUT})
-if (NASM_VERSION VERSION_LESS 2.13.03)
-    message(FATAL_ERROR "NASM version 2.13.03 (or newer) is required to build this package")
-endif ()
+vcpkg_list(SET MAKE_OPTIONS)
+set(INTEL_IPSEC_STATIC_OR_SHARED SHARED)
+if ("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+    vcpkg_list(APPEND MAKE_OPTIONS SHARED=n)
+    set(INTEL_IPSEC_STATIC_OR_SHARED STATIC)
+endif()
 
-get_filename_component(NASM_PATH ${NASM} DIRECTORY)
-set(ENV{PATH} " $ENV{PATH};${NASM_PATH} ")
+set(DEBUG_LIB "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib")
+set(RELEASE_LIB "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib")
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}"
-    OPTIONS
-        -DSOURCE_PATH=${SOURCE_PATH}
-        -DEXEC_ENV=${VCPKG_CMAKE_SYSTEM_NAME}
-        -DLIBRARY_LINKAGE=${VCPKG_LIBRARY_LINKAGE}
-)
+if (CMAKE_HOST_WIN32)
+    vcpkg_build_nmake(
+        SOURCE_PATH "${SOURCE_PATH}"
+        PROJECT_SUBPATH lib
+        PROJECT_NAME win_x64.mak
+        OPTIONS ${MAKE_OPTIONS}
+        OPTIONS_DEBUG DEBUG=y
+    )
 
-vcpkg_cmake_install()
+    if(NOT "${VCPKG_BUILD_TYPE}" STREQUAL "release")
+        file(INSTALL "${DEBUG_LIB}/libIPSec_MB.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+        set(INTEL_IPSEC_CONFIGURATION "DEBUG")
+        if ("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+            file(INSTALL "${DEBUG_LIB}/libIPSec_MB.pdb" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+            set(INTEL_IPSEC_LOCATION "debug/lib/libIPSec_MB.lib")
+            configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-debug.cmake" @ONLY)
+        else()
+            file(INSTALL "${DEBUG_LIB}/libIPSec_MB.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+            file(INSTALL "${DEBUG_LIB}/libIPSec_MB.pdb" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+            file(INSTALL "${DEBUG_LIB}/libIPSec_MB.exp" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+            set(INTEL_IPSEC_LOCATION "debug/bin/libIPSec_MB.dll")
+            set(INTEL_IPSEC_IMPLIB "debug/lib/libIPSec_MB.lib")
+            configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets-implib.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-debug.cmake" @ONLY)
+        endif()
+    endif()
 
-file(INSTALL "${SOURCE_PATH}/Release/lib/" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-file(INSTALL "${SOURCE_PATH}/Debug/lib/" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
-file(INSTALL "${SOURCE_PATH}/Release/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/intel-ipsecConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+    file(INSTALL "${RELEASE_LIB}/libIPSec_MB.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    set(INTEL_IPSEC_CONFIGURATION "RELEASE")
+    if ("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+        file(INSTALL "${RELEASE_LIB}/libIPSec_MB.pdb" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+        set(INTEL_IPSEC_LOCATION "lib/libIPSec_MB.lib")
+        configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-release.cmake" @ONLY)
+    else()
+        file(INSTALL "${RELEASE_LIB}/libIPSec_MB.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+        file(INSTALL "${RELEASE_LIB}/libIPSec_MB.pdb" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+        file(INSTALL "${RELEASE_LIB}/libIPSec_MB.exp" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+        set(INTEL_IPSEC_LOCATION "bin/libIPSec_MB.dll")
+        set(INTEL_IPSEC_IMPLIB "lib/libIPSec_MB.lib")
+        configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets-implib.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-release.cmake" @ONLY)
+    endif()
+else()
+    if ("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+        set(LIB_SUFFIX ".a")
+    else()
+        set(LIB_SUFFIX ".so")
+    endif()
+
+    find_program(MAKE make REQUIRED)
+    if(NOT "${VCPKG_BUILD_TYPE}" STREQUAL "release")
+        message(STATUS "Building ${TARGET_TRIPLET}-dbg")
+        set(INTEL_IPSEC_CONFIGURATION "DEBUG")
+        file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
+        file(COPY "${SOURCE_PATH}/" DESTINATION "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
+        vcpkg_execute_build_process(
+            COMMAND "${MAKE}" -j ${MAKE_OPTIONS}
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lib"
+            LOGNAME "build-${TARGET_TRIPLET}-dbg"
+        )
+
+        file(INSTALL "${DEBUG_LIB}/libIPSec_MB${LIB_SUFFIX}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+        set(INTEL_IPSEC_LOCATION "debug/lib/libIPSec_MB${LIB_SUFFIX}")
+        configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-debug.cmake" @ONLY)
+    endif()
+
+    message(STATUS "Building ${TARGET_TRIPLET}-rel")
+    set(INTEL_IPSEC_CONFIGURATION "RELEASE")
+    file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
+    file(COPY "${SOURCE_PATH}/" DESTINATION "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
+    vcpkg_execute_build_process(
+        COMMAND "${MAKE}" -j ${MAKE_OPTIONS}
+        WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lib"
+        LOGNAME "build-${TARGET_TRIPLET}-rel"
+    )
+
+    file(INSTALL "${RELEASE_LIB}/libIPSec_MB${LIB_SUFFIX}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+    set(INTEL_IPSEC_LOCATION "lib/libIPSec_MB${LIB_SUFFIX}")
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsec-targets.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsec-targets-release.cmake" @ONLY)
+endif()
+
+file(INSTALL "${SOURCE_PATH}/lib/intel-ipsec-mb.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsecConfig.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsecConfig.cmake" @ONLY)
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
