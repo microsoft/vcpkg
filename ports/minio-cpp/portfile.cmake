540dd4e43689213a65ff33f61a2df28cf49d7759
xuan.guo@atomdatatech.com <xuan.guo@atomdatatech.com>
2021-12-31 11:01:58 +0800

    add patch file

--- ports/minio-cpp/portfile.cmake
+++ ports/minio-cpp/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 0847e56f31e13d32c924029d5ffaa865f8396462
     SHA512 47817c8acc92060afca5ebd7c9bc7c9a9e09a50046180d7d3d67993f207d295c90e491417702ebb1ab2b0a6e85a5fcc334054ce4f6d542cb42c3675b5aed1c3f
     HEAD_REF main
+    PATCHES
+        fix_cmake_file.patch
 )
 
 vcpkg_cmake_configure(
