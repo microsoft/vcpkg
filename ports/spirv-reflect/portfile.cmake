35f3bbdf1545216e60edbe3922b04e1407b83430
Jan Kleszczyński <jankleszcz@gmail.com>
2022-01-02 18:10:57 +0100

    fixing version issue wrong sha

--- ports/spirv-reflect/portfile.cmake
+++ ports/spirv-reflect/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/SPIRV-Reflect
     REF d920b79aadafe3ffd52764ac80370b15a611c83a
-    SHA512 3e94adb9ec80f356bd51665f10e3e1d8e6236632d259a22fab97a156c6cf6fcbd1afc102ac4578fa3f3725b6cc0cbdf530c85fa133154d6c4e313324c1d6bbf4
+    SHA512 463e05ee9851cb5829f0e125cdd569583e9538cc
     HEAD_REF master
     PATCHES
         cmake-install.patch
