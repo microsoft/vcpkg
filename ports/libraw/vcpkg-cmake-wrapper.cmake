6612c203cba06dec79841f4cf9be02c8c6dbf577
JackBoosY <yuzaiyang@beyondsoft.com>
2022-01-04 08:19:43 -0800

    [libraw] Add feature openmp and fix usage

--- ports/libraw/vcpkg-cmake-wrapper.cmake
+++ ports/libraw/vcpkg-cmake-wrapper.cmake
@@ -3,6 +3,13 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 _find_package(${ARGS})
 set(CMAKE_MODULE_PATH ${LIBRAW_PREV_MODULE_PATH})
 
+if (@ENABLE_OPENMP@)
+    find_package(OpenMP REQUIRED)
+    if (OpenMP_FOUND)
+        list(APPEND LibRaw_LIBRARIES gomp)
+    endif()
+endif()
+
 if (@VCPKG_LIBRARY_LINKAGE@ STREQUAL "static")
     find_package(Jasper REQUIRED)
     if (Jasper_FOUND)
