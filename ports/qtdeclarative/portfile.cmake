de4983bbfb4e6d623a26a5456af71628903ffd61
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2022-01-04 13:11:36 +0100

    [qtdeclarative] no absolute paths

--- ports/qtdeclarative/portfile.cmake
+++ ports/qtdeclarative/portfile.cmake
@@ -24,3 +24,5 @@ qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS_RELEASE
                      CONFIGURE_OPTIONS_DEBUG
                     )
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/Qt6/QtQml/6.2.2/QtQml/private/qqmljsparser_p.h" "${SOURCE_PATH}" "")
