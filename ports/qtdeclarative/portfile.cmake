4918d77276bbfc34ecc9496c4d6e8aaab5254f8b
nicole mazzuca <mazzucan@outlook.com>
2022-01-05 20:17:31 -0800

    add comment

--- ports/qtdeclarative/portfile.cmake
+++ ports/qtdeclarative/portfile.cmake
@@ -25,4 +25,5 @@ qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS_DEBUG
                     )
 
+# remove `${SOURCE_PATH}` from the front of `#line` directives
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/Qt6/QtQml/6.2.2/QtQml/private/qqmljsparser_p.h" "${SOURCE_PATH}" "")
