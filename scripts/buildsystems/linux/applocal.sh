f61b534a20648b8b3f60f8df1bea97d247008b05
Martijn Otto <martijn@resolume.com>
2022-01-07 16:42:21 +0100

    Recursively process dependencies

--- scripts/buildsystems/linux/applocal.sh
+++ scripts/buildsystems/linux/applocal.sh
@@ -19,26 +19,39 @@ case "${EXECUTABLE}" in
         ;;
 esac
 
-# iterate over all dependencies of the binary
-for DEPENDENCY in $(readelf -d "${DESTDIR}/${EXECUTABLE}" | grep "Shared library" | awk -F'[\[|\]]' '{print $2}'); do
-    DEPENDENCY_PATH="${VCPKG_INSTALLED}/lib/${DEPENDENCY}"
+# copy dependencies from the given executable or shared library
+# to the matching library path, recursive
+function copy_dependencies() {
+    DESTDIR=$1
+    PREFIX=$2
+    BINARY=$3
+    VCPKG_INSTALLED=$4
 
-    # check if vcpkg has the requested dependency
-    if [ ! -e "${DEPENDENCY_PATH}" ]; then
-        continue
-    fi
+    # iterate over all dependencies of the binary
+    for DEPENDENCY in $(readelf -d "${BINARY}" | grep "Shared library" | awk -F'[\[|\]]' '{print $2}'); do
+        DEPENDENCY_PATH="${VCPKG_INSTALLED}/lib/${DEPENDENCY}"
 
-    # check if the dependency is already installed
-    if [ -e "${DESTDIR}/${PREFIX}/lib/${DEPENDENCY}" ]; then
-        continue
-    fi
+        # check if vcpkg has the requested dependency
+        if [ ! -e "${DEPENDENCY_PATH}" ]; then
+            continue
+        fi
 
-    mkdir -p ${DESTDIR}/${PREFIX}/lib
+        # check if the dependency is already installed
+        if [ ! -e "${DESTDIR}/${PREFIX}/lib/${DEPENDENCY}" ]; then
+            mkdir -p ${DESTDIR}/${PREFIX}/lib
 
-    if [ -L "${DEPENDENCY_PATH}" ]; then
-        # recreate the link in the install dir
-        cp $(realpath ${DEPENDENCY_PATH}) ${DESTDIR}/${PREFIX}/lib/
-    fi
+            if [ -L "${DEPENDENCY_PATH}" ]; then
+                # recreate the link in the install dir
+                cp $(realpath ${DEPENDENCY_PATH}) ${DESTDIR}/${PREFIX}/lib/
+            fi
 
-    cp --no-dereference ${DEPENDENCY_PATH} ${DESTDIR}/${PREFIX}/lib/
-done
+            cp --no-dereference ${DEPENDENCY_PATH} ${DESTDIR}/${PREFIX}/lib/
+        fi
+
+        # copy dependencies for the library itself
+        copy_dependencies "${DESTDIR}" "${PREFIX}" "${DESTDIR}/${PREFIX}/lib/${DEPENDENCY}" "${VCPKG_INSTALLED}"
+    done
+}
+
+# copy dependencies for the executable
+copy_dependencies "${DESTDIR}" "${PREFIX}" "${DESTDIR}/${EXECUTABLE}" "${VCPKG_INSTALLED}"
