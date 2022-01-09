9e797ab52d6df59f040b38c056540c3164b20163
Jan Kleszczyński <jankleszcz@gmail.com>
2022-01-09 12:10:25 +0100

    CMakeLists substitution instead of patch

--- ports/spirv-reflect/portfile.cmake
+++ ports/spirv-reflect/portfile.cmake
@@ -6,9 +6,9 @@ vcpkg_from_github(
     REF d920b79aadafe3ffd52764ac80370b15a611c83a
     SHA512 3e94adb9ec80f356bd51665f10e3e1d8e6236632d259a22fab97a156c6cf6fcbd1afc102ac4578fa3f3725b6cc0cbdf530c85fa133154d6c4e313324c1d6bbf4
     HEAD_REF master
-    PATCHES
-        cmake-install.patch
 )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
