a158cf636b084dbd681ef7f86c5272c9506847cd
injae <8687lee@gmail.com>
2022-01-10 16:10:09 +0900

    portfile.cmake update and remove features

--- ports/serdepp/portfile.cmake
+++ ports/serdepp/portfile.cmake
@@ -8,12 +8,11 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA 
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/serdepp TARGET_PATH share/serdepp)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/serdepp)
 
 file(REMOVE_RECURSE
     ${CURRENT_PACKAGES_DIR}/debug/cmake
