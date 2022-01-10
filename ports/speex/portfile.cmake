7cfb590e2a12e9b2057ab3b2fcf28a60c7a35f03
Daniel Schürmann <daschuer@mixxx.org>
2022-01-10 15:39:40 +0100

    [speex] don't use deprecated vcpkg functions

--- ports/speex/portfile.cmake
+++ ports/speex/portfile.cmake
@@ -14,12 +14,11 @@ vcpkg_from_github(
 if(VCPKG_TARGET_IS_WINDOWS)
   file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
   
-  vcpkg_configure_cmake(
+  vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON
   )
-  vcpkg_install_cmake()
+  vcpkg_cmake_install()
 
   if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/speex/speex.h"
