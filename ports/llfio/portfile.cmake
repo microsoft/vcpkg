b50250dd5bcd920105db8f11295c26ebd7a5eab6
Henrik S. Gaßmann <henrik@gassmann.onl>
2022-01-10 21:20:03 +0100

    [llfio] Update to version @2.0-20220112

--- ports/llfio/portfile.cmake
+++ ports/llfio/portfile.cmake
@@ -6,9 +6,13 @@ LLFIO depends on Outcome which depends on QuickCppLib which uses the vcpkg versi
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ned14/llfio
-    REF fba45d6bacbdc7710e257a0b3d229fe3ef3a1f5d
-    SHA512 d80e132a5b46859bf64d3808bfdc35331e6efd0668c297b7daf8c1453df00c5b8749103ad558456cfd71e9b5bd5b3bf7a1934871e64695bd97f90f2e04cd5621
+    REF 4a117d683b82a2e3e456c2ecc47a99c8406280fa
+    SHA512 7880356dbff10664a146a09558ba15f95cf6883ebe8e0af3d392fbd6f86f3455b9b5c8b6c5c1281c8fca93c358fcafd3468ab575eee0b483ec5b136ca59eef04
     HEAD_REF develop
+    PATCHES
+        # https://github.com/ned14/llfio/issues/83
+        # To be removed on next update
+        issue-83-fix-backport.patch
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)
@@ -27,23 +31,11 @@ vcpkg_check_features(
       status-code LLFIO_USE_EXPERIMENTAL_SG14_STATUS_CODE
 )
 
-# LLFIO needs a copy of QuickCppLib with which to bootstrap its cmake
-file(COPY "${CURRENT_INSTALLED_DIR}/include/quickcpplib"
-    DESTINATION "${SOURCE_PATH}/quickcpplib/repo/include/"
-)
-file(COPY "${CURRENT_INSTALLED_DIR}/share/ned14-internal-quickcpplib/"
-    DESTINATION "${SOURCE_PATH}/quickcpplib/repo/"
-)
-
 # LLFIO expects ntkernel-error-category to live inside its include directory
 file(REMOVE_RECURSE "${SOURCE_PATH}/include/llfio/ntkernel-error-category")
 if(VCPKG_TARGET_IS_WINDOWS)
   file(RENAME "${NTKEC_SOURCE_PATH}" "${SOURCE_PATH}/include/llfio/ntkernel-error-category")
 endif()
-
-# Already installed dependencies don't appear on the include path, which LLFIO assumes.
-string(APPEND VCPKG_CXX_FLAGS " \"-I${CURRENT_INSTALLED_DIR}/include\"")
-string(APPEND VCPKG_C_FLAGS " \"-I${CURRENT_INSTALLED_DIR}/include\"")
 
 set(extra_config)
 # cmake does not correctly set CMAKE_SYSTEM_PROCESSOR when targeting ARM on Windows
@@ -56,8 +48,7 @@ vcpkg_configure_cmake(
     PREFER_NINJA
     OPTIONS
         -DPROJECT_IS_DEPENDENCY=On
-        -Dquickcpplib_FOUND=1
-        -Doutcome_FOUND=1
+        -Dquickcpplib_DIR=${CURRENT_INSTALLED_DIR}/share/quickcpplib
         ${LLFIO_FEATURE_OPTIONS}
         -DLLFIO_ENABLE_DEPENDENCY_SMOKE_TEST=ON  # Leave this always on to test everything compiles
         -DCMAKE_DISABLE_FIND_PACKAGE_Git=ON
