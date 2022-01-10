76d4836f3b1e027758044fdbdde91256b0f0955d
Yury Bura <yurybura@gmail.com>
2022-01-10 22:47:12 +0300

    [boost] update to 1.78.0 (#21928)
    * [scripts] update scripts
    
    * [boost-*] generate ports
    
    * update versions
    
    * [scripts] update boost version in boost-modular-headers.cmake
    
    * [scripts] add post source stubs for boost-json and boost-nowide ports and re-generate Boost ports
    
    * update versions
    
    * [boost-asio] make OpenSSL optional #20718
    
    * update versions
    
    * [boost] [boost-mpi] [boost-python] disable python for iOS and Android, fixes #21474
    
    * update versions
    
    * [boost-fiber] add NUMA support, fixes #16507
    
    * update version
    
    * [boost-build] apply patch https://github.com/bfgroup/b2/pull/113 for fix build boost-fiber and boost-stacktrace
    
    * update version
    
    * [boost-atomic] fix compilation for uwp
    
    * update version
    
    * ignore mapnik's fail on arm64-windows
    
    * [boost-python] support ARM on Windows
    
    * update versions
    
    * Boost features testing within CI
    
    * [boost-modular-build-helper] fix boost_python file name in Jamroot.jam
    
    * [boost-mpi] fix build with python2 or python3
    
    * update versions
    
    * [boost-fiber] fix build with numa=on
    
    * update version
    
    * [boost-multiprecision] fix build with MSVC on ARM
    
    * update version
    
    * [boost-mpi] try to fix build on linux and osx
    
    * update versions
    
    * exclude static
    
    * guarantee the same result across platforms
    
    * minor changes to boost-modular-build-helper's CMake
    
    * correct versions
    
    * [boost-modular-build-helper] install Python extensions on Windows
    
    * [boost-mpi] add patch to fix build Python extension on Windows
    
    * update versions
    
    * [boost-mpi] remove python2 by request @BillyONeal @strega-nil-ms
    
    * update version
    
    * update versions
    
    Co-authored-by: nicole mazzuca <mazzucan@outlook.com>
    Co-authored-by: PhoebeHui <20694052+PhoebeHui@users.noreply.github.com>

--- ports/boost-fiber/portfile.cmake
+++ ports/boost-fiber/portfile.cmake
@@ -3,20 +3,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO boostorg/fiber
-    REF boost-1.77.0
-    SHA512 f5ee6acc30902b2e68861a194f478a6a0a9ea8de096465fcb48c27756718fe6493c8c4d8e08b0473edd45162106061de735a95ff8679d54862cc3a62968aaa94
+    REF boost-1.78.0
+    SHA512 271454964e12c0006ed9ef52911cf76d004141da54d5f96e63d106a216841d9a939d3dcd802fc9e860ac6a21d0b7fab9f385e8647ebbd2fd669e48fc9b6a80c9
     HEAD_REF master
+    PATCHES fix-build-numa.patch
 )
 
 file(READ "${SOURCE_PATH}/build/Jamfile.v2" _contents)
-string(REPLACE "import ../../config/checks/config" "import config/checks/config" _contents "${_contents}")
+string(REPLACE "import ../../config/checks/config" "import ../config/checks/config" _contents "${_contents}")
 file(WRITE "${SOURCE_PATH}/build/Jamfile.v2" "${_contents}")
-file(COPY "${CURRENT_INSTALLED_DIR}/share/boost-config/checks" DESTINATION "${SOURCE_PATH}/build/config")
-
+file(COPY "${CURRENT_INSTALLED_DIR}/share/boost-config/checks" DESTINATION "${SOURCE_PATH}/config")
 if(NOT DEFINED CURRENT_HOST_INSTALLED_DIR)
     message(FATAL_ERROR "boost-fiber requires a newer version of vcpkg in order to build.")
 endif()
 include(${CURRENT_HOST_INSTALLED_DIR}/share/boost-build/boost-modular-build.cmake)
-boost_modular_build(SOURCE_PATH ${SOURCE_PATH})
+boost_modular_build(
+    SOURCE_PATH ${SOURCE_PATH}
+    BOOST_CMAKE_FRAGMENT "${CMAKE_CURRENT_LIST_DIR}/b2-options.cmake"
+)
 include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)
 boost_modular_headers(SOURCE_PATH ${SOURCE_PATH})
--- ports/boost-range/portfile.cmake
+++ ports/boost-range/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO boostorg/range
-    REF boost-1.77.0
-    SHA512 502dfd6f4c08fd653fee6a9fac14c717ddcd53d3e9ff68c43b80ccac73b88bb98bfb92e88508b82eb8eafa96e29914fe43b0ab550f62e6708d5ea0465ae9d51e
+    REF boost-1.78.0
+    SHA512 d4614a4a6900cbda998574a43d48a8a0604b4fb2e4eb31a2a0ea33cafa9d018c6901af204b665f66abf3fbc0adb1bdab1ffb7d4ee8a125551ea5f66f6fb6bb24
     HEAD_REF master
 )
 
