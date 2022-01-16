bb99933d0151ceeeb41112f31925f5c0924b257d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-01-15 19:38:05 -0500

    [ports/libfswatch/portfile.cmake] Upgrade repo version with various cmake install and vcpkg related improvements

--- ports/libfswatch/portfile.cmake
+++ ports/libfswatch/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SamuelMarks/fswatch
-    REF 025ec52c92d10da6dd8e3ff6972165384ea4f04f
-    SHA512 60b34236b1dcc09250ff6f486614aaf659d35dd787c251d9d84d03c3fb2d317e15bbc4d38325c213d78dfe5241d8c69b4238681f9e3eca2d15be12d9933b2470
+    REF f78857599f7f65dff3ad3b3f1e28deaee8dab901
+    SHA512 654b8200a54cc18b948df6ad2d1dd88dd4b7913e91bd156f3c9fb3e91b9ffc3805508b3c5eeba22a6085d3a81274db65dd97fb0816ddbf91a1a823a9f03a74c8
     HEAD_REF multi-os-ci
 )
 
@@ -13,3 +13,8 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/COPYING"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/libfswatch"
      RENAME copyright)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
