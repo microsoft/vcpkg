ad7fdaa987f1ddac525b9eb5b3e135d90046b06d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-01-17 14:40:02 -0500

    [ports/libfswatch/portfile.cmake] Upgrade to multi-CMakeLists.txt version; disable fswatch (binary) build

--- ports/libfswatch/portfile.cmake
+++ ports/libfswatch/portfile.cmake
@@ -1,13 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SamuelMarks/fswatch
-    REF f78857599f7f65dff3ad3b3f1e28deaee8dab901
-    SHA512 654b8200a54cc18b948df6ad2d1dd88dd4b7913e91bd156f3c9fb3e91b9ffc3805508b3c5eeba22a6085d3a81274db65dd97fb0816ddbf91a1a823a9f03a74c8
+    REF f2c98ccc7173afcc8101d188c24b4ac6ea87e125
+    SHA512 65648923e038c521774e1a1bd42b119262bfeff022d780ebacd3ba6be7c30f1ad322f82683733a8c280bc7c5eec49b0adf95a3864f8c5a84e3741a9bb4220cd9
     HEAD_REF multi-os-ci
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-DBUILD_FSWATCH=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/COPYING"
