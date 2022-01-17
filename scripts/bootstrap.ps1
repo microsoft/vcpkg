043a595c2c4673b79080d46c96ab3d60b648a516
Jeremy Sinclair <4016293+snickler@users.noreply.github.com>
2022-01-17 16:28:47 -0500

    Add reference to tls12-download-arm64 back in

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -47,7 +47,7 @@ Write-Verbose "Examining $vcpkgRootDir for .vcpkg-root - Found"
 
 $versionDate = '2021-12-09'
 if ($env:PROCESSOR_ARCHITECTURE -eq 'ARM64' -or $env:PROCESSOR_IDENTIFIER -match "ARMv[8,9] \(64-bit\)") {
-    & "$scriptsDir/tls12-download.exe" github.com "/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg-arm64.exe" "$vcpkgRootDir\vcpkg.exe"
+    & "$scriptsDir/tls12-download-arm64.exe" github.com "/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg-arm64.exe" "$vcpkgRootDir\vcpkg.exe"
 } else {
     & "$scriptsDir/tls12-download.exe" github.com "/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg.exe" "$vcpkgRootDir\vcpkg.exe"
 }
