f1594fa625f75fe16ec2b25b69c4596c94380ed4
nicole mazzuca <mazzucan@outlook.com>
2022-01-21 11:55:01 -0800

    fix license file

--- ports/otl/portfile.cmake
+++ ports/otl/portfile.cmake
@@ -16,6 +16,16 @@ file(INSTALL "${SOURCE_PATH}/otlv${OTL_VERSION}.h"
     DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}" 
     RENAME otlv4.h)
 
-file(INSTALL "${SOURCE_PATH}/otlv${OTL_VERSION}.h" 
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" 
-    RENAME copyright)
+file(READ "${SOURCE_PATH}/otlv${OTL_VERSION}.h" copyright_contents)
+string(FIND "${copyright_contents}" "#ifndef OTL_H" start_of_source)
+if(start_of_source EQUAL "-1")
+    message(FATAL_ERROR "Could not find start of source; the header file has changed in a way that we cannot get the license text.")
+endif()
+string(SUBSTRING "${copyright_contents}" 0 "${start_of_source}" copyright_contents)
+string(REGEX REPLACE "// ?" "" copyright_contents "${copyright_contents}")
+string(REGEX REPLACE "=+\n" "" copyright_contents "${copyright_contents}")
+
+file(WRITE
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    "${copyright_contents}"
+)
