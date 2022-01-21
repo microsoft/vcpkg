2962f7865777f38278baed9797277f3d5f835dd5
Max Khon <fjoe@samodelkin.net>
2022-01-21 14:22:45 +0700

    Add support for build profiling
    Use --x-cmake-args=-DX_PORT_PROFILE=TRUE to enable port build profiling

--- scripts/cmake/vcpkg_execute_required_process.cmake
+++ scripts/cmake/vcpkg_execute_required_process.cmake
@@ -100,6 +100,10 @@ Halting portfile execution.
         endif()
     endif()
 
+    if(X_PORT_PROFILE AND NOT arg_ALLOW_IN_DOWNLOAD_MODE)
+        vcpkg_list(PREPEND arg_COMMAND "${CMAKE_COMMAND}" "-E" "time")
+    endif()
+
     vcpkg_execute_in_download_mode(
         COMMAND ${arg_COMMAND}
         OUTPUT_FILE "${log_out}"
