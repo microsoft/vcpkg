3e1690a128f7308fd66c9f77bd74e7c80908a9dd
chausner <chausner@users.noreply.github.com>
2022-01-23 00:50:44 +0100

    Update z3 to 4.8.14

--- ports/z3/portfile.cmake
+++ ports/z3/portfile.cmake
@@ -7,8 +7,8 @@ vcpkg_add_to_path("${PYTHON2_DIR}")
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Z3Prover/z3
-  REF z3-4.8.12
-  SHA512 0B377923BDAFFACA1846AA2ABD61003BBECADFCDFC908ED3097D0AAC8F32028AC39D93FB4A9C2E2C2BFFFBDBEE80AA415875F17DE6C2EE2AE8E2B7921F788C6E
+  REF z3-4.8.14
+  SHA512 10170516ca472258d2f9df28cd036e43023a76a25f1e1670290c62f3890d935bf82770970054a5fd3a0f02559409e7ed4b18fb08347c040ff2f9e0918e152aab
   HEAD_REF master
   PATCHES fix-install-path.patch
 )
@@ -17,15 +17,14 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   set(BUILD_STATIC "-DZ3_BUILD_LIBZ3_SHARED=OFF")
 endif()
 
-vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     ${BUILD_STATIC}
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/z3 TARGET_PATH share/Z3)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME Z3 CONFIG_PATH lib/cmake/z3)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
