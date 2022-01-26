56c17096a07b743a5b97046177be73b071a38496
Dmitry Kobets <dmitrykobets@microsoft.com>
2022-01-26 14:08:18 -0800

    Bump GSL to 4.0.0

--- ports/ms-gsl/portfile.cmake
+++ ports/ms-gsl/portfile.cmake
@@ -2,24 +2,20 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/GSL
-    REF 0f6dbc9e2915ef5c16830f3fa3565738de2a9230
-    SHA512 f72d7d9a18b8055401feb99d99f17c70c0c2015b1a2112ae13fedd27949ff7f9b30718b6afd0b5730ed5573390cb1cc987cd45b7e7fbb92f4134f11d1637ddb7
-    HEAD_REF master
+    REF ebf0498363c53f0d3c403b0548212c147e3747fe
+    SHA512 c28cde973c88e2ab7d686c1bb1cfc1d969632dc99eee88d62c2bddd50288e10ece0458adc75edad8c245a2197ffc5da2bff8c63d1b50917697adbc5fdfb57e6d
+    HEAD_REF main
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DGSL_TEST=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(
-    CONFIG_PATH share/cmake/Microsoft.GSL
-    TARGET_PATH share/Microsoft.GSL
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/Microsoft.GSL)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
