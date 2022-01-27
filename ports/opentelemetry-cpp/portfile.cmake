429f068ec77417d9c966f8467af06a6d35c317b0
jmanjon <67091862+juandemanjon@users.noreply.github.com>
2022-01-27 09:01:29 -0800

    [opentelemetry-cpp] update version to v1.1.1 (#22804)
    * Upgrade opentelemetry-cpp version to v1.1.1
    
    * delete port-version
    
    * Update ports/opentelemetry-cpp/vcpkg.json
    
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
    
    * update version
    
    Co-authored-by: Jonliu1993 <13720414433@163.com>
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/opentelemetry-cpp/portfile.cmake
+++ ports/opentelemetry-cpp/portfile.cmake
@@ -11,8 +11,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-telemetry/opentelemetry-cpp
-    REF v1.0.1
-    SHA512 ddaf5f52f5c100f385e09a6eb69f08fced2e890145939d35f969b05743733409e43f4747713259b68641511401c2c1e01a498b9b7a20fab0f52ee6f4b5d3c77c
+    REF v1.1.1
+    SHA512 e14712f2085134ef0061a36a8ec77195e7a4def9cecb05c37e9c9c0752e59a42621c92166d2166eb8df4767e7b6ea2b39fdc9bcf925c6d66ccaf9f250421db55
     HEAD_REF main
 )
 
