e01aa21ea8a010c9c26ad91fd20f7dac196287d1
Max Khon <fjoe@samodelkin.net>
2022-01-27 10:21:37 +0700

    [libmesh] Fix Windows build
    - compile .C files as C++
    - Fix unistd.h inclusions
    - export explicit template instantiations as libtool "export all" linking
      does not pick them up due to their sections marked as "(pick any)"
      in dumpbin output
    - other MS VC-related fixes

--- ports/libmesh/portfile.cmake
+++ ports/libmesh/portfile.cmake
@@ -1,14 +1,37 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    list(APPEND PATCHES
+        0001-windows-build.patch
+        0002-windows-build-contrib.patch
+        0003-libmesh-export.patch
+        0004-netcdf-getopt.patch
+    )
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libMesh/libmesh
     REF  21f623c837b3865ed65ec9608b357bdb1935d428 #1.5.0
     SHA512 53ad41ed0cd99cb5096ff338a3ff5d8a8ecbfb17dc1d7ee0d2b0cbffecbede7f7c11b7c3c2233cec9dde0988c8828ba0199247effd3442befc72230e641a185e
     HEAD_REF master
+    PATCHES ${PATCHES}
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)
-    set(OPTIONS --disable-qhull )
+    set(OPTIONS
+        --disable-qhull
+        --disable-fparser
+        ax_cv_cxx_openmp=none
+        ac_cv_c_restrict=restrict
+        ac_cv_lib_m_floor=yes
+        LIBS=-lgetopt
+    )
 endif()
+
+set(RELEASE_METHODS "opt")
+if("tests" IN_LIST FEATURES)
+    string(APPEND RELEASE_METHODS " devel")
+endif()
+
 # There a lot of configure options in this port which are not yet correctly handled by VCPKG
 # To only mention two:
 #  --enable-vtk-required   Error if VTK is not detected by configure
@@ -17,8 +40,23 @@ endif()
 # So this port can only be considered a Work In Progress
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
+    USE_WRAPPERS
     OPTIONS ${OPTIONS}
+    OPTIONS_DEBUG --with-methods=dbg
+    OPTIONS_RELEASE --with-methods=${RELEASE_METHODS}
 )
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(WRITE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/contrib/metis/config.h" "")
+    endif()
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(WRITE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/contrib/metis/config.h" "")
+    endif()
+
+    # use our version of "compile" wrapper for *.C compilation as C++ source
+    vcpkg_add_to_path(PREPEND "${SOURCE_PATH}/build-aux")
+endif()
 
 vcpkg_install_make()
 
