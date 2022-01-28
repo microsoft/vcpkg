2183c4d5b93e459d2453be8e2c24ee61fe1d8f2f
Kashyap Jois <kjois@iprdgroup.com>
2022-01-28 14:21:49 +0530

    Revert back to the original port

--- ports/zxing-cpp/portfile.cmake
+++ ports/zxing-cpp/portfile.cmake
@@ -2,38 +2,52 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO nu-book/zxing-cpp
-    REF v1.2.0
-    SHA512 e61b4e44ccaf0871b5d8badf9ce0a81576f55e5d6a9458907b9b599a66227adceabb8d51a0c47b32319d8aeff93e758b4785d3bd0440375247471d95999de487
+    REPO glassechidna/zxing-cpp
+    REF e0e40ddec63f38405aca5c8c1ff60b85ec8b1f10
+    SHA512 222be56e3937136bd699a5d259a068b354ffcd34287bc8e0e8c33b924e9760501b81c56420d8062e0a924fefe95451778781b2aaa07207b0f18ce4ec33732581
     HEAD_REF master
-    PATCHES ignore-pdb-install-symbols-in-lib.patch
+    PATCHES
+        0001-opencv4-compat.patch
+        0002-improve-features.patch
+        0003-fix-dependency-bigint.patch
 )
 
-set(EXTRA_CONFIGURE_OPTIONS )
-set(EXTRA_OPTIONS )
-if (VCPKG_TARGET_IS_UWP)
-   set(EXTRA_CONFIGURE_OPTIONS WINDOWS_USE_MSBUILD)
-   set(EXTRA_OPTIONS -DCMAKE_CXX_FLAGS=-wd4996)
-endif()
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    ${EXTRA_CONFIGURE_OPTIONS}
-        OPTIONS
-        -DBUILD_BLACKBOX_TESTS=OFF
-        -DBUILD_EXAMPLES=OFF
-        ${EXTRA_OPTIONS}
+file(REMOVE ${SOURCE_PATH}/cmake/FindModules/FindIconv.cmake)
+# Depends on port bigint
+file(REMOVE_RECURSE ${SOURCE_PATH}/core/src/bigint)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    opencv WITH_OPENCV
+    iconv WITH_ICONV
 )
 
-vcpkg_cmake_install()
-vcpkg_fixup_pkgconfig()
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS ${FEATURE_OPTIONS}
+)
+
+vcpkg_install_cmake()
+
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(
-    CONFIG_PATH lib/cmake/ZXing
-    PACKAGE_NAME ZXing
-    )
+vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/zxing/cmake TARGET_PATH share/zxing)
+
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+if (NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+else()
+    file(COPY ${CURRENT_PACKAGES_DIR}/bin/zxing DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/zxing)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/lib/zxing)
 
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/zxing-cpp" RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
