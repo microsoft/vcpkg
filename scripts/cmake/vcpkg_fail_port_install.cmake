eed4335a11b786dec3aa5bfa711b6a84dbbeddbb
JackBoosY <yuzaiyang@beyondsoft.com>
2022-01-29 04:21:43 -0800

    Apply suggestions

--- scripts/cmake/vcpkg_fail_port_install.cmake
+++ scripts/cmake/vcpkg_fail_port_install.cmake
@@ -1,12 +1,49 @@
+# DEPRECATED: in favor of the `supports` field in [`manifest file`](manifest-files.md) et al.
+
 #[===[.md:
 # vcpkg_fail_port_install
 
-This function is deprecated, please use `supports` field in manifest file or directly add `${PORT}:${FAILED_TRIPLET}=fail` to _scripts/ci.baseline.txt_ instead.
+Checks common requirements and fails the current portfile with a (default) error message
+
+## Usage
+```cmake
+vcpkg_fail_port_install(
+    [ALWAYS]
+    [MESSAGE <"Reason for failure">]
+    [ON_TARGET <Windows> [<OSX> ...]]
+    [ON_ARCH <x64> [<arm> ...]]
+    [ON_CRT_LINKAGE <static> [<dynamic> ...]])
+    [ON_LIBRARY_LINKAGE <static> [<dynamic> ...]]
+)
+```
+
+## Parameters
+### MESSAGE
+Additional failure message. If none is given, a default message will be displayed depending on the failure condition.
+
+### ALWAYS
+Will always fail early
+
+### ON_TARGET
+Targets for which the build should fail early. Valid targets are `<target>` from `VCPKG_IS_TARGET_<target>` (see `vcpkg_common_definitions.cmake`).
+
+### ON_ARCH
+Architecture for which the build should fail early.
+
+### ON_CRT_LINKAGE
+CRT linkage for which the build should fail early.
+
+### ON_LIBRARY_LINKAGE
+Library linkage for which the build should fail early.
+
+## Examples
+
+* [aws-lambda-cpp](https://github.com/Microsoft/vcpkg/blob/master/ports/aws-lambda-cpp/portfile.cmake)
 #]===]
 
-message("${Z_VCPKG_BACKCOMPAT_MESSAGE_LEVEL}" "vcpkg_fail_port_install has been removed and all values should be moved by adding `supports` field to manifest file or directly adding `${PORT}:${FAILED_TRIPLET}=fail` to _scripts/ci.baseline.txt_.\nPlease remove `vcpkg_fail_port_install(...)`.\n")
-
 function(vcpkg_fail_port_install)
+    z_vcpkg_deprecation_message("vcpkg_fail_port_install has been removed and all values should be moved by adding `supports` field to manifest file or directly adding `${PORT}:${FAILED_TRIPLET}=fail` to _scripts/ci.baseline.txt_.\nPlease remove `vcpkg_fail_port_install(...)`.\n")
+
     set(multi_args "ON_TARGET;ON_ARCH;ON_CRT_LINKAGE;ON_LIBRARY_LINKAGE")
     cmake_parse_arguments(PARSE_ARGV 0 "arg" "ALWAYS" "MESSAGE" "${multi_args}")
     if(DEFINED arg_UNPARSED_ARGUMENTS)
