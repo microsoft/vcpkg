9a5050248366d3f80ce63c2a83bdbdb6a2a815f3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-03 18:30:16 -0500

    [ports/cauthflow/portfile.cmake] Update

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/cauthflow
-    REF             a5ad7823b2c0f9372118962f3bca8f963fbc4d52
-    SHA512          4998deef77d06be4eb7534a227d10cc7ec77bdad53059c0672c6d9450d965c21f609416f341222f2a22deaaedf0905b38066dbe1da8fbf8ad83bdf29fb64e48e
+    REF             7a6315eb279246035b866335bd39b543083dd9df
+    SHA512          d98c7bc8d9b28b1bceaaf69597017b7a1787e6edadd73a4add0ecb1614e21f4ccf0f085d978afc68a05e014f049f8c1e952228a1995f3599e9ba679c88b6af47
     HEAD_REF        master
 )
 
