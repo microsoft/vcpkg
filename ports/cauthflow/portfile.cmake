d3556f5f58351e37c29dd0ba35bb1fafcc247e70
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-04 13:03:19 -0500

    [ports/cauthflow/portfile.cmake] Upgrade to version that exports targets

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/cauthflow
-    REF             5d785fd8a4a47f37d13e9f65f3d3de569ed74aa4
-    SHA512          339b6b3ecdc65ba7c15899e8caae3ebae61ae031dfe2e3eb8ff8cae6c091f76cd8f9e56cf64048a85089d4a37c42ee70cb6461430af294a8447646b120a4d655
+    REF             44581e272f9b55023639abb4a1fa154bb4ee74ce
+    SHA512          814071d9d419033d7c8abe78bf1b4427454c94b6e0f937f27d9d61faf7c299d2f4405aa5636c3e2fdf0c36972f435c541f967f57991a224b06c30758ccc652ca
     HEAD_REF        master
 )
 
@@ -13,9 +13,4 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
-foreach(dir "lib" "share" "tools" "include")
-    if (IS_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/${dir}")
-        file(INSTALL "${CURRENT_PACKAGES_DIR}/debug/${dir}" DESTINATION "${CURRENT_PACKAGES_DIR}/${PORT}")
-    endif (IS_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/${dir}")
-endforeach (dir "lib" "share" "tools")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
