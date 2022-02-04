561e46fc6b1ef980edfc9c582fdaec13d4deda3d
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-02-05 00:40:34 +0100

    Use patch

--- ports/audiofile/portfile.cmake
+++ ports/audiofile/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 004065d01e9b7338580390d4fdbfbaa46adede4e # 1.1.0
     SHA512 99d31035fc82ca8da3c555c30b6b40ea99e15e1f82002c7f04c567ab7aee1de71deddf6930564c56f3a2e83eea1b5f5e9ca631673ed4a943579732b8d62e9603
     HEAD_REF master
+    PATCHES
+        fix-cmakeLists.patch
 )
 
 vcpkg_cmake_configure(
