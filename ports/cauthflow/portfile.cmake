3e728f696e1411cdbb90ba1bd317d65115064b49
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-07 00:38:18 -0500

    [ports/cauthflow/portfile.cmake] [cauthflow_utils/CMakeLists.txt] Only install headers; [cauthflow/CMakeLists.txt] Join cauthflow_utils to cauthflow target for install

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/cauthflow
-    REF             590f42693a3cee88edde14fb1be5554702794050
-    SHA512          1c23663297fbe95f32a1c33bd17369afc0d383303446d3f3fbe4303b78848f451cdacfdbdf05123ca24cf628945a50b544fd6ff311ba302ecc11752a22c5dcd0
+    REF             7ae1918c4a2ece66e5e2221babf89ce7d9b7a6a8
+    SHA512          972060b89e281af18e07d0e60dd6f80d41e76652c4a4152850931b14d6596f2df54be69ec12933e438a1edb6b657ea08b3f86547f606a95d796dd236735fa525
     HEAD_REF        master
 )
 
