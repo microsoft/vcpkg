caa920f3d97389de572ef47592b8f6049c9e7f79
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-07 00:21:30 -0500

    [ports/cauthflow/portfile.cmake] `install` "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake" not in "share/${PROJECT_NAME}/${PROJECT_NAME}" but in "share/${PROJECT_NAME}"

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/cauthflow
     REF             8b02b21bd915520c1b037573491c44c78891d0f4
-    SHA512          0
+    SHA512          2f980ade9883cd5d3b527a47bc2658af3ce15690fcf72413bfa30c8bd31331f8ffce0e383c53f699a41df5ee2bf3a0b49c9f6d37518a5b1f4602a36a5a991a62
     HEAD_REF        master
 )
 
