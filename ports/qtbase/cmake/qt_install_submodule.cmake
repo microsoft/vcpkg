85d2d850acb9beac064be029a326c7324b98b64c
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-02-07 23:21:58 +0100

    forgot to chain vcpkg-cmake-config

--- ports/qtbase/cmake/qt_install_submodule.cmake
+++ ports/qtbase/cmake/qt_install_submodule.cmake
@@ -1,4 +1,5 @@
 include("${CURRENT_HOST_INSTALLED_DIR}/share/vcpkg-cmake/vcpkg-port-config.cmake")
+include("${CURRENT_HOST_INSTALLED_DIR}/share/vcpkg-cmake-config/vcpkg-port-config.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/qt_install_copyright.cmake")
 
 if(QT_IS_LATEST AND PORT STREQUAL "qtbase")
