555b3e06dfc4fa55d2cc8daccfa99351e9dafad2
bisaillonp <Pierre.Bisaillon@ixiasoft.com>
2022-02-07 17:51:03 -0500

    Essai initial

---
+++ ports/xqilla/portfile.cmake
@@ -0,0 +1,120 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
+#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#   VCPKG_TOOLCHAIN           = ON OFF
+#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
+#   BUILD_ARCH                = "Win32" "x64" "ARM"
+#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
+#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
+#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
+#   VCPKG_TARGET_IS_WINDOWS
+#   VCPKG_TARGET_IS_UWP
+#   VCPKG_TARGET_IS_LINUX
+#   VCPKG_TARGET_IS_OSX
+#   VCPKG_TARGET_IS_FREEBSD
+#   VCPKG_TARGET_IS_ANDROID
+#   VCPKG_TARGET_IS_MINGW
+#   VCPKG_TARGET_EXECUTABLE_SUFFIX
+#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
+#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
+#
+# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://sourceforge.net/projects/xqilla/files/XQilla-2.3.4.tar.gz/download"
+    FILENAME "xqilla"
+    SHA512 f744ff883675887494780d24ecdc94afa394d3795d1544b1c598016b3f936c340ad7cd84529ac12962e3c5ce2f1be928a0cd4f9b9eb70e6645a38b0728cb1994
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    # (Optional) A friendly name to use instead of the filename of the archive (e.g.: a version number or tag).
+    # REF 1.0.0
+    # (Optional) Read the docs for how to generate patches at:
+    # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
+    # PATCHES
+    #   001_port_fixes.patch
+    #   002_more_port_fixes.patch
+)
+
+# # Check if one or more features are a part of a package installation.
+# # See /docs/maintainers/vcpkg_check_features.md for more details
+# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+#   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
+#     tbb   WITH_TBB
+#   INVERTED_FEATURES
+#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
+# )
+
+find_file(PATH_TO_XERCESC "xercesc/util/XercesDefs.hpp")
+
+
+if(VCPKG_TARGET_IS_LINUX)
+  list(APPEND CONFIGURE_OPTIONS "--with-xerces=${PATH_TO_XERCESC}")
+  if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+	list(APPEND CONFIGURE_OPTIONS "--enable-static")
+  elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    list(APPEND CONFIGURE_OPTIONS "--enable-shared")	
+  endif()
+  vcpkg_configure_cmake(
+      SOURCE_PATH "${SOURCE_PATH}"
+	  AUTOCONFIG
+	  OPTIONS "${CONFIGURE_OPTIONS}"
+  )
+  
+  FIND_PROGRAM(MAKE make)
+  IF (NOT MAKE)
+    MESSAGE(FATAL_ERROR "MAKE not found")
+  ENDIF ()
+  message(STATUS "Building ${TARGET_TRIPLET}-dbg")
+  vcpkg_execute_build_process(
+    COMMAND make  "debug=1" "optimize=0" "-j${VCPKG_CONCURRENCY}"
+    WORKING_DIRECTORY "${WORKING_DIR}"
+    LOGNAME make-${TARGET_TRIPLET}-dbg
+  )
+  message(STATUS "Building ${TARGET_TRIPLET}-dbg done")
+  message(STATUS "Packaging ${TARGET_TRIPLET}-dbg")
+  vcpkg_execute_build_process(
+    COMMAND make install
+    WORKING_DIRECTORY "${WORKING_DIR}"
+    LOGNAME install-${TARGET_TRIPLET}-dbg
+  )
+  message(STATUS "Packaging ${TARGET_TRIPLET}-dbg done")
+
+  vcpkg_execute_build_process(
+    COMMAND make realclean
+    WORKING_DIRECTORY "${WORKING_DIR}"
+    LOGNAME realclean-${TARGET_TRIPLET}-dbg
+  )
+  message(STATUS "Building ${TARGET_TRIPLET}-rel")
+  vcpkg_execute_build_process(
+    COMMAND make  "-j${VCPKG_CONCURRENCY}"
+    WORKING_DIRECTORY "${WORKING_DIR}"
+    LOGNAME make-${TARGET_TRIPLET}-rel
+  )
+  message(STATUS "Building ${TARGET_TRIPLET}-rel done")
+  message(STATUS "Packaging ${TARGET_TRIPLET}-rel")
+  vcpkg_execute_build_process(
+    COMMAND make install
+    WORKING_DIRECTORY "${WORKING_DIR}"
+    LOGNAME install-${TARGET_TRIPLET}-rel
+  )
+  message(STATUS "Packaging ${TARGET_TRIPLET}-rel done")
+endif()
+
+# # Moves all .cmake files from /debug/share/xqilla/ to /share/xqilla/
+# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
+# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/xqilla)
+
+# # Handle copyright
+# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/xqilla RENAME copyright)
