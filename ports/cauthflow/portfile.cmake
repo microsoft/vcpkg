8ea28ad71b76cbc8fbe672e526c5ff21b16a5551
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-07 20:30:19 -0500

    [ports/cauthflow/portfile.cmake] `#ifndef CAUTHFLOW_CLIENT_FROM_CONFIG` then take `client_id` and `client_secret` from function parameters rather than config header

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             5b9329398105572fcacdcca06d2ef02b4e68412d
-    SHA512          fd57d5af80dc777373f07c10f6d43c100eb4849e345a66071a807d9f4da0fb92337c9bda8e0f3f4bbff0bda03d0bfbd17d4af62019e7dd39059b3d580923a87d
+    REF             9eb884a22da8f8ef0a1018a1cf03da3cfe991c0a
+    SHA512          634cfe31ea2bb227e25ad23c77ef0801f3b0b8a46f75fa0125dbea798261b44286311a04c0177ad689d5f064533e84b797e0075e0a82a9d9b022887125ad68bd
     HEAD_REF        master
 )
 
