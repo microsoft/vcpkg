95ece59712af90ea3ca8a8cd90cd8d995b4a0aed
bisaillonp <Pierre.Bisaillon@ixiasoft.com>
2022-02-09 09:09:43 -0500

    Export des fichiers include

--- ports/xqilla/portfile.cmake
+++ ports/xqilla/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-	PATCHES "fix-compare.patch"
+    PATCHES "fix-compare.patch"
 )
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
@@ -15,30 +15,16 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
 endif()
 
 
-if(VCPKG_TARGET_IS_LINUX)
-  list(APPEND CONFIGURE_OPTIONS "--with-xerces=${CURRENT_INSTALLED_DIR}")
-  if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-	list(APPEND CONFIGURE_OPTIONS "--enable-static")
-  elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    list(APPEND CONFIGURE_OPTIONS "--enable-shared")	
-  endif()
-  vcpkg_configure_make(
-      SOURCE_PATH "${SOURCE_PATH}"
-	  AUTOCONFIG
-	  OPTIONS ${CONFIGURE_OPTIONS}
-  )
-  
-  vcpkg_install_make()
-  vcpkg_fixup_pkgconfig()
-else()
-  file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-  vcpkg_cmake_configure(
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-	WINDOWS_USE_MSBUILD
-	OPTIONS 
-		${COMPILE_OPTIONS}
-  )
-  vcpkg_cmake_install()
-endif()
+    NO_CHARSET_FLAG
+    OPTIONS 
+        ${COMPILE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION ${CURRENT_PACKAGES_DIR}/share/xqilla RENAME copyright)
