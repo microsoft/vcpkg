36f0abdbaf3b31196939c327594878e2756b1232
Rémy Tassoux <contact@rt2.fr>
2022-02-10 03:22:31 +0100

    [implot] Fix build on osx without patch

--- ports/implot/portfile.cmake
+++ ports/implot/portfile.cmake
@@ -6,8 +6,6 @@ vcpkg_from_github(
     REF v0.13
     SHA512 ed653a14f1174b1689c7b8e5761455644e41e75a3cdf5a76d96adf8e286893418660809c1c158fa405c1c48a82752ae9c3b8e1eb14bd0015081604cdb44d95f4
     HEAD_REF master
-    PATCHES 
-        fix-osx-build.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
