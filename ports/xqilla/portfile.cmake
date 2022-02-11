803d860136c4ff0236c3152e08b5cdae018ababb
bisaillonp <Pierre.Bisaillon@ixiasoft.com>
2022-02-11 15:12:00 -0500

    Applying suggested modifications

--- ports/xqilla/portfile.cmake
+++ ports/xqilla/portfile.cmake
@@ -19,6 +19,7 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     NO_CHARSET_FLAG
+    WINDOWS_USE_MSBUILD
     OPTIONS 
         ${COMPILE_OPTIONS}
 )
