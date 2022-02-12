4f278cf552afc896b63704956fa8f3bf7570c025
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-12 17:15:15 -0500

    [ports/cauthflow] Add latest cauthflow which temporarily embeds libcurl-simple-https… TODO: revert back to two separate libs

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             593944495a3433596cbe7f180e8576931b6c05e3
-    SHA512          dbaa6cbfd6c6dba7f4d51ab6de67697bea2e864c92923d4f768723e4372ea6448bf12a9c2f903c61c40e159e076eadd6a449cfa6f185136837bee915cd13e256
+    REF             d573aaf57b38c6a5cfdc16e9a4c0f54c83c2f7c5
+    SHA512          90350bdc9c738a4e167247138136c2f07ae20cadb8be3050196135c59e11387673acc8e2f15622be6d37c937b3ed2456967b6e9ce1632d8be484a9dd2691d72b
     HEAD_REF        master
 )
 
