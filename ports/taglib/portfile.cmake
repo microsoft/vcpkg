400a9fd65277e29249de389630dd46797702de8e
Alexander Neumann <neumann@imt.uni-luebeck.de>
2022-02-14 15:50:44 +0100

    [taglib] fix static linkage

--- ports/taglib/portfile.cmake
+++ ports/taglib/portfile.cmake
@@ -11,13 +11,13 @@ if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
     set(WINRT_OPTIONS -DHAVE_VSNPRINTF=1 -DPLATFORM_WINRT=1)
 endif()
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     PREFER_NINJA
     OPTIONS ${WINRT_OPTIONS}
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 set(pcfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/taglib.pc")
 if(EXISTS "${pcfile}")
@@ -29,7 +29,6 @@ if(EXISTS "${pcfile}")
     vcpkg_replace_string("${pcfile}" "Requires: " "Requires: zlib")
     vcpkg_replace_string("${pcfile}" " -lz" "")
 endif()
-vcpkg_fixup_pkgconfig()
 
 # remove the debug/include files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
@@ -41,6 +40,7 @@ file(RENAME "${CURRENT_PACKAGES_DIR}/share/taglib/COPYING.LGPL" "${CURRENT_PACKA
 
 # remove bin directory for static builds (taglib creates a cmake batch file there)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/taglib/taglib_export.h" "defined(TAGLIB_STATIC)" "1")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
