7f57cd5a283fc3705fabefa6517747b942fc49f9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-15 09:14:39 -0500

    [ports/cauthflow] Add latest cauthflow with support for Linux

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             eae57cd0cfec9eda9c66e1dd5eade307f5e9df20
-    SHA512          8c1b400ea67617717cce14081a3d3d180c9648bb9097dcef14530c67f58d9fc3abd3a4057f8925c812889affcade9f16bee178431c6afb847dbcc7516aebbebc
+    REF             1d5afae65deb3bcb5bec9b426b48bfa3179430c8
+    SHA512          29e7533439c294793f161cf1e1dd0244f87e7be7b1aef84381b9ff790536dd2145143d420d119ac21e01bf54b6fac1d64706d88aaafa697b2ad703ee71fded94
     HEAD_REF        master
 )
 
