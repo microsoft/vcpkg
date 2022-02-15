b5066eceaefa26d7a1f70843a18441f439552caa
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-15 17:45:09 -0500

    [ports/libcurl-simple-https/portfile.cmake] Remove `file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")`

--- ports/libcurl-simple-https/portfile.cmake
+++ ports/libcurl-simple-https/portfile.cmake
@@ -15,4 +15,3 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
