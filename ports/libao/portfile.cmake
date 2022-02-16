26021150d8b032f7f874b040eaa4adb5c25d006e
xiaoyifang <yifang.xiao@outlook.com>
2022-02-16 20:46:55 +0800

    libao add def export file

--- ports/libao/portfile.cmake
+++ ports/libao/portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_from_github(
 
 if(VCPKG_TARGET_IS_WINDOWS)
   file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-  
+  file(COPY "${CMAKE_CURRENT_LIST_DIR}/libao.def" DESTINATION "${SOURCE_PATH}")
   vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON
