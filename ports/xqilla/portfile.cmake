7138f09298cba938922c98f678b13b4702f260fe
bisaillonp <Pierre.Bisaillon@ixiasoft.com>
2022-02-17 14:51:19 -0500

    Applying comments from CR

--- ports/xqilla/portfile.cmake
+++ ports/xqilla/portfile.cmake
@@ -19,10 +19,7 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     NO_CHARSET_FLAG
-    WINDOWS_USE_MSBUILD
-    GENERATOR Ninja
-    OPTIONS 
-        ${COMPILE_OPTIONS}
+    OPTIONS ${COMPILE_OPTIONS}
 )
 
 vcpkg_cmake_install()
