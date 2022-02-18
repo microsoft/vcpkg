6eba4c3f904abd02454a70cf45cfb1a97b893442
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-18 16:11:34 -0500

    [ports/libcurl-simple-https/portfile.cmake] [libcurl-simple-https/curl_simple_https.c] No alt option set when body is NULL

--- ports/libcurl-simple-https/portfile.cmake
+++ ports/libcurl-simple-https/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/curl-simple-https
-    REF             985f2e0ff4e0bb50423a2c1319eb4981d957f146
-    SHA512          96319bfdcfed363522828f6036c14477499ed2132aaded34539050cd8c6a217a08f8ce6aee06d4ace0e882169897522bbad806df7e7c52b57b5da3454d15b7b1
+    REF             040f626c4e004211373590fbc0d572a4e8126a83
+    SHA512          86f7aef50f5f5c80e5f4bf5466f9310605aa1fbbc56d59fed3db95450b31bca712a596298ea24674fb4d321644043f54c63e3b44e03a9e026316ae83827e68b0
     HEAD_REF        master
 )
 
