dc40109a0f47ffbb7d90ad0af3f8eb94d0d3c707
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-18 17:57:50 -0500

    [ports/libcurl-simple-https/portfile.cmake] [libcurl-simple-https/curl_simple_https.c] More debug statements

--- ports/libcurl-simple-https/portfile.cmake
+++ ports/libcurl-simple-https/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/curl-simple-https
-    REF             040f626c4e004211373590fbc0d572a4e8126a83
-    SHA512          86f7aef50f5f5c80e5f4bf5466f9310605aa1fbbc56d59fed3db95450b31bca712a596298ea24674fb4d321644043f54c63e3b44e03a9e026316ae83827e68b0
+    REF             fc79ebd6213e8339bac79b571c9784260c95a1e7
+    SHA512          b52ba4f389d1c289b2a3611826cfb76c2f0b22ac3eec2d012019e4b46a5a7052df487cfe55cbf367abe850d962d9fc2c9e19db344a764432252c467cc862ec79
     HEAD_REF        master
 )
 
