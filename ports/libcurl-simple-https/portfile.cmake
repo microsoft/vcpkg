e88600220be87cca23ed164a36f5bf16478a0a83
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-18 15:16:53 -0500

    [ports/libcurl-simple-https/portfile.cmake] [CMakeLists.txt] Bump `cmake_minimum_required` from `3.0` to `3.3`; [libcurl-simple-https/curl_simple_https.c] Use `calloc`; add debug statements

--- ports/libcurl-simple-https/portfile.cmake
+++ ports/libcurl-simple-https/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/curl-simple-https
-    REF             285ebd918e55f8bfd2291773f3f0611554ddf214
-    SHA512          3e613de7f011b24754767b3d07e7608a80e0c2440a98eaf5d812133a3062a4ea2b7eeedac603f231eae5acb7950312cabb0a144f430660e656c46c038c7364aa
+    REF             985f2e0ff4e0bb50423a2c1319eb4981d957f146
+    SHA512          96319bfdcfed363522828f6036c14477499ed2132aaded34539050cd8c6a217a08f8ce6aee06d4ace0e882169897522bbad806df7e7c52b57b5da3454d15b7b1
     HEAD_REF        master
 )
 
@@ -15,6 +15,5 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
-foreach(_dir "include" "share")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/${_dir}")
-endforeach(_dir "include" "share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share")
