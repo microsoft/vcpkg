7914108a2122ece0582d9fb26578e58e06e5fe9d
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-19 22:15:55 -0500

    [ports/libcurl-simple-https/portfile.cmake] [libcurl-simple-https/curl_simple_https.c] Set `CURLOPT_READDATA`, `CURLOPT_READFUNCTION`, `CURLOPT_POSTFIELDSIZE`: even with an empty HTTPS POST

--- ports/libcurl-simple-https/portfile.cmake
+++ ports/libcurl-simple-https/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/curl-simple-https
-    REF             61f7523e170106aea3a255a7e2133ae9ae5ab225
-    SHA512          eed179fee6d5cae5a860b6328cd4ba1f494f4fc5ead6099ea4ae1a82537c9befaee988c093b7cb4fa7a303757419deaab234e415c3451df89a41ea9b18158bcf
+    REF             5a115053ba4d249fc1af22c3673b4d014e56bcf5
+    SHA512          6274bfeec5235d39c627850b1b6ef03c3f1982c74f937b604137cf3cf87e982f971c4681760b42926a3fb15bc8268f2fa48c197919516066d73f53425aa24545
     HEAD_REF        master
 )
 
