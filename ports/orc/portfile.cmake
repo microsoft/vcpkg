b7d36c54fc5e9ef4ff65ed478c8e2b8a5d85d48b
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-02-20 22:06:27 +0100

    Update to 1.7.3

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apache/orc
-    REF 23ecc03e87548f6d6783c2d8af2b46672c52214c  # rel/release-1.6.4
-    SHA512 907984c7e036ddaa90e7cbfabb9af4f6fd3520820b9a8732b304f2213030f7d67cef89ad87d50e028a51bff06f68ff359345ad6894850e299b2fca343d7c0c3e
+    REF acfce62b56047e851e75b4f3212594026766da97  # rel/release-1.7.3
+    SHA512 7874751bdd0a84abc6a232e469cdf126937207ea8f974e02e5a57765ac1d9d6c180b80d708c29eb99f84073ae0032a46ea0a09a7ba36cdb900fdbe89d199eba7
     HEAD_REF master
     PATCHES
         0003-dependencies-from-vcpkg.patch
@@ -17,9 +17,9 @@ file(REMOVE "${SOURCE_PATH}/cmake_modules/FindSnappy.cmake")
 file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZLIB.cmake")
 
 if(CMAKE_HOST_WIN32)
-  set(PROTOBUF_EXECUTABLE ${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc.exe)
+  set(PROTOBUF_EXECUTABLE "${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc.exe")
 else()
-  set(PROTOBUF_EXECUTABLE ${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc)
+  set(PROTOBUF_EXECUTABLE "${CURRENT_INSTALLED_DIR}/tools/protobuf/protoc")
 endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
@@ -28,9 +28,8 @@ else()
   set(BUILD_TOOLS ON)
 endif()
 
-vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     -DBUILD_TOOLS=${BUILD_TOOLS}
     -DBUILD_CPP_TESTS=OFF
@@ -42,23 +41,23 @@ vcpkg_configure_cmake(
     -DENABLE_TEST=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-file(GLOB TOOLS ${CURRENT_PACKAGES_DIR}/bin/orc-*)
+file(GLOB TOOLS "${CURRENT_PACKAGES_DIR}/bin/orc-*")
 if(TOOLS)
-  file(COPY ${TOOLS} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/orc)
+  file(COPY ${TOOLS} DESTINATION "${CURRENT_PACKAGES_DIR}/tools/orc")
   file(REMOVE ${TOOLS})
 endif()
 
-file(GLOB BINS ${CURRENT_PACKAGES_DIR}/bin/*)
+file(GLOB BINS "${CURRENT_PACKAGES_DIR}/bin/*")
 if(NOT BINS)
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
