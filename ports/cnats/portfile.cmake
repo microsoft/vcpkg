6823a4729d74fcf89620416fa8e6a49ccbf19c60
Sylvain Prevost <sylvain-prevost@users.noreply.github.com>
2022-02-21 05:20:59 -0600

    Add missing quotes (potential space under windows)
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/cnats/portfile.cmake
+++ ports/cnats/portfile.cmake
@@ -58,7 +58,7 @@ vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
