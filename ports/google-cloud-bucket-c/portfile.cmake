e2fb5f3bf8adfd607cedb6499b4b1c0a57c674a2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-02-22 23:00:35 -0500

    [ports/google-cloud-bucket-c/portfile.cmake] Don't remove whole debug dir

--- ports/google-cloud-bucket-c/portfile.cmake
+++ ports/google-cloud-bucket-c/portfile.cmake
@@ -11,4 +11,5 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share")
