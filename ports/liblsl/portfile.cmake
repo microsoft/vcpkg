7ca5da5bea429ffaedf0ad22c2c050ea474f9e9b
chausner <chausner@users.noreply.github.com>
2022-02-23 19:45:02 +0100

    Update version to upcoming 1.16.0

--- ports/liblsl/portfile.cmake
+++ ports/liblsl/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sccn/liblsl
-    REF a9a06c78d4218b76e1c906c04452eec697f02f8e # NOTE: when updating version, also change it in the parameter to vcpkg_cmake_configure
+    REF 1a2e886e4a31eae7362574933734a39c3a523ca7 # NOTE: when updating version, also change it in the parameter to vcpkg_cmake_configure
     SHA512 b58665e836e6a1fc6b7669f7ba1208e14468b5f6b8ba3fdd06936cc47c8b32a027de49a4e252c8b6fabc13f846af30b7d209c6ba79842d165049f4baad7e4da9    
     HEAD_REF master
     PATCHES
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
         -DLSL_BUILD_STATIC=${LSL_BUILD_STATIC}
         -DLSL_BUNDLED_BOOST=OFF # we use the boost vcpkg packages instead
         -DLSL_BUNDLED_PUGIXML=OFF # we use the pugixml vcpkg package instead
-        -Dlslgitrevision=v1.15.2-a9a06c
+        -Dlslgitrevision=v1.16.0
         -Dlslgitbranch=master
 )
 
