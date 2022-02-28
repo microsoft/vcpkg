e26e1764a490c6283ccb28b16b1997ef56a62cff
Alex <alex.shaw.as@gmail.com>
2022-02-28 13:18:07 -0800

    Update ports/quickjs/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/quickjs/portfile.cmake
+++ ports/quickjs/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/quickjs-config.cmake DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(configure_file ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}/usage @ONLY)
 
 vcpkg_configure_cmake(
   SOURCE_PATH ${SOURCE_PATH}
