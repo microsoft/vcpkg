75ac34d5fa90c86d99510650b0d848fb6060ff28
Mengna-Li <543250287@qq.com>
2022-03-01 08:15:52 +0000

    fix link error

--- ports/llvm/portfile.cmake
+++ ports/llvm/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
         0009-fix-tools-install-path.patch
         0010-fix-libffi.patch
         0011-fix-libxml2.patch
+        0012-fix-build-type.patch
 )
 
 vcpkg_check_features(
