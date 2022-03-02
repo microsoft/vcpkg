7f09579a1a12caaeb042683fcd93692c22632f53
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-02 11:18:34 -0500

    [ports/cauthflow/portfile.cmake] [cauthflow/cauthflow/open_browser.c] Fix Windows headers ; `asprintf` only used on Linux so #include it for that only ; `free`

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             3244734796173b58ccf8b6f7e144420db4530a22
-    SHA512          00c9dcdce570e42b6027138cb0abeec0601d618be8892d13a06f22856cfbbf60ad358ee482e0f2ea1b6b9b7278e30b396fc6604fdfc84535849dfd63c42d9b95
+    REF             abb93290ebe357c68998483ef694cc3da7b6df62
+    SHA512          cfe84f06a3144dfe7cc091e211af55926ed4821e8e0789d235c276f58bfdb083c88a65342bfade28c5cea900c06a309bc0a6688c108b64db3aff9c0e775f1370
     HEAD_REF        master
 )
 
