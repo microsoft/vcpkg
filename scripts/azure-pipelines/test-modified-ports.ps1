3a793c2294c35039b9de0e1fce7f7121d3d4651b
Kai Pastor <dg0yt@darc.de>
2022-03-03 09:30:54 +0100

    Hide output from switch to parent hash

--- scripts/azure-pipelines/test-modified-ports.ps1
+++ scripts/azure-pipelines/test-modified-ports.ps1
@@ -161,7 +161,7 @@ if (($BuildReason -eq 'PullRequest') -and -not $NoParentHashes)
     Write-Host "Determining parent hashes using HEAD~1"
     $parentHashesFile = Join-Path $WorkingRoot 'parent-hashes.json'
     $parentHashes = @("--parent-hashes=$parentHashesFile")
-    & git revert -n -m 1 HEAD
+    & git revert -n -m 1 HEAD | Out-Null
     & "./vcpkg$executableExtension" ci $Triplet --dry-run --exclude=$skipList @hostArgs @commonArgs --no-binarycaching "--output-hashes=$parentHashesFile" `
         | ForEach-Object { if ($_ -match ' dependency information| determine pass') { Write-Host $_ } }
     & git reset --hard HEAD
