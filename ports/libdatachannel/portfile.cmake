2c640e6afc47c2fb43aba5b43a690a46179b5f34
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-03-05 18:30:26 +0100

    fix cmake again

--- ports/libdatachannel/portfile.cmake
+++ ports/libdatachannel/portfile.cmake
@@ -1,7 +1,7 @@
 set(PATCHES 0001-fix-for-vcpkg.patch)
 
 if(VCPKG_TARGET_IS_UWP)
-    list(PATCHES APPEND uwp-warnings.patch)
+    list(APPEND PATCHES uwp-warnings.patch)
 endif()
 
 vcpkg_from_github(
