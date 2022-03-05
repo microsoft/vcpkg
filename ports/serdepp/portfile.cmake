94aa60375f410661b9db00393cd59996ad1967a5
injae <8687lee@gmail.com>
2022-03-05 11:30:31 +0900

    port update

--- ports/serdepp/portfile.cmake
+++ ports/serdepp/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/serdepp")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/serdepp)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/cmake"
