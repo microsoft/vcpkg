67c98be6f3d461b387ddedb601963db39fe68087
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-03-06 18:26:09 +0100

    fix idevicerestore

--- ports/idevicerestore/portfile.cmake
+++ ports/idevicerestore/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 ba623be56c2f37853516d7d4c32e16f1ec72f33d512f18aa812ce6830af4b9e389f7af5321888dd0ddd168e282b652e379b60f90970680e213eabf489f406915
     HEAD_REF msvc-master
     PATCHES
-        libcurl_d.patch
+        fix-vcxproj.patch
         fix-libgen.h-cannot-be-found.patch
 )
 
@@ -13,7 +13,6 @@ vcpkg_install_msbuild(
     SOURCE_PATH ${SOURCE_PATH}
     PROJECT_SUBPATH idevicerestore.vcxproj
     LICENSE_SUBPATH COPYING
-    OPTIONS /wd4996
     USE_VCPKG_INTEGRATION
 )
 
