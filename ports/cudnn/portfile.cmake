7bf19719b9dbc2ba5a3b07ebae8a4b8970da7cf1
FrankXie <v-frankxie@microsoft.com>
2022-03-06 23:49:58 -0800

    [cudnn] update to v8.3.2

--- ports/cudnn/portfile.cmake
+++ ports/cudnn/portfile.cmake
@@ -8,7 +8,7 @@ endif()
 
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 
-set(MINIMUM_CUDNN_VERSION "7.6.5")
+set(MINIMUM_CUDNN_VERSION "8.3.2")
 
 include(${CURRENT_INSTALLED_DIR}/share/cuda/vcpkg_find_cuda.cmake)
 vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT CUDA_TOOLKIT_ROOT OUT_CUDA_VERSION CUDA_VERSION)
@@ -71,6 +71,6 @@ else()
   message(FATAL_ERROR "Please install CUDNN using your system package manager (the same way you installed CUDA). For example: apt install libcudnn8-dev.")
 endif()
 
-file(INSTALL "${CURRENT_PORT_DIR}/FindCUDNN.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(INSTALL "${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${CURRENT_PORT_DIR}/FindCUDNN.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
