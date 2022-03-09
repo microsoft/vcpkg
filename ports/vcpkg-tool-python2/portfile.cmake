b32919d7f86dd3eb7406e230c9fcb053f4f86d3d
Alexander Neumann <you@example.com>
2022-03-09 10:28:44 +0100

    move python2 to manual-tools adjust search path generate a details.cmake for vcpkg_find_acquire_program

--- ports/vcpkg-tool-python2/portfile.cmake
+++ ports/vcpkg-tool-python2/portfile.cmake
@@ -14,20 +14,20 @@ if(VCPKG_TARGET_IS_WINDOWS)
         set(download_filename "python-${program_version}.amd64.msi")
         set(download_sha512 6a81a413b80fd39893e7444fd47efa455d240cbb77a456c9d12f7cf64962b38c08cfa244cd9c50a65947c40f936c6c8c5782f7236d7b92445ab3dd01e82af23e)
     endif()
-    set(paths_to_search "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+    set(paths_to_search "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}")
 
     vcpkg_download_distfile(archive_path
         URLS ${download_urls}
         SHA512 "${download_sha512}"
         FILENAME "${download_filename}"
     )
-    set(output_path "${CURRENT_PACKAGES_DIR}/tools")
+    set(output_path "${CURRENT_PACKAGES_DIR}/manual-tools") # vcpkg.cmake adds everything in /tools to CMAKE_PROGRAM_PATH. That is not desired for Python2
     file(MAKE_DIRECTORY "${output_path}")
     cmake_path(NATIVE_PATH archive_path archive_path_native) # lessmsi is a bit picky about path formats.
     message(STATUS "Extracting Python2 ...")
     vcpkg_execute_in_download_mode(
                     COMMAND "${CURRENT_INSTALLED_DIR}/tools/vcpkg-tool-lessmsi/lessmsi.exe" x "${archive_path_native}" # Using output_path here does not work in bash
-                    WORKING_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools" 
+                    WORKING_DIRECTORY "${CURRENT_PACKAGES_DIR}/manual-tools" 
                     OUTPUT_FILE "${CURRENT_BUILDTREES_DIR}/lessmsi-out.log"
                     ERROR_FILE "${CURRENT_BUILDTREES_DIR}/lessmsi-err.log"
                     RESULT_VARIABLE error_code
@@ -36,8 +36,12 @@ if(VCPKG_TARGET_IS_WINDOWS)
         message(FATAL_ERROR "Couldn't extract Python2 with lessmsi!")
     endif()
     message(STATUS "Extracting Python2 ... finished!")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/tools/python-2.7.18.amd64/SourceDir/" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/python-2.7.18.amd64")
+    file(RENAME "${CURRENT_PACKAGES_DIR}/manual-tools/python-2.7.18.amd64/SourceDir/" "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}/")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/manual-tools/python-2.7.18.amd64"
+                        # Files below are not part of a msiexec installation/extraction. 
+                        "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}/Windows"
+                        "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}/Microsoft.VC90.CRT.manifest"
+                        "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}/msvcr90.dll")
     z_vcpkg_find_acquire_program_find_internal("PYTHON2"
         INTERPRETER "${interpreter}"
         PATHS ${paths_to_search}
@@ -47,6 +51,8 @@ if(VCPKG_TARGET_IS_WINDOWS)
         message(FATAL_ERROR "Unable to find python2: ${PYTHON2}")
     endif()
     message(STATUS "Using python2: ${PYTHON2}")
-    file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/version.txt" "${program_version}") # For vcpkg_find_acquire_program
+    set(details "set(program_version \"${program_version}\")\n")
+    string(APPEND details "set(search_path \"\${CURRENT_HOST_INSTALLED_DIR}/manual-tools/${PORT}\")\n")
+    file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/details.cmake" "${details}")
 endif()
 
