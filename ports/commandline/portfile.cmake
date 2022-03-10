139700ba657d4f01bc860a93a56d4ae8706b9443
Leonid Pospelov <pospelovlm@yandex.ru>
2022-03-10 04:10:17 +0300

    [commandline] Add new port

---
+++ ports/commandline/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lionkor/commandline
+    REF 4a8000d6b767263a79c589fcc74099a4454e07a9
+    SHA512 81ee2716b7048e51d26f75033be7a6d3a1aec9bfef833ad067112b26144023dfad8f5f8d145b4162dba2de3ad09de223dbe9143cf9e2f5f5102374d3412aebf7
+    HEAD_REF master
+    PATCHES
+        add-install.patch
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_EXAMPLES=OFF
+)
+vcpkg_build_cmake()
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+# Handle header
+file(INSTALL "${SOURCE_PATH}/commandline.h" DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
