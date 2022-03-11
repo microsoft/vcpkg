7460e23700ddc087929b0c88e99104a5bca25661
Cheney-Wang <v-xincwa@microsoft.com>
2022-03-11 01:23:41 -0800

    Add info for linux

--- ports/speex/portfile.cmake
+++ ports/speex/portfile.cmake
@@ -28,7 +28,9 @@ if(VCPKG_TARGET_IS_WINDOWS)
   endif()
 else()
   if(VCPKG_TARGET_IS_OSX)
-      message("${PORT} currently requires the following library from the system package manager:\n    autoconf\n    automake\n    libtool\n\nIt can be installed with brew install autoconf automake libtool")
+      message("${PORT} currently requires the following libraries from the system package manager:\n    autoconf\n    automake\n    libtool\n\nIt can be installed with brew install autoconf automake libtool")
+  elseif(VCPKG_TARGET_IS_LINUX)
+      message("${PORT} currently requires the following libraries from the system package manager:\n    autoconf\n    automake\n    libtool\n\nIt can be installed with apt-get install autoconf automake libtool")
   endif()
   vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
