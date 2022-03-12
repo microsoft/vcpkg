dcaf6cb44da3e96de577ad777247729f209285ee
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-03-12 23:13:57 +0100

    add bz2d patch

--- ports/python3/portfile.cmake
+++ ports/python3/portfile.cmake
@@ -14,6 +14,7 @@ set(PATCHES
     0004-dont-copy-vcruntime.patch
     0005-only-build-required-projects.patch
     0009-python.pc.patch
+    0010-bz2d.patch
 )
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     list(PREPEND PATCHES 0001-static-library.patch)
