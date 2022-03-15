3b92b4175617536ed3e1718326a495ab9a587a97
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-03-15 07:12:29 +0100

    use version-date

--- ports/pixel/portfile.cmake
+++ ports/pixel/portfile.cmake
@@ -6,7 +6,7 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO dascandy/pixel
-    REF c4411f67746fdd811aa5f8c102ac340e9eaf4ec5 # latest commit on master, v0.3
+    REF c4411f67746fdd811aa5f8c102ac340e9eaf4ec5
     SHA512 e4f704c076bb61220349524b0b1033a92c44128bb81e79dbd32ea2d1aa9d4abb0d6daab3617f69b59d1c1e50d750767153174fea015d8718804612f4d9f68ff6
     HEAD_REF master
     PATCHES
