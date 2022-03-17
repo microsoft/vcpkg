529e37c91abc9c6e5a893549c884e464cc4e00c1
Jason Ortiz <8240549+jason-ortiz@users.noreply.github.com>
2022-03-17 16:23:50 -0400

    Update Crashpad to support Linux and Android

--- ports/crashpad/portfile.cmake
+++ ports/crashpad/portfile.cmake
@@ -1,4 +1,7 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+set(VCPKG_TARGET_TRIPLET ${TARGET_TRIPLET})
+set(CMAKE_SYSTEM_VERSION 29)
+set(VCPKG_CRT_LINKAGE static)
 
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -9,25 +12,36 @@ vcpkg_from_git(
 vcpkg_find_acquire_program(PYTHON3)
 vcpkg_replace_string("${SOURCE_PATH}/.gn" "script_executable = \"python3\"" "script_executable = \"${PYTHON3}\"")
 
-function(checkout_in_path PATH URL REF)
-    if(EXISTS "${PATH}")
+function(checkout_into_path)
+    cmake_parse_arguments(PARSE_ARGV 0 "arg" "" "DEST;URL;REF;PATCHES" "")
+    
+    if(EXISTS "${arg_DEST}")
         return()
     endif()
 
     vcpkg_from_git(
         OUT_SOURCE_PATH DEP_SOURCE_PATH
-        URL "${URL}"
-        REF "${REF}"
+        URL "${arg_URL}"
+        REF "${arg_REF}"
+        PATCHES "${arg_PATCHES}"
     )
-    file(RENAME "${DEP_SOURCE_PATH}" "${PATH}")
+    file(RENAME "${DEP_SOURCE_PATH}" "${arg_DEST}")
     file(REMOVE_RECURSE "${DEP_SOURCE_PATH}")
 endfunction()
 
 # mini_chromium contains the toolchains and build configuration
-checkout_in_path(
-    "${SOURCE_PATH}/third_party/mini_chromium/mini_chromium"
-    "https://chromium.googlesource.com/chromium/mini_chromium"
-    "5654edb4225bcad13901155c819febb5748e502b"
+checkout_into_path(
+    DEST "${SOURCE_PATH}/third_party/mini_chromium/mini_chromium"
+    URL "https://chromium.googlesource.com/chromium/mini_chromium"
+    REF "502930381b23c5fa3911c8b82ec3e4ba6ceb3658"
+    PATCHES update-toolchain.patch
+)
+
+# lss
+checkout_into_path(
+    DEST "${SOURCE_PATH}/third_party/lss/lss"
+    URL "https://chromium.googlesource.com/linux-syscall-support.git"
+    REF "7bde79cc274d06451bf65ae82c012a5d3e476b5a"
 )
 
 function(replace_gn_dependency INPUT_FILE OUTPUT_FILE LIBRARY_NAMES)
@@ -61,10 +75,8 @@ replace_gn_dependency(
     "z;zlib;zlibd"
 )
 
-set(OPTIONS_DBG "is_debug=true")
-set(OPTIONS_REL "")
-
-if(CMAKE_HOST_WIN32)
+if("${VCPKG_TARGET_TRIPLET}" MATCHES ".*-windows")
+    message(STATUS "Matched Windows")
     # Load toolchains
     if(NOT VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
         set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${SCRIPTS}/toolchains/windows.cmake")
@@ -91,41 +103,133 @@ if(CMAKE_HOST_WIN32)
 
     set(OPTIONS_DBG "${OPTIONS_DBG} ${DISABLE_WHOLE_PROGRAM_OPTIMIZATION}")
     set(OPTIONS_REL "${OPTIONS_REL} ${DISABLE_WHOLE_PROGRAM_OPTIMIZATION}")
+
+    message(STATUS "Configure GN (Generate Ninja)")
+    vcpkg_configure_gn(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS_DEBUG "${OPTIONS_DBG}"
+        OPTIONS_RELEASE "${OPTIONS_REL}"
+    )
+elseif("${VCPKG_TARGET_TRIPLET}" MATCHES ".*-android")
+    message(STATUS "Matched Android")
+    if(NOT VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
+        set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${SCRIPTS}/toolchains/android.cmake")
+    endif()
+    include("${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}")
+
+    foreach(_VAR CMAKE_C_FLAGS CMAKE_C_FLAGS_DEBUG CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS
+        CMAKE_C_FLAGS_RELEASE CMAKE_CXX_FLAGS_RELEASE CMAKE_SHARED_LINKER_FLAGS)
+        string(STRIP "${${_VAR}}" ${_VAR})
+    endforeach()
+
+    set(OPTIONS_DBG "${OPTIONS_DBG} \
+        extra_cflags_c=\"${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}\" \
+        extra_cflags_cc=\"${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}\" \
+        extra_ldflags=\"${CMAKE_SHARED_LINKER_FLAGS}\"")
+
+    set(OPTIONS_REL "${OPTIONS_REL} \
+        extra_cflags_c=\"${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_RELEASE}\" \
+        extra_cflags_cc=\"${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}\" \
+        extra_ldflags=\"${CMAKE_SHARED_LINKER_FLAGS}\"")
+    
+    message(STATUS "Configure GN (Generate Ninja)")
+    string(TOLOWER ${VCPKG_CMAKE_SYSTEM_NAME} TARGET_OS)
+    vcpkg_configure_gn(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS "target_os=\"${TARGET_OS}\" \
+        target_cpu=\"${VCPKG_TARGET_ARCHITECTURE}\" \
+        android_api_level=${ANDROID_NATIVE_API_LEVEL} \
+        android_ndk_root=\"$ENV{ANDROID_NDK_HOME}\""
+        OPTIONS_DEBUG "${OPTIONS_DBG}"
+        OPTIONS_RELEASE "${OPTIONS_REL}"
+    )
+elseif("${VCPKG_TARGET_TRIPLET}" MATCHES ".*-linux")
+    message(STATUS "Matched Linux")
+    if(NOT VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
+        set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${SCRIPTS}/toolchains/linux.cmake")
+    endif()
+    include("${VCPKG_CHAINLOAD_TOOLCHAIN_FILE}")
+
+    set(OPTIONS_DBG "${OPTIONS_DBG} \
+        extra_cflags_c=\"${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}\" \
+        extra_cflags_cc=\"${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}\" \
+        extra_ldflags=\"${CMAKE_SHARED_LINKER_FLAGS}\"")
+
+    set(OPTIONS_REL "${OPTIONS_REL} \
+        extra_cflags_c=\"${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_RELEASE}\" \
+        extra_cflags_cc=\"${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}\" \
+        extra_ldflags=\"${CMAKE_SHARED_LINKER_FLAGS}\"")
+
+    message(STATUS "Configure GN (Generate Ninja)")
+    vcpkg_configure_gn(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS_DEBUG "${OPTIONS_DBG}"
+        OPTIONS_RELEASE "${OPTIONS_REL}"
+    )
 endif()
 
-vcpkg_configure_gn(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS_DEBUG "${OPTIONS_DBG}"
-    OPTIONS_RELEASE "${OPTIONS_REL}"
-)
-
+## Compile and install targets via GN and Ninja
+message(STATUS "Install GN (Generate Ninja)")
 vcpkg_install_gn(
     SOURCE_PATH "${SOURCE_PATH}"
-    TARGETS client util third_party/mini_chromium/mini_chromium/base handler:crashpad_handler
+    TARGETS 
+        client 
+        client:common 
+        util 
+        third_party/mini_chromium/mini_chromium/base 
+        handler:crashpad_handler 
+        tools:generate_dump
 )
 
-message(STATUS "Installing headers...")
+## Install headers
+message(STATUS "Installing headers to ${CURRENT_PACKAGES_DIR}/include/${PORT}")
 set(PACKAGES_INCLUDE_DIR "${CURRENT_PACKAGES_DIR}/include/${PORT}")
-function(install_headers DIR)
-    file(COPY "${DIR}" DESTINATION "${PACKAGES_INCLUDE_DIR}" FILES_MATCHING PATTERN "*.h")
-endfunction()
-install_headers("${SOURCE_PATH}/client")
-install_headers("${SOURCE_PATH}/util")
-install_headers("${SOURCE_PATH}/third_party/mini_chromium/mini_chromium/base")
-install_headers("${SOURCE_PATH}/third_party/mini_chromium/mini_chromium/build")
+file(GLOB_RECURSE HEADER_PATHS
+    RELATIVE "${SOURCE_PATH}"
+    "${SOURCE_PATH}/*.h"
+)
+foreach(HEADER_PATH ${HEADER_PATHS})
+    cmake_path(GET HEADER_PATH PARENT_PATH HEADER_FILE_DIR)
+    message(VERBOSE "Copying ${SOURCE_PATH}/${HEADER_PATH} to ${PACKAGES_INCLUDE_DIR}/${HEADER_FILE_DIR}")
+    file(COPY "${SOURCE_PATH}/${HEADER_PATH}" DESTINATION "${PACKAGES_INCLUDE_DIR}/${HEADER_FILE_DIR}")
+endforeach()
+file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/gen/build/chromeos_buildflags.h"
+    DESTINATION "${PACKAGES_INCLUDE_DIR}/build"
+)
+file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/gen/build/chromeos_buildflags.h.flags"
+    DESTINATION "${PACKAGES_INCLUDE_DIR}/build"
+)
 
-# remove empty directories
-file(REMOVE_RECURSE
-    "${PACKAGES_INCLUDE_DIR}/util/net/testdata"
-    "${PACKAGES_INCLUDE_DIR}/build/ios")
+if("${TARGET_OS}" STREQUAL "android")
+    ## Rename crashpad_handler executable to libcrashpad_handler.so so it can be bundled in future package
+    file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/crashpad_handler"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib"
+        RENAME "libcrashpad_handler.so"
+    )
+    file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/crashpad_handler"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
+        RENAME "libcrashpad_handler.so"
+    )
 
+    ## Rename generate_dump executable to libgenerate_dump.so so it can be bundled in future package
+    file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/generate_dump"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib"
+        RENAME "libgenerate_dump.so"
+    )
+    file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/generate_dump"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
+        RENAME "libgenerate_dump.so"
+    )
+endif()
+
+## Configure cmake config for find_package
 configure_file("${CMAKE_CURRENT_LIST_DIR}/crashpadConfig.cmake.in"
-        "${CURRENT_PACKAGES_DIR}/share/${PORT}/crashpadConfig.cmake" @ONLY)
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/${PORT}/build/config")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/${PORT}/util/mach/__pycache__")
+        "${CURRENT_PACKAGES_DIR}/share/${PORT}/crashpadConfig.cmake" @ONLY
+)
 
 vcpkg_copy_pdbs()
+## Install copyright file
 file(INSTALL "${SOURCE_PATH}/LICENSE"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright)
+    RENAME copyright
+)
