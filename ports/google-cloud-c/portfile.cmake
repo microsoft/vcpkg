d2f23905475e086d82fcbaf4b2de5d4837e17151
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-20 00:14:55 -0400

    [ports/google-cloud-c/portfile.cmake] First `cloudresourcemanager` implementation and use for first `project` implementation

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             d1662e3e6cf64e063c1836e0a2ba20e885851aff
-    SHA512          a940491a1590e61585c52815610a6bcb8ec2b41a8f6a270cf33b09b2053ee9bc8bb207a4b3bf02c24aecb51ae9bddf7cb5a29be427aeb69dcfafd6a68e06e235
+    REF             c185bb4dc33c3c46d2d34118f7d190b675fa7f2b
+    SHA512          461a57fb4b91daea0e81e7b8c71ae14003ab33a317dcec56960ae5071431bd8b354f4e077cd67f5e562e4055f9c17e89e608de061786771bce56ca3def10e706
     HEAD_REF        master
 )
 
@@ -11,4 +11,5 @@ vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/debug/include")
