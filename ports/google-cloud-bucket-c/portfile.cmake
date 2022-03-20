a46002ba253c12796749bef09b2d5018b93b7ee6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-20 18:57:07 -0400

    [ports/google-cloud-bucket-c/portfile.cmake] [google_cloud_bucket_c/buckets.cpp] Experiment with `std::copy` and `strdup` for `BucketC` array

--- ports/google-cloud-bucket-c/portfile.cmake
+++ ports/google-cloud-bucket-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             f0b0dc259c81c8ba889576e8cb518343de015e0d
-    SHA512          d81900bf2498d85058d88b9ecfa119abe33cd0e26fd367e3ef50b3f7909d2198046ced04de2acd984beac2e1d96b8641458b29f68612cc9c935b0f4f01ca762b
+    REF             f1b7b6ac389935d664e901a58a1a61eb7a977e92
+    SHA512          ca70ff9180926310e742ed2c069a546bf467b41ae4b7fc8c2bec133287628b5839fa38c34adfce2ec36cf45e7332120257bcdfb0bf29574d798b18a97c32117b
     HEAD_REF        master
 )
 
