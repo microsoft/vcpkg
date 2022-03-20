10f713f05828ed266b28f92af3a8b95fe80ea978
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-20 13:38:14 -0400

    [ports/google-cloud-c/portfile.cmake] [google_cloud_c/userinfo/userinfo.c] Add missing URL

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             5dd38428055f2856cce90444c051fe1cc899a85a
-    SHA512          51bbcb5887e869a43e8c62ab00e2c03ebfe28133c162d21df431818339c0bd2a7ffbe3c4c58b6a0395202dcdcea5103ccb766f9a6c500839e5b6ff62cce71d24
+    REF             bc4c64aa19767b8f15d4553debc44f8070fbffaf
+    SHA512          63e53dbf4b39f95a7d716185542801c30fa8bfe3ccecef152a241045e0217a61f862103dc674beda80befe5cdf33cc8c84b784270c39e129562282c3aa63107e
     HEAD_REF        master
 )
 
