d6729f262dc81d9fa6461dfffbf591195ba5cae2
PanForPancakes <87640847+PanForPancakes@users.noreply.github.com>
2022-03-21 16:39:59 +0400

    Update ports/color-console/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/color-console/portfile.cmake
+++ ports/color-console/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO imfl/color-console
-    REF 745f57141e6322e20cdda59a74ab1e00d107ade1 #v1.0.0
+    REF 745f57141e6322e20cdda59a74ab1e00d107ade1
     SHA512 d4d919eb211f2981daf6b9af93262fc8ac89bac0db755ba30b31d6aa1b981e5383fb23ef95d1ff004606d138fee630fd790c72e92440b684306c6780750bd9fd
     HEAD_REF master
 )
