f799e7d5f22b8b3ac015d341d83852c6000d0a3a
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-03-24 00:15:09 +0100

    wip commit

---
+++ ports/vcpkg-tool-flang-llvm/portfile.cmake
@@ -0,0 +1,233 @@
+set(LLVM_VERSION "13.0.0")
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO  flang-compiler/classic-flang-llvm-project
+    REF 0254365051067b50372914150a004048c5cbce54
+    SHA512 c412c1bfb93803568e83be3777491b1b99be9ae9338b3d4fd87b0422ad428b9e45ebc7488f668f21957930c07189ee72567c5661964e5458d0d0cf37fe1c0608
+    HEAD_REF release_13x
+    PATCHES
+        0002-fix-install-paths.patch    # This patch fixes paths in ClangConfig.cmake, LLVMConfig.cmake, LLDConfig.cmake etc.
+        0003-fix-openmp-debug.patch
+        0004-fix-dr-1734.patch
+        0005-fix-tools-path.patch
+        0007-fix-compiler-rt-install-path.patch
+        0009-fix-tools-install-path.patch
+        0010-fix-libffi.patch
+        0011-fix-libxml2.patch
+)
+
+# LLVM generates CMake error due to Visual Studio version 16.4 is known to miscompile part of LLVM.
+# LLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON disables this error.
+# See https://developercommunity.visualstudio.com/content/problem/845933/miscompile-boolean-condition-deduced-to-be-always.html
+# and thread "[llvm-dev] Longstanding failing tests - clang-tidy, MachO, Polly" on llvm-dev Jan 21-23 2020.
+list(APPEND FEATURE_OPTIONS
+    -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON
+)
+
+# Force enable or disable external libraries
+set(llvm_external_libraries
+    zlib
+    libxml2
+)
+foreach(external_library IN LISTS llvm_external_libraries)
+    string(TOLOWER "enable-${external_library}" feature_name)
+    string(TOUPPER "LLVM_ENABLE_${external_library}" define_name)
+    list(APPEND FEATURE_OPTIONS
+            -D${define_name}=FORCE_ON
+        )
+endforeach()
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_DIR ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path(${PYTHON3_DIR})
+
+set(VCPKG_BUILD_TYPE release) # Only need release tools
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/llvm"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        ${CROSS_OPTIONS}
+        -DLLVM_INCLUDE_EXAMPLES=OFF
+        -DCMAKE_INSTALL_BINDIR=tools/llvm-flang/bin
+        -DCMAKE_INSTALL_INCLUDEDIR=tools/llvm-flang/include
+        -DCMAKE_INSTALL_LIBDIR=tools/llvm-flang/lib
+        -DLLVM_BUILD_EXAMPLES=OFF
+        -DLLVM_INCLUDE_TESTS=OFF
+        -DLLVM_INCLUDE_BENCHMARKS=OFF
+        -DLLVM_BUILD_TESTS=OFF
+        -DLLVM_BUILD_UTILS=OFF
+        -DLLVM_INSTALL_UTILS=ON
+        
+        -DLLVM_TOOL_BUGPOINT_BUILD=OFF
+        -DLLVM_TOOL_BUGPOINT_PASSES_BUILD=OFF
+        -DLLVM_TOOL_DSYMÙTIL_BUILD=OFF
+        -DLLVM_TOOL_GOLD_BUILD=OFF
+        -DLLVM_TOOL_LLC_BUILD=OFF
+        -DLLVM_TOOL_LLVM_AS_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_BCANALYZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CAT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CFI_VERIFY_BUILD=OFF
+        -DLLVM_TOOL_LLVM_COV_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CVTRES_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CXXDUMP_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CXXFILT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_CXXMAP_BUILD=OFF
+        -DLLVM_TOOL_LLVM_C_TEST_BUILD=OFF
+        -DLLVM_TOOL_LLVM_DIFF_BUILD=OFF
+        -DLLVM_TOOL_LLVM_DIS_BUILD=OFF
+        -DLLVM_TOOL_LLVM_DWARFDUMP_BUILD=ON
+        -DLLVM_TOOL_LLVM_DWP_BUILD=OFF
+        -DLLVM_TOOL_LLVM_EXEGESIS_BUILD=OFF
+        -DLLVM_TOOL_LLVM_EXTRACT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_GO_BUILD=OFF
+        -DLLVM_TOOL_LLVM_GSYMUTIL_BUILD=OFF
+        -DLLVM_TOOL_LLVM_IFS_BUILD=OFF
+        -DLLVM_TOOL_LLVM_ISEL_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_ITANIUM_DEMANGLE_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_JITLINK_BUILD=OFF
+        -DLLVM_TOOL_LLVM_JITLISTENER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_LIBTOOL_DARWIN_BUILD=OFF
+        -DLLVM_TOOL_LLVM_LIPO_BUILD=OFF
+        -DLLVM_TOOL_LLVM_MCA_BUILD=OFF
+        -DLLVM_TOOL_LLVM_MC_ASSEMBLE_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_MC_BUILD=OFF
+        -DLLVM_TOOL_LLVM_MC_DISASSEMBLE_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_ML_BUILD=OFF
+        -DLLVM_TOOL_LLVM_MODEXTRACT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_OPT_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_OPT_REPORT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_PROFDATA_BUILD=OFF
+        -DLLVM_TOOL_LLVM_PROFGEN_BUILD=OFF
+        -DLLVM_TOOL_LLVM_REDUCE_BUILD=OFF
+        -DLLVM_TOOL_LLVM_RTDYLD_BUILD=OFF
+        -DLLVM_TOOL_LLVM_RUST_DEMANGLE_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SHLIB_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SIM_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SIZE_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SPECIAL_CASE_LIST_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SPLIT_BUILD=OFF
+        -DLLVM_TOOL_LLVM_STRESS_BUILD=OFF
+        -DLLVM_TOOL_LLVM_STRINGS_BUILD=OFF
+        -DLLVM_TOOL_LLVM_SYMBOLIZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_TAPI_DIFF_BUILD=OFF
+        -DLLVM_TOOL_LLVM_UNDNAME_BUILD=OFF
+        -DLLVM_TOOL_LLVM_XRAY_BUILD=OFF
+        -DLLVM_TOOL_LLVM_YAML_NUMERIC_PARSER_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_YAML_PARSER_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_LLVM_LTO2_BUILD=OFF
+        -DLLVM_TOOL_LLVM_LTO_BUILD=OFF
+        -DLLVM_TOOL_REMARKS_SHLIB_BUILD=OFF
+        -DLLVM_TOOL_SANCOV_BUILD=OFF
+        -DLLVM_TOOL_SANSTATS_BUILD=OFF
+        -DLLVM_TOOL_SPLIT_FILE_BUILD=OFF
+        -DLLVM_TOOL_VERIFY_USELISTORDER_BUILD=OFF
+        -DLLVM_TOOL_XCODE_TOOLCHAIN_BUILD=OFF
+        -DLLVM_TOOL_YAML2OBJ_BUILD=OFF
+        -DLLVM_TOOL_OBJ2YAML_BUILD=OFF
+        -DLLVM_TOOL_OPT_BUILD=OFF
+        -DLLVM_TOOL_OPT_VIEWER_BUILD=OFF
+        -DLLVM_TOOL_VFABI_DEMANGLE_FUZZER_BUILD=OFF
+        -DLLVM_TOOL_DSYMUTIL=OFF
+        -DLLVM_TOOL_LLI_BUILD=OFF
+        -DLLVM_TOOL_MLIR_BUILD=ON
+        -DCLANG_TOOL_AMDGPU_ARCH_BUILD=OFF
+        -DCLANG_TOOL_APINOTES_TEST_BUILD=OFF
+        -DCLANG_TOOL_ARCMT_TEST_BUILD=OFF
+        -DCLANG_TOOL_CLANG_CHECK_BUILD=OFF
+        -DCLANG_TOOL_CLANG_DIFF_BUILD=OFF
+        -DCLANG_TOOL_CLANG_EXTDEF_MAPPING_BUILD=OFF
+        -DCLANG_TOOL_CLANG_FORMAT_BUILD=OFF
+        -DCLANG_TOOL_CLANG_FORMAT_VS_BUILD=OFF
+        -DCLANG_TOOL_CLANG_FUZZER_BUILD=OFF
+        -DCLANG_TOOL_CLANG_IMPORT_TEST_BUILD=OFF
+        -DCLANG_TOOL_CLANG_OFFLOAD_BUNDLER_BUILD=OFF
+        -DCLANG_TOOL_CLANG_OFFLOAD_WRAPPER_BUILD=OFF
+        -DCLANG_TOOL_CLANG_REFACTOR_BUILD=OFF
+        -DCLANG_TOOL_CLANG_RENAME_BUILD=OFF
+        -DCLANG_TOOL_CLANG_REPL_BUILD=OFF
+        -DCLANG_TOOL_CLANG_SCAN_DEPS_BUILD=OFF
+        -DCLANG_TOOL_CLANG_SHLIB_BUILD=OFF
+        -DCLANG_TOOL_C_ARCMT_TEST_BUILD=OFF
+        -DCLANG_TOOL_C_INDEX_TEST_BUILD=OFF
+        -DCLANG_TOOL_DIAGTOOL_BUILD=OFF
+        -DCLANG_TOOL_SCAN_BUILD_BUILD=OFF
+        -DCLANG_TOOL_SCAN_BUILD_PY_BUILD=OFF
+        -DCLANG_TOOL_SCAN_VIEW_BUILD=OFF
+        #-DLLDB_ENABLE_CURSES=OFF
+        # Force TableGen to be built with optimization. This will significantly improve build time.
+        -DLLVM_OPTIMIZED_TABLEGEN=ON
+        #"-DLLVM_ENABLE_PROJECTS=${LLVM_ENABLE_PROJECTS}"
+        -DLLVM_ENABLE_CLASSIC_FLANG=ON 
+        -DLLVM_ENABLE_PROJECTS="clang\;clang-tools-extra\;openmp"
+        "-DLLVM_TARGETS_TO_BUILD=X86"
+        
+        #-DPACKAGE_VERSION=${LLVM_VERSION}
+        # Limit the maximum number of concurrent link jobs to 1. This should fix low amount of memory issue for link.
+        # Disable build LLVM-C.dll (Windows only) due to doesn't compile with CMAKE_DEBUG_POSTFIX
+        -DLLVM_BUILD_LLVM_C_DYLIB=OFF
+        # Path for binary subdirectory (defaults to 'bin')
+        -DLLVM_TOOLS_INSTALL_DIR=tools/llvm-flang
+)
+
+vcpkg_cmake_install(ADD_BIN_TO_PATH)
+
+function(llvm_cmake_package_config_fixup package_name)
+    cmake_parse_arguments("arg" "DO_NOT_DELETE_PARENT_CONFIG_PATH" "FEATURE_NAME" "" ${ARGN})
+    if(NOT DEFINED arg_FEATURE_NAME)
+        set(arg_FEATURE_NAME ${package_name})
+    endif()
+    if("${arg_FEATURE_NAME}" STREQUAL "${PORT}" OR "${arg_FEATURE_NAME}" IN_LIST FEATURES)
+        set(args)
+        list(APPEND args PACKAGE_NAME "${package_name}")
+        if(arg_DO_NOT_DELETE_PARENT_CONFIG_PATH)
+            list(APPEND args "DO_NOT_DELETE_PARENT_CONFIG_PATH")
+        endif()
+        vcpkg_cmake_config_fixup(${args})
+        file(INSTALL "${SOURCE_PATH}/${package_name}/LICENSE.TXT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${package_name}" RENAME copyright)
+        if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/${package_name}_usage")
+            file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/${package_name}_usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${package_name}" RENAME usage)
+        endif()
+    endif()
+endfunction()
+
+llvm_cmake_package_config_fixup("clang" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("flang" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("lld" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("mlir" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("polly" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("ParallelSTL" FEATURE_NAME "pstl" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+llvm_cmake_package_config_fixup("llvm")
+
+set(empty_dirs)
+
+if("clang-tools-extra" IN_LIST FEATURES)
+    list(APPEND empty_dirs "${CURRENT_PACKAGES_DIR}/include/clang-tidy/plugin")
+endif()
+
+
+if(empty_dirs)
+    foreach(empty_dir IN LISTS empty_dirs)
+        if(NOT EXISTS "${empty_dir}")
+            message(SEND_ERROR "Directory '${empty_dir}' is not exist. Please remove it from the checking.")
+        else()
+            file(GLOB_RECURSE files_in_dir "${empty_dir}/*")
+            if(files_in_dir)
+                message(SEND_ERROR "Directory '${empty_dir}' is not empty. Please remove it from the checking.")
+            else()
+                file(REMOVE_RECURSE "${empty_dir}")
+            endif()
+        endif()
+    endforeach()
+endif()
+
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/llvm-flang")
+
+# LLVM still generates a few DLLs in the static build:
+# * libclang.dll
+# * LTO.dll
+# * Remarks.dll
+set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
---
+++ ports/vcpkg-tool-flang/portfile.cmake
@@ -0,0 +1,42 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO  flang-compiler/flang
+    REF 59c43c4d99ade23d103aaf5299016a1666ceb2e1
+    SHA512 467c3a977d5a207a0115ece5db070c9b49c1b57595155d477d25fe0b49453facbc90566b6e5ae7a48038b59bda7481b4ed6cd969628f3019076864be74a3d6a3
+)
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_DIR ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path(${PYTHON3_DIR})
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/runtime/libpgmath"
+    OPTIONS
+        -DLLVM_TARGETS_TO_BUILD=X86
+        -DFLANG_LLVM_EXTENSIONS=ON
+        "-DLLVM_CONFIG=${CURRENT_INSTALLED_DIR}/tools/llvm/llvm-config.exe"
+        "-DLLVM_CMAKE_PATH=${CURRENT_INSTALLED_DIR}/share/llvm-flang"
+        #"-DLIBPGMATH_LLVM_LIT_EXECUTABLE=${CURRENT_INSTALLED_DIR}/tools/llvm-flang/llvm-lit.py"
+        "-DCMAKE_C_COMPILER=${CURRENT_INSTALLED_DIR}/tools/llvm-flang/clang-cl.exe"
+        "-DCMAKE_CXX_COMPILER=${CURRENT_INSTALLED_DIR}/tools/llvm-flang/clang-cl.exe"
+)
+vcpkg_cmake_install(ADD_BIN_TO_PATH)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DLLVM_TARGETS_TO_BUILD=X86
+        -DFLANG_LLVM_EXTENSIONS=ON
+        "-DLLVM_CONFIG=${CURRENT_INSTALLED_DIR}/tools/llvm/llvm-config.exe"
+        "-DLLVM_CMAKE_PATH=${CURRENT_INSTALLED_DIR}/share/llvm-flang"
+        -DCMAKE_Fortran_COMPILER=${CURRENT_PACKAGES_DIR}/bin/flang
+        -DCMAKE_Fortran_COMPILER_ID=Flang
+        "-DLIBPGMATH_LLVM_LIT_EXECUTABLE=${CURRENT_INSTALLED_DIR}/tools/llvm/llvm-lit.py"
+)
+vcpkg_cmake_install(ADD_BIN_TO_PATH)
+
+
+set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
