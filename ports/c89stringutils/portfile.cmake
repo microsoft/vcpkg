b55be7b8d5989b9bca0c80d1435246794ab1ef19
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-23 10:16:59 -0400

    [ports/c89stringutils/portfile.cmake] [c89stringutils/c89stringutils_string_extras.h] Add `concat_asprintf`; guard implementations better

--- ports/c89stringutils/portfile.cmake
+++ ports/c89stringutils/portfile.cmake
@@ -1,15 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            offscale/c89stringutils
-    REF             375c87aaf50a945b17a76727f3314eb217897caf
-    SHA512          395d942a133209daf510094814830e35daf2047c35b0ff15b17051d7095e4598fd830e0e7f763cac6929b867ff3b0c03c5350c4c3cfc68ed98b69c9c68c04be0
+    REF             43bd8d948b198dbc5ec9571f06ecb6fc40f1609b
+    SHA512          a5506ac5c233aadef8d703a0dfafe4152965699f3922671e74b9ae9635b5e73b1e9e6d305a0f6ce3d725dc4dadc64fae677b5349544c7055f5130d47c20bac05
     HEAD_REF        master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DBUILD_TESTS=OFF"
+        "-DBUILD_TESTING=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
