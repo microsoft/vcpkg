d742537157f1d6a742f8c8f46541993a549e60dc
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-24 12:00:35 -0400

    [ports/google-cloud-c] disable test building, update version, [google_cloud_c/*] Major refactor of directory structure to enable new #include namespace and INTERFACE target;

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,12 +1,16 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             be86523be428c0241e54dddbb4a1f4d1ef516173
-    SHA512          7cdc6c4d0812d1a23e0a934684d279ae6a79a3c03e7276037bec1ec985a60c4938a34e510b555f5ee2bc4a71675b68ca292e60ed21f5bf437799eaff7d2c2a79
+    REF             256cc555b71277b639b250c83e35d439a8b18f56
+    SHA512          7424a0f58bc6b2c98b0f2e4c223fb7ce3015ed3e1a7485e91c1c30c967892284ae674b2fc4441dce5caa296e5ccee24ef141b31c6ed9833e3d076700076e944c
     HEAD_REF        master
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
