7105d81ad31d775390be8d7859f7006533de3538
JackBoosY <yuzaiyang@beyondsoft.com>
2022-03-25 02:06:53 -0700

    [onnxruntime] Re-factory install process

--- ports/onnxruntime-gpu/portfile.cmake
+++ ports/onnxruntime-gpu/portfile.cmake
@@ -1,41 +1,73 @@
-vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-
-set(VERSION 1.5.1)
-
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/microsoft/onnxruntime/releases/download/v1.5.1/onnxruntime-win-x64-gpu-1.5.1.zip"
-    FILENAME "onnxruntime-win-x64-gpu-1.5.1.zip"
-    SHA512 893dbed1196b5c9730744dc5566cd3ad8ec677cbea04dd0572efc9a8b3563d3f1cbf40d0dea3d624d9451dc33272c0ae44d53d6deee6f249fa2520e60718ee52
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    NO_REMOVE_ONE_LEVEL
-    REF ${VERSION}
+    REPO microsoft/onnxruntime
+    REF  0d9030e79888d1d5828730b254fedc53c7b640c1 # v1.10.0
+    SHA512 502b68fae7d2e8441ec26253a9e0cdcf970ab2b61efecee7d964e9880e59d657971a82a666710944617c86d18fa99c2cb9640fcd15f63d05b2617b562a5bdb2f
+    HEAD_REF master
+    PATCHES fix-dependencies.patch
 )
 
-file(MAKE_DIRECTORY
-        ${CURRENT_PACKAGES_DIR}/include
-        ${CURRENT_PACKAGES_DIR}/lib
-        ${CURRENT_PACKAGES_DIR}/bin
-        ${CURRENT_PACKAGES_DIR}/debug/lib
-        ${CURRENT_PACKAGES_DIR}/debug/bin
-    )
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 
-file(COPY
-        ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/include
-        DESTINATION ${CURRENT_PACKAGES_DIR}
-    )
+if ("${VCPKG_HOST_TRIPLET}" STREQUAL "${VCPKG_TARGET_TRIPLET}")
+    set(BUILD_HOST ON)
+    set(CROSS_BUILD OFF)
+else()
+    set(BUILD_HOST OFF)
+    set(CROSS_BUILD ON)
+endif()
+string(COMPARE EQUAL "${VCPKG_HOST_TRIPLET}" "${VCPKG_TARGET_TRIPLET}" BUILD_HOST)
 
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-file(COPY ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/lib/onnxruntime.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/cmake"
+    OPTIONS
+        -Donnxruntime_BUILD_SHARED_LIB=${BUILD_SHARED}
+        -Donnxruntime_BUILD_FOR_NATIVE_MACHINE=${BUILD_HOST}
+        -Donnxruntime_CROSS_COMPILING=${CROSS_BUILD}
+        -Donnxruntime_RUN_ONNX_TESTS=OFF
+        -Donnxruntime_GENERATE_TEST_REPORTS=OFF
+        -Donnxruntime_ENABLE_STATIC_ANALYSIS=OFF
+        -Donnxruntime_ENABLE_PYTHON=OFF
+        -Donnxruntime_ENABLE_MEMLEAK_CHECKER=OFF
+        -Donnxruntime_USE_CUDA=OFF
+        -Donnxruntime_ENABLE_CUDA_LINE_NUMBER_INFO=OFF
+        -Donnxruntime_USE_NNAPI_BUILTIN=OFF
+        -Donnxruntime_DEV_MODE=OFF
+        -Donnxruntime_BUILD_UNIT_TESTS=OFF
+        -Donnxruntime_BUILD_CSHARP=OFF
+        -Donnxruntime_BUILD_OBJC=OFF
+        -Donnxruntime_USE_PREINSTALLED_EIGEN=ON
+        -Donnxruntime_BUILD_BENCHMARKS=OFF
+        -Donnxruntime_USE_LLVM=OFF
+        -Donnxruntime_USE_AVX=OFF
+        -Donnxruntime_USE_AVX2=OFF
+        -Donnxruntime_USE_AVX512=OFF
+        -Donnxruntime_USE_OPENMP=OFF
+        -Donnxruntime_BUILD_APPLE_FRAMEWORK=OFF
+        -Donnxruntime_ENABLE_MICROSOFT_INTERNAL=OFF
+        -Donnxruntime_USE_TENSORRT=OFF
+        -Donnxruntime_ENABLE_LTO=ON
+        -Donnxruntime_DEBUG_NODE_INPUTS_OUTPUTS=OFF
+        -Donnxruntime_USE_ROCM=OFF # AMD GPU SUPPORT
+        -Donnxruntime_PREFER_SYSTEM_LIB=ON
+        -Donnxruntime_MINIMAL_BUILD=ON
+        -Donnxruntime_EXTENDED_MINIMAL_BUILD=OFF
+        -Donnxruntime_MINIMAL_BUILD_CUSTOM_OPS=OFF
+        -Donnxruntime_DISABLE_EXTERNAL_INITIALIZERS=OFF
+        -Donnxruntime_USE_VALGRIND=OFF
+        -Donnxruntime_RUN_MODELTEST_IN_DEBUG_MODE=OFF
+        -Donnxruntime_FUZZ_TEST=OFF
+        -Donnxruntime_USE_NCCL=OFF
+        -Donnxruntime_USE_MPI=OFF
+        -Donnxruntime_ENABLE_BITCODE=OFF
+        -Donnxruntime_BUILD_OPSCHEMA_LIB=ON
+        -Donnxruntime_USE_EXTENSIONS=OFF
+        
+)
 
-# # Handle copyright
-file(INSTALL ${SOURCE_PATH}/onnxruntime-win-x64-gpu-1.5.1/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
