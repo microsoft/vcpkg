1a0d93a726dd5002cf6acc0c71df80a002af82e3
luncliff <luncliff@gmail.com>
2022-03-25 23:49:46 +0900

    [tensorpipe] use mainstream PR
    * rename existing patch files
    * update versions JSON

--- ports/tensorpipe/portfile.cmake
+++ ports/tensorpipe/portfile.cmake
@@ -1,12 +1,19 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+vcpkg_download_distfile(INSTALL_PACKAGE_CONFIG_PATCH
+    URLS "https://patch-diff.githubusercontent.com/raw/pytorch/tensorpipe/pull/435.diff"
+    FILENAME tensorpipe-pr-435.patch
+    SHA512 149539467ddd39feb6e715bf483d67954338998cbbcfef65de5a85831af902165f9347cd097fa8e82b10b2b8dbc388fcfe42664eeaf5de1954ae885f129583ed
+)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/tensorpipe
     REF 52791a2fd214b2a9dc5759d36725909c1daa7f2e
     SHA512 1e5faf17a7236c5506c08cb28be16069b11bb929bbca64ed9745ce4277d46739186ab7d6597da7437d90ed2d166d4c37ef2f3bceabe8083ef3adbb0e8e5f227e
     PATCHES
-        fix-cmakelists.patch
+        "${INSTALL_PACKAGE_CONFIG_PATCH}"
+        use-vcpkg.patch
         support-test.patch
 )
 
@@ -24,7 +31,7 @@ if("pybind11" IN_LIST FEATURES)
 endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DTP_ENABLE_SHM=${VCPKG_TARGET_IS_LINUX}
@@ -38,9 +45,9 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/Tensorpipe")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include
-                    ${CURRENT_PACKAGES_DIR}/debug/share
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
