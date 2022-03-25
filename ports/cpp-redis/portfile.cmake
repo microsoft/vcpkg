de46e8993df51948e688bd48b3389e4074566208
ParticleG <particle_g@outlook.com>
2022-03-25 13:20:30 +0800

    Apply patch: ‘sleep_for’ is not a member of ‘std::this_thread’

--- ports/cpp-redis/portfile.cmake
+++ ports/cpp-redis/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 4.3.1
     SHA512 abf372542c53f37f504b3211b840b100d07a8f4b2e7f5584cc7550ab16ed617838e2df79064374c7a409458d8567f4834686318ea3a40249c767e36c744c7a47
     HEAD_REF master
+    PATCHES 
+        "fix-sleep_for.patch"
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/tacopie/CMakeLists.txt DESTINATION ${SOURCE_PATH}/tacopie)
