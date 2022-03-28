c185970b1d6bab88dd8a200207f88ca86d431abb
Mathis Logemann <mathisloge@gmail.com>
2022-03-28 19:07:11 +0200

    spriv

--- ports/spirv-tools/portfile.cmake
+++ ports/spirv-tools/portfile.cmake
@@ -4,12 +4,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KhronosGroup/SPIRV-Tools
-    REF v2021.1
-    SHA512 e8478eacb86415f75a1e5b3f66a0508b01a9f7e9d8b070eb0329ca56be137f5543dd42125a1033cb8552c01f46e11affd7fda866231b3742c66de9b4341930d5
-    PATCHES
-        cmake-install.patch
-        install-config-typo.patch
-        0001-don-t-use-MP4.patch
+    REF v2022.1
+    SHA512 19ff4386c74c15f9b870d5348b76c7d643da5bf1637b1faed153d9ec9f00de941a83a22782f169b4aa5001c715721937c3bb3bc07541a60e503a0455a1d2287e
 )
 
 vcpkg_find_acquire_program(PYTHON3)
@@ -25,22 +21,23 @@ else()
     set(SKIP_EXECUTABLES OFF)
 endif()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DSPIRV-Headers_SOURCE_DIR=${CURRENT_INSTALLED_DIR}
+        -DSPIRV-Headers_SOURCE_DIR="${CURRENT_INSTALLED_DIR}"
         -DSPIRV_WERROR=OFF
         -DSPIRV_SKIP_EXECUTABLES=${SKIP_EXECUTABLES} # option SPIRV_SKIP_TESTS follows this value
         -DENABLE_SPIRV_TOOLS_INSTALL=${TOOLS_INSTALL}
         -DSPIRV_TOOLS_BUILD_STATIC=ON
+        -DENABLE_SPIRV_TOOLS_INSTALL=ON
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/SPIRV-Tools TARGET_PATH share/SPIRV-Tools) # the directory name is capitalized as opposed to the package name
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/SPIRV-Tools-link TARGET_PATH share/SPIRV-Tools-link)
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/SPIRV-Tools-opt TARGET_PATH share/SPIRV-Tools-opt)
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/SPIRV-Tools-reduce TARGET_PATH share/SPIRV-Tools-reduce)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH SPIRV-Tools/cmake PACKAGE_NAME SPIRV-Tools) # the directory name is capitalized as opposed to the package name
+vcpkg_cmake_config_fixup(CONFIG_PATH SPIRV-Tools-link/cmake PACKAGE_NAME SPIRV-Tools-link)
+vcpkg_cmake_config_fixup(CONFIG_PATH SPIRV-Tools-lint/cmake PACKAGE_NAME SPIRV-Tools-lint)
+vcpkg_cmake_config_fixup(CONFIG_PATH SPIRV-Tools-opt/cmake PACKAGE_NAME Tools-opt)
+vcpkg_cmake_config_fixup(CONFIG_PATH SPIRV-Tools-reduce/cmake PACKAGE_NAME SPIRV-Tools-reduce)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin") # only static linkage, i.e. no need to preserve .dll/.so files
@@ -50,8 +47,20 @@ file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/SPIRV-Tools-shared.dll")
 file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/libSPIRV-Tools-shared.so")
 file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/lib/libSPIRV-Tools-shared.so")
 if(TOOLS_INSTALL)
-    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
+    vcpkg_copy_tools(TOOL_NAMES spirv-as spirv-cfg spirv-dis spirv-link spirv-lint spirv-opt spirv-reduce spirv-val AUTO_CLEAN)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 endif()
+file(REMOVE_RECURSE 
+    "${CURRENT_PACKAGES_DIR}/debug/SPIRV-Tools" 
+    "${CURRENT_PACKAGES_DIR}/debug/SPIRV-Tools-link" 
+    "${CURRENT_PACKAGES_DIR}/debug/SPIRV-Tools-lint" 
+    "${CURRENT_PACKAGES_DIR}/debug/SPIRV-Tools-opt" 
+    "${CURRENT_PACKAGES_DIR}/debug/SPIRV-Tools-reduce" 
+    "${CURRENT_PACKAGES_DIR}/SPIRV-Tools" 
+    "${CURRENT_PACKAGES_DIR}/SPIRV-Tools-link" 
+    "${CURRENT_PACKAGES_DIR}/SPIRV-Tools-lint" 
+    "${CURRENT_PACKAGES_DIR}/SPIRV-Tools-opt" 
+    "${CURRENT_PACKAGES_DIR}/SPIRV-Tools-reduce"
+)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
