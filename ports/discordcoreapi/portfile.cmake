226385add2844787dc74a37b4ed6d5d4941a7098
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-03-30 08:13:35 -0400

    Updates!

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -1,14 +1,18 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 13398db46311e0fda4a5481bc6e5b8315aa5a166
-	SHA512 4efa8664a10b296e7a3bd2f99049a5ddd1d8571ed507c74583d54eb4c9d22d4d84961baac04f9be91e66435a9480f0d7c6a67d4da854a42bc12d0b937c62854b
+	REF 3011861d7af1c7c171988a3f0d6300572634340b
+	SHA512 c70b4073573ff69058831d5a494aed0ae6a120431a30250930897abe37e964ace5a94418f30a063c21a97a2bf52ceefd3e8925ef3998e885da6560c71184f4da
 	HEAD_REF main
 )
 
 vcpkg_cmake_configure(
 	SOURCE_PATH "${SOURCE_PATH}"
 )
+
+if ("${VCPKG_TARGET_TRIPLET}" STREQUAL "x64-linux")
+	set(VCPKG_CXX_FLAGS "-std=gnu++2a" "-fcoroutines" "-fconcepts" "-lpthread" "-O3")
+endif()
 
 vcpkg_cmake_install()
 
