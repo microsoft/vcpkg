a8b8a4ed2aa4f54364a028a798efc93f7baa0e02
Alison Wu <aliwu@microsoft.com>
2022-03-30 17:49:53 -0700

    Avoid deprecated helpers

--- ports/tinycbor/portfile.cmake
+++ ports/tinycbor/portfile.cmake
@@ -8,8 +8,8 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
