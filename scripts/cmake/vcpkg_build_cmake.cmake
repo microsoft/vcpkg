1e15675c8a8217d01015fb7420d19310ca103ed9
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-03-31 22:28:00 +0200

    add Unix Makefiles to list of valid generators for bootstraping ninja

--- scripts/cmake/vcpkg_build_cmake.cmake
+++ scripts/cmake/vcpkg_build_cmake.cmake
@@ -64,8 +64,12 @@ function(vcpkg_build_cmake)
         vcpkg_list(SET parallel_param "/m")
     elseif("${Z_VCPKG_CMAKE_GENERATOR}" STREQUAL "NMake Makefiles")
         # No options are currently added for nmake builds
+    elseif("${Z_VCPKG_CMAKE_GENERATOR}" STREQUAL "Unix Makefiles")
+        vcpkg_list(SET build_param "VERBOSE=1")
+        vcpkg_list(SET parallel_param "-j${VCPKG_CONCURRENCY}")
+        vcpkg_list(SET no_parallel_param "-j1")
     else()
-        message(FATAL_ERROR "Unrecognized GENERATOR setting from vcpkg_configure_cmake(). Valid generators are: Ninja, Visual Studio, and NMake Makefiles")
+        message(FATAL_ERROR "Unrecognized GENERATOR setting from vcpkg_configure_cmake(). Valid generators are: 'Ninja', 'Visual Studio', 'Unix Makefiles' and 'NMake Makefiles'")
     endif()
 
     vcpkg_list(SET target_param)
