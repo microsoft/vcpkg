8c558c5585e9b59379a6da9fe735cc57f4d8c454
Kai Pastor <dg0yt@darc.de>
2022-03-31 05:59:13 +0200

    Adjust webp lib names in skia, qt5-imageformats

--- ports/skia/portfile.cmake
+++ ports/skia/portfile.cmake
@@ -99,8 +99,8 @@ replace_skia_dep(icu "/include" "icuuc,icuucd" "icuuc" "U_USING_ICU_NAMESPACE=0"
 replace_skia_dep(libjpeg-turbo "/include" "jpeg,jpegd;turbojpeg,turbojpegd" "jpeg;turbojpeg" "")
 replace_skia_dep(libpng "/include" "libpng16,libpng16d" "libpng16" "")
 replace_skia_dep(libwebp "/include"
-    "webp,webpd;webpdemux,webpdemuxd;webpdecoder,webpdecoderd;libwebpmux,libwebpmuxd"
-    "webp;webpdemux;webpdecoder;libwebpmux" "")
+    "webp,webpd;webpdemux,webpdemuxd;webpdecoder,webpdecoderd;webpmux,webpmuxd"
+    "webp;webpdemux;webpdecoder;webpmux" "")
 replace_skia_dep(zlib "/include" "z,zlib,zlibd" "z,zlib" "")
 
 set(OPTIONS "\
