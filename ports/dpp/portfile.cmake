d753b5a2826ffed8f39f33a2d5f3cb4d1bb15040
brain <craigedwards@brainbox.cc>
2022-04-01 22:26:41 +0100

    Updating version database.

---
+++ ports/dpp/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO brainboxdotcc/DPP
+	REF 037647445048c9e4bf60621ca07761242452d417
+	SHA512 4b1725b094fc721ce295dae7ba661275d2e31769493c5f5ce242c6727be4bdffad993ca48916d11960e44f6402196cb08151e713d5a7618a6436093bc170812a
+	HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+if (UNIX)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
+endif()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
