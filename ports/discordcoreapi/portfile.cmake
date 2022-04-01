18a6f05d075576ec7cc2e599abd06ff0078d39b6
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-04-01 04:09:10 -0400

    Added DiscordCoreAPIConfig.cmake

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF de0cfa6402b6e22fd73cf5df0fa946f028455c90
-	SHA512 2f7f8a00ca7334b59d18086d7d518e2e8337bfd0e974dc55885a31e2b7f26aece6f4c9f753153963c716f9cf318ed48c038dc36b65d0d6ee706f8b81becf22a9
+	REF a7ead38372540c146dd83c60c06d11e4a59a206a
+	SHA512 78a4f2e091f2c71ea4e20f2524ae12314c4ffb7dc4b9aa2e8429dfb23271db30f9900e34d98d8cd28efbb81205f94622d8695cf9e8f96a2758275f182ad42ba4
 	HEAD_REF main
 )
 
@@ -12,7 +12,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(
 	INSTALL "${SOURCE_PATH}/License"
