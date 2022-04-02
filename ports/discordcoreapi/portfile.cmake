7f539967bddb7f26ec6ded5a4fca0f9391c76334
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-04-02 03:24:01 -0400

    Update ports/discordcoreapi/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -1,3 +1,7 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING "Discordcoreapi only supports g++ 11 on linux.")
+endif()
+
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
