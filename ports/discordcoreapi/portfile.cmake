637ab134a0a91775f33ff2018580d94c4f0a8a4e
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-04-03 06:40:48 -0400

    Fixed compilation issues.

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -5,8 +5,8 @@ endif()
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 8d2bbb06e5d4f285d315bc935f1e2d74f2e1d957
-	SHA512 a6c50067590b691ac2b032a8fbeca3a9ef4c4abacf1dbc0b190e9e110adbd0e17197ef35250d5240ee4ccea4439f7c890d2fd3a8660cf38afda27dd071a75034
+	REF 31ce81f8583c1377bedc7131d303684e5c0af5fd
+	SHA512 a9340d53d4c31e33aa9ab12aa0ce8b1aa997813a3609eac37906a42cbcbba4ba714c99b5d63573ab9e312a4ab4868ec6366871b63bfcb7f870c3efd576d4e9b9
 	HEAD_REF main
 )
 
@@ -18,11 +18,12 @@ vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
 
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+message("${VCPKG_LIBRARY_LINKAGE}")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
