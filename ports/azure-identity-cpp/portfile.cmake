185ac5089fdcadc69ef671920ddb30fe8f32f764
azure-sdk <azuresdk@microsoft.com>
2022-04-05 19:07:46 +0000

    [azure-identity-cpp] Update to 1.3.0-beta.1 ## 1.3.0-beta.1 (2022-04-05)
    ### Features Added
    
    - Added `ChainedTokenCredential`.

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-identity_1.2.0
-    SHA512 f27adda21a7d7b687cae08bed6b9f1e7ef10a9f36a886021c939da570c01fe4ab153bbc5852280bb147c879d6cdb4361d9655656398368814f4842ebc9ddc2dc
+    REF azure-identity_1.3.0-beta.1
+    SHA512 f0d2dfb7237b11932e0c100dd718b99bc84fc38ce66d4e1cc26953c6e49a1a2d0fba1af456ccde519e05143cbcb0d0079fb2f3b412e6c53f4c662f1ba8b3b0f2
 )
 
 vcpkg_cmake_configure(
