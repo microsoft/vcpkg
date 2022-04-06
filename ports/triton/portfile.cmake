acabf138e3d243032ce065be7d0932234448496b
MACHIZAUD Andréa <andrea.machizaud@adobe.com>
2022-04-07 00:58:31 +0200

    [triton] upgrade to support capstone 5

--- ports/triton/portfile.cmake
+++ ports/triton/portfile.cmake
@@ -4,19 +4,18 @@ vcpkg_from_github(
   REF 64a2b9b0490c82e41b37e623b1d0da14e2382e7a
   SHA512 ff99a270813043df2bc0da765e04aae4b9d5a911d20c6e5ffca1472eae8d6e1fcfff3cd56da023d6a77a647644839430bf72902acd84ec521a0e098f185d275c
   PATCHES
-    fix-dependencies.patch
-    fix-usage.patch
-    fix-python.patch
+    001-fix-dependency-z3.patch
+    002-fix-dependency-capstone.patch
+    003-fix-capstone-5.patch
+    004-fix-python.patch
 )
-
-file(REMOVE "${SOURCE_PATH}/CMakeModules/FindZ3.cmake")
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" STATICLIB)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATICCRT)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        "python"         PYTHON_BINDINGS
+        "python" PYTHON_BINDINGS
 )
 
 set(ADDITIONAL_OPTIONS )
@@ -36,13 +35,7 @@ vcpkg_cmake_configure(
         -DMSVC_STATIC=${STATICCRT}
         -DBUILD_EXAMPLES=OFF
         -DENABLE_TEST=OFF
-        -DCAPSTONE_PKGCONF_INCLUDE_DIRS="${CURRENT_INSTALLED_DIR}/include"
         ${ADDITIONAL_OPTIONS}
-    OPTIONS_DEBUG
-        -DCAPSTONE_PKGCONF_LIBRARY_DIRS="${CURRENT_INSTALLED_DIR}/debug/lib"
-    OPTIONS_RELEASE
-        -DCAPSTONE_PKGCONF_LIBRARY_DIRS="${CURRENT_INSTALLED_DIR}/lib"
-       
 )
 
 vcpkg_cmake_install()
