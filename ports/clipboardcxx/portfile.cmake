2f40b292622b26277b4f515b6df6153aa705c56d
Leonid Pospelov <pospelovlm@yandex.ru>
2022-04-07 21:09:51 +0300

    [clipboardcxx] Add new port

---
+++ ports/clipboardcxx/portfile.cmake
@@ -0,0 +1,14 @@
+# Download the code from GitHub
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO Arian8j2/ClipboardXX
+  REF d404c39ba384f8e16555610b3633cd7b58d84c59
+  SHA512 503bc78cd9fd6096fa92524973d19cbc9169fca91450837a2af7f1518eb928dce10c01e446de1ab76ae0dc366b26831df403f021118fe5c3c2eaeb4d752f638f
+  HEAD_REF master
+)
+
+# Install the header only source files to the right location
+file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
