931768e9377f6f65be654478b12a3feb9eddfbcf
Jonas Zaddach <jzaddach@cisco.com>
2022-04-08 11:07:58 +0200

    Updated to ClamAV 1.0.0

--- ports/clamav/portfile.cmake
+++ ports/clamav/portfile.cmake
@@ -2,33 +2,33 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO Cisco-Talos/clamav-devel
-  REF clamav-0.103.0
-  SHA512 e7ff4d98e0615a9fec0752bbfa2b882ae95034a8e01d0f7cc635ee520ff917c3be2a2d3273caa2fc1598e7d5ec4fd60d59b517cb439c5454d32447f8c8d7ba5a
+  REPO Cisco-Talos/clamav
+  REF clamav-1.0.0
+  SHA512 a1be526516e622fd3359461db7dd8eb0734f7ba8ecb0b63c1574e216885cd7bcdc69ffdbc5e507a0060d23769e3caa8423aa273ec57bb86e40049679a818152a
   FILE_DISAMBIGUATOR 1
-  HEAD_REF master
+  HEAD_REF main
   PATCHES
-      "build.patch"
       "cmakefiles.patch"
-      "curl.patch"
-      "mspack.patch"
 )
 
 vcpkg_cmake_configure(
-  SOURCE_PATH "${SOURCE_PATH}"
+  SOURCE_PATH ${SOURCE_PATH}
   OPTIONS
       -DENABLE_LIBCLAMAV_ONLY=ON
       -DENABLE_DOCS=OFF
       -DENABLE_SHARED_LIB=ON
       -DENABLE_STATIC_LIB=OFF
-      -DENABLE_EXTERNAL_MSPACK=ON
+      -DENABLE_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/NEWS.md" "${CURRENT_PACKAGES_DIR}/debug/NEWS.md")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/README.md" "${CURRENT_PACKAGES_DIR}/debug/README.md")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/COPYING.txt" "${CURRENT_PACKAGES_DIR}/debug/COPYING.txt")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # On Linux, clamav will still build and install clamav-config
