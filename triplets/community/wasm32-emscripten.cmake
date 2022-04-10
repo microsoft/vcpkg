c9231e31c574ca3283f62e98a8c07aedacbd3796
Konstantin Podsvirov <konstantin@podsvirov.pro>
2022-04-11 01:50:03 +0300

    Use EMSCRIPTEN variable instead of EMSDK
    Because it's directly point to appropriate version

--- triplets/community/wasm32-emscripten.cmake
+++ triplets/community/wasm32-emscripten.cmake
@@ -1,10 +1,10 @@
 set(VCPKG_ENV_PASSTHROUGH EMSDK PATH)
 
-if(NOT DEFINED ENV{EMSDK})
-   message(FATAL_ERROR "The EMSDK environment variable must be defined")
+if(NOT DEFINED ENV{EMSCRIPTEN})
+   message(FATAL_ERROR "The EMSCRIPTEN environment variable must be defined")
 endif()
 
-if(NOT EXISTS $ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
+if(NOT EXISTS $ENV{EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake)
    message(FATAL_ERROR "Emscripten.cmake toolchain file not found")
 endif()
 
@@ -12,4 +12,4 @@ set(VCPKG_TARGET_ARCHITECTURE wasm32)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Emscripten)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE $ENV{EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE $ENV{EMSCRIPTEN}/cmake/Modules/Platform/Emscripten.cmake)
