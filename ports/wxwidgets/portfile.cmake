20aba40cde37ba897506e2430e27d46c9714ef50
Kai Pastor <dg0yt@darc.de>
2022-04-11 07:41:55 +0200

    Transform installation fixup into patch

--- ports/wxwidgets/portfile.cmake
+++ ports/wxwidgets/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 33817f766b36d24e5e6f4eb7666f2e4c1ec305063cb26190001e0fc82ce73decc18697e8005da990a1c99dc1ccdac9b45bb2bbe5ba73e6e2aa860c768583314c
     HEAD_REF master
     PATCHES
-        disable-platform-lib-dir.patch
+        install-layout.patch
         fix-build.patch
         fix-linux-configure.patch # Remove this patch in the next update
         fix-libs-export.patch
@@ -61,35 +61,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-if (VCPKG_TARGET_IS_WINDOWS)
-    file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
-    if(DLLS)
-        file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin")
-        foreach(DLL IN LISTS DLLS)
-            get_filename_component(N "${DLL}" NAME)
-            file(RENAME "${DLL}" "${CURRENT_PACKAGES_DIR}/bin/${N}")
-        endforeach()
-    endif()
-    file(GLOB DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
-    if(DLLS)
-        file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin")
-        foreach(DLL IN LISTS DLLS)
-            get_filename_component(N "${DLL}" NAME)
-            file(RENAME "${DLL}" "${CURRENT_PACKAGES_DIR}/debug/bin/${N}")
-        endforeach()
-    endif()
-endif()
-
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_copy_tools(TOOL_NAMES wxrc AUTO_CLEAN)
 else()
     vcpkg_copy_tools(TOOL_NAMES wxrc wx-config wxrc-3.1 AUTO_CLEAN)
-    # FindwxWidgets.cmake/find_program doesn't accept a symlink for wxrc.
-    file(INSTALL "${CURRENT_PACKAGES_DIR}/tools/${PORT}/wxrc-3.1"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
-        RENAME wxrc
-        USE_SOURCE_PERMISSIONS
-    )
 endif()
 
 # do the copy pdbs now after the dlls got moved to the expected /bin folder above
@@ -136,15 +111,9 @@ if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/include/wx/setup.h")
     configure_file("${CMAKE_CURRENT_LIST_DIR}/setup.h.in" "${CURRENT_PACKAGES_DIR}/include/wx/setup.h" @ONLY)
 endif()
 
-file(GLOB configs LIST_DIRECTORIES false "${CURRENT_PACKAGES_DIR}/lib/wx/config/*")
+file(GLOB configs LIST_DIRECTORIES false "${CURRENT_PACKAGES_DIR}/lib/wx/config/*" "${CURRENT_PACKAGES_DIR}/tools/wx-config")
 foreach(config IN LISTS configs)
     vcpkg_replace_string("${config}" "${CURRENT_INSTALLED_DIR}" [[${prefix}]])
-    # tools/<port>/wx-config is a symlink with absolute path. Replace it with an actual config.
-    file(INSTALL "${config}"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
-        RENAME wx-config
-        USE_SOURCE_PERMISSIONS
-    )
 endforeach()
 file(GLOB configs LIST_DIRECTORIES false "${CURRENT_PACKAGES_DIR}/debug/lib/wx/config/*")
 foreach(config IN LISTS configs)
