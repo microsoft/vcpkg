ccd856d364d298125416f8c7a6c067d3630de0a3
Alexander Neumann <you@example.com>
2022-04-11 17:23:01 +0200

    use qface 2.0.5

--- ports/qtinterfaceframework/portfile.cmake
+++ ports/qtinterfaceframework/portfile.cmake
@@ -20,7 +20,9 @@ endif()
 vcpkg_find_acquire_program(PKGCONFIG)
 
 vcpkg_find_acquire_program(PYTHON3)
-x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" REQUIREMENTS_FILE "${CURRENT_PORT_DIR}/requirements_minimal.txt" PACKAGES qface==2.0.3)
+x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" 
+                            REQUIREMENTS_FILE "${CURRENT_PORT_DIR}/requirements_minimal.txt" 
+                            PACKAGES qface==2.0.5)
 
 if(VCPKG_CROSSCOMPILING)
     list(APPEND FEATURE_OPTIONS "-DVCPKG_HOST_TRIPLET=${_HOST_TRIPLET}")
