c22dec1d659ea50b14bbad107b2b60ca4d895eb2
Alberto Garcia Illera <agarciaillera@gmail.com>
2022-04-11 23:15:18 +1000

    update Triton version

--- ports/triton/portfile.cmake
+++ ports/triton/portfile.cmake
@@ -1,13 +1,12 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO JonathanSalwan/Triton
-  REF 64a2b9b0490c82e41b37e623b1d0da14e2382e7a
-  SHA512 ff99a270813043df2bc0da765e04aae4b9d5a911d20c6e5ffca1472eae8d6e1fcfff3cd56da023d6a77a647644839430bf72902acd84ec521a0e098f185d275c
+  REF 243026c9c1e07a5ca834c4aaf628d1079f6a85ea
+  SHA512 9e46c500203647de545286b78a7d4ae6da1796b8eed30fe7346ae6e51865ef70de1adb858c402c3687c471ad654cc6aefdff8893196f5ef7b45e4cee6dd9c577
   PATCHES
     001-fix-dependency-z3.patch
-    002-fix-dependency-capstone.patch
-    003-fix-capstone-5.patch
-    004-fix-python.patch
+    002-fix-capstone-5.patch
+    003-fix-python.patch
 )
 
 string(COMPARE NOTEQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" DYNAMICLIB)
@@ -31,6 +30,7 @@ vcpkg_cmake_configure(
     OPTIONS
         ${FEATURE_OPTIONS}
         -DZ3_INTERFACE=ON
+        -DTRITON_BOOST_INTERFACE=OFF
         -DBUILD_SHARED_LIBS=${DYNAMICLIB}
         -DMSVC_STATIC=${STATICCRT}
         -DBUILD_EXAMPLES=OFF
