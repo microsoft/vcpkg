6cfa4b16a5a7dee46ce7d3be097923837059fcff
Alexander Neumann <you@example.com>
2022-04-11 11:12:03 +0200

    add code from tensorflow about venv

--- ports/vcpkg-get-python-packages/x_vcpkg_get_python_packages.cmake
+++ ports/vcpkg-get-python-packages/x_vcpkg_get_python_packages.cmake
@@ -50,25 +50,37 @@ function(x_vcpkg_get_python_packages)
                                                WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
                                                LOGNAME "get-pip-${TARGET_TRIPLET}")
             endif()
-            foreach(_package IN LISTS arg_PACKAGES)
-                vcpkg_execute_required_process(COMMAND "${python_dir}/Scripts/pip${VCPKG_HOST_EXECUTABLE_SUFFIX}" install ${_package}
-                                               WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-                                               LOGNAME "pip-install-${TARGET_TRIPLET}")
-            endforeach()
+            vcpkg_execute_required_process(COMMAND "${python_dir}/Scripts/pip${VCPKG_HOST_EXECUTABLE_SUFFIX}" install virtualenv
+                               WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+                               LOGNAME "pip-install-${TARGET_TRIPLET}")
         else()
-            foreach(_package IN LISTS arg_PACKAGES)
-                vcpkg_execute_required_process(COMMAND "${python_dir}/easy_install${VCPKG_HOST_EXECUTABLE_SUFFIX}" ${_package}
-                                               WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-                                               LOGNAME "easy-install-${TARGET_TRIPLET}")
-            endforeach()
+            vcpkg_execute_required_process(COMMAND "${python_dir}/easy_install${VCPKG_HOST_EXECUTABLE_SUFFIX}" virtualenv #${_package}
+                                   WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+                                   LOGNAME "easy-install-${TARGET_TRIPLET}")
         endif()
-    else() # outside vcpkg
-        foreach(package IN LISTS arg_PACKAGES)
-            string(REGEX REPLACE "[>=<]+.+" "" package_no_version "${package}") # Remove version constrins for testing
-            vcpkg_execute_in_download_mode(COMMAND ${arg_PYTHON_EXECUTABLE} -c "import ${package_no_version}" RESULT_VARIABLE HAS_ERROR)
-            if(HAS_ERROR)
-                message(FATAL_ERROR "Python package '${package}' needs to be installed for port '${PORT}'.\nComplete list of required python packages: ${arg_PACKAGES}")
-            endif()
-        endforeach()
+    #else() # outside vcpkg
+    #    foreach(package IN LISTS arg_PACKAGES)
+    #        string(REGEX REPLACE "[>=<]+.+" "" package_no_version "${package}") # Remove version constrins for testing
+    #        vcpkg_execute_in_download_mode(COMMAND ${arg_PYTHON_EXECUTABLE} -c "import ${package_no_version}" RESULT_VARIABLE HAS_ERROR)
+    #        if(HAS_ERROR)
+    #            message(FATAL_ERROR "Python package '${package}' needs to be installed for port '${PORT}'.\nComplete list of required python packages: ${arg_PACKAGES}")
+    #        endif()
+    #    endforeach()
     endif()
+        
+    vcpkg_execute_required_process(COMMAND "${PYTHON3}" -m venv --symlinks "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv"
+                                   WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}" 
+                                   LOGNAME "prerequisites-venv-${TARGET_TRIPLET}")
+    vcpkg_add_to_path(PREPEND "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv/bin")
+    set(PYTHON3 "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv/bin/python3${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+    set(ENV{VIRTUAL_ENV} "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv")
+    vcpkg_execute_required_process(COMMAND "${PYTHON3}" -c "import site; print(site.getusersitepackages())" 
+                                   WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}" 
+                                   LOGNAME "prerequisites-pypath-${TARGET_TRIPLET}" 
+                                   OUTPUT_VARIABLE PYTHON_LIB_PATH)
+    set(ENV{PYTHON_BIN_PATH} "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv/bin/")
+    set(ENV{PYTHON_LIB_PATH} "${PYTHON_LIB_PATH}")
+    vcpkg_execute_required_process(COMMAND "${PYTHON3}" -m pip install -U ${arg_PACKAGES} 
+                                   WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}" 
+                                   LOGNAME "prerequisites-pip-${TARGET_TRIPLET}")
 endfunction()
