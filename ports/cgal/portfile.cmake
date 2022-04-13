3a71884e3672397c4365c6ed57d1f92a3f00f099
JackBoosY <yuzaiyang@beyondsoft.com>
2022-04-13 02:33:17 -0700

    Add double quotes

--- ports/cgal/portfile.cmake
+++ ports/cgal/portfile.cmake
@@ -1,3 +1,4 @@
+# Header only
 vcpkg_buildpath_length_warning(37)
 
 vcpkg_from_github(
@@ -29,28 +30,28 @@ vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 
 # Clean
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 else()
-    foreach(ROOT ${CURRENT_PACKAGES_DIR}/bin)
+    foreach(ROOT "${CURRENT_PACKAGES_DIR}/bin")
         file(REMOVE
-            ${ROOT}/cgal_create_CMakeLists
-            ${ROOT}/cgal_create_cmake_script
-            ${ROOT}/cgal_make_macosx_app
+            "${ROOT}/cgal_create_CMakeLists"
+            "${ROOT}/cgal_create_cmake_script"
+            "${ROOT}/cgal_make_macosx_app"
         )
     endforeach()
 endif()
 
-file(INSTALL ${SOURCE_PATH}/Installation/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/Installation/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 file(
     COPY
-        ${SOURCE_PATH}/Installation/LICENSE.BSL
-        ${SOURCE_PATH}/Installation/LICENSE.RFL
-        ${SOURCE_PATH}/Installation/LICENSE.GPL
-        ${SOURCE_PATH}/Installation/LICENSE.LGPL
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+        "${SOURCE_PATH}/Installation/LICENSE.BSL"
+        "${SOURCE_PATH}/Installation/LICENSE.RFL"
+        "${SOURCE_PATH}/Installation/LICENSE.GPL"
+        "${SOURCE_PATH}/Installation/LICENSE.LGPL"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
