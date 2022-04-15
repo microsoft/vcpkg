9c96d90c0a6be754fe8b58dad84113ed2093c75f
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-04-15 11:24:28 +0200

    [simbody] Disable docs

--- ports/simbody/portfile.cmake
+++ ports/simbody/portfile.cmake
@@ -20,6 +20,8 @@ vcpkg_cmake_configure(
         -DBUILD_VISUALIZER=OFF
         -DBUILD_EXAMPLES=OFF
         -DBUILD_TESTING=OFF
+        -DBUILD_TESTS_AND_EXAMPLES_STATIC=OFF
+        -DBUILD_TESTS_AND_EXAMPLES_SHARED=OFF
 )
 
 vcpkg_cmake_install()
