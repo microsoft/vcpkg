b6db5972fb0c49100b24623a825a22326f1cc41d
Kai Pastor <dg0yt@darc.de>
2022-04-15 09:21:24 +0200

    Parse keywords in qt_submodule_installation
    The unparsed arguments must not be blindly passed to
    qt_download_submodule and qt_build_submodule.

--- ports/qt5-base/cmake/qt_submodule_installation.cmake
+++ ports/qt5-base/cmake/qt_submodule_installation.cmake
@@ -1,12 +1,24 @@
 
 
 function(qt_submodule_installation)
-    cmake_parse_arguments(_csc "" "OUT_SOURCE_PATH" "" ${ARGN})
-    qt_download_submodule(OUT_SOURCE_PATH TARGET_SOURCE_PATH ${_csc_UNPARSED_ARGUMENTS})
+    cmake_parse_arguments(_csc
+        ""
+        "OUT_SOURCE_PATH"
+         "PATCHES;OPTIONS;OPTIONS_RELEASE;OPTIONS_DEBUG;BUILD_OPTIONS;BUILD_OPTIONS_RELEASE;BUILD_OPTIONS_DEBUG"
+         ${ARGN}
+    )
+    qt_download_submodule(OUT_SOURCE_PATH TARGET_SOURCE_PATH PATCHES ${_csc_PATCHES})
     if(QT_UPDATE_VERSION)
         SET(VCPKG_POLICY_EMPTY_PACKAGE enabled PARENT_SCOPE)
     else()
-        qt_build_submodule(${TARGET_SOURCE_PATH} ${_csc_UNPARSED_ARGUMENTS})
+        qt_build_submodule(${TARGET_SOURCE_PATH}
+            OPTIONS ${_csc_OPTIONS}
+            OPTIONS_RELEASE ${_csc_OPTIONS_RELEASE}
+            OPTIONS_DEBUG ${_csc_OPTIONS_DEBUG}
+            BUILD_OPTIONS ${_csc_BUILD_OPTIONS}
+            BUILD_OPTIONS_RELEASE ${_csc_BUILD_OPTIONS_RELEASE}
+            BUILD_OPTIONS_DEBUG ${_csc_BUILD_OPTIONS_DEBUG}
+        )
         qt_install_copyright(${TARGET_SOURCE_PATH})
     endif()
     if(DEFINED _csc_OUT_SOURCE_PATH)
