3bb6d7807407b4dfd4af6d0502cc073355097ec6
chausner <chausner@users.noreply.github.com>
2022-04-17 13:57:17 +0200

    Avoid CPM

--- ports/bext-ut/portfile.cmake
+++ ports/bext-ut/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v1.1.9
     SHA512 81a6b80948d3a203534244f62f5f3ac57593083cc0c32484498a7d01d29455f7dcb33e2ec0587609b8dff33a81a5551796d7681d48fd93e817d6d0c31697234e
     HEAD_REF master
+    PATCHES
+        avoid-cpm.patch
 )
 
 vcpkg_cmake_configure(
