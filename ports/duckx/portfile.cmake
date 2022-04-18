080057b8fc6b6de9b51cfb38469cd8eb063a0505
JackBoosY <yuzaiyang@beyondsoft.com>
2022-04-18 07:41:25 -0700

    Switch to ONLY_STATIC_LIBRARY on Windows

--- ports/duckx/portfile.cmake
+++ ports/duckx/portfile.cmake
@@ -1,3 +1,7 @@
+if (VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO amiremohamadi/DuckX
@@ -12,6 +16,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/duckx)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
