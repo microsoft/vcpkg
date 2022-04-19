a3a1e6b2dbe96351608a71aca2b6e7625c07542d
Sean Farrell <sean.farrell@rioki.org>
2022-04-19 13:16:56 +0200

    Adds rsig v0.1.0

---
+++ ports/rsig/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO rioki/rsig
+    REF v0.1.0
+    SHA512 73de6dfe0b18f141a388c9307d8dff9a0709ceb758f58c474a7ddc5d9d77f2f8808fe4d78f3ad88466f81ca61b15ae3504255595d78387b23f87974de46d1d2b
+    )
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+configure_file(${SOURCE_PATH}/README.md ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
