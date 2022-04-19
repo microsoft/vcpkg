a7eba21d2ba62365fa5f3ba086f7dbee1b7b7f48
Sean Farrell <sean.farrell@rioki.org>
2022-04-19 13:33:36 +0200

    Applies automated code reviews.
    - updates new cmake invocation
    - adds license field vcpkg.json

--- ports/rsig/portfile.cmake
+++ ports/rsig/portfile.cmake
@@ -7,13 +7,12 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 configure_file(${SOURCE_PATH}/README.md ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
