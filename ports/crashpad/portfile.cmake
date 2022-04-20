1c28cf6d1cb6e1078b0390df2d3124f817268bb5
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2022-04-20 14:54:30 +0700

    Update ports/crashpad/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/crashpad/portfile.cmake
+++ ports/crashpad/portfile.cmake
@@ -7,9 +7,7 @@ vcpkg_from_git(
 )
 
 vcpkg_find_acquire_program(PYTHON3)
-file(READ "${SOURCE_PATH}/.gn" GN_FILE_CONTENT)
-string(REPLACE "script_executable = \"python3\"" "script_executable = \"${PYTHON3}\"" GN_FILE_CONTENT ${GN_FILE_CONTENT})
-file(WRITE "${SOURCE_PATH}/.gn" ${GN_FILE_CONTENT})
+vcpkg_replace_string("${SOURCE_PATH}/.gn" "script_executable = \"python3\"" "script_executable = \"${PYTHON3}\"")
 
 function(checkout_in_path PATH URL REF)
     if(EXISTS "${PATH}")
