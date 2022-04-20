937cff12bc78d1a7a1e4805e6262d2f640820692
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-04-20 16:59:44 +0200

    Update download file

--- ports/atkmm/portfile.cmake
+++ ports/atkmm/portfile.cmake
@@ -2,11 +2,13 @@ if (VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 endif()
 
+set(ATKMM_VERSION 2.36.1)
+
 # Keep distfile, don't use GitLab!
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/atkmm/2.36/atkmm-2.36.0.tar.xz"
-    FILENAME "atkmm-2.36.0.tar.xz"
-    SHA512 8527dfa50191919a7dcf6db6699767352cb0dac800d834ee39ed21694eee3136a41a7532d600b8b3c0fcea52da6129b623e8e61ada728d806aa61fdc8dc8dedf
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/atkmm/2.36/atkmm-${ATKMM_VERSION}.tar.xz"
+    FILENAME "atkmm-${ATKMM_VERSION}.tar.xz"
+    SHA512 0
 )
 
 vcpkg_extract_source_archive_ex(
