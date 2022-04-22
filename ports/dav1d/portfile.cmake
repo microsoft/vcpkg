5b2f7370790d38bc58809ba0ec69b9f0cb20d285
Jonliu1993 <13720414433@163.com>
2022-04-22 16:17:37 +0800

    [dav1d] update to 0.9.2

--- ports/dav1d/portfile.cmake
+++ ports/dav1d/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_from_gitlab(
     GITLAB_URL https://code.videolan.org
     OUT_SOURCE_PATH SOURCE_PATH
     REPO videolan/dav1d
-    REF 0.8.2
-    SHA512 2f48c88fdbe627f089eb378f43b8f18e1c5cff4645148ee7e14b8f3f8e9d690b28e58fc9cb466e7a9a4c147f06b0d34c47c132070acefbd01abe23cd3c6047d8
+    REF 0.9.2
+    SHA512 131748370260b7792fb2fe62e3a0c6afdad37e37bbca7a09264df6630f87f46117299920ed47f4c49ea9c06797b8b2d5de3582bc082f57e97bda271e9a443043
     PATCHES
-        "patch_underscore_prefix.patch"
+        patch_underscore_prefix.patch
 )
 
 vcpkg_find_acquire_program(NASM)
