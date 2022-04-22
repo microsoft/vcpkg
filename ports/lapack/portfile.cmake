677cb689f13548b3f75ff6cd076e14c7e3dd6f24
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-04-22 23:20:53 +0200

    rename test ports with -test rename -impl ports to old name move -test ports into script/test_ports

--- ports/lapack/portfile.cmake
+++ ports/lapack/portfile.cmake
@@ -1,6 +1,22 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-# Make sure LAPACK can be found
-vcpkg_cmake_configure(SOURCE_PATH "${CURRENT_PORT_DIR}"
-                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
-                              -DCMAKE_MODULE_PATH="${CURRENT_PACKAGES_DIR}/share/lapack")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(BLA_STATIC ON)
+else()
+    set(BLA_STATIC OFF)
+endif()
+
+set(BLA_VENDOR Generic)
+set(installed_wrapper "${CURRENT_INSTALLED_DIR}/share/lapack/vcpkg-cmake-wrapper.cmake")
+set(installed_module "${CURRENT_INSTALLED_DIR}/share/lapack/FindLAPACK.cmake")
+if(VCPKG_TARGET_IS_OSX)
+    set(BLA_VENDOR Apple)
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/lapack/vcpkg-cmake-wrapper.cmake" @ONLY)
+    unset(installed_module)
+elseif((VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE MATCHES "arm") OR VCPKG_TARGET_IS_UWP)
+    configure_file("${CURRENT_INSTALLED_DIR}/share/clapack/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/lapack/vcpkg-cmake-wrapper.cmake" COPYONLY)
+    configure_file("${CURRENT_INSTALLED_DIR}/share/clapack/FindLAPACK.cmake" "${CURRENT_PACKAGES_DIR}/share/lapack/FindLAPACK.cmake" COPYONLY)
+else()
+    configure_file("${CURRENT_INSTALLED_DIR}/share/lapack-reference/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/lapack/vcpkg-cmake-wrapper.cmake" COPYONLY)
+    configure_file("${CURRENT_INSTALLED_DIR}/share/lapack-reference/FindLAPACK.cmake" "${CURRENT_PACKAGES_DIR}/share/lapack/FindLAPACK.cmake" COPYONLY)
+endif()
---
+++ scripts/test_ports/lapack-test/portfile.cmake
@@ -0,0 +1,6 @@
+SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+# Make sure LAPACK can be found
+vcpkg_cmake_configure(SOURCE_PATH "${CURRENT_PORT_DIR}"
+                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
+                              -DCMAKE_MODULE_PATH="${CURRENT_PACKAGES_DIR}/share/lapack")
