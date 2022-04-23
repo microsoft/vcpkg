ccd0d437a0e5d2ae53170875a7a5ee5c764e4eaa
Rémy Tassoux <contact@rt2.fr>
2022-04-23 03:41:35 +0200

    [celero] fix binaries install path

--- ports/celero/portfile.cmake
+++ ports/celero/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v2.8.3
     SHA512 f6774f6076bae5d37d4f5bd12153bc99b97893f43fe9253fe805b5b0c2475ffe878e32f3fc7391544ea24020e59d5ae48e44e5b3a302f7280ad15b6a6820f70b
     HEAD_REF master
+    PATCHES
+        fix-bin-install-path.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" CELERO_COMPILE_DYNAMIC_LIBRARIES)
