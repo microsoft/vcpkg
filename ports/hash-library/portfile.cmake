9ab728184fde5f7d891dbf36cd376ca0224e117d
chausner <chausner@users.noreply.github.com>
2022-04-24 20:42:44 +0200

    Add patch for macOS

--- ports/hash-library/portfile.cmake
+++ ports/hash-library/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF hash_library_v8
     SHA512 1c428710c0c3e4d5d1114d757a5d9145ed12c7e2fbbfa51635f43a349ddb5634bdf49e8d8fdbc7576e90b319989fb85efec433bb43ddb551c2cf29a8e80ba78b
     HEAD_REF master
+    PATCHES
+        001-fix-macos.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
