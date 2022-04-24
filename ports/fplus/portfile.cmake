c7de252bb96d474a5cf7782d2ece59c7d76bc817
chausner <chausner@users.noreply.github.com>
2022-04-24 22:21:01 +0200

    Remove unused option

--- ports/fplus/portfile.cmake
+++ ports/fplus/portfile.cmake
@@ -9,7 +9,6 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DFPLUS_BUILD_EXAMPLES=OFF
         -DFunctionalPlus_INSTALL_CMAKEDIR=share/FunctionalPlus
 )
 
