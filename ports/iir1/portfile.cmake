234d2647dfa7be3a25f8300291c2e9be691c53c4
chausner <chausner@users.noreply.github.com>
2022-04-24 22:07:33 +0200

    Only either build as shared or static

--- ports/iir1/portfile.cmake
+++ ports/iir1/portfile.cmake
@@ -4,10 +4,14 @@ vcpkg_from_github(
     REF 1.9.1
     SHA512 115692fb82637d21c2cbff974d931ef12a5c7fd8502560e2b807ceed69b1a6a4e05898aff37b089ccda7c4aa32e97e71f7e6007998ec52f6122da33eb222112e
     HEAD_REF master
+    PATCHES
+        fix-shared-static.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
