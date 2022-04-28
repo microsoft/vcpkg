2c5b3f22b7ad6ac30f5a77bc5cd2894a0e8d180f
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-04-28 16:03:40 +0200

    [jansson] cleanup

--- ports/jansson/portfile.cmake
+++ ports/jansson/portfile.cmake
@@ -6,45 +6,33 @@ vcpkg_from_github(
   HEAD_REF master
 )
 
-if(VCPKG_CRT_LINKAGE STREQUAL static)
-  set(JANSSON_STATIC_CRT ON)
-else()
-  set(JANSSON_STATIC_CRT OFF)
-endif()
-
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  set(JANSSON_BUILD_SHARED_LIBS ON)
-else()
-  set(JANSSON_BUILD_SHARED_LIBS OFF)
-endif()
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" JANSSON_STATIC_CRT)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" JANSSON_BUILD_SHARED_LIBS)
 
 # Jansson tries to random-seed its hash table with system-provided entropy.
 # This is not ported to UWP yet.
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+if(VCPKG_TARGET_IS_UWP)
   set(USE_WINDOWS_CRYPTOAPI OFF)
 else()
   set(USE_WINDOWS_CRYPTOAPI ON)
 endif()
 
-vcpkg_configure_cmake(
-  SOURCE_PATH ${SOURCE_PATH}
-  PREFER_NINJA
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
     -DJANSSON_STATIC_CRT=${JANSSON_STATIC_CRT}
     -DJANSSON_EXAMPLES=OFF
     -DJANSSON_WITHOUT_TESTS=ON
+    -DJANSSON_BUILD_DOCS=OFF
     -DJANSSON_BUILD_SHARED_LIBS=${JANSSON_BUILD_SHARED_LIBS}
     -DUSE_WINDOWS_CRYPTOAPI=${USE_WINDOWS_CRYPTOAPI}
     -DJANSSON_INSTALL_CMAKE_DIR:STRING=share/jansson
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
+vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
