5cec272bfa882071a3c9ec23dff54510c8659b24
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-04-29 22:41:07 -0400

    [ports/c-str-span/portfile.cmake] [c_str_span/c_str_span_precondition.c] Revert `AZ_ON_FAIL_DEBUG`; [CMakeLists.txt] Flatten `foreach` loop; [ports/c-str-span/vcpkg.json] Add homepage

--- ports/c-str-span/portfile.cmake
+++ ports/c-str-span/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            SamuelMarks/c-str-span
-    REF             8a9d48c43dead94202d1f30ee206506627d68552
-    SHA512          50ceb504afcf6950799e26fb02f624a28ea799fa20e1ea1a3f5b355dd1a8631b8ca319b216f608c60762e6cb198a3261dfba79f0b152b6e8e5368105bd9cbe57
+    REF             bb30730607fbef77a7046badca2e2c3df7d74ab3
+    SHA512          0e856527bd1a7c1a3a185cea0e2df0d19bd740a26a01e5590eba72a5a30991ce2be4a680dc651f0f687cf4989f81c17d546727b93a3db82dc83bce74980e6d64
     HEAD_REF        master
 )
 
@@ -13,7 +13,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/LICENSE-MIT"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/c-str-span"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
      RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share")
