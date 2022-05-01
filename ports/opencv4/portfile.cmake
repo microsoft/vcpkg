ca62490dde92b12739897503db158684f10b0951
Kurt Blackwell <kurt@blackwell.id.au>
2022-05-01 11:29:53 +0800

    Only apply changes to OpenCVModules-debug.cmake when it is expected to exist

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -457,11 +457,13 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME opencv CONFIG_PATH "share/opencv")
 vcpkg_copy_pdbs()
 
-# Update debug paths for libs in Android builds (e.g. sdk/native/staticlibs/armeabi-v7a)
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules-debug.cmake"
-    "\${_IMPORT_PREFIX}/sdk"
-    "\${_IMPORT_PREFIX}/debug/sdk"
-)
+if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+  # Update debug paths for libs in Android builds (e.g. sdk/native/staticlibs/armeabi-v7a)
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules-debug.cmake"
+      "\${_IMPORT_PREFIX}/sdk"
+      "\${_IMPORT_PREFIX}/debug/sdk"
+  )
+endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(READ "${CURRENT_PACKAGES_DIR}/share/opencv/OpenCVModules.cmake" OPENCV_MODULES)
