4632c04b5e63bb2df86bed41f5063902f4a830e7
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2022-05-04 00:35:44 +0200

    [discordcoreapi] no absolute paths

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
 	REF c5e7efee991f4dcff428ca21c2e157949e784a78
 	SHA512 5f42864f8375c87542550ff1f05e761d75a02de5810a4ca1f1b4893894d9be11a21915ff16771e3233103c4fce3ce8734359baacd5f5051d719e387a225343dd
 	HEAD_REF main
+	PATCHES
+		no-absolute.patch
 )
 
 vcpkg_cmake_configure(
