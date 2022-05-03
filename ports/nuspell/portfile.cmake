9f9f6572f58c1971ab1c3005afb8623e40bfa2ce
chausner <15180557+chausner@users.noreply.github.com>
2022-05-03 05:43:03 +0200

    [nuspell] Update to 5.1.0 (#24485)
    * Update nuspell to 5.1.0
    
    * Update CI baseline
    
    * Fix license
    
    * Update CI baseline
    
    Co-authored-by: chausner <chausner@users.noreply.github.com>

--- ports/nuspell/portfile.cmake
+++ ports/nuspell/portfile.cmake
@@ -1,17 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nuspell/nuspell
-    REF v5.0.0
-    SHA512 92605c5f0d413b86711fea3b993d656552d5e72440303d3e71907a503d060ead032dfeb0dd8dbb790f35599b8cca8854bdd8650f0560793d9c62a11aebccc2e5
+    REF v5.1.0
+    SHA512 08ce776d224c66ba4bdd8cb22943741f30e8e5ba03396bfef1db598321c9b0f8814dacffcb52287b3b669426c7b08e2c1a40323341a9ff9768a7187002e1d77a
     HEAD_REF master
 )
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS -DBUILD_TESTING=OFF
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
 )
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/nuspell)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/nuspell)
 vcpkg_fixup_pkgconfig(
     # nuspell.pc depends on icu-uc.pc which has -lm specified as private
     # library. Ignore this -lm, otherwise this function shows error
@@ -20,10 +20,7 @@ vcpkg_fixup_pkgconfig(
 )
 vcpkg_copy_tools(TOOL_NAMES nuspell AUTO_CLEAN)
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
-file(
-    INSTALL ${SOURCE_PATH}/COPYING.LESSER
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
-    RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING.LESSER" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
