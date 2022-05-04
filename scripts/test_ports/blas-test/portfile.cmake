244878ca6bbf993604ca9574565d4f4be93476f2
Alexander Neumann <you@example.com>
2022-05-04 17:32:24 +0200

    adjust test to check for pc files.

--- scripts/test_ports/blas-test/portfile.cmake
+++ scripts/test_ports/blas-test/portfile.cmake
@@ -2,5 +2,4 @@ SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 # Make sure BLAS can be found
 vcpkg_cmake_configure(SOURCE_PATH "${CURRENT_PORT_DIR}"
-                      OPTIONS -DCMAKE_PREFIX_PATH="${CURRENT_PACKAGES_DIR}"
-                              )
+                      OPTIONS -DVCPKG_HOST_TRIPLET=${HOST_TRIPLET})
