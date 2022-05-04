114c4b39feb9d0dd2c57300f22202fd6fd3a1c8c
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-04 17:56:49 +0200

    Now update both sha and version...

--- ports/stftpitchshift/portfile.cmake
+++ ports/stftpitchshift/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO jurihock/stftPitchShift
   HEAD_REF main
-  REF 14bb99d9e76cdf84be5a6433d0ebe6bc91f79977
-  SHA512 c4b9d57dc9241b50d9df618cb7959e0d5bc2cdd1c73e0ebfe8427c07c502b9c2c2bd810ef6bc67e5bdab4acb140e5f27bc256dcb62ff373a4b7d529c7e468906
+  REF e25def2f6aad9db865aded9f0550fd1c7925188f
+  SHA512 a7e9343ede83eb7d37a231e3db80659f8fa7c61c70811618c75a817fcc94902f5fa4582f81e4473a0e137f12c2eaf17025a057a7ef7c4b348451454e39b3fa2f
 )
 
 vcpkg_configure_cmake(
