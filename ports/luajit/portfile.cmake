133c7e38d6d905010e72e39a8b215af7402e1e52
Billy Robert O'Neal III <bion@microsoft.com>
2022-05-06 16:15:20 -0700

    Add quotes.

--- ports/luajit/portfile.cmake
+++ ports/luajit/portfile.cmake
@@ -10,13 +10,9 @@ vcpkg_from_github(
 )
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-	set (LJIT_STATIC "")
+    set (LJIT_STATIC "")
 else()
-	set (LJIT_STATIC "static")
-endif()
-
-if(VCPKG_TARGET_IS_OSX)
-    set(ENV{MACOSX_DEPLOYMENT_TARGET} "10.14")
+    set (LJIT_STATIC "static")
 endif()
 
 if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL debug)
@@ -27,28 +23,28 @@ if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL debug)
     if (VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
         vcpkg_execute_required_process_repeat(
             COUNT 1
-            COMMAND "${SOURCE_PATH}/src/msvcbuild.bat" ${SOURCE_PATH}/src ${VCPKG_CRT_LINKAGE} debug ${LJIT_STATIC}
+            COMMAND "${SOURCE_PATH}/src/msvcbuild.bat" "${SOURCE_PATH}/src" ${VCPKG_CRT_LINKAGE} debug ${LJIT_STATIC}
             WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg"
             LOGNAME build-${TARGET_TRIPLET}-dbg
         )
 
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/luajit.exe DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools)
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+        file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/luajit.exe" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/tools")
+        file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.lib"  DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
 
         if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-            file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-            file(COPY ${CURRENT_PACKAGES_DIR}/debug/bin/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools)
+            file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/lua51.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+            file(COPY "${CURRENT_PACKAGES_DIR}/debug/bin/lua51.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/tools")
         endif()
         vcpkg_copy_pdbs()
     else()
         vcpkg_execute_build_process(
-            COMMAND make -j${VCPKG_CONCURRENCY} -f ${SOURCE_PATH}/Makefile clean
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            COMMAND make -j${VCPKG_CONCURRENCY} -f "${SOURCE_PATH}/Makefile" clean
+            WORKING_DIRECTORY "${SOURCE_PATH}"
             LOGNAME clean-${TARGET_TRIPLET}-debug
         )
         vcpkg_execute_build_process(
-            COMMAND make -j${VCPKG_CONCURRENCY} -f ${SOURCE_PATH}/Makefile PREFIX=${CURRENT_PACKAGES_DIR}/debug CCDEBUG=-g3 CFLAGS=-O0 BUILDMODE=${VCPKG_LIBRARY_LINKAGE} install
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            COMMAND make -j${VCPKG_CONCURRENCY} -f "${SOURCE_PATH}/Makefile" "PREFIX=${CURRENT_PACKAGES_DIR}/debug" CCDEBUG=-g3 CFLAGS=-O0 BUILDMODE=${VCPKG_LIBRARY_LINKAGE} install
+            WORKING_DIRECTORY "${SOURCE_PATH}"
             LOGNAME build-${TARGET_TRIPLET}-debug
         )
         file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
@@ -66,28 +62,28 @@ if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL release)
     if (VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
         vcpkg_execute_required_process_repeat(
             COUNT 1
-            COMMAND "${SOURCE_PATH}/src/msvcbuild.bat" ${SOURCE_PATH}/src ${VCPKG_CRT_LINKAGE} ${LJIT_STATIC}
+            COMMAND "${SOURCE_PATH}/src/msvcbuild.bat" "${SOURCE_PATH}/src" ${VCPKG_CRT_LINKAGE} ${LJIT_STATIC}
             WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
             LOGNAME build-${TARGET_TRIPLET}-rel
         )
 
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/luajit.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools)
-        file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.lib  DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+        file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/luajit.exe" DESTINATION "${CURRENT_PACKAGES_DIR}/tools")
+        file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.lib"  DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
 
         if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-            file(INSTALL ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
-            vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools)
+            file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/lua51.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+            vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools")
         endif()
         vcpkg_copy_pdbs()
     else()
         vcpkg_execute_build_process(
-            COMMAND make -j${VCPKG_CONCURRENCY} -f ${SOURCE_PATH}/Makefile clean
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            COMMAND make -j${VCPKG_CONCURRENCY} -f "${SOURCE_PATH}/Makefile" clean
+            WORKING_DIRECTORY "${SOURCE_PATH}"
             LOGNAME clean-${TARGET_TRIPLET}-rel
         )
         vcpkg_execute_build_process(
-            COMMAND make -j${VCPKG_CONCURRENCY} -f ${SOURCE_PATH}/Makefile PREFIX=${CURRENT_PACKAGES_DIR} CCDEBUG= BUILDMODE=${VCPKG_LIBRARY_LINKAGE} install
-            WORKING_DIRECTORY ${SOURCE_PATH}
+            COMMAND make -j${VCPKG_CONCURRENCY} -f "${SOURCE_PATH}/Makefile" "PREFIX=${CURRENT_PACKAGES_DIR}" CCDEBUG= BUILDMODE=${VCPKG_LIBRARY_LINKAGE} install
+            WORKING_DIRECTORY "${SOURCE_PATH}"
             LOGNAME build-${TARGET_TRIPLET}-rel
         )
         file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/lua" "${CURRENT_PACKAGES_DIR}/lib/lua")
@@ -98,12 +94,12 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
-file(INSTALL ${SOURCE_PATH}/src/lua.h      DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/luajit.h   DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/luaconf.h  DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lualib.h   DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lauxlib.h  DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
-file(INSTALL ${SOURCE_PATH}/src/lua.hpp    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT})
+file(INSTALL "${SOURCE_PATH}/src/lua.h"      DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+file(INSTALL "${SOURCE_PATH}/src/luajit.h"   DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+file(INSTALL "${SOURCE_PATH}/src/luaconf.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+file(INSTALL "${SOURCE_PATH}/src/lualib.h"   DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+file(INSTALL "${SOURCE_PATH}/src/lauxlib.h"  DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+file(INSTALL "${SOURCE_PATH}/src/lua.hpp"    DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYRIGHT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
