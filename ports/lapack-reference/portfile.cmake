d5dcbf773ce46a698850b8b007bc8f4025afc787
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-05-08 22:28:24 +0200

    add intel patch

--- ports/lapack-reference/portfile.cmake
+++ ports/lapack-reference/portfile.cmake
@@ -8,10 +8,17 @@ if(EXISTS "${CURRENT_INSTALLED_DIR}/share/clapack/copyright")
     message(FATAL_ERROR "Can't build ${PORT} if clapack is installed. Please remove clapack:${TARGET_TRIPLET}, and try to install ${PORT}:${TARGET_TRIPLET} again.")
 endif()
 
-include(vcpkg_find_fortran)
 SET(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
 set(lapack_ver 3.10.0)
+
+x_vcpkg_find_fortran(OUT_OPTIONS Fortran_opts 
+                     OUT_OPTIONS_RELEASE Fortran_opts_rel 
+                     OUT_OPTIONS_DEBUG Fortran_opts_dbg)
+
+if(Z_VCPKG_IS_INTERNAL_Fortran_INTEL OR VCPKG_DETECTED_CMAKE_Fortran_COMPILER MATCHES "ifort${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+    set(PATCHES intel.patch)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -19,10 +26,11 @@ vcpkg_from_github(
     REF "v${lapack_ver}"
     SHA512 56055000c241bab8f318ebd79249ea012c33be0c4c3eca6a78e247f35ad9e8088f46605a0ba52fd5ad3e7898be3b7bc6c50ceb3af327c4986a266b06fe768cbf
     HEAD_REF master
+    PATCHES ${PATCHES}
 )
 
 if(NOT VCPKG_TARGET_IS_WINDOWS)
-    set(ENV{FFLAGS} "$ENV{FFLAGS} -fPIC")
+    set(ENV{FFLAGS} "$ENV{FFLAGS} -fPIC") # Should probably be in the toolchain
 endif()
 
 set(CBLAS OFF)
@@ -38,27 +46,14 @@ if("noblas" IN_LIST FEATURES)
     set(USE_OPTIMIZED_BLAS ON)
 endif()
 
-set(VCPKG_CRT_LINKAGE_BACKUP ${VCPKG_CRT_LINKAGE})
-vcpkg_find_fortran(FORTRAN_CMAKE)
-if(VCPKG_USE_INTERNAL_Fortran)
-    if(VCPKG_CRT_LINKAGE_BACKUP STREQUAL static) 
-    # If openblas has been built with static crt linkage we cannot use it with gfortran!
-        set(USE_OPTIMIZED_BLAS OFF) 
-        #Cannot use openblas from vcpkg if we are building with gfortran here. 
-        if("noblas" IN_LIST FEATURES)
-            message(FATAL_ERROR "Feature 'noblas' cannot be used without supplying an external fortran compiler")
-        endif()
-    endif()
-else()
-    set(USE_OPTIMIZED_BLAS ON)
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         "-DUSE_OPTIMIZED_BLAS=${USE_OPTIMIZED_BLAS}"
         "-DCBLAS=${CBLAS}"
-        ${FORTRAN_CMAKE}
+        ${Fortran_opts}
+    OPTIONS_DEBUG ${Fortran_opts_rel}
+    OPTIONS_RELEASE ${Fortran_opts_dbg}
 )
 
 vcpkg_cmake_install()
@@ -106,7 +101,7 @@ vcpkg_fixup_pkgconfig()
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 # remove debug includes
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 if(VCPKG_TARGET_IS_WINDOWS)
     if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/liblapack.lib")
