b90a3ec5b5a2f6dbfd477f45574223922db7f446
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-09 20:51:39 -0400

    [ports/google-cloud-c/portfile.cmake] { [google_cloud_c/client/cloud_auth.h] Put assert in macro `IS_CURLUE_OK`; [google_cloud_c/{client/cloud_auth.c,pubsub/policy.c,userinfo/userinfo.c}] Use `IS_CURLUE_OK`; [google_cloud_c/client/cloud_auth.c] Handle empty `path`s }

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             ab855001a23e765d849b23f3991f4919cd641404
-    SHA512          c41836b082928ba9f26b50d4610e89fdac1c1c1c1df4f9f9e124eb6963b137a4bc719ebf3e1aa0c1d1c2decbf7dded1e74854a6a5dd81b92adfc8857c678d966
+    REF             f8cd1e9ac117f75623630be7749fd6fae6ba976f
+    SHA512          c653197431a1b081f24a20ae321ca213e8ca40859fa5388b60314898433b2f5a0943dc78f73a31847d7a09c3d0b7e30575a89fcf57aa262e6c0fa352bcde3a8f
     HEAD_REF        master
 )
 
