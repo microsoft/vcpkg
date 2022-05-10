30d10bf121e25eb9f131fee96d0fd7fe615c4b11
neargye <neargye@gmail.com>
2022-05-11 00:15:44 +0400

    fix deprecated

--- ports/magic-enum/portfile.cmake
+++ ports/magic-enum/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
@@ -18,9 +18,9 @@ vcpkg_configure_cmake(
         -DMAGIC_ENUM_OPT_INSTALL=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/magic_enum TARGET_PATH share/magic_enum)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/magic_enum TARGET_PATH share/magic_enum)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
 
