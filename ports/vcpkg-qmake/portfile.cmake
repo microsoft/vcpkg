4d2e94c5a0bca00d33ca3023732603818fbbd801
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-05-10 23:57:45 +0200

    remove coypright copying.

--- ports/vcpkg-qmake/portfile.cmake
+++ ports/vcpkg-qmake/portfile.cmake
@@ -1,7 +1,6 @@
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_qmake_configure.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
-    "${CMAKE_CURRENT_LIST_DIR}/copyright"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 file(INSTALL "${VCPKG_ROOT_DIR}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
