85a64021912ae84949fc49cd33b15efb234db199
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-10 00:27:12 -0400

    [ports/google-cloud-c/vcpkg.json] Remove cauthflow dependency (only required in test); [ports/google-cloud-c/portfile.cmake] { [google_cloud_c/client/CMakeLists.txt] Remove `common` & `compute` dependencies on client to resolve weak cyclic dependency issue }

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             7ac6be153c3bf92f703d85e2ba28ca7a4feb2e7e
-    SHA512          98cb6350e6ef738cb029115b5db37fdaa1228edf660621339f3bea8cb47598c152d84cc90d058b746ebdd459aef26befe14f8635ae6e5e0ce7d11c381b6c24f2
+    REF             4617faeeb434efbdfbb1a972bb486a460f56caec
+    SHA512          fe5101a20757af00b682058f3b431fcd6c8aed91721be00758a2f7248022065e7789564882061273a6f1a2e53e5748c1927c670515986271d0a31eb35a1ceb14
     HEAD_REF        master
 )
 
