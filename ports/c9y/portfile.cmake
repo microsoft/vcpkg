590c91f76a659d9bb7fa1a60ab03beff84026b05
Sean Farrell <sean.farrell@rioki.org>
2022-05-10 15:51:39 +0200

    Implements review suggestions.

--- ports/c9y/portfile.cmake
+++ ports/c9y/portfile.cmake
@@ -6,11 +6,11 @@ vcpkg_from_github(
     )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-configure_file(${SOURCE_PATH}/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
