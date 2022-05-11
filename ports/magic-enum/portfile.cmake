acf1ecd5cafa98fbb645aac4f05873f8460c42e4
neargye <neargye@gmail.com>
2022-05-11 12:52:13 +0400

    delete extra blank line

--- ports/magic-enum/portfile.cmake
+++ ports/magic-enum/portfile.cmake
@@ -10,7 +10,6 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-
     OPTIONS
         -DMAGIC_ENUM_OPT_ENABLE_NONASCII=OFF
         -DMAGIC_ENUM_OPT_BUILD_EXAMPLES=OFF
