859b8757ac66ecde057a41ad39abd4b49148c41b
FrankXie <v-frankxie@microsoft.com>
2022-05-10 23:04:55 -0700

    [akali] Update to v1.43

--- ports/akali/portfile.cmake
+++ ports/akali/portfile.cmake
@@ -8,33 +8,33 @@ vcpkg_from_github(
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" AKALI_STATIC)
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DAKALI_STATIC:BOOL=${AKALI_STATIC}
         -DBUILD_TESTS:BOOL=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 
-if(EXISTS ${CURRENT_PACKAGES_DIR}/lib/cmake/akali)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/akali)
-elseif(EXISTS ${CURRENT_PACKAGES_DIR}/share/akali)
-    vcpkg_fixup_cmake_targets(CONFIG_PATH share/akali)
+if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/akali")
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/akali)
+elseif(EXISTS "${CURRENT_PACKAGES_DIR}/share/akali")
+    vcpkg_cmake_config_fixup(CONFIG_PATH share/akali)
 endif()
 
-file(READ ${CURRENT_PACKAGES_DIR}/include/akali/akali_export.h AKALI_EXPORT_H)
+file(READ "${CURRENT_PACKAGES_DIR}/include/akali/akali_export.h" "${AKALI_EXPORT_H}")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     string(REPLACE "#ifdef AKALI_STATIC" "#if 1" AKALI_EXPORT_H "${AKALI_EXPORT_H}")
 else()
     string(REPLACE "#ifdef AKALI_STATIC" "#if 0" AKALI_EXPORT_H "${AKALI_EXPORT_H}")
 endif()
-file(WRITE ${CURRENT_PACKAGES_DIR}/include/akali/akali_export.h "${AKALI_EXPORT_H}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/include/akali/akali_export.h" "${AKALI_EXPORT_H}")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-vcpkg_copy_pdbs()
+
