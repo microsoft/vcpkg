f496c465925bb604ecf556f7df332a0e1939a9e1
Billy Robert O'Neal III <bion@microsoft.com>
2022-05-12 22:18:58 -0700

    Update CUDA signing key.

--- scripts/azure-pipelines/linux/provision-image.sh
+++ scripts/azure-pipelines/linux/provision-image.sh
@@ -64,7 +64,7 @@ curl -sSL https://get.haskellstack.org/ | sh
 # Install CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 apt-get -y update
 apt-get install -y --no-install-recommends cuda-compiler-11-6 cuda-libraries-dev-11-6 cuda-driver-dev-11-6 \
