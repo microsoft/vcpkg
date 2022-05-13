88bec72fe415eadf248d0ffb706005c2d97ff6f1
Weiwen Zhao <panda.hust@gmail.com>
2022-05-13 20:42:52 +0800

    [paraview] Update to 5.10.0

--- ports/paraview/portfile.cmake
+++ ports/paraview/portfile.cmake
@@ -1,46 +1,47 @@
 file(READ "${CMAKE_CURRENT_LIST_DIR}/vcpkg.json" _vcpkg_json)
-string(JSON _ver_string GET "${_vcpkg_json}" "version")
+string(JSON _ver_string GET "${_vcpkg_json}" "version-semver")
 string(REGEX MATCH "^[0-9]+\.[0-9]+" VERSION "${_ver_string}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    "cuda"         PARAVIEW_USE_CUDA            #untested; probably only affects internal VTK build so it does nothing here 
-    "all_modules"  PARAVIEW_BUILD_ALL_MODULES   #untested
-    "mpi"          PARAVIEW_USE_MPI             #untested
-    "vtkm"         PARAVIEW_USE_VTKM
-    "python"       PARAVIEW_USE_PYTHON
-    "tools"        PARAVIEW_BUILD_TOOLS
+    FEATURES
+        "cuda"         PARAVIEW_USE_CUDA            #untested; probably only affects internal VTK build so it does nothing here
+        "all_modules"  PARAVIEW_BUILD_ALL_MODULES   #untested
+        "mpi"          PARAVIEW_USE_MPI             #untested
+        "vtkm"         PARAVIEW_USE_VTKM
+        "python"       PARAVIEW_USE_PYTHON
+        "tools"        PARAVIEW_BUILD_TOOLS
 )
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Kitware/ParaView
-    REF aad4b6f1e92154879209102edfab8367f1e7d191 # v5.9.1
-    SHA512  330fcb8525bdee9b02e06f05d4e91cc4d631d03df99c30f82bb97da5e06b5a2a6ff4ecee807b6f6c7110d2f53db1c17e4670d6078ae1cc89cfd7089b67d05bdb
+    REF d45270f25ec5714d845450b10d8f9cb409438d8d # v5.10.1
+    SHA512 e20fd30713f0628c8c72e38be0b9ddd2b0c52524131f04b24ab0fdd93982e5d386f1badb49c7edfc2373715b23dfdcf94d9d22ecfd1a0015efda8751e600457f
     HEAD_REF master
     PATCHES
         external_vtk.patch
-        cgns.patch
         python_include.patch
         python_wrapper.patch
         add-tools-option.patch
-        catalyst_install.patch
+        vtkPVStringFormatter.patch  # Borrow from https://github.com/Kitware/ParaView/commit/a40d6a3d43d7d77f5869e6c047aa4236caa24e92
+        ce4e972.patch # https://github.com/Kitware/ParaView/commit/ce4e972c7b6e9982ff2709d11258a0ddaa04b03e.diff
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     list(APPEND VisItPatches removedoublesymbols.patch)
 endif()
 
-#The following two dependencies should probably be their own port 
-#but require additional patching in paraview to make it work. 
+#The following two dependencies should probably be their own port
+#but require additional patching in paraview to make it work.
 
 #Get VisItBridge Plugin
 vcpkg_from_gitlab(
-    OUT_SOURCE_PATH VISITIT_SOURCE_PATH
+    OUT_SOURCE_PATH VISIT_SOURCE_PATH
     GITLAB_URL https://gitlab.kitware.com/
     REPO paraview/visitbridge
-    REF 42fce8ad6863ca2c1308741955cca1d0cf570d22
-    SHA512 03a6254989d3e286a462683af92caba1e90decbdcfb2e729f2d7e1116b04d63a05c28d02c4615d780fdd0d33e2719f96617233d6e0602410cc6d894f92fe6ee3
-    PATCHES 
+    REF 6f209632ce83d0068cb351ee8ae1c69ab80ec0e9
+    SHA512 7b406da0558ac025b6ca2f490041686804f8099e2361fc839715ee71a0ccff17696c677978d0cc10ead3d49180ea65b823087870b75a4a4fcf454c8c5b9176be
+    PATCHES
         ${VisItPatches}
 )
 #Get QtTesting Plugin
@@ -48,8 +49,8 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH QTTESTING_SOURCE_PATH
     GITLAB_URL https://gitlab.kitware.com/
     REPO paraview/qttesting
-    REF 72290689c7c55622d729bf95c97e7627026a234e
-    SHA512  fb18c6745b784b294f01d5391ba4cdcaa109443a193eb35fbf1553fdb3a4f7217f784fd4893fab72784cec5bd3fc821bf1e766e943d0f562c5917788800599b0
+    REF d9d12c9817ae7d6ae92394e8b714b30f343feaab
+    SHA512 642da6c0c4f603f7691480d373d04a267cd49a26e803893cbeb59015b7329ee17dae500130f6eb48222bfb098876f9c562a89aea656e076ddbcb1209b90b49f3
 )
 
 #Get Catalyst
@@ -57,12 +58,12 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH CATALYST_SOURCE_PATH
     GITLAB_URL https://gitlab.kitware.com/
     REPO paraview/catalyst
-    REF e36e4a5f3c67011c97c335cce23d2bc3abc0d086
-    SHA512  9926c272ab8785997f9c98cfaf696943081b0ddb0e9e343602722671b6f3eaef5b8de5dd049ca783b6844c7e328a96e1b09c8b24c16f001eeeed2d154d290480
+    REF 25994d8141795b57b6cb6d9e3d7655b82e3da74c
+    SHA512 4eb3b678df31f6ce843febaab2d5c6777ec84a64d5e3b3aa95d2912f7dfd99faf7d68288513d2364e2b218d619f0331f287aa51327c5405fd0a25a13df41053e
 )
 
 
-file(COPY "${VISITIT_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/Utilities/VisItBridge")
+file(COPY "${VISIT_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/Utilities/VisItBridge")
 file(COPY "${QTTESTING_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/ThirdParty/QtTesting/vtkqttesting")
 file(COPY "${CATALYST_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/ThirdParty/catalyst/vtkcatalyst/catalyst")
 
@@ -71,17 +72,19 @@ if("python" IN_LIST FEATURES)
     list(APPEND ADDITIONAL_OPTIONS
         -DPython3_FIND_REGISTRY=NEVER
         "-DPython3_EXECUTABLE:PATH=${PYTHON3}" # Required by more than one feature
-        )
+    )
     #VTK_PYTHON_SITE_PACKAGES_SUFFIX should be set to the install dir of the site-packages
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-     OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS
+        ${FEATURE_OPTIONS}
         -DPARAVIEW_BUILD_WITH_EXTERNAL:BOOL=ON
         -DPARAVIEW_USE_EXTERNAL_VTK:BOOL=ON
         -DPARAVIEW_ENABLE_VISITBRIDGE:BOOL=ON
         -DVTK_MODULE_ENABLE_ParaView_qttesting=YES
+        -DVTK_MODULE_USE_EXTERNAL_ParaView_vtkcatalyst:BOOL=OFF
         -DPARAVIEW_ENABLE_EMBEDDED_DOCUMENTATION:BOOL=OFF
         -DPARAVIEW_USE_QTHELP:BOOL=OFF
 
@@ -90,11 +93,12 @@ vcpkg_cmake_configure(
 
         # Workarounds for CMake issues
         -DHAVE_SYS_TYPES_H=0    ## For some strange reason the test first succeeds and then fails the second time around
-        -DWORDS_BIGENDIAN=0     ## Tests fails in VisItCommon.cmake for some unknown reason this is just a workaround since most systems are little endian. 
+        -DWORDS_BIGENDIAN=0     ## Tests fails in VisItCommon.cmake for some unknown reason this is just a workaround since most systems are little endian.
         ${ADDITIONAL_OPTIONS}
 
         #-DPARAVIEW_ENABLE_FFMPEG:BOOL=OFF
 )
+
 if(CMAKE_HOST_UNIX)
     # ParaView runs Qt tools so LD_LIBRARY_PATH must be set correctly for them to find *.so files
     set(BACKUP_LD_LIBRARY_PATH $ENV{LD_LIBRARY_PATH})
@@ -140,7 +144,7 @@ foreach(tool ${TOOLS})
     if(EXISTS ${filename})
         file(REMOVE "${filename}")
     endif()
-    
+
     # Move release tools
     set(filename ${CURRENT_PACKAGES_DIR}/bin/${tool}${VCPKG_TARGET_EXECUTABLE_SUFFIX})
     if(EXISTS ${filename})
@@ -155,7 +159,7 @@ foreach(tool ${TOOLS})
 endforeach()
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/${PORT})
 
-# # Handle copyright
+# Handle copyright
 file(INSTALL "${SOURCE_PATH}/Copyright.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME Copyright.txt) # Which one is the correct one?
 file(INSTALL "${SOURCE_PATH}/License_v1.2.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
@@ -168,7 +172,7 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
             file(RENAME "${CURRENT_PACKAGES_DIR}/debug/bin/${name}" "${CURRENT_PACKAGES_DIR}/debug/lib/${name}")
         endif()
     endmacro()
-    
+
     set(to_move Lib paraview-${VERSION} paraview-config)
     foreach(name ${to_move})
         move_bin_to_lib(${name})
