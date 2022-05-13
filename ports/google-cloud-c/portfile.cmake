e8577cd7d1da025272675caebe766f2721234915
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-13 19:29:40 -0400

    [ports/google-cloud-c/portfile.cmake] { [google_cloud_c_parse_emit_tests] Start writing tests for parsers/emitters starting with `policy`; [google_cloud_c/pubsub/policy.c] Fix parsing bug in `GetPolicyOptions_to_json` } ; [ports/google-cloud-c/vcpkg.json] Bump version-date

--- ports/google-cloud-c/portfile.cmake
+++ ports/google-cloud-c/portfile.cmake
@@ -1,15 +1,17 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             4c6243badaed5a05605ad3b0432a3b183d984285
-    SHA512          7e0241f95d65bb90047991b0763aae46288d8695813134be6b9ede4ef3ed151b96e95ed70e4efddf2155fc76b8ca05660efeaa94d1acac370a59de0b4ed7b3fe
+    REF             7bc35317a460581fc1b594efee23749cde6105c8
+    SHA512          16c0673c60b24f7e0d7551b0785929b027967e38f79f2a070b524831b4e023b635b9b1edf36cfc1f4fec2b41554de51912824a31a4ddccf0d70f8de4c6eb45fb
     HEAD_REF        master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_TESTING=OFF
+        "-DBUILD_TESTING=OFF"
+        "-DBUILD_REAL_API_TESTING=OFF"
+        "-DBUILD_PARSE_EMIT_TESTS=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/License.txt"
