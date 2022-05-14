ba4b27938bf228eab972c1d7f3a9af109bb5a20f
Hamish Moffatt <hamish@risingsoftware.com>
2022-05-14 18:33:05 +1000

    Argon2 port

---
+++ ports/argon2/portfile.cmake
@@ -0,0 +1,34 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO P-H-C/phc-winner-argon2
+    REF 20190702
+    SHA512 0a4cb89e8e63399f7df069e2862ccd05308b7652bf4ab74372842f66bcc60776399e0eaf979a7b7e31436b5e6913fe5b0a6949549d8c82ebd06e0629b106e85f
+    HEAD_REF master
+    PATCHES
+        visibility.patch
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION  "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/argon2.pc.in" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        hwopt     OPTIMIZATIONS
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
+    OPTIONS_DEBUG
+        -DARGON2_SKIP_HEADERS=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_pkgconfig()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+vcpkg_copy_tools(TOOL_NAMES argon2 AUTO_CLEAN)
