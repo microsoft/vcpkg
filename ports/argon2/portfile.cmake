94e5274ea1817c59d840133c61ecc9ecf37dfbd2
Hamish Moffatt <hamish@moffatt.id.au>
2022-05-16 09:53:18 +1000

    add feature to enable hardware optimizations

--- ports/argon2/portfile.cmake
+++ ports/argon2/portfile.cmake
@@ -8,9 +8,17 @@ vcpkg_from_github(
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION  "${SOURCE_PATH}")
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        hwopt     OPTIMIZATIONS
+)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
+    OPTIONS
+        ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         -DARGON2_SKIP_HEADERS=ON
 )
