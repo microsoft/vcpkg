e722b1ab3fa4652b815db58b4e3619f9cbdbe35e
FrankXie <v-frankxie@microsoft.com>
2022-05-15 23:40:18 -0700

    update v2.1.0

--- ports/termcolor/portfile.cmake
+++ ports/termcolor/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ikalnytskyi/termcolor
-    REF b3cb0f365f8435588df7a6b12a82b2ac5fc1fe95 #v2.0.0
+    REF b3cb0f365f8435588df7a6b12a82b2ac5fc1fe95 #v2.1.0
     SHA512 8a95c654b68728a2258eba1c40daf0e5cc69ba24e15e839f75341a694a20a930c042820d68c661ca1971b68dcc93f895513dc73774818b94e205a3a73199b550
     HEAD_REF master
 )
