7c89d0f5f3ee53348e3ae9741824542d12b87d92
Ryan <ryan.mckenna002@gmail.com>
2022-05-19 17:44:18 +0100

    set ref to latest commit id

--- ports/linmath/portfile.cmake
+++ ports/linmath/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO datenwolf/linmath.h
-    REF refs/heads/master
+    REF refs/heads/master/0538757
     SHA512 d32276efb6c0065bfb79dee3a3f624369c64e2ce4e5265e371be4c21e779a6dbf2a2e7ecfad1530cb70ad0d3ac8982faca330622df6d9ed13ce548e7e91f8de7
     HEAD_REF master
 )
