0b2585305095c11c201e1cb8f17aa94076011b50
JackBoosY <yuzaiyang@beyondsoft.com>
2022-05-19 00:05:33 -0700

    typo

--- ports/msmpi/portfile.cmake
+++ ports/msmpi/portfile.cmake
@@ -120,7 +120,7 @@ if(VCPKG_CRT_LINKAGE STREQUAL "static")
 endif()
 
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/MPI")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 # Handle copyright
 file(COPY "${SOURCE_PATH}/sdk/PFiles/Microsoft SDKs/MPI/License/MicrosoftMPI-SDK-EULA.rtf" DESTINATION ${CURRENT_PACKAGES_DIR}/share/msmpi)
