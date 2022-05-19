706da4be8e53b0d7c21051f37499ba42ec1fdb1c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-19 17:19:09 -0400

    [ports/tree-sitter-c/portfile.cmake] { [**CMakeLists.txt] `install` headers to `"${CMAKE_INSTALL_INCLUDEDIR}/${PROJECT_NAME}"` instead of `${CMAKE_INSTALL_INCLUDEDIR}` } ; [ports/tree-sitter-c/vcpkg.json] Match version to `master` version

--- ports/tree-sitter-c/portfile.cmake
+++ ports/tree-sitter-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "SamuelMarks/${PORT}"
-    REF             bf97c9482e90af607ff76346e14d4626aba0f1f9
-    SHA512          779f2eb4e57cab5fc26ed272af8e2a0249f4a683c7ee1ca929bacec0f925cd923e2a339423c1975b1c2081639cb9326f6760c163c94af1e4fc5d776712614aa9
+    REF             8aa3dd454aee6a8851b673d212a6954c357d6559
+    SHA512          7d83562f103734c53ca6ec05d3e9b918b4cfce13d965a8b38a0009e0eaa3390d9b98b61ddaae24f49f0d8d7559a609a66c05365761cd2ec1be548780dbfc12e0
     HEAD_REF        cmake
 )
 
