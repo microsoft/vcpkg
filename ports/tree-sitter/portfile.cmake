182f93b29195f8ee2c58fa4a7c6296d93280098a
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-20 12:59:56 -0400

    [ports/tree-sitter/portfile.cmake] { [cmake,**CMakeLists.txt] Rename '_' to '-'; install headers to 'tree_sitter'; utilise  library for 'tree_sitter' include }

--- ports/tree-sitter/portfile.cmake
+++ ports/tree-sitter/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "SamuelMarks/${PORT}"
-    REF             95773160c418a44449e1bd03e4c0aeae9e67cd11
-    SHA512          0946d9b02e0640293cf3d452843dbf1a2477e849a4b4f83c80fedf41e81c0c763b65762c998fc1171302f86a1ea7ec6ba18e64b424bacd1448c2bb43e3c145e2
+    REF             af293c7765d41836b0f55cae181c39199c36b9ff
+    SHA512          0a5b84ec31c9c40a2ce6ce5adecb38f3f5faeae6d8aa3266b115b8340af6117201a81aa75b10f3e0bc765222b2b34837b116477d7af7cce01edcf3cc25502fbf
     HEAD_REF        cmake
 )
 
