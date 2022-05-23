4533c5befaa3227f084ecc43f96d8d2155f09cec
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-05-23 21:04:44 +0200

    set MESON_SCRIPT instead of MESON.

--- ports/vcpkg-tool-meson/vcpkg-port-config.cmake
+++ ports/vcpkg-tool-meson/vcpkg-port-config.cmake
@@ -1,2 +1,1 @@
-vcpkg_find_acquire_program(PYTHON3)
-set(MESON "${PYTHON3}" "${CMAKE_CURRENT_LIST_DIR}/../../tools/meson/meson.py")
+set(MESON_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../../tools/meson/meson.py")
