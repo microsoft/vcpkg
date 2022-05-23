51cd043847e0b3cde5d82ebdb4aa06350d5e3022
Paul Levy <plevy@pelicanmapping.com>
2022-05-23 18:12:14 -0400

    Updated portfile and patches for the osgEarth 3.3 release

--- ports/osgearth/portfile.cmake
+++ ports/osgearth/portfile.cmake
@@ -11,18 +11,51 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gwaldron/osgearth
-    REF 15d5340f174212d6f93ae55c0d9af606c3d361c0 #version 3.2
-    SHA512 f922e8bbb041a498e948587f03e8dc8a07b92e641f38d50a8eafb8b3ce1e0c92bb1ee01360d57e794429912734b60cf05ba143445a442bc95af39e3dd9fc3670
+    REF 6b5fb806a9190f7425c32db65d3ea905a55a9c16 #version 3.3
+    SHA512 fe79ce6c73341f83d4aee8cb4da5341dead56a92f998212f7898079b79725f46b2209d64e68fe3b4d99d3c5c25775a8efd1bf3c3b3a049d4f609d3e30172d3bf
     HEAD_REF master
     PATCHES
         StaticOSG.patch # Fix port compilation in static-md module
         make-all-find-packages-required.patch
         fix-dependency-osg.patch
         remove-tool-debug-suffix.patch
-        fix-imgui.patch
-        fix-gcc11-compilation.patch
-        blend2d-fix.patch
 )
+
+message(STATUS "Downloading submodules")
+# Download all of the submodules from github manually since vpckg doesn't support submodules natively.
+# IMGUI
+vcpkg_from_github(
+    OUT_SOURCE_PATH IMGUI_SOURCE_PATH
+    REPO ocornut/imgui
+    REF 9e8e5ac36310607012e551bb04633039c2125c87 #master
+    SHA512 1f1f743833c9a67b648922f56a638a11683b02765d86f14a36bc6c242cc524c4c5c5c0b7356b8053eb923fafefc53f4c116b21fb3fade7664554a1ad3b25e5ff
+    HEAD_REF master
+)
+
+# LERC
+vcpkg_from_github(
+    OUT_SOURCE_PATH LERC_SOURCE_PATH
+    REPO Esri/lerc
+    REF 19542a00b9a8b5c1089f74239e5859e02e403212 #v2.2.1
+    SHA512 3a4d3049da08c2303c74ce8767a3e588e8eb024f63a04c7f93ccdbbcc0fbcc85870f1c339591e0900a2cd5fe607afcd07c21ad5e4a03c8c61620fe7e2c131501
+    HEAD_REF v2.2.1
+)
+
+# RAPIDJSON
+vcpkg_from_github(
+    OUT_SOURCE_PATH RAPIDJSON_SOURCE_PATH
+    REPO Tencent/rapidjson
+    REF f54b0e47a08782a6131cc3d60f94d038fa6e0a51 #v1.1.0
+    SHA512 f30796721c0bfc789d91622b3af6db8d4fb4947a6da3fcdd33e8f37449a28e91dbfb23a98749272a478ca991aaf1696ab159c53b50f48ef69a6f6a51a7076d01
+    HEAD_REF v1.1.0
+)
+
+# Copy the submodules to the right place
+file(COPY ${IMGUI_SOURCE_PATH}/ DESTINATION ${SOURCE_PATH}/src/third_party/imgui)
+file(COPY ${LERC_SOURCE_PATH}/ DESTINATION ${SOURCE_PATH}/src/third_party/lerc)
+file(COPY ${RAPIDJSON_SOURCE_PATH}/ DESTINATION ${SOURCE_PATH}/src/third_party/rapidjson)
+
+
 
 # Upstream bug, see https://github.com/gwaldron/osgearth/issues/1002
 file(REMOVE "${SOURCE_PATH}/src/osgEarth/tinyxml.h")
@@ -53,6 +86,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake/)
+
+
 if (WIN32 AND (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic"))
     #Release
     set(OSGEARTH_TOOL_PATH "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
@@ -79,10 +115,13 @@ endif()
 
 if ("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES osgearth_3pv osgearth_atlas osgearth_boundarygen osgearth_clamp
-        osgearth_conv osgearth_imgui osgearth_overlayviewer osgearth_tfs osgearth_toc osgearth_version osgearth_viewer
+        osgearth_conv osgearth_imgui osgearth_tfs osgearth_toc osgearth_version osgearth_viewer osgearth_createtile
+		osgearth_mvtindex
         AUTO_CLEAN
     )
 endif()
+
+
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
