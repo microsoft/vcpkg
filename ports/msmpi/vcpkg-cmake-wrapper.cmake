0b5186d0b3e1f8ad44a6e90f6194915e97136b92
JackBoosY <yuzaiyang@beyondsoft.com>
2022-05-24 00:06:06 -0700

    move unset before _find_package

--- ports/msmpi/vcpkg-cmake-wrapper.cmake
+++ ports/msmpi/vcpkg-cmake-wrapper.cmake
@@ -4,7 +4,6 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 
 # Suitable for FindMPI.cmake line 937
 set(ENV{MSMPI_INC} "${_IMPORT_PREFIX}/include")
+unset(_IMPORT_PREFIX)
 
 _find_package(${ARGS})
-
-unset(_IMPORT_PREFIX)
