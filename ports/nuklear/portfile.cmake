c63177c8a67d80cad075051d48ded4f6304ee979
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-05-24 11:27:57 -0400

    [ports/nuklear/portfile.cmake] Remove quotes around `${FEATURE_OPTIONS}`

--- ports/nuklear/portfile.cmake
+++ ports/nuklear/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS     "${FEATURE_OPTIONS}"
+    OPTIONS     ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
