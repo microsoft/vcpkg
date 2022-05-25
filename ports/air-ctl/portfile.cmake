79e3e20d77d444cc1de723281e2a59bd3ada607d
然Y <1294379167@qq.com>
2022-05-25 18:31:30 +0800

    Update portfile.cmake

--- ports/air-ctl/portfile.cmake
+++ ports/air-ctl/portfile.cmake
@@ -16,4 +16,4 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright).
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
