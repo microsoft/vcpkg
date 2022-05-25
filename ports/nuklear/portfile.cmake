5858af159a63fb9f88d68182221e67e3ceef4d4e
Robert Schumacher <roschuma@microsoft.com>
2022-05-25 19:31:25 +0000

    [nuklear] Revert reformat

--- ports/nuklear/portfile.cmake
+++ ports/nuklear/portfile.cmake
@@ -1,13 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO            Immediate-Mode-UI/Nuklear
-    REF             0a77211844458673446aa99b8a0ae1090a04e413 # 2022-05-12
-    SHA512          57ad5578c534d19ce6dd5e9b8c7b725da6e7f379a5289daed6b7128a1671dc5c368175d1e780a7fcb3405fad989e3bc50a8a921d62301e64e58014e351845cae
-    HEAD_REF        master
+    REPO Immediate-Mode-UI/Nuklear
+    REF 0a77211844458673446aa99b8a0ae1090a04e413 # 2022-05-12
+    SHA512 57ad5578c534d19ce6dd5e9b8c7b725da6e7f379a5289daed6b7128a1671dc5c368175d1e780a7fcb3405fad989e3bc50a8a921d62301e64e58014e351845cae
+    HEAD_REF master
 )
 
-file(COPY "${CURRENT_PORT_DIR}/CMakeLists.txt"
-     DESTINATION "${SOURCE_PATH}")
+file(COPY "${CURRENT_PORT_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -16,8 +15,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS     ${FEATURE_OPTIONS}
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
@@ -27,6 +26,4 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-nuklear)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-file(INSTALL "${SOURCE_PATH}/src/LICENSE"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-     RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/src/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
