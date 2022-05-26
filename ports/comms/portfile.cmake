b3779bc4936f3b46775d2f6b408b9f57917ddcc4
Mathis Logemann <mathisloge@gmail.com>
2022-05-26 17:10:23 +0200

    apply cr

--- ports/comms/portfile.cmake
+++ ports/comms/portfile.cmake
@@ -11,6 +11,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCC_COMMS_BUILD_UNIT_TESTS=OFF
+        -DBUILD_TESTING=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME LibComms CONFIG_PATH lib/LibComms/cmake)
