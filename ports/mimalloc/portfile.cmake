cb7ac1e715fb248f2fd922d3d14eb6a4c4a868d0
Rémy Tassoux <contact@rt2.fr>
2022-05-31 14:18:46 +0200

    [mimalloc] Rework the override feature

--- ports/mimalloc/portfile.cmake
+++ ports/mimalloc/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 f2fc0fbfb6384e85959897f129e5d5d9acc51bda536d5cabcd7d4177dbda9fb735b8a8c239b961f8bea31d37c9ae10f66da23aa91d497f95393253d4ac792bb3
     HEAD_REF master
     PATCHES
-        fix-cmake.patch
+        fix-install-paths.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -14,8 +14,23 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         secure      MI_SECURE
         override    MI_OVERRIDE
 )
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" MI_BUILD_STATIC)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" MI_BUILD_SHARED)
+
+if(MI_OVERRIDE AND VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(
+        ONLY_DYNAMIC_LIBRARY
+        ONLY_DYNAMIC_CRT
+    )
+endif()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    if(NOT VCPKG_TARGET_IS_WINDOWS AND MI_OVERRIDE)
+        set(MI_BUILD_OBJECT ON)
+    else()
+        set(MI_BUILD_STATIC ON)
+    endif()
+elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    set(MI_BUILD_SHARED ON)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -24,13 +39,12 @@ vcpkg_cmake_configure(
     OPTIONS_RELEASE
         -DMI_DEBUG_FULL=OFF
     OPTIONS
-        -DMI_USE_CXX=ON
         -DMI_BUILD_TESTS=OFF
-        -DMI_BUILD_OBJECT=OFF
-        ${FEATURE_OPTIONS}
         -DMI_BUILD_STATIC=${MI_BUILD_STATIC}
         -DMI_BUILD_SHARED=${MI_BUILD_SHARED}
+        -DMI_BUILD_OBJECT=${MI_BUILD_OBJECT}
         -DMI_INSTALL_TOPLEVEL=ON
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
@@ -43,13 +57,19 @@ file(COPY
 )
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mimalloc)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    vcpkg_replace_string(
-        "${CURRENT_PACKAGES_DIR}/include/mimalloc.h"
-        "!defined(MI_SHARED_LIB)"
-        "0 // !defined(MI_SHARED_LIB)"
-    )
+if(MI_OVERRIDE)
+    file(REMOVE "${CURRENT_PACKAGES_DIR}/include/mimalloc-override.h")
+else()
+    if(VCPKG_TARGET_IS_WINDOWS)
+        file(REMOVE_RECURSE
+            "${CURRENT_PACKAGES_DIR}/debug/bin/mimalloc-redirect.dll"
+            "${CURRENT_PACKAGES_DIR}/bin/mimalloc-redirect.dll"
+            "${CURRENT_PACKAGES_DIR}/debug/bin/mimalloc-redirect32.dll"
+            "${CURRENT_PACKAGES_DIR}/bin/mimalloc-redirect32.dll"
+        )
+    endif()
 endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
