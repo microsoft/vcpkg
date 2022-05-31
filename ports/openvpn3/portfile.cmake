c0c93ae90f8ef41cf1624328e1d6789539e5a144
Mengna-Li <543250287@qq.com>
2022-05-31 05:49:22 +0000

    export cmake targets

--- ports/openvpn3/portfile.cmake
+++ ports/openvpn3/portfile.cmake
@@ -29,8 +29,9 @@ foreach(HEADER IN LISTS HEADERS)
     file(WRITE "${HEADER}" "${_contents}")
 endforeach()
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in" "${CURRENT_PACKAGES_DIR}/share/openvpn3/openvpn3-config.cmake" @ONLY)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-openvpn CONFIG_PATH share/unofficial-openvpn)
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL
     "${SOURCE_PATH}/COPYRIGHT.AGPLV3"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
