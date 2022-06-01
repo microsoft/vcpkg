45fe29b04a8d0882c104eb84476be67540f73959
Robert Schumacher <roschuma@microsoft.com>
2022-06-01 19:24:45 +0000

    [nethost] Only build one flavor

--- ports/nethost/portfile.cmake
+++ ports/nethost/portfile.cmake
@@ -40,10 +40,8 @@ endif()
 
 set(BASE_RID "${RID_PLAT}-${RID_ARCH}")
 
-set(NETHOST_SOURCE_PATH "${SOURCE_PATH}/src/native/corehost/nethost/")
-
 vcpkg_cmake_configure(
-    SOURCE_PATH "${NETHOST_SOURCE_PATH}"
+    SOURCE_PATH "${SOURCE_PATH}/src/native/corehost/nethost/"
     # vcpkg's /utf-8 is incompatible with dotnet's own /source-charset:utf-8
     NO_CHARSET_FLAG
     OPTIONS
@@ -54,8 +52,11 @@ vcpkg_cmake_configure(
         "-DCLI_CMAKE_COMMON_HOST_VER:STRING=${PRODUCT_VERSION}"
         "-DCLI_CMAKE_PKG_RID:STRING=${BASE_RID}"
         "-DCLI_CMAKE_COMMIT_HASH:STRING=${COMMIT_HASH}"
-        "-DCLI_CMAKE_PLATFORM_ARCH_${ARCH_NAME}=1"
+        "-DCLR_CMAKE_TARGET_ARCH_${ARCH_NAME}=1"
+        "-DCLR_CMAKE_TARGET_ARCH=${RID_ARCH}"
         "-DCLR_CMAKE_HOST_ARCH=${RID_ARCH}"
+    MAYBE_UNUSED_VARIABLES
+        SKIP_VERSIONING # only used on WIN32
 )
 
 vcpkg_cmake_install()
@@ -65,6 +66,12 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-nethost)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/nethost.h" "#ifdef NETHOST_USE_AS_STATIC" "#if 1")
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/nethost.h" "#ifdef NETHOST_USE_AS_STATIC" "#if 0")
+endif()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.TXT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
