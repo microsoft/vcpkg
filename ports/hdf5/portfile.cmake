5e9fa9a0aece7036c30dc08af0e29dd32a340bc4
JackBoosY <yuzaiyang@beyondsoft.com>
2022-06-02 00:33:33 -0700

    Apply suggestions

--- ports/hdf5/portfile.cmake
+++ ports/hdf5/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
         szip.patch
         pkgconfig-requires.patch
         pkgconfig-link-order.patch
-        fix-parallel.patch # Remove this patch in the next update
+        fix-parallel.patch # Remove this patch in the next update, see https://github.com/HDFGroup/hdf5/pull/860
 )
 
 set(ALLOW_UNSUPPORTED OFF)
