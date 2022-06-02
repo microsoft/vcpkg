bddb25e15335ed06b3669d3c2693d8288c66bfef
Benoit Lavallee <blavallee@coveo.com>
2022-06-02 14:34:19 -0400

    add SHA512

--- ports/bitmagic/portfile.cmake
+++ ports/bitmagic/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tlk00/BitMagic
     REF v7.11.2
-    SHA512 0
+    SHA512 182456fc351a7df0cba0bb5b35d91bb8055891bf91a09fc5394149f3b0b1bd85b63ce88497d62ed68bb892481c15264cf44059d43023d1f2155ed43cafe3b933
     HEAD_REF master
 
 )
