25e88cfe5cb9d0079821b52ed5f4319f9240d6ca
Dennis Hezel <dh@3yourmind.com>
2022-06-04 20:30:28 +0200

    Update gRPC to 1.46.3 and upb to 2022-06-01

--- ports/upb/portfile.cmake
+++ ports/upb/portfile.cmake
@@ -3,15 +3,22 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO protocolbuffers/upb
-    REF  160625a9728b4031a21ad1e1c0146ea2c3a851eb # 2021-10-19
-    SHA512 13b205dd4278600e6ec05c829dc6c7e449747cccb118a3b83abc0ab5ef0ab180feb364ac84da8075471697fbba798ed3d9d763934d7fe9a64ac0560f5f9d3e83
+    REF  e5f26018368b11aab672e8e8bb76513f3620c579 # 2022-06-01
+    SHA512 37c22ab7718a323971449a495fe7386c37c9c0ad2cbc72c17621b5c18ac433c79f0f34cbde4339805aacc47cad84c3ae6b029931fa9a1639d8644f4caf0e5528
     HEAD_REF master
     PATCHES
-        fix-uwp.patch
-        fix-cmakelists.patch
-        add-all-libs-target.patch
-        add-cmake-install.patch
+        0001-make-cmakelists-py.patch
+        0002-fix-uwp.patch
 )
+
+vcpkg_find_acquire_program(PYTHON3)
+
+vcpkg_execute_required_process(
+    COMMAND "${PYTHON3}" "${SOURCE_PATH}/cmake/make_cmakelists.py" "cmake/CMakeLists.txt"
+    WORKING_DIRECTORY "${SOURCE_PATH}"
+    LOGNAME make_cmakelists)
+
+vcpkg_replace_string("${SOURCE_PATH}/cmake/CMakeLists.txt" "/third_party/utf8_range)" "utf8_range)")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
@@ -21,6 +28,6 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/upb/fuzz" "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
