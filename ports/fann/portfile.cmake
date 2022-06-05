f87ca2639baf832c0aaec987f3b1a8bb95ff2d14
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-06-05 18:13:20 +0200

    cppcms not supported on static

--- ports/fann/portfile.cmake
+++ ports/fann/portfile.cmake
@@ -1,6 +1,3 @@
-# Official design
-#vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libfann/fann
@@ -56,5 +53,9 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL "${SOURCE_PATH}/COPYING.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
