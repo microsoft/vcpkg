10e63345ed6cc620794eba6e4205c5972f4a93f1
Dennis Hezel <dh@3yourmind.com>
2022-06-06 10:02:40 +0200

    Downgrade upb to the version used by gRPC. No longer installed generated descriptor.upb.h files just to use them when compiling target triplet

--- ports/upb/portfile.cmake
+++ ports/upb/portfile.cmake
@@ -3,17 +3,13 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO protocolbuffers/upb
-    REF  e5f26018368b11aab672e8e8bb76513f3620c579 # 2022-06-01
-    SHA512 37c22ab7718a323971449a495fe7386c37c9c0ad2cbc72c17621b5c18ac433c79f0f34cbde4339805aacc47cad84c3ae6b029931fa9a1639d8644f4caf0e5528
+    REF  bef53686ec702607971bd3ea4d4fefd80c6cc6e8 # 2022-04-04
+    SHA512 3801625827a5511ab28e047d7111e6f43d522872eef929b61d7a64fdb9fff7a058af45fd55bd409610c07c735f50a2f6f45ad611bb5e4ee86cd656b89f0a8278
     HEAD_REF master
     PATCHES
         0001-make-cmakelists-py.patch
         0002-fix-uwp.patch
 )
-
-if(NOT TARGET_TRIPLET STREQUAL HOST_TRIPLET)
-    vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/upb")
-endif()
 
 vcpkg_find_acquire_program(PYTHON3)
 
@@ -31,16 +27,16 @@ vcpkg_check_features(
 )
 
 if(NOT VCPKG_UPB_BUILD_CODEGEN)
-    set(VCPKG_UPB_HOST_INSTALLED_DIR_ARG "-DVCPKG_UPB_HOST_INSTALLED_DIR=${CURRENT_HOST_INSTALLED_DIR}")
+    vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf" "${CURRENT_HOST_INSTALLED_DIR}/tools/upb")
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cmake"
     OPTIONS ${FEATURE_OPTIONS}
-        "${VCPKG_UPB_HOST_INSTALLED_DIR_ARG}"
+        "-DVCPKG_UPB_HOST_INCLUDE_DIR=${CURRENT_HOST_INSTALLED_DIR}/include"
 )
 
-vcpkg_cmake_install()
+vcpkg_cmake_install(ADD_BIN_TO_PATH)
 vcpkg_cmake_config_fixup()
 
 if (VCPKG_UPB_BUILD_CODEGEN)
