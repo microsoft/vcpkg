7d2449c3467c2b14f9a2c988d030892272d19bd2
Robert Schumacher <roschuma@microsoft.com>
2022-06-07 16:26:51 -0700

    [docs] Delete embedded documentation in favor of docs/ (#25096)
    * [docs] Delete embedded documentation in favor of docs/
    
    Drive-by for the helper ports:
    1. "documentation": "https://vcpkg.io/en/docs/README.html"
    2. "license": "MIT"
    3. Use `include_guard(GLOBAL)` in all script files
    4. Make sure any persistent variables are saved to the cache
    
    * [docs] Restore empty regenerate.ps1 to satisfy Azure Pipelines
    
    * [docs] PR comments

--- scripts/cmake/vcpkg_execute_required_process_repeat.cmake
+++ scripts/cmake/vcpkg_execute_required_process_repeat.cmake
@@ -1,20 +1,3 @@
-#[===[.md:
-# vcpkg_execute_required_process_repeat
-
-Execute a process until the command succeeds, or until the COUNT is reached.
-
-## Usage
-```cmake
-vcpkg_execute_required_process_repeat(
-    COMMAND <cmd> [<arguments>]
-    COUNT <num>
-    WORKING_DIRECTORY <directory>
-    LOGNAME <name>
-    [ALLOW_IN_DOWNLOAD_MODE]
-)
-```
-#]===]
-
 function(vcpkg_execute_required_process_repeat)
     cmake_parse_arguments(PARSE_ARGV 0 arg
         "ALLOW_IN_DOWNLOAD_MODE"
