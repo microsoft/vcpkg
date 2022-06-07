82fb9f5599120b30d985c1251d4326a9bb34ada6
Jonliu1993 <13720414433@163.com>
2022-06-07 14:48:07 +0800

    [clickhouse-cpp] update to 2.1.0

--- ports/clickhouse-cpp/portfile.cmake
+++ ports/clickhouse-cpp/portfile.cmake
@@ -6,9 +6,6 @@ vcpkg_from_github(
     REF a85a9827792bb91642e0e4511e8083677f0c1b1e    #v2.1.0
     SHA512 ecdf8af8fa49c2ebaae7d4a345c8df1a5ab86a9f39b3a4c4e27ef962807d5b62cb0aec57ae246fdc1a47e02f4224c7c4c999fafb53a5208e8008b0e2e4349cb5
     HEAD_REF master
-    PATCHES 
-        #fix-error-C4996.patch  #fix x64-uwp error:std::uncaught_exception() is deprecated in C++17
-        fix-error-C2664.patch
 )
 
 vcpkg_cmake_configure(
