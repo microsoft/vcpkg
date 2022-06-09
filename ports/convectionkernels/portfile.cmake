7de9b78e360e8a563cba883a7380024f63a371e1
DragonJoker <dragonjoker59@hotmail.com>
2022-06-09 17:27:56 +0200

    [cvtt] Renamed to convectionkernels

--- ports/convectionkernels/portfile.cmake
+++ ports/convectionkernels/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-cvtt)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-convectionkernels)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
