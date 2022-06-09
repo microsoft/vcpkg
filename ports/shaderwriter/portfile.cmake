9906c558c1a535d8b7471013eb3f7a14d6a2a859
DragonJoker <dragonjoker59@hotmail.com>
2022-06-09 09:40:13 +0200

    [shaderwriter] Fixed call to vcpkg_cmake_config_fixup.

--- ports/shaderwriter/portfile.cmake
+++ ports/shaderwriter/portfile.cmake
@@ -31,6 +31,6 @@ vcpkg_cmake_configure(
 
 vcpkg_copy_pdbs()
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/shaderwriter)
+vcpkg_cmake_config_fixup(PACKAGE_NAME ShaderWriter CONFIG_PATH lib/cmake/shaderwriter)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
