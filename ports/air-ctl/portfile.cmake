4706a778be6bfbf18abc2a45551436a05dba96af
然Y <inie0722@gmail.com>
2022-06-09 10:12:39 +0800

    ...

--- ports/air-ctl/portfile.cmake
+++ ports/air-ctl/portfile.cmake
@@ -1,8 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  inie0722/CTL
-    REF v1.0.0
-    SHA512 6e1fcd70be0750b3e45de80d6fb30471fdbd8bbd23afd7ff9fb8c3b9fe61669cd02773cf2b6573348dd2905a303845627d3f5f3e647f4e1261db4cc570cf5099
+    REF 58ca0f229aff093ada66955993cada93e01da18c 
     HEAD_REF master
 )
 
