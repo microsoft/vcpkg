71c5ae836fb61b57635d69933ef54e05f1b88b4c
Kai Pastor <dg0yt@darc.de>
2022-06-09 18:54:35 +0200

    [libiconv] License processing

--- ports/libiconv/portfile.cmake
+++ ports/libiconv/portfile.cmake
@@ -50,4 +50,19 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/${PORT}") # share contains un
 
 # Please keep, the default usage is broken
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/COPYING.LIB" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(READ "${SOURCE_PATH}/COPYING.LIB" copying_lib)
+file(READ "${SOURCE_PATH}/COPYING" copying_tool)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "
+The libiconv and libcharset libraries and their header files are under LGPL,
+see COPYING.LIB below.
+
+The iconv program and the documentation are under GPL, see COPYING below.
+
+# COPYING.LIB
+
+${copying_lib}
+
+# COPYING
+
+${copying_tool}
+")
