8d4a55a4ae3f5446c50a0312a199e5eb6c86b025
Lily Wang <v-lilywang@microsoft.com>
2022-06-10 03:20:54 -0700

    update portfile.cmake

--- ports/spdlog/portfile.cmake
+++ ports/spdlog/portfile.cmake
@@ -1,25 +1,18 @@
-if("stdformat" IN_LIST FEATURES)
-    set(SPDLOG_FMT_EXTERNAL OFF)
-    set(SPDLOG_USE_STD_FORMAT ON)
-    list(APPEND PATCHES fix-open-stdformat.patch)
-else()
-    set(SPDLOG_FMT_EXTERNAL ON)
-    set(SPDLOG_USE_STD_FORMAT OFF)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gabime/spdlog
     REF v1.10.0
     SHA512 e82ec0a0c813ed2f1c8a31a0f21dbb733d0a7bd8d05284feae3bd66040bc53ad47a93b26c3e389c7e5623cfdeba1854d690992c842748e072aab3e6e6ecc5666
     HEAD_REF v1.x
-    PATCHES ${PATCHES}
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         benchmark SPDLOG_BUILD_BENCH
         wchar     SPDLOG_WCHAR_SUPPORT
+        stdformat       SPDLOG_USE_STD_FORMAT
+    INVERTED_FEATURES
+        stdformat       SPDLOG_FMT_EXTERNAL
 )
 
 # SPDLOG_WCHAR_FILENAMES can only be configured in triplet file since it is an alternative (not additive)
@@ -36,15 +29,21 @@ endif()
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SPDLOG_BUILD_SHARED)
 
+# if using std::fromat, spdlog requires minimum c++20 
+set(CXX_STANDARD_OPTION ${CMAKE_CXX_STANDARD})
+if (SPDLOG_USE_STD_FORMAT AND (NOT DEFINED CXX_STANDARD_OPTION OR CXX_STANDARD_OPTION LESS 17))
+    set(CXX_STANDARD_OPTION 20)
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DSPDLOG_FMT_EXTERNAL=${SPDLOG_FMT_EXTERNAL}
         -DSPDLOG_INSTALL=ON
         -DSPDLOG_BUILD_SHARED=${SPDLOG_BUILD_SHARED}
         -DSPDLOG_WCHAR_FILENAMES=${SPDLOG_WCHAR_FILENAMES}
         -DSPDLOG_BUILD_EXAMPLE=OFF
+        -DCMAKE_CXX_STANDARD=${CXX_STANDARD_OPTION}
 )
 
 vcpkg_cmake_install()
@@ -55,13 +54,19 @@ vcpkg_copy_pdbs()
 # use vcpkg-provided fmt library (see also option SPDLOG_FMT_EXTERNAL above)
 if(SPDLOG_FMT_EXTERNAL)
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/bundled")
+    # add support for integration other than cmake
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
+        "// #define SPDLOG_FMT_EXTERNAL"
+        "#ifndef SPDLOG_FMT_EXTERNAL\n#define SPDLOG_FMT_EXTERNAL\n#endif"
+    )
+endif()
+if(SPDLOG_USE_STD_FORMAT)
+    vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
+        "// #define SPDLOG_USE_STD_FORMAT"
+        "#ifndef SPDLOG_USE_STD_FORMAT\n#define SPDLOG_USE_STD_FORMAT\n#endif"
+    )
 endif()
 
-# add support for integration other than cmake
-vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
-    "// #define SPDLOG_FMT_EXTERNAL"
-    "#ifndef SPDLOG_FMT_EXTERNAL\n#define SPDLOG_FMT_EXTERNAL\n#endif"
-)
 if(SPDLOG_WCHAR_SUPPORT)
     vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h
         "// #define SPDLOG_WCHAR_TO_UTF8_SUPPORT"
