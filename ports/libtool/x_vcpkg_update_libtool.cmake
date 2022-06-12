94f16f94444593c5bf392ecbc1116082b97fe7c6
Kai Pastor <dg0yt@darc.de>
2022-06-12 14:11:48 +0200

    Drop external docs from script

--- ports/libtool/x_vcpkg_update_libtool.cmake
+++ ports/libtool/x_vcpkg_update_libtool.cmake
@@ -1,34 +1,3 @@
-#[===[.md:
-# x_vcpkg_update_libtool
-
-Experimental
-Directly update libtool (`ltmain.sh`) in a source directory.
-This function can be called before `vcpkg_configure_make` in order to improve
-build time and platform support over building with stock libtool 2.4.6.
-It does not require regenerating `configure` via `autoreconf`.
-
-## Usage
-```cmake
-x_vcpkg_update_libtool(
-    SOURCE_PATH <${SOURCE_PATH}>
-    [RECURSE]
-)
-```
-## Parameters
-
-### SOURCE_PATH
-Specifies the directory containing the source code.
-By convention, this is usually set in the portfile as the variable `SOURCE_PATH`.
-
-### RECURSE
-Instead of just updating `${SOURCE_PATH}/build-aux/ltmain.sh`, look recursively
-for all files named `ltmain.sh`.
-
-## Examples
-
-* [gettext](https://github.com/microsoft/vcpkg/blob/master/ports/gettext/portfile.cmake)
-* [libiconv](https://github.com/microsoft/vcpkg/blob/master/ports/libiconv/portfile.cmake)
-#]===]
 if(Z_VCPKG_UPDATE_LIBTOOL_GUARD)
     return()
 endif()
