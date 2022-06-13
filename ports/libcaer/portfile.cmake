ada84bd8a469bae5bf603d11810fcec73eb21575
Rokas Jurevicius <rokas.jurevicius@inivation.com>
2022-06-13 10:28:24 +0200

    Adding pkgconfig fixup

--- ports/libcaer/portfile.cmake
+++ ports/libcaer/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 
+vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "libcaer" CONFIG_PATH "share/libcaer")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
