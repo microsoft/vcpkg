4fe63586ba2bc7c2dc9232de41cbb2503d972e5d
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-06-14 22:54:40 +0200

    [vcpkg tool] Add vcpkg_install_copyright

---
+++ scripts/cmake/vcpkg_install_copyright.cmake
@@ -0,0 +1,28 @@
+function(vcpkg_install_copyright)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "" "FILE_LIST")
+
+    if(DEFINED arg_UNPARSED_ARGUMENTS)
+        message(WARNING "${CMAKE_CURRENT_FUNCTION} was passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
+    endif()
+
+    list(LENGTH arg_FILE_LIST FILE_LIST_LENGTH)
+    if(FILE_LIST_LENGTH LESS_EQUAL 1)
+        message(FATAL_ERROR "Don't use ${CMAKE_CURRENT_FUNCTION} to install a single license file.")
+    endif()
+
+    set(out_string "")
+    message(STATUS "Files: ${arg_FILE_LIST}")
+
+    foreach(file_item IN LISTS arg_FILE_LIST)
+        if(NOT EXISTS "${file_item}" OR IS_DIRECTORY "${file_item}")
+            message(FATAL_ERROR "The file ${file_item} does not exist or is a directory.")
+        endif()
+
+        get_filename_component(file_name ${file_item} NAME)
+        file(READ "${file_item}" file_contents)
+
+        string(APPEND out_string "${file_name}:\n\n${file_contents}\n\n")
+    endforeach()
+
+    file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "${out_string}")
+endfunction()
