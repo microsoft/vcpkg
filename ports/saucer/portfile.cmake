88e45717fce1d3e7f3155e13e3103662ecbe7239
Curve <fynnbwdt@gmail.com>
2022-06-15 17:34:59 +0200

    [saucer] use vcpkg webview2

--- ports/saucer/portfile.cmake
+++ ports/saucer/portfile.cmake
@@ -3,14 +3,15 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO saucer/saucer
-    REF e7040e95c9cd13d831feed70e5d07ce06be6149d
-    SHA512 54d7142f6bb81d568d0ee67af97ab443bdaca2c9ddfdb26cdb0d7afc1fb8366583aaaf34ce56bfa99dfff7559e4bfe17b48e6c07f6fd8be4cfa938fb8e35ac39
-    HEAD_REF master
+    REF 4d41e0f356c5b95f77803cdb760a3c01eac0aabd
+    SHA512 b5fe7484c80f0efde3c9f445a3a38421aff48d589ee27778495b501de4d232da44d55072664371ade79891ea7218ab1ddab385e2316b7ae20b5c32cd2332dd56
+    HEAD_REF dev
+    PATCHES "unofficial-webview2.patch"
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH} 
-    OPTIONS -Dsaucer_prefer_remote=OFF
+    OPTIONS -Dsaucer_prefer_remote=OFF -Dsaucer_remote_webview2=OFF
 )
 
 vcpkg_cmake_install()
