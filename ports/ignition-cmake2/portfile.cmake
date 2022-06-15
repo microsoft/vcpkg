3a4f2cfabeba053f0313b9dd2f5a2d012ca08802
JackBoosY <yuzaiyang@beyondsoft.com>
2022-06-15 01:47:18 -0700

    Remove cmake find module installation, add pkgconfig support and fix PRIVATE_FOR limit

--- ports/ignition-cmake2/portfile.cmake
+++ ports/ignition-cmake2/portfile.cmake
@@ -2,13 +2,10 @@ set(PACKAGE_VERSION "2.5.0")
 
 ignition_modular_library(NAME cmake
                          VERSION ${PACKAGE_VERSION}
-                         OUT_SOURCE_PATH SOURCE_PATH
                          SHA512 e39ed44ae6f7ccc338412f466f1257f88989e0818bee801ddbe09350e906cd9ce709be24356310fdbfde22d1b5b5846fed0aa794c06dcf7caf82748a07b428d6
-                         PATCHES FindGTS.patch)
-
-# For ignition-common3
-file(INSTALL "${SOURCE_PATH}/cmake/FindGTS.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-)
+                         PATCHES
+                            FindGTS.patch
+                            add-pkgconfig-and-remove-privatefor-limit.patch)
 
 # Install custom usage
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" @ONLY)
