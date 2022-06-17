e98a6a3e00dd252b369a16a9734e83aa0bc7e2cf
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2022-06-17 06:33:56 -0400

    Updates!

--- ports/discordcoreapi/portfile.cmake
+++ ports/discordcoreapi/portfile.cmake
@@ -5,8 +5,8 @@ endif()
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF e80a0209c3192252ac7f7cd6a4d16afaf42f7902
-	SHA512 6f318e12613b5a5103ffdd093ab7e281ffcc1320de2f7612a55d5402d2dc85d24356acd8a27c0734b1eec2bc0dc6f8a6e1fa97ea200a18c2d05225ab472ecffc
+	REF 6be0c9a34924805a40c7c215c334081b31e649f5
+	SHA512 1bdd972729536430029ae5d4970fffe7239f4e6df5dd9cdb3a2a805b10ca55a7f3867ec74c7c78660182b596887ab274ebab1f6b3758800a68f176156fc4a817
 	HEAD_REF main
 )
 
@@ -15,14 +15,6 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 file(
 	INSTALL "${SOURCE_PATH}/License.md"
