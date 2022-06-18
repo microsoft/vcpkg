0404a0a5b02901e2d099c4b7b18ce5ae6849c833
Dennis Hezel <dh@3yourmind.com>
2022-06-18 20:12:43 +0200

    Switch from onqtam/doctest to doctest/doctest

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO onqtam/doctest
+    REPO doctest/doctest
     REF v2.4.9
     SHA512 c7337e2de371c18973a0f4cb76458d6ae387e78874c9bc8aa367ffd2d592514b774e7c5ebf44f83b7046f6b33c6905fd079c36f4c33eadf52b3d651d978182cb
     HEAD_REF master
