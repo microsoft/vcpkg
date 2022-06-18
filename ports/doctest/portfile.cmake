ec74ed717c183a36e839e29eb69e01b77158e313
Dennis Hezel <dh@3yourmind.com>
2022-06-18 19:58:30 +0200

    Use ${PORT} instead of raw port name in doctest portfile

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/doctest)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
