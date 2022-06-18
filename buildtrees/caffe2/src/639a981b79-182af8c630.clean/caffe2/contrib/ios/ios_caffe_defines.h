fb75744a753973c8d0c133a645096ca065ce26c8
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2022-06-19 01:00:50 +0800

    fix glog dependency

--- buildtrees/caffe2/src/639a981b79-182af8c630.clean/caffe2/contrib/ios/ios_caffe_defines.h
+++
@@ -1,3 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-#define IOS_CAFFE_EXPORT __attribute__((visibility("default")))
--- buildtrees/caffe2/src/639a981b79-182af8c630.clean/caffe2/operators/reshape_op_gpu.cc
+++
@@ -1,8 +0,0 @@
-#include "caffe2/core/context_gpu.h"
-#include "caffe2/operators/reshape_op.h"
-
-namespace caffe2 {
-
-REGISTER_CUDA_OPERATOR(Reshape, ReshapeOp<float, CUDAContext>);
-
-} // namespace caffe2
