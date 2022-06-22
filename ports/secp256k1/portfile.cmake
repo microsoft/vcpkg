a1ca1864544b9d9c9a632431b1d79cfc17c6bcc8
ROLL Software Brasil <rogerioorkutmsn@hotmail.com>
2022-06-22 15:12:26 -0300

    Update port unnoficial-secp256k1
    The port unnoficial-secp256k1 is used to simetric criptography.
    This update rename unnecessary files from build, as bench, precomputed and tests.

--- ports/secp256k1/portfile.cmake
+++ ports/secp256k1/portfile.cmake
@@ -10,6 +10,14 @@ vcpkg_from_github(
 file(COPY ${CURRENT_PORT_DIR}/libsecp256k1-config.h DESTINATION ${SOURCE_PATH})
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
+file(RENAME ${SOURCE_PATH}/src/bench.c ${SOURCE_PATH}/src/bench.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/bench_ecmult.c ${SOURCE_PATH}/src/bench_ecmult.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/bench_internal.c ${SOURCE_PATH}/src/bench_internal.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/precompute_ecmult.c ${SOURCE_PATH}/src/precompute_ecmult.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/precompute_ecmult_gen.c ${SOURCE_PATH}/src/precompute_ecmult_gen.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/tests.c ${SOURCE_PATH}/src/tests.c.ignore)
+file(RENAME ${SOURCE_PATH}/src/tests_exhaustive.c ${SOURCE_PATH}/src/tests_exhaustive.c.ignore)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
