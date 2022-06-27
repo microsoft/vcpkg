6ec921063ab5879a5d7fe7f798ae77bd66c8db97
FrankXie <v-frankxie@microsoft.com>
2022-06-26 23:58:45 -0700

    adopt the requests

--- ports/sol2/portfile.cmake
+++ ports/sol2/portfile.cmake
@@ -1,14 +1,14 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ThePhD/sol2
-    REF v3.3.0
-    SHA512 f1eba8f9ea270a3a3fff9c7a036d130ec848d065e54a8aefd2a19ad7f17dcb6b5744d979fac54c765e8317a4cdcf72e1b9d622d114f48c6502cf2db900c8d4a3 
+    REF eba86625b707e3c8c99bbfc4624e51f42dc9e561 #v3.3.0
+    SHA512 a1fbcb4efd9a8b8b97c351e90499644aea72a3db62c258e219a2912853936b76870b51e69d835c14cbf1a20733673ba474e259a0243fec419c411b995cd1511d 
     HEAD_REF develop
     PATCHES fix-namespace.patch
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
