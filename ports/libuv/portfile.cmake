fc50c5a95517817c967c18db79a9b44eb4aeac3e
JackBoosY <yuzaiyang@beyondsoft.com>
2022-06-27 03:00:15 -0700

    Use official CMakeLists.txt

--- ports/libuv/portfile.cmake
+++ ports/libuv/portfile.cmake
@@ -6,35 +6,26 @@ vcpkg_from_github(
     HEAD_REF v1.x
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS_DEBUG
-        -DUV_SKIP_HEADERS=ON
-        -D_GNU_SOURCE=ON
+    OPTIONS
+        -DQEMU=OFF
+        -DASAN=OFF
+        -DTSAN=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libuv CONFIG_PATH share/unofficial-libuv)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libuv)
 
-configure_file(
-    "${CMAKE_CURRENT_LIST_DIR}/unofficial-libuv-config.in.cmake"
-    "${CURRENT_PACKAGES_DIR}/share/unofficial-libuv/unofficial-libuv-config.cmake"
-    @ONLY
-)
-
-file(READ "${CURRENT_PACKAGES_DIR}/include/uv.h" UV_H)
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    string(REPLACE "defined(USING_UV_SHARED)" "1" UV_H "${UV_H}")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/uv.h" "defined(USING_UV_SHARED)" "1")
 else()
-    string(REPLACE "defined(USING_UV_SHARED)" "0" UV_H "${UV_H}")
-    configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/uv.h" "defined(USING_UV_SHARED)" "0")
 endif()
-file(WRITE "${CURRENT_PACKAGES_DIR}/include/uv.h" "${UV_H}")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
