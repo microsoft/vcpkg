3342179eed616d098e5735232544db25a6c0ac1f
Kai Pastor <dg0yt@darc.de>
2022-06-29 16:44:02 +0200

    Disable doc

--- ports/tidy-html5/portfile.cmake
+++ ports/tidy-html5/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF 5.8.0
     SHA512 f352165bdda5d1fca7bba3365560b64d6f70a4e010821cd246cde43bed5c23cea3408d461d3f889110fd35ec9b68aa2b4e95412b07775eb852b7ee1745007a44
     HEAD_REF master
+    PATCHES
+        disable-doc.patch
 )
 
 vcpkg_cmake_configure(
@@ -15,13 +17,11 @@ vcpkg_cmake_configure(
         -DBUILD_SHARED_LIB=OFF
         -DTIDY_CONSOLE_SHARED=OFF
 )
-vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/tools/libxslt")
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
     "${CURRENT_PACKAGES_DIR}/debug/bin/tidyd${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
 )
 
