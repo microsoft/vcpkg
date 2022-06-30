3c5607aa3a550680ee572e5bc96cbd47a2106f6a
Ronald <3665590+playgithub@users.noreply.github.com>
2022-06-30 14:42:11 +0800

    Update ports/limonp/portfile.cmake
    Co-authored-by: Frank <65999885+FrankXie05@users.noreply.github.com>

--- ports/limonp/portfile.cmake
+++ ports/limonp/portfile.cmake
@@ -13,13 +13,5 @@ vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-# # Moves all .cmake files from /debug/share/limonp/ to /share/limonp/
-# # See /docs/maintainers/ports/vcpkg-cmake-config/vcpkg_cmake_config_fixup.md for more details
-# When you uncomment "vcpkg_cmake_config_fixup()", you need to add the following to "dependencies" vcpkg.json:
-#{
-#    "name": "vcpkg-cmake-config",
-#    "host": true
-#}
-# vcpkg_cmake_config_fixup(CONFIG_PATH cmake TARGET_PATH share/limonp)
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
