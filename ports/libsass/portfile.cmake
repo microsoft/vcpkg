45410238085ac58eb3da5690aec09b353e8ef425
Jamlys Lee <jamlys_lee@163.com>
2022-07-01 15:16:44 +0800

    Add cross-compiling support on linux for libsass port.

--- ports/libsass/portfile.cmake
+++ ports/libsass/portfile.cmake
@@ -7,9 +7,38 @@ vcpkg_from_github(
     PATCHES remove_compiler_flags.patch
 )
 
+# `--host` option is needed for cross-compiling on linux, to select the correct toolchains,
+# instead of using the detected native toolchains, which would cause failure. To avoid this,
+# we can specify the corresponding toolset to `VCPKG_TOOLSET_PREFIX` in the target triplet.
+# For example, while cross-compiling for arm64, we can specify `aarch64-linux-gnu` to 
+# `VCPKG_TOOLSET_PREFIX` variable in a target triplet file.
+if(VCPKG_HOST_IS_LINUX)
+    execute_process(COMMAND "uname" "-m" OUTPUT_VARIABLE HOST_SYSTEM_PROCESSOR OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(BUILD_OPTION --build=${HOST_SYSTEM_PROCESSOR}-linux-gnu)
+    if(DEFINED VCPKG_TOOLSET_PREFIX)
+        # Give a change to select an alternative toolset by user.
+        set(--host=${VCPKG_TOOLSET_PREFIX})
+    else()
+        message(NOTICE
+            "\nAutomatically select building toolset for ${VCPKG_TARGET_ARCHITECTURE}. "
+            "VCPKG_TOOLSET_PREFIX can be set in the triplet file to use specific toolset."
+            " Like for arm64:\n    set(VCPKG_TOOLSET_PREFIX aarch64-linux-gnu)\n"
+        )
+        # Select propriate toolset according to VCPKG_TARGET_ARCHITECTURE
+        if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+            set(HOST_OPTION --host=arm-linux-gnueabihf)
+        elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+            set(HOST_OPTION --host=aarch64-linux-gnu)
+        endif()
+    endif()
+endif()
+
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
+    OPTIONS
+        ${HOST_OPTION}
+        ${BUILD_OPTION}
 )
 vcpkg_install_make(MAKEFILE GNUmakefile)
 vcpkg_fixup_pkgconfig()
