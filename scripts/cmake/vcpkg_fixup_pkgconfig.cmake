250d3d62bc82a43bc2ff1b76cd8b5e32d4414a71
Kai Pastor <dg0yt@darc.de>
2022-07-03 12:46:24 +0200

    Move contents processing into separate function

--- scripts/cmake/vcpkg_fixup_pkgconfig.cmake
+++ scripts/cmake/vcpkg_fixup_pkgconfig.cmake
@@ -1,3 +1,78 @@
+function(z_vcpkg_fixup_pkgconfig_process_data data_variable config)
+    set(contents "${${data_variable}}")
+    if(contents MATCHES "(^|\n)Libs[^:]*: *[^\n]*optimized" AND contents MATCHES "(^|\n)(Libs[^:]*: *[^\n]*debug[^\n]*)")
+        message(FATAL_ERROR "Error in ${file}: CMake linking keywords must be resolved in the portfile:\n${CMAKE_MATCH_2}")
+    endif()
+    if(contents MATCHES "(^|\n)Libs[^:]*: *([^\n]*-NOTFOUND)")
+        string(REGEX MATCH "[^ ]*\$" lib_notfound "${CMAKE_MATCH_2}")
+        message(FATAL_ERROR "Error in ${file}: 'Libs' refers to a missing lib:\n${lib_notfound}")
+    endif()
+    if(contents MATCHES "(^|\n)Libs[^:]*: *([^\n]*::[^\n ]*)")
+    string(REGEX MATCH "[^ ]*\$" target "${CMAKE_MATCH_2}")
+        message(FATAL_ERROR "Error in ${file}: 'Libs' refer to a CMake target:\n${target}")
+    endif()
+
+    # this normalizes all files to end with a newline, and use LF instead of CRLF;
+    # this allows us to use regex matches easier to modify these files.
+    if(NOT "${contents}" MATCHES "\n$")
+        string(APPEND contents "\n")
+    endif()
+    string(REPLACE "\r\n" "\n" contents "${contents}")
+
+    string(REPLACE "${CURRENT_PACKAGES_DIR}" [[${prefix}]] contents "${contents}")
+    string(REPLACE "${CURRENT_INSTALLED_DIR}" [[${prefix}]] contents "${contents}")
+    if(VCPKG_HOST_IS_WINDOWS)
+        string(REGEX REPLACE "^([a-zA-Z]):/" [[/\1/]] unix_packages_dir "${CURRENT_PACKAGES_DIR}")
+        string(REPLACE "${unix_packages_dir}" [[${prefix}]] contents "${contents}")
+        string(REGEX REPLACE "^([a-zA-Z]):/" [[/\1/]] unix_installed_dir "${CURRENT_INSTALLED_DIR}")
+        string(REPLACE "${unix_installed_dir}" [[${prefix}]] contents "${contents}")
+    endif()
+
+    string(REGEX REPLACE "(^|\n)prefix[\t ]*=[^\n]*" "" contents "${contents}")
+    if("${config}" STREQUAL "DEBUG")
+        # prefix points at the debug subfolder
+        string(REPLACE [[${prefix}/debug]] [[${prefix}]] contents "${contents}")
+        string(REPLACE [[${prefix}/include]] [[${prefix}/../include]] contents "${contents}")
+        string(REPLACE [[${prefix}/share]] [[${prefix}/../share]] contents "${contents}")
+    endif()
+    # quote -L, -I, and -l paths starting with `${blah}`
+    string(REGEX REPLACE " -([LIl])(\\\${[^}]*}[^ \n\t]*)" [[ -\1"\2"]] contents "${contents}")
+    # Remove line continuations before transformations
+    string(REGEX REPLACE "[ \t]*\\\\\n[ \t]*" " " contents "${contents}")
+    # This section fuses XYZ.private and XYZ according to VCPKG_LIBRARY_LINKAGE
+    #
+    # Pkgconfig searches Requires.private transitively for Cflags in the dynamic case,
+    # which prevents us from removing it.
+    #
+    # Once this transformation is complete, users of vcpkg should never need to pass
+    # --static.
+    if("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+        # how this works:
+        # we want to transform:
+        #   Libs: $1
+        #   Libs.private: $2
+        # into
+        #    Libs: $1 $2
+        # and the same thing for Requires and Requires.private
+
+        foreach(item IN ITEMS "Libs" "Requires" "Cflags")
+            set(line "")
+            if("${contents}" MATCHES "(^|\n)${item}: *([^\n]*)")
+                string(APPEND line " ${CMAKE_MATCH_2}")
+            endif()
+            if("${contents}" MATCHES "(^|\n)${item}\\.private: *([^\n]*)")
+                string(APPEND line " ${CMAKE_MATCH_2}")
+            endif()
+
+            string(REGEX REPLACE "(^|\n)${item}(\\.private)?:[^\n]*\n" [[\1]] contents "${contents}")
+            if(NOT "${line}" STREQUAL "")
+                string(APPEND contents "${item}:${line}\n")
+            endif()
+        endforeach()
+    endif()
+    set("${data_variable}" "${contents}" PARENT_SCOPE)
+endfunction()
+
 function(z_vcpkg_fixup_pkgconfig_check_files file config)
     set(path_suffix_DEBUG /debug)
     set(path_suffix_RELEASE "")
@@ -68,14 +143,8 @@ function(vcpkg_fixup_pkgconfig)
         endforeach()
     endif()
 
-    string(REGEX REPLACE "^([a-zA-Z]):/" [[/\1/]] unix_packages_dir "${CURRENT_PACKAGES_DIR}")
-    string(REGEX REPLACE "^([a-zA-Z]):/" [[/\1/]] unix_installed_dir "${CURRENT_INSTALLED_DIR}")
-
     foreach(config IN ITEMS RELEASE DEBUG)
         debug_message("${config} Files: ${arg_${config}_FILES}")
-        if("${VCPKG_BUILD_TYPE}" STREQUAL "debug" AND "${config}" STREQUAL "RELEASE")
-            continue()
-        endif()
         if("${VCPKG_BUILD_TYPE}" STREQUAL "release" AND "${config}" STREQUAL "DEBUG")
             continue()
         endif()
@@ -91,72 +160,7 @@ function(vcpkg_fixup_pkgconfig)
             endif()
             #Correct *.pc file
             file(READ "${file}" contents)
-            if(contents MATCHES "(^|\n)Libs[^:]*: *[^\n]*optimized" AND contents MATCHES "(^|\n)(Libs[^:]*: *[^\n]*debug[^\n]*)")
-                message(FATAL_ERROR "Error in ${file}: CMake linking keywords must be resolved in the portfile:\n${CMAKE_MATCH_2}")
-            endif()
-            if(contents MATCHES "(^|\n)Libs[^:]*: *([^\n]*-NOTFOUND)")
-                string(REGEX MATCH "[^ ]*\$" lib_notfound "${CMAKE_MATCH_2}")
-                message(FATAL_ERROR "Error in ${file}: 'Libs' refers to a missing lib:\n${lib_notfound}")
-            endif()
-            if(contents MATCHES "(^|\n)Libs[^:]*: *([^\n]*::[^\n ]*)")
-            string(REGEX MATCH "[^ ]*\$" target "${CMAKE_MATCH_2}")
-                message(FATAL_ERROR "Error in ${file}: 'Libs' refer to a CMake target:\n${target}")
-            endif()
-
-            # this normalizes all files to end with a newline, and use LF instead of CRLF;
-            # this allows us to use regex matches easier to modify these files.
-            if(NOT "${contents}" MATCHES "\n$")
-                string(APPEND contents "\n")
-            endif()
-            string(REPLACE "\r\n" "\n" contents "${contents}")
-
-            string(REPLACE "${CURRENT_PACKAGES_DIR}" [[${prefix}]] contents "${contents}")
-            string(REPLACE "${CURRENT_INSTALLED_DIR}" [[${prefix}]] contents "${contents}")
-            string(REPLACE "${unix_packages_dir}" [[${prefix}]] contents "${contents}")
-            string(REPLACE "${unix_installed_dir}" [[${prefix}]] contents "${contents}")
-
-            string(REGEX REPLACE "(^|\n)prefix[\t ]*=[^\n]*" "" contents "${contents}")
-            if("${config}" STREQUAL "DEBUG")
-                # prefix points at the debug subfolder
-                string(REPLACE [[${prefix}/debug]] [[${prefix}]] contents "${contents}")
-                string(REPLACE [[${prefix}/include]] [[${prefix}/../include]] contents "${contents}")
-                string(REPLACE [[${prefix}/share]] [[${prefix}/../share]] contents "${contents}")
-            endif()
-            # quote -L, -I, and -l paths starting with `${blah}`
-            string(REGEX REPLACE " -([LIl])(\\\${[^}]*}[^ \n\t]*)" [[ -\1"\2"]] contents "${contents}")
-            # Remove line continuations before transformations
-            string(REGEX REPLACE "[ \t]*\\\\\n[ \t]*" " " contents "${contents}")
-            # This section fuses XYZ.private and XYZ according to VCPKG_LIBRARY_LINKAGE
-            #
-            # Pkgconfig searches Requires.private transitively for Cflags in the dynamic case,
-            # which prevents us from removing it.
-            #
-            # Once this transformation is complete, users of vcpkg should never need to pass
-            # --static.
-            if("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
-                # how this works:
-                # we want to transform:
-                #   Libs: $1
-                #   Libs.private: $2
-                # into
-                #    Libs: $1 $2
-                # and the same thing for Requires and Requires.private
-
-                foreach(item IN ITEMS "Libs" "Requires" "Cflags")
-                    set(line "")
-                    if("${contents}" MATCHES "(^|\n)${item}: *([^\n]*)")
-                        string(APPEND line " ${CMAKE_MATCH_2}")
-                    endif()
-                    if("${contents}" MATCHES "(^|\n)${item}\\.private: *([^\n]*)")
-                        string(APPEND line " ${CMAKE_MATCH_2}")
-                    endif()
-
-                    string(REGEX REPLACE "(^|\n)${item}(\\.private)?:[^\n]*\n" [[\1]] contents "${contents}")
-                    if(NOT "${line}" STREQUAL "")
-                        string(APPEND contents "${item}:${line}\n")
-                    endif()
-                endforeach()
-            endif()
+            z_vcpkg_fixup_pkgconfig_process_data(contents "${config}")
             file(WRITE "${file}" "prefix=\${pcfiledir}/${relative_pc_path}\n${contents}")
         endforeach()
 
