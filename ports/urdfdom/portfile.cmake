b5473fb43a4fc3ec7c8dc0bfcfbab847d9a25d0f
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-07-06 00:17:25 +0200

    Enable static builds

--- ports/urdfdom/portfile.cmake
+++ ports/urdfdom/portfile.cmake
@@ -1,22 +1,28 @@
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO ros/urdfdom
-  REF 99ec1f99f2d175f07cc26e63082502ee62982dac # 3.1.0
-  SHA512 64bd96c6b56c300b92e2bd6a875c4bc3c4c5d2ee332a75a8d98099aee0db3e9c33fa7d75fdc4d013e7b6ac47296f524ef713639b06e66035135dfc2a8cca0276
-  HEAD_REF master
-  PATCHES
-    0002_fix_exports.patch
-    0005-fix-config.patch
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ros/urdfdom
+    REF 99ec1f99f2d175f07cc26e63082502ee62982dac # 3.1.0
+    SHA512 64bd96c6b56c300b92e2bd6a875c4bc3c4c5d2ee332a75a8d98099aee0db3e9c33fa7d75fdc4d013e7b6ac47296f524ef713639b06e66035135dfc2a8cca0276
+    HEAD_REF master
+    PATCHES
+        0002_fix_exports.patch
+        0005-fix-config.patch
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+        -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_copy_tools(TOOL_NAMES check_urdf urdf_mem_test urdf_to_graphiz urdf_to_graphviz AUTO_CLEAN)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
