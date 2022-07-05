3ceff33b9038a5e58d50c31c6a7697181f6e7686
LilyWangLL <v-lilywang@microsoft.com>
2022-07-05 00:42:11 -0700

    [snappy] Add feature BMI2 and SSSE3

--- ports/snappy/portfile.cmake
+++ ports/snappy/portfile.cmake
@@ -4,6 +4,13 @@ vcpkg_from_github(
     REF 1.1.9
     SHA512 f1f8a90f5f7f23310423574b1d8c9acb84c66ea620f3999d1060395205e5760883476837aba02f0aa913af60819e34c625d8308c18a5d7a9c4e190f35968b024
     HEAD_REF master
+    PATCHES fix-features.patch
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        bmi2   HAVE_BMI2
+        ssse3  HAVE_SSSE3
 )
 
 vcpkg_cmake_configure(
@@ -12,6 +19,7 @@ vcpkg_cmake_configure(
         -DSNAPPY_BUILD_TESTS=OFF
         -DSNAPPY_BUILD_BENCHMARKS=OFF
         -DCMAKE_DEBUG_POSTFIX=d
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
