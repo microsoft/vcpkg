5ca2fefae64823951010b9478f53ae04645eb278
FrankXie <v-frankxie@microsoft.com>
2022-07-06 00:15:14 -0700

    del variable

--- ports/vk-bootstrap/vk-bootstrap-targets-release.cmake
+++ ports/vk-bootstrap/vk-bootstrap-targets-release.cmake
@@ -7,6 +7,7 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET vk-bootstrap::vk-bootstrap APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(vk-bootstrap::vk-bootstrap PROPERTIES
     IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/vk-bootstrap.lib"
+    IMPORTED_LOCATION "${_IMPORT_PREFIX}/lib/vk-bootstrap.lib"
 )
 
 set(CMAKE_IMPORT_FILE_VERSION)
