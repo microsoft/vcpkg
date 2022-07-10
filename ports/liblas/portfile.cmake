b90474e7a02663c8a07fa7916566a819e9d54f42
Kai Pastor <dg0yt@darc.de>
2022-07-10 11:25:42 +0200

    Add tools feature

--- ports/liblas/portfile.cmake
+++ ports/liblas/portfile.cmake
@@ -20,11 +20,18 @@ vcpkg_extract_source_archive_ex(
 
 file(REMOVE_RECURSE "${SOURCE_PATH}/cmake/modules")
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools   WITH_UTILITIES
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS 
+    OPTIONS
+        ${FEATURE_OPTIONS}
         -DBUILD_OSGEO4W=OFF
         -DWITH_TESTS=OFF
+    OPTIONS_DEBUG
         -DWITH_UTILITIES=OFF
 )
 
@@ -40,5 +47,13 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
+
+if(WITH_UTILITIES)
+    set(tools lasinfo lasblock las2las las2txt txt2las ts2las)
+    if(NOT WIN32)
+        list(APPEND tools las2col las2pg)
+    endif()
+    vcpkg_copy_tools(TOOL_NAMES ${tools} AUTO_CLEAN)
+endif()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
