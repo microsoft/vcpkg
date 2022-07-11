7f95bec30a3a58fccd02059cfc01f01285a531ef
Neumann-A <30894796+Neumann-A@users.noreply.github.com>
2022-07-11 14:43:51 +0200

    don't use bin dir in static builds

--- triplets/move_pdb_files.cmake
+++ triplets/move_pdb_files.cmake
@@ -1,25 +1,30 @@
 
+set(install_subdir "bin")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(install_subdir "lib")
+endif()
+
 # For ports which are not CMake
-if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/bin/${PORT}.pdb" AND 
+if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/${install_subdir}/${PORT}.pdb" AND 
        EXISTS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}.pdb")
     file(INSTALL 
                 "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}.pdb" 
             DESTINATION
-                "${CURRENT_PACKAGES_DIR}/bin")
+                "${CURRENT_PACKAGES_DIR}/${install_subdir}")
     file(INSTALL 
                 "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}_strip.pdb" 
             DESTINATION
-                "${CURRENT_PACKAGES_DIR}/bin")
+                "${CURRENT_PACKAGES_DIR}/${install_subdir}")
 endif()
 
-if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/debug/bin/${PORT}.pdb" AND 
+if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}/${PORT}.pdb" AND 
        EXISTS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}.pdb")
     file(INSTALL 
                 "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}.pdb" 
             DESTINATION
-                "${CURRENT_PACKAGES_DIR}/debug/bin")
+                "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}")
     file(INSTALL 
                 "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}_strip.pdb" 
             DESTINATION
-                "${CURRENT_PACKAGES_DIR}/debug/bin")
+                "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}")
 endif()
