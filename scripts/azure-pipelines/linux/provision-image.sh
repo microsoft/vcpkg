6543c43964ff49678839cd58be7ca5efc20cc778
Billy O'Neal <bion@microsoft.com>
2022-07-11 16:47:32 -0700

    Add libxtst-dev from https://github.com/microsoft/vcpkg/pull/24934

--- scripts/azure-pipelines/linux/provision-image.sh
+++ scripts/azure-pipelines/linux/provision-image.sh
@@ -59,6 +59,9 @@ APT_PACKAGES="$APT_PACKAGES libxdamage-dev"
 # Additionally required by gtk3 and at-spi2-atk
 APT_PACKAGES="$APT_PACKAGES libdbus-1-dev"
 
+# Additionally required by at-spi2-atk
+APT_PACKAGES="$APT_PACKAGES libxtst-dev"
+
 # Additionally required/installed by Azure DevOps Scale Set Agents
 APT_PACKAGES="$APT_PACKAGES liblttng-ust0 libkrb5-3 zlib1g libicu66"
 
