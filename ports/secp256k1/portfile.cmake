dd9ced167a0fc89ce99c9ee0cb06fb2a5826e641
JackBoosY <yuzaiyang@beyondsoft.com>
2022-07-12 03:11:57 -0700

    Update, add features

--- ports/secp256k1/portfile.cmake
+++ ports/secp256k1/portfile.cmake
@@ -2,42 +2,46 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO "bitcoin-core/secp256k1"
-    REF "44c2452fd387f7ca604ab42d73746e7d3a44d8a2"
-    SHA512 1c1969b663843c71cba0148b14430bd2417b63a6ca7d6940585844b73aee2642622b080541ad3a1712390640f305f5bb1e251985d59674e1ded9cb4688f7830d
+    REPO bitcoin-core/secp256k1
+    REF 3efeb9da21368c02cad58435b2ccdf6eb4b359c3
+    SHA512 6d792943f9277a1b4c36dad62389cb38e0b93efb570b6af6c41afdb936d10ca30d4c2e4e743fc0f113d1f9785891d1e9d1fe224d7b8abd4197a9f5febf0febd6
 )
 
-file(COPY "${CURRENT_PORT_DIR}/libsecp256k1-config.h" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/secp256k1-config.cmake" DESTINATION "${SOURCE_PATH}")
 
-file(REMOVE "${SOURCE_PATH}/src/bench.c")
-file(REMOVE "${SOURCE_PATH}/src/bench.h")
-file(REMOVE "${SOURCE_PATH}/src/bench_ecmult.c")
-file(REMOVE "${SOURCE_PATH}/src/bench_internal.c")
-
-file(REMOVE "${SOURCE_PATH}/src/tests.c")
-file(REMOVE "${SOURCE_PATH}/src/tests_exhaustive.c")
-file(REMOVE "${SOURCE_PATH}/src/valgrind_ctime_test.c")
-
-file(REMOVE "${SOURCE_PATH}/contrib/lax_der_parsing.c)
-file(REMOVE "${SOURCE_PATH}/contrib/lax_der_parsing.h)
-file(REMOVE "${SOURCE_PATH}/contrib/lax_der_privatekey_parsing.c)
-file(REMOVE "${SOURCE_PATH}/contrib/lax_der_privatekey_parsing.h)
-
-file(REMOVE "${SOURCE_PATH}/examples/ecdh.c)
-file(REMOVE "${SOURCE_PATH}/examples/ecdsa.c)
-file(REMOVE "${SOURCE_PATH}/examples/schnorr.c)
-file(REMOVE "${SOURCE_PATH}/examples/random.h)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools       BUILD_TOOLS
+        examples    BUILD_EXAMPLES
+)
 
 vcpkg_cmake_configure(
 	SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
 	OPTIONS_DEBUG
-	-DINSTALL_HEADERS=OFF
+        -DINSTALL_HEADERS=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
 vcpkg_cmake_config_fixup(CONFIG_PATH "share/unofficial-${PORT}" PACKAGE_NAME unofficial-${PORT})
 
+if (BUILD_TOOLS OR BUILD_EXAMPLES)
+    set(SECP256K1_TOOLS "")
+    if (BUILD_TOOLS)
+        list(APPEND SECP256K1_TOOLS bench bench_internal bench_ecmult)
+    endif()
+    
+    if (BUILD_EXAMPLES)
+        list(APPEND SECP256K1_TOOLS ecdsa_example ecdh_example schnorr_example)
+    endif()
+    
+    vcpkg_copy_tools(TOOL_NAMES ${SECP256K1_TOOLS} AUTO_CLEAN)
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-configure_file(${CMAKE_CURRENT_LIST_DIR}/secp256k1-config.cmake ${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}/unofficial-secp256k1-config.cmake @ONLY)
+
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
