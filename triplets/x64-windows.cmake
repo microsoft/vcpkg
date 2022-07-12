b27e9457cb10c8c254fae5ffd13627035501172c
Neumann-A <30894796+Neumann-A@users.noreply.github.com>
2022-07-12 09:59:51 +0200

    use job pools correctly

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -1,6 +1,6 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DVCPKG_PORT=${PORT}" "-DCMAKE_JOB_POOL_LINK=1") # tell cmake to only run one link job pool
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DVCPKG_PORT=${PORT}" "-DCMAKE_JOB_POOLS=\"one_job=1\"" "-DCMAKE_JOB_POOL_LINK=one_job") # tell cmake to only run one link job pool
 set(VCPKG_MESON_CONFIGURE_OPTIONS "-Dbackend_max_links=1") # to tell meson to not run the linker in parallel
 set(VCPKG_POST_PORTFILE_INCLUDES "${CMAKE_CURRENT_LIST_DIR}/move_pdb_files.cmake")
