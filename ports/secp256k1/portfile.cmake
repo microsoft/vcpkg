56fff2a5abd94cf7fa4cd6cad90c00b8843ec1a1
JackBoosY <yuzaiyang@beyondsoft.com>
2022-07-12 03:16:13 -0700

    fix

--- ports/secp256k1/portfile.cmake
+++ ports/secp256k1/portfile.cmake
@@ -8,7 +8,6 @@ vcpkg_from_github(
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/secp256k1-config.cmake" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
