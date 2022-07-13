3b488443d1ccbfabe1ba2392a02bfdb05cbc88b2
Jamlys Lee <jamlys_lee@163.com>
2022-07-13 21:51:14 +0800

    A new solution to support dynamic build for linux.

--- scripts/cmake/vcpkg_copy_tools.cmake
+++ scripts/cmake/vcpkg_copy_tools.cmake
@@ -1,3 +1,58 @@
+function(z_copy_tool_file)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "TOOLNAME;DIRECTORY;DESTINATION" "")
+
+    if(DEFINED arg_UNPARSED_ARGUMENTS)
+        message(WARNING "${CMAKE_CURRENT_FUNCTION} was passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(NOT DEFINED arg_TOOLNAME)
+        message(FATAL_ERROR "TOOLNAME must be specified.")
+    endif()
+
+    if(NOT DEFINED arg_DIRECTORY)
+        message(FATAL_ERROR "DIRECTORY must be specified.")
+    endif()
+
+    if(NOT DEFINED arg_DESTINATION)
+        message(FATAL_ERROR "DESTINATION must be specified.")
+    endif()
+
+    if(NOT EXISTS ${arg_DESTINATION})
+        file(MAKE_DIRECTORY ${arg_DESTINATION})
+    endif()
+
+    # This is a simple and tricky approach to support dynamic build for linux.
+    if(VCPKG_HOST_IS_LINUX)
+        # Detect the file type, only deal with dynamic executable in this way.
+        execute_process(
+            COMMAND file -i -b ${arg_DIRECTORY}/${arg_TOOLNAME}
+            OUTPUT_VARIABLE file_mime_type
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        if(file_mime_type STREQUAL "application/x-sharedlib; charset=binary")
+            cmake_path(RELATIVE_PATH arg_DESTINATION
+                    BASE_DIRECTORY "${CURRENT_PACKAGES_DIR}"
+                    OUTPUT_VARIABLE relative_tool_dir
+            )
+            # Set the Z_CURRENT_TOOL_DEPENDENCIES_SEARCH_DIR variable, which is signed
+            # to LD_LIBRARY_PATH within templates/execute_tool.sh.in script file.
+            if(relative_tool_dir MATCHES "/debug/")
+                set(Z_CURRENT_TOOL_DEPENDENCIES_SEARCH_DIR "${CURRENT_PACKAGES_DIR}/debug/lib:${CURRENT_INSTALLED_DIR}/debug/lib")
+            else()
+                set(Z_CURRENT_TOOL_DEPENDENCIES_SEARCH_DIR "${CURRENT_PACKAGES_DIR}/lib:${CURRENT_INSTALLED_DIR}/lib")
+            endif()
+            # Here's the trick...
+            set(Z_CURRENT_TOOL_PATH "${arg_DESTINATION}/${arg_TOOLNAME}.original")
+            file(COPY_FILE "${arg_DIRECTORY}/${arg_TOOLNAME}" "${Z_CURRENT_TOOL_PATH}")
+            configure_file(${SCRIPTS}/templates/execute_tool.sh.in ${arg_DESTINATION}/${arg_TOOLNAME})\
+            # Done~
+            return()
+        endif()
+    endif()
+    
+    file(COPY "${arg_DIRECTORY}/${arg_TOOLNAME}" DESTINATION "${arg_DESTINATION}")
+endfunction()
+
 function(vcpkg_copy_tools)
     cmake_parse_arguments(PARSE_ARGV 0 arg "AUTO_CLEAN" "SEARCH_DIR;DESTINATION" "TOOL_NAMES")
 
@@ -23,7 +78,14 @@ function(vcpkg_copy_tools)
         set(tool_path "${arg_SEARCH_DIR}/${tool_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
         set(tool_pdb "${arg_SEARCH_DIR}/${tool_name}.pdb")
         if(EXISTS "${tool_path}")
-            file(COPY "${tool_path}" DESTINATION "${arg_DESTINATION}")
+            # Tricky helper to support dynamic build for linux.
+            z_copy_tool_file(
+                TOOLNAME "${tool_name}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
+                DIRECTORY "${arg_SEARCH_DIR}"
+                DESTINATION "${arg_DESTINATION}"
+            )
+            # Original tool file copying approach. Reserve it here to be enable when a better way is figured out.
+            #file(COPY "${tool_path}" DESTINATION "${arg_DESTINATION}")
         elseif(NOT "${VCPKG_TARGET_BUNDLE_SUFFIX}" STREQUAL "" AND NOT "${VCPKG_TARGET_BUNDLE_SUFFIX}" STREQUAL "${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
             set(bundle_path "${arg_SEARCH_DIR}/${tool_name}${VCPKG_TARGET_BUNDLE_SUFFIX}")
             if(EXISTS "${bundle_path}")
