3f916c89894a467ce86834e62e607c50c3375f9c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-07-15 00:16:45 +0200

    WIP commit

--- triplets/move_pdb_files.cmake
+++ triplets/move_pdb_files.cmake
@@ -1,30 +1,42 @@
+function(z_vcpkg_copy_pdbs)
+    cmake_parse_arguments(PARSE_ARGV 0 "arg" "" "" "")
 
-set(install_subdir "bin")
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    set(install_subdir "lib")
-endif()
+    if(DEFINED arg_UNPARSED_ARGUMENTS)
+        message(WARNING "${CMAKE_CURRENT_FUNCTION} was passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
+    endif()
 
-# For ports which are not CMake
-if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/${install_subdir}/${PORT}.pdb" AND 
-       EXISTS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}.pdb")
-    file(INSTALL 
-                "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}.pdb" 
-            DESTINATION
-                "${CURRENT_PACKAGES_DIR}/${install_subdir}")
-    file(INSTALL 
-                "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/${PORT}_strip.pdb" 
-            DESTINATION
-                "${CURRENT_PACKAGES_DIR}/${install_subdir}")
-endif()
+    file(GLOB_RECURSE rel_dlls_or_exe "${CURRENT_PACKAGES_DIR}/*.dll" "${CURRENT_PACKAGES_DIR}/*.exe")
+    set(dbg_dlls_or_exe "${rel_dlls_or_exe}")
+    list(FILTER dbg_dlls_or_exe INCLUDE "${CURRENT_PACKAGES_DIR}/debug/")
+    list(FILTER rel_dlls_or_exe EXCLUDE "${CURRENT_PACKAGES_DIR}/debug/")
 
-if(NOT EXISTS "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}/${PORT}.pdb" AND 
-       EXISTS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}.pdb")
-    file(INSTALL 
-                "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}.pdb" 
-            DESTINATION
-                "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}")
-    file(INSTALL 
-                "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/${PORT}_strip.pdb" 
-            DESTINATION
-                "${CURRENT_PACKAGES_DIR}/debug/${install_subdir}")
-endif()
+    set(vslang_backup "$ENV{VSLANG}")
+    set(ENV{VSLANG} 1033)
+
+    set(dlls_without_matching_pdbs "")
+
+    foreach(dll IN LISTS dlls)
+        execute_process(COMMAND dumpbin /PDBPATH "${dll}"
+                        COMMAND findstr PDB
+            OUTPUT_VARIABLE pdb_line
+            ERROR_QUIET
+            RESULT_VARIABLE error_code
+        )
+            if(error_code EQUAL "0" AND pdb_line MATCHES "PDB file found at.*'(.*)'")
+                set(pdb_path "${CMAKE_MATCH_1}")
+                cmake_path(GET dll PARENT_PATH dll_dir)
+                file(COPY "${pdb_path}" DESTINATION "${dll_dir}")
+            else()
+                list(APPEND dlls_without_matching_pdbs "${dll}")
+            endif()
+    endforeach()
+
+    set(ENV{VSLANG} "${vslang_backup}")
+
+    if(NOT dlls_without_matching_pdbs STREQUAL "")
+        list(JOIN dlls_without_matching_pdbs "\n\t" msg)
+        message(WARNING "Could not find a matching pdb file for:\n${msg}\n")
+    endif()
+endfunction()
+
+z_vcpkg_copy_pdbs()
