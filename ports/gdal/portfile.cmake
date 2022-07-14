534ae3f4e8e9a455c196ea06917ab120cbab4fcc
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2022-07-14 13:36:22 +0200

    [gdal] no absolute paths

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -147,6 +147,8 @@ if(NOT bin_files)
     )
 endif()
 
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/cpl_config.h" "#define GDAL_PREFIX \"${CURRENT_PACKAGES_DIR}\"" "")
+
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE.TXT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
