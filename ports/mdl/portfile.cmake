399394ba3a5aa94c2db9bd5db7ad53b48996f21b
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2022-07-18 22:46:25 +0200

    Fix incorrect clang path for UNIX

--- ports/mdl/portfile.cmake
+++ ports/mdl/portfile.cmake
@@ -10,30 +10,34 @@ if(VCPKG_TARGET_IS_WINDOWS)
         set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/win64")
         set(LLVM_FILENAME  "LLVM-${LLVM_VERSION}-win64.exe")
         set(LLVM_HASH      c2b1342469275279f833fdc1e17ba5a9f99021306d6ab3d7209822a01d690767739eebf92fd9f23a44de5c5d00260fed50d5262b23a8eccac55b8ae901e2815c)
+        set(LLVM_CLANG7    "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/win64/bin/clang${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
     else()
         message(FATAL_ERROR "Pre-built binaries for Clang 7 not available, aborting install (platform: ${VCPKG_CMAKE_SYSTEM_NAME}, architecture: ${VCPKG_TARGET_ARCHITECTURE}).")
     endif()
 elseif(VCPKG_TARGET_IS_LINUX)
     if (VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/x86_64-linux-gnu-ubuntu-16.04")
+        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}")
         set(LLVM_FILENAME  "clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04.tar.xz")
         set(LLVM_HASH      fb3dc588137426dc28a20ef5e34e9341b18114f03bf7d83fafbb301efbfd801bba08615b804817c80252e366de9d2f8efbef034e53a1b885b34c86c2fbbf9c28)
+        set(LLVM_CLANG7    "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04/bin/clang${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
     else()
         message(FATAL_ERROR "Pre-built binaries for Clang 7 not available, aborting install (platform: ${VCPKG_CMAKE_SYSTEM_NAME}, architecture: ${VCPKG_TARGET_ARCHITECTURE}).")
     endif()
 elseif(VCPKG_TARGET_IS_FREEBSD)
     if (VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/amd64-unknown-freebsd11")
+        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}")
         set(LLVM_FILENAME  "clang+llvm-${LLVM_VERSION}-amd64-unknown-freebsd11.tar.xz")
         set(LLVM_HASH      d501484c38cfced196128866a19f7fef1e0b5d609ea050d085b7deab04ac8cc2bbf74b3cfe6cd90d8ea17a1d9cfca028a6c933f0736153ba48785ddc8646574f)
+        set(LLVM_CLANG7    "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-amd64-unknown-freebsd11/bin/clang${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
     else()
         message(FATAL_ERROR "Pre-built binaries for Clang 7 not available, aborting install (platform: ${VCPKG_CMAKE_SYSTEM_NAME}, architecture: ${VCPKG_TARGET_ARCHITECTURE}).")
     endif()
 elseif(VCPKG_TARGET_IS_OSX)
     if (VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/x86_64-apple-darwin")
+        set(LLVM_DIRECTORY "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}")
         set(LLVM_FILENAME  "clang+llvm-${LLVM_VERSION}-x86_64-apple-darwin.tar.xz")
         set(LLVM_HASH      c5ca6a7756e0cecdf78d4d0c522fe7e803d4b1b2049cb502a034fe8f5ca30fcbf0e738ebfbc89c87de8adcd90ea64f637eb82e9130bb846b43b91f67dfa4b916)
+        set(LLVM_CLANG7    "${DOWNLOADS}/tools/llvm-${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-apple-darwin/bin/clang${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
     else()
         message(FATAL_ERROR "Pre-built binaries for Clang 7 not available, aborting install (platform: ${VCPKG_CMAKE_SYSTEM_NAME}, architecture: ${VCPKG_TARGET_ARCHITECTURE}).")
     endif()
@@ -41,33 +45,37 @@ else()
     message(FATAL_ERROR "Pre-built binaries for Clang 7 not available, aborting install (platform: ${VCPKG_CMAKE_SYSTEM_NAME}, architecture: ${VCPKG_TARGET_ARCHITECTURE}).")
 endif()
 
-vcpkg_download_distfile(LLVM_ARCHIVE_PATH
-  URLS     "${LLVM_BASE_URL}/${LLVM_FILENAME}"
-  SHA512   ${LLVM_HASH}
-  FILENAME "${LLVM_FILENAME}"
-)
+include(CMakePrintHelpers)
+cmake_print_variables(LLVM_BASE_URL LLVM_FILENAME LLVM_CLANG7)
 
-file(MAKE_DIRECTORY "${LLVM_DIRECTORY}")
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_find_acquire_program(7Z)
-    vcpkg_execute_in_download_mode(
-        COMMAND ${7Z} x
-            "${LLVM_ARCHIVE_PATH}"
-            "-o${LLVM_DIRECTORY}"
-            -y -bso0 -bsp0
-        WORKING_DIRECTORY "${LLVM_DIRECTORY}"
+if(NOT EXISTS ${LLVM_CLANG7})
+    vcpkg_download_distfile(LLVM_ARCHIVE_PATH
+      URLS     "${LLVM_BASE_URL}/${LLVM_FILENAME}"
+      SHA512   ${LLVM_HASH}
+      FILENAME "${LLVM_FILENAME}"
     )
-else()
-    vcpkg_execute_in_download_mode(
-        COMMAND "${CMAKE_COMMAND}" -E tar xzf "${LLVM_ARCHIVE_PATH}"
-        WORKING_DIRECTORY "${LLVM_DIRECTORY}"
-    )
-endif()
 
-set(CLANG7 "${LLVM_DIRECTORY}/bin/clang${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
-if(NOT EXISTS "${CLANG7}")
-    message(FATAL_ERROR "Missing required build tool clang 7, please check your setup.")
+    file(MAKE_DIRECTORY "${LLVM_DIRECTORY}")
+
+    if(VCPKG_TARGET_IS_WINDOWS)
+        vcpkg_find_acquire_program(7Z)
+        vcpkg_execute_in_download_mode(
+            COMMAND ${7Z} x
+                "${LLVM_ARCHIVE_PATH}"
+                "-o${LLVM_DIRECTORY}"
+                -y -bso0 -bsp0
+            WORKING_DIRECTORY "${LLVM_DIRECTORY}"
+        )
+    else()
+        vcpkg_execute_in_download_mode(
+            COMMAND "${CMAKE_COMMAND}" -E tar xzf "${LLVM_ARCHIVE_PATH}"
+            WORKING_DIRECTORY "${LLVM_DIRECTORY}"
+        )
+    endif()
+
+    if(NOT EXISTS "${LLVM_CLANG7}")
+        message(FATAL_ERROR "Missing required build tool clang 7, please check your setup.")
+    endif()
 endif()
 
 # MDL-SDK
@@ -121,7 +129,7 @@ vcpkg_cmake_configure(
         -DMDL_BUILD_ARNOLD_PLUGIN:BOOL=OFF
         
         -Dpython_PATH:PATH=${PYTHON3}
-        -Dclang_PATH:PATH=${CLANG7}
+        -Dclang_PATH:PATH=${LLVM_CLANG7}
 
         ${FEATURE_OPTIONS}
 )
