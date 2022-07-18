0e7952b591ab1c87cbb95aa0b58c7692c79b1e93
JackBoosY <yuzaiyang@beyondsoft.com>
2022-07-18 00:27:56 -0700

    Fix dependencies vulkan-headers and vulkan-loader, fix binary output path, fix install headers and add more options

--- ports/gpgmm/portfile.cmake
+++ ports/gpgmm/portfile.cmake
@@ -1,22 +1,31 @@
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO intel/gpgmm
-  REF v0.0.4
-  SHA512 2ffc3c8299f2d10cb1c0013cd306ba45781a644fa0aa426ef1dfa616e4b53671461a376f65b7068b1ff8a4a2d1a6f9539664174eb5830ea6a760ef5e5d0fc6b0
-  HEAD_REF main
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO intel/gpgmm
+    REF v0.0.4
+    SHA512 2ffc3c8299f2d10cb1c0013cd306ba45781a644fa0aa426ef1dfa616e4b53671461a376f65b7068b1ff8a4a2d1a6f9539664174eb5830ea6a760ef5e5d0fc6b0
+    HEAD_REF main
+    PATCHES
+        fix-dependency-vulkan.patch
+        fix-binary-path-and-install-hdrs.patch
 )
 
-# gpgmm\third_party config requires Git. Make the tool visible.
-vcpkg_find_acquire_program(GIT)
-get_filename_component(GIT_DIR "${GIT}" DIRECTORY)
-vcpkg_add_to_path("${GIT_DIR}")
-
 vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
-  DISABLE_PARALLEL_CONFIGURE
-  OPTIONS
-  -DGPGMM_STANDALONE=OFF
-  -DGPGMM_ENABLE_TESTS=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        ${EXTRA_OPTIONS}
+        -DGPGMM_ENABLE_VK=ON
+        "-DGPGMM_VK_TOOLS_DIR=$ENV{VULKAN_SDK}/Tools"
+        -DGPGMM_STANDALONE=OFF
+        -DGPGMM_ENABLE_TESTS=OFF
+    OPTIONS_DEBUG
+        -DGPGMM_ALWAYS_ASSERT=ON
+        -DGPGMM_FORCE_TRACING=ON
+        -DGPGMM_ENABLE_DEVICE_LEAK_CHECKS=ON
+        -DGPGMM_ENABLE_ALLOCATOR_LEAK_CHECKS=ON
+        -DGPGMM_ENABLE_MEMORY_LEAK_CHECKS=ON
+        -DGPGMM_ENABLE_RESOURCE_MEMORY_ALIGN_CHECKS=ON
 )
 
 vcpkg_cmake_install()
