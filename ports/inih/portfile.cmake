de690822c7dc58c7049a85ce1947eb0cf7220557
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-07-20 00:28:59 -0400

    [ports/inih] Upgrade to latest vcpkg recommendations

--- ports/inih/portfile.cmake
+++ ports/inih/portfile.cmake
@@ -10,12 +10,11 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/inih RENAME copyright)
 
