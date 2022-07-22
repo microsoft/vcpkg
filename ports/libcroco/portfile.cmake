9ad31449501ac2fd5ef91d954e5ba4f91b2aa273
Neumann-A <neumann@imt.uni-luebeck.de>
2022-07-22 16:50:04 +0200

    libcroco switch to make

--- ports/libcroco/portfile.cmake
+++ ports/libcroco/portfile.cmake
@@ -6,28 +6,23 @@ vcpkg_download_distfile(ARCHIVE
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE} 
+    ARCHIVE "${ARCHIVE}"
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-configure_file(${SOURCE_PATH}/config.h.win32 ${SOURCE_PATH}/src/config.h COPYONLY)
-file(READ "${SOURCE_PATH}/src/libcroco.symbols" SYMBOLS)
-string(REGEX REPLACE ";[^\n]*\n" "" DEF "EXPORTS\n${SYMBOLS}")
-file(WRITE "${SOURCE_PATH}/src/libcroco.def" "${DEF}")
-vcpkg_find_acquire_program(PKGCONFIG)
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-    OPTIONS
-        -DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}
+vcpkg_configure_make(
+    USE_WRAPPERS
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_install_make()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-libcroco TARGET_PATH share/unofficial-libcroco)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" 
+                    "${CURRENT_PACKAGES_DIR}/libcroco/bin/croco-0.6-config"
+                    "${CURRENT_PACKAGES_DIR}/libcroco/debug/bin")
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libcroco RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libcroco" RENAME copyright)
 
 vcpkg_copy_pdbs()
