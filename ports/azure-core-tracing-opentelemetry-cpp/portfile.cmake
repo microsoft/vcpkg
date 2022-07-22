e564bf4cbe66c11f668731382182c8b3d315c3a5
Billy Robert O'Neal III <bion@microsoft.com>
2022-07-22 13:49:15 -0700

    Nitpick on using identical patch file.

--- ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
+++ ports/azure-core-tracing-opentelemetry-cpp/portfile.cmake
@@ -4,9 +4,8 @@ vcpkg_from_github(
     REF azure-core-tracing-opentelemetry_1.0.0-beta.2
     SHA512 a0697ed440a08f3814013adf476c2a1333b462a2eb96bac18c47939c54686396639935568e229dad6bbbae06f54c324da82544634c6384a2c1498f9959836b6d
     PATCHES
-        "use-opentelemetry-cpp-141.patch"
+        "3844.patch" # https://github.com/Azure/azure-sdk-for-cpp/pull/3844 "Removed hard dependency on opentelemetry version"
 )
-# Removed hard dependency on specific opentelemetry-cpp version as per https://github.com/Azure/azure-sdk-for-cpp/pull/3844
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}/sdk/core/azure-core-tracing-opentelemetry/
