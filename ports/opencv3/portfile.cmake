5713f35a79d0284a70095bc60dae1f3f4f7792b7
Stefano Sinigardi <stesinigardi@hotmail.com>
2022-07-22 10:24:59 +0200

    cleanup after merge

--- ports/opencv3/portfile.cmake
+++ ports/opencv3/portfile.cmake
@@ -323,6 +323,7 @@ vcpkg_cmake_configure(
         -DOPENCV_DLLVERSION=3
         -DOPENCV_DEBUG_POSTFIX=d
         -DOPENCV_GENERATE_SETUPVARS=OFF
+        -DOPENCV_GENERATE_PKGCONFIG=ON
         # Do not build docs/examples
         -DBUILD_DOCS=OFF
         -DBUILD_EXAMPLES=OFF
