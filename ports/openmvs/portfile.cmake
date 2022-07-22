8bfd0ff4f9785d2a425ae8bab50bba7317ee85d4
Stefano Sinigardi <stesinigardi@hotmail.com>
2022-07-22 13:58:46 +0200

    [openmvs] trying to diagnose the problem - DO NOT MERGE

--- ports/openmvs/portfile.cmake
+++ ports/openmvs/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_github(
     PATCHES
         fix-build.patch
         no-absolute-paths.patch
+        find-opencv-debug.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
