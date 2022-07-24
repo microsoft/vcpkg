70a79642cc189ccac0034ed0f0dc7b59b029ed30
Johannes Schönberger <joschonb@microsoft.com>
2022-07-24 16:20:22 +0200

    Add mirror URLs for metis port

--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -4,7 +4,10 @@ set(OPTIONS -DSHARED=OFF)
 set(METIS_VERSION 5.1.0)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz"
+    URLS
+        "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz"
+        "https://github.com/mfem/tpls/blob/gh-pages/metis-${METIS_VERSION}.tar.gz?raw=true"
+        "https://github.com/hojatollahgholami/thirdpartyOpenfoam/blob/main/metis-${METIS_VERSION}.tar.gz?raw=true"
     FILENAME "metis-${METIS_VERSION}.tar.gz"
     SHA512 deea47749d13bd06fbeaf98a53c6c0b61603ddc17a43dae81d72c8015576f6495fd83c11b0ef68d024879ed5415c14ebdbd87ce49c181bdac680573bea8bdb25
 )
