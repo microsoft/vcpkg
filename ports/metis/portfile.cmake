65e8996dc1636cd193923a3b89b06c322f7cc92c
Johannes Schönberger <joschonb@microsoft.com>
2022-07-24 16:26:28 +0200

    Specific commit

--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -6,8 +6,8 @@ set(METIS_VERSION 5.1.0)
 vcpkg_download_distfile(ARCHIVE
     URLS
         "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz"
-        "https://github.com/mfem/tpls/blob/gh-pages/metis-${METIS_VERSION}.tar.gz?raw=true"
-        "https://github.com/hojatollahgholami/thirdpartyOpenfoam/blob/main/metis-${METIS_VERSION}.tar.gz?raw=true"
+        "https://github.com/mfem/tpls/blob/04a8d38f7d2fe4910405feddf067e0b0e962cf27/metis-${METIS_VERSION}.tar.gz?raw=true"
+        "https://github.com/hojatollahgholami/thirdpartyOpenfoam/blob/3130f101ba0fab6895d632a23bc382a03cccb1a3/metis-${METIS_VERSION}.tar.gz?raw=true"
     FILENAME "metis-${METIS_VERSION}.tar.gz"
     SHA512 deea47749d13bd06fbeaf98a53c6c0b61603ddc17a43dae81d72c8015576f6495fd83c11b0ef68d024879ed5415c14ebdbd87ce49c181bdac680573bea8bdb25
 )
