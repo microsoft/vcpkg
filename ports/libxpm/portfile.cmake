282e51388cd965a47a8d77cf6cd8c52334b79ef2
Vladimir Aubrecht <vladimir.aubrecht@me.com>
2022-07-25 15:36:57 +0200

    Libxpm is taken from gitlab now.

--- ports/libxpm/portfile.cmake
+++ ports/libxpm/portfile.cmake
@@ -1,10 +1,11 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO freedesktop/libXpm
-    REF 3425cbb0e6086f74783eafbe23df1121b655e006  #libXpm-v3.5.11
-    SHA512 8c540171442cf6f8a770c5ba92bc3e4c032e834447ba62d9d35a545945c6fbe5b0ac10714e12e5a312e05cb5808ca8fb19c83c7223b530ae67cb4f1d650c4129
+vcpkg_from_gitlab(
+GITLAB_URL https://gitlab.freedesktop.org/xorg
+OUT_SOURCE_PATH SOURCE_PATH
+REPO lib/libxpm
+REF libXpm-3.5.13
+SHA512 250c8bf672789a81cfa258a516d40936f48a56cfaee94bf3f628e3f4a462bdd90eaaea787d66daf09ce4809b89c3eaea1e0771de03a6d7f1a59b31cc82be1c44
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
