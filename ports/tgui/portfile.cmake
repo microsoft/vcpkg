335c772101435d34533fe49ee40539430ccb549e
Kai Pastor <dg0yt@darc.de>
2022-07-27 21:47:56 +0200

    [tgui] Revise port

--- ports/tgui/portfile.cmake
+++ ports/tgui/portfile.cmake
@@ -1,3 +1,9 @@
+if(VCPKG_TARGET_IS_ANDROID)
+    vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+elseif(VCPKG_TARGET_IS_IOS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO texus/TGUI
@@ -9,12 +15,12 @@ vcpkg_from_github(
         fix-dependencies.patch
 )
 
+file(REMOVE_RECURSE "${SOURCE_PATH}/src/Backends/SDL/cmake_modules") # Config available
+
 set(TGUI_SHARE_PATH "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 set(TGUI_TOOLS_PATH "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 
-# Enable static build
-file(REMOVE "${SOURCE_PATH}/cmake/Modules/FindSFML.cmake")
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" TGUI_SHARED_LIBS)
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" TGUI_USE_STATIC_STD_LIBS)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -26,11 +32,18 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
-    OPTIONS ${FEATURE_OPTIONS}
-        -DTGUI_MISC_INSTALL_PREFIX=${TGUI_SHARE_PATH}
-        -DTGUI_SHARED_LIBS=${TGUI_SHARED_LIBS}
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        "-DTGUI_MISC_INSTALL_PREFIX=${TGUI_SHARE_PATH}"
+        "-DCMAKE_INSTALL_DOCDIR=${TGUI_SHARE_PATH}"
+        -DTGUI_USE_STATIC_STD_LIBS=${TGUI_USE_STATIC_STD_LIBS}
         -DTGUI_BACKEND=Custom
-        -DTGUI_BUILD_EXAMPLES=OFF
+        -DTGUI_BUILD_DOC=OFF
+        -DTGUI_BUILD_FRAMEWORK=OFF
+    OPTIONS_DEBUG
+        -DTGUI_BUILD_GUI_BUILDER=OFF
+    MAYBE_UNUSED_VARIABLES
+        TGUI_BUILD_FRAMEWORK
 )
 
 vcpkg_cmake_install()
@@ -52,17 +65,25 @@ if("tool" IN_LIST FEATURES)
     endif()
 endif()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/nanosvg" "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/glad"
-     "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/stb"
-) # All folders are empty
-
-file(RENAME "${CURRENT_PACKAGES_DIR}/share/doc" "${CURRENT_PACKAGES_DIR}/share/${PORT}/doc")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/${PORT}/doc")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+     # Empty folders
+    "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/nanosvg"
+    "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/glad"
+    "${CURRENT_PACKAGES_DIR}/include/TGUI/extlibs/stb"
+)
 
 vcpkg_fixup_pkgconfig()
+set(lib " -ltgui")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    string(APPEND lib "-s")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/tgui.pc" " -ltgui" "${lib}")
+endif()
+if(NOT VCPKG_BUILD_TYPE)
+    string(APPEND lib "-d")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/tgui.pc" " -ltgui" "${lib}")
+endif()
 
-# Broken
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(RENAME "${CURRENT_PACKAGES_DIR}/share/${PORT}/license.txt" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
