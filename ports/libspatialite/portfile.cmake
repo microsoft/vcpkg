dd73b021dcd8b9ddc51c0eb01aeefcb72b837ed8
Matthias Kuhn <matthias@opengis.ch>
2022-07-27 21:39:11 +0200

    [libspatialite] mingw support (#25881)

--- ports/libspatialite/portfile.cmake
+++ ports/libspatialite/portfile.cmake
@@ -178,6 +178,7 @@ else()
     vcpkg_configure_make(
         SOURCE_PATH "${SOURCE_PATH}"
         AUTOCONFIG
+        DETERMINE_BUILD_TRIPLET
         OPTIONS
             ${TARGET_ALIAS}
             ${FREEXL_OPTION}
