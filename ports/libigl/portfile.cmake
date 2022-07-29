40b58ad5ae49a4e13313a4b5f0a5377b8e971b7e
FrankXie <v-frankxie@microsoft.com>
2022-07-29 00:39:30 -0700

    update option and fix build error

--- ports/libigl/portfile.cmake
+++ ports/libigl/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF  66b3ef2253e765d0ce0db74cec91bd706e5ba176 #2.4.0
     SHA512 7014ffdaa160bfa2509fc283cb7176d7994a37f51509c7374659292efad076c8fb594f9f6990bab1aa5562d1f66e93403ea35a5bf2a924436560a2d4669ffcfd
     HEAD_REF master
+    PATCHES
+       Do-not-export-eigen.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -22,20 +24,15 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS ${FEATURE_OPTIONS}
-        -DLIBIGL_INSTALL=ON
-        -DLIBIGL_USE_STATIC_LIBRARY=OFF # Header-only mode
-        -DLIBIGL_COPYLEFT_COMISO=OFF
-        -DLIBIGL_COPYLEFT_TETGEN=OFF
-        -DLIBIGL_RESTRICTED_TRIANGLE=OFF
-        -DLIBIGL_PREDICATES=OFF
-        -DLIBIGL_BUILD_TUTORIALS=ON
-        -DLIBIGL_PNG=OFF
+        # Build options
         -DLIBIGL_BUILD_TESTS=OFF
+        -DLIBIGL_BUILD_TUTORIALS=OFF
+        -DLIBIGL_INSTALL=OFF
+        -DLIBIGL_USE_STATIC_LIBRARY=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH share/libigl)
 vcpkg_copy_pdbs()
 
 # libigl is a header-only library.
