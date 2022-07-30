5a01771d97a623868b305a3be01c71b96e5f39ef
Robert Schumacher <roschuma@microsoft.com>
2022-07-30 04:44:36 +0000

    [gklib][metis] Fix casing in emitted config files

---
+++ ports/metis/metisConfig.cmake
@@ -0,0 +1,3 @@
+include(CMakeFindDependencyMacro)
+find_dependency(GKlib CONFIG)
+include("${CMAKE_CURRENT_LIST_DIR}/metisTargets.cmake")
--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -11,12 +11,8 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/metisConfig.cmake" "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/metis")
 vcpkg_cmake_config_fixup()
 
-file(INSTALL "${SOURCE_PATH}/include/metis.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" [=[
-metis provides CMake targets:
-    find_package(metis CONFIG REQUIRED)
-    target_link_libraries(main PRIVATE metis)
-]=])
