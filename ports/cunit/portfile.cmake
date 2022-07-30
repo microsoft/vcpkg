152042effc73c41bc69173b6a4dd325cc9310f23
Robert Schumacher <roschuma@microsoft.com>
2022-07-30 00:04:32 +0000

    [cunit] Define VERSION/RELEASE

--- ports/cunit/portfile.cmake
+++ ports/cunit/portfile.cmake
@@ -1,8 +1,11 @@
+set(VERSION 2.1)
+set(RELEASE 3)
+
 vcpkg_from_sourceforge(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cunit/CUnit
-    REF 2.1-3
-    FILENAME "CUnit-2.1-3.tar.bz2"
+    REF "${VERSION}-${RELEASE}"
+    FILENAME "CUnit-${VERSION}-${RELEASE}.tar.bz2"
     SHA512 547b417109332446dfab8fda17bf4ccd2da841dc93f824dc90a20635bcf1fb80fb2176500d8a0906940f3f3d3e2f77b2d70a71090c9ab84ad9af43f3582bc487
 )
 
@@ -10,6 +13,9 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    "-DVERSION=${VERSION}"
+    "-DRELEASE=${RELEASE}"
   OPTIONS_DEBUG
     -DDISABLE_INSTALL_HEADERS=ON
 )
