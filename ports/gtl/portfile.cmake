f8228a1eb5152605cc73dcdc8d2971f21e1d35e7
greg7mdp <greg7mdp@gmail.com>
2022-07-30 22:04:43 -0400

    [gtl] Update to 1.1.1

--- ports/gtl/portfile.cmake
+++ ports/gtl/portfile.cmake
@@ -3,14 +3,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO greg7mdp/gtl
-    REF 1.00
-    SHA512 842a5e2634919a04fdd87995a8ada2f949c51a070a7c175043e0f9105a93248325023f85b28f9406276c2912a0fb4015a2e9ba30113d4a0214492da0dc5e5716
+    REF v1.1.1
+    SHA512 f5e3f17c27a5cb10b87312b92dea73edc7dba8cf51b7bc520b625196a70b05d5aa02e975a2c96b69f60cc617488e218497c0825d944bcd298ae19afc9035c6a4
     HEAD_REF main
 )
 
 # Use greg7mdp/gtl's own build process, skipping examples and tests
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DGTL_BUILD_TESTS=OFF
+        -DGTL_BUILD_EXAMPLES=OFF
+        -DGTL_BUILD_BENCHMARKS=OFF
 )
 vcpkg_cmake_install()
 
