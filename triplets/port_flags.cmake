35c574ed27511d7205e55fd331f0b1c07e681886
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-02 22:47:47 +0200

    fine tune exceptions

--- triplets/port_flags.cmake
+++ triplets/port_flags.cmake
@@ -5,8 +5,10 @@ endif()
 if(PORT MATCHES "^(re2)$")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
-if(PORT MATCHES "^(arrow|flint|folly|glog|zydis)$")
+if(PORT MATCHES "^(arrow|flint|folly|glog|zydis|libirecovery|graphicsmagick)$")
     # arrow implicit deleted default constructor
+    # libirecovery missing getopt linkage
+    # graphicsmagick -> requires wrapping the allocators in namespace std
     # glog hardcodes builtin expected if build with clang-cl (#if 1 otehrwise #if 0) -> folly sees that. 
     # zydis UTF-16 encoding 
     # TODO: Recheck flint
