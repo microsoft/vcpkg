e89f6eaf42cccdebbafdca82fdfdd977f8879834
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-02 19:16:13 +0200

    port selection

--- triplets/port_flags.cmake
+++ triplets/port_flags.cmake
@@ -2,8 +2,13 @@ if(PORT STREQUAL "tbb")
     set(VCPKG_C_FLAGS "-mrtm")
     set(VCPKG_CXX_FLAGS "-mrtm")
 endif()
-if(PORT MATCHES "^(flint|folly|glog)$")
-    # glog hardcodes builtin expected if build with clang-cl (#if 1 otehrwise #if 0)
+if(PORT MATCHES "^(re2)$")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+if(PORT MATCHES "^(arrow|flint|folly|glog|zydis)$")
+    # arrow implicit deleted default constructor
+    # glog hardcodes builtin expected if build with clang-cl (#if 1 otehrwise #if 0) -> folly sees that. 
+    # zydis UTF-16 encoding 
     # TODO: Recheck flint
     message(STATUS "Falling back to cl!")
     unset(VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
