4a55d7542680dcfc401c7c6eb7754b6feca69051
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-03 23:40:45 +0200

    small "fixes"

--- ports/string-theory/portfile.cmake
+++ ports/string-theory/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS "-DST_BUILD_TESTS=OFF"
 )
 
 vcpkg_cmake_install()
--- triplets/port_flags.cmake
+++ triplets/port_flags.cmake
@@ -5,12 +5,16 @@ endif()
 if(PORT MATCHES "^(re2)$")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
-if(PORT MATCHES "^(arrow|flint|folly|glog|zydis|libirecovery|graphicsmagick)$")
+if(PORT MATCHES "^(arrow|flint|folly|glog|zydis|libirecovery|graphicsmagick|freerdp|gtk|irrlicht|libde265|cryptopp|llfio)$")
+    # llfio -> code has correctness issues which cl accepts. (wrong thread_local and fried declaration)
+    # cryptopp misses to pass -m<arch> flags for the clang-cl build
+    # gtk -> .res files in archives via /WHOLEARCHIVE not correctly handled by lld-link
+    # libde265 -> probably some macro collision
     # arrow implicit deleted default constructor
-    # libirecovery missing getopt linkage
+    # libirecovery missing getopt linkage -> linkage general problem in msbuild ports since autolinkage is deactivated`?
     # graphicsmagick -> requires wrapping the allocators in namespace std
     # glog hardcodes builtin expected if build with clang-cl (#if 1 otehrwise #if 0) -> folly sees that. 
-    # zydis UTF-16 encoding 
+    # zydis,freerdp,irrlicht UTF-16 encoding in rc file
     # TODO: Recheck flint
     message(STATUS "Falling back to cl!")
     unset(VCPKG_CHAINLOAD_TOOLCHAIN_FILE)
