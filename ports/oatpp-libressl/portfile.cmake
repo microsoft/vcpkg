a4129f8cdbc1a71208ca6d23d5d7d4f8f9bb5f01
JackBoosY <yuzaiyang@beyondsoft.com>
2022-08-02 22:53:21 -0700

    try to figure out what triplet needs crt macros

--- ports/oatpp-libressl/portfile.cmake
+++ ports/oatpp-libressl/portfile.cmake
@@ -12,9 +12,6 @@ vcpkg_from_github(
     PATCHES "libress-submodule-downgrade-required-libressl-version.patch"
 )
 
-set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
-set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -D_CRT_SECURE_NO_WARNINGS")
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
