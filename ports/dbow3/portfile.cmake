f78c640896f4eda0ea85fbaac2641349b889b7c3
Matthieu Penant <thieum22@hotmail.com>
2022-08-03 02:49:30 -0400

    Github-actions fixes (#2)
    * Update opencv4.json
    
    * fix github-actions issues
    
    * formating vcpkg.json
    
    * update git-trees

--- ports/dbow3/portfile.cmake
+++ ports/dbow3/portfile.cmake
@@ -9,18 +9,17 @@ vcpkg_from_github(
       fix_cmake.patch
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -DUSE_OPENCV_CONTRIB=ON
         -DBUILD_EXAMPLES=OFF
         -DBUILD_TESTS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake/DBow3)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake/DBow3)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
