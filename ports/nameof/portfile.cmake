cb601e260f30987dda602e033fabcf73f3bf21fc
neargye <neargye@gmail.com>
2022-08-04 17:42:04 +0400

    [nameof] Update to 0.10.2

--- ports/nameof/portfile.cmake
+++ ports/nameof/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Neargye/nameof
-    REF v0.10.1
-    SHA512 9b3ed7a3f5dcf1ad309341102d2aaa07fc8e9d6db7e08e42c2d9b5453c822ec16d6c6910adf7c4af8879c4084cac362f4fb4c07559cc49594b72b4917816ef5d
+    REF v0.10.2
+    SHA512 1ec508378d12eb4a65da1bee8011302f65e78bbcab82aae716afc673665af5283a5aae5f1d0de85c7d6f59bb7268e02fc01b171f50de26bb86dc27c3b46097fa
     HEAD_REF master
 )
 
