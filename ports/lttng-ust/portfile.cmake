810117703e9f8ca7cdcf2f5378b86da9d6956ec0
Diego Velazquez Alvarez <t-diegove@microsoft.com>
2022-08-05 11:10:21 -0700

    Add lttng-ust port.
    Format and add version.
    
    Update CI baseline.
    
    Update port version.
    
    [vcpkg] Fix cross compiling macOS (#26240)
    
    This follows the pattern in the Windows toolchain with respect to
    setting VCPKG_CMAKE_SYSTEM_VERSION and also how arm64 macOS can execute
    x86_64 through Rosetta.
    
    [buck-yeh-bux] fix clang-cl builds (#26248)
    
    * [buck-yeh-bux] fix clang-cl
    
    * v db
    
    [cppcms] Fix narrowing for clang-cl builds (#26251)
    
    * [cppcms] fix narrowing error with clang-cl
    
    * v db
    
    [crc32c] Clang-cl fix  (#26253)
    
    * [crc32c] fix clang-cl builds.
    
    * v db
    
    [json-c] clang-cl fix. (#26254)
    
    * [json-c] fix clang-cl builds
    
    * v db
    
    [cpr] disable werror (#26252)
    
    * [cpr] disable werror
    
    * v db
    
    [sqlcipher] update to 4.5.2 (#26262)
    
    * [sqlcipher] update to 4.5.2
    
    * requested changes
    
    * portfile.cmake - quote paths
    
    [osgearth] Fix naming of target and exported variables (#26258)
    
    * [osgearth] Fix target name and match exported variables
    
    * Fix version SHA
    
    Add port version to baseline.
    
    Add x64-linux build expected to fail.
    
    Add port 0 version.
    
    Update version database
    
    Specify supported CI so it only build dynamic.
    
    Update SHA.

---
+++ ports/lttng-ust/portfile.cmake
@@ -0,0 +1,47 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lttng/lttng-ust
+    REF v2.12.5
+    SHA512 1ba781218608ce10decedab49a947b7cc60bf8e69d77d5bc56629238b97e5006131eec4071143f14968e602c535f94a7c034d44e1f27eca3fbc5edb995a476d3
+    HEAD_REF main
+)
+
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_FIXUP_ELF_RPATH ON)
+
+if(EXISTS "${src_dir}/configure" AND "${src_dir}/configure.ac")
+     if(NOT VCPKG_MAINTAINER_SKIP_AUTOCONFIG)
+         set(requires_autoconfig ON) 
+         file(REMOVE "${SRC_DIR}/configure")
+         set(arg_AUTOCONFIG ON) 
+     endif() 
+ elseif(EXISTS "${src_dir}/configure" AND NOT arg_SKIP_CONFIGURE)
+ endif()
+
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+    OPTIONS
+        --prefix=/usr
+        --disable-man-pages
+        ${OPTIONS}
+
+)
+
+vcpkg_install_make()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_fixup_pkgconfig()
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
