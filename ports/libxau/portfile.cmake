b96b22a64d88cd6518b220e0a0bcf5f4c65b7fab
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-06 14:35:23 +0200

    rename xau to libxau

--- ports/libxau/portfile.cmake
+++ ports/libxau/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_gitlab(
     REPO lib/libxau
     REF d9443b2c57b512cfb250b35707378654d86c7dea # 1.0.9 
     SHA512  d46d2be838b8ea7116ee0e312adafa80f6551762b5f7b5e503bd40e74fc0b43b45255e8135d03f831d5c483b98aac992fcd91a7e22119261e76778571a72ef07
-    HEAD_REF master # branch name
+    HEAD_REF master
 ) 
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
@@ -26,6 +26,6 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# # Handle copyright
+# Handle copyright
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 endif()
