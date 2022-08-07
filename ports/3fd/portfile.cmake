7beb6f21a2077c3c6d7fd662d89f63da05c97438
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-07 21:16:44 +0200

    fix 3fd

--- ports/3fd/portfile.cmake
+++ ports/3fd/portfile.cmake
@@ -1,14 +1,3 @@
-# Check architecture:
-if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(BUILD_ARCH "Win32")
-elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(BUILD_ARCH "x64")
-elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    set(BUILD_ARCH "ARM")
-else()
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
-endif()
-
 # Check library linkage:
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY ONLY_DYNAMIC_CRT)
 
@@ -16,35 +5,45 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY ONLY_DYNAMIC_CRT)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO faburaya/3fd
-    REF 3a0fe606268721d1560b88dcca8647c67c0b275c # v2.6.3 (Stable)
-    SHA512 70630291b4055de2044ad76ef21e99d6ab6fd3468debb2a864a461cf8513642fe87f116e9dfff96ecff96f4577108493dc25aa40eeefcd93ee75990b13bb7b20
+    REF cde5d33a2e22dc6a02423334b0a8b766348cb296 # v2.8
+    SHA512 7ca4a9dd72ab32c162fa4add6c9b2638094bc870bb4147dedbe6325621b91f947b9ec45f677032f98b8b32c9999a20cfa640de3e9a00f506779dca10c2a88659
     HEAD_REF master
     PATCHES
-        RapidXML.patch
+        fix_cmake.patch
+        #RapidXML.patch
 )
 
 # Build:
+if(0)
 if (VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore") # UWP:
     vcpkg_install_msbuild(
         SOURCE_PATH "${SOURCE_PATH}"
-        PROJECT_SUBPATH "3FD/3FD.WinRT.UWP.vcxproj"
-        PLATFORM ${BUILD_ARCH}
+        PROJECT_SUBPATH "3fd/core/3fd-core-winrt.vcxproj"
         USE_VCPKG_INTEGRATION
+        ALLOW_ROOT_INCLUDES
     )
 elseif (NOT VCPKG_CMAKE_SYSTEM_NAME) # Win32:
     vcpkg_install_msbuild(
         SOURCE_PATH "${SOURCE_PATH}"
-        PROJECT_SUBPATH "3FD/3FD.vcxproj"
-        PLATFORM ${BUILD_ARCH}
+        PROJECT_SUBPATH "3fd/core/3fd-core.vcxproj"
         TARGET Build
         USE_VCPKG_INTEGRATION
+        ALLOW_ROOT_INCLUDES
     )
 else()
     message(FATAL_ERROR "Unsupported system: 3FD is not currently ported to VCPKG in ${VCPKG_CMAKE_SYSTEM_NAME}!")
 endif()
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    )
+vcpkg_cmake_install()
+
 
 # Install:
-file(GLOB HEADER_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/3FD/*.h")
+file(GLOB HEADER_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/3fd/*/*.h") # TODO fix include structure
 file(INSTALL
     ${HEADER_FILES}
     DESTINATION ${CURRENT_PACKAGES_DIR}/include/3FD
@@ -54,17 +53,16 @@ file(INSTALL
     PATTERN "targetver.h" EXCLUDE
 )
 
-file(INSTALL ${SOURCE_PATH}/btree  DESTINATION ${CURRENT_PACKAGES_DIR}/include/3FD)
-file(INSTALL ${SOURCE_PATH}/OpenCL/CL DESTINATION ${CURRENT_PACKAGES_DIR}/include/3FD)
+file(INSTALL "${SOURCE_PATH}/btree"  DESTINATION "${CURRENT_PACKAGES_DIR}/include/3FD")
 
-file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/3FD)
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/3FD")
 file(INSTALL
-    ${SOURCE_PATH}/3FD/3fd-config-template.xml
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/3FD
+    "${SOURCE_PATH}/3fd/core/3fd-config-template.xml"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/3FD"
 )
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/3fd RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/Acknowledgements.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/3fd)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/3fd" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/Acknowledgements.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/3fd")
 
 vcpkg_copy_pdbs()
