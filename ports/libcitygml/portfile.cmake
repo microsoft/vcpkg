7c2b133cc5848a81004b1a7742c6f2bd283268b1
maz-1 <maz-1@users.noreply.github.com>
2022-08-08 23:47:57 +0800

    [libictygml] fix syntax

--- ports/libcitygml/portfile.cmake
+++ ports/libcitygml/portfile.cmake
@@ -5,14 +5,15 @@ vcpkg_from_github(
     SHA512 765f9d05fba9108cb6f23803824a61ab0bf30b05ae4cf5c8faa87915c03bb3ad5c1fdc03d420aea2dd3708d60040d6e10232a714595ab161f6e1527f3176d2aa
     HEAD_REF master
     PATCHES
-        0002_fix_tools.patch
+        0001_fix_tools.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         osg         LIBCITYGML_OSGPLUGIN
         gdal        LIBCITYGML_USE_GDAL
-        tools       LIBCITYGML_TESTS
+        tests       LIBCITYGML_TESTS
+        tools       LIBCITYGML_TOOLS
 )
 
 if ("osg" IN_LIST FEATURES)
@@ -44,20 +45,21 @@ vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-if ("tools" IN_LIST FEATURES)
+if ("tests" IN_LIST FEATURES)
     vcpkg_copy_tools(
         TOOL_NAMES citygmltest
         AUTO_CLEAN
     )
-    if ("osg" IN_LIST FEATURES)
-        vcpkg_copy_tools(
-            TOOL_NAMES citygmlOsgViewer
-            AUTO_CLEAN
-        )
-    endif()
+endif()
+
+if ("tools" IN_LIST FEATURES AND "osg" IN_LIST FEATURES)
+    vcpkg_copy_tools(
+        TOOL_NAMES citygmlOsgViewer
+        AUTO_CLEAN
+    )
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
