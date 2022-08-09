6b841fcd53c6d787fb44f783053b6a190b196061
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-09 22:18:06 +0200

    arb akali to cl

--- triplets/port_flags.cmake
+++ triplets/port_flags.cmake
@@ -6,8 +6,9 @@ if(PORT MATCHES "^(re2)$")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
 # Note: All gn ports still use cl unless we figure out to pass it a toolchain somehow. 
-if(PORT MATCHES "^(arrow|alkali|flint|folly|glog|zydis|graphicsmagick|freerdp|gtk|irrlicht|libde265|llfio|mongo-c-driver|tcl|nvtt)$")
-    # alkali -> typedef private void (T::*POnTimer)(void); -> error
+if(PORT MATCHES "^(arrow|akali|arb|flint|folly|glog|zydis|graphicsmagick|freerdp|gtk|irrlicht|libde265|llfio|mongo-c-driver|tcl|nvtt)$")
+    # akali -> typedef private void (T::*POnTimer)(void); -> error
+    # arb -> probably related to flint!
     # mongo-c-driver -> strange redefinition error. Couldn't find why it claims that the defs are different. 
     # llfio -> code has correctness issues which cl accepts. (wrong thread_local and friend declaration)
     # gtk -> .res files in archives via /WHOLEARCHIVE not correctly handled by lld-link
