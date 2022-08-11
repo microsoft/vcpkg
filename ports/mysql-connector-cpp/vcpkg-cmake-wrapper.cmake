406c7c4222e9d8b3f410d85ab792185c35c599b1
xl000 <l_xb@foxmail.com>
2022-08-11 12:58:30 +0800

    Remove vcpkg-cmake-wrapper

--- ports/mysql-connector-cpp/vcpkg-cmake-wrapper.cmake
+++
@@ -1,9 +0,0 @@
-find_package(unofficial-mysql-connector-cpp CONFIG REQUIRED)
-
-if(TARGET mysqlcppconn AND NOT TARGET mysqlcppconn-static)
-    add_library(mysqlcppconn-static INTERFACE IMPORTED)
-    set_target_properties(mysqlcppconn-static PROPERTIES INTERFACE_LINK_LIBRARIES mysqlcppconn)
-elseif(TARGET mysqlcppconn-static AND NOT TARGET mysqlcppconn)
-    add_library(mysqlcppconn INTERFACE IMPORTED)
-    set_target_properties(mysqlcppconn PROPERTIES INTERFACE_LINK_LIBRARIES mysqlcppconn-static)
-endif()
