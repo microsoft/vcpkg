d8c1af9eeb76281f1c6b75b590e3b9b17c387155
diegovelaz <t-diegove@microsoft.com>
2022-08-11 10:35:47 -0700

    Remove support clause for static and add dynamic linkage check.

--- ports/lttng-ust/portfile.cmake
+++ ports/lttng-ust/portfile.cmake
@@ -7,8 +7,10 @@ vcpkg_from_github(
 )
 
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+#set(VCPKG_CRT_LINKAGE dynamic)
+#set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
