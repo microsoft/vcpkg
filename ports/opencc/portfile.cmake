2935d1bcf8e89e8c9a586a0a3148ea22c9755464
Mengna-Li <543250287@qq.com>
2022-08-12 06:54:19 +0000

    convert feature to dependence

--- ports/opencc/portfile.cmake
+++ ports/opencc/portfile.cmake
@@ -12,16 +12,12 @@ vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        rapidjson       USE_SYSTEM_RAPIDJSON
-)
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS ${FEATURE_OPTIONS}
         -DBUILD_DOCUMENTATION=OFF
         -DENABLE_GTEST=OFF
+        -DUSE_SYSTEM_RAPIDJSON=ON
 )
 
 vcpkg_cmake_install(
