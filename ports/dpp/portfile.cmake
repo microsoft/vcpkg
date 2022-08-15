fdba4408a7881481214b21c11855c48722af646d
Craig Edwards <braindigitalis@users.noreply.github.com>
2022-08-15 14:26:31 +0100

    fix breakage of wrapping

--- ports/dpp/portfile.cmake
+++ ports/dpp/portfile.cmake
@@ -1,10 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO brainboxdotcc/DPP
-    REF  
-7a9934df1cdfb9679464b0133e8f5421389283b0
-    SHA512  
-0a03741fec7f8af91ef5375145a1a3b4acb20b40bab6c0c351d270f1d06a9a87e751f2c006c11de57bc2e5158f5ec121f5c1cb6831e3b0c23f440a9edb36fe7b
+    REF 7a9934df1cdfb9679464b0133e8f5421389283b0
+    SHA512 0a03741fec7f8af91ef5375145a1a3b4acb20b40bab6c0c351d270f1d06a9a87e751f2c006c11de57bc2e5158f5ec121f5c1cb6831e3b0c23f440a9edb36fe7b
     HEAD_REF master
     PATCHES
         make-pkgconfig-required.patch
