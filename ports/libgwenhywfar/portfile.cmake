2f48f295fe3ebd8b37fad6609b2a76020d609ee5
Dawid Wróbel <me@dawidwrobel.com>
2022-08-16 17:16:10 +0200

    [libgwenhywfar] update to 5.10.1

--- ports/libgwenhywfar/portfile.cmake
+++ ports/libgwenhywfar/portfile.cmake
@@ -1,21 +1,23 @@
 set(VERSION_MAJOR 5)
-set(VERSION_MINOR 7)
-set(VERSION_PATCH 3)
+set(VERSION_MINOR 10)
+set(VERSION_PATCH 1)
 set(VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH})
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.aquamaniac.de/rdm/attachments/download/390/gwenhywfar-${VERSION}.tar.gz"
+    URLS "https://www.aquamaniac.de/rdm/attachments/download/465/gwenhywfar-${VERSION}.tar.gz"
     FILENAME "gwenhywfar-${VERSION}.tar.gz"
-    SHA512 c005c0af36786a000cf317647440304f6779b531256cdbd5d67180b7b0b2056c230391a3845057d6061875c505f88f9f23fe086dc7e8a04d35fed33c61a0fb47
+    SHA512 11781bec2dd1c4156b609574283179278b8070d604a792aeddf92c8f9b873b3ac09273a8558b9adba567af8d016ea10914d2a149f4b6813798b5800e34e29aa5
 )
 
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    ARCHIVE "${ARCHIVE}"
     REF ${VERSION}
     PATCHES
         0001-Add-support-for-static-builds.patch
         0001-Use-pkg-config-to-find-libgcrypt-gpg-error.patch
+        0001-Fix-variadic-marco-usage.patch                     # https://www.aquamaniac.de/rdm/issues/267
+        disable_docs.patch
 )
 
 if ("libxml2" IN_LIST FEATURES)
@@ -36,6 +38,9 @@ else()
     set(LDFLAGS "")
 endif()
 
+# AM_GNU_GETTEXT is required
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/gettext/aclocal/\"")
+
 vcpkg_configure_make(
     AUTOCONFIG
     SOURCE_PATH ${SOURCE_PATH}
@@ -43,11 +48,11 @@ vcpkg_configure_make(
         --disable-silent-rules
         --disable-binreloc
         --with-guis=${GUIS}
-        --with-libgpg-error-prefix=${CURRENT_INSTALLED_DIR}/tools/libgpg-error
-        --with-libgcrypt-prefix=${CURRENT_INSTALLED_DIR}/tools/libgcrypt
-        --with-qt5-qmake=${CURRENT_INSTALLED_DIR}/tools/qt5/bin/qmake
-        --with-qt5-moc=${CURRENT_INSTALLED_DIR}/tools/qt5/bin/moc
-        --with-qt5-uic=${CURRENT_INSTALLED_DIR}/tools/qt5/bin/uic
+        --with-libgpg-error-prefix="${CURRENT_INSTALLED_DIR}/tools/libgpg-error"
+        --with-libgcrypt-prefix="${CURRENT_INSTALLED_DIR}/tools/libgcrypt"
+        --with-qt5-qmake="${CURRENT_INSTALLED_DIR}/tools/qt5/bin/qmake"
+        --with-qt5-moc="${CURRENT_INSTALLED_DIR}/tools/qt5/bin/moc"
+        --with-qt5-uic="${CURRENT_INSTALLED_DIR}/tools/qt5/bin/uic"
         ${WITH_LIBXML2_CODE}
         "LDFLAGS=${LDFLAGS}"
 )
@@ -57,12 +62,12 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 foreach(GUI IN LISTS FEATURES_GUI)
-    vcpkg_cmake_config_fixup(PACKAGE_NAME  gwengui-cpp CONFIG_PATH lib/cmake/gwengui-${GUI}-${VERSION_MAJOR}.${VERSION_MINOR} DO_NOT_DELETE_PARENT_CONFIG_PATH)
+    vcpkg_cmake_config_fixup(PACKAGE_NAME gwengui-cpp CONFIG_PATH "lib/cmake/gwengui-${GUI}-${VERSION_MAJOR}.${VERSION_MINOR}" DO_NOT_DELETE_PARENT_CONFIG_PATH)
 endforeach()
-vcpkg_cmake_config_fixup(PACKAGE_NAME gwenhywfar CONFIG_PATH lib/cmake/gwenhywfar-${VERSION_MAJOR}.${VERSION_MINOR})
+vcpkg_cmake_config_fixup(PACKAGE_NAME gwenhywfar CONFIG_PATH "lib/cmake/gwenhywfar-${VERSION_MAJOR}.${VERSION_MINOR}")
 
 if ("tools" IN_LIST FEATURES)
-    vcpkg_copy_tools(SEARCH_DIR ${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin TOOL_NAMES gct-tool gsa mklistdoc typemaker typemaker2 xmlmerge AUTO_CLEAN)
+    vcpkg_copy_tools(SEARCH_DIR "${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin" TOOL_NAMES gct-tool gsa mklistdoc typemaker typemaker2 xmlmerge AUTO_CLEAN)
 endif()
 
 # the `dir` variable is not used in the script
