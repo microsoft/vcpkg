41ab333a900e51bec32c5d8278f2eb400733e150
Dawid Wróbel <me@dawidwrobel.com>
2022-08-16 22:12:12 +0200

    [opensp] license updates

--- ports/libopensp/portfile.cmake
+++ ports/libopensp/portfile.cmake
@@ -54,4 +54,5 @@ endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
