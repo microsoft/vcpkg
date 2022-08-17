b658429337f5bd7aa22f8ebf43a00611f899ed91
Billy O'Neal <bion@microsoft.com>
2022-08-17 16:04:16 -0700

    [pagmo2] Add license. See https://github.com/esa/pagmo2/blob/19d774fbb6128124305225803f8c1ad9e2f95c8c/src/bfe.cpp#L5-L27

--- ports/pagmo2/portfile.cmake
+++ ports/pagmo2/portfile.cmake
@@ -34,4 +34,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/pagmo")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/COPYING.lgpl3" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.lgpl3" "${SOURCE_PATH}/COPYING.gpl3")
