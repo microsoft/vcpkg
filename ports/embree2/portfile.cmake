a72770ab1f08cc56928946a5889bfd63d2a4afbb
Billy O'Neal <bion@microsoft.com>
2022-08-18 17:26:34 -0700

    [embree2] Remove completely.

--- ports/embree2/portfile.cmake
+++
@@ -1,8 +0,0 @@
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-
-message(FATAL_ERROR [=[
-The embree2 port is no longer updated and does not build with current tbb versions.
-Use embree3 instead.
-If you must use this port in your project, pin an older version of this port via a manifest file.
-See https://vcpkg.io/en/docs/examples/versioning.getting-started.html for instructions.
-]=])
