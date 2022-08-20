30a5bb33ad0e925f204d017425b007182669c2e9
Hamish Moffatt <hamish@moffatt.id.au>
2022-08-20 20:49:48 +1000

    always build with hardware optimizations if supported by compiler

--- ports/argon2/portfile.cmake
+++ ports/argon2/portfile.cmake
@@ -11,12 +11,6 @@ vcpkg_from_github(
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION  "${SOURCE_PATH}")
 
-vcpkg_check_features(
-    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        hwopt     OPTIMIZATIONS
-)
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
