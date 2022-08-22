2e2f2ff015f699bf79bd28513ee794b56d294128
Kai Pastor <dg0yt@darc.de>
2022-08-22 18:46:48 +0200

    Use native autotools build

--- ports/coinutils/portfile.cmake
+++ ports/coinutils/portfile.cmake
@@ -7,18 +7,26 @@ vcpkg_from_github(
     SHA512 1c2e7f796524d67d87253bc7938c1a6db3c8266acec6b6399aeb83c0fb253b77507e6b5e84f16b0b8e40098aef94676499f396d1c7f653b1e04cbadca7620185
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/Config.cmake.in DESTINATION ${SOURCE_PATH})
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}/CoinUtils"
+    NO_ADDITIONAL_PATHS
+    OPTIONS
+        --without-blas
+        --without-lapack
+        --without-glpk
+        --without-sample
+        --without-netlib
+        ac_cv_prog_coin_have_doxygen=no
+)
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+# vcpkg backward compatibility
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/CoinUtilsConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/coinutils" RENAME CoinUtilsConfig.cmake)
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-vcpkg_install_cmake()
-vcpkg_copy_pdbs()
-vcpkg_fixup_cmake_targets()
-
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/coinutils RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/coinutils" RENAME copyright)
