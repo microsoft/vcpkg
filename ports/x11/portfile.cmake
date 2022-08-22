dae2a8332fea439b96df8ee2e7833dff183adb76
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-08-22 23:36:45 +0200

    remove renamed ports

--- ports/x11/portfile.cmake
+++
@@ -1,3 +0,0 @@
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake"
-    "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
