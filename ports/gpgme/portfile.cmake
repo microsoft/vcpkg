03c3f3782d6505f3616fb3d182741d0a1972e0de
Dawid Wróbel <me@dawidwrobel.com>
2022-08-23 23:10:13 +0200

    WIP

--- ports/gpgme/portfile.cmake
+++ ports/gpgme/portfile.cmake
@@ -14,21 +14,38 @@ vcpkg_extract_source_archive(
         0001-build-Update-ax_cxx_compile_stdcxx-macro.patch
         0001-cpp-Fix-building-with-C-11.patch
         0001-qt-Fix-building-with-C-11.patch
-        0001-gpgme-tool-use-alloca-instead-of-VLAs.patch
-        0001-w32-guard-unistd.h-include-on-Windows.patch    # https://dev.gnupg.org/D561
-        disable-tests.patch                                 # https://dev.gnupg.org/D560
+        0001-Guard-unistd.h-includes.patch                      # https://dev.gnupg.org/D561
+        disable-tests.patch                                     # https://dev.gnupg.org/D560
         disable-docs.patch
         windows_unistd_io.patch
         versioninfo.patch
+        0001-Use-OS-agnostic-string-comparison-functions.patch
+        0001-Guard-sys-time.h-include-with-HAVE_SYS_TIME_H.patch
+        0001-Guiard-sys-types.h-include-with-HAVE_SYS_TYPES_H.patch
+        0001-Guard-strings.h.patch
+        0001-lang-cpp-Use-gpgme_ssize_t-gpgme_off_t-consistently.patch
+        0001-lang-cpp-add-missing-string-includes.patch
 )
 
 if (VCPKG_TARGET_IS_WINDOWS)
     if (NOT VCPKG_TARGET_IS_MINGW)
         set(EXTRA_LIBS "LIBS=\$LIBS -lgetopt")
 
-        set(ENV{CC} "clang-cl.exe")
-        set(ENV{CXX} "clang-cl.exe")
+        # set(ENV{CC} "compile clang-cl.exe")
+        # set(ENV{CXX} "compile clang-cl.exe")
+        set(windows_defs "-D__STDC__=1 -DWIN32 -D_WIN64")
+        string(APPEND windows_defs " -D_WIN32_WINNT=0x0A00 -DWINVER=0x0A00") # tweak for targeted windows
+        string(APPEND windows_defs " -D_CRT_SECURE_NO_DEPRECATE -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE")
+        string(APPEND windows_defs " -D_ATL_SECURE_NO_DEPRECATE -D_SCL_SECURE_NO_WARNINGS")
+        string(APPEND windows_defs " -D_CRT_INTERNAL_NONSTDC_NAMES -D_CRT_DECLARE_NONSTDC_NAMES") # due to -D__STDC__=1 required for e.g. _fopen -> fopen and other not underscored functions/defines
+        string(APPEND windows_defs " -D_FORCENAMELESSUNION") # Due to -D__STDC__ to access tagVARIANT members (ffmpeg)
+        string(APPEND windows_defs " -Wno-narrowing") # https://dev.gnupg.org/T6150
+        
+        set(EXTRA_CFLAGS "CFLAGS=\$CFLAGS ${windows_defs} -std:c11")
+        set(EXTRA_CPPFLAGS "CPPFLAGS=\$CPPFLAGS ${windows_defs}")
         set(ENV{LD} "lld-link.exe")
+        set(ENV{DLLTOOL} "llvm-dlltool.exe")
+        set(ENC{AR} "llvm-lib.exe")
     endif()
 endif()
 
@@ -50,6 +67,8 @@ vcpkg_configure_make(
         --with-libgpg-error-prefix=${CURRENT_INSTALLED_DIR}/tools/libgpg-error
         --with-libassuan-prefix=${CURRENT_INSTALLED_DIR}/tools/libassuan
         ${EXTRA_LIBS}
+        ${EXTRA_CFLAGS}
+        ${EXTRA_CPPFLAGS}
 )
 
 vcpkg_install_make()
