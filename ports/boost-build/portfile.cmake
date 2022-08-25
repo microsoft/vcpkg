8424da584e59e05956913bf96f87654aa3096c7e
Yury Bura <yurybura@gmail.com>
2022-08-25 20:46:01 +0300

    [boost] update to 1.80.0 (#26233)
    * [scripts/boost] modify script to use v1.80.0.beta1
    
    * [boost] generate ports
    
    * add versions
    
    * [boost-build] remove obsolete patches
    
    * update version
    
    * [boost-json] remove obsolete patch
    
    * [scripts/boost] modify script to use v1.80.0
    
    * [boost] generate ports v1.80.0
    
    * [boost] generate ports v1.80.0
    
    * [boost-locale] update patch
    
    * update versions
    
    * [scripts/boost] fix script and regenerate Boost ports
    
    * update versions
    
    * update version
    
    Co-authored-by: Yury Bura <yury.bura@sk.com>

--- ports/boost-build/portfile.cmake
+++ ports/boost-build/portfile.cmake
@@ -4,18 +4,16 @@ if(CMAKE_HOST_WIN32 AND VCPKG_CMAKE_SYSTEM_NAME AND NOT VCPKG_CMAKE_SYSTEM_NAME 
     return()
 endif()
 
-set(BOOST_VERSION 1.79.0)
+set(BOOST_VERSION 1.80.0)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO boostorg/build
     REF boost-${BOOST_VERSION}
-    SHA512 638f2883b9c49265d5566eadc8c93d16cb351edeb941110bad8a17871ea7d53df8eb13c80aaefb94f354b71a22454958af1ecb9a441686dbaa9631e33682a9f2
+    SHA512 9076732697f9d8f682b5857f6552b537e7745fedfcee3b82083d497a1f58a795ae73c652ab2a04340b9264155738f4eccfe33095bc996169a4e5117e641644fb
     HEAD_REF master
     PATCHES
-        0001-don-t-skip-install-targets.patch
         0002-fix-get-version.patch
-        0003-update-architecture-feature.jam-file.patch
 )
 
 vcpkg_download_distfile(ARCHIVE
