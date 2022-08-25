96aff3000974cbf811e375ee11b95a3bce8fa78b
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-08-25 09:38:58 +0000

    Fix upstream repo name

--- ports/cocoyaxi/portfile.cmake
+++ ports/cocoyaxi/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO idealvin/cocoyaxi
-    REF 3fd22601de4d7a06548ca4d24ac36b4f82cde8c5 #v2.0.3
-    SHA512 2b261458e81cc7488bc4d12335b623a750071082b949ffa638e09ca2263ebfab7f8dd83dcb7c6b0085046fda26b5a52992f8907afb39f8f9e74601479c2fb9a7
+    REPO idealvin/coost
+    REF v2.0.3
+    SHA512 b6d38181a8da7dd06cc6ee9c0310ebbc87db5fc0e82e1deb9afba7813d0741fed194887770c55a9a1c61ad677b365cac5ba4f0cf3f32ee376d86c6822d9e30c4
     HEAD_REF master
 )
 
@@ -10,16 +10,18 @@ string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        libcurl    WITH_LIBCURL
-        openssl    WITH_OPENSSL
+        libcurl WITH_LIBCURL
+        openssl WITH_OPENSSL
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         ${FEATURE_OPTIONS}
         -DSTATIC_VS_CRT=${STATIC_CRT}
-    DISABLE_PARALLEL_CONFIGURE
+    MAYBE_UNUSED_VARIABLES
+        STATIC_VS_CRT
 )
 
 vcpkg_cmake_install()
