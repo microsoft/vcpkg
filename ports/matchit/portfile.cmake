25e7d6124b62f45014fd6ddb5a232e703a71de9f
Daljit Singh <daljit97@live.com>
2022-08-25 13:10:00 +0100

    Remove installation of redudant files

--- ports/matchit/portfile.cmake
+++ ports/matchit/portfile.cmake
@@ -14,4 +14,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/matchit")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
