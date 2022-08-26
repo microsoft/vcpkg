ac5d924ffbf94eddf26f8df622fdbf50e3909d85
Kai Pastor <dg0yt@darc.de>
2022-08-26 06:10:10 +0200

    Patch autotools

--- ports/coinutils/portfile.cmake
+++ ports/coinutils/portfile.cmake
@@ -7,6 +7,8 @@ vcpkg_from_github(
     REPO coin-or/CoinUtils
     REF f709081c9b57cc2dd32579d804b30689ca789982 # releases/2.11.4
     SHA512 1c2e7f796524d67d87253bc7938c1a6db3c8266acec6b6399aeb83c0fb253b77507e6b5e84f16b0b8e40098aef94676499f396d1c7f653b1e04cbadca7620185
+    PATCHES
+        autotools.patch
 )
 
 vcpkg_configure_make(
