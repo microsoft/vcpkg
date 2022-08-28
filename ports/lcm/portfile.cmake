a24bd0759673352f96b8f3f1768f0eab6c7c785a
Thomas Heinrichs <46387399+Thomas1664@users.noreply.github.com>
2022-08-28 15:56:14 +0200

    Copy tools

--- ports/lcm/portfile.cmake
+++ ports/lcm/portfile.cmake
@@ -32,27 +32,16 @@ else()
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/lcm" "${CURRENT_PACKAGES_DIR}/lib/lcm")
 endif()
 
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/man")
 
-#file(GLOB EXES ${CURRENT_PACKAGES_DIR}/bin/*.exe)
-#if(EXES)
-#    file(COPY ${EXES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/lcm)
-#    file(REMOVE ${EXES})
-#endif()
-#file(GLOB DEBUG_EXES ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-#if(DEBUG_EXES)
-#    file(REMOVE ${DEBUG_EXES})
-#endif()
-#
-#if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-#    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-#endif()
+vcpkg_copy_tools(lcm-gen lcm-logger lcm-logplayer AUTO_CLEAN)
 
-#vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/lcm)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-vcpkg_copy_pdbs()
