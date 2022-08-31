1623a385d38a12b2b9633e2f376a96be580349a1
Clinton Ingram <clinton.ingram@outlook.com>
2022-08-31 13:57:26 -0700

    [zlib-ng] fix HEAD_REF (#26591)
    * fix head_ref
    
    * update versions

--- ports/zlib-ng/portfile.cmake
+++ ports/zlib-ng/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     REPO zlib-ng/zlib-ng
     REF "${ZLIB_FULL_VERSION}"
     SHA512 4888f17160d0a87a9b349704047ae0d0dc57237a10e11adae09ace957afa9743cce5191db67cb082991421fc961ce68011199621034d2369c0e7724fad58b4c5
-    HEAD_REF master
+    HEAD_REF develop
 )
 
 vcpkg_cmake_configure(
