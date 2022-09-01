186dc2b83faf761fdf868db2441b011ad0c44470
Cristian Boarna <cristian.boarna@gmail.com>
2022-09-01 10:17:05 +0300

    feat(libdatachannel): update to 0.17.10

--- ports/libdatachannel/portfile.cmake
+++ ports/libdatachannel/portfile.cmake
@@ -7,7 +7,7 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO paullouisageneau/libdatachannel
-    REF f8ac6955e3cd673ac23a2172cd7dc42fc013b7b3 #v0.17.9
+    REF 04cf4738961f55ba3f0aa39b4a61342f66bb3781 #v0.17.10
     SHA512 3a8e3a47032a1fad770639c1bac266388c87046432cd44b391f146820cf3d3a871834e104deb9fb6fa901d6f82eba8aea0baa0b0eeb7cfb24808c408b020407b
     HEAD_REF master
     PATCHES
