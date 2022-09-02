74a66f663d95f5ee2dc4634049c830c0017db003
Li Xiaobin <l_xb@foxmail.com>
2022-09-03 01:51:42 +0800

    [mysql-connector-cpp] update to 8.0.30 (#26536)
    * [mysql-connector-cpp] Upgrade to 8.0.30
    
    * version DB
    
    * [mysql-connector-cpp] remove arm supports
    
    * [mysql-connector-cpp] version DB

--- ports/mysql-connector-cpp/portfile.cmake
+++ ports/mysql-connector-cpp/portfile.cmake
@@ -1,54 +1,42 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mysql/mysql-connector-cpp
-    REF 1.1.13
-    SHA512 76cf3d8c9dbaa006ccbb3c672538c540167c7a9cf3406a371313dc346598b119515f25fea2a786bb8cab12d96fd607b69f320a6d897aaeeb668eff831920fb5f
+    REF 8.0.30
+    SHA512 fd6d8a03ba66ca028b3748218c60a721c9e2c79867c6cf2ea95d2649d3a252e9bd307986b149897dcc1d24a11785666c5480225a20c4baba7c87b376b7289a13
     HEAD_REF master
     PATCHES
-        fix-static-build.patch
+        fix-static-build8.patch
+        export-targets.patch
 )
 
-file(COPY
-    "${CMAKE_CURRENT_LIST_DIR}/mysql-connector-cpp-config.cmake.in"
-    "${CMAKE_CURRENT_LIST_DIR}/FindMySQL.cmake"
-    DESTINATION "${SOURCE_PATH}"
-)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/mysql-connector-cpp-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ENABLE_BUILD_STATIC)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ENABLE_BUILD_DYNAMIC)
-string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIBS)
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_MSVCRT)
 
-# Use mysql-connector-cpp's own build process, skipping examples and tests.
+# Use mysql-connector-cpp's own build process.
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     WINDOWS_USE_MSBUILD
     OPTIONS
-        -DMYSQLCPPCONN_BUILD_EXAMPLES:BOOL=OFF
-        -DMYSQLCPPCONN_BUILD_TESTS:BOOL=OFF
-        -DENABLE_BUILD_DYNAMIC=${ENABLE_BUILD_DYNAMIC}
-        -DENABLE_BUILD_STATIC=${ENABLE_BUILD_STATIC}
-        -DMYSQLCLIENT_STATIC_LINKING=${ENABLE_BUILD_STATIC}
-        -DSTATIC_CRT=${STATIC_CRT}
-    MAYBE_UNUSED_VARIABLES
-        STATIC_CRT
-        MYSQLCLIENT_STATIC_LINKING
+        "-DWITH_SSL=${CURRENT_INSTALLED_DIR}"
+        -DBUILD_STATIC=${BUILD_STATIC}
+        -DSTATIC_MSVCRT=${STATIC_MSVCRT}
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-mysql-connector-cpp)
-file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-file(REMOVE "${CURRENT_PACKAGES_DIR}/BUILDINFO"
-    "${CURRENT_PACKAGES_DIR}/LICENSE"
-    "${CURRENT_PACKAGES_DIR}/README"
-    "${CURRENT_PACKAGES_DIR}/debug/BUILDINFO"
-    "${CURRENT_PACKAGES_DIR}/debug/LICENSE"
-    "${CURRENT_PACKAGES_DIR}/debug/README"
+file(REMOVE
+    "${CURRENT_PACKAGES_DIR}/INFO_BIN"
+    "${CURRENT_PACKAGES_DIR}/INFO_SRC"
+    "${CURRENT_PACKAGES_DIR}/debug/INFO_BIN"
+    "${CURRENT_PACKAGES_DIR}/debug/INFO_SRC"
 )
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_copy_pdbs()
-
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
