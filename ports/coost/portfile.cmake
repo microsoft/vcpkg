c3723bbc964870a457a9fd8a81f97380f50da14b
idealvin <idealvin@qq.com>
2022-09-02 21:00:42 +0800

    add port coost

---
+++ ports/coost/portfile.cmake
@@ -0,0 +1,34 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO idealvin/coost
+    REF v3.0.0
+    SHA512 73720a430cb0f5480fcbb5e92d536f07e3a60fa30ca2d60be5ba11f1d5b2bee35da320f8200c18000ff0b1de50e18be0850fab945b48525319bcd198b388fcf6
+    HEAD_REF master
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        libcurl WITH_LIBCURL
+        openssl WITH_OPENSSL
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DSTATIC_VS_CRT=${STATIC_CRT}
+    MAYBE_UNUSED_VARIABLES
+        STATIC_VS_CRT
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
