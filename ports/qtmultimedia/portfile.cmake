6be6663b7afe850b26ebc1f4103b364f58b2274a
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-09-06 23:12:10 +0200

    adjust qtmultimedia

--- ports/qtmultimedia/portfile.cmake
+++ ports/qtmultimedia/portfile.cmake
@@ -1,7 +1,7 @@
 set(SCRIPT_PATH "${CURRENT_INSTALLED_DIR}/share/qtbase")
 include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
-set(${PORT}_PATCHES fix_windows_header_include.patch
+set(${PORT}_PATCHES
                     remove_unistd.patch
 )
 
