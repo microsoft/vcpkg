176edfbb330eff77de1752075d8e68ef55114152
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-09-08 13:53:23 +0200

    fix osx/ios logic

--- scripts/cmake/vcpkg_configure_make.cmake
+++ scripts/cmake/vcpkg_configure_make.cmake
@@ -774,7 +774,7 @@ function(vcpkg_configure_make)
             set(ENV{LDFLAGS_FOR_BUILD} "-avoid-version $ENV{LDFLAGS_FOR_BUILD}")
         endif()
 
-        if(VCPKG_TARGET_IS_OSX AND VCPKG_TARGET_IS_IOS)
+        if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
             # configure not using all flags to check if compiler works ...
             set(ENV{CC} "$ENV{CC} $ENV{CPPFLAGS} $ENV{CFLAGS}")
             set(ENV{CC_FOR_BUILD} "$ENV{CC_FOR_BUILD} $ENV{CPPFLAGS} $ENV{CFLAGS}")
