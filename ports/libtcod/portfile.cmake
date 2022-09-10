a202024a3fd0906497f48fb1f584261f113e6ae0
Kyle Benesch <4b796c65+github@gmail.com>
2022-09-09 21:45:59 -0700

    Update to 1.22.3
    Add unicode feature.  Enabled by default.

--- ports/libtcod/portfile.cmake
+++ ports/libtcod/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libtcod/libtcod
-    REF 1.22.2
-    SHA512 c02b6bb205f610416ea6cd9bdeb89a6976141e03a2cd2c44838f0d35edf2544e61af86d2ba904c6a913bbc3754592eed6063b732277bede55b79ffe3b983ee4d
+    REF 1.22.3
+    SHA512 350b741f8738f5ba3c6f282e76c1124326144111eb2444b17b1126e8ddead52cff1dba1d0ed80e6fefbb0db8f032b95d08292d73746eec149e191a8fa78651d7
     HEAD_REF main
 )
 
@@ -12,6 +12,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         "sdl" CMAKE_DISABLE_FIND_PACKAGE_SDL2
         "sdl" CMAKE_DISABLE_FIND_PACKAGE_GLAD
         "threads" CMAKE_DISABLE_FIND_PACKAGE_Threads
+        "unicode" CMAKE_DISABLE_FIND_PACKAGE_utf8proc
+        "unicode" CMAKE_DISABLE_FIND_PACKAGE_unofficial-utf8proc
         "zlib" CMAKE_DISABLE_FIND_PACKAGE_ZLIB
 )
 
