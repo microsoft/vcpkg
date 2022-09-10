4186327790f1ff82382e220cc070a2758067ed45
Matthias Kuhn <matthias@opengis.ch>
2022-09-10 09:30:04 +0200

    Move VCPKG_MAKE_BUILD_TRIPLET to android triplet file

--- scripts/cmake/vcpkg_configure_make.cmake
+++ scripts/cmake/vcpkg_configure_make.cmake
@@ -470,14 +470,6 @@ function(vcpkg_configure_make)
         endif()
     endif()
 
-    # Android - cross-compiling support
-    if(VCPKG_TARGET_IS_ANDROID)
-        if (requires_autoconfig AND NOT arg_BUILD_TRIPLET OR arg_DETERMINE_BUILD_TRIPLET)
-            set(arg_BUILD_TRIPLET "--host=${VCPKG_MAKE_BUILD_TRIPLET}")
-            debug_message("Using make triplet: ${arg_BUILD_TRIPLET}")
-        endif()
-    endif()
-    
     # Cleanup previous build dirs
     file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel"
                         "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg"
