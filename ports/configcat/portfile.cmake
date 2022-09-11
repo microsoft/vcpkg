f57db7869288bbd3c2241d5e9c4ee8d57b9720d4
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-09-11 17:41:26 +0200

    [configcat] Add new port

--- ports/configcat/portfile.cmake
+++ ports/configcat/portfile.cmake
@@ -17,6 +17,8 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/cmake/configcat")
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
