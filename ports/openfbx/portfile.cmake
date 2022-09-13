ed6a8909e9e5987b461ca2b58c5b25fd2d2a7032
Francisco Facioni <franciscof@bolt6.ai>
2022-09-13 12:04:46 +0200

    [openfbx] more renaming

--- ports/openfbx/portfile.cmake
+++ ports/openfbx/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/openfbxConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/unofficial-openfbxConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
