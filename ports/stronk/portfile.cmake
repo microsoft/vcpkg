aa3c408307b6804bd2e888997ef1b587acf09cee
Jonliu1993 <13720414433@163.com>
2022-09-14 14:20:13 +0800

    fix include wrong directroy

--- ports/stronk/portfile.cmake
+++ ports/stronk/portfile.cmake
@@ -1,21 +1,20 @@
 vcpkg_from_git(
-  OUT_SOURCE_PATH SOURCE_PATH
-  URL https://github.com/twig-energy/stronk
-  REF f298bca3102b48ada516b0b42f2d5e6899cbbc44
-  HEAD_REF main
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/twig-energy/stronk
+    REF f298bca3102b48ada516b0b42f2d5e6899cbbc44
+    HEAD_REF main
+    PATCHES
+        fix-wrong-include.patch
 )
 
 vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH ${SOURCE_PATH}
 )
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE
-  "${CURRENT_PACKAGES_DIR}/debug"
-)
+vcpkg_cmake_config_fixup()
 
-file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
