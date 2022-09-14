f4c4e35105022d94ca8a0424def26bf2d70c3d87
Leonid Pospelov <pospelovlm@yandex.ru>
2022-09-14 18:14:49 +0300

    Reformat with vscode cmake formatter

--- ports/node-api/portfile.cmake
+++ ports/node-api/portfile.cmake
@@ -1,14 +1,15 @@
 find_program(NODEJS NAMES node PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/node" "bin" NO_DEFAULT_PATHS)
+
 if(NOT NODEJS)
-    message(FATAL_ERROR "node not found in '${CURRENT_HOST_INSTALLED_DIR}/tools/node'")
+  message(FATAL_ERROR "node not found in '${CURRENT_HOST_INSTALLED_DIR}/tools/node'")
 endif()
 
 get_filename_component(NODEJS_DIR "${NODEJS}" DIRECTORY)
 
 if(VCPKG_HOST_IS_WINDOWS)
-    set(npm_command "${NODEJS_DIR}/npm.cmd")
+  set(npm_command "${NODEJS_DIR}/npm.cmd")
 else()
-    set(npm_command "${NODEJS_DIR}/npm")
+  set(npm_command "${NODEJS_DIR}/npm")
 endif()
 
 file(REMOVE_RECURSE "${DOWNLOADS}/tmp-cmakejs-output")
@@ -18,13 +19,13 @@ file(MAKE_DIRECTORY "${DOWNLOADS}/tmp-cmakejs-home")
 
 set(npm_args install cmake-js@7.0.0-3)
 execute_process(COMMAND "${npm_command}" ${npm_args}
-    WORKING_DIRECTORY ${NODEJS_DIR}
-    RESULT_VARIABLE npm_result
-    OUTPUT_VARIABLE npm_output
+  WORKING_DIRECTORY ${NODEJS_DIR}
+  RESULT_VARIABLE npm_result
+  OUTPUT_VARIABLE npm_output
 )
 
 if(NOT "${npm_result}" STREQUAL "0")
-    message(FATAL_ERROR "${npm_command} ${npm_args} exited with ${npm_result}:\n${npm_output}")
+  message(FATAL_ERROR "${npm_command} ${npm_args} exited with ${npm_result}:\n${npm_output}")
 endif()
 
 # Precent pollution of user home directory
@@ -33,32 +34,34 @@ string(REPLACE "process.env[(os.platform() === \"win32\") ? \"USERPROFILE\" : \"
 file(WRITE "${NODEJS_DIR}/node_modules/cmake-js/lib/environment.js" "${environment_js}")
 
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(cmake_js_arch "x64")
+  set(cmake_js_arch "x64")
 elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(cmake_js_arch "ia32")
+  set(cmake_js_arch "ia32")
 elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-    set(cmake_js_arch "arm64")
+  set(cmake_js_arch "arm64")
 elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
-    set(cmake_js_arch "arm")
+  set(cmake_js_arch "arm")
 else()
-    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+  message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
 endif()
 
 set(npm_args
-    # npm arguments:
-    run cmake-js-fetch
-    --scripts-prepend-node-path
-    # cmake-js arguments:
-    -- --out "${DOWNLOADS}/tmp-cmakejs-output" --arch "${cmake_js_arch}"
+
+  # npm arguments:
+  run cmake-js-fetch
+  --scripts-prepend-node-path
+
+  # cmake-js arguments:
+  -- --out "${DOWNLOADS}/tmp-cmakejs-output" --arch "${cmake_js_arch}"
 )
 execute_process(COMMAND "${npm_command}" ${npm_args}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-    RESULT_VARIABLE npm_result
-    OUTPUT_VARIABLE npm_output
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+  RESULT_VARIABLE npm_result
+  OUTPUT_VARIABLE npm_output
 )
 
 if(NOT "${npm_result}" STREQUAL "0")
-    message(FATAL_ERROR "${npm_command} ${npm_args} exited with ${npm_result}:\n${npm_output}")
+  message(FATAL_ERROR "${npm_command} ${npm_args} exited with ${npm_result}:\n${npm_output}")
 endif()
 
 include("${DOWNLOADS}/tmp-cmakejs-output/node.cmake")
@@ -69,7 +72,7 @@ file(COPY "${CMAKE_JS_LIB}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
 
 # Non-empty only for Windows at the current version of NodeJS
 if(CMAKE_JS_SRC)
-    file(COPY "${CMAKE_JS_SRC}" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+  file(COPY "${CMAKE_JS_SRC}" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 endif()
 
 # Handle copyright
