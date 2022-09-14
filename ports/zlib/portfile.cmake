343295a25e5c511b2ac50d13c8637d4165392f55
Chris Brammer <brammer@extend3d.de>
2022-09-14 16:13:03 +0200

    [zlib] Fix CVE-2022-37434

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -9,6 +9,8 @@ vcpkg_from_github(
         0001-Prevent-invalid-inclusions-when-HAVE_-is-set-to-0.patch
         debug-postfix-mingw.patch
         0002-android-build-mingw.patch
+        CVE-2022-37434-eff308af42.patch
+        CVE-2022-37434-1eb7682f84.patch
 )
 
 # This is generated during the cmake build
