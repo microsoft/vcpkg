c14a38fa257a459c8ffb58c6070087b30f19b2ee
Kai Pastor <dg0yt@darc.de>
2022-09-15 09:24:42 +0200

    Add feature 'tools'

--- ports/openexr/portfile.cmake
+++ ports/openexr/portfile.cmake
@@ -6,12 +6,21 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS OPTIONS
+    FEATURES
+        tools   OPENEXR_BUILD_TOOLS
+        tools   OPENEXR_INSTALL_TOOLS
+)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        ${OPTIONS}
         -DBUILD_TESTING=OFF
         -DOPENEXR_INSTALL_EXAMPLES=OFF
         -DDOCS=OFF
+    OPTIONS_DEBUG
+        -DOPENEXR_BUILD_TOOLS=OFF
+        -DOPENEXR_INSTALL_TOOLS=OFF
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
@@ -19,10 +28,12 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenEXR)
 vcpkg_fixup_pkgconfig()
 
-vcpkg_copy_tools(
-    TOOL_NAMES exrenvmap exrheader exrinfo exrmakepreview exrmaketiled exrmultipart exrmultiview exrstdattr exr2aces
-    AUTO_CLEAN
-)
+if(OPENEXR_INSTALL_TOOLS)
+    vcpkg_copy_tools(
+        TOOL_NAMES exrenvmap exrheader exrinfo exrmakepreview exrmaketiled exrmultipart exrmultiview exrstdattr exr2aces
+        AUTO_CLEAN
+    )
+endif()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
