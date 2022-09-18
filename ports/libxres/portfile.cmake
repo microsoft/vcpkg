8427f5c990c2cd1b2fc6595425efab3d485d243e
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-09-18 14:00:18 +0200

    fix arm ?

--- ports/libxres/portfile.cmake
+++ ports/libxres/portfile.cmake
@@ -14,9 +14,14 @@ vcpkg_from_gitlab(
 
 set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
 
+if (VCPKG_CROSSCOMPILING)
+    list(APPEND OPTIONS ac_cv_func_malloc_0_nonnull=yes)
+endif()
+
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
+    OPTIONS ${OPTIONS}
 )
 
 vcpkg_install_make()
