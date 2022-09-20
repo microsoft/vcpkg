0683137e9c9faef4ec84200a5bc8349c7892a024
Dawid Wróbel <me@dawidwrobel.com>
2022-09-20 13:39:56 +0200

    [kf5kcmutils] update static compilation patch

--- ports/kf5kcmutils/portfile.cmake
+++ ports/kf5kcmutils/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 959901d7ba447eff13e4c1341c5530fccecf42f7f5e4dc69bee669c9e22770f5af57ed2f08979aac5fd2e1015f2bbadf5d302d99e1e0031c20927d833e6a3cea
     HEAD_REF master
     PATCHES
-        fix_static_compilation.patch   # https://invent.kde.org/frameworks/kcmutils/-/merge_requests/104
+        0001-Fix-missing-kcmutils_proxy_model-export-in-static-bu.patch   # https://invent.kde.org/frameworks/kcmutils/-/merge_requests/104
 )
 
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
