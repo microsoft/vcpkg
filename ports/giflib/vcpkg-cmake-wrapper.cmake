f4aeaf0bc05608a8d712bebfca2ca296e90b6181
Lily Wang <494550702@qq.com>
2022-09-21 00:13:16 -0700

    Modify vcpkg-cmake-wrapper.cmake

--- ports/giflib/vcpkg-cmake-wrapper.cmake
+++ ports/giflib/vcpkg-cmake-wrapper.cmake
@@ -2,11 +2,9 @@ _find_package(unofficial-giflib CONFIG REQUIRED)
 add_library(GIF::GIF ALIAS unofficial::giflib::gif)
 
 get_filename_component(GIF_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}" PATH)
-get_filename_component(GIF_INCLUDE_DIRS "${LUA_INCLUDE_DIR}" PATH)
+get_filename_component(GIF_INCLUDE_DIRS "${GIF_INCLUDE_DIRS}" PATH)
 
 set(GIF_FOUND TRUE)
-set(GIF_INCLUDE_DIRS ${_IMPORT_PREFIX}/include)
+set(GIF_INCLUDE_DIRS ${GIF_INCLUDE_DIRS}/include)
 set(GIF_LIBRARIES unofficial::giflib::gif)
 set(GIF_VERSION @GIFLIB_VERSION@)
-
-unset(_IMPORT_PREFIX)
