ea0afa5cead8dffe10a2f8981c3d2bb14f9a2ccb
Kai Pastor <dg0yt@darc.de>
2022-09-22 08:34:52 +0200

    [libkml] No DLL

--- ports/libkml/portfile.cmake
+++ ports/libkml/portfile.cmake
@@ -1,3 +1,7 @@
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libkml/libkml
