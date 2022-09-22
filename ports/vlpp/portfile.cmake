6ddc132dd68f9f49ac5427746bb49df69cb00a05
JackBoosY <yuzaiyang@beyondsoft.com>
2022-09-22 01:47:55 -0700

    FIx feature option pos

--- ports/vlpp/portfile.cmake
+++ ports/vlpp/portfile.cmake
@@ -24,8 +24,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
-        ${FEATURE_OPTIONS}
         -DSKIP_HEADERS=ON
 )
 
