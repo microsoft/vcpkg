0e411cd595f593f087bdc914713a0f0a2da4b5ee
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-09-23 19:40:25 +0000

    Fix deprecated functions

--- ports/mfl/portfile.cmake
+++ ports/mfl/portfile.cmake
@@ -14,13 +14,12 @@ vcpkg_from_github(
         disable-tests.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/mfl)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mfl)
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
