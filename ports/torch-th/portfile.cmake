2f2d1a7007f0662d924d78833a1f87bbefff90dc
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-09-23 21:00:16 +0000

    torch-th doesn't provide any config

--- ports/torch-th/portfile.cmake
+++ ports/torch-th/portfile.cmake
@@ -17,7 +17,6 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/cmake")
