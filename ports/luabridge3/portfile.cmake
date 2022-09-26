287f7a066709c7502f20b5622a1dca534a425b61
FrankXie <v-frankxie@microsoft.com>
2022-09-25 23:59:29 -0700

    improve portfile.cmake

--- ports/luabridge3/portfile.cmake
+++ ports/luabridge3/portfile.cmake
@@ -4,18 +4,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kunitoki/LuaBridge3
     REF 3b5ccfb2331cecc4b1b9e8b8ccccb4bf5ce0a304 # 3.0
-    SHA512 01656f78532c7d2d39320ec0e64527da8233aad3a8fded45809c14c1186ec24a354efc476f4ec51e3d3d6c47a4e0500b56001b55d263da8e1863e484c6cc11fe
+    SHA512 f09864f0dbb29f1ee5c1602371a7eaf38bb65d5dd7469cdc8a4c9681367bfc52743b71b9b15f3733c1cf556763d7beca62d18abef301ddb87d56cdf25a233882
     HEAD_REF master
 )
 
-file(
-    COPY ${SOURCE_PATH}/Source/LuaBridge
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include/${PORT}
-)
+# Copy the header files
+file(COPY "${SOURCE_PATH}/Source/LuaBridge" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
 
 # Handle copyright
-configure_file(
-    ${SOURCE_PATH}/LICENSE.txt
-    ${CURRENT_PACKAGES_DIR}/share/luabridge/copyright
-    COPYONLY
-)
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
