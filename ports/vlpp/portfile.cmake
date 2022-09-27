dbe60d7fc95feecd7cf62d17807b1cffcb6e0541
JackBoosY <yuzaiyang@beyondsoft.com>
2022-09-27 01:24:26 -0700

    Fix arm build, switch tool build to cmake

--- ports/vlpp/portfile.cmake
+++ ports/vlpp/portfile.cmake
@@ -6,7 +6,27 @@ vcpkg_from_github(
     REF 0a7bf9b4f7e705f17efc2ada5aa2b089147234d4 # 1.1.0.0
     SHA512 b70081495f2843a45ea2aea37a2d00327e336a3313acfa20421de4748c880905279353c03ecc50f45e9cda0aae34aad69ba44de81fa2fd4d4855be6002dd068f
     HEAD_REF master
+    PATCHES fix-arm.patch
 )
+
+if ("tool" IN_LIST FEATURES)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH GACGEN_SOURCE_PATH
+        REPO vczh-libraries/GacUI
+        REF 83501365b241fb77b6c08693a001bf16510dcb8c
+        SHA512 db51341647139fec133ae19c122bd1ee3a4d07ddb822a7c156d24f2960279bfd6dd87b20a9d60fe1c2ce43c5dc8188830aea58c08a7382bc69fc0f3bed86524a
+        HEAD_REF master
+        PATCHES fix-arm.patch
+    )
+    vcpkg_from_github(
+        OUT_SOURCE_PATH CPPMERGE_SOURCE_PATH
+        REPO vczh-libraries/Workflow
+        REF 8bce9694692ba0da93a10d29224943656b02e573
+        SHA512 ee17e2a99c78a5abb62708719989e9ae224bcec7a78cb08882870d0e21f2017d24d02c1c4c75d0ac89549478d49679681b3d11d76f408988306c85b1d3ab28ae
+        HEAD_REF master
+        PATCHES fix-arm.patch
+    )
+endif()
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
@@ -17,27 +37,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         workflowlibrary     WORKFLOW_LIBRARY
         workflowruntime     WORKFLOW_RUNTIME
         workflowcompiler    WORKFLOW_COMPILER
-        gacuicore           GACUI
+        gacuicore           GACUI_CORE
         gacuirecompiler     GACUI_COMPILER
         gacuireflection     GACUI_REFLECTION
 )
-
-if ("tool" IN_LIST FEATURES)
-    vcpkg_from_github(
-        OUT_SOURCE_PATH GACGEN_SOURCE_PATH
-        REPO vczh-libraries/GacUI
-        REF 83501365b241fb77b6c08693a001bf16510dcb8c
-        SHA512 b70081495f2843a45ea2aea37a2d00327e336a3313acfa20421de4748c880905279353c03ecc50f45e9cda0aae34aad69ba44de81fa2fd4d4855be6002dd068f
-        HEAD_REF master
-    )
-    vcpkg_from_github(
-        OUT_SOURCE_PATH CPPMERGE_SOURCE_PATH
-        REPO vczh-libraries/Workflow
-        REF 8bce9694692ba0da93a10d29224943656b02e573
-        SHA512 b70081495f2843a45ea2aea37a2d00327e336a3313acfa20421de4748c880905279353c03ecc50f45e9cda0aae34aad69ba44de81fa2fd4d4855be6002dd068f
-        HEAD_REF master
-    )
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -48,21 +51,23 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-if ("tool" IN_LIST FEATURES)
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${CPPMERGE_SOURCE_PATH}
-        PROJECT_SUBPATH Tools/GacGen/GacGen/GacGen.vcxproj
-    )
-    
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${GACGEN_SOURCE_PATH}
-        PROJECT_SUBPATH Tools/CppMerge/CppMerge/CppMerge.vcxproj
-    )
-endif()
-
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-vlpp)
 
 if ("tool" IN_LIST FEATURES)
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.gacgen.txt" "${GACGEN_SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${GACGEN_SOURCE_PATH}"
+    )
+    vcpkg_cmake_install()
+
+    configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.cppmerge.txt" "${CPPMERGE_SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${CPPMERGE_SOURCE_PATH}"
+    )
+    vcpkg_cmake_install()
+    
+    vcpkg_copy_tools(TOOL_NAMES GacGen CppMerge AUTO_CLEAN)
+
     # Handle scripts
     if (VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_MINGW)
         set(TOOL_SCRIPT_SUFFIX ".ps1")
@@ -73,8 +78,6 @@ if ("tool" IN_LIST FEATURES)
     foreach (TOOL_SCRIPT IN LISTS TOOL_SCRIPTS)
         file(COPY "${TOOL_SCRIPT}" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
     endforeach()
-    # Handle tools
-    vcpkg_copy_tools(TOOL_NAMES GacGen CppMerge AUTO_CLEAN)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
