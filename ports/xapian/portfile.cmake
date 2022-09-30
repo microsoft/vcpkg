79bc6bc1681284a7a6289867477f36ac232467b7
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2022-09-30 16:09:09 +0800

    Update ports/xapian/portfile.cmake
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/xapian/portfile.cmake
+++ ports/xapian/portfile.cmake
@@ -9,9 +9,8 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE ${ARCHIVE}
 )
 if(WIN32)
-vcpkg_replace_string("${SOURCE_PATH}/configure.ac"
-"z zlib zdll" "z zlib zdll zlibd"
-)
+    vcpkg_replace_string("${SOURCE_PATH}/configure.ac" "z zlib zdll" "z zlib zdll zlibd")
+
 endif()
 
 vcpkg_configure_make(
