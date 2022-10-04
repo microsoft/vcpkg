0995d10a978ffea80cc58b7fab1047007960d3da
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-04 09:43:32 +0200

    fix angle

--- ports/egl-registry/vcpkg-cmake-wrapper.cmake
+++ ports/egl-registry/vcpkg-cmake-wrapper.cmake
@@ -1,5 +1,5 @@
 if(UNIX)
-    _find_package(OpenGL COMPONENTS EGL REQUIRED)
+    _find_package(OpenGL COMPONENTS EGL)
     if(OPENGL_egl_LIBRARY) # Only defined for Linux with GLVND
         set(EGL_LIBRARY "${OPENGL_egl_LIBRARY}" CACHE STRING "")
         set(EGL_INCLUDE_DIR "${OPENGL_EGL_INCLUDE_DIRS}" CACHE STRING "")
