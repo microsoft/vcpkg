3bb08aea1044bde0766f0d08916360b1d1172c90
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-08 20:00:07 +0200

    disable trace

--- ports/qtbase/portfile.cmake
+++ ports/qtbase/portfile.cmake
@@ -257,7 +257,7 @@ set(TOOL_NAMES
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
                      CONFIGURE_OPTIONS
-                        --trace-expand
+                        #--trace-expand
                         ${FEATURE_OPTIONS}
                         ${FEATURE_CORE_OPTIONS}
                         ${FEATURE_NET_OPTIONS}
