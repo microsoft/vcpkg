cdae60e2a5ab9ad027cf45f1738ee0b398abfc8f
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-08 16:16:20 +0200

    don't directly call _find_package

--- ports/egl/vcpkg-cmake-wrapper.cmake
+++ ports/egl/vcpkg-cmake-wrapper.cmake
@@ -1,5 +1,5 @@
 if(UNIX)
-    _find_package(OpenGL COMPONENTS EGL)
+    find_package(OpenGL COMPONENTS EGL)
     if(OPENGL_egl_LIBRARY)
         set(EGL_LIBRARY "${OPENGL_egl_LIBRARY}" CACHE STRING "")
         set(EGL_INCLUDE_DIR "${OPENGL_EGL_INCLUDE_DIRS}" CACHE STRING "")
