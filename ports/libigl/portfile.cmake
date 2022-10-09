3c3b13eb760e8a2d60712a765383b8851e5d1f06
FrankXie <v-frankxie@microsoft.com>
2022-10-09 02:04:47 -0700

    use hunter eigen

--- ports/libigl/portfile.cmake
+++ ports/libigl/portfile.cmake
@@ -1,3 +1,5 @@
+# Header-only library
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libigl/libigl
@@ -5,7 +7,7 @@ vcpkg_from_github(
     SHA512 7014ffdaa160bfa2509fc283cb7176d7994a37f51509c7374659292efad076c8fb594f9f6990bab1aa5562d1f66e93403ea35a5bf2a924436560a2d4669ffcfd
     HEAD_REF master
     PATCHES
-       do-not-install-eigen.patch
+      use-hunter-eigen.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -37,5 +39,5 @@ vcpkg_copy_pdbs()
 
 # libigl is a header-only library.
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 file(INSTALL "${SOURCE_PATH}/LICENSE.GPL" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
