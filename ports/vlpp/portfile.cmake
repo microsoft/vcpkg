326d3db8ae2a7bf5ade7b79f4cc1395eaf2be05d
JackBoosY <yuzaiyang@beyondsoft.com>
2022-10-10 03:00:58 -0700

    Update to latest

--- ports/vlpp/portfile.cmake
+++ ports/vlpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vczh-libraries/Release
-    REF 2526183a61d2791c744a22dc8967b474fa803c52 # 1.1.0.0
-    SHA512 4bab19da5274f4ade656176f10f38dc4896a51ddb6828a6a419f084d32cac8352ef8814d928a8a252a4a963e09fdf451a558acfce23118a32ad6fb8580677c85
+    REF 471674f8050f8168d7de7d73cb2e84101bdf7b29 # 1.1.0.0
+    SHA512 2b24e813626c390c88107db7d9253583f1eac4d091b46876973ac82b415be43d846c9c0965b4de8ea61b97da07a102055c36ed855878f446d0371555d6172b53
     HEAD_REF master
 )
 
@@ -10,20 +10,18 @@ if ("tool" IN_LIST FEATURES)
     vcpkg_from_github(
         OUT_SOURCE_PATH GACGEN_SOURCE_PATH
         REPO vczh-libraries/GacUI
-        REF 24e525948b54a08ec2cd5075033e7b167084de3a
-        SHA512 f8055c4f738de8a9f8c76d73e6ffd576c03e3f9c5c7a0c23cf0adb3128bb3f2cb7fb174bdbadee460d4e2833b9f7c29706101691dcb61e705d59e19fc5961e2f
+        REF e2faa7e3d2935dfc5ec5b99ca22c2a266f24a314
+        SHA512 14b9a3b12b694a37cfd499cc27927ca6df949bfed035a5a37b02ee09d267ef76f3e93242571f2921f2ec1074595a6bc7567664f5926272c14e16b79e5b544c2a
         HEAD_REF master
     )
     vcpkg_from_github(
         OUT_SOURCE_PATH CPPMERGE_SOURCE_PATH
         REPO vczh-libraries/Workflow
-        REF 72fcc5e120450874de259c1be2fdfd86bc656182
-        SHA512 ec4429708a755430b37050a78bea915230cb7a2a0c64681313f6875b6ad1ef17867d72d2a009d531d6c8778621b5d93634eef7d53af3c19be414bd0e19b9d828
+        REF e6340a43973f5663702b8c7424722a6c70af72c7
+        SHA512 93f4fd36b3c41169ee9384eed3f4d4deaa371c0e7bf56653fcaff649f0a2993af129177ada1265a92b7f47ff387a545417528f156205c210cb907d3c6eeb6992
         HEAD_REF master
     )
 endif()
-
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
@@ -38,7 +36,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+    SOURCE_PATH "${SOURCE_PATH}/Import"
     OPTIONS ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         -DSKIP_HEADERS=ON
@@ -49,16 +47,14 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-vlpp)
 
 if ("tool" IN_LIST FEATURES)
-    configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.gacgen.txt" "${GACGEN_SOURCE_PATH}/CMakeLists.txt" COPYONLY)
     vcpkg_cmake_configure(
-        SOURCE_PATH "${GACGEN_SOURCE_PATH}"
+        SOURCE_PATH "${GACGEN_SOURCE_PATH}/Tools/GacGen"
         LOGFILE_BASE "vlpp-gacgen"
     )
     vcpkg_cmake_install()
 
-    configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.cppmerge.txt" "${CPPMERGE_SOURCE_PATH}/CMakeLists.txt" COPYONLY)
     vcpkg_cmake_configure(
-        SOURCE_PATH "${CPPMERGE_SOURCE_PATH}"
+        SOURCE_PATH "${CPPMERGE_SOURCE_PATH}/Tools/CppMerge"
         LOGFILE_BASE "vlpp-cppmerge"
     )
     vcpkg_cmake_install()
