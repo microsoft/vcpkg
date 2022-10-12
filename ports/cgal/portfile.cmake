cba6b10ca299df139233c00105442a4ca2cc6a05
Laurent Rineau <laurent.rineau@cgal.org>
2022-10-12 18:19:12 +0200

    [cgal] update to 5.5.1

--- ports/cgal/portfile.cmake
+++ ports/cgal/portfile.cmake
@@ -4,8 +4,8 @@ vcpkg_buildpath_length_warning(37)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CGAL/cgal
-    REF v5.5
-    SHA512 d689b0d4e04ff0a4297939dc0d4c001de88e811583b73fe6173f46b38d51f31f336a09a66c47cf79a1511942a4f5cf4df5d57871deec926bec71183c63b3d2f1
+    REF v5.5.1
+    SHA512 5d247608ca19b46cddf9971a45829848904d1645dec9edf5059b15ae3be34c5b063e4e8be2e7018f7d754e7d1b16116d51ca8bc5c609a0a813df07a16118b5ce
     HEAD_REF master
 )
 
