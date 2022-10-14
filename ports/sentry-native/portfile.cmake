b5a08080af86023cc835eb63c077070475f79e7e
Jason Dictos <jason@triceimaging.com>
2022-10-14 17:52:44 +0000

    Add back the pthread patch

--- ports/sentry-native/portfile.cmake
+++ ports/sentry-native/portfile.cmake
@@ -12,6 +12,7 @@ vcpkg_extract_source_archive_ex(
         fix-warningC5105.patch
         fix-config-cmake.patch
         use-zlib-target.patch
+	TRICE.remove-pthread_create-linux.patch	# https://github.com/getsentry/sentry-native/issues/596
 )
 
 # Force crashpad TRICE
