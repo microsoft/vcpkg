a872c9f36e031005586c03dc76db4c503ef82ec7
Billy O'Neal <bion@microsoft.com>
2022-10-13 18:50:54 -0700

    [zlib] Update to 1.2.13
    This picks up the official fix for CVE-2022-37434.

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -1,15 +1,14 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO madler/zlib
-    REF v1.2.12
-    SHA512 5b029532a9f5f12ad425c12eccdf1b77c8d91801342c5b5e26ffb539f76a204e6c4882b40f0130f143f2cd38df90e90af2978cf4bb997e1fa3a0d1eff2ca979e
+    REF v1.2.13
+    SHA512 44b834fbfb50cca229209b8dbe1f96b258f19a49f5df23b80970b716371d856a4adf525edb4c6e0e645b180ea949cb90f5365a1d896160f297f56794dd888659
     HEAD_REF master
     PATCHES
-        cmake_dont_build_more_than_needed.patch
         0001-Prevent-invalid-inclusions-when-HAVE_-is-set-to-0.patch
-        debug-postfix-mingw.patch
-        0002-android-build-mingw.patch
-        CVE-2022-37434.patch
+        0002-skip-building-examples.patch
+        0003-build-static-or-shared-not-both.patch
+        0004-android-and-mingw-fixes.patch
 )
 
 # This is generated during the cmake build
@@ -19,7 +18,6 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DSKIP_INSTALL_FILES=ON
-        -DSKIP_BUILD_EXAMPLES=ON
     OPTIONS_DEBUG
         -DSKIP_INSTALL_HEADERS=ON
 )
