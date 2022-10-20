461165c6c4b3f921fba1f266723d8533ee2b8da5
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-20 14:11:20 +0200

    todo libglvnd

--- ports/libglvnd/portfile.cmake
+++ ports/libglvnd/portfile.cmake
@@ -1,3 +1,5 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+set(VCPKG_FIXUP_ELF_RPATH ON)
 
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org
@@ -7,6 +9,9 @@ vcpkg_from_gitlab(
     SHA512 3fda563239a9048c21c62005d074dafb79ec20213f8f02ffd8ac529d821a5ddb87ae44fb49159fce2a97581d9b7cff875ed7478621b6cf8191e2f5fd7f20a974
     HEAD_REF master
 )
+
+# TODO: Setup DEFAULT_EGL_VENDOR_CONFIG_DIRS
+# TODO: Fix absolute paths in libX11.a (will get integrated in the so here)
 
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
