9e0ce342be23ac4f081181999c9ed01a72f3b68c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-20 15:02:40 +0200

    Revert "Actually build the ports once in CI"
    This reverts commit 40a1933b1075b93a5b239d44b0ae6061d495d8c3.

--- ports/libdrm/portfile.cmake
+++ ports/libdrm/portfile.cmake
@@ -1,5 +1,4 @@
-if(0)
-
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
     message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_DRM_LIBRARIES in your triplet")
     set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 else()
