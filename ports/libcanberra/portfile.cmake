786daaec3ae215e6ded88781325f221f0f9901fe
Kai Pastor <dg0yt@darc.de>
2022-10-21 07:03:21 +0200

    Replace deprecated function

--- ports/libcanberra/portfile.cmake
+++ ports/libcanberra/portfile.cmake
@@ -10,10 +10,8 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 f7543582122256826cd01d0f5673e1e58d979941a93906400182305463d6166855cb51f35c56d807a56dc20b7a64f7ce4391368d24990c1b70782a7d0b4429c2
 )
 
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
+vcpkg_extract_source_archive(SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    REF ${VERSION}
     PATCHES
         ltdl.patch
         undefined_reference.diff  # https://sources.debian.org/patches/libcanberra/0.30-7/
