64f279151ce23030c878f5bc8ff9d226ae329ed2
Billy O'Neal <bion@microsoft.com>
2022-10-21 21:20:22 -0700

    Add usage and nitpick quotes.

--- ports/vowpalwabbit/portfile.cmake
+++ ports/vowpalwabbit/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_github(
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DVW_INSTALL=ON
         -DRAPIDJSON_SYS_DEP=ON
@@ -23,10 +23,10 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/VowpalWabbit/)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/VowpalWabbit)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
