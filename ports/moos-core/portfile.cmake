460f9593d54488dc158037f6cdc960cd27d9cc57
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-10-23 14:40:05 +0000

    Remove if empty

--- ports/moos-core/portfile.cmake
+++ ports/moos-core/portfile.cmake
@@ -37,6 +37,15 @@ if("db" IN_LIST FEATURES)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+if(VCPKG_TARGET_IS_WINDOWS)
+    file(GLOB "${CURRENT_PACKAGES_DIR}/lib/*")
+    list(LENGTH RESULT RES_LEN)
+    if(RES_LEN EQUAL 0)
+        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+        
+    endif()
+endif()
+
 
 
 file(INSTALL "${SOURCE_PATH}/Core/GPLCore.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
