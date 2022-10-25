7b2820e45c6430a8b43fc6b55bfdafba33b20c89
Kai Pastor <dg0yt@darc.de>
2022-10-25 09:19:11 +0200

    Fix graphviz

--- ports/graphviz/portfile.cmake
+++ ports/graphviz/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_gitlab(
     HEAD_REF main
     PATCHES
         0001-Fix-build.patch
+        ltdl-dlopen.patch
 )
 
 if(VCPKG_TARGET_IS_OSX)
@@ -17,8 +18,11 @@ elseif(VCPKG_TARGET_IS_LINUX)
     message("${PORT} currently requires the following libraries from the system package manager:\n    libtool\n\nThey can be installed with apt-get install libtool")
 endif()
 
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    set(EXTRA_CMAKE_OPTION "-DCMAKE_INSTALL_RPATH=${CURRENT_INSTALLED_DIR}/lib")
+vcpkg_list(SET OPTIONS)
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    vcpkg_list(APPEND OPTIONS "-DCMAKE_DISABLE_FIND_PACKAGE_LTDL=ON") # unsupported
+else()
+    vcpkg_list(APPEND OPTIONS "-DCMAKE_INSTALL_RPATH=${CURRENT_INSTALLED_DIR}/lib")
 endif()
 
 if(VCPKG_HOST_IS_WINDOWS)
@@ -40,10 +44,11 @@ vcpkg_cmake_configure(
         "-DGIT_EXECUTABLE=${GIT}"
         "-DPython3_EXECUTABLE=${PYTHON3}"
         "-DPKG_CONFIG_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/pkgconf/pkgconf"
-        ${EXTRA_CMAKE_OPTION}
+        ${OPTIONS}
 )
 
 vcpkg_cmake_install()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
@@ -57,14 +62,11 @@ if(VCPKG_TARGET_IS_WINDOWS)
     file(GLOB PLUGINS "${CURRENT_PACKAGES_DIR}/bin/gvplugin_*")
     file(COPY ${PLUGINS} DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
     vcpkg_execute_required_process(
-        COMMAND dot -c
+        COMMAND "${CURRENT_PACKAGES_DIR}/tools/${PORT}/dot" -c
         WORKING_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
         LOGNAME configure-plugins
     )
     file(COPY "${CURRENT_PACKAGES_DIR}/tools/${PORT}/config6" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
-# Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-vcpkg_fixup_pkgconfig()
