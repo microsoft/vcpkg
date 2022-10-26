f503f6a8a82dcddc78ea7a79574938aaf89baab8
Kai Pastor <dg0yt@darc.de>
2022-10-26 07:06:09 +0200

    Update to 1.2.35

--- ports/xmlsec/portfile.cmake
+++ ports/xmlsec/portfile.cmake
@@ -1,14 +1,18 @@
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
+
+string(REPLACE "." "_" release_tag "xmlsec-${VERSION}")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lsh123/xmlsec
-    REF d823da17c80b38ccc3c4262d7b2042b07e69e266 # xmlsec-1_2_34
-    SHA512 10ca5cb948723fcf1531efaab547c0665bc323cd52906decd314e0c78fff46ac7bc51eba5177838fc7f081f74f5e4a202d765c17dd0da6e378798676773a68ce
+    REF "${release_tag}"
+    SHA512 9a679fec678912ba0566fb108e2782de0e66d4b9ca335b55460ff7ca7dc47193a3396b43d0d2c2a014aab7ffaf527dc3604bc05fd5426841fd0a5dae634abc1e
     HEAD_REF master
     PATCHES 
         pkgconfig_fixes.patch
+        fix-size-max.patch # https://github.com/lsh123/xmlsec/pull/412
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -18,7 +22,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
 
 file(INSTALL "${SOURCE_PATH}/Copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-vcpkg_copy_pdbs()
