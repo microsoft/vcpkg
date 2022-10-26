25e59a6c36a97334328ce3a88bd13dc4f04f7ead
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-27 00:48:13 +0200

    WIP

--- ports/qtshadertools/portfile.cmake
+++ ports/qtshadertools/portfile.cmake
@@ -5,7 +5,7 @@ set(${PORT}_PATCHES)
 
 set(TOOL_NAMES qsb)
 
-file(COPY "${CURRENT_INSTALLED_DIR}/share/Qt6Gui/Qt6QEglFSX11IntegrationPluginTargets.cmake" DESTINATION "${CURRENT_BUILDTREES_DIR}" RENAME "Qt6QEglFSX11IntegrationPluginTargets.cmake.log")
+file(INSTALL "${CURRENT_INSTALLED_DIR}/share/Qt6Gui/Qt6QEglFSX11IntegrationPluginTargets.cmake" DESTINATION "${CURRENT_BUILDTREES_DIR}" RENAME "Qt6QEglFSX11IntegrationPluginTargets.cmake.log")
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
                      CONFIGURE_OPTIONS
