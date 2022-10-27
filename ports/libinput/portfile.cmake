99f9eb0cc9be09e77afee99d7de5547e7358346b
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-27 22:09:28 +0200

    add libinput portfile

---
+++ ports/libinput/portfile.cmake
@@ -0,0 +1,25 @@
+## requires AUTOCONF, LIBTOOL and PKCONF
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO libinput/libinput
+    REF 5b500c0e744db962fceadccd029fd5075654f4bc # 1.3.4 
+    SHA512  fb89248ca67996fe1b5bd42162153b7b913ced2a7f85b8f3b1e9feb2f1a298907d4a04c3b3cc67417553c8f4e82f74454e3febf5ec187e16969456bb89c55d60
+    HEAD_REF master # branch name
+) 
+
+
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS -Dtests=false
+            -Ddocumentation=false
+)
+vcpkg_install_meson()
+vcpkg_fixup_pkgconfig()
+
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
