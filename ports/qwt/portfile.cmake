40171ab07f94b4786e3c6a2614f7f324f6e7334b
Alexander Neumann <neumann@imt.uni-luebeck.de>
2022-10-27 13:46:53 +0200

    add FETCH_REF

--- ports/qwt/portfile.cmake
+++ ports/qwt/portfile.cmake
@@ -2,6 +2,7 @@ vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL "https://git.code.sf.net/p/qwt/git"
     REF "a9ac6b28ee990f5d51ea36523057a5af54875e2e"
+    FETCH_REF qwt-6.2
     PATCHES 
         config.patch
         fix_dll_install.patch
