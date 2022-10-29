a9c71dc2607924cb352067e26108824db30390bf
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-10-30 00:30:27 +0200

    libxmu update port

--- ports/libxmu/portfile.cmake
+++ ports/libxmu/portfile.cmake
@@ -6,9 +6,9 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.freedesktop.org/xorg
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lib/libxmu
-    REF e9efe2d027b4c46cf6834cc532222f8ad1d1d3c3 # 1.1.3 
-    SHA512  9d3ab7534afbb3d220ce846ecfc209536def28e707e68f393673bda6f92054e7a14212ae2400092afdc06bbb61d8315d460feaf5b551dc447390d6d952a5aa1f
-    HEAD_REF master # branch name
+    REF b29c739b577ee142877e69eb3fb07c7312d81557
+    SHA512  c2f53c962e3d7fc554182be835bae4799a8f430e54962a04dc30b4d8f3ec11eeecf45cc089ee5855d2bece2313602c6b34f81ac51247c4646cb8be50be75c788
+    HEAD_REF master
     PATCHES cl-build.patch
 ) 
 
