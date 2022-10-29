cc760f8791a67eba7fd2186e99ad7d7aea451b8c
BullyWiiPlaza <bullywiihacks@gmail.com>
2022-10-29 15:45:21 +0200

    Small fixes again according to the GitHub bot

--- ports/kissnet/portfile.cmake
+++ ports/kissnet/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
-vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/${PORT}")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
