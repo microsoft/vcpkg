64b7191daaa2ba3b0989a551a83d1f1038adb7cd
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-10-30 13:49:01 +0000

    [raylib] De-vendor dependencies

--- ports/raylib/portfile.cmake
+++ ports/raylib/portfile.cmake
@@ -10,25 +10,17 @@ These can be installed on Ubuntu systems via sudo apt install libgl1-mesa-dev li
     )
 endif()
 
-if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_LINUX)
-    set(patches fix-linkGlfw.patch)
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO raysan5/raylib
     REF bf2ad9df5fdcaa385b2a7f66fd85632eeebbadaa #v4.2.0
     SHA512 f6b1738d96fef89059062f570f67aaa8b143ccfbee78abfe5fbb25083371a4c432f3d1d0d357e4b475b4b72a6db7823c2341b70ac947759603b033c2b0acec47
     HEAD_REF master
-    PATCHES ${patches}
+    PATCHES fix-linkGlfw.patch
 )
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SHARED)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" STATIC)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        hidpi SUPPORT_HIGH_DPI
         use-audio USE_AUDIO
 )
 
@@ -42,9 +34,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_EXAMPLES=OFF
-        -DSHARED=${SHARED}
-        -DSTATIC=${STATIC}
-        -DUSE_EXTERNAL_GLFW=OFF # externl glfw3 causes build errors on Windows
+        -DUSE_EXTERNAL_GLFW=ON
         ${FEATURE_OPTIONS}
     OPTIONS_DEBUG
         -DENABLE_ASAN=${DEBUG_ENABLE_SANITIZERS}
@@ -60,17 +50,17 @@ vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     vcpkg_replace_string(
-        ${CURRENT_PACKAGES_DIR}/include/raylib.h
+        "${CURRENT_PACKAGES_DIR}/include/raylib.h"
         "defined(USE_LIBTYPE_SHARED)"
         "1 // defined(USE_LIBTYPE_SHARED)"
     )
