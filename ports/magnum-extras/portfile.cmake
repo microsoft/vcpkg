111014b704540110d86cb2d33e6209881ebc535c
Cheney-Wang <850426846@qq.com>
2022-11-02 02:23:54 -0700

    [magnum-*] Remove accidentally generated parameter and optimized portfile.cmake

--- ports/magnum-extras/portfile.cmake
+++ ports/magnum-extras/portfile.cmake
@@ -11,22 +11,23 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 # Handle features
 set(_COMPONENT_FLAGS "")
 foreach(_feature IN LISTS ALL_FEATURES)
-    # Uppercase the feature name and replace "-" with "_"
-    string(TOUPPER "${_feature}" _FEATURE)
-    string(REPLACE "-" "_" _FEATURE "${_FEATURE}")
+    if(_feature)
+        # Uppercase the feature name and replace "-" with "_"
+        string(TOUPPER "${_feature}" _FEATURE)
+        string(REPLACE "-" "_" _FEATURE "${_FEATURE}")
 
-    # Turn "-DWITH_*=" ON or OFF depending on whether the feature
-    # is in the list.
-    if(_feature IN_LIST FEATURES)
-        list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=ON")
-    else()
-        list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=OFF")
+        # Turn "-DWITH_*=" ON or OFF depending on whether the feature
+        # is in the list.
+        if(_feature IN_LIST FEATURES)
+            list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=ON")
+        else()
+            list(APPEND _COMPONENT_FLAGS "-DWITH_${_FEATURE}=OFF")
+        endif()
     endif()
 endforeach()
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${_COMPONENT_FLAGS}
         -DBUILD_STATIC=${BUILD_STATIC}
@@ -34,9 +35,9 @@ vcpkg_configure_cmake(
         -DMAGNUM_PLUGINS_RELEASE_DIR=${CURRENT_INSTALLED_DIR}/bin/magnum
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/MagnumExtras TARGET_PATH share/magnumextras)
+vcpkg_cmake_config_fixup(PACKAGE_NAME MagnumExtras CONFIG_PATH share/cmake/MagnumExtras)
 
 # Messages to the user
 if("ui" IN_LIST FEATURES)
@@ -45,29 +46,28 @@ endif()
 
 # Debug includes and share are the same as release
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
 # Clean up empty directories
 if("${FEATURES}" STREQUAL "core")
     file(REMOVE_RECURSE
-        ${CURRENT_PACKAGES_DIR}/bin
-        ${CURRENT_PACKAGES_DIR}/lib
-        ${CURRENT_PACKAGES_DIR}/debug
+        "${CURRENT_PACKAGES_DIR}/bin"
+        "${CURRENT_PACKAGES_DIR}/lib"
+        "${CURRENT_PACKAGES_DIR}/debug"
     )
     set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
-   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
+   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING
-    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+file(INSTALL "${SOURCE_PATH}/COPYING"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     RENAME copyright)
-
 
 vcpkg_copy_pdbs()
