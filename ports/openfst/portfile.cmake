142031fc7b55d0428c4cf032119ff34ad46f1c55
day253 <9634619+day253@users.noreply.github.com>
2022-11-05 15:55:54 +0000

    use make

--- ports/openfst/portfile.cmake
+++ ports/openfst/portfile.cmake
@@ -1,28 +1,39 @@
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_download_distfile(
+  ARCHIVE
+  URLS https://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.8.2.tar.gz
+  FILENAME openfst-1.8.2.tar.gz
+  SHA512 ca7f9f19e24141e1f1d0bbabf43795e6e278bce3887c14261d9ce204a0e01b1588eaf982755a9105247510a19f67da2f566e9b14b1d869497148f95b55606d5c
+)
+
+vcpkg_extract_source_archive(SOURCE_PATH ARCHIVE "${ARCHIVE}")
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(LINKAGE_DYNAMIC yes)
+    set(LINKAGE_STATIC no)
+else()
+    set(LINKAGE_DYNAMIC no)
+    set(LINKAGE_STATIC yes)
 endif()
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO kkm000/openfst
-    REF 338225416178ac36b8002d70387f5556e44c8d05 #1.7.2
-    SHA512 14e1ac44ff5d059dc12ab0412037744a207e54485443dbf6eab4e6fb4aab122bbbcbd54af3ce23732c7d960448756c5a5ec0e116e0a797fa343156bb865f3eb1
-    HEAD_REF winport
-    PATCHES
-        fix-build.patch
-)
-
-vcpkg_cmake_configure(
+vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
+    AUTOCONFIG
+    OPTIONS
+        "--enable-shared=${LINKAGE_DYNAMIC}"
+        "--enable-static=${LINKAGE_STATIC}"
+        "--prefix=${CURRENT_INSTALLED_DIR}"
+        "--disable-doxygen"
+    OPTIONS_DEBUG
+        "--enable-debug"
 )
 
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-openfst CONFIG_PATH share/unofficial-openfst)
+vcpkg_install_make()
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-vcpkg_copy_pdbs()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools")
 
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-vcpkg_fixup_pkgconfig()
