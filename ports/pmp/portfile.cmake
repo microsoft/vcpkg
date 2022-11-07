a23470ae169992cd21114eeb12f76e75b4bc6c55
Park DongHa <luncliff@gmail.com>
2022-11-07 16:01:55 +0900

    Update ports/pmp/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/pmp/portfile.cmake
+++ ports/pmp/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DPMP_BUILD_EXAMPLES=OFF
