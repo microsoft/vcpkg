256f54d696989c311c1abf27d81493a5fe3fbc42
Rémy Tassoux <contact@rt2.fr>
2022-11-08 17:31:41 +0100

    [joltphysics] New port

---
+++ ports/joltphysics/portfile.cmake
@@ -0,0 +1,40 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jrouwe/JoltPhysics
+    REF 944ed96c7f217b8ae3b4b9b67449f33db66d7b30
+    SHA512 0b88d7c4a7c90e30e50f985776860aaef7c4ecbe599b6d4fb0d37b50791f5d3957f5ac4657baa3b1cfb2fb586b606d25a6650c40c1cdbe4df89ba8cdc09b4e6c
+    HEAD_REF master
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_CRT)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/Build"
+    WINDOWS_USE_MSBUILD
+    OPTIONS
+        -DTARGET_UNIT_TESTS=OFF
+        -DTARGET_HELLO_WORLD=OFF
+        -DTARGET_PERFORMANCE_TEST=OFF
+        -DTARGET_SAMPLES=OFF
+        -DTARGET_VIEWER=OFF
+        -DUSE_STATIC_MSVC_RUNTIME_LIBRARY=${USE_STATIC_CRT}
+)
+
+vcpkg_cmake_build()
+
+file(
+    INSTALL "${SOURCE_PATH}/Jolt"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/include"
+    FILES_MATCHING
+        PATTERN "*.h"
+        PATTERN "*.inl"
+)
+
+file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/Debug/Jolt.lib"  DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/Release/Jolt.lib"  DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_copy_pdbs()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
