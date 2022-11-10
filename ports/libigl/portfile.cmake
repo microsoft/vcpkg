f0f6e970e1b9180f35acbf10e02305423a867829
FrankXie <v-frankxie@microsoft.com>
2022-11-09 23:11:03 -0800

    use hunter dep

--- ports/libigl/portfile.cmake
+++ ports/libigl/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     SHA512 7014ffdaa160bfa2509fc283cb7176d7994a37f51509c7374659292efad076c8fb594f9f6990bab1aa5562d1f66e93403ea35a5bf2a924436560a2d4669ffcfd
     HEAD_REF master
     PATCHES
-      use-hunter-eigen.patch
+        install-extra-headers.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -31,6 +31,7 @@ vcpkg_cmake_configure(
         -DLIBIGL_BUILD_TUTORIALS=OFF
         -DLIBIGL_INSTALL=ON
         -DLIBIGL_USE_STATIC_LIBRARY=OFF
+        -DHUNTER_ENABLED=ON
 )
 
 vcpkg_cmake_install()
