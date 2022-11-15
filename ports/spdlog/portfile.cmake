ff667f9f2611cc7726f9a646f48b019348cf6d30
Kiruya Momochi <65301509+KiruyaMomochi@users.noreply.github.com>
2022-11-15 20:09:29 +0900

    [spdlog] backporting https://github.com/gabime/spdlog/pull/2545

--- ports/spdlog/portfile.cmake
+++ ports/spdlog/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v1.11.0
     SHA512 210f3135c7af3ec774ef9a5c77254ce172a44e2fa720bf590e1c9214782bf5c8140ff683403a85b585868bc308286fbdeb1c988e4ed1eb3c75975254ffe75412
     HEAD_REF v1.x
+    PATCHES
+        fmt-header.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
