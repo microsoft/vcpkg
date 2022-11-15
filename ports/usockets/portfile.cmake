4d772ffe9afe7fe5b711951b7656e5add4953f62
Jonliu1993 <13720414433@163.com>
2022-11-15 17:18:04 +0800

    update ref

--- ports/usockets/portfile.cmake
+++ ports/usockets/portfile.cmake
@@ -9,7 +9,7 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO uNetworking/uSockets
-    REF 917ea86baad640967f62c4a92290e5211bb7966c  #v0.8.1
+    REF 917ea86baad640967f62c4a92290e5211bb7966c  #v0.8.2
     SHA512 bc7850d9e7e79a390817b7ce530d79d9dc0f04386da81abbd616f06a9108dc18f928b29f1e31ef99600fb3a18ed28e8d9815fc9c3bc6c0ea66b7fe81a2fa19ff
     HEAD_REF master
 )
