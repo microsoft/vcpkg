8257dd7dbab93cff8b26cd00a46d818208947745
Monica <v-liumonica@microsoft.com>
2022-11-15 18:08:51 +0800

    update format

--- ports/mlpack/portfile.cmake
+++ ports/mlpack/portfile.cmake
@@ -5,8 +5,8 @@ vcpkg_from_github(
     REF 28eb1858c59e4469da0e9689663a45fc140af9c4 # 4.0.0
     SHA512 b33aa5df48c9f0e5a5fac7bfb69fd2c64bc01f1ba0ae22990774e1805881c60e4652d2f23b6c95627da1a20e39ee6a90e327fdaa6d1e00bac8986dcecc15a89a
     HEAD_REF master
-	PATCHES
-		0001-Add-a-patch-that-will-hopefully-help-MSVC-debugging.patch
+    PATCHES
+        0001-Add-a-patch-that-will-hopefully-help-MSVC-debugging.patch
 )
 
 # Copy the header files
