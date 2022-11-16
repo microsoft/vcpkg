fe5a8268d43eb4601fcd8bbe00ab1adea90bded0
Billy Robert O'Neal III <bion@microsoft.com>
2022-11-16 13:12:47 -0800

    Add comment about new setting to indicate that it doesn't conflict with VCPKG_CRT_LINKAGE, add quotes and formatting.

--- ports/sqlitecpp/portfile.cmake
+++ ports/sqlitecpp/portfile.cmake
@@ -12,15 +12,16 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-	sqlcipher SQLITE_HAS_CODEC
+        sqlcipher SQLITE_HAS_CODEC
 )
+
 set(USE_STACK_PROTECTION "")
 if(VCPKG_TARGET_IS_MINGW)
     set(USE_STACK_PROTECTION "-DSQLITECPP_USE_STACK_PROTECTION=OFF")
 endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
         -DSQLITECPP_RUN_CPPLINT=OFF
@@ -28,7 +29,8 @@ vcpkg_cmake_configure(
         -DSQLITECPP_INTERNAL_SQLITE=OFF
         -DSQLITE_ENABLE_COLUMN_METADATA=OFF
         -DSQLITECPP_INTERNAL_SQLITE=OFF
-        -DSQLITECPP_USE_STATIC_RUNTIME=OFF
+        -DSQLITECPP_USE_STATIC_RUNTIME=OFF # unconditionally off because vcpkg's toolchains already do the right thing
+        # See https://github.com/SRombauts/SQLiteCpp/blob/e74403264ec7093060f4ed0e84bc9208997c8344/CMakeLists.txt#L40-L46
         ${USE_STACK_PROTECTION}
 )
 
