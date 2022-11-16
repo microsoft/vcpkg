microsoft/vcpkg

commit 5ec554416305ca7de10b3ef8b9b3a8bb27c7fe79

[taglib] Update to 1.13 (#27751)
* [taglib] Update to 1.13

* update

Co-authored-by: complexlogic <complexlogic@users.noreply.github.com>

ports/taglib/portfile.cmake
@@ -1,10 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO taglib/taglib
-    REF v1.12
-    SHA512 63c96297d65486450908bda7cc1583ec338fa5a56a7c088fc37d6e125e1ee76e6d20343556a8f3d36f5b7e5187c58a5d15be964c996e3586ea1438910152b1a6
+    REF v1.13
+    SHA512 b6e3253d158b41173073c0da1915f5e4a3de947db918660817cb1c755fba7e3723ea1a335fbbc30b0dcf942348a471b493fe2ce1d52d1a808578edee14e1bfc7
     HEAD_REF master
-    PATCHES msvc-disable-deprecated-warnings.patch
 )
 
 if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")

ports/taglib/msvc-disable-deprecated-warnings.patch
@@ -1,26 +1,0 @@
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 5fc91cc6..6f57e4ee 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -58,10 +58,17 @@ if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
- endif()
-
--if(MSVC AND ENABLE_STATIC_RUNTIME)
--  foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
--    string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
--  endforeach(flag_var)
-+if(MSVC)
-+  if(ENABLE_STATIC_RUNTIME)
-+    foreach(flag_var CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-+      string(REGEX REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
-+    endforeach(flag_var)
-+  endif()
-+  # Disable warnings for internal invocations of API functions
-+  # that have been marked with TAGLIB_DEPRECATED
-+  # https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-3-c4996
-+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4996")
-+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4996")
- endif()
-
- # Read version information from file taglib/toolkit/taglib.h into variables

commit f5100e7e856e23c6611eb191f1bd4636545a9dbb

[taglib] Disable tests and examples.
New build: https://dev.azure.com/vcpkg/public/_build/results?buildId=80953
REGRESSION: qt:x64-windows-static cascaded, but it is required to pass. (C:\a\2\s\scripts\azure-pipelines/../ci.baseline.txt).
REGRESSION: taglib:x64-windows-static failed with BUILD_FAILED. If expected, add taglib:x64-windows-static=fail to C:\a\2\s\scripts\azure-pipelines/../ci.baseline.txt.
REGRESSION: taglib:x64-windows-static-md failed with BUILD_FAILED. If expected, add taglib:x64-windows-static-md=fail to C:\a\2\s\scripts\azure-pipelines/../ci.baseline.txt.

Probably caused by https://github.com/microsoft/vcpkg/pull/27751 "[taglib] Update to 1.13"

We only saw in full CI run because the trigger is an optional dependency detection for cppunit being installed first:

cppunit.lib(TestResult.cpp.obj) : error LNK2038: mismatch detected for '_ITERATOR_DEBUG_LEVEL': value '0' doesn't match value '2' in main.cpp.obj
cppunit.lib(TestResult.cpp.obj) : error LNK2038: mismatch detected for 'RuntimeLibrary': value 'MT_StaticRelease' doesn't match value 'MTd_StaticDebug' in main.cpp.obj

Upstream has:

```
if(BUILD_TESTING)
  find_package(CppUnit)
  if(CppUnit_FOUND)
    add_subdirectory(tests)
  else()
    message(WARNING "BUILD_TESTING requested, but CppUnit not found, skipping tests.")
  endif()
endif()
```

upstream. Disabling.

Also checked whether cppunit was including release libs in debug, but they aren't; upstream of taglib has a broken find module for cppunit.

ports/taglib/portfile.cmake
@@ -12,7 +12,10 @@ endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS ${WINRT_OPTIONS}
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DBUILD_EXAMPLES=OFF
+        ${WINRT_OPTIONS}
 )
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()