1cc8347b82224931c3ad4d400edb70b21e7ff497
luncliff <dong-ha-park@linecorp.com>
2022-11-17 15:23:42 +0900

    [flatbuffers] use vcpkg_copy_tools

--- ports/flatbuffers/portfile.cmake
+++ ports/flatbuffers/portfile.cmake
@@ -35,14 +35,7 @@ vcpkg_fixup_pkgconfig()
 
 file(GLOB flatc_path ${CURRENT_PACKAGES_DIR}/bin/flatc*)
 if(flatc_path)
-    make_directory("${CURRENT_PACKAGES_DIR}/tools/flatbuffers")
-    get_filename_component(flatc_executable ${flatc_path} NAME)
-    file(
-        RENAME
-        ${flatc_path}
-        ${CURRENT_PACKAGES_DIR}/tools/flatbuffers/${flatc_executable}
-    )
-    vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/flatbuffers")
+    vcpkg_copy_tools(TOOL_NAMES flatc AUTO_CLEAN)
 else()
     file(APPEND "${CURRENT_PACKAGES_DIR}/share/flatbuffers/FlatbuffersConfig.cmake"
 "include(\"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/share/flatbuffers/FlatcTargets.cmake\")\n")
