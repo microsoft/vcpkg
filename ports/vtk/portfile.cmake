36afd755f3a312d99cc71543a5345b2f813ce154
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2022-11-19 22:58:35 +0100

    commit stuff

--- ports/vtk/portfile.cmake
+++ ports/vtk/portfile.cmake
@@ -172,7 +172,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        --debug-find-pkg=hdf5,HDF5
         ${FEATURE_OPTIONS}
         ${VTK_FEATURE_OPTIONS}
         -DBUILD_TESTING=OFF
