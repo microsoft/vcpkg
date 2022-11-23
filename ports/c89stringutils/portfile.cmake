14c438736aedd4b8acf7b952526525f6676fc42f
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-11-23 00:11:28 -0500

    [ports/c89stringutils] Fix CMake build with vcpkg

--- ports/c89stringutils/portfile.cmake
+++ ports/c89stringutils/portfile.cmake
@@ -9,10 +9,11 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DBUILD_TESTS=OFF"
+        "-DBUILD_TESTING=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/c89stringutils"
      RENAME copyright)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
+                    "${CURRENT_PACKAGES_DIR}/debug/share")
