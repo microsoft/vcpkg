a6c0367d813b8e4be3b493c722a1a1deafe72acc
Kai Pastor <dg0yt@darc.de>
2022-11-23 08:39:01 +0100

    Revise portfile

--- ports/brotli/portfile.cmake
+++ ports/brotli/portfile.cmake
@@ -15,16 +15,14 @@ vcpkg_cmake_configure(
     OPTIONS
         -DBROTLI_DISABLE_TESTS=ON
 )
-
 vcpkg_cmake_install()
-
 vcpkg_copy_pdbs()
-
-vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/brotli")
-vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-brotli PACKAGE_NAME unofficial-brotli)
 vcpkg_fixup_pkgconfig()
 
+vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-brotli PACKAGE_NAME unofficial-brotli)
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/tools")
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/brotli")
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-brotli")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
