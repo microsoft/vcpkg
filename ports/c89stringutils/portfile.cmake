f71e5fd7d4985f343a021695d4010c4f6fcc8938
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-11-23 00:03:14 -0500

    [ports/c89stringutils] Pin to latest release from 2022-06-23

--- ports/c89stringutils/portfile.cmake
+++ ports/c89stringutils/portfile.cmake
@@ -4,18 +4,15 @@ vcpkg_from_github(
     REF             bbf0e2e7ea0803e11dc4575a01d25f55fd5b48b1
     SHA512          186ccb2cd54d84e547f95c65fc86955f11f73670803384a12186ef91dd67d79067b5c050775ccca515b6a218079757af9edf2808c3e6e2902755e7cee410f935
     HEAD_REF        master
-    PATCHES
-        no_flags.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DBUILD_TESTING=OFF"
+        "-DBUILD_TESTS=OFF"
 )
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/cmake/LICENSE.txt"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/c89stringutils"
      RENAME copyright)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
-                    "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
