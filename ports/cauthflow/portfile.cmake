75cf25b10bf9391279fe132d7a722c8a51ac324c
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-11-23 16:46:25 -0500

    [ports/cauthflow] { [cauthflow_utils/CMakeLists.txt] Reflect parson official status } ; use version-date

--- ports/cauthflow/portfile.cmake
+++ ports/cauthflow/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO            "offscale/${PORT}"
-    REF             6520e9b6adfb03e4953c65a78c0ad461e7827401
-    SHA512          22dd41674931bf47e3f619cb82dedc40ef8a820a8588ad3f08801d35546f2892ef1feeb4abba78e79cbbbe42593156e2650c5fa870bb3ce75c7021d81c7d8464
+    REF             877fabe3b7076ad94991716b0d3566bb2d33af45
+    SHA512          0
     HEAD_REF        master
 )
 
