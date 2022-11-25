16e2acdb01429a14921dadd05727a17de5f9beac
Kai Pastor <dg0yt@darc.de>
2022-11-25 18:32:51 +0100

    Revise python3 injection

--- ports/skia/portfile.cmake
+++ ports/skia/portfile.cmake
@@ -9,12 +9,7 @@ vcpkg_from_git(
 # Replace hardcoded python paths
 vcpkg_find_acquire_program(PYTHON3)
 vcpkg_replace_string("${SOURCE_PATH}/.gn" "script_executable = \"python3\"" "script_executable = \"${PYTHON3}\"")
-
-if(VCPKG_TARGET_IS_LINUX)
-    vcpkg_replace_string("${SOURCE_PATH}/gn/toolchain/BUILD.gn" "command = \"$shell python3" "command = \"$shell '${PYTHON3}'")
-else()
-    vcpkg_replace_string("${SOURCE_PATH}/gn/toolchain/BUILD.gn" "command = \"$shell python" "command = \"$shell '${PYTHON3}'")
-endif()
+vcpkg_replace_string("${SOURCE_PATH}/gn/toolchain/BUILD.gn" "python3 " "\\\"${PYTHON3}\\\" ")
 
 function(checkout_in_path PATH URL REF)
     if(EXISTS "${PATH}")
