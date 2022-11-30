650d9ed0d3a304344728aeff1566cfe84884f3d8
Trish Lam <trishlam@microsoft.com>
2022-11-30 14:33:16 -0800

    Adding libphonenumber port, pushing changes to get help on error

---
+++ ports/libphonenumber/portfile.cmake
@@ -0,0 +1,15 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/libphonenumber
+    REF v8.13.1
+    SHA512 e847c263ccc076070a669334536e8f4fe1b6864e8c0cec9992c430b2728512a96d9cdc3e8f0978b79a9fce64edaed85c369773b58706ca189115375ac5dca597
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/cpp"
+)
+
+vcpkg_cmake_install()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
