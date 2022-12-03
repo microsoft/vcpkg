a97bdb05f4cd0590002ca953fbd158cab8db045c
dtmen <edi.krumbach@gmx.de>
2022-12-03 11:28:45 +0100

    Revert "Merge branch 'master' of https://github.com/erdogan61/vcpkg"
    This reverts commit 789c93e81a6ea996649e314eca852180035361ef, reversing
    changes made to c87a8183bd518b77b4092bfc27d24ad35686d4d7.

--- ports/brotli/portfile.cmake
+++ ports/brotli/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
         install.patch
         fix-arm-uwp.patch
         pkgconfig.patch
+        fix-ios.patch
 )
 
 vcpkg_cmake_configure(
@@ -15,15 +16,16 @@ vcpkg_cmake_configure(
     OPTIONS
         -DBROTLI_DISABLE_TESTS=ON
 )
+
 vcpkg_cmake_install()
+
 vcpkg_copy_pdbs()
+
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/brotli")
+vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-brotli PACKAGE_NAME unofficial-brotli)
 vcpkg_fixup_pkgconfig()
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-brotli-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-brotli")
-vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-brotli PACKAGE_NAME unofficial-brotli)
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/tools")
-vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/brotli")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-brotli")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
