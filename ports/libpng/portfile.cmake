84d64779166174c9d9cd04a18919a7ce30daaaaa
Honeybunch <amt3824@g.rit.edu>
2022-12-03 13:29:22 -0800

    Fixing android port of libpng

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -57,7 +57,7 @@ if(VCPKG_TARGET_IS_ANDROID)
     vcpkg_list(APPEND LD_VERSION_SCRIPT_OPTION "-Dld-version-script=OFF")
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
         # for armeabi-v7a, check whether NEON is available
-        vcpkg_list(APPEND LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION "-DPNG_ARM_NEON=check")
+        vcpkg_list(APPEND LIBPNG_HARDWARE_OPTIMIZATIONS_OPTION "-DPNG_ARM_NEON=on")
     endif()
 endif()
 
