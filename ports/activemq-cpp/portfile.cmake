b3cfe8578111f9fe3fcafca48638e49a3f280604
Billy Robert O'Neal III <bion@microsoft.com>
2022-12-05 12:34:12 -0800

    Whoops patches shouldn't be quoted.

--- ports/activemq-cpp/portfile.cmake
+++ ports/activemq-cpp/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    PATCHES "${PATCHES}"
+    PATCHES ${PATCHES}
 )
 
 if (VCPKG_TARGET_IS_LINUX)
