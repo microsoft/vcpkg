4cd487677ea10f1de548bfb0f12d4ff644983b60
Trish Lam <trishlam@microsoft.com>
2022-12-06 14:43:57 -0800

    Fixing port to incorporate reviews on the PR

--- ports/libphonenumber/portfile.cmake
+++ ports/libphonenumber/portfile.cmake
@@ -9,21 +9,22 @@ vcpkg_from_github(
         "fix-windows-static-link-icu-lib.patch"
         "fix-absl-with-geocoder-off.patch"
         # "fix-ninja-error-multiple-rules.patch" Commenting this out for remove-shared-lib patch instead
-        "remove-shared-lib.patch" # Getting ninja error with multiple rules
-                                  # generating same lib name (hence the fix-ninja patch) but that created
-                                  # an error during release, which I am fixing by commenting out the
-                                  # option to build shared libs versions of phonenumber & geocoding.
+        "remove-shared-lib.patch" # Getting ninja error with multiple rules generating
+                                  # same lib name (hence the fix-ninja patch above) but that created
+                                  # an error during release, which I am fixing by setting option
+                                  # set(BUILD_SHARED_LIB false) and have -DREGENERATE_METADATA=OFF.
                                   # Unsure if shared libs are necessary to keep. For now, temp fix.
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cpp"
     OPTIONS
-        # Geocoder does not build successfully on Windows (https://github.com/microsoft/vcpkg/pull/10088/files)
-        -DREGENERATE_METADATA=OFF
-        -DUSE_RE2=ON
         # Metadata is included in source so regeneration is unnecessary. Disabling removes need for java.exe in build system.
         # See https://github.com/google/libphonenumber/pull/2363
+        -DREGENERATE_METADATA=OFF
+        -DUSE_RE2=ON
+        # Geocoder does not build successfully on Windows.
+        # See https://github.com/microsoft/vcpkg/pull/10088/files
         -DBUILD_GEOCODER=OFF
 )
 
--- triplets/community/x64-windows-static-md.cmake
+++ triplets/community/x64-windows-static-md.cmake
@@ -1,4 +1,3 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_BUILD_TYPE release) # Debug build failing isctype.cpp assertion (c >= -1 && c <= 255) while running geocoding_data.exe program
