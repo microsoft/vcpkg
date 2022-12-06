01fcd652538a0b746f4638c29c73c4661c4c097c
Trish Lam <trishlam@microsoft.com>
2022-12-05 21:20:43 -0800

    Fixed debugs builds by turning geocoder off, fixed icu link problems in static builds, and added support for re2

--- ports/libphonenumber/portfile.cmake
+++ ports/libphonenumber/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_from_github(
-    ARCHIVE
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/libphonenumber
     REF v8.13.1
     SHA512 e847c263ccc076070a669334536e8f4fe1b6864e8c0cec9992c430b2728512a96d9cdc3e8f0978b79a9fce64edaed85c369773b58706ca189115375ac5dca597
     HEAD_REF master
     PATCHES 
-        "change-icui18n-lib-name.patch"
         "fix-re2-identifiers.patch"
+        "fix-windows-static-link-icu-lib.patch"
+        "fix-absl-with-geocoder-off.patch"
         # "fix-ninja-error-multiple-rules.patch" Commenting this out for remove-shared-lib patch instead
         "remove-shared-lib.patch" # Getting ninja error with multiple rules
                                   # generating same lib name (hence the fix-ninja patch) but that created
@@ -19,10 +19,16 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cpp"
     OPTIONS
+        # Geocoder does not build successfully on Windows (https://github.com/microsoft/vcpkg/pull/10088/files)
         -DREGENERATE_METADATA=OFF
         -DUSE_RE2=ON
+        # Metadata is included in source so regeneration is unnecessary. Disabling removes need for java.exe in build system.
+        # See https://github.com/google/libphonenumber/pull/2363
+        -DBUILD_GEOCODER=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
--- triplets/x64-windows-static.cmake
+++ triplets/x64-windows-static.cmake
@@ -1,5 +1,4 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
-set(VCPKG_BUILD_TYPE release) # Debug build failing isctype.cpp assertion (c >= -1 && c <= 255) while running geocoding_data.exe program
 
