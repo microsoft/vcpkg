2a744d41af76a394f270fcb152ab80cc5bddbdf2
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2022-12-12 13:00:37 -0600

    [tabulate] update to version 1.4 (#28294)
    * Updating tabulate to version 1.4
    
    The previous port was using a version prior to 1.0
    
    * [tabulate] updating to use latest cmake functions
    
    * [tabulate] Adding license
    
    License is MIT according to:
    https://github.com/p-ranav/tabulate/blob/v1.4/LICENSE
    
    * [tabulate] Adding double quotes to absolute paths in portfile.cmake
    
    * [tabulate] updating license
    
    Requested to add BSL-1.0 AND BSD-3-Clause
    
    * [tabulate] running x-add-version

--- ports/tabulate/portfile.cmake
+++ ports/tabulate/portfile.cmake
@@ -1,26 +1,23 @@
-# header-only library
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO p-ranav/tabulate
-    REF 8f7a31b58806384b755858fe5e69593f9f216ed0
-    SHA512 33a51617d363f9e8eed2153f60c5a087ed84edaeb21a0bc779d0af6228d4d7deb211e03c1eb7c8d56ac538de6a1cc0b641d219b4ff95d277f3daa7762f64a8f6
+    REF v1.4
+    SHA512 37d69d53f1c628955f2a35779dbf3053c5b449f7069d846b697b541df1719b00e9b72e9d6de361d9733b7e7543c25e444b1aa139f72000730eeeafa022bbf3c1
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -Dtabulate_BUILD_TESTS=OFF
         -DSAMPLES=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug ${CURRENT_PACKAGES_DIR}/lib)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/LICENSE.termcolor DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE.termcolor" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
