a0889775be0b66ef36ae1cb15665f90617d5fdb2
Lily Wang <494550702@qq.com>
2022-12-13 04:53:02 -0800

    [mathgl] Fix incorrect target name and incorrect executable path

--- ports/mathgl/portfile.cmake
+++ ports/mathgl/portfile.cmake
@@ -1,10 +1,14 @@
-set(MATHGL_VERSION "2.5")
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
+
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING "${PORT} currently requires the following packages:\n    libgl1-mesa-dev\n    This can be installed on Ubuntu systems via\n    sudo apt-get install -y libgl1-mesa-dev\n")
+endif()
 
 vcpkg_from_sourceforge(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mathgl/mathgl
-    REF mathgl%20${MATHGL_VERSION}
-    FILENAME "mathgl-${MATHGL_VERSION}.tar.gz"
+    REF mathgl%20${VERSION}
+    FILENAME "mathgl-${VERSION}.tar.gz"
     SHA512 1fe27962ffef8d7127c4e1294d735e5da4dd2d647397f09705c3ca860f90bd06fd447ff614e584f3d2b874a02262c5518be37d59e9e0a838dd5b8b64fd77ef9d
     PATCHES
         fix_cmakelists_and_cpp.patch
@@ -12,6 +16,7 @@ vcpkg_from_sourceforge(
         fix_default_graph_init.patch
         fix_mglDataList.patch
         fix_arma_sprintf.patch
+        fix-usage.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -38,42 +43,39 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-if(NOT VCPKG_CMAKE_SYSTEM_NAME OR VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-  vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+if(VCPKG_TARGET_IS_WINDOWS)
+  vcpkg_cmake_config_fixup(PACKAGE_NAME MathGL2 CONFIG_PATH cmake)
   file(REMOVE "${CURRENT_PACKAGES_DIR}/mathgl2-config.cmake")
   file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/mathgl2-config.cmake")
 else()
-  vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/mathgl2)
+  vcpkg_cmake_config_fixup(PACKAGE_NAME MathGL2 CONFIG_PATH lib/cmake/mathgl2)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-#somehow the native CMAKE_EXECUTABLE_SUFFIX does not work, so here we emulate it
-if(CMAKE_HOST_WIN32)
-  set(EXECUTABLE_SUFFIX ".exe")
-else()
-  set(EXECUTABLE_SUFFIX "")
+function(handle_executable_path PATH TOOL_NAME)
+    if(EXISTS "${CURRENT_PACKAGES_DIR}/${PATH}/${TOOL_NAME}.exe" OR "${CURRENT_PACKAGES_DIR}/${PATH}/${TOOL_NAME}")
+        vcpkg_copy_tools(TOOL_NAMES ${TOOL_NAME} SEARCH_DIR "${CURRENT_PACKAGES_DIR}/${PATH}" AUTO_CLEAN)
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/MathGL2/MathGLTargets-debug.cmake" 
+        "${VCPKG_IMPORT_PREFIX}/debug/${PATH}/${TOOL_NAME}.exe" "${VCPKG_IMPORT_PREFIX}/tools/mathgl/${TOOL_NAME}.exe")
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/MathGL2/MathGLTargets-release.cmake" 
+        "${VCPKG_IMPORT_PREFIX}/${PATH}/${TOOL_NAME}.exe" "${VCPKG_IMPORT_PREFIX}/tools/mathgl/${TOOL_NAME}.exe")
+    endif()
+endfunction()
+
+if("fltk" IN_LIST FEATURES)
+  handle_executable_path("bin" "mgllab")
+  handle_executable_path("bin" "mglview")
+endif()
+if("qt5" IN_LIST FEATURES)
+  handle_executable_path("bin" "mglview")
+  handle_executable_path("bin" "udav")
 endif()
 
-file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/bin/mgllab${EXECUTABLE_SUFFIX}")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/bin/mglview${EXECUTABLE_SUFFIX}")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/bin/mglconv${EXECUTABLE_SUFFIX}")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/bin/mgltask${EXECUTABLE_SUFFIX}")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/debug/bin/udav${EXECUTABLE_SUFFIX}")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools/mathgl/")
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin/mglconv${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/mathgl/mglconv${EXECUTABLE_SUFFIX}")
-file(RENAME "${CURRENT_PACKAGES_DIR}/bin/mgltask${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/mathgl/mgltask${EXECUTABLE_SUFFIX}")
-if (EXISTS "${CURRENT_PACKAGES_DIR}/bin/mgllab${EXECUTABLE_SUFFIX}")
-	file(RENAME "${CURRENT_PACKAGES_DIR}/bin/mgllab${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/mathgl/mgllab${EXECUTABLE_SUFFIX}")
-endif()
-if ("EXISTS ${CURRENT_PACKAGES_DIR}/bin/mglview${EXECUTABLE_SUFFIX}")
-	file(RENAME "${CURRENT_PACKAGES_DIR}/bin/mglview${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/mathgl/mglview${EXECUTABLE_SUFFIX}")
-endif()
-if (EXISTS "${CURRENT_PACKAGES_DIR}/bin/udav${EXECUTABLE_SUFFIX}")
-	file(RENAME "${CURRENT_PACKAGES_DIR}/bin/udav${EXECUTABLE_SUFFIX}" "${CURRENT_PACKAGES_DIR}/tools/mathgl/udav${EXECUTABLE_SUFFIX}")
-endif()
-
-vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/mathgl")
+handle_executable_path("bin" "mgltask")
+handle_executable_path("bin" "mglconv")
+handle_executable_path("lib/cgi-bin" "mgl.cgi")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cgi-bin" "${CURRENT_PACKAGES_DIR}/debug/lib/cgi-bin")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
@@ -83,4 +85,4 @@ vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/mgl2/config.h" "#define MG
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/mgl2/config.h" "#define MGL_FONT_PATH\t\"${CURRENT_PACKAGES_DIR}/fonts\"" "") # there is no fonts folder
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/mgl2/config.h" "#define MGL_FONT_PATH\t\"${CURRENT_PACKAGES_DIR}/share/mathgl/fonts\"" "")
 
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/mathgl" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
