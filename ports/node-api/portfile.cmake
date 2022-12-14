2485495a7c74db7048734363c6b1362a98bc2eaf
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2022-12-14 23:01:47 +0400

    better handle arch and os

--- ports/node-api/portfile.cmake
+++ ports/node-api/portfile.cmake
@@ -21,28 +21,38 @@ endif()
 
 set(NODEJS_VERSION 18.12.1)
 
-# prepare vars for dist download
-set(dist_urls)
-list(APPEND dist_urls "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-${nodejs_os}-${nodejs_arch}.zip")
-list(APPEND dist_urls "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-${nodejs_os}-${nodejs_arch}.tar.gz")
+set(SHA512 0)
 
-set(nodejs_sha512_darwin_arm64 8eb1713afdce23b0e8408d81aa47b3ea0955de328105d666a57efef8955b286c707c107377cff36164d8455558829ab65567b9cbe5997085afc841d95128fcd5)
-set(nodejs_sha512_darwin_x64 ffe7e878fd5e424b0ff0d2e7db5e9c02f283792df2f1a748bd6381226701bcd9f93ae48f88d295412afb10d1c924ca068f70aba9857236c8893a2b812eacf248)
-set(nodejs_sha512_linux_x64 f137a0dba52ded9f2f6b304c5f41fd2c75ba069ee31cfb89811b14254552c0d5ba10890f7001e64e8a6fee277302cb0ba915e0a417c047577384ac495c4ff447)
-set(nodejs_sha512_win_x64 0)
-set(nodejs_sha512_win_x86 0)
-set(nodejs_sha512_win_arm64 0)
-
-set(SHA512 "${nodejs_sha512_${nodejs_os}_${nodejs_arch}}")
-if ("${SHA512}" STREQUAL "")
-  message(FATAL_ERROR "No SHA512 specified for ${nodejs_os} ${nodejs_arch}")
+if(VCPKG_TARGET_IS_WINDOWS)
+  if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(SHA512 0)
+    set(DIST_URL "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-win-x64.zip")
+  elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(SHA512 0)
+    set(DIST_URL "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-win-x86.zip")
+  endif()
+elseif(VCPKG_TARGET_IS_OSX)
+  if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    set(SHA512 8eb1713afdce23b0e8408d81aa47b3ea0955de328105d666a57efef8955b286c707c107377cff36164d8455558829ab65567b9cbe5997085afc841d95128fcd5)
+    set(DIST_URL "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-darwin-arm64.tar.gz")
+  elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(SHA ffe7e878fd5e424b0ff0d2e7db5e9c02f283792df2f1a748bd6381226701bcd9f93ae48f88d295412afb10d1c924ca068f70aba9857236c8893a2b812eacf248)
+    set(DIST_URL "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-darwin-x64.tar.gz")
+  endif()
+elseif(VCPKG_TARGET_IS_LINUX)
+  if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(SHA f137a0dba52ded9f2f6b304c5f41fd2c75ba069ee31cfb89811b14254552c0d5ba10890f7001e64e8a6fee277302cb0ba915e0a417c047577384ac495c4ff447)
+    set(DIST_URL "https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.gz")
+  endif()
 endif()
+
+get_filename_component(DIST_FILENAME "${DIST_URL}" NAME)
 
 # download dist
 vcpkg_download_distfile(
   out_dist
-  URLS "${dist_urls}"
-  FILENAME "node-v${NODEJS_VERSION}-${nodejs_os}-${nodejs_arch}.tar.gz"
+  URLS "${DIST_URL}"
+  FILENAME "${DIST_FILENAME}"
   SHA512 "${SHA512}"
 )
 
