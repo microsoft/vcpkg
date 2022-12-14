507e0672abe3a70f3a350d9e1f3015c141525436
Jonliu1993 <13720414433@163.com>
2022-12-14 18:03:58 +0800

    remove patch

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -171,7 +171,6 @@ if("contrib" IN_LIST FEATURES)
       0013-fix-ceres.patch
       0014-fix-ogre.patch
       0016-fix-freetype-contrib.patch
-      0018-fix-depend-tesseract.patch
   )
   set(BUILD_WITH_CONTRIB_FLAG "-DOPENCV_EXTRA_MODULES_PATH=${CONTRIB_SOURCE_PATH}/modules")
 
