ffd7383a0489a0a67aaafc25844a067d6c8e94bc
Jonliu1993 <13720414433@163.com>
2022-12-15 14:48:46 +0800

    add space line

--- ports/amd-amf/portfile.cmake
+++ ports/amd-amf/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO GPUOpen-LibrariesAndSDKs/AMF
-        REF v1.4.26 # 1.4.26
+        REF v1.4.26
         SHA512 2c931ef7d38ade88a96041e0012605a5d25ec484bb2134f58310cf1a2c7212a869797cef99e2e751c8a6b3c473ba1f8762d4a5d41466d38cb5e31bf664a25f55
         HEAD_REF master
 )
