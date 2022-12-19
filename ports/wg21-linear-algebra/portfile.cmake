0457d56a72ffa5f9463b38f9161b62c8977317b1
Martin Stump <11492152+globberwops@users.noreply.github.com>
2022-12-19 17:54:56 +0100

    Replace deprecated functions

--- ports/wg21-linear-algebra/portfile.cmake
+++ ports/wg21-linear-algebra/portfile.cmake
@@ -1,23 +1,31 @@
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BobSteagall/wg21
-    REF v0.7.2
+    REF "v${VERSION}"
     SHA512 c249344d035d09760a9e5ea059ed6db5a1cb42b918735672bd7aa6dbda08f947855582f76ad61d33f59a847d8befe5caed57d25da2bcfc9fa8e6cef50a4c24e2
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
-    OPTIONS -DLA_INSTALL=1 -DLA_BUILD_PACKAGE=0 -DLA_ENABLE_TESTS=0
+    OPTIONS
+        -DLA_INSTALL=ON
+        -DLA_BUILD_PACKAGE=OFF
+        -DLA_ENABLE_TESTS=OFF
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME wg21_linear_algebra
-                         CONFIG_PATH lib/cmake/wg21_linear_algebra)
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME wg21_linear_algebra
+    CONFIG_PATH lib/cmake/wg21_linear_algebra
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cmake"
-                    "${CURRENT_PACKAGES_DIR}/debug"
-                    "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/cmake"
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
