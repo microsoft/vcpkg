50dd7fb5ba17dd2f9a20285769cfec59bb5da4f4
Matt Oliver <protogonoi@gmail.com>
2022-12-20 00:33:21 +1100

    [pangolin] Fixup port file.

--- ports/pangolin/portfile.cmake
+++ ports/pangolin/portfile.cmake
@@ -16,13 +16,9 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         test        BUILD_TESTS
         tools       BUILD_TOOLS
         examples    BUILD_EXAMPLES
-        gui         BUILD_PANGOLIN_GUI
-        vars        BUILD_PANGOLIN_VARS
-        video       BUILD_PANGOLIN_VIDEO
         pybind11    BUILD_PANGOLIN_PYTHON
-        eigen       BUILD_PANGOLIN_EIGEN
         ffmpeg      BUILD_PANGOLIN_FFMPEG
-        realsense   BUILD_PANGOLIN_LIBREALSENSE2
+        realsense   BUILD_PANGOLIN_REALSENSE2
         openni2     BUILD_PANGOLIN_OPENNI2
         uvc         BUILD_PANGOLIN_LIBUVC
         png         BUILD_PANGOLIN_LIBPNG
@@ -31,7 +27,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         openexr     BUILD_PANGOLIN_LIBOPENEXR
         zstd        BUILD_PANGOLIN_ZSTD
         lz4         BUILD_PANGOLIN_LZ4
-        module      BUILD_PYPANGOLIN_MODULE
 )
 
 file(REMOVE "${SOURCE_PATH}/CMakeModules/FindGLEW.cmake")
@@ -42,21 +37,16 @@ string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" MSVC_USE_STATIC_CRT)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS ${FEATURE_OPTIONS}
-        -DBUILD_EXTERN_GLEW=OFF
-        -DBUILD_EXTERN_LIBPNG=OFF
-        -DBUILD_EXTERN_LIBJPEG=OFF
         -DBUILD_PANGOLIN_PLEORA=OFF
         -DBUILD_PANGOLIN_TELICAM=OFF
         -DBUILD_PANGOLIN_DEPTHSENSE=OFF
         -DBUILD_PANGOLIN_OPENNI=OFF
         -DBUILD_PANGOLIN_UVC_MEDIAFOUNDATION=OFF
-        -DBUILD_PANGOLIN_LIBREALSENSE=OFF
+        -DBUILD_PANGOLIN_REALSENSE=OFF
         -DBUILD_PANGOLIN_V4L=OFF
         -DBUILD_PANGOLIN_LIBDC1394=OFF
-        -DBUILD_PANGOLIN_TOON=OFF
-        -DDISPLAY_WAYLAND=OFF
-        -DDISPLAY_X11=OFF
         -DBUILD_FOR_GLES_2=OFF
+        -DBUILD_PANGOLIN_LIBRAW=OFF
         -DMSVC_USE_STATIC_CRT=${MSVC_USE_STATIC_CRT}
     MAYBE_UNUSED_VARIABLES
         MSVC_USE_STATIC_CRT
@@ -78,11 +68,6 @@ endif()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    # Copy missing header file
-    file(COPY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/src/include/pangolin/pangolin_export.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include/pangolin")
 endif()
 
 # Put the license file where vcpkg expects it
