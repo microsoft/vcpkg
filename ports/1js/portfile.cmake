d4c4b965b34879e69faefca9e9acb4220f9c8827
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2022-12-21 01:45:02 +0400

    [1js] Add new port

---
+++ ports/1js/portfile.cmake
@@ -0,0 +1,10 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO faburaya/3fd
+    REF de0cd797248df7ae3f252e4cf703401d5609feff
+    SHA512 0
+    HEAD_REF main
+)
+
+file(INSTALL ${SOURCE_PATH}/1js/JsEngine.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/1js/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/1js RENAME copyright)
