825d8f440152e017d57df25a101071190f83a7cd
Matt Oliver <protogonoi@gmail.com>
2022-12-21 00:01:31 +1100

    [ignition-math6] Update to 6.13.0.

--- ports/ignition-math6/portfile.cmake
+++ ports/ignition-math6/portfile.cmake
@@ -1,4 +1,4 @@
 ignition_modular_library(NAME math
-                         VERSION "6.6.0"
-                         SHA512 6c0a6e7098f31b3dc9abbcd8714808669eca10e385748d4c1b44aa06dcfa5701906bea0277a99c3132fdd1a8c9a8e9c593099ac0eedfe5dec370018b2b63bfec
-                         PATCHES fix-isspace.patch)
+                         VERSION "6.13.0"
+                         SHA512 a596eba05c1e1aaa603ef9c64460399db603d5f2b62354388e6bf12748bd863d1241d6d8040afe83fcee2bd764e27d09b4a7e5544fbb73eb728db921ed6fd05a
+                         )
