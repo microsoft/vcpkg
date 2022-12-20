d510b698e5b329d67df53f372b3238a51add5243
Reito <cnschwarzer@qq.com>
2022-12-20 12:23:19 +0800

    fix msvc x64

--- ports/libyuv/portfile.cmake
+++ ports/libyuv/portfile.cmake
@@ -1,21 +1,58 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_git(
-    OUT_SOURCE_PATH SOURCE_PATH
-    URL https://chromium.googlesource.com/libyuv/libyuv
-    REF 287158925b0e03ea4499a18b4e08478c5781541b #2021-4-15
-    PATCHES
-        fix_cmakelists.patch
-        fix-build-type.patch
-        deprecated-warning.patch
+        OUT_SOURCE_PATH SOURCE_PATH
+        URL https://chromium.googlesource.com/libyuv/libyuv
+        REF 010dea8ba4158896e5608a52dd4372ca7f57cdca #2023-01-10
+        PATCHES
+        fix-cmakelists.patch
 )
 
-set(POSTFIX d)
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS_DEBUG
-        -DCMAKE_DEBUG_POSTFIX=${POSTFIX}
-)
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+if (VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW AND VCPKG_DETECTED_CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    message(STATUS "Set compiler to clang-cl on windows and MSVC target")
+    vcpkg_find_acquire_program(CLANG)
+    if (CLANG MATCHES "-NOTFOUND")
+        message(FATAL_ERROR "Clang is required.")
+    endif ()
+    get_filename_component(CLANG "${CLANG}" DIRECTORY)
+
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+        set(CLANG_TRIPLE "arm")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        set(CLANG_TRIPLE "aarch64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(CLANG_TRIPLE "i686")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(CLANG_TRIPLE "x86_64")
+    else()
+        message(FATAL_ERROR "Unsupported target architecture")
+    endif()
+
+    set(CLANG_TRIPLE "${CLANG_TRIPLE}-pc-windows-msvc")
+
+    message(STATUS "Using clang triple ${CLANG_TRIPLE}")
+    string(APPEND VCPKG_DETECTED_CMAKE_CXX_FLAGS --target=${CLANG_TRIPLE})
+    string(APPEND VCPKG_DETECTED_CMAKE_C_FLAGS --target=${CLANG_TRIPLE})
+
+    vcpkg_cmake_configure(
+            SOURCE_PATH ${SOURCE_PATH}
+            OPTIONS
+            -DCMAKE_CXX_COMPILER=${CLANG}/clang-cl.exe
+            -DCMAKE_C_COMPILER=${CLANG}/clang-cl.exe
+            -DCMAKE_CXX_FLAGS=${VCPKG_DETECTED_CMAKE_CXX_FLAGS}
+            -DCMAKE_C_FLAGS=${VCPKG_DETECTED_CMAKE_C_FLAGS}
+            OPTIONS_DEBUG
+            -DCMAKE_DEBUG_POSTFIX=d
+    )
+else ()
+    vcpkg_cmake_configure(
+            SOURCE_PATH ${SOURCE_PATH}
+            OPTIONS_DEBUG
+            -DCMAKE_DEBUG_POSTFIX=d
+    )
+endif ()
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
@@ -25,8 +62,5 @@ vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/libyuv)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
-vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/libyuv/convert.h "#ifdef HAVE_JPEG" "#if 1")
-vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/include/libyuv/convert_argb.h "#ifdef HAVE_JPEG" "#if 1")
-
-configure_file(${CMAKE_CURRENT_LIST_DIR}/libyuv-config.cmake  ${CURRENT_PACKAGES_DIR}/share/${PORT} COPYONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/libyuv-config.cmake ${CURRENT_PACKAGES_DIR}/share/${PORT} COPYONLY)
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
