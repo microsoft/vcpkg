c05ae03b8e55b4c685b8dfd939a57678cbdcabc8
Matt Oliver <protogonoi@gmail.com>
2022-12-22 00:16:39 +1100

    [ignition-math6] Change version to last known good 6.9.2.

--- ports/ignition-math6/portfile.cmake
+++ ports/ignition-math6/portfile.cmake
@@ -1,4 +1,4 @@
 ignition_modular_library(NAME math
-                         VERSION "6.13.0"
-                         SHA512 a596eba05c1e1aaa603ef9c64460399db603d5f2b62354388e6bf12748bd863d1241d6d8040afe83fcee2bd764e27d09b4a7e5544fbb73eb728db921ed6fd05a
+                         VERSION "6.9.2"
+                         SHA512 87fe43f745a2337e1b4c92abdb98180d55cf6034f9b85e93e42ab1c6e0af645912b9a246589115f919ca1fe7ecb38090fe76cdd0d6726315ffb0f090626c0634
                          )
