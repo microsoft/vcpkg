0800ac2c997141a11587678183e1889872d5a053
Joel Falcou <joel.falcou@lri.fr>
2022-12-23 10:58:41 +0100

    Add support for TTS
    Test library from https://jfalcou.github.io/tts/

---
+++ ports/tts/portfile.cmake
@@ -0,0 +1,13 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jfalcou/tts
+    REF 2.1
+    SHA512 d774073a91de26f7b0f2ceea25024ec2d8ca2cbbbdc1f82efa44df1ea11b8d9ae53ed4ceab974dcd0f50877a14fdbfb7172054646b24eb94952b4aa70de726f2
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_cmake_install()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/tts" RENAME copyright)
