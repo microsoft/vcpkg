1cd6f2fb4a1e818f63368f0a152881434b678644
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2022-12-26 22:31:52 +0100

    [tiny-process-library] Disable examples

--- ports/tiny-process-library/portfile.cmake
+++ ports/tiny-process-library/portfile.cmake
@@ -5,29 +5,27 @@ vcpkg_from_gitlab(
     REF v2.0.4
     SHA512 bbdd268361159b7c64cb60f29afa780ee5e57fa696f0683a55cb9824ec5985c8229a9a8217d2b9ecdd194b9a3acbbd75a1a821392361fbc85b1f6841f40c95db
     HEAD_REF master
+    PATCHES
+        disable-examples.patch
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON
 )
 
 vcpkg_install_cmake()
 
 vcpkg_copy_pdbs()
 
-vcpkg_fixup_cmake_targets(
-    CONFIG_PATH lib/cmake/${PORT}
-    TARGET_PATH share/${PORT}
+vcpkg_cmake_config_fixup(
+    CONFIG_PATH lib/cmake/tiny-process-library
 )
 
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
-    ${CURRENT_PACKAGES_DIR}/debug/share
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-# Handle copyright
-configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
