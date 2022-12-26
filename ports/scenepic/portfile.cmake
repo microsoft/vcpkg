5d2b847bdcf349e7892e5fe8bc631923c42cbe7d
jnyfah <jnyfaah@gmail.com>
2022-12-26 20:35:39 +0100

    [scenepic] new port

---
+++ ports/scenepic/portfile.cmake
@@ -0,0 +1,50 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO  microsoft/scenepic 
+    REF v1.0.16
+    SHA512 2d9dfcefa7a054cf0addb12113ab65cb7dd3a8a6f7b42f60558a5d47a6de45a9e801be3266b81358ff8ac075dd9e9e2b9369905d62f2383531d6e28e93406ac9
+    HEAD_REF main
+    PATCHES
+    "find_eigen_first.patch"
+)
+
+
+
+    # Run npm install and npm run build on the cloned project    
+execute_process(
+    COMMAND npm install
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+execute_process(
+    COMMAND npm run build
+    WORKING_DIRECTORY ${SOURCE_PATH}
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    )
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+  INSTALL "${SOURCE_PATH}/LICENSE"
+  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+  RENAME copyright)
