febf29e6e9ffc7216476c842af2465903fd24477
Kai Pastor <dg0yt@darc.de>
2022-12-28 08:18:29 +0100

    Fix typo

--- ports/freeglut/vcpkg-cmake-wrapper.cmake
+++ ports/freeglut/vcpkg-cmake-wrapper.cmake
@@ -23,7 +23,7 @@ if(GLUT_FOUND AND UNIX AND NOT ANDROID)
             set_property(TARGET GLUT::GLUT APPEND PROPERTY INTERFACE_LINK_LIBRARIES "${X11_Xrandr_LIB}")
         endif()
         # X11_xf86vmode_FOUND for CMake < 3.14
-        if((X11_xf86vm_FOUND OR X11_xf86vmode_FOUND) AND NOT "Xxf86vm" IN_LIST GLUT_LIBRARIES)
+        if((X11_Xxf86vm_FOUND OR X11_xf86vmode_FOUND) AND NOT "Xxf86vm" IN_LIST GLUT_LIBRARIES)
             list(APPEND GLUT_LIBRARIES "${X11_Xxf86vm_LIB}")
             set_property(TARGET GLUT::GLUT APPEND PROPERTY INTERFACE_LINK_LIBRARIES "${X11_Xxf86vm_LIB}")
         endif()
