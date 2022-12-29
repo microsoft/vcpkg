2e2a237b701e3284e5a9ad7d86b5c45538ec933e
Dennis Hezel <dh@3yourmind.com>
2022-12-29 19:28:33 +0100

    asio-grpc: Update to 2.4.0

--- ports/asio-grpc/portfile.cmake
+++ ports/asio-grpc/portfile.cmake
@@ -1,15 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Tradias/asio-grpc
-    REF v2.3.0
-    SHA512 c6ef02ff01e5fffdd8d1af99e39c632bc9ba76107f1f37232d521c2e5fd57fd32d820b9a5060fd8a71225201f3a27b2657e445e8434722d9728024000e4e694a
+    REF v2.4.0
+    SHA512 0aee11649d4df7c2f0964844b7f479eb6a1a8f51c1286345fdc14cba8581e951976cd48b1e00d1148b4fcf8519f578abb43038ba44996afd1de5195e24e22ee1
     HEAD_REF master
 )
+
+if ("boost-container" IN_LIST FEATURES AND "recycling-allocator" IN_LIST FEATURES)
+    message(WARNING "Features 'boost-container' and 'recycling-allocator' are mutually exclusive. Automatically disabling feature 'boost-container'.")
+    list(REMOVE_ITEM FEATURES "boost-container")
+endif()
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         boost-container ASIO_GRPC_USE_BOOST_CONTAINER
+        recycling-allocator ASIO_GRPC_USE_RECYCLING_ALLOCATOR
 )
 
 vcpkg_cmake_configure(
