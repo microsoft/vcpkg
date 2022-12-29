04fcd81d5adbf724b27e97a3bd10bba0808781ca
Pavel Kisliak <pavel_kisliak@epam.com>
2022-12-29 21:18:20 +0300

    Add copy 'usage' file

--- ports/bitserializer/portfile.cmake
+++ ports/bitserializer/portfile.cmake
@@ -36,3 +36,4 @@ endif()
 
 # Handle copyright
 file(INSTALL "${SOURCE_PATH}/license.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
