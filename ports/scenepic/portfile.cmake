5938dd7d76560c4decee10dec1ab1c6f458779a0
Jennifer Chukwu <jnyfaah@gmail.com>
2023-01-01 15:08:04 +0000

    format

--- ports/scenepic/portfile.cmake
+++ ports/scenepic/portfile.cmake
@@ -21,7 +21,6 @@ execute_process(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS
         -DCMAKE_BUILD_TYPE=Release
         -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET}
