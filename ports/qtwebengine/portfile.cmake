1b28e3d5a00cd0c58031f9c52e3321f385905386
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-01-02 18:35:48 +0100

    patch qtwebengine

--- ports/qtwebengine/portfile.cmake
+++ ports/qtwebengine/portfile.cmake
@@ -1,7 +1,7 @@
 set(SCRIPT_PATH "${CURRENT_INSTALLED_DIR}/share/qtbase")
 include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
-set(${PORT}_PATCHES
+set(${PORT}_PATCHES 1ce0133.diff
     )
 
 set(TOOL_NAMES gn QtWebEngineProcess qwebengine_convert_dict)
