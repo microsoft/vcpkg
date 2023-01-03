305a9f65d13237c6125cecd2956e695f2a6f67c4
Jennifer Chukwu <jnyfaah@gmail.com>
2023-01-03 09:34:48 +0000

    resolve comments

--- ports/scenepic/portfile.cmake
+++ ports/scenepic/portfile.cmake
@@ -1,7 +1,9 @@
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  microsoft/scenepic 
-    REF v1.0.16
+    REF "v${VERSION}"
     SHA512 2d9dfcefa7a054cf0addb12113ab65cb7dd3a8a6f7b42f60558a5d47a6de45a9e801be3266b81358ff8ac075dd9e9e2b9369905d62f2383531d6e28e93406ac9
     HEAD_REF main
     PATCHES
@@ -32,17 +34,14 @@ vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake PACKAGE_NAME scenepic)
 
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(COPY ${CURRENT_PACKAGES_DIR}/build DESTINATION ${CURRENT_PACKAGES_DIR}/share)
 
-file(COPY  ${CURRENT_PACKAGES_DIR}/cmake/scenepicTargets.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(COPY  ${CURRENT_PACKAGES_DIR}/cmake/scenepicConfigVersion.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(COPY  ${CURRENT_PACKAGES_DIR}/cmake/scenepicTargets-release.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(COPY  ${CURRENT_PACKAGES_DIR}/cmake/scenepicConfig.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
-file(COPY  ${CURRENT_PACKAGES_DIR}/build DESTINATION ${CURRENT_PACKAGES_DIR}/share)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug
+                    ${CURRENT_PACKAGES_DIR}/README.md
+                    ${CURRENT_PACKAGES_DIR}/CHANGELOG.md
+                    ${CURRENT_PACKAGES_DIR}/cmake
+                    ${CURRENT_PACKAGES_DIR}/build)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/README.md")
-file(REMOVE "${CURRENT_PACKAGES_DIR}/CHANGELOG.md")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cmake")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/build")
