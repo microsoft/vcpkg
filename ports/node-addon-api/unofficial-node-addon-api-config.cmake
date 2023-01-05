9053782826825228badca053b3f6da5fe1f01876
Leonid Pospelov <pospelovlm@yandex.ru>
2023-01-05 12:29:59 +0600

    Update unofficial-node-addon-api-config.cmake

--- ports/node-addon-api/unofficial-node-addon-api-config.cmake
+++ ports/node-addon-api/unofficial-node-addon-api-config.cmake
@@ -1,11 +1,11 @@
 find_package(unofficial-node-api CONFIG REQUIRED)
 
-add_library(unofficial::node-addon-api::node-addon-api INTERFACE)
+add_library(unofficial::node-addon-api::node-addon-api IMPORTED INTERFACE)
 target_link_libraries(unofficial::node-addon-api::node-addon-api INTERFACE unofficial::node-api::node-api)
 
 find_path(node-addon-api_INCLUDE_DIR
   NAMES napi.h
-  PATHS "${CMAKE_CURRENT_LIST_DIR}/../../include/node-addon-api"
+  PATHS "${CMAKE_CURRENT_LIST_DIR}/../../include/"
   NO_DEFAULT_PATH
   REQUIRED)
 set_target_properties(unofficial::node-addon-api::node-addon-api PROPERTIES
