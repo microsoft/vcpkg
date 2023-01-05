fd4d2f1eb59b781b15f219962b404831b60bcf44
Andrew Kornilov <aw.kornilov@gmail.com>
2023-01-05 11:37:31 +0300

    Use VERSION
    Co-authored-by: Frank <65999885+FrankXie05@users.noreply.github.com>

--- ports/attr/portfile.cmake
+++ ports/attr/portfile.cmake
@@ -1,8 +1,8 @@
-set(ATTR_VERSION 2.5.1)
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://download.savannah.nongnu.org/releases/attr/attr-${ATTR_VERSION}.tar.xz"
-    FILENAME "attr-${ATTR_VERSION}.tar.xz"
+    URLS "http://download.savannah.nongnu.org/releases/attr/attr-${VERSION}.tar.xz"
+    FILENAME "attr-${VERSION}.tar.xz"
     SHA512 9e5555260189bb6ef2440c76700ebb813ff70582eb63d446823874977307d13dfa3a347dfae619f8866943dfa4b24ccf67dadd7e3ea2637239fdb219be5d2932
 )
 
