bb40553729d12c3a3ad85ac59fe179e4b2bdadd7
Andrew Robbins <andrew@robbinsa.me>
2023-01-04 20:34:21 -0500

    prepare for repo shift to upstream

--- ports/libleidenalg/portfile.cmake
+++ ports/libleidenalg/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO theAeon/libleidenalg
-    REF "v${VERSION}"
+    REPO vtraag/libleidenalg
+    REF "${VERSION}"
     SHA512 8ce68de2d16462dbeb8d6d0bf7740e20d7977da6da4dff953e60f9dfc5964149be0fb85026a49d0b41589af4ef3454c64253c6a6f6fdaa84e460b0756896fb42
-    HEAD_REF master
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
