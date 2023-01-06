433debd3153e1f1405b9a001e6522ca294e795e3
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-01-06 16:12:44 +0100

    qtspeech prepare for features if required ports get added

--- ports/qtspeech/portfile.cmake
+++ ports/qtspeech/portfile.cmake
@@ -4,50 +4,25 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 set(${PORT}_PATCHES)
 set(TOOL_NAMES)
 
-#  -flite ............... Enable Flite support [auto] (Unix only)
-#  -flite-alsa .......... Enable Flite with ALSA support [auto] (Unix only)
-#  -speechd ............. Enable speech dispatcher support [auto] (Unix only)
+# Note: none of these features are implemented in the manifest yet
+# flite -> Missing port for flite
+# speechd -> missing port for speechd
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    "flite"         FEATURE_flite
+    "flite-alsa"    FEATURE_flite-alsa
+    "speechd"       FEATURE_speechd
+INVERTED_FEATURES
+    "flite"         CMAKE_DISABLE_FIND_PACKAGE_Flite
+    "flite-alsa"    CMAKE_DISABLE_FIND_PACKAGE_Alsa
+    "speechd"       CMAKE_DISABLE_FIND_PACKAGE_SpeechDispatcher
+)
 
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      TOOL_NAMES ${TOOL_NAMES}
+                     CONFIGURE_OPTIONS
+                        ${FEATURE_OPTIONS}
                      CONFIGURE_OPTIONS_MAYBE_UNUSED
                          QT_BUILD_EXAMPLES
                          QT_USE_DEFAULT_CMAKE_OPTIMIZATION_FLAGS
                     )
-
-
-# qt_find_package(Flite PROVIDED_TARGETS Flite::Flite MODULE_NAME texttospeech QMAKE_LIB flite)
-# qt_find_package(ALSA PROVIDED_TARGETS ALSA::ALSA MODULE_NAME texttospeech QMAKE_LIB flite_alsa)
-# qt_find_package(SpeechDispatcher PROVIDED_TARGETS SpeechDispatcher::SpeechDispatcher MODULE_NAME texttospeech QMAKE_LIB speechd)
-
-
-# #### Tests
-
-
-
-# #### Features
-
-# if (Flite_FOUND AND NOT TARGET Qt::Multimedia)
-    # message(WARNING
-            # "Flite was found, but Qt::Multimedia is not configured.\n"
-            # "The Flite engine will be disabled.")
-# endif()
-
-# qt_feature("flite" PRIVATE
-    # LABEL "Flite"
-    # CONDITION Flite_FOUND AND TARGET Qt::Multimedia
-# )
-# qt_feature("flite_alsa" PRIVATE
-    # LABEL "Flite with ALSA"
-    # CONDITION Flite_FOUND AND ALSA_FOUND AND TARGET Qt::Multimedia
-# )
-# qt_feature("speechd" PUBLIC
-    # LABEL "Speech Dispatcher"
-    # AUTODETECT UNIX
-    # CONDITION SpeechDispatcher_FOUND
-# )
-# qt_configure_add_summary_section(NAME "Qt TextToSpeech")
-# qt_configure_add_summary_entry(ARGS "flite")
-# qt_configure_add_summary_entry(ARGS "flite_alsa")
-# qt_configure_add_summary_entry(ARGS "speechd")
-# qt_configure_end_summary_section() # end of "Qt TextToSpeech" section
