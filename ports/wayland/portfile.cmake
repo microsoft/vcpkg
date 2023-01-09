8a414e2e3b6311366e1328313f693b0d48c18323
Victor Romero <romerosanchezv@gmail.com>
2023-01-09 20:02:59 +0000

    [wayland-protocols] Add force-build feature

--- ports/wayland/portfile.cmake
+++ ports/wayland/portfile.cmake
@@ -1,12 +1,13 @@
-if(NOT X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
-    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES")
-    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-else()
-
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         "force-build" FORCE_BUILD
 )
+
+if(NOT X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS AND NOT FORCE_BUILD)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+else()
+
 
 if (NOT FORCE_BUILD OR NOT X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES)
     message(FATAL_ERROR "To build wayland libraries the `force-build` feature must be enabled and the X_VCPKG_FORCE_VCPKG_WAYLAND_LIBRARIES triplet variable must be set.")
