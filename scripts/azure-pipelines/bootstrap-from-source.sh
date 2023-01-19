a0bb7983a22031e1ca12049cf4e46388a7b67890
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-01-19 16:08:17 +0100

    Trigger Build

--- scripts/azure-pipelines/bootstrap-from-source.sh
+++ scripts/azure-pipelines/bootstrap-from-source.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-git clone https://github.com/autoantwort/vcpkg-tool vcpkg-tool
+git clone https://github.com/microsoft/vcpkg-tool vcpkg-tool
 git -C vcpkg-tool switch -d $1
 rm -rf build.x64.release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DVCPKG_WARNINGS_AS_ERRORS=OFF -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_BUILD_TLS12_DOWNLOADER=OFF -B build.x64.release -S vcpkg-tool
