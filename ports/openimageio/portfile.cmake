2efc45360d24813f27a39fe4913c860f7981fca5
Alexander Neumann <neumann@imt.uni-luebeck.de>
2023-01-20 16:55:56 +0100

    make openimageio ignore qt5

--- ports/openimageio/portfile.cmake
+++ ports/openimageio/portfile.cmake
@@ -44,7 +44,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tools       OIIO_BUILD_TOOLS
         tools       USE_OPENGL
         tools       USE_QT
-        tools       USE_QT5
 )
 
 vcpkg_cmake_configure(
