71d728fec3fd7c438f5a5ec661971395c12aa8fe
Osyotr <Osyotr@users.noreply.github.com>
2023-01-20 14:26:03 +0300

    apply suggestions

--- ports/earcut-hpp/portfile.cmake
+++ ports/earcut-hpp/portfile.cmake
@@ -1,9 +1,7 @@
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mapbox/earcut.hpp
-    REF v${VERSION}
+    REF "v${VERSION}"
     SHA512 87f52bf99273dc47f78ebacd4ee0ccbab4edd3f9b85d97aed1c0d1165b3e2523e1a71f3a37a118e82170e79d57a2e09644d4115facb63dc6f704affb9c428e6b
     HEAD_REF master
 )
