f91dd1cea4d4b18e5ef544a70ff94dc28113681f
Yury Bura <yurybura@gmail.com>
2023-01-22 16:21:01 +0100

    [rocksdb] fix liburing option

--- ports/rocksdb/portfile.cmake
+++ ports/rocksdb/portfile.cmake
@@ -19,6 +19,7 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ROCKSDB_BUILD_SHARED)
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
     "jemalloc" WITH_JEMALLOC
+    "liburing" WITH_LIBURING
     "snappy" WITH_SNAPPY
     "lz4" WITH_LZ4
     "zlib" WITH_ZLIB
