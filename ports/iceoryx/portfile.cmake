d4c72e561a7918f466938ce3ef6f0c0d2f2793cb
Billy Robert O'Neal III <bion@microsoft.com>
2023-01-23 15:25:40 -0800

    Add usage text.

--- ports/iceoryx/portfile.cmake
+++ ports/iceoryx/portfile.cmake
@@ -39,3 +39,4 @@ if(TOML_CONFIG)
 endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
