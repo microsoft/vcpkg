e7bbb700820a66a73f2fcdb79419fcc6b5d47d41
Alexander Neumann <neumann@imt.uni-luebeck.de>
2023-01-24 11:57:28 +0100

    disable also second patch

--- ports/vtk/portfile.cmake
+++ ports/vtk/portfile.cmake
@@ -38,7 +38,7 @@ vcpkg_from_github(
         iotr.patch
         ${STRING_PATCH}
         #qt6.patch
-        reorder-includes.patch
+        #reorder-includes.patch
 )
 
 # =============================================================================
