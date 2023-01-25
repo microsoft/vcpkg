7292297f2f11d3d891a888d139f7539c01c38c78
Markus Vieth <mvieth@techfak.uni-bielefeld.de>
2023-01-25 21:12:34 +0100

    Add patch to remove problematic include

--- ports/pcl/portfile.cmake
+++ ports/pcl/portfile.cmake
@@ -13,6 +13,7 @@ vcpkg_from_github(
         install-examples.patch
         no-absolute.patch
         add_bigobj_option.patch
+        outofcore_viewer_remove_include.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PCL_SHARED_LIBS)
