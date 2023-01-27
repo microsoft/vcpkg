f17676fb1672923c9fc2b10066a0a447a7874d1e
Billy Robert O'Neal III <bion@microsoft.com>
2023-01-26 19:04:19 -0800

    Use a commit SHA rather than the ref.

--- ports/libleidenalg/portfile.cmake
+++ ports/libleidenalg/portfile.cmake
@@ -1,9 +1,7 @@
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO theAeon/libleidenalg
-    REF "${VERSION}"
+    REF 748dfca386b997fe5a140e21b9ef0f78a6e95ee6
     SHA512 7aeab3f870c5e0be8004c0daa2b48ca3ffd3589f9553b9599846ba788a425ee28b6accfa761960a97a280259d47c7be721cca0ae2ecc06c7262199cd6d30c880
     HEAD_REF main
 )
