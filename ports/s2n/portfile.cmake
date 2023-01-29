d8a4bc1613714aa89da5534852c877e54fcf5bce
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2023-01-29 16:46:21 -0300

    Revert S2N changes

--- ports/s2n/portfile.cmake
+++ ports/s2n/portfile.cmake
@@ -1,16 +1,17 @@
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO aws/s2n-tls
-    REF "v${VERSION}"
-    SHA512 cfdfcdaa9d57427211e896845358da85d7498fa0478b6916dd34f36c755100c0421c07ccd624ed2d4baf9fcfe0dde6629159769619ac22e9ef1535ba20bf8979
-    PATCHES
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO aws/s2n-tls
+        REF "v${VERSION}"
+        SHA512 cfdfcdaa9d57427211e896845358da85d7498fa0478b6916dd34f36c755100c0421c07ccd624ed2d4baf9fcfe0dde6629159769619ac22e9ef1535ba20bf8979
+        PATCHES
+        fix-cmake-target-path.patch
         remove-trycompile.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
+        FEATURES
         tests   BUILD_TESTING
-)
+        )
 
 set(EXTRA_ARGS)
 if(VCPKG_TARGET_ARCHITECTURE STREQUAL "wasm32")
@@ -18,27 +19,29 @@ if(VCPKG_TARGET_ARCHITECTURE STREQUAL "wasm32")
 endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
         ${EXTRA_ARGS}
         ${FEATURE_OPTIONS}
         -DUNSAFE_TREAT_WARNINGS_AS_ERRORS=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/s2n/cmake)
 
-string(REPLACE "dynamic" "shared" subdir "${VCPKG_LIBRARY_LINKAGE}")
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/${PORT}/cmake/${subdir}" DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/${PORT}/cmake")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/${PORT}-config.cmake" [[/${type}/]] "/")
+if(BUILD_TESTING)
+    message(STATUS Testing)
+    vcpkg_cmake_build(TARGET test LOGFILE_BASE test)
+endif()
 
 file(REMOVE_RECURSE
         "${CURRENT_PACKAGES_DIR}/debug/include"
-        "${CURRENT_PACKAGES_DIR}/debug/lib/${PORT}"
+        "${CURRENT_PACKAGES_DIR}/debug/lib/s2n"
         "${CURRENT_PACKAGES_DIR}/debug/share"
-        "${CURRENT_PACKAGES_DIR}/lib/${PORT}"
+        "${CURRENT_PACKAGES_DIR}/lib/s2n"
+        "${CURRENT_PACKAGES_DIR}/share/s2n/modules"
         )
 
-vcpkg_copy_pdbs()
-
+# Handle copyright
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
