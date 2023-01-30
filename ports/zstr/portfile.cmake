3a67afed9f4ea78555cd6d54bd4f8645edb5359e
chausner <15180557+chausner@users.noreply.github.com>
2023-01-30 19:25:21 +0100

    Apply suggestions from code review
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/zstr/portfile.cmake
+++ ports/zstr/portfile.cmake
@@ -1,7 +1,8 @@
+# header-only library
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO mateidavid/zstr
-  REF v1.0.7
+  REF "v${VERSION}"
   SHA512 3017da244810a45f7111f76f8d0dd988e162f08eab28b7465cad4549d84200fc834975275daf9588d35c6125e6f167c1e2dd5ec18022dac1eceabdc24d24cffe
   HEAD_REF master
 )
@@ -12,7 +13,7 @@ file(INSTALL "${SOURCE_PATH}/src/strict_fstream.hpp"
      DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 # Install license
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 # Install usage
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
