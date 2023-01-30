b45cd21eb448af1e320d6533407da55465981f6a
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-01-30 10:21:32 +0100

    Fix static builds and supports

--- ports/rkcommon/portfile.cmake
+++ ports/rkcommon/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF 0b8856cd9278474b56dc5bcff516a4b9482cf147
     SHA512 836e888e33406f6825b8f5570894402460b3ae65a68ca8aeecf2c8e712f70e9392fdbb2131d538dbf47fc48a0664568e1fd60968452c7517cfeb17c0e608fecf
     HEAD_REF master
+    PATCHES fix-static.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/cmake/rkcommon_redist_deps.cmake")
@@ -16,6 +17,12 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/rkcommon/common.h" "defined(rkcommon_SHARED)" "0")
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/rkcommon/common.h" "defined(rkcommon_SHARED)" "1")
+endif()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}-${VERSION}")
 
