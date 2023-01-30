db8e7f0ddd44ec2d87dbe003ab81acf5b3ce143a
Andrew Kornilov <aw.kornilov@gmail.com>
2023-01-31 00:43:30 +0300

    [nanosvg] Update to 2023-01-31

--- ports/nanosvg/portfile.cmake
+++ ports/nanosvg/portfile.cmake
@@ -1,15 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO memononen/nanosvg
-    REF 03042a6297399379198f98eb625ff8902bd84784
-    SHA512 163f205e81e830e8b2512ec1faa15ebaf82138fc8bd881ccfc5f19896df75e8cf77ccd20892fccd0fd3e5d6358438e6f3075fd4e6a4c4b064107451265c9f874
+    REF 9da543e8329fdd81b64eb48742d8ccb09377aed1
+    SHA512 c1f559bbf3bc5fa7606b653ec702f62589a321132e515695cfcd1a8ed50495a7ec1ff621a60788a3930cd5e16041b540c9b9481f9c476905757cbbf735edf8c8
     HEAD_REF master
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-
-set(VCPKG_BUILD_TYPE "release") # header-only
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME NanoSVG CONFIG_PATH lib/cmake/NanoSVG)
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
