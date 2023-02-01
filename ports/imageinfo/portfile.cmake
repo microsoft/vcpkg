6824d87fe93b4282ce58b079511bed754d275306
Jonliu1993 <13720414433@163.com>
2023-02-01 16:26:25 +0800

    Add usage

--- ports/imageinfo/portfile.cmake
+++ ports/imageinfo/portfile.cmake
@@ -21,3 +21,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/imageinfo)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
