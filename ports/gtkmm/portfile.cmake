c52e7eebc41e309a924a44b7ef4f1083d3b0fdab
FrankXie <v-frankxie@microsoft.com>
2023-02-01 18:39:54 -0800

    [gtkmm] update version to 4.9.2

--- ports/gtkmm/portfile.cmake
+++ ports/gtkmm/portfile.cmake
@@ -1,16 +1,16 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/gtkmm/4.6/gtkmm-4.6.0.tar.xz"
-    FILENAME "gtkmm-4.6.0.tar.xz"
-    SHA512 d1040be44d133cfa016efc581b79c5303806d0d441b57dcc22bd84a05c3e7934f9b7b894e71d7f4a0be332daba3dd58ef558f58070b83bf8a9de7d1027d92199
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/gtkmm/4.9/gtkmm-4.9.2.tar.xz"
+    FILENAME "gtkmm-4.9.2.tar.xz"
+    SHA512 b657558d86cb8ff1ab5e1d8ddd1f663c76a9b4383c8c4847253ddd8c0b751f7ca06cbfca554c5c9bb4dbb187f3b293830c882449eb1f3d26bdc1ecf662ef8e3a
 )
 
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
 )
 
 vcpkg_configure_meson(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -Dmsvc14x-parallel-installable=false # Use separate DLL and LIB filenames for Visual Studio 2017 and 2019
         -Dbuild-tests=false
