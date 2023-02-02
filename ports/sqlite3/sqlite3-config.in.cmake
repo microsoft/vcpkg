ee8c14c4a3743a4bdcb1c87c8aec1d39c4b7b1ae
Billy Robert O'Neal III <bion@microsoft.com>
2023-02-01 16:54:31 -0800

    Correctly note that zlib has no effect without tools.

--- ports/sqlite3/sqlite3-config.in.cmake
+++ ports/sqlite3/sqlite3-config.in.cmake
@@ -1,11 +1,7 @@
-include(CMakeFindDependencyMacro)
 
 if(NOT WIN32)
+    include(CMakeFindDependencyMacro)
     find_dependency(Threads)
-endif()
-
-if(@WITH_ZLIB@)
-    find_dependency(ZLIB CONFIG)
 endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/unofficial-sqlite3-targets.cmake)
