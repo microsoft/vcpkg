91d86bbaa2cff3381fcf708509ab2a5cc568891d
mdeetjen <marc.deetjen@gmail.com>
2023-02-03 13:08:50 -0500

    Updated to open3d repo

--- ports/open3d/portfile.cmake
+++ ports/open3d/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO polycam/open3d
-    REF 5f15943f8169dfee2d331a06ec19302373cb8a45
+    REF 00d552f22e77ce3f6a57f0eeb6cbae48e57f5066
     SHA512 dca93d6f9eb040895a5b1ea64ef915ead70b54b9e18f882295a21df55e99caeca2222d2eb1ba7bfa21f97b84c478f6ab9250e8e0c5c35d8d205bd34c8bdb43db
     HEAD_REF main
 )
