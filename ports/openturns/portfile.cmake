9daeeaa327f53a3e29d8e36c2a46c9cdef147eab
Billy Robert O'Neal III <bion@microsoft.com>
2023-02-03 15:41:02 -0800

    Add usage text.

--- ports/openturns/portfile.cmake
+++ ports/openturns/portfile.cmake
@@ -59,3 +59,4 @@ file(REMOVE "${CURRENT_PACKAGES_DIR}/include/pthread.h"
             "${CURRENT_PACKAGES_DIR}/include/unistd.h")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
