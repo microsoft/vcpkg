58386a8d73efcac58769ecc5eec4e1b7f801b071
Jose Sanchez <108306325+JoseSanchez7@users.noreply.github.com>
2023-02-03 00:15:27 -0800

    Change function calls

--- ports/umock-c/portfile.cmake
+++ ports/umock-c/portfile.cmake
@@ -8,18 +8,17 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
         -Drun_unittests=OFF
         -Drun_int_tests=OFF
         -Duse_installed_dependencies=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/umock_c)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake TARGET_PATH share/umock_c)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
 
