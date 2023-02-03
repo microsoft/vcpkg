dc774cfd6c815029f9132aa055f833314297fd3b
Kai Pastor <dg0yt@darc.de>
2023-02-03 22:01:05 +0100

    More

--- ports/mchehab-zbar/portfile.cmake
+++ ports/mchehab-zbar/portfile.cmake
@@ -30,8 +30,12 @@ else()
         DESTINATION "${SOURCE_PATH}/gettext-autoconf"
         PATTERNS "*/gettext-runtime/m4/gettext.m4"
                  "*/gettext-runtime/m4/iconv.m4"
+                 "*/gettext-runtime/m4/intlmacosx.m4"
                  "*/gettext-runtime/m4/nls.m4"
                  "*/gettext-runtime/m4/po.m4"
+                 "*/gettext-runtime/m4/progtest.m4"
+                 "*/gettext-runtime/gnulib-m4/host-cpu-c-abi.m4"
+                 "*/gettext-runtime/gnulib-m4/lib-ld.m4"
                  "*/gettext-runtime/gnulib-m4/lib-link.m4"
                  "*/gettext-runtime/gnulib-m4/lib-prefix.m4"
     )
