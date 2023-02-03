b14a063ee2620c7d1fd12187e053e7ec6f084373
Kai Pastor <dg0yt@darc.de>
2023-02-03 21:07:47 +0100

    Another m4 file

--- ports/mchehab-zbar/portfile.cmake
+++ ports/mchehab-zbar/portfile.cmake
@@ -17,7 +17,7 @@ if("nls" IN_LIST FEATURES)
 else()
     set(ENV{AUTOPOINT} true) # true, the program
     file(TOUCH "${SOURCE_PATH}/po/Makefile.in.in")
-    # Get build-time m4 files from gettext
+    # Get missing build-time m4 files from gettext source
     set(gettext_version 0.21.1)
     vcpkg_download_distfile(gettext_archive
         URLS "https://ftp.gnu.org/pub/gnu/gettext/gettext-${gettext_version}.tar.gz"
@@ -26,12 +26,13 @@ else()
         SHA512 ccd43a43fab3c90ed99b3e27628c9aeb7186398153b137a4997f8c7ddfd9729b0ba9d15348567e5206af50ac027673d2b8a3415bb3fc65f87ad778f85dc03a05
     )
     file(ARCHIVE_EXTRACT INPUT "${gettext_archive}"
-        DESTINATION "${SOURCE_PATH}/gettext-for-m4"
-        PATTERNS "*/gettext-runtime/m4/gettext.m4" "*/gettext-runtime/m4/iconv.m4"
+        DESTINATION "${SOURCE_PATH}/gettext-autoconf"
+        PATTERNS "*/gettext-runtime/m4/gettext.m4"
+                 "*/gettext-runtime/m4/iconv.m4"
+                 "*/gettext-runtime/gnulib-m4/lib-prefix.m4"
     )
-    file(GLOB_RECURSE m4_files "${SOURCE_PATH}/gettext-for-m4/*/*.m4")
+    file(GLOB_RECURSE m4_files "${SOURCE_PATH}/gettext-autoconf/*/*.m4")
     file(INSTALL ${m4_files} DESTINATION "${SOURCE_PATH}/config")
-    vcpkg_list(APPEND options "--disable-nls")
 endif()
 
 vcpkg_configure_make(
