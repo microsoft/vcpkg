c6c38c1bc8829d1d48b1fcfc08487d61141bf88f
as-shura <as-shura@web.de>
2023-02-04 13:34:44 +0100

    Fixed few things.

--- ports/rtmfp-cpp/portfile.cmake
+++ ports/rtmfp-cpp/portfile.cmake
@@ -12,7 +12,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_cmake_install()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/rtmfp)
+vcpkg_cmake_config_fixup (CONFIG_PATH lib/cmake/rtmfp)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
