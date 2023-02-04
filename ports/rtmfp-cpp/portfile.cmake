52c2c74b93d4ddac0b96f67143d64ff344e5367f
as-shura <as-shura@web.de>
2023-02-04 03:37:15 +0100

    Added rtmfp-cpp library.

---
+++ ports/rtmfp-cpp/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO zenomt/rtmfp-cpp
+    REF de55724f2894a4548a0dc3da29fcdf45d911f26a
+    SHA512 b928f39fe7fa38948acf267127c292aaa053c66e61b5a6ba239f8594a682344f760425d1b1c6adfe0af7de905d82836d35cc1c4ddb1396386408d72badac359b
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_cmake_install()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/rtmfp)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# Copyright and license
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
