1d1a014761aafe9b96a12c72b5f867b4bf00e96d
xiaozhuai <798047000@qq.com>
2023-02-06 11:42:19 +0800

    [libplist] update to 2.2.0 latest ref

--- ports/libplist/portfile.cmake
+++ ports/libplist/portfile.cmake
@@ -1,46 +1,25 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO libimobiledevice-win32/libplist
-    REF bbba7cabb78aad180a7a982ada5e1f21ff0ba873 # v1.3.6
-    SHA512 4cd59ed87c647259d0da99a20a05e01aa880f01f6b5cecd29e4247029a3d29f0f68b4552571eb3fd3c5549b4cb357801ffe43338b8ff34d44d6be5393d2e6b9d
-    HEAD_REF msvc-master
+    REPO libimobiledevice/libplist
+    REF 52826a6c229ed3e353d4dae711a6c52a96d99764 # v2.2.0-20230205
+    SHA512 3eb3012c544c613ce0e2e672d0adba6c4a475a07c62bdc35581e20b3e2f162307e45c460ac76166c135e3d0c5c3ea40aac3ef3497c6816736381fe09547abe4f
+    HEAD_REF master
     PATCHES
-        dllexport.patch
-        fix_static_build.patch
+        fix_windows_build.patch
 )
 
-configure_file("${CURRENT_PORT_DIR}/CMakeLists.txt" "${SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-
-set(pcfile "libplist-2.0.pc")
-set(pcfiletarget "libplist.pc")
-set(basepath "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/")
-if(EXISTS "${basepath}${pcfile}")
-    file(CREATE_LINK "${basepath}${pcfile}" "${basepath}${pcfiletarget}" COPY_ON_ERROR)
-endif()
-set(basepath "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/")
-if(EXISTS "${basepath}${pcfile}")
-    file(CREATE_LINK "${basepath}${pcfile}" "${basepath}${pcfiletarget}" COPY_ON_ERROR)
-endif()
-
-set(pcfile "libplist++-2.0.pc")
-set(pcfiletarget "libplist++.pc")
-set(basepath "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/")
-if(EXISTS "${basepath}${pcfile}")
-    file(CREATE_LINK "${basepath}${pcfile}" "${basepath}${pcfiletarget}" COPY_ON_ERROR)
-endif()
-set(basepath "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/")
-if(EXISTS "${basepath}${pcfile}")
-    file(CREATE_LINK "${basepath}${pcfile}" "${basepath}${pcfiletarget}" COPY_ON_ERROR)
-endif()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
