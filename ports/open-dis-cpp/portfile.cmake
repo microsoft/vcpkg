be07fd8cc4022e97bb552af14c4461a6a17226e5
Wade Hunkapiller <wade.hunk@gmail.com>
2023-02-06 10:30:44 -0600

    Update sha512

--- ports/open-dis-cpp/portfile.cmake
+++ ports/open-dis-cpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wadehunk/open-dis-cpp
     REF 6145d31a11ad6effc215a517f0aa19ef7128828d
-    SHA512 0
+    SHA512 61676d3dc459afe240ece6a47e848337bc332e945f961a6e64ad69446a9120feccd3ec0ba11ef4368908d72daa6a7f202e604e9314b3c91db28240da2d7eed1e
     HEAD_REF master
 )
 
