de21893609cd79458bf6f911bff79024d7190610
xiaozhuai <798047000@qq.com>
2023-02-06 14:58:49 +0800

    [libimobiledevice] using original upstream

--- ports/libimobiledevice/portfile.cmake
+++ ports/libimobiledevice/portfile.cmake
@@ -1,23 +1,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO libimobiledevice-win32/libimobiledevice
-    REF 0d4a7e905baeadafa098e629a5241fac6fbf7d24 # v1.3.17
-    SHA512 db6369b2fa8e7b659948602ac8c4cd568bf37c0f73e58150f61f1af7000de95aa1a6a9ae546d6a37de4cb08a8059127fe5ed067351c4092c01a45350586f755a
-    HEAD_REF msvc-master
+    REPO libimobiledevice/libimobiledevice
+    REF 9a5e78fe56267d6c721c92055c3ae9702bd25871 # v1.3.0-20230111
+    SHA512 17192ef78d144f6d90ec970f48fec6ab87c290a709b18dc0cf64d5360a15e3ee6db113c288c3ad83993cd8f433060f8383ee5a9ef7758a1727dd8e0d2d8b0a9e
+    HEAD_REF master
 )
 
-configure_file("${CURRENT_PORT_DIR}/CMakeLists.txt" "${SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
