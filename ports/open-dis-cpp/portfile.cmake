d8aba7d143146f75e3a97ab81f6bbbd09da1864f
Wade Hunkapiller <wade.hunk@gmail.com>
2023-02-06 10:53:33 -0600

    Update usage

--- ports/open-dis-cpp/portfile.cmake
+++ ports/open-dis-cpp/portfile.cmake
@@ -18,5 +18,7 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME OpenDIS CONFIG_PATH lib/cmake/OpenDIS)
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
