119fd59740583fbc7676f8545a3e195b50efde14
Wade Hunkapiller <wade.hunk@gmail.com>
2023-02-06 10:03:01 -0600

    Tweak portfile for sha

--- ports/open-dis-cpp/portfile.cmake
+++ ports/open-dis-cpp/portfile.cmake
@@ -1,14 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-dis/open-dis-cpp
-    REF "master"
-    SHA512 fa62188f773ad044644a58caf1e25bef417dfdea47c9da8a2ea7f997154b4f3976019e32e73cc533696a3d4e45ec4a8402b6df140878dfa2ff078740d61b4b0f
+    REF a17466dabb2944ee608330d0c0ea8d69e3defde8
+    SHA512 0
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    PREFER_NINJA
     OPTIONS
         ${options}
 )
