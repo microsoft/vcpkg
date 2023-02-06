1228e8fdb7edb25ba60f3c3efe05818317d9be31
Wade Hunkapiller <wade.hunk@gmail.com>
2023-02-06 10:12:57 -0600

    Remove debug include files

--- ports/open-dis-cpp/portfile.cmake
+++ ports/open-dis-cpp/portfile.cmake
@@ -13,6 +13,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 vcpkg_cmake_config_fixup(PACKAGE_NAME OpenDIS CONFIG_PATH lib/cmake/OpenDIS)
 
 # Handle copyright
