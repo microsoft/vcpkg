c68c75dfc06ded4c9105e8f2a78273979409df74
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-02-07 13:33:55 -0500

    [sdl2-image] Fix SDL2_image's findwebp.

--- ports/sdl2-image/portfile.cmake
+++ ports/sdl2-image/portfile.cmake
@@ -5,8 +5,7 @@ vcpkg_from_github(
     REF release-${VERSION}
     SHA512 ffd0f838525e17a43d03ea5d4a094b0c79b2627bdbfb4589645abfb606c594773d60184de31bda45999421da75a69f1987ed696b255991212e498161102972b1
     HEAD_REF main
-    PATCHES
-        fix-pkgconfig.patch
+    PATCHES fix-pkgconfig.patch fix-findwebp.patch
 )
 
 vcpkg_check_features(
