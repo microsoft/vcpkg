ad033a779766d89075dce0565b26d67320e5905b
Wade Hunkapiller <wade.hunk@gmail.com>
2023-02-06 21:59:58 -0600

    Update to 1.0.1 release

--- ports/open-dis-cpp/portfile.cmake
+++ ports/open-dis-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wadehunk/open-dis-cpp
-    REF 6145d31a11ad6effc215a517f0aa19ef7128828d
-    SHA512 61676d3dc459afe240ece6a47e848337bc332e945f961a6e64ad69446a9120feccd3ec0ba11ef4368908d72daa6a7f202e604e9314b3c91db28240da2d7eed1e
+    REF "v${VERSION}"
+    SHA512 0
     HEAD_REF master
 )
 
