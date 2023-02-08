58b6f3a77f6785411cba590b72e0640c2b2ae94b
FrankXie <v-frankxie@microsoft.com>
2023-02-07 23:21:33 -0800

    Pay attention to the format

--- ports/superlu/portfile.cmake
+++ ports/superlu/portfile.cmake
@@ -9,20 +9,20 @@ vcpkg_from_github(
     SHA512 1461b52bc18a8b0345beb70fdd46e07df497a13be840bcc061158ea1d0e61c8745806d1ad21cb2723db80f5ed762c3741f9c0ded2b2013df46da0e8bb6b77b83
     HEAD_REF master
     PATCHES
-      remove-make.inc.patch
+        remove-make.inc.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-    -DXSDK_ENABLE_Fortran=OFF
-    -Denable_tests=OFF
-    -Denable_internal_blaslib=OFF
-    -Denable_doc=OFF
+        -DXSDK_ENABLE_Fortran=OFF
+        -Denable_tests=OFF
+        -Denable_internal_blaslib=OFF
+        -Denable_doc=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
