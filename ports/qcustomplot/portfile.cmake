e93c62f47f63f1dcaefa1f2114a2b2eca6d5bdaa
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-02-08 21:38:23 +0100

    [qcustomplot] update and move to qt6

--- ports/qcustomplot/portfile.cmake
+++ ports/qcustomplot/portfile.cmake
@@ -1,40 +1,59 @@
-set(QCP_VERSION 2.0.1)
+set(QCP_VERSION 2.1.0)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.qcustomplot.com/release/${QCP_VERSION}/QCustomPlot.tar.gz"
+    URLS "https://www.qcustomplot.com/release/${QCP_VERSION}fixed/QCustomPlot.tar.gz"
     FILENAME "QCustomPlot-${QCP_VERSION}.tar.gz"
-    SHA512 a15598718146ed3c6b5d38530a56661c16269e530fe0dedb71b4cb2722b5733a3b57689d668a75994b79c19c6e61dcc133dbcb9ed77b93a165f4ac826a5685b9
+    SHA512 35ea24529c5a2d984fa6335184a17e90309d8e58d7d75e2a8d0ccb5cb1d7ac7bd16e157d5f625736cfe37f2aa8bf38d698058a8a746781876fa051cea2ffc765
 )
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    REF ${QCP_VERSION}
+    ARCHIVE "${ARCHIVE}"
+    REF "${QCP_VERSION}"
+    PATCHES fix_qt6_build.patch
 )
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://www.qcustomplot.com/release/${QCP_VERSION}/QCustomPlot-sharedlib.tar.gz"
+    URLS "https://www.qcustomplot.com/release/${QCP_VERSION}fixed/QCustomPlot-sharedlib.tar.gz"
     FILENAME "QCustomPlot-sharedlib-${QCP_VERSION}.tar.gz"
-    SHA512 ce90540fca7226eac37746327e1939a9c7af38fc2595f385ed04d6d1f49560da08fb5fae15d1b9d22b6ba578583f70de8f89ef26796770d41bf599c1b15c535d
+    SHA512 5736bff85eab41d9505489c184f38aa5fd5fab84730349a73122fd6cb4f6c283b16425905522d6b37918d70cb2984636b9f64fc9ecf41aa586fe9f3580671314
 )
 vcpkg_extract_source_archive(SharedLib_SOURCE_PATH ARCHIVE "${ARCHIVE}")
-file(RENAME "${SharedLib_SOURCE_PATH}" "${SOURCE_PATH}/qcustomplot-sharedlib")
+file(RENAME "${SharedLib_SOURCE_PATH}" "${SOURCE_PATH}/qcustomplot-sharedlib/")
+set(pro_file "${SOURCE_PATH}/qcustomplot-sharedlib/sharedlib-compilation/sharedlib-compilation.pro")
+vcpkg_replace_string("${pro_file}" "CONFIG += debug_and_release build_all" "")
 
 
-vcpkg_configure_qmake(SOURCE_PATH
-    ${SOURCE_PATH}/qcustomplot-sharedlib/sharedlib-compilation/sharedlib-compilation.pro
-)
+file(READ "${pro_file}" pro_string)
+string(APPEND pro_string [[
+win32 {
+    dlltarget.path = $$[QT_INSTALL_BINS]
+    INSTALLS += dlltarget
+}
+target.path    = $$[QT_INSTALL_LIBS]
+!static: target.CONFIG = no_dll
+INSTALLS     += target
 
-vcpkg_install_qmake(
-    RELEASE_TARGETS release-all
-    DEBUG_TARGETS debug-all
-)
+headers.files += ../../qcustomplot.h
+headers.path = $$[QT_INSTALL_PREFIX]/include
+INSTALLS     += headers
+]])
+file(WRITE "${pro_file}" "${pro_string}")
 
-# Install header file
-file(INSTALL ${SOURCE_PATH}/qcustomplot.h
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-)
+vcpkg_qmake_configure(SOURCE_PATH
+                        "${SOURCE_PATH}/qcustomplot-sharedlib/sharedlib-compilation"
+                      QMAKE_OPTIONS
+                        "QT+=opengl"
+                        "CONFIG+=create_prl"
+                        "DEFINES+=QCUSTOMPLOT_USE_OPENGL"
+                        )
+vcpkg_qmake_install()
 
 vcpkg_copy_pdbs()
 
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/qcustomplot.h" "#ifdef QCUSTOMPLOT_USE_OPENGL" "#define QCUSTOMPLOT_USE_OPENGL\n#ifdef QCUSTOMPLOT_USE_OPENGL")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/qcustomplot.h" "#if defined(QT_STATIC_BUILD)" "#if 1")
+endif()
+
 # Handle copyright
-configure_file(${SOURCE_PATH}/GPL.txt ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+configure_file("${SOURCE_PATH}/GPL.txt" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" COPYONLY)
