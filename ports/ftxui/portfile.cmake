da7f721a1a97f00d55b14b77e9138c999b513086
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-02-08 11:01:11 +0100

    [ftxui] Backport clang ICE workaround

--- ports/ftxui/portfile.cmake
+++ ports/ftxui/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF c033ca61ae8fb264542d0326d1309e0a3bde945a # 3.0.0
     SHA512 ca1468f30f90c3a886fbb6dea113699623d601da10b39a6a33d89780cd825bec8deb431872a2515fce05c7a5d581d4b56860b19654df8cf90e389dfa964f013c
     HEAD_REF master
+    PATCHES
+        fix_clang_crash-094d8d9d.patch # Included in next release. Upstream PR https://github.com/ArthurSonzogni/FTXUI/pull/421
 )
 
 vcpkg_cmake_configure(
@@ -23,4 +25,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
