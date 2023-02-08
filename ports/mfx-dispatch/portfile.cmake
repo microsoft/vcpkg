d8fad31450fdd2db3ebec0796b6a409087743d01
reito <cnschwarzer@qq.com>
2023-02-08 14:45:44 +0800

    fixes

--- ports/mfx-dispatch/portfile.cmake
+++ ports/mfx-dispatch/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lu-zero/mfx_dispatch
-    REF 1.35.1
+    REF "${VERSION}"
     SHA512 12517338342d3e653043a57e290eb9cffd190aede0c3a3948956f1c7f12f0ea859361cf3e534ab066b96b1c211f68409c67ef21fd6d76b68cc31daef541941b0
     HEAD_REF master
     PATCHES
@@ -21,10 +21,10 @@ if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "Linux")
     )
     vcpkg_install_make()
 else()
-    vcpkg_configure_cmake(
+    vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
     )
-    vcpkg_install_cmake()
+    vcpkg_cmake_install()
 endif()
 
 vcpkg_copy_pdbs()
