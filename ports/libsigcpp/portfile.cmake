899f9d1c5e95a5b99d67256a51b6e23dcc0109b9
6ziv <34367212+6ziv@users.noreply.github.com>
2023-02-08 11:33:28 +0800

    update version files

--- ports/libsigcpp/portfile.cmake
+++ ports/libsigcpp/portfile.cmake
@@ -4,9 +4,10 @@ vcpkg_from_github(
     REF 3.2.0
     SHA512 95ab0acfc2c5a151335e73bdc9b0e058af67d9706d0697bfd938e38c51e853fdb29d7a26484f192abe150640c60d5e30075a23deaa043a8deed70616bc9f508a
     HEAD_REF master
-    PATCHES 
+    PATCHES
         disable_tests_enable_static_build.patch
         fix-shared-windows-build.patch
+		fix_include_path.patch
 )
 
 vcpkg_cmake_configure(
@@ -18,7 +19,7 @@ vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(PACKAGE_NAME sigc++-3 CONFIG_PATH lib/cmake/sigc++-3)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/sigc++-3.0/include/sigc++config.h" "ifdef BUILD_SHARED" "if 1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/sigc++config.h" "ifdef BUILD_SHARED" "if 1")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
