44fa053e831645e48dec6c1dc99069b77e4a3788
as-shura <as-shura@web.de>
2023-02-09 21:05:18 +0100

    Initial commit.

---
+++ ports/libresources/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO as-shura/libresources
+    REF 1a1189f6d4b7c2c0c8037d3c0e2456f1db3b80b7
+    SHA512 0
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(PACKAGE_NAME libmupdf CONFIG_PATH lib/cmake/libmupdf)
+
+vcpkg_copy_pdbs()
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+
+
