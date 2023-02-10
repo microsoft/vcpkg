5df57a9b9fd173a38eea5a0f1bc75553e224b055
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-02-10 14:40:02 +0600

    Update ports/collada-dom/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/collada-dom/portfile.cmake
+++ ports/collada-dom/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/collada_dom-2.5)
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/licenses/license_e.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
