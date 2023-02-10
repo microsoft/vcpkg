7614cf8e7af6240c26b865b2a595ce5dd2db264e
Basit Ayantunde <rlamarrr@gmail.com>
2023-02-10 17:51:29 +0000

    Update ports/stx/portfile.cmake
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>

--- ports/stx/portfile.cmake
+++ ports/stx/portfile.cmake
@@ -24,10 +24,6 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH share/stx)
 vcpkg_copy_pdbs()
 
-file(
-  INSTALL ${SOURCE_PATH}/LICENSE
-  DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
-  RENAME copyright)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
