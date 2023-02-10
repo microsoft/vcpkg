d99a00be051154d0084504b5c42e09ed23cc6855
FrankXie <v-frankxie@microsoft.com>
2023-02-09 22:25:39 -0800

    [ms-quic] update version to 2.1.7

--- ports/ms-quic/portfile.cmake
+++ ports/ms-quic/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH QUIC_SOURCE_PATH
     REPO microsoft/msquic
-    REF v1.2.0
-    SHA512 6f63d42d950cbba88764332b31818a8627e7d3ecf7393cdef77daedd35a7bb04ac39c642991afb7cca502a346999233023e3b36011916c67e348179838aa7042
+    REF v2.1.7
+    SHA512 23ef4cc3794180b06d0ed138d6e96e37ef5f15ea0ccbf405f95f13c9fbd5aedc8a9c5c403b38b2381801796c50907ee36ebed1161da687bacd82cdea6880475a
     HEAD_REF master
     PATCHES
         fix-warnings.patch  # Remove /WX, -Werror
@@ -19,9 +19,36 @@ vcpkg_from_github(
     SHA512 23510a11203b96476c194a1987c7d4e758375adef0f6dfe319cd8ec4b8dd9b12ea64c4099cf3ba35722b992dad75afb1cfc5126489a5fa59f5ee4d46bdfbeaf6
     HEAD_REF OpenSSL_1_1_1k+quic
 )
+
 file(REMOVE_RECURSE ${QUIC_SOURCE_PATH}/submodules)
 file(MAKE_DIRECTORY ${QUIC_SOURCE_PATH}/submodules)
 file(RENAME ${OPENSSL_SOURCE_PATH} ${QUIC_SOURCE_PATH}/submodules/openssl)
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    LIST(APPEND QUIC_TLS "schannel")
+else()
+    LIST(APPEND QUIC_TLS "openssl")
+endif()
+
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools QUIC_BUILD_TOOLS
+)
+
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${QUIC_SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DQUIC_SOURCE_LINK=OFF
+        -DQUIC_TLS=${QUIC_TLS}
+        -DQUIC_USE_SYSTEM_LIBCRYPTO=OFF
+        -DQUIC_BUILD_TEST=OFF
+        -DQUIC_BUILD_SHARED=ON
+        -DQUIC_UWP_BUILD=${VCPKG_TARGET_IS_UWP}
+)
 
 vcpkg_find_acquire_program(PERL)
 get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
@@ -39,31 +66,10 @@ endif()
      vcpkg_add_to_path(PREPEND ${NASM_EXE_PATH})
  endif()
 
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        tools QUIC_BUILD_TOOLS
-)
-
-string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
-
-vcpkg_cmake_configure(
-    SOURCE_PATH ${QUIC_SOURCE_PATH}
-    OPTIONS
-        ${FEATURE_OPTIONS}
-        -DQUIC_SOURCE_LINK=OFF
-        -DQUIC_TLS=openssl
-        -DQUIC_TLS_SECRETS_SUPPORT=ON
-        -DQUIC_USE_SYSTEM_LIBCRYPTO=OFF
-        -DQUIC_BUILD_PERF=OFF
-        -DQUIC_BUILD_TEST=OFF
-        -DQUIC_STATIC_LINK_CRT=${STATIC_CRT}
-        -DQUIC_UWP_BUILD=${VCPKG_TARGET_IS_UWP}
-)
-
-vcpkg_cmake_build(TARGET OpenSSL_Build) # separate build log for quictls/openssl
+#vcpkg_cmake_build(TARGET OpenSSL_Build) # separate build log for quictls/openssl
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(PACKAGE_NAME msquic CONFIG_PATH lib/cmake/msquic)
+vcpkg_cmake_config_fixup(PACKAGE_NAME msquic CONFIG_PATH "lib/cmake/msquic")
 
 if("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES quicattack quicinterop quicinteropserver quicipclient quicipserver
@@ -72,9 +78,9 @@ if("tools" IN_LIST FEATURES)
     )
 endif()
 
-file(INSTALL ${QUIC_SOURCE_PATH}/LICENSE
-     DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
-) 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share
-                    ${CURRENT_PACKAGES_DIR}/debug/include
+vcpkg_install_copyright(FILE_LIST "${QUIC_SOURCE_PATH}/LICENSE") 
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/debug/include"
 )
+
