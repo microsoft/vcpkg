601a2864301a9799198467fd1c6571ab424c6579
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-02-10 23:06:46 -0500

    [fluidsynth] Bump to version 2.3.1.
    - Remove `fix-dependencies.patch`, as it was fixed upstream.
    - Update `gentables.patch`.
    - Rework the logic for enabled features.
    - Stop setting CMake variables that were removed upstream.
    - Handle the new CMake config files.
    - Copy pbds.
    - Don't install manpages.
    - Install usage file.

--- ports/fluidsynth/portfile.cmake
+++ ports/fluidsynth/portfile.cmake
@@ -1,11 +1,10 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO FluidSynth/fluidsynth
-    REF v2.2.8
-    SHA512 8173f2d368a214cf1eb7faae2f6326db43fb094ec9c83e652f953290c3f29c34ebd0b92cbb439bea8d814d3a7e4f9dc0c18c648df1d414989d5d8b4700c79535
+    REF "v${VERSION}"
+    SHA512 1633294bf6c714361c381151b62d9dd2c8f388490153e7964bfa14fd647a681db9ebfe1de0a06279972d6c5b30377f67361feb4db186b1faa235600f0ae02b22
     HEAD_REF master
     PATCHES
-        fix-dependencies.patch
         gentables.patch
 )
 
@@ -16,27 +15,31 @@ vcpkg_check_features(
         sndfile     enable-libsndfile
 )
 
-set(feature_list dbus jack libinstpatch midishare opensles oboe oss sdl2 pulseaudio readline lash systemd dart)
-foreach(_feature IN LISTS feature_list)
-    list(APPEND FEATURE_OPTIONS -Denable-${_feature}:BOOL=OFF)
-endforeach()
-
 # enable platform-specific features, and force the build to fail if the
 # required libraries are not found
-list(APPEND FEATURE_OPTIONS -Denable-dsound=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-wasapi=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-waveout=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-winmidi=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_MMSYSTEM_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_DSOUND_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_WASAPI_HEADERS=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_OBJBASE_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-coreaudio=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -Denable-coremidi=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -DCOREAUDIO_FOUND=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -DCOREMIDI_FOUND=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -Denable-alsa=${VCPKG_TARGET_IS_LINUX})
-list(APPEND FEATURE_OPTIONS -DALSA_FOUND=${VCPKG_TARGET_IS_LINUX})
+set(WINDOWS_FEATURES "enable-dsound" "enable-wasapi" "enable-waveout" "enable-winmidi" "HAVE_MMSYSTEM_H" "HAVE_DSOUND_H" "HAVE_WASAPI_HEADERS" "HAVE_OBJBASE_H")
+set(MACOS_FEATURES "enable-coreaudio" "enable-coremidi" "COREAUDIO_FOUND" "COREMIDI_FOUND")
+set(LINUX_FEATURES "enable-alsa" "ALSA_FOUND")
+set(DISABLED_FEATURES "enable-dbus" "enable-pipewire" "enable-jack" "enable-libinstpatch" "enable-midishare" "enable-opensles" "enable-oboe" "enable-oss" "enable-sdl2" "enable-pulseaudio" "enable-readline" "enable-lash" "enable-systemd" "enable-dart" "enable-framework")
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(ENABLED_FEATURES "${WINDOWS_FEATURES}")
+    list(APPEND DISABLED_FEATURES "${MACOS_FEATURES}" "${LINUX_FEATURES}")
+elseif(VCPKG_TARGET_IS_OSX)
+    set(ENABLED_FEATURES "${MACOS_FEATURES}")
+    list(APPEND DISABLED_FEATURES "${WINDOWS_FEATURES}" "${LINUX_FEATURES}")
+elseif(VCPKG_TARGET_IS_LINUX)
+    set(ENABLED_FEATURES "${LINUX_FEATURES}")
+    list(APPEND DISABLED_FEATURES "${WINDOWS_FEATURES}" "${MACOS_FEATURES}")
+endif()
+
+foreach(FEATURE IN LISTS ENABLED_FEATURES)
+    list(APPEND FEATURE_OPTIONS "-D${FEATURE}:BOOL=ON")
+endforeach()
+
+foreach(FEATURE IN LISTS DISABLED_FEATURES)
+    list(APPEND FEATURE_OPTIONS "-D${FEATURE}:BOOL=OFF")
+endforeach()
 
 vcpkg_find_acquire_program(PKGCONFIG)
 vcpkg_cmake_configure(
@@ -45,33 +48,25 @@ vcpkg_cmake_configure(
         "-DVCPKG_HOST_TRIPLET=${HOST_TRIPLET}"
         ${FEATURE_OPTIONS}
         -DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}
-        -DLIB_INSTALL_DIR=lib
-        -Denable-pkgconfig=ON
-        -Denable-framework=OFF # Needs system permission to install framework
-    OPTIONS_DEBUG
-        -Denable-debug:BOOL=ON
     MAYBE_UNUSED_VARIABLES
-        enable-coreaudio
-        enable-coremidi
-        enable-dart
+        ${DISABLED_FEATURES}
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/fluidsynth")
 vcpkg_fixup_pkgconfig()
 
-set(tools fluidsynth)
+set(TOOLS fluidsynth)
 if("buildtools" IN_LIST FEATURES)
-    list(APPEND tools make_tables)
+    list(APPEND TOOLS make_tables)
 endif()
-vcpkg_copy_tools(TOOL_NAMES ${tools} AUTO_CLEAN)
+vcpkg_copy_tools(TOOL_NAMES ${TOOLS} AUTO_CLEAN)
+vcpkg_copy_pdbs()
 
-# Remove unnecessary files
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE 
+    "${CURRENT_PACKAGES_DIR}/debug/include" 
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/share/man")
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
