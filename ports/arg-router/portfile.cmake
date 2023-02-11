96a9f2e5f965901146d9844973719ba924127f33
Cam Mannett <camden.mannett@protonmail.ch>
2023-02-11 20:07:49 +0000

    [arg-router] Bump to 1.1.1
    Bug fixes:
    * Critical issue that prevents non-CMake builds from being built
    
    Improvements:
    * Removed the git patch version segment and therefore the requirement on git to build. This allows for building from source without a working copy

--- ports/arg-router/portfile.cmake
+++ ports/arg-router/portfile.cmake
@@ -1,31 +1,29 @@
-set(ARCHIVE_NAME "arg_router-${VERSION}.zip")
-
-vcpkg_download_distfile(
-    ARCHIVE
-    URLS "https://github.com/cmannett85/arg_router/releases/download/v${VERSION}/${ARCHIVE_NAME}"
-    FILENAME "${ARCHIVE_NAME}"
-    SHA512 9cb75dafbdcbc02c774d5dcf17af126b7b1fc032b10cc0a2b5075897fbb80cdb0b84b631d265295210c3a7bdae682065f417d8ca70937118de97f14ed46bd31b
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO cmannett85/arg_router
+    REF v1.1.1
+    HEAD_REF main
+    SHA512 2951a54b4fb13abd10d4de3711d4d92f180e582c21e9a0d3599cb327e799727e826ea87aecd0fd7a6203585eac5a934afe25f98488ef6b36c12be97450ab8020
 )
 
-vcpkg_extract_source_archive(
-    SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DINSTALLATION_ONLY=ON
 )
 
+vcpkg_cmake_install()
+vcpkg_install_copyright(
+    FILE_LIST "${SOURCE_PATH}/LICENSE"
+)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(
-    COPY "${SOURCE_PATH}/include/arg_router/arg_router-config.cmake"
-         "${SOURCE_PATH}/include/arg_router/arg_router-config-version.cmake"
+    COPY "${CURRENT_PACKAGES_DIR}/include/arg_router/arg_router-config.cmake"
+         "${CURRENT_PACKAGES_DIR}/include/arg_router/arg_router-config-version.cmake"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/arg_router"
-)
-file(
-    COPY "${SOURCE_PATH}/include"
-    DESTINATION "${CURRENT_PACKAGES_DIR}"
 )
 file(
     COPY "${CMAKE_CURRENT_LIST_DIR}/usage"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
-vcpkg_install_copyright(
-    FILE_LIST "${SOURCE_PATH}/include/arg_router/LICENSE"
-)
