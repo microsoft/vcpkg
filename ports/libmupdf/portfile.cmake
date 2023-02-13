79f51ff974c51b20bff1c748e638b808befda95d
as-shura <as-shura@web.de>
2023-02-13 10:54:25 +0100

    Version added.

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -1,16 +1,16 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+set(VERSION "1.21.1")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ArtifexSoftware/mupdf
-    REF f39eee48564274635e231e2ab652221cce2ec1bd # 1.21.1
+    REF ${VERSION}
     SHA512 aa16d0939739f96c66ee802032c321adcebc86c9fa5bfe7ec64f20614377a4f61c358270216b57a1e4b1929337c4cc856d9ec4666ed8f0edaeaa7f2336a84ac3
     HEAD_REF master
     PATCHES
         dont-generate-extract-3rd-party-things.patch
 )
-
-set(PACKAGE_VERSION "1.21.1")
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
@@ -21,7 +21,7 @@ vcpkg_cmake_configure(
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         -DBUILD_EXAMPLES=OFF
-        -DPACKAGE_VERSION=${PACKAGE_VERSION}
+        -DPACKAGE_VERSION=${VERSION}
 )
 
 vcpkg_cmake_install()
