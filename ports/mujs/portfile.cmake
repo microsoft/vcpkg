4e5e300ac7719b8209e6d68fcbb75eefa6013346
Kai Pastor <dg0yt@darc.de>
2023-02-13 19:18:28 +0100

    [mujs] Fix CMake config, cleanup

--- ports/mujs/portfile.cmake
+++ ports/mujs/portfile.cmake
@@ -9,18 +9,18 @@ vcpkg_from_github(
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/mujsConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    "-DPACKAGE_VERSION=${VERSION}"
   OPTIONS_DEBUG
     -DDISABLE_INSTALL_HEADERS=ON
-    -DDISABLE_INSTALL_TOOLS=ON
 )
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup(PACKAGE_NAME mujs CONFIG_PATH lib/cmake/mujs)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-mujs)
 vcpkg_copy_pdbs()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
