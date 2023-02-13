f75a73220c09cebcc072c1470c72d0a559f6a399
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-02-13 13:57:57 -0500

    [fluidsynth] Install usage.

--- ports/fluidsynth/portfile.cmake
+++ ports/fluidsynth/portfile.cmake
@@ -76,3 +76,5 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/share/man")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
