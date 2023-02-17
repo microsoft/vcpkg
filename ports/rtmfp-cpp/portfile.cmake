9c673e91e590faeaabad16d457c07a32200d4c31
as-shura <as-shura@web.de>
2023-02-17 15:30:35 +0100

    Changed version.

--- ports/rtmfp-cpp/portfile.cmake
+++ ports/rtmfp-cpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zenomt/rtmfp-cpp
-    REF 18168ec641df5bbe22a0fac84b0efc02a4bc0a67
+    REF "v${VERSION}"
     SHA512 eea3e4b52a4639dce3ff3f88011ae5b2f07dbeb7237f854b9b3f1a829c04af930c8edfa3a1a3cad95890a372fc6cc436b00c4dadf9b06fc404f50bdd36634a08
     HEAD_REF main
 )
