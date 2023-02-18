6b9ac36fcbcad8b2194277508ff2338b57055369
Billy Robert O'Neal III <bion@microsoft.com>
2023-02-17 22:57:10 -0800

    [vtk] Ensure vtk uses pegtl-2, as declared in its dependencies list, rather than pegtl, if that port happened to get installed first.

--- ports/vtk/portfile.cmake
+++ ports/vtk/portfile.cmake
@@ -115,8 +115,8 @@ endif()
 if("qt" IN_LIST FEATURES)
     file(READ "${CURRENT_INSTALLED_DIR}/share/qtbase/vcpkg_abi_info.txt" qtbase_abi_info)
     if(qtbase_abi_info MATCHES "(^|;)gles2(;|$)")
-        message(FATAL_ERROR "VTK assumes qt to be build with desktop opengl. As such trying to build vtk with qt using GLES will fail.") 
-        # This should really be a configure error but using this approach doesn't require patching. 
+        message(FATAL_ERROR "VTK assumes qt to be build with desktop opengl. As such trying to build vtk with qt using GLES will fail.")
+        # This should really be a configure error but using this approach doesn't require patching.
     endif()
 endif()
 
