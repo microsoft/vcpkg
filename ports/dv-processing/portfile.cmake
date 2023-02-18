90e75460d29f179438b40bfea07f3fc4ef02d463
Luca Longinotti <l@longi.li>
2023-02-18 18:51:58 +0100

    dv-processing: fix SHA checksums, fix build on Windows.

--- ports/dv-processing/portfile.cmake
+++ ports/dv-processing/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_gitlab(
         GITLAB_URL https://gitlab.com/inivation
         OUT_SOURCE_PATH SOURCE_PATH
         REPO dv/dv-processing
-        REF 15302dfdd6ad2e1d18c15a6589a830e4e3e45f97
-        SHA512 0
+        REF b55a2e7a01ef49a861ee151bd542d4b32edfde30
+        SHA512 7abf828e27af0b708c7fc3c6c78f00f9089d202a7e4a1d6c9a1f9416d2e9e394d470dc1b40ae2f491350b87f11cd17869b12ef10cd7c647e06627833f6d205f9
         HEAD_REF rel_1.7
 )
 
@@ -12,7 +12,7 @@ vcpkg_from_gitlab(
         OUT_SOURCE_PATH CMAKEMOD_SOURCE_PATH
         REPO dv/cmakemod
         REF ec53dae89f6b037e9e640af5340d7bf67d84d278
-        SHA512 0
+        SHA512 e7907b1be9d85b02e1a1703cf001765119a7d07b1873148a0fbfe6945c519d85b1f9bc66b24f90d88759c2b32965304e1639f2ff136448be64fc88f81a0d4c2d
         HEAD_REF ec53dae89f6b037e9e640af5340d7bf67d84d278
 )
 
