0fb57e5c1dff1a3e562d97ef4d2acd2d10a42452
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-02-20 03:09:32 +0600

    better license handling

--- ports/node-api/portfile.cmake
+++ ports/node-api/portfile.cmake
@@ -8,14 +8,6 @@ vcpkg_from_github(
 
 file(INSTALL "${SOURCE_PATH}/include" DESTINATION "${CURRENT_PACKAGES_DIR}/include" RENAME "node")
 
-# we do not take the license from the dist file because it is not included as we download the headers only
-set(license_url "https://raw.githubusercontent.com/nodejs/node/v18.12.1/LICENSE")
-vcpkg_download_distfile(
-  out_license
-  URLS "${license_url}"
-  FILENAME "LICENSE"
-  SHA512 2d79b49a12178a078cf1246ef7589d127189914403cd6f4dfe277ced2b3ef441a6e6ee131f1c75f996d1c1528b7e1ae332e83c1dc44580b2b51a933ed0c50c48
-)
-file(INSTALL "${out_license}" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/unofficial-node-api-config.cmake.in" "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}/unofficial-node-api-config.cmake" @ONLY)
