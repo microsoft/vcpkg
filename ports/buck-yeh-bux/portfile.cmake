daeb5bba1848e374fe323f60eb694c0b1932efd9
buck-yeh <buck.yeh@gmail.com>
2023-02-24 22:32:54 +0800

    Revert REF line & try add linux support

--- ports/buck-yeh-bux/portfile.cmake
+++ ports/buck-yeh-bux/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO buck-yeh/bux
-    REF "v${VERSION}"
+    REF 15b94d1549e8fa83173a4f0fe6a877de8ef552a4 # v1.6.8
     SHA512 e5dcdba0ee07d3cf63edb1e5808436d9b2de4d1e393a6e447cca82f68d1b67dc94350d55926aa90d00d6f9a032cc76446d83b206dbb906be10de9f1c15a71359
     HEAD_REF main
 )
