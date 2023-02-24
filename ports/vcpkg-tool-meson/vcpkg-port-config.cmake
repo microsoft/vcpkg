3239e103c1ac94b67e583629b4bf2842cd0e1298
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-02-24 09:53:59 +0100

    remove unnecessary variables

--- ports/vcpkg-tool-meson/vcpkg-port-config.cmake
+++ ports/vcpkg-tool-meson/vcpkg-port-config.cmake
@@ -23,19 +23,13 @@ set(program MESON)
 set(program_version 1.0.0)
 set(program_name meson)
 set(search_names meson meson.py)
-set(interpreter PYTHON3)
-set(apt_package_name "meson")
-set(brew_package_name "meson")
 set(ref 05f85c79454e206e00c6ced5b6fc25ffcafed39c)
 set(path_to_search "${DOWNLOADS}/tools/meson-${program_version}")
 set(download_urls "https://github.com/mesonbuild/meson/archive/${ref}.tar.gz")
 set(download_filename "meson-${ref}.tar.gz")
 set(download_sha512 b4744b9cc28d85d6c7df9199e74fb695930b3af5d065035c6728b5883b5df4faae9ce1dcc0461c3fc28c68d854c6daf4edef5e7e448ba2eabff9fe8e9e4650d9)
-set(supported_on_unix ON)
-set(version_command --version)
-set(extra_search_args EXACT_VERSION_MATCH)
 
-find_program(SCRIPT_MESON NAMES meson meson.py PATHS "${path_to_search}" NO_DEFAULT_PATH) # NO_DEFAULT_PATH due top patching
+find_program(SCRIPT_MESON NAMES ${search_names} PATHS "${path_to_search}" NO_DEFAULT_PATH) # NO_DEFAULT_PATH due top patching
 
 if(NOT SCRIPT_MESON)
     vcpkg_download_distfile(archive_path
