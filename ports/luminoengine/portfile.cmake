706d541dd8f5c63aedddedf7a153ad7bd9adb5e2
jimwang118 <wangzhijie05@beyondsoft.com>
2023-02-27 14:50:54 +0800

    [luminoengine] Fix cannot find luminoengine library

--- ports/luminoengine/portfile.cmake
+++ ports/luminoengine/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v0.10.1
     SHA512 f43e48b36a48b5fcce4767de087f9953c905ac0af5522042a93c39ec75e4c9489b8910bc5b2f6fd129ce197309377a14b6eb9177a6ea9db4f5c2e7d1b13a137d
     HEAD_REF main
+	PATCHES
+        fix-pkgconfig.patch	  
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -25,7 +27,7 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/lumino)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_copy_pdbs()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
