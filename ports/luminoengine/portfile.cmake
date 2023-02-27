a5450f0096e98ca7b044943353faa6575db7310a
jimwang118 <wangzhijie05@beyondsoft.com>
2023-02-27 18:24:19 +0800

    fix ci error

--- ports/luminoengine/portfile.cmake
+++ ports/luminoengine/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO LuminoEngine/Lumino
-    REF "v${VERSION"
+    REF "v${VERSION}"
     SHA512 f43e48b36a48b5fcce4767de087f9953c905ac0af5522042a93c39ec75e4c9489b8910bc5b2f6fd129ce197309377a14b6eb9177a6ea9db4f5c2e7d1b13a137d
     HEAD_REF main
     PATCHES
