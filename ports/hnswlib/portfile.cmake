66338bddfe7c6937ae6b4c1fc6350e66b5be8ce3
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-02-28 10:57:46 +0100

    remove cmake config

--- ports/hnswlib/portfile.cmake
+++ ports/hnswlib/portfile.cmake
@@ -7,7 +7,6 @@ vcpkg_from_github(
 )
 
 file(COPY "${SOURCE_PATH}/hnswlib" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-hnswlibConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
