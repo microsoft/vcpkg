f5f79ed208b4ae66f17bff54a6abeef8a9af9731
Julian Zimmermann <Julian.Zimmermann@gti.de>
2023-02-28 07:55:44 +0100

    remove explicit set of version variable

--- ports/open62541/portfile.cmake
+++ ports/open62541/portfile.cmake
@@ -1,9 +1,7 @@
-set(VERSION v1.3.5)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open62541/open62541
-    REF "${VERSION}"
+    REF "v${VERSION}"
     SHA512 bb45d288a097b461d2a7106153c7f4b4c38c73cf767fe15c6c9c2213a6e3fcaf9b436fb70c1e7c6dcbc8ef45f232a5bd2f140285fab486358cb5a3a17da96d6e
     HEAD_REF master
 )
