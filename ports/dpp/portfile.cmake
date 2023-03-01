d66525aec38803fea1648e7f02e7366922778ee5
Craig Edwards <braindigitalis@users.noreply.github.com>
2023-03-01 04:23:51 +0000

    suggested tweak, removes sha and adds version ref
    Co-authored-by: Mengna Li <95600143+Adela0814@users.noreply.github.com>

--- ports/dpp/portfile.cmake
+++ ports/dpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO brainboxdotcc/DPP
-    REF 9ef9176b2beb91342b46a4435b34f9a3be9818f0
+    REF "v${VERSION}"
     SHA512  09666b3e05c379aa2da794aed0a59ff5f6b2dae06c6e700b2bd45486aa20003b4e6d28bd9436d1e28bf7dd1b5ca80d55e5a107fe9589587b88719caa2cecb243
     HEAD_REF master
 )
