3a958f43622d743cb2131e2b13a8e0321579a8d7
Gianluca Bianco <gianluca.bianco3@studio.unibo.it>
2023-03-01 09:58:24 +0100

    Update

--- ports/arsenalgear/portfile.cmake
+++ ports/arsenalgear/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO JustWhit3/arsenalgear-cpp
-    REF "v${VERSION}"
+    REF v2.1.0
     SHA512 27f2979da2d75851d5f8a63868d49b2f5b82064477a1c816667aaf0283e87f854712ab4aa473a3ef36c6ff895bb781c3b095ff617bf134051531aee9b8f03fd3
     HEAD_REF main
 )
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 # Move cmake configs
-vcpkg_cmake_config_fixup(PACKAGE_NAME arsenalgear CONFIG_PATH lib/cmake/arsenalgear)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/arsenalgear)
 
 # Remove duplicate files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
