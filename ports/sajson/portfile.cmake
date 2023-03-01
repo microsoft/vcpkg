2146b1ad5ef9e0ca07ae5dddd9cb7d447bedaf39
jyu49 <yu_jack@apple.com>
2023-03-01 10:23:20 +0800

    fix build

--- ports/sajson/portfile.cmake
+++ ports/sajson/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-sajson)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-sajson CONFIG_PATH share/cmake/unofficial-sajson)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
