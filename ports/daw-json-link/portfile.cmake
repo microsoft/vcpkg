4f27b738610a9dd1465be816cda593feccbe4fec
jimwang118 <wangzhijie05@beyondsoft.com>
2023-03-03 08:58:33 +0000

    update version

--- ports/daw-json-link/portfile.cmake
+++ ports/daw-json-link/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO beached/daw_json_link
-    REF "v${VERSION}"  #v3.10.0
+    REF "v${VERSION}"
     SHA512 dc412cf07c1ef8cdb2b6267d6b630b6396541a49c44ee2d2b2b21f49c61cde387aff81548635c7c043a88aa90f3632aefe6bf0ec79453c5b58c80669e6a84cd2
     HEAD_REF master
 )
