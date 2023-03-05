6d7d95fa5ae4b99b22bcc56f54c3705e427f701f
stellarpower <stellarpower@googlemail.com>
2023-03-05 18:01:33 +0000

    Remove unneeded patch (effects are already upstream)

--- ports/yomm2/portfile.cmake
+++ ports/yomm2/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     REF v1.1.2
     SHA512  f45c3f3d267dedaa3c76f9dab1a75be01941e3715e71b30b878be49157a5ba97f2188c9e635272be3ca396019b161bb21a30199ca504c94a18673685f5dbf06d
     HEAD_REF master
-    PATCHES "fix_find_boost.patch" "fix_uwp_osx.patch"
+    PATCHES "fix_find_boost.patch"
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
