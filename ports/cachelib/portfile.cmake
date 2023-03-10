6e7cb03d1a4979c2f487704381fec6a81f474f56
gavin <380274+gavxin@users.noreply.github.com>
2023-03-10 21:26:28 +0800

    find numa.h and warn when not found

--- ports/cachelib/portfile.cmake
+++ ports/cachelib/portfile.cmake
@@ -8,6 +8,15 @@ vcpkg_from_github(
         fix-build.patch
 )
 
+FIND_PATH(NUMA_INCLUDE_DIR NAME numa.h
+    PATHS ENV NUMA_ROOT
+    HINTS "$ENV{HOME}/local/include" /opt/local/include /usr/local/include /usr/include
+)
+
+IF (NOT NUMA_INCLUDE_DIR)
+    MESSAGE(FATAL_ERROR "Numa library not found.\nTry: 'sudo yum install numactl numactl-devel' (or sudo apt-get install libnuma1 libnuma-dev)")
+ENDIF ()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/cachelib"
     OPTIONS
