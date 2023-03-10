7cb2040973c4c39fc07e65ac527676f6c82f7a0d
reito <cnschwarzer@qq.com>
2023-03-10 22:26:01 +0800

    Fixup

--- ports/spout2/portfile.cmake
+++ ports/spout2/portfile.cmake
@@ -30,6 +30,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
+vcpkg_cmake_config_fixup()
+
 # Handle copyright & usage
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
