3a804064108115e6b1d1f546e7163d8fc60dd89e
Peter Adam Korodi <52385411+kp-cat@users.noreply.github.com>
2023-03-14 10:14:23 +0100

    Update ports/configcat/portfile.cmake
    Co-authored-by: Frank <65999885+FrankXie05@users.noreply.github.com>

--- ports/configcat/portfile.cmake
+++ ports/configcat/portfile.cmake
@@ -5,7 +5,7 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO configcat/cpp-sdk
-    REF v2.0.0
+    REF "v${VERSION}"
     SHA512 44e320079e92f7051e0924b4e02616e384abb89feadd1ce34823442fd195e195dea7503c4556d37ae157363ee1762421fe1d3f8e2cce70fff23e618128047acd
     HEAD_REF master
 )
