00d0d1c41a767d236484a76628aa6fd97e82b3e0
dan-shaw <51385773+dan-shaw@users.noreply.github.com>
2023-03-15 12:41:50 -0700

    fix

---
+++ scripts/azure-pipelines/android/create-docker.ps1
@@ -0,0 +1,38 @@
+# Create Docker image for Android
+
+[CmdLetBinding()]
+Param(
+  # Create a new resource group/container registry
+  [parameter(Mandatory=$false)]
+  [switch]$createNew
+)
+
+$Location = 'eastasia'
+$Date = (Get-Date -Format 'yyyy-MM-dd')
+$ResourceGroupName = "And-Registry"
+$ContainerRegistryName = "AndContainerRegistry"
+$ErrorActionPreference = 'Stop'
+
+if ($createNew) {
+    New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+    New-AzContainerRegistry -ResourceGroupName $ResourceGroupName -Name $ContainerRegistryName -EnableAdminUser -Sku Basic
+}
+
+$registry = Get-AzContainerRegistry -ResourceGroupName $ResourceGroupName -Name $ContainerRegistryName
+Connect-AzContainerRegistry -Name $registry.Name
+
+$imageName = "vcpkg-android"
+docker build . -t $imageName
+
+$remote = [string]::Format('andcontainerregistry.azurecr.io/{0}:{1}', $imageName, $Date)
+docker tag $imageName $remote
+
+docker push $remote
+
+#removes from local environment
+docker rmi --force $remote $imageName
+
+# pulls and runs ...
+docker logout
+
+Write-Host "Remote: $remote"
