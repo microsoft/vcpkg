720b903bf0265f73dc4de708d0def2cbc1bb3aec
Monica <v-liumonica@microsoft.com>
2023-03-17 16:30:48 +0800

    update onnx/onnx-optimizer

--- ports/onnx-optimizer/portfile.cmake
+++ ports/onnx-optimizer/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO onnx/optimizer
-    REF v0.2.6
-    SHA512 cc6373781f04bc6fafd3282d248e26eaf93556b86acbb0dee44b4aa7a5d59c634905c1a05d035e31a50fc0f06cfc686fa483b79a324dd973faa421747d4a681b
+    REF "v${VERSION}"
+    SHA512 831bcfca85d4a84b8dffd6cf6e061e6a9b67d14b56a1b8a4fadd9a9a2ce7c17f1488d39d97bf8c3d6a5e5f9ea02de2d56e5ed8e45a52e33f263ecd22b453a068
     HEAD_REF master
     PATCHES
         fix-cmakelists.patch
@@ -35,8 +35,7 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME ONNXOptimizer CONFIG_PATH lib/cmake/ONNXOptimizer)
 
-file(INSTALL "${SOURCE_PATH}/LICENSE"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE") 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share"
                     "${CURRENT_PACKAGES_DIR}/include/onnxoptimizer/test"
