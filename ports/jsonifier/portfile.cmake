51a98773554a719c899914049250beb7c96b64a3
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2023-03-19 04:03:55 -0400

    Updating the version file.

--- ports/jsonifier/portfile.cmake
+++ ports/jsonifier/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/Jsonifier
-	REF a62e165
-	SHA512 4523b68049b9a3908b343dd6ff57be5b316547f5f716bd3f97a3b61c4069637989377b3b9f57b772e679ec571be1c966ec7b8cb0a90826de73d02b92bf14def3
-	HEAD_REF Dev
+	REF d3fc62c
+	SHA512 efae44211d5282c49a177314ed6f5f5807664507b2644269fc6c89b2fcadc7c2133cb33da9298410f5f85ddf3655d36cea68ee1471f187c8ba51f8e48316b5e9
+	HEAD_REF main
 )
 
 vcpkg_cmake_configure(
@@ -13,12 +13,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 file(
 	INSTALL "${SOURCE_PATH}/License.md"
