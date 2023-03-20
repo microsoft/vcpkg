1ca05437fb49d81ef7200c0232cdc73b64a6402a
Ali Keles <83716339+akeles85@users.noreply.github.com>
2023-03-20 11:28:05 +0300

    review changes

--- ports/hazelcast-cpp-client/portfile.cmake
+++ ports/hazelcast-cpp-client/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hazelcast/hazelcast-cpp-client
-    REF v5.2.0
+    REF "v${VERSION}"
     SHA512 1e4203b546f3737e7fd2ea7a7ed04fa5b0663e7c319ed796140407d4e56d70e40eb5a9497d9f9eb7be182b038efe36f6ba1a42f10d24d3732cf54886c40db8eb 
     HEAD_REF master
 )
