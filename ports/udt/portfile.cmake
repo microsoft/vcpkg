6527ad67d60f5938369295c8f74a591a9ffcda9e
xiaozhuai <798047000@qq.com>
2023-03-22 14:55:48 +0800

    optimize defs

--- ports/udt/portfile.cmake
+++ ports/udt/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_sourceforge(
     FILENAME "udt.sdk.${VERSION}.tar.gz"
     SHA512 fc555ce1ddde2a8bd92c8adf470fd69a9a35d0a679def32b6ddbb18d67dc8b7d9dd928d772dc8598f08b350130f1e90bb4be58c46252a0a79ecc99f61eca8a92
     PATCHES
-        fix_platform_detection.patch
+        fix_defs.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
