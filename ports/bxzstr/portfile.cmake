b2e161c22c5e23beb617eec731a9cd00240bb362
Nathan Gober <ngober@protonmail.com>
2023-03-23 10:16:10 -0500

    Fixed bug where usage would not be added, added PATH_SUFFIXES

--- ports/bxzstr/portfile.cmake
+++ ports/bxzstr/portfile.cmake
@@ -19,3 +19,5 @@ file(INSTALL
      DESTINATION "${CURRENT_PACKAGES_DIR}/include/bxzstr")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
