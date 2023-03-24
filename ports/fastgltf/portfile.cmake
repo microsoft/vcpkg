7888445edc7aea2dff0763921651bbe88347405d
sean <43609023+spnda@users.noreply.github.com>
2023-03-24 15:09:54 +0100

    Use ${VERSION} constant

--- ports/fastgltf/portfile.cmake
+++ ports/fastgltf/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO spnda/fastgltf
-    REF 4a90faaed242d3042eae1abf8ecd5811a0072c2d
+    REF "${VERSION}"
     SHA512 063dcc6d1a8f7044bb4a51cd2732ebfd9b255f185ed36b695163309cb582f112ab3bb1bf1a1d02d3841f170f2f6ddde7f7ee419be40d0d88e83279a456e26aa4
     HEAD_REF main
 )
