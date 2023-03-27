d22351e05e171c6c1900d51ff7e03e7168a8e0d8
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-03-27 05:21:49 +0200

    [libkeyfinder] fix feature test

--- ports/libkeyfinder/portfile.cmake
+++ ports/libkeyfinder/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v2.2.6
     SHA512 c1b771cebfb925db521a344e28fd1d3bc6e6e921e45dcc81f90926e5b2020fea201a4bc05a65177d3559208a45746fd7784eb6f37352bb10ab7d7b820b40c0b6
     HEAD_REF main
+    PATCHES
+      catch3.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
