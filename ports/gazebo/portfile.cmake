fa958b79c1b49d3aed3dc829ccfcfc7456450c88
Tal Regev <tal.regev@gmail.com>
2023-03-29 01:39:29 +0300

    Fix gazebo dependencies

--- ports/gazebo/portfile.cmake
+++ ports/gazebo/portfile.cmake
@@ -1,19 +1,14 @@
-vcpkg_download_distfile(gazebo3211
-    URLS "https://patch-diff.githubusercontent.com/raw/osrf/gazebo/pull/3211.diff"
-    FILENAME "gazebo3211.diff"
-    SHA512 761e254866d4705acc0b81479285f979c436b3b611739a207a575031d8a8daba48de4fc0c8de5edb9a9f89725586c5caeef9e6e1e3d63a2d961ca09df974f7de
-)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO osrf/gazebo
-    REF 382dcc3f36095a8d79b5bc9c8b8ad346e867c51d
-    SHA512 57638cd0b23b5f2bfd32fdc159d6cd77ca34e3bd695c225591979aef4b7271eac93d3706fa1ffa2340f90013267a4171bebe1e4c142f19ad2bf67963dfed627e
-    HEAD_REF gazebo11
+    REPO gazebosim/gazebo-classic
+    REF gazebo11_${VERSION}
+    SHA512 50a5160dc1fb30cd517bf6937675359e2db01a46eeacad5e1ca5cf61ee36bcb4a9d425d6dabe32fecb705989fb54f2ad807b43b7f39f88a871e1bec9d3f6c70a
     PATCHES
         0001-Fix-deps.patch
-        "${gazebo3211}"
-        fix-tbb.patch # Effectively https://github.com/osrf/gazebo/pull/3174 with merge conflict resolutions
+        fix-build-type.patch
+        fix-opengl-def.patch
+        add-features.patch
+        fix-msgs.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -21,6 +16,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         openal    HAVE_OPENAL
         ffmpeg    FFMPEG_FEATURE
         gts       GTS_FEATURE
+        plugins   BUILD_PLUGINS
+        tools     BUILD_TOOLS
     INVERTED_FEATURES
         simbody   CMAKE_DISABLE_FIND_PACKAGE_Simbody
         dart      CMAKE_DISABLE_FIND_PACKAGE_DART
@@ -33,9 +30,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        ${FEATURE_OPTIONS}
         -DUSE_EXTERNAL_TINY_PROCESS_LIBRARY=ON
         "-DPKG_CONFIG_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/pkgconf/pkgconf${VCPKG_HOST_EXECUTABLE_SUFFIX}"
-        ${FEATURE_OPTIONS}
+        -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON
         -DBUILD_TESTING=OFF  # Not enabled by default, but to be sure
 )
 
@@ -50,11 +48,15 @@ foreach(postfix "" "-11")
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}${postfix}/setup.sh" "${CURRENT_INSTALLED_DIR}" "`dirname $0`/../..")
 endforeach()
 
+set(BINARIES_LIST gazebo gzclient gzserver)
+if (BUILD_TOOLS) 
+    set(BINARIES_LIST ${BINARIES_LIST} gz)
+endif()
 vcpkg_copy_tools(
-    TOOL_NAMES gazebo gz gzclient gzserver
+    TOOL_NAMES ${BINARIES_LIST}
     AUTO_CLEAN
 )
-set(EXTRA_OGRE_LIBS Codec_EXR Codec_FreeImage Codec_STBI OgreBites OgreMain OgreMeshLodGenerator OgreOverlay OgrePaging OgreProperty OgreRTShaderSystem OgreTerrain OgreVolume Plugin_BSPSceneManager Plugin_DotScene Plugin_OctreeSceneManager Plugin_OctreeZone Plugin_ParticleFX Plugin_PCZSceneManager RenderSystem_Direct3D11 RenderSystem_GL RenderSystem_GL3Plus)
+set(EXTRA_OGRE_LIBS Codec_FreeImage Codec_STBI OgreBites OgreMain OgreMeshLodGenerator OgreOverlay OgrePaging OgreProperty OgreRTShaderSystem OgreTerrain OgreVolume Plugin_BSPSceneManager Plugin_DotScene Plugin_OctreeSceneManager Plugin_OctreeZone Plugin_ParticleFX Plugin_PCZSceneManager RenderSystem_Direct3D11 RenderSystem_GL RenderSystem_GL3Plus)
 foreach(LIB IN LISTS EXTRA_OGRE_LIBS)
     set(FILE_NAME "${CMAKE_SHARED_LIBRARY_PREFIX}${LIB}${CMAKE_SHARED_LIBRARY_SUFFIX}")
     file(COPY "${CURRENT_INSTALLED_DIR}/bin/${FILE_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
