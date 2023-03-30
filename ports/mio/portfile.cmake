9b4eea087e0f8371843ca5aed8cb3a44469d0db1
Cheney-Wang <850426846@qq.com>
2023-03-30 00:18:35 -0700

    [mio] Update to 2023-03-03

--- ports/mio/portfile.cmake
+++ ports/mio/portfile.cmake
@@ -2,8 +2,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mandreyel/mio
-    REF 3f86a95c0784d73ce6815237ec33ed25f233b643
-    SHA512 18bbc41d5c3b29ecafe19cef29687380d8f4f27279af08edb0d5e65ee1d71162f3cf75d8efaa324cd11301f3f49fadaec3e4c1514607d5065ddd7a65bf32ee2a
+    REF 8b6b7d878c89e81614d05edca7936de41ccdd2da
+    SHA512 444131d4839f2244dd88722f5bfad2cfa47336e2a4405518a2ff8f0d80f2755321d7d627f8d5b890864a5dc3f3f810a1c7dd6588ff3e9039a6ef7d010e0f2f06
     HEAD_REF master
 )
 
@@ -20,4 +20,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/mio)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
