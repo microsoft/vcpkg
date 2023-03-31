2acc6bb670276999f0a526223b889ca4610e412c
Todor Prokopov <koprok@gmail.com>
2023-03-31 13:37:37 +0300

    [tinyfsm] Remove CMake export

--- ports/tinyfsm/portfile.cmake
+++ ports/tinyfsm/portfile.cmake
@@ -6,15 +6,6 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-set(VCPKG_BUILD_TYPE release) # header-only port
-
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-)
-
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-tinyfsm)
+file(INSTALL "${SOURCE_PATH}/include/tinyfsm.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
