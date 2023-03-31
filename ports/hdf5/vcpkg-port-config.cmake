02ef90b168fd8c88a52186ccd5a5213c6595d8cd
Kai Pastor <dg0yt@darc.de>
2023-03-31 08:13:15 +0200

    Move effects downstream

--- ports/hdf5/vcpkg-port-config.cmake
+++ ports/hdf5/vcpkg-port-config.cmake
@@ -2,11 +2,3 @@
 set(HDF5_WITH_PARALLEL [[
 HDF5 was built with parallel support.
 ]])
-if(PORT STREQUAL "seacas" AND NOT "mpi" IN_LIST FEATURES)
-    message(WARNING "${HDF5_WITH_PARALLEL} Enabling MPI in seacas.")
-    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DTPL_ENABLE_MPI:BOOL=ON")
-endif()
-if(PORT STREQUAL "vtk" AND NOT "mpi" IN_LIST FEATURES)
-    message(WARNING "${HDF5_WITH_PARALLEL} Enabling MPI in vtk.")
-    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DVTK_USE_MPI:BOOL=ON")
-endif()
