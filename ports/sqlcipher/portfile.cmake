13656deeb464e4962fa207083beb9842f16ee786
Jonliu1993 <13720414433@163.com>
2023-03-31 16:47:45 +0800

    update feature

--- ports/sqlcipher/portfile.cmake
+++ ports/sqlcipher/portfile.cmake
@@ -49,11 +49,11 @@ file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION "${SOURCE_PATH}")
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-    geopoly WITH_GEOPOLY
-    json1 WITH_JSON1
+        geopoly WITH_GEOPOLY
+        json1 WITH_JSON1
+        fts5 WITH_FTS5
     INVERTED_FEATURES
-    tool SQLITE3_SKIP_TOOLS
-    fts5 WITH_FTS5
+        tool SQLITE3_SKIP_TOOLS
 )
 
 vcpkg_cmake_configure(
