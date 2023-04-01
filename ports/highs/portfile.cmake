845aad67800f4c81f0f62146d33fe0147a693e94
Luke Marshall <52978038+mathgeekcoder@users.noreply.github.com>
2023-04-01 13:05:45 -0700

    Improved pkconfig and thread support

--- ports/highs/portfile.cmake
+++ ports/highs/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(
     SHA512 ce2003f7328d49e2292135b740204eb485c8c6694fa017c6604e6d4057913d5d5eea517561ffe4b7251b0fbe230bc8288ab6d91dd9432ffc96e792a1ce866424
     HEAD_REF master
       PATCHES
+	  fix-pkconfig-path.patch
       fix-hconfig-path.patch
       fix-cmake-output.patch
       fix-threads.patch
@@ -18,6 +19,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/highs")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
