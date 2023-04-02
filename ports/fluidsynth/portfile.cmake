d9deb04ff083a22a5aaf718b25ea2ba625de14dc
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-04-02 13:54:45 -0400

    [fluidsynth] Update CMake configure options.
    - List out the options per-platform.
    - Set the proper options for Android.

--- ports/fluidsynth/portfile.cmake
+++ ports/fluidsynth/portfile.cmake
@@ -15,45 +15,53 @@ vcpkg_check_features(
         sndfile     enable-libsndfile
 )
 
-set(feature_list dbus jack libinstpatch midishare opensles oboe openmp oss sdl2 pulseaudio readline lash systemd dart)
-foreach(_feature IN LISTS feature_list)
-    list(APPEND FEATURE_OPTIONS -Denable-${_feature}:BOOL=OFF)
-endforeach()
+# enable platform-specific features, force the build to fail if the required libraries are not found,
+# and disable all other features to avoid system libraries to be picked up
+set(WINDOWS_OPTIONS enable-dsound enable-wasapi enable-waveout enable-winmidi HAVE_MMSYSTEM_H HAVE_DSOUND_H HAVE_OBJBASE_H)
+set(MACOS_OPTIONS enable-coreaudio enable-coremidi COREAUDIO_FOUND COREMIDI_FOUND)
+set(LINUX_OPTIONS enable-alsa ALSA_FOUND)
+set(ANDROID_OPTIONS enable-opensles OpenSLES_FOUND)
+set(IGNORED_OPTIONS enable-coverage enable-dbus enable-floats enable-fpe-check enable-framework enable-jack enable-lash
+    enable-libinstpatch enable-midishare enable-oboe enable-openmp enable-oss enable-pipewire enable-portaudio
+    enable-profiling enable-pulseaudio enable-readline enable-sdl2 enable-systemd enable-trap-on-fpe enable-ubsan)
 
-# enable platform-specific features, and force the build to fail if the
-# required libraries are not found
-list(APPEND FEATURE_OPTIONS -Denable-dsound=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-wasapi=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-waveout=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-winmidi=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_MMSYSTEM_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_DSOUND_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_WASAPI_HEADERS=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -DHAVE_OBJBASE_H=${VCPKG_TARGET_IS_WINDOWS})
-list(APPEND FEATURE_OPTIONS -Denable-coreaudio=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -Denable-coremidi=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -DCOREAUDIO_FOUND=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -DCOREMIDI_FOUND=${VCPKG_TARGET_IS_OSX})
-list(APPEND FEATURE_OPTIONS -Denable-alsa=${VCPKG_TARGET_IS_LINUX})
-list(APPEND FEATURE_OPTIONS -DALSA_FOUND=${VCPKG_TARGET_IS_LINUX})
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(OPTIONS_TO_ENABLE ${WINDOWS_OPTIONS})
+    set(OPTIONS_TO_DISABLE ${MACOS_OPTIONS} ${LINUX_OPTIONS} ${ANDROID_OPTIONS})
+elseif(VCPKG_TARGET_IS_OSX)
+    set(OPTIONS_TO_ENABLE ${MACOS_OPTIONS})
+    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${LINUX_OPTIONS} ${ANDROID_OPTIONS})
+elseif(VCPKG_TARGET_IS_LINUX)
+    set(OPTIONS_TO_ENABLE ${LINUX_OPTIONS})
+    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${MACOS_OPTIONS} ${ANDROID_OPTIONS})
+elseif(VCPKG_TARGET_IS_ANDROID)
+    set(OPTIONS_TO_ENABLE ${ANDROID_OPTIONS})
+    set(OPTIONS_TO_DISABLE ${WINDOWS_OPTIONS} ${MACOS_OPTIONS} ${LINUX_OPTIONS})
+endif()
+
+foreach(_option IN LISTS OPTIONS_TO_ENABLE)
+    list(APPEND ENABLED_OPTIONS "-D{_option}:BOOL=ON")
+endforeach()
+    
+foreach(_option IN LISTS OPTIONS_TO_DISABLE IGNORED_OPTIONS)
+    list(APPEND DISABLED_OPTIONS "-D${_option}:BOOL=OFF")
+endforeach()
 
 vcpkg_find_acquire_program(PKGCONFIG)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DVCPKG_HOST_TRIPLET=${HOST_TRIPLET}"
+        -DVCPKG_HOST_TRIPLET="${HOST_TRIPLET}"
         ${FEATURE_OPTIONS}
-        -DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}
-        -Denable-framework=OFF # Needs system permission to install framework
+        ${ENABLED_OPTIONS}
+        ${DISABLED_OPTIONS}
+        -DPKG_CONFIG_EXECUTABLE="${PKGCONFIG}"
     MAYBE_UNUSED_VARIABLES
-        enable-coreaudio
-        enable-coremidi
-        enable-dart
-        ALSA_FOUND
-        COREAUDIO_FOUND
-        COREMIDI_FOUND
+        ${OPTIONS_TO_DISABLE}
         VCPKG_BUILD_MAKE_TABLES
+        enable-coverage
         enable-framework
+        enable-ubsan
 )
 
 vcpkg_cmake_install()
