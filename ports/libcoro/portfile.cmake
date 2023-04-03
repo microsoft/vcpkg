b8445c9f96242c172df14b4805a3e8ec04628b9e
chausner <chausner@users.noreply.github.com>
2023-04-03 20:34:52 +0200

    Apply code review suggestions

--- ports/libcoro/portfile.cmake
+++ ports/libcoro/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jbaldwin/libcoro
-    REF v0.7
+    REF "v${VERSION}"
     SHA512 746607793ba2e0c809e731b39423fa84e451a67129ba311dc3ccc257d5d31be0f94826ca1bffc8a88f9f4675ba4224d3290bab6e46116bcdee50acb2f96fbf81
     HEAD_REF master
     PATCHES
