7f453f83227ff33dca80662bbe0573f6396b4e9c
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-04-10 09:27:40 -0400

    [dumb] Apply suggestion from review.
    - Remove unofficial config since a pkg-config file is provided.
    - Update the match string for pkg-config fixes.
    - Limit ONLY_STATIC_LIBRARY to Windows.

--- ports/dumb/portfile.cmake
+++ ports/dumb/portfile.cmake
@@ -1,4 +1,6 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,7 +9,6 @@ vcpkg_from_github(
     SHA512 18b10a507d69a754cdf97fbeae41c17f211a6ba1f166a822276bdb6769d3edc326919067a3f4d1247d6715d7a5a8276669d83b9427e7336c6d111593fb7e36cf
     HEAD_REF master
     PATCHES
-        "export-unofficial-target.patch"
         "check-for-math-lib.patch"
         "do-not-overwrite-cflags.patch"
 )
@@ -23,25 +24,21 @@ vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/dumb.pc" "libdumb" "dumb")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/dumb.pc" "-llibdumb" "-ldumb")
 if(NOT VCPKG_BUILD_TYPE)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/dumb.pc" "libdumb" "dumbd")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/dumb.pc" "-llibdumb" "-ldumbd")
 endif()
 
 if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/dumb.pc" "-lm" "")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/dumb.pc" " -lm" "")
     if(NOT VCPKG_BUILD_TYPE)
-        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/dumb.pc" "-lm" "")
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/dumb.pc" " -lm" "")
     endif()
 endif()
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-dumb-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-dumb")
-vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-dumb PACKAGE_NAME unofficial-dumb)
 
 file(REMOVE_RECURSE 
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
