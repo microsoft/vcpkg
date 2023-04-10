4ba44efb47a3d415179f7d7f54da58bf74e32cf8
John Willard <john.willard@shotover.com>
2023-04-10 14:25:17 -0600

    [json-c] Update to version 2023-03-28

--- ports/json-c/portfile.cmake
+++ ports/json-c/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO json-c/json-c
-    REF d28ac67dde77566f53a97f22b4ea7cb36afe6582
-    SHA512 30063c8e32eb82e170647363055119f2f7eab19e1c3152673b966f41ed07e0349c3d6141b215b9912f9e84c2e06677b3d7ac949f720c7ebc2c95d692dc3881fe
+    REF d0f32a5a43d1b9dc0b2cd6af310e5f09b97c3423
+    SHA512 c4d4bb1a54ae95f47223636c51c8ad611d2a8d698e9dbdc2d3fd06946b23d34b78af930762eb7f98cb49ae0a78948a668b1c44e1bb590069d5dd90dbcbb078ab
     HEAD_REF master
     PATCHES pkgconfig.patch
             fix-clang-cl.patch
