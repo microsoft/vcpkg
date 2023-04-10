6cbd46894e8d36ce4325061d78064436a682197b
Theodore Tsirpanis <teo@tsirpanis.gr>
2023-04-11 02:03:47 +0300

    Stop using `tls12-download`.

--- scripts/azure-pipelines/windows/bootstrap-from-source.cmd
+++ scripts/azure-pipelines/windows/bootstrap-from-source.cmd
@@ -2,6 +2,6 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsD
 git clone https://github.com/microsoft/vcpkg-tool vcpkg-tool
 git -C vcpkg-tool switch -d %1
 rmdir /s /q build.x86.release > nul 2> nul
-cmake.exe -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DVCPKG_WARNINGS_AS_ERRORS=OFF -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_BUILD_TLS12_DOWNLOADER=OFF -B build.x86.release -S vcpkg-tool
+cmake.exe -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DVCPKG_WARNINGS_AS_ERRORS=OFF -DVCPKG_BUILD_FUZZING=OFF -B build.x86.release -S vcpkg-tool
 ninja.exe -C build.x86.release
 move build.x86.release\vcpkg.exe vcpkg.exe
--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -47,9 +47,9 @@ Write-Verbose "Examining $vcpkgRootDir for .vcpkg-root - Found"
 
 $versionDate = '2023-03-29'
 if ($env:PROCESSOR_ARCHITECTURE -eq 'ARM64' -or $env:PROCESSOR_IDENTIFIER -match "ARMv[8,9] \(64-bit\)") {
-    & "$scriptsDir/tls12-download-arm64.exe" github.com "/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg-arm64.exe" "$vcpkgRootDir\vcpkg.exe"
+    Invoke-WebRequest "https://github.com/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg-arm64.exe" -OutFile "$vcpkgRootDir\vcpkg.exe"
 } else {
-    & "$scriptsDir/tls12-download.exe" github.com "/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg.exe" "$vcpkgRootDir\vcpkg.exe"
+    Invoke-WebRequest "https://github.com/microsoft/vcpkg-tool/releases/download/$versionDate/vcpkg.exe" -OutFile "$vcpkgRootDir\vcpkg.exe"
 }
 
 Write-Host ""
