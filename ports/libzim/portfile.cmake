8263f1c9017e8430d3e6a841957c8cf77878939b
YiFang Xiao <Yifang.xiao@foxmail.com>
2023-04-10 09:49:02 +0800

    libzim: add package
    update libzim versions
    
    libzim:meson ignore xapian

---
+++ ports/libzim/portfile.cmake
@@ -0,0 +1,45 @@
+
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO openzim/libzim
+    REF 5d04de878c764fccb49d5aaf26f2006272e01993
+    SHA512 abcd5ef0e3f32cff0863c726a27cb42ad55522c17254f87e2bf6261ec564864017ea9136cb885d837a358f98ed123bf512551342703a7149cf93672f18e32db1
+    HEAD_REF main
+    PATCHES
+        0001-build-share-library.patch
+
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        xapian     WITH_XAPIAN
+)
+
+set(EXTRA_OPTIONS "")
+
+if(NOT WITH_XAPIAN)
+    list(APPEND EXTRA_OPTIONS "-Dwith_xapian=false")
+endif()
+
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+      -Dexamples=false
+      -Dstatic-linkage=false
+      -Ddefault_library=shared
+      ${EXTRA_OPTIONS}
+
+)
+
+
+vcpkg_install_meson(ADD_BIN_TO_PATH)
+
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_pkgconfig()
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
