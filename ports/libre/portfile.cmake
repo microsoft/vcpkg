5f82192e41c14fc92c375c434b9582efd22002a8
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-04-14 14:38:31 +0200

    add new port libre

---
+++ ports/libre/portfile.cmake
@@ -0,0 +1,26 @@
+#
+# portfile.cmake
+#
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO baresip/re
+    REF v3.0.0
+    SHA512 c37e49cca0d7ff591a3d178cbf58511d27e08be2c9b210353d9f65bb2cd76d135e0e023702140623630440ffdcc7b4c51ac29495bd85df4424627a5a69adba52
+    HEAD_REF main
+)
+
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_cmake_install()
+
+vcpkg_fixup_pkgconfig(SYSTEM_LIBRARIES m)
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
+                    "${CURRENT_PACKAGES_DIR}/lib/cmake")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libre" RENAME copyright)
