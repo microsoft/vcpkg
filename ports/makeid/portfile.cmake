15ca2851755535a0f8b97495e991dabccac6167c
Billy Robert O'Neal III <bion@microsoft.com>
2023-04-17 16:37:52 -0700

    Add TLS and quotes.

--- ports/makeid/portfile.cmake
+++ ports/makeid/portfile.cmake
@@ -1,10 +1,10 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://www.humus.name/3D/MakeID.h"
+    URLS "https://www.humus.name/3D/MakeID.h"
     FILENAME "MakeID.h-${VERSION}"
     SHA512 9b7cb5c1b71904f37f65fcac3d18194154029fbe04d89099d879ce8eb03e796662c78653322317ed72988d3695414aaa6e6c24cfff999bea5009ec47119c57a7
 )
 
-file(COPY ${ARCHIVE} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(COPY "${ARCHIVE}" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 set(license_text 
 "Public Domain
