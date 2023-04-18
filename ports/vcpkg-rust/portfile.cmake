89de7647cbe66a73240560df772fa78f9d7e0382
Jonas Zaddach <jzaddach@cisco.com>
2023-04-18 16:33:22 +0200

    Address Neumann-A's comments

--- ports/vcpkg-rust/portfile.cmake
+++ ports/vcpkg-rust/portfile.cmake
@@ -1,7 +1,3 @@
-if(VCPKG_CROSSCOMPILING)
-    message(FATAL_ERROR "vcpkg-rust is a host-only port; please mark it as a host port in your dependencies.")
-endif()
-
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_rust_install.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
