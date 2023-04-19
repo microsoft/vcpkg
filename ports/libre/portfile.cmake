1ffda444b024fd16a41865122b430a3c8cce766a
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-04-19 12:47:09 +0200

    install usage

--- ports/libre/portfile.cmake
+++ ports/libre/portfile.cmake
@@ -27,3 +27,5 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
