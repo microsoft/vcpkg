1e2008eb46a66dcd8b29263a1cc6a83229d3052d
FrankXie <v-frankxie@microsoft.com>
2023-04-19 20:10:54 -0700

    [libconfig] Export correct cmake config

--- ports/libconfig/portfile.cmake
+++ ports/libconfig/portfile.cmake
@@ -1,9 +1,15 @@
+if(VCPKG_USE_HEAD_VERSION)
+    list(APPEND PATCHES Export-correct-cmake-config.patch)
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hyperrealm/libconfig
     REF v1.7.3
     SHA512 3749bf9eb29bab0f6b14f4fc759f0c419ed27a843842aaabed1ec1fbe0faa8c93322ff875ca1291d69cb28a39ece86d512aec42c2140d566c38c56dc616734f4
     HEAD_REF master
+    PATCHES
+        ${PATCHES}
 )
 
 if (NOT VCPKG_USE_HEAD_VERSION)
@@ -31,7 +37,8 @@ vcpkg_copy_pdbs()
 
 if (VCPKG_USE_HEAD_VERSION)
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-  vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libconfig)
+  vcpkg_cmake_config_fixup()
+  vcpkg_cmake_config_fixup(PACKAGE_NAME libconfig++)
 endif()
 
 foreach(FILE ${CURRENT_PACKAGES_DIR}/include/libconfig.h++ ${CURRENT_PACKAGES_DIR}/include/libconfig.h)
