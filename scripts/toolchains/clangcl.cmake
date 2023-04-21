a18148efb8d01c384e5cff69ff78c16d81fd5c17
Chuck Walbourn <chuckw@microsoft.com>
2023-04-21 11:00:06 -0700

    Added arm64 clangcl triplets

--- scripts/toolchains/clangcl.cmake
+++ scripts/toolchains/clangcl.cmake
@@ -1,8 +1,25 @@
 if(NOT _VCPKG_CLANGCL_TOOLCHAIN)
 set(_VCPKG_CLANGCL_TOOLCHAIN 1)
 
-find_program(CMAKE_C_COMPILER "clang-cl.exe")
-find_program(CMAKE_CXX_COMPILER "clang-cl.exe")
+find_program(CMAKE_C_COMPILER "clang-cl.exe" REQUIRED)
+find_program(CMAKE_CXX_COMPILER "clang-cl.exe" REQUIRED) 
+
+get_property( _CMAKE_IN_TRY_COMPILE GLOBAL PROPERTY IN_TRY_COMPILE )
+if(NOT _CMAKE_IN_TRY_COMPILE)
+    set(_vcpkg_clangcl_arch "")
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        set(_vcpkg_clangcl_arch "-m32")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(_vcpkg_clangcl_arch "-m64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+        set(_vcpkg_clangcl_arch "--target=arm-pc-windows-msvc")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        set(_vcpkg_clangcl_arch "--target=arm64-pc-windows-msvc")
+    endif()
+
+    string(APPEND VCPKG_C_FLAGS ${_vcpkg_clangcl_arch})
+    string(APPEND VCPKG_CXX_FLAGS ${_vcpkg_clangcl_arch})
+endif()
 
 if(DEFINED XBOX_CONSOLE_TARGET)
     include("${CMAKE_CURRENT_LIST_DIR}/xbox.cmake")
