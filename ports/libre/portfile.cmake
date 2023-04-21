7760dfda7dffb503283d675bcd7a0518819b4029
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-04-21 08:38:49 +0200

    use vcpkg_cmake_config_fixup()

--- ports/libre/portfile.cmake
+++ ports/libre/portfile.cmake
@@ -26,9 +26,7 @@ vcpkg_cmake_install()
 
 vcpkg_fixup_pkgconfig(SYSTEM_LIBRARIES m)
 
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake"
-                    "${CURRENT_PACKAGES_DIR}/lib/cmake")
+vcpkg_cmake_config_fixup(PACKAGE_NAME libre CONFIG_PATH lib/cmake/re)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
