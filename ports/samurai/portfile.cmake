6e2aeb65078629a8878f4b9baeaed597bffd54d1
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-23 11:10:47 +0200

    make changes from review (part 2)

--- ports/samurai/portfile.cmake
+++ ports/samurai/portfile.cmake
@@ -7,20 +7,22 @@ vcpkg_from_github(
     SHA512 38750a3849023eeebaf356e399ff69f96eac8d696e366953ca5b689c038508b7e8dfea2bdb49c7f44ab909dcf94950160eabd3e6cbb55957c55461c008f17094
     HEAD_REF master
     PATCHES
-    0001-add-hdf5-dependency-in-cmake.patch
-    0002-fix-configure-file.patch
+        0001-add-hdf5-dependency-in-cmake.patch
 )
 
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_cmake_get_vars(cmake_vars_file)
+include(${cmake_vars_file})
+
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-    -DFETCHCONTENT_FULLY_DISCONNECTED=OFF
-    -DCMAKE_SYSTEM_PROCESSOR=${VCPKG_TARGET_ARCHITECTURE}
+        -DFETCHCONTENT_FULLY_DISCONNECTED=OFF
+        -DCMAKE_SYSTEM_PROCESSOR=${VCPKG_DETECTED_CMAKE_SYSTEM_PROCESSOR}
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
