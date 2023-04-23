a344f00239bba3600222ca87af08b7a562345b6b
Loic Gouarin <loic.gouarin@gmail.com>
2023-04-23 09:26:19 +0200

    make changes from review

--- ports/samurai/portfile.cmake
+++ ports/samurai/portfile.cmake
@@ -3,11 +3,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hpc-maths/samurai
-    REF v0.2.0
+    REF "v${VERSION}"
     SHA512 38750a3849023eeebaf356e399ff69f96eac8d696e366953ca5b689c038508b7e8dfea2bdb49c7f44ab909dcf94950160eabd3e6cbb55957c55461c008f17094
     HEAD_REF master
     PATCHES
     0001-add-hdf5-dependency-in-cmake.patch
+    0002-fix-configure-file.patch
 )
 
 vcpkg_cmake_configure(
@@ -17,13 +18,9 @@ vcpkg_cmake_configure(
     -DCMAKE_SYSTEM_PROCESSOR=${VCPKG_TARGET_ARCHITECTURE}
 )
 
-message(STATUS ${VCPKG_TARGET_ARCHITECTURE})
-
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
