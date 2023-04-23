19cbb3fcae44a9c477c76703f195b7097866411e
jimwang118 <wangzhijie05@beyondsoft.com>
2023-04-23 10:55:05 +0800

    fix libhdfs3 build error

--- ports/libhdfs3/portfile.cmake
+++ ports/libhdfs3/portfile.cmake
@@ -6,6 +6,26 @@ vcpkg_from_github(
     HEAD_REF apache-rpc-9
 )
 
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING [[
+Port libhdfs3 currently requires the following packages from the system package manager:
+    libuuid
+    libgsasl
+These development packages can be installed on the system via
+    git clone https://gitlab.com/gsasl/gsasl.git
+    wget http://sourceforge.net/projects/libuuid/files/libuuid-1.0.3.tar.gz
+]])
+elseif(VCPKG_TARGET_IS_OSX)
+    message(WARNING [[
+Port libhdfs3 currently requires the following packages from the system package manager:
+    libuuid
+    libgsasl
+These development packages can be installed on the system via
+    git clone https://gitlab.com/gsasl/gsasl.git
+    brew install util-linux
+]])
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
