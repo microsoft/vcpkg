4d3a2fa6d1845c2ad4d4b0cff19a6f9d8aead614
jyu49 <yu_jack@apple.com>
2023-04-24 11:03:13 +0800

    so far so good

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -14,11 +14,11 @@ vcpkg_extract_source_archive(
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/7zip-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup()
