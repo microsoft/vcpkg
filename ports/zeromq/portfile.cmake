e5ceca604980afe51d14792a6845c64d7271a33a
Jonliu1993 <13720414433@163.com>
2023-04-25 17:26:51 +0800

    Add option

--- ports/zeromq/portfile.cmake
+++ ports/zeromq/portfile.cmake
@@ -35,6 +35,7 @@ vcpkg_cmake_configure(
         -DWITH_DOCS=OFF
         -DWITH_NSS=OFF
         -DCMAKE_REQUIRE_FIND_PACKAKE_GnuTLS=ON
+        -DWITH_LIBBSD=OFF
         -DWITH_LIBSODIUM_STATIC=${BUILD_STATIC}
         ${FEATURE_OPTIONS}
         ${PLATFORM_OPTIONS}
