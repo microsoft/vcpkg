04739ffba4fe856ed939457fc7adf3ca8634e837
Rémi Bédard-Couture <remi.bc@outlook.com>
2023-04-26 14:14:23 -0400

    Update download hash

--- ports/tree-similarity/portfile.cmake
+++ ports/tree-similarity/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO DatabaseGroup/tree-similarity
     REF 76e62627438ea603c9881221f7444c7cb5407b6d
-    SHA512 0
+    SHA512 e344b949e390d2494f3d3568359d9f520cb459863f47c150034bffb263a2a71bc86bb60dbac673f30c55cf4e2a26b45ea39593928e42fb2121edb605dd4765ee
     HEAD_REF master
 )
 
