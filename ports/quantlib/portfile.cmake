820261a8864610c8da43d274e3e0e4d989a69055
Jonathan Sweemer <sweemer@gmail.com>
2023-04-26 02:37:28 +0000

    [quantlib] update to v1.30

--- ports/quantlib/portfile.cmake
+++ ports/quantlib/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF QuantLib-v1.30
     SHA512 7948f33fac124e615051863a1780ed2f98a626577174b54d0a276e604d3b034828e3d6c0c528687c4c0b974829dde830daf6be70f8de51467c9fc0f9a76eb525
     HEAD_REF master
+    PATCHES remove-amortizingbonds-all.patch
 )
 
 if (VCPKG_TARGET_IS_WINDOWS)
