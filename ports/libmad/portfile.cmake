c5000096fa2f42d09bd5d1c127a396b925705f50
Daniel Schürmann <daschuer@mixxx.org>
2023-05-02 21:12:49 +0200

    [libmad] Added md_size.patch length-check.patch
    They are uses in the debian package to fix This bug:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=287519

--- ports/libmad/portfile.cmake
+++ ports/libmad/portfile.cmake
@@ -8,7 +8,10 @@ vcpkg_from_sourceforge(
     REF 0.15.1b
     FILENAME "libmad-0.15.1b.tar.gz"
     SHA512 2cad30347fb310dc605c46bacd9da117f447a5cabedd8fefdb24ab5de641429e5ec5ce8af7aefa6a75a3f545d3adfa255e3fa0a2d50971f76bc0c4fc0400cc45
-    PATCHES 0001-Fix-MSVC-ARM.patch
+    PATCHES 
+        0001-Fix-MSVC-ARM.patch
+        md_size.patch
+        length-check.patch
 )
 
 #The archive only contains a Visual Studio 6.0 era DSP project file, so use a custom CMakeLists.txt
