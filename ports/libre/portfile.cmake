deb82f58ea1806edf298fb6823e9147022e8016c
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-05-04 13:32:31 +0200

    uncomment REMOVE_RECURSE

--- ports/libre/portfile.cmake
+++ ports/libre/portfile.cmake
@@ -34,6 +34,6 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME libre CONFIG_PATH lib/cmake/re)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-#file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
