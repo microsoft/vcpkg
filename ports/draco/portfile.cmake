c2fdcf89546b32abeea8deb991d62ecd07558e97
Connor Broyles <cbroyles94@gmail.com>
2023-05-04 10:44:05 -0400

    [draco] Use VERSION variable for git ref
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/draco/portfile.cmake
+++ ports/draco/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/draco
-    REF 1.5.6
+    REF "${VERSION}"
     SHA512 d4bc48aeac23aba377d1770a46e6676cb01596a436493385fb0c4ef9ba3f0fae42027232131a3d438250909aff4311353e114925753d045cc585af42660be0b1
     HEAD_REF master
     PATCHES
