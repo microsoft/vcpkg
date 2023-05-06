4725e9208b9f7f35bcb8921c5ca246a3562305a1
Lily Wang <494550702@qq.com>
2023-05-06 03:23:13 -0700

    apply suggestions

--- ports/libsbml/libsbml-config.cmake
+++ ports/libsbml/libsbml-config.cmake
@@ -1,3 +1,6 @@
 include(CMakeFindDependencyMacro)
-find_dependency(libsbml-static CONFIG REQUIRED)
-add_library(libsbml ALIAS libsbml-static)
+find_dependency(libsbml-static CONFIG)
+if(NOT TARGET libsbml)
+    add_library(libsbml INTERFACE IMPORTED)
+    target_link_libraries(libsbml INTERFACE libsbml-static)
+endif()
