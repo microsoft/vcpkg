873ca5b5dce0277d095c8754f0706ff99bad8227
Connor Broyles <cbroyles94@gmail.com>
2023-05-08 15:03:50 -0400

    [vsg] Devendor glslang

--- ports/vsg/portfile.cmake
+++ ports/vsg/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 da5c9448da6bd2bd6edcb82ed1ac67aded9c3fd957fb7d3c78bb741d8993a5e90b134433237a63bf1a6eb23b30a1e6b4ea2115416f8d32464bd04925d8dbcd34
     HEAD_REF master
+	PATCHES devendor-glslang.patch
 )
 
 vcpkg_cmake_configure(
