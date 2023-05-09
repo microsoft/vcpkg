0f9a31b97a8526cad2d6fb276daef3a3cc189265
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-05-09 12:15:58 +0200

    add patch file

--- ports/hnswlib/portfile.cmake
+++ ports/hnswlib/portfile.cmake
@@ -1,11 +1,3 @@
-# Backport CMake targets from develop
-vcpkg_download_distfile(
-    CMAKE_TARGETS_PATCH
-    URLS "https://github.com/nmslib/hnswlib/commit/dccd4f98acb9da404b7439606a97c4e3077a8d44.patch"
-    FILENAME dccd4f98acb9da404b7439606a97c4e3077a8d44.patch
-    SHA512 e514feaf9b5138627aa9847c12cba111acd6ae7315acff14aabe151e3339600418473f805053bb3b28ed92b32751ccac675feaa95cc3881090de672b70983140
-)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nmslib/hnswlib
@@ -13,7 +5,7 @@ vcpkg_from_github(
     SHA512 fd74c23040598973d7e0b5a6af73eb884ee2d30703187d1702fdd48eaf8f7f96d8fbb125d3763f90111d9fb7c5ab3434ebdb818da8717d35c5571e99083c812b
     HEAD_REF master
     PATCHES
-        ${CMAKE_TARGETS_PATCH}
+        cmake.patch # Backport CMake targets from nmslib/hnswlib #446 to 0.7.0 release.
 )
 
 set(VCPKG_BUILD_TYPE "release") # header-only port
