6490988252f020038b6e33e47bf9756dc8b25a21
Jonliu1993 <13720414433@163.com>
2023-05-09 16:41:02 +0800

    update version

--- ports/lunasvg/portfile.cmake
+++ ports/lunasvg/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sammycage/lunasvg
-  REF f924651b85cac47dbe15f51a4aa320461fc1d07b #2.3.1+20230331
+  REF "${VERSION}"
   SHA512 519ca76a02da8faa4d07e6fc8a0db32c75c5b9abbe3584664037f02760e667e487248c0a445cd8ca5b0add0828cc1e0922a9c8a0aea52558d4fa083f6ae802f7
   HEAD_REF master
 )
