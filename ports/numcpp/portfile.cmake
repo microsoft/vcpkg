f4bf63b4f0f902d34aa17c3808629ffa46b01e37
Jonliu1993 <13720414433@163.com>
2023-05-09 16:56:27 +0800

    [numcpp] update to 2.10.1

--- ports/numcpp/portfile.cmake
+++ ports/numcpp/portfile.cmake
@@ -2,15 +2,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO dpilger26/NumCpp
-    REF bdbfa50f96b370f30bf7f130b445e714654daa76 #Version_2.8.0
-    SHA512 c59b0a8f71e19eb67337bcb8f3a9493c22d68056091cdf24d0d3fd7511b5590cc184f6271505220f6d8f3364ea03db7f84c6ccf2ce85cd44703d877d63e14704
+    REF "Version_${VERSION}"
+    SHA512 8474e38b2522b52235b38abb556c60284b7c44861b450f310bfa1a66938053006e47dde9106c73dad843bf049a2dd8988cbe2d2ba747a86f548c1f356f39e081
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DNUMCPP_NO_USE_BOOST=ON
 )
 
 vcpkg_cmake_install()
@@ -19,4 +17,4 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME NumCpp CONFIG_PATH share/NumCpp/cmake)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
