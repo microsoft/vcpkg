8ecc3f307fc13c61bd34e72a204f86ca77a41905
Simon Perkins <simon.perkins@gmail.com>
2023-05-10 14:33:06 +0200

    --without-cfitsio

--- ports/wcslib/portfile.cmake
+++ ports/wcslib/portfile.cmake
@@ -16,12 +16,12 @@ vcpkg_configure_make(
         --disable-flex
         --disable-fortran
         --without-pgplot
-        --with-libcfitsio-include=${CURRENT_INSTALLED_DIR}/include/cfitsio
-        --with-libcfitsio-lib=${CURRENT_INSTALLED_DIR}/lib
-)
+        --without-cfitsio)
 
 vcpkg_install_make(MAKEFILE GNUmakefile)
+vcpkg_fixup_pkgconfig()
 vcpkg_install_copyright(FILE_LIST "${src}/COPYING")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
