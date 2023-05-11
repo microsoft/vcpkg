570db285b8073dc9e358ecbea04f97007ef1f7dd
Kai Pastor <dg0yt@darc.de>
2023-05-11 09:33:42 +0200

    Fix cross builds

--- ports/infoware/portfile.cmake
+++ ports/infoware/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF d64a0c948593c0555115f60c79225c0b9ae09510
     SHA512 3794cb78a1422bfc065037abbae81259e6061ba7b12ebd7b88581118e8eeebaf92d80cf7793b0f9f1da6754baf52835a6891663593dd0b0a38009a9cb141082b
     HEAD_REF master
+    PATCHES
+        cross-build.diff
 )
 
 vcpkg_check_features(
@@ -14,6 +16,10 @@ vcpkg_check_features(
         opengl  INFOWARE_USE_OPENGL
         x11     INFOWARE_USE_X11
 )
+
+if(VCPKG_CROSSCOMPILING)
+    list(APPEND FEATURE_OPTIONS "-DHOST_PCI_DATA=${CURRENT_HOST_INSTALLED_DIR}/share/${PORT}/pci_data.hpp")
+endif()
 
 vcpkg_find_acquire_program(GIT)
 
@@ -29,5 +35,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
