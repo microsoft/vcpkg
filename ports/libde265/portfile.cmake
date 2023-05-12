77db12d57698e28fd1f7614a8a90a05a5adefbd5
Mengna-Li <543250287@qq.com>
2023-05-12 07:37:44 +0000

    fix usage issue

--- ports/libde265/portfile.cmake
+++ ports/libde265/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         fix-libde265-headers.patch
+        fix-interface-include.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
