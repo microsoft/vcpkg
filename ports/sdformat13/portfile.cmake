0fba32fb61686b77b0c79ed2416d5ebf43a37291
Kai Pastor <dg0yt@darc.de>
2023-05-13 19:34:05 +0200

    sdformat13 fixes (#1)
    * Fix cmake config export
    
    * Cleanup
    
    * Add usage
    
    * Update versions

--- ports/sdformat13/portfile.cmake
+++ ports/sdformat13/portfile.cmake
@@ -1,12 +1,12 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gazebosim/sdformat
-    REF sdformat13_${VERSION}
-    SHA512 180e28631c02d92bfd17d31ec32edb234e6c56c8fad011d04198481a39bffc26355b9001c598fa63a30ce47ad8abaeaed05e3008957c5e77df3a09b0170f0e6d
+    REF "sdformat13_${VERSION}"
+    SHA512 adc555527aadaede84d6fde11555bf4872028d9e895fc47c89f4077452fde9a52b233d60d0cbff5a098e261c7810aa2dc38a6275ec9f37ba4d0161af98e2aade
     HEAD_REF sdf13
     PATCHES
         no-absolute.patch
+        cmake-config.patch
 )
 
 # Ruby is required by the sdformat build process
@@ -14,22 +14,22 @@ vcpkg_find_acquire_program(RUBY)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS 
-        -DBUILD_TESTING=OFF
-        -DUSE_INTERNAL_URDF=OFF
-        -DUSE_EXTERNAL_TINYXML=ON
+    OPTIONS
         "-DRUBY=${RUBY}"
+        -DBUILD_TESTING=OFF
+        -DSKIP_PYBIND11=ON
+        -DUSE_INTERNAL_URDF=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
+        -DCMAKE_REQUIRE_FIND_PACKAGE_GzURDFDOM=ON
+        -DCMAKE_DISABLE_FIND_PACKAGE_Python3=ON
 )
 
 vcpkg_cmake_install()
-
-# Fix cmake targets and pkg-config file location
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/sdformat12")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/sdformat13")
 vcpkg_fixup_pkgconfig()
 
-# Remove debug files
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
