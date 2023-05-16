8d9bd306900a0bd0c9e4cd934122ef7c0535e1c3
Lily Wang <494550702@qq.com>
2023-05-16 01:22:40 -0700

    apply suggestions

--- ports/pdcurses/unofficial-pdcurses-config.cmake
+++ ports/pdcurses/unofficial-pdcurses-config.cmake
@@ -4,18 +4,22 @@ if(NOT TARGET unofficial::pdcurses::pdcurses)
     get_filename_component(z_vcpkg_pdcurses_root "${z_vcpkg_pdcurses_root}" PATH)
     get_filename_component(z_vcpkg_pdcurses_root "${z_vcpkg_pdcurses_root}" PATH)
     
-    find_library(Z_VCPKG_PDCURSES_LIBRARY_RELEASE NAMES pdcurses PATHS "${z_vcpkg_pdcurses_root}/lib" NO_DEFAULT_PATH REQUIRED)
-    
     set_target_properties(unofficial::pdcurses::pdcurses PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_pdcurses_root}/include"
-      IMPORTED_LOCATION_RELEASE "${Z_VCPKG_PDCURSES_LIBRARY_RELEASE}"
-      IMPORTED_CONFIGURATIONS "Release"
     )
+    
+    find_library(Z_VCPKG_PDCURSES_LIBRARY_RELEASE NAMES pdcurses PATHS "${z_vcpkg_pdcurses_root}/lib" NO_DEFAULT_PATH)
+    if(EXISTS "${Z_VCPKG_PDCURSES_LIBRARY_RELEASE}")
+        set_property(TARGET unofficial::pdcurses::pdcurses APPEND PROPERTY IMPORTED_CONFIGURATIONS "Release")
+        set_target_properties(unofficial::pdcurses::pdcurses PROPERTIES IMPORTED_LOCATION_RELEASE "${Z_VCPKG_PDCURSES_LIBRARY_RELEASE}")
+    endif()
 
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        find_library(Z_VCPKG_PDCURSES_LIBRARY_DEBUG NAMES pdcurses PATHS "${z_vcpkg_pdcurses_root}/debug/lib" NO_DEFAULT_PATH REQUIRED)
-        set_property(TARGET unofficial::pdcurses::pdcurses APPEND PROPERTY IMPORTED_CONFIGURATIONS "Debug")
-        set_target_properties(unofficial::pdcurses::pdcurses PROPERTIES IMPORTED_LOCATION_DEBUG "${Z_VCPKG_PDCURSES_LIBRARY_DEBUG}")
+        find_library(Z_VCPKG_PDCURSES_LIBRARY_DEBUG NAMES pdcurses PATHS "${z_vcpkg_pdcurses_root}/debug/lib" NO_DEFAULT_PATH)
+        if(EXISTS "${Z_VCPKG_PDCURSES_LIBRARY_DEBUG}")
+            set_property(TARGET unofficial::pdcurses::pdcurses APPEND PROPERTY IMPORTED_CONFIGURATIONS "Debug")
+            set_target_properties(unofficial::pdcurses::pdcurses PROPERTIES IMPORTED_LOCATION_DEBUG "${Z_VCPKG_PDCURSES_LIBRARY_DEBUG}")
+        endif()
     endif()
     
     unset(z_vcpkg_pdcurses_root)
