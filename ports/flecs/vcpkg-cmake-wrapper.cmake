acbe4fbd066225de419a45c0a48dbbf572ba45db
Monica <v-liumonica@microsoft.com>
2023-05-18 15:54:11 +0800

    remove vcpkg-cmake-wrapper.cmake

--- ports/flecs/vcpkg-cmake-wrapper.cmake
+++
@@ -1,9 +0,0 @@
-_find_package(${ARGS})
-
-if(TARGET flecs::flecs AND NOT TARGET flecs::flecs_static)
-    add_library(flecs::flecs_static INTERFACE IMPORTED)
-    set_target_properties(flecs::flecs_static PROPERTIES INTERFACE_LINK_LIBRARIES flecs::flecs)
-elseif(TARGET flecs::flecs_static AND NOT TARGET flecs::flecs)
-    add_library(flecs::flecs INTERFACE IMPORTED)
-    set_target_properties(flecs::flecs PROPERTIES INTERFACE_LINK_LIBRARIES flecs::flecs_static)
-endif()
