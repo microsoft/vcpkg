1a26cb48d508314ccc7735b3dbcd47eda3d3c636
sonodima <62671493+sonodima@users.noreply.github.com>
2023-05-19 09:12:25 +0200

    use ${VERSION} in ports/wmipp/portfile.cmake
    Co-authored-by: jim wang <122244446+jimwang118@users.noreply.github.com>

--- ports/wmipp/portfile.cmake
+++ ports/wmipp/portfile.cmake
@@ -1,7 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sonodima/wmipp
-    REF v1.0.0
+    REF "v${VERSION}"
+
     SHA512 e52ad6edadcb5b56c941d18e10968f10ea4fbbb0773132c2eb929e83cbf4aa22b72a161f55b358541e27786ff7f967786eb156b7ff519f8d3449d8b5ef2aa727 
     HEAD_REF main
 )
