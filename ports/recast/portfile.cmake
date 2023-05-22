000fe1f8b77eaaf3fbcfd63cd8dcc05c1dfe2186
jyu49 <yu_jack@apple.com>
2023-05-22 14:48:04 +0800

    Apply suggestion

--- ports/recast/portfile.cmake
+++ ports/recast/portfile.cmake
@@ -1,1 +1,4 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+file(READ "${CMAKE_CURRENT_LIST_DIR}/usage" usage)
+message(WARNING "find_package(unofficial-recast) is deprecated.\n${usage}")
--- ports/recastnavigation/portfile.cmake
+++ ports/recastnavigation/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/recastnavigation)
 
 vcpkg_fixup_pkgconfig()
 
