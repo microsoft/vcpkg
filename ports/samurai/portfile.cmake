41e8e44496b33f0c34212b46de768fcd40fd9e27
Loic Gouarin <loic.gouarin@gmail.com>
2023-05-22 13:20:43 +0200

    update version

--- ports/samurai/portfile.cmake
+++ ports/samurai/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hpc-maths/samurai
     REF "v${VERSION}"
-    SHA512 38750a3849023eeebaf356e399ff69f96eac8d696e366953ca5b689c038508b7e8dfea2bdb49c7f44ab909dcf94950160eabd3e6cbb55957c55461c008f17094
+    SHA512 3eb7b09b94ce3736ba5f9a20979be585a638e9c3818649f12e0dadd6d0ebe4c477f8404649029a4f2ecf2376a839b2ab2f2b2441132b510a8f8002cb5d360857
     HEAD_REF master
     PATCHES
         0001-add-hdf5-dependency-in-cmake.patch
@@ -19,7 +19,6 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DFETCHCONTENT_FULLY_DISCONNECTED=OFF
-        -DENABLE_VCPKG=ON
 )
 
 vcpkg_cmake_install()
