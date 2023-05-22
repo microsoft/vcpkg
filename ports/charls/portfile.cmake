56ecc662490c83527890fbe9f65033d6bda2ee67
Jonliu1993 <13720414433@163.com>
2023-05-22 16:56:19 +0800

    update function

--- ports/charls/portfile.cmake
+++ ports/charls/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/charls)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
 
 vcpkg_copy_pdbs()
 
