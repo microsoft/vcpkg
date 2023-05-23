86c03a243eef6cec5d0568f8f554cc9417eb6aad
Simon Perkins <simon.perkins@gmail.com>
2023-05-23 22:54:33 +0200

    Support various casacore features

--- ports/casacore/portfile.cmake
+++ ports/casacore/portfile.cmake
@@ -1,84 +1,48 @@
 include(vcpkg_find_fortran)
 
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT_DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   CURRENT_INSTALLED_DIR     = ${VCPKG_ROOT_DIR}\installed\${TRIPLET}
-#   DOWNLOADS                 = ${VCPKG_ROOT_DIR}\downloads
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#   VCPKG_TOOLCHAIN           = ON OFF
-#   TRIPLET_SYSTEM_ARCH       = arm x86 x64
-#   BUILD_ARCH                = "Win32" "x64" "ARM"
-#   MSBUILD_PLATFORM          = "Win32"/"x64"/${TRIPLET_SYSTEM_ARCH}
-#   DEBUG_CONFIG              = "Debug Static" "Debug Dll"
-#   RELEASE_CONFIG            = "Release Static"" "Release DLL"
-#   VCPKG_TARGET_IS_WINDOWS
-#   VCPKG_TARGET_IS_UWP
-#   VCPKG_TARGET_IS_LINUX
-#   VCPKG_TARGET_IS_OSX
-#   VCPKG_TARGET_IS_FREEBSD
-#   VCPKG_TARGET_IS_ANDROID
-#   VCPKG_TARGET_IS_MINGW
-#   VCPKG_TARGET_EXECUTABLE_SUFFIX
-#   VCPKG_TARGET_STATIC_LIBRARY_SUFFIX
-#   VCPKG_TARGET_SHARED_LIBRARY_SUFFIX
-#
-# 	See additional helpful variables in /docs/maintainers/vcpkg_common_definitions.md
-
-# Also consider vcpkg_from_* functions if you can; the generated code here is for any web accessable
-# source archive.
-#  vcpkg_from_github
-#  vcpkg_from_gitlab
-#  vcpkg_from_bitbucket
-#  vcpkg_from_sourceforge
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/casacore/casacore/archive/refs/tags/v3.5.0.tar.gz"
-    FILENAME "v3.5.0.tar.gz"
+vcpkg_from_github(
+    OUT_SOURCE_PATH src
+    REPO "casacore/casacore"
+    REF "v3.5.0"
     SHA512 5ec72450dc60b833864416850e08a4a0903f02b9917e0218aafcef15475dedce88318ea526f44e27b214acad14d31542fed7ea2462d6b9590d178c1085466db4
-)
-
-vcpkg_extract_source_archive(
-    src
-    ARCHIVE "${ARCHIVE}"
-    # (Optional) A friendly name to use instead of the filename of the archive (e.g.: a version number or tag).
-    # REF 1.0.0
-    # (Optional) Read the docs for how to generate patches at:
-    # https://github.com/Microsoft/vcpkg/blob/master/docs/examples/patching.md
     PATCHES
         001-casacore-cmake.patch
 )
 
-# # Check if one or more features are a part of a package installation.
-# # See /docs/maintainers/vcpkg_check_features.md for more details
-# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-#   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
-#     tbb   WITH_TBB
-#   INVERTED_FEATURES
-#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
-# )
-
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(BISON)
 vcpkg_find_fortran(fortran_args)
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tablelocking        ENABLE_TABLELOCKING
+        dysco               BUILD_DYSCO
+        threads             USE_THREADS
+        readline            USE_READLINE
+        adios2              USE_ADIOS2
+        hdf5                USE_HDF5
+        mpi                 USE_MPI
+        openmp              USE_OPENMP
+        python3             BUILD_PYTHON3
+        stacktrace          USE_STACKTRACE
+        casabuild           CASA_BUILD)
+
+if(${BUILD_PYTHON3} STREQUAL "ON")
+    message(FATAL_ERROR "Python3 Support not available: https://github.com/microsoft/vcpkg/discussions/29645")
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${src}"
     OPTIONS
-        -DUSE_THREADS=ON
+        ${FEATURE_OPTIONS}
         -DBUILD_PYTHON=OFF
-        -DBUILD_PYTHON3=OFF
-        -DBUILD_DYSCO=OFF
         -DBUILD_TESTING=OFF
         ${fortran_args}
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
 
-# Uncomment the line below if necessary to install the license file for the port
-# as a file named `copyright` to the directory `${CURRENT_PACKAGES_DIR}/share/${PORT}`
-# vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${src}/COPYING")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
