ee455af105b3b15b6bfa7dd50012c115d78fefbe
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2023-05-23 22:14:33 +0200

    Removed unnecessary added empty lines from portfile

--- ports/hidapi/portfile.cmake
+++ ports/hidapi/portfile.cmake
@@ -16,22 +16,17 @@ vcpkg_cmake_configure(
     OPTIONS -DHIDAPI_BUILD_HIDTEST=OFF
     ${FEATURE_OPTIONS}
 )
-
 vcpkg_cmake_install()
-
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
-
 
 if ("pp-data-dump" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES pp_data_dump AUTO_CLEAN)
 endif()
 
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/hidapi/libhidapi.cmake" "\"/hidapi\"" "\"\${_IMPORT_PREFIX}/include\"")
-
-
 
 file(INSTALL "${SOURCE_PATH}/LICENSE-bsd.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
