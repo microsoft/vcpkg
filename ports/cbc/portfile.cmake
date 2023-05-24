ae7615740629608a7231afe8983aa42aadaf81fa
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-05-24 13:52:06 +0200

    adapt to new version

--- ports/cbc/portfile.cmake
+++ ports/cbc/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO coin-or/Cbc
-    REF 6fe3addaa76436d479d4431add67b371e11d3e83
-    SHA512 1f2a756514e5aac66f710aa5af58b35a74747b9a9dd1d8817442afaca6a17c5ad42cc25c25b28e4bade8ab52a7561e467abe90864efffab25fb1fee82e975bd2
+    REF 133f1171f1667c930e46efd350f4be8ad995b231
+    SHA512 b9a1dceaed7b2f00e2dd35aefd8ddc0a5f55bbeba3ab21ea51034df79dabd69a6b3e46dbc05c6b2ed6059952c7164cd2ac0886d79e4dc85af92bef97f799be83
     PATCHES fix-c1083-error.patch
 )
 
