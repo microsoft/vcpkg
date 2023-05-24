a57f1d65d57fc052b09fee2ae07c4f7c879e4538
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-05-24 11:20:38 +0200

    [blake3] Backport DSO/DLL support

--- ports/blake3/portfile.cmake
+++ ports/blake3/portfile.cmake
@@ -1,5 +1,3 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BLAKE3-team/BLAKE3
@@ -8,7 +6,7 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-# these three files are included in the next release
+# these four files are included in the next release
 vcpkg_download_distfile(
     CMAKELISTS_SOURCE_PATH
     URLS https://raw.githubusercontent.com/BLAKE3-team/BLAKE3/76f9339312e1d52632a1cfb9df285c01911d99ce/c/CMakeLists.txt
@@ -27,9 +25,16 @@ vcpkg_download_distfile(
     FILENAME libblake3.pc.in
     SHA512 cfbaea63368e655c3ac3357f791b6332ae0241f3fd4f98e19c48f28e633e25a5b1125d1cc6b4815edfd013a76379dffcf9723852a7b76f2910d19dc77e538fa2
 )
+vcpkg_download_distfile( # we need the dllexport macro (otherwise these files are identical)
+    BLAKE3_H_SOURCE_PATH
+    URLS https://raw.githubusercontent.com/BLAKE3-team/BLAKE3/76f9339312e1d52632a1cfb9df285c01911d99ce/c/blake3.h
+    FILENAME blake3.h
+    SHA512 0f9852ff0e527e8fe0d567408c5c100ee3707a1b942ac1fbd78208a68ecfd32814729b5082720278d25d4cf45eb1020fcf3e4199cab0a67a6ca6af4321bcbc3e
+)
 file(COPY_FILE "${CMAKELISTS_SOURCE_PATH}" "${SOURCE_PATH}/c/CMakeLists.txt")
 file(COPY_FILE "${CMAKECONFIGIN_SOURCE_PATH}" "${SOURCE_PATH}/c/blake3-config.cmake.in")
 file(COPY_FILE "${PKGCONFIGIN_SOURCE_PATH}" "${SOURCE_PATH}/c/libblake3.pc.in")
+file(COPY_FILE "${BLAKE3_H_SOURCE_PATH}" "${SOURCE_PATH}/c/blake3.h")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/c"
