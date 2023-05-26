c08faf6017a1b37df238b3643d8691c2563cf845
Monica <v-liumonica@microsoft.com>
2023-05-26 18:39:55 +0800

    add 00016 patch

--- ports/grpc/portfile.cmake
+++ ports/grpc/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_from_github(
         snprintf.patch
         00012-fix-use-cxx17.patch
         00015-disable-download-archive.patch
+        00016-fix-gRPC_BUILD_CODEGEN.patch
 )
 
 if(NOT TARGET_TRIPLET STREQUAL HOST_TRIPLET)
