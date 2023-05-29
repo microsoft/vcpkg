6ecf71f705ce9d28e15ce145c8ee11d84262a54c
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2023-05-29 23:42:37 +0200

    restore original

--- ports/sqlite3/vcpkg-cmake-wrapper.cmake
+++ ports/sqlite3/vcpkg-cmake-wrapper.cmake
@@ -1,12 +1,12 @@
-find_package(unofficial-sqlite3 CONFIG)
-if(TARGET unofficial::sqlite3::sqlite3)
-    set(SQLite3_LIBRARY unofficial::sqlite3::sqlite3)
-    if(NOT TARGET SQLite::SQLite3)
-        add_library(SQLite::SQLite3 INTERFACE IMPORTED)
-        set_target_properties(SQLite::SQLite3 PROPERTIES
-            INTERFACE_LINK_LIBRARIES unofficial::sqlite3::sqlite3
-        )
-    endif()
+find_path(SQLite3_INCLUDE_DIRS NAMES sqlite3.h PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include" NO_DEFAULT_PATH)
+find_library(SQLite3_LIBRARY_RELEASE NAMES SQLite3 sqlite PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib" NO_DEFAULT_PATH)
+find_library(SQLite3_LIBRARY_DEBUG NAMES SQLite3 sqlite PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib" NO_DEFAULT_PATH)
+if(NOT SQLite3_INCLUDE_DIRS OR NOT (SQLite3_LIBRARY_RELEASE OR SQLite3_LIBRARY_DEBUG))
+    message(FATAL_ERROR "Broken installation of vcpkg port SQLite3")
 endif()
-
+if(CMAKE_VERSION VERSION_LESS 3.14)
+    include(SelectLibraryConfigurations)
+    select_library_configurations(SQLite3)
+    unset(SQLite3_FOUND)
+endif()
 _find_package(${ARGS})
