d333bb3df10dd027a3ba19f300b79441573c37ed
Kai Pastor <dg0yt@darc.de>
2023-05-29 21:36:28 +0200

    [vcpkg-tool-bazel] Fix installation, cleanup

--- ports/vcpkg-tool-bazel/portfile.cmake
+++ ports/vcpkg-tool-bazel/portfile.cmake
@@ -12,12 +12,10 @@ if(VCPKG_TARGET_IS_LINUX)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
         set(download_urls "https://github.com/bazelbuild/bazel/releases/download/${program_version}/bazel-${tool_subdirectory}-arm64")
         set(download_filename "bazel-${tool_subdirectory}-arm64")
-        set(raw_executable ON)
         set(download_sha512 11e953717f0edd599053a9c6ab849c266f6b34cd6f39dd99301a138aeb9d10113d055f7a2452f6ae601a9e9c19c816d22732958bb147e493dae9c63b13e0f1e0)
     else()
         set(download_urls "https://github.com/bazelbuild/bazel/releases/download/${program_version}/bazel-${tool_subdirectory}-x86_64")
         set(download_filename "bazel-${tool_subdirectory}-x86_64")
-        set(raw_executable ON)
         set(download_sha512 c9f117414f31bc85a1f6a91f3d1c0a4884a4bb346bb60b00599c2da8225d085f67bc865f1429c897681cb99471767171aed148c77ce80d9525841c873d9cc912)
     endif()
 elseif(VCPKG_TARGET_IS_OSX)
@@ -25,12 +23,10 @@ elseif(VCPKG_TARGET_IS_OSX)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
         set(download_urls "https://github.com/bazelbuild/bazel/releases/download/${program_version}/bazel-${tool_subdirectory}-arm64")
         set(download_filename "bazel-${tool_subdirectory}-arm64")
-        set(raw_executable ON)
         set(download_sha512 303b5c897eab93fb164dda53ecf6294fd3376a5de17a752388f4e7f612a8a537acc7d99a021ca616c1d7989d10c3c14cd87689dad60b9f654bf75ecc606bb23e)
     else()
         set(download_urls "https://github.com/bazelbuild/bazel/releases/download/${program_version}/bazel-${tool_subdirectory}-x86_64")
         set(download_filename "bazel-${tool_subdirectory}-x86_64")
-        set(raw_executable ON)
         set(download_sha512 609db0a2f9d6eab292271b44acf08978159ca43a90f3228e32afe430e830f5418a041480d75e5b502be192897693f6b80a9ab9e7ce549e3655e188c39d29baaf)
     endif()
 elseif(VCPKG_TARGET_IS_WINDOWS)
@@ -46,17 +42,15 @@ elseif(VCPKG_TARGET_IS_WINDOWS)
     endif()
 endif()
 
-vcpkg_download_distfile(archive_path
+vcpkg_download_distfile(program_path
     URLS ${download_urls}
     SHA512 "${download_sha512}"
     FILENAME "${download_filename}"
 )
-message(STATUS "archive_path: '${archive_path}'")
 
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/tools")
-file(INSTALL "${archive_path}"
+file(INSTALL "${program_path}"
     DESTINATION "${CURRENT_PACKAGES_DIR}/tools"
-    RENAME "${program}"
+    RENAME "${program}${VCPKG_TARGET_EXECUTABLE_SUFFIX}"
     FILE_PERMISSIONS
         OWNER_READ OWNER_WRITE OWNER_EXECUTE
         GROUP_READ GROUP_EXECUTE
