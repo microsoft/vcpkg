104a73e765cb9704b063c0ba088c9651844fd613
Mengna-Li <543250287@qq.com>
2023-05-30 02:52:22 +0000

    install .dll

--- ports/sqlitecpp/portfile.cmake
+++ ports/sqlitecpp/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
     SHA512 9702b17c55b1b3bc46a72d5c204b560249e9c1f02647c864fd4ca54011e4b0238638800ee870baa5106512a9568338d3faa9c9f9799d42fbd558d10376e3b73a
     PATCHES
         fix_dependency.patch
+        add_runtime_destination.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
