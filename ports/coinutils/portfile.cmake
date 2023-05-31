f8fb4930e8b6da8aac150454edbb974aadc4f6f6
FrankXie <v-frankxie@microsoft.com>
2023-05-31 02:01:42 -0700

    [coinutils] Add feature glpk

--- ports/coinutils/portfile.cmake
+++ ports/coinutils/portfile.cmake
@@ -11,12 +11,15 @@ file(COPY "${CURRENT_INSTALLED_DIR}/share/coin-or-buildtools/" DESTINATION "${SO
 set(ENV{ACLOCAL} "aclocal -I \"${SOURCE_PATH}/BuildTools\"")
 
 #--enable-msvc
+if("glpk" IN_LIST FEATURES)
+    string(APPEND with-glpk "--with-glpk")
+endif()
 
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
     OPTIONS
-        --with-glpk
+        ${with_glpk}
         --with-lapack
         --without-netlib
         --without-sample
