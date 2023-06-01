16c187f4d2b6a5897b0b39eeebba28a6b9454162
McFlyboy <hsn95@live.com>
2023-06-01 02:24:05 +0200

    Used VERSION variable

--- ports/vulkan-memory-allocator-hpp/portfile.cmake
+++ ports/vulkan-memory-allocator-hpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO YaaZ/VulkanMemoryAllocator-Hpp
-    REF v3.0.1-1
+    REF "v${VERSION}"
     SHA512 71709a889ea4527c2ee273521fe62b61bb87cda3e3c3ae2964cc18bd70ac69629aeed00b1e92d4470ba6cb08394813880018401847a6d4ed5c15e4ee1fb60ff1
     HEAD_REF master
 )
