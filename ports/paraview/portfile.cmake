7996ca0ba80fc059583ac1115cacd85f189e0cc9
Osyotr <Osyotr@users.noreply.github.com>
2023-06-04 23:56:02 +0300

    [paraview] don't hardcode python version

--- ports/paraview/portfile.cmake
+++ ports/paraview/portfile.cmake
@@ -76,7 +76,10 @@ file(COPY "${ICET_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/ThirdParty/IceT/vtk
 if("python" IN_LIST FEATURES)
     set(python_ver "")
     if(NOT VCPKG_TARGET_IS_WINDOWS)
-        set(python_ver 3.10)
+        file(GLOB _py3_include_path "${CURRENT_HOST_INSTALLED_DIR}/include/python3*")
+        string(REGEX MATCH "python3\\.([0-9]+)" _python_version_tmp ${_py3_include_path})
+        set(PYTHON_VERSION_MINOR "${CMAKE_MATCH_1}")
+        set(python_ver "3.${PYTHON_VERSION_MINOR}")
     endif()
     list(APPEND ADDITIONAL_OPTIONS
         -DPython3_FIND_REGISTRY=NEVER
