e7c0690a484adeef3bf39895887a63ad49c60358
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-06-04 21:29:05 +0200

    vcpkg_cmake_config_fixup: add PACKAGE_NAME

--- ports/libre/portfile.cmake
+++ ports/libre/portfile.cmake
@@ -27,7 +27,7 @@ vcpkg_cmake_install()
 
 vcpkg_fixup_pkgconfig()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/re)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/re PACKAGE_NAME re)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
