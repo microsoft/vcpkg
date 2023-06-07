c77e7459edfd223a73b458544694486c5d106078
jacobkahn <jacobkahn1@gmail.com>
2023-06-07 15:27:14 -0700

    Use  in portfile

--- ports/flashlight-text/portfile.cmake
+++ ports/flashlight-text/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO flashlight/text
-    REF v0.0.4
+    REF v${VERSION}
     SHA512 d9ed9b687c441e356b19a035ecef1abc76d90e3e0d8a9c32899bd7a81e379a9ae51bc6d68e7bae10cf83b87ec76289afbd81909d70799ca8d24f996e8667ad85
     HEAD_REF main
 )
