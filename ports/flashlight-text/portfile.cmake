453d718edacc441e35fb802e00cac4359e9402a5
jacobkahn <jacobkahn1@gmail.com>
2023-06-07 22:55:56 -0700

    Omit config fixup arg

--- ports/flashlight-text/portfile.cmake
+++ ports/flashlight-text/portfile.cmake
@@ -25,7 +25,7 @@ vcpkg_cmake_configure(
 
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME ${PORT})
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
