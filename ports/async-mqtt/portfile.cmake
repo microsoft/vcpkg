e76f5e611f3a7bab59add5730c32be7636d230a4
Takatoshi Kondo <redboltz@gmail.com>
2023-06-09 18:45:06 +0900

    Reflect review comments.
    Add header-only on the top of file.
    Use ${VERSION} instead of the literal.
    Fix indent.
    Add double qoute to path
    Remove copyright install.

--- ports/async-mqtt/portfile.cmake
+++ ports/async-mqtt/portfile.cmake
@@ -1,7 +1,9 @@
+#header-only library
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO redboltz/async_mqtt
-    REF 1.0.1
+    REF "${VERSION}"
     SHA512 02ce6cd4deeef5bf539366ebe9d3e273b232e44788b2fc2fe77329e77e5340322a05808a26c47d003db6dab8d3df2bfe5a3f1c42842cf48ec7960f9327447c5d
     HEAD_REF main
 )
@@ -9,19 +11,17 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-    -DASYNC_MQTT_BUILD_TOOLS=OFF
-    -DASYNC_MQTT_BUILD_EXAMPLES=OFF
-    -DASYNC_MQTT_BUILD_UNIT_TESTS=OFF
-    -DASYNC_MQTT_BUILD_SYSTEM_TESTS=OFF
-    -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
+        -DASYNC_MQTT_BUILD_TOOLS=OFF
+        -DASYNC_MQTT_BUILD_EXAMPLES=OFF
+        -DASYNC_MQTT_BUILD_UNIT_TESTS=OFF
+        -DASYNC_MQTT_BUILD_SYSTEM_TESTS=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME async_mqtt_iface CONFIG_PATH lib/cmake/async_mqtt_iface)
+vcpkg_cmake_config_fixup(PACKAGE_NAME async_mqtt_iface CONFIG_PATH "lib/cmake/async_mqtt_iface")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
