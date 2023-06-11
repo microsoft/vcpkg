caa5574f72546b6d4ebcd5beb4fab8bf3da30e98
Osyotr <Osyotr@users.noreply.github.com>
2023-06-11 23:38:23 +0300

    [vcpkg-get-python-packages] Delete unused file, update hardcoded python version

--- ports/vcpkg-get-python-packages/portfile.cmake
+++ ports/vcpkg-get-python-packages/portfile.cmake
@@ -2,7 +2,6 @@ file(COPY
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/copyright"
     "${CMAKE_CURRENT_LIST_DIR}/x_vcpkg_get_python_packages.cmake"
-    "${CMAKE_CURRENT_LIST_DIR}/python310._pth"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 include("${CMAKE_CURRENT_LIST_DIR}/x_vcpkg_get_python_packages.cmake")
