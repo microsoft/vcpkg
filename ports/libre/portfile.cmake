6b2d69a78d28828f6356c0f7bb420a4c220c9cd0
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2023-06-12 09:15:52 +0200

    patch from Kai

--- ports/libre/portfile.cmake
+++ ports/libre/portfile.cmake
@@ -1,3 +1,6 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -5,13 +8,12 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 fce61b96fd4a330c82045c470486e90e1128e242b7c311e9c6002d9752b775bbc430bf256de85f63841172c60630be3aa91e11bfa1501c71599d213701c6b459
     HEAD_REF main
+    PATCHES
+        wip.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" LIBRE_BUILD_SHARED)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" LIBRE_BUILD_STATIC)
-
-set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
-set(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -22,14 +24,11 @@ vcpkg_cmake_configure(
         -DCMAKE_REQUIRE_FIND_PACKAGE_OpenSSL=ON
         -DCMAKE_REQUIRE_FIND_PACKAGE_ZLIB=ON
 )
-
 vcpkg_cmake_install()
-
 vcpkg_fixup_pkgconfig()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/re PACKAGE_NAME re)
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libre)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
