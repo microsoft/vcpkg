baf99dedf92296a881c02157ce6e06f8a7c33d92
endingly <jiasongdeng@qq.com>
2023-06-13 14:20:30 +0800

    Update ports/games101-cgl/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/games101-cgl/portfile.cmake
+++ ports/games101-cgl/portfile.cmake
@@ -18,9 +18,5 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(
-    INSTALL "${SOURCE_PATH}/license"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright
-)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/license")
 
