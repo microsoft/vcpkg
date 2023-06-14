edd4de0a1b90e2ae772f21f5917ec6fcf6879512
Adam Casey <acasey23@bloomberg.net>
2023-06-14 16:26:40 +0100

    Review comments

--- ports/bde/portfile.cmake
+++ ports/bde/portfile.cmake
@@ -1,11 +1,6 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-set(BDE_VERSION 3.117.0.0)
-set(BDE_TOOLS_VERSION 3.117.0.0)
-
-# Paths used in build
-set(SOURCE_PATH_DEBUG ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/bde-${BDE_VERSION})
-set(SOURCE_PATH_RELEASE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/bde-${BDE_VERSION})
+set(BDE_TOOLS_VERSION "${VERSION}")
 
 # Acquire Python and add it to PATH
 vcpkg_find_acquire_program(PYTHON3)
@@ -15,49 +10,38 @@ get_filename_component(PYTHON3_EXE_PATH ${PYTHON3} DIRECTORY)
 vcpkg_from_github(
     OUT_SOURCE_PATH TOOLS_PATH
     REPO "bloomberg/bde-tools"
-    REF 3.117.0.0
+    REF "${BDE_TOOLS_VERSION}"
     SHA512 3c39da8d1ea40459e36e11ada93cc2821ae1b16a831f93cccab463996394a400cc08bb1654642eae1aa5187f139d7fb80c4729e464051eee182133eb8a74158d
-    HEAD_REF 3.117.0.0
+    HEAD_REF main
 )
 
 message(STATUS "Configure bde-tools-v${BDE_TOOLS_VERSION}")
-if(VCPKG_CMAKE_SYSTEM_NAME)
-    set(ENV{PATH} "$ENV{PATH}:${PYTHON3_EXE_PATH}")
-    set(ENV{PATH} "$ENV{PATH}:${TOOLS_PATH}/bin")
-else()
-    set(ENV{PATH} "$ENV{PATH};${PYTHON3_EXE_PATH}")
-    set(ENV{PATH} "$ENV{PATH};${TOOLS_PATH}/bin")
-endif()
+vcpkg_add_to_path("${PYTHON3_EXE_PATH}")
+vcpkg_add_to_path("${TOOLS_PATH}/bin")
 
 # Acquire BDE sources
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "bloomberg/bde"
-    REF 3.117.0.0
+    REF "${VERSION}"
     SHA512 810b4a06a08739dcd990751dd543aa7dc58355f9d64a7c96ef0cf45c81501946434db42ad5bcf5d16110d5a463586b587ce09a446136e824298f39a8a871b490
-    HEAD_REF 3.117.0.0
+    HEAD_REF main
 )
-
-# Clean up previous builds
-file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel
-                    ${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg)
-
-set(BDE_OPTIONS "-DBDE_BUILD_TARGET_CPP17=ON" "-DCMAKE_CXX_STANDARD=17" "-DCMAKE_CXX_STANDARD_REQUIRED=ON" "-DCMAKE_CXX_EXTENSIONS=OFF")
-
-# Configure release
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    list(APPEND BDE_OPTIONS "-DBDE_BUILD_TARGET_OPT=1")
-else()
-    list(APPEND BDE_OPTIONS "-DBDE_BUILD_TARGET_DBG=1")
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     WINDOWS_USE_MSBUILD
     OPTIONS 
-    ${BDE_OPTIONS}
-    -DBBS_BUILD_SYSTEM=1
-    -DBdeBuildSystem_DIR:PATH=${TOOLS_PATH}/BdeBuildSystem
+        -DBDE_BUILD_TARGET_CPP17=ON
+        -DCMAKE_CXX_STANDARD=17
+        -DCMAKE_CXX_STANDARD_REQUIRED=ON
+        -DCMAKE_CXX_EXTENSIONS=OFF
+        -DBBS_BUILD_SYSTEM=1
+        "-DBdeBuildSystem_DIR:PATH=${TOOLS_PATH}/BdeBuildSystem"
+    OPTIONS_RELEASE
+        -DBDE_BUILD_TARGET_OPT=1
+    OPTIONS_DEBUG
+        -DBDE_BUILD_TARGET_DBG=1
 )
 
 # Build release
