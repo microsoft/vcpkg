756e5306d3cd936eebe7f7e4646f3d1176c8c5b5
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-06-16 08:17:27 +0200

    Apply suggestions from code review
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/coin-or-cbc/portfile.cmake
+++ ports/coin-or-cbc/portfile.cmake
@@ -3,7 +3,10 @@ vcpkg_from_github(
     REPO coin-or/Cbc
     REF 6f83969e50b6f67c60654704c1f71f970c621a3b
     SHA512 f56b5806b2acffe7259410254640009f9b7a27713972d771b1083a05ca197a65715b007f42f0b8bf6e87b2a889e2889a7466222e0b06f01a4c5297cbaf455c4d
-    PATCHES "dep.patch" "pkgconf_win.patch" "disable_glpk.patch"
+    PATCHES
+        dep.patch
+        pkgconf_win.patch
+        disable_glpk.patch
 )
 
 file(COPY "${CURRENT_INSTALLED_DIR}/share/coin-or-buildtools/" DESTINATION "${SOURCE_PATH}")
@@ -11,19 +14,19 @@ file(COPY "${CURRENT_INSTALLED_DIR}/share/coin-or-buildtools/" DESTINATION "${SO
 set(ENV{ACLOCAL} "aclocal -I \"${SOURCE_PATH}/BuildTools\"")
 
 vcpkg_configure_make(
-SOURCE_PATH "${SOURCE_PATH}"
-USE_WRAPPERS
-OPTIONS
-    --with-coinutils
-    --with-clp
-    --with-cgl
-    --with-osi
-    --without-ositests
-    --without-sample
-    --without-netlib
-    --without-miplib3
-    --enable-relocatable
-    --disable-readline
+    SOURCE_PATH "${SOURCE_PATH}"
+    USE_WRAPPERS
+    OPTIONS
+        --with-coinutils
+        --with-clp
+        --with-cgl
+        --with-osi
+        --without-ositests
+        --without-sample
+        --without-netlib
+        --without-miplib3
+        --enable-relocatable
+        --disable-readline
 )
 
 vcpkg_install_make()
@@ -33,4 +36,4 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
