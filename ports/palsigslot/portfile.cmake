38dbebc75f444ecab684ee006e6899708729f5d4
chausner <chausner@users.noreply.github.com>
2023-06-17 01:31:39 +0200

    Add back HEAD_REF

--- ports/palsigslot/portfile.cmake
+++ ports/palsigslot/portfile.cmake
@@ -3,6 +3,7 @@ vcpkg_from_github(
     REPO palacaze/sigslot
     REF "v${VERSION}"
     SHA512 ed8614d9c2e418259b1bce6d0a528b54939700876969c5e8dfcfc466c167690180cb18324602f83d521c79dcba2565e5317647416b70cf8773860ab956d9b62f
+    HEAD_REF master
 )
 
 vcpkg_cmake_configure(
