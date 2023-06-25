995737b5e0dae14f5358c7d7cc4bed8cee4378f2
Charles Milette <charles.milette@gmail.com>
2023-06-24 21:20:15 -0400

    [amd-adl-sdk] new port

---
+++ ports/amd-adl-sdk/portfile.cmake
@@ -0,0 +1,11 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO GPUOpen-LibrariesAndSDKs/display-library
+        REF 881fa21e177203a30eb498d43bcbbec3d9b18d37
+        SHA512 e65bcb840929bfcc840abb361d92245c511948567c659e75f0b2784dc869cd851904819e71281afde5b4f3000ef0de7e3d33a32e5102110a26daca898414ceae
+        HEAD_REF master
+)
+
+# Install the ADL headers to the default vcpkg location
+file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Public-Documents/ADL SDK EULA.pdf")
