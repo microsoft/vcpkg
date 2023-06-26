143e9ec7131765906c2e93ada05ae603a2041e0a
FrankXie <v-frankxie@microsoft.com>
2023-06-26 01:14:06 -0700

    libexif

--- ports/libexif/unofficial-libexif-config.cmake
+++ ports/libexif/unofficial-libexif-config.cmake
@@ -1,36 +1,30 @@
+
+find_library(Z_VCPKG_LIBEXIF_LIBRARY_RELEASE NAMES libexif PATHS "${z_vcpkg_libexif_root}/lib" NO_DEFAULT_PATH)
+find_library(Z_VCPKG_LIBEXIF_LIBRARY_DEBUG NAMES libexif PATHS "${z_vcpkg_libexif_root}/debug/lib" NO_DEFAULT_PATH)
+
 if(NOT TARGET unofficial::libexif::libexif)
     add_library(unofficial::libexif::libexif UNKNOWN IMPORTED)
-    get_filename_component(z_vcpkg_pdcurses_root "${CMAKE_CURRENT_LIST_FILE}" PATH)
-    get_filename_component(z_vcpkg_pdcurses_root "${z_vcpkg_pdcurses_root}" PATH)
-    get_filename_component(z_vcpkg_pdcurses_root "${z_vcpkg_pdcurses_root}" PATH)
+    get_filename_component(z_vcpkg_LIBEXIF_root "${CMAKE_CURRENT_LIST_FILE}" PATH)
+    get_filename_component(z_vcpkg_LIBEXIF_root "${z_vcpkg_libexif_root}" PATH)
+    get_filename_component(z_vcpkg_LIBEXIF_root "${z_vcpkg_libexif_root}" PATH)
 
     set_target_properties(unofficial::libexif::libexif PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_pdcurses_root}/include"
+      INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_libexif_root}/include"
     )
-
-    find_library(Z_VCPKG_PDCURSES_LIBRARY_RELEASE NAMES libexif PATHS "${z_vcpkg_pdcurses_root}/lib" NO_DEFAULT_PATH)
-    find_file(Z_VCPKG_PDCURSES_DLL_RELEASE NAMES libexif.dll PATHS "${z_vcpkg_pdcurses_root}/bin" NO_DEFAULT_PATH)
-    if(EXISTS "${Z_VCPKG_PDCURSES_LIBRARY_RELEASE}")
+    
+    if(EXISTS "${Z_VCPKG_LIBEXIF_LIBRARY_RELEASE}")
         set_property(TARGET unofficial::libexif::libexif APPEND PROPERTY IMPORTED_CONFIGURATIONS "Release")
         set_target_properties(unofficial::libexif::libexif PROPERTIES
-            IMPORTED_IMPLIB_RELEASE "${Z_VCPKG_PDCURSES_LIBRARY_RELEASE}"
+            IMPORTED_IMPLIB_RELEASE "${Z_VCPKG_LIBEXIF_LIBRARY_RELEASE}"
         )
     endif()
-    if(EXISTS "${Z_VCPKG_PDCURSES_DLL_RELEASE}")
-        set_target_properties(unofficial::libexif::libexif PROPERTIES IMPORTED_LOCATION_RELEASE "${Z_VCPKG_PDCURSES_DLL_RELEASE}")
-    endif()
-
-    find_library(Z_VCPKG_PDCURSES_LIBRARY_DEBUG NAMES libexif PATHS "${z_vcpkg_pdcurses_root}/debug/lib" NO_DEFAULT_PATH)
-    find_file(Z_VCPKG_PDCURSES_DLL_DEBUG NAMES libexif.dll PATHS "${z_vcpkg_pdcurses_root}/debug/bin" NO_DEFAULT_PATH)
-    if(EXISTS "${Z_VCPKG_PDCURSES_LIBRARY_DEBUG}")
+  
+    if(EXISTS "${Z_VCPKG_LIBEXIF_LIBRARY_DEBUG}")
         set_property(TARGET unofficial::libexif::libexif APPEND PROPERTY IMPORTED_CONFIGURATIONS "Debug")
         set_target_properties(unofficial::libexif::libexif PROPERTIES
-            IMPORTED_IMPLIB_DEBUG "${Z_VCPKG_PDCURSES_LIBRARY_DEBUG}"
+            IMPORTED_IMPLIB_DEBUG "${Z_VCPKG_LIBEXIF_LIBRARY_DEBUG}"
         )
     endif()
-    if(EXISTS "${Z_VCPKG_PDCURSES_DLL_DEBUG}")
-        set_target_properties(unofficial::libexif::libexif PROPERTIES IMPORTED_LOCATION_DEBUG "${Z_VCPKG_PDCURSES_DLL_DEBUG}")
-    endif()
 
-    unset(z_vcpkg_pdcurses_root)
+    unset(z_vcpkg_LIBEXIF_root)
 endif()
