bbe0440388472a4ad8edb0092fbba50d0f308f18
Mengna-Li <543250287@qq.com>
2023-06-27 02:24:48 +0000

    fix usage

--- ports/baresip-libre/portfile.cmake
+++ ports/baresip-libre/portfile.cmake
@@ -27,7 +27,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libre)
+vcpkg_cmake_config_fixup(PACKAGE_NAME libre CONFIG_PATH lib/cmake/libre)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
