2eadaea7cbeab8e58067e30feb5cd1bdcf122e70
FrankXie <v-frankxie@microsoft.com>
2023-06-28 00:58:08 -0700

    [zeromq] Enable libsodium with feature curve

--- ports/zeromq/portfile.cmake
+++ ports/zeromq/portfile.cmake
@@ -14,15 +14,19 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        sodium          WITH_LIBSODIUM
         draft           ENABLE_DRAFTS
         websockets      ENABLE_WS
         websockets-secure WITH_TLS
+        curve           ENABLE_CURVE
 )
 
 set(PLATFORM_OPTIONS "")
 if(VCPKG_TARGET_IS_MINGW)
     set(PLATFORM_OPTIONS -DCMAKE_SYSTEM_VERSION=6.0 -DZMQ_HAVE_IPC=0)
+endif()
+
+if("curve" IN_LIST FEATURES)
+    list(APPEND FEATURE_OPTIONS -DWITH_LIBSODIUM=ON)
 endif()
 
 vcpkg_cmake_configure(
