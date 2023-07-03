5e0e3c2538b089ac19c1f718eada6d6b94a3f0ea
Monica <v-liumonica@microsoft.com>
2023-07-03 17:36:19 +0800

    update croncpp

--- ports/croncpp/portfile.cmake
+++ ports/croncpp/portfile.cmake
@@ -2,25 +2,22 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO mariusbancila/croncpp
-  REF 11cce4666a06c40346c7ba380ddd90c53806809d #master on 9/4/2020
-  SHA512 8f4d892ce90d8eca3711b21728bb599bf64857b20c0b143c5277687d0b6e5d5b8bf3e6dc7f9e8d028ba4e5ee711a5a9e750bcc2f771177d2f659c0c19e12207a
+  REF "${VERSION}"
+  SHA512 77d4ff1ff121d5a924d79b880045100cb128123a56bd97ba70342316b1d8db283ea0460d24f8a60eb231bd9187c0c8b5237742550d40f06ddbe7cfd03bfc4d48
   HEAD_REF master
-  PATCHES
-    0001-fix-cmake.patch
-    no-test.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-      -DBUILD_TESTS=OFF
+      -DCRONCPP_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
