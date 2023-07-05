0948256da15be26ccf4662ac7412d46e3a54ca79
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-07-05 15:53:31 +0200

    fix zstd in boost-iostreams

--- ports/boost-iostreams/portfile.cmake
+++ ports/boost-iostreams/portfile.cmake
@@ -6,7 +6,9 @@ vcpkg_from_github(
     REF boost-1.82.0
     SHA512 2949f3612cbc809f723a16168ccfb39249ba1e730ec9fa5201a41189bbce22b35e9adeb1478f7cbf012346f67ac7d8841770c8f977711ef0aea0fd6dd01dd785
     HEAD_REF master
-    PATCHES Removeseekpos.patch
+    PATCHES 
+        Removeseekpos.patch
+        fix-zstd.diff
 )
 
 set(FEATURE_OPTIONS "")
