59cd39e4908b4d80680c12a0a2484c0d7c9831c3
Nick Anderson <105019436+niande-xbox@users.noreply.github.com>
2023-07-07 10:13:38 -0700

    Update REF after updating opentelemetry-cpp-contrib per Lily's suggestion. Remove BUILD_EXAMPLE. Add needed /debug/share removal.

--- ports/opentelemetry-fluentd/portfile.cmake
+++ ports/opentelemetry-fluentd/portfile.cmake
@@ -5,20 +5,20 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-telemetry/opentelemetry-cpp-contrib
-    REF 20721e51235565020fc7fef2ba1aee92fc5744e8  # Maps to 2.0.0
+    REF 49a42a3bd950d3359f0b3921c13f6adc7cfc793a  # Maps to 2.0.0
     HEAD_REF main
-    SHA512 5761eb75a16f558b6ca1abcbb38a1f2f33c02f045f54972ffdcdeb35ec12b340930b3c119f55602ff80772ca1de1fdfffd4f89d158779e33d0c92e95460c7bb1
+    SHA512 a2d621363860b0ff391ddf53cb2a293c7652c17d10e99bd9d085969df64a8f13b32e550124879833dd43f9e9428a422bc1ae7ca4a737722e7b24be886d6b576d
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/exporters/fluentd"
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DBUILD_EXAMPLE=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
