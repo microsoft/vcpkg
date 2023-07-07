cffa6925adc0e45190e52c92c4a1dd9eb019792b
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-07-06 21:04:21 -0700

    Dependency search fix

--- ports/azure-core-amqp-cpp/portfile.cmake
+++ ports/azure-core-amqp-cpp/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO Azure/azure-sdk-for-cpp
     REF azure-core-amqp_1.0.0-beta.1
     SHA512 4fc520c546cd395d4c50a9385d33db09b50865046b5383b1442057d4ac78ffc6a6be221af6aef3dcb938ffde1927e8de4270d5b1c9cf5d97ac8b1581b51d014b
+    PATCHES
+        00100-fix_dependency.patch
 )
 
 vcpkg_cmake_configure(
