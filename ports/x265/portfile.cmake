b4704c47ded493b9b2ee6daf31871a6d034ca9ae
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-07-08 13:50:13 -0600

    [x265] Update to version 3.5

--- ports/x265/portfile.cmake
+++ ports/x265/portfile.cmake
@@ -1,8 +1,8 @@
-vcpkg_from_github(
+vcpkg_from_bitbucket(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO videolan/x265
-    REF 07295ba7ab551bb9c1580fdaee3200f1b45711b7 #v3.4
-    SHA512 21a4ef8733a9011eec8b336106c835fbe04689e3a1b820acb11205e35d2baba8c786d9d8cf5f395e78277f921857e4eb8622cf2ef3597bce952d374f7fe9ec29
+    REPO multicoreware/x265_git
+    REF 3.5 #v3.5
+    SHA512 e95e454b438114cf90e32818847afa65b54caf69442a4a39dc92f125a7ec6f99c83ec509549ced3395cd5a77305abef0ecdad38b4a359f82fb17fce6c4c7cc7a
     HEAD_REF master
     PATCHES
         disable-install-pdb.patch
