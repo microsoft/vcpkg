39367aaff7c7d2c5dde7670959211da20485a2fa
Kai Pastor <dg0yt@darc.de>
2023-07-09 14:22:33 +0200

    [vcpkg-ci-libgit2] Cleanup

--- scripts/test_ports/vcpkg-ci-libgit2/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-libgit2/portfile.cmake
@@ -1,4 +1,1 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-file(COPY_FILE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig/libssh2.pc" "${CURRENT_BUILDTREES_DIR}/libssh2.pc.log")
-file(COPY_FILE "${CURRENT_INSTALLED_DIR}/lib/pkgconfig/libgit2.pc" "${CURRENT_BUILDTREES_DIR}/libgit2.pc.log")
-message(FATAL_ERROR STOP)
