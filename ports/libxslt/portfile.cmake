17a53fd07cbaf949dfe47802bfcc88dd97c3613c
Marc <31337222+marcbull@users.noreply.github.com>
2023-07-12 12:12:59 +0200

    [libxslt] set license id and use vcpkg_install_copyright()

--- ports/libxslt/portfile.cmake
+++ ports/libxslt/portfile.cmake
@@ -80,4 +80,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libxslt")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/Copyright" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Copyright")
