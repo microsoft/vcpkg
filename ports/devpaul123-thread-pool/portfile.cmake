7650a06354156a4427b25b8e53bc961b9d5ef082
Paul T <DeveloperPaul123@users.noreply.github.com>
2023-07-13 15:15:32 -0400

    Update ports/devpaul123-thread-pool/portfile.cmake
    Co-authored-by: Mengna Li <95600143+Adela0814@users.noreply.github.com>

--- ports/devpaul123-thread-pool/portfile.cmake
+++ ports/devpaul123-thread-pool/portfile.cmake
@@ -7,8 +7,7 @@ vcpkg_from_github(
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    GENERATOR "Ninja"
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DTP_BUILD_TESTS=OFF
         -DTP_BUILD_EXAMPLES=OFF
