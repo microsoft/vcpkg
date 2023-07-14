5d2834360fe55cd9688fa7c9ad02c89bb250749f
Cheney-Wang <850426846@qq.com>
2023-07-14 01:10:10 -0700

    fix conflicts

--- ports/librsvg/portfile.cmake
+++ ports/librsvg/portfile.cmake
@@ -34,4 +34,5 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
