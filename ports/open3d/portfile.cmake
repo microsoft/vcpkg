9abf918badf90e483b8a84a4a2c0933beecc96b2
Omri Kalinsky <omri@polycam.ai>
2023-07-14 16:29:55 -0500

    Fixing missing mime type with external textures and texture pass through in Open3D.

--- ports/open3d/portfile.cmake
+++ ports/open3d/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO polycam/open3d
-    REF a0d85e641a250762c8fa6a6ee9d9e16df9e0e11e
+    REF 7d094c210c6f15cc2dd9f9bd0b49a88c5c630b3b
     SHA512 3a421b1c34f8e38d4802ff7fadc994c9f2b0bcbe03c51b97402661115c0a088294731fbdf7a67217f38b111c6e24e20d8c293daf03a0fbc1b5993135dfc1ae75
     HEAD_REF main
 )
