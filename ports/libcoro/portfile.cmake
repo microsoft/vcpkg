0493a8540f5ef251705be4a36ccc88d984955cb5
chausner <chausner@users.noreply.github.com>
2023-07-15 12:41:42 +0200

    Fix hash

--- ports/libcoro/portfile.cmake
+++ ports/libcoro/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jbaldwin/libcoro
     REF "v${VERSION}"
-    SHA512 746607793ba2e0c809e731b39423fa84e451a67129ba311dc3ccc257d5d31be0f94826ca1bffc8a88f9f4675ba4224d3290bab6e46116bcdee50acb2f96fbf81
+    SHA512 5e9a820ac6623bd12e04090eb61eecf1e26bd230b0106ab5af13568d12f20ea39e58b2f126ae1262233c1141b23d9466397e8ee42b5342a500d8cca26a34d81c
     HEAD_REF master
     PATCHES
         0001-allow-shared-lib.patch
