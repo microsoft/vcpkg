4d373463287fb14e0c58cb6b0432df7f2c82d095
Charles Milette <charles.milette@gmail.com>
2023-07-16 19:06:13 -0400

    [amd-adl-sdk] Add copyright and fix SHA512

--- ports/amd-adl-sdk/portfile.cmake
+++ ports/amd-adl-sdk/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO GPUOpen-LibrariesAndSDKs/display-library
     REF "${VERSION}"
-    SHA512 e65bcb840929bfcc840abb361d92245c511948567c659e75f0b2784dc869cd851904819e71281afde5b4f3000ef0de7e3d33a32e5102110a26daca898414ceae
+    SHA512 805bc1a7f221b33955d79943833d04838b459f316c2a9ad5fa1831588b07c0bbe5975aca07c90117c10c6ff22ee12a69d5a26a75e7191eb6c40c1dccccd192af
     HEAD_REF master
 )
 
 # Install the ADL headers to the default vcpkg location
 file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Public-Documents/ADL SDK EULA.pdf")
+vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/COPYRIGHT.md")
