9a6cc4beba43ddd3873bfda2459808882299bec3
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-29 15:37:15 -0400

    Add cpptrace port

---
+++ ports/cpptrace/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO jeremy-rifkin/cpptrace
+	REF 4b11b87e4d905d003d0325a53994441cc767017a
+	SHA512
+5ed41245ec7aef8d769d7a0718293817d93d75008abd1290b52a42b2c33bbc464fa91f1501856d68935489d81e4002634d26a21fc879eb7d5322327831ea2821
+	HEAD_REF main
+)
+
+vcpkg_configure_cmake(
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+)
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright)
