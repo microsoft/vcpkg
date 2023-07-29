792a43cdf83c26d9b2b8f5bc6d626cc56e0a8fcd
myd7349 <myd7349@gmail.com>
2023-07-29 18:13:12 +0800

    [libxdf] Use vcpkg_install_copyright

--- ports/libxdf/portfile.cmake
+++ ports/libxdf/portfile.cmake
@@ -20,4 +20,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
