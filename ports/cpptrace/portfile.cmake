199be1f7718fa1e9eee27670a18a482309c6b335
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-07-29 15:43:58 -0400

    Update portfile

--- ports/cpptrace/portfile.cmake
+++ ports/cpptrace/portfile.cmake
@@ -7,12 +7,11 @@ vcpkg_from_github(
 	HEAD_REF main
 )
 
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
 	SOURCE_PATH "${SOURCE_PATH}"
-	PREFER_NINJA
 )
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
