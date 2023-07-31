e3bc091a3d7727456187107a8e2150a82c913c45
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2023-07-31 09:30:03 +0200

    Add rhash patch

--- ports/rhasheq/portfile.cmake
+++ ports/rhasheq/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF cf5442f6468871beb6088991501e6ba052fe4467
     SHA512 2be63bc32c7cea35eefbcc7186255e9cd8ba6ed4b03cb9ee2e62740fe17ad74a81ee7e4ada37f4a2dca99a82becf790d6aa86822cdbeba3972cf11d54095ca91
     HEAD_REF master
+    PATCHES
+        find-rhash.patch
 )
 
 vcpkg_cmake_configure(
