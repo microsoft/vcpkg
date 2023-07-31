5b11c3312c68c5d057d34cb81b27a1713e380776
sdcb <sdflysha@qq.com>
2023-07-31 08:53:02 +0800

    [flycv]: delete usage

--- ports/flycv/portfile.cmake
+++ ports/flycv/portfile.cmake
@@ -22,5 +22,5 @@ vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+# file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
