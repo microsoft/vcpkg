b433560e65322426cb8dd118cb7432c788d01860
diamant3 <diamant3@proton.me>
2023-07-31 13:54:31 +0800

    update portfile and vcpkg.json

--- ports/mruby/portfile.cmake
+++ ports/mruby/portfile.cmake
@@ -3,13 +3,15 @@ set(VERSION 3.2.0)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mruby/mruby
-    REF 87260e7bb1a9edfb2ce9b41549c4142129061ca5
-    SHA512 dbc0602ac7265076dc9fa1f862585fe0a3669be32cef0f0b13f1fdc18e82e45d2b9eb205723a87581a8a96382fb0ca23ec19fe0494cba29f7a2eaa512e5b26cc
+    REF ${VERSION}
+    SHA512 bb46fa0eda6507cabe775e3f9cceec6da64d5a96c20e704e7ada94f5b4906989553c363cfd85789c4adcb030a6cfd36b8a99d8247f32687c913bbe06edb9bbca
     HEAD_REF master
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+vcpkg_execute_in_download_mode(
+    COMMAND rake
+    WORKING_DIRECTORY "${SOURCE_PATH}"
 )
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
