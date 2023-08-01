a7a66184862f39130d65d7b3ecace1036934cd30
myd7349 <myd7349@gmail.com>
2023-08-01 21:00:10 +0800

    [libxdf] Fix pugixml dependency

--- ports/libxdf/portfile.cmake
+++ ports/libxdf/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 81ff3598442d3ea166ec54b74248ad7b7eca5fcfdb72526978966398ad9e6524883183e71b68e349c46c2705779bf2b4922ce097f01afe7a62faa449ab8fa075
     HEAD_REF main
+    PATCHES
+        fix-pugixml-dependency.patch
 )
 
 vcpkg_cmake_configure(
