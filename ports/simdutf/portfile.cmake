41f878612d363d77facbc67f7bef274080b3e63b
JuliusBrueggemann <julius9402@gmail.com>
2023-08-07 16:13:06 +0200

    Apply suggestions from code review
    Co-authored-by: Mengna Li <95600143+Adela0814@users.noreply.github.com>

--- ports/simdutf/portfile.cmake
+++ ports/simdutf/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO simdutf/simdutf
-    REF v3.2.15
+    REF "v${VERSION}"
     SHA512 b8d8296569910e08a6cbbdf6d97bbaacca7ef24a568fa6dd2c87fe1361af86640619e49160b9577ae0702f4876634ba3f4381287ebb3e5e21d8eda3e9ece104e
     HEAD_REF master
 )
@@ -13,7 +13,9 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS "-DSIMDUTF_TESTS=OFF -DSIMDUTF_BENCHMARKS=OFF ${FEATURE_OPTIONS}"
+    OPTIONS ${FEATURE_OPTIONS}
+        -DSIMDUTF_TESTS=OFF
+        -DSIMDUTF_BENCHMARKS=OFF
 )
 
 vcpkg_cmake_install()
@@ -24,4 +26,4 @@ if ("tools" IN_LIST FEATURES)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/LICENSE-APACHE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE-APACHE")
