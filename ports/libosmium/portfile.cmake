7e557d79d81a99c3b3a17a920d5b59bc423b2543
Ildar Khayrutdinov <ildarkhairutdin@gmail.com>
2023-08-09 13:11:16 +0300

    cleanup

--- ports/libosmium/portfile.cmake
+++ ports/libosmium/portfile.cmake
@@ -3,7 +3,6 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO osmcode/libosmium
     REF "v${VERSION}"
-
     SHA512 dca79a6771be759b8f1c2167d8b5981a3b5d4c6a4d6a0df5ae438c63de3f25120abfabd20a88385eeda28e287d63f2b27f2ac87301d475f3ab46111809c4b2e3
 )
 set(BOOST_ROOT "${CURRENT_INSTALLED_DIR}")
