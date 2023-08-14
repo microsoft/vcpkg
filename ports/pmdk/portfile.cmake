aea0daf417b809de9f330bdf25bbd94e2c18397f
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-14 18:05:22 +0200

    pmdk msbuild

--- ports/pmdk/portfile.cmake
+++ ports/pmdk/portfile.cmake
@@ -1,7 +1,5 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 
-set(PMDK_VERSION "1.12.0")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pmem/pmdk
@@ -11,44 +9,30 @@ vcpkg_from_github(
 )
 
 # Build only the selected projects
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/src/PMDK.sln
+vcpkg_msbuild_install(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH src/PMDK.sln
     TARGET "Solution Items\\libpmem,Solution Items\\libpmemlog,Solution Items\\libpmemblk,Solution Items\\libpmemobj,Solution Items\\libpmempool,Solution Items\\Tools\\pmempool"
-    OPTIONS /p:SRCVERSION=${PMDK_VERSION}
+    OPTIONS /p:SRCVERSION=${VERSION}
+    ADDITIONAL_LIBS getopt.lib
 )
 
-set(DEBUG_ARTIFACTS_PATH ${SOURCE_PATH}/src/x64/Debug)
-set(RELEASE_ARTIFACTS_PATH ${SOURCE_PATH}/src/x64/Release)
+set(DEBUG_ARTIFACTS_PATH "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/src/x64/Debug")
+set(RELEASE_ARTIFACTS_PATH "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/src/x64/Release")
 
 # Install header files
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/include/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(GLOB HEADER_FILES ${SOURCE_PATH}/src/include/libpmemobj/*.h)
-file(INSTALL ${HEADER_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/include/libpmemobj)
+file(GLOB HEADER_FILES "${SOURCE_PATH}/src/include/*.h")
+file(INSTALL ${HEADER_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(GLOB HEADER_FILES "${SOURCE_PATH}/src/include/libpmemobj/*.h")
+file(INSTALL ${HEADER_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/libpmemobj")
 
 # Remove unneeded header files
-file(REMOVE ${CURRENT_PACKAGES_DIR}/include/libvmmalloc.h)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/include/librpmem.h)
-
-# Install libraries (debug)
-file(GLOB LIB_DEBUG_FILES ${DEBUG_ARTIFACTS_PATH}/libs/libpmem*.lib)
-file(INSTALL ${LIB_DEBUG_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/lib/libpmemcommon.lib)
-file(GLOB LIB_DEBUG_FILES ${DEBUG_ARTIFACTS_PATH}/libs/libpmem*.dll)
-file(INSTALL ${LIB_DEBUG_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
-
-# Install libraries (release)
-file(GLOB LIB_RELEASE_FILES ${RELEASE_ARTIFACTS_PATH}/libs/libpmem*.lib)
-file(INSTALL ${LIB_RELEASE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(REMOVE ${CURRENT_PACKAGES_DIR}/lib/libpmemcommon.lib)
-file(GLOB LIB_RELEASE_FILES ${RELEASE_ARTIFACTS_PATH}/libs/libpmem*.dll)
-file(INSTALL ${LIB_RELEASE_FILES} DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(REMOVE "${CURRENT_PACKAGES_DIR}/include/libvmmalloc.h")
+file(REMOVE "${CURRENT_PACKAGES_DIR}/include/librpmem.h")
 
 # Install tools (release only)
-file(INSTALL ${RELEASE_ARTIFACTS_PATH}/libs/pmempool.exe DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
+file(INSTALL "${RELEASE_ARTIFACTS_PATH}/libs/pmempool.exe" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 
 vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/pmdk)
 
-vcpkg_copy_pdbs()
-
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
