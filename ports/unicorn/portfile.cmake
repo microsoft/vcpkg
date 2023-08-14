16910f869aacc3ab24ac3dfe67c6b4f78bcfbefa
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-14 12:37:06 +0200

    [unicorn] msbuild

--- ports/unicorn/portfile.cmake
+++ ports/unicorn/portfile.cmake
@@ -1,7 +1,3 @@
-if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
-    message(FATAL_ERROR "WindowsStore not supported")
-endif()
-
 if(VCPKG_CRT_LINKAGE STREQUAL "dynamic" AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     message(FATAL_ERROR "unicorn can currently only be built with /MT or /MTd (static CRT linkage)")
 endif()
@@ -17,40 +13,17 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(UNICORN_PLATFORM "Win32")
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    set(UNICORN_PLATFORM "x64")
-else()
-    message(FATAL_ERROR "Unsupported architecture")
-endif()
-
-vcpkg_build_msbuild(
-    PROJECT_PATH "${SOURCE_PATH}/msvc/unicorn.sln"
-    PLATFORM "${UNICORN_PLATFORM}"
+vcpkg_msbuild_install(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH "msvc/unicorn.sln"
 )
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Release/unicorn.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Release/unicorn.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Debug/unicorn.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Debug/unicorn.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
-else()
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Release/unicorn_static.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-    file(INSTALL "${SOURCE_PATH}/msvc/${UNICORN_PLATFORM}/Debug/unicorn_static.lib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
-endif()
 
 file(
     INSTALL "${SOURCE_PATH}/msvc/distro/include/unicorn"
     DESTINATION "${CURRENT_PACKAGES_DIR}/include"
     RENAME "unicorn"
 )
-file(
-    INSTALL "${SOURCE_PATH}/COPYING"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/unicorn"
-    RENAME "copyright"
-)
-file(
-    INSTALL "${SOURCE_PATH}/COPYING_GLIB"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/unicorn"
+vcpkg_install_copyright(FILE_LIST
+    "${SOURCE_PATH}/COPYING"
+    "${SOURCE_PATH}/COPYING_GLIB"
 )
