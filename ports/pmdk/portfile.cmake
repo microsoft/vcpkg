c1317a44a76a4fffaed0ecad89528bb636d8403c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-14 18:40:34 +0200

    devendor getopt from pmdk

--- ports/pmdk/portfile.cmake
+++ ports/pmdk/portfile.cmake
@@ -6,7 +6,10 @@ vcpkg_from_github(
     REF 73d8f958e855904dc0776a7d77d0f0d3698a65b1 #v1.12.0
     SHA512 ffe77796c9028478985ca98e4162a671e3e7f580faa46b31d0dcf8c5e97aa6478044efdf7ad238285044f18f754a20a4e2a1b5992c7b9cffa709884eb62007ab
     HEAD_REF master
+    PATCHES "remove_getopt.patch"
 )
+
+file(REMOVE  "${SOURCE_PATH}/src/windows/getopt" "${SOURCE_PATH}/src/test/getopt")
 
 # Build only the selected projects
 vcpkg_msbuild_install(
