0a2eae2c92111c069866177278fb28428bef4770
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-14 23:03:52 +0200

    [mdnsresponder] msbuild

--- ports/mdnsresponder/portfile.cmake
+++ ports/mdnsresponder/portfile.cmake
@@ -12,61 +12,13 @@ ELSE()
   SET(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
 ENDIF()
 
-function(FIX_VCXPROJ VCXPROJ_PATH)
-  file(READ ${VCXPROJ_PATH} ORIG)
-  if(${VCPKG_CRT_LINKAGE} STREQUAL "dynamic")
-    string(REGEX REPLACE
-      "<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>"
-      "<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>"
-      ORIG "${ORIG}")
-    string(REGEX REPLACE
-      "<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"
-      "<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>"
-      ORIG "${ORIG}")
-  else()
-    string(REGEX REPLACE
-      "<RuntimeLibrary>MultiThreadedDebugDLL</RuntimeLibrary>"
-      "<RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>"
-      ORIG "${ORIG}")
-    string(REGEX REPLACE
-      "<RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>"
-      "<RuntimeLibrary>MultiThreaded</RuntimeLibrary>"
-      ORIG "${ORIG}")
-  endif()
-  if(${VCPKG_LIBRARY_LINKAGE} STREQUAL "dynamic")
-    string(REPLACE
-      "<ConfigurationType>StaticLibrary</ConfigurationType>"
-      "<ConfigurationType>DynamicLibrary</ConfigurationType>"
-      ORIG "${ORIG}")
-  else()
-    string(REPLACE
-      "<ConfigurationType>DynamicLibrary</ConfigurationType>"
-      "<ConfigurationType>StaticLibrary</ConfigurationType>"
-      ORIG "${ORIG}")
-  endif()
-  
-  string(REPLACE
-    "<DebugInformationFormat>ProgramDatabase</DebugInformationFormat>"
-    "<DebugInformationFormat>OldStyle</DebugInformationFormat>"
-    ORIG "${ORIG}")
-  file(WRITE ${VCXPROJ_PATH} "${ORIG}")
-endfunction()
-
-FIX_VCXPROJ("${SOURCE_PATH}/mDNSWindows/DLL/dnssd.vcxproj")
-if(${VCPKG_CRT_LINKAGE} STREQUAL "dynamic" AND ${VCPKG_LIBRARY_LINKAGE} STREQUAL "static")
-    FIX_VCXPROJ("${SOURCE_PATH}/mDNSWindows/DLLStub/DLLStub.vcxproj")
-endif()
-FIX_VCXPROJ("${SOURCE_PATH}/Clients/DNS-SD.VisualStudio/dns-sd.vcxproj")
-
-vcpkg_install_msbuild(
+vcpkg_msbuild_install(
     SOURCE_PATH "${SOURCE_PATH}"
-    PROJECT_SUBPATH "mDNSWindows/mDNSResponder.sln"
+    PROJECT_SUBPATH mDNSResponder.sln
     PLATFORM ${BUILD_ARCH}
     TARGET dns-sd
-    SKIP_CLEAN
 )
 
 file(INSTALL "${SOURCE_PATH}/mDNSShared/dns_sd.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-vcpkg_copy_pdbs()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
