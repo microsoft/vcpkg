fde9d02717708d573bac35a2f59f25204904ce26
Zhao Liu <v-zhli17@microsoft.com>
2023-08-14 23:35:55 -0700

    update hash

--- ports/gmsh/portfile.cmake
+++ ports/gmsh/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.onelab.info
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gmsh/gmsh
-    REF gmsh_4_9_0
-    SHA512 e70a09741a86a9131094e77742078aec1cc94517e1d7c855c257bc93c21c057e25c7ac5168d31ec4d905d78f31d5704faf63bfd3a81b4b9e2ebbcfacf2fdaa8b
+    REF gmsh_4_11_1
+    SHA512 4c10a41659ee4f70ba5091f9ae1c4c3ee285ccf217c3de1157a0d6d694e6f1df9a6b1329b2b24029dd52f945dd7605e477302bdb358106a8d97e903eaba425dc
     HEAD_REF master
     PATCHES fix-install.patch
 )
