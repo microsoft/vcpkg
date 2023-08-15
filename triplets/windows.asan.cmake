ff9e9def089d924527fe60a507d43356e0ce2db7
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-15 22:44:48 +0200

    remove _dbg_ for the runtime chunk

--- triplets/windows.asan.cmake
+++ triplets/windows.asan.cmake
@@ -77,7 +77,7 @@ if(VCPKG_USE_SANITIZERS)
         # /wholearchive: is not used for asan_dynamic by clang. Only the _thunk has wholearchive
         set(sanitizer_libs_exe_rel "/wholearchive:clang_rt.asan_dynamic-${asan_arch}.lib /wholearchive:clang_rt.asan_dynamic_runtime_thunk-${asan_arch}.lib")
         set(sanitizer_libs_dll_rel "${sanitizer_libs_exe_rel}")
-        set(sanitizer_libs_exe_dbg "/wholearchive:clang_rt.asan_dbg_dynamic-${asan_arch}.lib /wholearchive:clang_rt.asan_dbg_dynamic_runtime_thunk-${asan_arch}.lib")
+        set(sanitizer_libs_exe_dbg "/wholearchive:clang_rt.asan_dbg_dynamic-${asan_arch}.lib /wholearchive:clang_rt.asan_dynamic_runtime_thunk-${asan_arch}.lib")
         set(sanitizer_libs_dll_dbg "${sanitizer_libs_exe_dbg}")
       else()
         # TODO
