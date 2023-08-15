8ab316841a6ac86594d43919861e598754729f86
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-15 10:09:50 +0200

    fix static builds

--- ports/mdnsresponder/portfile.cmake
+++ ports/mdnsresponder/portfile.cmake
@@ -12,6 +12,30 @@ ELSE()
   SET(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
 ENDIF()
 
+function(FIX_VCXPROJ VCXPROJ_PATH)
+  file(READ ${VCXPROJ_PATH} ORIG)
+
+  if(${VCPKG_LIBRARY_LINKAGE} STREQUAL "dynamic")
+    string(REPLACE
+      "<ConfigurationType>StaticLibrary</ConfigurationType>"
+      "<ConfigurationType>DynamicLibrary</ConfigurationType>"
+      ORIG "${ORIG}")
+  else()
+    string(REPLACE
+      "<ConfigurationType>DynamicLibrary</ConfigurationType>"
+      "<ConfigurationType>StaticLibrary</ConfigurationType>"
+      ORIG "${ORIG}")
+  endif()
+
+  file(WRITE ${VCXPROJ_PATH} "${ORIG}")
+endfunction()
+
+FIX_VCXPROJ("${SOURCE_PATH}/mDNSWindows/DLL/dnssd.vcxproj")
+if(${VCPKG_CRT_LINKAGE} STREQUAL "dynamic" AND ${VCPKG_LIBRARY_LINKAGE} STREQUAL "static")
+    FIX_VCXPROJ("${SOURCE_PATH}/mDNSWindows/DLLStub/DLLStub.vcxproj")
+endif()
+FIX_VCXPROJ("${SOURCE_PATH}/Clients/DNS-SD.VisualStudio/dns-sd.vcxproj")
+
 vcpkg_msbuild_install(
     SOURCE_PATH "${SOURCE_PATH}"
     PROJECT_SUBPATH mDNSWindows/mDNSResponder.sln
