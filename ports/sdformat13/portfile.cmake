46edb3453a44465c26c37a6e175efc99a6628b3a
Tal Regev <tal.regev@gmail.com>
2023-08-17 00:55:41 +0300

    Fix according comments review

--- ports/sdformat13/portfile.cmake
+++ ports/sdformat13/portfile.cmake
@@ -31,5 +31,4 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
                     "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
