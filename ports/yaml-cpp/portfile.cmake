80f35767837973652216401f4d569ddad35da269
Pierre Wendling <pierre.wendling.4@gmail.com>
2023-08-16 18:19:12 -0400

    [yaml-cpp] Backport CMake fixes from upstream.
    The patch is a shortened version from:
    https://github.com/jbeder/yaml-cpp/pull/1212
    
    It fixes issues with downstream projects using the old target, and makes
    the CMake config more easily relocatable.
    
    The patch was rebased on the 0.8.0 release, and the CI and tests bits
    were omitted.

--- ports/yaml-cpp/portfile.cmake
+++ ports/yaml-cpp/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "${VERSION}"
     SHA512 aae9d618f906117d620d63173e95572c738db518f4ff1901a06de2117d8deeb8045f554102ca0ba4735ac0c4d060153a938ef78da3e0da3406d27b8298e5f38e
     HEAD_REF master
+    PATCHES
+        "yaml-cpp-pr-1212.patch"
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" YAML_BUILD_SHARED_LIBS)
@@ -14,12 +16,13 @@ vcpkg_cmake_configure(
         -DYAML_CPP_BUILD_TOOLS=OFF
         -DYAML_CPP_BUILD_TESTS=OFF
         -DYAML_BUILD_SHARED_LIBS=${YAML_BUILD_SHARED_LIBS}
+        -DYAML_CPP_INSTALL_CMAKEDIR=share/${PORT}
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/yaml-cpp")
+vcpkg_cmake_config_fixup()
 if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/yaml-cpp.pc" "-lyaml-cpp" "-lyaml-cppd")
 endif()
