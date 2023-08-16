65e7f001b0cae153c7f352f42625552bf976605c
Danyil Melnytskyi <ddann700@gmail.com>
2023-08-16 09:37:23 +0200

    Yet another refactor

--- ports/libunwind/portfile.cmake
+++ ports/libunwind/portfile.cmake
@@ -1,13 +1,13 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "libunwind/libunwind"
-    REF v1.7.2
+    REF "v${VERSION}"
     HEAD_REF "v1.7-stable"
     SHA512 b560f45cfa8ca3a60b41779afbcd862860c0f7af4014298c0b22eaec8b39740349c2077a940fb0d235c8f7d6a567e00c5cb0a04c251053f90c51320616784fd2
 )
 
 vcpkg_configure_make(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
 )
 vcpkg_install_make()
