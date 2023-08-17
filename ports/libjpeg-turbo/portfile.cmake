77bf6797678475d5810c2c5e8da9a97f0e486f5a
jimwang118 <wangzhijie05@beyondsoft.com>
2023-08-17 14:49:50 +0800

    add usage

--- ports/libjpeg-turbo/portfile.cmake
+++ ports/libjpeg-turbo/portfile.cmake
@@ -91,6 +91,7 @@ file(REMOVE_RECURSE
      "${CURRENT_PACKAGES_DIR}/debug/include"
      "${CURRENT_PACKAGES_DIR}/share/man")
 
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/jpeg")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
