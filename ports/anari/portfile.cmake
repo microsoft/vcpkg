d12e72fd7617bfd600ac113e7edeae864fd72bf1
acdemiralp <demiralpali@gmail.com>
2023-08-17 03:54:50 +0200

    Revert removing python3 dependency. Quote python3 directory.

--- ports/anari/portfile.cmake
+++ ports/anari/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
-vcpkg_add_to_path(${PYTHON3_DIR})
+vcpkg_add_to_path("${PYTHON3_DIR}")
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
