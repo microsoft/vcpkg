c2b8c02c21cc487cd65b8410a22372e37b2738ec
acdemiralp <demiralpali@gmail.com>
2023-08-20 08:03:11 +0200

    Implemented parsing OptiX version from include/optix.h.

--- ports/optix/FindOptiX.cmake
+++ ports/optix/FindOptiX.cmake
@@ -10,7 +10,13 @@
 if (OptiX_ROOT_DIR)
   set(OptiX_INCLUDE_DIR "${OptiX_ROOT_DIR}/include")
 
-  ### TODO: Extract OptiX_VERSION from ${OptiX_INCLUDE_DIR}/optix.h.
+  file(READ "${OptiX_INCLUDE_DIR}/optix.h" OptiX_HEADER_FILE)
+  string(REGEX MATCH "#define OPTIX_VERSION ([0-9]*)" _ ${OptiX_HEADER_FILE})
+  set(OptiX_VERSION_CODE ${CMAKE_MATCH_1})
+  math(EXPR OptiX_MAJOR_VERSION "${OptiX_VERSION_CODE} / 10000")
+  math(EXPR OptiX_MINOR_VERSION "(${OptiX_VERSION_CODE} % 10000) / 100")
+  math(EXPR OptiX_MICRO_VERSION "${OptiX_VERSION_CODE} % 100")
+  set(OptiX_VERSION "${OptiX_MAJOR_VERSION}.${OptiX_MINOR_VERSION}.${OptiX_MICRO_VERSION}")
   
   add_library(OptiX::OptiX INTERFACE IMPORTED)
   target_include_directories(OptiX::OptiX INTERFACE ${OptiX_INCLUDE_DIR})
