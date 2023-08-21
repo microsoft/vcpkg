32a6cdc2b1dd400e004d83b993beb8196f5fde71
acdemiralp <demiralpali@gmail.com>
2023-08-21 12:17:34 +0200

    FindOptiX.cmake progress.

--- ports/optix/FindOptiX.cmake
+++ ports/optix/FindOptiX.cmake
@@ -1,3 +1,5 @@
+# This is the hardest-trying FindOptiX.cmake in existence.
+#
 # Defines:
 # - OptiX_FOUND
 # - OptiX_INCLUDE_DIR
@@ -12,14 +14,28 @@ list(APPEND OptiX_POTENTIAL_DIRS
   "${OptiX_ROOT}"
   "${OptiX_ROOT_DIR}"       # https://github.com/nvidia/visrtx uses this style.
   "${OptiX_ROOT_PATH}"
-  
+
+  "${OPTIX_DIR}"
+  "${OPTIX_INSTALL_DIR}"
+  "${OPTIX_PATH}"
+  "${OPTIX_ROOT}"
+  "${OPTIX_ROOT_DIR}"
+  "${OPTIX_ROOT_PATH}"
+
   "$ENV{OptiX_DIR}"
   "$ENV{OptiX_INSTALL_DIR}"
   "$ENV{OptiX_PATH}"
   "$ENV{OptiX_ROOT}"
   "$ENV{OptiX_ROOT_DIR}"
   "$ENV{OptiX_ROOT_PATH}"
-  
+
+  "$ENV{OPTIX_DIR}"
+  "$ENV{OPTIX_INSTALL_DIR}"
+  "$ENV{OPTIX_PATH}"
+  "$ENV{OPTIX_ROOT}"
+  "$ENV{OPTIX_ROOT_DIR}"
+  "$ENV{OPTIX_ROOT_PATH}"
+
   "$ENV{PATH}"
 )
 
@@ -28,7 +44,7 @@ if(WIN32)
     "$ENV{PROGRAMDATA}"
     "$ENV{PROGRAMFILES}"
     "$ENV{PROGRAMFILES\(X86\)}"
-    
+
     "$ENV{PROGRAMDATA}/NVIDIA"
     "$ENV{PROGRAMFILES}/NVIDIA"
     "$ENV{PROGRAMFILES\(X86\)}/NVIDIA"
@@ -44,40 +60,87 @@ elseif(LINUX)
     "$ENV{CPLUS_INCLUDE_PATH}"
 
     "/include"
+    "/include/nvidia"
     "/lib"
+    "/lib/nvidia"
+    "/lib64"
+    "/lib64/nvidia"
     "/opt"
+    "/opt/nvidia"
 
     "/usr"
     "/usr/include"
+    "/usr/include/nvidia"
     "/usr/lib"
+    "/usr/lib/nvidia"
+    "/usr/lib64"
+    "/usr/lib64/nvidia"
     "/usr/opt"
+    "/usr/opt/nvidia"
 
     "/usr/local"
     "/usr/local/include"
+    "/usr/local/include/nvidia"
     "/usr/local/lib"
+    "/usr/local/lib/nvidia"
+    "/usr/local/lib64"
+    "/usr/local/lib64/nvidia"
     "/usr/local/opt"
-    
+    "/usr/local/opt/nvidia"
+
     "/var"
     "/var/include"
+    "/var/include/nvidia"
     "/var/lib"
+    "/var/lib/nvidia"
+    "/var/lib64"
+    "/var/lib64/nvidia"
     "/var/opt"
-  
+    "/var/opt/nvidia"
+
     "/var/local"
     "/var/local/include"
+    "/var/local/include/nvidia"
     "/var/local/lib"
+    "/var/local/lib/nvidia"
+    "/var/local/lib64"
+    "/var/local/lib64/nvidia"
     "/var/local/opt"
+    "/var/local/opt/nvidia"
   )
 endif()
 
+list(APPEND OptiX_POTENTIAL_SUFFIXES
+  "/optix"
+  "/OptiX"
+  "/OPTIX"
+)
+list(APPEND OptiX_POTENTIAL_VERSIONED_SUFFIXES
+  "/optix *"
+  "/OptiX *"
+  "/OPTIX *"
+  "/optix-*"
+  "/OptiX-*"
+  "/OPTIX-*"
+  "/OptiX SDK *"
+  "/OptiX-SDK-*"
+  "/NVIDIA-OptiX-SDK-*"
+)
+
 set(OptiX_VALID_POTENTIAL_DIRS)
-foreach(OptiX_POTENTIAL_DIR IN LISTS "${OptiX_POTENTIAL_DIRS}")
-  if(exists "${OptiX_POTENTIAL_DIR}")
+foreach(OptiX_POTENTIAL_DIR IN LISTS OptiX_POTENTIAL_DIRS)
+  if(IS_DIRECTORY "${OptiX_POTENTIAL_DIR}")
     list(APPEND OptiX_VALID_POTENTIAL_DIRS "${OptiX_POTENTIAL_DIR}")
+  
+    foreach(OptiX_POTENTIAL_SUFFIX IN LISTS OptiX_POTENTIAL_SUFFIXES)
+      if(IS_DIRECTORY "${OptiX_POTENTIAL_DIR}/${OptiX_POTENTIAL_SUFFIX}")
+        list(APPEND OptiX_VALID_POTENTIAL_DIRS "${OptiX_POTENTIAL_DIR}/${OptiX_POTENTIAL_SUFFIX}")
+      endif()
+    endforeach()
+
+    # TODO Check with OptiX_POTENTIAL_VERSIONED_SUFFIXES, append to OptiX_VALID_POTENTIAL_DIRS, ideally only correct versions.
   endif()
 endforeach()
-
-# TODO Suffixes: /OptiX SDK * /NVIDIA-OptiX-SDK-* /OptiX-SDK-*
-message(INFO " OptiX_VALID_POTENTIAL_DIRS: " ${OptiX_VALID_POTENTIAL_DIRS})
 
 find_path(
   OptiX_ROOT_DIR
@@ -85,10 +148,7 @@ find_path(
     "include/optix.h"
   PATHS
     ${OptiX_POTENTIAL_DIRS}
-  PATH_SUFFIXES
-    ""
 )
-# TODO END
 
 if (OptiX_ROOT_DIR)
   set(OptiX_INCLUDE_DIR "${OptiX_ROOT_DIR}/include")
