1bf57d650de34695e1613b95c6264abc4c5efafa
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-28 15:05:50 +0200

    make gmp use the new stuff

--- ports/gmp/portfile.cmake
+++ ports/gmp/portfile.cmake
@@ -2,8 +2,6 @@ if(EXISTS "${CURRENT_INSTALLED_DIR}/include/gmp.h" OR "${CURRENT_INSTALLED_DIR}/
     message(FATAL_ERROR "Can't build ${PORT} if mpir is installed. Please remove mpir, and try install ${PORT} again if you need it.")
 endif()
 
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
-
 vcpkg_download_distfile(
     ARCHIVE
     URLS
@@ -40,51 +38,33 @@ if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
 endif()
 
 set(disable_assembly OFF)
-set(ccas "")
-set(asmflags "-c")
-vcpkg_cmake_get_vars(cmake_vars_file)
-include("${cmake_vars_file}")
-if(VCPKG_DETECTED_CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+set(languages "C;CXX")
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-        string(APPEND asmflags " --target=i686-pc-windows-msvc")
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-        string(APPEND asmflags " --target=x86_64-pc-windows-msvc")
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
-        string(APPEND asmflags " --target=arm64-pc-windows-msvc")
     else()
         set(disable_assembly ON)
-    endif()
-    if(NOT disable_assembly)
-        vcpkg_find_acquire_program(CLANG)
-        set(ccas "${CLANG}")
     endif()
 elseif(VCPKG_TARGET_IS_MINGW AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     # not exporting asm functions
     set(disable_assembly ON)
-elseif(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    set(ccas "${VCPKG_DETECTED_CMAKE_C_COMPILER}")
-    vcpkg_list(APPEND OPTIONS "ABI=32")
-    string(APPEND asmflags " -m32")
-else()
-    set(ccas "${VCPKG_DETECTED_CMAKE_C_COMPILER}")
 endif()
 
 if(disable_assembly)
     vcpkg_list(APPEND OPTIONS "--enable-assembly=no")
-elseif(ccas)
-    cmake_path(GET ccas PARENT_PATH ccas_dir)
-    vcpkg_add_to_path("${ccas_dir}")
-    cmake_path(GET ccas FILENAME ccas_command)
+else()
+    list(APPEND languages "ASM")
 endif()
-vcpkg_list(APPEND OPTIONS "CCAS=${ccas_command}" "ASMFLAGS=${asmflags}")
 
 if(VCPKG_CROSSCOMPILING)
     set(ENV{HOST_TOOLS_PREFIX} "${CURRENT_HOST_INSTALLED_DIR}/manual-tools/${PORT}")
 endif()
 
-vcpkg_configure_make(
+vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
+    LANGUAGES "${languages}"
     OPTIONS
         ${OPTIONS}
         --enable-cxx
@@ -92,7 +72,7 @@ vcpkg_configure_make(
         --with-readline=no
         "gmp_cv_prog_exeext_for_build=${VCPKG_HOST_EXECUTABLE_SUFFIX}"
 )
-vcpkg_install_make()
+vcpkg_make_install()
 vcpkg_fixup_pkgconfig()
 
 if(NOT VCPKG_CROSSCOMPILING)
