91aa9b432f6dd65307971c3dac1f81362052da9f
jimwang118 <wangzhijie05@beyondsoft.com>
2023-08-30 16:21:11 +0800

    update version

--- ports/easyloggingpp/portfile.cmake
+++ ports/easyloggingpp/portfile.cmake
@@ -32,7 +32,6 @@ vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-#file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 vcpkg_fixup_pkgconfig()
