c7f2cc1d8bb81f3d429ac912135253b7df9f3c5e
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-08-31 14:08:18 +0200

    create vcpkg-scripts and move stuff around

---
+++ ports/vcpkg-scripts/vcpkg-port-config.cmake
@@ -0,0 +1,5 @@
+file(GLOB cmake_files "${CMAKE_CURRENT_LIST_DIR}/*.cmake")
+list(REMOVE_ITEM cmake_files "${CMAKE_CURRENT_LIST_FILE}")
+foreach(cmake_file IN LISTS cmake_files)
+    include("${cmake_file}")
+endforeach()
