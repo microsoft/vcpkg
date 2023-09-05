93c6d84931b27954bc45b03f2441477336ec7395
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2023-09-05 08:31:05 +0200

    [NEW] SFCGAL
    SFCGAL is a C++ wrapper library around CGAL with the aim of supporting ISO
    19107:2013 and OGC Simple Features Access 1.2 for 3D operations.
    
    SFCGAL provides standard compliant geometry types and operations, that can be
    accessed from its C or C++ APIs. PostGIS uses the C API, to expose some
    SFCGAL's functions in spatial databases
    
    https://gitlab.com/sfcgal/SFCGAL
    https://sfcgal.org/

---
+++ ports/sfcgal/portfile.cmake
@@ -0,0 +1,29 @@
+# Header only
+vcpkg_buildpath_length_warning(37)
+
+vcpkg_from_gitlab(
+	GITLAB_URL https://gitlab.com
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO Oslandia/SFCGAL
+	REF v1.5.1
+	SHA512 8d33235512a14997b00b4419f42d1195fd40186b56af63cd4494555031799af2a2fc1d0b3c2cd706ce8f6fc6be844af60be3a57774515326a782494e1de44ec2 
+	HEAD_REF master
+	)
+
+
+vcpkg_cmake_configure(
+	SOURCE_PATH "${SOURCE_PATH}"
+	OPTIONS
+	-DSFCGAL_BUILD_TESTS=OFF
+	-DBUILD_TESTING=OFF
+	)
+
+vcpkg_install_cmake()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH bin/sfcgal-config)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
