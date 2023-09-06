29e932623293c8c6e381a95b9b87434b8f69ae57
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-09-06 16:33:26 +0400

    openvino: 2023.2.0 release

--- ports/openvino/portfile.cmake
+++ ports/openvino/portfile.cmake
@@ -1,15 +1,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO openvinotoolkit/openvino
-    REF 50c85f01ab44b2470f08b0f8824cced109628fc6
-    SHA512 1673cdab4f0d73ba5e3d0ac1e8a7b136d25df59704575a468016650c3c6c0dcba0bdc0a2306f61a14d49792651b21e9233d166610e0b463624b309758f1b1c04
+    REPO ilya-lavrenov/openvino
+    REF e059a311302c43f74868d7a221a505b97c49e62d
+    SHA512 cbc2d7783c0a0d3dcab3ea997daa9c183ae85b5ec121d1613b1dad627f2089acf6206aba4706519d4b3309a33675a2bd54a8765f614ccea160a872672fdafa09
     PATCHES
         001-disable-tools.patch
-        002-typo-in-default-option-value.patch
-        003-fix-find-onnx.patch
-        004-onednn-build.patch
-        005-rename-utils.patch
-        007-dnnl-library-rename.patch
+        002-cpu-plugin-x86.patch
     HEAD_REF master)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -31,19 +27,19 @@ if(ENABLE_INTEL_GPU)
     # python is required for conversion of OpenCL source files into .cpp.
     vcpkg_find_acquire_program(PYTHON3)
 
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" AND ENABLE_INTEL_CPU)
         message(WARNING
             "OneDNN for GPU is not available for static build, which is required for dGPU."
-            "Please, consider using VCPKG_LIBRARY_LINKAGE=\"dynamic\".")
-        list(APPEND FEATURE_OPTIONS "-DENABLE_ONEDNN_FOR_GPU=OFF")
+            "Please, consider using VCPKG_LIBRARY_LINKAGE=\"dynamic\" or disable CPU plugin,"
+            "which uses another flavor of oneDNN.")
     else()
         vcpkg_from_github(
             OUT_SOURCE_PATH DEP_SOURCE_PATH
             REPO oneapi-src/oneDNN
-            REF f27dedbfc093f51032a4580198bb80579440dc15
-            SHA512 882eb42e31490df1b35b5e55bef1be8452b710b7a16f5ad648961510abd288e16dbd783e0163aab9dd161fd3a9bd836b0f4afc82b14043d80d1dad9c3400af1b
+            REF ec0b2ee85fc2a2dbdeec10035c5ef5813d8fb5ea
+            SHA512 abc09c9ab190cc043ba675fdcaf2da0069eacce14aad6e788a9957d8b6704cfcefe5a707e78d544d25acac35bc83217660ee64528150311f577d2ccbdd165de1
             PATCHES 006-onednn-gpu-build.patch
-        )    
+        )
         file(COPY "${DEP_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/plugins/intel_gpu/thirdparty/onednn_gpu")
     endif()
 
@@ -56,10 +52,18 @@ if(ENABLE_INTEL_CPU)
     vcpkg_from_github(
         OUT_SOURCE_PATH DEP_SOURCE_PATH
         REPO openvinotoolkit/oneDNN
-        REF 48bf41e04ba8cdccb1e7ad166fecfb329f5f84a1
-        SHA512 8a5ef1ce07545bc28328d1cfd49a8ee8f2ff13c2e393623cb842982b83963881f3d096230805d2a187100c68a2ca30c99add5a975f3f623d9f4a51517c2d585f
-    )    
+        REF 31c8555b923e16b4ddfdcd1d1f126c115b5e0da7
+        SHA512 77a9e5a84ff62ae8014b8b5c48d05ee004ee26320bc8aa9bafc8323c55d609119b0553385defb842e0394ec07ec34d48b4df322ad6d10990d907618ef27a326c
+    )
     file(COPY "${DEP_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/plugins/intel_cpu/thirdparty/onednn")
+
+    vcpkg_from_github(
+        OUT_SOURCE_PATH DEP_SOURCE_PATH
+        REPO openvinotoolkit/mlas
+        REF c7c8a631315000f17c650af34431009d2f22129c
+        SHA512 4146598ce6b30a3eaea544d6703b949061118398e67b773aa11a3c0f3e8fbcc446d09ea893b9879f8869e977162bcada0d4895cb225cf2e2469fb71cd5942e53
+    )
+    file(COPY "${DEP_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/plugins/intel_cpu/thirdparty/mlas")
 
     if(VCPKG_TARGET_ARCHITECTURE MATCHES "arm")
         # scons (python tool) is required for ARM Compute Library building
@@ -77,7 +81,7 @@ if(ENABLE_INTEL_CPU)
             REPO ARM-software/ComputeLibrary
             REF v23.02.1
             SHA512 ee9439e0804bacd365f079cedc548ffe2c12b0d4a86780e0783186884eb5a6d7aa7ceac11c504e242bedc55c3d026b826c90adaafbdbd3e5cfa2562a1c4ee04d
-        )    
+        )
         file(COPY "${DEP_SOURCE_PATH}/" DESTINATION "${SOURCE_PATH}/src/plugins/intel_cpu/thirdparty/ComputeLibrary")
     endif()
 endif()
@@ -105,7 +109,6 @@ vcpkg_cmake_configure(
         "-DENABLE_NCC_STYLE=OFF"
         "-DENABLE_CPPLINT=OFF"
         "-DENABLE_SAMPLES=OFF"
-        "-DENABLE_COMPILE_TOOL=OFF"
         "-DENABLE_TEMPLATE=OFF"
         "-DENABLE_INTEL_GNA=OFF"
         "-DENABLE_PYTHON=OFF"
@@ -125,7 +128,6 @@ vcpkg_install_copyright(
     FILE_LIST
         "${SOURCE_PATH}/LICENSE"
         "${SOURCE_PATH}/licensing/third-party-programs.txt"
-        "${SOURCE_PATH}/licensing/tbb_third-party-programs.txt"
         "${SOURCE_PATH}/licensing/onednn_third-party-programs.txt"
         "${SOURCE_PATH}/licensing/runtime-third-party-programs.txt"
     COMMENT
