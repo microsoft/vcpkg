af1ff7a7d530a2530af610435e5f13d1e01fb08f
acdemiralp <demiralpali@gmail.com>
2023-09-07 00:09:36 +0200

    Remove embree dependency and helide patches. Disable helide device.

--- ports/anari/portfile.cmake
+++ ports/anari/portfile.cmake
@@ -4,7 +4,6 @@ vcpkg_from_github(
   REF "v${VERSION}"
   SHA512 51937d160a9508c56cf123eda13002c705acff501366710f83da1c62d875f8427cec27f10ea2d05f4637be141fb9a87935f4b0b9f0fabb6bd6a7cca6a4f48ee1
   HEAD_REF main
-  PATCHES fix-cmake.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
@@ -16,6 +15,7 @@ vcpkg_cmake_configure(
   OPTIONS
     -DBUILD_CTS=OFF
     -DBUILD_EXAMPLES=OFF
+    -DBUILD_HELIDE_DEVICE=OFF
     -DBUILD_TESTING=OFF
     -DBUILD_VIEWER=OFF
 )
