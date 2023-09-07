69feefb5c01912faadb8c0b2161717c0b311c3ef
Leander Schulten <Leander.Schulten@rwth-aachen.de>
2023-09-07 03:13:30 +0200

    Restore wpilib

--- ports/wpilib/portfile.cmake
+++ ports/wpilib/portfile.cmake
@@ -1,41 +1,49 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wpilibsuite/allwpilib
-    REF d37f990ce3a8a36bc791dd989dcea2868759e556
-    SHA512 0d22ad2fe80973935e87e4864acaacfd96023aa11b77ba84f0a39dde17ba9d86f864971d8f1c9f4a6a1c56db59758cac112d645822da8e0b34b41b065332dff1
-    PATCHES 
-        fmtlib-fix.patch # https://github.com/wpilibsuite/allwpilib/pull/5429
-        no-drake.patch # https://github.com/wpilibsuite/allwpilib/pull/5427
-        fix-unsed-variable.patch # https://github.com/wpilibsuite/allwpilib/pull/5430
-        fix-fmtlib-10.patch # https://github.com/wpilibsuite/allwpilib/pull/5433
-        fix-libuv.patch
+    REF 165ebe4c79c437c7ba6c03af4a88a8c8680f742a
+    SHA512 f6ee07db0a119a7ac5876c4b0cf74abfb6af635d3d3ba913300138c450f62f6595ac4849bc499346f9f0179cc563f548a5e8a9a47122af593b425af453afd99f
+    PATCHES
+        no-werror.patch
+        windows-install-location.patch
+        missing-find_dependency.patch
+        fix-usage.patch
 )
 
+if("allwpilib" IN_LIST FEATURES)
+    vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH_APRILTAG
+        REPO wpilibsuite/apriltag
+        REF e55b751f2465bd40a880d9acb87d24289e2af89e
+        SHA512 a5d824d11312f7f5229bad162349586e9c855cd1dc03f33235c045f2d5235932227eb17f9e9c801b46a28991cddcf7ad16d39549560251d7d9d52ce72f094a73
+    )
+endif()
+
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-  FEATURES
-    cameraserver WITH_CSCORE
-    allwpilib WITH_SIMULATION_MODULES
-    allwpilib WITH_WPILIB
+    FEATURES
+        cameraserver WITH_CSCORE
+        allwpilib WITH_SIMULATION_MODULES
+        allwpilib WITH_WPILIB
 )
 
 vcpkg_find_acquire_program(PYTHON3)
 x_vcpkg_get_python_packages(PYTHON_EXECUTABLE "${PYTHON3}" PACKAGES jinja2)
 
-vcpkg_find_acquire_program(PKGCONFIG)
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-      ${FEATURE_OPTIONS}
-      "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
-      -DWITH_JAVA=OFF
-      -DWITH_EXAMPLES=OFF
-      -DWITH_TESTS=OFF
-      -DWITH_GUI=OFF
-      -DWITH_SIMULATION_MODULES=OFF
-      -DUSE_SYSTEM_FMTLIB=ON
-      -DUSE_SYSTEM_LIBUV=ON
-      -DUSE_SYSTEM_EIGEN=ON
+        ${FEATURE_OPTIONS}
+        -DWITH_JAVA=OFF
+        -DWITH_EXAMPLES=OFF
+        -DWITH_TESTS=OFF
+        -DWITH_GUI=OFF
+        -DWITH_SIMULATION_MODULES=OFF
+        -DUSE_SYSTEM_FMTLIB=ON
+        -DUSE_SYSTEM_LIBUV=ON
+        -DUSE_SYSTEM_EIGEN=ON
+        "-DFETCHCONTENT_SOURCE_DIR_APRILTAGLIB=${SOURCE_PATH_APRILTAG}"
+    MAYBE_UNUSED_VARIABLES
+        FETCHCONTENT_SOURCE_DIR_APRILTAGLIB
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME wpilib)
@@ -48,4 +56,4 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
