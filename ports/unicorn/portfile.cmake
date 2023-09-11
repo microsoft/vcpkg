1b498317636dafd5a3c7828479bd9674e53d3465
Zhao Liu <v-zhli17@microsoft.com>
2023-09-10 23:21:54 -0700

    [unicorn] update to 2.0.1

--- ports/unicorn/portfile.cmake
+++ ports/unicorn/portfile.cmake
@@ -8,11 +8,11 @@ set(VCPKG_CRT_LINKAGE "static")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO unicorn-engine/unicorn
-    REF 6c1cbef6ac505d355033aef1176b684d02e1eb3a # v2.0.0
-    SHA512 7d23def10d1a72a9514ef10f2f9b50f29ce24d4994c06ce5980141a2bed8cdd76478d2e7849b7783e574d1fe97945dc7cd0ce1990c4b4b0479a5170eba4169e1
+    REF "${VERSION}.post1"
+    SHA512 8694d6bc92e3424a8ad050316413d53e56e0f55e7cad7517fb3e98e670a0f1768b060ead8f195da13607cec89a964364f05a8b9d0dc074f4ac5e51026f8343ad
     HEAD_REF master
     PATCHES
-        fix-usage.patch
+        fix-build.patch
 )
 
 vcpkg_cmake_configure(
@@ -24,10 +24,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_fixup_pkgconfig()
-
-if(EXISTS "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}")
-    vcpkg_cmake_config_fixup(PACKAGE_NAME "unofficial-${PORT}" CONFIG_PATH "share/unofficial-${PORT}")
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
