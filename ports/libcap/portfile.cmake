cd334f8e3b991e7f0b5721ab7cf13bc24abef1e3
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2023-09-12 11:12:54 +0000

    * Copy of cap_names.h checked in for cross compilation * Decouple cap_names.h from libcap.* make targets * Use vcpkg_cmake_get_vars instead of ENV{CC} etc. * Remove unnecessary self dependency of libcap * Update libcap to 1.2.69

--- ports/libcap/portfile.cmake
+++ ports/libcap/portfile.cmake
@@ -3,27 +3,40 @@ vcpkg_from_git(
   URL git://git.kernel.org/pub/scm/libs/libcap/libcap.git
   FETCH_REF "libcap-${VERSION}"
   REF 3c7dda330bd9a154bb5b878d31fd591e4951fe17
+  PATCHES
+    cap_names.separation.patch
 )
+# Note: if libcap version is updated, the according cap_names.h should be updated if necessary.
 
 # SKIP_CONFIGURE is broken https://github.com/microsoft/vcpkg/issues/14389
 # so we use patch to create empty configure executable
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/configure" DESTINATION "${SOURCE_PATH}")
+
+# This needs to be called before configure
+vcpkg_cmake_get_vars(cmake_vars_file)
+
+if(VCPKG_CROSSCOMPILING)
+  file(COPY "${CMAKE_CURRENT_LIST_DIR}/cap_names.h" DESTINATION "${SOURCE_PATH}/libcap/")
+endif ()
 
 vcpkg_configure_make(
   SOURCE_PATH "${SOURCE_PATH}"
   COPY_SOURCE
 )
 
-if(VCPKG_CROSSCOMPILING)
+include("${cmake_vars_file}")
+list(APPEND libcap_OPTIONS "prefix=${CURRENT_INSTALLED_DIR}" "CC=${VCPKG_DETECTED_CMAKE_C_COMPILER}" "AR=${VCPKG_DETECTED_CMAKE_AR}" "RANLIB=${VCPKG_DETECTED_CMAKE_RANLIB}")
+
+if (NOT VCPKG_CROSSCOMPILING)
+  # patch decouples cap_names.h target from libcap.* for cross compile compatibility,
+  # so for host=target build we manually make it
   vcpkg_build_make(SUBPATH libcap
     BUILD_TARGET cap_names.h
     LOGFILE_ROOT "configure"
     OPTIONS
-      prefix=${CURRENT_INSTALLED_DIR}
+      ${libcap_OPTIONS}
   )
-endif()
-
-list(APPEND libcap_OPTIONS "prefix=${CURRENT_INSTALLED_DIR}" "CC=$ENV{CC}" "AR=$ENV{AR}" "RANLIB=$ENV{RANLIB}")
+endif ()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   vcpkg_build_make(SUBPATH libcap
