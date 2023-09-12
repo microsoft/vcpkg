75f71e58db140184c6e7d8b5d30571cc445d972c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-09-12 20:59:52 +0200

    was  this the issue?

--- ports/vcpkg-make/vcpkg_make_common.cmake
+++ ports/vcpkg-make/vcpkg_make_common.cmake
@@ -101,7 +101,7 @@ function(z_vcpkg_make_prepare_compile_flags)
     set(RCFLAGS "${VCPKG_COMBINED_RC_FLAGS_${var_suffix}}")
 
     foreach(var IN ITEMS ABIFLAGS LDFLAGS ARFLAGS RCFLAGS)
-        vcpkg_list(APPEND flags ${${var}})
+        vcpkg_list(APPEND flags ${var})
     endforeach()
 
 
