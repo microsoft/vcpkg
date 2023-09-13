5dd56f9e1fee5fac8be979b3d8562e08ff8f63f8
Kai Pastor <dg0yt@darc.de>
2023-09-14 00:32:30 +0200

    [zeroc-ice] Fix Ice_LIBRARIES test

--- ports/zeroc-ice/vcpkg-ci-IceConfig.cmake
+++ ports/zeroc-ice/vcpkg-ci-IceConfig.cmake
@@ -2,6 +2,6 @@ enable_language(CXX)
 set(Ice_DEBUG 1)
 include(CMakeFindDependencyMacro)
 find_dependency(Ice COMPONENTS Ice++11)
-set(vcpkg-ci-zeroc-ice_LIBRARIES "${Ice_LIBRARIES}")
+set(vcpkg-ci-Ice_LIBRARIES "${Ice_LIBRARIES}")
 
 message(FATAL_ERROR STOP)
