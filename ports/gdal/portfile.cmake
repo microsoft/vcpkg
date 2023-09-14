3a642528351d8c71483efcbafdcc3843d8ea769f
Jim-Wang <wangzhijie05@beyondsoft.com>
2023-09-14 17:27:42 +0800

    fix build error on arm64

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -2,13 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OSGeo/gdal
     REF "v${VERSION}"
-    SHA512 38d488a5c70f0f4b6a3b99a14c747760574680f1f7fc5c6e4c373e3599a03c3227bb1bdefc14c7770f15b5087ff706362ed5a0fc0f5623835f006561f5db9595
+    SHA512 95b0dee07a616c8fb26ded2c538a6933ba070c0567e88af9356daea9b1df6c910edb4fcf55766839c1873829d20948b1714b3e2285e5ac57de8fcf0970ff53ff
     HEAD_REF master
     PATCHES
         find-link-libraries.patch
         fix-gdal-target-interfaces.patch
         libkml.patch
-        Fix-quote-variable.patch
         fix-jpeg.patch
 )
 # `vcpkg clean` stumbles over one subdir
