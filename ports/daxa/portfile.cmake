69fd3b58da58d9f67eb33cf874c49669616b3ac9
GabeRundlett <gabe@rundlett.com>
2023-09-13 21:47:46 -0600

    Fix Daxa port

--- ports/daxa/portfile.cmake
+++ ports/daxa/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/Ipotrick/Daxa
-    REF 61b699ac12de475cf4a79bc99106b865acddaf18
+    REF b7d61344a442a554a01e2f75af3522f626687aad
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
