74d6d7c9d95da2866152e76b0f79811a52e51d83
marinedalek <marinedalek@gmail.com>
2023-09-19 00:40:20 +0100

    [libremidi] Add new port

---
+++ ports/libremidi/portfile.cmake
@@ -0,0 +1,17 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO jcelerier/libremidi
+    REF "v4.0"
+    SHA512 8799f1edc1d700c96d4219398278dcf6c5e920c8ad08a39a70a64f4d21bc41863642cf9981b3c546e488463b3311f556f81df0c7eafeee9c405fcb83fa6e638c 
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libremidi PACKAGE_NAME libremidi)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
