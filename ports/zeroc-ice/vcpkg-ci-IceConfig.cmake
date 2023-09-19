3641a4a4414ed4d875d0fb176d96f9d6dd54b298
Kai Pastor <dg0yt@darc.de>
2023-09-19 08:55:03 +0200

    WIP

--- ports/zeroc-ice/vcpkg-ci-IceConfig.cmake
+++ ports/zeroc-ice/vcpkg-ci-IceConfig.cmake
@@ -1,5 +1,5 @@
 enable_language(CXX)
 set(Ice_DEBUG 1)
 include(CMakeFindDependencyMacro)
-find_dependency(Ice COMPONENTS Ice++11)
+find_dependency(Ice COMPONENTS Ice Ice++11)
 set(vcpkg-ci-Ice_LIBRARIES "${Ice_LIBRARIES}")
