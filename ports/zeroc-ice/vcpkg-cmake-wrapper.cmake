9a9144c8dffdbfb0628e7b6c2e230b36a376860e
Kai Pastor <dg0yt@darc.de>
2023-09-19 08:26:09 +0200

    WIP

--- ports/zeroc-ice/vcpkg-cmake-wrapper.cmake
+++ ports/zeroc-ice/vcpkg-cmake-wrapper.cmake
@@ -21,13 +21,15 @@ foreach(z_vcpkg_ice_component IN ITEMS Freeze
         find_library(Ice_${z_vcpkg_ice_cache}_LIBRARY_RELEASE
             NAMES ${z_vcpkg_ice_component} ${z_vcpkg_ice_component}37
             NAMES_PER_DIR
-            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
+            PATH_SUFFIXES lib lib/manual-link
             NO_DEFAULT_PATH
         )
         find_library(Ice_${z_vcpkg_ice_cache}_LIBRARY_DEBUG
             NAMES ${z_vcpkg_ice_component}d ${z_vcpkg_ice_component}37d ${z_vcpkg_ice_component} ${z_vcpkg_ice_component}37
             NAMES_PER_DIR
-            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib" "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
+            PATH_SUFFIXES debug/lib debug/lib/manual-link lib lib/manual-link
             NO_DEFAULT_PATH
         )
     endif()
@@ -35,13 +37,15 @@ foreach(z_vcpkg_ice_component IN ITEMS Freeze
         find_library(Ice_${z_vcpkg_ice_cache}++11_LIBRARY_RELEASE
             NAMES ${z_vcpkg_ice_component}++11 ${z_vcpkg_ice_component}37++11
             NAMES_PER_DIR
-            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
+            PATH_SUFFIXES lib lib/manual-link
             NO_DEFAULT_PATH
         )
         find_library(Ice_${z_vcpkg_ice_cache}++11_LIBRARY_DEBUG
             NAMES ${z_vcpkg_ice_component}++11d ${z_vcpkg_ice_component}37++11d ${z_vcpkg_ice_component}++11 ${z_vcpkg_ice_component}37++11
             NAMES_PER_DIR
-            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib" "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+            PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
+            PATH_SUFFIXES debug/lib debug/lib/manual-link lib lib/manual-link
             NO_DEFAULT_PATH
         )
     endif()
