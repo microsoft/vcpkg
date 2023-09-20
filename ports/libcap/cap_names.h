5be487d27af4647b6ab7ce7f8b6ea6e312b0ac3f
Jón Bjarni <jbbjarnason@gmail.com>
2023-09-20 16:56:16 +0000

    [libsystemd] add new port (#31150)
    * add libsystemd, libcap, libxcrypt
    
    * baseline libsystemd, libcap, libxcrypt
    
    * versions tree libsystemd, libcap, libxcrypt
    
    * licenses for libsystemd, libcap, libxcrypt in vcpkg.json
    
    * update tree for libsystemd, libcap, libxcrypt
    
    * allow restricted header libxcrypt
    
    * tree allow restricted header libxcrypt
    
    * remove message Warning
    
    * use targets exist during configure, instead of configure_file variable to search for zstd
    
    * update git-tree
    
    * add quotes to full paths and remove messages in portfiles
    
    * update git-tree
    
    * remove extra spaces
    
    * update git-tree
    
    * remove extra line
    
    * update git tree
    
    * do configure hack differently and remove cross compile patch and override compiler during build_make
    
    * update git tree
    
    * try either gcc or clang for libcap and specify log file root for each build command
    
    * update git-tree
    
    * libcap use host dependency of self
    
    * libcap update git tree
    
    * libsystemd is LGPL2.1
    
    * update libsystemd git-tree
    
    * change from find_package to find_dependency in unofficial cmake export
    of libsystemd
    
    * update libsystemd git-tree
    
    * update libxcrypt
    
    * update git tree libxcrypt
    
    * patch pkgconfig file of libsystemd
    
    * update libsystemd git-tree
    
    * update libsystemd to 254
    
    * update git-tree libsystemd
    
    * * Copy of cap_names.h checked in for cross compilation
    * Decouple cap_names.h from libcap.* make targets
    * Use vcpkg_cmake_get_vars instead of ENV{CC} etc.
    * Remove unnecessary self dependency of libcap
    * Update libcap to 1.2.69
    
    * Update git-tree
    
    * cleanup old versions from version jsons

---
+++ ports/libcap/cap_names.h
@@ -0,0 +1,56 @@
+/*
+ * DO NOT EDIT: this file is generated automatically from
+ *
+ *     <uapi/linux/capability.h>
+ */
+
+#define __CAP_BITS       41
+#define __CAP_NAME_SIZE  23
+
+#ifdef LIBCAP_PLEASE_INCLUDE_ARRAY
+#define LIBCAP_CAP_NAMES { \
+      /* 0 */	"cap_chown", \
+      /* 1 */	"cap_dac_override", \
+      /* 2 */	"cap_dac_read_search", \
+      /* 3 */	"cap_fowner", \
+      /* 4 */	"cap_fsetid", \
+      /* 5 */	"cap_kill", \
+      /* 6 */	"cap_setgid", \
+      /* 7 */	"cap_setuid", \
+      /* 8 */	"cap_setpcap", \
+      /* 9 */	"cap_linux_immutable", \
+      /* 10 */	"cap_net_bind_service", \
+      /* 11 */	"cap_net_broadcast", \
+      /* 12 */	"cap_net_admin", \
+      /* 13 */	"cap_net_raw", \
+      /* 14 */	"cap_ipc_lock", \
+      /* 15 */	"cap_ipc_owner", \
+      /* 16 */	"cap_sys_module", \
+      /* 17 */	"cap_sys_rawio", \
+      /* 18 */	"cap_sys_chroot", \
+      /* 19 */	"cap_sys_ptrace", \
+      /* 20 */	"cap_sys_pacct", \
+      /* 21 */	"cap_sys_admin", \
+      /* 22 */	"cap_sys_boot", \
+      /* 23 */	"cap_sys_nice", \
+      /* 24 */	"cap_sys_resource", \
+      /* 25 */	"cap_sys_time", \
+      /* 26 */	"cap_sys_tty_config", \
+      /* 27 */	"cap_mknod", \
+      /* 28 */	"cap_lease", \
+      /* 29 */	"cap_audit_write", \
+      /* 30 */	"cap_audit_control", \
+      /* 31 */	"cap_setfcap", \
+      /* 32 */	"cap_mac_override", \
+      /* 33 */	"cap_mac_admin", \
+      /* 34 */	"cap_syslog", \
+      /* 35 */	"cap_wake_alarm", \
+      /* 36 */	"cap_block_suspend", \
+      /* 37 */	"cap_audit_read", \
+      /* 38 */	"cap_perfmon", \
+      /* 39 */	"cap_bpf", \
+      /* 40 */	"cap_checkpoint_restore", \
+  }
+#endif /* LIBCAP_PLEASE_INCLUDE_ARRAY */
+
+/* END OF FILE */
