1b8c7c139f3447f2804a7eee93fab73e0e89c500
Zhao Liu <v-zhli17@microsoft.com>
2023-09-20 02:51:19 -0700

    [leveldb] Install util header files

--- ports/leveldb/portfile.cmake
+++ ports/leveldb/portfile.cmake
@@ -38,4 +38,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
