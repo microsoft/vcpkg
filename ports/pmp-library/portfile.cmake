58328198c3d7c639f4fac01e6c1a8b492dd40f87
cDc <cdc.seacave@gmail.com>
2023-09-21 12:50:09 +0300

    reviews

--- ports/pmp-library/portfile.cmake
+++ ports/pmp-library/portfile.cmake
@@ -22,4 +22,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/pmp" PACKAGE_NAME pmp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
