9b17dc71011692e898aaa7debf0745c54df0beb3
Kai Pastor <dg0yt@darc.de>
2023-09-21 07:52:37 +0200

    Revert "Trace"
    This reverts commit 91e6428688d3281ce8ce4427b4f6712b0ebfa176.

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -73,5 +73,3 @@ endif()
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
-
-message(FATAL_ERROR STOP)
