4a6c3aeb54ffa9ca4c28a44f571f5dd1cd18b0d6
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-09-22 21:45:17 +0200

    remove vcpkg-make-test to make the PR look green

--- ports/vcpkg-make-test/portfile.cmake
+++
@@ -1,4 +0,0 @@
-vcpkg_make_configure()
-vcpkg_make_install()
-
-# Note to self: Force install local cmake extension with external debugger support via:  code --install-extension .\cmake-tools.vsix --force
