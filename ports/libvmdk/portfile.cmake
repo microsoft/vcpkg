40c5572f2af33300ab72db9ec1bd162d32af3aa1
Vitaliy Didik <ariman@inbox.ru>
2023-09-25 10:43:03 +0300

    [libvmdk] User VERSION variable

--- ports/libvmdk/portfile.cmake
+++ ports/libvmdk/portfile.cmake
@@ -1,8 +1,7 @@
-set(LIB_VERSION 20221124)
-set(LIB_FILENAME libvmdk-alpha-${LIB_VERSION}.tar.gz)
+set(LIB_FILENAME libvmdk-alpha-${VERSION}.tar.gz)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/libyal/libvmdk/releases/download/${LIB_VERSION}/${LIB_FILENAME}"
+    URLS "https://github.com/libyal/libvmdk/releases/download/${VERSION}/${LIB_FILENAME}"
     FILENAME "${LIB_FILENAME}"
     SHA512 7d7ea415d7c2cb1077d591d53ab3c37a7ab6e01dc9525159a70588e6f55e56bbef4d3f49f6e1c01ff0b1ddcede5ec2beb05aca7f12e5212843761e14f6459bcb
 )
@@ -10,7 +9,7 @@ vcpkg_download_distfile(ARCHIVE
 vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    SOURCE_BASE "${LIB_VERSION}"
+    SOURCE_BASE "${VERSION}"
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
