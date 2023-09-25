36b69c81fb21ef8993f18640dbddf45c7899c520
Honeybunch <amt3824@g.rit.edu>
2023-09-25 11:48:41 -0700

    Fixing portfile install dir

--- ports/joltphysics/portfile.cmake
+++ ports/joltphysics/portfile.cmake
@@ -36,5 +36,5 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
