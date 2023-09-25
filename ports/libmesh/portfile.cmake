25ef80ce01176d91484036b8c262c163d25eb3ae
Jim-Wang <wangzhijie05@beyondsoft.com>
2023-09-25 16:42:17 +0800

    update to 1.7.1

--- ports/libmesh/portfile.cmake
+++ ports/libmesh/portfile.cmake
@@ -26,74 +26,28 @@ endif()
 # There a lot of configure options in this port which are not yet correctly handled by VCPKG
 # To only mention two:
 #  --enable-vtk-required   Error if VTK is not detected by configure
-#  --enable-capnp-required Error if Cap'n Proto support is not detected by
-# but there are a lot more which need to be checked/fixed
-# So this port can only be considered a Work In Progress
-vcpkg_configure_make(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS ${OPTIONS}
-    OPTIONS_DEBUG --with-methods=dbg
-    OPTIONS_RELEASE --with-methods=opt
-)
-
+	@@ -25,14 +39,14 @@ vcpkg_configure_make(
 vcpkg_install_make()
 
-<<<<<<< .mine
 if (EXISTS ${CURRENT_PACKAGES_DIR}/contrib/bin/libtool)
     file(COPY ${CURRENT_PACKAGES_DIR}/contrib/bin/libtool DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
     file(REMOVE ${CURRENT_PACKAGES_DIR}/contrib/bin/libtool)
 endif()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/contrib)
-=======
-file(RENAME "${CURRENT_PACKAGES_DIR}/examples" "${CURRENT_PACKAGES_DIR}/share/${PORT}/examples")
 
-
-
-
->>>>>>> .theirs
-
-<<<<<<< .mine
 file(GLOB LIBMESH_EXAMPLES ${CURRENT_PACKAGES_DIR}/examples/*)
 foreach (LIBMESH_EXAMPLE ${LIBMESH_EXAMPLES})
     file(COPY ${LIBMESH_EXAMPLE} DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 endforeach()
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/examples)
 
-=======
+	@@ -46,8 +60,10 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/contrib ${CURRENT_PACKAGES_DIR
+                    ${CURRENT_PACKAGES_DIR}/debug/share ${CURRENT_PACKAGES_DIR}/tools/libmesh/debug
+                    ${CURRENT_PACKAGES_DIR}/Make.common ${CURRENT_PACKAGES_DIR}/debug/Make.common)
 
-
-
-
-
-
->>>>>>> .theirs
-if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-# Remove tools and debug include directories
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/contrib" "${CURRENT_PACKAGES_DIR}/debug/contrib"
-                    "${CURRENT_PACKAGES_DIR}/etc" "${CURRENT_PACKAGES_DIR}/debug/etc"
-                    "${CURRENT_PACKAGES_DIR}/debug/examples" "${CURRENT_PACKAGES_DIR}/debug/include"
-                    "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/tools/libmesh/debug"
-                    "${CURRENT_PACKAGES_DIR}/Make.common" "${CURRENT_PACKAGES_DIR}/debug/Make.common")
-
-<<<<<<< .mine
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/etc")
 
 vcpkg_fixup_pkgconfig()
-=======
-file(GLOB config_files "${CURRENT_PACKAGES_DIR}/bin/*-config" "${CURRENT_PACKAGES_DIR}/debug/bin/*-config" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin/*-config")
-file(REMOVE_RECURSE ${config_files})
-
->>>>>>> .theirs
 vcpkg_copy_pdbs()
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libmesh/libmesh_config.h" "${CURRENT_INSTALLED_DIR}" "")
 
-<<<<<<< .mine
 vcpkg_install_copyright(FILE_LIST "${CURRENT_PORT_DIR}/copyright")
-
-=======
-vcpkg_install_copyright(FILE_LIST "${CURRENT_PORT_DIR}/copyright")
-
->>>>>>> .theirs
