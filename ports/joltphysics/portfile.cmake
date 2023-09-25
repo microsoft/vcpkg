a86eac9b9871ff46260a7cd3d752da6a057d52b3
Honeybunch <amt3824@g.rit.edu>
2023-09-25 11:57:36 -0700

    Fixing port name

--- ports/joltphysics/portfile.cmake
+++ ports/joltphysics/portfile.cmake
@@ -37,5 +37,5 @@ vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
