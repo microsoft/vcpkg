05acd14a762c816cc814881be5c613fc3ce0d30f
Honeybunch <amt3824@g.rit.edu>
2023-09-25 20:09:27 -0700

    Making sure config gets fixed up

--- ports/joltphysics/portfile.cmake
+++ ports/joltphysics/portfile.cmake
@@ -33,12 +33,11 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
 vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(COPY "${CURRENT_PACKAGES_DIR}/debug/share/unofficial-${PORT}/unofficial-${PORT}-targets-debug.cmake"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}")
+vcpkg_cmake_config_fixup(CONFIG_PATH share/unofficial-${PORT} PACKAGE_NAME unofficial-${PORT})
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
