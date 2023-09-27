e251e1f359d930d03f5d51d8546af5f829fc848d
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-09-27 08:58:34 +0800

    [fplus] update to 0.2.20-p0

--- ports/fplus/portfile.cmake
+++ ports/fplus/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Dobiasd/FunctionalPlus
-    REF v0.2.18-p0
-    SHA512 119aaef75020ef06818bf5d33db8bce272e89d69699df9be636bc6fdf06b584e1842440896a431ea2a75b88ce01472f3a9886b8dd781f5e5533315e9ad6860ac
+    REF "v${VERSION}"
+    SHA512 
     HEAD_REF master
 )
 
