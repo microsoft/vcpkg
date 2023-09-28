7afe0a4540bcf160f25661f0168b2e513317d970
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-09-28 23:38:27 +0800

    fix deps

--- ports/discount/portfile.cmake
+++ ports/discount/portfile.cmake
@@ -32,6 +32,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/discount)
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
