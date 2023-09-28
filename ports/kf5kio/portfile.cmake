94822b5504ee34a8bd5a5d586d1fd14f428a333c
Kai Pastor <dg0yt@darc.de>
2023-09-28 08:13:22 +0200

    [kf5kio] Cleanup

--- ports/kf5kio/portfile.cmake
+++ ports/kf5kio/portfile.cmake
@@ -1,11 +1,11 @@
-if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL static)
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     list(APPEND PATCHES fix_libiconv.patch)
 endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/kio
-    REF v5.98.0
+    REF "v${VERSION}"
     SHA512 6ffb44963b266f806150bc3ff9a1c3cf13f2fe7b4bda57e27d9bd9b931bfd757d50ee29f4143f6d282a25f675c8024aff16dc2f91e00ec0c7663eb8effdfee30
     HEAD_REF master
     PATCHES
@@ -32,7 +32,8 @@ vcpkg_cmake_configure(
         -DKDE_INSTALL_PLUGINDIR=plugins
         -DKDE_INSTALL_LIBEXECDIR=bin
         ${FEATURE_OPTIONS}
-    MAYBE_UNUSED_VARIABLES CMAKE_DISABLE_FIND_PACKAGE_KF5Notifications
+    MAYBE_UNUSED_VARIABLES
+        CMAKE_DISABLE_FIND_PACKAGE_KF5Notifications
 )
 
 vcpkg_cmake_install()
@@ -65,5 +66,3 @@ endif()
 
 file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
 vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
-
-
