a5a4bc5f5fe2fe8df8cd10c9aa58c1927fdc4221
luncliff <luncliff@gmail.com>
2023-09-29 21:16:31 +0900

    [llgi] split patch to pull request

--- ports/llgi/portfile.cmake
+++ ports/llgi/portfile.cmake
@@ -1,5 +1,12 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+# see https://github.com/altseed/LLGI/pull/150
+vcpkg_download_distfile(LLGI_PR_150_PATCH
+    URLS "https://patch-diff.githubusercontent.com/raw/altseed/LLGI/pull/150.diff"
+    FILENAME llgi-pr-150.patch
+    SHA512 4843985b00f7515295b70357bd1787e810fdc3f23e3f832280e43d4b617483e825f4f6abd8f75c1c9364305999515c999e4dbb85b29eb62fcb8b5229002db244
+)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO altseed/LLGI
@@ -7,7 +14,8 @@ vcpkg_from_github(
     SHA512 6d4e284bec2b5ef166fe1c12c235e8c41c03a2994756b434a3d1c902450b380fda8d3251424c1fe5200559e7d4285009563d6922f9e9278fdbaa8340576308eb
     HEAD_REF master
     PATCHES
-        fix-cmake.patch
+        ${LLGI_PR_150_PATCH}
+        fix-cmake-use-vcpkg.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
