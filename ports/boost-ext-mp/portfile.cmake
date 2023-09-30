b2b2e7959635bc09b722b50b8ff9f3628685b2b1
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-01 02:02:57 +0800

    fix deps

--- ports/boost-ext-mp/portfile.cmake
+++ ports/boost-ext-mp/portfile.cmake
@@ -7,12 +7,16 @@ vcpkg_from_github(
     PATCHES fix-build-flags.patch
 )
 
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(PACKAGE_NAME mp CONFIG_PATH "share/cmake/mp-0.0.1")
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
