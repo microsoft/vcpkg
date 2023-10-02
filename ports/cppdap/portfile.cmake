55b6e46dc4b9a0c633b1ff212f5c81cb8a03fb53
Frederik Carlier <frederik.carlier@keysight.com>
2023-10-02 20:23:53 +0200

    PR feedback

--- ports/cppdap/portfile.cmake
+++ ports/cppdap/portfile.cmake
@@ -1,5 +1,3 @@
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  google/cppdap
@@ -16,8 +14,8 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/cppdap")
 
-vcpkg_cmake_config_fixup(
-    CONFIG_PATH "lib/cmake/cppdap")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
