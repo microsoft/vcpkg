e8b71cff0e1309dd25a96655272154ec8b41f6e5
Liam <byteslice@airmail.cc>
2023-10-05 00:19:06 -0400

    [breakpad] update to 2023.01.27

--- ports/breakpad/portfile.cmake
+++ ports/breakpad/portfile.cmake
@@ -3,10 +3,11 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/breakpad
-    REF v2022.07.12
-    SHA512 872fa74520709d6510b798c7adfb7fed34a84b1831e774087515c23a005b0ea76ef7758bb565f0ff9f2153206cf53958621463fba0e055c9d31dc68f687e2b8f
+    REF v2023.01.27
+    SHA512 c6802c55653289780232b20e2abc0458c49f3cdff108c3ddfd6e40a2f378da34adbc158548e9c88cbfdbba9526477da9b68c2c45e205231e2802fe533b6bd6a4
     HEAD_REF master
     PATCHES
+        fix-const-char.patch
         fix-unique_ptr.patch
 )
 
@@ -37,6 +38,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/client/linux/data" "${CURRENT_PACKAGES_DIR}/include/client/linux/sender")
+
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/breakpad/breakpad-config.cmake" "include(CMakeFindDependencyMacro)
+find_dependency(ZLIB)
+")
 
 if("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES microdump_stackwalk minidump_dump minidump_stackwalk core2md pid2md dump_syms minidump-2-core minidump_upload sym_upload core_handler AUTO_CLEAN)
