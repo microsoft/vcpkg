d2828cacae13e3e0d265bcffc9112a4607366e04
ChenPi11 <102936596+ChenPi11@users.noreply.github.com>
2023-10-05 01:36:25 +0000

    [cppp-reiconv] Add Python required.

--- ports/cppp-reiconv/portfile.cmake
+++ ports/cppp-reiconv/portfile.cmake
@@ -4,10 +4,14 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 32a17e9931cbd329c15f34efe95a435857eb0b5ceeda862d0cf81a29141aa91da3329de5e21d0d718e37dadece3c5bfdc42efe7d3be46a593760540cb7240585
 )
 
-vcpkg_extract_source_archive_ex(
+vcpkg_extract_source_archive(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
 )
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_DIR ${PYTHON3} DIRECTORY)
+vcpkg_add_to_path("${PYTHON3_DIR}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
