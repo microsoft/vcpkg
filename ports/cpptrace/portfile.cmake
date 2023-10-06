1658e111d0559c8d04484f61464a8ccd151f257f
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-10-06 17:33:22 -0400

    fixes

--- ports/cpptrace/portfile.cmake
+++ ports/cpptrace/portfile.cmake
@@ -24,5 +24,6 @@ vcpkg_cmake_config_fixup(
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
