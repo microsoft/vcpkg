2894e40594fde88fb0052f08c92ac84a461ccd0b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-10-08 10:24:37 -0400

    [ports/keccak-tiny] New port

---
+++ ports/keccak-tiny/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO            coruus/${PORT}
+    REF             64b6647514212b76ae7bca0dea9b7b197d1d8186
+    SHA512          5cf14061efc1b3c934dfb28a08e2a478036109c449aed41d4deb975a9f0748db06f83c1de9e5d991009d04f0220a397f5f66232a4db04bbc0dea0c624522752c
+    HEAD_REF        master
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"
+     DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-${PORT})
+
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright" "CC0-1.0")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" [=[
+${PORT} provides CMake targets:
+    find_package(unofficial-${PORT} CONFIG REQUIRED)
+    target_link_libraries(main PRIVATE unofficial::${PORT}::${PORT})
+]=])
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/debug/include")
