70888d3ec098cdfbf1fe2f18433a715d12aed097
RT2 <contact@rt2.fr>
2023-10-09 18:38:30 +0200

    [enkits] Add usage

--- ports/enkits/portfile.cmake
+++ ports/enkits/portfile.cmake
@@ -24,3 +24,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/unofficial-enkiTS)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
