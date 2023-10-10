b2ca02fa770f3ebade29c319ad9150ed8614cbfc
Honeybunch <amt3824@g.rit.edu>
2023-10-09 18:59:35 -0700

    Updating usage

--- ports/enkits/portfile.cmake
+++ ports/enkits/portfile.cmake
@@ -24,4 +24,5 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
