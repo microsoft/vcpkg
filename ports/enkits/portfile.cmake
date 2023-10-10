33e91381d5a9aa782d711f84ebda1fe351f1c9bb
Honeybunch <amt3824@g.rit.edu>
2023-10-09 19:11:12 -0700

    Fixing very silly typo

--- ports/enkits/portfile.cmake
+++ ports/enkits/portfile.cmake
@@ -24,5 +24,6 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
