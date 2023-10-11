47856c69edfbf05fb3385b75fbb77588600cbece
Pravila <pravila0011@gmail.com>
2023-10-11 11:05:02 +0100

    Update ports/better-enums/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/better-enums/portfile.cmake
+++ ports/better-enums/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO aantron/better-enums
-    REF 0.11.3
+    REF ${VERSION}
     SHA512 5997c74932803fb96beabbe029d80f6fdeab7c46f781a4e11ef775242d294dfd82ca05cac99787dd68a622db62510fd5533e9c0e85a62c7792c0dbe6237af6d9
     HEAD_REF master
 )
