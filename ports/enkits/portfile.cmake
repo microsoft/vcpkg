0485e8703f1ad2792595ed8a45178d8310af3bab
Honeybunch <amt3824@g.rit.edu>
2023-10-11 09:29:27 -0700

    Trying to get cmake_config_fixup to work

--- ports/enkits/portfile.cmake
+++ ports/enkits/portfile.cmake
@@ -25,6 +25,5 @@ vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
