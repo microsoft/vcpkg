microsoft/vcpkg

commit 51475feaccb720ab40e5a0e3f8da70a4510defc0

[talib] New port (#34316)

ports/talib/portfile.cmake
@@ -1,0 +1,81 @@
+vcpkg_from_sourceforge(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO "ta-lib/ta-lib"
+    REF "${VERSION}"
+    FILENAME "ta-lib-${VERSION}-msvc.zip"
+    SHA512 5f211327b6a1d4f00d0a2b9e276adadd118d7aa29fc87c6771d550fda124a863b4a20e3803f325f7c903c82ea12bfb23121a5f0566eeaa434e0f107a6eedb737
+)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_CRT_LINKAGE STREQUAL "dynamic")
+    set(LFLAG "d")
+else()
+    set(LFLAG "m")
+endif()
+
+# Debug build
+if (NOT VCPKG_BUILD_TYPE)
+    vcpkg_execute_build_process(
+        COMMAND nmake -f Makefile
+        WORKING_DIRECTORY "${SOURCE_PATH}/c/make/c${LFLAG}d/win32/msvc"
+        LOGNAME build-${TARGET_TRIPLET}-dbg
+    )
+
+    file(
+        INSTALL "${SOURCE_PATH}/c/lib/ta_abstract_c${LFLAG}d.lib"
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        RENAME ta_abstract.lib
+    )
+    file(
+        INSTALL "${SOURCE_PATH}/c/lib/ta_libc_c${LFLAG}d.lib"
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        RENAME ta_libc.lib
+    )
+    file(
+        INSTALL "${SOURCE_PATH}/c/lib/ta_func_c${LFLAG}d.lib"
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        RENAME ta_func.lib
+    )
+    file(
+        INSTALL "${SOURCE_PATH}/c/lib/ta_common_c${LFLAG}d.lib"
+        DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+        RENAME ta_common.lib
+    )
+endif()
+
+# Release build
+vcpkg_execute_build_process(
+    COMMAND nmake -f Makefile
+    WORKING_DIRECTORY "${SOURCE_PATH}/c/make/c${LFLAG}r/win32/msvc"
+    LOGNAME build-${TARGET_TRIPLET}-rel
+)
+
+file(
+    INSTALL "${SOURCE_PATH}/c/lib/ta_abstract_c${LFLAG}r.lib"
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+    RENAME ta_abstract.lib
+)
+file(
+    INSTALL "${SOURCE_PATH}/c/lib/ta_libc_c${LFLAG}r.lib"
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+    RENAME ta_libc.lib
+)
+file(
+    INSTALL "${SOURCE_PATH}/c/lib/ta_func_c${LFLAG}r.lib"
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+    RENAME ta_func.lib
+)
+file(
+    INSTALL "${SOURCE_PATH}/c/lib/ta_common_c${LFLAG}r.lib"
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+    RENAME ta_common.lib
+)
+
+# Include files
+file(
+    INSTALL "${SOURCE_PATH}/c/include"
+    DESTINATION ${CURRENT_PACKAGES_DIR}
+)
+
+# License file
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.TXT")

commit a5b6ed497b4a8e0d025d6e620e5cb1b52807b6ea

[libosip2, talib] Don't install include/makefile.am.
Detected by https://dev.azure.com/vcpkg/public/_build/results?buildId=95376

```
error: The following files are already installed in D:/installed/x86-windows and are in conflict with talib:x86-windows
Installed by libosip2:x86-windows
include/Makefile.am

Stored binaries in 1 destinations in 446 ms.
Elapsed time to handle talib:x86-windows: 40 s
```

Broken by https://github.com/microsoft/vcpkg/pull/34316 which adds talib

ports/libosip2/portfile.cmake
@@ -34,7 +34,7 @@ if(VCPKG_TARGET_IS_WINDOWS)
         PROJECT_SUBPATH "platform/vsnet/osip2.vcxproj"
     )
 
-    file(COPY "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+    file(COPY "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include" PATTERN Makefile.am EXCLUDE)
 
     vcpkg_msbuild_install(
         SOURCE_PATH "${SOURCE_PATH}"

ports/talib/portfile.cmake
@@ -75,6 +75,7 @@ file(
 file(
     INSTALL "${SOURCE_PATH}/c/include"
     DESTINATION ${CURRENT_PACKAGES_DIR}
+    PATTERN Makefile.am EXCLUDE
 )
 
 # License file