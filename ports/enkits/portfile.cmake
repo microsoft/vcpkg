fdb0aeaec1d4dd2edfb4e73602625fb15af641ab
Honeybunch <amt3824@g.rit.edu>
2023-10-12 17:24:06 -0700

    Fixing config fixup

--- ports/enkits/portfile.cmake
+++ ports/enkits/portfile.cmake
@@ -22,7 +22,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup()
+# Must specify config path due to case sensitivity on some filesystems
+vcpkg_cmake_config_fixup(CONFIG_PATH share/enkiTS)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
