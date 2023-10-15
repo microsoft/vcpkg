f90aef353913fca011cc0a9f4aa2d43860fbbe10
Alexander ROZDAYBEDA <alexander.rozdaybeda@avanquest.com>
2023-10-15 22:17:14 +0300

    move install headers to portfile

--- ports/cld3/portfile.cmake
+++ ports/cld3/portfile.cmake
@@ -9,11 +9,13 @@ vcpkg_from_github(
   SHA512 c3650ffbf5855aaf04d03930f01c6efd76e1f2b2d47365348721f16531a14653ae5b3aff8fefa8e5fa1c769fdf1a9b441a88bc687f97f8c579b84f17c6984c9e
   HEAD_REF master
   PATCHES
-      fix-install.patch
+      fix-build.patch
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS ${FEATURE_OPTIONS}
+    -DINSTALL_LIB_DIR:STRING=lib
 )
 
 vcpkg_cmake_install()
@@ -22,5 +24,13 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(GLOB PUBLIC_HEADER_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/src/*.h")
+file(INSTALL ${PUBLIC_HEADER_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/cld3")
 
+file(GLOB HEADERS_SCRIPT_SPAN_FILES LIST_DIRECTORIES false "${SOURCE_PATH}/src/script_span/*.h")
+file(INSTALL ${HEADERS_SCRIPT_SPAN_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/cld3/script_span")
+
+file(GLOB HEADERS_PROTO_FILES LIST_DIRECTORIES false "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/cld_3/protos/*.h")
+file(INSTALL ${HEADERS_PROTO_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/cld_3/protos")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
