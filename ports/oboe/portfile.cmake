f36ef49ec9f3e450ccdd0c8c77e4d30b4dadfdea
Vitaly Petrov <v31337@gmail.com>
2023-10-17 10:21:43 +0200

    Fixes by review

--- ports/oboe/portfile.cmake
+++ ports/oboe/portfile.cmake
@@ -1,30 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/oboe
-    REF 1.7-stable
-    SHA512 2b89b9ba40e3b1f6f8d2b7ab4f7ef5bfec70d89e1f93b85cc01ab30793c50a5d4a550a768e0ddf3050614f54b98c00b3847c25e9ff7cfc0698cfbb4910102321
+    REF ${VERSION}
+    SHA512 7eeaf85f9889e03dd1e7f5de0e9f2cee815fc555fddfdb8c4d3450d67f6ae11b0ca43b63c73e869bfc4629d2f8e5bdb23a5833c665ca5226c339f74b9b34a8ad
     HEAD_REF master
     PATCHES
         fix_install.patch
 )
 
-set(CMAKE_ARGS "")
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-    set(CMAKE_ARGS "-DBUILD_SHARED_LIBS=true -DANDROID_STL=c++_shared")
-else()
-    set(CMAKE_ARGS "-DBUILD_SHARED_LIBS=false")
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DOBOE_EXAMPLES=OFF
-        ${CMAKE_ARGS}
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
