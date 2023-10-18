d45ad919af2fcc09c156f679b99503feb84d4ecf
LilyWangLL <494550702@qq.com>
2023-10-18 00:40:17 -0700

    update portfile.cmake

--- ports/wxwidgets/portfile.cmake
+++ ports/wxwidgets/portfile.cmake
@@ -189,4 +189,4 @@ endif()
 configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/licence.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/docs/licence.txt")
