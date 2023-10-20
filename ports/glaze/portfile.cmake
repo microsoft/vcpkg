646abf9fbb3b40282b9594d67a4dab6993f01298
vzhli17 <v-zhli17@microsoft.com>
2023-10-20 00:13:54 -0700

    [glaze] update to 1.5.3

--- ports/glaze/portfile.cmake
+++ ports/glaze/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stephenberry/glaze
     REF "v${VERSION}"
-    SHA512 f9730a9b572d50f4d7f50cb7d8584d3c1169ecf34e58017e96cc581de066e864fa5551f6a8d32aaf1a99ddbcebe3b3cbc2961b77a9b33dc83fb75f4558007531
+    SHA512 59895a4a43092ad2c77151cd57db56e87cddcb9e2f8c97eefc148360471cf4c54cf0c49eddc793caebfe81a85505fc329b0c8aeda03ebbbf319c3c041a22f253
 )
 
 vcpkg_cmake_configure(
