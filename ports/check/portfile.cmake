4f1305170b3012dc30dcf471d14fa89af1e3b5be
Kai Pastor <dg0yt@darc.de>
2023-10-22 13:55:03 +0200

    [check] Follow VCPKG_LIBRARY_LINKAGE

--- ports/check/portfile.cmake
+++ ports/check/portfile.cmake
@@ -4,7 +4,9 @@ vcpkg_from_github(
     REF 11970a7e112dfe243a2e68773f014687df2900e8 # 0.15.2
     SHA512 210c9617fa1c1ce16bef983b0e6cb587b1774c3f7ce27a53ca7799642dc7a14be8de567d69dc0e57845684c6f7991d772c73654f63c8755afda3b37a35c7156e
     HEAD_REF master
-    PATCHES fix-lib-path.patch
+    PATCHES
+        fix-lib-path.patch
+        linkage.patch
 )
 
 vcpkg_cmake_configure(
@@ -18,19 +20,12 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/check)
-
 vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/check)
+vcpkg_fixup_pkgconfig()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-# cleanup
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/man")
 
-vcpkg_fixup_pkgconfig()
-
-file(INSTALL "${SOURCE_PATH}/COPYING.LESSER" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(COPY "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.LESSER")
