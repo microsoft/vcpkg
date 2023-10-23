c4e027e5e6563a4071aa837d2051e49895d619ec
Kai Pastor <dg0yt@darc.de>
2023-10-23 19:23:15 +0200

    [gz-common5] Take care of pkg-config

--- ports/gz-common5/portfile.cmake
+++ ports/gz-common5/portfile.cmake
@@ -8,6 +8,12 @@ ignition_modular_library(
    OPTIONS 
       -DBUILD_TESTING=OFF
       -DUSE_EXTERNAL_TINYXML2=ON
+      "-DPKG_CONFIG_EXECUTABLE=${CURRENT_HOST_INSTALLED_DIR}/tools/pkgconf/pkgconf${VCPKG_HOST_EXECUTABLE_SUFFIX}"
    PATCHES
       gz_remotery_vis.patch
 )
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/gz-common5-graphics/gz-common5-graphics-config.cmake" "find_package(GTS "
+"# Ensure that consumers get a pkg-config tool which is needed for GTS
+set(PKG_CONFIG_EXECUTABLE \"\${CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/pkgconf/pkgconf${VCPKG_HOST_EXECUTABLE_SUFFIX}\" CACHE STRING vcpkg)
+find_package(GTS ")
