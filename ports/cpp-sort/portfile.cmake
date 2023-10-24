e13df78d7aec3eb8aa1eef18aba4c66729a8e570
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-24 16:51:46 +0800

    [cpp-sort] add new port

--- ports/cpp-sort/portfile.cmake
+++ ports/cpp-sort/portfile.cmake
@@ -14,11 +14,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME cpp-sort CONFIG_PATH "lib/cmake/cpp-sort")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/cpp-sort")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
