2715f0b4443cfbf09bfe583b0f806ab4ebaacadb
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-26 02:17:47 +0800

    [itsy-bitsy] add new port

---
+++ ports/itsy-bitsy/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ThePhD/itsy_bitsy
+    REF d5b6bf9509bb2dff6235452d427f0b1c349d5f8b
+    SHA512 06489e46ad55a7fa55ddf88290509b157cf53518a8d9532d5a56e9907e5efaa298cb8946807e497461d322f62b4bad9b16864ef0def527edc8503f7a7884b8e1
+    HEAD_REF main
+    PATCHES fix-cmake.patch
+)
+
+set(VCPKG_BUILD_TYPE release) # header-only
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME itsy.bitsy CONFIG_PATH "lib/cmake/itsy.bitsy")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+#file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
---
+++ ports/ztd-cuneicode/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO soasis/cuneicode
+  REF c4bfc7fba686ddb7e1d69f10943775b99865983b
+  SHA512 dcf34a7139be938e3fd3155dd7bc2dc101afdfd364aea00ec4e23cd4d6a4562b6936a3f8b3584c12bbc6bcaa436498e2c4ca5d3aa95f39161639ecd47a11f116
+  HEAD_REF main
+  PATCHES fix-cmake-install.patch
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
