97318029628bf4c8bc706b1ed0235cc081fffb9a
Kai Pastor <dg0yt@darc.de>
2023-10-26 22:16:15 +0200

    [libmysql] Fix deps, cont.

--- ports/libmysql/portfile.cmake
+++ ports/libmysql/portfile.cmake
@@ -13,8 +13,9 @@ vcpkg_from_github(
         install-exports.patch
         fix_dup_symbols.patch
 )
-
-file(REMOVE_RECURSE "${SOURCE_PATH}/include/boost_1_70_0")
+file(GLOB third_party "${SOURCE_PATH}/extra/*" "${SOURCE_PATH}/include/boost_1_70_0")
+list(REMOVE_ITEM third_party "${SOURCE_PATH}/extra/libedit")
+file(REMOVE_RECURSE ${third_party})
 
 #Skip the version check for Visual Studio
 set(FORCE_UNSUPPORTED_COMPILER "")
@@ -28,23 +29,31 @@ string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static"  STATIC_CRT_LINKAGE)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DINSTALL_INCLUDEDIR=include/mysql
+        -DINSTALL_DOCDIR=share/${PORT}/doc
+        -DINSTALL_MANDIR=share/${PORT}/doc
+        -DINSTALL_INFODIR=share/${PORT}/doc
+        -DINSTALL_DOCREADMEDIR=share/${PORT}
+        -DINSTALL_SHAREDIR=share
+        -DINSTALL_MYSQLSHAREDIR=share/${PORT}
         -DWITHOUT_SERVER=ON
         -DWITH_BUILD_ID=OFF
         -DWITH_UNIT_TESTS=OFF
         -DENABLED_PROFILING=OFF
-        -DENABLE_TESTING=OFF
         -DWIX_DIR=OFF
         -DIGNORE_BOOST_VERSION=ON
-        -DWITH_SYSTEMD=OFF
         -DWITH_TEST_TRACE_PLUGIN=OFF
         -DMYSQL_MAINTAINER_MODE=OFF
         -DBUNDLE_RUNTIME_LIBRARIES=OFF
         -DDOWNLOAD_BOOST=OFF
-        -DWITH_SSL=system
-        -DWITH_ICU=system
-        -DWITH_LIBEVENT=system
+        -DWITH_CURL=none
+        -DWITH_EDITLINE=bundled # not in vcpkg
         -DWITH_LZ4=system
+        -DWITH_RAPIDJSON=system
+        -DWITH_SSL=system
+        -DWITH_SYSTEMD=OFF
         -DWITH_ZLIB=system
+        -DWITH_ZSTD=system
         -DFORCE_UNSUPPORTED_COMPILER=${FORCE_UNSUPPORTED_COMPILER}
         -DINSTALL_STATIC_LIBRARIES=${BUILD_STATIC_LIBS}
         -DLINK_STATIC_RUNTIME_LIBRARIES=${STATIC_CRT_LINKAGE}
@@ -56,6 +65,8 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install(ADD_BIN_TO_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libmysql)
+vcpkg_fixup_pkgconfig()
 
 list(APPEND MYSQL_TOOLS
     comp_err
@@ -90,72 +101,20 @@ endif()
 
 vcpkg_copy_tools(TOOL_NAMES ${MYSQL_TOOLS} AUTO_CLEAN)
 
-file(RENAME "${CURRENT_PACKAGES_DIR}/share" "${CURRENT_PACKAGES_DIR}/${PORT}")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share")
-file(RENAME "${CURRENT_PACKAGES_DIR}/${PORT}" "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-if(NOT VCPKG_BUILD_TYPE)
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/${PORT}")
-    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/share")
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/${PORT}" "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}")
-endif()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libmysql CONFIG_PATH share/${PORT}/unofficial-libmysql)
-
-# switch mysql into /mysql
-file(RENAME "${CURRENT_PACKAGES_DIR}/include" "${CURRENT_PACKAGES_DIR}/include2")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/include")
-file(RENAME "${CURRENT_PACKAGES_DIR}/include2" "${CURRENT_PACKAGES_DIR}/include/mysql")
-
-## delete useless vcruntime/scripts/bin/msg file
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
-    "${CURRENT_PACKAGES_DIR}/debug/man"
-    "${CURRENT_PACKAGES_DIR}/docs"
-    "${CURRENT_PACKAGES_DIR}/debug/docs"
-    "${CURRENT_PACKAGES_DIR}/lib/debug"
-    "${CURRENT_PACKAGES_DIR}/lib/plugin"
-    "${CURRENT_PACKAGES_DIR}/debug/lib/plugin"
 )
 
-# delete dynamic dll on static build
-if (BUILD_STATIC_LIBS)
-    # libmysql.dll
-    file(REMOVE_RECURSE 
-        "${CURRENT_PACKAGES_DIR}/bin" 
-        "${CURRENT_PACKAGES_DIR}/debug/bin"
-    )
-    file(REMOVE
-        "${CURRENT_PACKAGES_DIR}/lib/libmysql.lib"
-        "${CURRENT_PACKAGES_DIR}/debug/lib/libmysql.lib"
-        "${CURRENT_PACKAGES_DIR}/lib/libmysql.pdb"
-        "${CURRENT_PACKAGES_DIR}/debug/lib/libmysql.pdb"
-    )
-endif()
-
-## remove misc files
-file(REMOVE
-    "${CURRENT_PACKAGES_DIR}/LICENSE"
-    "${CURRENT_PACKAGES_DIR}/README"
-    "${CURRENT_PACKAGES_DIR}/debug/LICENSE"
-    "${CURRENT_PACKAGES_DIR}/debug/README"
-)
-
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/mysql/mysql_com.h" "#include <mysql/udf_registration_types.h>" "#include \"mysql/udf_registration_types.h\"")
 if (NOT VCPKG_TARGET_IS_WINDOWS)
     set(MYSQL_CONFIG_FILE "${CURRENT_PACKAGES_DIR}/tools/libmysql/mysql_config")
-    vcpkg_replace_string(${MYSQL_CONFIG_FILE} "/bin/mysql_.*config" "/tools/libmysql/mysql_.*config")  # try to get correct $basedir
-    vcpkg_replace_string(${MYSQL_CONFIG_FILE} "${CURRENT_PACKAGES_DIR}" "$basedir")  # use $basedir to format paths
-    vcpkg_replace_string(${MYSQL_CONFIG_FILE} "-l\$\<\$\<CONFIG:DEBUG\>:${CURRENT_INSTALLED_DIR}/debug/lib/libz.a> " "")  # remove debug version of libz
-    vcpkg_replace_string(${MYSQL_CONFIG_FILE} 
-        "\$\<\$\<NOT:\$\<CONFIG:DEBUG\>\>:${CURRENT_INSTALLED_DIR}" 
-        "`dirname $0`/../../../../installed/${TARGET_TRIPLET}")  # correct path for release version of libz
-    vcpkg_replace_string(${MYSQL_CONFIG_FILE} "\> -l" " -l") # trailing > tag for previous item
+    vcpkg_replace_string("${MYSQL_CONFIG_FILE}" "/bin/mysql_.*config" "/tools/libmysql/mysql_.*config")
+    vcpkg_replace_string("${MYSQL_CONFIG_FILE}" "'${CURRENT_PACKAGES_DIR}" "\"\$basedir\"\'")
 endif()
 
 file(INSTALL "${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-# copy license
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+set(libedit_copying "${SOURCE_PATH}/COPYING for libedit")
+file(COPY_FILE "${SOURCE_PATH}/extra/libedit/libedit-20210910-3.1/COPYING" "${libedit_copying}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${libedit_copying}")
