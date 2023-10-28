d6fe9b94a36bf19d0885a269b3527ffedd225f5c
Julia DeMille <me@jdemille.com>
2023-10-27 19:36:20 -0500

    libclipboard: Satisfy CI.

--- ports/libclipboard/portfile.cmake
+++ ports/libclipboard/portfile.cmake
@@ -9,4 +9,7 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
+# Satisfy CI.
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
