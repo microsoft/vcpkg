b224b9eaa60b4ca4e9522859882a9aa46f619dad
Julia DeMille <me@jdemille.com>
2023-10-27 23:24:50 -0500

    libclipboard: More fixes.

--- ports/libclipboard/portfile.cmake
+++ ports/libclipboard/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
 )
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "LibClipboard")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 # Satisfy CI.
