dc167137234fa11a2248d77528b067e304692ffd
Kai Pastor <dg0yt@darc.de>
2023-10-28 18:54:38 +0200

    WIP

--- ports/libmysql/vcpkg-cmake-wrapper.cmake
+++ ports/libmysql/vcpkg-cmake-wrapper.cmake
@@ -1,9 +1,3 @@
 find_package(unofficial-libmysql CONFIG REQUIRED)
-if (TARGET mysqlclient)
-    set(MYSQL_LIBRARY mysqlclient)
-elseif (TARGET libmysql)
-    set(MYSQL_LIBRARY libmysql)
-endif()
-
 set(libmysql_FOUND 1)
-set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+set(MYSQL_LIBRARIES unofficial::libmysql::libmysql)
