e461b506121abead6aa5cfe88b6e84a1fcfa9fb0
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-29 11:04:05 +0800

    fix ztd marco

--- ports/ztd-cuneicode/portfile.cmake
+++ ports/ztd-cuneicode/portfile.cmake
@@ -7,7 +7,9 @@ vcpkg_from_github(
   REF c4bfc7fba686ddb7e1d69f10943775b99865983b
   SHA512 dcf34a7139be938e3fd3155dd7bc2dc101afdfd364aea00ec4e23cd4d6a4562b6936a3f8b3584c12bbc6bcaa436498e2c4ca5d3aa95f39161639ecd47a11f116
   HEAD_REF main
-  PATCHES fix-cmake-install.patch
+  PATCHES 
+      fix-cmake-install.patch
+      fix-ztd-marco.patch
 )
 
 vcpkg_cmake_configure(
