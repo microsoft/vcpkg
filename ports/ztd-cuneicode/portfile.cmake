04d55bea5023a006fd41f42c708f7925790d952a
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-29 11:31:57 +0800

    use upstream changes

--- ports/ztd-cuneicode/portfile.cmake
+++ ports/ztd-cuneicode/portfile.cmake
@@ -4,12 +4,11 @@ endif()
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO soasis/cuneicode
-  REF c4bfc7fba686ddb7e1d69f10943775b99865983b
-  SHA512 dcf34a7139be938e3fd3155dd7bc2dc101afdfd364aea00ec4e23cd4d6a4562b6936a3f8b3584c12bbc6bcaa436498e2c4ca5d3aa95f39161639ecd47a11f116
+  REF 05f89cbc2ba2b8e8a9136693151235e49ce91119
+  SHA512 ac768e5173cba55695e273c2244e97a3a4e839700b108bea52111389655b26147275a545f210d1975748aa4775c1868d1bd128ebe9a5f556c1e38adcaf03fb85
   HEAD_REF main
   PATCHES 
       fix-cmake-install.patch
-      fix-ztd-marco.patch
 )
 
 vcpkg_cmake_configure(
