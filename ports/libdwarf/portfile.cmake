7811b9e9bd7d9d9008a0199517f2920540eef217
Kai Pastor <dg0yt@darc.de>
2023-10-31 00:45:34 +0100

    Update portfile and patches (#1)

--- ports/libdwarf/portfile.cmake
+++ ports/libdwarf/portfile.cmake
@@ -4,27 +4,33 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 3117c69cc77d5a1189aeb1ea7e74d917dedfb84e9e9e98e3df7fec930f8183d12f55bb12e4871ed3746cdb19a29aba924bc73d6334b23bbb6413a1f4be67d499
     HEAD_REF main
-    PATCHES v0.8.0-patches.patch
+    PATCHES
+        dependencies.diff
+        msvc-runtime.diff
+        no-suffix.diff
 )
 
-# Apparently -DBUILD_NON_SHARED=On is the right option regardless of VCPKG_LIBRARY_LINKAGE being static or not. I'm very
-# confused what the libdwarf cmake script is doing.
-vcpkg_list(SET options -DBUILD_NON_SHARED=On)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_NON_SHARED)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS ${options}
+    OPTIONS
+        -DBUILD_NON_SHARED=${BUILD_NON_SHARED}
+        -DBUILD_SHARED=${BUILD_SHARED}
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(
-    PACKAGE_NAME "libdwarf"
-    CONFIG_PATH "lib/cmake/libdwarf"
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/libdwarf")
+vcpkg_fixup_pkgconfig()
+
+vcpkg_copy_tools(TOOL_NAMES dwarfdump AUTO_CLEAN)
+
+if(BUILD_SHARED)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libdwarf/libdwarf.h" "ifndef LIBDWARF_STATIC" "if 1")
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
