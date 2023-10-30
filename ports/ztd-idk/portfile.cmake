bcd0465bacab30cd676826690b3035613c347854
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-10-30 14:22:08 +0800

    fix comparison

--- ports/ztd-idk/portfile.cmake
+++ ports/ztd-idk/portfile.cmake
@@ -7,7 +7,9 @@ vcpkg_from_github(
   REF 00c82f4fd3b119ace0ea5e654945e9b7f3fd0b20 
   SHA512 14c33d97ef45da3f5f4d7064ed9de7e89a03b10d0537ee54c11b456561845e52cf95d034ef951e51d3eeb96e9d4b8db0175fa5ef59d8bb53bcc01fe3935742aa
   HEAD_REF main
-  PATCHES fix-cmake-install.patch
+  PATCHES 
+      fix-cmake-install.patch
+      fix-comparison.patch
 )
 
 vcpkg_cmake_configure(
