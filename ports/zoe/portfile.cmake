2dc7705acf8484f2c8ef328d7af09d0a1c6b71fa
winsoft666 <winsoft666@outlook.com>
2023-10-31 15:35:19 +0800

    [zoe] Add usage and config fixup.

--- ports/zoe/portfile.cmake
+++ ports/zoe/portfile.cmake
@@ -19,13 +19,11 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/zoe")
-    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/zoe)
-elseif(EXISTS "${CURRENT_PACKAGES_DIR}/share/zoe")
-    vcpkg_cmake_config_fixup(CONFIG_PATH share/zoe)
-endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/zoe)
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_copy_pdbs()
