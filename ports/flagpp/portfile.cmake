5d8aa23a7d383b6655e0a219494b2c468cee36f1
Jim-Wang <wangzhijie05@beyondsoft.com>
2023-10-31 10:26:00 +0000

    add new port

---
+++ ports/flagpp/portfile.cmake
@@ -0,0 +1,15 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Curve/flagpp
+    REF "v${VERSION}"
+    SHA512 82e07880105495213f8a78dba42bf3fb36d862e891280cca1ea02b694d6cc33f72903817e74e48d701ffa901ab0f9604f3204631256974afd7a755153a106e29
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+
+vcpkg_cmake_install()
+
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/LICENSE")
