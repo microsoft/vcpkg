869f2974023665500f978c2970554edc2d53bb7d
winsoft666 <winsoft666@outlook.com>
2023-10-31 17:05:22 +0800

    [zoe] remove usage.

--- ports/zoe/portfile.cmake
+++ ports/zoe/portfile.cmake
@@ -21,7 +21,6 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH share/zoe)
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
