8f24ff9a583cc10b6e7a5f1043fef0f6035e4b6f
xiaozhuai <798047000@qq.com>
2023-11-01 18:23:01 +0800

    [angle] Fix Class WebSwapCGLLayer is implemented in both WebKit.framework and angle

--- ports/angle/portfile.cmake
+++ ports/angle/portfile.cmake
@@ -46,6 +46,7 @@ vcpkg_from_github(
         001-fix-uwp.patch
         002-fix-builder-error.patch
         003-fix-mingw.patch
+        004-fix-webswapcgllayer-both-implemented.patch
 )
 
 # Generate angle_commit.h
