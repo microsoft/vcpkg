241609173a8866def637a3d1d56507f221172d7b
Mengna-Li <543250287@qq.com>
2023-11-02 10:16:55 +0000

    update

--- ports/libbson/portfile.cmake
+++ ports/libbson/portfile.cmake
@@ -54,6 +54,10 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/share/mongo-c-driver"
 )
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION  "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(
