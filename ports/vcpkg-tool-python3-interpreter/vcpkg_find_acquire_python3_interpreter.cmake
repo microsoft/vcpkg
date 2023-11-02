58169a40c7d099ff3b9fb6e9bd08703fb3422521
Billy Robert O'Neal III <bion@microsoft.com>
2023-11-01 19:52:01 -0700

    Update Python and add MIN_VERSION.

--- ports/vcpkg-tool-python3-interpreter/vcpkg_find_acquire_python3_interpreter.cmake
+++ ports/vcpkg-tool-python3-interpreter/vcpkg_find_acquire_python3_interpreter.cmake
@@ -1,12 +1,16 @@
 include_guard(GLOBAL)
 function(vcpkg_find_acquire_python3_interpreter out_var)
-    cmake_parse_arguments(PARSE_ARGV 1 arg "" "" "")
+    cmake_parse_arguments(PARSE_ARGV 1 arg "" "MIN_VERSION" "")
     if(DEFINED arg_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "Unrecognized arguments: ${arg_UNPARSED_ARGUMENTS}")
     endif()
 
     if(CMAKE_HOST_WIN32)
-        set(program_version 3.10.7)
+        set(program_version 3.11.5)
+        if(DEFINED arg_MIN_VERSION AND ("${program_version}" VERSION_LESS "${arg_MIN_VERSION}"))
+            message(FATAL_ERROR "This function fetches Python ${program_version}, but ${arg_MIN_VERSION} was requested.")
+        endif()
+
         cmake_host_system_information(RESULT is64_bit QUERY IS_64BIT)
         set(installed_pythons "")
         foreach(candidate IN ITEMS "$ENV{ProgramFiles}/Python3*" "$ENV{ProgramW6432}/Python3*" "$ENV{ProgramFiles\(x86\)}/Python3*")
@@ -22,7 +26,7 @@ function(vcpkg_find_acquire_python3_interpreter out_var)
                 TOOL_NAME python
                 VERSION ${program_version}
                 DOWNLOAD_FILENAME "python-${program_version}-embed-amd64.zip"
-                SHA512 29b47f8073b54c092a2c8b39b09ab392f757a8c09149e8d2de043907fffb250b5f6801175e16fedb4fae7b6555822acdc57d81d13c2fea95ef0f6ed717f86cb9
+                SHA512 29a526da7624423b09ea1c8f94d83e5ad2d7ba7553c5651d8fcbe1b2483c62f27f9db105d1fdcfca3357b44d456fe1141274725bc97ad5166edfec14f251eb36
                 URLS "https://www.python.org/ftp/python/${program_version}/python-${program_version}-embed-amd64.zip"
                 TOOL_SUBDIRECTORY "python-${program_version}-amd64/python3"
                 VERSION_COMMAND "--version"
@@ -37,7 +41,7 @@ function(vcpkg_find_acquire_python3_interpreter out_var)
                 TOOL_NAME python
                 VERSION ${program_version}
                 DOWNLOAD_FILENAME "python-${program_version}-embed-win32.zip"
-                SHA512 a69445906a909ce5f2554c544fe4251a8ab9c5028b531975b8c78fa8e98295b2bf06e1840f346a3c0edf485a7792c40c9d318bffd36b9c7829ac72b7cf8697bc
+                SHA512 d5412c5bc2a0664f86e504a536c201789c8fd8b97c641bbb7b254c87c2f13504d25fa9d0b74e27a1c54c2d9fb592f9546d8c1e82c506dc9c76a21774c4c3ea75
                 URLS "https://www.python.org/ftp/python/${program_version}/python-${program_version}-embed-win32.zip"
                 TOOL_SUBDIRECTORY "python-${program_version}-x86/python3"
                 VERSION_COMMAND "--version"
@@ -47,13 +51,18 @@ function(vcpkg_find_acquire_python3_interpreter out_var)
         endif()
 
         if(tool_acquired)
-            file(REMOVE "${out_extract_root}/python310._pth")
+            file(REMOVE "${out_extract_root}/python311._pth")
         endif()
     else()
+        set(SEARCH_VERSION "3.0.0")
+        if(DEFINED arg_MIN_VERSION)
+            set(SEARCH_VERSION "${arg_MIN_VERSION}")
+        endif()
+
         vcpkg_find_acquire_tool(
             OUT_TOOL_PATH out_var_value
             TOOL_NAME "python3"
-            VERSION 3.0.0
+            VERSION "${SEARCH_VERSION}"
             BREW_PACKAGE_NAME "python@3"
             APT_PACKAGE_NAME "python3"
             DNF_PACKAGE_NAME "python"
