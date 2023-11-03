27f9b1f38ecce4b544d513740345585174107956
Saad <saadmuizz@gmail.com>
2023-11-03 12:50:13 -0400

    update nanojson

--- ports/nanojson/portfile.cmake
+++ ports/nanojson/portfile.cmake
@@ -1,34 +1,13 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH source
         REPO saadshams/nanojson
-        TAG 1.0.0
+        REF 1.0.0
+        SHA512 0
+        HEAD_REF master
 )
 
-add_definitions(-DYOUR_LIBRARY_VERSION="1.0.0")  # Define a version macro
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
-# Build YourLibrary
-vcpkg_configure_cmake(
-        SOURCE_PATH ${source}
-        BINARY_DIR build
-        CMAKE_CACHE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${vcpkg_installed_dir}
-)
+vcpkg_cmake_install()
 
-vcpkg_install(
-        DESTINATION include  # Install headers
-        FILES include/
-        INSTALL_HEADERS
-)
-
-vcpkg_install(
-        DESTINATION api  # Install library files
-        FILES build/libnanojson.a
-        DISPLAY_NAME "nanoJSON"
-)
-
-file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/COPYRIGHT.txt" DESTINATION "${vcpkg_installed_dir}/share/${PORT}")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/README.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-# Set the version of the library
-set(VCPKG_TARGET_TRIPLET ${VCPKG_TARGET_TRIPLET} PARENT_SCOPE)
-set(YOUR_LIBRARY_VERSION 1.0.0 PARENT_SCOPE)
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/nanojson" RENAME copyright)
