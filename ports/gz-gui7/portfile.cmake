845aa1da03ffe07f47062994ed1ad30dabf569f6
Tal Regev <tal.regev@gmail.com>
2023-11-04 09:13:04 +0200

    Add port gui7

--- ports/gz-gui7/portfile.cmake
+++ ports/gz-gui7/portfile.cmake
@@ -1,25 +1,28 @@
-set(PACKAGE_NAME physics)
+set(PACKAGE_NAME gui)
 
 ignition_modular_library(
    NAME ${PACKAGE_NAME}
    REF ${PORT}_${VERSION}
    VERSION ${VERSION}
-   SHA512 c29594663509234e25c7d0a33848c0fe222c2b9471513978c18ea6873a17c66c43b4037c74e8849995fa6449c2dddc0f2ee669605893daf65119c277a17f39e1
+   SHA512 29f37a31bbf90dd35f37e80053c1aff9fb404b7a09c8c10e640da505cc6261387e6ce77e3bf379a911e6131c684f866cf1ef8d83777112b3c7f148b1f95cc72f
    OPTIONS 
    PATCHES
       dependencies.patch
 )
 
 if(VCPKG_TARGET_IS_WINDOWS)   
-   file(GLOB plugins "${CURRENT_PACKAGES_DIR}/lib/gz-physics-6/engine-plugins/*.dll")
+   file(GLOB plugins "${CURRENT_PACKAGES_DIR}/lib/gz-gui-7/plugins/*.dll")
    if (NOT plugins STREQUAL "")
       file(COPY ${plugins} DESTINATION "${CURRENT_PACKAGES_DIR}/engine-plugins/")
       file(REMOVE ${plugins})
    endif()
 
-   file(GLOB plugins_debug "${CURRENT_PACKAGES_DIR}/debug/lib/gz-physics-6/engine-plugins/*.dll")
+   file(GLOB plugins_debug "${CURRENT_PACKAGES_DIR}/debug/lib/gz-gui-7/plugins/*.dll")
    if (NOT plugins_debug STREQUAL "")
       file(COPY ${plugins_debug} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/engine-plugins/")
       file(REMOVE ${plugins_debug})
    endif()
+
+    # Lacking pc files for Qt
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
 endif()
