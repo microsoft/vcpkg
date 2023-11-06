97fff3d9ff0edee7a48f7b3e4ade79bea288e859
Rémy Tassoux <contact@rt2.fr>
2023-11-06 18:38:28 +0100

    [quill] Update to 3.4.0 (#34921)
    * [quill] Update to 3.4.0
    
    * [quill] Update version files
    
    * [quill] Fix version tag
    
    * [quill] Update version files
    
    * [quill] Enable android build
    
    * [quill] Update version files
    
    * [quill] Disable thread name support on android build
    
    * [quill] Update version files

--- ports/quill/portfile.cmake
+++ ports/quill/portfile.cmake
@@ -4,14 +4,19 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO odygrd/quill
     REF v${VERSION}
-    SHA512 b59316c4c2f0544ce88059440950f1457522eebe2efe2eed1ad1b3000e5016dd641a3157fbd2491ba7d558dc281d52f6f6e30892fa87e7a2716c86e1e17387d2
+    SHA512 bd42bdb9a377a02aa9d807f26c38d560ae75faca76314cf9c18baeca354e09a5ac4e81ab854ffd7f4715828b39a7e6aef3c67d0d425f983e38b80254b0eeeede
     HEAD_REF master
 )
+
+if(VCPKG_TARGET_IS_ANDROID)
+    set(ADDITIONAL_OPTIONS -DQUILL_NO_THREAD_NAME_SUPPORT=ON)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DQUILL_FMT_EXTERNAL=ON
+        ${ADDITIONAL_OPTIONS}
 )
 
 vcpkg_cmake_install()
@@ -28,6 +33,9 @@ endif()
 vcpkg_fixup_pkgconfig()
 
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/quill/TweakMe.h" "// #define QUILL_FMT_EXTERNAL" "#define QUILL_FMT_EXTERNAL")
+if(VCPKG_TARGET_IS_ANDROID)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/quill/TweakMe.h" "// #define QUILL_NO_THREAD_NAME_SUPPORT" "#define QUILL_NO_THREAD_NAME_SUPPORT")
+endif()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
