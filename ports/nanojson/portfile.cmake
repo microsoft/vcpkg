5f221fe26f1b13f1f2065752f71ad54bb74a966b
Saad <saadmuizz@gmail.com>
2023-11-07 02:22:51 -0500

    deleted license

--- ports/nanojson/portfile.cmake
+++ ports/nanojson/portfile.cmake
@@ -10,4 +10,4 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
 vcpkg_cmake_install()
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
