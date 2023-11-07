1c63399d4f187759ed1a65dce437b491c2517967
Saad <saadmuizz@gmail.com>
2023-11-06 20:59:18 -0500

    update sha512
    Co-authored-by: Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>

--- ports/nanojson/portfile.cmake
+++ ports/nanojson/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH source
         REPO saadshams/nanojson
         REF 1.0.0
-        SHA512 0
+        SHA512 d7ac163455b585ac00fafa3cc6c7090ec39e43b45e95f0d7006e56dcedc7bde05070ad339df48b25ff2e3a545fab96ca80eb5f668fc87557add70c3578bb74ba
         HEAD_REF master
 )
 
