b817b54b9dab729b721545122a60a904f7a95a27
Saad <saadmuizz@gmail.com>
2023-11-09 12:23:20 -0500

    update hash

--- ports/nanojson/portfile.cmake
+++ ports/nanojson/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_github(
         OUT_SOURCE_PATH source
         REPO saadshams/nanojson
         REF 1.0.0
-        SHA512 d7ac163455b585ac00fafa3cc6c7090ec39e43b45e95f0d7006e56dcedc7bde05070ad339df48b25ff2e3a545fab96ca80eb5f668fc87557add70c3578bb74ba
-        HEAD_REF master
+        SHA512 2b6c415c5523acb1aea08a3106a0199e4451b5b9837837886698c672246de586701b3076b4686c82b8d72b331f98303a7fb072b029e6a7b899533cea42e6dd96
+        HEAD_REF main
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${source})
