125d90f2e0434dfc36a1bd5a33bf9442ec8a3ef3
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-11-13 14:46:14 +0800

    [aws-sdk-cpp] update to 1.11.200

--- ports/aws-sdk-cpp/portfile.cmake
+++ ports/aws-sdk-cpp/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO aws/aws-sdk-cpp
     REF "${VERSION}"
-    SHA512 63de900870e9bec23d42e9458e0e9b1579a9e2dc7b0f404eae1b0dd406898b6d6841c5e2f498710b3828f212705437da3a2fe94813a6c3a842945100a05ae368
+    SHA512 c752988f4f22ce78ffc11c953b468c28514ecad0e306fa651d957cba39fb9c27ec5f5eb951cdfd32f70ec442e08db6917eb17bb5c13ed4b883511ad4c32bfc31
     PATCHES
         patch-relocatable-rpath.patch
         fix-aws-root.patch
