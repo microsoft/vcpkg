96977f57f1f876a8ee553c05f0a5a8dec3ed1fe3
Nick D'Ademo <nickdademo@gmail.com>
2023-11-14 18:54:50 +0800

    add header install.

--- ports/obfuscate/portfile.cmake
+++ ports/obfuscate/portfile.cmake
@@ -6,4 +6,6 @@ vcpkg_from_github(
   HEAD_REF master
 )
 
+file(INSTALL ${SOURCE_PATH}/obfuscate.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
