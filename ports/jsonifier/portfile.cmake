dadb003a32ee352c2c07b2698716dad6d06c6fe5
Cheney-Wang <850426846@qq.com>
2023-11-13 19:07:20 -0800

    [jsonifier] Remove duplicate header files

--- ports/jsonifier/portfile.cmake
+++ ports/jsonifier/portfile.cmake
@@ -4,14 +4,16 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 e2611b116cd6d73426b32fea11b3b52527466e7e59d8e8c842ccca9dac2b42679457d7ad77e11512c15fb319e650c74bbee0dce3ae7d24c127b756790eebf020
     HEAD_REF main
+    PATCHES
+        uninstall-head.patch
 )
+
+set(VCPKG_BUILD_TYPE release) # header-only
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 
 vcpkg_cmake_install()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.md")
