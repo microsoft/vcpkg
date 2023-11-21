33783b1a24f1ab09ce0a72df1071f39e6254916d
Billy Robert O'Neal III <bion@microsoft.com>
2023-11-21 14:29:41 -0800

    [baresip-libre] Update to 3.6.2 and workaround missing <threads.h>
    Included is a workaround for https://developercommunity.visualstudio.com/t/Missing-threadsh-in-MSVC-178/10514752 implemented as upstream's commit ebdf9d724cfd0a04f194ecfcb678b702ad062be2

--- ports/baresip-libre/portfile.cmake
+++ ports/baresip-libre/portfile.cmake
@@ -2,14 +2,22 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
+# Workaround https://developercommunity.visualstudio.com/t/Missing-threadsh-in-MSVC-178/10514752
+vcpkg_download_distfile(HAVE_THREADS_PATCH
+    URLS https://github.com/baresip/re/commit/ebdf9d724cfd0a04f194ecfcb678b702ad062be2.patch?full_desc=true
+    FILENAME baresip-libre-ebdf9d724cfd0a04f194ecfcb678b702ad062be2.patch
+    SHA512 2ed0361cef8d599c3369ba9d2b077781484666be2f5fe172301e727d1653060a8980dfe3e4d340d0f09c92470d677200f963442eb90f9efc429c15ccf75ac353
+)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO baresip/re
     REF "v${VERSION}"
-    SHA512 054227c3cbd41d8801d1d0aed38029adb63d52e33e8becafbdba3e973d55863e40e9be0463b6a2d91b34b3b2aea7d80e9d7ec7adadd5e63ca844e416f4d6c411
+    SHA512 59988a665db0682bb8a4dd8dc720f0ed80f49f5148aa02f175dfa01b051b9637db89b6f7343bc0544eba9adb7f0d69e0755b99ecaf4cf7eb303d90d1625c9e11
     HEAD_REF main
     PATCHES
         fix-static-library-build.patch
+        "${HAVE_THREADS_PATCH}"
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" LIBRE_BUILD_SHARED)
