c50a687dbe03927346a203d4d7a82eb2b895a87d
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-11-21 22:00:46 -0600

    Bump

--- ports/cpptrace/portfile.cmake
+++ ports/cpptrace/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jeremy-rifkin/cpptrace
     REF "v${VERSION}"
-    SHA512 fb1019da8f2de85f6fa7a8b08e16f16c755d7e94371bb23a7851b747db033913f2b42faf3afa6d0b1d338847746117cd8dd2dfe6d9fae7d9288176dfc1a8d720
+    SHA512 9f551f63b55d437df58e6fae1e63d80194342611d87445f63ad0db7629445e2a5b20a115f4fea40ecf9f4336f9c969475e092a6c7a2cc39f5a75e8788b379a66
     HEAD_REF main
     # PATCHES
     #   libdwarf_fixes.patch
