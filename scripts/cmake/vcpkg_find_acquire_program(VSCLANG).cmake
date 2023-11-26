1ac17d9335c389b047dfa1ac7ece3df96297eea5
Kai Pastor <dg0yt@darc.de>
2023-11-26 17:43:37 +0100

    Add vcpkg_find_acquire_program(VSCLANG)

---
+++ scripts/cmake/vcpkg_find_acquire_program(VSCLANG).cmake
@@ -0,0 +1,19 @@
+if(NOT CMAKE_HOST_WIN32)
+    message(FATAL_ERROR "Visual Studio is only supported on Windows hosts.")
+endif()
+
+set(program_name "clang")
+set(paths_to_search
+    # LLVM in Visual Studio
+    "$ENV{VCINSTALLDIR}/Tools/Llvm/x64/bin"
+    "$ENV{VCINSTALLDIR}/Tools/Llvm/bin"
+)
+find_program(VSCLANG
+    NAMES clang
+    PATHS ${paths_to_search}
+    NO_DEFAULT_PATH
+)
+if(NOT VSCLANG)
+    list(JOIN paths_to_search "\n   " paths)
+    message(FATAL_ERROR "Failed to find Visual Studio's clang in:\n   ${paths}\nOpen the Visual Studio Installer and choose \"C++ Clang tools for Windows\" under \"Desktop development with C++\" optional components.")
+endif()
