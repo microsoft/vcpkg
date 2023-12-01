bbf80bcf104d73bda987790d719f72d68424465a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2023-12-01 04:06:12 -0800

    [juce] use find_path in addition for freetype

--- ports/juce/portfile.cmake
+++ ports/juce/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
   HEAD_REF master
   PATCHES
   "0001-build-allow-setting-JUCE_PLUGINHOST_LADSPA.patch"
-  "0002-build-link-Freetype-for-juce_graphics.patch"
+  "0002-build-fix-the-freetype-path-for-juce_graphics.patch"
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
