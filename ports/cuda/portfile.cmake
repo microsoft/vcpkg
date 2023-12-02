24daba228681897374b3a9a39213d0aaa17259c5
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-02 23:53:58 +0100

    add a wrapper for cuda

--- ports/cuda/portfile.cmake
+++ ports/cuda/portfile.cmake
@@ -257,7 +257,9 @@ file(REMOVE_RECURSE
 vcpkg_install_copyright(FILE_LIST ${licenses})
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg_find_cuda.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}//vcpkg_find_cuda.cmake" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/CUDAToolkit/vcpkg-cmake-wrapper.cmake" @ONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg_find_cuda.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg_find_cuda.cmake" @ONLY)
 
 vcpkg_fixup_pkgconfig()
 
