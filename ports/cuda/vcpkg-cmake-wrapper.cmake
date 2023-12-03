cfe778d24ebf2f9a6e8c47d332e9ac05c4a6bb64
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-03 14:54:53 +0100

    Fix paths

--- ports/cuda/vcpkg-cmake-wrapper.cmake
+++ ports/cuda/vcpkg-cmake-wrapper.cmake
@@ -1,3 +1,3 @@
-set(CUDA_TOOLKIT_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../@TARGET_TRIPLET@/tools/cuda")
-set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/../../@TARGET_TRIPLET@/tools/cuda")
+set(CUDA_TOOLKIT_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../../tools/cuda")
+set(CUDAToolkit_ROOT "${CMAKE_CURRENT_LIST_DIR}/../../tools/cuda")
 _find_package(${ARGS})
--- ports/cuda/vcpkg-port-config.cmake
+++ ports/cuda/vcpkg-port-config.cmake
@@ -1,4 +1,4 @@
 include("${CMAKE_CURRENT_LIST_DIR}/vcpkg_find_cuda.cmake")
 
 set(ENV{CUDA_PATH} "${CURRENT_INSTALLED_DIR}")
-vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/cuda/bin")
+vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/tools/cuda/bin")
