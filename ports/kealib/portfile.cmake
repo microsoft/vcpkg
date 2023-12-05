631ba72c109d67c57b9f46fc4c5e5317e7f4fc9e
Kai Pastor <dg0yt@darc.de>
2023-12-05 04:44:53 +0100

    [kealib] Cleanup, fix CMake config (#35461)

--- ports/kealib/portfile.cmake
+++ ports/kealib/portfile.cmake
@@ -4,7 +4,9 @@ vcpkg_from_github(
     REF "kealib-${VERSION}"
     SHA512 82399f1332ff2aeb6342732e9e5c897c813109fd18e77cfc8d866f06adf4faa7f080f1f3c0a3b777fb3a679912dacf4851b7ad09a338d6087dd1d26eb2d1689f
     HEAD_REF master
-    PATCHES hdf5_include.patch
+    PATCHES
+        kealib-target.diff
+        no-kea-config-script.diff
 )
 
 vcpkg_cmake_configure(
@@ -16,13 +18,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
-endif()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libkea)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libkea PACKAGE_NAME libkea)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
