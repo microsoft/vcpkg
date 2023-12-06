eaa5edd793b335fa63ecf55d996b6ec3d07e0b4c
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-12-06 08:30:02 +0100

    fix env lookup and wrapper

--- ports/cuda/vcpkg-port-config.cmake
+++ ports/cuda/vcpkg-port-config.cmake
@@ -1,4 +1,4 @@
 include("${CMAKE_CURRENT_LIST_DIR}/vcpkg_find_cuda.cmake")
 
-set(ENV{CUDA_PATH} "${CURRENT_INSTALLED_DIR}")
+set(ENV{CUDA_PATH} "${CURRENT_INSTALLED_DIR}/tools/cuda")
 vcpkg_add_to_path(PREPEND "${CURRENT_INSTALLED_DIR}/tools/cuda/bin")
