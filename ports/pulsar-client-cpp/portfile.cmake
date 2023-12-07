d2f40357ba6eeae8c9635f94b213c642125fbc65
Yunze Xu <xyzinfernity@163.com>
2023-12-07 16:05:54 +0800

    [pulsar-client-cpp] New port

---
+++ ports/pulsar-client-cpp/portfile.cmake
@@ -0,0 +1,35 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO apache/pulsar-client-cpp
+    REF "v${VERSION}"
+    SHA512 ab257f5e82d3815a232dd73297c6ff032536de3d9e5adec6c53fa0276fc02efb1a84e153278f21881de1d3a786e26c4d4d2aff78c1d3fbf932f4d5b6e8cae9dc
+    HEAD_REF main
+    PATCHES
+      0001-use-find-package.patch
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" PULSAR_BUILD_STATIC)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_PERF_TOOLS=OFF
+        -DBUILD_STATIC_LIB=${PULSAR_BUILD_STATIC}
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+
+file(COPY "${CURRENT_PORT_DIR}/unofficial-pulsar-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-pulsar/")
+
+vcpkg_copy_pdbs()
---
+++ ports/pulsar-client-cpp/unofficial-pulsar-config.cmake
@@ -0,0 +1,38 @@
+if (NOT TARGET unofficial::pulsar::pulsar)
+    get_filename_component(VCPKG_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
+    get_filename_component(VCPKG_IMPORT_PREFIX "${VCPKG_IMPORT_PREFIX}" PATH)
+    get_filename_component(VCPKG_IMPORT_PREFIX "${VCPKG_IMPORT_PREFIX}" PATH)
+
+    find_path(_pulsar_include_dir NAMES "pulsar/Client.h" PATH ${VCPKG_IMPORT_PREFIX})
+    if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+        set(VCPKG_IMPORT_PREFIX "${VCPKG_IMPORT_PREFIX}/debug")
+    endif ()
+    if ("@VCPKG_LIBRARY_LINKAGE" STREQUAL "dynamic" OR PULSAR_FORCE_DYNAMIC_LIBRARY)
+        find_library(_pulsar_library NAMES libpulsar.so libpulsar.dylib pulsar.lib PATH ${VCPKG_IMPORT_PREFIX})
+    else ()
+        find_library(_pulsar_library NAMES libpulsar.a pulsar-static.lib PATH ${VCPKG_IMPORT_PREFIX})
+    endif ()
+    if (NOT _pulsar_include_dir OR NOT _pulsar_library)
+        message(FATAL_ERROR "Broken installation of vcpkg port pulsar-client-cpp")
+    endif ()
+
+    # Find 3rd party dependencies
+    find_package(protobuf CONFIG REQUIRED)
+    find_package(CURL CONFIG REQUIRED)
+    find_package(zstd CONFIG REQUIRED)
+    find_package(snappy CONFIG REQUIRED)
+
+    add_library(unofficial::pulsar::pulsar INTERFACE IMPORTED)
+    set_target_properties(unofficial::pulsar::pulsar PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${_pulsar_include_dir}"
+        IMPORTED_LOCATION "${_pulsar_library}")
+    target_link_libraries(unofficial::pulsar::pulsar INTERFACE
+        ${_pulsar_library}
+        protobuf::libprotobuf
+        CURL::libcurl
+        $<IF:$<TARGET_EXISTS:zstd::libzstd_shared>,zstd::libzstd_shared,zstd::libzstd_static>
+        Snappy::snappy
+        )
+    unset(_pulsar_library)
+endif ()
+unset(_pulsar_include_dir)
