dfe5ee7a69f85213aea768769dbd4a3785936f5f
Kai Pastor <dg0yt@darc.de>
2023-12-07 08:04:38 +0100

    WIP [skip actions]

--- ports/libunistring/portfile.cmake
+++ ports/libunistring/portfile.cmake
@@ -19,12 +19,17 @@ vcpkg_extract_source_archive(SOURCE_PATH
         parallelize-symbol-collection.patch
 )
 
+vcpkg_find_acquire_program(PKGCONFIG)
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
     USE_WRAPPERS
     OPTIONS
         "--with-libiconv-prefix=${CURRENT_INSTALLED_DIR}"
+    ADDITIONAL_MSYS_PACKAGES
+        DIRECT_PACKAGES
+            "https://mirror.msys2.org/msys/x86_64/msys2-runtime-3.4.10-2-x86_64.pkg.tar.zst"
+            da1fd58e00fb43b17f7191262136fa49829003a8a8bd0d0eee857bdc24c02668363b379a3cdcb098b74872059acd5c3852d95436855e86e094c2e7c26e476716
 )
 
 vcpkg_install_make()
@@ -40,3 +45,5 @@ The libunistring library and its header files are dual-licensed under
         "${SOURCE_PATH}/COPYING.LIB"
         "${SOURCE_PATH}/COPYING"
 )
+
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/share") # fail post-build check
