5add5f2a0ac2b23f48fc28954365f1fc6b065a42
Schaich, Alonso <alonsoschaich@fastmail.fm>
2023-12-09 07:35:03 +0900

    Revert "[vcpkg-tool-meson] Use adverticed version of meson (microsoft/vcpkg#35002)"
    Meson 0.63 can't build gobject-introspection on a tier 1 supported target.
    
    Upgrading meson to 0.63 and handling regressions of that update is out of
    scope of #35002 anyways, and should be handled either on it's own PR,
    microsoft/vcpkg#28084 or microsoft/vcpkg#35348 .

--- ports/vcpkg-tool-meson/portfile.cmake
+++ ports/vcpkg-tool-meson/portfile.cmake
@@ -31,8 +31,8 @@ vcpkg_find_acquire_program(PYTHON3)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mesonbuild/meson
-    REF "${VERSION}"
-    SHA512 95a3146c88719c6c02b07c7b5f0930057140f9d6f3b3eddbc32f47080e7ddcce5f2903f607616eb8b42dad86b2c33e906ced7352ad31995fc644dbe69f81b90e
+    REF bb91cea0d66d8d036063dedec1f194d663399cdf
+    SHA512 e5888eb35dd4ab5fc0a16143cfbb5a7849f6d705e211a80baf0a8b753e2cf877a4587860a79cad129ec5f3474c12a73558ffe66439b1633d80b8044eceaff2da
     PATCHES
         meson-intl.patch
         remove-freebsd-pcfile-specialization.patch
