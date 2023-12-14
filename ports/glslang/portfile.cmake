0adb308eb3961038f734b7a360ca60ee173f01f4
Kai Pastor <dg0yt@darc.de>
2023-12-14 17:19:34 +0100

    [glslang] Update path replacement

--- ports/glslang/portfile.cmake
+++ ports/glslang/portfile.cmake
@@ -42,8 +42,8 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glslang DO_NOT_DELETE_PARENT_CONFIG_PATH)
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/glslang-config.cmake"
-    "${PACKAGE_PREFIX_DIR}/lib/cmake/glslang/glslang-targets.cmake"
-    "${PACKAGE_PREFIX_DIR}/share/${PORT}/glslang-targets.cmake"
+    [[${PACKAGE_PREFIX_DIR}/lib/cmake/glslang/glslang-targets.cmake]]
+    [[${CMAKE_CURRENT_LIST_DIR}/glslang-targets.cmake]]
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
