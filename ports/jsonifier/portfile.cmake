4ec4584501ef975d968783ff7574369a554f69be
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2023-12-14 02:36:20 -0500

    [Jsonifier] to v0.9.93 and [DiscordCoreAPI] to v2.0.4 (#35625)
    Updating the necessary files.

--- ports/jsonifier/portfile.cmake
+++ ports/jsonifier/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO realtimechris/jsonifier
     REF "v${VERSION}"    
-    SHA512 436dc66699ef924ca953e4d05b035ff7279fbbae932e74902eed0c6b9fd3e2bf282425c82f0d7d65392307602b5c603cf956d513df4c080c76566d4d9c61b373
+    SHA512 da9a67e6987970462c22ac2174231c4c2d5531bd0ebd329cdaf8147f8777e03fb9a7c9ac2b23297dbe51fdf894720ac101ca11277f8a610a089fe9d06d0dbd38
     HEAD_REF main
     PATCHES
         uninstall-head.patch
