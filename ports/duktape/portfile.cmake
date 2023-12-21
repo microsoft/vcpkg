003e8639294937a828fc9994a437cbc4ed2a30ae
xiaozhuai <798047000@qq.com>
2023-12-21 17:14:49 +0800

    [duktape] Optimize build script and provide duktape target

--- ports/duktape/portfile.cmake
+++ ports/duktape/portfile.cmake
@@ -11,19 +11,17 @@ vcpkg_extract_source_archive(
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/duktapeConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/duktape-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
+set(DUK_CONFIG_H_PATH "${SOURCE_PATH}/src/duk_config.h")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
-  set(DUK_CONFIG_H_PATH "${SOURCE_PATH}/src/duk_config.h")
-  file(READ "${DUK_CONFIG_H_PATH}" CONTENT)
-  string(REPLACE "#undef DUK_F_DLL_BUILD" "#define DUK_F_DLL_BUILD" CONTENT "${CONTENT}")
-  file(WRITE "${DUK_CONFIG_H_PATH}" "${CONTENT}")
+    file(READ "${DUK_CONFIG_H_PATH}" CONTENT)
+    string(REPLACE "#undef DUK_F_DLL_BUILD" "#define DUK_F_DLL_BUILD" CONTENT "${CONTENT}")
+    file(WRITE "${DUK_CONFIG_H_PATH}" "${CONTENT}")
 else()
-  set(DUK_CONFIG_H_PATH "${SOURCE_PATH}/src/duk_config.h")
-  file(READ "${DUK_CONFIG_H_PATH}" CONTENT)
-  string(REPLACE "#define DUK_F_DLL_BUILD" "#undef DUK_F_DLL_BUILD" CONTENT "${CONTENT}")
-  file(WRITE "${DUK_CONFIG_H_PATH}" "${CONTENT}")
+    file(READ "${DUK_CONFIG_H_PATH}" CONTENT)
+    string(REPLACE "#define DUK_F_DLL_BUILD" "#undef DUK_F_DLL_BUILD" CONTENT "${CONTENT}")
+    file(WRITE "${DUK_CONFIG_H_PATH}" "${CONTENT}")
 endif()
 
 vcpkg_cmake_configure(
@@ -31,11 +29,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_copy_pdbs()
+vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_cmake_config_fixup()
-vcpkg_copy_pdbs()
-
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
