517bede56d57c1bccf4c7ac5808a70d17af71795
vzhli17 <v-zhli17@microsoft.com>
2023-12-20 23:43:26 -0800

    Replace the old function

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -51,4 +51,4 @@ endif()
 configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in"  "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYRIGHT")
+vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/COPYRIGHT")
