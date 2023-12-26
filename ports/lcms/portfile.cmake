b9f62e9dc7f7c7b2b3bbb53a05a490599b5d304e
LilyWangLL <494550702@qq.com>
2023-12-25 19:07:08 -0800

    Fix build error

--- ports/lcms/portfile.cmake
+++ ports/lcms/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "lcms${VERSION}"
     SHA512 c0d857123a0168cb76b5944a20c9e3de1cbe74e2b509fb72a54f74543e9c173474f09d50c495b0a0a295a3c2b47c5fa54a330d057e1a59b5a7e36d3f5a7f81b2
     HEAD_REF master
+    PATCHES
+        fix-builderror.patch # Upstream commit: https://github.com/mm2/Little-CMS/commit/f7b3c637c20508655f8b49935a4b556d52937b69
 )
 
 vcpkg_configure_meson(
