2e632686d0f821d8fdd420fefc6907db09e5230d
Jim-Wang <wangzhijie05@beyondsoft.com>
2023-12-27 10:01:27 +0800

    pkgconf

--- ports/libofx/portfile.cmake
+++ ports/libofx/portfile.cmake
@@ -17,22 +17,14 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 
 vcpkg_find_acquire_program(PKGCONFIG)
 
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-    set(path_suffix "/debug")
-endif()
-if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-    set(path_suffix "")
-endif()
-vcpkg_backup_env_variables(VARS PKG_CONFIG_PATH)
-vcpkg_host_path_list(PREPEND ENV{PKG_CONFIG_PATH} "${CURRENT_INSTALLED_DIR}${path_suffix}/lib/pkgconfig")
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DENABLE_OFXCONNECT=OFF # depends on libxml++ ABI 2.6, while vcpkg ships ABI 4.0. See https://libxmlplusplus.github.io/libxmlplusplus/#abi-versions
         ${FEATURE_OPTIONS}
         "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+        -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON
 )
-vcpkg_restore_env_variables(VARS PKG_CONFIG_PATH)
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
