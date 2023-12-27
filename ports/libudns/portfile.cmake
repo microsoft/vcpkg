4c6f51cf676b505d41a38263ef0d5f70965ebe39
Kai Pastor <dg0yt@darc.de>
2023-12-27 07:07:37 +0100

    [libudns] Fix cross builds

--- ports/libudns/portfile.cmake
+++ ports/libudns/portfile.cmake
@@ -1,10 +1,13 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # port needs work
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ortclib/udns
     REF udns_0_4
     SHA512 4df8def718c75872536d42a757237d6c8e0afce8a53aedd7fea73814dc5cf8b5d6c9ae8f01a8cfc76864aa8293c172f08953a6750a66749ba19a3721bb4cf2ec
     HEAD_REF master
-    PATCHES ignore_unknown_options.patch
+    PATCHES
+        configure.patch
 )
 
 vcpkg_configure_make(
@@ -12,7 +15,7 @@ vcpkg_configure_make(
     COPY_SOURCE
 )
 
-vcpkg_build_make()
+vcpkg_build_make(BUILD_TARGET staticlib)
 vcpkg_fixup_pkgconfig()
 # Install
 if(NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL debug)
