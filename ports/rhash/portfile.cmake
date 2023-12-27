061d49f25c1cc416d07eb1af39574867c102f00b
Kai Pastor <dg0yt@darc.de>
2023-12-27 09:10:49 +0100

    [rhash] Ignore incompatible cast

--- ports/rhash/portfile.cmake
+++ ports/rhash/portfile.cmake
@@ -1,16 +1,19 @@
-set(RHASH_XVERSION 1.4.2)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rhash/RHash
     REF "v${VERSION}"
     SHA512 00a7e5e058b53ce20ae79509815452ed9cb699d1322b678220b72c61dea3ea2f8fa131acfade8bb6d9f6af913f0c3c472330841181b22314b8755166310c946f
     HEAD_REF master
+    PATCHES
+        cast-version.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}/librhash")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/librhash"
+    OPTIONS
+        "-DRHASH_XVERSION=${VERSION}"
     OPTIONS_DEBUG
         -DRHASH_SKIP_HEADERS=ON
 )
