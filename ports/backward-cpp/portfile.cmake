121a72443f76f239f9ee98d180efe25368029d05
endingly <jiasongdeng@qq.com>
2023-12-28 09:35:55 +0800

    add usage

--- ports/backward-cpp/portfile.cmake
+++ ports/backward-cpp/portfile.cmake
@@ -15,3 +15,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/backward)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
