2fd98d585cbd30063735e042356eab41f6955577
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-01-02 00:47:36 +0100

    restore extraction

--- ports/libcroco/portfile.cmake
+++ ports/libcroco/portfile.cmake
@@ -4,6 +4,11 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 038a3ac9d160a8cf86a8a88c34367e154ef26ede289c93349332b7bc449a5199b51ea3611cebf3a2416ae23b9e45ecf8f9c6b24ea6d16a5519b796d3c7e272d4
 )
 
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+)
+
 set(OPTIONS "")
 if(VCPKG_TARGET_IS_OSX)
     list(APPEND OPTIONS "--disable-Bsymbolic")
