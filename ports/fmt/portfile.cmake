a344996f1d9c80a73c5d9b9b0426886d26b29da5
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-01-02 17:39:37 -0500

    [fmt] Update to 10.2.0 (#35971)

--- ports/fmt/portfile.cmake
+++ ports/fmt/portfile.cmake
@@ -2,11 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO fmtlib/fmt
     REF "${VERSION}"
-    SHA512 288c349baac5f96f527d5b1bed0fa5f031aa509b4526560c684281388e91909a280c3262a2474d963b5d1bf7064b1c9930c6677fe54a0d8f86982d063296a54c
+    SHA512 b90f8ab1692fcae9146f8cad5c5c26a2b5ceb6a0460003e01cabe8a75c0aa2fea1c3760dc3214eddaf08984a1695747ea8b1f3124c40c54cbadfd45458fa4b2d
     HEAD_REF master
     PATCHES
         fix-write-batch.patch
-        fix-format-conflict.patch
 )
 
 vcpkg_cmake_configure(
@@ -35,4 +34,4 @@ file(REMOVE_RECURSE
 )
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.rst")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
