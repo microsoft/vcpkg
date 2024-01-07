ca6b42fcd454e9ac9504c34e38dd4c91587a27c9
Carsten Grimm <carsten.grimm@ipolog.ai>
2024-01-07 17:01:23 +0100

    [simonbrunel-qtpromise] add patch for installing header files

--- ports/simonbrunel-qtpromise/portfile.cmake
+++ ports/simonbrunel-qtpromise/portfile.cmake
@@ -4,11 +4,17 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 0d6316ec9503a7781b4d9e615e6d538b21b6282a76e5e28e3f323bcdb740e6f66e6c55944e31fc62cec7cc25a90b0f7318277f044a630500202971ca6e2e85b6
     HEAD_REF master
+    PATCHES
+        patches/install_headers.patch
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
-file(COPY "${SOURCE_PATH}/include" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
-file(COPY "${SOURCE_PATH}/src" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}" 
+    OPTIONS 
+        -DQTPROMISE_HEADER_INSTALL_DESTINATION="${CURRENT_PACKAGES_DIR}/include/${PORT}"
+        -DQTPROMISE_HEADER_INSTALL_COMPONENTS="Release"
+)
+vcpkg_cmake_install()
 
 set(USE_QT_VERSION "6")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/unofficial-config.cmake.in" "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}/unofficial-${PORT}-config.cmake" @ONLY)
