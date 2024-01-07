0b142c3e8f2bb5876bf7a9a38dec3b32bb963214
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2024-01-07 11:36:53 +0100

    remove debug include

--- ports/intel-ipsec/portfile.cmake
+++ ports/intel-ipsec/portfile.cmake
@@ -71,7 +71,7 @@ else()
 endif()
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/intel-ipsecConfig.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/intel-ipsecConfig.cmake" @ONLY)
-file(REMOVE_RECURSE "${DEBUG_LIB}" "${RELEASE_LIB}" "${CURRENT_PACKAGES_DIR}/man")
+file(REMOVE_RECURSE "${DEBUG_LIB}" "${RELEASE_LIB}" "${CURRENT_PACKAGES_DIR}/man" "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
