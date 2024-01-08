dd7ffcdd9a8814a3c983efa0c5cac06516871523
Nick Logozzo <nlogozzo225@gmail.com>
2024-01-08 13:14:17 -0500

    libaura --> libnick

---
+++ ports/libnick/portfile.cmake
@@ -0,0 +1,31 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NickvisionApps/libnick
+    REF "${VERSION}"
+    SHA512 db06770546c0c57375fdbb409c1a960ca5fb19831548ca585f93b0b1eca04e7f1f6d2d46e48aebb0ed2eeb24b9e50622e4770f66d617b7cbffc5ea9849bd0f16
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libnick)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_pkgconfig()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
