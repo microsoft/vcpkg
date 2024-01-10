d0338a4046ff41de05c6ca53733cc67dfc68267a
tonitaga <onlyforcroyale@gmail.com>
2024-01-11 01:02:38 +0300

    add mlt-config.cmake autogen

--- ports/mtl/portfile.cmake
+++ ports/mtl/portfile.cmake
@@ -1,16 +1,25 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tonitaga/Matrix-Template-Library-CPP
-    REF b04caec651bcbd906d8e35612d26af56b656165a
-    SHA512 8d2d16240a26abcc339d4ecdca717ee2d4f2c936032ad0ef2fee923b6fdf43ee6e774eae9094b18e9c37167fe8e45e8d7632dd19008a154127e8e0d81e30bf9b
+    REF 733c95a93155468994b3bcacab1b8953478eb210
+    SHA512 f654b12e19c0981fe1c15d44ffa068019ec5323b78409a46eab17af09b49ea511903b49fd004ceccf3f0ca016604484943f3ade9ca411b821351364ad227f57f
     HEAD_REF vcpkg
 )
 
-file(COPY ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-file(INSTALL "${SOURCE_PATH}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/cmake/Findmtl.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/share/mtl/mtl-targets.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/share/mtl/mtlConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DMTL_CMAKE_DIR=share/mtl
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "mtl" CONFIG_PATH "lib/cmake/mtl")
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib" "${CURRENT_PACKAGES_DIR}/debug")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 
