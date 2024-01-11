4909b5d1a612a22a936c5f70a8f5ac326c4b0e3a
Saad <saadmuizz@gmail.com>
2024-01-11 18:27:01 -0500

    update

--- ports/nanojsonc/portfile.cmake
+++ ports/nanojsonc/portfile.cmake
@@ -2,9 +2,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
-        REPO saadshams/nanojsonc
+        REPO open-source-patterns/nanojsonc
         REF "${VERSION}"
-        SHA512 22d8e1b97260dfeaaf47f8a4d9bbf8bb74e955f1b22b27a4cd5a3a115fcffceba17993949ac1a0522be0213c39ed34c0f44e58f02536196473981eeeab0c5eac
+        SHA512 65ee05d9df7e44702fce22c458aa53762cfe00d3fdf1ce93a034849c56ddc0dbb4bb86e20c175b5d9a2ce43a97c82c99f042c122abcfe44dfb5d9d88e8b32e39
         HEAD_REF main
 )
 
@@ -12,10 +12,8 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTS=OFF)
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/nanojsoncTargets")
+vcpkg_cmake_config_fixup() # removes /debug/share
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include") # removes debug/include
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE") # Install License
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}") # Install Usage
