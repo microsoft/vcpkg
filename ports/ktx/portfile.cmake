6ad5fd07e994fba068ff704940ad86a46ac130d2
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-01-11 14:11:16 +0800

    fix download method

--- ports/ktx/portfile.cmake
+++ ports/ktx/portfile.cmake
@@ -14,14 +14,6 @@ file(REMOVE "${SOURCE_PATH}/other_include/zstd_errors.h")
 
 vcpkg_list(SET OPTIONS)
 if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_acquire_msys(MSYS_D2U
-        PACKAGES
-            dos2unix
-        DIRECT_PACKAGES
-            # Required for "dos2unix"
-            "https://mirror.msys2.org/msys/x86_64/dos2unix-7.5.1-1-x86_64.pkg.tar.zst"
-            83d85e6ccea746ef9e8153a0d605e774dbe7efc0ee952804acfee4ffd7e3b0386a353b45ff989dd99bc3ce75968209fea3d246ad2af88bbb5c4eca12fc5a8f92
-    )
     vcpkg_acquire_msys(MSYS_ROOT
         PACKAGES
             bash
@@ -29,8 +21,10 @@ if(VCPKG_TARGET_IS_WINDOWS)
             # Required for "getopt"
             "https://repo.msys2.org/msys/x86_64/util-linux-2.35.2-3-x86_64.pkg.tar.zst"
             da26540881cd5734072717133307e5d1a27a60468d3656885507833b80f24088c5382eaa0234b30bdd9e8484a6638b4514623f5327f10b19eed36f12158e8edb
+            # Required for "dos2unix"
+            "https://mirror.msys2.org/msys/x86_64/dos2unix-7.5.1-1-x86_64.pkg.tar.zst"
+            83d85e6ccea746ef9e8153a0d605e774dbe7efc0ee952804acfee4ffd7e3b0386a353b45ff989dd99bc3ce75968209fea3d246ad2af88bbb5c4eca12fc5a8f92
     )
-    file(COPY "${MSYS_D2U}/usr/bin/dos2unix.exe" DESTINATION "${MSYS_ROOT}/usr/bin")
     vcpkg_add_to_path("${MSYS_ROOT}/usr/bin")
     vcpkg_list(APPEND OPTIONS "-DBASH_EXECUTABLE=${MSYS_ROOT}/usr/bin/bash.exe")
 endif()
@@ -54,6 +48,7 @@ vcpkg_cmake_configure(
         # Do not regenerate headers (needs more dependencies)
         -DCMAKE_DISABLE_FIND_PACKAGE_Vulkan=1
     DISABLE_PARALLEL_CONFIGURE
+         CMAKE_DISABLE_FIND_PACKAGE_Vulkan
 )
 
 vcpkg_cmake_install()
