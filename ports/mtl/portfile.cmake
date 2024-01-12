1dd9727a04849a13cf43afa4a2c0bbabbf8e0880
Gubaydullin Nurislam <108585608+tonitaga@users.noreply.github.com>
2024-01-12 08:52:07 +0300

    Update ports/mtl/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/mtl/portfile.cmake
+++ ports/mtl/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME "mtl" CONFIG_PATH "lib/cmake/mtl")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib" "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
