a7cdadefd1932cd9e4692cefc2fbd271f17d47cf
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2024-01-16 11:01:48 +0100

    Update ports/zeromq/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/zeromq/portfile.cmake
+++ ports/zeromq/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO zeromq/libzmq
-    REF v4.3.5
+    REF "v${VERSION}"
     SHA512 108d9c5fa761c111585c30f9c651ed92942dda0ac661155bca52cc7b6dbeb3d27b0dd994abde206eacfc3bc88d19ed24e45b291050c38469e34dca5f8c9a037d
     PATCHES 
         fix-arm.patch
