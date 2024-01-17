a88c37aea541ca8ef409d9192020b10ca5508e04
chausner <chausner@users.noreply.github.com>
2024-01-17 20:28:38 +0100

    Update to version 3.10

--- ports/lzav/portfile.cmake
+++ ports/lzav/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO avaneev/lzav
     REF ${VERSION}
-    SHA512 bcae6a0d1882196d5677248800b296164ffa3fc64d768c0bede43290a11315923e46617508822a8aad9b15eee84c46cf8b38836a8bd86b7cc32dd4d40c020a8a
+    SHA512 09663ab9a9dcdedea0656c7ac1fbb062af2e7ce6b0e6cfb978dde376efaded22a97aadead9083212fdaea60d78021d870767f642de305209ebd0e445b412cef7
     HEAD_REF master
 )
 
-file(COPY "${SOURCE_PATH}/lzav.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include/lzav")
+file(COPY "${SOURCE_PATH}/lzav.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
