4f179d63af1fada14d573bd6bb7637455cc2a3f6
data-queue <144965964+data-queue@users.noreply.github.com>
2024-01-17 20:09:13 -0800

    Update bootstrap-from-source.sh

--- scripts/azure-pipelines/bootstrap-from-source.sh
+++ scripts/azure-pipelines/bootstrap-from-source.sh
@@ -2,7 +2,8 @@
 set -e
 
 git clone https://github.com/microsoft/vcpkg-tool vcpkg-tool
-git -C vcpkg-tool switch -d $1
+git -C vcpkg-tool fetch origin pull/1326/head:1326
+git -C vcpkg-tool switch -d 1326
 rm -rf build.x64.release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DVCPKG_WARNINGS_AS_ERRORS=OFF -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_BUILD_TLS12_DOWNLOADER=OFF -B build.x64.release -S vcpkg-tool
 ninja -C build.x64.release
