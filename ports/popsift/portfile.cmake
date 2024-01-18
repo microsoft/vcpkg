0ee56e9183d90e5b07f20785635726f86ef38a41
Cheney-Wang <850426846@qq.com>
2024-01-17 23:38:35 -0800

    Fix issue in windows toolchain

--- ports/popsift/portfile.cmake
+++ ports/popsift/portfile.cmake
@@ -8,7 +8,6 @@ vcpkg_from_github(
         fix_missing_thrust_include.patch
         144.patch
         cuda_12_1.patch
-        allow-unsupported-compiler.patch
 )
 
 vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT CUDA_TOOLKIT_ROOT)
