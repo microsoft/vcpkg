630872e2688bc221fdbbb163fa7e8e0fa945fc5f
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-01-19 23:11:47 +0100

    xnnpack does not install configs

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -26,7 +26,6 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
