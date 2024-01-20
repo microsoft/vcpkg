08ba5254ede47261f7c074a821508339bfe778c5
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2024-01-20 18:21:43 -0300

    Use vcpkg_configure_make and vcpkg_build_make.

--- ports/firebird/posix/portfile.cmake
+++ ports/firebird/posix/portfile.cmake
@@ -1,11 +1,11 @@
 # Firebird libraries are already relocatable and this causes problems with their runpaths.
 set(VCPKG_FIXUP_ELF_RPATH OFF)
 
-
-# Release build
-
-vcpkg_execute_build_process(
-    COMMAND ./autogen.sh
+vcpkg_configure_make(
+    SOURCE_PATH "${SOURCE_PATH}"
+    COPY_SOURCE
+    AUTOCONFIG
+    OPTIONS
         --enable-client-only
         --enable-binreloc
         --with-builtin-tomcrypt
@@ -15,31 +15,32 @@ vcpkg_execute_build_process(
         --with-plugins=plugins/${PORT}
         --with-fbmsg=share/${PORT}
         --with-tzdata=share/${PORT}/tzdata
-    WORKING_DIRECTORY "${SOURCE_PATH}"
-    LOGNAME configure-${TARGET_TRIPLET}-rel
+    OPTIONS_DEBUG
+        --enable-developer
 )
 
-vcpkg_execute_build_process(
-    COMMAND make -j ${VCPKG_CONCURRENCY}
-    WORKING_DIRECTORY "${SOURCE_PATH}"
-    LOGNAME make-${TARGET_TRIPLET}-rel
-)
+vcpkg_build_make()
+
+
+# Release build
+
+set(SOURCE_COPY_REL_PATH "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/include"
+    INSTALL "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/include"
     DESTINATION "${CURRENT_PACKAGES_DIR}"
 )
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/lib"
+    INSTALL "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/lib"
     DESTINATION "${CURRENT_PACKAGES_DIR}"
     USE_SOURCE_PERMISSIONS
     PATTERN "libtom*" EXCLUDE
 )
 
 file(GLOB EXT_LIBS_RELEASE
-    "${SOURCE_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
-    "${SOURCE_PATH}/extern/libtommath/.libs/libtommath.so*"
+    "${SOURCE_COPY_REL_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
+    "${SOURCE_COPY_REL_PATH}/extern/libtommath/.libs/libtommath.so*"
 )
 file(
     INSTALL ${EXT_LIBS_RELEASE}
@@ -48,7 +49,7 @@ file(
 )
 
 file(GLOB PLUGINS_FILES_RELEASE
-    "${SOURCE_PATH}/gen/Release/firebird/plugins/*"
+    "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/plugins/*"
 )
 file(
     INSTALL ${PLUGINS_FILES_RELEASE}
@@ -57,50 +58,30 @@ file(
 )
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/firebird.msg"
+    INSTALL "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/firebird.msg"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/tzdata"
+    INSTALL "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/tzdata"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 )
 
 
 # Debug build
 
-vcpkg_execute_build_process(
-    COMMAND ./autogen.sh
-        --enable-developer
-        --enable-client-only
-        --enable-binreloc
-        --with-builtin-tomcrypt
-        --with-builtin-tommath
-        --with-termlib=:libncurses.a
-        --with-atomiclib=:libatomic.a
-        --with-plugins=plugins/${PORT}
-        --with-fbmsg=share/${PORT}
-        --with-tzdata=share/${PORT}/tzdata
-    WORKING_DIRECTORY "${SOURCE_PATH}"
-    LOGNAME configure-${TARGET_TRIPLET}-dbg
-)
-
-vcpkg_execute_build_process(
-    COMMAND make -j ${VCPKG_CONCURRENCY}
-    WORKING_DIRECTORY "${SOURCE_PATH}"
-    LOGNAME make-${TARGET_TRIPLET}-dbg
-)
+set(SOURCE_COPY_DBG_PATH "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg")
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Debug/firebird/lib"
+    INSTALL "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/lib"
     DESTINATION "${CURRENT_PACKAGES_DIR}/debug"
     USE_SOURCE_PERMISSIONS
     PATTERN "libtom*" EXCLUDE
 )
 
 file(GLOB EXT_LIBS_DEBUG
-    "${SOURCE_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
-    "${SOURCE_PATH}/extern/libtommath/.libs/libtommath.so*"
+    "${SOURCE_COPY_DBG_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
+    "${SOURCE_COPY_DBG_PATH}/extern/libtommath/.libs/libtommath.so*"
 )
 file(
     INSTALL ${EXT_LIBS_DEBUG}
@@ -109,7 +90,7 @@ file(
 )
 
 file(GLOB PLUGINS_FILES_DEBUG
-    "${SOURCE_PATH}/gen/Debug/firebird/plugins/*"
+    "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/plugins/*"
 )
 file(
     INSTALL ${PLUGINS_FILES_DEBUG}
@@ -118,11 +99,11 @@ file(
 )
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/firebird.msg"
+    INSTALL "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/firebird.msg"
     DESTINATION "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}"
 )
 
 file(
-    INSTALL "${SOURCE_PATH}/gen/Release/firebird/tzdata"
+    INSTALL "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/tzdata"
     DESTINATION "${CURRENT_PACKAGES_DIR}/debug/share/${PORT}"
 )
