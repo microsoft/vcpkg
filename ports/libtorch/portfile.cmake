8c0e148f9e575f09afe7afa5a1f547f97148c2e7
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-01-20 01:44:36 +0100

    fix glog symbol on osx

--- ports/libtorch/portfile.cmake
+++ ports/libtorch/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
         fxdiv.patch
         protoc.patch
         fix-sleef.patch
+        fix-glog.patch
 )
 file(REMOVE_RECURSE "${SOURCE_PATH}/caffe2/core/macros.h") # We must use generated header files
 
