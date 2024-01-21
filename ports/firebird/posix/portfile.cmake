cece708ebbde3c462f925461fb73041c6203ac35
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2024-01-21 15:18:21 -0300

    Use vcpkg dependencies and fix plugins rpath.

--- ports/firebird/posix/portfile.cmake
+++ ports/firebird/posix/portfile.cmake
@@ -1,6 +1,8 @@
 # Firebird libraries are already relocatable and this causes problems with their runpaths.
 set(VCPKG_FIXUP_ELF_RPATH OFF)
 
+vcpkg_find_acquire_program(PATCHELF)
+
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     COPY_SOURCE
@@ -8,10 +10,6 @@ vcpkg_configure_make(
     OPTIONS
         --enable-client-only
         --enable-binreloc
-        --with-builtin-tomcrypt
-        --with-builtin-tommath
-        --with-termlib=:libncurses.a
-        --with-atomiclib=:libatomic.a
         --with-plugins=plugins/${PORT}
         --with-fbmsg=share/${PORT}
         --with-tzdata=share/${PORT}/tzdata
@@ -35,22 +33,18 @@ file(
     INSTALL "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/lib"
     DESTINATION "${CURRENT_PACKAGES_DIR}"
     USE_SOURCE_PERMISSIONS
-    PATTERN "libtom*" EXCLUDE
-)
-
-file(GLOB EXT_LIBS_RELEASE
-    "${SOURCE_COPY_REL_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
-    "${SOURCE_COPY_REL_PATH}/extern/libtommath/.libs/libtommath.so*"
-)
-file(
-    INSTALL ${EXT_LIBS_RELEASE}
-    DESTINATION "${CURRENT_PACKAGES_DIR}/lib"
-    USE_SOURCE_PERMISSIONS
 )
 
 file(GLOB PLUGINS_FILES_RELEASE
     "${SOURCE_COPY_REL_PATH}/gen/Release/firebird/plugins/*"
 )
+
+foreach(plugin ${PLUGINS_FILES_RELEASE})
+    execute_process(
+        COMMAND "${PATCHELF}" --set-rpath "$ORIGIN/../../lib" ${plugin}
+    )
+endforeach()
+
 file(
     INSTALL ${PLUGINS_FILES_RELEASE}
     DESTINATION "${CURRENT_PACKAGES_DIR}/plugins/${PORT}"
@@ -76,22 +70,18 @@ file(
     INSTALL "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/lib"
     DESTINATION "${CURRENT_PACKAGES_DIR}/debug"
     USE_SOURCE_PERMISSIONS
-    PATTERN "libtom*" EXCLUDE
-)
-
-file(GLOB EXT_LIBS_DEBUG
-    "${SOURCE_COPY_DBG_PATH}/extern/libtomcrypt/.libs/libtomcrypt.so*"
-    "${SOURCE_COPY_DBG_PATH}/extern/libtommath/.libs/libtommath.so*"
-)
-file(
-    INSTALL ${EXT_LIBS_DEBUG}
-    DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib"
-    USE_SOURCE_PERMISSIONS
 )
 
 file(GLOB PLUGINS_FILES_DEBUG
     "${SOURCE_COPY_DBG_PATH}/gen/Debug/firebird/plugins/*"
 )
+
+foreach(plugin ${PLUGINS_FILES_DEBUG})
+    execute_process(
+        COMMAND "${PATCHELF}" --set-rpath "$ORIGIN/../../lib" ${plugin}
+    )
+endforeach()
+
 file(
     INSTALL ${PLUGINS_FILES_DEBUG}
     DESTINATION "${CURRENT_PACKAGES_DIR}/debug/plugins/${PORT}"
