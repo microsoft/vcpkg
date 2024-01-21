93b6af905134f29cea34a2a91b0d143516d6a8fa
Justen Di Ruscio <justend29@gmail.com>
2024-01-21 10:26:25 -0400

    [speedb] new port

---
+++ ports/speedb/portfile.cmake
@@ -0,0 +1,62 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO speedb-io/speedb
+  REF "speedb/v${VERSION}"
+  SHA512 "ca8f78a8295e4b7d8d361deb256fd8605ca7362c3681585aaab8ada5c7b2ed7e3780fad19a4265c19a3dd85d319d668a35af80f76fbb44c4eb5e7526befd5973"
+  HEAD_REF main
+  PATCHES
+    0001-fix-dependencies.patch
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" WITH_MD_LIBRARY)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ROCKSDB_BUILD_SHARED)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+  FEATURES
+    "liburing" WITH_LIBURING
+    "snappy" WITH_SNAPPY
+    "lz4" WITH_LZ4
+    "zlib" WITH_ZLIB
+    "zstd" WITH_ZSTD
+    "bzip2" WITH_BZ2
+    "numa" WITH_NUMA
+    "tbb" WITH_TBB
+)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    -DWITH_GFLAGS=OFF
+    -DWITH_TESTS=OFF
+    -DWITH_BENCHMARK_TOOLS=OFF
+    -DWITH_TOOLS=OFF
+    -DUSE_RTTI=ON
+    -DROCKSDB_INSTALL_ON_WINDOWS=ON
+    -DFAIL_ON_WARNINGS=OFF
+    -DWITH_MD_LIBRARY=${WITH_MD_LIBRARY}
+    -DPORTABLE=1 # Minimum CPU arch to support, or 0 = current CPU, 1 = baseline CPU
+    -DROCKSDB_BUILD_SHARED=${ROCKSDB_BUILD_SHARED}
+    -DCMAKE_DISABLE_FIND_PACKAGE_Git=TRUE
+    ${FEATURE_OPTIONS}
+  OPTIONS_DEBUG
+    -DCMAKE_DEBUG_POSTFIX=d
+    -DWITH_RUNTIME_DEBUG=ON
+  OPTIONS_RELEASE
+    -DWITH_RUNTIME_DEBUG=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/speedb)
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_fixup_pkgconfig()
+
+vcpkg_install_copyright(
+  FILE_LIST
+    "${SOURCE_PATH}/LICENSE.leveldb"
+    "${SOURCE_PATH}/LICENSE")
