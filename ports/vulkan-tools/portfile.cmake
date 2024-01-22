84b93dbfc28ffcbc4856c21af9b1aa8b1a7279a6
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2024-01-22 15:38:28 +0100

    fix parallel config

--- ports/vulkan-tools/portfile.cmake
+++ ports/vulkan-tools/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "vulkan-sdk-${VERSION}"
     SHA512 5d70498341b8d2447128cd5ab3fd081b706830f5f85d9f7e752dfca9c74fa4c1567207eef6e34e9bf99ff65ec2855b835eb30362fd544e1cf61ecc7a7119df06
     HEAD_REF main
+    PATCHES
+        fix-parallel-config.patch
 )
 
 if(NOT VCPKG_TARGET_IS_ANDROID)
