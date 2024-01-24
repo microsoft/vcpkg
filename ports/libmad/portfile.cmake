e5e9f2f145e539a97b4a56c857892e89a74751e0
Avery King <avery98@pm.me>
2024-01-24 13:31:39 -0800

    [libmad] update to 0.16.4 (#35898)
    * [libmad] update to 0.16.4
    
    Switches libmad to Tenacity's maintained version.
    
    Signed-off-by: Avery King <avery98@pm.me>
    
    * update filename
    
    Co-authored-by: Kai Pastor <dg0yt@darc.de>
    
    * update version db
    
    ---------
    
    Signed-off-by: Avery King <avery98@pm.me>
    Co-authored-by: Javier Matos Denizac <javier.matosd@gmail.com>
    Co-authored-by: Kai Pastor <dg0yt@darc.de>
    Co-authored-by: Javier Matos Denizac <javiermat@microsoft.com>

--- ports/libmad/portfile.cmake
+++ ports/libmad/portfile.cmake
@@ -2,33 +2,26 @@ if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
-vcpkg_from_sourceforge(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO mad/libmad
-    REF 0.15.1b
-    FILENAME "libmad-0.15.1b.tar.gz"
-    SHA512 2cad30347fb310dc605c46bacd9da117f447a5cabedd8fefdb24ab5de641429e5ec5ce8af7aefa6a75a3f545d3adfa255e3fa0a2d50971f76bc0c4fc0400cc45
-    PATCHES 0001-Fix-MSVC-ARM.patch
+vcpkg_download_distfile(
+    ARCHIVE
+    URLS "https://codeberg.org/tenacityteam/libmad/archive/${VERSION}.tar.gz"
+    FILENAME "tenacityteam-libmad-${VERSION}.tar.gz"
+    SHA512 6752c199096f999ed478dea712eb669913eec182eec8a56bf4871e77e6c38798d13146febf646007427f43f86aea8d40b9ec2b72928ac3132066ebddcaa0cfde
 )
 
-#The archive only contains a Visual Studio 6.0 era DSP project file, so use a custom CMakeLists.txt
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/mad.pc.in" DESTINATION "${SOURCE_PATH}")
-
-#Use the msvc++ config.h header
-file(COPY "${SOURCE_PATH}/msvc++/config.h" DESTINATION "${SOURCE_PATH}")
+vcpkg_extract_source_archive(SOURCE_PATH ARCHIVE "${ARCHIVE}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        "-DVERSION=${VERSION}"
+        -DEXAMPLE=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "mad" CONFIG_PATH "lib/cmake/mad")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
