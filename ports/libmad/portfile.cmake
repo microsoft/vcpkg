0bf316396aa3e3e21573194ee4e64fa82698be2d
Monica <v-liumonica@microsoft.com>
2024-01-25 18:41:54 -0800

    fix libmad hash

--- ports/libmad/portfile.cmake
+++ ports/libmad/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_download_distfile(
     ARCHIVE
     URLS "https://codeberg.org/tenacityteam/libmad/archive/${VERSION}.tar.gz"
     FILENAME "tenacityteam-libmad-${VERSION}.tar.gz"
-    SHA512 6752c199096f999ed478dea712eb669913eec182eec8a56bf4871e77e6c38798d13146febf646007427f43f86aea8d40b9ec2b72928ac3132066ebddcaa0cfde
+    SHA512 e56b84e112e3a95cd4fde7be1ce94c4a277275da3cc69ca051113162a84158ac9a1fe9c3f2de186202de5eca5c112aacb87533206288a084cf5ba048e6d95c22
 )
 
 vcpkg_extract_source_archive(SOURCE_PATH ARCHIVE "${ARCHIVE}")
