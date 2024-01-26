e856dfdc91b9684b19649901e5b3d493480c5dc4
Frank <1433351828@qq.com>
2024-01-26 07:48:15 +0000

    [OpenMesh] Use the official usage

--- ports/openmesh/portfile.cmake
+++ ports/openmesh/portfile.cmake
@@ -55,6 +55,4 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
   endif()
 endif()
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/OpenMesh/vcpkg-cmake-wrapper.cmake" @ONLY)
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
