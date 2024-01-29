d7f2f15c8f28bfbb29a040f1ca008fe6ca2f1978
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-01-30 05:10:45 +0800

    --no-edit

--- ports/simdutf/portfile.cmake
+++ ports/simdutf/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_pkgconfig()
 if ("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES sutf AUTO_CLEAN)
 endif()
