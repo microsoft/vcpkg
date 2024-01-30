f68e32abc8a01be23d8816c21d26a59732e4a4eb
Kai Pastor <dg0yt@darc.de>
2024-01-30 09:14:26 +0100

    [allegro5] Fixup

--- ports/allegro5/portfile.cmake
+++ ports/allegro5/portfile.cmake
@@ -9,61 +9,51 @@ vcpkg_from_github(
         msvc-arm64-atomic.patch
 )
 
+if(VCPKG_TARGET_IS_ANDROID AND NOT ENV{ANDROID_HOME})
+    message(FATAL_ERROR "${PORT} requires environment variable ANDROID_HOME to be set." )
+endif()
+
+if(NOT ENV{PKG_CONFIG})
+    vcpkg_find_acquire_program(PKGCONFIG)
+    set(ENV{PKG_CONFIG} "${PKGCONFIG}")
+endif()
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" VCPKG_BUILD_SHARED_LIBS)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        direct3d    WANT_D3D
+        opengl      WANT_OPENGL
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         "-DCMAKE_PROJECT_INCLUDE=${CMAKE_CURRENT_LIST_DIR}/cmake-project-include.cmake"
-        -DWANT_DOCS=OFF
-        -DALLEGRO_SDL=OFF
-        -DWANT_DEMO=OFF
-        -DSHARED=${VCPKG_BUILD_SHARED_LIBS}
         -DINSTALL_PKG_CONFIG_FILES=true
-        -DWANT_EXAMPLES=OFF
-        -DWANT_TESTS=OFF
-        -DWANT_AUDIO=ON
-        -DWANT_COLOR=ON
-        -DWANT_D3D=ON
+        -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON
+        -DSHARED=${VCPKG_BUILD_SHARED_LIBS}
+        ${options}
+        -DALLEGRO_SDL=OFF
         -DWANT_D3D9EX=OFF # Not available on vcpkg
-        -DWANT_DSOUND=ON
-        -DWANT_FLAC=ON
-        -DWANT_FONT=ON
+        -DWANT_DEMO=OFF
+        -DWANT_DOCS=OFF
+        -DWANT_EXAMPLES=OFF
         -DWANT_GLES3=ON
-        -DWANT_IMAGE=ON
-        -DWANT_IMAGE_JPG=ON
-        -DWANT_IMAGE_PNG=ON
-        -DWANT_MEMFILE=ON
+        -DWANT_IMAGE_FREEIMAGE=OFF
         -DWANT_MODAUDIO=OFF # Not available on vcpkg right now
-        -DWANT_NATIVE_DIALOG=ON
-        -DWANT_NATIVE_IMAGE_LOADER=ON
-        -DWANT_OGG_VIDEO=ON
-        -DWANT_OPENAL=ON
-        -DWANT_OPENGL=ON
+        -DWANT_MP3=OFF
         -DWANT_OPENSL=OFF # Not yet available on vcpkg
-        -DWANT_OPUS=ON
-        -DWANT_PHYSFS=ON
         -DWANT_POPUP_EXAMPLES=OFF
-        -DWANT_PRIMITIVES=ON
-        -DWANT_RELEASE_LOGGING=OFF
-        -DWANT_SHADERS_D3D=ON
-        -DWANT_SHADERS_GL=ON
+        -DWANT_TESTS=OFF
         -DWANT_TREMOR=OFF # Not yet available on vcpkg
-        -DWANT_TTF=ON
-        -DWANT_VIDEO=ON
-        -DWANT_VORBIS=ON
-    OPTIONS_RELEASE
-        -DWANT_ALLOW_SSE=ON
-    OPTIONS_DEBUG
-        -DWANT_ALLOW_SSE=OFF
 )
 
 vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME allegro CONFIG_PATH lib/cmake/allegro)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/allegro)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/debug/include")
