bc94cf35c95b6461ac48b4a4a8b6152bbb7a7dca
Billy Robert O'Neal III <bion@microsoft.com>
2024-01-30 10:48:02 -0800

    Also update version and fix header installation.

--- ports/jhasse-poly2tri/portfile.cmake
+++ ports/jhasse-poly2tri/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jhasse/poly2tri
-    REF 81612cb108b54c14c695808f494f432990b279fd
-    SHA512 4310ca8c2c2e62374883e942aa3c78a4c132f5c827b7082a7af60e81586dad589371e52ab08edd473454d37226bcd65c57acdb1e9ec31d49f73af32401d18c79
+    REF 0171f030bd3d5c6747c29d93403546eed668a1b6
+    SHA512 b55d543ae7f9b447d3e0e39b66cf1ce55a48ed7949819db01d8adc0972182519c4b6b533e704a282da45a4d64f510fd33cd81ccb52307dc0e63622e83bcf0192
     HEAD_REF master
     PATCHES
         cmake.patch
@@ -13,9 +13,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME poly2tri)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
