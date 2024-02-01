32156edb63706be59d2baf648d267987ee1cecee
Jon <v-zhli17@microsoft.com>
2024-02-01 03:03:45 -0800

    [cpp-httplib] fix httplibConfig.cmake can't find Brotli

--- ports/cpp-httplib/portfile.cmake
+++ ports/cpp-httplib/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         fix-find-brotli.patch
+        fix_brotli_find_dependency.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
