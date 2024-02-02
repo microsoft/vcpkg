5d9488fd21fdcec1e789c39d7a02b0b08e09f938
Jon <v-zhli17@microsoft.com>
2024-02-01 23:10:12 -0800

    merge patch

--- ports/cpp-httplib/portfile.cmake
+++ ports/cpp-httplib/portfile.cmake
@@ -6,7 +6,6 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         fix-find-brotli.patch
-        fix_brotli_find_dependency.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
