fe011a7808c7e37d710f7b75f7847ae7d57e445b
Tom Chengich <tomcengich@gmail.com>
2024-02-05 00:00:33 +0200

    Fix MacOS build.

--- ports/libvhdi/portfile.cmake
+++ ports/libvhdi/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
     SOURCE_BASE "${VERSION}"
+    PATCHES macos_fixes.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
