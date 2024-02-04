b6c7cc2d7552b5316306bce9c3d3301a1388d5ab
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-02-05 07:20:43 +0800

    [c-ares] update to 1.26.0

--- ports/c-ares/portfile.cmake
+++ ports/c-ares/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO c-ares/c-ares
     REF "cares-${_c_ares_version_major}_${_c_ares_version_minor}_${_c_ares_version_patch}"
-    SHA512 a99ca6066490ef31b37f6be12f9fa1d7599c3736c04c8df50a4a0d2e489c99c1c776028fcfec4e6029c8aa5845f3bede364cceb39cd95b2689305d23277bdd8d
+    SHA512 1ff8d35cc0e022a0478149ca80a0a1f80b2c8d04e108b6cb9912ecc8c391017b6443129b9b52c2cf82b21ef338de0c80d89c92c6d7f95fe1f9c42575ed1a3919
     HEAD_REF main
     PATCHES
         avoid-docs.patch
