d1bb578e152d41c147ebfc7a5e85664226c708be
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-02-04 09:53:55 +0800

    [audiofile] update to 1.1.1

--- ports/audiofile/portfile.cmake
+++ ports/audiofile/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO adamstark/AudioFile
-    REF 004065d01e9b7338580390d4fdbfbaa46adede4e # 1.1.0
-    SHA512 99d31035fc82ca8da3c555c30b6b40ea99e15e1f82002c7f04c567ab7aee1de71deddf6930564c56f3a2e83eea1b5f5e9ca631673ed4a943579732b8d62e9603
+    REF "${VERSION}"
+    SHA512 0
     HEAD_REF master
     PATCHES
         fix-cmakeLists.patch
