1ad37a3d1e9ac121864c847b7bd5d3c0d8f96961
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-02-06 08:46:32 +0800

    bde config fix

--- ports/cwalk/portfile.cmake
+++ ports/cwalk/portfile.cmake
@@ -16,6 +16,7 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cwalk)
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
