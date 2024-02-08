31d25092b99c3ab83742ba16fa761f505d32aef9
Andrew Mee <primary.edw@gmail.com>
2024-02-08 10:45:35 +1000

    Update ports/am-midi2/portfile.cmake
    Following suggested improvements
    
    Co-authored-by: WangWeiLin-MV <156736127+WangWeiLin-MV@users.noreply.github.com>

--- ports/am-midi2/portfile.cmake
+++ ports/am-midi2/portfile.cmake
@@ -12,4 +12,5 @@ vcpkg_from_github(
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
