10bbdc9e89ed437d8be119ae0f6a598e8053b1a4
Andrew Mee <primary.edw@gmail.com>
2024-02-08 10:45:10 +1000

    Update ports/am-midi2/portfile.cmake
    Following suggested improvments
    
    Co-authored-by: WangWeiLin-MV <156736127+WangWeiLin-MV@users.noreply.github.com>

--- ports/am-midi2/portfile.cmake
+++ ports/am-midi2/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO midi2-dev/AM_MIDI2.0Lib
-    REF v0.6
+    REF "v${VERSION}"
     SHA512 fc8b937fd28fe616c58c3e9991e50e262a791c0aa457d9d1263825a5264ae16385a89752cca66d4864bc4f68df329d1c4fa59f6af2aa937a829932b1c263450f
     HEAD_REF main
 )
