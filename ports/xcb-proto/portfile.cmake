a5efc83c8e18b62597cd3eb6c2d3dd3d7438bb8f
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-02-09 11:40:37 +0100

    Fix xcb-util-errors

--- ports/xcb-proto/portfile.cmake
+++ ports/xcb-proto/portfile.cmake
@@ -36,12 +36,14 @@ vcpkg_configure_make(
     AUTOCONFIG
     OPTIONS
         ac_cv_path_PYTHON='${PYTHON3}'
-        am_cv_python_pyexecdir=\\\${prefix}/tools/python3/site-packages
-        am_cv_python_pythondir=\\\${prefix}/tools/python3/site-packages
+        am_cv_python_pyexecdir=\\\${prefix}/${PYTHON3_SITE}
+        am_cv_python_pythondir=\\\${prefix}/${PYTHON3_SITE}
         )
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/xcb-proto.pc" "\${pc_sysrootdir}" "")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/xcb-proto.pc" "\${pc_sysrootdir}" "")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
