a34d93360177c1d6a3a1375c97b33e9bbaef5f78
miyanyan <1138989048@qq.com>
2024-02-18 17:26:48 +0800

    [pocketpy] remove usage install

--- ports/pocketpy/portfile.cmake
+++ ports/pocketpy/portfile.cmake
@@ -29,5 +29,4 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
