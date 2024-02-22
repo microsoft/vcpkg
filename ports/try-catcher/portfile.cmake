70c433e03d451653836445ccf5c18723e7b533f6
barnarddale <dale@dalebarnard.com>
2024-02-21 21:37:16 -0500

    Added copying usage and LICENSE

--- ports/try-catcher/portfile.cmake
+++ ports/try-catcher/portfile.cmake
@@ -8,3 +8,7 @@ vcpkg_from_github(
 
 # This is a header-only modern C++ package.
 file(INSTALL "${SOURCE_PATH}/TryCatcher.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
