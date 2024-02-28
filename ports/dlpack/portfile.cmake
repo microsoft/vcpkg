7ac22914a3298c00216619f6d13ba71def9ee600
Martin Valgur <martin.valgur@gmail.com>
2024-02-28 10:22:27 +0200

    Apply suggestions from code review
    Co-authored-by: Frank <65999885+FrankXie05@users.noreply.github.com>

--- ports/dlpack/portfile.cmake
+++ ports/dlpack/portfile.cmake
@@ -16,9 +16,9 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/dlpack")
 
-# remove debug for header-only library
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
