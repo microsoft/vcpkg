571db396455a4edf2053f16a02f8cc0e629c96c9
Martin Valgur <martin.valgur@gmail.com>
2024-03-01 13:30:08 +0200

    [liblzf] apply review comments

--- ports/liblzf/portfile.cmake
+++ ports/liblzf/portfile.cmake
@@ -20,11 +20,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "unofficial-liblzf")
 
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
