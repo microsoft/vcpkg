993c65445ae97cbd89fff76e7dc09de9d4798b0e
Dennis Hezel <dh@3yourmind.com>
2024-03-01 17:14:00 +0100

    protobuf: Change patches of several ports from find_package(protobuf back to find_package(Protobuf. Re-enable mysql-connector-cpp ci build. Fix port-version of cld3

--- ports/osgearth/portfile.cmake
+++ ports/osgearth/portfile.cmake
@@ -97,5 +97,4 @@ endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
