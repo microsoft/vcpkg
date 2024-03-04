c6f67c577ec7ad1a2e0b18292b8e9e1d6e838144
miyanyan <1138989048@qq.com>
2024-03-04 19:42:03 +0800

    [pocketpy] update to 1.4.2

--- ports/pocketpy/portfile.cmake
+++ ports/pocketpy/portfile.cmake
@@ -2,10 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pocketpy/pocketpy
     REF "v${VERSION}"
-    SHA512 fe78e896b656fedbd60da02bf8497c7fc4254109160c72198dc066f2530c69fa0dcd069d6d3241a95709d803a5dfd6f89acf2b6c40a40b7b8f2b262de29d01a3
+    SHA512 994736b57250d415fbae7c7fcb82c7bed6423fb9eb97ab7fd7d1c3ad584dfa32f85dda31fa1dc49842b900d1bdf73f825a220587342cf8d0b0f85cc06c27a15a
     HEAD_REF master
-    PATCHES
-        add-cmake-install.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
