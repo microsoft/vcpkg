f70dcc9afe52a7894c3d00b10d82e593537cd3fe
miyanyan <1138989048@qq.com>
2024-03-07 09:11:58 +0800

    [proxy] update to 2.2.1

--- ports/proxy/portfile.cmake
+++ ports/proxy/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/proxy
     REF ${VERSION}
-    SHA512 d8e635db541a53c1ea6014bf202dc59c0a358dd9288bcef2eeccc1dfdc0366ed63c8c25663880e65e8e03b8851a208a0a7028439df74cce8600c9cd01c4b8310
+    SHA512 31ccca739eacd3bde8f5db4c4e6350bb37790681186dd6f2d2d8ee3fc1d4e2b42beab2b8c893793cb3ea6128deaf812f8cb658a74a2df5abc1af1eea95972509
     HEAD_REF main
 )
 
