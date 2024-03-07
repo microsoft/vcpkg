542e4bdfa736ae2c1dd252f5ceb834a3023d2040
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-03-07 10:27:24 +0800

    bde tmp

--- ports/libcbor/portfile.cmake
+++ ports/libcbor/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_build()
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_copy_pdbs()
