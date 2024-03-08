14b907b6ccd28c21891e8ba0a007460ca2ca12da
Atliac <Atliac@outlook.com>
2024-03-08 23:32:49 +0800

    [minitest] Update to 1.1.1

--- ports/minitest/portfile.cmake
+++ ports/minitest/portfile.cmake
@@ -1,25 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Atliac/minitest
-    REF v1.1.0
+    REF v1.1.1
     SHA512 8fcafbb9fb066c212b8aea692ae8afe364b86110138ebc5bd43db3dc291f9adb5a107b67074ce47a3ed3b9f57a61526b24e49aeb133c7705bc33b70090042a06
     HEAD_REF master
 )
 
-if(WIN32)
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DBUILD_TESTS=OFF
 )
-else()
-# use g++13 on linux
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
-        -DBUILD_TESTS=OFF
-        -DCMAKE_CXX_COMPILER=g++-13
-        -DCMAKE_C_COMPILER=gcc-13
-)
-endif()
 
 vcpkg_cmake_install()
 
