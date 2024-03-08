309d6073b31b49c9f5d3919aa79cc616875a31f7
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-03-08 18:17:38 +0800

    [libe57] update to 1.1.334

--- ports/libe57/portfile.cmake
+++ ports/libe57/portfile.cmake
@@ -1,11 +1,10 @@
-set(VERSION 1.1.332)
 set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/E57RefImpl_src-${VERSION}")
 
 vcpkg_from_sourceforge(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO e57-3d-imgfmt
     FILENAME "E57RefImpl_src-${VERSION}.zip"
-    SHA512 86adb88cff32d72905e923b1205d609a2bce2eabd78995c59a7957395b233766a5ce31481db08977117abc1a70bbed90d2ce0cdb9897704a8c63d992e91a3907
+    SHA512 0
     PATCHES 
         "0001_cmake.patch"
         "0002_replace_tr1_with_cpp11.patch"
