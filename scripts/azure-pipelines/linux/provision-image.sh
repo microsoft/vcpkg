4f7696edf8b1f4bbb2a43f0557dade888f1ee3ee
John Wason <wason@wasontech.com>
2024-03-08 17:30:40 -0500

    Add libbluetooth-dev apt package to Linux provision-image.sh

--- scripts/azure-pipelines/linux/provision-image.sh
+++ scripts/azure-pipelines/linux/provision-image.sh
@@ -94,6 +94,9 @@ APT_PACKAGES="$APT_PACKAGES golang-go"
 ## required by libdecor and mesa
 APT_PACKAGES="$APT_PACKAGES wayland-protocols"
 
+## required by robotraconteur
+APT_PACKAGES="$APT_PACKAGES libbluetooth-dev"
+
 ## CUDA
 APT_PACKAGES="$APT_PACKAGES cuda-compiler-12-1 cuda-libraries-dev-12-1 cuda-driver-dev-12-1 \
   cuda-cudart-dev-12-1 libcublas-12-1 libcurand-dev-12-1 cuda-nvml-dev-12-1 libcudnn8-dev libnccl2 \
