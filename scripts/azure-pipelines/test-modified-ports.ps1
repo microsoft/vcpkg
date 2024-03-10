7db9c5192ebe579bce6fc8555272d6020271f5d3
data-queue <144965964+data-queue@users.noreply.github.com>
2024-03-10 01:22:37 +0000

    fix

--- scripts/azure-pipelines/test-modified-ports.ps1
+++ scripts/azure-pipelines/test-modified-ports.ps1
@@ -161,8 +161,7 @@ if (($BuildReason -eq 'PullRequest') -and -not $NoParentHashes)
         # but changes must trigger at least some testing.
         Copy-Item "scripts/buildsystems/vcpkg.cmake" -Destination "scripts/test_ports/cmake"
         Copy-Item "scripts/buildsystems/vcpkg.cmake" -Destination "scripts/test_ports/cmake-user"
-        & "./vcpkg$executableExtension" install "$libraries"
-        # & "./vcpkg$executableExtension" ci "--triplet=$Triplet" --dry-run "--ci-baseline=$PSScriptRoot/../ci.baseline.txt" @commonArgs --no-binarycaching "--output-hashes=$parentHashesFile"
+        & "./vcpkg$executableExtension" ci "--triplet=$Triplet" --dry-run "--ci-baseline=$PSScriptRoot/../ci.baseline.txt" @commonArgs --no-binarycaching "--output-hashes=$parentHashesFile"
     }
     else
     {
@@ -177,7 +176,7 @@ if (($BuildReason -eq 'PullRequest') -and -not $NoParentHashes)
 Copy-Item "scripts/buildsystems/vcpkg.cmake" -Destination "scripts/test_ports/cmake"
 Copy-Item "scripts/buildsystems/vcpkg.cmake" -Destination "scripts/test_ports/cmake-user"
 
-& "./vcpkg$executableExtension" install "$libraries"
+& "./vcpkg$executableExtension" install $libraries "--triplet=$Triplet" --failure-logs=$failureLogs --x-xunit=$xunitFile "--ci-baseline=$PSScriptRoot/../ci.baseline.txt" @commonArgs @cachingArgs @parentHashes @skipFailuresArg
 # & "./vcpkg$executableExtension" ci "--triplet=$Triplet" --failure-logs=$failureLogs --x-xunit=$xunitFile "--ci-baseline=$PSScriptRoot/../ci.baseline.txt" @commonArgs @cachingArgs @parentHashes @skipFailuresArg
 
 $failureLogsEmpty = (-Not (Test-Path $failureLogs) -Or ((Get-ChildItem $failureLogs).count -eq 0))
