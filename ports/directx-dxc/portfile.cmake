161d1686170ee14193705f1b3e382d204b91b7c0
Kai Pastor <dg0yt@darc.de>
2024-03-15 06:49:18 +0100

    Cleanup

--- ports/directx-dxc/portfile.cmake
+++ ports/directx-dxc/portfile.cmake
@@ -116,7 +116,5 @@ configure_file("${CMAKE_CURRENT_LIST_DIR}/directx-dxc-config.cmake.in"
   "${CURRENT_PACKAGES_DIR}/share/${PORT}/${PORT}-config.cmake"
   @ONLY)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${LICENSE_TXT}")
