d24e4ef2dfeeaf50a6d7088ab543fed07f65acdf
Andrew Mee <primary.edw@gmail.com>
2024-03-20 10:21:59 +1000

    Updates to v0.7 of libmidi2

--- ports/libmidi2/portfile.cmake
+++ ports/libmidi2/portfile.cmake
@@ -3,12 +3,14 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO midi2-dev/AM_MIDI2.0Lib
     REF "v${VERSION}"
-    SHA512 fc8b937fd28fe616c58c3e9991e50e262a791c0aa457d9d1263825a5264ae16385a89752cca66d4864bc4f68df329d1c4fa59f6af2aa937a829932b1c263450f
+    SHA512 663c2449b30a426fae0c4b57eae6e3c4d95a402ce72f175f39064f251420ff06ef6d58cf635614c27938284ae8b51793d53fa87426ac122318cddaa55b48ef02
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
 
