353b87f701eac899aaa122a5d88bb5a091ccdc88
Atliac <Atliac@outlook.com>
2024-03-23 17:52:12 +0800

    Update hash

--- ports/atliac-minitest/portfile.cmake
+++ ports/atliac-minitest/portfile.cmake
@@ -2,20 +2,21 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Atliac/minitest
     REF "v${VERSION}"
-    SHA512 3c15a2fff59c5050eaa995da10fa57ee4c414dda75f5d2801c1defcc0dd7096d24f831a57c6bf059b971275d3369b3b9283aa1de738a5b7ce322008a50c4e3d7
+    SHA512 bd39aa9d4f897f03f0f42b28ee7aabc0fbe5168d09009ebcb253ee3cb5f52ae1d81e9c6657aaa93fe8f67d20d9d92874432a06cadcb78f750681772bb3316d78
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DBUILD_TESTS=OFF
+        -DMINITEST_PACKAGE_NAME=${PORT}
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME atliac-minitest)
+vcpkg_cmake_config_fixup()
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage")
 
 vcpkg_copy_pdbs()
 
