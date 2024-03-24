75050ab57dd44ea976f4b2db9f8622cb0984e054
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2024-03-24 14:47:52 +0000

    [node] add placeholder for windows support

--- ports/node/nodeConfig.cmake
+++ ports/node/nodeConfig.cmake
@@ -6,12 +6,24 @@ set_target_properties(node PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
     "${CMAKE_CURRENT_LIST_DIR}/../../include/node" # this is easy for process.cc compile
 )
 
-# Set the location of the library for debug configuration
-set_target_properties(node PROPERTIES
-    IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/libnode.so.122"
-)
+if(WIN32)
+    # Set the location of the library for debug configuration on Windows
+    set_target_properties(node PROPERTIES
+        IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/libnode.lib"
+    )
 
-# Set the location of the library for release configuration
-set_target_properties(node PROPERTIES
-    IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "${CMAKE_CURRENT_LIST_DIR}/../../lib/libnode.so.122"
-)
+    # Set the location of the library for release configuration on Windows
+    set_target_properties(node PROPERTIES
+        IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "${CMAKE_CURRENT_LIST_DIR}/../../lib/libnode.lib"
+    )
+else()
+    # Set the location of the library for debug configuration on Linux
+    set_target_properties(node PROPERTIES
+        IMPORTED_LINK_INTERFACE_LIBRARIES_DEBUG "${CMAKE_CURRENT_LIST_DIR}/../../debug/lib/libnode.so.122"
+    )
+
+    # Set the location of the library for release configuration on Linux
+    set_target_properties(node PROPERTIES
+        IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "${CMAKE_CURRENT_LIST_DIR}/../../lib/libnode.so.122"
+    )
+endif()
