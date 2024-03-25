77a2bf53a24e75be60595f17fd9ae129cd52a15a
WangWeiLin-MV <156736127+WangWeiLin-MV@users.noreply.github.com>
2024-03-25 18:00:11 +0800

    [usockets] Update to 0.8.8 and fix CMake dependency

--- ports/usockets/portfile.cmake
+++ ports/usockets/portfile.cmake
@@ -1,28 +1,21 @@
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY) #Upstream only support static compilation: https://github.com/uNetworking/uSockets/commit/b950efd6b10f06dd3ecb5b692e5d415f48474647
-
-if(NOT VCPKG_TARGET_IS_LINUX)
-   set(USE_LIBUV ON)
-endif()
-
-if ("network" IN_LIST FEATURES AND NOT VCPKG_TARGET_IS_WINDOWS)
-    message(FATAL_ERROR "Feature 'network' is only supported on Windows")
-endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO uNetworking/uSockets
     REF "v${VERSION}"
-    SHA512 a4f71e146003056c4a3e99512e6989fd4a37b5943dfcaf593b7b276231c426abd2e1f60de752443fb62ac5926ecf9812866af3c8ce6c8eeeddffc92fa0eb5afb
+    SHA512 726b1665209d0006d6621352c12019bbab22bed75450c5ef1509b409d3c19c059caf94775439d3b910676fa2a4a790d490c3e25e5b8141423d88823642be7ac7
     HEAD_REF master
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        ssl CMAKE_USE_OPENSSL
-        event CMAKE_USE_EVENT
-        network CMAKE_USE_NETWORK
+        ssl     WITH_OPENSSL
 )
+set(LIBUS_COMPLEMENT_OPTIONS "")
+if(VCPKG_TARGET_IS_WINDOWS) # windows requires a third party event-loop
+    list(APPEND LIBUS_COMPLEMENT_OPTIONS "-DWITH_LIBUV=ON") # system default or WITH_LIBUV
+endif()
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
@@ -30,13 +23,15 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
-        "-DLIBUS_USE_LIBUV=${USE_LIBUV}"
-    OPTIONS_DEBUG
-        -DINSTALL_HEADERS=OFF
+        ${LIBUS_COMPLEMENT_OPTIONS}
+        -DVERSION=${VERSION}
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "unofficial-uSockets")
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-vcpkg_copy_pdbs()
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
