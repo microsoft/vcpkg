972904545c6ab9644abfebc93ae753d1fa1b90e5
Vijai Kumar S <vijai@vijaikumar.in>
2024-03-25 17:50:16 +0530

    Pushing fixed portfile (for testing)

--- ports/lib3mf/portfile.cmake
+++ ports/lib3mf/portfile.cmake
@@ -1,51 +1,66 @@
 include(vcpkg_execute_required_process)
 
-# Define the source path
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/lib3mf)
+#set(VCPKG_BUILD_TYPE release) 
 
-# Remove the existing source path if necessary
-file(REMOVE_RECURSE ${SOURCE_PATH})
-
-# Clone the repository
-vcpkg_execute_required_process(
-    COMMAND git clone https://github.com/vijaiaeroastro/lib3mf.git ${SOURCE_PATH}
-    WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-    LOGNAME git-clone
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO 3MFConsortium/lib3mf
+    REF 23a0a899c940467716456fee3936d756bdf97740 # This is the commit you are interested in.
+    SHA512 7dab8804c7ee5b568057e07863da536b9fc2a97f74388096d77ae2a786bf0fcf8a1cac477327ea50d5f4d1aebfad968bb9c24b4f6ab29589777cab32c843bf0f # You need to replace this with the actual SHA-512 hash of the commit.
+    TAG v2.3.1 # This is the version you mentioned.
+    SUBMODULES RECURSE
 )
 
-vcpkg_execute_required_process(
-    COMMAND git --version
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME git-version
-)
+# Custom function to parse .gitmodules and clone submodules
+function(clone_submodules source_path)
+  file(READ "${source_path}/.gitmodules" gitmodules_content)
+  string(REGEX MATCHALL "\\[submodule \"([^\"]+)\"\\][^\[]*path = ([^\n]+)\n[^\[]*url = ([^\n]+)" submodule_matches "${gitmodules_content}")
 
-# Checkout the specific commit
-vcpkg_execute_required_process(
-    COMMAND git checkout 0b212ba3de1fb2abf44e1a345fe3d4496c2f3622
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME git-checkout
-)
+  foreach(submodule_match ${submodule_matches})
+    string(REGEX REPLACE "\\[submodule \"([^\"]+)\"\\][^\[]*path = ([^\n]+)\n[^\[]*url = ([^\n]+)" "\\1;\\2;\\3" submodule_info "${submodule_match}")
+    separate_arguments(submodule_info)
+    list(GET submodule_info 0 submodule_name)
+    list(GET submodule_info 1 submodule_path)
+    list(GET submodule_info 2 submodule_url)
 
-# Initialize and update submodules
-vcpkg_execute_required_process(
-    COMMAND git submodule update --init --recursive
-    WORKING_DIRECTORY ${SOURCE_PATH}
-    LOGNAME git-submodule
-)
+    # Normalize submodule name to be filesystem-friendly
+    string(REPLACE "/" "_" normalized_submodule_name "${submodule_name}")
+
+    # Define a log file name based on the submodule name
+    set(logfile "${CMAKE_CURRENT_BINARY_DIR}/submodule-${normalized_submodule_name}-clone-log.txt")
+
+    message(STATUS "Cloning submodule ${submodule_name} from ${submodule_url} to ${submodule_path}")
+
+    # Clone the submodule and direct output to the log file
+    vcpkg_execute_required_process(
+      COMMAND git clone ${submodule_url} "${source_path}/${submodule_path}"
+      WORKING_DIRECTORY ${source_path}
+      LOGNAME ${logfile}
+    )
+  endforeach()
+endfunction()
+
+
+# Now call the function with the path to where the source is located
+clone_submodules(${SOURCE_PATH})
+
 
 # Proceed with the usual build process
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS -DLIB3MF_TESTS=OFF -DUSE_INCLUDED_GTEST=OFF
+    OPTIONS -DLIB3MF_TESTS=OFF
 )
 
+# Install the package
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/")
 
-foreach(_file IN ITEMS ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/lib3mf.pc ${CURRENT_PACKAGES_DIR}/lib/pkgconfig/lib3mf.pc)
-    file(READ "${_file}" _contents)
-    string(REPLACE "/home/vijai/Code/DEPS/vcpkg" "\${prefix}" _contents "${_contents}")
-    file(WRITE "${_file}" "${_contents}")
-endforeach()
+# Copy all PDB's
+vcpkg_copy_pdbs()
 
+# Install the license
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Remove some of the debug stuff
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/pkgconfig")
