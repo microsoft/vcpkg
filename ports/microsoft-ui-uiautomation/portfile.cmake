7273340ed7507153363f8d230a3c3453cb27b8b1
srktirumala <contactcmpn@gmail.com>
2024-03-26 15:42:40 +0530

    some fixes

--- ports/microsoft-ui-uiautomation/portfile.cmake
+++ ports/microsoft-ui-uiautomation/portfile.cmake
@@ -1,21 +1,23 @@
 # portfile.cmake
-include(vcpkg_common_functions)
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Microsoft/Microsoft-UI-UIAutomation
-    REF master
-    SHA512 a1388b50a70b14d545adfdce995cc8c88b410d49166ec58ed12b8457248884574e428d8d22c01510907b49042363f4608d4fb4a86f3d79f9fefff746c692e9d1
-    HEAD_REF master
+    REF d8c87fae212678915d9b73505e188c3b63db0d79
+    SHA512 4266d5f236ee7f4e2daf02177544876f025b6ec04e9ce0ad63afd0c4674ef1411865a9188d6a8a3a13b64bab1da70bebba25f03b3fc1bf39272f0c04bcbbe28f
+    HEAD_REF main
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_pkgconfig()
+vcpkg_cmake_install()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-vcpkg_install_copyright(FILE_PATH "${SOURCE_PATH}/LICENSE.txt")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "Microsoft-UI-UIAutomation")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
