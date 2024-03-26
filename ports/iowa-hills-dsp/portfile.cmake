b18a2281baabf076472b480c3b4d2a5a6aa20ecc
John Farrier <john.farrier@digitalinblue.com>
2024-03-26 09:11:59 -0400

    Port updated to use authoritative source.

--- ports/iowa-hills-dsp/portfile.cmake
+++ ports/iowa-hills-dsp/portfile.cmake
@@ -1,21 +1,22 @@
-vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO DigitalInBlue/iowahills_dsp
+    REPO hayguen/iowahills_dsp
     REF "v${VERSION}"
-    SHA512 0047d8f8f2962b082d14b0a93c30b0a9d5a8be106a71053d15c5743dc4a1ad2744e0261989a42d190536fb8fb203c8ab2d50bac1fcdad309e75d07fd0dfadfa6
+    SHA512 095fecb1a4bf074a3e11da7e6edaba4d375c5603bed5f2578b52f900dbd20ac59f2414a8f9432eba1742807fe8553cc1edd63606fc38400cdda77bf32ee49eb1
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/iowa_hills_dsp.lib")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/iowa_hills_dsp" RENAME copyright)
+# debug/share was not being generated, so the VCPKG_BUILD_TYPE is being set to
+# "release" so that fixup completes successfully.
+set(VCPKG_BUILD_TYPE "release")
+vcpkg_cmake_config_fixup(PACKAGE_NAME iowa_hills_dsp)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
