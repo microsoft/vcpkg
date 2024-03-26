37d5b2c747c6e6f5398891d8f495ad2e87b38be6
Julien Jerphanion <git@jjerphan.xyz>
2024-03-26 16:20:26 +0100

    Remove patch for requiring CMake 3.27
    Since #37457 has been merged.
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- ports/cpp-sparrow/portfile.cmake
+++ ports/cpp-sparrow/portfile.cmake
@@ -6,8 +6,6 @@ vcpkg_from_github(
     REF 799b3ce5173c7731d30faf167f3dbf6c6fcab6fe
     SHA512 f9ed9b59c3510178c220716f5ef66b3112d9c79d026cfdcb05464023be6b4c4edb1d862287c9579d8eaf50a37dbb4f7cfc51d95ed077a9b8f5040c7c95fd0f63
     HEAD_REF main
-    PATCHES
-        0001-build-Require-CMake-3.27.patch
 )
 
 vcpkg_cmake_configure(
