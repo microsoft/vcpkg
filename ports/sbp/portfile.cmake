6594d43da9a8c9fb168c353809494ee9325e2cf1
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-03-28 09:57:23 +0800

    [sbp] update to 5.0.4

--- ports/sbp/portfile.cmake
+++ ports/sbp/portfile.cmake
@@ -7,8 +7,8 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO swift-nav/libsbp
-    REF v3.4.10
-    SHA512 bbdcefad9ff8995759b577790bcffb94355bd0ee29f259fa8d51f54907e252b55871dc5a841e21d23e661fd5b33109761eb20b66c2fb73e9e7de8a34cc8d6528
+    REF "v${VERSION}"
+    SHA512 4d86f71cffa57a2c028dd9e81505b6e277fc39ab0a8214660df1d4cbe6d26bbe5f7ba0b56456dc3fb1995628ddefe3b6e557f36abf1fc98dd3813213a024fdb4
     HEAD_REF master
     PATCHES
       "win32-install-fix.patch"
@@ -17,8 +17,8 @@ vcpkg_from_github(
 vcpkg_from_github(
     OUT_SOURCE_PATH CMAKE_EXTRA_MODS
     REPO swift-nav/cmake
-    REF 373d4fcafbbc0c208dc9ecb278d36ed8c9448eda
-    SHA512 afefc8c7a3fb43ee65b9b8733968a5836938460abbf1bc9e8330f83c3ac4a5819f71a36dcb034004296161c592f4d61545ba10016d6666e7eaf1dca556d99e2e
+    REF "v${VERSION}"
+    SHA512 4d86f71cffa57a2c028dd9e81505b6e277fc39ab0a8214660df1d4cbe6d26bbe5f7ba0b56456dc3fb1995628ddefe3b6e557f36abf1fc98dd3813213a024fdb4
     HEAD_REF master
 )
 
