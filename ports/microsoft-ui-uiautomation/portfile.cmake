12d7335915a8ea4af9ca8aa691e5c80c2fe9487e
srktirumala <contactcmpn@gmail.com>
2024-03-29 17:23:11 +0530

    some port fixes

--- ports/microsoft-ui-uiautomation/portfile.cmake
+++ ports/microsoft-ui-uiautomation/portfile.cmake
@@ -1,5 +1,5 @@
 # portfile.cmake
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -9,15 +9,14 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+vcpkg_msbuild_install(
+    SOURCE_PATH "${SOURCE_PATH}/src/UIAutomation/"
+    PROJECT_SUBPATH "UIAutomation.sln"
+    OPTIONS
+        "/r:True"
+	    "/p:RestorePackagesConfig=True"
 )
 
-vcpkg_cmake_install()
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "Microsoft-UI-UIAutomation")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
