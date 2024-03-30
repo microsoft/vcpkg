44dd1fcb42ece16a70061c8f0bb6157ce541c7d3
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-03-30 16:43:32 +0800

    [urdfdom-headers] update to 1.1.1

--- ports/urdfdom-headers/portfile.cmake
+++ ports/urdfdom-headers/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ros/urdfdom_headers
-    REF 2981892df9da19d10f58dc84de63820e4f554f63 # 1.1.0
-    SHA512 cc47d2fb9781f4c7f1af25ccfb4dc8cc9e72d2ec22cb2fe16866bb0e7ed40494181a413dcd74cb0407b4f5c20262f076f8ae87d605ba0e5477a57ff29ba30967
+    REF "v${VERSION}"
+    SHA512 0
     HEAD_REF master
     PATCHES fix-include-path.patch
   )
