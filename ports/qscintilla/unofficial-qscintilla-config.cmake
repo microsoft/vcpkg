1abd9a7400a527fa5d6faf6f73d97113c0e344db
Lily Wang <v-lilywang@microsoft.com>
2024-04-01 03:02:48 -0700

    Add INTERFACE_LINK_LIBRARIES

--- ports/qscintilla/unofficial-qscintilla-config.cmake
+++ ports/qscintilla/unofficial-qscintilla-config.cmake
@@ -4,6 +4,10 @@ if(NOT TARGET unofficial::qscintilla::qscintilla)
     get_filename_component(z_vcpkg_qscintilla_root "${z_vcpkg_qscintilla_root}" PATH)
     get_filename_component(z_vcpkg_qscintilla_root "${z_vcpkg_qscintilla_root}" PATH)
 
+    include(CMakeFindDependencyMacro)
+    find_dependency(Qt6PrintSupport CONFIG)
+    find_dependency(Qt6Widgets CONFIG)
+
     set_target_properties(unofficial::qscintilla::qscintilla PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${z_vcpkg_qscintilla_root}/include"
     )
@@ -11,13 +15,17 @@ if(NOT TARGET unofficial::qscintilla::qscintilla)
     find_library(Z_VCPKG_QSCINTILLA_LIBRARY_RELEASE NAMES libqscintilla2_qt6 qscintilla2_qt6 PATHS "${z_vcpkg_qscintilla_root}/lib" NO_DEFAULT_PATH)
     if(EXISTS "${Z_VCPKG_QSCINTILLA_LIBRARY_RELEASE}")
         set_property(TARGET unofficial::qscintilla::qscintilla APPEND PROPERTY IMPORTED_CONFIGURATIONS "Release")
-        set_target_properties(unofficial::qscintilla::qscintilla PROPERTIES IMPORTED_LOCATION_RELEASE "${Z_VCPKG_QSCINTILLA_LIBRARY_RELEASE}")
+        set_target_properties(unofficial::qscintilla::qscintilla PROPERTIES
+            IMPORTED_LOCATION_RELEASE "${Z_VCPKG_QSCINTILLA_LIBRARY_RELEASE}"
+            INTERFACE_LINK_LIBRARIES "Qt::PrintSupport;Qt6::PrintSupport;Qt::PrintSupportPrivate;Qt6::PrintSupportPrivate;Qt::Widgets;Qt6::Widgets;Qt::WidgetsPrivate;Qt6::WidgetsPrivate")
     endif()
 
     find_library(Z_VCPKG_QSCINTILLA_LIBRARY_DEBUG NAMES libqscintilla2_qt6 qscintilla2_qt6d libqscintilla2_qt6_debug PATHS "${z_vcpkg_qscintilla_root}/debug/lib" NO_DEFAULT_PATH)
     if(EXISTS "${Z_VCPKG_QSCINTILLA_LIBRARY_DEBUG}")
         set_property(TARGET unofficial::qscintilla::qscintilla APPEND PROPERTY IMPORTED_CONFIGURATIONS "Debug")
-        set_target_properties(unofficial::qscintilla::qscintilla PROPERTIES IMPORTED_LOCATION_DEBUG "${Z_VCPKG_QSCINTILLA_LIBRARY_DEBUG}")
+        set_target_properties(unofficial::qscintilla::qscintilla PROPERTIES
+            IMPORTED_LOCATION_DEBUG "${Z_VCPKG_QSCINTILLA_LIBRARY_DEBUG}"
+            INTERFACE_LINK_LIBRARIES "Qt::PrintSupport;Qt6::PrintSupport;Qt::PrintSupportPrivate;Qt6::PrintSupportPrivate;Qt::Widgets;Qt6::Widgets;Qt::WidgetsPrivate;Qt6::WidgetsPrivate")
     endif()
 
     unset(z_vcpkg_qscintilla_root)
