4cc70f7b2751d54316acb8f2f086aed68bc35495
Kai Pastor <dg0yt@darc.de>
2024-04-03 22:32:51 +0200

    [unit-test-cmake] Cover rpath fixup utilities

---
+++ scripts/test_ports/unit-test-cmake/test-z_vcpkg_calculate_corrected_rpath.cmake
@@ -0,0 +1,36 @@
+# z_vcpkg_calculate_corrected_rpath(...)
+
+block(SCOPE_FOR VARIABLES)
+
+set(CURRENT_PACKAGES_DIR "/P")
+set(CURRENT_INSTALLED_DIR "/I")
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/lib")
+]] out [[$ORIGIN]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/plugins/group")
+]] out [[$ORIGIN:$ORIGIN/../../lib]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/debug/lib")
+]] out [[$ORIGIN]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/debug/plugins/group")
+]] out [[$ORIGIN:$ORIGIN/../../lib]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/tools/port")
+]] out [[$ORIGIN:$ORIGIN/../../lib]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/tools/port/bin")
+]] out [[$ORIGIN:$ORIGIN/../../../lib]])
+
+unit_test_check_variable_equal([[
+    z_vcpkg_calculate_corrected_rpath(OUT_NEW_RPATH_VAR "out" ORG_RPATH "" ELF_FILE_DIR "/P/tools/port/debug/bin")
+]] out [[$ORIGIN:$ORIGIN/../../../../debug/lib]])
+
+endblock()
