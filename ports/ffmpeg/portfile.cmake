b806e243612a401b53a2b2682acceb25a71aab3f
Matt Oliver <protogonoi@gmail.com>
2024-04-07 16:07:04 +1000

    [ffmpeg] Fix OSX cross compilation.

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -19,6 +19,7 @@ vcpkg_from_github(
         0015-Fix-xml2-detection.patch
         0020-fix-aarch64-libswscale.patch
         0022-fix-iconv.patch
+        0024-fix-osx-host-c11.patch
 )
 
 if(SOURCE_PATH MATCHES " ")
