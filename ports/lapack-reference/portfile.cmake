3aeecf93e6941923f206fce7d89fd38944a3b5fa
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-04-09 07:51:10 +0000

    modify config

--- ports/lapack-reference/portfile.cmake
+++ ports/lapack-reference/portfile.cmake
@@ -144,6 +144,8 @@ if(VCPKG_TARGET_IS_WINDOWS)
     endif()
 endif()
 
+#file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/lapack")
+#file(COPY "${CMAKE_CURRENT_LIST_DIR}/FindLAPACK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/lapack")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
