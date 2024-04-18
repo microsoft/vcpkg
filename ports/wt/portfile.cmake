79d06ae08bbc6e66d56ef253c3da53bd2d4b8fc6
Yury Bura <yurybura@gmail.com>
2024-04-18 06:08:56 -0700

    [wt] fix build with Boost 1.85.0

--- ports/wt/portfile.cmake
+++ ports/wt/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
         0002-link-glew.patch
         0005-XML_file_path.patch
         0006-GraphicsMagick.patch
+        fix-compatibility-with-boost-1.85.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SHARED_LIBS)
