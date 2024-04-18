10551b4f56be17216aeab42eff8ac1364d15a7e3
Kevin Lalumiere <klalumiere@coveo.com>
2024-04-17 20:11:38 -0400

    [hareflow] Minor update + add vcpkg_cmake_config_fixup

--- ports/hareflow/portfile.cmake
+++ ports/hareflow/portfile.cmake
@@ -1,11 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO coveooss/hareflow
-    REF a59406398c60d4908a9f894c187ef90370119594 # 0.1.0
-    SHA512 ed9e3b54f879fed65cf7765b37951a0da04e99b09950316d9108ea35917d05b4733c5cb8f3c161e42f1baab5978affd4522e5b5028861b3f7d6cc0bfe0a40363
+    REF a37ca1c6ea3475ea767362845a1d848a86fb20a2 # 0.1.1
+    SHA512 e5c7932f000197cd1f9153fc0706be3eff8fe2d4f64eb34f219d5d0419f3d7d232ed3f65669f78c3de3f97ff11c8afde1d07e34ae8082f4b6a2264de12ddd5b6
     HEAD_REF main
-    PATCHES
-        fix-missing-headers.patch # https://github.com/coveooss/hareflow/pull/9
 )
 
 if(VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_IOS)
@@ -20,6 +18,8 @@ vcpkg_cmake_configure(
         "-DCMAKE_INSTALL_RPATH=${rpath}"
 )
 vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
