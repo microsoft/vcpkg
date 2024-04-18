c531f2f8d7d13c03238dcf4eccbf5cd044fa88c4
Kevin Lalumiere <kevin.lalumiere@gmail.com>
2024-04-18 05:42:15 -0400

    Use vcpkg_install_copyright
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/hareflow/portfile.cmake
+++ ports/hareflow/portfile.cmake
@@ -21,5 +21,5 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
