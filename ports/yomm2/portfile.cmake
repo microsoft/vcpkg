0b57b2861cb6ac814fedefc4fee11e26079e3e4b
Jean-Louis Leroy <jll63@users.noreply.github.com>
2024-04-21 12:09:15 -0400

    [yomm2] update to v1.5.0

--- ports/yomm2/portfile.cmake
+++ ports/yomm2/portfile.cmake
@@ -4,7 +4,6 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512  35d869f79b278ae219d61e0ae3b01902c5df5457d2ced7bfd109cf0e75f3f7835ce3d4751c34838d134531f6483dc89b7d67d5ecab6e8af42b4b735284573db4
     HEAD_REF master
-    PATCHES "fix_install.patch"
 )
 
 set(YOMM2_SHARED OFF)
@@ -14,7 +13,7 @@ endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS 
+    OPTIONS
         -DYOMM2_ENABLE_EXAMPLES=OFF
         -DYOMM2_SHARED=${YOMM2_SHARED}
 )
