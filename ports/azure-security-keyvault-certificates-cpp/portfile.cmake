5cc3a0e5cd1b9e6eaa461b6e4b74ea138a810b84
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2024-04-22 09:56:49 -0700

    [azure-*-cpp] Add HEAD_REF to manifests

--- ports/azure-security-keyvault-certificates-cpp/portfile.cmake
+++ ports/azure-security-keyvault-certificates-cpp/portfile.cmake
@@ -4,8 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-security-keyvault-certificates_4.2.1
+    REF "azure-security-keyvault-certificates_${VERSION}"
     SHA512 f18d205ee0be9ab4289860fe4fdc0f4a6c3571eb0a8d0ca0ea66e7fef477e04ed0ba455e1a5af687e0980d7f12b919d1a5c85ee40d41dbcfd695f5d62843dcd5
+    HEAD_REF main
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/keyvault/azure-security-keyvault-certificates")
--- ports/azure-storage-files-shares-cpp/portfile.cmake
+++ ports/azure-storage-files-shares-cpp/portfile.cmake
@@ -4,8 +4,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF azure-storage-files-shares_12.8.0
+    REF "azure-storage-files-shares_${VERSION}"
     SHA512 a22cce4f43fd77aabdb43fdbc9ea3e5a501f5abeaafe5f5fea7e8737b6b017dc2ca3b674ac0c5f8bcda9b836af7644b3c17db050b02db5e076728047aa5c2ec0
+    HEAD_REF main
 )
 
 if(EXISTS "${SOURCE_PATH}/sdk/storage/azure-storage-files-shares")
