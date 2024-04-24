a77515a43541642434ffe5859867652833494f7f
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-04-24 23:20:13 +0500

    some headers + debug/release win build

--- ports/node/portfile.cmake
+++ ports/node/portfile.cmake
@@ -13,8 +13,24 @@ vcpkg_add_to_path(PREPEND "${PYTHON3_EXE_PATH}")
 if(VCPKG_TARGET_IS_WINDOWS)
   set(nodejs_options openssl-no-asm static x64)
 
+  if(NOT DEFINED VCPKG_BUILD_TYPE)
+    execute_process(
+      COMMAND "${SOURCE_PATH}/vcbuild.bat" debug ${nodejs_options}
+      WORKING_DIRECTORY "${SOURCE_PATH}"
+
+      OUTPUT_VARIABLE NODE_BUILD_SH_OUT
+      ERROR_VARIABLE NODE_BUILD_SH_ERR
+      RESULT_VARIABLE NODE_BUILD_SH_RES
+      ECHO_OUTPUT_VARIABLE
+      ECHO_ERROR_VARIABLE
+    )
+    if(NOT NODE_BUILD_SH_RES EQUAL 0)
+      message(FATAL_ERROR "Failed to build nodejs Debug (code ${NODE_BUILD_SH_RES})")
+    endif()
+  endif()
+
   execute_process(
-    COMMAND "${SOURCE_PATH}/vcbuild.bat" ${nodejs_options}
+    COMMAND "${SOURCE_PATH}/vcbuild.bat" release ${nodejs_options}
     WORKING_DIRECTORY "${SOURCE_PATH}"
 
     OUTPUT_VARIABLE NODE_BUILD_SH_OUT
@@ -23,13 +39,18 @@ if(VCPKG_TARGET_IS_WINDOWS)
     ECHO_OUTPUT_VARIABLE
     ECHO_ERROR_VARIABLE
   )
-  if (NOT NODE_BUILD_SH_RES EQUAL 0)
-    message(FATAL_ERROR "Failed to build nodejs (code ${NODE_BUILD_SH_RES})")
+  if(NOT NODE_BUILD_SH_RES EQUAL 0)
+    message(FATAL_ERROR "Failed to build nodejs Release (code ${NODE_BUILD_SH_RES})")
   endif()
 
+  file(GLOB libs_debug "${SOURCE_PATH}/Debug/*.lib")
+  foreach(path ${libs_debug})
+    file(COPY "${path}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+  endforeach()
+
   file(GLOB libs "${SOURCE_PATH}/Release/*.lib")
-  foreach(header ${libs})
-    file(COPY "${header}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+  foreach(path ${libs})
+    file(COPY "${path}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
   endforeach()
 else()
   set(nodejs_options)
@@ -45,16 +66,28 @@ else()
     ECHO_ERROR_VARIABLE
   )
 
-  if (NOT NODE_BUILD_SH_RES EQUAL 0)
+  if(NOT NODE_BUILD_SH_RES EQUAL 0)
     message(FATAL_ERROR "Failed to configure nodejs (code ${NODE_BUILD_SH_RES})")
   endif()
 
   vcpkg_install_make()
 endif()
 
-file(GLOB headers "${SOURCE_PATH}/src/*.h")
-foreach(header ${headers})
-  file(COPY "${header}" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-endforeach()
+# main header
+file(COPY "${SOURCE_PATH}/src/node.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# node.h requirements
+file(COPY "${SOURCE_PATH}/src/node_api.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(COPY "${SOURCE_PATH}/src/node_version.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# TODO: v8.h
+# TODO: v8-platform.h
+
+# node_api.h requirements
+file(COPY "${SOURCE_PATH}/src/js_native_api.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(COPY "${SOURCE_PATH}/src/node_api_types.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# js_native_api.h requirements
+file(COPY "${SOURCE_PATH}/src/js_native_api_types.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
