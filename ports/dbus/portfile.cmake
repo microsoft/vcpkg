microsoft/vcpkg

commit 01246903ea57e145c098bcacc862c2f0684df4f7

[dbus] dbus-daemon, dbus-launch,... are first class artifacts (#37736)
dbus-daemon, dbus-launch and the other executables in the dbus port are
integral components of the D-Bus system. libdbus-1 is of no utility in
itself without dbus-daemon. A case could be made that libdbus-1 in Vcpkg
could be used on Linux against the distro-provided dbus-daemon, but this
is not true on Windows and other platforms where the dbus package is
also available (and usefully so). Therefore these executables must be
treated as first-class artifacts in the vcpkg port that can be installed
along with shared libraries to a prefix by consuming applications.

The release version of the dbus-* executables are available in the tools
folder, but that leaves a hole for dynamically linked debug builds.
These cannot install the dbus-* executables from the tools folder as
they are release binaries that are not (necessarily) compatible with the
installed libdbus-1.so/dll and other shared library dependencies. To
properly support debug builds with dynamic linking the debug variant of
these executables must also be preserved. They cannot be moved to
tools/<port>/debug as that change of directory layout is not compatible
with the RUNPATH in the executables [$ORIGIN:$ORIGIN/../../lib]. Instead
all dbus-* executables are left in the bin and bin/debug layout.

Fixes #37625

- [x] Changes comply with the [maintainer
guide](https://github.com/microsoft/vcpkg-docs/blob/main/vcpkg/contributing/maintainer-guide.md).
- [x] SHA512s are updated for each updated download.
- [x] The "supports" clause reflects platforms that may be fixed by this
new version.
- [x] Any fixed [CI
baseline](https://github.com/microsoft/vcpkg/blob/master/scripts/ci.baseline.txt)
entries are removed from that file.
- [x] Any patches that are no longer applied are deleted from the port's
directory.
- [x] The version database is fixed by rerunning `./vcpkg x-add-version
--all` and committing the result.

ports/dbus/portfile.cmake
@@ -83,6 +83,7 @@ else()
     list(APPEND TOOLS cleanup-sockets uuidgen)
 endif()
 list(TRANSFORM TOOLS PREPEND "dbus-" )
+vcpkg_copy_tools(TOOL_NAMES ${TOOLS} SEARCH_DIR ${CURRENT_PACKAGES_DIR}/debug/bin DESTINATION "${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}")
 vcpkg_copy_tools(TOOL_NAMES ${TOOLS} AUTO_CLEAN)
 
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)

commit 469cb686e5ac596d3834d4f31f15f6ae400b4037

[dbus] Fix release only build (#38350)
Basically fixes the issue introduced in #37736

Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

ports/dbus/portfile.cmake
@@ -7,12 +7,12 @@ vcpkg_from_gitlab(
     REF "dbus-${VERSION}"
     SHA512 8e476b408514e6540c36beb84e8025827c22cda8958b6eb74d22b99c64765eb3cd5a6502aea546e3e5f0534039857b37edee89c659acef40e7cab0939947d4af
     HEAD_REF master
-    PATCHES 
+    PATCHES
         cmake.dep.patch
         pkgconfig.patch
         getpeereid.patch # missing check from configure.ac
         libsystemd.patch
-) 
+)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS options
     FEATURES
@@ -53,7 +53,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "DBus1" CONFIG_PATH "lib/cmake/DBus1")
-vcpkg_fixup_pkgconfig() 
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
@@ -83,7 +83,9 @@ else()
     list(APPEND TOOLS cleanup-sockets uuidgen)
 endif()
 list(TRANSFORM TOOLS PREPEND "dbus-" )
-vcpkg_copy_tools(TOOL_NAMES ${TOOLS} SEARCH_DIR ${CURRENT_PACKAGES_DIR}/debug/bin DESTINATION "${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}")
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    vcpkg_copy_tools(TOOL_NAMES ${TOOLS} SEARCH_DIR ${CURRENT_PACKAGES_DIR}/debug/bin DESTINATION "${CURRENT_PACKAGES_DIR}/debug/tools/${PORT}")
+endif()
 vcpkg_copy_tools(TOOL_NAMES ${TOOLS} AUTO_CLEAN)
 
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)