d5f3476db5277cd6d972e2ad7837e80c8923be9a
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-04-24 14:46:16 +0200

    update numpy

--- ports/numpy/portfile.cmake
+++ ports/numpy/portfile.cmake
@@ -1,4 +1,5 @@
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled) # Numpy includes are stored in the module itself
+set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 set(VCPKG_BUILD_TYPE release) # No debug builds required for pure python modules since vcpkg does not install a debug python executable. 
 
 find_program(VCPKG_PYTHON3 NAMES python${PYTHON3_VERSION_MAJOR}.${PYTHON3_VERSION_MINOR} python${PYTHON3_VERSION_MAJOR} python PATHS "${VCPKG_PYTHON3_BASEDIR}" NO_DEFAULT_PATH)
@@ -12,41 +13,41 @@ if(VCPKG_TARGET_IS_WINDOWS)
   vcpkg_add_to_path(PREPEND "${VCPKG_PYTHON3_BASEDIR}/Scripts")
 endif()
 
+cmake_path(GET SCRIPT_MESON PARENT_PATH MESON_DIR)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO numpy/numpy
-    REF 411a55b9ec084c3315fe5f199351c31d0eb3c352
-    SHA512 44d46d7c4b1e8a568e63951cf424f49dd47a5f3f9ad673a548c954ba373d353af03e12f07d7fa6f523c1dd91431d0f428d1e24703757928b1e9a0f50ada28ee7
+    REF v${VERSION}
+    SHA512 01b6a124c72d082f1dafdd98cdaaa84ab57f2bf0112d89d9355fa458a04deb8309c7e78449767429049971793c040e51412060681218a51c671ac6086dba2fa4
     HEAD_REF main
 )
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH_SIMD
     REPO intel/x86-simd-sort
-    REF 3c9bf9a5c69bc90f51e34aa039f914652d8b31cd
-    SHA512 1940346206e9988c42dfeb0907d34ac6320db067913c837b14028c89c5f41508c1abc1162996a305430a1186bf824d95129ae48178e77d44120cbf1246230749
+    REF v5.0
+    SHA512 8e7a1929b7234399d8ac30cd296b69efdfc0d9d03a91507f3b4e06f486b6b715199b35e0495f330c21b70165bcab48842cd7c0a887df4e6508b6151ad1dc9c2a
     HEAD_REF main
 )
+
+file(COPY "${SOURCE_PATH_SIMD}/" DESTINATION "${SOURCE_PATH}/numpy/core/src/npysort/x86-simd-sort")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH_MESON_NUMPY
     REPO numpy/meson
-    REF 067efcb7f59d4cef86c11f9ef7dd64828c48a9b8
-    SHA512 f1bb457de6e1123c7a69ee39c39dacfdb01001391dbb78e3c864ca5506029409a6153f951e59e4ba4bb0441340a0c686c95a287087e92856dac4009e6379b5a5
+    REF 4e370ca8ab73c07f7b84abe8a4b937caace050a4
+    SHA512 dec6e3b9428f95790f85a863778227a73e4f432f8f54e87d61fd6499b5a0723c59a334fcaf880afd59ae50c924d8f2cfa340a143f752cb39f976c731ca0ea123
     HEAD_REF main
 )
 
-cmake_path(GET SCRIPT_MESON PARENT_PATH MESON_DIR)
 file(COPY "${SOURCE_PATH_MESON_NUMPY}/mesonbuild/modules/features" DESTINATION "${MESON_DIR}/mesonbuild/modules")
-
-file(COPY "${SOURCE_PATH_SIMD}/" DESTINATION "${SOURCE_PATH}/numpy/core/src/npysort/x86-simd-sort")
-set(VCPKG_POLICY_MISMATCHED_NUMBER_OF_BINARIES enabled)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH_SVML
     REPO numpy/SVML
-    REF 958caeac44d66878ab36d364c863bde47c0c69e2
-    SHA512 444c1f765e875cc3eb55e48bcb8fa0c0d5b2e47cc5db16287e424f16b025cdbaf18f5d5eb6ccee6f5d0311e545ba9b8fdfa0f0cbc09508b38ce372afb339f8c7
+    REF 32bf2a98420762a63ab418aaa0a7d6e17eb9627a
+    SHA512 6242569840ac4b645c6070f72f0691ffc090b1c0c87cb935cb04ffd39c6d334bff0cbddcbf8a2ae5de2b1d5bb322f2a5a2dfd8a30b2e25bf7f6af732ffc47040
     HEAD_REF main
 )
 
@@ -71,7 +72,7 @@ vcpkg_configure_meson(
     OPTIONS 
         -Dblas=blas
         -Dlapack=lapack
-        -Duse-ilp64=true
+        #-Duse-ilp64=true
     ADDITIONAL_BINARIES
       cython=['${VCPKG_CYTHON}']
       python3=['${VCPKG_PYTHON3}']
