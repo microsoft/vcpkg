1ed7a81aa14940f3ba637fc86f9c54d008b4ecef
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-04-25 11:24:29 +0200

    use VCPKG_HASH_ADDITIONAL_FILES and update release only triplet

--- triplets/community/x64-windows-release.cmake
+++ triplets/community/x64-windows-release.cmake
@@ -2,3 +2,6 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
+
+set(VCPKG_MAKE_CONFIGURE_CACHE "${CMAKE_CURRENT_LIST_DIR}/../../scripts/toolchains/windows.configure.txt") # ${SCRIPTS} could be used here but the compiler detection does not know about that.
+set(VCPKG_HASH_ADDITIONAL_FILES "${VCPKG_MAKE_CONFIGURE_CACHE}")
