55cc98fdf6cce97eea8adf52f417621f23121029
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-04-25 17:12:25 +0200

    adjust wrapper since it cannot be removed

--- ports/boost-uninstall/vcpkg-cmake-wrapper.cmake
+++ ports/boost-uninstall/vcpkg-cmake-wrapper.cmake
@@ -1,4 +1,3 @@
-#set(Boost_USE_STATIC_LIBS OFF)
-#set(Boost_USE_MULTITHREADED ON)
 # Need to keep this file due to vcpkg.cmake otherwise injecting a different behavior.
+set(Boost_NO_BOOST_CMAKE OFF)
 _find_package(${ARGS})
