87219741058d93898609ac7fea76765e08d053c4
Seedking <875828815@qq.com>
2024-04-27 11:29:37 +0800

    update to 3.8.0

--- ports/stringzilla/portfile.cmake
+++ ports/stringzilla/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ashvardanian/StringZilla
     REF "v${VERSION}"
-    SHA512 ddfc85994ca490a259766d4038d6cdcc455d057e9f0d64bb4338c7dfa3e75ee633c01e91ce4e2e7e0648b61eadf98fc1a08cbd69f6e0d7f8d5a8ae5d641a8cf2
+    SHA512 c14f1281143f061906497b7a36de18e52bbc8f47cd7da8348036afe0a8db57cc50de2cdb842650efefe029b10d3d2ee0f82eadba4b2fc1888bbc3cb5c987b3bb
     HEAD_REF master
 )
 
