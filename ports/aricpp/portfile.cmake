7a0cc826c4f9bee352444b0aac717689bc50b442
FrankXie <v-frankxie@microsoft.com>
2024-04-28 02:05:28 +0000

    version

--- ports/aricpp/portfile.cmake
+++ ports/aricpp/portfile.cmake
@@ -2,7 +2,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO daniele77/aricpp
-    REF "v${VERSION}"
+    REF v1.1.3
     SHA512 5b4c2829177f338f46f5d48baee8d3dd35bdce11780ed1b4b20ddb738573d12227982574877be8f9a8cea355e202cac97eb85a16fd582d2e348a9404b34467fd
     HEAD_REF master
 )
