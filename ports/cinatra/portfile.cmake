89453ac01a2de401e6b1faab7e5fec62a9a084ef
Employee_NO427 <zijian.guan@outlook.com>
2024-05-05 16:12:37 +0800

    Add new ports for cinatra and cmdline

---
+++ ports/cinatra/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO qicosmos/cinatra
+    REF 0.9.1
+    SHA512 55bba51bb6190b76bc6415d3c36e82daeb5f174f1cb10490951cf05863f55653a6d68ffd3b66c5b3b2ebb6e68a7a84c090e973a6718f3f2a5849b04330e4b180
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_install_cmake()
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cinatra RENAME copyright)
+vcpkg_copy_pdbs()
+file(REMOVE_RECURSE ${SOURCE_PATH}/.git)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+
+
+
+
+
+
---
+++ ports/cmdline/portfile.cmake
@@ -0,0 +1,15 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO tanakh/cmdline
+    REF master
+    SHA512 4d16185c9d8a75a90f21063580301a837bfbb40d9d2aae1449243d5dcbd1d32ea78701ee0c9b74b8e527cc9d36e9397023f08a9dd3d96a55acec9fbcf532a635
+    HEAD_REF master
+)
+
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cmdline)
+
+file(INSTALL ${SOURCE_PATH}/cmdline.h
+     DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+
+     
+
