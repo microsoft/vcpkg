4fddfab82a12b1b697378a97d67ac0b70f221561
JackBoosY <398808571@qq.com>
2024-05-05 23:26:16 +0800

    Apply suggestion

--- ports/alicevision/portfile.cmake
+++ ports/alicevision/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO alicevision/AliceVision
-  REF v3.2.0
+  REF "v${VERSION}"
   SHA512 448e20b2bb6623034ab89ea56b0c7fdf09af34d7d0d47740590fa9cd9aa2b6864f3950c4e968d260c660a41a2f5877c10edf8cfd0dcdd18e58ce289b7336e9ec
   HEAD_REF master
   PATCHES fix-dynamic-boost.patch
