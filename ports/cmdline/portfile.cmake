be03cce7ceac67c82ac706133b6b879016ec1a8a
Employee_NO427 <45109567+EmployeeNo427@users.noreply.github.com>
2024-05-06 06:11:21 -0400

    Update ports/cmdline/portfile.cmake
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/cmdline/portfile.cmake
+++ ports/cmdline/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tanakh/cmdline
-    REF master
+    REF e4cd007fb8f0314002d9a5b4d82939106e4144e4
     SHA512 4d16185c9d8a75a90f21063580301a837bfbb40d9d2aae1449243d5dcbd1d32ea78701ee0c9b74b8e527cc9d36e9397023f08a9dd3d96a55acec9fbcf532a635
     HEAD_REF master
 )
