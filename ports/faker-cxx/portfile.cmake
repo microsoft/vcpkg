2fad384e30cc0f5a37779d8dbbefe79fb8b0bd0a
Christian Göhring <c4f60a5d@opayq.com>
2024-05-09 14:52:42 +0200

    Apply suggestions from code review
    usage new lines, vcpkg copyright function, config path fix, head_ref and ref
    
    Co-authored-by: JonLiu1993 <63675417+JonLiu1993@users.noreply.github.com>
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/faker-cxx/portfile.cmake
+++ ports/faker-cxx/portfile.cmake
@@ -3,9 +3,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cieslarmichal/faker-cxx
-    REF 266142b
+    REF "v${VERSION}"
     SHA512 0ad3550d45df2adda70ad64fc1afffc2d39f6644e46029b6b3f0fd42eed071b55daeee9da7456b8b75e1da566bd0d1605518722b2831dba31bf9787506ecfa9d
-    HEAD_REF vcpkg
+    HEAD_REF master
     PATCHES
         "CMakeLists.txt.patch"
         "FormatHelper.h.patch"
@@ -22,10 +22,9 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME "faker-cxx"
-    CONFIG_PATH /lib/cmake/faker-cxx
+    CONFIG_PATH lib/cmake/faker-cxx
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/LICENSE"           DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/")
-file(RENAME "${CURRENT_PACKAGES_DIR}/share/${PORT}/LICENSE" "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
