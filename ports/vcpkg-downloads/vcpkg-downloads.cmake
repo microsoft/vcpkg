fe2b3f6800e7f8c242e0b0075542816c24cba037
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-05-09 10:46:26 +0200

    [vcpkg-downloads] Helper script to parse download.json

---
+++ ports/vcpkg-downloads/vcpkg-downloads.cmake
@@ -0,0 +1,222 @@
+include_guard(GLOBAL)
+
+function(vcpkg_download_from_json)
+  cmake_parse_arguments(PARSE_ARGV 0 "arg" "" "" "JSONS")
+  if(DEFINED arg_UNPARSED_ARGUMENTS)
+      message(FATAL_ERROR "vcpkg_cmake_install was passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
+  endif()
+
+  if(NOT arg_JSONS)
+    list(APPEND arg_JSONS "${CMAKE_CURRENT_LIST_DIR}/download.json")
+  endif()
+
+  foreach(json IN LISTS arg_JSONS)
+    z_vcpkg_download_from_json(vars "${json}")
+  endforeach()
+  
+  foreach(o IN LISTS vars)
+    set("${o}" "${${o}}" PARENT_SCOPE)
+  endforeach()
+endfunction()
+
+function(z_vcpkg_download_from_json outs json_file)
+  file(READ "${json_file}" json_data)
+  z_read_json_member(outvars "${json_data}" "downloads")
+  foreach(o IN LISTS outvars)
+    message("${o}=${${o}}")
+  endforeach()
+
+  set(out "")
+
+  foreach(download_item IN LISTS downloads.index_list)
+    set(prefix downloads.${download_item})
+    foreach(member IN LISTS ${prefix}.members) # Expand all CMake variables in members
+      string(CONFIGURE "${${prefix}.${member}}" "${prefix}.${member}" ESCAPE_QUOTES)
+    endforeach()
+
+    cmake_language(CALL z_vcpkg_download_from_${${prefix}.from} "${prefix}")
+    list(APPEND out "${${prefix}.output_variable}")
+    
+    if(${prefix}.target_dir) # Check --editable?
+      file(REMOVE_RECURSE ${${prefix}.target_dir})
+      # Would like to use rename here ?
+      file(COPY "${${${prefix}.output_variable}}/" DESTINATION "${${prefix}.target_dir}")
+      set("${${prefix}.output_variable}" "${${prefix}.target_dir}" PARENT_SCOPE)
+    endif()
+
+  endforeach()
+  set(${outs} ${${outs}} ${out} PARENT_SCOPE)
+endfunction()
+
+##### Download stuff
+
+function(z_vcpkg_download_from_github data_prefix)
+  set(opts "")
+  if(${data_prefix}.host)
+    list(APPEND opts GITHUB_HOST "${${data_prefix}.host}")
+  endif()
+
+  vcpkg_from_github(
+      OUT_SOURCE_PATH "${${data_prefix}.output_variable}"
+      REPO "${${data_prefix}.repository}"
+      REF "${${data_prefix}.ref}"
+      SHA512 "${${data_prefix}.sha512}"
+      HEAD_REF "${${data_prefix}.head_ref}"
+      PATCHES ${${data_prefix}.patches}
+      ${opts}
+  )
+  set("${${data_prefix}.output_variable}" "${${${data_prefix}.output_variable}}" PARENT_SCOPE)
+endfunction()
+
+function(z_vcpkg_download_from_gitlab data_prefix)
+  set(opts "")
+  if(${data_prefix}.host)
+    list(APPEND opts GITLAB_URL "${${data_prefix}.host}")
+  endif()
+
+  vcpkg_from_gitlab(
+    OUT_SOURCE_PATH "${${data_prefix}.output_variable}"
+    REPO "${${data_prefix}.repository}"
+    REF "${${data_prefix}.ref}"
+    SHA512 "${${data_prefix}.sha512}"
+    HEAD_REF "${${data_prefix}.head_ref}"
+    PATCHES ${${data_prefix}.patches}
+    ${opts}
+  )
+  set("${${data_prefix}.output_variable}" "${${${data_prefix}.output_variable}}" PARENT_SCOPE)
+endfunction()
+
+function(z_vcpkg_download_from_bitbucket data_prefix)
+  vcpkg_from_bitbucket(
+      OUT_SOURCE_PATH "${${data_prefix}.output_variable}"
+      REPO "${${data_prefix}.repository}"
+      REF "${${data_prefix}.ref}"
+      SHA512 "${${data_prefix}.sha512}"
+      HEAD_REF "${${data_prefix}.head_ref}"
+      PATCHES ${${data_prefix}.patches}
+  )
+  set("${${data_prefix}.output_variable}" "${${${data_prefix}.output_variable}}" PARENT_SCOPE)
+endfunction()
+
+function(z_vcpkg_download_from_git data_prefix)
+  vcpkg_from_git(
+    OUT_SOURCE_PATH "${${data_prefix}.output_variable}"
+    URL "${${data_prefix}.url}"
+    REF "${${data_prefix}.ref}"
+    #SHA512 "${${data_prefix}.sha512}"
+    HEAD_REF "${${data_prefix}.head_ref}"
+    PATCHES ${${data_prefix}.patches}
+    # [LFS [<url>]]
+  )
+  set("${${data_prefix}.output_variable}" "${${${data_prefix}.output_variable}}" PARENT_SCOPE)
+endfunction()
+
+function(z_vcpkg_download_from_sourceforge data_prefix)
+  vcpkg_from_sourceforge(
+      OUT_SOURCE_PATH "${${data_prefix}.output_variable}"
+      REPO "${${data_prefix}.repository}"
+      REF "${${data_prefix}.ref}"
+      SHA512 "${${data_prefix}.sha512}"
+      #HEAD_REF "${${data_prefix}.head_ref}"
+      PATCHES ${${data_prefix}.patches}
+      FILENAME "${${data_prefix}.filename}"
+      #[NO_REMOVE_ONE_LEVEL]
+  )
+  set("${${data_prefix}.output_variable}" "${${${data_prefix}.output_variable}}" PARENT_SCOPE)
+endfunction()
+
+function(z_vcpkg_download_from_file data_prefix)
+endfunction()
+
+##### JSON Parser
+
+function(z_read_json_array out_vars json_data)
+  set(out "")
+  set(argn_list ${ARGN})
+  list(JOIN argn_list "." argn_list_dot)
+
+  string(JSON ${argn_list_dot}_length LENGTH "${json_data}" ${ARGN})
+  set(${argn_list_dot}.size "${${argn_list_dot}_length}")
+  list(APPEND out ${argn_list_dot}.size)
+  set(${argn_list_dot}.index_list "")
+  list(APPEND out ${argn_list_dot}.index_list)
+
+  math(EXPR ${argn_list_dot}_length "${${argn_list_dot}_length}-1" OUTPUT_FORMAT DECIMAL)
+
+  if(NOT ${argn_list_dot}_length LESS 0)
+    foreach(${argn_list_dot}_index RANGE ${${argn_list_dot}_length})
+      set(out_index "")
+      z_read_json_member(out_index "${json_data}" ${ARGN} "${${argn_list_dot}_index}")
+      list(APPEND out ${out_index})
+      list(APPEND ${argn_list_dot}.index_list "${${argn_list_dot}_index}")
+      unset(out_index)
+    endforeach()
+  endif()
+
+  foreach(o IN LISTS out)
+    set("${o}" "${${o}}" PARENT_SCOPE)
+  endforeach()
+  set("${out_vars}" ${out} PARENT_SCOPE)
+endfunction()
+
+function(z_read_json_obj out_vars json_data)
+  set(out "")
+  set(argn_list ${ARGN})
+  list(JOIN argn_list "." argn_list_dot)
+
+  string(JSON ${argn_list_dot}_length LENGTH "${json_data}" ${ARGN})
+  set(${argn_list_dot}.size "${${argn_list_dot}_length}")
+  list(APPEND out ${argn_list_dot}.size)
+  set(${argn_list_dot}.members "")
+  list(APPEND out ${argn_list_dot}.members)
+
+  math(EXPR ${argn_list_dot}_length "${${argn_list_dot}_length}-1" OUTPUT_FORMAT DECIMAL)
+
+  if(NOT ${argn_list_dot}_length LESS 0)
+    foreach(${argn_list_dot}_index RANGE ${${argn_list_dot}_length})
+      set(out_index "")
+      string(JSON member_name MEMBER "${json_data}" ${ARGN} "${${argn_list_dot}_index}")
+      z_read_json_member(out_index "${json_data}" ${ARGN} "${member_name}")
+      list(APPEND out ${out_index})
+      list(APPEND ${argn_list_dot}.members "${member_name}")
+      unset(out_index)
+      unset(member_name)
+    endforeach()
+  endif()
+  foreach(o IN LISTS out)
+    set("${o}" "${${o}}" PARENT_SCOPE)
+  endforeach()
+  set("${out_vars}" ${out} PARENT_SCOPE)
+endfunction()
+
+function(z_read_json_member out_vars json_data)
+  set(out "")
+  set(argn_list ${ARGN})
+  message(STATUS "${ARGN}")
+  list(JOIN argn_list "." argn_list_dot)
+  string(JSON "z_${argn_list_dot}.json_type" TYPE "${json_data}" ${ARGN})
+
+  if("${z_${argn_list_dot}.json_type}" MATCHES "^(NULL|NUMBER|STRING|BOOLEAN)$")
+    string(JSON "${argn_list_dot}" GET "${json_data}" ${ARGN})
+    # Cannot expand CMake variables here if you want output variables to be used in the same JSON file
+    # string(CONFIGURE "${${argn_list_dot}}" "${argn_list_dot}" ESCAPE_QUOTES) 
+    list(APPEND out "${argn_list_dot}")
+  elseif("${z_${argn_list_dot}.json_type}" STREQUAL "ARRAY")
+    set(array_out "")
+    z_read_json_array(array_out "${json_data}" ${ARGN})
+    list(APPEND out ${array_out})
+    unset(array_out)
+  elseif("${z_${argn_list_dot}.json_type}" STREQUAL "OBJECT")
+    set(object_out "")
+    z_read_json_obj(object_out "${json_data}" ${ARGN})
+    list(APPEND out ${object_out})
+    unset(object_out)
+  else()
+    message(FATAL_ERROR "Unknown json type found")
+  endif()
+  
+  foreach(o IN LISTS out)
+    set("${o}" "${${o}}" PARENT_SCOPE)
+  endforeach()
+  set("${out_vars}" ${out} PARENT_SCOPE)
+endfunction()
