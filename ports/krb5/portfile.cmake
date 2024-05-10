566ce0ab4d2a15f7c3593879dc93e6550d39d5bd
Sharadh Rajaraman <sharadh@cuno.io>
2024-05-10 18:49:39 +0100

    Update krb5 - install binaries to correct locations - Don't remove compile_et and krb5-config - Use GitHub for source

--- ports/krb5/portfile.cmake
+++ ports/krb5/portfile.cmake
@@ -1,37 +1,95 @@
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://kerberos.org/dist/krb5/1.21/krb5-${VERSION}.tar.gz"
-    FILENAME "krb5-${VERSION}.tar.gz"
-    SHA512 4e09296b412383d53872661718dbfaa90201e0d85f69db48e57a8d4bd73c95a90c7ec7b6f0f325f6bc967f8d203b256b071c0191facf080aca0e2caec5d0ac49
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO krb5/krb5
+	REF "krb5-1.21.2-final"
+	SHA512 184ef8645d7e17f30a8e3d4005364424d2095b3d0c96f26ecef0c2dd2f3a096a0dd40558ed113121483717e44f6af41e71be0e5e079c76a205535d0c11a2ea34
+	PATCHES relative_paths.patch
 )
 
-vcpkg_extract_source_archive(
-    SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-)
+if ("${VCPKG_TARGET_IS_OSX}")
+	# Required for recent macOS and static build: https://mailman.mit.edu/pipermail/krbdev/2024-January/013653.html
+	if ("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+		set(LD_FLAGS "-framework Kerberos")
+	endif ()
+	set(EXTRA_OPTIONS
+		"--disable-nls"
+		"--disable-silent-rules"
+		"--without-system-verto"
+		"--without-keyutils"
+	)
+endif ()
 
 vcpkg_configure_make(
-    SOURCE_PATH "${SOURCE_PATH}/src"
-    AUTOCONFIG
-    OPTIONS
-        "CFLAGS=-fcommon \$CFLAGS"
+	SOURCE_PATH "${SOURCE_PATH}"
+	PROJECT_SUBPATH src
+	AUTOCONFIG
+	NO_ADDITIONAL_PATHS
+	DETERMINE_BUILD_TRIPLET
+	OPTIONS
+		--disable-rpath
+		"CFLAGS=-fcommon \$CFLAGS"
+		"LDFLAGS=${LD_FLAGS}"
+    ${EXTRA_OPTIONS}
 )
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/krb5/cat1")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/krb5/cat5")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/krb5/cat7")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/krb5/cat8")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/krb5/")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/krb5/")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/var")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/var")
+vcpkg_copy_tools(
+	SEARCH_DIR "${CURRENT_PACKAGES_DIR}/bin"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin"
+	TOOL_NAMES
+		gss-client
+		kadmin
+		kdestroy
+		kinit
+		klist
+		kpasswd
+		kswitch
+		ktutil
+		kvno
+		sclient
+		sim_client
+		uuclient
+		compile_et
+		k5srvutil
+		krb5-config
+	AUTO_CLEAN
+)
 
-# remove due to absolute path error
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/krb5/bin/compile_et")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/krb5/bin/krb5-config")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools/krb5/debug/")
+vcpkg_copy_tools(
+	SEARCH_DIR "${CURRENT_PACKAGES_DIR}/sbin"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/sbin"
+	TOOL_NAMES
+		gss-server
+		kadmind
+		kdb5_util
+		kprop
+		kpropd
+		kproplog
+		krb5kdc
+		sim_server
+		sserver
+		uuserver
+		kadmin.local
+		krb5-send-pr
+)
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/doc/copyright.rst")
+# Required because AUTO_CLEAN doesn't work with sbin
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/sbin")
+
+# Empty directories
+file(REMOVE_RECURSE
+	"${CURRENT_PACKAGES_DIR}/var"
+	"${CURRENT_PACKAGES_DIR}/share/man/cat1"
+	"${CURRENT_PACKAGES_DIR}/share/man/cat5"
+	"${CURRENT_PACKAGES_DIR}/share/man/cat7"
+	"${CURRENT_PACKAGES_DIR}/share/man/cat8"
+)
+# Also empty when static
+if("${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
+	file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/krb5")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/NOTICE")
