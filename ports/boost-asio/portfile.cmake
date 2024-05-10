cbf4a6641528cee6f172328984576f51698de726
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-05-10 05:29:06 +0200

    [Boost] Update to 1.85 (#38523)
    closes #38201
    supersedes #38237
    
    Lets see if I forgot to cherry pick something.
    
    ---------
    
    Co-authored-by: Yury Bura <yurybura@gmail.com>

--- ports/boost-asio/portfile.cmake
+++ ports/boost-asio/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO boostorg/asio
     REF boost-${VERSION}
-    SHA512 88d1d8d1bbea29149d9747ab9ccb99a7bfbd928f8ed1aff90adb6382fcefd32dc9687e6832f8ac21ddf918f0608623aa123d7943aa5f697a6b946fe2311e05f6
+    SHA512 2b5072ab187ac03a0addf78b62f15d31a6d7e59336784611d1db78359c287417d8c285f569f2f45c70dd66a14b2ea69880c8f2cbec6f157db119e94693e0f555
     HEAD_REF master
     PATCHES
         windows_alloca_header.patch
