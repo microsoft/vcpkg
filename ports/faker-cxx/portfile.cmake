29e252fcc5202527a093f118de08852fb2cbdc20
Christian Göhring <cg@2601.me>
2024-05-14 22:13:26 +0200

    vcpkg_cmake_config_fixup clean, patch files references without quotes, only static lib on win only

--- ports/faker-cxx/portfile.cmake
+++ ports/faker-cxx/portfile.cmake
@@ -1,4 +1,6 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,9 +9,9 @@ vcpkg_from_github(
     SHA512 0ad3550d45df2adda70ad64fc1afffc2d39f6644e46029b6b3f0fd42eed071b55daeee9da7456b8b75e1da566bd0d1605518722b2831dba31bf9787506ecfa9d
     HEAD_REF master
     PATCHES
-        "CMakeLists.txt.patch"
-        "FormatHelper.h.patch"
-        "Helper.h.patch"
+        CMakeLists.txt.patch
+        FormatHelper.h.patch
+        Helper.h.patch
 )
 
 vcpkg_cmake_configure(
@@ -20,10 +22,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(
-    PACKAGE_NAME "faker-cxx"
-    CONFIG_PATH lib/cmake/faker-cxx
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/faker-cxx)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
