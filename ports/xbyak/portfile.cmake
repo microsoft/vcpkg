aa10d0b0e1dfd42d2a500fbedab6595d0db188cd
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-05-15 00:23:22 -0600

    change version-string to version, regex to format

--- ports/xbyak/portfile.cmake
+++ ports/xbyak/portfile.cmake
@@ -1,7 +1,8 @@
+string(REGEX REPLACE "^([0-9]+)[.]([1-9])\$" "\\1.0\\2" VERSION_STR "${VERSION}")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO herumi/xbyak
-    REF "v${VERSION}"
+    REF "v${VERSION_STR}"
     SHA512 42b8338a1bf565ad0359542748f2e93df36aae567fcd15236b212b70e64b5e1d417f88c8a617935a4acc1ae048592522da221cb0d890eb0efc4f9a808366d7f3
     HEAD_REF master
 )
