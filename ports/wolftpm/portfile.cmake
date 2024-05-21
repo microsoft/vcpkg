f25e8c75cdc2286dfc26e023598c578ed9777704
Lealem Amedie <lealemta47@gmail.com>
2024-05-21 15:48:56 -0700

    Remove outdated patch

--- ports/wolftpm/portfile.cmake
+++ ports/wolftpm/portfile.cmake
@@ -5,7 +5,6 @@ vcpkg_from_github(
     SHA512 8cd63e0b62a2a72838c4ffabe00802d1297989ffbe3fbf20fc523160fc463ab6653ecab4aafb784a3f32d46a5efc71479e29dc1ca2fe1362573deb5b43525773
     HEAD_REF master
     PATCHES
-      cmake-patch.diff
     )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
