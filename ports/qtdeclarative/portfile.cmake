1e184e64c266003025e26e14e3f4689c40266547
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-05-21 18:12:11 +0200

    remove patch in qtdeclarative

--- ports/qtdeclarative/portfile.cmake
+++ ports/qtdeclarative/portfile.cmake
@@ -3,9 +3,7 @@ include("${SCRIPT_PATH}/qt_install_submodule.cmake")
 
 vcpkg_buildpath_length_warning(44)
 
-set(${PORT}_PATCHES
-  cb6b62f.diff
-)
+set(${PORT}_PATCHES "")
 
  set(TOOL_NAMES 
         qml
