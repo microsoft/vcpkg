004df0eb4d310f9d37ab44b1a30d7cd19bf3ab9e
Lealem Amedie <lealemta47@gmail.com>
2024-05-22 07:55:19 -0700

    Address reviewer feedback

--- ports/wolftpm/portfile.cmake
+++ ports/wolftpm/portfile.cmake
@@ -4,7 +4,6 @@ vcpkg_from_github(
     REF v3.2.0
     SHA512 8cd63e0b62a2a72838c4ffabe00802d1297989ffbe3fbf20fc523160fc463ab6653ecab4aafb784a3f32d46a5efc71479e29dc1ca2fe1362573deb5b43525773
     HEAD_REF master
-    PATCHES
     )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
