6fe2d757fbcd9f0accc0731b7f48bdb51e1435c8
lealem47 <60322859+lealem47@users.noreply.github.com>
2024-05-22 09:36:05 -0600

    Update ports/wolfmqtt/portfile.cmake
    Co-authored-by: WangWeiLin-MV <156736127+WangWeiLin-MV@users.noreply.github.com>

--- ports/wolfmqtt/portfile.cmake
+++ ports/wolfmqtt/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wolfssl/wolfmqtt
-    REF v1.19.0
+    REF "v${VERSION}"
     SHA512 354ab619144bd29258e65bd06219dddb4c89d5709c3246d6968239ab29426fb3882f2d5acad866cc431f6164c5e41b7da3a24ab7923fabc27deafd2801b92580
     HEAD_REF master
     )
