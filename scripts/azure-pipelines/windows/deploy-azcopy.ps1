b4934aff66b41288c7df34a991cf107cef7c106c
Billy Robert O'Neal III <bion@microsoft.com>
2024-05-24 19:11:13 -0700

    Deploy cudnn with delegated-SAS rather than azcopy.

--- scripts/azure-pipelines/windows/deploy-azcopy.ps1
+++
@@ -1,4 +0,0 @@
-$azcopyZipPath = "$PSScriptRoot\azcopyv10.zip"
-& curl.exe -L -o $azcopyZipPath 'https://azcopyvnext.azureedge.net/releases/release-10.24.0-20240326/azcopy_windows_amd64_10.24.0.zip'
-Expand-Archive -LiteralPath $azcopyZipPath -DestinationPath $env:PROGRAMFILES
-Remove-Item -LiteralPath $azcopyZipPath -Force
