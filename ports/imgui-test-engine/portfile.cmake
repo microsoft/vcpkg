6013d616be2ba2c3de64db1e996d46c6f8bd628a
RT2 <contact@rt2.fr>
2024-05-27 00:58:32 +0200

    [imgui-test-engine] Add std-function feature

--- ports/imgui-test-engine/portfile.cmake
+++ ports/imgui-test-engine/portfile.cmake
@@ -13,7 +13,8 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES 
-    implot IMGUI_TEST_ENGINE_IMPLOT
+        implot       IMGUI_TEST_ENGINE_IMPLOT
+        std-function IMGUI_TEST_ENGINE_STD_FUNCTION
 )
 
 file(REMOVE_RECURSE "${SOURCE_PATH}/imgui_test_engine/thirdparty/stb")
@@ -33,6 +34,10 @@ vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imgui_te_imconfig.h" "#def
 
 if ("implot" IN_LIST FEATURES)
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imgui_te_imconfig.h" "#define IMGUI_TEST_ENGINE_ENABLE_IMPLOT 0" "#define IMGUI_TEST_ENGINE_ENABLE_IMPLOT 1")
+endif()
+if ("std-function" IN_LIST FEATURES)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imgui_te_imconfig.h" "#define IMGUI_TEST_ENGINE_ENABLE_STD_FUNCTION 0" "#define IMGUI_TEST_ENGINE_ENABLE_STD_FUNCTION 1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/imgui_te_engine.h" "#if IMGUI_TEST_ENGINE_ENABLE_STD_FUNCTION" "#if 1")
 endif()
 
 vcpkg_copy_pdbs()
