939f2211bb1db3f23c42f53c169796145c643268
Cheney-Wang <850426846@qq.com>
2024-05-29 00:32:07 -0700

    Fixup config file

--- ports/vulkan-memory-allocator/portfile.cmake
+++ ports/vulkan-memory-allocator/portfile.cmake
@@ -18,5 +18,7 @@ vcpkg_cmake_configure(
 
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME VulkanMemoryAllocator CONFIG_PATH "share/cmake/VulkanMemoryAllocator")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
