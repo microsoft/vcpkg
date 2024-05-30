dd797e6993ffd708552fd68d50695398e71b058b
Billy Robert O'Neal III <bion@microsoft.com>
2024-05-29 18:38:47 -0700

    Add missing copy of clt.dmg into the guest.

--- scripts/azure-pipelines/osx/setup-guest.sh
+++ scripts/azure-pipelines/osx/setup-guest.sh
@@ -15,6 +15,7 @@ echo Type 'vcpkg' and press enter
 ssh-copy-id -i "$SSH_PUBLIC_KEY" $SSH_COOKIE
 echo Keys deployed
 ssh $SSH_COOKIE -i "$SSH_KEY" echo hello from \`hostname\`
+scp -i "$SSH_KEY" ./clt.dmg $SSH_COOKIE:/Users/vcpkg/clt.dmg
 scp -i "$SSH_KEY" ./setup-box.sh $SSH_COOKIE:/Users/vcpkg/setup-box.sh
 ssh $SSH_COOKIE -i "$SSH_KEY" chmod +x /Users/vcpkg/setup-box.sh
 ssh $SSH_COOKIE -i "$SSH_KEY" /Users/vcpkg/setup-box.sh
