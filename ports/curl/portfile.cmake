8379cad977b989908bfe6329377c41d4ee18f9d6
Tal Regev <tal.regev@gmail.com>
2024-05-30 23:55:52 +0300

    Add rtmp feature to curl

--- ports/curl/portfile.cmake
+++ ports/curl/portfile.cmake
@@ -38,6 +38,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         zstd        CURL_ZSTD
         psl         CURL_USE_LIBPSL
         gssapi      CURL_USE_GSSAPI
+        rtmp        USE_LIBRTMP
     INVERTED_FEATURES
         ldap        CURL_DISABLE_LDAP
         ldap        CURL_DISABLE_LDAPS
