a2cb46d881f66cbeafc30b0dd33989331aed9b6c
Kai Pastor <dg0yt@darc.de>
2024-05-30 09:04:48 +0200

    [bgfx] Code cleanup

--- ports/bgfx/portfile.cmake
+++ ports/bgfx/portfile.cmake
@@ -1,3 +1,7 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+  vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 vcpkg_download_distfile(
   ARCHIVE_FILE
   URLS https://github.com/bkaradzic/bgfx.cmake/releases/download/v${VERSION}/bgfx.cmake.v${VERSION}.tar.gz
@@ -7,7 +11,7 @@ vcpkg_download_distfile(
 
 vcpkg_extract_source_archive(
   SOURCE_PATH
-  ARCHIVE ${ARCHIVE_FILE}
+  ARCHIVE "${ARCHIVE_FILE}"
 )
 
 vcpkg_check_features(
@@ -17,23 +21,11 @@ vcpkg_check_features(
     multithreaded BGFX_CONFIG_MULTITHREADED
 )
 
-if (TARGET_TRIPLET MATCHES "(windows|uwp)")
-  # bgfx doesn't apply __declspec(dllexport) which prevents dynamic linking
-  set(BGFX_LIBRARY_TYPE "STATIC")
-elseif (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
   set(BGFX_LIBRARY_TYPE "SHARED")
 else ()
   set(BGFX_LIBRARY_TYPE "STATIC")
 endif ()
-
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-inject-packages.cmake" DESTINATION "${SOURCE_PATH}")
-
-# It's important to have `${CMAKE_CURRENT_LIST_DIR}` verbatim escaped in bgfxConfig.cmake
-if(WIN32)
-  set(BGFX_ADDITIONAL_TOOL_PATHS "\$\$\{CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/bgfx \$\$\{CMAKE_CURRENT_LIST_DIR}/../../../bgfx_${HOST_TRIPLET}/tools/bgfx")
-else()
-  set(BGFX_ADDITIONAL_TOOL_PATHS "\"\\\$\$\{CMAKE_CURRENT_LIST_DIR}/../../../${HOST_TRIPLET}/tools/bgfx \\\$\$\{CMAKE_CURRENT_LIST_DIR}/../../../bgfx_${HOST_TRIPLET}/tools/bgfx\"")
-endif()
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
@@ -43,9 +35,8 @@ vcpkg_cmake_configure(
     -DBGFX_AMALGAMATED=ON
     -DBGFX_BUILD_EXAMPLES=OFF
     -DBGFX_OPENGLES_VERSION=30
-    -DBGFX_CMAKE_USER_SCRIPT=vcpkg-inject-packages.cmake
-    # #25529: Need to inject an extra path because VCPKG_HOST_TARGET is not determined automatically
-    -DBGFX_ADDITIONAL_TOOL_PATHS=${BGFX_ADDITIONAL_TOOL_PATHS}
+    "-DBGFX_CMAKE_USER_SCRIPT=${CURRENT_PORT_DIR}/vcpkg-inject-packages.cmake"
+    "-DBGFX_ADDITIONAL_TOOL_PATHS=${CURRENT_INSTALLED_DIR}/../${HOST_TRIPLET}/tools/bgfx"
     ${FEATURE_OPTIONS}
 )
 
@@ -57,19 +48,10 @@ if ("tools" IN_LIST FEATURES)
   vcpkg_copy_tools(TOOL_NAMES bin2c shaderc geometryc geometryv texturec texturev AUTO_CLEAN)
 endif ()
 
-# Handle copyright
-file(
-  INSTALL "${CURRENT_PACKAGES_DIR}/share/licences/${PORT}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright
-)
+vcpkg_install_copyright(FILE_LIST "${CURRENT_PACKAGES_DIR}/share/licences/${PORT}/LICENSE")
 
 file(REMOVE_RECURSE
   "${CURRENT_PACKAGES_DIR}/share/licences"
   "${CURRENT_PACKAGES_DIR}/debug/include"
   "${CURRENT_PACKAGES_DIR}/debug/share"
 )
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static" OR NOT VCPKG_TARGET_IS_WINDOWS)
-  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
