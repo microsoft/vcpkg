1416a341608f18ee357a2e245addc593832ea438
Kai Pastor <dg0yt@darc.de>
2024-06-01 10:54:24 +0200

    [fdk-aac] Export C++ linkage to pc files

--- ports/fdk-aac/portfile.cmake
+++ ports/fdk-aac/portfile.cmake
@@ -11,6 +11,8 @@ if(HE_AAC)
         SHA512 616207e85035d1659a2b7808ca6ec02ef53c1c4b39eb280fe861f82a4cf548e5db2ac381c496bad37dfc2b8c6677fe704d9fd8449e43d1f93d3e636239e0191b
         HEAD_REF master
         OUT_SOURCE_PATH SOURCE_PATH
+        PATCHES
+            cxx-linkage-pkgconfig.patch
     )
 else()
     list(PREPEND legal_notes "README.fedora")
@@ -21,6 +23,8 @@ else()
         HEAD_REF stripped4
         SHA512 0c37f8fd1bd0e817d2b3970138bef5b2a7a3150ab1a772273c8f5cba09be04afa2f31780f0ea063dd786a71844aa4cb5821349a4bcc5ebe70e827c3561eda2a9
         OUT_SOURCE_PATH SOURCE_PATH
+        PATCHES
+            cxx-linkage-pkgconfig.patch
     )
 endif()
 
