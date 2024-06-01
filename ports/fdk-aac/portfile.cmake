0c8b39923255b9e295722ee2b9744828df93cb6c
Kai Pastor <dg0yt@darc.de>
2024-06-01 10:47:45 +0200

    [fdk-aac] Update legal notes

--- ports/fdk-aac/portfile.cmake
+++ ports/fdk-aac/portfile.cmake
@@ -3,6 +3,7 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS FEATURES
         he-aac HE_AAC
 )
 
+set(legal_notes "NOTICE")
 if(HE_AAC)
     vcpkg_from_github(
         REPO mstorsjo/fdk-aac
@@ -12,12 +13,13 @@ if(HE_AAC)
         OUT_SOURCE_PATH SOURCE_PATH
     )
 else()
+    list(PREPEND legal_notes "README.fedora")
     vcpkg_from_gitlab(
         GITLAB_URL https://gitlab.freedesktop.org/
         REPO wtaymans/fdk-aac-stripped
-        REF 585981a49f2186b0d2e47c64bf6b5abf539395f8 # corresponds to v2.0.2 tag in mstorsjo/fdk-aac GitHub repository
+        REF 529b87452cd33d45e1d0a5066d20b64f10b38845 # corresponds to v2.0.2 tag in mstorsjo/fdk-aac GitHub repository
         HEAD_REF stripped4
-        SHA512 e0e56396ed0be427302ed4b54fc6e8dc522a172c288b7c1ec40cc3a9ceb13518ca7bbb874bc71b88b2a91e0bbbe4ad0bab6910efa1db63d91e6370976641bac4
+        SHA512 0c37f8fd1bd0e817d2b3970138bef5b2a7a3150ab1a772273c8f5cba09be04afa2f31780f0ea063dd786a71844aa4cb5821349a4bcc5ebe70e827c3561eda2a9
         OUT_SOURCE_PATH SOURCE_PATH
     )
 endif()
@@ -35,4 +37,5 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/NOTICE")
+list(TRANSFORM legal_notes PREPEND "${SOURCE_PATH}/")
+vcpkg_install_copyright(FILE_LIST ${legal_notes})
