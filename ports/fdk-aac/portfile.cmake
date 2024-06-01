ed1898948483f478a296264f2998d57a8cedaf9b
Kai Pastor <dg0yt@darc.de>
2024-06-01 10:43:02 +0200

    [fdk-aac] Code cleanup

--- ports/fdk-aac/portfile.cmake
+++ ports/fdk-aac/portfile.cmake
@@ -23,14 +23,16 @@ else()
 endif()
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS_RELEASE -DBUILD_PROGRAMS=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_PROGRAMS=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
-vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/NOTICE")
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/NOTICE")
