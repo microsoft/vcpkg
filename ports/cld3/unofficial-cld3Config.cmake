89660d45187db8a01e979efca356fbbcbdf6acf4
Dennis Hezel <dh@3yourmind.com>
2024-06-05 15:43:01 +0200

    protobuf: Include CONFIG in vcpkg-cmake-wrapper

--- ports/cld3/unofficial-cld3Config.cmake
+++ ports/cld3/unofficial-cld3Config.cmake
@@ -1,4 +1,4 @@
 include(CMakeFindDependencyMacro)
-find_dependency(Protobuf CONFIG)
+find_dependency(Protobuf)
  
 include("${CMAKE_CURRENT_LIST_DIR}/unofficial-cld3-targets.cmake")
--- ports/gamenetworkingsockets/portfile.cmake
+++ ports/gamenetworkingsockets/portfile.cmake
@@ -20,7 +20,6 @@ vcpkg_cmake_configure(
         -DBUILD_TOOLS=OFF
         -DUSE_CRYPTO=${CRYPTO_BACKEND}
         -DUSE_CRYPTO25519=${CRYPTO_BACKEND}
-        -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=on
 )
 
 vcpkg_cmake_install()
