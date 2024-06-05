23517226e7693bc54f5231dff13e08818afac98d
Petr Kobalicek <kobalicek.petr@gmail.com>
2024-06-05 09:03:00 +0200

    [asmjit] Update to 2024-06-05

--- ports/asmjit/portfile.cmake
+++ ports/asmjit/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO asmjit/asmjit
-    REF 51b10b19b6631434d3f9ad536a6fb140944a36d2 # commited on 2023-03-25
-    SHA512 1fba5159d2adad64e9a2b07a1f90de6988d1da47b9802ca8b57c61a89d8a90924525f6d0d6607279994bdbadcf693b2cc96cd7e4bf7f018ad64127b640dc38fb
+    REF 4a61c23ab65a8feb58d1bcdb7cbe8c8bdc36b7d1 # commited on 2024-06-05
+    SHA512 ccc04655d031bd5a89d217aac85f6a8bdb756684257877de8df2162b2ec6e0d95cd7e01c0d46f817465fccb0fa52a4552d5034c48f361f2854f2e05aa32b7c89
     HEAD_REF master
 )
 
@@ -16,16 +16,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/asmjit)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/asmjit/core/api-config.h"
-        "#if !defined(ASMJIT_STATIC)"
-        "#if 0"
-    )
-endif()
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
