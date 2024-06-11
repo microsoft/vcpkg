026e28830718c078ff442130ac12afbbdcfed61e
Alfonso Abella <aag@mega.co.nz>
2024-06-11 14:25:16 +0200

    [Readline-unix] Add find module for unix

--- ports/readline-unix/portfile.cmake
+++ ports/readline-unix/portfile.cmake
@@ -31,4 +31,7 @@ file(REMOVE_RECURSE
 
 vcpkg_fixup_pkgconfig()
 
+configure_file("${CMAKE_CURRENT_LIST_DIR}/Findunofficial-readline-unix.cmake.in" "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}/Findunofficial-readline-unix.cmake" @ONLY)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-${PORT}")
+
 file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
