2cc1232353c9c9b95b51ac6bd8722520144283b6
Billy Robert O'Neal III <bion@microsoft.com>
2024-06-12 16:36:43 -0700

    Rename to michaelmiller-sec21

--- ports/michaelmiller-sec21/portfile.cmake
+++ ports/michaelmiller-sec21/portfile.cmake
@@ -1,16 +1,16 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO MichaelMiller-/sec21
-        REF "v${VERSION}"
-        SHA512 b9aab9d305e435db066f413a8e49d5622e83dc02d8e0ff0150a0edabf1b1dfd286f7aa5a3593b34c6f616fdd79caa3edbfeb09ff3b66aab80c0138aa2ca5950c
-        HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO MichaelMiller-/sec21
+    REF "v${VERSION}"
+    SHA512 b9aab9d305e435db066f413a8e49d5622e83dc02d8e0ff0150a0edabf1b1dfd286f7aa5a3593b34c6f616fdd79caa3edbfeb09ff3b66aab80c0138aa2ca5950c
+    HEAD_REF master
 )
 
 vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS -DBUILD_TESTING=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
