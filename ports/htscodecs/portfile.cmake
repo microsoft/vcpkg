938a3afd927962db53bb964949fc3ab13a5c36a4
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2024-06-13 02:18:10 +0300

    Use a simpler patch for disabling tests in htscodecs.

--- ports/htscodecs/portfile.cmake
+++ ports/htscodecs/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 7533570b8b1cad0b9ed118170e5a9ff34fdde40b090f4ba3756f7899e2cd7230f8172425ecf6bd7b83b0b0b1a2a24f3d21795db7f0bc2c3add0a55342b970d1a
     HEAD_REF master
     PATCHES
-        0001-no-tests.patch
+        0001-no-tests.patch # https://github.com/samtools/htscodecs/pull/120
 )
 
 set(FEATURE_OPTIONS)
