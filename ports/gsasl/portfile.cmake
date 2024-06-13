51c92be57681edcbbf540d4ab1a2660bf2775093
Tal Regev <tal.regev@gmail.com>
2024-06-13 23:11:29 +0300

    disable nls and gssapi

--- ports/gsasl/portfile.cmake
+++ ports/gsasl/portfile.cmake
@@ -1,5 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnu.org/gnu/gsasl/gsasl-${VERSION}.tar.gz"
+    URLS 
+        "https://ftp.gnu.org/gnu/gsasl/gsasl-${VERSION}.tar.gz"
+        "https://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gsasl/gsasl-${VERSION}.tar.gz"
     FILENAME "gsasl-${VERSION}.tar.gz"
     SHA512 161b8a315862a79807ba067c5ae840175b0d8ec14806aceafc3f92d571713b94d1b8c1a5b188c47bf94a79b9a1f133065f96b087baa5e7f360ae7fb8336381ab
 )
@@ -8,22 +10,20 @@ vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     PATCHES
-        remove-testes-examples-docs.patch
+        remove-tests-examples-docs.patch
 )
-
-if(VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_ANDROID)
-    # Add autopoint
-    vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/tools/gettext/bin")
-endif()
 
 if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     vcpkg_replace_string("${SOURCE_PATH}/lib/src/gsasl.h" "defined GSASL_STATIC" "1")
 endif()
 
+set(ENV{AUTOPOINT} true)
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTOCONFIG
     OPTIONS
+        --disable-nls
+        --disable-gssapi
 )
 
 vcpkg_install_make()
