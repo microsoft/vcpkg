a792320a15069071f6cb8ae58d2d1850116646f7
Julien Jerphanion <git@jjerphan.xyz>
2024-06-13 09:45:57 +0200

    Update namespace and bump version
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>

--- ports/cpp-sparrow/portfile.cmake
+++ ports/cpp-sparrow/portfile.cmake
@@ -2,9 +2,9 @@
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO xtensor-stack/sparrow # 0.0.1
-    REF 799b3ce5173c7731d30faf167f3dbf6c6fcab6fe
-    SHA512 f9ed9b59c3510178c220716f5ef66b3112d9c79d026cfdcb05464023be6b4c4edb1d862287c9579d8eaf50a37dbb4f7cfc51d95ed077a9b8f5040c7c95fd0f63
+    REPO man-group/sparrow
+    REF 0.0.2
+    SHA512 388c7401c9168cb389f619f196a02f69e54934a66d06828d9d3cc5ea64c858f6512ccaca94309eebbeefbc57a9e864f0f203a187413e4d8ce688791e00d39757
     HEAD_REF main
 )
 
