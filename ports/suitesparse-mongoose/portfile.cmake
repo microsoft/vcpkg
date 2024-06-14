efa21a112bc3fc3c92f1046a83dfcba2d3a97a6a
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 13:57:27 +0300

    [SuiteSparse] add suitesparse-mongoose

---
+++ ports/suitesparse-mongoose/portfile.cmake
@@ -0,0 +1,31 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO DrTimothyAldenDavis/SuiteSparse
+    REF v7.7.0
+    SHA512 aa62dae81ae423ce7162ae83b46e5cf606d95482e6c6bb7ae6d61e15987761119d9418ef3a96648e6ba2327871a2847eef8ace197aa375279d71c80329d6f451
+    HEAD_REF dev
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/Mongoose"
+    OPTIONS
+        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+        -DSUITESPARSE_USE_CUDA=OFF
+        -DSUITESPARSE_USE_STRICT=ON
+        -DSUITESPARSE_USE_FORTRAN=OFF
+        -DSUITESPARSE_DEMOS=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_Python=ON  # Only used for tests
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME SuiteSparse_Mongoose
+    CONFIG_PATH lib/cmake/SuiteSparse_Mongoose
+)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Mongoose/Doc/License.txt")
