92dc8cc273fd93f22c6bdff7bc14a4bb0609abe4
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 13:30:46 +0300

    [SuiteSparse] add suitesparse-amd

--- ports/suitesparse-amd/portfile.cmake
+++ ports/suitesparse-amd/portfile.cmake
@@ -6,31 +6,20 @@ vcpkg_from_github(
     HEAD_REF dev
 )
 
-set(PACKAGE_NAME SuiteSparse_config)
+set(PACKAGE_NAME AMD)
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    openmp SUITESPARSE_USE_OPENMP
-)
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/${PACKAGE_NAME}"
     OPTIONS
         -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
-        -DSUITESPARSE_USE_CUDA=OFF  # not applicable here, skip check
-        -DSUITESPARSE_USE_STRICT=ON  # don't allow implicit dependencies
-        -DSUITESPARSE_USE_FORTRAN=OFF  # use Fortran sources translated to C instead
+        -DSUITESPARSE_USE_CUDA=OFF
+        -DSUITESPARSE_USE_STRICT=ON
+        -DSUITESPARSE_USE_FORTRAN=OFF
         -DSUITESPARSE_DEMOS=OFF
-        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
-
-# Move SuiteSparseBLAS.cmake, SuiteSparsePolicy.cmake etc files
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/cmake")
-file(RENAME "${CURRENT_PACKAGES_DIR}/lib/cmake/SuiteSparse" "${CURRENT_PACKAGES_DIR}/share/SuiteSparse")
 
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME ${PACKAGE_NAME}
@@ -40,4 +29,4 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/${PACKAGE_NAME}/Doc/License.txt")
