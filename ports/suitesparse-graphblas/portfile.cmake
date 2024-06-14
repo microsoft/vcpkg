17bc7512881fcdbfa33b7d4a55b74b3a2a105e53
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 14:28:25 +0300

    [SuiteSparse] add suitesparse-graphblas

---
+++ ports/suitesparse-graphblas/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO DrTimothyAldenDavis/GraphBLAS
+    REF v${VERSION}
+    SHA512 70bbf2331cdc013ef18456712a823e2f95e8f1773933f94bc55e5b7e1bc8a225bf56b21d9b66caabaf98ee909975820b0d899101289642c6e50253892c5af48e
+    HEAD_REF stable
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    compact GRAPHBLAS_COMPACT
+    openmp  GRAPHBLAS_USE_OPENMP
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+        -DSUITESPARSE_USE_CUDA=OFF
+        -DSUITESPARSE_USE_STRICT=ON
+        -DSUITESPARSE_USE_FORTRAN=OFF
+        -DSUITESPARSE_DEMOS=OFF
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME GraphBLAS
+    CONFIG_PATH lib/cmake/GraphBLAS
+)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
