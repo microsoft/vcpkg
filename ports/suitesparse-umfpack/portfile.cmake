ac4405d8bbbc8d7b51c9b01b94e1a28f3c813e79
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 15:38:20 +0300

    [SuiteSparse] add suitesparse-umfpack

---
+++ ports/suitesparse-umfpack/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO DrTimothyAldenDavis/SuiteSparse
+    REF v7.7.0
+    SHA512 aa62dae81ae423ce7162ae83b46e5cf606d95482e6c6bb7ae6d61e15987761119d9418ef3a96648e6ba2327871a2847eef8ace197aa375279d71c80329d6f451
+    HEAD_REF dev
+)
+
+set(PACKAGE_NAME UMFPACK)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    cholmod UMFPACK_USE_CHOLMOD
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/${PACKAGE_NAME}"
+    OPTIONS
+        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+        -DSUITESPARSE_USE_CUDA=OFF
+        -DSUITESPARSE_USE_STRICT=ON
+        -DSUITESPARSE_USE_FORTRAN=OFF
+        -DSUITESPARSE_DEMOS=OFF
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME ${PACKAGE_NAME}
+    CONFIG_PATH lib/cmake/${PACKAGE_NAME}
+)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/${PACKAGE_NAME}/Doc/License.txt")
