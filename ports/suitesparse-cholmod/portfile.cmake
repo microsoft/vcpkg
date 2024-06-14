6cc3b669cc06522d7bf17460c8c7db058ce8b44e
Martin Valgur <martin.valgur@gmail.com>
2024-06-14 15:24:49 +0300

    [SuiteSparse] add suitesparse-cholmod

---
+++ ports/suitesparse-cholmod/portfile.cmake
@@ -0,0 +1,57 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO DrTimothyAldenDavis/SuiteSparse
+    REF v7.7.0
+    SHA512 aa62dae81ae423ce7162ae83b46e5cf606d95482e6c6bb7ae6d61e15987761119d9418ef3a96648e6ba2327871a2847eef8ace197aa375279d71c80329d6f451
+    HEAD_REF dev
+)
+
+set(PACKAGE_NAME CHOLMOD)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+    matrixops  CHOLMOD_MATRIXOPS
+    modify     CHOLMOD_MODIFY
+    partition  CHOLMOD_PARTITION
+    supernodal CHOLMOD_SUPERNODAL
+)
+
+set(CUDA_ENABLED OFF)
+if("cuda" IN_LIST FEATURES)
+    set(CUDA_ENABLED ON)
+    set(CUDA_ARCHITECTURES "native")
+endif()
+if("cuda-redist" IN_LIST FEATURES)
+    set(CUDA_ENABLED ON)
+    set(CUDA_ARCHITECTURES "all-major")
+endif()
+
+set(GPL_ENABLED OFF)
+if(CHOLMOD_MATRIXOPS OR CHOLMOD_MODIFY OR CHOLMOD_SUPERNODAL OR CUDA_ENABLED)
+    set(GPL_ENABLED ON)
+endif()
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/${PACKAGE_NAME}"
+    OPTIONS
+        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+        -DCHOLMOD_GPL=${GPL_ENABLED}
+        -DSUITESPARSE_USE_CUDA=${CUDA_ENABLED}
+        -DSUITESPARSE_USE_STRICT=ON
+        -DSUITESPARSE_USE_FORTRAN=OFF
+        -DSUITESPARSE_DEMOS=OFF
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME ${PACKAGE_NAME}
+    CONFIG_PATH lib/cmake/${PACKAGE_NAME}
+)
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/${PACKAGE_NAME}/Doc/License.txt")
