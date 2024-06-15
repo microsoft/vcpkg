911bdfc6c57da37c5897758f7492167a2650fb99
quqiOnfree <quqiOnfree@outlook.com>
2024-06-16 02:12:42 +0800

    fix build problems in x86-windows and x64-windows

--- ports/mariadb-connector-cpp/portfile.cmake
+++ ports/mariadb-connector-cpp/portfile.cmake
@@ -5,7 +5,17 @@ vcpkg_from_github(
     HEAD_REF master
     SHA512 efc0d7866b76b1baa20ab6bbbeb45825ca5e3d8b460e09805343f0e6b638bb0cfcd812d01bd183c5c0077eece5f1bdd5f207e753aa54b5ed218576b7cb37b241
     PATCHES
-        use-vcpkg.patch
+        0001-use-vcpkg.patch
+        0002-Add-install-command.patch
+        0003-modify-configure_file-to-share.patch
+        0004-modify-to-share2.patch
+        0005-remove-force-build-with-mt.patch
+        0006-add-include.patch
+        0007-add-include2.patch
+        0008-add-TARGET_INCLUDE_DIRECTORIES.patch
+        0009-add-include3.patch
+        0010-add-include4.patch
+        0011-add-include5.patch
 )
 
 vcpkg_cmake_configure(
@@ -13,7 +23,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-mariadb-connector-cpp)
+vcpkg_cmake_config_fixup(PACKAGE_NAME mariadbcpp)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
