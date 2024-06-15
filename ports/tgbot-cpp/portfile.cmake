e6bdb551b785c00aa89d93336d6e16bf45baffe9
llnulldisk <48621230+llnulldisk@users.noreply.github.com>
2024-06-15 21:25:05 +0200

    [tgbot-cpp] Update to v1.8

--- ports/tgbot-cpp/portfile.cmake
+++ ports/tgbot-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO reo7sp/tgbot-cpp
-  REF v1.7.3
-  SHA512 845a051a4c7d753680759a09c1d2e1384d81f399b84d553e6785c65a6249a6a770f17eaf57ca28efd420dda78dc6c78096f045ddd87ac98a56c2c54d0b3a110b
+  REF v1.8
+  SHA512 b43eaadefb7631e7c1619e0290fe92917536be632614c9e9f048119b4c9eb9cf4d72e2db1c4ffdaa1d6a43deedc66dfa73d8670ba119e2fa67266310c233bb32
   HEAD_REF master
 )
 
@@ -14,6 +14,7 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
