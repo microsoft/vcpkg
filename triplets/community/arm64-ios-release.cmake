879bb6bed3c44c7d7b5b7a491118647bcec6a25d
Pe13 <paolo.peforni@gmail.com>
2024-06-16 16:50:32 +0200

    arm64-ios-release: set the VCPKG_OSX_SYSROOT CMake variable so that ports can use it. Added a hint for the user on the variable VCPKG_OSX_DEPLOYMENT_TARGET

--- triplets/community/arm64-ios-release.cmake
+++ triplets/community/arm64-ios-release.cmake
@@ -2,5 +2,20 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME iOS)
+#set(VCPKG_OSX_DEPLOYMENT_TARGET 16.0) # uncomment to specify the min ios version you want to support
+                                       # (not all ports support this option)
+
+# setting the sysroot for cmake and for the env
+execute_process(
+        COMMAND /usr/bin/xcrun --sdk iphoneos --show-sdk-path
+        OUTPUT_VARIABLE sdk_path
+        ERROR_VARIABLE xcrun_error
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_STRIP_TRAILING_WHITESPACE
+)
+if (NOT sdk_path)
+    message(FATAL_ERROR "Can't determine iphoneos SDK path. Error: ${xcrun_error}")
+endif ()
+set(VCPKG_OSX_SYSROOT "${sdk_path}")
 
 set(VCPKG_BUILD_TYPE release)
