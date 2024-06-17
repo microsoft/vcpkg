9991fe0286a657e22b726694d356b40123e19969
Andrey Gladilin <skyogre@yandex.ru>
2024-06-17 18:11:23 +0300

    fix cppparser packaging

--- ports/cppparser/portfile.cmake
+++ ports/cppparser/portfile.cmake
@@ -1,26 +1,23 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO agladilin/cppparser
     REF "${VERSION}"
-    SHA512 6fea5a6f7a172b8e828382871ad1dcb102ab700d617a1d2b753b6e0bfef7639e0b40778fdceb196e6b0b1b5e75c9173a1cb1eb667906ec7471118b74c5dbfce1
+    SHA512 024d4cbe4e492aad23b1aba6576dd93a26d4b887f8d5792732a0ca98afec01134d90891e379a31f26df84a65d648b9187723b3dc57826294dfdeb108ae3d665d
     HEAD_REF master
 )
 
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "cppparser" CONFIG_PATH lib)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib)
 
 file(REMOVE_RECURSE
-    ${CURRENT_PACKAGES_DIR}/debug/include
+    "${CURRENT_PACKAGES_DIR}/debug/include"
 )
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
-
-#install(
-#    FILES ${config_version_file} ${install_config}
-#    DESTINATION "${CPPPARSER_INSTALL_CONFIG_DIR}"
-#)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
