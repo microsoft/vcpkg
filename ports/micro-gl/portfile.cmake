b8568c9ed71e075b53c06434ed08cc0c57c42372
Diamond Rivero <diamant3@proton.me>
2024-06-18 16:52:57 +0800

    update SHA512

--- ports/micro-gl/portfile.cmake
+++ ports/micro-gl/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO micro-gl/micro-gl
     REF 1c1dafeccb1b92467d3fd82de00e022a318c8ce8
-    SHA512 0
+    SHA512 57dd6d8bd633a79d5837c70572547e090c87bbec3e5e0d4f2914f0af11d683b200a452568a78964a3eaa271a6d316863a9a5f2a5f79ae7649e279904ec950962
     HEAD_REF master
 )
 
