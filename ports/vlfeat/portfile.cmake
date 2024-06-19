f5d75b64762ebc77db474355437804488a2607b0
Martin Valgur <martin.valgur@gmail.com>
2024-06-19 09:53:00 +0300

    [vlfeat] use vcpkg_install_copyright()

--- ports/vlfeat/portfile.cmake
+++ ports/vlfeat/portfile.cmake
@@ -36,4 +36,4 @@ vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
