0b274ef11e07be716300e7079acbaa370171ab03
c8ef <c8ef@outlook.com>
2024-06-19 13:47:42 +0800

    add libaio package

---
+++ ports/libaio/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_download_distfile(ARCHIVE
+	URLS "https://pagure.io/libaio/archive/libaio-${VERSION}/libaio-libaio-${VERSION}.tar.gz"
+    FILENAME "libaio-${VERSION}.tar.gz"
+    SHA512 8058c927de0b5f7079fc232d2be23272537694bf271488af1dc0330b58afc307931792ab138512c5e00aa3ea921935a6d862f575fb0cc2bf323de63d8df208cd
+)
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    PATCHES
+        cmake.patch
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
