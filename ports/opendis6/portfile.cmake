6455eda96a36cf90bb45d81e5f86b7e7ca7f93b0
Cameron Howell <crhowell3@gmail.com>
2024-06-19 13:00:50 -0500

    Addressed changes requested by moderators

--- ports/opendis6/portfile.cmake
+++ ports/opendis6/portfile.cmake
@@ -1,9 +1,7 @@
-if(VCPKG_TARGET_IS_WINDOWS)
-  vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
+  OPTIONS
+    -DINSTALL_INCLUDE_DIR=include
   REPO crhowell3/opendis6
   REF "${VERSION}"
   SHA512 f83e2dc8c3c06f420c88af35bc818561383760adb026548ce69f483568d87c1457dfbb3d25279d24bb2f08b8ff539f1ad3ed42db1963e1618f01b442d4ff791a
@@ -20,5 +18,5 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenDIS)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
