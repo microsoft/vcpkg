dbfd16095da5e2790774422a805a4756832c1fe6
c8ef <c8ef@outlook.com>
2024-06-20 10:32:13 +0800

    aio

--- ports/libaio/portfile.cmake
+++ ports/libaio/portfile.cmake
@@ -1,5 +1,5 @@
 vcpkg_download_distfile(ARCHIVE
-	URLS "https://pagure.io/libaio/archive/libaio-${VERSION}/libaio-libaio-${VERSION}.tar.gz"
+    URLS "https://pagure.io/libaio/archive/libaio-${VERSION}/libaio-libaio-${VERSION}.tar.gz"
     FILENAME "libaio-${VERSION}.tar.gz"
     SHA512 8058c927de0b5f7079fc232d2be23272537694bf271488af1dc0330b58afc307931792ab138512c5e00aa3ea921935a6d862f575fb0cc2bf323de63d8df208cd
 )
