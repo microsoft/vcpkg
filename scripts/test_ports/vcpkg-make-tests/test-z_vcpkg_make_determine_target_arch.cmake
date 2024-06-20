409d0650b233485018dfddd650ccba1352979a6c
Javier Matos <javiermatos@Javiers-Laptop.local>
2024-06-20 13:49:36 -0400

    use VCPKG_HOST_IS_OSX instead of APPLE

--- scripts/test_ports/vcpkg-make-tests/test-z_vcpkg_make_determine_target_arch.cmake
+++ scripts/test_ports/vcpkg-make-tests/test-z_vcpkg_make_determine_target_arch.cmake
@@ -8,7 +8,7 @@ if (NOT "${result_arch}" STREQUAL "x86_64")
 endif()
 
 # Test Case 2: Universal Architecture (OSX)
-if (APPLE)
+if (VCPKG_HOST_IS_OSX)
     set(VCPKG_TARGET_ARCHITECTURE "x86_64")
     set(VCPKG_OSX_ARCHITECTURES "x86_64;arm64")
     set(result_arch)
