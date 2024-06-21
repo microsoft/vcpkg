004391fbc92b7cb8c5d9188cbf5be5e3c98f8dd3
dsmith111 <davidcsmith12@gmail.com>
2024-06-20 21:54:21 -0700

    Devendor fast double parser
    Reformat manifest
    
    Version database
    
    Finish devendoring

--- ports/lightgbm/portfile.cmake
+++ ports/lightgbm/portfile.cmake
@@ -7,19 +7,6 @@ vcpkg_from_github(
         vcpkg_lightgbm_use_vcpkg_libs.patch
 )
 
-# Fast double parser is a non-vcpkg dependency of LightGBM
-vcpkg_from_github(
-    OUT_SOURCE_PATH FAST_DOUBLE_PARSER_SOURCE_PATH
-    REPO lemire/fast_double_parser
-    REF efec03532ef65984786e5e32dbc81f6e6a55a115
-    SHA512 2917167f05a270253b4c6360b598a69df43e8305601abb59d12c4085dd5db0b93b0e8725f61595a0424f0a26b59ec1a97d58df1ab33674ce36f0da9bd818c485
-    HEAD_REF master
-)
-
-# Remove exisiting folder in case it was not cleaned
-file(REMOVE_RECURSE ${SOURCE_PATH}/external_libs/fast_double_parser)
-file(COPY ${FAST_DOUBLE_PARSER_SOURCE_PATH}/ DESTINATION ${SOURCE_PATH}/external_libs/fast_double_parser)
-
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         gpu USE_GPU
