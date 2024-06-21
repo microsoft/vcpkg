1e1296f7e3d63f9668b7d22b1f931ccda1a5e70d
c8ef <c8ef@outlook.com>
2024-06-21 15:10:10 +0800

    pc

--- ports/libaio/portfile.cmake
+++ ports/libaio/portfile.cmake
@@ -9,11 +9,13 @@ vcpkg_extract_source_archive(
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/pkgconfig.pc.in" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 vcpkg_cmake_install()
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
