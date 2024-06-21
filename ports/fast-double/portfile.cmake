6663cf26f382fe2fc90961749dd2d0ea3b308d60
dsmith111 <davidcsmith12@gmail.com>
2024-06-20 21:18:48 -0700

    Clean up comments

--- ports/fast-double/portfile.cmake
+++ ports/fast-double/portfile.cmake
@@ -8,12 +8,11 @@ vcpkg_from_github(
 
 set(VCPKG_BUILD_TYPE release) # header-only port
 
-# No need to configure since there are no compiled components
 file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME FastDouble CONFIG_PATH share/cmake/FastDouble)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib") # Ensure no lib folder is included
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
