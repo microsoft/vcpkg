0d86cead1db72da82526552e3ba2185576c74520
André Nguyen <anguyen@applanix.com>
2024-06-21 12:21:15 -0400

    [small-gicp] disable openmp on osx by patching CMakeLists.txt
    OpenMP is not yet available through vcpkg.

--- ports/small-gicp/portfile.cmake
+++ ports/small-gicp/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF v0.1.0
     SHA512 bdd0d1e39d25877e58b2753addbad082f2fdf3962809fe646cab8ba63438eff05e2276afb2803aaed0a3905e0251208e2faaf8c1c416b551ffbdd54b9743ddb6
     HEAD_REF master
+    PATCHES disable_openmp_osx.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -12,17 +13,10 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         tbb   BUILD_WITH_TBB
 )
 
-if(VCPKG_TARGET_IS_OSX)
-    set(BUILD_WITH_OPENMP OFF)
-else()
-    set(BUILD_WITH_OPENMP "auto")
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DBUILD_WITH_OPENMP=${BUILD_WITH_OPENMP}
 )
 
 vcpkg_cmake_install()
