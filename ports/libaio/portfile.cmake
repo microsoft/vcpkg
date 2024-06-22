dc1d1f0c95a5eaa3957c4c587fad5a1e12e47fda
c8ef <c8ef@outlook.com>
2024-06-22 17:57:48 +0800

    aio

--- ports/libaio/portfile.cmake
+++ ports/libaio/portfile.cmake
@@ -13,6 +13,8 @@ file(COPY "${CMAKE_CURRENT_LIST_DIR}/pkgconfig.pc.in" DESTINATION "${SOURCE_PATH
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DVERSION=${VERSION}
 )
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
