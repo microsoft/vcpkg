82871cff89c788d962f3641d3ff4faf63c5b4577
cheater <Cheater.dev@hotmail.com>
2024-06-26 21:30:33 +0300

    update links to jinja2cpp original branch

--- ports/jinja2cpp/portfile.cmake
+++ ports/jinja2cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Cheaterdev/Jinja2Cpp
+    REPO jinja2cpp/Jinja2Cpp
     REF "${VERSION}"
-    SHA512 345800e8b361186325b1f479154fdf0fa234029fb9bfcbce4540f1ea5a9a3ab8f098db4969f73a2c5996703e3ad12be4b29e51fde49cf4a5bc6fcbd8f88dc976
+    SHA512 10fa5b6d8d64b33f078611f25ce4c1be325f98ce81cb03cf3af5cbbd2c3d5b29867245d68bf5bdb3edf9c8ef79577bb6eaa1c5b681dc314a81f6caeaca89b1f3
     HEAD_REF master
 )
 
@@ -14,11 +14,11 @@ vcpkg_cmake_configure(
     -DJINJA2CPP_BUILD_TESTS=OFF
     -DJINJA2CPP_STRICT_WARNINGS=OFF
     -DJINJA2CPP_BUILD_SHARED=${JINJA2CPP_BUILD_SHARED}
-	-DCMAKE_DISABLE_FIND_PACKAGE_expected-lite=ON
-	-DCMAKE_DISABLE_FIND_PACKAGE_variant-lite=ON
-	-DCMAKE_DISABLE_FIND_PACKAGE_optional-lite=ON
-	-DCMAKE_DISABLE_FIND_PACKAGE_string-view-lite=ON
-	-DJINJA2CPP_DEPS_MODE=external
+    -DCMAKE_DISABLE_FIND_PACKAGE_expected-lite=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_variant-lite=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_optional-lite=ON
+    -DCMAKE_DISABLE_FIND_PACKAGE_string-view-lite=ON
+    -DJINJA2CPP_DEPS_MODE=external
 )
 
 vcpkg_cmake_install()
@@ -31,6 +31,7 @@ file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/static/jinja2cpp.lib" "${CURRENT_
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/share/pkgconfig/jinja2cpp.pc" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/jinja2cpp.pc")
 file(RENAME "${CURRENT_PACKAGES_DIR}/share/pkgconfig/jinja2cpp.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/jinja2cpp.pc")
+
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
