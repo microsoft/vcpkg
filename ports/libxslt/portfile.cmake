aac1163b6673716d6e931c271e62600ca18170b9
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-06-26 15:35:23 +0800

    update version

--- ports/libxslt/portfile.cmake
+++ ports/libxslt/portfile.cmake
@@ -51,7 +51,6 @@ if(NOT VCPKG_BUILD_TYPE)
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/tools/libxslt/debug/xslt-config" [[$(cd "$(dirname "$0")"; pwd -P)/..]] [[$(cd "$(dirname "$0")/../../../debug"; pwd -P)]])
     vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/tools/libxslt/debug/xslt-config" [[${prefix}/include]] [[${prefix}/../include]])
 endif()
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libxslt/xsltconfig.h" "#define LIBXSLT_DEFAULT_PLUGINS_PATH() \"${CURRENT_INSTALLED_DIR}/lib/libxslt-plugins\"" "")
 vcpkg_copy_tools(TOOL_NAMES xsltproc AUTO_CLEAN)
 
 vcpkg_fixup_pkgconfig()
