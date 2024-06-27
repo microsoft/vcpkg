ed61357f82208c93ba7b0ee08720ec33536ecca8
c8ef <c8ef@outlook.com>
2024-06-27 11:47:09 +0800

    [folly] enable libdwarf

--- ports/cachelib/portfile.cmake
+++ ports/cachelib/portfile.cmake
@@ -5,18 +5,17 @@ vcpkg_from_github(
     SHA512 8463c449c798e139b83e8be7716e613e38e654247cb430708e718c38c0d67fbd0b51c02aadaa1909325d0633ff3617ab61b9ddbf44060930ff79107681a6c90a
     HEAD_REF main
     PATCHES
-        fix-build.patch
         fix-glog.patch
 )
 
-FIND_PATH(NUMA_INCLUDE_DIR NAME numa.h
+find_path(NUMA_INCLUDE_DIR NAME numa.h
     PATHS ENV NUMA_ROOT
     HINTS "$ENV{HOME}/local/include" /opt/local/include /usr/local/include /usr/include
 )
 
-IF (NOT NUMA_INCLUDE_DIR)
+if(NOT NUMA_INCLUDE_DIR)
     MESSAGE(FATAL_ERROR "Numa library not found.\nTry: 'sudo yum install numactl numactl-devel' (or sudo apt-get install libnuma1 libnuma-dev)")
-ENDIF ()
+endif()
 
 file(REMOVE "${SOURCE_PATH}/cmake/FindGlog.cmake")
 vcpkg_cmake_configure(
