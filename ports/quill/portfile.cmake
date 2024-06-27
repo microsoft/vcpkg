abe1c2fd1cf4f21e51c85b653322b3ec7c1951af
Rémy Tassoux <contact@rt2.fr>
2024-06-27 05:16:53 +0200

    [quill] Update to 4.5.0 (#39541)

--- ports/quill/portfile.cmake
+++ ports/quill/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO odygrd/quill
     REF v${VERSION}
-    SHA512 f41c0eea126cea4eda675e2380e59f1a6262a6bb3a32f7aeba53733f82ce4cbf3c169d3e7b0b5acd0fb9f95b8edc1854c64730271bbc24ec27fc05a66ebaf6d5
+    SHA512 bf1708107342f8a300f60889ec266e26c2dc5037e85aa488b28fc02ada7c02ecd0f0f9b73d95cf6336a11f6e8bd2cdc149d2b3648d0971b4e17c8c9c870cc919
     HEAD_REF master
 )
 
