6a02259ac77997e94a44caf4d2276131feaf681e
Lee Maguire <lee.maguire@mongodb.com>
2024-06-28 10:36:37 +0100

    address comments

--- ports/cpprealm/portfile.cmake
+++ ports/cpprealm/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO realm/realm-cpp
-        REF "ae469c149503570c4dd74b1b0e7be488b6996284"
-        SHA512 "2ee7291b6dc7a5d6d101a794e5072bb0122f462fd597039089aabb387a8a6244eef055249c2db00fa4f2cfa7c6bb375ff22c50f761852bad5654554065329d2d"
+        REF "v${VERSION}"
+        SHA512 "d880daea7bceb3143052cae9d8883b448162484adacc566cca5315ac096964d0c79c421b25a76d5c2efbd8c3294dfb5acd3dd7314a6c154163e7d141b9eb2e51"
         HEAD_REF "main")
 
 set(CPPREALM_CMAKE_OPTIONS -DREALM_CPP_NO_TESTS=ON -DREALM_ENABLE_EXPERIMENTAL=ON -DREALMCXX_VERSION="2.1.0")
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "cpprealm" CONFIG_PATH "cmake")
+vcpkg_cmake_config_fixup(CONFIG_PATH "cmake")
 
 file(READ ${CURRENT_PACKAGES_DIR}/debug/include/cpprealm/internal/bridge/bridge_types.hpp DEBUG_TYPE_HEADER_CONTENTS)
 set(REGEX_PATTERN "\\{([^()]*)\\}")
