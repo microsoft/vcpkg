microsoft/vcpkg

commit 21e73b82792ff81b0c42c03fe542af28b762995d

[pocketpy] update to 1.4.6 (#39510)

ports/pocketpy/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pocketpy/pocketpy
     REF "v${VERSION}"
-    SHA512 8c0b773fa6b113a7ef5bdf20837e46e61e17ed257c052e31c8bf08e88a911482b5617091c85f62d77f6145d8713c7ae08d70ce9f438f253ce21fb850520f0bcf
+    SHA512 6c9872c4a402bc702e577067c05d593034f45f150ebbf033ef204b4c7deff6cd2da0f9db44e0bb37aefdeb7a4d99e5a9c4a93ece57316f561c5bf4cd33cd12e3
     HEAD_REF master
 )
 

commit 547252439a45444473d3131da53f105d7f3934fe

[vcpkg baseline][pocketpy] Don't install include/pybind11/* (#39571)
Detected by
https://dev.azure.com/vcpkg/public/_build/results?buildId=104413&view=results

```
error: The following files are already installed in /mnt/vcpkg-ci/installed/x64-linux and are in conflict with pocketpy:x64-linux
Installed by pybind11:x64-linux  
include/pybind11/embed.h
    include/pybind11/functional.h
    include/pybind11/operators.h
    include/pybind11/pybind11.h
    include/pybind11/stl.h
```

Broken by https://github.com/microsoft/vcpkg/pull/39510, in which
pybind11 support was added.

ports/pocketpy/fix-conflict.patch
@@ -1,0 +1,14 @@
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index ed02678..9297b20 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -86,7 +86,8 @@ if (PK_INSTALL)
+         DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/ 
+         DESTINATION include 
+         FILES_MATCHING PATTERN "*.h"
+-        PATTERN "typings" EXCLUDE
++        PATTERN "typings" EXCLUDE
++        PATTERN "pybind11" EXCLUDE
+     )
+     
+     # generate config.cmake

ports/pocketpy/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 6c9872c4a402bc702e577067c05d593034f45f150ebbf033ef204b4c7deff6cd2da0f9db44e0bb37aefdeb7a4d99e5a9c4a93ece57316f561c5bf4cd33cd12e3
     HEAD_REF master
+    PATCHES
+        fix-conflict.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)