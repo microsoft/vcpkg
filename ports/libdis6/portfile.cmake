157d813836d886c3818d439c36c3b1b229a4d0f7
Cameron Howell <jango.djarin@proton.me>
2024-06-29 23:41:08 -0500

    Updated portfile and usage to match upstream

--- ports/libdis6/portfile.cmake
+++ ports/libdis6/portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libdis6-${LIBDIS6_VERSION})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libdis6)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
