e905a5ce9b693bf699a8047c49c0309f0ce0cf6f
Cameron Howell <jango.djarin@proton.me>
2024-06-29 22:43:37 -0500

    Renamed opendis6 to libdis6

--- ports/libdis6/portfile.cmake
+++ ports/libdis6/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO crhowell3/opendis6
+  REPO crhowell3/libdis6
   REF "${VERSION}"
   SHA512 f83e2dc8c3c06f420c88af35bc818561383760adb026548ce69f483568d87c1457dfbb3d25279d24bb2f08b8ff539f1ad3ed42db1963e1618f01b442d4ff791a
   HEAD_REF main
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenDIS)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libdis6)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
