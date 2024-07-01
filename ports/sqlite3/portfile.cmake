cab3237703900a9debbf2467dbc40a9eebffff9a
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-07-01 09:04:22 +0000

    modify feature name

--- ports/sqlite3/portfile.cmake
+++ ports/sqlite3/portfile.cmake
@@ -36,12 +36,12 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
 vcpkg_check_features(OUT_FEATURE_OPTIONS none # only using the script-mode side-effects
     FEATURES
         dbstat              SQLITE_ENABLE_DBSTAT_VTAB
+        dbpage-vtab         SQLITE_ENABLE_DBPAGE_VTAB
         fts3                SQLITE_ENABLE_FTS3
         fts4                SQLITE_ENABLE_FTS4
         memsys3             SQLITE_ENABLE_MEMSYS3
         memsys5             SQLITE_ENABLE_MEMSYS5
         limit               SQLITE_ENABLE_UPDATE_DELETE_LIMIT
-        recovery            SQLITE_ENABLE_DBPAGE_VTAB
         rtree               SQLITE_ENABLE_RTREE
         session             SQLITE_ENABLE_SESSION
         session             SQLITE_ENABLE_PREUPDATE_HOOK
