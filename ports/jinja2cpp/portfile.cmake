c733adc31f8f02ae7607738fbe3e85846de95e7f
cheater <Cheater.dev@hotmail.com>
2024-07-03 19:14:54 +0300

    patch instead of copy

--- ports/jinja2cpp/portfile.cmake
+++ ports/jinja2cpp/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF "${VERSION}"
     SHA512 10fa5b6d8d64b33f078611f25ce4c1be325f98ce81cb03cf3af5cbbd2c3d5b29867245d68bf5bdb3edf9c8ef79577bb6eaa1c5b681dc314a81f6caeaca89b1f3
     HEAD_REF master
+    PATCHES build_fixes.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" JINJA2CPP_BUILD_SHARED)
@@ -25,31 +26,14 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/${PORT}")
 
-
-find_library(JINJA2CPP_REL NAMES jinja2cpp ${REL_NAMES} PATHS "${CURRENT_PACKAGES_DIR}/lib/static" NO_DEFAULT_PATH)
-find_library(JINJA2CPP_DBG NAMES jinja2cpp ${DBG_NAMES} PATHS "${CURRENT_PACKAGES_DIR}/debug/lib/static" NO_DEFAULT_PATH)
-
-if(JINJA2CPP_REL)
- get_filename_component(JINJA2CPP_REL_EXT "${JINJA2CPP_REL}" EXT)
- file(RENAME "${JINJA2CPP_REL}" "${CURRENT_PACKAGES_DIR}/lib/jinja2cpp.${JINJA2CPP_REL_EXT}")
-endif()
-
-if(JINJA2CPP_DBG)
- get_filename_component(JINJA2CPP_DBG_EXT "${JINJA2CPP_DBG}" EXT)
- file(RENAME "${JINJA2CPP_DBG}" "${CURRENT_PACKAGES_DIR}/debug/lib/jinja2cpp.${JINJA2CPP_DBG_EXT}")
-endif()
-
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/pkgconfig" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
 file(RENAME "${CURRENT_PACKAGES_DIR}/debug/share/pkgconfig/jinja2cpp.pc" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/jinja2cpp.pc")
 file(RENAME "${CURRENT_PACKAGES_DIR}/share/pkgconfig/jinja2cpp.pc" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/jinja2cpp.pc")
 
-vcpkg_fixup_pkgconfig()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/pkgconfig")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/static" "${CURRENT_PACKAGES_DIR}/lib/static")
 
-file(INSTALL
-    ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright
-)
+vcpkg_fixup_pkgconfig()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
