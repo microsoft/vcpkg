3c63a3ca48f0a4b66cddb50fe7f68cea5e2ac4f8
Kai Pastor <dg0yt@darc.de>
2024-07-05 07:52:51 +0200

    [sdl2] Export C++ linkage when needed (android)

--- ports/sdl2/portfile.cmake
+++ ports/sdl2/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
     PATCHES
         deps.patch
         alsa-dep-fix.patch
+        cxx-linkage-pkgconfig.diff
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL_STATIC)
