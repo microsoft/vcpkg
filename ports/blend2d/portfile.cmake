fd092e34a5293a595abc8cc607e5fb27890a4a10
Jakub Melka <Mgr.Jakub.Melka@gmail.com>
2024-07-06 21:58:37 +0200

    Use external ASMJIT

--- ports/blend2d/portfile.cmake
+++ ports/blend2d/portfile.cmake
@@ -15,27 +15,12 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     tls        BLEND2D_NO_TLS
 )
 
-if(NOT BLEND2D_NO_JIT)
-  vcpkg_from_github(
-    OUT_SOURCE_PATH ASMJIT_SOURCE_PATH
-    REPO asmjit/asmjit
-    REF ffac9f36fb045dd2c6a81e1b5b9ccc115e5ef924 # commited on 2024-06-28
-    SHA512 3a594bdea7095f66356f8c21e03a4db2535811988726c25a29e66721cb703dc9dceddeb5f8a6c7daeafba3fbf6dd07e71375446d8dfb182f57c14bce28192774
-    HEAD_REF master
-  )
-
-  file(REMOVE_RECURSE "${SOURCE_PATH}/3rdparty/asmjit")
-
-  get_filename_component(ASMJIT_SOURCE_DIR_NAME "${ASMJIT_SOURCE_PATH}" NAME)
-  file(COPY "${ASMJIT_SOURCE_PATH}" DESTINATION "${SOURCE_PATH}/3rdparty")
-  file(RENAME "${SOURCE_PATH}/3rdparty/${ASMJIT_SOURCE_DIR_NAME}" "${SOURCE_PATH}/3rdparty/asmjit")
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         "-DBLEND2D_STATIC=${BLEND2D_STATIC}"
         "-DBLEND2D_NO_FUTEX=OFF"
+        "-DBLEND2D_EXTERNAL_ASMJIT=ON"
         ${FEATURE_OPTIONS}
 )
 
