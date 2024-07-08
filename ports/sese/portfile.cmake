21bb1213fd3cc5885ed79903f6c88d218158a155
kaoru <shiina_kaoru@outlook.com>
2024-07-08 14:11:53 +0800

    update portfile.cmake

--- ports/sese/portfile.cmake
+++ ports/sese/portfile.cmake
@@ -16,7 +16,7 @@ set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sese)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO libsese/sese
-        REF ${VERSION}
+        REF "${VERSION}"
         SHA512 6a87cabe6cbd69ab41de85be27ff397c1ae49f95c11151a27e8b9329afe4ff3b580084be53c129e883be50e13fadccb3b6cc1eed833c44ce4f8457dedc71b758
 )
 
