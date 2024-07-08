78ffd239a4ab80c2c939cda463da33f66de77c5c
Martin Valgur <martin.valgur@gmail.com>
2024-07-08 13:54:03 +0300

    [openmp] Use llvm-openmp only for Clang and AppleClang

--- ports/openmp/vcpkg-cmake-wrapper.cmake
+++ ports/openmp/vcpkg-cmake-wrapper.cmake
@@ -1,6 +1,8 @@
+# Uses llvm-openmp from Vcpkg for Clang and AppleClang and the native OpenMP implementation for all other compilers.
+
 set(_CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}")
-# if(CMAKE_CXX_COMPILER_ID MATCHES "^(Clang|AppleClang)$")
+if(CMAKE_CXX_COMPILER_ID MATCHES "^(Clang|AppleClang)$")
     list(PREPEND CMAKE_MODULE_PATH "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/llvm-openmp")
-# endif()
+endif()
 _find_package(${ARGS})
 set(CMAKE_MODULE_PATH "${_CMAKE_MODULE_PATH}")
