5e22a88df97d4f03fcf53d6909da5e8343f2ed29
Martin Valgur <martin.valgur@gmail.com>
2024-07-08 14:34:18 +0300

    Add usage info

--- ports/openmp/portfile.cmake
+++ ports/openmp/portfile.cmake
@@ -1,3 +1,4 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/openmp/vcpkg-cmake-wrapper.cmake" COPYONLY)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
