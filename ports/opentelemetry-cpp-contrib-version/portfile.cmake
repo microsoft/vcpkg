095f956e60751cfd7af7da5b04839f963e13abe4
Pablo Rodriguez Avila <pravila0011@gmail.com>
2024-07-09 15:45:49 +0100

    Add opentelemetry-cpp-contrib-version port

---
+++ ports/opentelemetry-cpp-contrib-version/portfile.cmake
@@ -0,0 +1,5 @@
+file(INSTALL
+    "${CMAKE_CURRENT_LIST_DIR}/clone_opentelemetry_cpp_contrib.cmake"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
--- ports/opentelemetry-cpp/portfile.cmake
+++ ports/opentelemetry-cpp/portfile.cmake
@@ -50,13 +50,8 @@ set(OPENTELEMETRY_CPP_EXTERNAL_COMPONENTS "OFF")
 if(WITH_GENEVA OR WITH_USER_EVENTS)
     # Geneva and user events exporters from opentelemetry-cpp-contrib are tightly coupled with opentelemetry-cpp repo, 
     # so they should be ported as a feature under opentelemetry-cpp.
-    vcpkg_from_github(
-        OUT_SOURCE_PATH CONTRIB_SOURCE_PATH
-        REPO open-telemetry/opentelemetry-cpp-contrib
-        REF 93733bb2e52273474f4298d10645bf7d1157fc55
-        HEAD_REF main
-        SHA512 bfe297e313b8e960a4557dd8750c814af2f9b574dd248f8c30b452160f5b7aba53af85b2c5b87a423ea3878e8a852022e3ff3d08a983db073c68418e64f671b2
-    )
+    include(${CURRENT_HOST_INSTALLED_DIR}/share/opentelemetry-cpp-contrib-version/clone_opentelemetry_cpp_contrib.cmake)
+    clone_opentelemetry_cpp_contrib(CONTRIB_SOURCE_PATH)
     
     if(WITH_GENEVA)
         set(OPENTELEMETRY_CPP_EXTERNAL_COMPONENTS "${CONTRIB_SOURCE_PATH}/exporters/geneva")
