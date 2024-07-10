929b73bedd3443330f5dca83332350c7ff9d8604
Kai Pastor <dg0yt@darc.de>
2024-07-10 07:34:28 +0200

    [vcpkg-gn] VCPKG_POLICY_CMAKE_HELPER_PORT

--- ports/vcpkg-gn/portfile.cmake
+++ ports/vcpkg-gn/portfile.cmake
@@ -1,11 +1,9 @@
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-
-if(NOT TARGET_TRIPLET STREQUAL _HOST_TRIPLET)
-    message(FATAL_ERROR "vcpkg-gn is a host-only port; please mark it as a host port in your dependencies.")
-endif()
+set(VCPKG_POLICY_CMAKE_HELPER_PORT enabled)
+set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
 
 file(INSTALL
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_gn_configure.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/vcpkg_gn_install.cmake"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
