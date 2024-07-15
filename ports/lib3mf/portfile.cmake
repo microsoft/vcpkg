6af43375794a6acc3c4ef199ed60ff8d173eb34a
Vijai Kumar S <vijai@vijaikumar.in>
2024-07-15 22:54:58 +0530

    Updated lib3mf 2.3.2 port to not use any vendored dependencies

--- ports/lib3mf/portfile.cmake
+++ ports/lib3mf/portfile.cmake
@@ -1,22 +1,36 @@
 # Manually clone the repository with submodules
-# Pull it from artifacts
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/3MFConsortium/lib3mf/releases/download/v2.3.2/lib3mf-2.3.2-source-with-submodules.zip"
-    FILENAME "lib3mf-2.3.2-source-with-submodules.zip"
-    SHA512 222821e4d739a3277b96977ec656a6498e75d19e62a34cd7cf204ef388643d2cfc1610f38abe9f8c60a4c450248f3a4de39822c367b6cbb405d43148d32d52d2
+    URLS "https://github.com/3MFConsortium/lib3mf/archive/refs/tags/v2.3.2.zip"
+    FILENAME "lib3mf-2.3.2.zip"
+    SHA512 5bf888c06a2429bfaca45b5ed96b2a7077a84ca3d1c4d114967eca0798189d9cbe7454eb810dcc18ccf31561efca271c4f68e7666f9f2665a8592df530f0d0bb
 )
 
 # Extract it
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
-    NO_REMOVE_ONE_LEVEL
 )
+
+# Apply the patch
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES "lib3mf_vcpkg.patch"
+)
+
+# Only dynamic libraries (Based on Jan's Port)
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 # Proceed with the usual build process
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS -DLIB3MF_TESTS=OFF
+    OPTIONS 
+    -DUSE_INCLUDED_ZLIB=OFF
+    -DUSE_INCLUDED_LIBZIP=OFF
+    -DUSE_INCLUDED_BASE_64=OFF
+    -DUSE_INCLUDED_FAST_FLOAT=OFF
+    -DUSE_INCLUDED_SSL=OFF
+    -DBUILD_FOR_CODECOVERAGE=OFF
+    -DLIB3MF_TESTS=OFF
 )
 
 # Install the package
