675fb9922745a040c96acc2037647026d7393791
Timothy Flynn <trflynn89@pm.me>
2024-07-16 11:48:22 -0400

    [simdutf] Add the fastbase64 tool

--- ports/simdutf/portfile.cmake
+++ ports/simdutf/portfile.cmake
@@ -23,7 +23,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_fixup_pkgconfig()
 if ("tools" IN_LIST FEATURES)
-    vcpkg_copy_tools(TOOL_NAMES sutf AUTO_CLEAN)
+    vcpkg_copy_tools(TOOL_NAMES fastbase64 sutf AUTO_CLEAN)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
