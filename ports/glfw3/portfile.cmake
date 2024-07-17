6c0698147666fbde9dc1355760c1a7bb8cc421c3
xiaozhuai <798047000@qq.com>
2024-07-17 21:26:19 +0800

    [glfw3] Fix build on wasm32-emscripten

--- ports/glfw3/portfile.cmake
+++ ports/glfw3/portfile.cmake
@@ -6,8 +6,15 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-if(VCPKG_TARGET_IS_LINUX)
-    message(
+if (VCPKG_TARGET_IS_EMSCRIPTEN)
+    # emscripten has built-in glfw3 library
+    set(VCPKG_BUILD_TYPE release)
+    file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/glfw3Config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/glfw3")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+else()
+
+    if(VCPKG_TARGET_IS_LINUX)
+        message(
 "GLFW3 currently requires the following libraries from the system package manager:
     xinerama
     xcursor
@@ -20,8 +27,8 @@ These can be installed on Ubuntu systems via sudo apt install libxinerama-dev li
 Alternatively, when targeting the Wayland display server, use the packages listed in the GLFW documentation here:
 
 https://www.glfw.org/docs/3.3/compile.html#compile_deps_wayland")
-else(VCPKG_TARGET_IS_OSX)
-    message(
+    else(VCPKG_TARGET_IS_OSX)
+        message(
 "GLFW3 currently requires the following libraries from the system package manager:
     xinerama
     xcursor
@@ -30,30 +37,32 @@ else(VCPKG_TARGET_IS_OSX)
     pkg-config
 
 These can be installed via brew install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config")
+    endif()
+
+    vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+        wayland         GLFW_BUILD_WAYLAND
+    )
+
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+            -DGLFW_BUILD_EXAMPLES=OFF
+            -DGLFW_BUILD_TESTS=OFF
+            -DGLFW_BUILD_DOCS=OFF
+            ${FEATURE_OPTIONS}
+    )
+
+    vcpkg_cmake_install()
+
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glfw3)
+
+    vcpkg_fixup_pkgconfig()
+
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+    file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+    vcpkg_copy_pdbs()
+
 endif()
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-    wayland         GLFW_BUILD_WAYLAND
-)
-
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DGLFW_BUILD_EXAMPLES=OFF
-        -DGLFW_BUILD_TESTS=OFF
-        -DGLFW_BUILD_DOCS=OFF
-        ${FEATURE_OPTIONS}
-)
-
-vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glfw3)
-
-vcpkg_fixup_pkgconfig()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-vcpkg_copy_pdbs()
