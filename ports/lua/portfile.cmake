c061006a6ff6dd3e62b4eea5e588bc1507585f5a
Tristan Ayala <tristandayala@gmail.com>
2024-07-18 17:44:16 -0700

    Link to CMAKE_DL_LIBS on any system that they're present on. Remove repeated code. Check for existence of dl library through CMAKE_DL_LIBS instead. Add VERSION to download. Add quotes around select expressions.

--- ports/lua/portfile.cmake
+++ ports/lua/portfile.cmake
@@ -1,8 +1,8 @@
 
 vcpkg_download_distfile(
     ARCHIVE
-    URLS "https://www.lua.org/ftp/lua-5.3.5.tar.gz"
-    FILENAME "lua-5.3.5.tar.gz"
+    URLS "https://www.lua.org/ftp/lua-${VERSION}.tar.gz"
+    FILENAME "lua-${VERSION}.tar.gz"
     SHA512 4f9516acc4659dfd0a9e911bfa00c0788f0ad9348e5724fe8fb17aac59e9c0060a64378f82be86f8534e49c6c013e7488ad17321bafcc787831d3d67406bd0f4
 )
 vcpkg_extract_source_archive_ex(
@@ -11,8 +11,8 @@ vcpkg_extract_source_archive_ex(
     PATCHES vs2015-impl-c99.patch
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/cpp/CMakeLists.txt DESTINATION ${SOURCE_PATH}/cpp)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/cpp/CMakeLists.txt" DESTINATION "${SOURCE_PATH}/cpp")
 
 vcpkg_check_features(
     OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -47,12 +47,12 @@ endif()
 
 # Handle post-build CMake instructions
 configure_file(
-    ${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in
-    ${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake
+    "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake.in"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake"
     @ONLY
 )
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 # Handle copyright
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/COPYRIGHT DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/COPYRIGHT" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
