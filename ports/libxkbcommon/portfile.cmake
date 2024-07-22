3adadf41968f1bc8c7731b3c6456c7e822019a0a
Cheney-Wang <850426846@qq.com>
2024-07-21 19:38:10 -0700

    Expand the scope of disable-test.patch

--- ports/libxkbcommon/portfile.cmake
+++ ports/libxkbcommon/portfile.cmake
@@ -6,8 +6,7 @@ else()
 set(PATCHES "")
 if(VCPKG_TARGET_IS_WINDOWS)
     #vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # Meson is not able to automatically export symbols for DLLs
-    set(PATCHES disable-test.patch 
-                build.patch)
+    set(PATCHES build.patch)
 endif()
 
 vcpkg_from_github(
@@ -16,7 +15,9 @@ vcpkg_from_github(
     REF "xkbcommon-${VERSION}"
     SHA512  454fbb2861405ca957d64035e924c1bbb7d43db7867903963fc053b7ecb64a8fba89a21cc8ac18ebeec9b61ae0789fb88c52521a850dc371857f28b08e80167b
     HEAD_REF master
-    PATCHES ${PATCHES}
+    PATCHES
+        disable-test.patch
+        ${PATCHES}
 )
 
 vcpkg_find_acquire_program(FLEX)
