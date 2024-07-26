a7c7b5909bbaf2140a8f96b188a4cc727fe31363
Vijai Kumar S <vijaiaeroastro@users.noreply.github.com>
2024-07-26 07:58:28 +0530

    Remove comments
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/lib3mf/portfile.cmake
+++ ports/lib3mf/portfile.cmake
@@ -33,23 +33,13 @@ vcpkg_cmake_configure(
     -DLIB3MF_TESTS=OFF
 )
 
-# Install the package
 vcpkg_cmake_install()
-
-# Copy all PDB's
 vcpkg_copy_pdbs()
-
-# Fix the path issue for CMake config files
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/lib3mf)
-
-# Fix up package configs (Get rid of absolute paths)
 vcpkg_fixup_pkgconfig()
 
-# Install the license
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Install the usage file
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-# Remove some of the debug stuff
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
