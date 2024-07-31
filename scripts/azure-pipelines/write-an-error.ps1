484ef4ec310f5359978b8c43b6b2668d47860c8a
Billy Robert O'Neal III <bion@microsoft.com>
2024-07-31 14:08:10 -0700

    Wat???

--- scripts/azure-pipelines/write-an-error.ps1
+++ scripts/azure-pipelines/write-an-error.ps1
@@ -1,1 +1,5 @@
-Write-Host 'This is an error that should fail the run'
+if ($IsWindows) {
+    & cmd.exe /c "echo `"This is an error that should fail the run`" 1>&2"
+} else {
+    & bash "echo `"This is an error that should fail the run`" 1>&2"
+}
