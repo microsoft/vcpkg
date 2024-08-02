645ec16d3304d47b8e819c311b065773777f233e
xiaozhuai <798047000@qq.com>
2024-08-02 15:27:56 +0800

    use vcpkg_install_copyright

--- ports/glfw3/portfile.cmake
+++ ports/glfw3/portfile.cmake
@@ -54,4 +54,4 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
