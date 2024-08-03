fe1cde61e971d53c9687cf9a46308f8f55da19fa
miyanyan <40262194+miyanyan@users.noreply.github.com>
2024-08-03 15:41:25 +0800

    [fmt] update to 11.0.2 (#39738)

--- ports/libtorch/portfile.cmake
+++ ports/libtorch/portfile.cmake
@@ -37,6 +37,7 @@ vcpkg_from_github(
         fix-calculate-minloglevel.patch
         force-cuda-include.patch
         fix-aten-cutlass.patch
+        fix-build-error-with-fmt11.patch
 )
 
 file(REMOVE_RECURSE "${SOURCE_PATH}/caffe2/core/macros.h") # We must use generated header files
