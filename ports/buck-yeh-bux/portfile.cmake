cb9ee66098574e0abc64003a3fe8c7f72d096472
buck-yeh <github-actions>
2024-08-05 00:36:00 +0800

    [buck-yeh-bux] Update to 1.7.0
    1. [Change] Say goodbye to `fmt`
    2. [Change] `I_LexAttr` becomes abstract

--- ports/buck-yeh-bux/portfile.cmake
+++ ports/buck-yeh-bux/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO buck-yeh/bux
     REF "${VERSION}"
-    SHA512 7b58e67d0744ead50fb5a826118d1026dc85b6bd880a2d3f30a9d355fc20f48697057eec139ad3960a65cebfbd0e30b99bee42b71bd23058c815a50b6df10c5c
+    SHA512 b7750509af9b46e8ed16326a73de91085bb6e89b3f284bfd0602436399d171e78cd56a7a4fae5cd410861652c6916b4b0962d47cdf43c13b0812d4633fd7f160
     HEAD_REF main
     PATCHES
         fix-build-error-with-fmt11.patch
