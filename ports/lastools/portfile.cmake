b8abb449b074acddd6d81249355c4c2d682f40a5
Kai Pastor <dg0yt@darc.de>
2024-08-04 11:50:25 +0200

    [lastools] No uwp. Add feature tools

--- ports/lastools/portfile.cmake
+++ ports/lastools/portfile.cmake
@@ -5,26 +5,31 @@ vcpkg_from_github(
     SHA512 cb150562b09c5a79df1e2c730481ceda340f235e1efb7824564d1f95a9981eada087af06bc3907a777a55d315a1521fb8a09249f2aeefd9e40e6c783b9c7a11c
     HEAD_REF master
     PATCHES 
-        "fix_install_paths_lastools.patch"
-        "fix_include_directories_lastools.patch"
+        fix_install_paths_lastools.patch
+        fix_include_directories_lastools.patch
+        build-tools.diff
 )
 
-if(VCPKG_TARGET_IS_UWP)
-    vcpkg_replace_string("${SOURCE_PATH}/CMakeLists.txt" "add_subdirectory(src)" "#add_subdirectory(src)")
-endif()
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        tools   BUILD_TOOLS
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${options}
+    OPTIONS_DEBUG
+        -DBUILD_TOOLS=OFF
 )
-
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/LASlib PACKAGE_NAME laslib)
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME LASlib CONFIG_PATH share/lastools/LASlib)
-
-if(NOT VCPKG_TARGET_IS_UWP)
+if(BUILD_TOOLS)
     vcpkg_copy_tools(TOOL_NAMES las2las64 las2txt64 lascopcindex64 lasdiff64 lasindex64 lasinfo64 lasmerge64 lasprecision64 laszip64 txt2las64  AUTO_CLEAN)
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt" "${SOURCE_PATH}/COPYING.txt")
