75a63ab8da6e752c0f7b99b71a3fd6d2f6ec7cac
Monica <v-liumonica@microsoft.com>
2024-08-07 01:28:42 -0700

    update and fix

--- ports/libjuice/portfile.cmake
+++ ports/libjuice/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO paullouisageneau/libjuice
     REF "v${VERSION}"
-    SHA512 5696ada382b70e5e8edc123c021cc1bf48a091253e20c705d1a23de3e5a9a240c138090228d02c5937a2418202e10216fafce4579195d5c2c92e7f7be107e622
+    SHA512 8a7e26daf05219ee2e490052b36be732bad35d570b66e9c08c36e4364ba183a511ea01e4444a8bf94dffd764a3f45f38900f7f4e26a531568b6c3adbd2bd7b53
     HEAD_REF master
     PATCHES
         fix-for-vcpkg.patch
@@ -13,10 +13,15 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         nettle USE_NETTLE
 )
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    list(APPEND arg_OPTIONS "-DJUICE_STATIC=ON")
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        ${arg_OPTIONS}
         -DNO_TESTS=ON
 )
 
