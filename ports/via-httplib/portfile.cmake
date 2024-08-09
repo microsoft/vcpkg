f20343dd7fcec0f8563e20999af740638d61d29e
Ema <emazv72@users.noreply.github.com>
2024-08-09 15:17:27 +0200

    Update ports/via-httplib/portfile.cmake
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/via-httplib/portfile.cmake
+++ ports/via-httplib/portfile.cmake
@@ -2,8 +2,8 @@ set(VCPKG_BUILD_TYPE release) # header-only
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kenba/via-httplib
-    REF b1baffa347b5a5a0b02e687ccc6273f26e1f7515 # v1.9.0
-    SHA512 a756c8869f5d17df3bdf8a83995f211b7a23cef6aacf916c9f4fbff365b8a6f4d2499154fc6de2db97de65b2dccbcc4a953aed37da68d8478cf1a9e38ac03d9a
+    REF ${VERSION}
+    SHA512 3a36d251b6dfe9ad40d798761169a70877c6d12a94ea2799670d701a4449e2fe15558bc9f50fa46e17e3f2a53b91eb233254efd5000eeece07890e1a804f301d
     HEAD_REF master    
 )
 
