2c046f7e6778865eb3b2011414f87bf32fdae0fa
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-08-15 10:58:13 +0800

    update to  24.07

--- ports/dpdk/portfile.cmake
+++ ports/dpdk/portfile.cmake
@@ -83,4 +83,4 @@ endif()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share" "${CURRENT_PACKAGES_DIR}/share/doc")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/license/README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/license/README")
