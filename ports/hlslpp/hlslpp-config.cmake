a70904e5935db37886aa187259df3bf20990c10a
Pantelis Lekakis <p.lekakis@icloud.com>
2024-08-16 08:33:36 +0100

    Update ports/hlslpp/hlslpp-config.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/hlslpp/hlslpp-config.cmake
+++ ports/hlslpp/hlslpp-config.cmake
@@ -1,4 +1,4 @@
-add_library(hlslpp INTERFACE)
-add_library(hlslpp::hlslpp ALIAS hlslpp)
+add_library(hlslpp::hlslpp INTERFACE)
+add_library(hlslpp ALIAS hlslpp::hlslpp)
 
 target_include_directories(hlslpp INTERFACE "${CMAKE_CURRENT_LIST_DIR}/../../include/hlslpp")
