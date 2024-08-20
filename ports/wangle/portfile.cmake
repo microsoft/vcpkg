ed6f9d83d964d420ed2c279157486b44fba54aeb
c8ef <c8ef@outlook.com>
2024-08-21 03:33:19 +0800

    [folly related] update to 08.19 (#40545)

--- ports/wangle/portfile.cmake
+++ ports/wangle/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebook/wangle
     REF "v${VERSION}"
-    SHA512 fbbcb02832c85c03e90fc8ddb0395e0afefedecf2909ef8c15ffe7f3bb76f63320724903288a1f1fd850ed23a08999c3371c28b542eb6a098d497f7b5a1e305f
+    SHA512 3a48aad4a3bc862ebb3c00db3099164d3c4c47e7dc1822ece1c07700e7b8c2d673d9d470cc7d8d4fc599e2299c2c1fd068e82e1dbd942c04ca8a6c3fc3be82ee
     HEAD_REF main
     PATCHES
         fix-config-cmake.patch
