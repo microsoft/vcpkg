e43528257053b44069b474f46906158e55e6c7e8
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-08-20 02:35:53 +0000

    update license file

--- ports/libstemmer/portfile.cmake
+++ ports/libstemmer/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://snowballstem.org/dist/libstemmer_c-${VERSION}.tar.gz"
-    FILENAME "libstemmer_c-${VERISON}.tgz"
+    FILENAME "libstemmer_c-${VERSION}.tgz"
     SHA512 a61a06a046a6a5e9ada12310653c71afb27b5833fa9e21992ba4bdf615255de991352186a8736d0156ed754248a0ffb7b7712e8d5ea16f75174d1c8ddd37ba4a
 )
 
