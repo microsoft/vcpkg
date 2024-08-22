49a078ccc55984a7e61f1fbae97db9b898c2423b
Peter Csajtai <peter.csajtai@outlook.com>
2024-08-22 12:06:04 +0200

    Update z4kn4fein-semver to version 0.4.0

--- ports/z4kn4fein-semver/portfile.cmake
+++ ports/z4kn4fein-semver/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO z4kn4fein/cpp-semver
-    REF v0.2.1
-    SHA512 1eac4bfc87d8719c3172a32897ff50063959faee0df747cb9b45a1bc32dd4f2a2e4f6ac5700e99854f9c06e7e70f3bfbc4a1bedbb99730fef06c0e587f4614ff
-    HEAD_REF master
+    REF "v${VERSION}"
+    SHA512 15555d3082c40edbd9647ee6faafd65324fdd9c2bfee84b95da64b3d369c3e5e28cde8f47035fc592e82007390d31825fc7284aa5f13e2721983c29cdab9156b
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
