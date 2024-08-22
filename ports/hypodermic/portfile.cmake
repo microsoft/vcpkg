3bf3fb6f7745ad6f46ee49948bea79a6e1460532
Riccardo Ressi <104351458+rressi-at-globus@users.noreply.github.com>
2024-08-22 09:59:25 +0200

    Update ports/hypodermic/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/hypodermic/portfile.cmake
+++ ports/hypodermic/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ybainier/Hypodermic
     REF ba5516d4e78230c1187debb01ebdf01e5d11f62d # 2023-03-03
-    SHA512 0
+    SHA512 40a119baafc23149117e286c38c00bdc0debda1cb6fefbecfdbc6e1a779789c6c884d1e7513aaf55060d550232c3dc8777ef0ebf0cd94e998ff2d5d6d375d2ff
     HEAD_REF master
     PATCHES
         "disable_hypodermic_tests.patch"
