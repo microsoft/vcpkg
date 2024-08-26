f05366317baa39d2d65dee6c9ebca15c80c6f6ff
Kai Pastor <dg0yt@darc.de>
2024-08-26 10:04:51 +0200

    [lunasvg] Update to 2.4.1 (#40598)

--- ports/lunasvg/portfile.cmake
+++ ports/lunasvg/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sammycage/lunasvg
   REF "v${VERSION}"
-  SHA512 05dec8310e04e6b2eeae265f9605446ce2da134b624951fa1dc4f7d3f16e62f5c5f3bfceae29f09111e2e22003310b6fba063945e75ba97e0cef82f7f31d4cae
+  SHA512 4c771c292d73d4a1c1498011fe8969b9f04a58853d2977c3ce4895d5ad6c1a282c8bfba5e4f1d045e56e0820e57a3a21d3cc0d178efe6ad73c82121aa75b1cdf
   HEAD_REF master
   PATCHES
     fix-cmake.patch
@@ -19,8 +19,11 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-lunasvg)
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/lunasvg.h" "defined(LUNASVG_BUILD_STATIC)" "1")
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
