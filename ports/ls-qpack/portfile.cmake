89685cc5d65e6fecb674f9279824144a6a7c823c
talregev <talregev@users.noreply.github.com>
2024-08-29 10:05:03 +0300

    [ls-qpack] Add cmake export (#40652)

--- ports/ls-qpack/portfile.cmake
+++ ports/ls-qpack/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
     HEAD_REF master
     PATCHES
         xxhash.patch
+        cmake-export.patch
 )
 
 vcpkg_find_acquire_program(PKGCONFIG)
@@ -26,8 +27,18 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-ls-qpack)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE 
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(READ "${CURRENT_PACKAGES_DIR}/share/unofficial-ls-qpack/unofficial-ls-qpack-config.cmake" cmake_config)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/unofficial-ls-qpack/unofficial-ls-qpack-config.cmake"
+"include(CMakeFindDependencyMacro)
+find_dependency(xxhash CONFIG)
+${cmake_config}
+")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
