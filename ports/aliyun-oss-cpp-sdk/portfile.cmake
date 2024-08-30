8280145803124a89ec80ca12fdb89be7784e1ada
牧亦 <luanjun.lj@alibaba-inc.com>
2024-08-30 17:14:01 +0800

    feature: add aliyun-oss-cpp-sdk

--- ports/aliyun-oss-cpp-sdk/portfile.cmake
+++ ports/aliyun-oss-cpp-sdk/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO aliyun/aliyun-oss-cpp-sdk
-    REF ${VERSION}
+    REF "${VERSION}"
     SHA512 7773961ad380d28cda96e16ae6491a76e03f0cb5f0c5135b660179dd449d730e1dfffb916489ed60e13815f53566c24cd9cfd8985c468438369341358eeed3bd
     HEAD_REF master
     PATCHES
@@ -10,9 +10,9 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS ${FEATURE_OPTIONS}
+    OPTIONS
         -DBUILD_SAMPLE=OFF
-        -DTARGET_OUTPUT_NAME_PREFIX=\"\"
+        -DTARGET_OUTPUT_NAME_PREFIX=\"aliyun-oss-\"
 )
 
 vcpkg_cmake_install()
