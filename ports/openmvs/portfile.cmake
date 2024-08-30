4587c868db2609bfd27c060a53c4897e723d08de
Monica <v-liumonica@microsoft.com>
2024-08-30 01:48:04 -0700

    move Common.lib to manual-link/

--- ports/openmvs/portfile.cmake
+++ ports/openmvs/portfile.cmake
@@ -83,4 +83,15 @@ configure_file("${SOURCE_PATH}/MvgMvsPipeline.py.in" "${CURRENT_PACKAGES_DIR}/to
 configure_file("${SOURCE_PATH}/MvgOptimizeSfM.py.in" "${CURRENT_PACKAGES_DIR}/tools/${PORT}/MvgOptimizeSfM.py" @ONLY)
 file(INSTALL "${SOURCE_PATH}/build/Modules/FindVCG.cmake" DESTINATION ${CURRENT_PACKAGES_DIR}/tools/${PORT})
 
+if(VCPKG_TARGET_IS_WINDOWS)
+    # Because of the installation order of CI, openmvs's Common.lib will replace crashpad's common.lib.
+    # https://github.com/microsoft/vcpkg/pull/40519#issuecomment-2304229773
+    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/lib/manual-link")
+    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/Common.lib" "${CURRENT_PACKAGES_DIR}/lib/manual-link/Common.lib")
+    if(NOT VCPKG_BUILD_TYPE)
+        file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/lib/manual-link")
+        file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/Common.lib" "${CURRENT_PACKAGES_DIR}/debug/lib/manual-link/Common.lib")
+    endif()
+endif()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
