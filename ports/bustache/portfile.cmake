479aadec3b5de5e812020cb578aa2df11d755a52
bc-universe <9028220+bc-universe@users.noreply.github.com>
2024-08-31 14:02:40 +0200

    Update portfile.cmake

--- ports/bustache/portfile.cmake
+++ ports/bustache/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO jamboree/bustache
     REF 1a6d4422bff46c7c8f37d2ba48c910532bdc8b37
-    SHA512 0
+    SHA512 a6eccc815c9ee1d7de9e1c8e2affc6d1a393fe3017eb492cbb3698282d567304a6b234d37aac6a48cd315c68187ec89f62c63ea4e9786bbb963b35f7d18990d9
 
     HEAD_REF master
 )
@@ -20,7 +20,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "share/bustache/cmake")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/bustache/cmake")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
