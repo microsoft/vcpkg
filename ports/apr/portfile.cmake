9b5746c85e63adf47a7bd3161f2044271057097d
Timofei Zhakov <tima@chemodax.net>
2024-08-31 14:49:15 +0200

    [apr] fix library linkage selection
    APR requires APR_BUILD_STATIC and APR_BUILD_SHARED options to be set for configuring the library linkage. After this commit they will be specified correctly and the library will work correctly in static configuration.

--- ports/apr/portfile.cmake
+++ ports/apr/portfile.cmake
@@ -19,9 +19,14 @@ if (VCPKG_TARGET_IS_WINDOWS)
             private-headers APR_INSTALL_PRIVATE_H
     )
 
+    string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" APR_BUILD_STATIC)
+    string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" APR_BUILD_SHARED)
+
     vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
         OPTIONS
+            -DAPR_BUILD_STATIC=${APR_BUILD_STATIC}
+            -DAPR_BUILD_SHARED=${APR_BUILD_SHARED}
             -DINSTALL_PDB=OFF
             -DMIN_WINDOWS_VER=Windows7
             -DAPR_HAVE_IPV6=ON
