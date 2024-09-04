a188d95651b7370de351409a76535ddcd018d3c7
bc-universe <9028220+bc-universe@users.noreply.github.com>
2024-09-04 20:35:05 +0200

    Fix portfile.cmake

--- ports/bustache/portfile.cmake
+++ ports/bustache/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/bustache/cmake")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/bustache")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
