57eb9453e8ee6bd5d2767a5fb3aed137bd3d1561
c8ef <c8ef@outlook.com>
2024-09-05 13:30:26 +0800

    [roaring] update to 4.1.2 (#40801)

--- ports/roaring/portfile.cmake
+++ ports/roaring/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO RoaringBitmap/CRoaring
     REF "v${VERSION}"
-    SHA512 f2963c1ece4f8ce5b88b594b821972eb29c0fd2d5d4b66518877e89c232d34e4f0fa5722823093d8089f6ec5dea769c1a7d09c212509b5817646b9f990ada8f8
+    SHA512 6a81d4425f9dd422aafc04b49ca5f70ca87f1cc64be15b936ffd89fe23c3b50c6a615e9af5ccdcdd5f5ad5851d2de762b8fb3412c65b243c12acb7abcb3bce58
     HEAD_REF master
 )
 
