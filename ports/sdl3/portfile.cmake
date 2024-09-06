8d9d8e3a0226ca0c05a3eae3f7d5cac1278bae44
Honeybunch <amt3824@g.rit.edu>
2024-09-06 11:21:36 -0700

    Fixing build for ios and osx

--- ports/sdl3/portfile.cmake
+++ ports/sdl3/portfile.cmake
@@ -52,7 +52,11 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+if(VCPKG_TARGET_IS_IOS OR VCPKG_TARGET_IS_OSX)
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
+else()
+    vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+endif()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
