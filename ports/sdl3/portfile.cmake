8d9311be4940a7a5dee6713db29b09065b6d114c
Honeybunch <amt3824@g.rit.edu>
2024-09-06 11:27:32 -0700

    Another tweak to fix linux

--- ports/sdl3/portfile.cmake
+++ ports/sdl3/portfile.cmake
@@ -52,10 +52,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-if(VCPKG_TARGET_IS_IOS OR VCPKG_TARGET_IS_OSX)
-    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
-else()
+if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_cmake_config_fixup(CONFIG_PATH cmake)
+else()
+    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
 endif()
 
 file(REMOVE_RECURSE
