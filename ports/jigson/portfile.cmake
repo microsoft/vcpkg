2ef541e05dcce1120ec0948f0ebd0084376616ab
Joshua Sledden <JoshuaSledden@gmail.com>
2024-09-06 08:24:30 +0100

    fix: portfile and vcpkg fixes

--- ports/jigson/portfile.cmake
+++ ports/jigson/portfile.cmake
@@ -5,7 +5,8 @@ vcpkg_from_github(
   SHA512 c2efa21603e3b9d06fe67a4dc5dabb43341baf3609022a03fba3178d2eef7407d2511987c98a91495c9671e42166c641f313d479be55a243dc474fc392d5411e
 )
 
-file(INSTALL "${SOURCE_PATH}/Jigson.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(GLOB HEADER_FILES "${SOURCE_PATH}/*.hpp" "${SOURCE_PATH}/*.h")
+file(INSTALL ${HEADER_FILES} DESTINATION "${CURRENT_PACKAGES_DIR}/include/jigson")
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
