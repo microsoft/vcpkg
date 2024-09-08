15958906f3205b64138969d7c06194c781998237
Kai Pastor <dg0yt@darc.de>
2024-09-08 21:04:08 +0200

    [mbedtls] Update to 3.6.1 (#40687)

--- ports/oatpp-mbedtls/portfile.cmake
+++ ports/oatpp-mbedtls/portfile.cmake
@@ -1,14 +1,16 @@
-set(OATPP_VERSION "1.3.0")
-
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO oatpp/oatpp-mbedtls
-    REF ${OATPP_VERSION}
+    REF ${VERSION}
     SHA512 3eea805f2a02110daec25b7455543c59d8e72acd37d412fa98cb1c90f58e4edcd9cc62c16331efcca36a524834fa0f314f2f69a7a4d0d1108a758f811a68e021
     HEAD_REF master
-    PATCHES find-mbedtls.patch
+    PATCHES
+        find-mbedtls.patch
+        mbedtls-3.patch
 )
 
 vcpkg_cmake_configure(
@@ -18,8 +20,16 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME oatpp-mbedtls CONFIG_PATH lib/cmake/oatpp-mbedtls-${OATPP_VERSION})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/oatpp-mbedtls-${VERSION}")
 vcpkg_copy_pdbs()
 
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(READ "${CURRENT_PACKAGES_DIR}/share/oatpp-mbedtls/oatpp-mbedtlsConfig.cmake" cmake_config)
+    file(WRITE "${CURRENT_PACKAGES_DIR}/share/oatpp-mbedtls/oatpp-mbedtlsConfig.cmake" "
+include(CMakeFindDependencyMacro)
+find_dependency(oatpp CONFIG)
+${cmake_config}")
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
