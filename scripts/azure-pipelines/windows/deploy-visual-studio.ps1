e8aa7d842e7143f8f4c540f1f055e2cb2dac11e5
Billy Robert O'Neal III <bion@microsoft.com>
2024-09-11 16:13:16 -0700

    Update Visual Studio to 17.11.3.

--- scripts/azure-pipelines/windows/deploy-visual-studio.ps1
+++ scripts/azure-pipelines/windows/deploy-visual-studio.ps1
@@ -8,8 +8,8 @@ if (Test-Path "$PSScriptRoot/utility-prefix.ps1") {
 }
 
 # See https://learn.microsoft.com/en-us/visualstudio/releases/2022/release-history
-# 17.8.12
-$VisualStudioBootstrapperUrl = 'https://download.visualstudio.microsoft.com/download/pr/a762cfe2-a734-454f-b7ed-26b22a4ea98d/d5e65f8ed2070bd56a564f88ce1f67eec82347170d4176b5d94c5713cbd4b9a5/vs_Enterprise.exe'
+# 17.11.3
+$VisualStudioBootstrapperUrl = 'https://download.visualstudio.microsoft.com/download/pr/030a0062-e1e5-4a6c-9385-a6f146dbd9db/85d31bd580a0b36936164315bd423d22af90f90d180a540ef4245522e0bcd085/vs_Enterprise.exe'
 $Workloads = @(
   'Microsoft.VisualStudio.Workload.NativeDesktop',
   'Microsoft.VisualStudio.Workload.Universal',
