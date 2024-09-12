94b343acfe7577ea44dd08fa6e2f1f0496b25ddf
Honeybunch <amt3824@g.rit.edu>
2024-09-12 09:44:20 -0700

    Removing unnecessary patch

--- ports/libmysofa/portfile.cmake
+++ ports/libmysofa/portfile.cmake
@@ -6,7 +6,6 @@ vcpkg_from_github(
     HEAD_REF "v${VERSION}"
     PATCHES
       use-vcpkg-zlib.patch
-      skip-install-sample.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
@@ -26,5 +25,6 @@ vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
