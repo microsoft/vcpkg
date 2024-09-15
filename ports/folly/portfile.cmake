4fd06866107d18ffde10456f818ec51ed0253bed
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-09-14 21:41:09 -0700

    [folly] add fmt flag workaround for old cmake

--- ports/folly/portfile.cmake
+++ ports/folly/portfile.cmake
@@ -17,6 +17,7 @@ vcpkg_from_github(
         fix-deps.patch
         disable-uninitialized-resize-on-new-stl.patch
         fix-unistd-include.patch
+        fix-fmt-patch-for-old-cmake.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/CMake/FindFmt.cmake")
