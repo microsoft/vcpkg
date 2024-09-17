7aaa5116a6467301518c28a0a75db7e99bbd8d96
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-09-17 17:41:36 +0800

    license

--- ports/blake3/portfile.cmake
+++ ports/blake3/portfile.cmake
@@ -18,4 +18,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_A2")
