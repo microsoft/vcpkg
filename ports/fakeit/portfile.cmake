4ba3f035bf2ce8032cd144afcda8ef8652873c0d
Kai Pastor <dg0yt@darc.de>
2024-09-17 18:07:22 +0200

    [fakeit] Fix cmake config fixup

--- ports/fakeit/portfile.cmake
+++ ports/fakeit/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/FakeIt)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
