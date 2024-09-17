536609d785c026c574ff952f60d00b0c195a5c93
Javier Matos <javiermatos@Javiers-Laptop.local>
2024-09-17 17:00:49 -0400

    stop using 7z.msi

--- scripts/cmake/vcpkg_find_acquire_program(7Z).cmake
+++ scripts/cmake/vcpkg_find_acquire_program(7Z).cmake
@@ -1,9 +1,7 @@
 set(program_name 7z)
 if(CMAKE_HOST_WIN32)
     set(tool_subdirectory "24.08")
-    set(paths_to_search "${DOWNLOADS}/tools/7zip_msi-${tool_subdirectory}-windows/Files/7-Zip") # vcpkg fetch 7zip_msi path
-    list(APPEND paths_to_search "${DOWNLOADS}/tools/7z/${tool_subdirectory}/Files/7-Zip")
-    set(download_urls "https://github.com/ip7z/7zip/releases/download/24.08/7z2408.msi" "https://7-zip.org/a/7z2408.msi")
-    set(download_filename "7z2408.msi")
-    set(download_sha512 0bc88c99ad921a6f828d9ed9b8391510d1d0c2f8ecb7ad2921838c97c20f215e2ab03198009d159e2d1859bb20f157010f492288ce61c86544df06742cebbc34)
-endif()
+    list(APPEND paths_to_search "${DOWNLOADS}/tools/7z/${tool_subdirectory}/7-Zip")
+    set(download_urls "https://github.com/ip7z/7zip/releases/download/24.08/7z2408-extra.7z")
+    set(download_filename "7z2408.7z")
+    set(download_sha512 "35f55236fccfb576ca014e29d0c35f4a213e53f06683bd2e82f869ed02506e230c8dd623c01d0207244d6a997031f737903456b7ad4a44db1717f0a17a78602e")
