04e24aed8bec486960341be33ff02dadda3321ec
Timofei Zhakov <tima@chemodax.net>
2024-09-19 00:38:30 +0200

    [serf] quot everything!

--- ports/serf/portfile.cmake
+++ ports/serf/portfile.cmake
@@ -13,18 +13,18 @@ vcpkg_extract_source_archive_ex(
 )
 
 # Note: custom architecture is not supported on Unix.
-if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
   set(SCONS_ARCH "TARGET_ARCH=x86_64")
-elseif(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
+elseif(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
   set(SCONS_ARCH "TARGET_ARCH=x86")
 else()
   set(SCONS_ARCH "")
 endif()
 
 if(EXISTS "${CURRENT_INSTALLED_DIR}/bin/libapr-1.dll")
-  set(APR_STATIC no)
+  set(APR_STATIC "no")
 else()
-  set(APR_STATIC yes)
+  set(APR_STATIC "yes")
 endif()
 
 vcpkg_find_acquire_program(SCONS)
@@ -34,29 +34,29 @@ if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
 
   if(VCPKG_TARGET_IS_WINDOWS)
     SET(apr_opts
-      APR=${CURRENT_INSTALLED_DIR}/debug
-      APU=${CURRENT_INSTALLED_DIR}/debug
-      APR_STATIC=${APR_STATIC}
+      "APR=${CURRENT_INSTALLED_DIR}/debug"
+      "APU=${CURRENT_INSTALLED_DIR}/debug"
+      "APR_STATIC=${APR_STATIC}"
     )
   else()
     SET(apr_opts
-      APR=${CURRENT_INSTALLED_DIR}/tools/apr/debug/bin/apr-1-config
-      APU=${CURRENT_INSTALLED_DIR}/tools/apr-util/debug/bin/apu-1-config
+      "APR=${CURRENT_INSTALLED_DIR}/tools/apr/debug/bin/apr-1-config"
+      "APU=${CURRENT_INSTALLED_DIR}/tools/apr-util/debug/bin/apu-1-config"
     )
   endif()
 
   vcpkg_execute_build_process(
-      COMMAND ${SCONS}
-          SOURCE_LAYOUT=no
-          PREFIX=${CURRENT_PACKAGES_DIR}/debug
-          LIBDIR=${CURRENT_PACKAGES_DIR}/debug/lib
-          OPENSSL=${CURRENT_INSTALLED_DIR}
-          ZLIB=${CURRENT_INSTALLED_DIR}
-          ${apr_opts}
-          ${SCONS_ARCH}
-          DEBUG=yes
-          install-lib install-pc
-      WORKING_DIRECTORY ${SOURCE_PATH}
+      COMMAND "${SCONS}"
+          "SOURCE_LAYOUT=no"
+          "PREFIX=${CURRENT_PACKAGES_DIR}/debug"
+          "LIBDIR=${CURRENT_PACKAGES_DIR}/debug/lib"
+          "OPENSSL=${CURRENT_INSTALLED_DIR}"
+          "ZLIB=${CURRENT_INSTALLED_DIR}"
+          "${apr_opts}"
+          "${SCONS_ARCH}"
+          "DEBUG=yes"
+          "install-lib" "install-pc"
+      WORKING_DIRECTORY "${SOURCE_PATH}"
       LOGNAME "scons-dbg"
   )
 
@@ -88,29 +88,29 @@ if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
 
   if(VCPKG_TARGET_IS_WINDOWS)
     SET(apr_opts
-      APR=${CURRENT_INSTALLED_DIR}
-      APU=${CURRENT_INSTALLED_DIR}
-      APR_STATIC=${APR_STATIC}
+      "APR=${CURRENT_INSTALLED_DIR}"
+      "APU=${CURRENT_INSTALLED_DIR}"
+      "APR_STATIC=${APR_STATIC}"
     )
   else()
     SET(apr_opts
-      APR=${CURRENT_INSTALLED_DIR}/tools/apr/bin/apr-1-config
-      APU=${CURRENT_INSTALLED_DIR}/tools/apr-util/bin/apu-1-config
+      "APR=${CURRENT_INSTALLED_DIR}/tools/apr/bin/apr-1-config"
+      "APU=${CURRENT_INSTALLED_DIR}/tools/apr-util/bin/apu-1-config"
     )
   endif()
 
   vcpkg_execute_build_process(
-    COMMAND ${SCONS}
-        SOURCE_LAYOUT=no
-        PREFIX=${CURRENT_PACKAGES_DIR}
-        LIBDIR=${CURRENT_PACKAGES_DIR}/lib
-        OPENSSL=${CURRENT_INSTALLED_DIR}
-        ZLIB=${CURRENT_INSTALLED_DIR}
-        ${apr_opts}
-        ${SCONS_ARCH}
-        DEBUG=no
-        install-lib install-inc install-pc
-    WORKING_DIRECTORY ${SOURCE_PATH}
+    COMMAND "${SCONS}"
+        "SOURCE_LAYOUT=no"
+        "PREFIX=${CURRENT_PACKAGES_DIR}"
+        "LIBDIR=${CURRENT_PACKAGES_DIR}/lib"
+        "OPENSSL=${CURRENT_INSTALLED_DIR}"
+        "ZLIB=${CURRENT_INSTALLED_DIR}"
+        "${apr_opts}"
+        "${SCONS_ARCH}"
+        "DEBUG=no"
+        "install-lib" "install-inc" "install-pc"
+    WORKING_DIRECTORY "${SOURCE_PATH}"
     LOGNAME "scons-rel"
   )
 
