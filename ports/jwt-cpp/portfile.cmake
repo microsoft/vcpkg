d3767dd59071a48a0f3ea425f051a923bb9b18b8
Billy Robert O'Neal III <bion@microsoft.com>
2024-09-20 14:12:04 -0700

    Disable installation from sources rather than after the fact; remove nlohmann_json search

--- ports/jwt-cpp/portfile.cmake
+++ ports/jwt-cpp/portfile.cmake
@@ -7,8 +7,9 @@ vcpkg_from_github(
     SHA512 b6fdb93e3f2f065a2eb45fe16cb076a932b8d4bfad2251bd66d2be40d8afaf5c27a9cf17aaea61d8bfa3f5ff9ed3b45f90962dc14d72704ac5b9d717c12cc79f
     HEAD_REF master
     PATCHES
-        picojson.diff
+        external-json.diff
 )
+file(REMOVE_RECURSE "${SOURCE_PATH}/include/picojson")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -16,8 +17,7 @@ vcpkg_cmake_configure(
         -DJWT_BUILD_EXAMPLES=OFF
         -DJWT_CMAKE_FILES_INSTALL_DIR=share/${PORT}
 )
+
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/picojson")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
