047220454e83a07580522d73e2e386184105a0ab
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-09-24 14:39:24 +0100

    version database

---
+++ ports/openxlsx/portfile.cmake
@@ -0,0 +1,23 @@
+# Header-only library
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO troldal/OpenXLSX
+    REF master
+    SHA512 e3d27b6e21dcb727398ec1c78cdde4adfb9d5d354380e22217f63c46dc63a1dcdfde54733b70ac9b7e5fa7f6bc47028b311f03fdccc980ed9132ac0a23a23ed4
+    HEAD_REF master
+)
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DOPENXLSX_BUILD_TESTS=OFF
+)
+vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenXLSX)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+# file(INSTALL "${SOURCE_PATH}/include/boost/sml.hpp"
+#     DESTINATION "${CURRENT_PACKAGES_DIR}/include/boost"
+# )
+
+# Handle copyright
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
