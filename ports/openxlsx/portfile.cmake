35c16e7c3d565a99a8324d2eed096a010d43875e
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-09-26 20:54:44 +0100

    respond to comments

--- ports/openxlsx/portfile.cmake
+++ ports/openxlsx/portfile.cmake
@@ -6,13 +6,13 @@ vcpkg_from_github(
     SHA512 e3d27b6e21dcb727398ec1c78cdde4adfb9d5d354380e22217f63c46dc63a1dcdfde54733b70ac9b7e5fa7f6bc47028b311f03fdccc980ed9132ac0a23a23ed4
     HEAD_REF master
 )
-vcpkg_configure_cmake(
+vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
         -DOPENXLSX_BUILD_TESTS=OFF
 )
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/OpenXLSX)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_copy_pdbs()
