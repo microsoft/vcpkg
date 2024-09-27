7d4395fdc38a4ea80880dabeb4cfeb4d371e6532
Jürgen Herrmann <Shadowrom2020@users.noreply.github.com>
2024-09-27 10:06:45 +0200

    Update ports/libcred/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/libcred/portfile.cmake
+++ ports/libcred/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mamba-org/libcred
-    REF 1adb08315a91045c5e820c224864a9b4aeedee42 #v0.1.0
+    REF ${VERSION}
     SHA512 2b86cf0017c8e43f6d6eba378c00b56781b5d70b73d6251d8acde49f7fc698b41aaf2f0bb293211836ec9c3779828ae95cdc4041f3d0147c37352df007ad82bf
     HEAD_REF main
 )
