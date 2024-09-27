6c35e65e8fb447d49d6e6db09036bf7cd4c5a05b
Monica <v-liumonica@microsoft.com>
2024-09-26 23:18:29 -0700

    fix feature pybind11

--- ports/onnx/portfile.cmake
+++ ports/onnx/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
     PATCHES
         fix-cmakelists.patch
         fix-dependency-protobuf.patch
+        fix-pybind11.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
