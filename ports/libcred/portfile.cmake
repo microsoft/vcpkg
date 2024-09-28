9336b701b9a02b05628289db9825a275b37cc7c9
Shadowrom <shadowrom@me.com>
2024-09-28 08:52:04 +0200

    fix deps and tests

--- ports/libcred/portfile.cmake
+++ ports/libcred/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
     REF ${VERSION}
     SHA512 77470b552cafc2506f9f1be56a2cd7aa412a4b568120037bf730273b5cc7c4dbabebb0abb6b192e3aef69912c6b5721d9e80b0cae0059f4fe814a5c0a8f3dcfb
     HEAD_REF main
+    PATCHES
+    disable_tests.patch
 )
 
 vcpkg_configure_meson(SOURCE_PATH "${SOURCE_PATH}")
