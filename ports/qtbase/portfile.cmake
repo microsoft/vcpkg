0cd69cddf81e5acbab4679d6891c2f0b9da5c395
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-09-30 18:45:52 +0200

    Try fixing linux regressions

--- ports/qtbase/portfile.cmake
+++ ports/qtbase/portfile.cmake
@@ -21,7 +21,8 @@ set(${PORT}_PATCHES
         clang-cl_QGADGET_fix.diff
         fix-host-aliasing.patch
         fix_deploy_windows.patch
-        )
+        fix-link-lib-discovery.patch
+)
 
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
     list(APPEND ${PORT}_PATCHES env.patch)
