79ea68afa1d72975b1862e3b565596cc5d38cc0c
BamButz <bambutz@cryptonica.de>
2024-10-02 00:14:13 +0200

    docs: added usage file

--- ports/steamworks-sdk/portfile.cmake
+++ ports/steamworks-sdk/portfile.cmake
@@ -71,3 +71,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Readme.txt")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
