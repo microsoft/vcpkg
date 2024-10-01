03f08dbddfb61b580681ec7977163cdfe6776cb2
Stefano Sinigardi <stesinigardi@hotmail.com>
2024-10-01 17:01:37 +0200

    fix inconsistencies

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -2,7 +2,7 @@ SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 file(WRITE "${CURRENT_INSTALLED_DIR}/share/opencv/vcpkg-cmake-wrapper.cmake"
 [[
-set(OpenCV_DIR "${CURRENT_INSTALLED_DIR}/share/opencv4/" CACHE PATH "Path to OpenCVConfig.cmake" FORCE)
-set(OpenCV_ROOT "${CURRENT_INSTALLED_DIR}/share/opencv4/") #to force find_package(OpenCV) to look for specific version and not randomly pick one in between opencv* installed
+set(OpenCV_DIR "${CMAKE_CURRENT_LIST_DIR}/../opencv4/" CACHE PATH "Path to OpenCVConfig.cmake" FORCE)
+set(OpenCV_ROOT "${CMAKE_CURRENT_LIST_DIR}/../opencv4/") #to force find_package(OpenCV) to look for specific version and not randomly pick one in between opencv* installed
 _find_package(${ARGS})
 ]])
