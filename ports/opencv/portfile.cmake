53c9d64be76ddf5f7004b71267f1ff842e48d2f8
Stefano Sinigardi <stesinigardi@hotmail.com>
2024-10-02 12:14:32 +0200

    install to packages to not break cache

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -1,6 +1,6 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-file(WRITE "${CURRENT_INSTALLED_DIR}/share/opencv/vcpkg-cmake-wrapper.cmake"
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/opencv/vcpkg-cmake-wrapper.cmake"
 [[
 set(OpenCV_DIR "${CMAKE_CURRENT_LIST_DIR}/../opencv4/" CACHE PATH "Path to OpenCVConfig.cmake" FORCE)
 set(OpenCV_ROOT "${CMAKE_CURRENT_LIST_DIR}/../opencv4/") #to force find_package(OpenCV) to look for specific version and not randomly pick one in between opencv* installed
