28c5273910e2b22f58c72a57ea71f17696682795
Kai Pastor <dg0yt@darc.de>
2024-10-03 05:52:06 +0200

    [vxl] Revise deps, enable CI

--- ports/vxl/portfile.cmake
+++ ports/vxl/portfile.cmake
@@ -1,10 +1,6 @@
-set(VXL_BUILD_CORE_IMAGING OFF)
-if("core-imaging" IN_LIST FEATURES)
-  set(VXL_BUILD_CORE_IMAGING ON)
-  if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openjpeg.h")
-    set(VXL_BUILD_CORE_IMAGING OFF)
-    message(WARNING "Can't build VXL CORE_IMAGING features with non built-in OpenJpeg. Please remove OpenJpeg, and try install VXL again if you need them.")
-  endif()
+if(VCPKG_TARGET_IS_WINDOWS)
+    # Cf. https://vxl.github.io/vxl-users-faq.html
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
 
 vcpkg_from_github(
@@ -14,9 +10,25 @@ vcpkg_from_github(
     SHA512 4c6f6dcd793a50cbbc9c7f61c561ed4a747ded67e3dceb09792998c0f0d4294445a441fed668d59297560f196274b1c25593ce67b0aa7597cbb773328e8612c0
     HEAD_REF master
     PATCHES
-        fix_dependency.patch
-        testlib.patch
-        fix_limits.patch
+        clipper.diff
+        disable-tests.diff
+        file_formats.diff
+        geotiff.diff
+        int_64.diff
+        limits.diff
+        mingw.diff
+        omit-broken-install.diff
+        openjpeg.diff
+        rply.diff
+)
+file(GLOB_RECURSE vendored_sources "${SOURCE_PATH}/v3p/*.c" "${SOURCE_PATH}/v3p/*.cpp" "${SOURCE_PATH}/v3p/*.cxx")
+list(FILTER vendored_sources EXCLUDE REGEX "/netlib/")
+file(REMOVE_RECURSE ${vendored_sources})
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        core-imaging  VXL_BUILD_CORE_IMAGING
+        openjpeg      VXL_FORCE_V3P_OPENJPEG2  # vendored legacy 1.2 lib
 )
 
 set(USE_WIN_WCHAR_T OFF)
@@ -24,20 +36,30 @@ if(VCPKG_TARGET_IS_WINDOWS)
     set(USE_WIN_WCHAR_T ON)
 endif()
 
+# Avoid try-run which doesn't work for cross-builds.
+# Users may override with VCPKG_CMAKE_CONFIGURE_OPTIONS.
+string(COMPARE EQUAL "${VCPKG_TARGET_ARCHITECTURE}" "x64" VXL_HAS_SSE2_HARDWARE_SUPPORT)
+set(VCL_HAS_LFS ON)
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(VCL_HAS_LFS OFF)
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DVXL_BUILD_EXAMPLES=OFF
+        ${options}
         -DBUILD_TESTING=OFF
-        -DVXL_BUILD_CORE_IMAGING=${VXL_BUILD_CORE_IMAGING}
+        -DCMAKE_POLICY_DEFAULT_CMP0120=OLD  # vxl needs WriteCompilerDetectionHeader
+        -DVXL_BUILD_EXAMPLES=OFF
+        -DVXL_HAS_SSE2_HARDWARE_SUPPORT=${VXL_HAS_SSE2_HARDWARE_SUPPORT}
+        -DVCL_HAS_LFS=${VCL_HAS_LFS}
         -DVXL_FORCE_V3P_BZLIB2=OFF
-        -DVXL_USING_NATIVE_BZLIB2=TRUE # for disable build built-in bzip2 (v3p/bzlib/CMakeLists.txt#L10-L26)
-        -DVXL_FORCE_V3P_CLIPPER=ON # TODO : need add clipper port to turn off
+        -DVXL_USING_NATIVE_BZLIB2=ON
+        -DVXL_FORCE_V3P_CLIPPER=OFF
         -DVXL_FORCE_V3P_DCMTK=OFF
         -DVXL_FORCE_V3P_GEOTIFF=OFF
         -DVXL_FORCE_V3P_J2K=OFF
         -DVXL_FORCE_V3P_JPEG=OFF
-        -DVXL_FORCE_V3P_OPENJPEG2=ON # TODO : need fix compile error when using openjpeg port to turn off
         -DVXL_FORCE_V3P_PNG=OFF
         -DVXL_FORCE_V3P_TIFF=OFF
         -DVXL_FORCE_V3P_ZLIB=OFF
@@ -50,16 +72,14 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/vxl/cmake)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-# Remove tests which assume that the source dir still exists
+# Don't provide source dir; test lib not installed.
 file(REMOVE "${CURRENT_PACKAGES_DIR}/include/vxl/vcl/vcl_where_root_dir.h")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/vxl/core/testlib")
 
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/vxl/cmake/VXLConfig.cmake" "${CURRENT_BUILDTREES_DIR}" "") # only used in comment
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/vxl/VXLConfig.cmake" "# ${CURRENT_BUILDTREES_DIR}" "")
 
-file(INSTALL "${SOURCE_PATH}/core/vxl_copyright.h" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/core/vxl_copyright.h")
