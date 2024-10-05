83be7e9d4453bad8659a2d5aba6614547276c388
BamButz <bambutz@cryptonica.de>
2024-10-05 09:43:57 +0200

    Removed module+wrapper Added imported targets Moved to unofficial-steamworks-sdk package

--- ports/steamworks-sdk/portfile.cmake
+++ ports/steamworks-sdk/portfile.cmake
@@ -1,9 +1,7 @@
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
 vcpkg_download_distfile(ARCHIVE
     URLS "https://partner.steamgames.com/downloads/steamworks_sdk_160.zip"
     FILENAME "steamworks_sdk_160.zip"
-    SKIP_SHA512
+    SKIP_SHA512 # Not sure why, but the SHA512 hash is changing with each download
 )
 
 vcpkg_extract_source_archive(
@@ -13,11 +11,9 @@ vcpkg_extract_source_archive(
 
 if(VCPKG_TARGET_IS_WINDOWS)
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-        file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/win64/FindSteamworks-SDK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
         set(SDK_DESTINATION_PATH "${SOURCE_PATH}/redistributable_bin/win64/steam_api64")
         set(APP_TICKET_DESTINATION_PATH "${SOURCE_PATH}/public/steam/lib/win64/sdkencryptedappticket64")
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
-        file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/win32/FindSteamworks-SDK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
         set(SDK_DESTINATION_PATH "${SOURCE_PATH}/redistributable_bin/steam_api")
         set(APP_TICKET_DESTINATION_PATH "${SOURCE_PATH}/public/steam/lib/win32/sdkencryptedappticket")
     endif()
@@ -35,7 +31,6 @@ if(VCPKG_TARGET_IS_WINDOWS)
 elseif(VCPKG_TARGET_IS_OSX)
     set(VCPKG_FIXUP_MACHO_RPATH FALSE)
     set(STEAMCMD_PATH "${SOURCE_PATH}/tools/ContentBuilder/builder_osx")
-    file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/osx/FindSteamworks-SDK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
     file(INSTALL "${SOURCE_PATH}/redistributable_bin/osx/libsteam_api.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
     file(INSTALL "${SOURCE_PATH}/redistributable_bin/osx/libsteam_api.dylib" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
     if("appticket" IN_LIST FEATURES)
@@ -46,7 +41,6 @@ elseif(VCPKG_TARGET_IS_LINUX)
     set(VCPKG_FIXUP_ELF_RPATH FALSE)
     set(STEAMCMD_PATH "${SOURCE_PATH}/tools/ContentBuilder/builder_linux")
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
-        file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/linux64/FindSteamworks-SDK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
         file(INSTALL "${SOURCE_PATH}/redistributable_bin/linux64/libsteam_api.so" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
         file(INSTALL "${SOURCE_PATH}/redistributable_bin/linux64/libsteam_api.so" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
         if("appticket" IN_LIST FEATURES)
@@ -54,7 +48,6 @@ elseif(VCPKG_TARGET_IS_LINUX)
             file(INSTALL "${SOURCE_PATH}/public/steam/lib/linux64/libsdkencryptedappticket.so" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
         endif()
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
-        file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/linux32/FindSteamworks-SDK.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
         file(INSTALL "${SOURCE_PATH}/redistributable_bin/linux32/libsteam_api.so" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
         file(INSTALL "${SOURCE_PATH}/redistributable_bin/linux32/libsteam_api.so" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
         if("appticket" IN_LIST FEATURES)
@@ -68,7 +61,7 @@ file(COPY "${STEAMCMD_PATH}/" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}
 file(COPY "${SOURCE_PATH}/public/steam" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Readme.txt")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-steamworks-sdk-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-steamworks-sdk")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Readme.txt")
---
+++ ports/steamworks-sdk/unofficial-steamworks-sdk-config.cmake
@@ -0,0 +1,58 @@
+if (NOT TARGET unofficial::steamworks-sdk)
+    get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}" PATH)
+    get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
+
+    add_library(unofficial::steamworks-sdk UNKNOWN IMPORTED)
+
+    find_path(STEAMWORKS_SDK_INCLUDE_DIR
+        NAMES steam/steam_api.h
+        DOC "The Steamworks SDK include directory"
+    )
+
+    find_library(VCPKG_STEAMWORKS_SDK_LIBRARY_RELEASE
+        NAMES steam_api steam_api64
+        PATHS "${_IMPORT_PREFIX}/lib"
+        DOC "The Steamworks SDK library"
+        REQUIRED
+    )
+
+    find_library(VCPKG_STEAMWORKS_SDK_LIBRARY_DEBUG
+        NAMES steam_api steam_api64
+        PATHS "${_IMPORT_PREFIX}/debug/lib"
+        DOC "The Steamworks SDK debug library"
+        REQUIRED
+    )
+
+    set_target_properties(unofficial::steamworks-sdk PROPERTIES
+        IMPORTED_CONFIGURATIONS "Release;Debug"
+        INTERFACE_INCLUDE_DIRECTORIES "${STEAMWORKS_SDK_INCLUDE_DIR}"
+        IMPORTED_LOCATION_RELEASE "${VCPKG_STEAMWORKS_SDK_LIBRARY_RELEASE}"
+        IMPORTED_LOCATION_DEBUG "${VCPKG_STEAMWORKS_SDK_LIBRARY_DEBUG}"
+    )
+
+    #####
+    ## Feature: appticket
+    #####
+    find_library(VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_RELEASE
+            NAMES sdkencryptedappticket sdkencryptedappticket64
+            PATHS "${_IMPORT_PREFIX}/lib"
+            DOC "The Steamworks SDK appticket library"
+    )
+
+    find_library(VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_DEBUG
+            NAMES sdkencryptedappticket sdkencryptedappticket64
+            PATHS "${_IMPORT_PREFIX}/debug/lib"
+            DOC "The Steamworks SDK appticket debug library"
+    )
+
+    if(VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_RELEASE AND VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_DEBUG)
+        add_library(unofficial::steamworks-sdk::appticket UNKNOWN IMPORTED)
+
+        set_target_properties(unofficial::steamworks-sdk::appticket PROPERTIES
+                IMPORTED_CONFIGURATIONS "Release;Debug"
+                INTERFACE_INCLUDE_DIRECTORIES "${STEAMWORKS_SDK_INCLUDE_DIR}"
+                IMPORTED_LOCATION_RELEASE "${VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_RELEASE}"
+                IMPORTED_LOCATION_DEBUG "${VCPKG_STEAMWORKS_SDK_APPTICKET_LIBRARY_DEBUG}"
+        )
+    endif()
+endif()
