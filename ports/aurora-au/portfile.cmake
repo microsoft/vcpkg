1a1b18016c688818b49b74aaeab00652d370f516
Andre Nguyen <nguyen.apv@gmail.com>
2024-10-05 14:13:43 -0400

    [aurora-au] update version to 0.3.5

--- ports/aurora-au/portfile.cmake
+++ ports/aurora-au/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     SHA512 4aa3282f6b76fbadd04ca572734f72c86b1b0b4e85fc21a03d1ab00b83d3aea319ab2dac3934361b5f6fa7c4a0dccece94fe0a57f3d73d208315b51b1950e374
     HEAD_REF main
     PATCHES 
-        cmakelists.patch
+        disable-googletest.patch
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
@@ -20,3 +20,4 @@ vcpkg_cmake_config_fixup(
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")  # Remove empty directory
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
