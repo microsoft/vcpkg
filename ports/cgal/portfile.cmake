12f85a895ba0cd3aa382b235a71b567c95f2d651
Laurent Rineau <laurent.rineau@cgal.org>
2024-10-07 14:11:27 +0200

    add a patch
    patch from upstream PR https://github.com/CGAL/cgal/pull/8523

--- ports/cgal/portfile.cmake
+++ ports/cgal/portfile.cmake
@@ -7,6 +7,8 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 f61e608898d798b90ce07260928b682161f00e964b43b9876ef6604d10c30787a0814e13afde90f7d703efd6b83c61dd4a9d9f50d21068bd50c5c15f94b5755b
     HEAD_REF master
+    PATCHES
+        PR-8523__CGAL_CGAL-protect_against_macro_free-GF.patch
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
