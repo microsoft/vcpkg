ca9247b07ffa4ef0f4e376273bc227207856b3f8
kaoru <shiina_kaoru@outlook.com>
2024-10-07 19:28:49 +0800

    update to 2.3.0

--- ports/sese/portfile.cmake
+++ ports/sese/portfile.cmake
@@ -17,9 +17,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO libsese/sese
         REF "${VERSION}"
-        SHA512 6a87cabe6cbd69ab41de85be27ff397c1ae49f95c11151a27e8b9329afe4ff3b580084be53c129e883be50e13fadccb3b6cc1eed833c44ce4f8457dedc71b758
-        PATCHES
-            001-fix-fmt-error.patch
+        SHA512 a1008c351ea3e8745d629bdcceb4a6d089ae5a84137bbd49b8abbbb271032ddf279e9b20f155181b6a7d3d8cb17c2ec2f1b7a12464fb0cac8402628e473966cb
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
