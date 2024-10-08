2951b6ec85b2c0fa297659578c120d2639125b1e
Kaoru <shiinasama2001@gmail.com>
2024-10-09 03:53:59 +0800

    [sese] update usage (#41407)

--- ports/sese/portfile.cmake
+++ ports/sese/portfile.cmake
@@ -18,8 +18,6 @@ vcpkg_from_github(
         REPO libsese/sese
         REF "${VERSION}"
         SHA512 a1008c351ea3e8745d629bdcceb4a6d089ae5a84137bbd49b8abbbb271032ddf279e9b20f155181b6a7d3d8cb17c2ec2f1b7a12464fb0cac8402628e473966cb
-        PATCHES
-            001-msvc-pdbs.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
