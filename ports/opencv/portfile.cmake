c71f0f804b9efe068e3b2dc3a3b26df58ea0498d
Stefano Sinigardi <stesinigardi@hotmail.com>
2024-10-08 13:57:58 +0200

    do not write file on-the-fly

--- ports/opencv/portfile.cmake
+++ ports/opencv/portfile.cmake
@@ -1,8 +1,4 @@
 SET(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/opencv/vcpkg-cmake-wrapper.cmake"
-[[
-set(OpenCV_DIR "${CMAKE_CURRENT_LIST_DIR}/../opencv4/" CACHE PATH "Path to OpenCVConfig.cmake" FORCE)
-set(OpenCV_ROOT "${CMAKE_CURRENT_LIST_DIR}/../opencv4/") #to force find_package(OpenCV) to look for specific version and not randomly pick one in between opencv* installed
-_find_package(${ARGS})
-]])
+set(USE_OPENCV_VERSION "4")
+configure_file("${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/opencv/vcpkg-cmake-wrapper.cmake" @ONLY)
