3c4e67e0af67b25e8765f9170529c2b73ea80daf
c8ef <c8ef@outlook.com>
2024-10-08 22:04:34 +0800

    [liblzma] update to 5.6.3

--- ports/liblzma/portfile.cmake
+++ ports/liblzma/portfile.cmake
@@ -5,7 +5,6 @@ vcpkg_from_github(
     SHA512 0f814f4282c87cb74a8383199c1e55ec1bf49519daaf07f7b376cb644770b75cc9257c809b661405fcfd6cda28c54d799c67eb9e169665c35b1b87529468085e
     HEAD_REF master
     PATCHES
-        win_output_name.patch # Fix output name on Windows. Autotool build does not generate lib prefixed libraries on windows. 
         build-tools.patch
 )
 
