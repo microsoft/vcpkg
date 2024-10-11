1adec9d89dba2d688c2ab6f2e78e1e8c86182da9
c8ef <c8ef@outlook.com>
2024-10-11 09:22:14 +0800

    [folly related] update to 10.07

--- ports/mvfst/portfile.cmake
+++ ports/mvfst/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 8ec9e7daf1c259f31e90eaa32cfa29d377e279f93c2072528447455f4f8ea897ab17f2759222a2ea9a73fdafe96b7d8223cf1efbadad2e8ea180b23832090aa7
     HEAD_REF main
+    PATCHES fix-cmake.patch
 )
 
 vcpkg_cmake_configure(
