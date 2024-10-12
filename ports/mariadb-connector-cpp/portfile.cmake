055b0a3ab0fc9d6d021a8f8b147f206e4e6e64e6
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-10-12 14:03:31 +0800

    [mesa] add version

--- ports/mariadb-connector-cpp/portfile.cmake
+++ ports/mariadb-connector-cpp/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO mariadb-corporation/mariadb-connector-cpp
     REF ${VERSION}
     HEAD_REF master
-    SHA512 0
+    SHA512 90ce780e19babda02608134c99e8c0e7601a41ee5531097735beb54ec94c2dd38ecf4f457e9cac04831d7e886fe7c7b7a6d9fe799bf71d52ba168158ec36dc67
     PATCHES
         fix-carray.diff
         libmariadb.diff
