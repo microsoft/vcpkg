43ae8e371eec05ff0b3f859135863bff97f11e2e
Saikari <lin@sz.cn.eu.org>
2024-10-14 00:20:30 +0300

    Fixed deps in portfile.cmake

--- ports/libmem/portfile.cmake
+++ ports/libmem/portfile.cmake
@@ -4,42 +4,18 @@ vcpkg_from_github(
     REF 5.0.2
     SHA512 d7c5a1a42d65a00ed3aa8ba8f6974650801d3436ae90e072fea29d4dcb32a3963e2610c89a16b87d94a9613c8f2f0e8deb83b673a1771a9cd1eb716a56106a16
     HEAD_REF master
+    PATCHES
+        fix_DLL.patch
 )
 
-vcpkg_from_git(
-    OUT_SOURCE_PATH CAPSTONE_PATH
-    URL https://github.com/aquynh/capstone
-    REF dec87d6190c682227f554f5721cf7b00b58bdad5
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt"
+     DESTINATION "${SOURCE_PATH}"
 )
-
-vcpkg_from_git(
-    OUT_SOURCE_PATH KEYSTONE_PATH
-    URL https://github.com/keystone-engine/keystone
-    REF 2ba6b8c5e76fa66942e3122b495206f6c974834a
-)
-
-vcpkg_from_git(
-    OUT_SOURCE_PATH VCVARS_PATH
-    URL https://github.com/nathan818fr/vcvars-bash
-    REF 7041b84cb3e035a0351430a817dff4ed6a16dfd7
-)
-
-set(EXTERNAL_DIR ${SOURCE_PATH}/external)
-
-file(MAKE_DIRECTORY ${EXTERNAL_DIR})
-
-file(COPY ${CAPSTONE_PATH}/ DESTINATION ${EXTERNAL_DIR}/capstone)
-
-file(COPY ${KEYSTONE_PATH}/ DESTINATION ${EXTERNAL_DIR}/keystone)
-
-file(COPY ${VCVARS_PATH}/ DESTINATION ${EXTERNAL_DIR}/vcvars-bash)
-
 
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
         GENERATOR "NMake Makefiles"
-        DISABLE_PARALLEL_CONFIGURE
     )
 else()
     vcpkg_configure_cmake(
@@ -47,7 +23,6 @@ else()
         DISABLE_PARALLEL_CONFIGURE
 )
 endif()
-
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
