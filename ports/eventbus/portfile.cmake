9847da256f140482af1ed1912a1fc274da6bc8f3
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-10-14 22:06:30 +0100

    fix compilation errors

--- ports/eventbus/portfile.cmake
+++ ports/eventbus/portfile.cmake
@@ -1,18 +1,22 @@
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
+
+# before this revision the build would fail on windows
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gelldur/EventBus
-    REF master
-    SHA512 68be92be9a1adc37498e24850ae87384a34ed0343f29a5e525c984f5e220dd721b506fdd7f627e369300c07300f5fff06d88991f69c1e9bc0fff0d2a5b8f0eba
+    REF 4689564c4c775456bfa0dfd976b4f48aca5f4d2a
+    SHA512 0f1f3c21d1c5a18da87e331f252cb464143ba2038a26e6edb6b11c9544c02dd1919fe728e803b382e8f6b89550582d7905170437c184f784cfa3f28c784a7e59
     HEAD_REF master
 )
+
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    WINDOWS_USE_MSBUILD
     OPTIONS
     -DENABLE_TEST=OFF 
+    -DCMAKE_CXX_STANDARD=17
 )
 
 vcpkg_cmake_install()
