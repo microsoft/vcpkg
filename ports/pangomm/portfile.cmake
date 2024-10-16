facca101b09b4ab9d39697961115be773ff35c0e
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-10-16 20:29:50 +0800

    [pangomm] update to 2.54.0

--- ports/pangomm/portfile.cmake
+++ ports/pangomm/portfile.cmake
@@ -1,8 +1,8 @@
 # Keep distfile, don't use GitLab!
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/GNOME/sources/pangomm/2.50/pangomm-2.50.1.tar.xz"
-    FILENAME "pangomm-2.50.1.tar.xz"
-    SHA512 bffc55eedc4f278480a74afcf119b79a295bf5775123f582746ba425e19e2690d627baa9a9813b70db9e063db7efe959f365567dd7bcbc1fc862212ba8225a98
+    URLS "https://ftp.gnome.org/pub/GNOME/sources/pangomm/2.54/pangomm-2.54.0.tar.xz"
+    FILENAME "pangomm-2.54.0.tar.xz"
+    SHA512 bc74be8d1c692815fe07e41c75386a2fb8038ba2a335cad488bf996ecc18f1e1b434ef4bd6c15a9c5b5c42f4349c98403acc96a32ecc5eb4dca8cee9de44ca08
 )
 
 vcpkg_extract_source_archive(
