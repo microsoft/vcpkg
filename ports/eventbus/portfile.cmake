eff9c5a4e1b730b58d0f7442d0e67445f157287b
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-10-17 23:57:05 +0100

    fix a bug caused by case sensitivity

--- ports/eventbus/portfile.cmake
+++ ports/eventbus/portfile.cmake
@@ -23,5 +23,5 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/eventbus)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/EventBus)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
