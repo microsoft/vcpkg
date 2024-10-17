81d6ab70dbc523f4672d380d139334a13accbad2
motazmuhammad <motaz.muhammad@kaust.edu.sa>
2024-10-17 23:46:08 +0100

    solve a bug

--- ports/eventbus/portfile.cmake
+++ ports/eventbus/portfile.cmake
@@ -23,8 +23,5 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/eventbus)
-endif()
-
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/eventbus)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
