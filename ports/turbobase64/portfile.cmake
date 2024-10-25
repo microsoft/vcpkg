02c03eb0580240b646de3b4d60995376533321ae
Cheney-Wang <850426846@qq.com>
2024-10-25 01:18:12 -0700

    Use version

--- ports/turbobase64/portfile.cmake
+++ ports/turbobase64/portfile.cmake
@@ -1,9 +1,11 @@
+string(REGEX REPLACE "^([0-9]+)[.]([0-9])$" "\\1.0\\2" TURBO_VERSION "${VERSION}")
+
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO powturbo/Turbo-Base64
-    REF ${VERSION}
+    REF ${TURBO_VERSION}
     SHA512 de8aaace0faf6552cf692f131a4d03882b88252732bb4160c48e5cc630a0c2f637fa27309e084d02305cdf7ef28020e6c9fbb82b50c1916e46aabc95baea75ad
     HEAD_REF master
 )
