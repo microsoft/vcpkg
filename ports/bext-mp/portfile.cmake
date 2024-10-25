f2d909bfc92b14aed1c4c3137b077b83bb83cc74
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2024-10-25 13:26:10 +0800

    place LICENSE in source folder

--- ports/bext-mp/portfile.cmake
+++ ports/bext-mp/portfile.cmake
@@ -4,10 +4,11 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 101978e93e8a4ec095263ce3c45bdae2b599cf98b239dabb6823679578784f64860bc5eacfc8f908fc1669d7b5391b49e3df0bb2037e0463fc89e2ee781bf3d2
     HEAD_REF main
+    PATCHES add-LICENSE.patch
 )
 
 set(VCPKG_BUILD_TYPE release) # header-only
 
 file(COPY "${SOURCE_PATH}/mp"  DESTINATION "${CURRENT_PACKAGES_DIR}/include/")
 
-vcpkg_install_copyright(FILE_LIST "${CMAKE_CURRENT_LIST_DIR}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
