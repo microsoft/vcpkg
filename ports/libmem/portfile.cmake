47c18a2320b4d10d53ea07786cbec4768b85365d
Saikari <lin@sz.cn.eu.org>
2024-10-28 16:23:53 +0300

    Update portfile.cmake

--- ports/libmem/portfile.cmake
+++ ports/libmem/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
 #    PATCHES
 #        fix_DLL.patch
 
+set(VCPKG_LIBRARY_LINKAGE static)
+
 if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE EQUAL "static") 
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /DLM_EXPORT")
 endif()
