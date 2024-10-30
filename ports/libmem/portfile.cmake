8b6c8771c88373057d8ad19e795af1d79ea66250
Saikari <lin@sz.cn.eu.org>
2024-10-30 14:53:31 +0300

    Update portfile.cmake
    Remove unused code.

--- ports/libmem/portfile.cmake
+++ ports/libmem/portfile.cmake
@@ -5,8 +5,6 @@ vcpkg_from_github(
     SHA512 d7c5a1a42d65a00ed3aa8ba8f6974650801d3436ae90e072fea29d4dcb32a3963e2610c89a16b87d94a9613c8f2f0e8deb83b673a1771a9cd1eb716a56106a16
     HEAD_REF master
 )
-#    PATCHES
-#        fix_DLL.patch
 
 set(VCPKG_LIBRARY_LINKAGE static)
 
@@ -32,12 +30,8 @@ else()
     )
 endif()
 
-#vcpkg_cmake_config_fixup(PACKAGE_NAME "libmem")
-
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
