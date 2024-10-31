5bd9020bae6d7487fa5208bf190cf3babe202569
Kai Pastor <dg0yt@darc.de>
2024-10-31 21:17:37 +0100

    [libunwind] Auto-generate pkgconfig usage

--- ports/libunwind/portfile.cmake
+++ ports/libunwind/portfile.cmake
@@ -31,6 +31,4 @@ vcpkg_configure_make(
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
