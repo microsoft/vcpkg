4d38b6259202cadeda61aedc067c85b830b9a30e
Kai Pastor <dg0yt@darc.de>
2024-11-01 22:06:42 +0100

    [vcpkg-ci-gdal] Build executables

--- scripts/test_ports/vcpkg-ci-gdal/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-gdal/portfile.cmake
@@ -1,1 +1,12 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+file(COPY_FILE "${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig/gdal.pc" "${CURRENT_BUILDTREES_DIR}/gdal.pc.log")
+
+vcpkg_find_acquire_program(PKGCONFIG)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${CURRENT_PORT_DIR}/project"
+    OPTIONS
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+)
+vcpkg_cmake_build()
