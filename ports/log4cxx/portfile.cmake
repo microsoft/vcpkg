60795890270711a69797d010ba41a46393a6971e
Stephen Webb <swebb2066@gmail.com>
2024-11-05 15:07:32 +1100

    No pkg_config when using MSVC

--- ports/log4cxx/portfile.cmake
+++ ports/log4cxx/portfile.cmake
@@ -4,12 +4,17 @@ vcpkg_download_distfile(ARCHIVE
     SHA512 bd481d69e29b3c8908bbc91489bf4e752e6edb147404454c0e88fd8f107d68ae5a98e220ab912692e555ca071d1cff7fb99ffa51194cfa7d070593ce6285d2b0
 )
 
+vcpkg_download_distfile(MAKE_PKG_CONFIG_SUPPORT_OPT_IN
+  URLS https://github.com/apache/logging-log4cxx/commit/4642a50c70b6cbd9b68d7e8dace9c049c8198b07.patch?full_index=1
+  SHA512 4b3628d98d233a2e68d1183a8bb2156c2f1e6f80ab50cfe75a6df799d14bc3c7ba028fbb7ff524c56530a2260be37fd9f3d089422027987b5c8c36e9978c254c
+  FILENAME Make_pkg_config_support_opt_in-4642a50c70b6cbd9b68d7e8dace9c049c8198b07.patch
+)
+
 vcpkg_extract_source_archive(
     SOURCE_PATH ARCHIVE "${ARCHIVE}"
     PATCHES
         fix-find-package.patch
-        # Make pkg_config support opt-in when building with Microsoft Visual Studio
-        https://github.com/apache/logging-log4cxx/commit/4642a50c70b6cbd9b68d7e8dace9c049c8198b07.patch?full_index=1
+        ${MAKE_PKG_CONFIG_SUPPORT_OPT_IN}
 )
 
 vcpkg_cmake_configure(
