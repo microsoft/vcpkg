7fd612ee1c57192d49028ed6a922e5e8bdb257f3
Javier Matos Denizac <javier.matosd@gmail.com>
2024-11-05 16:29:42 -0500

    Revert msiexec dependency by adding using vcpkg.exe to unpack 7z.exe #41604 (#41973)

--- scripts/cmake/vcpkg_extract_archive.cmake
+++ scripts/cmake/vcpkg_extract_archive.cmake
@@ -34,10 +34,14 @@ function(vcpkg_extract_archive)
             WORKING_DIRECTORY "${archive_directory}"
         )
     elseif("${archive_extension}" MATCHES [[\.7z\.exe$]])
+        vcpkg_find_acquire_program(7Z)
         vcpkg_execute_in_download_mode(
-            COMMAND "$ENV{VCPKG_COMMAND}" z-extract "${arg_ARCHIVE}" "${arg_DESTINATION}"
-            WORKING_DIRECTORY "${arg_DESTINATION}"
-        )
+            COMMAND ${7Z} x
+                "${arg_ARCHIVE}"
+                "-o${arg_DESTINATION}"
+                -y -bso0 -bsp0
+                WORKING_DIRECTORY "${arg_DESTINATION}"
+            )
     else()
         vcpkg_execute_in_download_mode(
             COMMAND "${CMAKE_COMMAND}" -E tar xzf "${arg_ARCHIVE}"
