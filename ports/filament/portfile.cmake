2d2288fe3ae4080e3fd92eeb3d79b1883417263f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-11-06 07:33:07 -0800

    [filament] add Filament port

---
+++ ports/filament/portfile.cmake
@@ -0,0 +1,62 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/filament
+    REF v${VERSION}
+    SHA512 5c1873e83706135163c7113b715de233c9b17c5028232bdc8dc9e2b83436ad2f422b77bb08d9637523ce04b205a836f511fce51c3932e49715d94ee22077c573
+    HEAD_REF master
+)
+
+if(VCPKG_CRT_LINKAGE STREQUAL "static")
+    set(USE_STATIC_CRT ON)
+else()
+    set(USE_STATIC_CRT OFF)
+endif()
+
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/debug/bin")  #so that tools that are self-produced during build process, which might depend on vcpkg-provided shared libraries, do not silently fail due to missing .dlls
+    vcpkg_add_to_path("${CURRENT_INSTALLED_DIR}/bin")
+endif()
+
+if(VCPKG_TARGET_IS_LINUX)
+    vcpkg_find_acquire_program(CLANG)
+    set(COMPILER -DCMAKE_CXX_COMPILER=${CLANG}++ -DCMAKE_C_COMPILER=${CLANG})
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${COMPILER}
+        -DUSE_STATIC_CRT=${USE_STATIC_CRT}
+        -DFILAMENT_SKIP_SAMPLES=ON
+        -DFILAMENT_ENABLE_LTO=OFF
+        -DFILAMENT_USE_SWIFTSHADER=OFF
+        -DFILAMENT_USE_EXTERNAL_GLES3=OFF
+    MAYBE_UNUSED_VARIABLES
+        USE_STATIC_CRT
+)
+
+vcpkg_cmake_install()
+
+vcpkg_copy_tools(TOOL_NAMES
+    cmgen
+    filamesh
+    glslminifier
+    matc
+    matedit
+    matinfo
+    mipgen
+    normal-blending
+    resgen
+    roughness-prefilter
+    specular-color
+    uberz
+    AUTO_CLEAN
+)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE ${CURRENT_PACKAGES_DIR}/LICENSE ${CURRENT_PACKAGES_DIR}/README.md)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/LICENSE ${CURRENT_PACKAGES_DIR}/debug/README.md)
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
