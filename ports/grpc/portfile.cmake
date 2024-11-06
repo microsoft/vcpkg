48dd7f746c8975fc7871188bbc6a58e017b986c9
Dennis Hezel <dh@3yourmind.com>
2024-11-06 20:29:57 +0100

    grpc: Fix upb NaN. Update brpc to 1.11.0 and add official protobuf v5. Download official srpc protobuf v5 patch

--- ports/grpc/portfile.cmake
+++ ports/grpc/portfile.cmake
@@ -16,6 +16,8 @@ vcpkg_from_github(
         00006-utf8-range.patch
         00015-disable-download-archive.patch
         00016-fix-plugin-targets.patch
+        00017-fix-NAN-on-Win11.patch
+        00018-fix-windows-event-engine.patch
 )
 # Ensure de-vendoring
 file(REMOVE_RECURSE
