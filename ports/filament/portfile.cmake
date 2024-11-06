31cc4f0d9cb07158230fe2f92f9f919f0bc27844
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-11-06 05:47:55 -0800

    [filament] fix SPVRemapper linkage

--- ports/filament/portfile.cmake
+++ ports/filament/portfile.cmake
@@ -14,7 +14,6 @@ vcpkg_from_github(
         0004-glslang.patch
         0005-miktspace.patch
         0007-std-includes.patch
-        0008-civetweb.patch
 )
 
 if(VCPKG_CRT_LINKAGE STREQUAL "static")
