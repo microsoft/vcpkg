12022601a2572887a2e47cb09fe11fbc21e021b2
Osyotr <Osyotr@users.noreply.github.com>
2024-11-09 00:15:50 +0300

    Fix config dir
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/libpng/portfile.cmake
+++ ports/libpng/portfile.cmake
@@ -94,7 +94,7 @@ vcpkg_cmake_configure(
         PNG_ARM_NEON
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME PNG CONFIG_PATH lib/cmake/PNG)
+vcpkg_cmake_config_fixup(PACKAGE_NAME png CONFIG_PATH lib/cmake/PNG)
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/libpng)
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/png")
 
