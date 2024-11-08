d1692bf6b7b7030ec7bc2e0eba78b3d94b9b058a
Stefano Sinigardi <stesinigardi@hotmail.com>
2024-11-08 09:05:05 +0100

    fix opencl regression

--- ports/opencv4/portfile.cmake
+++ ports/opencv4/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_from_github(
       0014-fix-cmake-in-list.patch
       0015-fix-freetype.patch
       0017-fix-flatbuffers.patch
+      0019-opencl-kernel.patch
 )
 # Disallow accidental build of vendored copies
 file(REMOVE_RECURSE "${SOURCE_PATH}/3rdparty/openexr")
