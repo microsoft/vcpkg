62c629702301b874a2b566742858f296f3d3d8a6
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-11-09 15:41:55 +0800

    Modify format

--- ports/cpp-pinyin/portfile.cmake
+++ ports/cpp-pinyin/portfile.cmake
@@ -1,14 +1,14 @@
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO wolfgitpr/cpp-pinyin
-        REF  "${VERSION}"
-        SHA512 cdd78cdc493ab352bfd7c5adfb4642bc587fb26f65b4d81a07e7c89c377222a30730f3e800f028106b66cbc35e32709c1a0e470e9737b6ee9718e3ce9da8137a
-        HEAD_REF main
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO wolfgitpr/cpp-pinyin
+    REF  "${VERSION}"
+    SHA512 cdd78cdc493ab352bfd7c5adfb4642bc587fb26f65b4d81a07e7c89c377222a30730f3e800f028106b66cbc35e32709c1a0e470e9737b6ee9718e3ce9da8137a
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
         -DCPP_PINYIN_BUILD_STATIC=FALSE
         -DCPP_PINYIN_BUILD_TESTS=FALSE
         -DVCPKG_DICT_DIR=${CURRENT_PACKAGES_DIR}/share/${PORT}
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME ${PORT} CONFIG_PATH lib/cmake/${PORT})
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
@@ -24,5 +24,5 @@ if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
