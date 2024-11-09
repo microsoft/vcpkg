d017e914d29d6bf2fa36e42153ad249935e9a390
wolfgitpr <133209402+wolfgitpr@users.noreply.github.com>
2024-11-09 09:40:31 +0800

    Add cpp-pinyin to vcpkg

---
+++ ports/cpp-pinyin/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO wolfgitpr/cpp-pinyin
+        REF  "${VERSION}"
+        SHA512 cdd78cdc493ab352bfd7c5adfb4642bc587fb26f65b4d81a07e7c89c377222a30730f3e800f028106b66cbc35e32709c1a0e470e9737b6ee9718e3ce9da8137a
+        HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+        -DCPP_PINYIN_BUILD_STATIC=FALSE
+        -DCPP_PINYIN_BUILD_TESTS=FALSE
+        -DVCPKG_DICT_DIR=${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME ${PORT} CONFIG_PATH lib/cmake/${PORT})
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
