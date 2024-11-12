bcc04e9b950c10fa4141080500f9c3b474fdd93c
Jim-Wang <v-wangjim@microsoft.com>
2024-11-12 02:47:06 +0000

    rename config

--- ports/libwebm/portfile.cmake
+++ ports/libwebm/portfile.cmake
@@ -22,8 +22,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_copy_pdbs()
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-webm-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-webm")
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-webm)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-libwebm-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-libwebm")
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libwebm)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.TXT")
