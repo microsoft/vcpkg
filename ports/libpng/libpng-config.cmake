e4b6b9d927d58b89e7ea0e5e315af66c71a0909e
Osyotr <Osyotr@users.noreply.github.com>
2024-11-12 17:33:28 +0300

    Remove REQUIRED from config

--- ports/libpng/libpng-config.cmake
+++ ports/libpng/libpng-config.cmake
@@ -2,7 +2,7 @@ file(READ "${CMAKE_CURRENT_LIST_DIR}/usage" usage)
 message(WARNING "find_package(libpng) is deprecated.\n${usage}")
 
 include(CMakeFindDependencyMacro)
-find_dependency(PNG CONFIG REQUIRED)
+find_dependency(PNG CONFIG)
 
 if(NOT TARGET png_shared)
     add_library(png_shared ALIAS PNG::PNG)
