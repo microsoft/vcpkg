29d5cee85f8b7d189dfc706487d854d0e89a0cf1
aboelens <151673787+aboelens@users.noreply.github.com>
2024-11-17 14:23:11 +0100

    Update ports/osqp-eigen/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/osqp-eigen/portfile.cmake
+++ ports/osqp-eigen/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME  "osqpeigen" CONFIG_PATH "lib/cmake/OsqpEigen")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "osqpeigen" CONFIG_PATH "lib/cmake/OsqpEigen")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
