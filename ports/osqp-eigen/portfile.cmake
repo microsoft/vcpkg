0a049c6612f81bee2ae326d7cef88b7791311133
aboelens <151673787+aboelens@users.noreply.github.com>
2024-11-17 14:22:27 +0100

    Update ports/osqp-eigen/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/osqp-eigen/portfile.cmake
+++ ports/osqp-eigen/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "robotology/osqp-eigen"
-    REF v0.7.0     
+    REF v${VERSION}
     SHA512 92a801892ce11d3167e3f1eb98cd988e7be7cbc3caa070b1628977b7d881b34c1207d9c1443ba381d88be4133bda6d3e9195c931f9554537c86832841f1e61fb 
     PATCHES osqp-eigen.patch
 )
