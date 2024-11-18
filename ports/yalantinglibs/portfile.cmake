78492bdde43673a672d74eff971e0cd519c5551c
gsk <171930433@qq.com>
2024-11-18 22:01:51 +0800

    using 171930433 yalantinglibs

--- ports/yalantinglibs/portfile.cmake
+++ ports/yalantinglibs/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO alibaba/yalantinglibs
+    REPO 171930433/yalantinglibs
     REF "${VERSION}"
-    SHA512 e217a6518f72e2acb0cf647c6c81b51e79779eb71e8f7149402337947ee1dbe56a6cc76aa745adfd890b38c08806aac9d5f33379f8161a2eed82b7e97ad429c0
+    SHA512 acde5540f309b7cef2df9b1fc253b9d6b0abedec3a646332599fa83b32284b34a3a970546605e0e6b5e375e4b52e799a990ad962d8f4f271ffee3d4c5b1b1b65
     HEAD_REF main
 )
 
@@ -20,6 +20,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "yalantinglibs" CONFIG_PATH "lib/cmake/yalantinglibs")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
