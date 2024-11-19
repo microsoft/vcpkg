34584c596ca50ee74bc4eca39c37b6734ca57a5f
gsk <171930433@qq.com>
2024-11-19 09:10:13 +0800

    resolve conversation

--- ports/yalantinglibs/portfile.cmake
+++ ports/yalantinglibs/portfile.cmake
@@ -1,3 +1,5 @@
+set(VCPKG_BUILD_TYPE release)  # header-only
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO 171930433/yalantinglibs
@@ -17,10 +19,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "yalantinglibs" CONFIG_PATH "lib/cmake/yalantinglibs")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/yalantinglibs")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
