7c6c8b37240252bdd99cf0287aa217ea7a2687b1
Billy Robert O'Neal III <bion@microsoft.com>
2024-11-20 10:31:36 -0800

    Fix copyright file, patching, and find_dependency handling.

--- ports/spatial-hash/portfile.cmake
+++ ports/spatial-hash/portfile.cmake
@@ -8,14 +8,11 @@ vcpkg_from_github(
     REF bf592f26d84beca96e3ddc295ee1cf5b7341dee5
     SHA512 c6e0c0475f2ca9bd9b21b227874202a12191496a446e44c493d6a181636912a342c56a8742cb5597a164f108bce74ec9534e224db4fa916c76930b232c82895f
     PATCHES
-    compatible_vcpkg_cmake.patch
-        fix_cmake_config.patch
+        compatible-vcpkg-cmake.patch
 )
 
-set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled) # Since there is a '-' character in the port name it copies  to wrong directory (shared/spatial-hash), so we do it manually.
-
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DSPATIAL_HASH_BUILD_TESTS=OFF
 )
@@ -26,6 +23,6 @@ vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/spatial_hash
 )
 vcpkg_copy_pdbs()
-file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/spatial_hash RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
