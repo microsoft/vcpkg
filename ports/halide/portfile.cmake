ce78d48358524af9bb1c2078362b0a73ab84c4d4
FrankXie <v-frankxie@microsoft.com>
2024-11-21 08:59:03 +0000

    [Halide] Fix download url

--- ports/halide/portfile.cmake
+++ ports/halide/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO halide/Halide
-    REF "${VERSION}"
+    REF "v${VERSION}"
     SHA512 286cbef25b5cc0f5095cbc80a2fd1cacf369948c58c14406ac6bcc28a7a37c81417d601975083f03670e22276a1886b8801bdc91aa6fe80049a276a1c8fd08b9
     HEAD_REF main
 )
