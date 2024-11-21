b4bc90fdcb3f0853446ce97e5ed0d772a5f7a4e9
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-11-21 23:30:10 +0100

    adjust cmake generator instance

--- ports/msvc/portfile_toolkit.cmake
+++ ports/msvc/portfile_toolkit.cmake
@@ -2,6 +2,8 @@ block(PROPAGATE VCToolkit_VERSION VCToolkit_REDIST_VERSION)
   set(VCToolkit_FILES "")
   include("${CMAKE_CURRENT_LIST_DIR}/download_toolkit.cmake")
   set(prefix VCToolkit)
+  
+  set(vs_base_dir "${CURRENT_PACKAGES_DIR}/VS")
 
   set(vsix_installers "")
   set(msi_installers "")
@@ -26,7 +28,7 @@ block(PROPAGATE VCToolkit_VERSION VCToolkit_REDIST_VERSION)
       math(EXPR counter "${counter} + 1")
       message(STATUS "Extracting '${item}'")
       vcpkg_execute_required_process(
-        COMMAND "${pwsh_exe}" -ExecutionPolicy Bypass -File "${CMAKE_CURRENT_LIST_DIR}/extract-vsix.ps1" "-VsixFile" "${item}" "-ExtractTo" "${CURRENT_PACKAGES_DIR}/VS"
+        COMMAND "${pwsh_exe}" -ExecutionPolicy Bypass -File "${CMAKE_CURRENT_LIST_DIR}/extract-vsix.ps1" "-VsixFile" "${item}" "-ExtractTo" "${vs_base_dir}"
         WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
         LOGNAME "extract_toolkit_${counter}.log"
       )
@@ -47,10 +49,8 @@ block(PROPAGATE VCToolkit_VERSION VCToolkit_REDIST_VERSION)
 
   file(REMOVE_RECURSE "${installFolderSdk}/Program Files")
 
-  file(GLOB vc_redist_version_folder LIST_DIRECTORIES true "${CURRENT_PACKAGES_DIR}/VS/VC/Redist/MSVC/*")
-  cmake_path(GET vc_redist_version_folder FILENAME VCToolkit_REDIST_VERSION)
-  file(GLOB vc_toolkit_version_folder LIST_DIRECTORIES true "${CURRENT_PACKAGES_DIR}/VS/VC/Tools/MSVC/*")
-  cmake_path(GET vc_toolkit_version_folder FILENAME VCToolkit_VERSION)
-endblock()
+  file(COPY_FILE "${vs_base_dir}/VC/Auxiliary/Build/Microsoft.VCToolsVersion.v143.default.txt" "${vs_base_dir}/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt")
 
-#D:\vcpkg_folders\no_msvc\packages\msvc_x64-windows-release\VS\VC\Tools\MSVC\14.41.34120
+  file(STRINGS "${vs_base_dir}/VC/Auxiliary/Build/Microsoft.VCRedistVersion.default.txt" VCToolkit_REDIST_VERSION)
+  file(STRINGS "${vs_base_dir}/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt" VCToolkit_VERSION)
+endblock()
