ac1961585c33db479f06a9c3880cfeadfdc00870
Deshan Xiao <deshanxiao@microsoft.com>
2024-11-26 20:13:01 +0800

    init

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -1,23 +1,10 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_github(
+vcpkg_from_git(
+  URL git@github.com:deshanxiao/orc.git
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO apache/orc
-  REF "v${VERSION}"
-  SHA512 6109b27047b0688566756f9ca6833689cdf0bd7a5086c4212ef1fd6484fb73d9077fa3017137a7fd65f0ab51518c80fc43ab55bd308ad146f6f4b6d97c51b4fc
-  HEAD_REF master
-  PATCHES
-    fix-cmake.patch
+  HEAD_REF deshan/vcpkg
 )
-
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindGTest.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindLZ4.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZSTD.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindProtobuf.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindSnappy.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZLIB.cmake")
-
-set(PROTOBUF_EXECUTABLE "${CURRENT_HOST_INSTALLED_DIR}/tools/protobuf/protoc${VCPKG_HOST_EXECUTABLE_SUFFIX}")
 
 if(VCPKG_TARGET_IS_WINDOWS)
   set(BUILD_TOOLS OFF)
@@ -44,27 +31,16 @@ vcpkg_cmake_configure(
     -DBUILD_JAVA=OFF
     -DINSTALL_VENDORED_LIBS=OFF
     -DBUILD_LIBHDFSPP=OFF
-    -DPROTOBUF_EXECUTABLE:FILEPATH=${PROTOBUF_EXECUTABLE}
     -DSTOP_BUILD_ON_WARNING=OFF
     -DENABLE_TEST=OFF
+    -DORC_PACKAGE_KIND=vcpkg
   MAYBE_UNUSED_VARIABLES
     ENABLE_TEST
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-orc)
+vcpkg_cmake_config_fixup(PACKAGE_NAME orc)
 vcpkg_copy_pdbs()
-
-file(READ "${CURRENT_PACKAGES_DIR}/share/unofficial-orc/unofficial-orc-config.cmake" cmake_config)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/unofficial-orc/unofficial-orc-config.cmake"
-"include(CMakeFindDependencyMacro)
-find_dependency(Snappy CONFIG)
-find_dependency(ZLIB)
-find_dependency(zstd CONFIG)
-find_dependency(lz4 CONFIG)
-find_dependency(Protobuf CONFIG)
-${cmake_config}
-")
 
 file(GLOB TOOLS ${CURRENT_PACKAGES_DIR}/bin/orc-*)
 if(TOOLS)
