ab745ae4a81fe7022872a20c7009ad0d6d0973bc
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-12-01 16:37:27 +0100

    print sdk version

--- scripts/cmake/vcpkg_get_windows_sdk.cmake
+++ scripts/cmake/vcpkg_get_windows_sdk.cmake
@@ -1,6 +1,7 @@
 function(vcpkg_get_windows_sdk out_var)
     if("$ENV{WindowsSDKVersion}" MATCHES [[^([0-9.]*)(\\|/)?$]])
         set("${out_var}" "${CMAKE_MATCH_1}" PARENT_SCOPE)
+        message(STATUS "Found WindowsSDKVersion:${CMAKE_MATCH_1}")
     else()
         message(FATAL_ERROR "Unexpected format for ENV{WindowsSDKVersion} ($ENV{WindowsSDKVersion})")
     endif()
