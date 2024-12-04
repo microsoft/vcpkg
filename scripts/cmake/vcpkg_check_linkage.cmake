ceca62d469f380bad3f15eb1a596eff788ca4dbb
Billy Robert O'Neal III <bion@microsoft.com>
2024-12-03 18:01:18 -0800

    vcpkg_check_linkage: Better explain the ONLY_DYNAMIC_LIBRARY situation
    Related: https://github.com/microsoft/vcpkg/pull/42456

--- scripts/cmake/vcpkg_check_linkage.cmake
+++ scripts/cmake/vcpkg_check_linkage.cmake
@@ -20,10 +20,27 @@ function(vcpkg_check_linkage)
         message(STATUS "Note: ${PORT} only supports static library linkage. Building static library.")
         set(VCPKG_LIBRARY_LINKAGE static PARENT_SCOPE)
     elseif(arg_ONLY_DYNAMIC_LIBRARY AND "${VCPKG_LIBRARY_LINKAGE}" STREQUAL "static")
-        message(STATUS "Note: ${PORT} only supports dynamic library linkage. Building dynamic library.")
-        if("${VCPKG_CRT_LINKAGE}" STREQUAL "static")
-            message(FATAL_ERROR "Refusing to build unexpected dynamic library against the static CRT.
-    If this is desired, please configure your triplet to directly request this configuration.")
+    if("${VCPKG_CRT_LINKAGE}" STREQUAL "static")
+        message(FATAL_ERROR "This port can only build as a dynamic library, but the triplet \
+selects a static library and a static CRT. Building a dynamic library with a static CRT creates \
+conditions many developers find surprising, and for which most ports are unprepared, so vcpkg will \
+not change VCPKG_LIBRARY_LINKAGE to for you in this case.
+For example, on Windows, each DLL will get its own copy of the CRT, meaning such DLLs cannot share \
+standard library components over the DLL boundary. On non-Windows, different .sos or .dylibs may \
+cause mutually incompatible symbols from different CRT versions to be concurrently loaded.
+If you can edit the port calling vcpkg_check_linkage emitting this message, consider adding \
+(!static | !static-crt) to the \"supports\" expression so that this combination can fail early.
+If you are merely consuming this port, you can consider choosing a triplet which selects a dynamic \
+CRT and/or library linkage. If you really know what you're doing, understand the potential \
+problems a static CRT with a dynamic library can cause, and are confident that this port safely \
+handles that configuration and want to proceed anyway, author a custom triplet which explicitly \
+sets VCPKG_LIBRARY_LINKAGE to dynamic and VCPKG_CRT_LINKAGE to static. For example:
+    if(\"\${PORT}\" STREQUAL \"${PORT}\")
+        set(VCPKG_LIBRARY_LINKAGE dynamic)
+        set(VCPKG_CRT_LINKAGE static)
+    endif()")
+        else()
+            message(STATUS "Note: ${PORT} only supports dynamic library linkage. Building dynamic library.")
         endif()
         set(VCPKG_LIBRARY_LINKAGE dynamic PARENT_SCOPE)
     endif()
