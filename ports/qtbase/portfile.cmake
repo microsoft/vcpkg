ecf1ce8511fd83f4be7378378e59939e7c0b1ca0
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2024-12-05 00:16:00 +0100

    remove patch in qtbase

--- ports/qtbase/portfile.cmake
+++ ports/qtbase/portfile.cmake
@@ -22,7 +22,6 @@ set(${PORT}_PATCHES
         fix-host-aliasing.patch
         fix_deploy_windows.patch
         fix-link-lib-discovery.patch
-        fix-missing-symbol.patch
 )
  
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
