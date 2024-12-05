0708895a720afcf4a8f88ea4e5341be2003a02b2
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-12-05 09:01:09 +0000

    fix absolute paths

--- ports/lv2/portfile.cmake
+++ ports/lv2/portfile.cmake
@@ -1,13 +1,9 @@
-set(VCPKG_POLICY_SKIP_ABSOLUTE_PATHS_CHECK enabled)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lv2/lv2
     REF "v${VERSION}"
     SHA512 d63a223b1e1ab9282392637ea2878cfca5dc466553dcea45fb6d8bc5fe657d0705f01db45affcda29344166fba2738a33da5c15ef44ceec58989e406131e1ded
     HEAD_REF main
-    PATCHES
-        fix-dll-install.patch
 )
 
 vcpkg_configure_meson(
@@ -22,6 +18,6 @@ vcpkg_install_meson()
 
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
-
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 
