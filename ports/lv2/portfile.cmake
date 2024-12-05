74518852018b10c3a8aae9ed364e339649d00da0
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-12-05 07:23:01 +0000

    update to  0.24.24

--- ports/lv2/portfile.cmake
+++ ports/lv2/portfile.cmake
@@ -1,4 +1,4 @@
-set(VCPKG_BUILD_TYPE release) # header-only
+set(VCPKG_POLICY_SKIP_ABSOLUTE_PATHS_CHECK enabled)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -6,19 +6,18 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 d63a223b1e1ab9282392637ea2878cfca5dc466553dcea45fb6d8bc5fe657d0705f01db45affcda29344166fba2738a33da5c15ef44ceec58989e406131e1ded
     HEAD_REF main
+    PATCHES
+        fix-dll-install.patch
 )
 
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -Ddocs=disabled
-        -Dplugins=disabled
-        -Dtests=disabled
 )
 
 vcpkg_install_meson()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/lv2-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
