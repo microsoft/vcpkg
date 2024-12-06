a822e2808d634c19e531c87318b1a5740cfc9137
FrankXie <v-frankxie@microsoft.com>
2024-12-06 07:38:59 +0000

    remove usage

--- ports/serd/portfile.cmake
+++ ports/serd/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_configure_meson(
 )
 
 vcpkg_install_meson()
+vpckg_fixup_pkgconfig()
 
 vcpkg_copy_tools(TOOL_NAMES serdi AUTO_CLEAN)
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
