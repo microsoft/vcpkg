7e12d12f8d672b41e090a8f2372efeaa20594264
Jim-Wang <wangzhijie05@beyondsoft.com>
2024-12-06 03:05:43 +0000

    fix build type

--- ports/lv2/portfile.cmake
+++ ports/lv2/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 d63a223b1e1ab9282392637ea2878cfca5dc466553dcea45fb6d8bc5fe657d0705f01db45affcda29344166fba2738a33da5c15ef44ceec58989e406131e1ded
     HEAD_REF main
-    PATCHES
-        fix-dll-install.patch
 )
 
 vcpkg_configure_meson(
