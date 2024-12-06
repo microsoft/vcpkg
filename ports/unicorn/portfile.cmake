044a1ffbdb2daf28f629ffedd10cf8ec09a600a8
さくらみこ <lin@sz.cn.eu.org>
2024-12-07 00:41:57 +0300

    bump unicorn ver

--- ports/unicorn/portfile.cmake
+++ ports/unicorn/portfile.cmake
@@ -1,15 +1,8 @@
-if(VCPKG_CRT_LINKAGE STREQUAL "dynamic" AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    message(FATAL_ERROR "unicorn can currently only be built with /MT or /MTd (static CRT linkage)")
-endif()
-
-# Note: this is safe because unicorn is a C library and takes steps to avoid memory allocate/free across the DLL boundary.
-set(VCPKG_CRT_LINKAGE "static")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO unicorn-engine/unicorn
-    REF "${VERSION}.post1"
-    SHA512 8694d6bc92e3424a8ad050316413d53e56e0f55e7cad7517fb3e98e670a0f1768b060ead8f195da13607cec89a964364f05a8b9d0dc074f4ac5e51026f8343ad
+    REF "${VERSION}"
+    SHA512 d6184b87a0fb729397ec2ac2cb8bfd9d10c9d4276e49efa681c66c7c54d1a325305a920332a708e68989cc299d0d1a543a1ceeaf552a9b44ec93084f7bf85ef2
     HEAD_REF master
     PATCHES
         fix-build.patch
