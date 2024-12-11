34c960c45617b7c298b7ebded3293ca2e11865ff
Kai Pastor <dg0yt@darc.de>
2024-12-11 21:15:36 +0100

    [juce] Fix test

--- scripts/test_ports/vcpkg-ci-juce/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-juce/portfile.cmake
@@ -1,10 +1,8 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-vcpkg_find_acquire_program(PKGCONFIG)
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${CURRENT_PORT_DIR}/project"
     OPTIONS
-        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+        "-DWITH_CURL=${VCPKG_TARGET_IS_LINUX}"
 )
 vcpkg_cmake_build()
