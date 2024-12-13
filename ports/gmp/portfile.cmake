dd6c0c9739eabfcfcd2463cfafdd60894a3d0326
Javier Matos <javiermatos@Javiers-Laptop.local>
2024-12-13 15:55:26 -0500

    Restore GMP to what is in master

--- ports/gmp/portfile.cmake
+++ ports/gmp/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_extract_source_archive(SOURCE_PATH
         subdirs.patch
         msvc_symbol.patch
         arm64-coff.patch
+        remove_compiler_info.patch
 )
 
 vcpkg_list(SET OPTIONS)
@@ -30,44 +31,66 @@ endif()
 
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
     vcpkg_list(APPEND OPTIONS
+        "ac_cv_func_memset=yes"
         "gmp_cv_asm_w32=.word"
         "gmp_cv_check_libm_for_build=no"
     )
 endif()
 
 set(disable_assembly OFF)
-set(languages "C;CXX")
-if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+set(ccas "")
+set(asmflags "-c")
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+if(VCPKG_DETECTED_CMAKE_C_COMPILER_ID STREQUAL "MSVC")
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+        string(APPEND asmflags " --target=i686-pc-windows-msvc")
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        string(APPEND asmflags " --target=x86_64-pc-windows-msvc")
     elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        string(APPEND asmflags " --target=arm64-pc-windows-msvc")
     else()
         set(disable_assembly ON)
+    endif()
+    if(NOT disable_assembly)
+        vcpkg_find_acquire_program(CLANG)
+        set(ccas "${CLANG}")
     endif()
 elseif(VCPKG_TARGET_IS_MINGW AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     # not exporting asm functions
     set(disable_assembly ON)
+elseif(VCPKG_TARGET_IS_LINUX AND VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(ccas "${VCPKG_DETECTED_CMAKE_C_COMPILER}")
+    vcpkg_list(APPEND OPTIONS "ABI=32")
+    string(APPEND asmflags " -m32")
+else()
+    set(ccas "${VCPKG_DETECTED_CMAKE_C_COMPILER}")
 endif()
 
 if(disable_assembly)
     vcpkg_list(APPEND OPTIONS "--enable-assembly=no")
-else()
-    list(APPEND languages "ASM")
+elseif(ccas)
+    cmake_path(GET ccas PARENT_PATH ccas_dir)
+    vcpkg_add_to_path("${ccas_dir}")
+    cmake_path(GET ccas FILENAME ccas_command)
 endif()
+vcpkg_list(APPEND OPTIONS "CCAS=${ccas_command}" "ASMFLAGS=${asmflags}")
 
 if(VCPKG_CROSSCOMPILING)
     set(ENV{HOST_TOOLS_PREFIX} "${CURRENT_HOST_INSTALLED_DIR}/manual-tools/${PORT}")
 endif()
 
- # dumpbin detection fails with autoconf 2.72
 if(VCPKG_HOST_IS_WINDOWS)
+    # dumpbin detection fails with autoconf 2.72
     set(ENV{WANT_AUTOCONF} 2.71)
 endif()
-
-vcpkg_make_configure(
-    AUTORECONF
+vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
-    LANGUAGES ${languages}
+    AUTOCONFIG
+    ADDITIONAL_MSYS_PACKAGES
+        DIRECT_PACKAGES
+            "https://mirror.msys2.org/msys/x86_64/autoconf2.71-2.71-3-any.pkg.tar.zst"
+            dd312c428b2e19afd00899eb53ea4255794dea4c19d1d6dea2419cb6a54209ea2130d48abbc20af12196b9f628143436f736fbf889809c2c2291be0c69c0e306
     OPTIONS
         ${OPTIONS}
         --enable-cxx
@@ -75,7 +98,7 @@ vcpkg_make_configure(
         --with-readline=no
         "gmp_cv_prog_exeext_for_build=${VCPKG_HOST_EXECUTABLE_SUFFIX}"
 )
-vcpkg_make_install()
+vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
 
 if(NOT VCPKG_CROSSCOMPILING)
