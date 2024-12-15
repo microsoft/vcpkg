73e2542a52ebac1b3b7b708f4c557e482b7cd72b
Kai Pastor <dg0yt@darc.de>
2024-12-15 10:13:47 +0100

    Fixup

--- ports/gettext/portfile.cmake
+++ ports/gettext/portfile.cmake
@@ -28,6 +28,7 @@ vcpkg_extract_source_archive(SOURCE_PATH
         subdirs.patch
         parallel-gettext-tools.patch
         config-step-order.patch
+        iconv-ostream-flush.diff
 )
 
 set(subdirs "")
