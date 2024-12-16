1a712966dc1f07b7b165c8b7db106d9a36f14226
Jon <v-zhli17@microsoft.com>
2024-12-15 18:48:36 -0800

    remove patch

--- ports/aurora-au/portfile.cmake
+++ ports/aurora-au/portfile.cmake
@@ -6,11 +6,14 @@ vcpkg_from_github(
     REF "${VERSION}"
     SHA512 7e78ac2502037ad87d3171c366c8c91b030678af26d6db75cf12384faa077e40b159b570ddd774b76bc7ca34d74d1898958b6d6b33d56a8eb5ca1f96c1a9ed66
     HEAD_REF main
-    PATCHES 
-        disable-googletest.patch
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DAU_EXCLUDE_GTEST_DEPENDENCY=1
+    )
+
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
