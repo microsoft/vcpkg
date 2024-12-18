d6e7b6ae84505135db974f4e05f2c7949bd9fa18
Kai Pastor <dg0yt@darc.de>
2024-12-18 08:14:52 +0100

    WIP

--- scripts/test_ports/vcpkg-ci-fortran/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-fortran/portfile.cmake
@@ -4,7 +4,24 @@ vcpkg_cmake_get_vars(cmake_vars_file)
 include("${cmake_vars_file}")
 
 include(vcpkg_find_fortran)
-vcpkg_find_fortran(FORTRAN_CMAKE)
+# Side effects!
+block(SCOPE_FOR VARIABLES
+    PROPAGATE
+        FORTRAN_CMAKE
+        VCPKG_CRT_LINKAGE
+        VCPKG_LIBRARY_LINKAGE
+        VCPKG_POLICY_SKIP_DUMPBIN_CHECKS
+        VCPKG_USE_INTERNAL_Fortran
+    # DO NOT PROPAGATE:
+    #   VCPKG_CHAINLOAD_TOOLCHAIN_FILE
+)
+    vcpkg_find_fortran(FORTRAN_CMAKE)
+endblock()
+
+if(VCPKG_USE_INTERNAL_Fortran)
+    list(FILTER FORTRAN_CMAKE EXCLUDE REGEX "-DCMAKE_C_COMPILER=")
+    list(APPEND FORTRAN_CMAKE --trace-expand)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${CURRENT_PORT_DIR}/project"
