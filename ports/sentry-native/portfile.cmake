cfb8b40ef5e945e047db478a5daf1421386b5693
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2024-12-18 12:16:24 +0100

    [sentry-native] update to 0.7.17
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- ports/sentry-native/portfile.cmake
+++ ports/sentry-native/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/getsentry/sentry-native/releases/download/${VERSION}/sentry-native.zip"
     FILENAME "sentry-native-${VERSION}.zip"
-    SHA512 a7e363d6ce119d88a34c3503107c338eb59f0faaedb107f66ebbc7ce4cb89500ff53fc9fad4ffdde5aa5235f6d72e9ed4bf978432e35752340dc1735518d8b0d
+    SHA512 559e831720e27ed2bf0702c7fc29a3c2dfa553e2c74b8c7adbc3f5071a5eea0e434127fc8cf9d8f29aa668ab627c84dc13194227203aab085a437684e4591895
 )
 
 vcpkg_extract_source_archive(
