9c70ba4a68e3eb2296da794b3d9e2b69308ef246
Saikari <lin@sz.cn.eu.org>
2024-12-18 16:34:12 +0300

    Update ports/omath/portfile.cmake
    Co-authored-by: Mengna-Li <543250287@qq.com>

--- ports/omath/portfile.cmake
+++ ports/omath/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO orange-cpp/omath
-    REF v${VERSION}
+    REF "v${VERSION}"
     SHA512 467b1abbdf5b9a7f49ed50824eaa4641f05d6088e84f40320b5c82a1bdbf685cc8d0f0a4f4ab6be49e3a8ed13103ee3e808dde3b556a00742f7b53c519c183e3
     HEAD_REF master
 )
