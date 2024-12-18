1575b189a95cf421dcab3018d94d4fe54d02aa10
さくらみこ <lin@sz.cn.eu.org>
2024-12-19 01:55:40 +0300

    test

--- scripts/test_ports/vcpkg-ci-lief/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-lief/project/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <LIEF/LIEF.hpp>
 #include <LIEF/version.h>
+#include <memory>
 using namespace std;
 using namespace LIEF;
 int main()
 {
+   std::unique_ptr<assembly::Instruction> inst = get_inst();
+   std::cout << "address = " << inst.value().address() << std::endl;
    std::cout << "access flags public = " << LIEF::DEX::to_string(LIEF::DEX::access_flags_list[1]) << std::endl;
    std::cout << "Version = " << LIEF_VERSION << std::endl;
    return 0;
