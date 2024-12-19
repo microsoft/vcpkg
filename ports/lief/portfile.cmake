e512c2716732fec83c25fe6ed57cbcfe94168b70
さくらみこ <lin@sz.cn.eu.org>
2024-12-19 07:39:16 +0300

    rollback, check only pkgconfig, remove usage

--- ports/lief/portfile.cmake
+++ ports/lief/portfile.cmake
@@ -100,7 +100,10 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/LIEF")
 
-
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/lief/LIEFConfig.cmake"
+    [[include("${LIEF_${lib_type}_export}")]]
+    [[include("${CMAKE_CURRENT_LIST_DIR}/LIEFExport-${lib_type}.cmake")]]
+)
 
 vcpkg_fixup_pkgconfig()
 
@@ -109,5 +112,4 @@ file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
