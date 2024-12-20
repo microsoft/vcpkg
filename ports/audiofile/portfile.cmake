393ebe70181d385ae21b27878a7b851e06afb8bd
xiaozhuai <798047000@qq.com>
2024-12-20 17:13:35 +0800

    fix

--- ports/audiofile/portfile.cmake
+++ ports/audiofile/portfile.cmake
@@ -6,8 +6,6 @@ vcpkg_from_github(
     REF "${VERSION}"
     SHA512 ee8af7687fe420634ea8dacef7ecc0c4d3dd1de13c6131202710b3055fdc5ffb27ac1e8ff034690a3ce5d512b6182a788adfa5852a29ac532a08322b14083e8a
     HEAD_REF master
-    PATCHES
-        fix-cmakeLists.patch
 )
 
 vcpkg_cmake_configure(
@@ -19,8 +17,9 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME AudioFile)
+vcpkg_cmake_config_fixup(PACKAGE_NAME AudioFile CONFIG_PATH lib/cmake/AudioFile)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
