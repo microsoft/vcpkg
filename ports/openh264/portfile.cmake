608f50d8142534f8b997888b15865c703101b608
tartanpaint <35690467+tartanpaint@users.noreply.github.com>
2024-12-24 17:18:22 +0000

    Update pkg-config patch to preserve lib type fix

--- ports/openh264/portfile.cmake
+++ ports/openh264/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 cb6d3ca8d5277325dd64dec399421c4c62bc1fd012fe1521d7195e95ce7f59527919cf698829044dca3d9b1d8288c49b49111d01c9d2896c819da806492af838
     PATCHES
-        revert-pkgconfig-changes.patch  # vcpkg fix following https://github.com/cisco/openh264/pull/3351
+        revert-pkgconfig-changes.patch  # vcpkg fix following https://github.com/cisco/openh264/pull/3351, specifically https://github.com/cisco/openh264/pull/3351/commits/e614eb9cdfc02c5e01b72f6415b975d8576885e9
 )
 
 if((VCPKG_TARGET_ARCHITECTURE STREQUAL "x86" OR VCPKG_TARGET_ARCHITECTURE STREQUAL "x64"))
