8001826bc15badb577cbd928bc4d3a0c02273270
LilyWangLL <494550702@qq.com>
2024-12-25 00:29:20 -0800

    2.5.0-b.27

--- ports/libodb/portfile.cmake
+++ ports/libodb/portfile.cmake
@@ -1,14 +1,12 @@
 vcpkg_download_distfile(ARCHIVE
     URLS "https://pkg.cppget.org/1/beta/odb/libodb-${VERSION}.tar.gz"
     FILENAME "libodb-${VERSION}.tar.gz"
-    SHA512 f99eba87130f7c3ed0b707e1f4efdb839c97c221fee24056d955072767c36106297abe76e5f82054cf5bc3bf0fda631e7c92e4943645d6ff2be57831006505ef
+    SHA512 d8091686e7d99345025754b7ce777c8fd000a355235064c45163cfff328fcac7b20b9eb145418a02b8ecec7b9c775efd3a94272e63512ba4f0181784391f5a92
 )
 
 vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
-    PATCHES
-        fix-linux.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
