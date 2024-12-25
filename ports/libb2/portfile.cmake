d3eb4af87ed23c3e76e46c310246ff52737d7971
paperchalice <liujunchang97@outlook.com>
2024-12-25 19:22:40 +0800

    [libb2] Fix Windows build

--- ports/libb2/portfile.cmake
+++ ports/libb2/portfile.cmake
@@ -4,11 +4,15 @@ vcpkg_from_github(
     REF 2c5142f12a2cd52f3ee0a43e50a3a76f75badf85
     SHA512 cf29cf9391ae37a978eb6618de6f856f3defa622b8f56c2d5a519ab34fd5e4d91f3bb868601a44e9c9164a2992e80dde188ccc4d1605dffbdf93687336226f8d
     HEAD_REF master
+    PATCHES
+        fix-msvc-cpuid.patch
 )
 
-set(OPTIONS)
-if(CMAKE_HOST_WIN32)
-    set(OPTIONS --disable-native) # requires cpuid
+# Ensure we can detect cpu features on {x64,x86}-windows.
+if (VCPKG_HOST_IS_WINDOWS AND (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86") AND VCPKG_CROSSCOMPILING)
+set(pre_run PRERUN_SHELL
+    "sed -i 's/cross_compiling=yes/cross_compiling=no/g' configure"
+)
 endif()
 
 vcpkg_configure_make(
@@ -16,7 +20,7 @@ vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         ax_cv_check_cflags___O3=no # see https://github.com/microsoft/vcpkg/pull/17912#issuecomment-840514179
-        ${OPTIONS}
+    ${pre_run}
 )
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
