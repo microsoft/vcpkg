d0472beda443c6f0f4e156a8fdf95b2156b18596
l.feng <43399351+msclock@users.noreply.github.com>
2024-12-25 15:47:38 +0800

    Disable TIME_UTC for android

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -9,6 +9,17 @@ vcpkg_from_github(
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/7zip-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+
+if(VCPKG_TARGET_IS_ANDROID)
+    message(STATUS "Disable TIME_UTC on android")
+    vcpkg_replace_string("${SOURCE_PATH}/CPP/Windows/TimeUtils.cpp"
+        [[if defined(TIME_UTC)]]
+        [[if defined(TIME_UTC) && !defined(__ANDROID__)]]
+    )
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
