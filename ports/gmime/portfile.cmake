e76b94604ddb8cd74ab0523a2a69207baece6d08
Kai Pastor <dg0yt@darc.de>
2024-12-27 12:58:37 +0100

    iconv-detect.h [skip actions]

--- ports/gmime/portfile.cmake
+++ ports/gmime/portfile.cmake
@@ -9,8 +9,16 @@ vcpkg_extract_source_archive(
     ARCHIVE "${ARCHIVE}"
 )
 
-set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/aclocal/\"")
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/aclocal/\"") # for glib
 set(ENV{GTKDOCIZE} true)
+
+vcpkg_list(SET options)
+set(iconv_detect_h "${CURRENT_HOST_INSTALLED_DIR}/share/${PORT}/iconv-detect-${VCPKG_CMAKE_SYSTEM_NAME}.h")
+if(EXISTS "${iconv_detect_h}")
+    vcpkg_list(APPEND options "ac_cv_have_iconv_detect_h=yes")
+elseif(VCPKG_CROSSCOMPILING)
+    vcpkg_list(APPEND options "ac_cv_have_iconv_detect_h=no")
+endif()
 
 vcpkg_configure_make(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -21,6 +29,13 @@ vcpkg_configure_make(
         --disable-introspection
         --disable-vala
 )
+
+if(EXISTS "${iconv_detect_h}")
+    file(COPY_FILE "${iconv_detect_h}" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/iconv-detect.h")
+    if(NOT VCPKG_BUILD_TYPE)
+        file(COPY_FILE "${iconv_detect_h}" "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/iconv-detect.h")
+    endif()
+endif()
 
 if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
     file(COPY "${SOURCE_PATH}/build/vs2017/unistd.h" DESTINATION "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
@@ -35,4 +50,14 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
+if(NOT VCPKG_CROSSCOMPILING)
+    file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/iconv-detect.h"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+        RENAME "iconv-detect-${VCPKG_CMAKE_SYSTEM_NAME}.h"
+    )
+endif()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
+file(INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/iconv-detect.h" DESTINATION "${CURRENT_BUILDTREES_DIR}" RENAME "iconv-detect.h-${TARGET_TRIPLET}.log")
+message(FATAL_ERROR STOP)
