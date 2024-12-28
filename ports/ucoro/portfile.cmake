9c3ca4f47a70ab700c6b7511036905c108d8eec9
miyanyan <1138989048@qq.com>
2024-12-28 12:26:14 +0800

    remove empty debug folder

--- ports/ucoro/portfile.cmake
+++ ports/ucoro/portfile.cmake
@@ -19,8 +19,7 @@ vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE 
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/debug"
 )
 
 file(INSTALL "${SOURCE_PATH}/LICENSE_1_0.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
