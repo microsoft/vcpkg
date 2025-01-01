27b58ba0a1f4af7d60462af8ab4b802166b64aed
Christian Panov <christianpanov@abv.bg>
2025-01-01 18:13:15 +0200

    Added lwlog library

---
+++ ports/lwlog/portfile.cmake
@@ -0,0 +1,20 @@
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO ChristianPanov/lwlog
+    REF master
+    SHA512 439ebc237eb7b26d073d29f012c40538beb8a9ca925feb2f85e2b8a6a984402623880288c5e7d6d4a897998ec32ad2180d52e3eae00526522dc206f7c8e469d4
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_TESTS=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
