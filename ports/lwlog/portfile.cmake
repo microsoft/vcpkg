fa4a690ff297574f4609a60ffe4092f7952f6f95
Christian Panov <christianpanov@abv.bg>
2025-01-01 19:10:20 +0200

    Fixes

--- ports/lwlog/portfile.cmake
+++ ports/lwlog/portfile.cmake
@@ -7,11 +7,7 @@ vcpkg_from_github(
     SHA512 439ebc237eb7b26d073d29f012c40538beb8a9ca925feb2f85e2b8a6a984402623880288c5e7d6d4a897998ec32ad2180d52e3eae00526522dc206f7c8e469d4
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS
-        -DBUILD_TESTS=OFF
-)
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
 vcpkg_cmake_install()
 
