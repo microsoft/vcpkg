5c9a37ceac1390392b81e280718986533610a2d6
Kai Pastor <dg0yt@darc.de>
2025-01-01 07:15:45 +0100

    Roll msh3 [skip actions]

--- ports/msh3/portfile.cmake
+++ ports/msh3/portfile.cmake
@@ -3,12 +3,11 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nibanks/msh3
-    REF #[[ v${VERSION} ]] 3b471e9bef5c486df2f5a3e2d220f5b76d0ba705
-    SHA512 9d6da0eb116dd051b7de78aa9edd47af04de05cb1915a3378f7cf96f8497156e486f1ff9ccb55ed02e2d12c1bfd84fd9a962cadcff8a66954102dcdd36b7250d
+    REF #[[ v${VERSION} ]] ba595031e093b479beaa0f410cb70405bc5fd067
+    SHA512 e882079ab7996c458af8af9c5d828172217591832621bd2a4353a6a6821dc16cf221253a3041e42e94bf9972bcd6a812d8fbe8eb1a02c84cb21cd95edf29fe66
     HEAD_REF main
     PATCHES
         win32-crt.diff
-        wip.diff
 )
 
 vcpkg_cmake_configure(
@@ -17,17 +16,11 @@ vcpkg_cmake_configure(
         -DMSH3_INSTALL_PKGCONFIG=ON
         -DMSH3_USE_EXTERNAL_LSQPACK=ON
         -DMSH3_USE_EXTERNAL_MSQUIC=ON
-        # WIP
-        -DMSH3_TEST=ON
-        -DMSH3_TOOL=ON
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup()
-
-# WIP
-vcpkg_copy_tools(TOOL_NAMES msh3app msh3test  AUTO_CLEAN)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
