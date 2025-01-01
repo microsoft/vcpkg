25e24149951c4713a27c505dc609392786fe363f
Kai Pastor <dg0yt@darc.de>
2025-01-01 09:27:21 +0100

    Revise patches and portfile

--- ports/rtlsdr/portfile.cmake
+++ ports/rtlsdr/portfile.cmake
@@ -1,18 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO osmocom/rtl-sdr
-    REF v2.0.2
+    REF v${VERSION}
     SHA512 20a1630df7d4da5d263c5ffd4d83a7c2a6fc674e3838bf02b2b59c1da8d946dafc48790d410ab2fcbc0362c2ac70e5cdcae9391c5f04803bf2cdddafd6f58483
     HEAD_REF master
+    PATCHES
+        dependencies.diff
+        library-linkage.diff
 )
 
-file(
-    COPY "${CMAKE_CURRENT_LIST_DIR}/Findlibusb.cmake"
-    DESTINATION "${SOURCE_PATH}/cmake/Modules"
-)
+vcpkg_find_acquire_program(PKGCONFIG)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+        "-DCMAKE_REQUIRE_FIND_PACKAGE_PkgConfig=1"
 )
 
 vcpkg_cmake_install()
@@ -20,19 +23,8 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/rtlsdr)
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+vcpkg_copy_tools(TOOL_NAMES rtl_adsb rtl_biast rtl_eeprom rtl_fm rtl_power rtl_sdr rtl_tcp rtl_test  AUTO_CLEAN)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-else()
-    file(GLOB DEBUG_TOOLS "${CURRENT_PACKAGES_DIR}/debug/bin/*.exe")
-    file(GLOB RELEASE_TOOLS "${CURRENT_PACKAGES_DIR}/bin/*.exe")
-    file(
-        INSTALL ${RELEASE_TOOLS}
-        DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}"
-    )
-    vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}")
-    file(REMOVE ${DEBUG_TOOLS} ${RELEASE_TOOLS})
-endif()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
