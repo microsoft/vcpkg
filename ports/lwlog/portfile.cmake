67dc5f3412864f11ff370e7ea5f3b0cab98fd8e3
Christian Panov <christianpanov@abv.bg>
2025-01-02 11:28:52 +0200

    Update ports/lwlog/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/lwlog/portfile.cmake
+++ ports/lwlog/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
     REPO ChristianPanov/lwlog
     REF "v${VERSION}"
     SHA512 439ebc237eb7b26d073d29f012c40538beb8a9ca925feb2f85e2b8a6a984402623880288c5e7d6d4a897998ec32ad2180d52e3eae00526522dc206f7c8e469d4
+    HEAD_REF master
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
