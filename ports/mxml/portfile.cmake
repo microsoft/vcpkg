cc4b1600dc445fe5fd20f1af7849884871e3b5bc
Ross maxx <mrroshan127@gmail.com>
2025-01-02 11:23:47 +0530

    checksum

--- ports/mxml/portfile.cmake
+++ ports/mxml/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO michaelrsweet/mxml
     REF fd47c7d115191c8a6bce2c781ffee41e179530f2 # 3.3.1
-    SHA512 0
+    SHA512 29bb9785de32d3ba72535179856247f9b4ef466857f42a0576ed88175c807ca64fe4f9ee4cfef1ba57fb44780af26649316fb7baa1770e7d298d1cf13dff17cc
     HEAD_REF master
 )
 
