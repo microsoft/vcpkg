452b083da44479f62ec1266d6a0ccce02c203f10
Kai Pastor <dg0yt@darc.de>
2025-01-05 00:24:17 +0100

    [bullet3] Move CMake package into share/bullet3

--- ports/bullet3/portfile.cmake
+++ ports/bullet3/portfile.cmake
@@ -38,9 +38,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME bullet CONFIG_PATH share/bullet)
-
+vcpkg_cmake_config_fixup(CONFIG_PATH share/bullet)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
