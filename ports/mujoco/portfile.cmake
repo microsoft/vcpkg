8c1e29bb0b0ebbdc68e0049c4fbf0d49aa3de75d
Tal Regev <tal.regev@gmail.com>
2025-01-06 13:20:59 +0200

    Update mujoco to 2.3.7
    Signed-off-by: Tal Regev <tal.regev@gmail.com>

--- ports/mujoco/portfile.cmake
+++ ports/mujoco/portfile.cmake
@@ -2,9 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO deepmind/mujoco
     REF ${VERSION}
-    SHA512 518b9ae09ea7cd4d2095df1be985a30957c6d57846933e111c326e45b61ac002bea014cd0d7902653c85e61ef0ba156b3324eff0edb5613467fe8e07e92dd6da
+    SHA512 40e73339b65f051e9774f5d7f8ca0984b5bdce52fcb46a5e5a8449efc09d7eb5441b918d07ebd8b5cf2f69616ff1a8e32d6b5dab76c9440da1a5e6ce3328c261
     PATCHES
         fix_dependencies.patch
+        fix_x86.patch
 )
 
 vcpkg_cmake_configure(
