983a06376fdebe14a2fecac6b2a27e1622f32656
Ziya Suzen <ziya@suzen.net>
2025-01-08 19:12:29 +0000

    [cnats] Fix Android test warning patch

--- ports/cnats/portfile.cmake
+++ ports/cnats/portfile.cmake
@@ -7,6 +7,7 @@ vcpkg_from_github(
     PATCHES
         fix-sodium-dep.patch
         fix_install_path.patch
+        fix_android_test_warning.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
