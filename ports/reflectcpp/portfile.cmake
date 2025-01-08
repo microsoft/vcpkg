cbefffffb3ac8ec3a6c13abe4616f38fed39ce71
Dr. Patrick Urbanke <patrick@getml.com>
2025-01-08 20:40:28 +0100

    Remove TOML support (for now_

--- ports/reflectcpp/portfile.cmake
+++ ports/reflectcpp/portfile.cmake
@@ -18,7 +18,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         cbor                REFLECTCPP_CBOR
         flexbuffers         REFLECTCPP_FLEXBUFFERS
         msgpack             REFLECTCPP_MSGPACK
-        toml                REFLECTCPP_TOML
         ubjson              REFLECTCPP_UBJSON
         xml                 REFLECTCPP_XML
         yaml                REFLECTCPP_YAML
