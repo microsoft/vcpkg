8b10afd0f08a3acf65c7580c7c157eadeaca3a28
Saikari <lin@sz.cn.eu.org>
2025-01-09 18:48:24 +0300

    fixup test port

--- scripts/test_ports/vcpkg-ci-libmem/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-libmem/portfile.cmake
@@ -1,3 +1,8 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-vcpkg_cmake_configure(SOURCE_PATH "${CURRENT_PORT_DIR}/project")
+vcpkg_find_acquire_program(PKGCONFIG)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${CURRENT_PORT_DIR}/project"
+    OPTIONS
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
+)
 vcpkg_cmake_build()
