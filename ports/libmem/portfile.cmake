e4e98ae60f08a80151382eccf142352fde550e8d
Saikari <lin@sz.cn.eu.org>
2025-01-09 12:10:43 +0300

    fmt

--- ports/libmem/portfile.cmake
+++ ports/libmem/portfile.cmake
@@ -18,8 +18,5 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 vcpkg_copy_pdbs()
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
