373dcab07873adcc648c86c6cbcee16f5dd9a164
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2025-01-10 07:58:46 -0500

    [anari] Remove now unused bin and debug/bin folder removal
    Nothing gets installed in there with the new set of flags.

--- ports/anari/portfile.cmake
+++ ports/anari/portfile.cmake
@@ -42,13 +42,6 @@ file(REMOVE_RECURSE
   "${CURRENT_PACKAGES_DIR}/share/anari/code_gen/__pycache__"
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE
-      "${CURRENT_PACKAGES_DIR}/bin"
-      "${CURRENT_PACKAGES_DIR}/debug/bin"
-  )
-endif()
-
 vcpkg_install_copyright(
   FILE_LIST "${SOURCE_PATH}/LICENSE"
 )
