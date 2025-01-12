72e7ce42e1060e4f33d6333e906f23f8d9d3dcd7
Tal Regev <tal.regev@gmail.com>
2025-01-12 21:10:22 +0200

    Add gz-fuel_tools10
    Signed-off-by: Tal Regev <tal.regev@gmail.com>

---
+++ ports/gz-fuel-tools10/portfile.cmake
@@ -0,0 +1,11 @@
+set(PACKAGE_NAME fuel-tools)
+
+ignition_modular_library(
+   NAME ${PACKAGE_NAME}
+   REF ${PORT}_${VERSION}
+   VERSION ${VERSION}
+   SHA512 a87bb313c17c9624e49cdf39399f90630256155fc410518aa03b340cc0b26e8955fa2d72623974aa4b474c270a25451989a293aaa015b43642f1018aeff8fdd4
+   OPTIONS 
+   PATCHES
+      remove_docs.patch
+)
