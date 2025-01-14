82cd8935326269a4de1c2f937f3bbccd4f0b9915
tartanpaint <35690467+tartanpaint@users.noreply.github.com>
2025-01-14 11:16:06 +0000

    Reference the official patch submitted to rapidxml for https://sourceforge.net/p/rapidxml/bugs/16/ with path tweaks for applying via vcpkg

--- ports/rapidxml/portfile.cmake
+++ ports/rapidxml/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_sourceforge(
     FILENAME "rapidxml-${VERSION}.zip"
     SHA512 6c10583e6631ccdb0217d0a5381172cb4c1046226de6ef1acf398d85e81d145228e14c3016aefcd7b70a1db8631505b048d8b4f5d4b0dbf1811d2482eefdd265
     PATCHES
-        Fix-clang-gcc-compilation.patch # Fix clang and gcc builds
+        0001-fix-for-a-bug-in-gcc-that-won-t-let-rapidxml-compile.patch # https://sourceforge.net/p/rapidxml/bugs/16/
 )
 
 # Handle copyright
