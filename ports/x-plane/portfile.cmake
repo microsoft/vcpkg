5ca437c94884760efbb987adbb9eea4ae91ce6ea
Monica <v-liumonica@microsoft.com>
2025-01-14 22:41:35 -0800

    Fix download address

--- ports/x-plane/portfile.cmake
+++ ports/x-plane/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 set(XPSDK_VERSION "401")
 vcpkg_download_distfile(
     XPLANE_SDK_ZIP
-    URLS "https://developer.x-plane.com/wp-content/plugins/code-sample-generation/sample_templates/XPSDK${XPSDK_VERSION}.zip"
+    URLS "https://developer.x-plane.com/wp-content/plugins/code-sample-generation/sdk_zip_files/XPSDK${XPSDK_VERSION}.zip"
     FILENAME "XPSDK${XPSDK_VERSION}.zip"
     SHA512 8e00789befd15f5b1cb4f426ddf9c3f7f021c5fba50b907e8af5fbf09abbc362804b5d1543332855d01e8ae91b9c50a55933e63df6e11e88e58c10ca8f949bf4
 )
