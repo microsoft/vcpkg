ec7b0db2f86c667329e5506d7d7a3868c4175b91
n0f4ph4mst3r <80906258+n0f4ph4mst3r@users.noreply.github.com>
2025-01-23 14:14:37 +0300

    Update portfile
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/libva/portfile.cmake
+++ ports/libva/portfile.cmake
@@ -19,5 +19,5 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
