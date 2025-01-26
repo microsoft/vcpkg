00ed0e680a34e5e6457b879fba60b880924070f1
Ross maxx <mrroshan127@gmail.com>
2025-01-26 15:21:30 +0530

    bumped to mxml 4

--- ports/mxml/portfile.cmake
+++ ports/mxml/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO michaelrsweet/mxml
-    REF fd47c7d115191c8a6bce2c781ffee41e179530f2 # 3.3.1
+    REF 0d5afc4278d7a336d554602b951c2979c3f8f296 # 4.0.4
     SHA512 29bb9785de32d3ba72535179856247f9b4ef466857f42a0576ed88175c807ca64fe4f9ee4cfef1ba57fb44780af26649316fb7baa1770e7d298d1cf13dff17cc
     HEAD_REF master
 )
