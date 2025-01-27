2ac272374c057e2432d6baf6cf49814b76bcbf29
Kai Pastor <dg0yt@darc.de>
2025-01-27 20:30:54 +0100

    [sdl3-image] Cleanup

--- ports/sdl3-image/portfile.cmake
+++ ports/sdl3-image/portfile.cmake
@@ -26,29 +26,19 @@ vcpkg_cmake_configure(
         -DSDLIMAGE_SAMPLES=OFF
         -DSDLIMAGE_VENDORED=OFF
 )
-
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
 
 if(EXISTS "${CURRENT_PACKAGES_DIR}/cmake")
     vcpkg_cmake_config_fixup(PACKAGE_NAME SDL3_image CONFIG_PATH cmake)
-elseif(EXISTS "${CURRENT_PACKAGES_DIR}/SDL3_image.framework/Resources")
-    vcpkg_cmake_config_fixup(PACKAGE_NAME SDL3_image CONFIG_PATH SDL3_image.framework/Resources)
 else()
     vcpkg_cmake_config_fixup(PACKAGE_NAME SDL3_image CONFIG_PATH lib/cmake/SDL3_image)
-endif()
-
-vcpkg_fixup_pkgconfig()
-
-if(NOT VCPKG_TARGET_IS_LINUX AND NOT VCPKG_TARGET_IS_ANDROID AND NOT VCPKG_BUILD_TYPE)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/SDL3-image.pc" "-lSDL3_image" "-lSDL3_imaged")
 endif()
 
 file(REMOVE_RECURSE 
     "${CURRENT_PACKAGES_DIR}/debug/share"
     "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/SDL3_image.framework"
-    "${CURRENT_PACKAGES_DIR}/debug/SDL3_image.framework"
 )
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
