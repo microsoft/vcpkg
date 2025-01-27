707982004138b530c42d14eddcc8c3947dc150a5
Timur Chernykh <tim.cherry.co@gmail.com>
2025-01-27 16:17:59 +0300

    [VMAware-VM-detection] updated to version 2.0

--- ports/vmaware-vm-detection/portfile.cmake
+++ ports/vmaware-vm-detection/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kernelwernel/VMAware
     REF v${VERSION}
-    SHA512 93a074c53f47125c93ea1a940f5aad6ec34857d6610463ee7a6ba4e0de334567b6e3812d39d965026e2a956cb3ea55db5d4ffea26379c846ac64359bf1ee3e8d
+    SHA512 0
     HEAD_REF master
 )
 
