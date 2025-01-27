725e2035bcc48e4eb82f6d028f33e64e5ee5b87d
Timur Chernykh <tim.cherry.co@gmail.com>
2025-01-27 16:16:16 +0300

    [VMAware-VM-detection] removed gpl feature

--- ports/vmaware-vm-detection/portfile.cmake
+++ ports/vmaware-vm-detection/portfile.cmake
@@ -8,10 +8,6 @@ vcpkg_from_github(
 
 # Header only
 set(VCPKG_BUILD_TYPE release)
-if("gpl3" IN_LIST FEATURES)
-    file(INSTALL "${SOURCE_PATH}/src/vmaware.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include/vmaware")
-else()
-    file(INSTALL "${SOURCE_PATH}/src/vmaware_MIT.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include/vmaware" RENAME "vmaware.hpp")
-endif()
+file(INSTALL "${SOURCE_PATH}/src/vmaware_MIT.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include/vmaware" RENAME "vmaware.hpp")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
