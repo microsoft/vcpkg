9325851667ec3a7ec735da8cfebd61a6c1917d1e
Kai Pastor <dg0yt@darc.de>
2025-01-31 18:28:43 +0100

    [lunasvg] Update to 3.1.1

--- ports/lunasvg/portfile.cmake
+++ ports/lunasvg/portfile.cmake
@@ -2,28 +2,25 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO sammycage/lunasvg
   REF "v${VERSION}"
-  SHA512 4c771c292d73d4a1c1498011fe8969b9f04a58853d2977c3ce4895d5ad6c1a282c8bfba5e4f1d045e56e0820e57a3a21d3cc0d178efe6ad73c82121aa75b1cdf
+  SHA512 d79c7e35164df971a29145427316adca778416866635a97050f090ffdea1c313cba11255d9993ba7e4cbb3c7faa5b6ff0767e8c7739a680d34be377e6b9c5091
   HEAD_REF master
-  PATCHES
-    fix-cmake.patch
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
   OPTIONS
+    -DCMAKE_REQUIRE_FIND_PACKAGE_plutovg=1
     -DLUNASVG_BUILD_EXAMPLES=OFF
-    -DBUILD_SHARED_LIBS=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-lunasvg)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/lunasvg)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/lunasvg.h" "defined(LUNASVG_BUILD_STATIC)" "1")
+  vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/lunasvg/lunasvg.h" "defined(LUNASVG_BUILD_STATIC)" "1")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
