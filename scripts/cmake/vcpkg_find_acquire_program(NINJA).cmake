77abc3b6df8c27c7aeb6371be8dcffca6fef12c8
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2025-02-06 09:07:32 +0100

    Introduce function z_use_vcpkg_fetch

--- scripts/cmake/vcpkg_find_acquire_program(NINJA).cmake
+++ scripts/cmake/vcpkg_find_acquire_program(NINJA).cmake
@@ -1,11 +1,1 @@
-set(program_name ninja)
-if(NOT NINJA)
-  vcpkg_execute_in_download_mode(
-      COMMAND "$ENV{VCPKG_COMMAND}" fetch ninja
-      RESULT_VARIABLE error_code
-      OUTPUT_VARIABLE NINJA
-      WORKING_DIRECTORY "${DOWNLOADS}"
-  )
-  string(STRIP "${NINJA}" NINJA)
-  set(NINJA "${NINJA}" CACHE STRING "" FORCE)
-endif()
+z_use_vcpkg_fetch(NINJA)
