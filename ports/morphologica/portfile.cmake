31c20afd62cd453f810acc209e1d6ddf1b7e9f3c
rremilian <92378685+rremilian@users.noreply.github.com>
2025-02-12 07:58:33 +0200

    [morphologica] minor changes

--- ports/morphologica/portfile.cmake
+++ ports/morphologica/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ABRG-Models/morphologica
-    REF v${VERSION}
+    REF "v${VERSION}"
     SHA512 6f26b9fb19587308613c7ba5b89ce894025a271edda4f1262daf2fff68336cbc9d14e744aced18d78282178a04bc2076ad893d800ed20105045f8f236272ebe9
     PATCHES
         remove_number_type.patch
@@ -18,4 +18,3 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
-
