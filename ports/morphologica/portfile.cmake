5088a6a732f1cd82344631074b9b460805a2bad8
rremilian <92378685+rremilian@users.noreply.github.com>
2025-02-13 16:22:55 +0200

    [morphologica] add fix_egl.patch

--- ports/morphologica/portfile.cmake
+++ ports/morphologica/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO ABRG-Models/morphologica
     REF "v${VERSION}"
     SHA512 a81620b571b207eba50835da1722fd6320bcddc9d9b4268c43b42e71d4ee10e84210902336a515437a2406b9f56bb2f82db5f540d9d8f54d2006c6dcc671bb72 
+    PATCHES
+        fix_egl.patch
 )
 
 vcpkg_cmake_configure(
