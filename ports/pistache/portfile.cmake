b33bc438c8349df889f7588239a97d5e176e856c
toge <toge.mail@gmail.com>
2025-02-15 03:10:43 +0900

    [pistache] update to 0.4.25

--- ports/pistache/portfile.cmake
+++ ports/pistache/portfile.cmake
@@ -1,17 +1,43 @@
-if(NOT VCPKG_TARGET_IS_LINUX)
-    message(FATAL_ERROR "${PORT} currently only supports Linux platform.")
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pistacheio/pistache
-    REF 9dc080b9ebbe6fc1726b45e9db1550305938313e #2021-03-31
-    SHA512 b55c395fb98af85317590ed2502564af5e92e30a35618132568c6ab589a6d0971570ad20ddbd1f49d9dd8cf54692866c69cfc1350c6fdccf9efb039aacf153b4
+    REF "v${VERSION}"
+    SHA512 2f6d3178354bd4fe78e48fbb0b15055c2a92bee4f4fcee26c3bcc2076df8ca47ef4cac931ee565f5003837beb78a2456ed7d2b6a39083860631426fd074b497c
     HEAD_REF master
 )
 
+if ("ssl" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_SSL=true)
+endif()
+
+if ("rapidjson" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_RAPIDJSON=true)
+else()
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_RAPIDJSON=false)
+endif()
+
+if ("brotli" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_CONTENT_ENCODING_BROTLI=true)
+endif()
+
+if ("zstd" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_CONTENT_ENCODING_ZSTD=true)
+endif()
+
+if ("deflate" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_USE_CONTENT_ENCODING_DEFLATE=true)
+endif()
+
+if ("libevent" IN_LIST FEATURES)
+    list(APPEND BUILD_OPTIONS -DPISTACHE_FORCE_LIBEVENT=true)
+endif()
+
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${BUILD_OPTIONS}
+        -DPISTACHE_BUILD_TESTS=false
+        -DPISTACHE_BUILD_EXAMPLES=false
 )
 vcpkg_install_meson()
 
