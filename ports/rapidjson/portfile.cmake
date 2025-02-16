ad1bdaced91f5d4de9fe0c6de44ff335e98f066e
Adam Tajti <adam.tajti@gmail.com>
2025-02-16 04:16:40 +0100

    build fixes for mysql-connector-cpp and vowpal-wabbit

--- ports/rapidjson/portfile.cmake
+++ ports/rapidjson/portfile.cmake
@@ -34,11 +34,10 @@ if(VCPKG_TARGET_IS_WINDOWS)
 endif()
 
 file(READ "${CURRENT_PACKAGES_DIR}/share/${PORT}/RapidJSONConfig.cmake" _contents)
-string(REPLACE "VERSION 3.5" "VERSION 3.5...3.31" _contents "${_contents}")
 string(REPLACE "\${RapidJSON_SOURCE_DIR}" "\${RapidJSON_CMAKE_DIR}/../.." _contents "${_contents}")
 string(REPLACE "set( RapidJSON_SOURCE_DIR \"${SOURCE_PATH}\")" "" _contents "${_contents}")
 string(REPLACE "set( RapidJSON_DIR \"${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel\")" "" _contents "${_contents}")
 string(REPLACE "\${RapidJSON_CMAKE_DIR}/../../../include" "\${RapidJSON_CMAKE_DIR}/../../include" _contents "${_contents}")
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/RapidJSONConfig.cmake" "${_contents}\nset(RAPIDJSON_INCLUDE_DIRS \"\${RapidJSON_INCLUDE_DIRS}\")\n")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/${PORT}/RapidJSONConfig.cmake" ${_contents})
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/license.txt")
