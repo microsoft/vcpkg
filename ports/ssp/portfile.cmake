afb5a34b777a74e7ff77e02ec70f95b9a2ee34a1
toge <toge.mail@gmail.com>
2025-02-19 10:29:19 +0900

    remove empty lib directory

--- ports/ssp/portfile.cmake
+++ ports/ssp/portfile.cmake
@@ -17,6 +17,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME ssp CONFIG_PATH lib/cmake/ssp-${VERSION})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
