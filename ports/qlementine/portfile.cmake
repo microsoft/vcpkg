7e6298371f917bfe0d7be053357c77368f25cba7
Nick Logozzo <nlogozzo225@gmail.com>
2025-02-18 23:16:40 -0500

    [qlementine] Add port

---
+++ ports/qlementine/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO oclero/qlementine
+    REF "v${VERSION}"
+    SHA512 5ec5db373492173797ce363506b196578ca365549e7bc3de2eb7ae36879002dd724fe650fda8099668dca96197f0889c4f610d267e708690cbd432277e09e643
+    PATCHES
+        qt-no-keywords.patch
+        cmake-install-targets.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DQLEMENTINE_SANDBOX=OFF
+        -DQLEMENTINE_SHOWCASE=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/qlementine)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_copy_pdbs()
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
