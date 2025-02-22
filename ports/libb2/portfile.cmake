ffdf37e6c3116bf91cd18a6a60276105d6e592a3
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2025-02-22 17:57:32 +0100

    more adjustments

--- ports/libb2/portfile.cmake
+++ ports/libb2/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 2c5142f12a2cd52f3ee0a43e50a3a76f75badf85
     SHA512 cf29cf9391ae37a978eb6618de6f856f3defa622b8f56c2d5a519ab34fd5e4d91f3bb868601a44e9c9164a2992e80dde188ccc4d1605dffbdf93687336226f8d
     HEAD_REF master
+    PATCHES
+      remove-flag.patch
 )
 
 vcpkg_make_configure(
@@ -11,6 +13,7 @@ vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         --disable-native            # toolchain may supply -march=native
+        --disable-openmp
         ax_cv_check_cflags___O3=no  # vcpkg provides actual flag
 )
 vcpkg_make_install()
