e95223a2c0f8a91a011c699e7850ae6dc69d7155
toge <toge.mail@gmail.com>
2025-02-25 01:16:47 +0900

    rmdir debug/share

--- ports/cppmicroservices/portfile.cmake
+++ ports/cppmicroservices/portfile.cmake
@@ -30,9 +30,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install(ADD_BIN_TO_PATH)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
 vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
