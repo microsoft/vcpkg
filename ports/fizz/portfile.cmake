edda6fc0844258d28e6a3a45e1f48358a9ec31ba
c8ef <c8ef@outlook.com>
2025-02-26 23:14:20 +0800

    [folly related] update to 02.24

--- ports/fizz/portfile.cmake
+++ ports/fizz/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebookincubator/fizz
     REF "v${VERSION}"
-    SHA512 cc3f86efaad98599f1b66731ff6ea54dd53c7433c41e8f90dbfaaf3267ce66dc0d9fcb36dde97d343cd53d0c3dece4bb8e0aaacc338be208d5dd25354d3726f8
+    SHA512 43ce493f1c3620f993088c2fbe1519bfcac1db2b46977eac91df8088a63aa62fe1173315ad6fae08a0ec3335e2420fb2c4c16be9d9453495398b6d67f67aad15
     HEAD_REF main
     PATCHES
         fix-build.patch
