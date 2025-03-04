c52334ed63719ebae02aa95c8f99d4fa6c9b4b05
David Fiedler <david.fido.fiedler@gmail.com>
2025-03-04 18:09:50 +0100

    Update future-config port with the support of the older GCC versions

--- ports/future-config/portfile.cmake
+++ ports/future-config/portfile.cmake
@@ -2,28 +2,24 @@ vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO F-I-D-O/Future-Config
 	REF v0.1.0
-	SHA512 a2bb1863353658340a34ac5bdae6c762d8c7f4da6ab064f459335d7ce7b6eecca9dadee5a82d5c9077011bd5b48cee176378e0cd7bff97058996b4efd23101dd
+	SHA512 d3a0685dfd0c327e5a9ed87e01169bfdb856b2d08ab16b947b0750cfac939e33b2ef69a7ebffee2569b60bcc507ba95fc1247bdf3f9471cf45db9ae16053571c
 	HEAD_REF master
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
 
 vcpkg_cmake_configure(
-	SOURCE_PATH ${SOURCE_PATH}/cpp
+	SOURCE_PATH "${SOURCE_PATH}/cpp"
 	OPTIONS
-		-DFCONFIG_BUILD_SHARED_LIBS=${BUILD_SHARED}
-		-DFCONFIG_ENABLE_TESTS=OFF
+		-D FCONFIG_BUILD_SHARED_LIBS=${BUILD_SHARED}
+		-D FCONFIG_ENABLE_TESTS=OFF
+		-D FCONFIG_BUILDER_COPY_LIBRARY_DEPENDENCIES_MANUALLY=OFF
+		-D FCONFIG_INSTALL_BUILDER_TOOL_AND_HEADERS_DEBUG=OFF
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup()
-
-# Copy the builder tool dependencies
-vcpkg_copy_tools(
-	TOOL_NAMES fconfig_builder
-	AUTO_CLEAN
-)
 
 # move the jinja template for the builder tool from bin directory to tools directory
 set(BIN_DIR "${CURRENT_PACKAGES_DIR}/bin")
@@ -32,15 +28,15 @@ set(PORT_TOOL_DATA_DIR "${CURRENT_PACKAGES_DIR}/tools/${PORT}/data")
 file(MAKE_DIRECTORY "${PORT_TOOL_DATA_DIR}")
 file(RENAME "${BIN_DATA_DIR}/config.jinja" "${PORT_TOOL_DATA_DIR}/config.jinja")
 file(REMOVE_RECURSE "${BIN_DATA_DIR}")
-# also delete the bin directory if it is empty
-file(GLOB dir_to_rm_content "${BIN_DIR}/*")
-if("${dir_to_rm_content}" STREQUAL "")
-	file(REMOVE_RECURSE "${BIN_DIR}")
-endif()
+
+# Copy the builder tool dependencies
+vcpkg_copy_tools(
+	TOOL_NAMES fconfig_builder
+	AUTO_CLEAN
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 
-# copy the usage example
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 
