d122ce3003a8d97be299abe899969b7c231ce2f8
Nick Logozzo <nlogozzo225@gmail.com>
2025-03-04 11:03:45 -0500

    [dpp] Update to 10.1.0

--- ports/dpp/portfile.cmake
+++ ports/dpp/portfile.cmake
@@ -2,12 +2,32 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO brainboxdotcc/DPP
     REF "v${VERSION}"
-    SHA512 66ae46cfe31414d857cf5c694836d725ed7f0408615c4f21848fc91320fe35c54dce869268cc9da2db3178be9d02adadbcd6a86d43f49f74e124636a8de749e8
+    SHA512 91c28f5ec96e3cb8312a01dffe03c0a4280d0499a310c594954ff1967d25f919805dfd069df2a77ad7e12d187438eba33763e433f8ed524ae48e73f976a5a2c7
+    PATCHES
+        "include.patch"
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        coro DPP_FORMATTERS # Using coro requires C++20, so let's enable formatters as well since we are using C++20
+    INVERTED_FEATURES
+        coro DPP_NO_CORO
+)
+
+if("coro" IN_LIST FEATURES)
+    set(CXX_STANDARD 20)
+else()
+    set(CXX_STANDARD 17)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
+    OPTIONS
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+        -DCMAKE_CXX_STANDARD=${CXX_STANDARD}
+        -DCMAKE_CXX_STANDARD_REQUIRED=ON
+        -DDPP_BUILD_TEST=OFF
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
