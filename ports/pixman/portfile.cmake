933ca14b56f56374a4d3cd591cb626e9ba8b9960
Kai Pastor <dg0yt@darc.de>
2025-03-05 06:32:19 +0100

    Cleanup

--- ports/pixman/portfile.cmake
+++ ports/pixman/portfile.cmake
@@ -1,51 +1,3 @@
-if(VCPKG_TARGET_IS_UWP)
-    list(APPEND OPTIONS
-            -Dmmx=disabled
-            -Dsse2=disabled
-            -Dssse3=disabled)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
-    if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-        set(VCPKG_C_FLAGS "/arch:SSE2 ${VCPKG_C_FLAGS}")
-    endif()
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        set(VCPKG_CXX_FLAGS "/arch:SSE2 ${VCPKG_CXX_FLAGS}")
-    endif()
-    list(APPEND OPTIONS
-            -Dmmx=enabled
-            -Dsse2=enabled
-            -Dssse3=enabled)
-elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
-    #x64 in general has all those intrinsics. (except for UWP for some reason)
-    list(APPEND OPTIONS
-            -Dmmx=enabled
-            -Dsse2=enabled
-            -Dssse3=enabled)
-elseif(VCPKG_TARGET_ARCHITECTURE MATCHES "arm")
-    list(APPEND OPTIONS
-            #-Darm-simd=enabled does not work with arm64-windows
-            -Dmmx=disabled
-            -Dsse2=disabled
-            -Dssse3=disabled)
-elseif(VCPKG_TARGET_ARCHITECTURE MATCHES "mips")
-    list(APPEND OPTIONS
-            -Dmmx=disabled
-            -Dsse2=disabled
-            -Dssse3=disabled)
-endif()
-
-if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_TARGET_ARCHITECTURE MATCHES "arm")
-    list(APPEND OPTIONS
-                -Da64-neon=disabled
-                -Darm-simd=disabled
-                -Dneon=disabled
-                )
-endif()
-
-if(VCPKG_TARGET_IS_OSX)
-    # https://github.com/microsoft/vcpkg/issues/29168
-    list(APPEND OPTIONS -Da64-neon=disabled)
-endif()
-
 vcpkg_from_gitlab(
     OUT_SOURCE_PATH SOURCE_PATH
     GITLAB_URL https://gitlab.freedesktop.org
@@ -57,7 +9,29 @@ vcpkg_from_gitlab(
         missing_intrin_include.patch
 )
 
-# Meson install wrongly pkgconfig file!
+set(x86_architectures x86 x64)
+if(VCPKG_TARGET_ARCHITECTURE IN_LIST x86_architectures AND NOT VCPKG_TARGET_IS_UWP)
+    list(APPEND OPTIONS
+        -Dmmx=enabled
+        -Dsse2=enabled
+        -Dssse3=enabled
+    )
+else()
+    list(APPEND OPTIONS
+        -Dmmx=disabled
+        -Dsse2=disabled
+        -Dssse3=disabled
+    )
+    if(VCPKG_TARGET_IS_WINDOWS)
+        # -Darm-simd=enabled does not work with arm64-windows
+        list(APPEND OPTIONS
+            -Da64-neon=disabled
+            -Darm-simd=disabled
+            -Dneon=disabled
+        )
+    endif()
+endif()
+
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS ${OPTIONS}
@@ -72,6 +46,4 @@ vcpkg_fixup_pkgconfig()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# # Handle copyright
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-file(INSTALL "${SOURCE_PATH}/README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME readme.txt)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
