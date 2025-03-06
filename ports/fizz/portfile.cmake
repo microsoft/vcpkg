9131319f1178079d627b8ccc833398f2cfd17740
Connector Switch <c8ef@outlook.com>
2025-03-07 01:53:36 +0800

    [folly related] update to 03.03 (#44162)

--- ports/fizz/portfile.cmake
+++ ports/fizz/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebookincubator/fizz
     REF "v${VERSION}"
-    SHA512 43ce493f1c3620f993088c2fbe1519bfcac1db2b46977eac91df8088a63aa62fe1173315ad6fae08a0ec3335e2420fb2c4c16be9d9453495398b6d67f67aad15
+    SHA512 774c73081c1e3b4f72463468cd373c948589f3f2de0ae43a5f449ef81a7bfbafa6038a4b0cc18e525b4be47586af3c491ba43f24006d778afcab95d3360efea6
     HEAD_REF main
     PATCHES
         fix-build.patch
