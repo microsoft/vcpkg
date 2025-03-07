cf64dd34a413f01515b65dedbcbae06ff84c5dd8
Mengna-Li <543250287@qq.com>
2025-03-07 20:29:08 +0800

    [freeglut] update to 3.6.0 (#44202)
    Co-authored-by: Mengna-Li <v-limengna@microsoft.com>

--- ports/freeglut/portfile.cmake
+++ ports/freeglut/portfile.cmake
@@ -2,14 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO FreeGLUTProject/freeglut
     REF "v${VERSION}"
-    SHA512 4bb6d6c086bac7a9c0ec78062dce58987555785abe6375f462ee249f65210a964a28fb10ba7ee8a42d7fafb00eb8d196eb403d65d255f02f88467369c187228b
+    SHA512 9be8dcc266daacc21aa0e11261bfe864260de6802e98e7dc84964904df4a89e52960eba94d58f4c18fc1f0ff3f13810d59fc33313c2a6e3f07d18f0b50b95849
     HEAD_REF master
     PATCHES
         android.patch
-        gles.patch # amends https://github.com/freeglut/freeglut/commit/093a5a46
         x11-dependencies-export.patch
         fix-debug-macro.patch
-        no_x64_enforcement.patch
         windows-output-name.patch
 )
 
@@ -65,4 +63,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/glut")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
