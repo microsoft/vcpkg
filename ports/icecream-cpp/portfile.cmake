3b1b03bed0caae3069ac5eceb065cf095f45690c
toge <toge.mail@gmail.com>
2025-03-09 20:53:31 +0900

    remove lib dir

--- ports/icecream-cpp/portfile.cmake
+++ ports/icecream-cpp/portfile.cmake
@@ -17,5 +17,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/icecream-cpp)
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
