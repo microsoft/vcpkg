43bf36a45b32fd75917d3d335b5461088e5f1a67
Darkx32 <Darkx32@users.noreply.github.com>
2025-03-09 17:02:21 -0300

    Update portfile.cmake

--- ports/audio-engine/portfile.cmake
+++ ports/audio-engine/portfile.cmake
@@ -18,6 +18,8 @@ vcpkg_cmake_install()
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
