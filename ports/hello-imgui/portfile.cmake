7ce2863c73fcb2553a8624ad6c8633b28663c180
zhao liu (BEYONDSOFT CONSULTING INC) <v-zhli17@microsoft.com>
2025-03-10 01:55:17 -0700

    [hello-imgui] Remove the freetype-lunasvg features

--- ports/hello-imgui/portfile.cmake
+++ ports/hello-imgui/portfile.cmake
@@ -16,7 +16,6 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     "experimental-dx11-binding" HELLOIMGUI_HAS_DIRECTX11
     "experimental-dx12-binding" HELLOIMGUI_HAS_DIRECTX12
     "glfw-binding" HELLOIMGUI_USE_GLFW3
-    "freetype-plutosvg" HELLOIMGUI_USE_FREETYPE # When hello_imgui is built with freetype, it will also build with plutosvg
 )
 
 if (NOT HELLOIMGUI_HAS_OPENGL3
