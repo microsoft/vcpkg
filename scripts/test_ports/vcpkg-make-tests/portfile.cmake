39620c8e39e70054ed42cdfc7e9c4099df9328ad
Javier Matos <javiermatos@Javiers-Laptop.local>
2025-03-12 14:07:01 -0400

    change how we include unit tests

--- scripts/test_ports/vcpkg-make-tests/portfile.cmake
+++ scripts/test_ports/vcpkg-make-tests/portfile.cmake
@@ -1,5 +1,5 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-include("${CMAKE_CURRENT_LIST_DIR}/../unit-test-cmake/portfile.cmake")
+include("${CURRENT_HOST_INSTALLED_DIR}/share/unit-test-cmake/test-macros.cmake")
 
 if("z-vcpkg-make-prepare-compile-flags" IN_LIST FEATURES)
     include("${CMAKE_CURRENT_LIST_DIR}/test-z_vcpkg_make_prepare_compile_flags.cmake")
