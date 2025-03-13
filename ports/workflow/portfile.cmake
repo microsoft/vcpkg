4c8da1be01ae8de1a9d817896e93395c9dee81d1
Mengna-Li <v-limengna@microsoft.com>
2025-03-13 01:45:04 +0000

    update to 0.11.8, windows branch to 0.10.10

--- ports/workflow/portfile.cmake
+++ ports/workflow/portfile.cmake
@@ -2,8 +2,8 @@ if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO sogou/workflow
-        REF "v${VERSION}-win"
-        SHA512 c34518ca35f19ab5539ea82cc73ecbc9828413530cec8dbbe56b17517ec6b7b0326a29e5b343b950afe128829c8e23e75d19494e17f7be4fce9edb524c44ee56
+        REF "v0.10.10-win"
+        SHA512 61ba0cee8e56bb112c6562697bbd7f53e55c1a12ea2388a23076c4e732ae1337b2b0fcc4f518b2afeb1373a173c1ba7375b175c3df3b25d3013a4d2e292c837c
         HEAD_REF windows
     )
 else()
@@ -11,7 +11,7 @@ else()
         OUT_SOURCE_PATH SOURCE_PATH
         REPO sogou/workflow
         REF "v${VERSION}"
-        SHA512 25258e9dc161c5b30395caa3525a4ba5de5763cad5761cbdc8bb23d2468eb624ec49455a5c9ab628c219d7436f707da0138229c5fa82bcfccc24a485649acb56
+        SHA512 ea90fb1a9c289a76dfa02b077cb0d99ec27157747f1b73d4437a089560a2659baebd463e2e6f699fbd44ec01e59bcd4d4b2f4556377dd57834f02bde0aefdca3
         HEAD_REF master
     )
 endif()
@@ -35,4 +35,4 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
