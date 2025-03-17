52b567320727f0506642da51fc843d79b70748d8
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-03-17 15:36:13 -0400

    [irsdkcpp] Adding IRSDKCPP release v1.0.0

---
+++ ports/irsdkcpp/portfile.cmake
@@ -0,0 +1,21 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO vrkit-platform/irsdk-cpp
+    REF "v${VERSION}"
+    # REF master
+    SHA512 0a04336b350bc345d8c3d85d58d556680dcd2c13079c3eee1906230a4754b08088f71496c2e19433a7fbaacb1213deb40911321f921e15777614b3dd4bc892f9
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCMAKE_CXX_STANDARD=23
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/irsdkcpp)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
