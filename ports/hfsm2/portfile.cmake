2958a000decab93f7c30bce2e8e0c6c7f75cfdce
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2025-03-18 20:38:19 +0100

    Updated HFSM2 to 2.6.0

--- ports/hfsm2/portfile.cmake
+++ ports/hfsm2/portfile.cmake
@@ -1,16 +1,29 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO andrew-gresyk/HFSM2
-    REF 1_10_1
-    SHA512 49ae09a3e4052c7a70390510d128d4d0a41f3c2b6bb9cf2c0b36d272ca1a2524c1d2b544e4c36655681255ce6a5e2f3f90f8c4e9957e8c49e49a62927134a718
+    REF 883576e98f2da1683de5bd5877547b06fd5300dd
+    SHA512 47680d716a8cbe3e38283c2ec50ef8f3ae470bdb8a7bb3fd8eba75cd5a3a006de3a829ae4e6d034cfc513281b797aed0ce65886879d82406f3cac2d7da6df06b  
     HEAD_REF master
 )
 
-# Install include directory
-file(INSTALL "${SOURCE_PATH}/include" DESTINATION "${CURRENT_PACKAGES_DIR}")
+set(VCPKG_BUILD_TYPE release) # header-only port
 
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DHFSM2_BUILD_TESTS=OFF
+        -DHFSM2_BUILD_EXAMPLES=OFF
+)
 
-# Copy usage file
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/hfsm2 PACKAGE_NAME hfsm2)
+vcpkg_fixup_pkgconfig()
+
+# Remove empty directories if they exist
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
