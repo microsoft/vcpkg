679952072114ebb34b8b577dca3a9ab714eaf866
River <26424577+wusatosi@users.noreply.github.com>
2025-03-21 18:38:49 -0400

    disable building test and example

--- ports/beman-exemplar/portfile.cmake
+++ ports/beman-exemplar/portfile.cmake
@@ -10,6 +10,9 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBEMAN_EXEMPLAR_BUILD_TESTS=OFF
+        -DBEMAN_EXEMPLAR_BUILD_EXAMPLES=OFF
 )
 
 vcpkg_cmake_install()
