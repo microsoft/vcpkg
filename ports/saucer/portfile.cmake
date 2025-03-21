55b654725c3ad3b0f0d297310b8b76eecf263aaf
Jim-Wang <wangzhijie05@beyondsoft.com>
2025-03-21 01:23:27 +0000

    update webview2

--- ports/saucer/portfile.cmake
+++ ports/saucer/portfile.cmake
@@ -4,11 +4,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO saucer/saucer
     REF "v${VERSION}"
-    SHA512 6b5090c7754cac99d410ae59e207a44cd58db7fc9ee59412181c13449c5aed7e1cb61b1ec0703809084b406e01bbb821ecafa5caee4c2704ef72f07d2979a7e0
+    SHA512 083b92079bf324fb9e50b3d6291ee3654b8e4e2926c292c9dc092b1c4ce336ce4d4bbea7e14d52291340c692887b8ab92d1f9f3d50aed6092b5465242572bfdc
     HEAD_REF dev
     PATCHES
         fix_findpkg.patch
-        fix-build-error-with-fmt11.patch
 )
 
 vcpkg_cmake_configure(
@@ -17,11 +16,16 @@ vcpkg_cmake_configure(
         ${BACKEND_OPTION}
         -Dsaucer_prefer_remote=OFF
         -Dsaucer_remote_webview2=OFF
+        -Dsaucer_serializer=Rflpp
+        -Dsaucer_tests=OFF
+        -Dsaucer_examples=OFF
     MAYBE_UNUSED_VARIABLES
         saucer_remote_webview2
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/reflectcpp")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
