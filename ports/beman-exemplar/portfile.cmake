e11c5b1de21fc5113cd8b8eb477fe5194575d312
River <26424577+wusatosi@users.noreply.github.com>
2025-03-21 17:13:44 -0400

    Apply suggestions from code review
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/beman-exemplar/portfile.cmake
+++ ports/beman-exemplar/portfile.cmake
@@ -15,15 +15,13 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
-    PACKAGE_NAME "beman-exemplar"
+    PACKAGE_NAME "beman.exemplar"
     CONFIG_PATH "lib/cmake/beman.exemplar"
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Move LICENSE file to the correct location
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-# Copy usage file
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
