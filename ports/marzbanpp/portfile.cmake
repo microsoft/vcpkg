46e2bbc31ad1ef22ef14bf7c5b57eed78452886e
Andrey <apugachevdev@gmail.com>
2025-03-22 13:26:54 +0300

    Add marzbanpp library

---
+++ ports/marzbanpp/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO andrascii/marzbanpp
+    REF "1.0.1"
+    SHA512 8db3ca283d119efadf9f9ba5d573263c92402a2dda82683499918a5889ddff502a0bcff0658a26b927f2d3a373e42dbcf59cf7aad2fab6313210eb39fff78b00
+    HEAD_REF marzbanpp
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "marzbanpp")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
