aa1ccfa1f7c4c04aa479b27fc1e13044847c3041
Andrey <46888555+andrascii@users.noreply.github.com>
2025-03-22 15:04:48 +0300

    Update ports/marzbanpp/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/marzbanpp/portfile.cmake
+++ ports/marzbanpp/portfile.cmake
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME "marzbanpp")
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
