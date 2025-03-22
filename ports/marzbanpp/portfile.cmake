50e685696b2be38b528119be3898c318bf903c5d
Andrey <46888555+andrascii@users.noreply.github.com>
2025-03-22 21:23:00 +0300

    Update ports/marzbanpp/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/marzbanpp/portfile.cmake
+++ ports/marzbanpp/portfile.cmake
@@ -3,9 +3,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO andrascii/marzbanpp
-    REF "1.0.1"
+    REF "${VERSION}"
     SHA512 6cb410f7e451bf2cde5c13cfafdf1ac3256f0e2943afa26132acadbc87ee9847abc0260617af9e53d020205af776deb4860b3766c6a39761834ac381f2907114
-    HEAD_REF marzbanpp
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
