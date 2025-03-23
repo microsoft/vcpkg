affe8ebbbd2c3b1240968d830f633705d591b6ce
Jonathan Glanz <jonathan.glanz@gmail.com>
2025-03-23 18:26:46 -0400

    [irsdkcpp] Filled in all the missing installation and configuration required to enable CMAKE to produce usable shared & static libraries from the same configuration

--- ports/irsdkcpp/portfile.cmake
+++ ports/irsdkcpp/portfile.cmake
@@ -2,30 +2,28 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vrkit-platform/irsdk-cpp
     REF "v${VERSION}"
-    SHA512 cf991d4a519c24f481b124aec139e4f6a07c8b0aa5458e156f056129ee5dd258c7b4e1a44f4aee8daf5008df2388b3788c47cc492e2dd2fb2e8d1fc1d57e74d1
-    HEAD_REF master
+    SHA512 97775a282430f8ab5c80b489761c6d28a62dad3fff85a430419f8f47843d7da58e97afa5fc52fe41941be1711e2352dd493f0feeef6c1ca0e96871399f8c7efa
+    HEAD_REF develop
 )
 
-# string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" IRSDKCPP_BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" IRSDKCPP_BUILD_SHARED)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCMAKE_CXX_STANDARD=23
-        -DBUILD_TESTS=OFF
-        -DBUILD_DOCS=OFF
-        -DBUILD_EXAMPLES=OFF
+        -DIRSDKCPP_BUILD_TESTS=OFF
+        -DIRSDKCPP_BUILD_DOCS=OFF
+        -DIRSDKCPP_BUILD_EXAMPLES=OFF
         -DIRSDKCPP_BUILD_SHARED=${IRSDKCPP_BUILD_SHARED}
-        -DIRSDKCPP_BUILD_STATIC=ON
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/irsdkcpp)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
