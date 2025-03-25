dabb05ed7c6746200c20643a1fff306493559893
Guillermo Calvo <guillermo@guillermo.dev>
2025-03-25 23:04:23 +0100

    Delete usage file

--- ports/resultlib/portfile.cmake
+++ ports/resultlib/portfile.cmake
@@ -9,6 +9,5 @@ vcpkg_from_github(
 )
 
 file(COPY "${SOURCE_PATH}/src/result.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/NOTICE")
