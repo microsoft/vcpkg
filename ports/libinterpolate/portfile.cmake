f8bb56ebcfb25306d19a5d2a19760168e6714019
Jan Aleksandrov <jan.aleksandrov@valhalla-turrets.com>
2025-03-26 08:46:45 +0100

    libinterpolate code review fixes

--- ports/libinterpolate/portfile.cmake
+++ ports/libinterpolate/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_github(
     REF ${VERSION}
     SHA512 6d53e1fb3af3067ddd13e491563e8da5af9756efba5a2def486f014c23969633ca73cf43dd2f93047716ebb6565f5e9911b6ab85abef2db3b9faefc26ab3aa59  
     HEAD_REF master
-    PATCHES
-        fix-version-detection.patch
 )
 
 set(VCPKG_BUILD_TYPE release) # header-only port
@@ -21,9 +19,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libInterpolate)
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
