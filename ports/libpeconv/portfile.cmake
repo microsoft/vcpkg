a77cd3580270fb372fd8fe69a102863d788c3d8f
miyanyan <1138989048@qq.com>
2025-03-26 19:55:33 +0800

    only support windows

--- ports/libpeconv/portfile.cmake
+++ ports/libpeconv/portfile.cmake
@@ -3,7 +3,7 @@ set(VCPKG_LIBRARY_LINKAGE static)  # static-only
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO hasherezade/libpeconv
-    REF e90f742
+    REF e90f7423a0232e4ce765ad7118a7212825e11e8f
     SHA512 e18bdebd77d4d1e3c13133586b590821e726314f7bdc097c1cee1a129b9bfda0bcde74f665f0c89d3afdb38bdb4cf0ce1ebbe32efa85df72f251b08e8d12f994
     HEAD_REF master
     PATCHES
