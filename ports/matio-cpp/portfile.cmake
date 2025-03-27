ff16adedaa75b02b342c3372b0eab0a43701ac42
Alberto Luaces <aluaces@udc.es>
2025-03-27 12:58:07 +0100

    portfile.cmake formatting.
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/matio-cpp/portfile.cmake
+++ ports/matio-cpp/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     SHA512 efae9cec18b44291625f0770dabe8fd23c5b1d7cf77849cab827880ca96328430d4146014ace82d443bbc00cc313fd23cd5d67a7f1b58235efa7b5cb291a8ea4
     HEAD_REF master
     PATCHES
-    fix-dependencies.patch
+        fix-dependencies.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/matioCppConfig.cmake.in" DESTINATION "${SOURCE_PATH}")
