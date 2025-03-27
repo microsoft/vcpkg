67d7b3a59fbefa5b136b5cd7a223f80e6b9be64f
Alberto Luaces <aluaces@udc.es>
2025-03-27 12:59:41 +0100

    Simplification of LICENSE file installation
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/matio-cpp/portfile.cmake
+++ ports/matio-cpp/portfile.cmake
@@ -25,5 +25,5 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME matioCpp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
