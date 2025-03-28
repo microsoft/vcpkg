6b54e505826013a5b1dfebfefa087b69bafc8733
Nicolas BLANC <sinfomicien@gmail.com>
2025-03-28 08:55:26 +0100

    [libdmtx] Replace hardcoded version by ${VERSION} in  ports/libdmtx/portfile.cmake
    Co-authored-by: WangWeiLin-MV <156736127+WangWeiLin-MV@users.noreply.github.com>

--- ports/libdmtx/portfile.cmake
+++ ports/libdmtx/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO dmtx/libdmtx
-    REF v0.7.7
+    REF v${VERSION}
     SHA512 802a697669afeb74da0cc3736fe7301fcc1653c1e3bebc343a8baf76e52226cc5509231519343267a92e22ebdfcc5b2825380339991340f054f0a6685d2ffcdc
     PATCHES
         001-cmake-add-install-target.patch
