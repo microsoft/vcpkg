fe36832bed75774a82f5960caa80bcfe0ae2f596
zhang bing <zhangbing@hfut.edu.cn>
2025-03-28 22:21:14 +0800

    Add features of i64 and fp64 for 64bit integer index and double precision.

--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -1,11 +1,29 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
+if("i64" IN_LIST FEATURES)
+    set(IDXWIDTH 64)
+else()
+    set(IDXWIDTH 32)
+endif()
+
+if("fp64" IN_LIST FEATURES)
+    set(REALWIDTH 64)
+else()
+    set(REALWIDTH 32)
+endif()
+
+configure_file(
+    "${CURRENT_PORT_DIR}/build-fixes.patch"
+    "${CURRENT_BUILDTREES_DIR}/build-fixes-dynamic.patch"
+    @ONLY
+)
+
 vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
     REPO KarypisLab/METIS
     REF 94c03a6e2d1860128c2d0675cbbb86ad4f261256
     SHA512 9f24329fa0f0856d0b5d10a489574d857bc4538d9639055fc895363cf70aa37342eaf7bc08819500ff6d5b98a4aa99f4241880622b540d4c484ca19e693d3480
     PATCHES
-        build-fixes.patch
+        "${CURRENT_BUILDTREES_DIR}/build-fixes-dynamic.patch"
     )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
