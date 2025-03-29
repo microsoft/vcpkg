92d3e50472e3b8955d32298d5165c2bffbe5a97a
Kai Pastor <dg0yt@darc.de>
2025-03-29 23:13:32 +0100

    Fixup

--- ports/folly/portfile.cmake
+++ ports/folly/portfile.cmake
@@ -28,9 +28,6 @@ file(REMOVE "${SOURCE_PATH}/build/fbcode_builder/CMake/FindLibEvent.cmake")
 file(REMOVE "${SOURCE_PATH}/build/fbcode_builder/CMake/FindSodium.cmake")
 file(REMOVE "${SOURCE_PATH}/build/fbcode_builder/CMake/FindZstd.cmake")
 
-# Required to run build/generate_escape_tables.py et al.
-vcpkg_find_acquire_program(PYTHON3)
-
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" MSVC_USE_STATIC_RUNTIME)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -50,7 +47,6 @@ vcpkg_cmake_configure(
         -DMSVC_USE_STATIC_RUNTIME=${MSVC_USE_STATIC_RUNTIME}
         -DCMAKE_INSTALL_DIR=share/folly
         -DCMAKE_POLICY_DEFAULT_CMP0167=NEW
-        "-DPYTHON_EXECUTABLE=${PYTHON3}"
         -DVCPKG_LOCK_FIND_PACKAGE_fmt=ON
         -DVCPKG_LOCK_FIND_PACKAGE_LibDwarf=OFF
         -DVCPKG_LOCK_FIND_PACKAGE_Libiberty=OFF
