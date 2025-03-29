570caa554d2110a462a197708b3c7f4c0553e9f7
LazyJazz <zijian.lv@hotmail.com>
2025-03-29 08:34:07 +0800

    Add variables for submodule referencing

--- ports/directx-dxc/portfile.cmake
+++ ports/directx-dxc/portfile.cmake
@@ -1,7 +1,15 @@
 set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
 
-set(DIRECTX_DXC_TAG v1.8.2502)
+set(DIRECTX_DXC_TAG 1.8.2502)
 set(DIRECTX_DXC_VERSION 2025_02_20)
+
+# for macOS platform:
+# since vcpkg_from_github() does not support submodules, we need to checkout the submodules manually
+# check for newest hash values by the link blow, REMEMBER to update the version tag in the URL
+# https://github.com/microsoft/DirectXShaderCompiler/tree/release-1.8.2502/external
+set(DX_HEADERS_MODULE_HASH 980971e835876dc0cde415e8f9bc646e64667bf7)
+set(SPIRV_HEADERS_MODULE_HASH 3f17b2af6784bfa2c5aa5dbb8e0e74a607dd8b3b)
+set(SPIRV_TOOLS_MODULE_HASH 4d2f0b40bfe290dea6c6904dafdf7fd8328ba346)
 
 if (NOT VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
    message(STATUS "Note: ${PORT} always requires dynamic library linkage at runtime.")
@@ -9,13 +17,13 @@ endif()
 
 if (VCPKG_TARGET_IS_LINUX)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/microsoft/DirectXShaderCompiler/releases/download/${DIRECTX_DXC_TAG}/linux_dxc_${DIRECTX_DXC_VERSION}.x86_64.tar.gz"
+        URLS "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v${DIRECTX_DXC_TAG}/linux_dxc_${DIRECTX_DXC_VERSION}.x86_64.tar.gz"
         FILENAME "linux_dxc_${DIRECTX_DXC_VERSION}.tar.gz"
         SHA512 48d246349a7b8c998d80969a3d0a383c9fd287c7130c0ea3b214a2e8630d36ac38b78c1263a954777d89760910092f7a9812d421784706efa182cefeb017c3c6
     )
 elseif (VCPKG_TARGET_IS_WINDOWS)
     vcpkg_download_distfile(ARCHIVE
-        URLS "https://github.com/microsoft/DirectXShaderCompiler/releases/download/${DIRECTX_DXC_TAG}/dxc_${DIRECTX_DXC_VERSION}.zip"
+        URLS "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v${DIRECTX_DXC_TAG}/dxc_${DIRECTX_DXC_VERSION}.zip"
         FILENAME "dxc_${DIRECTX_DXC_VERSION}.zip"
         SHA512 2381852e0d57be65ab919df00d79cda3564cd3695c6415065680738b66de1fc106106baae322973c6a48337b97e603294ee5118f71559298b7097181e73e4b31
     )
@@ -23,14 +31,14 @@ elseif (VCPKG_TARGET_IS_OSX)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO "microsoft/DirectXShaderCompiler"
-        REF "2399215226737c64e76afc55ffd874ecd6fc459f"
-        SHA512 7fdc95859068f6d4cff8b73fe2c99f9305fce9ab8d7b6c212642c15d017b87084ac8f86a4b73aa9aa9d0519f2a0bf9ac851be0228636fc74dac6aa3e078bb880
+        REF "release-${DIRECTX_DXC_TAG}"
+        SHA512 720301093c9e04c23518fdf50aec110bc1a4eafc9d585e73652b7db06c86185e0eca7dd75d4b97f13bdd4e6362ebc0ecc61a5a65f90e193a82dc0bbb48e09b3b
     )
 endif ()
 
 vcpkg_download_distfile(
     LICENSE_TXT
-    URLS "https://raw.githubusercontent.com/microsoft/DirectXShaderCompiler/${DIRECTX_DXC_TAG}/LICENSE.TXT"
+    URLS "https://raw.githubusercontent.com/microsoft/DirectXShaderCompiler/v${DIRECTX_DXC_TAG}/LICENSE.TXT"
     FILENAME "LICENSE.${DIRECTX_DXC_VERSION}"
     SHA512  9feaa85ca6d42d5a2d6fe773706bbab8241e78390a9d61ea9061c8f0eeb5a3e380ff07c222e02fbf61af7f2b2f6dd31c5fc87247a94dae275dc0a20cdfcc8c9d
 )
@@ -58,17 +66,17 @@ else ()
         LOGNAME fetch-submodule-spirv-tools
     )
     vcpkg_execute_required_process(
-        COMMAND git checkout 980971e835876dc0cde415e8f9bc646e64667bf7
+        COMMAND git checkout ${DX_HEADERS_MODULE_HASH}
         WORKING_DIRECTORY ${SOURCE_PATH}/external/DirectX-Headers
         LOGNAME checkout-submodule-dx-headers
     )
     vcpkg_execute_required_process(
-        COMMAND git checkout 3f17b2af6784bfa2c5aa5dbb8e0e74a607dd8b3b
+        COMMAND git checkout ${SPIRV_HEADERS_MODULE_HASH}
         WORKING_DIRECTORY ${SOURCE_PATH}/external/SPIRV-Headers
         LOGNAME checkout-submodule-spirv-headers
     )
     vcpkg_execute_required_process(
-        COMMAND git checkout 4d2f0b40bfe290dea6c6904dafdf7fd8328ba346
+        COMMAND git checkout ${SPIRV_TOOLS_MODULE_HASH}
         WORKING_DIRECTORY ${SOURCE_PATH}/external/SPIRV-Tools
         LOGNAME checkout-submodule-spirv-tools
     )
