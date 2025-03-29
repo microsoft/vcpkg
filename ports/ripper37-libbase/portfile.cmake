f7b7e23134a6ed5382ecbcc6cab98b69b1ee5bd6
RippeR37 <ripper37@gmail.com>
2025-03-29 16:54:22 +0100

    Revert package name/dirs in .../share/ to use libbase

--- ports/ripper37-libbase/portfile.cmake
+++ ports/ripper37-libbase/portfile.cmake
@@ -23,7 +23,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "ripper37-libbase")
+vcpkg_cmake_config_fixup(PACKAGE_NAME "libbase")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
