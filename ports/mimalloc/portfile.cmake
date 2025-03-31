bd64a929ac7ea1cd1571bcee5054945660e73a36
Kai Pastor <dg0yt@darc.de>
2025-03-31 19:02:22 +0200

    [mimalloc] Update to 2.2.3

--- ports/mimalloc/portfile.cmake
+++ ports/mimalloc/portfile.cmake
@@ -2,12 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO microsoft/mimalloc
     REF "v${VERSION}"
-    SHA512 4e30976758015c76a146acc1bfc8501e2e5c61b81db77d253de0d58a8edef987669243f232210667b32ef8da3a33286642acb56ba526fd24c4ba925b44403730
+    SHA512 55262050f63868e3029cd929a74d312dc0f34b606534b1d0b3735eecc8eed68aae97523a50228b4ac4044e1e03192f2909440e3a27607e2d364607ac0bda828f
     HEAD_REF master
-    PATCHES
-        fix-cmake.patch
-        add_ref_to_page_malloc_zero.patch
-        fix-param.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -57,18 +53,5 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_fixup_pkgconfig()
-set(mi_basename "mimalloc")
-if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    string(APPEND mi_basename "-static")
-endif()
-if("secure" IN_LIST FEATURES)
-    string(APPEND mi_basename "-secure")
-endif()
-if(NOT "mimalloc" STREQUAL "${mi_basename}")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/mimalloc.pc" " -lmimalloc" " -l${mi_basename}")
-endif()
-if(NOT VCPKG_BUILD_TYPE)
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/mimalloc.pc" " -lmimalloc" " -l${mi_basename}-debug")
-endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
