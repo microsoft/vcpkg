9f5cb377ff98f9d889be1e23ce10dedf8268bb6d
Mys Vac <mys_vac@hotmail.com>
2025-04-02 09:58:45 +0800

    Apply suggestions from code review
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/cpp-jsonlib/portfile.cmake
+++ ports/cpp-jsonlib/portfile.cmake
@@ -1,4 +1,7 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -14,11 +17,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME "cpp-jsonlib")
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
