6da9bee6cd3fc0a2902879bbc01464f74c3a31fe
toge <toge.mail@gmail.com>
2025-04-02 23:01:20 +0900

    [protobuf] update to 6.30.2

--- ports/protobuf/portfile.cmake
+++ ports/protobuf/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO protocolbuffers/protobuf
     REF "v${VERSION}"
-    SHA512 32a9ae3de113b8c94e2aed21ad8f58e5ed4419a6d4078e51f614f0fabbf3bfe6c4affc62c2c1326e030a54df0fdcc47bb715b45022191a363f17680ec651b68e
+    SHA512 f2ee857a36b49f87257a306b3f3c361770d74aaf24c4650b9d00994e1e1a0b09079fb0ce5ffb4d5a4a32d8ca46e3247d6db454918fa0b104fc8d58e8a0546a96
     HEAD_REF master
     PATCHES
         fix-static-build.patch
