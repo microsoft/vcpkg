eb2379cbc77bdd89c9fa576e2dec1d50d54e6887
Kai Pastor <dg0yt@darc.de>
2025-04-04 23:15:10 +0200

    [tomlplusplus] Install CMake config

--- ports/tomlplusplus/portfile.cmake
+++ ports/tomlplusplus/portfile.cmake
@@ -11,13 +11,21 @@ vcpkg_from_github(
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -Dgenerate_cmake_config=false
         -Dbuild_tests=false
         -Dbuild_examples=false
 )
 
 vcpkg_install_meson()
 vcpkg_fixup_pkgconfig()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/tomlplusplus)
+if(NOT VCPKG_BUILD_TYPE)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/tomlplusplus/tomlplusplusConfig.cmake"
+        [[IMPORTED_LOCATION ("..PACKAGE_PREFIX_DIR.)(/lib/[^"]*")]]
+        [[IMPORTED_CONFIGURATIONS "DEBUG;RELEASE"
+          IMPORTED_LOCATION_DEBUG \1/debug\2
+          IMPORTED_LOCATION_RELEASE \1\2]]
+        REGEX
+    )
+endif()
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
