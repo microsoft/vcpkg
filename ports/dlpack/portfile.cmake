046ec5a08c20ee8344b4a1fafa7b2a16b357a459
Billy Robert O'Neal III <bion@microsoft.com>
2025-04-07 12:18:44 -0700

    Remove the CMake version patch.

--- ports/dlpack/portfile.cmake
+++ ports/dlpack/portfile.cmake
@@ -6,7 +6,6 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 ff24ddf8a138f20aeede2708327d8239bd3cc4e2223a6fbce1589638dd2d844827ce6af1d3eb1a14165e608f424f5d4ff358c5f55600b150083c6c8b83e35afd
     HEAD_REF main
-    PATCHES cmake_version.patch
 )
 
 vcpkg_cmake_configure(
