774efa8eb35dce6fa1a7d3ba5d164ac187e35f27
Kai Pastor <dg0yt@darc.de>
2025-04-09 07:42:21 +0200

    [vcpkg-ci-gz-sim] New test port

---
+++ scripts/test_ports/vcpkg-ci-gz-sim/portfile.cmake
@@ -0,0 +1,14 @@
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO gazebosim/gz-sim
+    REF 9.0.0
+    SHA512 4ac9debe27a41233c7c2116bd80f277ebe74f4ae639f06555cec4209bb7af6fe741197705fac222b4e00c8493daaf701b1eefee4ff639fdea70703bed80e0f8a
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/examples/standalone/light_control"
+)
+vcpkg_cmake_build()
