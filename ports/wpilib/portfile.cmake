8c80eac466d133f7ea12bf4accac8f092e2182fd
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-09 20:19:31 -0400

    try fix

--- ports/wpilib/portfile.cmake
+++ ports/wpilib/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     REF 49b4b064cf98c0e367f54ac8719779cc158eba3a
     SHA512 fe73f09f37cd79e9d6f6f5a4a1a98286f10b12406c1bd7f0d1daf0377d8c7c28217afc97b07842e5c483a2a08586ac699b30ec0d030255f8ab31831aefa6ab58
     PATCHES
-    	fix-eigen-constexpr.patch
+    	fix-constexpr.patch
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -15,8 +15,12 @@ vcpkg_cmake_configure(
 	-DWITH_GUI=OFF
 	-DNO_WERROR=ON
         -DWITH_SIMULATION_MODULES=OFF
+	-DUSE_SYSTEM_FMTLIB=ON
+	-DUSE_SYSTEM_LIBUV=ON
+	_DUSE_SYSTEM_EIGEN=ON
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME wpilib)
 vcpkg_cmake_config_fixup(PACKAGE_NAME apriltag)
 vcpkg_cmake_config_fixup(PACKAGE_NAME cameraserver)
 vcpkg_cmake_config_fixup(PACKAGE_NAME cscore)
