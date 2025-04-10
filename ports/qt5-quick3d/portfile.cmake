a71a7f0a86c93c04b60e4f4be5315dee1f8296e7
yanzixiang <whyzix@qq.com>
2025-04-10 12:30:11 +0800

    move qt5-quick3d_and_quicktimeline hash to qt5-base/cmake/qt_port_hashes.cmake

--- ports/qt5-quick3d/portfile.cmake
+++ ports/qt5-quick3d/portfile.cmake
@@ -1,3 +1,2 @@
-set(QT_HASH_qt5-quick3d 58abd8036ca59b00a73a7c08cc1b95c039bfd806d45d556e63db65abc97c2b3c8a7aeb4aabf3d1f58a854378a66c5092eff8b0fe45b7027189adf7239dcaa547)
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
 qt_submodule_installation()
--- ports/qt5-quicktimeline/portfile.cmake
+++ ports/qt5-quicktimeline/portfile.cmake
@@ -1,5 +1,4 @@
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
-set(QT_HASH_qt5-quicktimeline 0d27b672a76fdb6ba531bc823792bbcda2f286cebf9b64332651544344c1d78c9d397d40b3ccd426cea4dea6ea0971cc142ce0258a1f5a92a2239b39aef79054)
 
 include(${CURRENT_INSTALLED_DIR}/share/qt5/qt_port_functions.cmake)
 qt_submodule_installation()
