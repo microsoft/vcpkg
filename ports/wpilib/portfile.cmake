62d1a6bc44b7f0fb740dd30f4854d5c5e14ce773
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-10 08:40:31 -0400

    apriltag build option

--- ports/wpilib/portfile.cmake
+++ ports/wpilib/portfile.cmake
@@ -5,10 +5,18 @@ vcpkg_from_github(
     SHA512 fe73f09f37cd79e9d6f6f5a4a1a98286f10b12406c1bd7f0d1daf0377d8c7c28217afc97b07842e5c483a2a08586ac699b30ec0d030255f8ab31831aefa6ab58
     PATCHES
     	constexpr-fix.patch
+	add-apriltag-toggle.patch
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+	FEATURES
+		apriltag WITH_APRILTAG
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+    	${FEATURE_OPTIONS}
         -DWITH_JAVA=OFF
         -DWITH_EXAMPLES=OFF
         -DWITH_TESTS=OFF
@@ -19,12 +27,21 @@ vcpkg_cmake_configure(
 	-DUSE_SYSTEM_LIBUV=ON
 	_DUSE_SYSTEM_EIGEN=ON
 )
+
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME wpilib)
+vcpkg_cmake_config_fixup(PACKAGE_NAME apriltag)
+vcpkg_cmake_config_fixup(PACKAGE_NAME cameraserver)
+vcpkg_cmake_config_fixup(PACKAGE_NAME cscore)
+vcpkg_cmake_config_fixup(PACKAGE_NAME hal)
 vcpkg_cmake_config_fixup(PACKAGE_NAME ntcore)
+vcpkg_cmake_config_fixup(PACKAGE_NAME romiVendordep)
+vcpkg_cmake_config_fixup(PACKAGE_NAME wpilib)
+vcpkg_cmake_config_fixup(PACKAGE_NAME wpilibc)
+vcpkg_cmake_config_fixup(PACKAGE_NAME wpilibNewCommands)
 vcpkg_cmake_config_fixup(PACKAGE_NAME wpimath)
 vcpkg_cmake_config_fixup(PACKAGE_NAME wpinet)
 vcpkg_cmake_config_fixup(PACKAGE_NAME wpiutil)
+vcpkg_cmake_config_fixup(PACKAGE_NAME xrpVendordep)
 
 vcpkg_copy_pdbs()
 
