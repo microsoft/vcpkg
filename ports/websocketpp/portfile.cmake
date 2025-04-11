6076a79c3aa05817d83d3b4ce0f4394b4f734638
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-10 22:15:50 -0400

    update for linux config path

--- ports/websocketpp/portfile.cmake
+++ ports/websocketpp/portfile.cmake
@@ -16,6 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH cmake/)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/websocketpp/)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
