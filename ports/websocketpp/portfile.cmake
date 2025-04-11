5736447239c9ad353f78c095c39c08d4d4c51583
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-10 22:23:05 -0400

    test

--- ports/websocketpp/portfile.cmake
+++ ports/websocketpp/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH cmake/)
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/websocketpp/)
+vcpkg_cmake_config_fixup(PACKAGE_NAME websocketpp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
