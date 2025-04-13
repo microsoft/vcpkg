1fd0a36887f9c851e84c4e669394e16c7806ca75
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-13 18:58:29 -0400

    correct licensefile name

--- ports/libcamera/portfile.cmake
+++ ports/libcamera/portfile.cmake
@@ -24,4 +24,4 @@ vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/man")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.rst")
