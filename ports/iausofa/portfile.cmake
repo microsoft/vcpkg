c9bc69cfd6bddf486e1a8946bb88eb1556294238
longzhmm <longzhmm@163.com>
2025-04-14 19:22:51 +0800

    Completed requested changes

--- ports/iausofa/portfile.cmake
+++ ports/iausofa/portfile.cmake
@@ -1,11 +1,10 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-# https://github.com/${REPO}/archive/${REF}.tar.gz
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO longzhmm/vcpkg_iausofa
     REF 8058fedf19ae2563dc643c04fde1d5f2b1057176
-    SHA512 de13bb1786e1c4b7556e8b53ac42c2b6a466c960ee69f85714e6f33aee72cfa9804edf43595f43bb660274c88ad2cd3a88f8675705bfdfb73cd08e3d6d5a93e4  # This is a temporary value. We will modify this value in the next section.
+    SHA512 de13bb1786e1c4b7556e8b53ac42c2b6a466c960ee69f85714e6f33aee72cfa9804edf43595f43bb660274c88ad2cd3a88f8675705bfdfb73cd08e3d6d5a93e4  
     HEAD_REF main
 )
 
