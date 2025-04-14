9cf96efebc2a4b528a8b9703c0e3a092e6765c41
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-13 20:23:52 -0400

    try fix

--- ports/libcamera/portfile.cmake
+++ ports/libcamera/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
 	REF "v${VERSION}"
 	SHA512 251e6436cb6d41cf80a502889dcbfa2925fc1a9addecce16f9d38b4cd6b8a9bb519553b917315e21b61ec90349d7aa6132c071071aacc7040dc7ab9108abd51d
 	HEAD_REF master
+	PATCHES
+		fix-absolute-paths.patch
 )
 
 vcpkg_find_acquire_program(PYTHON3)
