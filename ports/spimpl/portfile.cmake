469cb15df33923d97b4a6d06cdb99e2704b8fd39
flightless-coder <milica.bijelovic@gmail.com>
2025-04-15 16:47:33 +0200

    Add possibility to target spimpl package

--- ports/spimpl/portfile.cmake
+++ ports/spimpl/portfile.cmake
@@ -6,8 +6,16 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/spimplConfig.cmake" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+vcpkg_cmake_install()
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/spimpl.h")
 
-file(INSTALL "${SOURCE_PATH}/spimpl.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include/spimpl")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/README.md")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
