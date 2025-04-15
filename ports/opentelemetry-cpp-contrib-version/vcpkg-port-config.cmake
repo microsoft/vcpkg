dced804783ed84b0acc159d93eb9b1ecf48a66f6
Tom Tan <Tom.Tan@microsoft.com>
2025-04-15 15:10:49 -0700

    [opentelemetry-cpp-contrib-version] Update to 2025-04-15

--- ports/opentelemetry-cpp-contrib-version/vcpkg-port-config.cmake
+++ ports/opentelemetry-cpp-contrib-version/vcpkg-port-config.cmake
@@ -4,9 +4,9 @@ function(clone_opentelemetry_cpp_contrib CONTRIB_SOURCE_PATH)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO open-telemetry/opentelemetry-cpp-contrib
-        REF 69b8a42741b7f7063ad063d7957f7bc97bf701a4
+        REF 12a60a7009c900797ca0609e9f611d640da4548e
         HEAD_REF main
-        SHA512 1db239ab73f7c4d8ba97d42dd9f97b728d18a9a3a2fad342fda66231f0144aae829d9ceebd584979acc026aa9f0ca7aac0b73aa4eff2fc2f1f059b0f53eaecd3
+        SHA512 b41f3ea435f47af086e6a1477fa3f77c6d2b3e7c04e08d559cb17c35365e17395c3ee8a795e22acd0315e261fdf5713be73997e15d8c83edf87fbc463c0a3449
     )
     set(${CONTRIB_SOURCE_PATH} ${SOURCE_PATH} CACHE INTERNAL "")
 endfunction()
