dd0119aa10e5e88ced4961961912fce054072c70
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2025-04-16 17:36:45 +0200

    [sentry-native] update to 0.8.4
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- ports/sentry-native/portfile.cmake
+++ ports/sentry-native/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/getsentry/sentry-native/releases/download/${VERSION}/sentry-native.zip"
     FILENAME "sentry-native-${VERSION}.zip"
-    SHA512 6317e30f15f87d92dcbec72cea0f61ded012c768c23a0ed2eaab4355e0264aefab4a16cec15628e0a1527a605e4edf5b168b701593d782fb6e515a05ac7215d1
+    SHA512 0b33aa400303f78107883fde9e6a10d29d1b1847c71a8e0b5047603f8ad6ba48cf44b8ad74f90ca7fc2199b5a110992b76861f88108bc7433e10825f2c656ee8
 )
 
 vcpkg_extract_source_archive(
