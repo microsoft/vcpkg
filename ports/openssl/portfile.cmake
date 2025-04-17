15d0e47ccfc3066da0c2c350d11e3cf4f0ce8ee1
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2025-04-17 17:13:12 +0200

    Update ports/openssl/portfile.cmake
    Co-authored-by: Billy O'Neal <bion@microsoft.com>

--- ports/openssl/portfile.cmake
+++ ports/openssl/portfile.cmake
@@ -11,7 +11,7 @@ endif()
 vcpkg_download_distfile(
     WINDOWS_CRASH_PATCH
     URLS https://github.com/openssl/openssl/commit/2b5e7253b9a6a4cde64d3f2f22d71272f6ad32c5.patch?full_index=1
-    FILENAME windows/certstore-crash.patch
+    FILENAME openssl-certstore-crash-2b5e7253b9a6a4cde64d3f2f22d71272f6ad32c5.patch
     SHA512 a13054a457ee72fd3d1760810b5323d04be5aaf18f199824515ca596a72e98154ace4fefd747e32a7cc32c6e4ed2363b100bf65a729cf2361fcc76715d5b7cd1
 )
 
