a493dc39d5d65e199c09c39dee78cac42a34a78d
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-17 20:28:23 -0400

    fix no longer needed rename

--- ports/gstreamer/portfile.cmake
+++ ports/gstreamer/portfile.cmake
@@ -301,12 +301,6 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/KHR"
                     "${CURRENT_PACKAGES_DIR}/include/GL"
 )
 
-if("plugins-base" IN_LIST FEATURES)
-    file(RENAME "${CURRENT_PACKAGES_DIR}/lib/gstreamer-1.0/include/gst/gl/gstglconfig.h"
-                "${CURRENT_PACKAGES_DIR}/include/gstreamer-1.0/gst/gl/gstglconfig.h"
-    )
-endif()
-
 list(APPEND GST_BIN_TOOLS
     gst-inspect-1.0
     gst-launch-1.0
