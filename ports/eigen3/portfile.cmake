3bf208744fa9811c8e86edc1c39dbe564e9769ff
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-20 18:22:16 -0400

    fix eigen3

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -1,11 +1,14 @@
 vcpkg_buildpath_length_warning(37)
 
+block(SCOPE_FOR VARIABLES PROPAGATE SOURCE_PATH)
+set(VCPKG_BUILD_TYPE release) # header-only
+
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF 5330960900f23d84ef998aac6f27a635eed31753
-    SHA512 70c4f19059861988bad9a85da3468d2a1867dd942d54c2472b81a84205e1fdb93cd748ab938f02d7cb853aaf2d0cd98b2a453a87a5b90e609b1b680633129046
+    REF 33f5f596143d0fce32316ec6fa4bd9f23b4dd9d2
+    SHA512 8151a727c35fa2c88af04a6cfd765fa1928e89eedcf5d1425f8f801be4ec422906572028376ab78b08a770d6d5c42bcfc9682556ca94e2d50a470ca104024fbd
     HEAD_REF master
 )
 
@@ -15,23 +18,21 @@ vcpkg_cmake_configure(
         -DBUILD_TESTING=OFF
         -DEIGEN_BUILD_DOC=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
-	"-DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3"
-	"-DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig"
+        "-DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3"
+        "-DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig"
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
-
+endblock()
 
 if(NOT VCPKG_BUILD_TYPE)
-	    file(INSTALL "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/eigen3.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
+    file(INSTALL "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/eigen3.pc" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig")
 endif()
-
 vcpkg_fixup_pkgconfig()
 
 file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
 # Copy the eigen header files to conventional location for user-wide MSBuild integration
 file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.README")
