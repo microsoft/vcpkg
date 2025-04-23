5ebe1032af00f2957553c14595c71c2203c6a52a
myd7349 <myd7349@gmail.com>
2025-04-23 23:51:49 +0800

    Revert "[libdicom] Overwrite version"
    This reverts commit 790ef203595718d423488f9c51adc0f9dbdae265.
    
    Revert "[libdicom] Fix cross compile"
    
    This reverts commit 75555387d1b6594173b61a9df096702b73a4e69e.

--- ports/libdicom/portfile.cmake
+++ ports/libdicom/portfile.cmake
@@ -4,24 +4,12 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 dd3145721436eaab80e14750210c1b7528a0d23b77aa2e94acfd1bb24d22e3e3a616133f48244aa1927bf835a5d541c3ca3136518b740cd58114cd753f662917
     HEAD_REF main
-    PATCHES
-        fix-cross-compile.patch
 )
-
-set(dict_lookup_path "share/${PORT}/dict-lookup")
-
-set(options "")
-if(VCPKG_CROSSCOMPILING)
-    set(pregenerated_dict "${CURRENT_HOST_INSTALLED_DIR}/${dict_lookup_path}")
-    cmake_path(NATIVE_PATH pregenerated_dict pregenerated_dict)
-    set(options "-Dpregenerated_dict=${pregenerated_dict}")
-endif()
 
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -Dtests=false
-        ${options}
 )
 
 vcpkg_install_meson()
@@ -31,17 +19,6 @@ vcpkg_copy_pdbs()
 vcpkg_copy_tools(TOOL_NAMES dcm-dump dcm-getframe AUTO_CLEAN)
 
 vcpkg_fixup_pkgconfig()
-
-if(NOT VCPKG_CROSSCOMPILING)
-    file(
-        COPY "${CURRENT_PACKAGES_DIR}/${dict_lookup_path}/dicom-dict-lookup.h"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/${dict_lookup_path}"
-    )
-    file(
-        COPY "${CURRENT_PACKAGES_DIR}/${dict_lookup_path}/dicom-dict-lookup.c"
-        DESTINATION "${CURRENT_PACKAGES_DIR}/${dict_lookup_path}"
-    )
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
