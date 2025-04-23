f04cffa040ead433d649f4b47790b725a962558b
Вячеслав <infinitehash@hotmail.com>
2025-04-23 12:51:15 +0300

    Update ports/fltk/portfile.cmake
    Co-authored-by: Cheney Wang <38240633+Cheney-W@users.noreply.github.com>

--- ports/fltk/portfile.cmake
+++ ports/fltk/portfile.cmake
@@ -78,10 +78,7 @@ endif()
 if(EXISTS "${CURRENT_PACKAGES_DIR}/bin/fluid${VCPKG_TARGET_EXECUTABLE_SUFFIX}" OR
    EXISTS "${CURRENT_PACKAGES_DIR}/bin/fluid${VCPKG_TARGET_BUNDLE_SUFFIX}")
    file(REMOVE "${CURRENT_PACKAGES_DIR}/bin/fluid.icns" "${CURRENT_PACKAGES_DIR}/debug/bin/fluid.icns")
-   vcpkg_copy_tools(TOOL_NAMES fluid AUTO_CLEAN)
-   vcpkg_copy_tools(TOOL_NAMES fluid-cmd AUTO_CLEAN)
-   vcpkg_copy_tools(TOOL_NAMES fltk-options AUTO_CLEAN)
-   vcpkg_copy_tools(TOOL_NAMES fltk-options-cmd AUTO_CLEAN)
+   vcpkg_copy_tools(TOOL_NAMES fluid fluid-cmd fltk-options fltk-options-cmd AUTO_CLEAN)
 elseif(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE
         "${CURRENT_PACKAGES_DIR}/debug/bin"
