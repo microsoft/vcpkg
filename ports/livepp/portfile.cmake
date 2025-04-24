e5c900a9666862166d36edc8b590c03fc7984398
Rémy Tassoux <contact@rt2.fr>
2025-04-24 21:19:56 +0200

    [livepp] Fetch the eula file from the livepp archive instead of a separate link

--- ports/livepp/portfile.cmake
+++ ports/livepp/portfile.cmake
@@ -8,13 +8,6 @@ vcpkg_download_distfile(
     SHA512 07c5a9b30950d9c243d4fed671325e5e389e1e41e5c51feaa3d2cb7d64506f8baabf33f0c30851df8464a6c5f9fb15f11afac0c405ff8c4e513bc3034e63289d
 )
 
-vcpkg_download_distfile(
-	LICENSE
-    URLS https://liveplusplus.tech/downloads/LPP_EULA.pdf
-    FILENAME LPP_EULA.pdf
-    SHA512 19b435ab38e16c1c9527fd854c6a5643761819fc89367e7df76e9990b42af78eeff76ff3be172a6c0036a8428daf2f7a76b9e05c9c8b857bdee870a0045acbde
-)
-
 vcpkg_extract_source_archive(
 	SOURCE_PATH
 	ARCHIVE "${ARCHIVE}"
@@ -31,4 +24,4 @@ file(INSTALL "${SOURCE_PATH}/Agent" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/$
 file(INSTALL "${SOURCE_PATH}/Broker" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 file(INSTALL "${SOURCE_PATH}/CLI" DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}")
 
-vcpkg_install_copyright(FILE_LIST "${LICENSE}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/EULA/LPP_EULA.pdf")
