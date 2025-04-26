0c1e213460ea797d1dec0ea49f963a11f7ea5eeb
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-04-26 10:38:39 -0400

    libnuma

---
+++ ports/libnuma/portfile.cmake
@@ -0,0 +1,16 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO numactl/numactl
+    REF "v${VERSION}"
+    SHA512 0
+    HEAD_REF master
+)
+
+vcpkg_make_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTORECONF
+)
+
+vcpkg_make_install()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.GPL2")
