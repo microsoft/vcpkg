31f818e1d232662039f4780c550b847355360939
Kai Pastor <dg0yt@darc.de>
2025-04-26 06:49:12 +0200

    WIP [skip actions]

--- ports/gobject-introspection/vcpkg-port-config.cmake
+++ ports/gobject-introspection/vcpkg-port-config.cmake
@@ -8,7 +8,6 @@ function(vcpkg_get_gobject_introspection_python out_var)
             "Building and using ${PORT} will fail if the host cannot execute target binaries."
         )
     endif()
-    include("${CURRENT_HOST_INSTALLED_DIR}/share/vcpkg-get-python-packages/vcpkg-port-config.cmake")
     if(EXISTS "${CURRENT_HOST_INSTALLED_DIR}/share/python3/vcpkg-port-config.cmake")
         # Engage host python include guards. (Host python is not a dependency.)
         include("${CURRENT_HOST_INSTALLED_DIR}/share/python3/vcpkg-port-config.cmake")
@@ -25,11 +24,44 @@ function(vcpkg_get_gobject_introspection_python out_var)
             unset(z_vcpkg_get_vcpkg_installed_python CACHE)
         endif()
         vcpkg_get_vcpkg_installed_python(target_python)
-        x_vcpkg_get_python_packages(OUT_PYTHON_VAR gobject_introspection_python
-            PYTHON_EXECUTABLE "${target_python}"
-            PYTHON_VERSION "3"
-            PACKAGES setuptools
+
+        # Varation of x_vcpkg_get_python_packages, using venv also for windows
+        message(STATUS "Setting up python virtual environment...")
+        set(venv_path "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-venv")
+        file(REMOVE_RECURSE "${venv_path}") # Remove old venv
+        file(MAKE_DIRECTORY "${venv_path}")
+
+        set(python_sub_path /bin)
+        set(python_venv_options --symlinks)
+        if(CMAKE_HOST_WIN32)
+            set(python_sub_path /Scripts)
+            set(python_venv_options --copies)
+
+            get_filename_component(python_dir "${target_python}" DIRECTORY)
+            file(MAKE_DIRECTORY "${python_dir}/DLLs")
+            file(GLOB python_zipped_stdlib "${python_dir}/python3*.zip")
+            if(python_zipped_stdlib)
+                file(COPY ${python_zipped_stdlib} DESTINATION "${venv_path}/Scripts")
+            endif()
+        endif()
+
+        set(ENV{PYTHONNOUSERSITE} "1")
+        vcpkg_execute_required_process(
+            COMMAND "${target_python}" -I -m venv ${python_venv_options} "${venv_path}"
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+            LOGNAME "venv-setup-${TARGET_TRIPLET}"
         )
+        set(gobject_introspection_python "${venv_path}${python_sub_path}/python${VCPKG_HOST_EXECUTABLE_SUFFIX}")
+        set(ENV{VIRTUAL_ENV} "${venv_path}")
+        unset(ENV{PYTHONHOME})
+        unset(ENV{PYTHONPATH})
+        message(STATUS "Installing python packages: setuptools")
+        vcpkg_execute_required_process(
+            COMMAND "${gobject_introspection_python}" -I -m pip install setuptools
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+            LOGNAME "pip-install-packages-${TARGET_TRIPLET}"
+        )
+        message(STATUS "Setting up python virtual environment... finished.")
     endblock()
     set("${out_var}" "${gobject_introspection_python}" PARENT_SCOPE)
 endfunction()
