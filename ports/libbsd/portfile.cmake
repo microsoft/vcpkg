22cc6bd7aab60bad61d35b6778da5593c0de23d2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-04-26 15:08:25 -0600

    [ports/libbsd] Download distfile instead; use `vcpkg_configure_make`

--- ports/libbsd/portfile.cmake
+++ ports/libbsd/portfile.cmake
@@ -2,39 +2,29 @@ if(VCPKG_TARGET_IS_LINUX)
     message("${PORT} currently requires the following tools and libraries from the system package manager:\n    autoreconf\n    libudev\n\nThese can be installed on Ubuntu systems via apt-get install autoconf")
 endif()
 
-vcpkg_from_git(
-    OUT_SOURCE_PATH SOURCE_PATH
-    URL https://gitlab.freedesktop.org/libbsd/libbsd
-    REF 04a24db27ad1572f766bad772cdd9c146e6d9cf0
-    FETCH_REF "0.12.2"
-    HEAD_REF master
+vcpkg_download_distfile(
+        LIBBSD_ARCHIVE
+        URLS "https://libbsd.freedesktop.org/releases/libbsd-0.12.2.tar.xz"
+        FILENAME "libbsd-0.12.2.tar.xz"
+        SHA512 ce43e4f0486d5f00d4a8119ee863eaaa2f968cae4aa3d622976bb31ad601dfc565afacef7ebade5eba33fff1c329b5296c6387c008d1e1805d878431038f8b21
+)
+vcpkg_extract_source_archive(
+        SOURCE_PATH
+        ARCHIVE "${LIBBSD_ARCHIVE}"
+#        SOURCE_BASE "libbsd-0.12.2"
 )
 
 vcpkg_list(SET MAKE_OPTIONS)
 vcpkg_list(SET LIBBSD_LINK_LIBRARIES)
-vcpkg_execute_required_process(
-        COMMAND "./autogen"
-        WORKING_DIRECTORY "${SOURCE_PATH}"
-        LOGNAME "autoconf-${TARGET_TRIPLET}"
+vcpkg_configure_make(
+        SOURCE_PATH "${SOURCE_PATH}"
+        AUTOCONFIG
+        OPTIONS
+            ${MAKE_OPTIONS}
 )
-vcpkg_execute_required_process(
-        COMMAND "./configure"
-        WORKING_DIRECTORY "${SOURCE_PATH}"
-        LOGNAME "configure-${TARGET_TRIPLET}"
-)
-vcpkg_execute_required_process(
-        COMMAND "make"
-        WORKING_DIRECTORY "${SOURCE_PATH}"
-        LOGNAME "make-${TARGET_TRIPLET}"
-)
-#vcpkg_configure_make(
-#        SOURCE_PATH "${SOURCE_PATH}"
-#        AUTOCONFIG
-#        OPTIONS
-#            ${MAKE_OPTIONS}
-#)
 vcpkg_install_make()
 
 vcpkg_fixup_pkgconfig()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
