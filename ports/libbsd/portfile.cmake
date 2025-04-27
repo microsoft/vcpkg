a398c355a9851803c4b88ad712ae585445b6f0f4
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-04-26 19:08:37 -0600

    [ports/libbsd/vcpkg.json] Add optional gettext dependency ; [ports/libbsd/portfile.cmake] add `file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")` to resolve warning

--- ports/libbsd/portfile.cmake
+++ ports/libbsd/portfile.cmake
@@ -28,3 +28,4 @@ vcpkg_fixup_pkgconfig()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
