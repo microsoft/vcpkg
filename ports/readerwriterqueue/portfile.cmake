c11d2b9b95a2d76ee122cc2312fb1e6ef68aa555
miyanyan <1138989048@qq.com>
2025-04-28 20:11:55 +0800

    [readerwriterqueue] fix wrong usage

--- ports/readerwriterqueue/portfile.cmake
+++ ports/readerwriterqueue/portfile.cmake
@@ -13,7 +13,7 @@ vcpkg_cmake_configure(
 ) 
 
 vcpkg_cmake_install() 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake) 
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT}) 
 vcpkg_fixup_pkgconfig() 
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
