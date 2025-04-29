37a3d5c8ca632ab3c8ec2386dabfbf236d3a9465
Tal Regev <tal.regev@gmail.com>
2025-04-29 20:18:10 +0300

    test vcpkg

--- scripts/azure-pipelines/bootstrap-from-source.sh
+++ scripts/azure-pipelines/bootstrap-from-source.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-git clone https://github.com/microsoft/vcpkg-tool vcpkg-tool
-git -C vcpkg-tool switch -d $1
+git clone https://github.com/talregev/vcpkg-tool -b TalR/upload_by_chunks vcpkg-tool
 rm -rf build.x64.release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DVCPKG_DEVELOPMENT_WARNINGS=OFF -DVCPKG_WARNINGS_AS_ERRORS=OFF -DVCPKG_BUILD_FUZZING=OFF -DVCPKG_BUILD_TLS12_DOWNLOADER=OFF -B build.x64.release -S vcpkg-tool
 ninja -C build.x64.release
