36e7cec76bcb5ec5ca21a4157d49bdb77bb687c9
Connector Switch <c8ef@outlook.com>
2025-04-30 05:22:04 +0800

    [folly related] update to 04.21 (#45176)
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/wangle/portfile.cmake
+++ ports/wangle/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebook/wangle
     REF "v${VERSION}"
-    SHA512 d3aa8dc6ef184356f520341b858d9064f18a413cccec52d541cb4fc7895f700c1cbbf00ede7ee8ac52d6cc7c0779e68c6078a475ace25f24854c706bbe462798
+    SHA512 e88a99b43e89e343bacd706cefe9a4ed79b961cb8ea4e230979d383e927e7bc45ad6b543b5fe276699863796cd6ebe310176944c369e8853e7f2746ec90798e3
     HEAD_REF main
     PATCHES
         fix-config-cmake.patch
