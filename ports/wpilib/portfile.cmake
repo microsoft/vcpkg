ce0315f5882c2b125a1a7b95f986c80cac0abdf5
Yury Bura <yurybura@gmail.com>
2025-04-30 12:09:41 +0200

    [wpilib] update to v2025.3.2

--- ports/wpilib/portfile.cmake
+++ ports/wpilib/portfile.cmake
@@ -1,31 +1,24 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wpilibsuite/allwpilib
-    REF 165ebe4c79c437c7ba6c03af4a88a8c8680f742a
-    SHA512 f6ee07db0a119a7ac5876c4b0cf74abfb6af635d3d3ba913300138c450f62f6595ac4849bc499346f9f0179cc563f548a5e8a9a47122af593b425af453afd99f
-    PATCHES
-        no-werror.patch
-        windows-install-location.patch
-        missing-find_dependency.patch
-        fix-usage.patch
-        fix-build-error-with-fmt11.patch
-        fix-fmt.patch #https://github.com/wpilibsuite/allwpilib/pull/6796
+    REF v${VERSION}
+    SHA512 11b5394efbc54e724a48a93d960d69befecf38fd22457074458283e7e42fa011865a80022ff18162e65074e0bf9f008d298471b6ec76636361ba4eadbfdb512c
 )
 
 if("allwpilib" IN_LIST FEATURES)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH_APRILTAG
         REPO wpilibsuite/apriltag
-        REF e55b751f2465bd40a880d9acb87d24289e2af89e
-        SHA512 a5d824d11312f7f5229bad162349586e9c855cd1dc03f33235c045f2d5235932227eb17f9e9c801b46a28991cddcf7ad16d39549560251d7d9d52ce72f094a73
+        REF da208cc38c1b78fe89861616d44c0692e76b6b8b
+        SHA512 8d450d05ea6b65ff690af2424a3fc73e5a5887474058b4983dde5012fad25913ef9a122de897439cc837d59c18c692a8a2b4fa2195bd47f81760cc26828230c5
     )
 endif()
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         cameraserver WITH_CSCORE
-        allwpilib WITH_SIMULATION_MODULES
         allwpilib WITH_WPILIB
+        allwpilib WITH_SIMULATION_MODULES
 )
 
 vcpkg_find_acquire_program(PYTHON3)
@@ -35,11 +28,12 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DNO_WERROR=ON
         -DWITH_JAVA=OFF
+        -DWITH_DOCS=OFF
         -DWITH_EXAMPLES=OFF
         -DWITH_TESTS=OFF
         -DWITH_GUI=OFF
-        -DWITH_SIMULATION_MODULES=OFF
         -DUSE_SYSTEM_FMTLIB=ON
         -DUSE_SYSTEM_LIBUV=ON
         -DUSE_SYSTEM_EIGEN=ON
