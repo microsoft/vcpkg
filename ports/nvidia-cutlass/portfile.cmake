37f54b3a462d191af3df4b22e1a5d8f00d4140c1
endingly <jiasongdeng@qq.com>
2025-04-30 08:41:56 +0800

    fix
    Signed-off-by: endingly <jiasongdeng@qq.com>

--- ports/nvidia-cutlass/portfile.cmake
+++ ports/nvidia-cutlass/portfile.cmake
@@ -15,7 +15,7 @@ vcpkg_cmake_configure(
     OPTIONS
         -DCMAKE_SUPPRESS_REGENERATION=ON # for some reason it keeps regenerating in Windows
         "-DCUTLASS_REVISION:STRING=v${VERSION}"
-        # -DCUTLASS_NATIVE_CUDA=OFF
+        -DCUTLASS_NATIVE_CUDA=OFF
         -DCUTLASS_ENABLE_HEADERS_ONLY=ON
         -DCUTLASS_ENABLE_TOOLS=OFF
         -DCUTLASS_ENABLE_LIBRARY=OFF
@@ -31,9 +31,9 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "NvidiaCutlass")
 
 # note CUTLASS_ENABLE_LIBRARY=OFF
-# vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/NvidiaCutlass/NvidiaCutlassConfig.cmake"
-#     "add_library" "# add_library" # comment out the command
-# )
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/NvidiaCutlass/NvidiaCutlassConfig.cmake"
+    "add_library" "# add_library" # comment out the command
+)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug"
