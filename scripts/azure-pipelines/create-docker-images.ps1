c1ded9e322cc47da2900b283ea2e7c1c7ccc8bbe
Javier Matos <javiermatos@Javiers-Laptop.local>
2025-05-02 17:46:09 -0400

    add arm64 linux to CI

--- scripts/azure-pipelines/create-docker-images.ps1
+++ scripts/azure-pipelines/create-docker-images.ps1
@@ -3,11 +3,12 @@
 [CmdletBinding()]
 param(
     [switch]$OnlyAndroid,
-    [switch]$OnlyLinux
+    [switch]$OnlyLinux,
+    [switch]$OnlyArm64Linux
 )
 
-if ($OnlyAndroid -and $OnlyLinux) {
-    Write-Error "At most one of {-OnlyAndroid, -OnlyLinux} may be set"
+if (($OnlyAndroid + $OnlyLinux + $OnlyArm64Linux) -gt 1) {
+    Write-Error "At most one of {-OnlyAndroid, -OnlyLinux, -OnlyArm64Linux} may be set"
     return 1
 }
 
@@ -15,21 +16,29 @@ if ($OnlyAndroid) {
     Write-Host "Only building Android"
     $BuildAndroid = $true
     $BuildLinux = $false
+    $BuildArm64Linux = $false
 } elseif ($OnlyLinux) {
     Write-Host "Only building Linux"
     $BuildAndroid = $false
     $BuildLinux = $true
+    $BuildArm64Linux = $false
+} elseif ($OnlyArm64Linux) {
+    Write-Host "Only building Arm64 Linux"
+    $BuildAndroid = $false
+    $BuildLinux = $false
+    $BuildArm64Linux = $true
 } else {
     $BuildAndroid = $true
     $BuildLinux = $true
+    $BuildArm64Linux = $true
 }
 
 function Build-Image {
     param(
-    $AcrRegistry,
-    [string]$Location,
-    [string]$ImageName,
-    [string]$Date
+        $AcrRegistry,
+        [string]$Location,
+        [string]$ImageName,
+        [string]$Date
     )
 
     Push-Location $Location
@@ -37,7 +46,6 @@ function Build-Image {
         docker build . -t $ImageName --build-arg BUILD_DATE=$Date
         $remote = [string]::Format('{0}/{1}:{2}', $AcrRegistry.LoginServer, $ImageName, $Date)
         docker image tag $ImageName $remote
-
         docker push $remote
         Write-Host "Remote: $remote"
     } finally {
@@ -64,6 +72,13 @@ if ($BuildLinux) {
     Build-Image -AcrRegistry $registry `
         -Location "$PSScriptRoot/linux" `
         -ImageName "vcpkg-linux" `
+        -Date $Date
+}
+
+if ($BuildArm64Linux) {
+    Build-Image -AcrRegistry $registry `
+        -Location "$PSScriptRoot/linux-arm64" `
+        -ImageName "vcpkg-arm64-linux" `
         -Date $Date
 }
 
