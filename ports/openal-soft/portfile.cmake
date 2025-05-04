a7b090a1139eb946f7fcd52c45e2d9771c94dbf8
Kai Pastor <dg0yt@darc.de>
2025-05-04 12:24:28 +0200

    Restore pulseaudio feature

--- ports/openal-soft/portfile.cmake
+++ ports/openal-soft/portfile.cmake
@@ -13,6 +13,8 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         pipewire    ALSOFT_BACKEND_PIPEWIRE
         pipewire    ALSOFT_REQUIRE_PIPEWIRE
+        pulseaudio  ALSOFT_BACKEND_PULSEAUDIO
+        pulseaudio  ALSOFT_REQUIRE_PULSEAUDIO
 )
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
@@ -60,7 +62,6 @@ vcpkg_cmake_configure(
         -DLIBTYPE=${OPENAL_LIBTYPE}
         "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
         # order by CMakeLists.txt
-        -DALSOFT_BACKEND_PULSEAUDIO=OFF # cycle with gstreamer[openal] pulseaudio
         -DALSOFT_BACKEND_ALSA=${ALSOFT_REQUIRE_LINUX}
         -DALSOFT_REQUIRE_ALSA=${ALSOFT_REQUIRE_LINUX}
         -DALSOFT_BACKEND_OSS=OFF
