a8211b477c162ddc6baa90e6f74c8081926210c6
Nenad Banfic <nebanfic@microsoft.com>
2025-05-07 08:55:36 -0700

    Set HEAD_REF to main

--- ports/kleidiai/portfile.cmake
+++ ports/kleidiai/portfile.cmake
@@ -10,7 +10,7 @@ vcpkg_from_github(
     REPO ARM-software/kleidiai
     REF "v${VERSION}"
     SHA512 793bb36aaf32f72f78e87cea4f9971f75d160b8a18e5a06e4c234c6cb7a736fb359a80e3be56b91b265b5e478e52c277b9dfd405633012d26bcf5692e52051c3
-    HEAD_REF master
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
