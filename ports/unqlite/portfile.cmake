9bbde8a63f55456e63be65763e1bf2f933bb8ec3
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2025-05-08 15:58:50 -0700

    [unqlite] add thread-safety feature

--- ports/unqlite/portfile.cmake
+++ ports/unqlite/portfile.cmake
@@ -8,8 +8,15 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+FEATURES
+  "threads"     ENABLE_THREADS
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
