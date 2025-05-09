eb9b89f1ae7b9ed3ecb7f0a1da025e23cecefc0d
Zhichao Wang <zhicwang@microsoft.com>
2025-05-09 15:57:09 +0800

    fix comment

--- ports/cmark-gfm/portfile.cmake
+++ ports/cmark-gfm/portfile.cmake
@@ -1,7 +1,9 @@
+string(REGEX REPLACE "^([0-9]+\\.[0-9]+\\.[0-9]+)\\.([0-9]+)$" "\\1.gfm.\\2" GFM_VERSION "${VERSION}")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO github/cmark-gfm
-    REF 0.29.0.gfm.13
+    REF ${GFM_VERSION}
     SHA512 435298fcf782dfc5b64c578ac839759b9d5cd0c08eb90d6702f26278062a0f4887c65c18e89e2c9f6be23f10dd835c769a7e0f8c934be068b6754dcca30cdd7c
     HEAD_REF master
     PATCHES
@@ -20,8 +22,8 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCMARK_TESTS=OFF
-        -DCMARK_STATIC=ON
-        -DCMARK_SHARED=OFF
+        -DCMARK_SHARED=${CMARK_SHARED}
+        -DCMARK_STATIC=${CMARK_STATIC}
 )
 
 vcpkg_cmake_install()
