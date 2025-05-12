24e2adaaf80f71df4a3fd1395ce4d8d3dc2199e0
Rémy Tassoux <contact@rt2.fr>
2025-05-12 12:10:27 +0200

    [livepp] Add guard to prevent redefining livepp target

--- ports/livepp/unofficial-liveppConfig.cmake
+++ ports/livepp/unofficial-liveppConfig.cmake
@@ -1,3 +1,5 @@
-add_library(unofficial::livepp::livepp INTERFACE IMPORTED)
-set_target_properties(unofficial::livepp::livepp PROPERTIES
-     INTERFACE_COMPILE_DEFINITIONS LIVEPP_PATH="${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools/livepp")
+if(NOT TARGET unofficial::livepp::livepp)
+     add_library(unofficial::livepp::livepp INTERFACE IMPORTED)
+     set_target_properties(unofficial::livepp::livepp PROPERTIES
+          INTERFACE_COMPILE_DEFINITIONS LIVEPP_PATH="${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools/livepp")
+endif()
