f7ad18887be19ae52711a92d4f2ddbd5675539c2
Saad <saadmuizz@gmail.com>
2025-05-14 17:45:23 -0400

    update
    update to 1.2.0

--- ports/nanojsonc/portfile.cmake
+++ ports/nanojsonc/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO open-source-patterns/nanojsonc
         REF "${VERSION}"
-        SHA512 a83f38c67222d227faf352495b174a16ff6d5a0a45e9d6eda499ca6e8978d2dba184f820c7b726d80468a11fee02460cbd8b189d69a46716bd01c46e7b0607fd
+        SHA512 a79d4bbaf800dcc894df487cf375edb1db1d7e5e3d4c4444f24b80751a718c81cc867eb4890a1b364aed9f93296ee798aa361e24eb7c02627e98dcae5c073880
         HEAD_REF main
 )
 
