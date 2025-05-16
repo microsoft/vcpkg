68aaafe3faa929a2845c52aa827d6937abe1651e
Kai Pastor <dg0yt@darc.de>
2025-05-16 05:55:05 +0200

    [gstreamer] Update to 1.26.1

--- ports/gstreamer/portfile.cmake
+++ ports/gstreamer/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gstreamer/gstreamer
     REF "${VERSION}"
-    SHA512 5ca978cad5a661b081528be0fa74e199115c186afa1a0c9f55a9238fb2b452b680e75e8721a54077b9f4d717da5ef5801c359a0a89a5a02056caea067adab88f
+    SHA512 60ca414d05e219b760c6b22c693a5d127b368736a160217919719ad99d7e0ec7bc7a10390edeb27cc65fc647fd41e4bd9edddb60cb429f81e267c6e78a229c60
     HEAD_REF main
     PATCHES
         fix-clang-cl.patch
