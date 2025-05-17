671c6f875737733d2d1e2a10d4f46ed0cfdb04ff
Kai Pastor <dg0yt@darc.de>
2025-05-17 08:41:21 +0200

    [libmodbus] Update to 3.1.11

--- ports/libmodbus/portfile.cmake
+++ ports/libmodbus/portfile.cmake
@@ -2,9 +2,16 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stephane/libmodbus
     REF "v${VERSION}"
-    SHA512 6a01da1f8b486e356ff44874f1479d9d121463958a5ed06e60d910328ccc9b2d431b4a1fd72861c5c645c97b5887a076b763ad6a9ae6b18402dd043ec525b1e2
+    SHA512 cb506d5b72b629591002450221ea512a067209ee60c588dc88c45494ea983af2c05c47b2d0ba7db02e46d7a30110547ec96f9f98d643756f9528a99291683f70
     HEAD_REF master
+    PATCHES
+        cflags.diff
+        pkgconfig.diff
 )
+
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    set(ENV{WARNING_CFLAGS} "-D_CRT_SECURE_NO_DEPRECATE=1 -D_CRT_NONSTDC_NO_DEPRECATE=1")
+endif()
 
 vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -16,13 +23,6 @@ vcpkg_make_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 file(COPY "${CURRENT_PORT_DIR}/libmodbusConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libmodbus")
-
-if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libmodbus.pc" " -lmodbus" " -lmodbus -lws2_32")
-    if(NOT VCPKG_BUILD_TYPE)
-        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libmodbus.pc" " -lmodbus" " -lmodbus -lws2_32")
-    endif()
-endif()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
