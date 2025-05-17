64d4e9e8be0fe78f32933c097103203161fc13d4
Kai Pastor <dg0yt@darc.de>
2025-05-17 08:38:21 +0200

    [libmodbus] Use official build system

--- ports/libmodbus/portfile.cmake
+++ ports/libmodbus/portfile.cmake
@@ -4,28 +4,29 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 6a01da1f8b486e356ff44874f1479d9d121463958a5ed06e60d910328ccc9b2d431b4a1fd72861c5c645c97b5887a076b763ad6a9ae6b18402dd043ec525b1e2
     HEAD_REF master
-    PATCHES fix-static-linkage.patch
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" "${CMAKE_CURRENT_LIST_DIR}/config.h.cmake"
-     DESTINATION "${SOURCE_PATH}"
-)
-
-vcpkg_cmake_configure(
+vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS_DEBUG
-        -DDISABLE_INSTALL_HEADERS=ON
+    AUTORECONF
+    OPTIONS
+        --enable-tests=no
 )
-
-vcpkg_cmake_install()
+vcpkg_make_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
+vcpkg_fixup_pkgconfig()
+file(COPY "${CURRENT_PORT_DIR}/libmodbusConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libmodbus")
 
 if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/modbus.h" "elif defined(LIBBUILD)" "elif 1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libmodbus.pc" " -lmodbus" " -lmodbus -lws2_32")
+    if(NOT VCPKG_BUILD_TYPE)
+        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libmodbus.pc" " -lmodbus" " -lmodbus -lws2_32")
+    endif()
 endif()
 
-# Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.LESSER")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.LESSER")
