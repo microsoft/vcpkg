0442c20d0de2f4ee66f119745579238f504857c7
Kai Pastor <dg0yt@darc.de>
2025-05-18 19:51:17 +0200

    [libpq] Fix wrapper

--- ports/libpq/vcpkg-cmake-wrapper.cmake
+++ ports/libpq/vcpkg-cmake-wrapper.cmake
@@ -1,10 +1,5 @@
-# Give the CMake module a little bit of help to find the debug libraries
-find_library(PostgreSQL_LIBRARY_DEBUG
-NAMES pq
-PATHS
-  "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib"
-NO_DEFAULT_PATH
-)
+find_library(PostgreSQL_LIBRARY_RELEASE NAMES pq PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib" NO_DEFAULT_PATH)
+find_library(PostgreSQL_LIBRARY_DEBUG NAMES pq PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib" NO_DEFAULT_PATH)
 _find_package(${ARGS})
 if(PostgreSQL_FOUND AND NOT "@VCPKG_TARGET_IS_WINDOWS@")
     find_library(PostgreSQL_DL_LIBRARY NAMES dl)
