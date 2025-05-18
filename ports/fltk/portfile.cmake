91152c16f2d1dd12d0385bb749fbd90d94ea675e
Hash2000 <infinite_2000@mail.ru>
2025-05-18 10:01:26 +0300

    next test

--- ports/fltk/portfile.cmake
+++ ports/fltk/portfile.cmake
@@ -24,8 +24,9 @@ vcpkg_check_features(
         opengl  FLTK_USE_GL
 )
 
-if ("x11" IN_LIST FEATURES)
-    message(WARNING "You will need to install Xorg dependencies to use feature x11:\nsudo apt install libx11-dev libxft-dev libxext-dev\n")
+if(VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX OR VCPKG_TARGET_IS_ANDROID)    
+    message(WARNING "Building ${PORT} requires packages. please run the following command.\n
+        sudo apt-get update -y && sudo apt-get install -y libasound2-dev libglu1-mesa-dev libwayland-dev wayland-protocols libdbus-1-dev libxkbcommon-dev libegl-dev libopengl-dev libpango1.0-dev libgtk-3-dev")
 endif()
 
 set(fluid_path_param "")
