6a19f0a4d535dc2dff96af3626967d87739ca356
Moritz Beutel <mbeutel@users.noreply.github.com>
2025-05-19 16:19:35 +0200

    [gsl-lite] Update to v1.0.0

--- ports/gsl-lite/portfile.cmake
+++ ports/gsl-lite/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gsl-lite/gsl-lite
     REF "v${VERSION}"
-    SHA512 8215dc36418964b6c66f4bc8cd6f1eb39446e1563aaae3b015cb4f39bf2e5b69d34ef822cc0b4794a2ceef6944928543770727afcdc8060a6f61b548066d7c2b
+    SHA512 375ba0ba1ce2323d6918949c0a616500b3f666e3ec4d7046b41105b813f45c041787fd08f3a61498928edd304899e616ce950ff4be77b1119adc40ddacdc95e3
     HEAD_REF master
 )
 
@@ -11,13 +11,13 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(
-    CONFIG_PATH "lib/cmake/gsl-lite"
+    CONFIG_PATH "share/cmake/gsl-lite"
 )
 
 file(WRITE ${CURRENT_PACKAGES_DIR}/include/gsl-lite.hpp "#ifndef GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED
 #define GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED
-#pragma message(\"The header <gsl-lite.hpp> is deprecated and provided by Vcpkg for compatibility only; please include <gsl/gsl-lite.hpp> instead.\")
-#include <gsl/gsl-lite.hpp>
+#pragma message(\"The header <gsl-lite.hpp> is deprecated and provided by Vcpkg for compatibility only; please include <gsl-lite/gsl-lite.hpp> instead.\")
+#include <gsl-lite/gsl-lite.hpp>
 #endif // GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED")
 
 file(REMOVE_RECURSE
@@ -30,3 +30,5 @@ file(INSTALL
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     RENAME copyright
 )
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" @ONLY)
