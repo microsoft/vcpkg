2ac0d1805d6dc6c49a0cfc29e4527995f65c225a
flightless-coder <75297064+flightless-coder@users.noreply.github.com>
2025-05-21 09:40:49 +0200

    Update ports/spimpl/unofficial-spimpl-config.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/spimpl/unofficial-spimpl-config.cmake
+++ ports/spimpl/unofficial-spimpl-config.cmake
@@ -1,7 +1,8 @@
-add_library(unofficial::spimpl::spimpl INTERFACE IMPORTED)
-
-set_target_properties(
-  unofficial::spimpl::spimpl
-  PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../include"
-)
+if(NOT TARGET unofficial::spimpl::spimpl)
+  add_library(unofficial::spimpl::spimpl INTERFACE IMPORTED)
+  set_target_properties(
+    unofficial::spimpl::spimpl
+    PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../include"
+  )
+endif()
