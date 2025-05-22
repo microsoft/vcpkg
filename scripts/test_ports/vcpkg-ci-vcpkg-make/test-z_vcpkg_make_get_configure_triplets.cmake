588005408eef0a6d95355ca2b401ac02fba1b748
Kai Pastor <dg0yt@darc.de>
2025-05-22 09:50:02 +0200

    [vcpkg-make] Use --build when using --host

--- scripts/test_ports/vcpkg-ci-vcpkg-make/test-z_vcpkg_make_get_configure_triplets.cmake
+++ scripts/test_ports/vcpkg-ci-vcpkg-make/test-z_vcpkg_make_get_configure_triplets.cmake
@@ -1,21 +1,48 @@
-unit_test_check_variable_not_equal(
-    [[ z_vcpkg_make_determine_host_arch(host_arch) ]]
-    host_arch ""
-)
-
-if(VCPKG_TARGET_IS_LINUX AND NOT host_arch STREQUAL "arm64")
-    block(SCOPE_FOR VARIABLES)
-    set(VCPKG_CROSSCOMPILING TRUE)
-    unit_test_check_variable_equal(
-        [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "aarch64-linux-gnu-gcc") ]]
-        actual "--host=aarch64-linux-gnu"
+block(SCOPE_FOR VARIABLES  PROPAGATE build_opt)
+    z_vcpkg_make_get_configure_triplets(configure_triplets)
+    if(configure_triplets STREQUAL "")
+        # Initially empty configure_triplets is okay.
+        # Force non-empty configure_triplets via VCPKG_MAKE_BUILD_TRIPLET.
+        set(VCPKG_MAKE_BUILD_TRIPLET "--host=vcpkg")
+        z_vcpkg_make_get_configure_triplets(configure_triplets)
+    endif()
+    string(REGEX MATCH "--host=[^;]*" host_opt "${configure_triplets};")
+    unit_test_check_variable_not_equal(
+        [[ # match --host ]]
+        host_opt ""
     )
-    set(VCPKG_CROSSCOMPILING FALSE)
+    string(REGEX MATCH "--build=[^;]*" build_opt "${configure_triplets};")
+    unit_test_check_variable_not_equal(
+        [[ # match --build ]]
+        build_opt ""
+    )
+endblock()
+
+if(VCPKG_MAKE_BUILD_TRIPLET)
+    if(VCPKG_MAKE_BUILD_TRIPLET MATCHES "--host" AND NOT VCPKG_MAKE_BUILD_TRIPLET MATCHES "--build")
+        unit_test_check_variable_equal(
+            [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "x86_64-linux-gnu-clang-12") ]]
+            actual "${VCPKG_MAKE_BUILD_TRIPLET};${build_opt}"
+        )
+    endif()
+elseif(VCPKG_TARGET_IS_ANDROID)
+    unit_test_check_variable_equal(
+        [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "/bin/armv7a-linux-androideabi28-clang") ]]
+        actual "--host=armv7a-linux-androideabi28;${build_opt}"
+    )
+elseif(VCPKG_TARGET_IS_LINUX)
     unit_test_check_variable_equal(
         [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "gcc") ]]
         actual ""
     )
-    endblock()
+    unit_test_check_variable_equal(
+        [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "/bin/aarch64-linux-gnu-gcc-13") ]]
+        actual "--host=aarch64-linux-gnu;${build_opt}"
+    )
+    unit_test_check_variable_equal(
+        [[ z_vcpkg_make_get_configure_triplets(actual COMPILER_NAME "/usr/bin/x86_64-linux-gnu-clang-12") ]]
+        actual "--host=x86_64-linux-gnu;${build_opt}"
+    )
 endif()
 
 # branching as in vcpkg_make_configure
@@ -77,3 +104,15 @@ elseif(VCPKG_TARGET_IS_MINGW)
         CMAKE_MATCH_1
     )
 endif()
+
+# VCPKG_MAKE_BUILD_TRIPLET robustness
+set(VCPKG_MAKE_BUILD_TRIPLET "--host=HHH;--build=BBB")
+unit_test_check_variable_equal(
+    [[ z_vcpkg_make_get_configure_triplets(actual) ]]
+    actual "--host=HHH;--build=BBB"
+)
+set(VCPKG_MAKE_BUILD_TRIPLET "--build=bbb;--host=hhh")
+unit_test_check_variable_equal(
+    [[ z_vcpkg_make_get_configure_triplets(actual) ]]
+    actual "--host=hhh;--build=bbb"
+)
