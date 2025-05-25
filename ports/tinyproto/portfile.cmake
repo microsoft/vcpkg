73d3f0e37648f7e34514364044c27a42fb03a352
Reza Alizadeh Majd <r.a.majd@gmail.com>
2025-05-25 14:19:41 +0330

    tinyproto: prepare for project PR

--- ports/tinyproto/portfile.cmake
+++ ports/tinyproto/portfile.cmake
@@ -1,17 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO lexus2k/tinyproto
-    REF e0d5a3b972ae0b50bbca57ac436041364996577c
-    SHA512 1699d09815f06c40fc1581d6474e217675054b87b0fa7656fc2db7e181a2336f8efd8caadd3af700d685a37543e901d659995cc5baa219848cbfe11ca8124425
+    REPO ramajd/tinyproto
+    REF 7fcc42152b4b30c72495c018d5d29b229f37d22a
+    SHA512 86831b9c94ee57dcc561bfef46c1ae2b4bdad95de656be4f5b28acf59fe0ad05c6636b74dcd41dbc95677bee96711968111676c1664fb27dfacc9a5d7bdbefb4
     HEAD_REF master
-    PATCHES 
-        install.patch
 )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 
 vcpkg_cmake_install()
 
+vcpkg_cmake_config_fixup(
+	PACKAGE_NAME "tinyproto"
+	CONFIG_PATH lib/cmake/tinyproto
+)
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
