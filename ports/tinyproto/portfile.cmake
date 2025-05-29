c027ba29edb7d8fd6ab10e1005ebdd6f7d8c088d
Kai Pastor <dg0yt@darc.de>
2025-05-29 10:21:21 +0200

    [tinyproto] Switch to `version-date` (#45730)

--- ports/tinyproto/portfile.cmake
+++ ports/tinyproto/portfile.cmake
@@ -3,19 +3,14 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lexus2k/tinyproto
-    REF dc7a6c2186fdb38f0a263d26e81e204437b7ee66
+    REF dc7a6c2186fdb38f0a263d26e81e204437b7ee66 # 1.0.0 + 156 commits
     SHA512 13dc128567357b08cdee15ab710649f9eaaf58b466afba429ac0059aeaa4c908445dd20e9576ddfd471712aa7556805b7d415c2e7c6d8e8a00d7c076d0d270c2
     HEAD_REF master
 )
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
-
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(
-	PACKAGE_NAME "tinyproto"
-	CONFIG_PATH lib/cmake/tinyproto
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/tinyproto")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
