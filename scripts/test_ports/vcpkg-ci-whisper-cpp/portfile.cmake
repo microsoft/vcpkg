f8dc3ca540874a7b4d432acec97c30015748de09
Kai Pastor <dg0yt@darc.de>
2025-05-29 17:54:54 +0200

    [vcpkg-ci-whisper-cpp] New test port

---
+++ scripts/test_ports/vcpkg-ci-whisper-cpp/portfile.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+vcpkg_find_acquire_program(PKGCONFIG)
+set(ENV{PKGCONFIG} "${PKGCONFIG}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${CURRENT_PORT_DIR}/project"
+)
+vcpkg_cmake_build()
