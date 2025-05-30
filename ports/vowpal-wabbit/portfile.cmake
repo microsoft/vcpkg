bae4124e35f41296cabc5a501cb60214f01a84cf
Yury Bura <yurybura@gmail.com>
2025-05-30 12:54:40 +0200

    [vowpal-wabbit] remove including of `<sys/timeb.h>`

--- ports/vowpal-wabbit/portfile.cmake
+++ ports/vowpal-wabbit/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
         cmake_remove_bin_targets.patch
         fix-build-error-with-fmt11.patch
         fix-external-libraries.patch
+        remove-legacy-header.patch
 )
 
 vcpkg_cmake_configure(
