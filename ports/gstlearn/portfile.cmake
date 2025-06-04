abdd84613ac465076aed8d113171c14d8c1d40e6
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-06-04 14:05:20 -0400

    whoops fix sha

--- ports/gstlearn/portfile.cmake
+++ ports/gstlearn/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gstlearn/gstlearn
     REF "stable_${VERSION}"
-    SHA512 0
+    SHA512 5e7850647a29a51138c66ac4815340b83598707751d01cc87e2eef70bc7ec8ff6e995b5f83fa6da9875c62150ea811467a7fd6c88944ef4e92c9a86eb5c80dcd
     HEAD_REF dev
 )
 
