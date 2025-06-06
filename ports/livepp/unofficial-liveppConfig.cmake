095872bdff89833e4572ff9c559fd403e43f9d78
Rémy Tassoux <contact@rt2.fr>
2025-06-07 00:20:06 +0200

    [livepp] Add VCPKG_ prefix to LIVEPP_PATH macro

--- ports/livepp/unofficial-liveppConfig.cmake
+++ ports/livepp/unofficial-liveppConfig.cmake
@@ -1,5 +1,5 @@
 if(NOT TARGET unofficial::livepp::livepp)
      add_library(unofficial::livepp::livepp INTERFACE IMPORTED)
      set_target_properties(unofficial::livepp::livepp PROPERTIES
-          INTERFACE_COMPILE_DEFINITIONS LIVEPP_PATH="${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools/livepp")
+          INTERFACE_COMPILE_DEFINITIONS VCPKG_LIVEPP_PATH="${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/tools/livepp")
 endif()
