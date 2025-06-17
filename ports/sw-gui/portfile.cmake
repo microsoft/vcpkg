38f0c2f1c9c174c33fb0983db579803222fab6ab
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-06-17 19:18:22 +0800

    Update ports/sw-gui/portfile.cmake
    Co-authored-by: Lily Wang <94091114+LilyWangLL@users.noreply.github.com>

--- ports/sw-gui/portfile.cmake
+++ ports/sw-gui/portfile.cmake
@@ -1,8 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO    Mzying2001/sw
-    REF     0.0.1
-    SHA512  2e1c3ea6197244cf750d17bc470e86f76e4027db919529d78bbcd30ef824eface20ea1b60fb403331f3d2ed63222088dbb114cc4cb4584fd4090723867f2abd2
+    REPO Mzying2001/sw
+    REF ${VERSION}
+    SHA512 2e1c3ea6197244cf750d17bc470e86f76e4027db919529d78bbcd30ef824eface20ea1b60fb403331f3d2ed63222088dbb114cc4cb4584fd4090723867f2abd2
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
