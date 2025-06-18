2f22fb7674e95d5700c958d38da0596ec6cc25c7
Andrei Gavrilin <gav2xlin@gmail.com>
2025-06-18 17:16:54 +0200

    [tracy] update to 0.12.1

--- ports/tracy/portfile.cmake
+++ ports/tracy/portfile.cmake
@@ -1,18 +1,11 @@
-vcpkg_download_distfile(PATCH_MISSING_CHRONO_INCLUDE
-    URLS https://github.com/wolfpld/tracy/commit/50ff279aaddfd91dc3cdcfd5b7aec3978e63da25.diff?full_index=1
-    SHA512 f9594297ea68612b68bd631497cd312ea01b34280a0f098de0d2b99810149345251a8985a6430337d0b55d2f181ceac10d563b64cfe48f78959f79ec7a6ea3b5
-    FILENAME wolfpld-tracy-PR982.diff
-)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wolfpld/tracy
     REF "v${VERSION}"
-    SHA512 d3d99284e3c3172236c3f02b3bc52df111ef650fb8609e54fb3302ece28e55a06cd16713ed532f1e1aad66678ff09639dfc7e01a1e96880fb923b267a1b1b79b
+    SHA512 991ac48a00943b349b1bed9110fa7886d1378bf4e0f488b214aca244db988a9c39bfb8b4df6e60cea604fbb5d43120d6be68a0fcfbf41300547a4726a62a98d3
     HEAD_REF master
     PATCHES
         build-tools.patch
-		"${PATCH_MISSING_CHRONO_INCLUDE}"
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -40,6 +33,10 @@ vcpkg_cmake_configure(
     OPTIONS
         -DDOWNLOAD_CAPSTONE=OFF
         -DLEGACY=ON
+        -DCPM_LOCAL_PACKAGES_ONLY=ON
+        -DCPM_DONT_UPDATE_MODULE_PATH=ON
+        -DFETCHCONTENT_FULLY_DISCONNECTED=ON
+        -DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS
         ${FEATURE_OPTIONS}
     OPTIONS_RELEASE
         ${TOOLS_OPTIONS}
@@ -49,7 +46,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(PACKAGE_NAME Tracy)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Tracy CONFIG_PATH lib/cmake/Tracy)
 
 function(tracy_copy_tool tool_name tool_dir)
     vcpkg_copy_tools(
