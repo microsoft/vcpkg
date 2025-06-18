31655ccdfd59c95608cd81c99081aeb7c889d92e
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-06-18 12:52:54 -0600

    [ports/kubazip] Upgrade

--- ports/kubazip/portfile.cmake
+++ ports/kubazip/portfile.cmake
@@ -3,19 +3,21 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kuba--/zip
-    REF "v${VERSION}"
-    SHA512 dd516f2e6765e49c00063bd72b49d97bc6f1254921045269b534a801e41e0728171cbf09a73713b236f40c4cf30b5473e74579b82962004b79166b38ad6a3e0d
+    REF a0fcdbf0fae4ebba741cc41cd1b286e52f3e3424
+    SHA512 23db0c92893a07de786c6520425de8373f4642f8438248cb1e8ffca6668b73032e666cbb5175cf0f2cc06dd913cc1a76a6eac6bd7b0be355a146f4a865cd7568
     HEAD_REF master
     PATCHES
-        fix_targets.patch
+        c89-support-and-rename-zip-kubazip.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCMAKE_DISABLE_TESTING=ON
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/kubazip)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
