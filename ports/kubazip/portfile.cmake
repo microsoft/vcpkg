13f280edd1397b66ba06f79aed12935fbe0228c6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-06-18 21:57:25 -0600

    [ports/kubazip] Upgrade

--- ports/kubazip/portfile.cmake
+++ ports/kubazip/portfile.cmake
@@ -2,18 +2,19 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO kuba--/zip
-    REF a0fcdbf0fae4ebba741cc41cd1b286e52f3e3424
-    SHA512 23db0c92893a07de786c6520425de8373f4642f8438248cb1e8ffca6668b73032e666cbb5175cf0f2cc06dd913cc1a76a6eac6bd7b0be355a146f4a865cd7568
+    REPO SamuelMarks/zip
+    REF 0bb9deec79589aea4e1923dfc2c4fc5b8dfe1770
+    SHA512 f98edabddec0677d5d80129640077b45ac566fa8b5bb325d1906326c01a6f6a486a25ed0b5e2a29366bf0130074e3c9ebb9dcfd7f3ab9084bcd2daa3f0267145
     HEAD_REF master
-    PATCHES
-        c89-support-and-rename-zip-kubazip.patch
 )
 
+include(GNUInstallDirs)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCMAKE_DISABLE_TESTING=ON
+        -DCMAKE_DISABLE_TESTING="ON"
+        -DNEW_PROJECT_NAME="kubazip"
+        -DNEW_INSTALL_PATH="${CMAKE_INSTALL_INCLUDEDIR}/${PORT}"
 )
 
 vcpkg_cmake_install()
