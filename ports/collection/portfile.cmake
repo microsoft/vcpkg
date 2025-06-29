4009c190d062c18ef569a47269d65754567d796a
Saad <saadmuizz@gmail.com>
2025-06-29 01:56:51 -0400

    update
    extern

--- ports/collection/portfile.cmake
+++ ports/collection/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-source-patterns/collection
     REF "${VERSION}"
-    SHA512 dd9de90d163eee072b3ac5cf2205c4b3465310ac8c35f60b1c800ef8b2c56ba7a5bd225e310135da76587ab06edb431d568e590290d1b8fb792595bbe438fd0b
+    SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
     HEAD_REF main
 )
 
