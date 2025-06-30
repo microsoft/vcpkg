eab6b4a8adf8edd18ceadce84078aa86f9ed6bde
toge <toge.mail@gmail.com>
2025-06-30 17:19:37 +0900

    [stlab] update to 2.0.0, [stlab-copy-on-write] add new port

--- ports/stlab/portfile.cmake
+++ ports/stlab/portfile.cmake
@@ -2,10 +2,11 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stlab/libraries
     REF "v${VERSION}"
-    SHA512 ceed4fffc381bebd5456d56e8f9d84094da2a9994c8be60e9c1fe4a72ae5f2c398448169927af1439615b55c549332dfe4c38a2b3b8bdf84e3c550fd14bf125c
+    SHA512 343eeb970374d3ff989d9e688f4971b717f2e20b6d6fe1a4a1754484b4b7330075cb94e427d972d936fc8dc4650b1f00ca63d72981fb2d21373c1733b56fd124
     HEAD_REF main
     PATCHES
         cross-build.patch
+        devendoring.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -25,7 +26,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/stlab)
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/share/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/stlabConfig.cmake"
     "find_dependency(Boost 1.74.0)"
