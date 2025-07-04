c120bec9bbbd13b8fcf69fa1a1013a2249f15147
night1rider <backman23@live.com>
2025-07-04 16:38:03 -0600

    wolfTPM: Adding feature for No Active Thread Local Storage

--- ports/wolftpm/portfile.cmake
+++ ports/wolftpm/portfile.cmake
@@ -6,12 +6,38 @@ vcpkg_from_github(
     HEAD_REF master
     )
 
+if ("${VERSION}" VERSION_GREATER_EQUAL "3.9.2") #CMAKE logic added in versions newer than 3.9.1
+  if ("no-active-thread-ls" IN_LIST FEATURES)
+    set(ENABLE_NO_ACTIVE_THREAD_LS yes)
+  else()
+    set(ENABLE_NO_ACTIVE_THREAD_LS no)
+  endif()
+endif()
+
+
+vcpkg_cmake_get_vars(cmake_vars_file)
+include("${cmake_vars_file}")
+
+foreach(config RELEASE DEBUG)
+  if ("${VERSION}" VERSION_LESS "3.9.2") #CMAKE logic not present in versions older than 3.9.2
+    if ("no-active-thread-ls" IN_LIST FEATURES)
+      string(APPEND VCPKG_COMBINED_C_FLAGS_${config} " -DWOLFTPM_NO_ACTIVE_THREAD_LS")
+    endif()
+  endif()
+endforeach()
+
+# Add debug flag for debug builds
+string(APPEND VCPKG_COMBINED_C_FLAGS_DEBUG " -DDEBUG_WOLFTPM")
+
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
       -DWOLFTPM_EXAMPLES=no
       -DWOLFTPM_BUILD_OUT_OF_TREE=yes
+      -DWOLFTPM_NO_ACTIVE_THREAD_LS=${ENABLE_NO_ACTIVE_THREAD_LS}
+    OPTIONS_RELEASE
+      -DCMAKE_C_FLAGS=${VCPKG_COMBINED_C_FLAGS_RELEASE}
     OPTIONS_DEBUG
-      -DCMAKE_C_FLAGS='-DDEBUG_WOLFTPM'
+      -DCMAKE_C_FLAGS=${VCPKG_COMBINED_C_FLAGS_DEBUG}
     )
 
 vcpkg_cmake_install()
