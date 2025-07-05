28f04de97dcb2c6b672bb999e751bc0d138b52b6
toge <toge.mail@gmail.com>
2025-07-05 15:34:55 +0900

    remove PACKAGE_NAME
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/stlab-copy-on-write/portfile.cmake
+++ ports/stlab-copy-on-write/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME stlab-copy-on-write CONFIG_PATH lib/cmake/stlab-copy-on-write)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/stlab-copy-on-write)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
