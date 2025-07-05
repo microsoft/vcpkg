1185c09fa764d82eee977d0f9ab54a9420e8a921
James Grant <42079499+jamesg-nz@users.noreply.github.com>
2025-07-05 19:25:01 +1200

    [qpdf] Don't use Ninja specific install targets

--- ports/qpdf/portfile.cmake
+++ ports/qpdf/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO qpdf/qpdf
     REF v${VERSION}
     SHA512 22395160ff16556fe3544790dff1ade63489cfc494c46ae84e7db4b41e0592b7b6ee4d80e4d3862491f09db91ab13868abcfc22b7918a74fe2966669d619469b
+    PATCHES
+        cmake-library-only.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
@@ -18,7 +20,6 @@ vcpkg_find_acquire_program(PKGCONFIG)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    GENERATOR Ninja
     OPTIONS
         ${FEATURE_OPTIONS}
         -DREQUIRE_CRYPTO_NATIVE=ON
@@ -26,9 +27,7 @@ vcpkg_cmake_configure(
         -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
 )
 
-# Don't want qpdf program, but no build option to exclude it. Use Ninja generator subdirectory install targets.
-vcpkg_cmake_build(TARGET libqpdf/install)
-vcpkg_cmake_build(TARGET include/install)
+vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/qpdf)
 vcpkg_copy_pdbs()
