559e9ce4f9f60b8ff6ff6dd49da84341fef9af95
Saikari <lin@sz.cn.eu.org>
2025-07-07 00:43:22 +0300

    fix broken keystone package

--- ports/keystone/portfile.cmake
+++ ports/keystone/portfile.cmake
@@ -1,12 +1,21 @@
+vcpkg_download_distfile(FIX_GCC15
+    URLS https://github.com/keystone-engine/keystone/pull/593.diff?full_index=1
+    FILENAME fix-gcc15.patch
+    SHA512 2cfe7e48bd244f2540d31eb59922dd2a49093f0f97fa69e1e3b7582ea0a3ee9454ad071ef2e65fd04be66ca8febdd93f52cda1cc197598200b34702de0b602e6
+)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO keystone-engine/keystone
     REF dc7932ef2b2c4a793836caec6ecab485005139d6 # 0.9.2
     SHA512 ebcdb1cca6dfdf76e0ad2a42a667044806e5c083c07357908298c6ef23d15960f887efa05c1cb3dee90ebdcd5af819bcf8af0fa1aa068aa9a0c6703dee29514e
     HEAD_REF master
+    PATCHES
+        "${FIX_GCC15}"
 )
 
 vcpkg_find_acquire_program(PYTHON3)
+vcpkg_find_acquire_program(PKGCONFIG)
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" KEYSTONE_BUILD_STATIC_RUNTIME)
 
@@ -14,7 +23,8 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DKEYSTONE_BUILD_STATIC_RUNTIME=${KEYSTONE_BUILD_STATIC_RUNTIME}
-        -DPYTHON_EXECUTABLE=${PYTHON3}
+        "-DPYTHON_EXECUTABLE=${PYTHON3}"
+        "-DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}"
 
         # Add support for only a subset of architectures
         #-DLLVM_TARGETS_TO_BUILD="AArch64;X86"
