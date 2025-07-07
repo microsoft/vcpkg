5bc7cf635137faeee4dc8877b74080ba27a2380b
toge <toge.mail@gmail.com>
2025-07-08 01:22:50 +0900

    fix build error on windows

--- ports/blosc2/portfile.cmake
+++ ports/blosc2/portfile.cmake
@@ -29,7 +29,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 if (VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_cmake_config_fixup(CONFIG_PATH "cmake/Blosc2")
+    vcpkg_cmake_config_fixup(CONFIG_PATH "cmake")
 else()
     vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Blosc2")
 endif()
