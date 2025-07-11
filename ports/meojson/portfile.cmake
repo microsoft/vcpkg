2eb8c80b0bd3db5258c18329bb8c04b304f18647
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2025-07-11 23:27:19 +0800

    fix from community member feedbak

--- ports/meojson/portfile.cmake
+++ ports/meojson/portfile.cmake
@@ -1,4 +1,4 @@
-#header-only library
+set(VCPKG_BUILD_TYPE release)  # header-only library
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -19,8 +19,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/meojson)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
