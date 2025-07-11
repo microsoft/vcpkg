bdf6b71a9ea8137f0ae409544a0d81a4b3baf793
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2025-07-11 20:23:19 +0800

    remove unnecessary configuration repair steps

--- ports/meojson/portfile.cmake
+++ ports/meojson/portfile.cmake
@@ -17,10 +17,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/meojson)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
