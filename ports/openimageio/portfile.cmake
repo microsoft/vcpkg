634cee449533eed6665ff610f35e57d3330b985d
Joachim Reichel <43646584+jreichel-nvidia@users.noreply.github.com>
2025-07-15 10:49:50 +0200

    One more try with -DPython3_EXECUTABLE.

--- ports/openimageio/portfile.cmake
+++ ports/openimageio/portfile.cmake
@@ -51,6 +51,11 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         viewer      ENABLE_IV
 )
 
+if("pybind11" IN_LIST FEATURES)
+    vcpkg_get_vcpkg_installed_python(PYTHON3)
+    list(APPEND FEATURE_OPTIONS "-DPython3_EXECUTABLE=${PYTHON3}")
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
