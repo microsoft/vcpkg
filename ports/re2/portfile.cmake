73fa087182e7da7de8dc9694152630ec569d188e
Kai Pastor <dg0yt@darc.de>
2025-07-17 07:36:41 +0200

    [re2] Fix download

--- ports/re2/portfile.cmake
+++ ports/re2/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/re2
-    REF "${VERSION}"
-    SHA512 5336c9aa1dcf528290bfae08f277f2d6d9a590cca3ece362b3f37d13192a13cd8580afe1919bdff137de22cfdb4254269cbcc4a7835d65962278d68ff2886d53
+    REF 8ea5841693c6c0b837c6ed2189217e8f8d6fee9c
+    SHA512 547ba362da00457fd0687ce1c45ba74ef5426c82abab66b9e05c13401730195c928ec301b5d6bf65216d6e595dab2d67f486e83f0a8fd73a07aab9d4ad138c5f
     HEAD_REF master
 )
 
@@ -13,11 +13,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
 vcpkg_fixup_pkgconfig()
 
-vcpkg_copy_pdbs()
-
-# Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
