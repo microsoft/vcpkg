6771a12fc0fbef13b3c3e40e5993e2bf3a17b9fb
Mike Malburg <m.malburg@techsmith.com>
2025-07-21 13:44:44 -0400

    Update rlottie to the latest commit
    This updates to the latest commit, but is mainly just meant to pull in fixes for these CVEs:
    - https://www.cve.org/CVERecord?id=CVE-2025-0634
    - https://www.cve.org/CVERecord?id=CVE-2025-53074
    - https://www.cve.org/CVERecord?id=CVE-2025-53075
    
    All three of these reference this PR as a fix for this: https://github.com/Samsung/rlottie/pull/571, which was merged in on June 10, here: https://github.com/Samsung/rlottie/commit/507ea027e47d3e1dc7ddbd9994621215eae7ebb9

--- ports/rlottie/portfile.cmake
+++ ports/rlottie/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Samsung/rlottie
-        REF e3026b1e1a516fff3c22d2b1b9f26ec864f89a82
-        SHA512 3b9985606d9c475e77ecb018cfe65cde1170f10e9d2c3e18b60178d3954a4870e5141aa06bb79e803fcdbcf98742bcf72a359625a3b1409125ec3a4a1b0126c4
+        REF 36ddb42d78d1b13c1b1d7e1699aef8a9f339ab6f
+        SHA512 ac7673afc60ca3c35a3d144c01ee611a2b01b052155ce225ad27b1f5d21dae9fa6cf02b5761a6ee8faade639b0aba2862938e2b26b8e3b43451974e7b0adef41
         PATCHES
             vcpkg.patch
 )
