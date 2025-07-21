8125e7de3e6b6f027d172fc00280ed7e8392a7ac
Mike Malburg <m.malburg@techsmith.com>
2025-07-21 13:00:44 -0400

    Update patches using latest source
    Originally, there was just one vcpkg.patch.  I have separated this out
    into separate patch files, each named to indicate what change it is making.

--- ports/rlottie/portfile.cmake
+++ ports/rlottie/portfile.cmake
@@ -4,7 +4,10 @@ vcpkg_from_github(
         REF 36ddb42d78d1b13c1b1d7e1699aef8a9f339ab6f
         SHA512 ac7673afc60ca3c35a3d144c01ee611a2b01b052155ce225ad27b1f5d21dae9fa6cf02b5761a6ee8faade639b0aba2862938e2b26b8e3b43451974e7b0adef41
         PATCHES
-            vcpkg.patch
+            0001-cmakelists-vcpkg-and-rapidjson-support.patch
+            0002-lottieparser-fix-rapidjson-include.patch
+            0003-vector-cmakelists-remove-pixman.patch
+            0004-vector-vdrawhelper-remove-pixman.patch
 )
 
 vcpkg_cmake_configure(
