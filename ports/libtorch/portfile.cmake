33620dcb4fbd241ca4f44d54c66a8c1039883be6
luncliff <luncliff@gmail.com>
2025-07-22 23:03:39 +0900

    [libtorch] recreate patches, match case of fixup names
    - fix xnnpack-microkernels-prod link error
    - fix nnpack, kleidiai, vulkan build
    - use cudnn-frontend port
    - use shaderc port for glslc executable

--- ports/libtorch/portfile.cmake
+++ ports/libtorch/portfile.cmake
@@ -1,5 +1,7 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
+set(GIT_COMMIT e2d141dbde55c2a4370fac5165b0561b6af4798b) # cmake/Codegen.cmake
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/pytorch
@@ -9,6 +11,8 @@ vcpkg_from_github(
     PATCHES
         fix-cmake.patch
         fix-glog.patch
+        fix-kineto.patch
+        fix-vulkan.patch
 )
 
 file(REMOVE_RECURSE "${SOURCE_PATH}/caffe2/core/macros.h") # We must use generated header files
@@ -21,16 +25,6 @@ vcpkg_from_github(
     HEAD_REF main
 )
 file(COPY "${src_kineto}/" DESTINATION "${SOURCE_PATH}/third_party/kineto")
-
-vcpkg_from_github(
-    OUT_SOURCE_PATH src_cudnn
-    REPO NVIDIA/cudnn-frontend # new port ?
-    REF v1.12.0
-    SHA512 331ebbbd3439ab1b680d543d0550d63407148e9731c62e4d805eddb49bad5bc9ca7a38d9dd6ac4b976c70955155254fdee037a98f386f5e34c744eb3c2de095f
-    HEAD_REF main
-)
-file(COPY "${src_cudnn}/" DESTINATION "${SOURCE_PATH}/third_party/cudnn_frontend")
-
 
 file(REMOVE
   "${SOURCE_PATH}/cmake/Modules/FindBLAS.cmake"
@@ -63,33 +57,26 @@ message(STATUS "Using Python3: ${PYTHON3}")
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
     dist    USE_DISTRIBUTED # MPI, Gloo, TensorPipe
-    zstd    USE_ZSTD
     fbgemm  USE_FBGEMM
-    opencv  USE_OPENCV
-    # These are alternatives !
-    # tbb     USE_TBB
-    # tbb     AT_PARALLEL_NATIVE_TBB # AT_PARALLEL_ are alternatives
-    # openmp  USE_OPENMP
-    # openmp  AT_PARALLEL_OPENMP # AT_PARALLEL_ are alternatives
-    leveldb USE_LEVELDB
+    openmp  USE_OPENMP
+    openmp  AT_PARALLEL_OPENMP # AT_PARALLEL_ are alternatives
     opencl  USE_OPENCL
+    mkldnn  USE_MKLDNN
+    mkldnn  AT_MKLDNN_ENABLED
     cuda    USE_CUDA
     cuda    USE_CUDNN
     cuda    USE_NCCL
-    cuda    USE_SYSTEM_NCCL
     cuda    USE_NVRTC
     cuda    AT_CUDA_ENABLED
     cuda    AT_CUDNN_ENABLED
     cuda    USE_MAGMA
     vulkan  USE_VULKAN
-    #vulkan  USE_VULKAN_SHADERC_RUNTIME
     vulkan  USE_VULKAN_RELAXED_PRECISION
     rocm    USE_ROCM  # This is an alternative to cuda not a feature! (Not in vcpkg.json!) -> disabled
     llvm    USE_LLVM
     mpi     USE_MPI
     nnpack  USE_NNPACK  # todo: check use of `DISABLE_NNPACK_AND_FAMILY`
     nnpack  AT_NNPACK_ENABLED
-    qnnpack USE_QNNPACK # todo: check use of `USE_PYTORCH_QNNPACK`
 #   No feature in vcpkg yet so disabled. -> Requires numpy build by vcpkg itself
     python  BUILD_PYTHON
     python  USE_NUMPY
@@ -99,16 +86,26 @@ if("dist" IN_LIST FEATURES)
     if(VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_OSX)
         list(APPEND FEATURE_OPTIONS -DUSE_TENSORPIPE=ON)
     endif()
-    if(VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_OSX)
+    if(VCPKG_TARGET_IS_OSX)
         list(APPEND FEATURE_OPTIONS -DUSE_LIBUV=ON)
     endif()
     list(APPEND FEATURE_OPTIONS -DUSE_GLOO=${VCPKG_TARGET_IS_LINUX})
 endif()
 
+if("vulkan" IN_LIST FEATURES) # Vulkan::glslc in FindVulkan.cmake
+    find_program(GLSLC NAMES glslc PATHS "${CURRENT_HOST_INSTALLED_DIR}/tools/shaderc" REQUIRED)
+    message(STATUS "Using glslc: ${GLSLC}")
+    list(APPEND FEATURE_OPTIONS "-DVulkan_GLSLC_EXECUTABLE:FILEPATH=${GLSLC}")
+endif()
+
+set(IS_MOBILE_BUILD OFF)
 if(VCPKG_TARGET_IS_ANDROID OR VCPKG_TARGET_IS_IOS)
-    list(APPEND FEATURE_OPTIONS -DINTERN_BUILD_MOBILE=ON)
-else()
-    list(APPEND FEATURE_OPTIONS -DINTERN_BUILD_MOBILE=OFF)
+    set(IS_MOBILE_BUILD ON)
+endif()
+
+set(TARGET_IS_APPLE OFF)
+if(VCPKG_TARGET_IS_IOS OR VCPKG_TARGET_IS_OSX)
+    set(TARGET_IS_APPLE ON)
 endif()
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" USE_STATIC_RUNTIME)
@@ -118,64 +115,37 @@ vcpkg_cmake_configure(
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DPYTHON_EXECUTABLE:FILEPATH=${PYTHON3}
-        #-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}
+        "-DPROTOBUF_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DCAFFE2_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DPython_EXECUTABLE:FILEPATH=${PYTHON3}" # cmake/Codegen.cmake
+        -DBUILD_CUSTOM_PROTOBUF=OFF # cmake/ProtoBuf.cmake
+        -DCOMMIT_SHA=${GIT_COMMIT} # cmake/Codegen.cmake
+        -DINTERN_BUILD_MOBILE=${IS_MOBILE_BUILD}
+        -DATEN_NO_TEST=ON
         -DCAFFE2_STATIC_LINK_CUDA=ON
         -DCAFFE2_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
-        -DBUILD_CUSTOM_PROTOBUF=OFF
-        -DUSE_LITE_PROTO=OFF
-        -DBUILD_TEST=OFF
-        -DATEN_NO_TEST=ON
-        -DUSE_SYSTEM_LIBS=ON
-        -DUSE_METAL=OFF
-        -DUSE_PYTORCH_METAL=OFF
-        -DUSE_PYTORCH_METAL_EXPORT=OFF
+        -DCAFFE2_CMAKE_BUILDING_WITH_MAIN_REPO=OFF
+        -DBUILD_JNI=${VCPKG_TARGET_IS_ANDROID}
         -DUSE_GFLAGS=ON
         -DUSE_GLOG=ON
-        -DUSE_LMDB=ON
         -DUSE_ITT=OFF
-        -DUSE_ROCKSDB=ON
         -DUSE_OBSERVERS=OFF
-        -DUSE_PYTORCH_QNNPACK=OFF
-        -DUSE_KINETO=OFF
         -DUSE_ROCM=OFF
-        -DUSE_NUMA=OFF
-        -DUSE_SYSTEM_ONNX=ON
-        -DUSE_SYSTEM_FP16=ON
-        -DUSE_SYSTEM_EIGEN_INSTALL=ON
-        -DUSE_SYSTEM_CPUINFO=ON
-        -DUSE_SYSTEM_PTHREADPOOL=ON
-        -DUSE_SYSTEM_PYBIND11=ON
-        -DUSE_SYSTEM_ZSTD=ON
-        -DUSE_SYSTEM_GLOO=ON
-        -DUSE_SYSTEM_NCCL=ON
-        -DUSE_SYSTEM_LIBS=ON
-        -DUSE_SYSTEM_FXDIV=ON
-        -DUSE_SYSTEM_SLEEF=ON
-        -DUSE_SYSTEM_XNNPACK=ON
-        -DBUILD_JNI=${VCPKG_TARGET_IS_ANDROID}
+        -DUSE_NUMA=${VCPKG_TARGET_IS_LINUX}
         -DUSE_NNAPI=${VCPKG_TARGET_IS_ANDROID}
-        ${BLAS_OPTIONS}
-        # BLAS=MKL not supported in this port
         -DUSE_MKLDNN=OFF
         -DUSE_MKLDNN_CBLAS=OFF
-        #-DCAFFE2_USE_MKL=ON
-        #-DAT_MKL_ENABLED=ON
-        -DAT_MKLDNN_ENABLED=OFF
-        -DUSE_OPENCL=ON
-        -DUSE_NUMPY=ON
-        -DUSE_KINETO=OFF #
-    OPTIONS_RELEASE
-      -DPYTHON_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/python311.lib
+        -DUSE_KINETO=OFF
+        -DUSE_COREML_DELEGATE=${VCPKG_TARGET_IS_IOS}
+        -DUSE_PYTORCH_METAL=${TARGET_IS_APPLE}
+        -DUSE_PYTORCH_METAL_EXPORT=${VCPKG_TARGET_IS_OSX}
+        -DUSE_PYTORCH_QNNPACK=OFF
+        -DUSE_SYSTEM_LIBS=ON
     OPTIONS_DEBUG
-      -DPYTHON_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/python311_d.lib
+        -DPRINT_CMAKE_DEBUG_INFO=ON
     MAYBE_UNUSED_VARIABLES
         USE_NUMA
-        USE_SYSTEM_BIND11
         MKLDNN_CPU_RUNTIME
-        PYTHON_LIBRARY
 )
 
 vcpkg_cmake_install()
@@ -183,13 +153,6 @@ vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME caffe2 CONFIG_PATH "share/cmake/Caffe2" DO_NOT_DELETE_PARENT_CONFIG_PATH)
 vcpkg_cmake_config_fixup(PACKAGE_NAME torch CONFIG_PATH "share/cmake/Torch")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/torch/TorchConfig.cmake" "/../../../" "/../../")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/caffe2/Caffe2Config.cmake" "/../../../" "/../../")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/caffe2/Caffe2Config.cmake"
-  "set(Caffe2_MAIN_LIBS torch_library)"
-  "set(Caffe2_MAIN_LIBS torch_library)\nfind_dependency(Eigen3)")
-
-
 
 # Traverse the folder and remove "some" empty folders
 function(cleanup_once folder)
@@ -232,22 +195,4 @@ file(REMOVE_RECURSE
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-# Cannot build python bindings yet
-#if("python" IN_LIST FEATURES)
-#  set(ENV{USE_SYSTEM_LIBS} 1)
-#  vcpkg_replace_string("${SOURCE_PATH}/setup.py" "@TARGET_TRIPLET@" "${TARGET_TRIPLET}-rel")
-#  vcpkg_replace_string("${SOURCE_PATH}/tools/setup_helpers/env.py" "@TARGET_TRIPLET@" "${TARGET_TRIPLET}-rel")
-#  vcpkg_replace_string("${SOURCE_PATH}/torch/utils/cpp_extension.py" "@TARGET_TRIPLET@" "${TARGET_TRIPLET}-rel")
-#  vcpkg_python_build_and_install_wheel(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -x)
-#endif()
-
 set(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled) # torch_global_deps.dll is empty.c and just for linking deps
-
-set(config "${CURRENT_PACKAGES_DIR}/share/torch/TorchConfig.cmake")
-file(READ "${config}" contents)
-string(REGEX REPLACE "set\\\(NVTOOLEXT_HOME[^)]+" "set(NVTOOLEXT_HOME \"\$ENV{CUDA_PATH}\"" contents "${contents}")
-#string(REGEX REPLACE "set\\\(NVTOOLEXT_HOME[^)]+" "set(NVTOOLEXT_HOME \"\${CMAKE_CURRENT_LIST_DIR}/../../tools/cuda/\"" contents "${contents}")
-string(REGEX REPLACE "\\\${NVTOOLEXT_HOME}/lib/x64/nvToolsExt64_1.lib" "" contents "${contents}")
-file(WRITE "${config}" "${contents}")
-
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/torch/csrc/autograd/custom_function.h" "struct TORCH_API Function" "struct Function")
