c61fab9f26df824ddef8e4fe5e1accae22ea2276
Nick Logozzo <nlogozzo225@gmail.com>
2025-07-26 18:45:50 -0400

    fix: Config file

--- ports/maddy/portfile.cmake
+++ ports/maddy/portfile.cmake
@@ -13,7 +13,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/maddy)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
