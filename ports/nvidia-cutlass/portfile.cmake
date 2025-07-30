82342be0e4721aaf474c724df4777207a165111d
Steve Brain <sbrain@vixvizion.com>
2025-07-30 17:04:08 +1000

    Merge branch 'nvidia-cutlass'

--- ports/nvidia-cutlass/portfile.cmake
+++ ports/nvidia-cutlass/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NVIDIA/cutlass
     REF "v${VERSION}"
-    SHA512 c950ab718e67ffc972911b81890eae767a27d32dfc13f72b91e21e7c6b98eadfb3a5eebb9683091e61aed61709481451cfcd95d660e723686bf79a155e9f0b17
+    SHA512 a08aac281fb3bdea82c0a044dc643c40e4803d02e55bbea450021cb7a5472aed86e79c5df41cd981976af8403f18cc48d8069045c4e68339430d3a3caeb109ac
     HEAD_REF main
 )
 
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
         "-DCUTLASS_REVISION:STRING=v${VERSION}"
         -DCUTLASS_NATIVE_CUDA=OFF
         -DCUTLASS_ENABLE_HEADERS_ONLY=ON
-        -DCUTLASS_ENABLE_TOOLS=OFF
+        -DCUTLASS_ENABLE_TOOLS=ON
         -DCUTLASS_ENABLE_LIBRARY=OFF
         -DCUTLASS_ENABLE_PROFILER=OFF
         -DCUTLASS_ENABLE_PERFORMANCE=OFF
@@ -26,14 +26,13 @@ vcpkg_cmake_configure(
         -DCUTLASS_ENABLE_CUBLAS=ON
         -DCUTLASS_ENABLE_CUDNN=ON
         "-DPython3_EXECUTABLE:FILEPATH=${PYTHON3}"
+    MAYBE_UNUSED_VARIABLES
+        CUTLASS_NATIVE_CUDA
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/NvidiaCutlass" PACKAGE_NAME "NvidiaCutlass")
 
-# note CUTLASS_ENABLE_LIBRARY=OFF
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/NvidiaCutlass/NvidiaCutlassConfig.cmake"
-    "add_library" "# add_library" # comment out the command
-)
+
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug"
