c7f17c4e677a8a89108bdd75af637194d51b8eff
miyanyan <1138989048@qq.com>
2025-07-30 16:16:35 +0800

    update commit id

--- ports/yalantinglibs/portfile.cmake
+++ ports/yalantinglibs/portfile.cmake
@@ -3,7 +3,7 @@ set(VCPKG_BUILD_TYPE release)  # header-only
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO alibaba/yalantinglibs
-    REF b569ab7cda5744a037def20b0d13e99b62f059f7 # Use commit id to avoid target to multiple commits(0.5.0 worked but 0.5.3 failed), see https://github.com/alibaba/yalantinglibs/issues/1027
+    REF d4337e3297fa9bb8ef67473d7f40e766da39f4e3 # Use commit id to avoid target to multiple commits(the author will fix it in the next release), see https://github.com/alibaba/yalantinglibs/issues/1027
     SHA512 06a61247102e0f90b76987b7a5e244dd5e727ee2c67aedac472bf8984b4e9a91c8634ec3c210ad3f70266207870df0f83b2bc6a1e23ad5febb16d095f322173b
     HEAD_REF main
 )
