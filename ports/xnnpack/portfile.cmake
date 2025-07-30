c01f539e825ddad2426fad36e18d2ec624be5b97
Steve <steve@imagus.au>
2025-07-30 10:50:15 +1000

    added python to xnnpack build requirements

--- ports/xnnpack/portfile.cmake
+++ ports/xnnpack/portfile.cmake
@@ -12,10 +12,11 @@ vcpkg_from_github(
         fix-build.patch
 )
 
-
+vcpkg_find_acquire_program(PYTHON3)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        "-DPython_EXECUTABLE=${PYTHON3}"
         -DXNNPACK_USE_SYSTEM_LIBS=ON
         -DXNNPACK_ENABLE_ASSEMBLY=ON
         -DXNNPACK_ENABLE_MEMOPT=ON
