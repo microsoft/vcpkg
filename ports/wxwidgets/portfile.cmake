7e2617de0f67061d5af2bb3e5fc7682c0d17b7ca
RippeR37 <ripper37@gmail.com>
2025-07-31 21:07:10 +0200

    Fixed include path on windows

--- ports/wxwidgets/portfile.cmake
+++ ports/wxwidgets/portfile.cmake
@@ -209,11 +209,7 @@ if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/lib/mswud/wx/setup.h")
 endif()
 
 if(NOT "debug-support" IN_LIST FEATURES)
-    if(VCPKG_TARGET_IS_WINDOWS)
-        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/wx/debug.h" "#define wxDEBUG_LEVEL 1" "#define wxDEBUG_LEVEL 0")
-    else()
-        vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/wx-3.3/wx/debug.h" "#define wxDEBUG_LEVEL 1" "#define wxDEBUG_LEVEL 0")
-    endif()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/wx-3.3/wx/debug.h" "#define wxDEBUG_LEVEL 1" "#define wxDEBUG_LEVEL 0")
 endif()
 
 if("example" IN_LIST FEATURES)
