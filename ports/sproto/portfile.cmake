ece8436739d24bff17edf90ed0977117d70110b6
cuihairu <cuihairu@gmail.com>
2025-07-31 17:50:40 +0800

    Add sproto port with comprehensive fixes and improvements
    - Fix post-build validation issues with appropriate policies
    - Use unofficial- namespace for CMake targets per maintainer guidelines
    - Provide separate CMakeLists.txt file for better maintainability
    - Add sproto-config.cmake.in template file
    - Use vcpkg_install_copyright for standard copyright handling
    - Add comprehensive usage file with CMake and C API examples
    - Update version database

--- ports/sproto/portfile.cmake
+++ ports/sproto/portfile.cmake
@@ -11,59 +11,9 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Since sproto uses Makefile, we need to create a CMakeLists.txt
-file(WRITE "${SOURCE_PATH}/CMakeLists.txt" "
-cmake_minimum_required(VERSION 3.14)
-project(sproto C)
-
-set(CMAKE_C_STANDARD 99)
-
-# Add the main library
-add_library(sproto sproto.c)
-target_include_directories(sproto PUBLIC
-    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
-    $<INSTALL_INTERFACE:include>
-)
-
-# Install library and headers
-install(TARGETS sproto 
-    EXPORT sproto-targets
-    LIBRARY DESTINATION lib
-    ARCHIVE DESTINATION lib
-    RUNTIME DESTINATION bin
-)
-
-install(FILES sproto.h DESTINATION include)
-
-# Install CMake config files
-install(EXPORT sproto-targets
-    FILE sproto-targets.cmake
-    NAMESPACE sproto::
-    DESTINATION share/sproto
-)
-
-# Create and install config file
-include(CMakePackageConfigHelpers)
-configure_package_config_file(
-    \"\${CMAKE_CURRENT_LIST_DIR}/sproto-config.cmake.in\"
-    \"\${CMAKE_CURRENT_BINARY_DIR}/sproto-config.cmake\"
-    INSTALL_DESTINATION share/sproto
-)
-
-install(FILES
-    \"\${CMAKE_CURRENT_BINARY_DIR}/sproto-config.cmake\"
-    DESTINATION share/sproto
-)
-")
-
-# Create config template
-file(WRITE "${SOURCE_PATH}/sproto-config.cmake.in" "
-@PACKAGE_INIT@
-
-include(\"\${CMAKE_CURRENT_LIST_DIR}/sproto-targets.cmake\")
-
-check_required_components(sproto)
-")
+# Copy our CMakeLists.txt and config template to source
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/sproto-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -80,6 +30,9 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib" "${CURRENT_PACKAGES_DIR}/lib")
 
 # Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Install usage file
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_cmake_config_fixup(CONFIG_PATH share/sproto)
