f8deb8591a78f900ec3b1f444a48f1172b67ef96
Billy Robert O'Neal III <bion@microsoft.com>
2025-07-30 17:01:34 -0700

    Fix SHA, version DB, and filename.

--- ports/tiny-crc32c/portfile.cmake
+++ ports/tiny-crc32c/portfile.cmake
@@ -2,16 +2,17 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AndyTechnologies/tiny-crc32c
 	REF v1.0.1
-    SHA512 35a818ba7b66c57e489a1aef885d2f96336a09f40c65e742d9d91ef6e195913632dec9624b40a3a86f14486fdcefd90ff0c12e1c9953f346032100a5f7c172a6
+    SHA512 2f2033459b75d7fca35e849131098da127e75074aafed838dccf9f838cdb378dde0992e42a9cc52b3e8636b1d9b726f80a44e65a215f2e09b32e012d12b032ac
     HEAD_REF main
 )
 
 file(INSTALL
-    "${SOURCE_PATH}/include/tiny_crc32c.hpp"
+    "${SOURCE_PATH}/include/tiny_crc32.hpp"
     DESTINATION "${CURRENT_PACKAGES_DIR}/include"
 )
 
 file(INSTALL
     "${SOURCE_PATH}/LICENSE"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-    RENAME copyright)
+    RENAME copyright
+)
