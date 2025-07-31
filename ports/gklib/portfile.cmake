4e28be771a092e763fe63d194c2ec31b45c1a288
Kai Pastor <dg0yt@darc.de>
2025-08-01 01:04:41 +0200

    [gklib] Update, revise

--- ports/gklib/portfile.cmake
+++ ports/gklib/portfile.cmake
@@ -8,7 +8,6 @@ vcpkg_from_github(
     REF 6e7951358fd896e2abed7887196b6871aac9f2f8
     SHA512 54ba87f2c47e025ada0fe6fe608d9d144df5cd13e97e71892dbba4d50cd96409add309937a540cdf8bd2632cbfbc0e22e080a32d114ba6037008c8676aa8d88d
     PATCHES
-        android.patch
         build-fixes.patch
 )
 
@@ -17,13 +16,13 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SHARED)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DCMAKE_INSTALL_INCLUDEDIR=include/GKlib
         -DGKLIB_BUILD_APPS=OFF
-        -DHINSTALL_PATH=GKlib
         -DSHARED=${SHARED}
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/GKlib")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
