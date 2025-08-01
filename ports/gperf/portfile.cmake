02be40379c659bd791227dd11a1d71352ebc439d
Kai Pastor <dg0yt@darc.de>
2025-08-01 22:33:53 +0200

    Fixup

--- ports/gperf/portfile.cmake
+++ ports/gperf/portfile.cmake
@@ -1,8 +1,11 @@
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+set(VCPKG_BUILD_TYPE release) # tool only
 
 vcpkg_download_distfile(ARCHIVE
-    URLS http://ftp.gnu.org/pub/gnu/gperf/gperf-3.3.tar.gz
-    FILENAME gperf-3.3.tar.gz
+    URLS
+        "https://ftpmirror.gnu.org/gnu/gperf/gperf-${VERSION}.tar.gz"
+        "https://ftp.gnu.org/pub/gnu/gperf/gperf-${VERSION}.tar.gz"
+    FILENAME gperf-${VERSION}.tar.gz
     SHA512 246b75b8ce7d77d6a8725cd15f1cf2e68da404812573af1d5bf32dbe6ad4228f48757baefc77bcb1f5597c2397043c04d31d8a04ab507bfa7a80f85e1ab6045f
 )
 
@@ -14,12 +17,10 @@ vcpkg_extract_source_archive(
 vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTORECONF
+    OPTIONS_RELEASE
+        "--bindir=\\\${prefix}/tools/${PORT}"  # legacy from vendored CMake build
 )
 
 vcpkg_make_install()
 
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
