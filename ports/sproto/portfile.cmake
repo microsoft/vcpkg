ed4c5b32fcfdb4d17d3aff16793170fe2702c591
cuihairu <cuihairu@gmail.com>
2025-08-04 12:38:33 +0800

    Address PR review comments for sproto port
    - Change CMake namespace from unofficial-sproto:: to unofficial::sproto::
    - Update package name and config path to use unofficial-sproto
    - Switch to PRIVATE linkage for lua dependency
    - Use unofficial-lua CONFIG instead of Lua REQUIRED
    - Remove backward compatibility alias from config
    - Update usage instructions to use unofficial-sproto
    - Add Windows symbol export support with SPROTO_STATIC macro
    - Fix config file naming to unofficial-sproto-config.cmake

--- ports/sproto/portfile.cmake
+++ ports/sproto/portfile.cmake
@@ -5,9 +5,9 @@ vcpkg_from_github(
     REF 63df1ad8be4a7b295d389afaca7019e86f70d39c
     SHA512 5613a04e6197b6fa00828f457aeee0270a7f4d300df609d62e405123f3623516c5761bd2c6b0b8e21be12aa30ca3288ae6307121bf8461535ad8c3efe9a750a2
     HEAD_REF master
+    PATCHES add-symbol-exports.patch
 )
 
-# Copy our CMakeLists.txt and config template to source
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/sproto-config.cmake.in" DESTINATION "${SOURCE_PATH}")
 
@@ -19,13 +19,10 @@ vcpkg_cmake_build()
 
 vcpkg_cmake_install()
 
-# Remove debug includes
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-# Install usage file
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-vcpkg_cmake_config_fixup(CONFIG_PATH share/sproto)
+vcpkg_cmake_config_fixup(CONFIG_PATH "share/unofficial-sproto" PACKAGE_NAME "unofficial-sproto")
