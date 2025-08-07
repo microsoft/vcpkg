a75728b73ccf6f67fc7e3bc6a4dc28320e7a9c07
Tolker-KU <rasmus@tolker.dk>
2025-08-07 17:11:19 +0200

    [arcus] update to 5.3.0

--- ports/arcus/portfile.cmake
+++ ports/arcus/portfile.cmake
@@ -2,11 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ultimaker/libArcus
     REF ${VERSION}
-    SHA512 452c541360d74a8f58ab1b20df59efd36756812a9ecd09804ba16877956fb240d367bd968271a9c010496598ef0b459f62aa287553d4ba3fdb4cd2742c25553f
+    SHA512 8106bbcd595921d56e39bf694fbee43c6146a9c661edf9fb1fe271bbcf199a202e399cfbda5b83711c9daad1c55d8242ba23ce4fb52c416ddd862fb6de2bcab3
     HEAD_REF main
-    PATCHES
-        0001-fix-protobuf-deprecated.patch
-        0002-protobuf-version.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ENABLE_STATIC)
