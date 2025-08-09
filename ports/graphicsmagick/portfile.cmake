15daecf5ce19476007038aa2bd76b0241c4e4f1e
Kai Pastor <dg0yt@darc.de>
2025-08-09 06:08:37 +0200

    Remove cruft

--- ports/graphicsmagick/portfile.cmake
+++ ports/graphicsmagick/portfile.cmake
@@ -7,25 +7,19 @@ vcpkg_from_gitlab(
     REF ${graphicsmagick_version}
     SHA512 e64842dbbe2026e7d75b4004f615f32b4e2d57ce8dbd9bc90f87ee6e180d7e2feb61da6c25d404c43ac8d7661f94f7be3bd2882928dbd0e276b5c9040690f6f4
     PATCHES
-        # GM always requires a dynamic BZIP2. This patch makes this dependent if _DLL is defined
-        dynamic_bzip2.patch
-
-        # Bake GM's own modules into the .dll itself.  This fixes a bug whereby
-        # 'vcpkg install graphicsmagick' did not lead to a copy of GM that could
-        # load either PNG or JPEG files (due to missing GM Modules, with names
-        # matching "IM_*.DLL").
-        disable_graphicsmagick_modules.patch
         fix-png.patch
-        dirnet.patch
 )
 
-configure_file ("${CMAKE_CURRENT_LIST_DIR}/magick_types.h" "${SOURCE_PATH}/magick/magick_types.h.in" COPYONLY)
+set(options "")
+if(VCPKG_TARGET_IS_WINDOWS)
+    set(options ac_cv_header_dirent_dirent_h=no)
+endif()
 
 vcpkg_make_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     AUTORECONF
     OPTIONS
-        VERBOSE=yes
+        ${options}
         --with-heif=no
         --with-gs=no
         --with-jbig=no
@@ -34,6 +28,7 @@ vcpkg_make_configure(
         --with-lcms2=no
         --with-libzip=no
         --with-lzma=no
+        --with-modules=no
         --with-mpeg2=no
         --with-trio=no
         --with-x=no
@@ -41,15 +36,12 @@ vcpkg_make_configure(
         --with-wmf=no
         --with-zstd=no
 )
-
 vcpkg_make_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE "${CURRENT_PACKAGES_DIR}/tools/${PORT}/debug/bin/gm${VCPKG_TARGET_EXECUTABLE_SUFFIX}")
 vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/${PORT}/bin")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 set(config_scripts
     "GraphicsMagick++-config"
@@ -67,5 +59,6 @@ foreach(filename IN LISTS config_scripts)
     endif()
 endforeach()
 
-# copy license
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Copyright.txt")
