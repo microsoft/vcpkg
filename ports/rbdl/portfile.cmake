29cdd6109e0f239fdbbfc1e60d12269d6e83bf1d
Alexander Vieth <a.vieth@tudelft.nl>
2025-08-12 12:43:14 +0200

    [rbdl] fix build after update Eigen3

--- ports/rbdl/portfile.cmake
+++ ports/rbdl/portfile.cmake
@@ -38,4 +38,4 @@ vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/RBDL PACKAGE_NAME RBDL)
 vcpkg_fixup_pkgconfig()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
