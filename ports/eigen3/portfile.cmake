0af949d5b3edec023337b3615997cd49bc8081d4
Alexander Vieth <a.vieth@tudelft.nl>
2025-08-12 11:12:25 +0200

    remove debug folders

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -34,6 +34,8 @@ vcpkg_cmake_config_fixup()
 
 vcpkg_fixup_pkgconfig()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
 vcpkg_install_copyright(
     FILE_LIST
         "${SOURCE_PATH}/COPYING.README"
