873b475386c390c17fe528eafdb77d26733e1f98
night1rider <backman23@live.com>
2025-08-13 16:26:30 -0600

    wolfTPM: Adding patch to CMakelists to fix default logic from wolfTPM PR 430

--- ports/wolftpm/portfile.cmake
+++ ports/wolftpm/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 e6b73ec95f6c0cd37a71063c13db2a95175c54d2c63f69959ed68b4992277f1304136e1ecc5419ce4ff070d9162dbb30a9c6f78c7238383d72c686f1cdc1ab7c
     HEAD_REF master
+    PATCHES
+      "wolftpm_pr430.diff"
     )
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
