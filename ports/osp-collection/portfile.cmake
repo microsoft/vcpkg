19543abe556b9dce131ff32f4f394c3f7fa8cc7e
Victor Romero <romerosanchezv@gmail.com>
2025-08-14 13:25:05 -0700

    Fix package name in vcpkg_cmake_config_fixup

--- ports/osp-collection/portfile.cmake
+++ ports/osp-collection/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup() # removes /debug/share
+vcpkg_cmake_config_fixup(PACKAGE_NAME "collection" CONFIG_PATH "lib/cmake/collection")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include") # removes debug/include
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}") # usage
