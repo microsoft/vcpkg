2fb875f4b5c3e59911e7f081604f0ab434f387fa
Saad <saadmuizz@gmail.com>
2025-08-16 06:19:34 -0400

    update

--- ports/osp-collection/portfile.cmake
+++ ports/osp-collection/portfile.cmake
@@ -4,13 +4,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-source-patterns/collection
     REF "${VERSION}"
-    SHA512 cfa5f79d2935704a10a95b0f3ebacf73e500425ea38f55e0c93f3c59a8b7c336164f899b9462aa60c6c878589992efa5a3efb0d414cc0f357d7dc269cccb215f
+    SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(PACKAGE_NAME "collection" CONFIG_PATH "lib/cmake/collection")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include") # removes debug/include
