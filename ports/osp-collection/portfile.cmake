2ab1b8e685212671f7c15679c8437e858f8158f5
Saad <saadmuizz@gmail.com>
2025-08-16 14:21:26 -0400

    update

--- ports/osp-collection/portfile.cmake
+++ ports/osp-collection/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO open-source-patterns/collection
     REF "${VERSION}"
-    SHA512 f3781cbb4e9e190df38c3fe7fa80ba69bf6f9dbafb158e0426dd4604f2f1ba794450679005a38d0f9f1dad0696e2f22b8b086b2d7d08a0f99bb4fd3b0f7ed5d8
+    SHA512 9e1f6b61241c8395d88210d8ea40bbbeb88f299784ab6449993e87fdd1427dc757125ffdec3201b74a1617e15320da52929cb02f9592dbc0525e99d740d88082
     HEAD_REF main
 )
 
@@ -12,6 +12,6 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "collection" CONFIG_PATH "lib/cmake/collection")
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include") # removes debug/include
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}") # usage
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE") # LICENSE
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
