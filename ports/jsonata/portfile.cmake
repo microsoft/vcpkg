acbb8239781d005fb3188af3511d6df8742ce44b
Robert Yokota <rayokota@gmail.com>
2025-08-20 23:31:07 -0700

    Add jsonata to vcpkg

---
+++ ports/jsonata/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO rayokota/jsonata-cpp
+    REF "v${VERSION}"
+    SHA512 d2e64e1cce5d6db93f15913b6b8ccdc199c1be2e02247715d048dd1ae3322629db88f2361e73e705491606437829c86ae1c0256b274631de52f8b5081bddc429
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "jsonata" CONFIG_PATH "lib/cmake/jsonata")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
