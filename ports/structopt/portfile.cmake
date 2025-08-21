2a1c2ad6e0e0e9c0810fc612899ef80c148bc569
Ethan J. Musser <emusser@carnegierobotics.com>
2025-08-21 15:40:28 -0400

    Add missing licenses and remove duplicates

--- ports/structopt/portfile.cmake
+++ ports/structopt/portfile.cmake
@@ -16,9 +16,22 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 # Header-only library
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/structopt)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/structopt")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+vcpkg_install_copyright(
+    FILE_LIST
+        "${SOURCE_PATH}/LICENSE"
+        "${SOURCE_PATH}/LICENSE.magic_enum"
+        "${SOURCE_PATH}/LICENSE.visit_struct"
+)
+
+# Remove redundant license files that are installed by the library
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/share/licenses)
+
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/usage"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage"
+    COPYONLY
+)
