f66bd56b40ee3dcddf8c101bde867ecb6036f99d
Cadons <matteo.cadoni01@gmail.com>
2025-08-23 13:18:23 +0200

    Rename `tusclient` to `cadons-ctus` and update port files accordingly.

--- ports/cadons-ctus/portfile.cmake
+++ ports/cadons-ctus/portfile.cmake
@@ -2,15 +2,9 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Cadons/libtusclient
         REF 1.0.0
-        SHA512 4e164aa3a9d6de33e3a91084ece689820c5d448a02be242a22e5b243fc30fba3adaf1508a8adb6d8a7c7c5278b7ac43ae003a8260f8670fbe919a7f5b2a138eb
+        SHA512 bdca3c3c8baba50f55fdc9422d349e7d20b18ca79eefc5d0681de6a9145f2901a070a0b2313e1c80052ec85170174a11338fb5e89fba71b422521c9f2c5155be
         HEAD_REF main
 )
-
-if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    set(BUILD_SHARED_LIBS ON)
-else ()
-    set(BUILD_SHARED_LIBS OFF)
-endif ()
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
@@ -23,7 +17,7 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME tusclient
+vcpkg_cmake_config_fixup(PACKAGE_NAME cadons-ctus
         CONFIG_PATH lib/cmake/tusclient)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
