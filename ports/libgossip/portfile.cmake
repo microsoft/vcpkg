de7fe8926dcd55a8118e3da0e9205b8fd52702ed
caomengxuan666 <2507560089@qq.com>
2025-08-25 23:06:42 +0800

    [libgossip] update version database to 1.0.0.1

--- ports/libgossip/portfile.cmake
+++ ports/libgossip/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO caomengxuan666/libgossip
     REF "v${VERSION}"
-    SHA512 09e44975ff1f3d23ce9987ac0ef66e96d69d4f81d23da855e7d45f25d33beae5a0c0ea58696c5359eea9766d80849741db3d04b4cf2bc40d2a0b423975472d57
+    SHA512 a1b9d0e1f0b83a321748c3cd18a9e8e314f916b58ba54b88d3715a95aea88ac6dbf62b5923c26ccc76af9c5715b2f6ae8bf10e3e4f01fe8dbaed2ec019460f9c
     HEAD_REF main
 )
 vcpkg_cmake_configure(
