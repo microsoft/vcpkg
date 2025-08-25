85f1ebd28a6d0842c25be2fd7705ab27e17f5082
NurislamGubaydullin <nurislam.gubaydullin@incomsystem.ru>
2025-08-25 11:10:18 +0300

    add vcpkg_cmake_configure

--- ports/defer/portfile.cmake
+++ ports/defer/portfile.cmake
@@ -9,11 +9,17 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME defer CONFIG_PATH share/defer/cmake)
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
