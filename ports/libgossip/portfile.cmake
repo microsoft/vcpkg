a365a38d83541f2d7d65761a9d2ca06469b71c07
caomengxuan666 <2507560089@qq.com>
2025-08-25 21:57:37 +0800

    [libgossip] Add New port

---
+++ ports/libgossip/portfile.cmake
@@ -0,0 +1,16 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO caomengxuan666/libgossip
+    REF "v${VERSION}"
+    SHA512 09e44975ff1f3d23ce9987ac0ef66e96d69d4f81d23da855e7d45f25d33beae5a0c0ea58696c5359eea9766d80849741db3d04b4cf2bc40d2a0b423975472d57
+    HEAD_REF main
+)
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DBUILD_EXAMPLES=OFF
+)
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libgossip)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
