448b291737bde098c4291f62dd3788f60518c99a
Kai Pastor <dg0yt@darc.de>
2025-08-28 00:15:33 +0200

    WIP

--- ports/spz/portfile.cmake
+++ ports/spz/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 e1ee9314bd0a698e73db6d02937a20eea9419864d45eaa03e184cd9fca07dc08a92c2dfedf8e2415cbfc1ca3917435eb54ad0badd0f584aeeb2038b3bf7f000a
     HEAD_REF main
+    PATCHES
+        android-log.diff
 )
 
 vcpkg_cmake_configure(
