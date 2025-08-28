f35746996b7a1b7270d61634c7727f91fb0cd59c
Cadons <matteo.cadoni01@gmail.com>
2025-08-28 19:40:16 +0200

    Update `cadons-ctus` port to use dynamic versioning and adjust configuration paths

--- ports/cadons-ctus/portfile.cmake
+++ ports/cadons-ctus/portfile.cmake
@@ -1,22 +1,22 @@
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO Cadons/ctus
-        REF 1.0.0
-        SHA512 efc633effdbcf0616519ef72a83bd511cc3e9a6e208454292aefef0eea75a9953cebd78c9567b830d12b1faa3b78a767bfe9fc57e293748b9333ac2055f7d07d
-        HEAD_REF main
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Cadons/ctus
+    REF ${VERSION}
+    SHA512 28c930364e4bb207bd336139d570287a467c55fab9ef91bb546330ca5ed106986b4eaadb31e956dc5e4c2ded9a49917e0a5775e0ad157cddb0bf56a2afb3b150
+    HEAD_REF main
 )
 
 vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}"
-        OPTIONS
-        -DBUILD_TESTING=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DBUILD_TESTING=OFF
 )
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME cadons-ctus
-        CONFIG_PATH lib/cmake/ctus)
+vcpkg_cmake_config_fixup(PACKAGE_NAME ctus CONFIG_PATH lib/cmake/ctus)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
