3b3d8ebb7bb56b24a2e60522569dbe805c882fc2
Saikari <lin@sz.cn.eu.org>
2025-09-01 00:37:28 +0300

    fixup

--- scripts/test_ports/vcpkg-ci-orange-math/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-orange-math/project/main.cpp
@@ -1,4 +1,12 @@
-#include <omath/Vector2.hpp>
+#if defined(__has_include)
+    #if __has_include(<omath/omath.hpp>)
+		#include <omath/omath.hpp>
+	#else
+		#include <omath/Vector2.hpp>
+	#endif
+#else
+    #include <omath/Vector2.hpp>
+#endif
 
 int main()
 {
