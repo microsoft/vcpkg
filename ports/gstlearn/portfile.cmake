bfa39ae589baa09e30384af7f21d37ff090ac7a9
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-09-03 18:50:39 -0400

    1.8.0

--- ports/gstlearn/portfile.cmake
+++ ports/gstlearn/portfile.cmake
@@ -4,10 +4,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gstlearn/gstlearn
     REF "stable_${VERSION}"
-    SHA512 5e7850647a29a51138c66ac4815340b83598707751d01cc87e2eef70bc7ec8ff6e995b5f83fa6da9875c62150ea811467a7fd6c88944ef4e92c9a86eb5c80dcd
+    SHA512 51a3c0ffcff311931c03d224a08675ab91747c270f61851080a3331b1b4468f2de8cb2d262b91bd3781ed1b92153901cdfcaba47a011cb4ec3156f7662d109a7
     HEAD_REF dev
-    PATCHES
-        fix-missing-cmath.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -23,14 +21,6 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/lib/gstlearnd.dll")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/bin")
-file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/gstlearnd.dll" "${CURRENT_PACKAGES_DIR}/debug/bin/gstlearnd.dll")
-endif()
-if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/gstlearn.dll")
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin")
-file(RENAME "${CURRENT_PACKAGES_DIR}/lib/gstlearn.dll" "${CURRENT_PACKAGES_DIR}/bin/gstlearn.dll")
-endif()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
