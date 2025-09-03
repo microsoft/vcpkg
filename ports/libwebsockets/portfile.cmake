b03b2e2f081e865bcc8b8397657583c91fe2bc41
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2025-09-03 09:36:42 -0400

    update to 4.4.1

--- ports/libwebsockets/portfile.cmake
+++ ports/libwebsockets/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO warmcat/libwebsockets
     REF "v${VERSION}"
-    SHA512 629a4fe6b0c8f6b4a1d38128201f5a856bdcf8a33923120c24ceb8d7f7602906608ef4e289e98b66bb94633ed95f5901e2e46d8214dcc61cf276cc975b9d7134
+    SHA512 86f85066ce0e9f7f29af5b7c7be803bd19f1a7e6afd8bcdd7c3afc1a66735324317852d7b19bbe9a5af69e759ec532dd28bebffa256f3e1c30ade7ead41a4275
     HEAD_REF master
     PATCHES
         fix-dependency-libuv.patch
