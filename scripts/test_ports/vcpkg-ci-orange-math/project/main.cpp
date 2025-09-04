54ae549362512382c0bf0d360578158aec890803
Saikari <lin@sz.cn.eu.org>
2025-09-04 12:37:24 +0300

    Update main.cpp

--- scripts/test_ports/vcpkg-ci-orange-math/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-orange-math/project/main.cpp
@@ -1,15 +1,8 @@
-#if defined(__has_include)
-    #if __has_include(<omath/omath.hpp>)
-		#include <omath/omath.hpp>
-	#else
-		#include <omath/Vector2.hpp>
-	#endif
-#else
-    #include <omath/Vector2.hpp>
-#endif
+#include <omath/Vector2.hpp>
 
 int main()
 {
 	omath::Vector2 w = omath::Vector2(20.0, 30.0);
 	return 0;
 }
+
