1fb69855aae3ce5c0d203b57bd7d9ada18d6a8ed
Ethan J. Musser <emusser@carnegierobotics.com>
2025-09-04 12:52:45 -0400

    Disable CRT linkage check on Windows builds

--- ports/rerun-sdk/portfile.cmake
+++ ports/rerun-sdk/portfile.cmake
@@ -6,6 +6,14 @@ vcpkg_download_distfile(
     SHA512 2ddeb2447ac890c124fdfd3ff84719a03590366ea59bb14decdda525c70317fe1f7b1ee8fbbc45059d0c5e50eab20b023ec38d6168968ea342253793a7b7ab40
 )
 
+# Workaround: The distributed SDK contains a prebuilt rerun_c that is built in Release mode.  On Windows, this means
+# that it always links to the release MSVC C runtime (CRT) and causes vcpkg's post-build CRT linkage check to fail for
+# Debug builds.  As such, this post-build check is suppressed for Windows builds.
+if(VCPKG_TARGET_IS_WINDOWS)
+    # TODO: Remove this policy when rerun ships a Debug rerun_c.
+    set(VCPKG_POLICY_SKIP_CRT_LINKAGE_CHECK enabled)
+endif()
+
 vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
