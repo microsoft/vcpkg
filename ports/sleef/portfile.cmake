bfe9594efdb7005480b74f2f14298a1719aabb9f
Kai Pastor <dg0yt@darc.de>
2025-09-04 09:33:19 +0200

    [sleef] Add feature for DFT

--- ports/sleef/portfile.cmake
+++ ports/sleef/portfile.cmake
@@ -9,16 +9,21 @@ vcpkg_from_github(
         export-link-libs.diff
 )
 
-set(CROSSCOMP_OPTIONS "")
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        dft     SLEEF_BUILD_DFT
+        dft     SLEEF_ENFORCE_DFT
+)
+
 if(VCPKG_CROSSCOMPILING)
-    set(CROSSCOMP_OPTIONS "-DNATIVE_BUILD_DIR=${CURRENT_HOST_INSTALLED_DIR}/manual-tools/${PORT}")
+    set(options "-DNATIVE_BUILD_DIR=${CURRENT_HOST_INSTALLED_DIR}/manual-tools/${PORT}")
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        ${options}
         -DSLEEF_BUILD_LIBM=ON
-        -DSLEEF_BUILD_DFT=ON -DSLEEF_ENFORCE_DFT=ON
         -DSLEEF_BUILD_QUAD=ON
         -DSLEEF_BUILD_GNUABI_LIBS=${VCPKG_TARGET_IS_LINUX}
         -DSLEEF_BUILD_TESTS=OFF
@@ -26,7 +31,6 @@ vcpkg_cmake_configure(
         -DSLEEF_DISABLE_SVE=ON  # arm64 build issues, officially unmaintained
         -DSLEEF_ENABLE_TLFLOAT=OFF
         -DSLEEF_ENABLE_TESTER4=OFF
-        ${CROSSCOMP_OPTIONS}
 )
 
 vcpkg_cmake_install()
@@ -35,8 +39,12 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sleef)
 vcpkg_fixup_pkgconfig()
 
 if(NOT VCPKG_CROSSCOMPILING)
+    set(tools mkrename qmkrename mkalias mkdisp qmkdisp)
+    if("dft" IN_LIST FEATURES)
+        list(APPEND tools mkdispatch mkunroll)
+    endif()
     vcpkg_copy_tools(
-        TOOL_NAMES mkrename qmkrename mkalias mkdispatch mkdisp qmkdisp mkunroll 
+        TOOL_NAMES ${tools}
         SEARCH_DIR "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/bin"
         DESTINATION "${CURRENT_PACKAGES_DIR}/manual-tools/${PORT}/bin"
         AUTO_CLEAN)
