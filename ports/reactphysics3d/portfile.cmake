5ecc1ae6ff4a69dd6a06225c3f3f408d1782b1f3
Robin Wils <robin.wilz@gmail.com>
2025-09-06 19:32:12 +0200

    [reactphysics3d] add patch from 421 upstream PR

--- ports/reactphysics3d/portfile.cmake
+++ ports/reactphysics3d/portfile.cmake
@@ -1,3 +1,9 @@
+vcpkg_download_distfile(FIX_UPSTREAM_421
+    URLS https://github.com/DanielChappuis/reactphysics3d/pull/421.patch?full_index=1
+    SHA512 71ab7d5024fff100546d1cc934976f15e3ee3fe8df29ff62e1c743d3f0c5f6dad73def0b9d0a560fa423e610cb1388c88c3226d8e2b9f2b1afdf4535204541ff
+	FILENAME reactphysics3d-421.patch
+)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO DanielChappuis/reactphysics3d
@@ -5,7 +11,7 @@ vcpkg_from_github(
     SHA512 3ba9ec0e399d2dc46c126e4aa20718b9024f8097f36157e31b469f5135a726d3c0811e79335db970dfab7f258d1506dd4cefa46edca73f5940bf561dc9a5b11a
     HEAD_REF master
     PATCHES
-        https://github.com/DanielChappuis/reactphysics3d/pull/421.patch?full_index=1
+		"${FIX_UPSTREAM_421}"
 )
 
 vcpkg_cmake_configure(
