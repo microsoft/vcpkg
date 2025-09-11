4101ed56851c29a848070f9cd20f130cdd5452c1
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2025-09-11 14:42:11 -0700

    Patch Android build

--- ports/azure-core-cpp/portfile.cmake
+++ ports/azure-core-cpp/portfile.cmake
@@ -7,6 +7,8 @@ vcpkg_from_github(
     REF "azure-core_${VERSION}"
     SHA512 30b949e1154674b474287a4b36a29270d9fdd7a1f5b43d2507859f0a75176821a98c9a700665b513cc5e36a8089d48d614838ed0f900f42ef5c87dd92f2805b6
     HEAD_REF main
+    PATCHES
+        dt_android.patch
 )
 
 vcpkg_check_features(
