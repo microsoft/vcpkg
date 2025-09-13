9e1a9d42a560d5f4bc5b3d10fcef03059aa7f1cb
sonozaki-noriko <199164954+sonozaki-noriko@users.noreply.github.com>
2025-09-13 13:42:16 +0100

    [libbluray] Update to 1.2.0

--- ports/libbluray/portfile.cmake
+++ ports/libbluray/portfile.cmake
@@ -3,49 +3,40 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO videolan/libbluray
     REF ${VERSION}
-    SHA512 30ccbbfcd168c8e2ad55ed7043a3732f45069aa582f902b6e3dca753946f0fc565f7624ac00eb53e48c458b7b88b3f8e461fa2dcc006a147598fd8cc3c604be0
+    SHA512 e728f8d93b311d540a3883ba869fdc2c10e91b4009bf1796947b510d3646088dfd7aeabaebb3a1dcbf49d5afee48743bfa620fd93aa54bf948238510e7e7719f
     PATCHES
         msvc.diff
 )
 
-vcpkg_find_acquire_program(PKGCONFIG)
-set(ENV{PKG_CONFIG} "${PKGCONFIG}")
-
 if("freetype" IN_LIST FEATURES)
-    list(APPEND options --with-freetype)
+    list(APPEND options -Dfreetype=enabled)
 else()
-    list(APPEND options --without-freetype)
+    list(APPEND options -Dfreetype=disabled)
 endif()
 
 if("fontconfig" IN_LIST FEATURES)
-    list(APPEND options --with-fontconfig)
+    list(APPEND options -Dfontconfig=enabled)
 else()
-    list(APPEND options --without-fontconfig)
+    list(APPEND options -Dfontconfig=disabled)
 endif()
 
 if("libxml2" IN_LIST FEATURES)
-    list(APPEND options --with-libxml2)
+    list(APPEND options -Dlibxml2=enabled)
 else()
-    list(APPEND options --without-libxml2)
+    list(APPEND options -Dlibxml2=disabled)
 endif()
 
-vcpkg_configure_make(
+vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
-    AUTOCONFIG
     OPTIONS
-        --disable-examples
-        --disable-bdjava-jar
-        --disable-doxygen-doc
+        -Denable_tools=false
+        -Dbdj_jar=disabled
+        -Dauto_features=disabled
         ${options}
 )
 
-vcpkg_install_make()
+vcpkg_install_meson()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
-
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
