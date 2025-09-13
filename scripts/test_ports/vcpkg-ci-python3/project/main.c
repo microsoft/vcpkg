28cb9caabc34538465af28c76e202a4f4a76dce3
Adam Johnson <AdamJohnso@gmail.com>
2025-09-13 13:23:19 -0500

    Remove stdlib/isolation from test.

--- scripts/test_ports/vcpkg-ci-python3/project/main.c
+++ scripts/test_ports/vcpkg-ci-python3/project/main.c
@@ -3,20 +3,9 @@
 int main()
 {
     PyConfig config;
-#ifdef BATTERIES
-    PyConfig_InitIsolatedConfig(&config);
-#else
     PyConfig_InitPythonConfig(&config);
-#endif
-    config.optimization_level = 2;
     config.write_bytecode = 0;
-    config.user_site_directory = 0;
     PyConfig_SetString(&config, &config.program_name, L"test");
-
-#ifdef BATTERIES
-    PyWideStringList_Append(&config.module_search_paths, BATTERIES);
-    config.module_search_paths_set = 1;
-#endif
 
     PyStatus status = Py_InitializeFromConfig(&config);
     if (PyStatus_Exception(status)) {
