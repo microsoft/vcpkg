ebfde62b5d7afda05e60e4fc5572ff702215a8a1
Vincent Saulue-Laborde <34475300+vsaulue@users.noreply.github.com>
2025-09-15 15:01:12 +0200

    [gustave] Remove PACKAGE_NAME from portfile
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/gustave/portfile.cmake
+++ ports/gustave/portfile.cmake
@@ -20,10 +20,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(
-    PACKAGE_NAME "Gustave"
-    CONFIG_PATH "cmake"
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH "cmake")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SRC_PATH}/LICENSE.txt")
