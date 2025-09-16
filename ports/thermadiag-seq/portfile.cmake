495a91baeaa3783ec524f3f3fb580ee324a29134
toge <toge.mail@gmail.com>
2025-09-16 18:53:30 +0900

    rename to thermadiag-seq

--- ports/thermadiag-seq/portfile.cmake
+++ ports/thermadiag-seq/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 a784727e9b720e811ffd4431b194305fdabd5719abeb69df6d6a85e4f16f796ab702e1c4790a3509f70f52d00f38b1775daab33648b781b7771d165c328692f8
     HEAD_REF main
+    PATCHES
+        rename-package-name.patch
 )
 
 set(VCPKG_BUILD_TYPE release) # header-only port
@@ -16,8 +18,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/seq)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/thermadiag-seq)
 vcpkg_fixup_pkgconfig()
+file(RENAME "${CURRENT_PACKAGES_DIR}/share/pkgconfig/seq.pc" "${CURRENT_PACKAGES_DIR}/share/pkgconfig/${PORT}.pc")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
