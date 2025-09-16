a3dc243d590d9d507cc05537c2021c879d9da421
Kai Pastor <dg0yt@darc.de>
2025-09-16 08:36:39 +0200

    [libltdl] New port

---
+++ ports/libltdl/vcpkg-port-config.cmake
@@ -0,0 +1,48 @@
+# Provide variables to use lib ltldl with autoconf.
+#
+# - <out_var_basename>_LIBTOOLIZE
+#   A libtoolize (wrapper) which disables the check for  libltdl.la.
+#   la files are removed from packages in vcpkg (and in most distros).
+#   They add little value in modern environments, and they use absolute paths.
+# - <out_var_basename>_OPTIONS_RELEASE, <out_var_basename>_OPTIONS_DEBUG:
+#   Options to pass to vcpkg_make_configure.
+#
+# Usage:
+#   vcpkg_libltdl_get_vars(LIBLTDL)
+#   set(ENV{LIBTOOLIZE} "${LIBLTDL_LIBTOOLIZE}")
+#   
+#   vcpkg_make_configure(
+#       SOURCE_PATH "${SOURCE_PATH}"
+#       AUTORECONF
+#       OPTIONS
+#           --with-included-ltdl=no
+#       OPTIONS_RELEASE
+#           ${LIBLTDL_OPTIONS_RELEASE}
+#       OPTIONS_DEBUG
+#           ${LIBLTDL_OPTIONS_RELEASE}
+#   )
+
+function(vcpkg_libltdl_get_vars out_var_basename)
+    # Select host libtoolize: triplet, environment, PATH, plain name.
+    if(NOT VCPKG_LIBLTDL_LIBTOOLIZE)
+        set(VCPKG_LIBLTDL_LIBTOOLIZE "$ENV{LIBTOOLIZE}")
+    endif()
+    if(VCPKG_LIBLTDL_LIBTOOLIZE STREQUAL "")
+        find_program(VCPKG_LIBLTDL_LIBTOOLIZE NAMES libtoolize glibtoolize)
+    endif()
+    if(VCPKG_LIBLTDL_LIBTOOLIZE)
+        set(ENV{VCPKG_LIBLTDL_LIBTOOLIZE} "${VCPKG_LIBLTDL_LIBTOOLIZE}")
+    endif()
+
+    vcpkg_list(SET options_release
+        "--with-ltdl-include=${CURRENT_INSTALLED_DIR}/include"
+        "--with-ltdl-lib=${CURRENT_INSTALLED_DIR}/lib"
+    )
+    vcpkg_list(SET options_debug
+        "--with-ltdl-include=${CURRENT_INSTALLED_DIR}/include"
+        "--with-ltdl-lib=${CURRENT_INSTALLED_DIR}/debug/lib"
+    )
+    set("${out_var}_OPTIONS_RELEASE" "${options_release}" PARENT_SCOPE)
+    set("${out_var}_OPTIONS_DEBUG" "${options_debug}" PARENT_SCOPE)
+    set("${out_var}_LIBTOOLIZE" "${CURRENT_INSTALLED_DIR}/manual-tools/libltdl/libtoolize-ltdl-no-la" PARENT_SCOPE)
+endfunction()
