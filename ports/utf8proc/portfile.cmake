63e56fbfcc5e09e3207edfd65dfd8f3a586bee38
Kai Pastor <dg0yt@darc.de>
2025-09-18 06:13:55 +0200

    Add unofficial-utf8proc polyfill

--- ports/utf8proc/portfile.cmake
+++ ports/utf8proc/portfile.cmake
@@ -30,4 +30,7 @@ file(WRITE "${CURRENT_PACKAGES_DIR}/include/utf8proc.h" "${UTF8PROC_H}")
 
 vcpkg_fixup_pkgconfig()
 
+# Legacy
+file(INSTALL "${CURRENT_PORT_DIR}/unofficial-utf8proc-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-utf8proc")
+
 file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
