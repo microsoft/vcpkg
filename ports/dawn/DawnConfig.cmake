457e81a0a9b47dbe39a06faf6579b5ba897aa593
xiaozhuai <798047000@qq.com>
2025-09-19 16:57:15 +0800

    Fix emscripten config file

--- ports/dawn/DawnConfig.cmake
+++ ports/dawn/DawnConfig.cmake
@@ -1,8 +1,7 @@
-if (NOT TARGET webgpu_dawn)
-    add_library(webgpu_dawn INTERFACE IMPORTED)
-    set_target_properties(webgpu_dawn PROPERTIES
+if (NOT TARGET dawn::webgpu_dawn)
+    add_library(dawn::webgpu_dawn INTERFACE IMPORTED)
+    set_target_properties(dawn::webgpu_dawn PROPERTIES
         INTERFACE_COMPILE_OPTIONS "--use-port=${CMAKE_CURRENT_LIST_DIR}/emdawnwebgpu.port.py"
         INTERFACE_LINK_OPTIONS "--use-port=${CMAKE_CURRENT_LIST_DIR}/emdawnwebgpu.port.py"
     )
-    add_library(dawn::webgpu_dawn ALIAS webgpu_dawn)
 endif()
