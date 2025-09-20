5330e7f9a037687fd527179f9a69e83cdea76e1b
Kai Pastor <dg0yt@darc.de>
2025-09-20 08:39:22 +0200

    Trim

--- ports/libltdl/vcpkg-port-config.cmake
+++ ports/libltdl/vcpkg-port-config.cmake
@@ -22,15 +22,9 @@
 #   )
 
 function(vcpkg_libltdl_get_vars prefix)
-    # Select host libtoolize: triplet, environment, PATH, plain name.
-    if(NOT VCPKG_LIBLTDL_LIBTOOLIZE)
-        set(VCPKG_LIBLTDL_LIBTOOLIZE "$ENV{LIBTOOLIZE}")
-    endif()
-    if(VCPKG_LIBLTDL_LIBTOOLIZE STREQUAL "")
-        find_program(VCPKG_LIBLTDL_LIBTOOLIZE NAMES libtoolize glibtoolize)
-    endif()
-    if(VCPKG_LIBLTDL_LIBTOOLIZE)
-        set(ENV{VCPKG_LIBLTDL_LIBTOOLIZE} "${VCPKG_LIBLTDL_LIBTOOLIZE}")
+    # Forward libtoolize from environment to libtoolize-ltdl-no-la.
+    if(NOT "$ENV{LIBTOOLIZE}" STREQUAL "")
+        set(ENV{VCPKG_LIBLTDL_LIBTOOLIZE} "$ENV{LIBTOOLIZE}")
     endif()
 
     vcpkg_list(SET options_release
