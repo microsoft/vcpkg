427895fdd1051939da4d13c06cd8094b47577192
luncliff <luncliff@gmail.com>
2025-09-21 20:04:27 +0900

    [libtorch] merge patch files

--- ports/libtorch/portfile.cmake
+++ ports/libtorch/portfile.cmake
@@ -1,38 +1,43 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
+set(GIT_COMMIT e2d141dbde55c2a4370fac5165b0561b6af4798b) # cmake/Codegen.cmake
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pytorch/pytorch
     REF "v${VERSION}"
     SHA512 a9fc2252af9031c2cd46dde558c491aea8bc322fb80157a7760f300a44b759d4bfe866f030fbb974b80493057cfff4dd512498f99a100ed6d05bf620258ed37e
-    HEAD_REF master
+    HEAD_REF main
     PATCHES
         fix-cmake.patch
         fix-osx.patch
-        fix-vulkan.patch
+        fix-vulkan.patch # use vulkan-memory-allocator from vcpkg
         fix-glog.patch
         fix-pytorch-pr-156630.patch # https://github.com/pytorch/pytorch/pull/156630
-        fix-dist-cuda.patch
-        )
+        fix-miniz.patch # https://github.com/pytorch/pytorch/commit/a02e88d19c01a7226fa69fa0bf3a6a0b9a21c7e2
+)
 
-file(REMOVE_RECURSE "${SOURCE_PATH}/caffe2/core/macros.h") # We must use generated header files
+file(REMOVE_RECURSE
+    "${SOURCE_PATH}/caffe2/core/macros.h" # We must use generated header files
+    "${SOURCE_PATH}/third_party/miniz-3.0.2" # use vcpkg port 'miniz'
+)
 
+# even though we are using `USE_KINETO=OFF`, some files are using the headers
+# https://github.com/pytorch/kineto/pull/1099 removed MTIA_WORKLOADD enum. use a commit before the change
 vcpkg_from_github(
     OUT_SOURCE_PATH src_kineto
     REPO pytorch/kineto
-    REF d9753139d181b9ff42872465aac0e5d3018be415
-    SHA512 f037fac78e566c40108acf9eace55a8f67a2c5b71f298fd3cd17bf22cf05240c260fd89f017fa411656a7505ec9073a06a3048e191251d5cfc4b52c237b37d0b
+    REF 3f4beb08ad7e49be28eafa1216233df73e5be06f # 2025-06-03
+    SHA512 742e5119e130d3a01bf92480af9285c93663126b22de2c597fa566fb571969a724bb225ddb122e1cb32bce13d6507f6d3e8500205d0d6811f9c67838f44828ef
     HEAD_REF main
-    PATCHES
-      kineto.patch
 )
 file(COPY "${src_kineto}/" DESTINATION "${SOURCE_PATH}/third_party/kineto")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH src_cudnn
-    REPO NVIDIA/cudnn-frontend # new port ?
-    REF 2533f5e5c1877fd76266133c1479ef1643ce3a8b #  1.6.1 
-    SHA512 8caacdf9f7dbd6ce55507f5f7165db8640b681e2a7dfd6a841de8eaa3489cff5ba41d11758cc464320b2ff9a491f8234e1749580cf43cac702f07cf82611e084
+    REPO NVIDIA/cudnn-frontend
+    REF "v1.14.0"
+    SHA512 95dbf57594eda08905f176f6f18be297cfbac571460829c55959f15e87f92e9019812ff367bf857dc26d6961d9c6393e667b09e855732e3ab7645e93f325efa1
     HEAD_REF main
 )
 file(COPY "${src_cudnn}/" DESTINATION "${SOURCE_PATH}/third_party/cudnn_frontend")
@@ -60,8 +65,7 @@ message(STATUS "Using protoc: ${PROTOC}")
 
 x_vcpkg_get_python_packages(
     PYTHON_VERSION 3
-    PACKAGES typing-extensions pyyaml 
-    # numpy
+    PACKAGES typing-extensions pyyaml numpy
     OUT_PYTHON_VAR PYTHON3
 )
 
@@ -70,22 +74,18 @@ message(STATUS "Using Python3: ${PYTHON3}")
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
   FEATURES
     dist    USE_DISTRIBUTED # MPI, Gloo, TensorPipe
-    zstd    USE_ZSTD
     fbgemm  USE_FBGEMM
-    opencv  USE_OPENCV
+    openmp  USE_OPENMP
     opencl  USE_OPENCL
     mkldnn  USE_MKLDNN
+    mkldnn  USE_MKLDNN_CBLAS
     cuda    USE_CUDA
     cuda    USE_CUDNN
     cuda    USE_NCCL
-    cuda    USE_SYSTEM_NCCL
     cuda    USE_NVRTC
-    cuda    AT_CUDA_ENABLED
-    cuda    AT_CUDNN_ENABLED
     cuda    USE_MAGMA
     vulkan  USE_VULKAN
     vulkan  USE_VULKAN_RELAXED_PRECISION
-    rocm    USE_ROCM  # This is an alternative to cuda not a feature! (Not in vcpkg.json!) -> disabled
     llvm    USE_LLVM
     mpi     USE_MPI
     nnpack  USE_NNPACK  # todo: check use of `DISABLE_NNPACK_AND_FAMILY`
@@ -107,10 +107,10 @@ if("dist" IN_LIST FEATURES)
 endif()
 
 if("cuda" IN_LIST FEATURES)
-  vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root)
+    vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root) 
     list(APPEND FEATURE_OPTIONS
         "-DCMAKE_CUDA_COMPILER=${NVCC}"
-        "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
+        "-DCUDAToolkit_ROOT=${cuda_toolkit_root}" 
     )
 endif()
 
@@ -125,6 +125,13 @@ if(VCPKG_TARGET_IS_ANDROID OR VCPKG_TARGET_IS_IOS)
     set(TARGET_IS_MOBILE ON)
 endif()
 
+# By default MKL is used. We will use Eigen 
+set(BLAS "Eigen")
+if(TARGET_IS_MOBILE)
+    # In mobile, Eigen(embedded source) is used. We will use OpenBLAS instead.
+    set(BLAS "OpenBLAS") # see how port 'blas' works
+endif()
+
 set(TARGET_IS_APPLE OFF)
 if(VCPKG_TARGET_IS_IOS OR VCPKG_TARGET_IS_OSX)
     set(TARGET_IS_APPLE ON)
@@ -136,63 +143,42 @@ vcpkg_cmake_configure(
     DISABLE_PARALLEL_CONFIGURE
     OPTIONS
         ${FEATURE_OPTIONS}
-        -DProtobuf_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}
-        -DPython_EXECUTABLE:FILEPATH=${PYTHON3}
-        -DPython3_EXECUTABLE:FILEPATH=${PYTHON3}
-        -DBUILD_PYTHON=OFF
-        -DUSE_NUMPY=OFF
+        "-DPROTOBUF_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DCAFFE2_CUSTOM_PROTOC_EXECUTABLE:FILEPATH=${PROTOC}"
+        "-DPython_EXECUTABLE:FILEPATH=${PYTHON3}" # cmake/Codegen.cmake
+        -DBUILD_CUSTOM_PROTOBUF=OFF # cmake/ProtoBuf.cmake
+        -DCOMMIT_SHA=${GIT_COMMIT} # cmake/Codegen.cmake
+        -DINTERN_BUILD_MOBILE=${TARGET_IS_MOBILE}
+        -DATEN_NO_TEST=ON
         -DCAFFE2_STATIC_LINK_CUDA=ON
         -DCAFFE2_USE_MSVC_STATIC_RUNTIME=${USE_STATIC_RUNTIME}
-        -DBUILD_CUSTOM_PROTOBUF=OFF
-        -DBUILD_PYTHON=OFF
-        -DUSE_LITE_PROTO=OFF
-        -DBUILD_TEST=OFF
-        -DATEN_NO_TEST=ON
-        -DUSE_SYSTEM_LIBS=ON
-        -DUSE_METAL=OFF
+        -DCAFFE2_CMAKE_BUILDING_WITH_MAIN_REPO=OFF
         -DUSE_FLASH_ATTENTION=OFF
-        -DUSE_PYTORCH_METAL=OFF
-        -DUSE_PYTORCH_METAL_EXPORT=OFF
-        -DUSE_PYTORCH_QNNPACK:BOOL=OFF
         -DUSE_ITT=OFF
-        -DUSE_ROCKSDB=ON
-        -DUSE_OBSERVERS=OFF
         -DUSE_KINETO=OFF
-        -DUSE_ROCM=OFF
-        -DUSE_NUMA=OFF
+        -DUSE_ROCM=OFF # This is an alternative to cuda
+        -DUSE_NUMA=${VCPKG_TARGET_IS_LINUX}
         -DUSE_SYSTEM_LIBS=ON
         -DBUILD_JNI=${VCPKG_TARGET_IS_ANDROID}
         -DUSE_NNAPI=${VCPKG_TARGET_IS_ANDROID}
-        ${BLAS_OPTIONS}
-        # BLAS=MKL not supported in this port
-        -DUSE_MKLDNN=OFF
-        -DUSE_MKLDNN_CBLAS=OFF
-        #-DCAFFE2_USE_MKL=ON
-        #-DAT_MKL_ENABLED=ON
-        -DAT_MKLDNN_ENABLED=OFF
-        -DUSE_OPENCL=ON
-        -DUSE_KINETO=OFF #
-    # Should be enabled in-future along with the "python" feature (currently disabled)
-    # OPTIONS_RELEASE
-    #  -DPYTHON_LIBRARY=${CURRENT_INSTALLED_DIR}/lib/python311.lib
-    # OPTIONS_DEBUG
-    #  -DPYTHON_LIBRARY=${CURRENT_INSTALLED_DIR}/debug/lib/python311_d.lib
+        -DBLAS=${BLAS} # BLAS=MKL not supported in this port
+        -DUSE_COREML_DELEGATE=${VCPKG_TARGET_IS_IOS}
+        -DUSE_PYTORCH_METAL=${TARGET_IS_APPLE}
+        -DUSE_PYTORCH_METAL_EXPORT=${VCPKG_TARGET_IS_OSX}
+        -DUSE_PYTORCH_QNNPACK=OFF
+    OPTIONS_DEBUG
+        -DPRINT_CMAKE_DEBUG_INFO=ON
     MAYBE_UNUSED_VARIABLES
         USE_NUMA
-        USE_SYSTEM_BIND11
         MKLDNN_CPU_RUNTIME
-        PYTHON_LIBRARY
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
 vcpkg_cmake_config_fixup(PACKAGE_NAME Caffe2 CONFIG_PATH "share/cmake/Caffe2" DO_NOT_DELETE_PARENT_CONFIG_PATH)
-vcpkg_cmake_config_fixup(PACKAGE_NAME torch CONFIG_PATH "share/cmake/Torch" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(PACKAGE_NAME Torch CONFIG_PATH "share/cmake/Torch" DO_NOT_DELETE_PARENT_CONFIG_PATH)
 vcpkg_cmake_config_fixup(PACKAGE_NAME ATen CONFIG_PATH "share/cmake/ATen" )
-
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/torch/TorchConfig.cmake" "/../../../" "/../../")
 
 # Traverse the folder and remove "some" empty folders
 function(cleanup_once folder)
