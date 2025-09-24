a4ebabdc8ecc0afee960a549a82b9026fe373f7c
Nouridin <modi9hofi@gmail.com>
2025-09-24 16:05:26 +0300

    fixed compatibility

--- ports/cconfig/portfile.cmake
+++ ports/cconfig/portfile.cmake
@@ -3,7 +3,6 @@ vcpkg_from_github(
     REPO Nouridin/cconfig
     REF "v${VERSION}" # e.g., v1.0.0
     SHA512 8CE0C0FCA4E55AF9CFD56BA7779F4775703752D328518FE72F242336A7D4DB08B53284CA6148FC65BDBFE7D5BE4F025F49DFC7B13A45E2B69F350E15966C1929
-    HEAD_REF main
 )
 
 # Copy the main header
@@ -17,6 +16,3 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 # Remove auto-generated usage file if present
 file(REMOVE "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage")
-
-# Add a minimal test to ensure the package is usable
-vcpkg_test_cmake(TEST_ROOT_DIR "${CURRENT_PORT_DIR}")
--- ports/cconfig/test_package/test.c
+++ ports/cconfig/test_package/test.c
@@ -2,8 +2,6 @@
 #include "cconfig.h"
 
 int main() {
-    // This is a minimal test to ensure the header can be included
-    // and the library can be linked.
     printf("cconfig header included successfully.\n");
     return 0;
 }
