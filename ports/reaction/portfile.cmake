878ce5d1ae36db679a20749fd1c3480d1fe3eb87
lumia431 <844727475@qq.com>
2025-09-26 10:13:56 +0800

    [reaction] Add new port

--- ports/reaction/portfile.cmake
+++ ports/reaction/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lumia431/reaction
-    REF b06294a1092f17f6dae94af78ec0be6c29c83f90
-    SHA512 26c0537c8734b7cfb14195a4cca8f63ca93499cc367ba601679a60021a9fb7cf74f0411432f9f82d019f135fc9113cd6c72b41fa42b16999f4c333f7f2ccc510
+    REF "${VERSION}"
+    SHA512 7747b621c790318d3240f8634bf2310420c93b1418f9ac1384d784ab08658f12d2631e59d5927dc2b81c7801d150a785ea1cdfbb0b7025ea0531047023f8dab1
     HEAD_REF master
 )
 
@@ -15,13 +15,12 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/reaction)
-
-file(INSTALL "${SOURCE_PATH}/include/reaction.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+
