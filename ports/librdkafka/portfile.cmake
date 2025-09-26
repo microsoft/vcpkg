c41e5dd1b2399f000c0c625b0e8778db69181339
toge <toge.mail@gmail.com>
2025-09-27 03:49:54 +0900

    [librdkafka] update to 2.11.1 (#47507)

--- ports/librdkafka/portfile.cmake
+++ ports/librdkafka/portfile.cmake
@@ -2,10 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO confluentinc/librdkafka
     REF "v${VERSION}"
-    SHA512 82d96cc77905e203a0178e378c78192b41afdc8e16fafd80cc16464fde2dc089b918d4664fd58857b5c888cdd8c40ffabe58993e9a7c2e2ff3e4ac9496927826
+    SHA512 e0eba567605578dd681d536a25f92251ccbdfab1630133c36f1d0e4c97b126cce2ccc64276a9dff88df17a847f72c8d04686db63579e1f57e76b5bd87aa2f411
     HEAD_REF master
     PATCHES
         lz4.patch
+        # remove it when https://github.com/confluentinc/librdkafka/pull/5136 is merged
+        fix_oauthbearer_check.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" RDKAFKA_BUILD_STATIC)
