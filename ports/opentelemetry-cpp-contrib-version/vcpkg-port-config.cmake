74227d50f6dd2e1fec4dd33c1170d87b3d665a59
Serkan Kenar <serkankenar@gmail.com>
2025-09-29 18:18:36 +0100

    [opentelemetry-cpp] Update to 1.23.0 (#47555)

--- ports/opentelemetry-cpp-contrib-version/vcpkg-port-config.cmake
+++ ports/opentelemetry-cpp-contrib-version/vcpkg-port-config.cmake
@@ -4,9 +4,9 @@ function(clone_opentelemetry_cpp_contrib CONTRIB_SOURCE_PATH)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO open-telemetry/opentelemetry-cpp-contrib
-        REF 9176445fccead4f356d56040372f090f218158c1
+        REF ec0b6cf82ec204e13ff9b0b231ffda05c6191196
         HEAD_REF main
-        SHA512 7e88efe814fa165f1391b02a5414f02ffd953ee817cd89521888a492c7ff0e7e0cdb099b93b94f95722f9247501c29319c254773f7c55635d325a1ad9a08e28e
+        SHA512 3ea1780895e51a414713d3487dc82e071a4fd5b98dc055661b7d4267d5a099274d7cf2b043085f152174e870924086b9ef0c92a00efc7605ba53b5732ae55f33
     )
     set(${CONTRIB_SOURCE_PATH} ${SOURCE_PATH} CACHE INTERNAL "")
 endfunction()
