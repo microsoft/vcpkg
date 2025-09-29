1bcf3b61b3a4535545286bf14c016937868eb8bd
Kai Pastor <dg0yt@darc.de>
2025-09-29 18:20:28 +0200

    [ggml] Update, backport vulkan loader patch, restore test integrity (#47495)

--- scripts/test_ports/vcpkg-ci-ggml/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-ggml/portfile.cmake
@@ -6,7 +6,6 @@ vcpkg_from_github(
     REF v0.9.1
     SHA512 c31aeaaba328cd217f34191f1ce87720bb34dc39dc036f2ba8c92710636706f5be2cfcf86dc8c38ec737b020908da0e136447de10e7d9e6db698c812e7d21ae3
     HEAD_REF master
-    PATCHES
 )
 
 vcpkg_cmake_configure(
