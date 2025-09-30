89e71cb9e4ccfbb57b394d883a27102b49e103bf
Alexander Vieth <a.vieth@tudelft.nl>
2025-09-30 13:08:11 +0200

    add patch comment

--- ports/ltla-cppkmeans/portfile.cmake
+++ ports/ltla-cppkmeans/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 17ea2822ccf35adc18a5162ac5bb469bbd46fba32072e524fda4e3baa12bc04f9666c69ccb8fe71b02e471197acc392958bf5d6036ba9e7829d7a8d4522471d5
     HEAD_REF master
     PATCHES
-        0001-fix-dependencies.patch
+        0001-fix-dependencies.patch # https://github.com/LTLA/CppKmeans/pull/15
 )
 
 set(VCPKG_BUILD_TYPE "release") # header-only port
