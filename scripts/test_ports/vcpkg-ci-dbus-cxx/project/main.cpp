714841b4cd17c1243aab044bfefd31e01128500a
Saikari <lin@sz.cn.eu.org>
2025-10-03 14:29:08 +0300

    improve vcpkg-ci-dbus-cxx

--- scripts/test_ports/vcpkg-ci-dbus-cxx/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-dbus-cxx/project/main.cpp
@@ -1,6 +1,11 @@
 #include <dbus-cxx.h>
+#include <dbus-cxx-glib.h>
+#include <dbus-cxx-uv.h>
+#include <memory>
 int main()
 {
    auto connection = DBus::Connection::create(DBus::BusType::SESSION);
+   std::shared_ptr<DBus::Dispatcher> disp = DBus::GLib::GLibDispatcher::create();
+   std::shared_ptr<DBus::Dispatcher> disp = DBus::Uv::UvDispatcher::create();
    return 0;
 }
