18c56427f867c8f3f86d46a51a0d6aadd582272a
Henrik Quanz <fhquanz@web.de>
2025-10-04 15:43:19 +0200

    Use git-svn to fetch from svn repositories

--- scripts/cmake/vcpkg_from_svn.cmake
+++ scripts/cmake/vcpkg_from_svn.cmake
@@ -1,14 +1,15 @@
 function(z_vcpkg_parse_revision)
     cmake_parse_arguments(PARSE_ARGV 0 "arg"
         ""
-        "URL;SVN_COMMAND;OUT_VAR"
-        ""
+        "OUT_SOURCE_PATH;SVN_COMMAND;OUT_VAR;WORKING_DIRECTORY"
+        "PATCHES"
     )
         vcpkg_execute_in_download_mode(
-            COMMAND "${arg_SVN_COMMAND}" info "${arg_URL}"
+            COMMAND ${arg_SVN_COMMAND} info
             OUTPUT_VARIABLE info_string 
             ERROR_VARIABLE info_string 
             RESULT_VARIABLE error_code
+            WORKING_DIRECTORY ${arg_WORKING_DIRECTORY}
         )
 
         if(error_code)
@@ -19,59 +20,77 @@ function(z_vcpkg_parse_revision)
             vcpkg_list(SET revision ${CMAKE_MATCH_1})
             set("${arg_OUT_VAR}" "${revision}" PARENT_SCOPE)
         else()
-            message(FATAL_ERROR "Unable to parse the revision.")
+            message(FATAL_ERROR "Unable to parse the svn revision for port ${PORT}.")
         endif()
 
 endfunction()
 
 function(vcpkg_from_svn)
     cmake_parse_arguments(PARSE_ARGV 0 "arg"
-        ""
-        "OUT_SOURCE_PATH;URL;REF;HEAD_REF;IGNORE_EXTERNALS"
+        "STDLAYOUT"
+        "OUT_SOURCE_PATH;URL;REF;FETCH_REF;HEAD_REF;TRUNK;BRANCHES;TAGS"
         "PATCHES"
     )
 
     if(DEFINED arg_UNPARSED_ARGUMENTS)
         message(WARNING "vcpkg_from_svn was passed extra arguments: ${arg_UNPARSED_ARGUMENTS}")
     endif()
-
     if(NOT DEFINED arg_OUT_SOURCE_PATH)
         message(FATAL_ERROR "OUT_SOURCE_PATH must be specified")
     endif()
+
     if(NOT DEFINED arg_URL)
         message(FATAL_ERROR "URL must be specified")
     endif()
     if(NOT DEFINED arg_REF AND NOT DEFINED arg_HEAD_REF)
         message(FATAL_ERROR "At least one of REF or HEAD_REF must be specified")
     endif()
-
-    set(hash_working_dir "${arg_URL}")
-    if(DEFINED ${arg_REF})
-        string(APPEND hash_working_dir "-${arg_REF}")
-    else()
-        string(APPEND hash_working_dir "-${arg_HEAD_REF}")
+    if(DEFINED arg_FETCH_REF AND NOT DEFINED arg_REF)
+        message(FATAL_ERROR "REF must be specified if FETCH_REF is specified")
     endif()
-    string(SHA256 hash_working_dir "${hash_working_dir}")
+
+    set(hash_archive "${arg_URL}")
+    if(DEFINED arg_REF)
+        string(APPEND hash_archive "-${arg_REF}-${arg_FETCH_REF}")
+    else()
+        string(APPEND hash_archive "-${arg_HEAD_REF}-${arg_REF}")
+    endif()
+    string(SHA1 hash_archive "${hash_archive}")
+    string(SHA1 hash_url "${arg_URL}")
 
     vcpkg_list(SET extract_working_directory_param)
     vcpkg_list(SET skip_patch_check_param)
-    vcpkg_list(SET svn_ignore_externals_param)
-    set(svn_working_directory "${DOWNLOADS}/svn-${hash_working_dir}")
-    vcpkg_list(SET svn_depth_param --depth infinity)
+    vcpkg_list(SET svn_standard_layout_param)
+    vcpkg_list(SET svn_trunk_param)
+    vcpkg_list(SET svn_tags_param)
+    vcpkg_list(SET svn_branches_param)
+    vcpkg_list(SET branch_to_use "trunk")
+    vcpkg_list(SET ref_to_fetch)
+    vcpkg_list(SET git_svn_fetch_parent_param "--no-follow-parent")
+
+    set(svn_working_directory "${DOWNLOADS}/git-svn-tmp-${hash_url}")
     set(do_download OFF)
-    if(DEFINED IGNORE_EXTERNALS)
-        vcpkg_list(SET "svn_ignore_externals_param" --ignore-externals)
+
+    if(arg_STDLAYOUT)
+            vcpkg_list(SET svn_standard_layout_param "--stdlayout")
+    endif()
+    if(DEFINED arg_TRUNK)
+            vcpkg_list(SET svn_trunk_param "--trunk=${arg_TRUNK}")
+    endif()
+    if(DEFINED arg_BRANCHES)
+            vcpkg_list(SET svn_branches_param "--trunk=${arg_BRANCHES}")
+    endif()
+    if(DEFINED arg_TAGS)
+            vcpkg_list(SET svn_tags_param "--trunk=${arg_TAGS}")
     endif()
 
     if(VCPKG_USE_HEAD_VERSION AND DEFINED arg_HEAD_REF)
         vcpkg_list(SET working_directory_param "WORKING_DIRECTORY" "${CURRENT_BUILDTREES_DIR}/src/head")
         vcpkg_list(SET skip_patch_check_param SKIP_PATCH_CHECK)
-        set(arg_URL "${arg_URL}/${arg_HEAD_REF}")
-        z_vcpkg_parse_revision(
-            URL "${arg_URL}" SVN_COMMAND "${svn}" OUT_VAR ref_to_fetch)
-        set(arg_REF ${ref_to_fetch})
-        string(REPLACE "/" "_-" sanitized_ref "${arg_REF}")
-
+        set(branch_to_use "${arg_HEAD_REF}")
+        vcpkg_list(SET ref_to_fetch "0:HEAD")
+        message(STATUS "Fetching all revisions from svn repository. This can take a while for large repositories.")
+        set(svn_working_directory "${CURRENT_BUILDTREES_DIR}/src/git-svn-tmp-${hash_url}")
         if(NOT _VCPKG_NO_DOWNLOADS)
             set(do_download ON)
         endif()
@@ -83,12 +102,17 @@ function(vcpkg_from_svn)
             message(STATUS "Package does not specify HEAD_REF. Falling back to non-HEAD version.")
         endif()
 
-        set(ref_to_fetch "${arg_REF}")
-        string(REPLACE "/" "_-" sanitized_ref "${arg_REF}")
+        if(DEFINED arg_FETCH_REF)
+            set(branch_to_use "${arg_FETCH_REF}")
+            vcpkg_list(SET ref_to_fetch "${arg_REF}")
+        else()
+            set(branch_to_use "trunk")
+            vcpkg_list(SET ref_to_fetch "${arg_REF}")
+        endif()
     endif()
 
-    set(temp_archive "${DOWNLOADS}/temp/${PORT}-${url_hash}-${sanitized_ref}.tar.gz")
-    set(archive "${DOWNLOADS}/${PORT}-${url_hash}-${sanitized_ref}.tar.gz")
+    set(temp_archive "${DOWNLOADS}/temp/${PORT}-${hash_archive}.tar.gz")
+    set(archive "${DOWNLOADS}/${PORT}-${hash_archive}.tar.gz")
 
     if(NOT EXISTS "${archive}")
         if(_VCPKG_NO_DOWNLOADS)
@@ -98,29 +122,92 @@ function(vcpkg_from_svn)
     endif()
 
     if(do_download)
-        message(STATUS "Fetching ${arg_URL} ${ref_to_fetch}...")
-        find_program(SVN NAMES svn svn.cmd)
-        file(MAKE_DIRECTORY "${DOWNLOADS}")
-        # Note: git init is safe to run multiple times
-        if(VCPKG_USE_HEAD_VERSION)
-            set(expected_rev_parse HEAD)
-        else(rev_parse_ref)
-            set(expected_rev_parse "${arg_REF}")
+        message(STATUS "Fetching ${arg_URL} ${branch_to_use}...")
+        find_program(GIT NAMES git git.cmd)
+        vcpkg_list(SET SVN "${GIT}" -c init.defaultBranch=trunk svn)
+
+        vcpkg_execute_in_download_mode(
+            COMMAND ${SVN} --version
+            OUTPUT_VARIABLE svn_version_output
+            ERROR_VARIABLE svn_version_error
+            RESULT_VARIABLE svn_version_result
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+        )
+        if(svn_version_result)
+            message(FATAL_ERROR "git-svn is required for ${PORT}.")
         endif()
+
+        file(MAKE_DIRECTORY "${DOWNLOADS}")
+
+        # Note: git svn init is safe to run multiple times, if url is the same
+        vcpkg_execute_required_process(
+            ALLOW_IN_DOWNLOAD_MODE
+            COMMAND ${SVN} init ${svn_standard_layout_param} ${svn_trunk_param}
+            ${svn_branches_param} ${svn_tags_param}
+            ${arg_URL} ${svn_working_directory} 
+            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+            LOGNAME "git-svn-init-${TARGET_TRIPLET}"
+        )
 
         vcpkg_execute_required_process(
             ALLOW_IN_DOWNLOAD_MODE
-            COMMAND "${SVN}" checkout "--force"  -r "${ref_to_fetch}" ${svn_depth_param}
-              ${svn_ignore_externals_param} ${arg_URL} ${svn_working_directory}
-            WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-            LOGNAME "svn-checkout-${TARGET_TRIPLET}"
+            COMMAND ${SVN} fetch ${git_svn_fetch_parent_param} --revision "${ref_to_fetch}"
+            WORKING_DIRECTORY "${svn_working_directory}"
+            LOGNAME "git-svn-fetch-${TARGET_TRIPLET}"
         )
 
-        z_vcpkg_parse_revision(URL "${svn_working_directory}" SVN_COMMAND "${svn}" OUT_VAR rev_parse_ref)
+        vcpkg_execute_in_download_mode(
+            COMMAND ${GIT} switch --detach origin/${branch_to_use} 
+            OUTPUT_VARIABLE git_svn_switch
+            ERROR_VARIABLE git_svn_switch
+            RESULT_VARIABLE error_code
+            WORKING_DIRECTORY "${svn_working_directory}"
+        )
+
+        if(error_code)
+            message(FATAL_ERROR "Unable to switch to the svn branch origin/${branch_to_use}.\
+Maybe the layout of the svn repository was not specifed correctly. (git svn switch --detach origin/${branch_to_use} failed)")
+        endif()
+
+        if(NOT VCPKG_USE_HEAD_VERSION)
+            vcpkg_execute_in_download_mode(
+                COMMAND ${SVN} find-rev "r${arg_REF}"
+                OUTPUT_VARIABLE find_ref 
+                ERROR_VARIABLE find_ref_err
+                RESULT_VARIABLE error_code
+                WORKING_DIRECTORY "${svn_working_directory}"
+            )
+            if(error_code)
+                message(FATAL_ERROR "Could not find the specified revision ${arg_REF} in svn repository for port ${PORT}. \
+(git svn find-rev r${arg_REF} failed)")
+            endif()
+
+            string(STRIP "${find_ref}" find_ref)
+            vcpkg_execute_in_download_mode(
+                COMMAND ${GIT} checkout "${find_ref}"
+                OUTPUT_VARIABLE git_checkout_ref
+                ERROR_VARIABLE git_checkout_ref 
+                RESULT_VARIABLE error_code
+                WORKING_DIRECTORY "${svn_working_directory}"
+            )
+            if(error_code)
+                message(FATAL_ERROR "Could not checkout the revision ${find_ref} for port ${PORT}.\
+It can help to delete the svn working directory manually. (git checkout ${find_ref} failed in ${svn_working_directory}!)")
+            endif()
+        endif()
+
+        z_vcpkg_parse_revision( 
+            SVN_COMMAND "${SVN}" 
+            OUT_VAR rev_parse_ref
+            WORKING_DIRECTORY "${svn_working_directory}"
+        )
 
         string(STRIP "${rev_parse_ref}" rev_parse_ref)
-        if(NOT "${rev_parse_ref}" STREQUAL "${arg_REF}")
-                message(FATAL_ERROR "After checkout ${ref_to_fetch}, the requested REV (${arg_REF}) does not match \
+
+        if(VCPKG_USE_HEAD_VERSION)
+            set(VCPKG_HEAD_VERSION "${rev_parse_ref}" PARENT_SCOPE)
+        elseif(NOT "${rev_parse_ref}" STREQUAL "${arg_REF}")
+                message(FATAL_ERROR "After checkout ${branch_to_use}, the requested REV (${arg_REF}) does not match \
 the revision matched by svn info. \
     [Expected : ( ${arg_REF} )]) \
     [  Actual : ( ${rev_parse_ref} )]"
@@ -128,12 +215,12 @@ the revision matched by svn info. \
         endif()
 
         file(MAKE_DIRECTORY "${DOWNLOADS}/temp")
-        file(GLOB source_files "${svn_working_directory}/*")
+        # Always archive the current head
         vcpkg_execute_required_process(
             ALLOW_IN_DOWNLOAD_MODE
-            COMMAND "${CMAKE_COMMAND}" -E tar "cvf" "${temp_archive}" -- ${source_files}
+            COMMAND "${GIT}" -c core.autocrlf=false archive "HEAD" -o "${temp_archive}"
             WORKING_DIRECTORY "${svn_working_directory}"
-            LOGNAME svn-archive
+            LOGNAME git-archive
         )
         file(RENAME "${temp_archive}" "${archive}")
     else()
@@ -143,7 +230,7 @@ the revision matched by svn info. \
     vcpkg_extract_source_archive_ex(
         OUT_SOURCE_PATH SOURCE_PATH
         ARCHIVE "${archive}"
-        REF "${sanitized_ref}"
+        REF "${hash_archive}"
         PATCHES ${arg_PATCHES}
         NO_REMOVE_ONE_LEVEL
         ${extract_working_directory_param}
--- scripts/test_ports/vcpkg-from-svn-test/portfile.cmake
+++ scripts/test_ports/vcpkg-from-svn-test/portfile.cmake
@@ -1,103 +1,156 @@
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
+macro(windows_to_mingw_path NativePath ResultPath)
+  string(REGEX REPLACE "^([a-zA-Z]):[\\/]" "/\\1/" _tmp "${NativePath}")
+  string(REPLACE "\\" "/" ${ResultPath} "${_tmp}")
+endmacro()
+
 set(svn_test_repo "${CURRENT_BUILDTREES_DIR}/test-svn-repo")
+set(svn_test_repo2 "${CURRENT_BUILDTREES_DIR}/test-svn-repo2")
 set(svn_test_repo_checkout "${CURRENT_BUILDTREES_DIR}/svn-repo")
-set(svn_test_repo_layout "${CURRENT_BUILDTREES_DIR}/svn-repo-layout")
 
-set(svn_remote "file:///${svn_test_repo}")
-set(svn_remote_trunk "file:///${svn_test_repo}/trunk")
+windows_to_mingw_path("${svn_test_repo}" svn_test_repo_sanitized)
+windows_to_mingw_path("${svn_test_repo2}" svn_test_repo2_sanitized)
+windows_to_mingw_path("${svn_test_repo_checkout}" svn_test_repo_checkout_sanitized)
 
-vcpkg_find_acquire_program(SVN)
-vcpkg_find_acquire_program(SVNADMIN)
+set(svn_remote "file:///${svn_test_repo_sanitized}")
+set(svn_remote2 "file:///${svn_test_repo2_sanitized}")
 
-vcpkg_list(SET svnadmin ${SVNADMIN})
-vcpkg_list(SET svn ${SVN})
-
-message(STATUS "Creating test svn repository")
-file(REMOVE_RECURSE "${svn_test_repo}")
-file(REMOVE_RECURSE "${svn_test_repo_checkout}")
-file(REMOVE_RECURSE "${svn_test_repo_layout}")
-vcpkg_execute_required_process(
-    COMMAND ${svnadmin} create "test-svn-repo"
-    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-    LOGNAME "svn-init"
+vcpkg_find_acquire_program(GIT)
+vcpkg_list(SET git_config
+    -c core.autocrlf=false
+    -c user.email=vcpkg@example.com
+    -c user.name=vcpkg
+    -c init.defaultBranch=main
 )
-# Create default repo folders
+vcpkg_list(SET git ${GIT} ${git_config})
 
-message(STATUS "Creating default layout")
-file(MAKE_DIRECTORY "${svn_test_repo_layout}/trunk" "${svn_test_repo_layout}/tags" "${svn_test_repo_layout}/branches")
-vcpkg_execute_required_process(
-    COMMAND ${svn} import -m "Add default layout" ${svn_test_repo_layout} ${svn_remote}
+vcpkg_execute_in_download_mode(
+    COMMAND ${GIT} svn --version
+    OUTPUT_VARIABLE svn_version_output
+    ERROR_VARIABLE svn_version_error
+    RESULT_VARIABLE svn_version_result
     WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-    LOGNAME "svn-add-folders"
 )
 
+if(svn_version_result)
+    message(NOTICE "git-svn is required for test to continue. The test ${PORT} was skipped!")
+    return()
+endif()
+
+message(STATUS "Setup of test svn repository")
+
+macro(remove_recursive_if_exists Path)
+    if(EXISTS "${Path}")
+        file(REMOVE_RECURSE "${Path}")
+    endif()
+endmacro()
+
+remove_recursive_if_exists("${svn_test_repo}")
+remove_recursive_if_exists("${svn_test_repo_checkout}")
+file(MAKE_DIRECTORY "${svn_test_repo}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/empty-test-repo/" DESTINATION "${svn_test_repo}/")
+
 vcpkg_execute_required_process(
-    COMMAND ${svn} checkout ${svn_remote_trunk} ${svn_test_repo_checkout}
+    COMMAND ${git} svn clone --stdlayout ${svn_remote} "${svn_test_repo_checkout_sanitized}"
     WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
-    LOGNAME "svn-checkout-local"
+    LOGNAME "git-svn-checkout-local"
 )
 
 file(WRITE "${svn_test_repo_checkout}/README.txt" "first commit")
 vcpkg_execute_required_process(
-    COMMAND ${svn} add "README.txt"
+    COMMAND ${git} add "README.txt"
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-add.1"
+    LOGNAME "git-svn-add.1"
 )
 
 vcpkg_execute_required_process(
-    COMMAND ${svn} commit -m "first commit"
+    COMMAND ${git} commit -m "First Commit"
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-commit.1"
+    LOGNAME "git-svn-commit.1"
 )
 
 set(ref_1 "2")
 
+vcpkg_execute_required_process(
+    COMMAND ${git} svn dcommit 
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-svn-dcommit.1"
+)
 # Tag this commit, this will increase ref
 vcpkg_execute_required_process(
-    COMMAND ${svn} copy -m "tag first commit" . "${svn_remote}/tags/1.0.0"
+    COMMAND ${git} svn branch -t -m "Create tag" 1.0.0
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-copy.1"
+    LOGNAME "git-svn-tag.1"
+)
+
+vcpkg_execute_required_process(
+    COMMAND ${git} svn dcommit 
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-svn-dcommit.2"
 )
 
 set(ref_tag "3")
 
+vcpkg_execute_required_process(
+    COMMAND ${git} checkout main 
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-checkout.1"
+)
+
+vcpkg_execute_required_process(
+    COMMAND ${git} switch main
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-svn-switch.1"
+)
+
 file(WRITE "${svn_test_repo_checkout}/README.txt" "second commit")
 vcpkg_execute_required_process(
-    COMMAND ${svn} commit -m "second commit"
+    COMMAND ${git} commit -a -m "second commit"
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-commit.2"
+    LOGNAME "git-svn-commit.2"
+)
+
+vcpkg_execute_required_process(
+    COMMAND ${git} svn dcommit 
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-svn-dcommit.3"
 )
 
 set(ref_2 "4")
 
 vcpkg_execute_required_process(
-    COMMAND ${svn} copy -m "branch second commit" . "${svn_remote}/branches/develop"
+    COMMAND ${git} svn branch -m "Create develop branch" develop 
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-copy.2"
+    LOGNAME "git-svn-branch.1"
 )
-
 vcpkg_execute_required_process(
-    COMMAND ${svn} switch --force ^/branches/develop 
+    COMMAND ${git} checkout origin/develop 
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-switch.1"
+    LOGNAME "git-svn-checkout-branch.1"
 )
 
 file(WRITE "${svn_test_repo_checkout}/README.txt" "develop branch")
 vcpkg_execute_required_process(
-    COMMAND ${svn} commit -m "develop branch"
+    COMMAND ${git} commit -a -m "develop branch"
     WORKING_DIRECTORY "${svn_test_repo_checkout}"
-    LOGNAME "svn-commit.3"
+    LOGNAME "git-svn-commit.3"
+)
+
+vcpkg_execute_required_process(
+    COMMAND ${git} svn dcommit 
+    WORKING_DIRECTORY "${svn_test_repo_checkout}"
+    LOGNAME "git-svn-dcommit.4"
 )
 
 set(ref_branch "6")
-
 
 message(STATUS "Testing regular mode")
 set(VCPKG_USE_HEAD_VERSION OFF)
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
-    URL "${svn_remote}/trunk"
+    URL "${svn_remote}"
+    STDLAYOUT
     REF "${ref_1}"
 )
 
@@ -108,11 +161,13 @@ ${contents}
 ")
 endif()
 
+
 message(STATUS "Testing regular mode that happens to match HEAD")
 set(VCPKG_USE_HEAD_VERSION OFF)
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
-    URL "${svn_remote}/trunk"
+    URL "${svn_remote}"
+    STDLAYOUT
     REF "${ref_2}"
 )
 file(READ "${source_path}/README.txt" contents)
@@ -127,8 +182,18 @@ set(VCPKG_USE_HEAD_VERSION ON)
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
     URL "${svn_remote}"
+    STDLAYOUT
+    REF "${ref_2}"
+    FETCH_REF "trunk"
     HEAD_REF "trunk"
 )
+
+if(NOT "${VCPKG_HEAD_VERSION}" STREQUAL "${ref_2}")
+    message(FATAL_ERROR "Failed to checkout the right HEAD commit.
+    Expected: ${ref_2}
+    Actual  : ${VCPKG_HEAD_VERSION}
+")
+endif()
 
 file(READ "${source_path}/README.txt" contents)
 if(NOT "${contents}" STREQUAL "second commit")
@@ -141,8 +206,10 @@ set(VCPKG_USE_HEAD_VERSION OFF)
 message(STATUS "Testing regular mode with branch")
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
-    URL "${svn_remote}/branches/develop"
+    URL "${svn_remote}"
+    STDLAYOUT
     REF ${ref_branch}
+    FETCH_REF "develop"
 )
 file(READ "${source_path}/README.txt" contents)
 if(NOT "${contents}" STREQUAL "develop branch")
@@ -153,8 +220,10 @@ endif()
 message(STATUS "Testing regular mode with tag")
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
-    URL "${svn_remote}/tags/1.0.0"
+    URL "${svn_remote}"
+    STDLAYOUT
     REF ${ref_tag}
+    FETCH_REF "tags/1.0.0"
 )
 file(READ "${source_path}/README.txt" contents)
 if(NOT "${contents}" STREQUAL "first commit")
@@ -168,8 +237,17 @@ set(VCPKG_USE_HEAD_VERSION ON)
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
     URL "${svn_remote}"
-    HEAD_REF /branches/develop
+    STDLAYOUT
+    HEAD_REF develop
 )
+
+if(NOT "${VCPKG_HEAD_VERSION}" STREQUAL "${ref_branch}")
+    message(FATAL_ERROR "Failed to checkout the right HEAD commit.
+    Expected: ${ref_2}
+    Actual  : ${VCPKG_HEAD_VERSION}
+")
+endif()
+
 file(READ "${source_path}/README.txt" contents)
 if(NOT "${contents}" STREQUAL "develop branch")
     message(FATAL_ERROR "Failed to checkout the branch commit. Contents were:
@@ -182,11 +260,67 @@ set(VCPKG_USE_HEAD_VERSION ON)
 vcpkg_from_svn(
     OUT_SOURCE_PATH source_path
     URL "${svn_remote}"
-    HEAD_REF /tags/1.0.0
+    STDLAYOUT
+    HEAD_REF tags/1.0.0
 )
+
+if(NOT "${VCPKG_HEAD_VERSION}" STREQUAL "${ref_tag}")
+    message(FATAL_ERROR "Failed to checkout the right HEAD commit.
+    Expected: ${ref_2}
+    Actual  : ${VCPKG_HEAD_VERSION}
+")
+endif()
+
 file(READ "${source_path}/README.txt" contents)
 if(NOT "${contents}" STREQUAL "first commit")
     message(FATAL_ERROR "Failed to checkout the branch commit. Contents were:
+${contents}
+")
+endif()
+
+message(STATUS "Testing head mode + no HEAD_REF -> just uses REF")
+set(VCPKG_USE_HEAD_VERSION ON)
+vcpkg_from_svn(
+    OUT_SOURCE_PATH source_path
+    URL "${svn_remote}"
+    STDLAYOUT
+    REF ${ref_1}
+)
+
+if(NOT "${VCPKG_HEAD_VERSION}" STREQUAL "${ref_tag}")
+    message(FATAL_ERROR "Failed to checkout the right HEAD commit.
+    Expected: ${ref_1}
+    Actual  : ${VCPKG_HEAD_VERSION}
+")
+endif()
+
+file(READ "${source_path}/README.txt" contents)
+if(NOT "${contents}" STREQUAL "first commit")
+    message(FATAL_ERROR "Failed to checkout the branch commit. Contents were:
+${contents}
+")
+endif()
+
+message(STATUS "Testing normal mode with a different url.")
+vcpkg_execute_required_process(
+    COMMAND ${CMAKE_COMMAND} -E copy_directory
+            ${svn_test_repo}
+            ${svn_test_repo2}
+    WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+    LOGNAME "copy-repository"
+)
+
+set(VCPKG_USE_HEAD_VERSION OFF)
+vcpkg_from_svn(
+    OUT_SOURCE_PATH source_path
+    URL "${svn_remote2}"
+    STDLAYOUT
+    REF "${ref_1}"
+)
+
+file(READ "${source_path}/README.txt" contents)
+if(NOT "${contents}" STREQUAL "first commit")
+    message(FATAL_ERROR "Failed to checkout the first commit. Contents were:
 ${contents}
 ")
 endif()
