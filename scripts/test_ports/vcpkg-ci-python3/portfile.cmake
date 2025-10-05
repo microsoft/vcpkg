f4c1321efb85dec768297a77903a20655bc3e014
Kai Pastor <dg0yt@darc.de>
2025-10-05 18:35:29 +0200

    Check library per build type

--- scripts/test_ports/vcpkg-ci-python3/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-python3/portfile.cmake
@@ -2,5 +2,11 @@ set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${CURRENT_PORT_DIR}/project"
+    OPTIONS_DEBUG
+        "-DEXPECTED_LIBRARY_KEYWORD=debug"
+        "-DEXPECTED_LIBRARY_PREFIX=${CURRENT_INSTALLED_DIR}/debug/lib"
+    OPTIONS_RELEASE
+        "-DEXPECTED_LIBRARY_KEYWORD=optimized"
+        "-DEXPECTED_LIBRARY_PREFIX=${CURRENT_INSTALLED_DIR}/lib"
 )
 vcpkg_cmake_build()
