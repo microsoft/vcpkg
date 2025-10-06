4331ccb54f262f900713cf04cb5fc64f9d8d9f1d
Saikari <lin@sz.cn.eu.org>
2025-10-06 05:53:45 +0300

    simplify

--- scripts/test_ports/vcpkg-ci-tgbot-cpp/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-tgbot-cpp/project/main.cpp
@@ -1,11 +1,9 @@
 #include <tgbot/tgbot.h>
-#include <string>
 using namespace TgBot;
 int main()
 {
-   std::string token("TOKEN");
    CurlHttpClient curlHttpClient;
-   Bot bot(token, curlHttpClient);
+   Bot bot("TOKEN", curlHttpClient);
    bot.getApi().deleteWebhook();
    return 0;
 }
