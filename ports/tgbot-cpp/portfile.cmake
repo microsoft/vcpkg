89c96f45a7e29025f235cfa70c2ad2cc5ad3f07e
Saikari <lin@sz.cn.eu.org>
2025-10-06 05:17:17 +0300

    fixup

--- ports/tgbot-cpp/portfile.cmake
+++ ports/tgbot-cpp/portfile.cmake
@@ -1,13 +1,16 @@
 vcpkg_from_github(
-  OUT_SOURCE_PATH SOURCE_PATH
-  REPO reo7sp/tgbot-cpp
-  REF "v${VERSION}"
-  SHA512 34eac9aac2cbf6025bde24c1a2bdb79b143a18b8fffd81e51340ee3cbb61338b1747e3d54c2d8b0f99e381231756bf11daa4b6ba4da1fd0a1ef40969dee7c647
-  HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO reo7sp/tgbot-cpp
+    REF "v${VERSION}"
+    SHA512 34eac9aac2cbf6025bde24c1a2bdb79b143a18b8fffd81e51340ee3cbb61338b1747e3d54c2d8b0f99e381231756bf11daa4b6ba4da1fd0a1ef40969dee7c647
+    HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DENABLE_TESTS=OFF
+        -DBUILD_DOCUMENTATION=OFF
 )
 
 vcpkg_cmake_install()
@@ -16,7 +19,7 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
 endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
