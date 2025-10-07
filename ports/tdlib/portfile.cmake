5df5e578bbf1b1ab630a3aeb4d8bad8536e04717
Saikari <lin@sz.cn.eu.org>
2025-10-07 08:45:11 +0300

    tdlib: add new package

---
+++ ports/tdlib/portfile.cmake
@@ -0,0 +1,24 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO tdlib/td
+    REF "v${VERSION}"
+    HEAD_REF master
+    SHA512 7992bc295900b4a770ec3316cc5d32eac5ced45d9019d65d56d753875fb07ccaca80db6ed8217472cef9a40ac0bf54b438214728f110f3b1ea62078252740640
+)
+
+vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/gperf")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DTD_ENABLE_JNI=${VCPKG_TARGET_IS_ANDROID}
+        -DTD_ENABLE_DOTNET=OFF
+        -DTD_ENABLE_LTO=OFF
+        -DTD_ENABLE_MULTI_PROCESSOR_COMPILATION=${VCPKG_DETECTED_MSVC}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Td")
+vcpkg_copy_pdbs()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")
