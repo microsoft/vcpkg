5e4fc95b386ef6fd606bd14b3b0c5be0c3eff819
Saikari <lin@sz.cn.eu.org>
2025-10-07 21:46:43 +0300

    test

--- ports/tdlib/portfile.cmake
+++ ports/tdlib/portfile.cmake
@@ -11,21 +11,23 @@ vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/gperf")
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DTD_INSTALL_SHARED_LIBRARIES=ON
-        -DTD_INSTALL_STATIC_LIBRARIES=ON
+        #-DTD_INSTALL_SHARED_LIBRARIES=OFF
+        #-DTD_INSTALL_STATIC_LIBRARIES=ON
         -DTD_ENABLE_JNI=${VCPKG_TARGET_IS_ANDROID}
         -DTD_ENABLE_DOTNET=OFF
         -DTD_GENERATE_SOURCE_FILES=OFF
+        -DTD_E2E_ONLY=OFF
         -DTD_ENABLE_DOTNET=OFF
         -DTD_ENABLE_LTO=OFF
         -DTD_ENABLE_MULTI_PROCESSOR_COMPILATION=${VCPKG_DETECTED_MSVC}
+        -DBUILD_TESTING=OFF
     MAYBE_UNUSED_VARIABLES
         TD_ENABLE_MULTI_PROCESSOR_COMPILATION
 )
 
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Td")
+#vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Td")
 vcpkg_copy_pdbs()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")
