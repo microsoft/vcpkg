0ce2a9db0f3c743f4323484b1ed66a92a8a52ca6
Kai Pastor <dg0yt@darc.de>
2025-10-08 20:10:21 +0200

    Update to 1.26.10, minor amendments [skip actions]

--- ports/libmupdf/portfile.cmake
+++ ports/libmupdf/portfile.cmake
@@ -4,11 +4,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ArtifexSoftware/mupdf
     REF "${VERSION}"
-    SHA512 76b0cd9ea2ea203ae37ce17dd2de8fceb13d0ff2fc24a31483306c6a6ecd4851d2ecb81e177c3ebc1e4eea7404697b4aeba451883dd40b21f10bb3eb101da411
+    SHA512 c0f802fd2b181587df1748a8db7163bbcd3951b943d1321afcff56fccb515dfe99061288bc691323d0854305a1d4205c99457954b10439adb122975429cbce72
     HEAD_REF master
-    PATCHES
-        dont-generate-extract-3rd-party-things.patch
-#        fix-NAN-on-Win11.patch # https://github.com/ArtifexSoftware/mupdf/pull/54
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
@@ -25,7 +22,6 @@ endif()
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_EXAMPLES=OFF
         ${OPTIONS}
 )
 vcpkg_cmake_install()
