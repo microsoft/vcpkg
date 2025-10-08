26a19f54c53d76f3993f7697264bfa282d29b3a5
Martin Moene <martin.moene@gmail.com>
2025-10-08 13:59:34 +0200

    [nonstd-lite-bit] Add new port at v1.2.0

---
+++ ports/nonstd-lite-bit/portfile.cmake
@@ -0,0 +1,32 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO nonstd-lite/bit-lite
+    REF "v${VERSION}"
+    SHA512 96706a536891cdeaa7a3c2285a610b0fcf0a7096fe89aca8eef6d8c8db89c71263d3eaa2fc97cdd80992a0ce196a0e3aaa979b48e452820302fd7db891c7b761
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBIT_LITE_OPT_BUILD_TESTS=OFF
+        -DBIT_LITE_OPT_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    CONFIG_PATH lib/cmake/bit-lite
+)
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
+    "${CURRENT_PACKAGES_DIR}/lib"
+)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+
+file(INSTALL
+    "${CMAKE_CURRENT_LIST_DIR}/usage"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+)
