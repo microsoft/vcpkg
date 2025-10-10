39db02c3d58b1ef94f66b27c27133cb596fcf719
Saikari <lin@sz.cn.eu.org>
2025-10-10 05:10:49 +0300

    fixup

--- ports/mapbox-variant/portfile.cmake
+++ ports/mapbox-variant/portfile.cmake
@@ -16,6 +16,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/variant)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/lib" "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
