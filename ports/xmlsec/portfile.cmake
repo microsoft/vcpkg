b40187e58b1bb38197e4b0b6d96deb3e45691218
Raul Metsma <raul@metsma.ee>
2025-10-15 10:33:42 +0300

    [xmlsec] Update to 1.3.8
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- ports/xmlsec/portfile.cmake
+++ ports/xmlsec/portfile.cmake
@@ -3,15 +3,19 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lsh123/xmlsec
     REF "${release_tag}"
-    SHA512 28130c10d79f652e3533e6ede5fdaab0f6db5a4bbaaca4713b62df9af2ae2d5314acf82d01f344f87faf95c12099fd77e0858cbe5232a96de1d531e6284ede1b
+    SHA512 40a6d73c308408f5bea71b779ad070b74609f7bff2cd28f6c4191e8a67981d81041b9c7acbdba36192ce7bcaac21e55c5ad58096d20d613c1a010d45491c7ba4
     HEAD_REF master
     PATCHES 
         pkgconfig_fixes.patch
 )
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES "with-dl" BUILD_WITH_DYNAMIC_LOADING
+)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
     OPTIONS_DEBUG -DINSTALL_HEADERS_TOOLS=OFF
 )
 
