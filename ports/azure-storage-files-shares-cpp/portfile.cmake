ba31875a70e2ec9c6b1ad9abd1d1374c25353c99
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2025-10-16 17:08:08 -0700

    Add cgmanifest.json removal during installation

--- ports/azure-storage-files-shares-cpp/portfile.cmake
+++ ports/azure-storage-files-shares-cpp/portfile.cmake
@@ -9,6 +9,9 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
+file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
+file(REMOVE_RECURSE ${unused})
+
 if(EXISTS "${SOURCE_PATH}/sdk/storage/azure-storage-files-shares")
   file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/storage/_")
   file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/_")
