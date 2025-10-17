7f288f81b2c99509a5d7f98e3d25ac3c450276cd
Gordon Smith <GordonJSmith@gmail.com>
2025-10-17 13:34:39 +0100

    feat: add lexilla (scintilla lexer)
    fixes #39058
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- ports/lexilla/portfile.cmake
+++ ports/lexilla/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-  URLS "https://www.scintilla.org/scintilla556.zip"
-  FILENAME "scintilla556.zip"
-  SHA512 8e845a94379fff88222fa9e4e5f534f62595420dd933166e4d9cc67b197c79f578405cb020892142ead1afd85bd42f1dc4361a339134a087e14760ff33d0a1cf
+  URLS "https://www.scintilla.org/lexilla545.zip"
+  FILENAME "lexilla545.zip"
+  SHA512 03e590a883e31135abc7eccdd089fbe3fe074955db70cbd546b58f32a77109f252c2283519e43f6a6e4c69fae9a99912c2bd828a771ceebeabf67655dde45877
 )
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
@@ -12,6 +12,8 @@ if(VCPKG_CRT_LINKAGE STREQUAL "static")
   list(APPEND PATCHES 0002-static-crt.patch)
 endif()
 
+list(APPEND PATCHES 0003-fix-include-path.patch)
+
 vcpkg_extract_source_archive(
   SOURCE_PATH
   ARCHIVE ${ARCHIVE}
@@ -21,8 +23,9 @@ vcpkg_extract_source_archive(
 
 vcpkg_install_msbuild(
   SOURCE_PATH "${SOURCE_PATH}"
-  PROJECT_SUBPATH Win32/Scintilla.vcxproj
+  PROJECT_SUBPATH src/Lexilla.vcxproj
 )
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.txt")
 file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}" FILES_MATCHING PATTERN "*.*")
+file(INSTALL "${SOURCE_PATH}/lexlib/" DESTINATION "${CURRENT_PACKAGES_DIR}/include/${PORT}/lexlib" FILES_MATCHING PATTERN "*.h")
