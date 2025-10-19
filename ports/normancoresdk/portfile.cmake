1906f05b458d3028364e339079a9d1b8214ccffc
avremy-nai <avremy.back@norman-ai.com>
2025-10-19 13:16:44 +0300

    changed sha due to package changes

--- ports/normancoresdk/portfile.cmake
+++ ports/normancoresdk/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO norman-ml/norman_core_sdk_cpp
     REF v${VERSION}  
-    SHA512 b54cfffeca6117f19d328a4538fd6c8b1936089bc86735b4a7522e8721ca7ff9a191dd6513a562530e135fe95dadb687d598e76315af30d7d1847d4bdbb0ed57
+    SHA512 c2d0350c4b29eec65ed501335bd3e912a7e8ad3eddcf3c91ccbdad27275df680995406ce3f173521a08622b4ec6fc63ce10042153ac6ba315684b368caecfd11
     HEAD_REF main
 )
 
