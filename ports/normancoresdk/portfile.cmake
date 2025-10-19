77ef328eac9adf245ec5267245467b89f47c0942
avremy-nai <avremy.back@norman-ai.com>
2025-10-19 12:13:57 +0300

    changed version and applied suggestion

--- ports/normancoresdk/portfile.cmake
+++ ports/normancoresdk/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO norman-ml/norman_core_sdk_cpp
-    REF v0.0.1
+    REF v${VERSION}  
     SHA512 b54cfffeca6117f19d328a4538fd6c8b1936089bc86735b4a7522e8721ca7ff9a191dd6513a562530e135fe95dadb687d598e76315af30d7d1847d4bdbb0ed57
     HEAD_REF main
 )
