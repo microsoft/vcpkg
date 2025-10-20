c507a394b063632677b67ad9bd1fb6b7ef33b89a
hanjingo <hehehunanchina@live.com>
2025-10-20 14:14:17 +0800

    ports/high-jump: add port v1.0.3, normalize files and remove markup fences

--- ports/high-jump/portfile.cmake
+++ ports/high-jump/portfile.cmake
@@ -1,13 +1,12 @@
+﻿set(PORT "high-jump")
+set(VERSION "1.0.3")
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/hanjingo/high-jump/releases/download/v1.0.3/high-jump-source-v1.0.3.tar.gz"
-    FILENAME "high-jump-source-v1.0.3.tar.gz"
-    SHA512 584886e5d8e574f6eaaed00bf9d8a3d4fef9fb6c55b8e94d7d04b8418da66156756471dbe64eeadde5e856964b91f53d78418fce3c6852d9799a7acca1745519
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    REPO hanjingo/high-jump
+    REF v${VERSION}
+    ARCHIVE_NAME high-jump-source-v${VERSION}.tar.gz
+    SHA512 584886e5d8e574f6eaaed00bf9d8a3d4fef9fb6c55b8e94d7d04b8418da66156756471dbe64eeadde5e856964b91f53d78418fce3c6852d9799a7acca1745519
 )
 
 vcpkg_cmake_configure(
@@ -35,4 +34,3 @@ endif()
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
