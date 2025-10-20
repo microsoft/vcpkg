eb4635b9da4e3f5588f72d0301f48f6c74b368fb
Saikari <lin@sz.cn.eu.org>
2025-10-20 11:04:39 +0300

    test

--- ports/tdlib/portfile.cmake
+++ ports/tdlib/portfile.cmake
@@ -30,5 +30,6 @@ vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/Td")
 vcpkg_copy_pdbs()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")
