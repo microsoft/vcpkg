7055c2bc75a92f99ef5d47b9b948e345c83f53f7
Nick D'Ademo <nickdademo@gmail.com>
2025-10-21 14:38:37 +0800

    fix config fixup

--- ports/mimicpp/portfile.cmake
+++ ports/mimicpp/portfile.cmake
@@ -19,7 +19,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/mimipp/cmake)
+vcpkg_cmake_config_fixup(CONFIG_PATH cmake/mimicpp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
