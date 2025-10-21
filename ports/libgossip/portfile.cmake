4bc994d8a24d5ec8868838c08459c644efe48af5
toge <toge.mail@gmail.com>
2025-10-21 23:35:13 +0900

    revert drop support msvc shared build

--- ports/libgossip/portfile.cmake
+++ ports/libgossip/portfile.cmake
@@ -1,3 +1,7 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # DLL broken in 1.1.2.0
+endif()
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO caomengxuan666/libgossip
