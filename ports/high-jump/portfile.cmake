525c5d6c265c5a3c6ad73af8dc9eebba3b8626b9
hanjingo <hehehunanchina@live.com>
2025-10-23 18:44:52 +0800

    ports/high-jump: update REPO to hanjingo/hanjingo-high-jump

--- ports/high-jump/portfile.cmake
+++ ports/high-jump/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO hanjingo/high-jump
+    REPO hanjingo/hanjingo-high-jump
     REF v${VERSION}
     ARCHIVE_NAME high-jump-source-v${VERSION}.tar.gz
     SHA512 de21d3e9f8005164362257d3ff6503e3aa7b9fb96959a3e95d6e7593604b60435d11d1c7e2b4035709bbd0ec5aa9056789f3f934d14f60fbc76bfb4fcb9da379
