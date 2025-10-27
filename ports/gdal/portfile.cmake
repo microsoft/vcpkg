cf1490f51190945a70666eaeeb6f63482da080b5
Kai Pastor <dg0yt@darc.de>
2025-10-27 20:56:08 +0100

    [arrow,gdal,arrow-adbc] Test and fix integration (#47958)
    Co-authored-by: Matthias Kuhn <matthias@opengis.ch>

--- ports/gdal/portfile.cmake
+++ ports/gdal/portfile.cmake
@@ -5,8 +5,10 @@ vcpkg_from_github(
     SHA512 84a9bd58e9992d2d447788727228410184ef31e881026aee1f48766ed8b25039ab1b09afe95c97b66d3a0751bab524dc9bb57ab2c78af53632b52ec8dcd6f4ad
     HEAD_REF master
     PATCHES
+        adopt-compile-only.diff
         find-link-libraries.patch
         fix-gdal-target-interfaces.patch
+        generate-config.diff
         libkml.patch
         sqlite3.diff
         target-is-valid.patch
@@ -24,6 +26,7 @@ vcpkg_replace_string("${SOURCE_PATH}/ogr/ogrsf_frmts/flatgeobuf/flatbuffers/base
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
         arrow            GDAL_USE_ARROW
+        arrow-adbc       GDAL_USE_ADBCDRIVERMANAGER
         archive          GDAL_USE_ARCHIVE
         cfitsio          GDAL_USE_CFITSIO
         curl             GDAL_USE_CURL
