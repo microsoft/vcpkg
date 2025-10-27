dcc19ec361ff2b2139e0fd6b5eb2917bd21dae5a
Dzmitry Baryshau <dima8w@gmail.com>
2025-10-27 22:57:23 +0200

    [sail] Comment sorting

--- ports/sail/portfile.cmake
+++ ports/sail/portfile.cmake
@@ -12,6 +12,7 @@ vcpkg_from_github(
 # Enable selected codecs
 set(ONLY_CODECS "")
 
+# The list is sorted by priority, with each priority group separated by triple space
 foreach(CODEC gif jpeg png svg webp   avif ico   heif openexr psd tiff   bmp hdr jpeg2000 jpegxl pnm qoi tga   jbig pcx wal xbm xpm xwd)
     if (${CODEC} IN_LIST FEATURES)
         list(APPEND ONLY_CODECS ${CODEC})
