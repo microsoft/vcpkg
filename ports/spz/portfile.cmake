c2caf857fa030c7e6fda5e9f4a445b7c7d6b6a23
Kevin Ring <kevin@kotachrome.com>
2025-10-28 18:16:18 +1100

    Remove unnecessary quotes.
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/spz/portfile.cmake
+++ ports/spz/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_github(
     SHA512 0e6bd1dd3f8625cc6730c0cc3a53f65a414a0504c463ca108ac972e4f09e949c49fd98d1033e27947080ead573695747c2a0b9c1a3d8aac7a39351abeb70bfc6
     HEAD_REF main
     PATCHES
-      "optional-build-tools.patch"
+        optional-build-tools.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
