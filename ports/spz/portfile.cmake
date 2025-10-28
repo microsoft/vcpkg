bad70adc2f7f23e41e1956e8d27f6225a1b519f1
Kevin Ring <kevin@kotachrome.com>
2025-10-28 17:04:47 +1100

    spz tools as a separate vcpkg feature.

--- ports/spz/portfile.cmake
+++ ports/spz/portfile.cmake
@@ -8,15 +8,38 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 0e6bd1dd3f8625cc6730c0cc3a53f65a414a0504c463ca108ac972e4f09e949c49fd98d1033e27947080ead573695747c2a0b9c1a3d8aac7a39351abeb70bfc6
     HEAD_REF main
+    PATCHES
+      "optional-build-tools.patch"
 )
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools   BUILD_TOOLS
+)
+
+if(tools IN_LIST FEATURES)
+    # Tools targets are missing C++17 flag
+    list(APPEND FEATURE_OPTIONS "-DCMAKE_CXX_STANDARD=17")
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCMAKE_CXX_STANDARD=17
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
+
+if(tools IN_LIST FEATURES)
+    vcpkg_copy_tools(
+        TOOL_NAMES
+            ply_to_spz
+            spz_to_ply
+            spz_info
+        AUTO_CLEAN
+    )
+endif()
+
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/spz")
 
