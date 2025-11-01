d0a9754d4f77c2d8ea066b5d2f59ea5932974845
toge <toge.mail@gmail.com>
2025-11-02 01:44:09 +0900

    [aws-c-cal] update to 0.9.8

--- ports/aws-c-cal/portfile.cmake
+++ ports/aws-c-cal/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO awslabs/aws-c-cal
     REF "v${VERSION}"
-    SHA512 75581decf739351cc566bc645a6c5bd26ac576dcee4568ca5c74f5f18417d329715837f5393362a109a80f04c945ea2d90503ea791dccadb13ad6d1904a4b39e
+    SHA512 e5fa5299164f4ce7860ada9a1c56ba74d6f68ca82cb1e09c53098f7642cb3541efe2c394fad2ff77716f795f813d87dff3ccd6df582b49a6fdca785531150d7a
     HEAD_REF master
     PATCHES remove-libcrypto-messages.patch
 )
