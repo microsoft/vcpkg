9218200407fd522907c6781109e6b06014e36471
Kai Pastor <dg0yt@darc.de>
2025-11-02 16:30:24 +0100

    Fix wasm

--- ports/icu/portfile.cmake
+++ ports/icu/portfile.cmake
@@ -24,7 +24,7 @@ vcpkg_list(SET CONFIGURE_OPTIONS)
 vcpkg_list(SET BUILD_OPTIONS)
 
 if(VCPKG_TARGET_IS_EMSCRIPTEN)
-    vcpkg_list(APPEND CONFIGURE_OPTIONS --disable-extras)
+    vcpkg_list(APPEND CONFIGURE_OPTIONS --disable-extras icu_cv_host_frag=mh-linux)
     vcpkg_list(APPEND BUILD_OPTIONS "\"PKGDATA_OPTS=--without-assembly -O ../data/icupkg.inc\"")
 elseif(VCPKG_TARGET_IS_UWP)
     vcpkg_list(APPEND CONFIGURE_OPTIONS --disable-extras ac_cv_func_tzset=no ac_cv_func__tzset=no)
