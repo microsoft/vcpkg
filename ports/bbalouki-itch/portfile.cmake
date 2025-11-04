1235c127173c0a08f1beaa9f9d4d55b8ab297018
bbalouki <bbalouki@outlook.com>
2025-11-04 12:06:13 +0000

    Add itch library to vcpkg

---
+++ ports/bbalouki-itch/portfile.cmake
@@ -0,0 +1,35 @@
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO bbalouki/itchcpp
+    REF "v${VERSION}"
+    SHA512 8f277f28b9fe9f455502e995c43702a1cfc454aa759e7d7f22091e8d424c6bfeae22769f8d6c20d336aa8dfca99b1148570ff17a4a9d08ade5e5b141fbee0cc3
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DITCH_BUILD_TESTS=OFF
+        -DITCH_BUILD_BENCHMARKS=OFF
+        -DITCH_BUILD_EXAMPLES=OFF
+        -DITCH_PROJECT_ENV=PROD
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME "itch"
+    CONFIG_PATH "lib/cmake/itch"
+   
+)
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
