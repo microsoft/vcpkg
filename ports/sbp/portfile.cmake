72b779495e7183f90c710721bd730ad1fc2e04e3
Ethan J. Musser <emusser@carnegierobotics.com>
2025-11-05 16:55:45 -0500

    Add a `usage` file

--- ports/sbp/portfile.cmake
+++ ports/sbp/portfile.cmake
@@ -36,4 +36,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
