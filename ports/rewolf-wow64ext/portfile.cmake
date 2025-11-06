995c25f501b2ce16a40d6cbf5681dbe079a07601
Saikari <lin@sz.cn.eu.org>
2025-11-06 05:32:57 +0300

    fix: add configuration parameters to vcpkg_cmake_config_fixup for wow64ext

--- ports/rewolf-wow64ext/portfile.cmake
+++ ports/rewolf-wow64ext/portfile.cmake
@@ -16,7 +16,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/wow64ext" PACKAGE_NAME "wow64ext")
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
