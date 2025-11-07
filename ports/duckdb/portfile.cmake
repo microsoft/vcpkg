cbeac9ea8bf15239e7631d3e2d7c6d31e91add0b
Kai Pastor <dg0yt@darc.de>
2025-11-07 22:20:33 +0100

    [duckdb] Do not remove bin [skip actions]

--- ports/duckdb/portfile.cmake
+++ ports/duckdb/portfile.cmake
@@ -1,3 +1,5 @@
+set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled) # FIXME
+
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO duckdb/duckdb
@@ -87,10 +89,6 @@ elseif(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/DuckDB")
     vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/DuckDB")
 elseif(EXISTS "${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}")
     vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/${PORT}")
-endif()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(REMOVE_RECURSE
