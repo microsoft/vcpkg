f4d7a18540167f3e834cc11c7a957e1e7591dec9
deadlightreal <richardfabianmain@gmail.com>
2025-11-07 23:40:08 +0100

    should work finally.

--- ports/deadlightreal-swiftnet/portfile.cmake
+++ ports/deadlightreal-swiftnet/portfile.cmake
@@ -2,14 +2,15 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO deadlightreal/SwiftNet
     REF 1.0.0
-    SHA512 667c51d60c16a47b799555740e9b8578a626af77c50745e0f3704779b70eb99338dabfb78e29b9b3e805e22348048b9887ea3a83d3b0591c87c37a21a495dcd0
+    SHA512 5e13a0ecf0a6a8a58a3f2e8d570ae3668517ad83a9bf3494170238456f0eedcbcba84ce4b366c0961ce5b0bbb97bea489dcad83d043abbe50e4ec6694bb9dcb0
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}/src
-    OPTIONS -DBUILD_TESTING=OFF
 )
 
 vcpkg_cmake_install()
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
