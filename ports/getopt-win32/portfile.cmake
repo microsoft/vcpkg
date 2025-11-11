834697388d9a5526627eff1516b77729b27c342b
PaperChalice <liujunchang97@outlook.com>
2025-11-11 17:55:31 +0800

    [getopt-win32] update to 2.42

--- ports/getopt-win32/portfile.cmake
+++ ports/getopt-win32/portfile.cmake
@@ -1,18 +1,25 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO xiaozhuai/getopt-win32
-    REF b69a586f0b1aa37b77c3cf0a9dedba1900007678
-    SHA512 6e52b6f198073e85b3a29a2fe21435c591fce02c68cb8cf9b546791dd1197e9b91dd9104af449071b4a1d1485817a483f1989a2e585c2d0470cfd079290fe155
-    HEAD_REF master
+    REPO ludvikjerabek/getopt-win
+    REF v${VERSION}
+    SHA512 9ca4e7ed7a1fe7bad9d9ef91b5e65c18a716f4c999818e3dd4f644fc861e1ae12e64255c27f12c0df3b1e44757d3d36c068682dd86d184c6f957b2cabda7bbf3
+    HEAD_REF getopt_glibc_2.42_port
 )
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  set(lib_type_options "-DBUILD_STATIC_LIBS=OFF")
+else ()
+  set(lib_type_options "-DBUILD_STATIC_LIBS=ON" "-DBUILD_SHARED_LIBS=OFF")
+endif ()
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    ${lib_type_options}
+    -DBUILD_TESTING=OFF
+)
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-
-file(COPY "${SOURCE_PATH}/getopt.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     vcpkg_replace_string(
@@ -23,11 +30,11 @@ if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
 endif()
 
 vcpkg_cmake_config_fixup(
-    CONFIG_PATH  "share/unofficial-getopt-win32"
-    PACKAGE_NAME "unofficial-getopt-win32"
+    CONFIG_PATH  "lib/cmake/getopt"
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
