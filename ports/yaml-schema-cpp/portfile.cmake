add0b335383962c8ceea4a311d2fb2de656d09eb
toge <toge.mail@gmail.com>
2025-11-13 04:39:22 +0900

    remove _YAML_SCHEMA_CPP_ROOT_DIR for test codes

--- ports/yaml-schema-cpp/portfile.cmake
+++ ports/yaml-schema-cpp/portfile.cmake
@@ -6,12 +6,14 @@ vcpkg_from_github(
     REF 24623cc5c4a5acb63dea9581ebcd84601bac1da1
     SHA512 d7622401ebce65b107eb363af88868fef4725c50ce0aa659a4510223275a3bc6b9d1fc1c71946247ea0142f7bc460f39c02f91e3c6466fdadacb9ec2d4f7fe38
     HEAD_REF main
+    PATCHES
+        remove-root-dir-for-tests.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DBUILD_TESTS=OFF
+        -DBUILD_TESTS=OFFq
 )
 vcpkg_cmake_install()
 
