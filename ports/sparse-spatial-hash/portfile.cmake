7b43973f72e7fe5cc74a85aeaf717b5f276542ce
Alex Towell <queelius@gmail.com>
2025-11-12 08:00:27 -0600

    Update ports/sparse-spatial-hash/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/sparse-spatial-hash/portfile.cmake
+++ ports/sparse-spatial-hash/portfile.cmake
@@ -17,7 +17,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sparse_spatial_hash)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sparse_spatial_hash PACKAGE_NAME sparse_spatial_hash)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
