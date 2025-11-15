946f25a031a7dabe8df7acd8501c849244ab0c82
Billy Robert O'Neal III <bion@microsoft.com>
2025-11-14 19:35:25 -0800

    A few nitpicks

--- ports/deadlightreal-swiftnet/portfile.cmake
+++ ports/deadlightreal-swiftnet/portfile.cmake
@@ -1,12 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO deadlightreal/SwiftNet
-    REF 0.1.0
+    REF "${VERSION}"
     SHA512 5248ddd2486d5892952c72a3c93fc7dec0320df1cdc9ae13cddcb7e57aa709c5bdbd5fa791d59e2b54c749a58b9f4ab6a9b912bd31193aa510eca0762b60ce4d
 )
 
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}/src
+    SOURCE_PATH "${SOURCE_PATH}/src"
 )
 
 vcpkg_cmake_install()
