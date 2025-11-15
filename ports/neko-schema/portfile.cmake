360fefd3eeb9d9e83d5261b895a92ceafce4819d
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-16 04:25:19 +0900

    [neko-schema] Remove empty dir

--- ports/neko-schema/portfile.cmake
+++ ports/neko-schema/portfile.cmake
@@ -19,6 +19,8 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NekoSchema PACKAGE_NAME nekoschema)
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
