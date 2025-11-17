413c87a5432576d84e7856ca88bdd297bb7f422c
Saikari <lin@sz.cn.eu.org>
2025-11-17 19:53:02 +0300

    try commit before c++17

--- ports/asmjit/portfile.cmake
+++ ports/asmjit/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO asmjit/asmjit
-    REF 356dddbc5508dd65f466098da26a2e47584eafdb # commited on 2025-05-24
+    REF cecc73f2979e9704c81a2c2ec79a7475b31c56ac # commited on 2025-05-10
     SHA512 86f11483320162cb08279eab5d00b82494cf22a941aeee5c2b370d3112377f0066d890e5dd4ff3947be295a96b2f89a015fbafc82ba795ab7e08af3e1e0f0db0
     HEAD_REF master
 )
