c95842466acc3b5ffb9e87497270d341bbdb1926
cDc <cdc.seacave@gmail.com>
2025-11-17 23:15:51 +0200

    [tinyexif] Update to 1.0.4

--- ports/tinyexif/portfile.cmake
+++ ports/tinyexif/portfile.cmake
@@ -3,15 +3,20 @@ vcpkg_from_github(
     REPO cdcseacave/TinyEXIF
     REF ${VERSION}
     SHA512 1285566c70f4de3c882a433d65595f18d848ecf8e9b16e1ea3aa7a1773fb70ba090c7cc726238132cccfc403c3750950175c675d25206be38cddb64f16193795
-    HEAD_REF master
+    HEAD_REF 1.0.4
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+if(BUILD_STATIC_LIBS)
+    set(_BUILD_SHARED_LIBS OFF)
+else()
+    set(_BUILD_SHARED_LIBS ON)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
+        -DBUILD_SHARED_LIBS=${_BUILD_SHARED_LIBS}
         -DBUILD_DEMO=OFF
 )
 
@@ -23,11 +28,5 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(READ "${CURRENT_PACKAGES_DIR}/share/tinyexif/TinyEXIFConfig.cmake" _contents)
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/tinyexif/TinyEXIFConfig.cmake" "
-include(CMakeFindDependencyMacro)
-find_dependency(tinyxml2)
-${_contents}")
-
 # Handle copyright
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/README.md")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
