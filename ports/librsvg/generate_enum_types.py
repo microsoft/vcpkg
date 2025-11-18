28250e4ddc85d8aca481feb64eeaeb403f6e0541
Andrew Kaster <andrew@ladybird.org>
2025-11-18 16:07:23 -0700

    librsvg no abspaths

--- ports/librsvg/generate_enum_types.py
+++ ports/librsvg/generate_enum_types.py
@@ -19,18 +19,18 @@ def main():
         sys.executable,
         args.glib_mkenums,
         "--fhead",
-        R"#if !defined (__RSVG_RSVG_H_INSIDE__) && !defined (RSVG_COMPILATION)\n#warning \"Including <librsvg/librsvg-enum-types.h> directly is deprecated.\"\n#endif\n\n#ifndef __LIBRSVG_ENUM_TYPES_H__\n#define __LIBRSVG_ENUM_TYPES_H__\n\n#include <glib-object.h>\n\nG_BEGIN_DECLS\n",
+        R'#if !defined (__RSVG_RSVG_H_INSIDE__) && !defined (RSVG_COMPILATION)\n#warning \"Including <librsvg/librsvg-enum-types.h> directly is deprecated.\"\n#endif\n\n#ifndef __LIBRSVG_ENUM_TYPES_H__\n#define __LIBRSVG_ENUM_TYPES_H__\n\n#include <glib-object.h>\n\nG_BEGIN_DECLS\n',
         "--fprod",
-        R"/* enumerations from \"@filename@\" */\n",
+        R'/* enumerations from \"@filename@\" */\n',
         "--vhead",
-        R"GType @enum_name@_get_type (void);\n#define RSVG_TYPE_@ENUMSHORT@ (@enum_name@_get_type())\n",
+        R'GType @enum_name@_get_type (void);\n#define RSVG_TYPE_@ENUMSHORT@ (@enum_name@_get_type())\n',
         "--ftail",
-        R"G_END_DECLS\n\n#endif /* __LIBRSVG_ENUM_TYPES_H__ */",
+        R'G_END_DECLS\n\n#endif /* __LIBRSVG_ENUM_TYPES_H__ */',
         "--output",
-        output_dir / "librsvg-enum-types.h"
+        "librsvg-enum-types.h"
     ] + args.inputs
 
-    subprocess.run(header_arguments, check=True)
+    subprocess.run(header_arguments, cwd=output_dir, check=True)
 
     source_arguments = [
         sys.executable,
@@ -46,10 +46,10 @@ def main():
         "--vtail",
         R'      { 0, NULL, NULL }\n    };\n    etype = g_@type@_register_static ("@EnumName@", values);\n  }\n  return etype;\n}\n ',
         "--output",
-        output_dir / "librsvg-enum-types.c"
+        "librsvg-enum-types.c"
     ] + args.inputs
 
-    subprocess.run(source_arguments, check=True)
+    subprocess.run(source_arguments, cwd=output_dir, check=True)
 
     return 0
 
