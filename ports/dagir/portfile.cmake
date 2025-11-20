a54023c1c3f6a9af0e7c550dcb5d2bfbe7333f4c
Victor Romero <romerosanchezv@gmail.com>
2025-11-20 10:47:08 -0800

    Fix call to vcpkg_cmake_config_fixup and update version db

--- ports/dagir/portfile.cmake
+++ ports/dagir/portfile.cmake
@@ -1,24 +1,24 @@
+set(VCPKG_BUILD_TYPE release) # header-only library
+
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO Alan-Jowett/dagir
   REF 0.1.0
-  SHA512 0450B03C282DAA9B941A56283CCC00663C8EB66C9D02BDAE05D2EA5DD60C4048A30BA4B4D3F51FE51D7A7F43132D48989140FC02D088522A2177FF779C204ED3
+  SHA512 0450b03c282daa9b941a56283ccc00663c8eb66c9d02bdae05d2ea5dd60c4048a30ba4b4d3f51fe51d7a7f43132d48989140fc02d088522a2177ff779c204ed3
 )
-set(VCPKG_POLICY_SKIP_MISPLACED_CMAKE_FILES_CHECK enabled)
-set(VCPKG_POLICY_SKIP_LIB_CMAKE_MERGE_CHECK enabled)
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DDAGIR_BUILD_TESTS=OFF -DDAGIR_EXAMPLES=OFF)
+
+vcpkg_cmake_configure(
+  SOURCE_PATH ${SOURCE_PATH} 
+  OPTIONS 
+    -DDAGIR_BUILD_TESTS=OFF 
+    -DDAGIR_EXAMPLES=OFF
+)
+
 vcpkg_cmake_install()
-if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
-  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-endif()
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/share/dagir")
-vcpkg_cmake_config_fixup()
 
-file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/dagir")
-if(EXISTS "${SOURCE_PATH}/LICENSE")
-  vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-endif()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/DagIR")
 
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRTargets.cmake" "add_library(dagir::dagir INTERFACE IMPORTED)\nset_target_properties(dagir::dagir PROPERTIES INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/../../include\")\n")
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRConfig.cmake" "include(\"\${CMAKE_CURRENT_LIST_DIR}/DagIRTargets.cmake\")\n")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
