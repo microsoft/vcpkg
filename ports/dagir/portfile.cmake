1975f7602fe78f519151e84ab6ba9b5e1b089857
Alan Jowett <alan.jowett@microsoft.com>
2025-11-20 08:50:26 -0800

    ports/dagir: minimal header-only portfile — fetch, configure (tests/examples off), install, fixup, relocatable CMake config
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- ports/dagir/portfile.cmake
+++ ports/dagir/portfile.cmake
@@ -5,27 +5,15 @@ vcpkg_from_github(
   SHA512 0450B03C282DAA9B941A56283CCC00663C8EB66C9D02BDAE05D2EA5DD60C4048A30BA4B4D3F51FE51D7A7F43132D48989140FC02D088522A2177FF779C204ED3
 )
 
-vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-    -DDAGIR_BUILD_TESTS=OFF
-    -DDAGIR_EXAMPLES=OFF
-)
-
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DDAGIR_BUILD_TESTS=OFF -DDAGIR_EXAMPLES=OFF)
 vcpkg_cmake_install()
-
 file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/debug/share/dagir")
 vcpkg_cmake_config_fixup()
 
+file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/share/dagir")
 if(EXISTS "${SOURCE_PATH}/LICENSE")
   vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 endif()
 
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRTargets.cmake"
-  "add_library(dagir::dagir INTERFACE IMPORTED)\n"
-  "set_target_properties(dagir::dagir PROPERTIES INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/../../include\")\n"
-)
-
-file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRConfig.cmake"
-  "include(\"\${CMAKE_CURRENT_LIST_DIR}/DagIRTargets.cmake\")\n"
-)
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRTargets.cmake" "add_library(dagir::dagir INTERFACE IMPORTED)\nset_target_properties(dagir::dagir PROPERTIES INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/../../include\")\n")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/dagir/DagIRConfig.cmake" "include(\"\${CMAKE_CURRENT_LIST_DIR}/DagIRTargets.cmake\")\n")
