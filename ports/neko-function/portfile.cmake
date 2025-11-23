78897a05bc63b4c8c22f200bcdd26c65c53affe2
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-24 06:22:06 +0900

    [neko-function] Add new port

---
+++ ports/neko-function/portfile.cmake
@@ -0,0 +1,37 @@
+﻿vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO moehoshio/NekoFunction
+    REF v1.0.1
+    SHA512 d58df376945168844e0769e22590e312390a0e79a19dde760dfadb3622386901073ef98c9d550dbdb2594ffefd26f43930871e7b24d750e2fbe65d3521356718
+    HEAD_REF main
+)
+
+set(VCPKG_BUILD_TYPE release)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        archive  NEKO_FUNCTION_ENABLE_ARCHIVE
+        hash     NEKO_FUNCTION_ENABLE_HASH
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
+        -DNEKO_FUNCTION_BUILD_TESTS=OFF
+        -DNEKO_FUNCTION_AUTO_FETCH_DEPS=OFF
+        -DNEKO_FUNCTION_ENABLE_MODULE=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NekoFunction PACKAGE_NAME nekofunction)
+
+# Only remove lib directory if both archive and hash features are disabled (header-only mode)
+if(NOT "archive" IN_LIST FEATURES AND NOT "hash" IN_LIST FEATURES)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
