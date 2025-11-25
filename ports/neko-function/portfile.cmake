55eaf3f8a460ac4caef40a6154a136860da5f99e
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-26 06:31:22 +0900

    [neko-function] Updated version to v1.0.9 and improved the header-only detection logic in the port file.

--- ports/neko-function/portfile.cmake
+++ ports/neko-function/portfile.cmake
@@ -1,8 +1,8 @@
 ﻿vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO moehoshio/NekoFunction
-    REF v1.0.7
-    SHA512 afe897309bf9c793268b34c7410fa986c5f123a1440dc6542c493c0c763642697d387df2f917c2f1dad9eb862ad03da0348e76f324289466dbcb3ff01cece81b
+    REF v1.0.9
+    SHA512 00038529a7c1934304e77d9833494459c322e1f02812dbe469444c1a0afd65f8ac079b2929453bf3b2e8a515966aae9c30fd22d885a8eb170c25116b08c3f9e2
     HEAD_REF main
 )
 
@@ -12,16 +12,13 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         hash     NEKO_FUNCTION_ENABLE_HASH
 )
 
-function(neko_function_is_header_only OUT_IS_HEADER_ONLY)
-    if(NOT "archive" IN_LIST FEATURES AND NOT "hash" IN_LIST FEATURES)
-        set(${OUT_IS_HEADER_ONLY} TRUE PARENT_SCOPE)
-    else()
-        set(${OUT_IS_HEADER_ONLY} FALSE PARENT_SCOPE)
-    endif()
-endfunction()
+if("archive" IN_LIST FEATURES)
+    set(NEKO_FUNCTION_IS_HEADER_ONLY FALSE)
+else()
+    set(NEKO_FUNCTION_IS_HEADER_ONLY TRUE)
+endif()
 
-neko_function_is_header_only(IS_HEADER_ONLY)
-if(IS_HEADER_ONLY)
+if(NEKO_FUNCTION_IS_HEADER_ONLY)
     set(VCPKG_BUILD_TYPE release)
 endif()
 
@@ -39,8 +36,7 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NekoFunction PACKAGE_NAME nekofun
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-neko_function_is_header_only(IS_HEADER_ONLY)
-if(IS_HEADER_ONLY)
+if(NEKO_FUNCTION_IS_HEADER_ONLY)
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 endif()
 
