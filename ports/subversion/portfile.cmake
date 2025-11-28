6ca806ab0f13f5f82ea3cdef3dd6a69027024955
Cappecasper03 <03repsac@gmail.com>
2025-11-28 17:10:55 +0100

    Fixed portfile.cmake

--- ports/subversion/portfile.cmake
+++ ports/subversion/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
         fix-expat-regex.patch
         fix-expat-libname.patch
 )
-    
+
 if(VERSION VERSION_GREATER_EQUAL "1.15.0")
     set(USE_CMAKE_BUILD TRUE)
 else()
@@ -25,7 +25,7 @@ if(USE_CMAKE_BUILD)
         WORKING_DIRECTORY "${SOURCE_PATH}"
         LOGNAME "gen-make-${TARGET_TRIPLET}"
     )
-    
+
     set(CMAKE_OPTIONS
         -DSVN_ENABLE_PROGRAMS=OFF
         -DSVN_ENABLE_TESTS=OFF
@@ -33,34 +33,27 @@ if(USE_CMAKE_BUILD)
         -DSVN_ENABLE_RA_SVN=ON
         -DSVN_ENABLE_RA_LOCAL=ON
     )
-    
+
     vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
         OPTIONS ${CMAKE_OPTIONS}
     )
-    
+
     vcpkg_cmake_install()
     vcpkg_cmake_fixup(CONFIG_PATH lib/cmake/subversion)
     vcpkg_copy_pdbs()
-    
-    if(ENABLE_TOOLS)
-        vcpkg_copy_tools(
-            TOOL_NAMES svn svnadmin svnlook svnsync svnrdump svnmucc svnserve
-            AUTO_CLEAN
-    vcpkg_cmake_install()
-    vcpkg_cmake_fixup(CONFIG_PATH lib/cmake/subversion)
-    vcpkg_copy_pdbs()
-    
-elseif(VCPKG_TARGET_IS_WINDOWS)")
+
+elseif(VCPKG_TARGET_IS_WINDOWS)
+    if(VCPKG_PLATFORM_TOOLSET MATCHES "v143")
+        set(VSNET_VERSION "2022")
+    elseif(VCPKG_PLATFORM_TOOLSET MATCHES "v142")
+        set(VSNET_VERSION "2019")
+    elseif(VCPKG_PLATFORM_TOOLSET MATCHES "v141")
+        set(VSNET_VERSION "2017")
+    else()
+        set(VSNET_VERSION "2022")
     endif()
-    
-    set(GEN_MAKE_ARGS
-        "-t" "vcproj"
-        "--vsnet-version=${VSNET_VERSION}"
-        "--with-apr=${CURRENT_INSTALLED_DIR}"
-        "--with-apr-util=${CURRENT_INSTALLED_DIR}"
-        "--with-zlib=${CURRENT_INSTALLED_DIR}"
-        "--with-openssl=${CURRENT_INSTALLED_DIR}"
+
     set(GEN_MAKE_ARGS
         "-t" "vcproj"
         "--vsnet-version=${VSNET_VERSION}"
@@ -72,7 +65,14 @@ elseif(VCPKG_TARGET_IS_WINDOWS)")
         "--with-sqlite=${CURRENT_INSTALLED_DIR}"
     )
 
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")KE_ARGS}
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+        list(APPEND GEN_MAKE_ARGS "--disable-shared")
+        list(APPEND GEN_MAKE_ARGS "--with-static-apr")
+        list(APPEND GEN_MAKE_ARGS "--with-static-openssl")
+    endif()
+
+    vcpkg_execute_required_process(
+        COMMAND ${PYTHON3} gen-make.py ${GEN_MAKE_ARGS}
         WORKING_DIRECTORY "${SOURCE_PATH}"
         LOGNAME "gen-make-${TARGET_TRIPLET}"
     )
@@ -95,7 +95,7 @@ elseif(VCPKG_TARGET_IS_WINDOWS)")
         file(GLOB RELEASE_LIBS "${SOURCE_PATH}/Release/subversion/libsvn_*/*.lib")
         list(FILTER RELEASE_LIBS EXCLUDE REGEX "libsvn_test")
         file(INSTALL ${RELEASE_LIBS} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-        
+
         if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
             file(GLOB RELEASE_DLLS "${SOURCE_PATH}/Release/subversion/libsvn_*/*.dll")
             file(INSTALL ${RELEASE_DLLS} DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
@@ -106,28 +106,14 @@ elseif(VCPKG_TARGET_IS_WINDOWS)")
         file(GLOB DEBUG_LIBS "${SOURCE_PATH}/Debug/subversion/libsvn_*/*.lib")
         list(FILTER DEBUG_LIBS EXCLUDE REGEX "libsvn_test")
         file(INSTALL ${DEBUG_LIBS} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
-        
+
         if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
             file(GLOB DEBUG_DLLS "${SOURCE_PATH}/Debug/subversion/libsvn_*/*.dll")
             file(INSTALL ${DEBUG_DLLS} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
         endif()
     endif()
 
-    if(ENABLE_TOOLS AND (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release"))
-        file(GLOB TOOLS "${SOURCE_PATH}/Release/subversion/svn/*.exe" 
-                        "${SOURCE_PATH}/Release/subversion/svnadmin/*.exe"
-                        "${SOURCE_PATH}/Release/subversion/svnserve/*.exe"
-                        "${SOURCE_PATH}/Release/subversion/svnlook/*.exe"
-                        "${SOURCE_PATH}/Release/subversion/svnsync/*.exe"
-                        "${SOURCE_PATH}/Release/subversion/svnrdump/*.exe"
-                        "${SOURCE_PATH}/Release/subversion/svnmucc/*.exe")
-        if(TOOLS)
-        endif()
-    endif()
-
 else()
-    if(ENABLE_BDB)
-        list(APPEND CONFIGURE_OPTIONS "--with-berkeley-db=${CURRENT_INSTALLED_DIR}")
     set(CONFIGURE_OPTIONS
         --with-apr=${CURRENT_INSTALLED_DIR}
         --with-apr-util=${CURRENT_INSTALLED_DIR}
@@ -143,6 +129,20 @@ else()
     )
 
     vcpkg_configure_make(
+        SOURCE_PATH "${SOURCE_PATH}"
+        AUTOCONFIG
+        OPTIONS
+            ${CONFIGURE_OPTIONS}
+    )
+
+    vcpkg_install_make()
+    vcpkg_fixup_pkgconfig()
+endif()
+
+file(
+    INSTALL "${CMAKE_CURRENT_LIST_DIR}/unofficial-subversion-config.cmake"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-subversion"
+)
 
 if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
