eef301d55b499a56d770bcde713e062dac300011
moehoshio <132435834+moehoshio@users.noreply.github.com>
2025-11-29 04:02:32 +0900

    [neko-network] Add new port

---
+++ ports/neko-network/portfile.cmake
@@ -0,0 +1,21 @@
+﻿vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO moehoshio/NekoNetwork
+    REF v1.0.0
+    SHA512 99660c39967c3bf98d3c184a477062be06df9a045c5fad36f0343fe71650dd9c73957953ba296f44952737cc32e6122d9549fb6f15dc0c08f6d9d6623407c646
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DNEKO_NETWORK_BUILD_TESTS=OFF
+        -DNEKO_NETWORK_AUTO_FETCH_DEPS=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/NekoNetwork PACKAGE_NAME nekonetwork)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
