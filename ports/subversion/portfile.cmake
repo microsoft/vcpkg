bacc86e3bc08c68517efe07f88e8eb76fe7ff9cc
Cappecasper03 <03repsac@gmail.com>
2025-11-28 15:44:06 +0100

    Added usage to portfile

--- ports/subversion/portfile.cmake
+++ ports/subversion/portfile.cmake
@@ -206,8 +206,9 @@ file(
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/unofficial-subversion"
 )
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
 if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
