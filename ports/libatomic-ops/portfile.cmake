7153a6bb53b312d7e7ef1c782dd6b98f7abd644d
Ivan Maidanski <ivmai@mail.ru>
2025-11-29 09:58:01 +0300

    [libatomic-ops] update repo/homepage URL, turn off tests compilation

--- ports/libatomic-ops/portfile.cmake
+++ ports/libatomic-ops/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO ivmai/libatomic_ops
+    REPO bdwgc/libatomic_ops
     REF "v${VERSION}"
     SHA512 3980e52faaef12fe5794389a88c985124b408e7c2051aae5966939ee1577cd0b7a9e807a373791086f38fb82a7dc2bd062ebbe8efc1124383060f78625fb99cc
     HEAD_REF master
@@ -9,6 +9,7 @@ vcpkg_from_github(
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DBUILD_TESTING=OFF
         -Denable_docs=OFF
     OPTIONS_DEBUG
         -Dinstall_headers=OFF
