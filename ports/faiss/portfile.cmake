1b77aab39d172071a8e61530cc7425fde62ae657
Kai Pastor <dg0yt@darc.de>
2025-11-30 10:20:12 +0100

    [faiss] Update to 1.13.0

--- ports/faiss/portfile.cmake
+++ ports/faiss/portfile.cmake
@@ -2,37 +2,35 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebookresearch/faiss
     REF "v${VERSION}"
-    SHA512 38d4215e3e019915d8b367ff0e8d14901b1495f6f45b835e9248276567a422b0370baab6bd887045442dd1e268b7fe7c347107162e66bb3ec6b1a53be4b2e441
+    SHA512 739641644a6a0b12430ab172fdd7c657b4c88e6389688c359919e2286bb494ff2011a33905719dc9dd95116c7c834f76969c457b67104223447ac04de339000d
     HEAD_REF master
-    PATCHES
-        fix-dependencies.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        gpu FAISS_ENABLE_GPU
+        gpu     FAISS_ENABLE_GPU
 )
 
-if ("${FAISS_ENABLE_GPU}")
-    if (NOT VCPKG_CMAKE_SYSTEM_NAME AND NOT ENV{CUDACXX})
-        set(ENV{CUDACXX} "$ENV{CUDA_PATH}/bin/nvcc.exe")
-    endif()
+if ("gpu" IN_LIST FEATURES)
+    vcpkg_find_cuda(OUT_CUDA_TOOLKIT_ROOT cuda_toolkit_root)
+    list(APPEND FEATURE_OPTIONS
+        "-DCMAKE_CUDA_COMPILER=${NVCC}"
+        "-DCUDAToolkit_ROOT=${cuda_toolkit_root}"
+    )
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DFAISS_ENABLE_MKL=OFF
         -DFAISS_ENABLE_PYTHON=OFF  # Requires SWIG
         -DBUILD_TESTING=OFF
 )
-
 vcpkg_cmake_install()
-
+vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup()
 
-vcpkg_copy_pdbs()
-
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
