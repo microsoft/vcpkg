1ca36302ef3e82d242dde1d6838f573b847d9fee
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2025-12-02 00:12:17 +0100

    [Qt] Update to 6.9.3 (#48472)
    Co-authored-by: Billy Robert O'Neal III <bion@microsoft.com>

--- ports/qtdatavis3d/port.data.cmake
+++ ports/qtdatavis3d/port.data.cmake
@@ -1,3 +1,3 @@
-set(qtdatavis3d_HASH "2d8042db3589d571d4851820791bb6dfd4f283baffd33f99ed322a471fdc319d64eeb3dac1e36bb2fdeb1957116edd23bbf6e3c51a8e041a00a9a1d13ca8a645")
-set(qtdatavis3d_URL "https://download.qt.io/archive/qt/6.9/6.9.2/submodules/qtdatavis3d-everywhere-src-6.9.2.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.2/submodules/qtdatavis3d-everywhere-src-6.9.2.tar.xz")
-set(qtdatavis3d_FILENAME "qtdatavis3d-everywhere-src-6.9.2.tar.xz")
+set(qtdatavis3d_HASH "7e4c7fa293060fe591378c50aa80fbba174bb6e0bb7528ad620ddf87b4a78b1eab5ebe1d96a7a544d52dd8ce05425a2af58ab2fa9ed1d29f2d64ac27267614a8")
+set(qtdatavis3d_URL "https://download.qt.io/archive/qt/6.9/6.9.3/submodules/qtdatavis3d-everywhere-src-6.9.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.3/submodules/qtdatavis3d-everywhere-src-6.9.3.tar.xz")
+set(qtdatavis3d_FILENAME "qtdatavis3d-everywhere-src-6.9.3.tar.xz")
--- ports/qtvirtualkeyboard/port.data.cmake
+++ ports/qtvirtualkeyboard/port.data.cmake
@@ -1,3 +1,3 @@
-set(qtvirtualkeyboard_HASH "0e7bdcaa3b37499f7160c12ad7b178365b1b246a77fed6ae1903eefab4dfb09758305893a62496d515c0a762beb7602cdf73cc01a5ecbd01d47c4bfbe01a3835")
-set(qtvirtualkeyboard_URL "https://download.qt.io/archive/qt/6.9/6.9.2/submodules/qtvirtualkeyboard-everywhere-src-6.9.2.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.2/submodules/qtvirtualkeyboard-everywhere-src-6.9.2.tar.xz")
-set(qtvirtualkeyboard_FILENAME "qtvirtualkeyboard-everywhere-src-6.9.2.tar.xz")
+set(qtvirtualkeyboard_HASH "98a91be495adde8a0e8396883fe147f34d90f8985903c885a74a76cd6946f6d366338880f306767eee171a74df7cf5ddff079e6a67be86045c82e0356e8f7890")
+set(qtvirtualkeyboard_URL "https://download.qt.io/archive/qt/6.9/6.9.3/submodules/qtvirtualkeyboard-everywhere-src-6.9.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.3/submodules/qtvirtualkeyboard-everywhere-src-6.9.3.tar.xz")
+set(qtvirtualkeyboard_FILENAME "qtvirtualkeyboard-everywhere-src-6.9.3.tar.xz")
