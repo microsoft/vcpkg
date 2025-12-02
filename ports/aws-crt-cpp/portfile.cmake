566e680baf2152f22bb752683d0b0f5289960569
SunBlack <SunBlack@users.noreply.github.com>
2025-12-02 11:17:36 +0100

    [aws-crt-cpp] Update to 0.35.3

--- ports/aws-crt-cpp/portfile.cmake
+++ ports/aws-crt-cpp/portfile.cmake
@@ -2,9 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO awslabs/aws-crt-cpp
     REF "v${VERSION}"
-    SHA512 14a4aef4f8d1084ec352bb252c7b1a84263288bdcb6e1fa18bf54f218e3900fb46ce1e4d231f467c2f3e8b7749ea3a30c83bc1393a3a6cb24b851a7b35f07a43
-    PATCHES
-        no-werror.patch
+    SHA512 aae1c233f4d021e4781108e9ba8bf563b8441080834e31c5950a374a1ac81124b4e49e3e6a0228a38e596861e585462f80d0ff8d40b4684e68552c284138d175
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATIC_CRT)
@@ -17,6 +15,7 @@ vcpkg_cmake_configure(
         -DBUILD_DEPS=OFF
         "-DCMAKE_MODULE_PATH=${CURRENT_INSTALLED_DIR}/share/aws-c-common" # use extra cmake files
         -DBUILD_TESTING=FALSE
+        -DAWS_WARNINGS_ARE_ERRORS=OFF
 )
 
 vcpkg_cmake_install()
