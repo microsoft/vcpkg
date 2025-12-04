1c8f22df973b2f75679ee050a99a637047d76cdd
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2025-12-04 22:24:58 +0100

    [Qt] update to 6.10.0

--- ports/qtquick3dphysics/port.data.cmake
+++ ports/qtquick3dphysics/port.data.cmake
@@ -1,3 +1,3 @@
-set(qtquick3dphysics_HASH "c8a2d15299aec583722ea16b34158dda74663f6b5e105c71f706f6065744d43e9c6dc76b8fc9fd6aa0bfc00451ccb22cd398cd3549bde2f6f6cfe1217fd3fa7f")
-set(qtquick3dphysics_URL "https://download.qt.io/archive/qt/6.9/6.9.3/submodules/qtquick3dphysics-everywhere-src-6.9.3.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.9/6.9.3/submodules/qtquick3dphysics-everywhere-src-6.9.3.tar.xz")
-set(qtquick3dphysics_FILENAME "qtquick3dphysics-everywhere-src-6.9.3.tar.xz")
+set(qtquick3dphysics_HASH "382c1802ddd03615cd6d158c21041266f43dd830bcd388d73e8ccf28800edd941f0a231691919837327c49fb1397d4f0e07f01b250f95ce8d05c1353ac1cd5ed")
+set(qtquick3dphysics_URL "https://download.qt.io/archive/qt/6.10/6.10.0/submodules/qtquick3dphysics-everywhere-src-6.10.0.tar.xz;https://mirrors.ocf.berkeley.edu/qt/archive/qt/6.10/6.10.0/submodules/qtquick3dphysics-everywhere-src-6.10.0.tar.xz")
+set(qtquick3dphysics_FILENAME "qtquick3dphysics-everywhere-src-6.10.0.tar.xz")
