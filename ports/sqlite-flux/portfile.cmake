4ba97a60d0cab61679faae32ad31947109908b4e
a-alomran <dev.alomran@gmail.com>
2025-12-05 13:38:05 +0100

    [sqlite-flux] Update to version 1.1.1 (#48559)
    Co-authored-by: Oryxbit <oryxsbit@gmail.com>

--- ports/sqlite-flux/portfile.cmake
+++ ports/sqlite-flux/portfile.cmake
@@ -1,25 +1,19 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO a-alomran/sqlite_flux
-    REF v1.0.0
-    SHA512 7c1486e0b89a719230368f9202049a823420b0305999bef0c4b15567b5fe97aec47862fbffd65e8986423d56a9cdf9d10abace4385e08825ad20a324ec729347
+    REF v1.1.1
+    SHA512 abd71e9b99b27b4edebe8f49e978212498b0914170b9fe41b4111121eb2d732b9b2939add9f6732d474008628eb3e05e0945f7687504da837d497a93d96d3376
     HEAD_REF main
 )
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
-
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME sqlite_flux
     CONFIG_PATH lib/cmake/sqlite_flux
 )
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
