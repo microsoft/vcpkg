52200c41884445c77c44082e4c4e6eafc4a1e2a2
浮生若梦 <1070753498@qq.com>
2025-12-09 16:17:31 +0800

    [forge] fix udpate glad2

--- ports/forge/portfile.cmake
+++ ports/forge/portfile.cmake
@@ -5,7 +5,9 @@ vcpkg_from_github(
     REF v1.0.8
     SHA512 08e5eb89d80f7fa3310f0eb37481492b5c1dfff00b33c308169862d8b25cf93ad1d9c0db78667c0207a7f6f8ca4046c196bd3a987af839ea1864b49c738ee8e3
     HEAD_REF master
-    PATCHES cmake_config.patch
+    PATCHES 
+        cmake_config.patch
+        fix-glad2.patch
 )
 file(REMOVE "${SOURCE_PATH}/CMakeModules/FindOpenGL.cmake")
 
