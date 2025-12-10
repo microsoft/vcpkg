00cfb533a1d7b4d9045b83260787192ef8faeb7b
Thibault LESCOAT <thibault.lescoat@tyndallfx.com>
2025-12-10 10:48:10 +0100

    ARM64 support on Windows

--- ports/stella-cv-fbow/portfile.cmake
+++ ports/stella-cv-fbow/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
 	REPO stella-cv/FBoW
 	REF c6e3c29e3332a0b0834021797e2aa4e8eb66a3c1
 	SHA512 a7f80874c396163a8cbebfbcdf150ea1f1de99ac58a1bd26f69257046e7fe3d32478c80a89eeab329a845e9a6a8c1264cf8750ccd44b380d93def9535048dbb4
+	PATCHES
+		fix-arm-windows.patch
 )
 
 vcpkg_cmake_configure(
