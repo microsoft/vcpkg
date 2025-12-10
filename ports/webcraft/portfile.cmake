e2bb8ac45f9d4c73b132c2f4b1316b968a1569e0
Aditya Rao <adityarao2005@gmail.com>
2025-12-09 22:47:42 -0500

    Update port to use commit hash and refresh version db

--- ports/webcraft/portfile.cmake
+++ ports/webcraft/portfile.cmake
@@ -3,8 +3,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO adityarao2005/WebCraft   # <--- CHANGE THIS to your actual User/Repo
-    REF v1.0.1                         # <--- The tag/commit you want to release (can be empty if only using --head)
-    SHA512 e6ac8bc32167d6b8f367d33236af8178213149131c35ece167a2d0098844542126a4eca93f76d8a1e9e94e0811cdd337ade448c32ebdb27583c4c7ec3e808557                           # <--- Keep as 0. We will get the real hash in Step 3.
+    REF 8eaab1781be6617fead4c234cb3d6fddd730e2c6                         # <--- The tag/commit you want to release (can be empty if only using --head)
+    SHA512 138ffb972b0dad20f8db5f55ff6033bb782548e2c9e2f5fce43232d01f3cdab3324e7cc3572b95fc527a3222461fcffdf320000998205559e4fd2edefc2b6e27
     HEAD_REF main                      # <--- The branch to use when building with --head
 )
 
@@ -18,7 +18,7 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME WebCraft 
-    CONFIG_PATH share/webcraft
+    CONFIG_PATH share/WebCraft
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
