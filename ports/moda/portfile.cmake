e4e83815987fea3582a443da4ccfbeb40cc55f6c
dudenzz <kubadt@gmail.com>
2025-12-11 20:40:34 +0100

    [new port] moda

--- ports/moda/portfile.cmake
+++ ports/moda/portfile.cmake
@@ -25,5 +25,5 @@ vcpkg_cmake_config_fixup(
 vcpkg_install_copyright(
     FILE_LIST "${SOURCE_PATH}/LICENSE"
 )
-
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
