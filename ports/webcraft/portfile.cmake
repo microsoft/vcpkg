74fe61a5355274e095a032cc5b8f71fcdb2e45d3
Aditya Rao <adityarao2005@gmail.com>
2025-12-11 22:34:59 -0500

    adding a fix which I made for CMakeLists.txt and updated the values.. if this works then I'll create a version based one and try to run it

--- ports/webcraft/portfile.cmake
+++ ports/webcraft/portfile.cmake
@@ -1,10 +1,11 @@
 # ports/webcraft/portfile.cmake
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO adityarao2005/WebCraft   # <--- CHANGE THIS to your actual User/Repo
-    REF 8eaab1781be6617fead4c234cb3d6fddd730e2c6                         # <--- The tag/commit you want to release (can be empty if only using --head)
-    SHA512 138ffb972b0dad20f8db5f55ff6033bb782548e2c9e2f5fce43232d01f3cdab3324e7cc3572b95fc527a3222461fcffdf320000998205559e4fd2edefc2b6e27
+    REF 0294f663255271b328da8f8643489ba9e91b6845                         # <--- The tag/commit you want to release (can be empty if only using --head)
+    SHA512 65a1ffbd07770d02083e1fd61152ca50b1f203f423199daa7d5d8a960a6ee12c7f1ba11df876eef0a78e419c4ec2a073c05f10396d46db98d309a0e24a8b0fb1
     HEAD_REF main                      # <--- The branch to use when building with --head
 )
 
@@ -18,8 +19,9 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME WebCraft 
-    CONFIG_PATH share/WebCraft
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
