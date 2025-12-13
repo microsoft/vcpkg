ae163e517ba4071178d9ed6f37600b4766ff074c
Aditya Rao <adityarao2005@gmail.com>
2025-12-13 00:02:08 -0500

    updated the cmake lists one last time

--- ports/webcraft/portfile.cmake
+++ ports/webcraft/portfile.cmake
@@ -22,7 +22,10 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH share/WebCraft)
+vcpkg_cmake_config_fixup(
+	PACKAGE_NAME WebCraft
+	CONFIG_PATH share/WebCraft
+)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
