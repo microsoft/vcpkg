4687ea587e18f2061eb9998bd0932f8a6f7f4855
myd7349 <myd7349@gmail.com>
2025-12-14 18:58:13 +0800

    [picobench] Fix ref version

--- ports/picobench/portfile.cmake
+++ ports/picobench/portfile.cmake
@@ -4,7 +4,7 @@ set(VCPKG_BUILD_TYPE release)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO iboB/picobench
-    REF "v${VERSION}"
+    REF "v2.08"
     SHA512 fcf0f459afa70cc7eb41fceb2485976fdceb4731652c9f3420d8b072fb1286b8afcb59d568f4693ef3f704e2fd779f134a6cce2773b1af7c47e8a3546bae0937
     HEAD_REF main
 )
