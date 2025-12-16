9e298e35a23535a2641b96ee89005e24d425325a
xavier2k6 <42386382+xavier2k6@users.noreply.github.com>
2025-12-16 15:10:49 +0000

    [vcpkg scripts] Update `jom` to `1.1.5`

--- scripts/cmake/vcpkg_find_acquire_program(JOM).cmake
+++ scripts/cmake/vcpkg_find_acquire_program(JOM).cmake
@@ -1,5 +1,5 @@
 set(program_name jom)
-set(program_version_string 1_1_4)
+set(program_version_string 1_1_5)
 if(CMAKE_HOST_WIN32)
     set(download_urls
         "https://download.qt.io/official_releases/jom/jom_${program_version_string}.zip"
@@ -7,7 +7,7 @@ if(CMAKE_HOST_WIN32)
         "https://mirrors.ukfast.co.uk/sites/qt.io/official_releases/jom/jom_${program_version_string}.zip"
     )
     set(download_filename "jom_${program_version_string}.zip")
-    set(download_sha512 a683bd829c84942223a791dae8abac5cfc2e3fa7de84c6fdc490ad3aa996a26c9fa0be0636890f02c9d56948bbe3225b43497cb590d1cb01e70c6fac447fa17b)
+    set(download_sha512 5e63b8cabe11c996d2d028c13978030c6c1cc1bace3f414f64c1ef6ac9174870903fa3f607226e2bc7d637e9e7fb561c03bef0a8b208a234599531b8b143c001)
     set(tool_subdirectory "jom-${program_version_string}")
     set(paths_to_search "${DOWNLOADS}/tools/jom/${tool_subdirectory}")
 endif()
