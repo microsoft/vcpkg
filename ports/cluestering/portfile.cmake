92b22900140406c7d259202f387fea55cb31f95c
Simone Balducci <simone.balducci00@gmail.com>
2025-12-16 18:11:47 +0100

    Remove installation of empty directories

--- ports/cluestering/portfile.cmake
+++ ports/cluestering/portfile.cmake
@@ -15,3 +15,8 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/CLUEstering")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/lib"
+    "${CURRENT_PACKAGES_DIR}/debug"
+)
