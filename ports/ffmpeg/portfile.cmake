9d86876931f8f8b5130df7f9897fbae2e8e4d005
Kim Syehoon <syehoon.kim@sbs.co.kr>
2025-12-17 10:37:00 +0900

    portfile applies libzmq patch file

--- ports/ffmpeg/portfile.cmake
+++ ports/ffmpeg/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_from_github(
         0041-add-const-for-opengl-definition.patch
         0043-fix-miss-head.patch
         0044-fix-vulkan-debug-callback-abi.patch
+        0050-libzmq-pkgconfig.patch
 )
 
 if(SOURCE_PATH MATCHES " ")
