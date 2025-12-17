d5e71556725c1f02632bf78f7f1f19e566fc15d2
Thomas1664 <46387399+Thomas1664@users.noreply.github.com>
2025-12-17 22:43:23 +0100

    ?!

--- ports/cimg/portfile.cmake
+++ ports/cimg/portfile.cmake
@@ -3,7 +3,7 @@ set(VCPKG_BUILD_TYPE release) # header-only
 vcpkg_from_github(OUT_SOURCE_PATH SOURCE_PATH
     REPO GreycLab/CImg
     REF "v.${VERSION}"
-    SHA512 0f3a4ddb5fcfb26f403e7f9aaa1e76bce1fc85e27934619f692572efaf111ec2c568b55c8b48d9629a15cb9ae7eeeac70d86f7dfb36082419b2d07c5fd0a0c8e
+    SHA512 7a0a66cf8dfb9923be0ee576f5dc6c53a8704a92ce9b1b3b8d1a7c6b5a1d94f231fce25a813f0fb6e6afd0f75111a8efb91f3e05c57621900ef588e00bf8d06c
     HEAD_REF master
 )
 
