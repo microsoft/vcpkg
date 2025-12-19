7da9ee3a36f1e12c3bda40d2e21e1fac5bea316b
Nick D'Ademo <nickdademo@gmail.com>
2025-12-20 03:08:35 +1100

    fix package name in fixup

--- ports/qzxing/portfile.cmake
+++ ports/qzxing/portfile.cmake
@@ -20,7 +20,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-qzxing)
 vcpkg_copy_pdbs()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
