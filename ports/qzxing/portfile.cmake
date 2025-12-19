324eaa32bd2a10eef960c76db5de570be68bc613
Nick D'Ademo <nickdademo@gmail.com>
2025-12-20 03:18:31 +1100

    add file removes

--- ports/qzxing/portfile.cmake
+++ ports/qzxing/portfile.cmake
@@ -23,4 +23,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-qzxing)
 vcpkg_copy_pdbs()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
