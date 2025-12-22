0b16e95e0bffec6f32309164bfa1e9925f5413e2
Vallabh Mahajan <vallabhmahajan570@gmail.com>
2025-12-22 20:04:34 +0530

    [godot-cpp] update to 4.5

--- ports/godot-cpp/portfile.cmake
+++ ports/godot-cpp/portfile.cmake
@@ -2,34 +2,37 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO "godotengine/godot-cpp"
+    REPO godotengine/godot-cpp
     REF "godot-${VERSION}-stable"
-    SHA512 "3c97d6f0bbd952977d8085483d538b650d44ee0f9c6d84215128d9702d071b23a91bacab3a5259320f89d11884b3a5d5b638bc757c11d7447c000223fa976de8"
-    HEAD_REF "master"
-    PATCHES
-        "packagable.patch"
+    SHA512 "7fe4d02e409fa2c4c4476fb02c0cf6deb79bb1daa6aa370369587542283dc9d9d51fa6cd9604ac6b5cbd28e9ee5dd8844482c44c3d535f5e0190a7031feab68c"
+    HEAD_REF master
 )
-
-vcpkg_find_acquire_program(PYTHON3)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        "-DPython3_EXECUTABLE=${PYTHON3}"
 )
 
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "unofficial-${PORT}")
+vcpkg_cmake_build(TARGET godot-cpp)
+
+file(INSTALL "${SOURCE_PATH}/include/" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(INSTALL "${SOURCE_PATH}/gdextension" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+file(GLOB DEBUG_LIBS 
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/bin/*.lib"
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/*.lib"
+)
+file(INSTALL ${DEBUG_LIBS} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+
+file(GLOB RELEASE_LIBS 
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/bin/*.lib"
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/*.lib"
+)
+file(INSTALL ${RELEASE_LIBS} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+
 vcpkg_copy_pdbs()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
 
-file(
-    INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-)
-
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/usage")
+    file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+endif()
