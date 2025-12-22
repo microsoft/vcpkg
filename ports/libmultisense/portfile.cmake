ce38bffa21ba6f08e3c651eef63af86f62d81f25
Ethan J. Musser <emusser@carnegierobotics.com>
2025-12-22 18:21:40 -0500

    Update `libmultisense` to `7.4.0`

--- ports/libmultisense/portfile.cmake
+++ ports/libmultisense/portfile.cmake
@@ -2,13 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO carnegierobotics/LibMultiSense
     REF ${VERSION}
-    SHA512 69472f288de46c0ecdbbbcb8c280610c1c80778d660098e3b639ab653b108096c3fb4cd92a21afd4745b959a0c80812c5bf2d42053760bbceeafd90e67c20388
+    SHA512 ccb865c6bfaed6a415cd7acf22ca24921ef53e8ac71f2f213f89fe2cdd24893d60507cba6ea60f2d690b834d3eb3631fc80da657936738028ee5ffac986f0ae8
     HEAD_REF master
     PATCHES
         0000-platform-specific-links.patch
         0001-find-public-api-dependencies.patch
         0002-disable-error-on-warning.patch
-        0003-utilities-cc-unreachable-code.patch
 )
 
 vcpkg_check_features(
