6a4c82ad66e1d57bd2806f551ee705cf1239df68
cDc <cdc.seacave@gmail.com>
2025-12-22 14:23:38 +0000

    [tinynpy] update TinyNPY to v1.1

--- ports/tinynpy/portfile.cmake
+++ ports/tinynpy/portfile.cmake
@@ -1,23 +1,25 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cdcseacave/TinyNPY
-    REF 0a11001009cc3e8f3c95c61b92c0ce1bd52d5383
-    SHA512 a5fe962e433896a5c7363808e87fca1297c98496cf6b6fd98154f4f4bc113ea213e281ebcdbceadf47fc573b6d707097f3a06cc58bf26351ebf7f935954d79c0
+    REF master
+    SHA512 3401ab531bd90dcdac8e1288b7209921a70734fea4ef1fbdf190f2cb45663ca6b7e7d2f9378928bd849a506f5aac966f20de8a76cf2f4b63d1c7813d4f625f82
     HEAD_REF master
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC_LIBS)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" _BUILD_SHARED_LIBS)
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" LINK_CRT_STATIC)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DBUILD_STATIC_LIBS=${BUILD_STATIC_LIBS}
-        -DBUILD_DEMO=OFF
+    -DBUILD_SHARED_LIBS=${_BUILD_SHARED_LIBS}
+    -DLINK_CRT_STATIC_LIBS=${LINK_CRT_STATIC}
+    -DBUILD_DEMO=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/TinyNPY")
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/TinyNPY)
 
 vcpkg_copy_pdbs()
 
