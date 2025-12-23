05bd3976f0dcc264fb4b8ba1533104dea673adfa
Cappecasper03 <03repsac@gmail.com>
2025-12-23 09:40:46 +0100

    Reverted changes to apr and sqlite3

--- ports/sqlite3/sqlite3-config.in.cmake
+++ ports/sqlite3/sqlite3-config.in.cmake
@@ -3,10 +3,8 @@ include(CMakeFindDependencyMacro)
 if(NOT WIN32)
     find_dependency(Threads)
 endif()
+if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
+    find_package(ICU COMPONENTS uc i18n)
+endif()
 
 include(${CMAKE_CURRENT_LIST_DIR}/unofficial-sqlite3-targets.cmake)
-
-if("@VCPKG_LIBRARY_LINKAGE@" STREQUAL "static")
-    find_package(ICU COMPONENTS uc i18n REQUIRED)
-    set_property(TARGET unofficial::sqlite3::sqlite3 APPEND PROPERTY INTERFACE_LINK_LIBRARIES ICU::uc ICU::i18n)
-endif()
