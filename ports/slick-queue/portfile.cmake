98b5e107cbb90044e7ff13653901c9cd4f71ddd4
kzhdev <kzhdev@gmail.com>
2025-12-23 09:51:11 -0600

    add usage file

--- ports/slick-queue/portfile.cmake
+++ ports/slick-queue/portfile.cmake
@@ -24,3 +24,5 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib
 
 # Install license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
