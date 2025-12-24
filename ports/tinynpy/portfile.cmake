55c2d499499df30d9498459dbba811175cbb1938
cDc <cdc.seacave@gmail.com>
2025-12-24 10:52:26 +0200

    Update ports/tinynpy/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/tinynpy/portfile.cmake
+++ ports/tinynpy/portfile.cmake
@@ -6,7 +6,6 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" _BUILD_SHARED_LIBS)
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" LINK_CRT_STATIC)
 
 vcpkg_cmake_configure(
