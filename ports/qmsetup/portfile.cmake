7267fd9fc5f00e20ff714c38d704a9c09b13187d
shixiong2333 <50489744+shixiong2333@users.noreply.github.com>
2025-12-24 09:41:21 +0800

    qmsetup syscmdline specify commit ref

--- ports/qmsetup/portfile.cmake
+++ ports/qmsetup/portfile.cmake
@@ -1,12 +1,12 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stdware/qmsetup
-    REF "main"
-    SHA512 021c2f3205c20dcdc240445395292fff406eec758c7a83de0e3f17633b11c188d582006eb47052abf3bb83e6544d29f660574dc2f711af3475c215309ac22354
+    REF 2f10ebc3723a9b03edf309611483ee643f10add7
+    SHA512 a1a6e4d60f68ce910a4f897beca870109ff5fead250e3e194051e1760ab62c1a1ad3ef6b58d20f5cb08da8c50121c3d55da3f2b8600c99d3b96289613fb42214
     HEAD_REF master
-    PATCHES
-        fix-msvc-runtime.patch
 )
+
+set(VCPKG_BUILD_TYPE release) # header-only port
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -16,8 +16,6 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/qmsetup)
 
 vcpkg_copy_tools(TOOL_NAMES qmcorecmd AUTO_CLEAN)
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
