fbacef9ae088704b2a9a0cfde8201211b443b0cc
MatusGuy <MatusGuy@users.noreply.github.com>
2025-12-26 21:25:47 +0000

    Rename package & target to include "unofficial"
    See https://learn.microsoft.com/en-us/vcpkg/contributing/maintainer-guide#add-cmake-exports-in-an-unofficial--namespace
    
    Also removed polyfill variables

--- ports/libraqm/portfile.cmake
+++ ports/libraqm/portfile.cmake
@@ -14,9 +14,10 @@ vcpkg_install_meson()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
-configure_file("${CMAKE_CURRENT_LIST_DIR}/raqm-config.cmake.in"
-	"${CURRENT_PACKAGES_DIR}/share/RAQM/raqm-config.cmake" @ONLY)
+configure_file(
+    "${CMAKE_CURRENT_LIST_DIR}/unofficial-libraqm-config.cmake.in"
+	"${CURRENT_PACKAGES_DIR}/share/unofficial-libraqm/unofficial-libraqm-config.cmake"
+    @ONLY
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
