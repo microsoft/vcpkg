865728511b5011d7415f8898fa7154568f380657
Kai Pastor <dg0yt@darc.de>
2025-12-26 10:38:42 +0100

    [gl2ps] Download distfile

--- ports/gl2ps/portfile.cmake
+++ ports/gl2ps/portfile.cmake
@@ -1,10 +1,11 @@
-vcpkg_from_gitlab(
-    GITLAB_URL https://gitlab.onelab.info
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO gl2ps/gl2ps
-    REF gl2ps_1_4_2
-    SHA512 cb4abd79f6790e229a0b05a6d12e4bd4d24885c89c4cb8644e49b0459361565c5c5379b53d85f59eeaba16144d3288dbd06c90f55a739f0928a788224ccb8085
-    HEAD_REF master
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://geuz.org/gl2ps/src/gl2ps-${VERSION}.tgz"
+    FILENAME "gl2ps-${VERSION}.tgz"
+    SHA512 46652e1b3825ace61dbd77c4b0bf451e7671c248eb18bbd3369e2fac00056ea4cd5d2578561984313c239e3b02f78b9d9a76d963c935af65a13bc2abfc538620
+)
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
     PATCHES
         separate-static-dynamic-build.patch
 )
