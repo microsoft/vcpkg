10336bd089a27759f83c30bdbc300c32d70f94fa
Kai Pastor <dg0yt@darc.de>
2025-12-27 11:03:46 +0100

    [orc] Move external project download to portfile

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -6,14 +6,18 @@ vcpkg_from_github(
   REF "v${VERSION}"
   SHA512 b81e1c2cebfd97757aaaa8dc9bed913810b0a47e599556bc3e9989dadf6b7665c335c37cd29d8430cd9b1af9048f37efaa56869872eb5f50fe35a570ec109a40
   HEAD_REF master
+  PATCHES
+    external-project.diff
 )
+file(GLOB modules "${SOURCE_PATH}/cmake_modules/Find*.cmake")
 
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindGTest.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindLZ4.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZSTD.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindProtobuf.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindSnappy.cmake")
-file(REMOVE "${SOURCE_PATH}/cmake_modules/FindZLIB.cmake")
+set(orc_format_version 1.1.1)
+vcpkg_download_distfile(ORC_FORMAT_ARCHIVE
+  URLS "https://dlcdn.apache.org/orc/orc-format-${orc_format_version}/orc-format-${orc_format_version}.tar.gz"
+  FILENAME "apache-orc-format-${orc_format_version}.tar.gz"
+  SHA512 8aa0bcd3345ed8be836995d4347175526f4b0fc91f41e27f29279fad39b94ff157f5cd597bc2d9f3dc403f5ba405807675a283abe822f8a83758b7c3b8292c1c
+)
+set(ENV{ORC_FORMAT_URL} "file://${ORC_FORMAT_ARCHIVE}")
 
 if(VCPKG_TARGET_IS_WINDOWS)
   set(BUILD_TOOLS OFF)
