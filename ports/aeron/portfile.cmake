76ca68eac5a63e9f1ad2cdfc8c66b0d76329428e
cuihairu <chuihairu@gmail.com>
2026-01-02 20:05:29 +0800

    [aeron] Add new port (version 1.49.3)
    Add Aeron - efficient reliable UDP unicast, UDP multicast, and IPC
    message transport library.
    
    Features:
    - Core C and C++ client libraries (default)
    - Optional 'archive' feature for recording/replaying message streams
      (requires Java for code generation)
    
    Fixes:
    - Move DLLs from lib/ to bin/ on Windows
    - Use vcpkg_copy_tools for aeronmd executables
    - Skip archive feature test in CI (requires Java)

--- ports/aeron/portfile.cmake
+++ ports/aeron/portfile.cmake
@@ -10,17 +10,43 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+# Set archive option based on feature
+if("archive" IN_LIST FEATURES)
+    set(BUILD_ARCHIVE ON)
+else()
+    set(BUILD_ARCHIVE OFF)
+endif()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DAERON_INSTALL_TARGETS=ON
         -DAERON_TESTS=OFF
         -DAERON_BUILD_SAMPLES=OFF
+        -DBUILD_AERON_ARCHIVE_API=${BUILD_ARCHIVE}
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/aeron)
+
+# Move DLLs from lib to bin (aeron builds both static and shared libraries)
+if(VCPKG_TARGET_IS_WINDOWS)
+    file(MAKE_DIRECTORY "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+    file(GLOB RELEASE_DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
+    file(GLOB DEBUG_DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
+    if(RELEASE_DLLS)
+        file(COPY ${RELEASE_DLLS} DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+        file(REMOVE ${RELEASE_DLLS})
+    endif()
+    if(DEBUG_DLLS)
+        file(COPY ${DEBUG_DLLS} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+        file(REMOVE ${DEBUG_DLLS})
+    endif()
+endif()
+
+# Copy aeronmd tools
+vcpkg_copy_tools(TOOL_NAMES aeronmd aeronmd_s AUTO_CLEAN)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
