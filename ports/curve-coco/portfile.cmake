8cc8de94956264e5a9f424b35f420c9121ab2009
toge <toge.mail@gmail.com>
2026-01-08 02:41:54 +0900

    [ereignis] update to 6.3.0

--- ports/curve-coco/portfile.cmake
+++ ports/curve-coco/portfile.cmake
@@ -1,4 +1,4 @@
-set(VCPKG_BUILD_TYPE release) # Header-only library
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # the project doesn't support SHARED
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Curve/coco
@@ -23,7 +23,5 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/coco-${VERSION}" PACKAGE_NAME "coco")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")  # from CMake config
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
--- ports/ereignis/portfile.cmake
+++ ports/ereignis/portfile.cmake
@@ -2,12 +2,28 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Curve/ereignis
     REF "v${VERSION}"
-    SHA512 73b9fbf01caee6f0cc49de771ee5fc5e1da208acd2d3d40647c3e9c19df121b05c3403393539fa4bb510cb8e58769fe9afa5b036ce04c564266fc49b8ddea8e5
+    SHA512 f6a00270d02533aab41b3bddee0d7af8fc46e2f17bf832f7aa293a175631c4ed1ec92a345f7175d0785c90966d10bb0a4f8fba059e2d25eff2fdb357b8d64201
+    HEAD_REF master
+    PATCHES
+        remove-cpm.patch
+)
+
+file(WRITE "${SOURCE_PATH}/cmake/CPM.cmake" "# disabled by vcpkg")
+
+# Replace CPM and download PackageProject directly to avoid issues with FETCHCONTENT_FULLY_DISCONNECTED
+vcpkg_from_github(
+    OUT_SOURCE_PATH PACKAGE_PROJECT_PATH
+    REPO TheLartians/PackageProject.cmake
+    REF "v1.13.0"
+    SHA512 3cf0523bddc213f206ed0ca57803550cb7db9e293392d3741138be47f49d9027ef517e1656235a349a62b492d35c3fc677714dc00afe59e2d36144a9689cfa8f
     HEAD_REF master
 )
+file(RENAME "${PACKAGE_PROJECT_PATH}" "${SOURCE_PATH}/cmake/packageproject.cmake")
 
 vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
 vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/ereignis-${VERSION})
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
