e168ccff4c9d74952b0b105f0aa364d6bf3aba31
SunBlack <SunBlack@users.noreply.github.com>
2026-01-08 00:56:15 +0100

    [sdl2pp] Update to 0.18.1

--- ports/sdl2pp/portfile.cmake
+++ ports/sdl2pp/portfile.cmake
@@ -1,12 +1,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libSDL2pp/libSDL2pp
-    REF ${VERSION}
-    SHA512 3682281432ce9dec0dbc7c786496564c906db9933138e1f2b881f93b5602a7170e06e67e87d35a9e5944ef80f6e13b9835e33209c52869f0ea2bc224f639a749
+    REF "${VERSION}"
+    SHA512 655412c93df5e6207064a07328785add4e7700a656295f03f0f2df4898ce62bd259340de28bf2a79db4fce765d2000ce6a43312dbe524f2b2b909a2dbf324859
     HEAD_REF master
     PATCHES
         fix-dependencies.patch
-        fix-usage.patch
 )
 
 vcpkg_check_features(
@@ -17,34 +16,21 @@ vcpkg_check_features(
         sdl2-ttf   SDL2PP_WITH_TTF
 )
 
-file(REMOVE "${SOURCE_PATH}/cmake/FindSDL2.cmake"
-            "${SOURCE_PATH}/cmake/FindSDL2_image.cmake"
-            "${SOURCE_PATH}/cmake/FindSDL2_mixer.cmake"
-            "${SOURCE_PATH}/cmake/FindSDL2_ttf.cmake"
-)
-
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" SDL2PP_STATIC)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${FEATURE_OPTIONS}
-        -DCMAKEMODDIR=share/${PORT}
         -DSDL2PP_WITH_EXAMPLES=OFF
         -DSDL2PP_WITH_TESTS=OFF
         -DSDL2PP_STATIC=${SDL2PP_STATIC}
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake")
 vcpkg_fixup_pkgconfig()
 
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/FindSDL2PP.cmake" "HINTS \"${CURRENT_PACKAGES_DIR}/include\"" "")
-vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/FindSDL2PP.cmake" "HINTS \"${CURRENT_PACKAGES_DIR}/lib\"" "")
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.txt")
