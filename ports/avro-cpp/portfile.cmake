5e210f43bba64dc55d031e2f8e980db5de941a6a
SunBlack <SunBlack@users.noreply.github.com>
2026-01-08 12:05:00 +0100

    [avro-cpp] Update to 1.12.1

--- ports/avro-cpp/portfile.cmake
+++ ports/avro-cpp/portfile.cmake
@@ -2,52 +2,39 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO apache/avro
     REF "release-${VERSION}"
-    SHA512 8cc6ef3cf1e0a919118c8ba5817a1866dc4f891fa95873c0fe1b4b388858fbadee8ed50406fa0006882cab40807fcf00c5a2dcd500290f3868d9d06b287eacb6
+    SHA512 4e7fd7ebb41f6149a499d0d38babd99d07f936143b47a60f7c568a589fb0e6369301c7230bde518b554eaeaa9ded1ed1fae2661cbd5ebc49fb5f22d97c066f05
     HEAD_REF master
     PATCHES
         fix-cmake.patch
-        fix-fmt.patch
-        fix-std32_t.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        tools              AVRO_BUILD_EXECUTABLES
     INVERTED_FEATURES
         snappy             CMAKE_DISABLE_FIND_PACKAGE_Snappy
 )
 
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}/lang/c++"
     OPTIONS
-        -DBUILD_TESTING=OFF
+        -DAVRO_BUILD_STATIC=${BUILD_STATIC}
+        -DAVRO_BUILD_SHARED=${BUILD_SHARED}
+        -DAVRO_BUILD_TESTS=OFF
         ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-${PORT})
-
-file(READ "${CURRENT_PACKAGES_DIR}/share/unofficial-avro-cpp/unofficial-avro-cpp-config.cmake" cmake_config)
-if("snappy" IN_LIST FEATURES)
-    file(WRITE "${CURRENT_PACKAGES_DIR}/share/unofficial-avro-cpp/unofficial-avro-cpp-config.cmake"
-"include(CMakeFindDependencyMacro)
-find_dependency(Boost REQUIRED COMPONENTS filesystem iostreams program_options regex system)
-find_dependency(fmt CONFIG)
-find_dependency(Snappy)
-${cmake_config}
-")
-else()
-    file(WRITE "${CURRENT_PACKAGES_DIR}/share/unofficial-avro-cpp/unofficial-avro-cpp-config.cmake"
-"include(CMakeFindDependencyMacro)
-find_dependency(Boost REQUIRED COMPONENTS filesystem iostreams program_options regex system)
-find_dependency(fmt CONFIG)
-${cmake_config}
-")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/avro-cpp")
+vcpkg_copy_pdbs()
+if(AVRO_BUILD_EXECUTABLES)
+    vcpkg_copy_tools(TOOL_NAMES avrogencpp AUTO_CLEAN)
 endif()
 
-vcpkg_copy_pdbs()
-vcpkg_copy_tools(TOOL_NAMES avrogencpp AUTO_CLEAN)
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/lang/c++/LICENSE")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
