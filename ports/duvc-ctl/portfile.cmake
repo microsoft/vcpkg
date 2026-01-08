986330b9bc7a385333e1e333740c3b32618dd7a0
allanhanan <allan.hanan04@gmail.com>
2026-01-08 12:04:32 +0530

    vcpkg(duvc-ctl): add v2.0.1#0

--- ports/duvc-ctl/portfile.cmake
+++ ports/duvc-ctl/portfile.cmake
@@ -6,11 +6,19 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    set(DUVC_BUILD_SHARED ON)
+    set(DUVC_BUILD_STATIC OFF)
+else()
+    set(DUVC_BUILD_SHARED OFF)
+    set(DUVC_BUILD_STATIC ON)
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DDUVC_BUILD_SHARED=ON
-        -DDUVC_BUILD_STATIC=ON
+        -DDUVC_BUILD_SHARED=${DUVC_BUILD_SHARED}
+        -DDUVC_BUILD_STATIC=${DUVC_BUILD_STATIC}
         -DDUVC_BUILD_C_API=OFF
         -DDUVC_BUILD_CLI=OFF
         -DDUVC_BUILD_TESTS=OFF
@@ -21,15 +29,24 @@ vcpkg_configure_cmake(
         -DDUVC_INSTALL_CMAKE_CONFIG=ON
 )
 
-vcpkg_install_cmake()
+vcpkg_cmake_install()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/duvc-ctl)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/duvc-ctl")
+vcpkg_fixup_pkgconfig()
+
 
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+endif()
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/share")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+endif()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/LICENSE"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+     RENAME copyright)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
