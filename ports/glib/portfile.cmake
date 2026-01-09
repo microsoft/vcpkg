d6b3370bdf89f17857263b1bcfa4ebfd1a6a92f4
Vitalii Koshura <lestat.de.lionkur@gmail.com>
2026-01-09 20:17:26 +0100

    [glib] update to 2.86.3
    Signed-off-by: Vitalii Koshura <lestat.de.lionkur@gmail.com>

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -10,7 +10,7 @@ vcpkg_download_distfile(GLIB_ARCHIVE
         "https://download.gnome.org/sources/${PORT}/${VERSION_MAJOR_MINOR}/${PORT}-${VERSION}.tar.xz"
         "https://www.mirrorservice.org/sites/ftp.gnome.org/pub/GNOME/sources/${PORT}/${VERSION_MAJOR_MINOR}/${PORT}-${VERSION}.tar.xz"
     FILENAME "${PORT}-${VERSION}.tar.xz"
-    SHA512 b9ef7ea7e1eeff142a8ab76fb0552331fd642133a31505e2939f57d7d9c5ec86dcefddc725c715925a42c348d71e2a8bf2eb399150108199b6daeca2761e08d6
+    SHA512 2b53aba22eef2d21cb40334fe55715bf0ca5009e5e105c462cdedfb45da96cca35e7edc95af27022893832feb5bfc0b0ee554382c8c8f55a2a777b864cfc53ba
 )
 
 vcpkg_extract_source_archive(SOURCE_PATH
