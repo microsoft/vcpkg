e126fdfe9a63ad26f89d504b04440e2743ff13ed
Kai Pastor <dg0yt@darc.de>
2026-01-10 10:49:29 +0100

    [kf5config] Fixup

--- ports/kf5config/portfile.cmake
+++ ports/kf5config/portfile.cmake
@@ -5,7 +5,6 @@ vcpkg_from_github(
     SHA512 dfd6e09ccaf132745f3bcba6276c6f5d0394c4aa2dd50ac234415e2fca3c5681d1c11e8f4141fdb9dc18aba806922c99dd6195d9013962e11de7c50cc2ca01a7
     HEAD_REF master
 )
-
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
 file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
 
@@ -13,7 +12,6 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DKDE_INSTALL_LIBEXECDIR=bin
 )
 
 vcpkg_cmake_install()
@@ -25,22 +23,12 @@ vcpkg_copy_tools(
     AUTO_CLEAN
 )
 
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    set(LIBEXEC_SUBFOLDER "kf5/")
-endif()
-
 vcpkg_copy_tools(
     TOOL_NAMES kconf_update kconfig_compiler_kf5
-    SEARCH_DIR "${CURRENT_PACKAGES_DIR}/bin/${LIBEXEC_SUBFOLDER}"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/${LIBEXEC_SUBFOLDER}"
     AUTO_CLEAN
 )
 
 file(APPEND "${CURRENT_PACKAGES_DIR}/tools/${PORT}/qt.conf" "Data = ../../share")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
