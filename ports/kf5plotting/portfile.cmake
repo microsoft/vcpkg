78a0ffcf96ae970028355066af14e08231256349
Kai Pastor <dg0yt@darc.de>
2026-01-10 09:14:32 +0100

    [kf5plotting] Fixup

--- ports/kf5plotting/portfile.cmake
+++ ports/kf5plotting/portfile.cmake
@@ -5,7 +5,6 @@ vcpkg_from_github(
     SHA512 c82c72b72100afabc350392e5293f80e374a98c648d707e5386d4f92917833ef1cfb7d090a415162a48517b092f51752668e63099407e267df1ac11d3c2367e2
     HEAD_REF master
 )
-
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
 file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
 
@@ -20,10 +19,6 @@ vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5Plotting)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-if(NOT VCPKG_BUILD_TYPE)
-    file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/plugins" "${CURRENT_PACKAGES_DIR}/debug/plugins")
-endif()
-file(RENAME "${CURRENT_PACKAGES_DIR}/lib/plugins" "${CURRENT_PACKAGES_DIR}/plugins")
 
 file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
 vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
