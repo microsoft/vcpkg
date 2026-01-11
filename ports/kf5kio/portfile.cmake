9e00519c19ff083e4c9d6317274c8db57e19cda6
Kai Pastor <dg0yt@darc.de>
2026-01-11 12:58:53 +0100

    [kf5kio] Revise, cleanup

--- ports/kf5kio/portfile.cmake
+++ ports/kf5kio/portfile.cmake
@@ -7,57 +7,49 @@ vcpkg_from_github(
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    INVERTED_FEATURES
-        "kf5notifications"  CMAKE_DISABLE_FIND_PACKAGE_KF5Notifications
-        "kf5wallet"         CMAKE_DISABLE_FIND_PACKAGE_KF5Wallet
+    FEATURES
+        designer-plugin     BUILD_DESIGNERPLUGIN
+        kf5notifications    VCPKG_LOCK_FIND_PACKAGE_KF5Notifications
+        kf5wallet           VCPKG_LOCK_FIND_PACKAGE_KF5Wallet
+        qml                 VCPKG_LOCK_FIND_PACKAGE_Qt5Qml
 )
-
-# Prevent KDEClangFormat from writing to source effectively blocking parallel configure
-file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DCMAKE_FIND_PACKAGE_TARGETS_GLOBAL=ON
+        -DCMAKE_JOB_POOL_LINK=console # Serialize linking to avoid OOM
         -DBUILD_TESTING=OFF
+        -DCMAKE_DISABLE_FIND_PACKAGE_ACL=ON
+        -DCMAKE_DISABLE_FIND_PACKAGE_Git=ON
         -DCMAKE_DISABLE_FIND_PACKAGE_GSSAPI=ON
+        -DCMAKE_DISABLE_FIND_PACKAGE_KDED=ON
         -DCMAKE_DISABLE_FIND_PACKAGE_KF5DocTools=ON
-        -DCMAKE_VERBOSE_MAKEFILE=ON
-        -DKDE_INSTALL_QTPLUGINDIR=plugins
-        -DKDE_INSTALL_PLUGINDIR=plugins
-        -DKDE_INSTALL_LIBEXECDIR=bin
+        -DCMAKE_DISABLE_FIND_PACKAGE_SwitcherooControl=ON
+        -DKF6_COMPAT_BUILD=ON
         ${FEATURE_OPTIONS}
     MAYBE_UNUSED_VARIABLES
-        CMAKE_DISABLE_FIND_PACKAGE_KF5Notifications
+        VCPKG_LOCK_FIND_PACKAGE_KF5Notifications
+        VCPKG_LOCK_FIND_PACKAGE_KF5Wallet
+
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5KIO)
 vcpkg_copy_pdbs()
 
-set(LIBEXEC_TOOLS kio_http_cache_cleaner kiod5 kioexec kioslave5)
-
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    if(NOT VCPKG_TARGET_IS_ANDROID)
-        list(APPEND LIBEXEC_TOOLS kpac_dhcp_helper)
-    endif()
-
-    list(TRANSFORM LIBEXEC_TOOLS PREPEND "kf5/")
-endif()
-
 vcpkg_copy_tools(
-    TOOL_NAMES kcookiejar5 ktelnetservice5 ktrash5 protocoltojson ${LIBEXEC_TOOLS}
+    TOOL_NAMES kcookiejar5 ktelnetservice5 ktrash5 protocoltojson kio_http_cache_cleaner kiod5 kioexec kioslave5
     AUTO_CLEAN
 )
+if(NOT VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_ANDROID)
+    vcpkg_copy_tools(TOOL_NAMES kpac_dhcp_helper AUTO_CLEAN)
+endif()
 
 file(APPEND "${CURRENT_PACKAGES_DIR}/tools/${PORT}/qt.conf" "Data = ../../share")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
 vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
