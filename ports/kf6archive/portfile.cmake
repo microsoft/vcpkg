c3e9b796f73906d2f9081b0c30a8188858aa1b28
Kai Pastor <dg0yt@darc.de>
2026-01-11 10:59:41 +0100

    [kf6archive] Revise, cleanup

--- ports/kf6archive/portfile.cmake
+++ ports/kf6archive/portfile.cmake
@@ -4,29 +4,34 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 f87fd53ba029b05d3c233ea8d8a9dbb7b2aba2aec55a38b26cfd0f6e1c49d8c1297c06d634175ccc5bbbce00261a3387a2da3c705011e9cbae538eae2723fef3
     HEAD_REF master
+    PATCHES
+        zstd.diff
 )
 
-vcpkg_find_acquire_program(PKGCONFIG)
+vcpkg_check_features(OUT_FEATURE_OPTIONS options
+    FEATURES
+        bzip2           WITH_BZIP2
+        bzip2           VCPKG_LOCK_FIND_PACKAGE_BZip2
+        lzma            WITH_LIBLZMA
+        lzma            VCPKG_LOCK_FIND_PACKAGE_LibLZMA
+        zstd            WITH_LIBZSTD
+        zstd            VCPKG_LOCK_FIND_PACKAGE_LibZstd
+    INVERTED_FEATURES
+        translations    KF_SKIP_PO_PROCESSING
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    DISABLE_PARALLEL_CONFIGURE
     OPTIONS
+        ${options}
         -DBUILD_TESTING=OFF
-        -DPKG_CONFIG_EXECUTABLE=${PKGCONFIG}
+        -DCMAKE_DISABLE_FIND_PACKAGE_Git=1
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF6Archive)
 vcpkg_copy_pdbs()
 
-# Static builds should not have libraries in the bin directory
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/data")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/data")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
