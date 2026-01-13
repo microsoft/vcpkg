22f6c45d30c82a5332bde80a636d980b4f3950d8
Aleks Tuchkov <projects@tuchkov.work>
2026-01-13 11:45:41 +0100

    [jsonfusion] update to use boost-pfr dependency

--- ports/jsonfusion/portfile.cmake
+++ ports/jsonfusion/portfile.cmake
@@ -2,13 +2,12 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tucher/JsonFusion
     REF "v${VERSION}"
-    SHA512 7c37b8a36c0bca64a18bff652f8056ebc19f9eb3ed6ca1f661a050a6019d7ad4a9360df58f9839ec142316256af054f51f7321c94eaa4ce9e6300306406df556
+    SHA512 57312afa55a6ac3f91fa298eb4108114dd74c294418d139c22fc619fa2c7893db224a3e670571c5bde074f91cccba531d1d3e868ec1453158aa7c09c31b36321
     HEAD_REF master
 )
 
 file(INSTALL "${SOURCE_PATH}/include/JsonFusion" 
      DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-file(INSTALL "${SOURCE_PATH}/include/pfr" 
-     DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
 # Install license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
