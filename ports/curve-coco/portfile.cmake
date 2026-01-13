9dd7d0e84877ca9dfce3792702f8b935b2144794
toge <toge.mail@gmail.com>
2026-01-13 17:15:50 +0900

    remove debug/include

--- ports/curve-coco/portfile.cmake
+++ ports/curve-coco/portfile.cmake
@@ -24,4 +24,6 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/coco-${VERSION}" PACKAGE_NAME "coco")
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
