4d245e7e686685d9473f760f1a1a73dffc3ffa9b
toge <toge.mail@gmail.com>
2026-01-15 23:59:41 +0900

    disable LSL_FRAMEWORK

--- ports/liblsl/portfile.cmake
+++ ports/liblsl/portfile.cmake
@@ -16,6 +16,7 @@ vcpkg_cmake_configure(
         -DLSL_BUILD_STATIC=${LSL_BUILD_STATIC}
         -DLSL_BUNDLED_BOOST=OFF # we use the boost vcpkg packages instead
         -DLSL_BUNDLED_PUGIXML=OFF # we use the pugixml vcpkg package instead
+        -DLSL_FRAMEWORK=OFF
         -Dlslgitrevision=v${VERSION}
         -Dlslgitbranch=master
 )
