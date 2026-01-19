a2e60b58aea20e109990daf750ed06de6afc4011
Ethin Probst <ethindp@pm.me>
2026-01-19 10:25:39 -0600

    Update Prism to latest version

--- ports/prism/portfile.cmake
+++ ports/prism/portfile.cmake
@@ -2,10 +2,13 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ethindp/prism
-    REF v0.3.4
-    SHA512 10bbafd5cfd4561846efb9a7b4c7745418158852c33485a3a9c38dfca1e87a931b118f813bdcddf2ce94188c0e1fcc99315b2d7b1f38e7752c7b3b7f1e0b44ff
+    REF v0.4.1
+    SHA512 9058623b6f5a236ffdc4ce3ca42aab341b834250cfac37a39d8fb35854a5ad2a0fa0b97b8346fe3a9613f5e5e0d9117c0e81ecc346f2fcd96c3e1d23a7f2403f
     HEAD_REF master
 )
+if(VCPKG_TARGET_IS_LINUX)
+    message(STATUS "Note: Speech Dispatcher must be installed on the host system (e.g., 'apt install libspeechd-dev') to enable the speech dispatcher backend.")
+endif()
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
@@ -17,8 +20,5 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME prism CONFIG_PATH share/prism)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")  # Add this line
 vcpkg_copy_pdbs()
-if(VCPKG_TARGET_IS_LINUX)
-    message(STATUS "Note: Speech Dispatcher must be installed on the host system (e.g., 'apt install libspeechd-dev').")
-endif()
 set(VCPKG_POLICY_SKIP_MISMATCHED_PDB_CHECK enabled)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
