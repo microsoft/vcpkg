12370a3b2f82dcdc095149919dcb1318b26b436e
Arnout Boelens <arnout.boelens@tatasteeleurope.com>
2026-01-20 14:03:55 +0100

    Versions

--- ports/osqp-eigen/portfile.cmake
+++ ports/osqp-eigen/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "gbionics/osqp-eigen"
-    REF v0.11.0
+    REF "v${VERSION}"
     SHA512 89f3e83dbaf925f7690c11a553c402c3cadda2d33c3f94f25096b11708f9f8753a3f4ef64d632c553399e95467e887fc37972be94fcad74c63de989ad3a1dde4
     PATCHES osqp-eigen.patch
 )
--- ports/qdldl/portfile.cmake
+++ ports/qdldl/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "osqp/qdldl"
-    REF v0.1.9
+    REF "v${VERSION}"
     SHA512 67722bb872cbafe61bdbe4a582cc7e4ebc729a1eca933cc8b758e9a9a5648903ee6ac147c14b33a136c11a647f39ea42cfe3c71147366ed294258b5b66d7d1da
     PATCHES qdldl.patch
 )
