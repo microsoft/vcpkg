868159971c7098c74889bf85a59102268c53fdbd
Dalton Messmer <messmer.dalton@gmail.com>
2026-01-22 02:56:43 -0500

    Add deprecation message to Findasiosdk.cmake

--- ports/asiosdk/Findasiosdk.cmake
+++ ports/asiosdk/Findasiosdk.cmake
@@ -3,6 +3,9 @@ else(WIN32)
   message(FATAL_ERROR "Findasiosdk.cmake: Unsupported platform ${CMAKE_SYSTEM_NAME}" )
 endif(WIN32)
 
+file(READ "${CMAKE_CURRENT_LIST_DIR}/usage" usage)
+message(WARNING "find_package(asiosdk) is deprecated.\n${usage}")
+
 # if this script is invoked multiple times, we end up adding
 # "asiosdk" to the directory multiple times, leading to incorrect
 # include paths
