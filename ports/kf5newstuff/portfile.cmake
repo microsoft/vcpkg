0106609259f5e18222b05635de80add986338f5a
Kai Pastor <dg0yt@darc.de>
2026-01-24 14:53:31 +0100

    Fixup

--- ports/kf5newstuff/portfile.cmake
+++ ports/kf5newstuff/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 7734b5403720e4031d30844361251f744364d109c60dd59e6424cf1aa2f7a5b87f5f81893c0cab5721dc0875fc5e9b6e510436e4485776ec3f30d6d36ffca476
     HEAD_REF master
+    PATCHES
+        disable-macos-bundle.diff
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS options
@@ -15,15 +17,11 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DCMAKE_MACOSX_BUNDLE=OFF
         -DCMAKE_DISABLE_FIND_PACKAGE_Git=1
         -DCMAKE_DISABLE_FIND_PACKAGE_KF5Kirigami2=1
         -DCMAKE_DISABLE_FIND_PACKAGE_KF5Syndication=1
         -DCMAKE_REQUIRE_FIND_PACKAGE_Qt5Quick=1
         ${options}
-        --trace-expand
-    MAYBE_UNUSED_VARIABLES
-        CMAKE_MACOSX_BUNDLE
 )
 
 vcpkg_cmake_install()
