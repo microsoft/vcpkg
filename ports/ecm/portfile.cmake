f14401ca0f2754347c3864da7488a9b955b4e47a
Kai Pastor <dg0yt@darc.de>
2026-01-25 09:49:26 +0100

    [ecm] Force vcpkg filesystem, revise downstream (#34091)

--- ports/ecm/portfile.cmake
+++ ports/ecm/portfile.cmake
@@ -1,3 +1,7 @@
+# cmake-scripts only
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+set(VCPKG_BUILD_TYPE release)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO KDE/extra-cmake-modules
@@ -7,6 +11,12 @@ vcpkg_from_github(
     PATCHES
         fix_generateqmltypes.patch # https://invent.kde.org/frameworks/extra-cmake-modules/-/merge_requests/201
         fix-wrong-version.patch
+        # Adjust default installation dirs to vcpkg layout, reduce cross-platform variation
+        uniform-dataroot-dir.patch
+        uniform-libexec-dir.patch
+        uniform-plugin-dir.patch
+        # Avoid race while configuring downstream ports
+        kde-clang-format.diff
 )
 
 vcpkg_cmake_configure(
@@ -19,15 +29,8 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
 vcpkg_cmake_config_fixup(CONFIG_PATH share/ECM/cmake)
 
-# Remove debug files
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
+file(COPY "${CURRENT_PORT_DIR}/vcpkg-port-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(COPY "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING-CMAKE-SCRIPTS")
-
-# Allow empty include directory
-set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
--- ports/kf5kcmutils/portfile.cmake
+++ ports/kf5kcmutils/portfile.cmake
@@ -6,38 +6,20 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-# Prevent KDEClangFormat from writing to source effectively blocking parallel configure
-file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
-
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DKDE_INSTALL_QMLDIR=qml
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/KF5KCMUtils)
 vcpkg_copy_pdbs()
 
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    set(LIBEXEC_FOLDER "lib/libexec")
-    set(LIBEXEC_SUBFOLDER "kf5/")
-else()
-    set(LIBEXEC_FOLDER "bin")
-    set(LIBEXEC_SUBFOLDER "")
-endif()
-
 vcpkg_copy_tools(
     TOOL_NAMES kcmdesktopfilegenerator
-    SEARCH_DIR "${CURRENT_PACKAGES_DIR}/${LIBEXEC_FOLDER}/${LIBEXEC_SUBFOLDER}"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/tools/${PORT}/${LIBEXEC_SUBFOLDER}"
     AUTO_CLEAN
 )
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
