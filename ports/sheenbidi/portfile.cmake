a1e45e9993be31a4268497ca9e852af64ab9267b
Muhammad Tayyab Akram <dear_tayyab@yahoo.com>
2026-01-26 11:59:30 +0500

    [sheenbidi] Remove patch

--- ports/sheenbidi/portfile.cmake
+++ ports/sheenbidi/portfile.cmake
@@ -3,8 +3,6 @@ vcpkg_from_github(
     REPO Tehreer/SheenBidi
     REF "v${VERSION}"
     SHA512 9efd97b3c212350debaf3e0f3cf022f6b4c93e3de80a77ed92ddf34a2da23c1ddeb3dc5d90b357bd7f3cb3c8c8780c045b7c897836d771bdc96e35d2b9b1935b
-    PATCHES
-        cmake-install.patch
 )
 
 vcpkg_cmake_configure(
