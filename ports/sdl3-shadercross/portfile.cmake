31cbf54299fa3f6106f1fe368dc1bfc28aee2809
Sebastian Schäfer <Agorath@users.noreply.github.com>
2026-01-26 21:45:07 +0100

    DirectXShaderCompiler include search removed.
    This should, hopefully, finally fix the build problems caused by not finding the non-needed DXC headers.

--- ports/sdl3-shadercross/portfile.cmake
+++ ports/sdl3-shadercross/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF 7b7365a86611b2a7b6462e521cf1c43a037d0970
     SHA512 52efd2c2507d6ae874cdc177945e15494920f11148e9e9cf8da27fb5ccacb5fcbe44581005e132a84631e9d438616aa1247b7ae23f4ef1785203cdcb08af19af
     HEAD_REF main
+    PATCHES
+        fix-directx-shader-compiler-includes.patch
 )
 
 vcpkg_cmake_configure(
@@ -14,21 +16,20 @@ vcpkg_cmake_configure(
         -DSDLSHADERCROSS_INSTALL_RUNTIME=OFF
         -DSDLSHADERCROSS_SPIRVCROSS_SHARED=OFF
         -DSDLSHADERCROSS_VENDORED=OFF
-        -DDirectXShaderCompiler_INCLUDE_PATH=${CURRENT_INSTALLED_DIR}/include/directx-dxc
 )
 
 vcpkg_cmake_install()
-vcpkg_copy_pdbs()
-vcpkg_fixup_pkgconfig()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME "sdl3_shadercross")
-
-vcpkg_copy_tools(TOOL_NAMES shadercross AUTO_CLEAN)
+vcpkg_cmake_config_fixup(PACKAGE_NAME "sdl3_shadercross" CONFIG_PATH "share/sdl3_shadercross/SDL3_shadercross")
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
 )
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
+
+vcpkg_copy_tools(TOOL_NAMES shadercross AUTO_CLEAN)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sdl3_shadercross")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
