3708b4646c795909fcb5d119e432eaeded087736
Sebastian Schäfer <Agorath@users.noreply.github.com>
2026-01-26 21:55:30 +0100

    usage was in the wrong directory.

--- ports/sdl3-shadercross/portfile.cmake
+++ ports/sdl3-shadercross/portfile.cmake
@@ -31,5 +31,5 @@ vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_tools(TOOL_NAMES shadercross AUTO_CLEAN)
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/sdl3_shadercross")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
