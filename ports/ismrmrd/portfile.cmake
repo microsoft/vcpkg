4c3df477469dbd38f647fb6de90dfa2526bef1e3
SunBlack <SunBlack@users.noreply.github.com>
2026-01-26 11:34:33 +0100

    [ismrmrd] Update to 1.14.3

--- ports/ismrmrd/portfile.cmake
+++ ports/ismrmrd/portfile.cmake
@@ -1,51 +1,37 @@
-if (VCPKG_TARGET_ARCHITECTURE MATCHES "x86")
-    set(WIN32_INCLUDE_STDDEF_PATCH "x86-windows-include-stddef.patch")
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ismrmrd/ismrmrd
     REF "v${VERSION}"
-    SHA512 4bf3fbab89436636c64e4917d09117134f4d1c6446bae7ef570e0af5850998f066d881f45c414f40e1d1be942f34fda6836a1b3e4cb5ec904f9ecf2a432964c1
+    SHA512 1e91b78f49c6df0e09d7d6e92f21ad26ed68bcb17cce9d5ae6233d567d16d93e4007a5a76c1b0797581bed68b60ec70a886843d355a1dc98b98dd515f0c177e0
     HEAD_REF master
     PATCHES
-        ${WIN32_INCLUDE_STDDEF_PATCH}
-        fix-depends-hdf5.patch
-        fix-nodiscard-warning.patch
+        win32_runtime_fix.patch
 )
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        hdf5         USE_HDF5_DATASET_SUPPORT
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DUSE_HDF5_DATASET_SUPPORT=ON
-        -DVCPKG_TARGET_TRIPLET=ON
         -DBUILD_TESTS=OFF
         -DBUILD_EXAMPLES=OFF
         -DBUILD_UTILITIES=OFF
+        -DBUILD_STATIC=${BUILD_STATIC}
+        ${FEATURE_OPTIONS}
 )
 
+vcpkg_cmake_build() # For some reason the install target doesn't build the library before
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ISMRMRD/)
 
-if(EXISTS "${CURRENT_PACKAGES_DIR}/lib/ismrmrd.dll")
-    file(COPY "${CURRENT_PACKAGES_DIR}/lib/ismrmrd.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/ismrmrd.dll")
-endif()
-
-if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/lib/ismrmrd.dll")
-    file(COPY "${CURRENT_PACKAGES_DIR}/debug/lib/ismrmrd.dll" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/ismrmrd.dll")
-endif()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/ismrmrd/cmake")
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/")
-endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
