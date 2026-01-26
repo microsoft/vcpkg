c7fd81bfc8cf21714dfed5ee6d64a8554cf2cd71
Markus87 <Markus_Schoder@gmx.at>
2026-01-26 13:14:46 +0100

    Replaced vcpkg-cmake-wrapper with upstream CMake target

--- ports/anyxx/portfile.cmake
+++ ports/anyxx/portfile.cmake
@@ -1,19 +1,22 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO bitfactory-software/anyxx
-    REF "04668bd1874a0adf555c8674571a29f0cbef131b"
-    SHA512 66009802fae4f10c0fc043d92f244a257881a26aa53df57c85890ea03ead81994c3dde39cde9a11906a1529018b826b9cb1a92c4be2d6acab67d4924a60347c2
+    REF "${VERSION}"
+    SHA512 2dff8f7def0489a469485f3130d0d7f7dcaec675f46110504b60c7a2b8247ca302b523316011c0172154a4ff833f6e903eea8e99f958cfd4b24d32327095f234
     HEAD_REF master
 )
 
-# This is a header only library
-file(INSTALL "${SOURCE_PATH}/bit_factory/anyxx.hpp" DESTINATION "${CURRENT_PACKAGES_DIR}/include/bit_factory")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+	OPTIONS
+		-Danyxx_INSTALL_ONLY=ON
+)
 
-# Supply usage file
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "anyxx")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
-# Handle copyright
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME "copyright")
-
-# Install the wrapper so find_package() works
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
