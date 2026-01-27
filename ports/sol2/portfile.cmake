b55d7789e8472ab0e662770cc963d3efbb953c98
Kai Pastor <dg0yt@darc.de>
2026-01-27 08:10:07 +0100

    [sol2] Fix cmake config fixup

--- ports/sol2/portfile.cmake
+++ ports/sol2/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_from_github(
 set(VCPKG_BUILD_TYPE release) # header-only
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sol2)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/sol2)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
