3bde8377e6f116990770d102414d4c8f8347e9c3
Kai Pastor <dg0yt@darc.de>
2026-01-27 09:48:32 +0100

    [osqp] Repair, cleanup

--- ports/osqp/portfile.cmake
+++ ports/osqp/portfile.cmake
@@ -3,7 +3,8 @@ vcpkg_from_github(
     REPO "osqp/osqp"
     REF "v${VERSION}"
     SHA512 00ead2c476aca935202c2a02e5a0309efee6db65ec4e7c56f3597324a2f224a16502a34e7552cd5600c085d327c308317894718f9ac825ec669895ac19a45c41
-    PATCHES osqp.patch
+    PATCHES
+        osqp.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" OSQP_BUILD_STATIC_LIB)
@@ -18,11 +19,10 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(
-    CONFIG_PATH "lib/cmake/osqp"
-)
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/osqp")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(COPY "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
