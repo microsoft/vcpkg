cb43eb3e3a348d293cf47cc45f22b357bf1bfd2a
Radames S. Cruz Moreno <racruzmo@microsoft.com>
2026-01-28 14:02:54 -0800

    Apply code review changes
    Remove development code from portfile.cmake
    Remove 'supports: static' from vcpkg.json

--- ports/cppvrf/portfile.cmake
+++ ports/cppvrf/portfile.cmake
@@ -1,18 +1,12 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-if(DEFINED ENV{CPPVRF_SOURCE_PATH})
-    # Development option: use local source tree by setting
-    # CPPVRF_SOURCE_PATH environment variable.
-    set(SOURCE_PATH "$ENV{CPPVRF_SOURCE_PATH}")
-else()
-    vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO Microsoft/cppvrf
-        REF "v${VERSION}"
-        SHA512 92f99477ad6d2ea2ff8214be30277f161e14d735b5019942919696af5aec14e088960074b6e534b58c7afa9487a71954ed271e81292b301bdbb1ff1679aa46ea
-        HEAD_REF main
-    )
-endif()
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO Microsoft/cppvrf
+	REF "v${VERSION}"
+	SHA512 92f99477ad6d2ea2ff8214be30277f161e14d735b5019942919696af5aec14e088960074b6e534b58c7afa9487a71954ed271e81292b301bdbb1ff1679aa46ea
+	HEAD_REF main
+)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 
