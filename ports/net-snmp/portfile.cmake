1f741e9e984c4e14a22a5cb52491a4d4d8a37ebe
camelcoding <mss_de@yahoo.de>
2026-01-28 22:42:45 +0100

    get rid of loop use vcpkg_build_nmake directly with PRERUN_SHELL_RELEASE and PRERUN_SHELL_DEBUG cleanup code update version

--- ports/net-snmp/portfile.cmake
+++ ports/net-snmp/portfile.cmake
@@ -24,122 +24,125 @@ vcpkg_find_acquire_program(PERL)
 get_filename_component(PERL_PATH ${PERL} DIRECTORY)
 vcpkg_add_to_path("${PERL_PATH}")
 
-if (NOT DEFINED VCPKG_BUILD_TYPE)
-    list(APPEND BUILD_TYPES "release")
-    list(APPEND BUILD_TYPES "debug")
-elseif(VCPKG_BUILD_TYPE STREQUAL "release")
-    list(APPEND BUILD_TYPES "release")
-elseif(VCPKG_BUILD_TYPE STREQUAL "debug")
-    list(APPEND BUILD_TYPES "debug")
+set(NET_SNMP_FEATURE_LIST "")
+list(APPEND NET_SNMP_FEATURE_LIST "--with-sdk")    
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    list(APPEND NET_SNMP_FEATURE_LIST "--linktype=static")    
+else()
+    list(APPEND NET_SNMP_FEATURE_LIST "--linktype=dynamic")    
 endif()
 
-foreach(BUILD_TYPE ${BUILD_TYPES})
+set(TARGET_DIR "${CURRENT_PACKAGES_DIR}")
+set(TARGET_DIR_DEBUG "${CURRENT_PACKAGES_DIR}/debug")
 
-    set(NET_SNMP_FEATURE_LIST "")
-    list(APPEND NET_SNMP_FEATURE_LIST "--with-sdk")    
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-        list(APPEND NET_SNMP_FEATURE_LIST "--linktype=static")    
-    else()
-        list(APPEND NET_SNMP_FEATURE_LIST "--linktype=dynamic")    
-    endif()
+if("ssl" IN_LIST FEATURES)
+    list(APPEND NET_SNMP_FEATURE_LIST "--with-ssl")    
+    list(APPEND NET_SNMP_FEATURE_LIST "--with-sslincdir=${CURRENT_INSTALLED_DIR}/include")    
+    list(APPEND NET_SNMP_FEATURE_LIST "--with-ssllibdir=${CURRENT_INSTALLED_DIR}/lib")    
+endif()
 
-    set(BUILD_DIR "${SOURCE_PATH}/win32/")
-    if(BUILD_TYPE STREQUAL "release")
-        set(TARGET_DIR "${SOURCE_PATH}/release_install")
-    else()
-        set(TARGET_DIR "${SOURCE_PATH}/debug_install")
-    endif()
+list(JOIN NET_SNMP_FEATURE_LIST " " NET_SNMP_FEATURES)
+message(INFO " active features:${NET_SNMP_FEATURES}")
 
-    list(APPEND NET_SNMP_FEATURE_LIST "--config=${BUILD_TYPE}")
-    list(APPEND NET_SNMP_FEATURE_LIST "--prefix=${TARGET_DIR}")
+set(TARGETS "")
+if ("tools" IN_LIST FEATURES) 
+    set(TARGETS all)
+else()
+    set(TARGETS libs)
+endif()
 
-    if("ssl" IN_LIST FEATURES)
-        list(APPEND NET_SNMP_FEATURE_LIST "--with-ssl")    
-        list(APPEND NET_SNMP_FEATURE_LIST "--with-sslincdir=${CURRENT_INSTALLED_DIR}/include")    
-        list(APPEND NET_SNMP_FEATURE_LIST "--with-ssllibdir=${CURRENT_INSTALLED_DIR}/lib")    
-    endif()
+vcpkg_build_nmake(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH win32
+    PRERUN_SHELL_RELEASE "${PERL}" Configure ${NET_SNMP_FEATURE_LIST} --config=release --prefix=${TARGET_DIR}
+    PRERUN_SHELL_DEBUG "${PERL}" Configure ${NET_SNMP_FEATURE_LIST} --config=debug --prefix=${TARGET_DIR_DEBUG}
+    PROJECT_NAME "Makefile"
+    TARGET ${TARGETS} install install_devel
+    LOGFILE_ROOT build-net-snmp
+)
 
-    list(JOIN NET_SNMP_FEATURE_LIST " " NET_SNMP_FEATURES)
-    message(INFO " active features:${NET_SNMP_FEATURES}")
-
-    set(TARGETS "")
-    if ("tools" IN_LIST FEATURES) 
-        set(TARGETS all)
-    else()
-        set(TARGETS libs)
-    endif()
-
-    vcpkg_build_nmake(
-        SOURCE_PATH "${SOURCE_PATH}"
-        PROJECT_SUBPATH win32
-        PRERUN_SHELL "${PERL}" Configure ${NET_SNMP_FEATURE_LIST}
-        PROJECT_NAME "Makefile"
-        TARGET ${TARGETS} install
-        OPTIONS_RELEASE install_devel
-        LOGFILE_ROOT build-${BUILD_TYPE}
+# remove not needed files
+file(REMOVE
+    "${TARGET_DIR}/bin/mib2c"
+    "${TARGET_DIR}/bin/mib2c.bat"
+    "${TARGET_DIR}/bin/snmpconf"
+    "${TARGET_DIR}/bin/snmpconf.bat"
+    "${TARGET_DIR}/bin/traptoemail"
+    "${TARGET_DIR}/bin/traptoemail.bat"
+    "${TARGET_DIR_DEBUG}/bin/mib2c"
+    "${TARGET_DIR_DEBUG}/bin/mib2c.bat"
+    "${TARGET_DIR_DEBUG}/bin/snmpconf"
+    "${TARGET_DIR_DEBUG}/bin/snmpconf.bat"
+    "${TARGET_DIR_DEBUG}/bin/traptoemail"
+    "${TARGET_DIR_DEBUG}/bin/traptoemail.bat"
     )
 
-    # remove not needed files
-    file(REMOVE
-        "${TARGET_DIR}/bin/mib2c"
-        "${TARGET_DIR}/bin/mib2c.bat"
-        "${TARGET_DIR}/bin/snmpconf"
-        "${TARGET_DIR}/bin/snmpconf.bat"
-        "${TARGET_DIR}/bin/traptoemail"
-        "${TARGET_DIR}/bin/traptoemail.bat")
+file(REMOVE_RECURSE
+    "${TARGET_DIR}/etc"
+    "${TARGET_DIR}/temp"
+    "${TARGET_DIR}/snmp"
+    "${TARGET_DIR_DEBUG}/etc"
+    "${TARGET_DIR_DEBUG}/temp"
+    "${TARGET_DIR_DEBUG}/snmp"
+)
 
-    file(REMOVE_RECURSE
-        "${TARGET_DIR}/etc"
-        "${TARGET_DIR}/temp"
-        "${TARGET_DIR}/snmp"
-    )
-
-    file(GLOB LIB_CANDIDATES "${BUILD_DIR}/lib/${BUILD_TYPE}/*")
-    set(LIB_FILES)
-    foreach(_f IN LISTS LIB_CANDIDATES)
-        if(NOT IS_DIRECTORY "${_f}")
-            list(APPEND LIB_FILES "${_f}")
-        endif()
-    endforeach()
-    if(LIB_FILES)
-        file(MAKE_DIRECTORY "${TARGET_DIR}/lib")
-        file(COPY ${LIB_FILES} DESTINATION "${TARGET_DIR}/lib")
+file(GLOB LIB_CANDIDATES "${TARGET_DIR}/lib/release/*")
+set(LIB_FILES)
+foreach(_f IN LISTS LIB_CANDIDATES)
+    if(NOT IS_DIRECTORY "${_f}")
+        list(APPEND LIB_FILES "${_f}")
     endif()
-
-    SET(DEST "")
-
-    file(REMOVE
-        "${TARGET_DIR}/include/net-snmp/net-snmp-config.h"
-        "${TARGET_DIR}/lib/netsnmp.exp"
-    )
-
-    if(BUILD_TYPE STREQUAL "release")
-        SET(DEST "${CURRENT_PACKAGES_DIR}")
-    else()
-        SET(DEST "${CURRENT_PACKAGES_DIR}/debug")
-        #remove header
-        file(REMOVE_RECURSE 
-             "${TARGET_DIR}/include"
-             "${TARGET_DIR}/share"
-        )
-    endif()
-
-    file(INSTALL
-        "${TARGET_DIR}/"
-        DESTINATION
-        "${DEST}"
-    )
-
-    if ("tools" IN_LIST FEATURES) 
-        vcpkg_copy_tools(
-            TOOL_NAMES encode_keychange snmpbulkget snmpbulkwalk snmpd snmpdelta snmpdf snmpget snmpgetnext snmpnetstat snmpset snmpstatus snmptable snmptest snmptranslate snmptrap snmptrapd snmpusm snmpvacm snmpwalk
-            SEARCH_DIR "${TARGET_DIR}/bin"
-            DESTINATION "${DEST}/tools"
-            AUTO_CLEAN
-        )
-    endif()
-
 endforeach()
+if(LIB_FILES)
+    file(MAKE_DIRECTORY "${TARGET_DIR}/lib")
+    file(COPY ${LIB_FILES} DESTINATION "${TARGET_DIR}/lib")
+endif()
+
+file(GLOB LIB_CANDIDATES_DEBUG "${TARGET_DIR_DEBUG}/lib/debug/*")
+set(LIB_FILES_DEBUG "")
+foreach(_f IN LISTS LIB_CANDIDATES_DEBUG)
+    if(NOT IS_DIRECTORY "${_f}")
+        list(APPEND LIB_FILES_DEBUG "${_f}")
+    endif()
+endforeach()
+if(LIB_FILES_DEBUG)
+    file(MAKE_DIRECTORY "${TARGET_DIR_DEBUG}/lib")
+    file(COPY ${LIB_FILES_DEBUG} DESTINATION "${TARGET_DIR_DEBUG}/lib")
+endif()
+
+SET(DEST "")
+
+file(REMOVE
+    "${TARGET_DIR}/include/net-snmp/net-snmp-config.h"
+    "${TARGET_DIR}/lib/netsnmp.exp"
+)
+
+file(REMOVE_RECURSE 
+    "${TARGET_DIR_DEBUG}/include"
+    "${TARGET_DIR_DEBUG}/share"
+)
+
+file(INSTALL
+    "${TARGET_DIR}/"
+    DESTINATION
+    "${CURRENT_PACKAGES_DIR}"
+)
+
+file(INSTALL
+    "${TARGET_DIR_DEBUG}/"
+    DESTINATION
+    "${CURRENT_PACKAGES_DIR}/debug"
+)
+
+if ("tools" IN_LIST FEATURES) 
+    vcpkg_copy_tools(
+        TOOL_NAMES encode_keychange snmpbulkget snmpbulkwalk snmpd snmpdelta snmpdf snmpget snmpgetnext snmpnetstat snmpset snmpstatus snmptable snmptest snmptranslate snmptrap snmptrapd snmpusm snmpvacm snmpwalk
+        SEARCH_DIR "${TARGET_DIR}/bin"
+        DESTINATION "${CURRENT_PACKAGES_DIR}/tools"
+        AUTO_CLEAN
+    )
+endif()
+
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
 
