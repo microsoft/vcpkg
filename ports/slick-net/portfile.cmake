971b0cff488ecf46d077b4ea32cbd7da42662614
kzhdev <kzhdev@gmail.com>
2026-01-29 14:11:05 -0800

    [slick-net] update to v1.2.3 (#49683)

--- ports/slick-net/portfile.cmake
+++ ports/slick-net/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SlickQuant/slick-net
     REF "v${VERSION}"
-    SHA512 5871099ac415fb8afa223798e5590afeded1e7a40aa071e727408e07bb8920293354f16024cbe6cbfb53c0935de8be27828f4681e53ea790e5404f06dbcdf603
+    SHA512 6e7e64ff7ccc9b384ece333a3f3a1f1185743f0f443c3f3b6ff28c15f5790b9efbfaa5f1db5d74058f522072ffc62148874b56628c5ddb9da7776ca3582d9eb1
     HEAD_REF main
     PATCHES
         slick-queue.patch
