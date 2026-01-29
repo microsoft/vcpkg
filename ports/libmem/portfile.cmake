661ceec1c249323722bee1415de7a153e6e09a19
Saikari <lin@sz.cn.eu.org>
2026-01-29 05:10:38 +0300

    Fix SHA512 checksum for libmem in portfile.cmake

--- ports/libmem/portfile.cmake
+++ ports/libmem/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rdbo/libmem
     REF "${VERSION}"
-    SHA512 0
+    SHA512 1accb68bf0af12079c273f88af451e0eebd1550ba166b2821ea63fb810bf0debb95cef406d6624605b831a7e7adb7523ae2c267a0b67c5a6bdfff45f4c6221f4
     HEAD_REF master
     PATCHES
         0001-CMakeLists.patch
