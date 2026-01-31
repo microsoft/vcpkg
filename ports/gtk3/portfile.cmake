microsoft/vcpkg

commit 37745174adcaae3b9873d090a150c429d5b45fc5

gtk3: support Wayland on UNIX (#49571)
Signed-off-by: Rafael Kitover <rkitover@gmail.com>

ports/gtk3/portfile.cmake
@@ -38,11 +38,16 @@ else()
     list(APPEND OPTIONS_RELEASE -Dintrospection=false)
 endif()
 
+if(VCPKG_TARGET_IS_LINUX OR VCPKG_TARGET_IS_FREEBSD OR VCPKG_TARGET_IS_OPENBSD)
+    list(APPEND OPTIONS -Dwayland_backend=true)
+else()
+    list(APPEND OPTIONS -Dwayland_backend=false)
+endif()
+
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${OPTIONS}
-        -Dwayland_backend=false
         -Ddemos=false
         -Dexamples=false
         -Dtests=false

commit bb5b432f5f2bdd89171136afb11bb48ae81328d2

[gtk3, gstreamer] Avoid multiple definition of the symbol `properties`.
Hopefully resolves build error on x64-linux: https://dev.azure.com/vcpkg/public/_build/results?buildId=126505

```console
/usr/bin/ld: /mnt/vcpkg-ci/installed/x64-linux/debug/lib/pkgconfig/../../lib/libgtk-3.a(inspector_size-groups.c.o):/mnt/vcpkg-ci/b/gtk3/x64-linux-dbg/../src/NOME-gtk-3-f64471d55d.clean/gtk/inspector/size-groups.c:47: multiple definition of `properties'; subprojects/gstreamer/plugins/elements/libgstcoreelements.a(gstqueue.c.o):/mnt/vcpkg-ci/b/gstreamer/x64-linux-dbg/../src/1.26.5-9c0ab21028.clean/subprojects/gstreamer/plugins/elements/gstqueue.c:130: first defined here
```

I am not sure how to submit these patches to either gtk3 or gstreamer.

This failure started happening between January 19 and January 21; probably caused by #49571

ports/gstreamer/fix-multiple-def.patch
@@ -33,3 +33,16 @@ index e09e533..123b888 100644
  extern float step_size_inverse[8];
  
  
+diff --git a/subprojects/gstreamer/plugins/elements/gstqueue.c b/subprojects/gstreamer/plugins/elements/gstqueue.c
+index 8a654cc..3f98a9d 100644
+--- a/subprojects/gstreamer/plugins/elements/gstqueue.c
++++ b/subprojects/gstreamer/plugins/elements/gstqueue.c
+@@ -127,7 +127,7 @@ enum
+   PROP_LAST
+ };
+ 
+-GParamSpec *properties[PROP_LAST];
++static GParamSpec *properties[PROP_LAST];
+ 
+ /* default property values */
+ #define DEFAULT_MAX_SIZE_BUFFERS  200   /* 200 buffers */

ports/gtk3/avoid-multiple-definition.diff
@@ -1,0 +1,13 @@
+diff --git a/gtk/inspector/size-groups.c b/gtk/inspector/size-groups.c
+index eb185ed..f7c604d 100644
+--- a/gtk/inspector/size-groups.c
++++ b/gtk/inspector/size-groups.c
+@@ -44,7 +44,7 @@ enum {
+   LAST_PROPERTY
+ };
+ 
+-GParamSpec *properties[LAST_PROPERTY] = { NULL };
++static GParamSpec *properties[LAST_PROPERTY] = { NULL };
+ 
+ GType size_group_row_get_type (void);
+ 

ports/gtk3/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_extract_source_archive(SOURCE_PATH
         0001-build.patch
         cairo-cpp-linkage.patch
         egl-conditional.diff # https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9067
+        avoid-multiple-definition.diff
 )
 
 vcpkg_find_acquire_program(PKGCONFIG)