94c54a6761417695a93351a77a3f0cb06171bf00
Prachya Saechua <psaechua@cpaxtra.co.th>
2026-02-04 01:20:25 +0700

    Update git-tree for neoactivemq-cpp version 1.1.5
    Signed-off-by: Prachya Saechua <psaechua@cpaxtra.co.th>

--- ports/neoactivemq-cpp/portfile.cmake
+++ ports/neoactivemq-cpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO blackb1rd/neoactivemq-cpp
     REF "v${VERSION}"
-    SHA512 6de73236e382cf7e154972b85aef0197e234279451c46657c018fd41b5e9b834753ed51392d14bb8ec94e4ccccf06efef217b090fa3f3e0bbd595239c28fe29c
+    SHA512 b9e70c2eb156b84770586aaedda1de5450787ad041871ccf34f87f5239e8c8d162fd1db4afc5c6603a5a1fd8f6a93672b66fdac796736c98316f6ebef78be98e
     HEAD_REF main
 )
 
@@ -20,7 +20,6 @@ vcpkg_cmake_configure(
         -DAMQCPP_BUILD_EXAMPLES=OFF
         -DAMQCPP_BUILD_TESTS=OFF
         -DAMQCPP_SHARED_LIB=${AMQCPP_SHARED_LIB}
-        -DAMQCPP_DISABLE_CCACHE=ON
 )
 
 vcpkg_cmake_install()
