a0310710215fd3368ad695b461475025e38748ce
Yoctopuce <dev@yoctopuce.com>
2026-02-04 11:14:35 +0100

    remove patch and add copyright
    Signed-off-by: Yoctopuce <dev@yoctopuce.com>

--- ports/yoctolib/portfile.cmake
+++ ports/yoctolib/portfile.cmake
@@ -4,10 +4,6 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 ed405d77c05288e123851a79e86beaf9778cce487c5d5d4a556f47b3a690517e71b004e5b3e0ae5532cb24ed46a1c04ce4f18c34cccf475fc1ca45a331808c43
     HEAD_REF master
-    PATCHES
-        001-cmake_config.patch
-        002-add_missing_win32_bcrypt_linkage.patch
-        003-fix_win32_shared_build.patch
 )
 
 if(VCPKG_TARGET_IS_LINUX)
@@ -24,7 +20,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-yoctolib)
+vcpkg_cmake_config_fixup(PACKAGE_NAME yoctolib)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
