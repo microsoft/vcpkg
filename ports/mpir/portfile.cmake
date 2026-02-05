e2860d8451402592a6458426023c5a28d2cc355a
Kai Pastor <dg0yt@darc.de>
2026-02-05 21:32:12 +0100

    [mpir] Port to vcpkg-make (#49799)

--- ports/mpir/portfile.cmake
+++ ports/mpir/portfile.cmake
@@ -5,38 +5,31 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO wbhart/mpir
-    REF cdd444aedfcbb190f00328526ef278428702d56e # 3.0.0
+    REF cdd444aedfcbb190f00328526ef278428702d56e # tag: mpir-3.0.0
     SHA512 f46e45bdba27c9f89953ba23186b694486fd3010bd370ea2de71a4649a2816e716a6520c9baa96936f1884437ef03f92b21c0b1fb5b757beba5a05fed30b2bfc
     HEAD_REF master
     PATCHES 
         enable-runtimelibrary-toggle.patch
         fix-static-include-files.patch
+        subdirs.diff
+        vcpkg-cross-builds.diff
 )
 
-if(NOT VCPKG_TARGET_IS_WINDOWS)
+if(NOT VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_MINGW)
     vcpkg_find_acquire_program(YASM)
 
-    set(SHARED_STATIC --disable-static --enable-shared)
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-        set(SHARED_STATIC --enable-static --disable-shared --with-pic)
-    endif()
-
-    set(OPTIONS --disable-silent-rules --enable-gmpcompat --enable-cxx ${SHARED_STATIC})
-
-    string(APPEND VCPKG_C_FLAGS " -Wno-implicit-function-declaration")
-    string(APPEND VCPKG_CXX_FLAGS " -Wno-implicit-function-declaration")
-
-    # on Linux, autoconf is required; on macOS, it isn't
-    if(VCPKG_TARGET_IS_LINUX)
-        set(AUTOCONFIG "AUTOCONFIG")
-    endif()
-
-    vcpkg_configure_make(
+    vcpkg_make_configure(
         SOURCE_PATH "${SOURCE_PATH}"
-        ${AUTOCONFIG}
-        OPTIONS ${OPTIONS}
+        AUTORECONF
+        OPTIONS
+            --enable-cxx
+            --enable-gmpcompat
+            --with-pic
+            "--with-yasm=${YASM}"
+            "CFLAGS=\$CFLAGS -Wno-implicit-function-declaration"
+            "CCAS=" # use default $CC -c
     )
-    vcpkg_install_make()
+    vcpkg_make_install()
 else()
     set(MSVC_VERSION 14)
     if(VCPKG_PLATFORM_TOOLSET MATCHES "v14(1|2|3)")
@@ -72,12 +65,9 @@ else()
     file(REMOVE "${CURRENT_PACKAGES_DIR}/lib/COPYING.LIB" "${CURRENT_PACKAGES_DIR}/debug/lib/COPYING.LIB")
 endif()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-endif()
 file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/debug/include"
-        "${CURRENT_PACKAGES_DIR}/debug/share"
-    )
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING" "${SOURCE_PATH}/COPYING.LIB")
