b0cb3cbe2a212d8968e2a78898848f962d790116
kzhdev <kzhdev@gmail.com>
2026-02-06 14:54:33 -0600

    Fix build errors

--- ports/slick-socket/portfile.cmake
+++ ports/slick-socket/portfile.cmake
@@ -6,16 +6,29 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
+# Header-only library (header-only wrapper, links to wepoll on Windows)
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_SLICK_SOCKET_EXAMPLES=OFF
+        -DBUILD_SLICK_SOCKET_TESTING=OFF
+)
+
 vcpkg_cmake_install()
 
-# Fix up CMake config files before removing lib directory
+# Fix up CMake config files
 vcpkg_cmake_config_fixup(
     PACKAGE_NAME slick-socket
     CONFIG_PATH lib/cmake/slick-socket
 )
 
-# Header-only library
+# Header-only library - remove lib and debug directories
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 # Install license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Install usage
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
