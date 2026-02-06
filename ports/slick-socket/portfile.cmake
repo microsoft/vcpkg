6d95c64e00277004257b4ab945060dea9c51cc1c
kzhdev <kzhdev@gmail.com>
2026-02-06 14:35:28 -0600

    Addressed CR comments: Removed wepoll from codebase, added it as dependency; removeed usage file

--- ports/slick-socket/portfile.cmake
+++ ports/slick-socket/portfile.cmake
@@ -2,33 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SlickQuant/slick-socket
     REF "v${VERSION}"
-    SHA512 d5e4bca03f6ccf0fd49df20917bb1f33408c03c2beb2a1f5c9bb43a6211151364706d43c0b004a0dc5184f38482bf67cc244792675b01d81c896a5d1789e20ce
+    SHA512 6fe2a2fa8d5b1ad8abac365027438ee98552ba7c67a0dae508df1a28f3416647c3804b421427491eb2c593d4be070f639034df054fd5b12bf65e156d18b0a31c
     HEAD_REF main
-)
-
-# On Windows, only static library is supported (shared has no exports)
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-endif()
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    set(BUILD_STATIC ON)
-    set(BUILD_SHARED OFF)
-else()
-    set(BUILD_STATIC OFF)
-    set(BUILD_SHARED ON)
-endif()
-
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DBUILD_SLICK_SOCKET_EXAMPLES=OFF
-        -DBUILD_SLICK_SOCKET_TESTING=OFF
-        -DBUILD_SLICK_SOCKET_STATIC_LIBS=${BUILD_STATIC}
-        -DBUILD_SLICK_SOCKET_SHARED_LIBS=${BUILD_SHARED}
-    MAYBE_UNUSED_VARIABLES
-        BUILD_SLICK_SOCKET_STATIC_LIBS
-        BUILD_SLICK_SOCKET_SHARED_LIBS
 )
 
 vcpkg_cmake_install()
@@ -39,15 +14,8 @@ vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/slick-socket
 )
 
-# Remove duplicate headers from debug build
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-    # Header-only library in linux and macos
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
-endif()
+# Header-only library
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
 
 # Install license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
