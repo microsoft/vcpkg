aa2d37682e3318d93aef87efa7b0e88e81cd3d59
SunBlack <SunBlack@users.noreply.github.com>
2026-02-07 05:37:58 +0100

    [workflow] Split Windows branch to separate port (#48855)

--- ports/workflow-win/portfile.cmake
+++ ports/workflow-win/portfile.cmake
@@ -1,17 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO sogou/workflow
-    REF "v${VERSION}"
-    SHA512 78996029efd23ca7843f4089d801729174bf8e6bcb1babbe6f557db0c0b4094069dce38328332ddcddffec6710e77cfd03110264d0c670b8cd42b23dda5061ed
-    HEAD_REF master
+    REF b92ead03ec62609a3cc1293041a9caa58a6b4800
+    SHA512 4f9ac3daefcafaffe9121bd2b91b7a9311bd9f09690f723c970ebeab9f092fe3cd6745983c459c9781f673e898d74b6382e654db758914e9c73f9462394e2f73
+    HEAD_REF windows
 )
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" BUILD_STATIC_RUNTIME)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     DISABLE_PARALLEL_CONFIGURE # because configure_package_config_file to ${PROJECT_SOURCE_DIR}
+    OPTIONS
+        -DWORKFLOW_BUILD_STATIC_RUNTIME=${BUILD_STATIC_RUNTIME}
 )
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/workflow")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/workflow" PACKAGE_NAME "workflow")
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE
