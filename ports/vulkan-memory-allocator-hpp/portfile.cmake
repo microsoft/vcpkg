be722f1800acbcaa834883b37466f6b66e77591a
Tom Conder <tom@itstom.co>
2026-02-08 11:35:09 -0600

    [vulkan-memory-allocator-hpp] set version directly

--- ports/vulkan-memory-allocator-hpp/portfile.cmake
+++ ports/vulkan-memory-allocator-hpp/portfile.cmake
@@ -1,12 +1,9 @@
-
-# The git tag is in the format "v3.3.0+2".
-set(git_ref "${VERSION}+2")
-message(STATUS "Using git ref v${git_ref} for version ${VERSION}")
+set(GIT_TAG v3.3.0+2)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO YaaZ/VulkanMemoryAllocator-Hpp
-    REF "v${git_ref}"
+    REF "${GIT_TAG}"
     SHA512 72fccbba9ad422baa0f9e9389a72ccf4aa760ea1f15ecdf6d08604d60c25969938a300db6350363841ba66a40ca7804265477faeb601e142de9d7211da08ada2
     HEAD_REF master
 )
