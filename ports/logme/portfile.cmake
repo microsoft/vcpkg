b534e0ef40a73080826ff3b9463ee184675d178e
Victor Romero <romerosanchezv@gmail.com>
2026-02-09 11:43:29 -0800

    vcpkg-cmake vcpkg-cmake-config host dependencies

--- ports/logme/portfile.cmake
+++ ports/logme/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO efmsoft/logme
-  REF v2.4.4
+  REF "v${VERSION}"
   SHA512 49e1e980d0c8079757d44e16e435dd8bea4c42f43c914d29d5f385e7bcd4068d461c1120b844bdc7c8cf6ced8fe9abc1ddef51f67f3cb04b1df46d2fbe71b40d
   HEAD_REF master
 )
@@ -23,7 +23,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/logme)
-
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
