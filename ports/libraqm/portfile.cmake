94ad7453669fe396c013c58a598c0d4fc6eb56b7
Tobias Markus <tobbi.bugs@googlemail.com>
2026-02-10 00:09:39 +0100

    Build libraqm without tests on x86-emscripten target
    Fixes #49390

--- ports/libraqm/portfile.cmake
+++ ports/libraqm/portfile.cmake
@@ -6,8 +6,23 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+if(VCPKG_TARGET_IS_EMSCRIPTEN)
+    # libraqm tests fail to build on emscripten. See https://github.com/microsoft/vcpkg/issues/49390 for more information
+    option(BUILD_TESTS OFF)
+else()
+    option(BUILD_TESTS ON)
+endif()
+
+if(BUILD_TESTS)
+    list(APPEND configure_options "-Dtests=true")
+else()
+    list(APPEND configure_options "-Dtests=false")
+endif()
+
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${configure_options}
 )
 
 vcpkg_install_meson()
