d9b93cc6506674fe52a87cc5be6851a51bbf30be
Billy Robert O'Neal III <bion@microsoft.com>
2026-02-09 22:02:25 -0800

    Add tests and fix nitpicks.
    * Adds a test port to verify that vcpkg is doing the correct thing despite `VCPKG_POLICY_EMPTY_INCLUDE_FOLDER`
    * Adds usage text as the automatic one does not work for this case.
    * Avoids debug build by setting `VCPKG_BUILD_TYPE` rather than building debug anyway and deleting it afterwards.

--- ports/abcmake/portfile.cmake
+++ ports/abcmake/portfile.cmake
@@ -5,6 +5,7 @@ vcpkg_from_github(
     SHA512 85724b25e158f41f0aa0e5f01ea0530a46f6b4397606b1af115c8aec1c29d317aaaf40a6161795687d713b6f00f66b13a1ab3982f351a139dc79a7d4ac42b7da
 )
 
+set(VCPKG_BUILD_TYPE release) # CMake support file only port
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
 vcpkg_cmake_configure(
@@ -13,6 +14,5 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
