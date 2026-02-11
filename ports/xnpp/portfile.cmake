1bcd0418e71e308ee4171fca526aec3cd5469e24
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-02-11 11:34:04 -0300

    Fix xnpp port

--- ports/xnpp/portfile.cmake
+++ ports/xnpp/portfile.cmake
@@ -2,24 +2,20 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO borgesdan/xnpp
     REF v0.1.0
-    SHA512 14c483b1856d1645356948adccd42cea6ccc91a97041637e1f4751ac8b329034127359d2c87b768485c6b41825aa28764be72583551d20c2832054fcd6af298c
+    SHA512 399fc3189e8fb40ab9c66ffe4417840ef08e5cae22085585ad55bd3e38409b030b00e95a5569c37cee0f56dd35ae824d1bdc9e80dc9a9dec2b1a9418b7c9166e
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DXNPP_BUILD_LIB=ON
-        -DXNPP_BUILD_APP=OFF
+        -DXNPP_BUILD_CONTENTPIPELINE=OFF
 )
 
 vcpkg_cmake_install()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin" "${CURRENT_PACKAGES_DIR}/bin")
-endif()
-
 vcpkg_cmake_config_fixup(
-    CONFIG_PATH lib/cmake/Xnpp
+    CONFIG_PATH lib/cmake/xnpp
 )
 
 vcpkg_install_copyright(
