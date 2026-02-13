27445090148702ec5ad46ee782a3eb82ac31e860
Rusty Conover <rusty@conover.me>
2026-02-13 11:06:31 -0500

    [nanoarrow] Use official ASF archive URL

--- ports/nanoarrow/portfile.cmake
+++ ports/nanoarrow/portfile.cmake
@@ -1,10 +1,13 @@
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO apache/arrow-nanoarrow
-    REF "a579fbf5d192e85b6249935e117de7d02a6dc4e9"
-    SHA512 9e08268c73a1743e964c58038fbc9b175705dc1a83e3436d468dc66571f4cad8d31d55ffa0c2b9d7a9c340a93f225e34bdd13d259571cfd8809b8add448e5624
-    HEAD_REF main
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://archive.apache.org/dist/arrow/apache-arrow-nanoarrow-${VERSION}/apache-arrow-nanoarrow-${VERSION}.tar.gz"
+    FILENAME "apache-arrow-nanoarrow-${VERSION}.tar.gz"
+    SHA512 98f9f4c8dada0175e39e02d2baa01d0f63ad94636925cd289cbffa423de26bf0ede437aaa1ec10ff91e7d375e72cfddd950d040602520ab7891ab4c6337ce4f7
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
 )
 
 file(REMOVE_RECURSE "${SOURCE_PATH}/thirdparty")
