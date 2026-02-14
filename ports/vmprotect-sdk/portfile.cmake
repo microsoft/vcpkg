520271e2c2fbc74a8878acfe3423ba9cafbd9415
Orange <orange-cpp@yandex.ru>
2026-02-14 12:06:24 +0300

    updated hash

--- ports/vmprotect-sdk/portfile.cmake
+++ ports/vmprotect-sdk/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO orange-cpp/vmprotect-sdk
     REF "${VERSION}"
-    SHA512 f21bfb34c09844cb4cb87b9fb080cb2f66672c293b22e7b24a0c6a9e1e5b3ab9b77690af1f86ae8afc18fc8968ae8002aabac7baf87f442167e2fe89c5753eae
+    SHA512 cd9ec34dd2cf0be03c10f4bde10318201d9819a64565d86115753dd56d79634d1baf0ad77b807b469c904e18b3ce02f5c0dce6b7f254e23e645f77b4e9c101e9
     HEAD_REF master
 )
 
