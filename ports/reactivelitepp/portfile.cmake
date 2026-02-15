4f158be65075438aface2361f09dc43a676bb513
Herve_e <herve.eruam@infomaniak.com>
2026-02-15 21:31:30 +0100

    [reactivelitepp] Add reactivelitepp to vcpkg

---
+++ ports/reactivelitepp/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO herve-er/ReactiveLitepp
+    REF "v${VERSION}"
+    SHA512 1dd55a159315e9f063254674ee3e3ebd862e8f130c0a3e835ca108494e8f4f44bd05cc9f2c4ee0bea8baf2aba3699caea5abb4bba29f4986b75fb19b3eadbbdb
+    HEAD_REF main
+)
+
+set(VCPKG_BUILD_TYPE release) # header-only port
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
+# Header-only library - remove debug folder
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+# Install usage and documentation
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/README.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
