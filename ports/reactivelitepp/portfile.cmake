149f7ce96bfd62130b78785a0ac26ab1a23dd15f
Herve_e <herve.eruam@infomaniak.com>
2026-02-15 21:33:25 +0100

    [reactivelitepp] version database

--- ports/reactivelitepp/portfile.cmake
+++ ports/reactivelitepp/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-set(VCPKG_BUILD_TYPE release) # header-only port
+set(VCPKG_BUILD_TYPE release)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -17,11 +17,9 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
 
-# Header-only library - remove debug folder
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-# Install usage and documentation
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${SOURCE_PATH}/README.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
