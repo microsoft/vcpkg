60d2ee45ce36f9293f6f667177e477e2ab6bce59
Ayush Sah <75406648+xARSENICx@users.noreply.github.com>
2026-02-19 19:01:32 +0530

    address reviewer feedback
    - fix-install-path.patch: unify install dirs (lowercase include, CMake config path)
    - portfile.cmake: use v${VERSION}, CONFIG_PATH CMake, remove redundant PACKAGE_NAME
    - vcpkg.json: version 3.96 to match upstream tag
    - usage: add trailing newline

--- ports/universal/portfile.cmake
+++ ports/universal/portfile.cmake
@@ -1,7 +1,8 @@
+set(VCPKG_BUILD_TYPE release)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stillwater-sc/universal
-    REF v3.96
+    REF "v${VERSION}"
     SHA512 b4015a3c68aa17417f39867de0f036e7706442f9d7cdc470b6f237b98e341d6875a73fd22579713cf28976b7dd6bbfbad700023dda44a977ed0722efa5880284
     HEAD_REF master
     PATCHES
@@ -17,13 +18,11 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(
-    PACKAGE_NAME universal
-    CONFIG_PATH share/universal
-)
+vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share") 
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/include/universal/internal/variablecascade"
+)
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
