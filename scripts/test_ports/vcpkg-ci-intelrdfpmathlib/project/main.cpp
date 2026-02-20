03538f5247ee14e1e7e6f28772d13f53fca776bb
Andrew Tribick <ajtribick@googlemail.com>
2026-02-20 23:04:44 +0100

    match library defines

--- scripts/test_ports/vcpkg-ci-intelrdfpmathlib/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-intelrdfpmathlib/project/main.cpp
@@ -1,13 +1,12 @@
-#define DECIMAL_CALL_BY_REFERENCE 0
-#define DECIMAL_GLOBAL_ROUNDING 1
-
 #include <bid_conf.h>
 #include <bid_functions.h>
 
 int main()
 {
     unsigned int flags = 0;
-    BID_UINT128 x = bid128_from_string(const_cast<char*>("1.25673"), &flags);
-    BID_UINT128 y = bid128_from_int32(5);
-    auto result = bid128_add(x, y, &flags);
+    _IDEC_round round_mode = 0;
+    BID_UINT128 x, y;
+    bid128_from_string(const_cast<char*>("1.25673"), round_mode, &flags);
+    bid128_from_int32(5);
+    auto result = bid128_add(x, y, round_mode, &flags);
 }
