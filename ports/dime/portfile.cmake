e57e1e49727ffaac5ff171c66378cf5ae380b275
Francisco Facioni <franciscof@bolt6.ai>
2026-02-20 13:25:15 +0100

    [dime] add port

---
+++ ports/dime/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO coin3d/dime
+  REF 8cec48a1c27475f040f08a504ca0c59bd88a9d5b
+  SHA512 bffe8a994aac87b697ec000d6e162a505368e0bd3d1031ce17319ad20ec042f4c96b81283c6fa1092e3a53196d576db4a5b1e8d670d8ec9eb82681b71614724e
+  PATCHES
+    fix-cmake.patch
+    fix-precision.patch
+)
+
+set(BUILD_SHARED_LIBS OFF)
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+   set(BUILD_SHARED_LIBS ON)
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+    -DDIME_BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+    -DDIME_BUILD_DOCUMENTATION=OFF
+    -DDIME_BUILD_AWESOME_DOCUMENTATION=OFF
+    -DDIME_BUILD_INTERNAL_DOCUMENTATION=OFF
+    -DDIME_BUILD_DOCUMENTATION_MAN=OFF
+    -DDIME_BUILD_DOCUMENTATION_QTHELP=OFF
+    -DDIME_BUILD_DOCUMENTATION_CHM=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
+vcpkg_fixup_pkgconfig()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_tools(TOOL_NAMES dxf2vrml dxfsphere)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
