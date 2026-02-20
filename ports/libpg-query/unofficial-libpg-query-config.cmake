7b1517cae3dbf9a8f2af35abeeae17f0dcbe4424
Altan Birler <altan.birler@tum.de>
2026-02-20 23:40:38 +0100

    remove vendored in libs

--- ports/libpg-query/unofficial-libpg-query-config.cmake
+++ ports/libpg-query/unofficial-libpg-query-config.cmake
@@ -1,3 +1,7 @@
+include(CMakeFindDependencyMacro)
+find_dependency(protobuf-c CONFIG REQUIRED)
+find_dependency(xxHash CONFIG REQUIRED)
+
 if(NOT TARGET unofficial::libpg-query::libpg-query)
     get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
     get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
@@ -6,6 +10,41 @@ if(NOT TARGET unofficial::libpg-query::libpg-query)
     add_library(unofficial::libpg-query::libpg-query UNKNOWN IMPORTED)
     set_target_properties(unofficial::libpg-query::libpg-query PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+    )
+    find_library(PROTOBUF_C_LIBRARY_RELEASE NAMES protobuf-c
+        PATHS "${_IMPORT_PREFIX}"
+        PATH_SUFFIXES lib
+        NO_DEFAULT_PATH
+    )
+    find_library(PROTOBUF_C_LIBRARY_DEBUG NAMES protobuf-c
+        PATHS "${_IMPORT_PREFIX}/debug"
+        PATH_SUFFIXES lib
+        NO_DEFAULT_PATH
+    )
+    if(NOT PROTOBUF_C_LIBRARY_DEBUG)
+        set(PROTOBUF_C_LIBRARY_DEBUG "${PROTOBUF_C_LIBRARY_RELEASE}")
+    endif()
+
+    find_library(XXHASH_LIBRARY_RELEASE NAMES xxhash
+        PATHS "${_IMPORT_PREFIX}"
+        PATH_SUFFIXES lib
+        NO_DEFAULT_PATH
+    )
+    find_library(XXHASH_LIBRARY_DEBUG NAMES xxhash
+        PATHS "${_IMPORT_PREFIX}/debug"
+        PATH_SUFFIXES lib
+        NO_DEFAULT_PATH
+    )
+    if(NOT XXHASH_LIBRARY_DEBUG)
+        set(XXHASH_LIBRARY_DEBUG "${XXHASH_LIBRARY_RELEASE}")
+    endif()
+
+    set_property(TARGET unofficial::libpg-query::libpg-query APPEND PROPERTY
+        INTERFACE_LINK_LIBRARIES
+            "$<$<CONFIG:Debug>:${PROTOBUF_C_LIBRARY_DEBUG}>"
+            "$<$<NOT:$<CONFIG:Debug>>:${PROTOBUF_C_LIBRARY_RELEASE}>"
+            "$<$<CONFIG:Debug>:${XXHASH_LIBRARY_DEBUG}>"
+            "$<$<NOT:$<CONFIG:Debug>>:${XXHASH_LIBRARY_RELEASE}>"
     )
 
     find_library(LIBPG_QUERY_LIBRARY_RELEASE NAMES pg_query
@@ -34,5 +73,9 @@ if(NOT TARGET unofficial::libpg-query::libpg-query)
 
     unset(LIBPG_QUERY_LIBRARY_RELEASE)
     unset(LIBPG_QUERY_LIBRARY_DEBUG)
+    unset(PROTOBUF_C_LIBRARY_RELEASE)
+    unset(PROTOBUF_C_LIBRARY_DEBUG)
+    unset(XXHASH_LIBRARY_RELEASE)
+    unset(XXHASH_LIBRARY_DEBUG)
     unset(_IMPORT_PREFIX)
 endif()
