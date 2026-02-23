33345abdd178a4789dc45c9bc69cf21583bff584
toge <toge.mail@gmail.com>
2026-02-23 20:57:57 +0900

    [luau] update to 0.709

--- ports/luau/portfile.cmake
+++ ports/luau/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO luau-lang/luau
     REF ${VERSION}
-    SHA512 e4febe2afc102b9f1eda70ef0e1f07ee9f61e4b444a09d2a3d1390f6a7e0edfb73b5716273409006c8d7138492d0e7b9ff0d470b60ec96e66577dccc3dfaef6e
+    SHA512 28bbc79978b2da7a420ccef6e804b998741c75451f6b97476e172c20bb123bc12c43b8a67f5ed64d9e10dc6b6aefc462b369795061f248d67fc275ea078819c8
     HEAD_REF master
     PATCHES
         cmake-config-export.patch
