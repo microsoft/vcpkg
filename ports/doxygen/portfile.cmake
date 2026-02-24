d9d4646ea401501e6fda8b5933abe20106664108
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-02-24 11:29:22 -0500

    doxygen builds

---
+++ ports/doxygen/portfile.cmake
@@ -0,0 +1,44 @@
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO doxygen/doxygen
+    REF Release_1_16_1
+    SHA512 99db422f65ee32a76f2b9c016b035ccc1297d34b494c947235544bbaf5c44962273b52fa2c728af6ee0b17c5614b8808367fcce1d7dd872557eeb5cb37c34d32
+    HEAD_REF master
+    PATCHES
+        fix-sqlite3-link.patch
+)
+
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(BISON)
+
+vcpkg_cmake_configure(
+	SOURCE_PATH "${SOURCE_PATH}"
+	OPTIONS
+	    -Duse_sys_spdlog=ON
+	    -Duse_sys_fmt=ON
+	    -Duse_sys_sqlite3=ON
+	    -DFLEX_EXECUTABLE=${FLEX}
+	    -DBISON_EXECUTABLE=${BISON}
+)
+
+vcpkg_cmake_install()
+
+set(DOXY_TOOLS "doxygen")
+if("app" IN_LIST FEATURES)
+  list(APPEND DOXY_TOOLS "doxyapp")
+endif()
+if("parse" IN_LIST FEATURES)
+  list(APPEND DOXY_TOOLS "doxyparse")
+endif()
+if("search" IN_LIST FEATURES)
+  list(APPEND DOXY_TOOLS "doxysearch")
+endif()
+if("wizard" IN_LIST FEATURES)
+  list(APPEND DOXY_TOOLS "doxywizard")
+endif()
+vcpkg_copy_tools(TOOL_NAMES ${DOXY_TOOLS} AUTO_CLEAN)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
