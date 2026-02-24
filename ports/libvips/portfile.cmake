dd404346b6e375a11183d76f84c5e9c4d8892f23
Angel Sánchez <angelsanchez@inedit.com>
2026-02-24 18:22:36 +0100

    libvips port is building on macOS

--- ports/libvips/portfile.cmake
+++ ports/libvips/portfile.cmake
@@ -1,3 +1,5 @@
+# ./vcpkg install "libvips[exif,fontconfig,jpeg,lcms,pangocairo,rsvg,zlib]" --recurse
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libvips/libvips
@@ -7,22 +9,26 @@ vcpkg_from_github(
 )
 
 vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/tools/glib/")
-vcpkg_get_gobject_introspection_programs(PYTHON3 GIR_COMPILER GIR_SCANNER)
+#vcpkg_get_gobject_introspection_programs(PYTHON3 GIR_COMPILER GIR_SCANNER)
 
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -Dtests=false
-		-Dfuzzing_engine=none
-	ADDITIONAL_BINARIES
-        glib-genmarshal='${CURRENT_INSTALLED_DIR}/tools/glib/glib-genmarshal'
-        glib-mkenums='${CURRENT_INSTALLED_DIR}/tools/glib/glib-mkenums'
+        -Dintrospection=disabled
+	#	-Dfuzzing_engine=none
+	#ADDITIONAL_BINARIES
+    #    glib-genmarshal='${CURRENT_INSTALLED_DIR}/tools/glib/glib-genmarshal'
+    #    glib-mkenums='${CURRENT_INSTALLED_DIR}/tools/glib/glib-mkenums'
 	#	"g-ir-scanner='${GIR_SCANNER}'"
 )
 
 vcpkg_install_meson()
 
 #vcpkg_copy_pdbs()
-#vcpkg_fixup_pkgconfig()
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
