b763f24476f2bd366d591dc9340754ef2421707a
Florian Schimmel <florian.schimmel@sap.com>
2026-02-25 13:55:32 +0100

    Fix locations of dlls in windows

--- ports/odbccpp/portfile.cmake
+++ ports/odbccpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SAP/odbc-cpp-wrapper
     REF "v${VERSION}"
-    SHA512 49e7d47c039119e1bccf89975801978f0e7f68b5aedabfd2c3d2fd6b439a6c0139e3afd52b497bad15496b0009bd431e5bb88cf9c4cd06c3bf16feca11bd7a24
+    SHA512 8e79536bc24bd4f59ddc9554824df255fabb7ca651839ff2c90f8a352f61da99beb1df5042c2785d86a3294bcf8c0a93064ed89a62400063755cb5a7df47ca58
     HEAD_REF master
     PATCHES
         use-vcpkg-unixodbc.patch
