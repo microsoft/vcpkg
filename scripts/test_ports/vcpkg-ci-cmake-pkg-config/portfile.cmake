99c0022b1924f4f273668cd415637af3e04f842d
jyu49 <yu_jack@apple.com>
2026-02-25 15:57:46 +0800

    fix usage

--- scripts/test_ports/vcpkg-ci-cmake-pkg-config/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-cmake-pkg-config/portfile.cmake
@@ -2,7 +2,5 @@ set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${CURRENT_PORT_DIR}/project"
-    OPTIONS_DEBUG
-        -DBUILD_TYPE=debug
 )
 vcpkg_cmake_build()
