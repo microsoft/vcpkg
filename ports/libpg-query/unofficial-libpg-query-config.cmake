101ce35aa56cc59454a454a68f2163157a4255a8
Altan Birler <altan.birler@tum.de>
2026-02-25 14:22:16 +0100

    simplify

--- ports/libpg-query/unofficial-libpg-query-config.cmake
+++ ports/libpg-query/unofficial-libpg-query-config.cmake
@@ -12,33 +12,6 @@ if(NOT TARGET unofficial::libpg-query::libpg-query)
     set_target_properties(unofficial::libpg-query::libpg-query PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
     )
-    find_library(PROTOBUF_C_LIBRARY_RELEASE NAMES protobuf-c
-        PATHS "${_IMPORT_PREFIX}"
-        PATH_SUFFIXES lib
-        NO_DEFAULT_PATH
-    )
-    find_library(PROTOBUF_C_LIBRARY_DEBUG NAMES protobuf-c
-        PATHS "${_IMPORT_PREFIX}/debug"
-        PATH_SUFFIXES lib
-        NO_DEFAULT_PATH
-    )
-    if(NOT PROTOBUF_C_LIBRARY_DEBUG)
-        set(PROTOBUF_C_LIBRARY_DEBUG "${PROTOBUF_C_LIBRARY_RELEASE}")
-    endif()
-
-    find_library(XXHASH_LIBRARY_RELEASE NAMES xxhash
-        PATHS "${_IMPORT_PREFIX}"
-        PATH_SUFFIXES lib
-        NO_DEFAULT_PATH
-    )
-    find_library(XXHASH_LIBRARY_DEBUG NAMES xxhash
-        PATHS "${_IMPORT_PREFIX}/debug"
-        PATH_SUFFIXES lib
-        NO_DEFAULT_PATH
-    )
-    if(NOT XXHASH_LIBRARY_DEBUG)
-        set(XXHASH_LIBRARY_DEBUG "${XXHASH_LIBRARY_RELEASE}")
-    endif()
 
     set_property(TARGET unofficial::libpg-query::libpg-query APPEND PROPERTY
         INTERFACE_LINK_LIBRARIES
@@ -74,9 +47,5 @@ if(NOT TARGET unofficial::libpg-query::libpg-query)
 
     unset(LIBPG_QUERY_LIBRARY_RELEASE)
     unset(LIBPG_QUERY_LIBRARY_DEBUG)
-    unset(PROTOBUF_C_LIBRARY_RELEASE)
-    unset(PROTOBUF_C_LIBRARY_DEBUG)
-    unset(XXHASH_LIBRARY_RELEASE)
-    unset(XXHASH_LIBRARY_DEBUG)
     unset(_IMPORT_PREFIX)
 endif()
