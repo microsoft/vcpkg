4a81f5ab5a3000b3f11d0a1abc7b26c7bce604bd
Angel Sánchez <angelsanchez@inedit.com>
2026-02-26 11:23:03 +0100

    explicitly add meson tooling to use the newer version, and a little cleanup of options

--- ports/libvips/portfile.cmake
+++ ports/libvips/portfile.cmake
@@ -1,4 +1,5 @@
-# ./vcpkg install "libvips[exif,fontconfig,jpeg,lcms,pangocairo,rsvg,zlib]" --recurse
+
+set(VCPKG_USE_HEAD_VERSION TRUE)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -9,23 +10,15 @@ vcpkg_from_github(
 )
 
 vcpkg_add_to_path("${CURRENT_HOST_INSTALLED_DIR}/tools/glib/")
-#vcpkg_get_gobject_introspection_programs(PYTHON3 GIR_COMPILER GIR_SCANNER)
 
 vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -Dtests=false
-        -Dintrospection=disabled
-	#	-Dfuzzing_engine=none
-	#ADDITIONAL_BINARIES
-    #    glib-genmarshal='${CURRENT_INSTALLED_DIR}/tools/glib/glib-genmarshal'
-    #    glib-mkenums='${CURRENT_INSTALLED_DIR}/tools/glib/glib-mkenums'
-	#	"g-ir-scanner='${GIR_SCANNER}'"
 )
 
 vcpkg_install_meson()
 
-#vcpkg_copy_pdbs()
+vcpkg_copy_pdbs()
 
 vcpkg_fixup_pkgconfig()
 
