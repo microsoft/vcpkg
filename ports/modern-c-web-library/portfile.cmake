0d27fc5481d60023624cd2c52a38856f53831277
Kamran Khan <24863283+kamrankhan78694@users.noreply.github.com>
2026-02-27 02:04:02 +0400

    [modern-c-web-library] Update to v1.0.0
    - Update REF to v1.0.0 (production release)
    - Update SHA512 hash
    - Add HEAD_REF main
    - Improve formatting per vcpkg style guidelines

--- ports/modern-c-web-library/portfile.cmake
+++ ports/modern-c-web-library/portfile.cmake
@@ -1,28 +1,28 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO kamrankhan78694/modern-c-web-library
-    REF v0.3.0
-    SHA512 f57556aaed8c1a00c9e42f6db5a90f4a94faaea71f8815a288ae6aa13956819170c72d849e021591be4d23e4f986d2ecffdd72485795a851e1a4ce94bcf86358
+    REF v1.0.0
+    SHA512 46797a93f9fbc1f15612fa9d2187675ec368f2346b216725765d26eefbfef3aa0330ea40c7f620c850dca05c199c07facfd0089ca152ac62e1708c7045e10edc
+    HEAD_REF main
 )
 
-# Configure build: disable tests/examples for packaging
 vcpkg_cmake_configure(
-    SOURCE_PATH ${SOURCE_PATH}
-    OPTIONS -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_EXAMPLES=OFF
 )
-
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup()
 
 vcpkg_copy_pdbs()
-# Remove debug include directory to satisfy policy (headers identical)
-if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/include")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-endif()
 
+# Remove debug include directory to satisfy policy (headers identical)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+# Install usage file
 file(COPY "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(
-    FILE_LIST "${SOURCE_PATH}/LICENSE"
-)
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
