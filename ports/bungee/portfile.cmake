3a6e43faa882631a5df46bedbbb7d7395e3351fd
JoergAtGithub <JoergAtGithub@worldwartweb.com>
2026-02-27 21:04:37 +0100

    Add vcpkg_cmake_config_fixup() infrastructure

--- ports/bungee/portfile.cmake
+++ ports/bungee/portfile.cmake
@@ -23,6 +23,7 @@ vcpkg_cmake_configure(
         -DBUNGEE_PRESET=
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/bungee)
 vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
