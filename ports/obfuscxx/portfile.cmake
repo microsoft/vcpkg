bfaf07784ad6df38e66bbe39ecb1ab174e2198d2
Javier Matos Denizac <javiermat@microsoft.com>
2026-03-05 13:05:52 -0800

    dg0yt suggestions

--- ports/obfuscxx/portfile.cmake
+++ ports/obfuscxx/portfile.cmake
@@ -1,9 +1,7 @@
-set(OBFUSCXX_VERSION "v1.3.1")
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nevergiveupcpp/obfuscxx
-    REF ${OBFUSCXX_VERSION}
+    REF v${VERSION}
 	SHA512 78f12676dce516847650fd06a4f5e745cb02c0877285ec135890c295978726741ba658e4bf543779384b6fa88bd1003b4be3f8290f69dd5cb554da715e6e4972
 )
 
@@ -13,7 +11,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME obfuscxx CONFIG_PATH lib/cmake/obfuscxx)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/obfuscxx)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
