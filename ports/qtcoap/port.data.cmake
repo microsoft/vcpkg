5f5b98417af90d75f6d6fe8166a52bbfe1996608
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2026-03-06 20:52:19 +0100

    [Qt] Update to 6.10.2

--- ports/qtcoap/port.data.cmake
+++ ports/qtcoap/port.data.cmake
@@ -1,3 +1,3 @@
-set(qtcoap_HASH "0578e389562624188016673e8d94ecdbdebe2a7047bda2184462fbccdba43b55ebf75b280245453167473ef45855aa00f25f37c52f7f7ffa51989aebb9c08eb4")
-set(qtcoap_URL "https://github.com/qt/qtcoap/archive/v6.10.1.tar.gz")
-set(qtcoap_FILENAME "qt-qtcoap-v6.10.1.tar.gz")
+set(qtcoap_HASH "d28b1679bcde40ade0234ebf7448fc52c43848f8ddbbd987e739dd59707b53e957fb2d62b7a58bdde7a26e590b36ce0b90e6e4d9a32268672688b56c3ca441d3")
+set(qtcoap_URL "https://github.com/qt/qtcoap/archive/v6.10.2.tar.gz")
+set(qtcoap_FILENAME "qt-qtcoap-v6.10.2.tar.gz")
