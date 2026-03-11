5c6e3b692c596f76e68416d5a13085c978440099
Victor Romero <romerosanchezv@gmail.com>
2026-03-11 13:36:59 -0700

    Update ports/lazperf/portfile.cmake
    Co-authored-by: Kai Pastor <dg0yt@darc.de>

--- ports/lazperf/portfile.cmake
+++ ports/lazperf/portfile.cmake
@@ -13,14 +13,10 @@ vcpkg_cmake_configure(
     OPTIONS
         -DWITH_TESTS=OFF
 )
-
 vcpkg_cmake_install()
-
 vcpkg_copy_pdbs()
-
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/LAZPERF PACKAGE_NAME lazperf)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/LAZPERF)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-# Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
