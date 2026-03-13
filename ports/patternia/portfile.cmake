fe0af70cf02c32c7c77a724f1a763864c5c53316
Yingfan Guo <115908952+sentomk@users.noreply.github.com>
2026-03-13 14:25:53 +0800

    [patternia] Update to version 0.9.0 (#50429)

--- ports/patternia/portfile.cmake
+++ ports/patternia/portfile.cmake
@@ -1,22 +1,28 @@
-set(VCPKG_BUILD_TYPE release) # Header-only library
+set(VCPKG_BUILD_TYPE release)
+
 vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO sentomk/patternia
-    REF "v${VERSION}"
-    SHA512 e88dd6fab225a670f41599d07c2ae5ce311365f9ed091fe6cd298369f6a8da5cb6094915db3780f80f590eb2d22f03eca7030ca78ba15671b06fb4eef246ca4f
-    HEAD_REF main
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO SentoMK/patternia
+  REF "v${VERSION}"
+  SHA512 2b207cdf92f36a8bf07ea0478c806406c3d35eb3b94142bfc93605b6260eb9652011e82f8ecfddbb3c45612c6ed42ae1250d48b96e5a1a17c1705c690769f776
+  HEAD_REF main
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DPTN_INSTALL=ON
-        -DPTN_BUILD_TESTS=OFF
-        -DPTN_BUILD_BENCHMARKS=OFF
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    -DPTN_BUILD_TESTS=OFF
+    -DPTN_BUILD_BENCHMARKS=OFF
+    -DPTN_BUILD_SAMPLES=OFF
+    -DPTN_DEV_INDEX=OFF
+    -DPTN_INSTALL=ON
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/${PORT})
 
+vcpkg_cmake_config_fixup(PACKAGE_NAME patternia CONFIG_PATH lib/cmake/patternia)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
