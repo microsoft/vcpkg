3b8cc729411d4dba1cb8e817f38e5b6132d8c6de
toge <toge.mail@gmail.com>
2026-03-18 05:28:57 +0900

    [libgossip] update to 1.2.1.3 (#50503)

--- ports/libgossip/portfile.cmake
+++ ports/libgossip/portfile.cmake
@@ -1,12 +1,8 @@
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # DLL broken in 1.1.2.0
-endif()
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO caomengxuan666/libgossip
     REF "v${VERSION}"
-    SHA512 971f51f6583d3246a5696b35a419fd30ce8ebbd7940b63bb9731f7ec2f12e0f6d68b3924c0be81de30d4eb33f24a39be4db72d2cee5f2ca5e52081aa7c568699
+    SHA512 ae765138fac7077af09ce8ca769b5bf1bd6ca0a81e17cc4b397436309152fd8678f29ce699fc6534d56c5bead980b8b7cf0fbb7df528ef805c51d3210ab1eb24
     HEAD_REF main
     PATCHES
         fix-dependencies.patch
