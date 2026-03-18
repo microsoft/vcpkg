11af8bb423873236683a16854da85e99f43a9a9d
Faith Hughes <alienfragger@gmail.com>
2026-03-18 13:43:50 -0700

    Update Python to 3.14.3 and add support for mpdecimal library
    - Updated Python version from 3.12.13 to 3.14.3 in vcpkg.json and baseline.json.
    - Added mpdecimal library as a dependency with version 4.0.1.
    - Introduced new portfile for mpdecimal with appropriate build configurations.
    - Removed deprecated Py_NO_LINK_LIB patch as it is no longer needed.
    - Adjusted sysconfig include paths for better compatibility.
    - Fixed various build configurations for Windows, ensuring proper handling of static and dynamic libraries.
    - Updated project files to reflect changes in preprocessor definitions and library linking.
    - Enhanced the handling of external dependencies by removing bundled versions and linking against system-installed libraries.

---
+++ ports/mpdecimal/portfile.cmake
@@ -0,0 +1,99 @@
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-${VERSION}.tar.gz"
+    FILENAME "mpdecimal-${VERSION}.tar.gz"
+    SHA512 431fa8ab90d6b8cdecc38b1618fd89d040185dec3c1150203e20f40f10a16160058f6b8abddd000f6ecb74f4dc42d9fef8111444f1496ab34c34f6b814ed32b7
+)
+
+vcpkg_extract_source_archive(SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    PATCHES msvc-crt.patch
+)
+
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+        set(NMAKE_MACHINE "x64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+        set(NMAKE_MACHINE "ansi64")
+    elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+        set(NMAKE_MACHINE "ansi32")
+    else()
+        set(NMAKE_MACHINE "ppro")
+    endif()
+
+    if(VCPKG_CRT_LINKAGE STREQUAL "static")
+        set(CRT_REL "/MT")
+        set(CRT_DBG "/MTd")
+    else()
+        set(CRT_REL "/MD")
+        set(CRT_DBG "/MDd")
+    endif()
+
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+        set(BUILD_TARGET "libmpdec-${VERSION}.dll")
+        set(LIB_NAME "libmpdec-${VERSION}.dll.lib")
+        set(DLL_NAME "libmpdec-${VERSION}.dll")
+        set(INSTALL_DLL ON)
+    else()
+        set(BUILD_TARGET "libmpdec-${VERSION}.lib")
+        set(LIB_NAME "libmpdec-${VERSION}.lib")
+        set(INSTALL_DLL OFF)
+    endif()
+
+    vcpkg_build_nmake(
+        SOURCE_PATH "${SOURCE_PATH}"
+        PROJECT_SUBPATH "libmpdec"
+        PROJECT_NAME "Makefile.vc"
+        CL_LANGUAGE C
+        OPTIONS
+            "MACHINE=${NMAKE_MACHINE}"
+            "${BUILD_TARGET}"
+        OPTIONS_RELEASE
+            "OPT=${CRT_REL} /O2 /GS /EHsc /DNDEBUG"
+            "OPT_SHARED=${CRT_REL} /O2 /GS /EHsc /DNDEBUG"
+        OPTIONS_DEBUG
+            "OPT=${CRT_DBG} /Od /Zi /EHsc"
+            "OPT_SHARED=${CRT_DBG} /Od /Zi /EHsc"
+            "DEBUG=1"
+    )
+
+    set(LIBMPDEC_REL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/libmpdec")
+    set(LIBMPDEC_DBG "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/libmpdec")
+
+    # Install header (generated during release build)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(COPY "${LIBMPDEC_REL}/mpdecimal.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+    endif()
+
+    # Install release library
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        file(COPY "${LIBMPDEC_REL}/${LIB_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
+        file(RENAME "${CURRENT_PACKAGES_DIR}/lib/${LIB_NAME}" "${CURRENT_PACKAGES_DIR}/lib/mpdec.lib")
+        if(INSTALL_DLL)
+            file(COPY "${LIBMPDEC_REL}/${DLL_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/bin")
+        endif()
+    endif()
+
+    # Install debug library
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        file(COPY "${LIBMPDEC_DBG}/${LIB_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
+        file(RENAME "${CURRENT_PACKAGES_DIR}/debug/lib/${LIB_NAME}" "${CURRENT_PACKAGES_DIR}/debug/lib/mpdec.lib")
+        if(INSTALL_DLL)
+            file(COPY "${LIBMPDEC_DBG}/${DLL_NAME}" DESTINATION "${CURRENT_PACKAGES_DIR}/debug/bin")
+        endif()
+    endif()
+else()
+    vcpkg_make_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+            "--disable-cxx"
+    )
+    vcpkg_make_install(TARGETS install)
+    vcpkg_fixup_pkgconfig()
+
+    file(REMOVE_RECURSE
+        "${CURRENT_PACKAGES_DIR}/share"
+        "${CURRENT_PACKAGES_DIR}/debug/share"
+    )
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYRIGHT.txt")
