489dc7c3c20480a8d1ba110b9bd7f02b1521e11f
toge <toge.mail@gmail.com>
2026-03-18 14:21:55 +0900

    [ryml] update to 0.11.0

--- ports/ryml/portfile.cmake
+++ ports/ryml/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO biojppm/rapidyaml
     REF "v${VERSION}"
-    SHA512 e0dd2409e08e556c65fb906de546c1012627aea97a36964d65a1265e7c2465bee1d7dceafebb843ae294a348f8c5fe7b6887e7ddffbc1b1eb419e924d94b41fa
+    SHA512 425d98e5d74c2628b8e91f6bc5e02439765669ee3e4bf783f550b1221dc80453efcd382f92ac423883d53b8fac0536610a11a3d7e248eb62981b118dce9a89b8
     HEAD_REF master
     PATCHES cmake-fix.patch
 )
