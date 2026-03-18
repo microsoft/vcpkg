b2d0032d9ee254bc5df36dc0518ad1401c0b2132
Ayush Sah <75406648+xARSENICx@users.noreply.github.com>
2026-03-18 09:19:58 +0530

    Revert CMake project rename; keep upstream targets as universal per reviewer

--- ports/stillwater-universal/portfile.cmake
+++ ports/stillwater-universal/portfile.cmake
@@ -18,7 +18,7 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(CONFIG_PATH CMake)
+vcpkg_cmake_config_fixup(CONFIG_PATH CMake PACKAGE_NAME universal)
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/include/universal/internal/variablecascade"
