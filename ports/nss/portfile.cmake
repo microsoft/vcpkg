4c5e9dceaaf9bc17231fdd450082455e37f35355
Javier Matos Denizac <javiermat@microsoft.com>
2026-03-18 08:30:04 -0700

    remove spatialite-tools from baseline

--- ports/nss/portfile.cmake
+++ ports/nss/portfile.cmake
@@ -188,17 +188,14 @@ foreach(out_var IN ITEMS NSPR_LIBS_RELEASE NSPR_LIBS_DEBUG SQLITE_LIBS_RELEASE S
                 cygpath_u(dir "${dir}")
             endif()
             if(VCPKG_DETECTED_MSVC)
-                list(APPEND ${out_var} "-LIBPATH:${dir}")
+                string(APPEND ${out_var} "-LIBPATH:${dir}")
             else()
-                list(APPEND ${out_var} "-L${dir}")
+                string(APPEND ${out_var} "-L${dir}")
             endif()
         elseif(item MATCHES "^-l(.*)")
-            list(APPEND ${out_var} "${item}")
+            string(APPEND ${out_var} "${item}")
         endif()
     endforeach()
-    # Convert CMake list (semicolons) to space-separated string so the
-    # entire value stays as one token inside "-Dxxx_libs=..." arguments.
-    list(JOIN ${out_var} " " ${out_var})
 endforeach()
 
 # configuring and building in an autotools-like environment, but using gyp-next and ninja
