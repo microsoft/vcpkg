d341de9277f14e3dd9fbcd08a09b73e40e08ef64
Weihang Ding <798047000@qq.com>
2026-03-20 05:40:34 +0800

    [thorvg] Fix linkage (#50558)

--- ports/thorvg/portfile.cmake
+++ ports/thorvg/portfile.cmake
@@ -6,12 +6,6 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    list(APPEND BUILD_OPTIONS -Dstatic=true)
-else()
-    list(APPEND BUILD_OPTIONS -Dstatic=false)
-endif()
-
 if ("tools" IN_LIST FEATURES)
     list(APPEND BUILD_OPTIONS -Dtools=all)
 endif()
@@ -21,10 +15,11 @@ vcpkg_configure_meson(
     OPTIONS
         ${BUILD_OPTIONS}
         # see ${SOURCE_PATH}/meson_options.txt
+        -Dstatic=true # Use static modules
         -Dengines=['sw']
         -Dloaders=all
         -Dsavers=all
-        -Dsimd=false # The reason for setting 'Dsimd=false' was that the creator said a false setting was necessary
+        -Dsimd=true
         -Dbindings=capi
         -Dtests=false
         -Dstrip=false
@@ -37,6 +32,12 @@ vcpkg_configure_meson(
 )
 vcpkg_install_meson()
 vcpkg_fixup_pkgconfig()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/thorvg-1/thorvg.h" "#ifndef TVG_STATIC" "#if 0")
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/thorvg-1/thorvg.h" "#ifndef TVG_STATIC" "#if 1")
+endif()
 
 if ("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES tvg-svg2png tvg-lottie2gif AUTO_CLEAN)
