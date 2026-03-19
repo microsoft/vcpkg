28339d75aee84d5aa17e9ebddecc3b5320482461
Faith Hughes <faith@parfont.com>
2026-03-19 01:19:23 -0700

    Add installation of usage documentation for mpdecimal

--- ports/mpdecimal/portfile.cmake
+++ ports/mpdecimal/portfile.cmake
@@ -98,3 +98,4 @@ else()
 endif()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYRIGHT.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
