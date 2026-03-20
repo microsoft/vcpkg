f2209004773af5057b46a406c1c95f5db4460561
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-03-20 15:20:24 +0500

    [quran] Add new port version 1.0.0

--- ports/quran/portfile.cmake
+++ ports/quran/portfile.cmake
@@ -1,7 +1,3 @@
-# portfile.cmake for quran
-# This port builds libquran from a source archive.
-
-# HEAD_REF is the branch name used when installing with --head
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SENODROOM/libquran
@@ -10,18 +6,14 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
-# Alternatively, when distributing as a local overlay port with the
-# source bundled alongside, use:
-#
-#   set(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/src")
-#
-# and include the full source tree inside ports/quran/src/.
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DQURAN_BUILD_TESTS=OFF
         -DQURAN_BUILD_EXAMPLES=OFF
+        -DBUILD_SHARED_LIBS=OFF
 )
 
 vcpkg_cmake_install()
@@ -31,12 +23,10 @@ vcpkg_cmake_config_fixup(
     CONFIG_PATH  lib/cmake/quran
 )
 
-# Remove duplicate includes installed under debug
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-# Install the license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-# Validate
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
