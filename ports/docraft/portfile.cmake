1568dbbda98f32ad509511223b7d3390b7b79671
cadons <cadons@mail.umbranet.ovh>
2026-03-21 10:31:53 +0100

    Update portfile and vcpkg.json for version formatting changes

--- ports/docraft/portfile.cmake
+++ ports/docraft/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Cadons/Docraft
-    REF ${VERSION}
+    REF v${VERSION}
     SHA512 fee1f78fb013b592faf3107e6bd77485f14755d0a1add6b3f906b896f743c693bd900ce26f6d347a577e00f507d541917657ab6ccae93a2de800ab68a1adbd93
     HEAD_REF main
 )
