6291f9c111744ff7cc314726fdae8dbcccbe68fc
Sergei Lebedev <limentas@gmail.com>
2026-03-22 09:39:26 +0100

    Fix `pgkconfig` fix

--- ports/ffts/portfile.cmake
+++ ports/ffts/portfile.cmake
@@ -19,10 +19,11 @@ vcpkg_cmake_configure(
         -DENABLE_SHARED=${ENABLE_SHARED}
         -DENABLE_STATIC=${ENABLE_STATIC}
 )
-vcpkg_fixup_pkgconfig()
+
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYRIGHT")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYRIGHT")
