2a2ae8af963f626e88591113b36d14e12700a9bf
xiaotang2333333 <xiaotang2333@gmail.com>
2026-03-23 15:08:55 +0800

    [mxml1] add mxml v3.3.1

---
+++ ports/mxml1/portfile.cmake
@@ -0,0 +1,62 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO michaelrsweet/mxml
+    REF "v${VERSION}"
+    SHA512 43e6a92806d9c3f5db39fbf960c15ebfa6d92ef98274b7ce39b57724d6c26ad4362d6d8f3c1023efda92e6a815df068e5038a0cd479562b6be9dbdda8e827a41
+    HEAD_REF master
+    PATCHES
+    0001-win-platform-fix.patch # Issue347
+)
+
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    message(WARNING "mxml built as a static library on Windows is not thread-safe.")
+endif()
+
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    # Force Z7 debug information format for MSVC builds
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+        set(MXML_VCNET_PROJECT "vcnet/mxmlstat.vcxproj")
+    else()
+        set(MXML_VCNET_PROJECT "vcnet/mxml1.vcxproj")
+    endif()
+
+    vcpkg_replace_string("${SOURCE_PATH}/${MXML_VCNET_PROJECT}"
+      "<DebugInformationFormat>ProgramDatabase</DebugInformationFormat>"
+      "<DebugInformationFormat>OldStyle</DebugInformationFormat>"
+    )
+    vcpkg_replace_string("${SOURCE_PATH}/${MXML_VCNET_PROJECT}"
+      "<DebugInformationFormat>EditAndContinue</DebugInformationFormat>"
+      "<DebugInformationFormat>OldStyle</DebugInformationFormat>"
+    )
+    vcpkg_replace_string("${SOURCE_PATH}/${MXML_VCNET_PROJECT}"
+        "<MinimalRebuild>true</MinimalRebuild>"
+        "<MinimalRebuild>false</MinimalRebuild>"
+    )
+
+    vcpkg_msbuild_install(
+        SOURCE_PATH "${SOURCE_PATH}"
+        PROJECT_SUBPATH "${MXML_VCNET_PROJECT}"
+        TARGET Build
+    )
+    file(INSTALL "${SOURCE_PATH}/mxml.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+else()
+    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+        set(MXML_SHARED_OPT --disable-shared)
+    else()
+        set(MXML_SHARED_OPT --enable-shared)
+    endif()
+
+    vcpkg_make_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        COPY_SOURCE
+        OPTIONS
+        ${MXML_SHARED_OPT}
+    )
+    vcpkg_make_install()
+    vcpkg_fixup_pkgconfig()
+endif()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
