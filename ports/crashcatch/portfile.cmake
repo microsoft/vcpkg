c3124a638b71e8b81673898b468f55abf772a242
Keith <keithpotz@gmail.com>
2026-03-23 16:38:21 -0400

    Apply suggestion from @vicroms
    Co-authored-by: Victor Romero <romerosanchezv@gmail.com>

--- ports/crashcatch/portfile.cmake
+++ ports/crashcatch/portfile.cmake
@@ -29,8 +29,5 @@ file(REMOVE_RECURSE
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-configure_file(
-    "${CMAKE_CURRENT_LIST_DIR}/usage"
-    "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage"
-    COPYONLY
-)
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage")
