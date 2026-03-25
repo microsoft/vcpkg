0a369d5cd90e7a1d1a8c4f686619bcfb6e49dc01
Daniele E. Domenichelli <daniele.domenichelli@kdab.com>
2026-03-25 18:31:14 +0100

    [kwidgetsaddons] New port

---
+++ ports/kwidgetsaddons/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO KDE/kwidgetsaddons
+    REF "v${VERSION}"
+    SHA512 9d46c45b7a7d15b54550b16a085f88c65b0b160eeffc0f8a36c33e904daa712276b4cfdc776b86d832b15114ea3523ac57a78f7878ac53715f22e52393822810
+    HEAD_REF master
+)
+
+# Prevent KDEClangFormat from writing to source effectively blocking parallel configure
+file(WRITE "${SOURCE_PATH}/.clang-format" "DisableFormat: true\nSortIncludes: false\n")
+
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        designerplugin BUILD_DESIGNERPLUGIN
+    INVERTED_FEATURES
+        translations   KF_SKIP_PO_PROCESSING
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+        -DBUILD_PYTHON_BINDINGS=OFF
+        -DKDE_INSTALL_PLUGINDIR=plugins
+        -DKDE_INSTALL_QTPLUGINDIR=plugins
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME kf6widgetsaddons CONFIG_PATH lib/cmake/KF6WidgetsAddons)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
+vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
