798559d8117ad78c8480e64e3937f9d9e1a65f4f
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2026-03-27 19:02:26 +0700

    chore(mcp-cpp-sdk): add vcpkg_fixup_pkgconfig call

--- ports/mcp-cpp-sdk/portfile.cmake
+++ ports/mcp-cpp-sdk/portfile.cmake
@@ -21,6 +21,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/mcp_sdk")
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
