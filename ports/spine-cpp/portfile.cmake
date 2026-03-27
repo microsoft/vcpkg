2029e33d7a831cfcd1e08f38bdc5e175261b4469
Todor Prokopov <koprok@gmail.com>
2026-03-27 18:15:20 +0200

    [spine-cpp] Install cmake export

--- ports/spine-cpp/portfile.cmake
+++ ports/spine-cpp/portfile.cmake
@@ -22,7 +22,9 @@ vcpkg_cmake_configure(
         ${FEATURE_OPTIONS}
 )
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
