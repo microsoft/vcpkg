4e787f5af6ef09f4b7b0afb3e5ffdd29cce64762
Aksheita Dholakia <aksheitadholakia@gmail.com>
2026-03-27 15:30:32 +0530

    Create portfile.cmake

---
+++ ports/zenith-c/portfile.cmake
@@ -0,0 +1,14 @@
+# 1. Download the code from your GitHub
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Aksh13673/Zenith
+    REF v1.0.0
+    SHA512 0  ]
+    HEAD_REF main
+)
+
+# 2. Copy the header to the system include folder
+file(INSTALL "${SOURCE_PATH}/zenith_c.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+
+# 3. Install the license
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
