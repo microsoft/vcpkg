cbb91974e731b45e16dcc9b7e2f0783ed03bceb4
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2026-03-29 10:44:03 +0800

    [gpgme] update to 2.0.1

--- ports/gpgme/portfile.cmake
+++ ports/gpgme/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_download_distfile(tarball
     URLS "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gpgme/gpgme-${VERSION}.tar.bz2"
          "https://gnupg.org/ftp/gcrypt/gpgme/gpgme-${VERSION}.tar.bz2"
     FILENAME "gpgme-${VERSION}.tar.bz2"
-    SHA512 ee58dc2a4273c740d5b9ef13cc655d5e600ddddd137fb85a781c31e8854829283b4ce241d7810a963d9a125d603213600f37e7d0c1ce3b3cf1b935e62cf60777
+    SHA512 66666666666666666666666666666666666666666666666666666666666668888888888888888888888888888888888888888888888888888888888888888888
 )
 vcpkg_extract_source_archive(
     SOURCE_PATH
