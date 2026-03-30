67f33c50a4e508e6c4bfa289f7de0287e85a5c8a
xiaozhuai <798047000@qq.com>
2026-03-30 13:35:32 +0800

    Fix

--- ports/dingo/portfile.cmake
+++ ports/dingo/portfile.cmake
@@ -7,7 +7,11 @@ vcpkg_from_github(
     SHA512 9fb6cf64b1a9ee99404d69d9756530c9ffc591116579469ad39b5fa458a127628899bcac4e99a9bedaf7cae6c19c1a0e5816043868e9daf6d60858a1c2e17c0b
 )
 
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DDINGO_INSTALL=ON
+)
 vcpkg_cmake_install()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
