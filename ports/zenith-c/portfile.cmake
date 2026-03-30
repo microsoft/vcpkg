075a871e48f372bf984f0687d19e1f57fcaa0ba7
Aksheita Dholakia <aksheitadholakia@gmail.com>
2026-03-30 19:21:13 +0530

    Update portfile.cmake

--- ports/zenith-c/portfile.cmake
+++ ports/zenith-c/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Aksh13673/Zenith
     REF v1.0.0
-    SHA512 0 # <--- This will cause a failure. See below how to fix it.
+    SHA512 b7da7251786576b55be979f485122f8725968425ba36d640816b0b3db6168f8ccf4120ba20526e9930c8c7294e64d43900ad2aef9d5f28175210d0c3a417
     HEAD_REF main
 )
 
