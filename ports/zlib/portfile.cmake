c3f52be6af2a0d1068b57de47c64de45e0287214
Serkan Kenar <serkankenar@gmail.com>
2026-03-30 12:44:35 +0100

    Update zlib to 1.3.2.

--- ports/zlib/portfile.cmake
+++ ports/zlib/portfile.cmake
@@ -3,16 +3,9 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO madler/zlib
     REF v${VERSION}
-    SHA512 8c9642495bafd6fad4ab9fb67f09b268c69ff9af0f4f20cf15dfc18852ff1f312bd8ca41de761b3f8d8e90e77d79f2ccacd3d4c5b19e475ecf09d021fdfe9088
+    SHA512 16fea4df307a68cf0035858abe2fd550250618a97590e202037acd18a666f57afc10f8836cbbd472d54a0e76539d0e558cb26f059d53de52ff90634bbf4f47d4
     HEAD_REF master
-    PATCHES
-        0001-Prevent-invalid-inclusions-when-HAVE_-is-set-to-0.patch
-        0002-build-static-or-shared-not-both.patch
-        0003-android-and-mingw-fixes.patch
 )
-
-# This is generated during the cmake build
-file(REMOVE "${SOURCE_PATH}/zconf.h")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
