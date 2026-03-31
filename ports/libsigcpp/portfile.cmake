8dc16b8925291b89887692e194fd7c0ebff8dd41
xiaozhuai <798047000@qq.com>
2026-03-31 12:53:42 +0800

    Fix static linkage on windows

--- ports/libsigcpp/portfile.cmake
+++ ports/libsigcpp/portfile.cmake
@@ -19,7 +19,9 @@ vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(PACKAGE_NAME sigc++-3 CONFIG_PATH lib/cmake/sigc++-3)
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/sigc++config.h" "ifdef BUILD_SHARED" "if 1" IGNORE_UNCHANGED)
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/sigc++config.h" "#ifdef SIGC_DLL" "#if 1")
+else()
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/sigc++config.h" "#ifdef SIGC_DLL" "#if 0")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
