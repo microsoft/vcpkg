be81cebbaad41912e657689509e6f79cde743cd8
xiaozhuai <798047000@qq.com>
2026-03-31 12:10:28 +0800

    [dispenso] Bump to 1.5.1

--- ports/dispenso/portfile.cmake
+++ ports/dispenso/portfile.cmake
@@ -2,10 +2,8 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebookincubator/dispenso
     REF "v${VERSION}"
-    SHA512 2c1da1e5050cdc788af81aeda44cb5e54a1aa40e85149e24835daf37e4a34c26d794d94f7b8539985f2732a1e9ec8d82c89776ed3c7449710ecaff01586bac9e
+    SHA512 e2a93a4780d214447151d43f3b1758229c5a4835ab4339f30f0d6e4068d8c833519891de64d13993131f3b535ae08ccd7ceca0b2b2399ef054bb523cd9b8c772
     HEAD_REF main
-    PATCHES
-        fix-arm64-platform-define.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" DISPENSO_SHARED)
