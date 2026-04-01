2119c09c4960bad36a9ebb6a528952b5844fe304
Keith <keithpotz@gmail.com>
2026-04-01 12:27:10 -0400

    Update ports/crashcatch/portfile.cmake
    Co-authored-by: Victor Romero <romerosanchezv@gmail.com>

--- ports/crashcatch/portfile.cmake
+++ ports/crashcatch/portfile.cmake
@@ -19,4 +19,4 @@ vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/CrashCatch")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage")
+     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
