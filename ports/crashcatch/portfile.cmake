cf15402110bed0a9cfc751a0203b77222c4ddd28
Victor Romero <romerosanchezv@gmail.com>
2026-04-01 17:31:42 -0700

    Disable tests and examples, fix post-build checks

--- ports/crashcatch/portfile.cmake
+++ ports/crashcatch/portfile.cmake
@@ -1,5 +1,4 @@
-# CrashCatch is a header-only library — no compilation required.
-set(VCPKG_BUILD_TYPE release)
+set(VCPKG_BUILD_TYPE release) # header-only library
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,16 +6,21 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 ba3f431b1c1da9f8ead4038ff8073df3010394ce84a6976ba6b9e9f50842a3aacb367b443a92fef75317ee49cf08384a39d76b944bf1e2735d3e2a3647c63f01
     HEAD_REF main
+    PATCHES
+        fix-disable-examples-tests.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCRASHCATCH_BUILD_EXAMPLES=OFF
+        -DCRASHCATCH_BUILD_TESTS=OFF
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/CrashCatch")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
-     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
