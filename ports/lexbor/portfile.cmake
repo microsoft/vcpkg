dfcaca393348617e59cc933ad439cedf2695232f
toge <toge.mail@gmail.com>
2026-04-03 11:11:00 +0900

    fixup pkgconfig

--- ports/lexbor/portfile.cmake
+++ ports/lexbor/portfile.cmake
@@ -23,6 +23,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/lexbor)
+vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include"
