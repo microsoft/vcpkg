4e9000fc6337a4cba79755df6fe0161850414b2b
Tom Conder <tom@itstom.co>
2026-04-03 14:46:15 -0500

    [libigl] patch Combo and ListBox getter signature

--- scripts/test_ports/vcpkg-ci-libigl/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-libigl/portfile.cmake
@@ -6,8 +6,6 @@ vcpkg_from_github(
     REF v2.6.0
     SHA512 7c6ae5b94020a01df5d6d0a358592293595d8d8bf04bf42e6acc09bcd6ed012071069373a71ed6f24ce878aa79447dd189b42bc8a3a70819ef05dccc60a2cf68
     HEAD_REF master
-    PATCHES
-        fix-imgui-combo-getter.patch
 )
 
 vcpkg_cmake_configure(
