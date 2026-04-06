1a9aaed8c6cf8c91681985867098a2f9094f091e
toge <toge.mail@gmail.com>
2026-04-07 07:44:52 +0900

    [doctest] update to 2.5.1 (#50985)

--- ports/doctest/portfile.cmake
+++ ports/doctest/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO doctest/doctest
     REF "v${VERSION}"
-    SHA512 16c2d195ae13f08dd1c9ee12af25135b69ed13ce569d1344f61ace6bc6a42898bb8d24358546049d82552db0759fac82fccfb59e4f970b4a80b3d06be6c95f2b
+    SHA512 6a1b44707cc409e106d02c1efa8d28b0c86e26dfa4c0fc6f7050b0cfd9b02859fbd502c067dcc88e70ee23b7adb35869f0555deebd8615c5342eb87c0f271fac
     HEAD_REF master
 )
 
