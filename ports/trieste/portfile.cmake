8378268431399ba8a5a8f37e6af749bf6e1be0e4
Kai Pastor <dg0yt@darc.de>
2026-04-09 05:38:58 +0200

    [trieste] Do not take generic artifact names

--- ports/trieste/portfile.cmake
+++ ports/trieste/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 cd92ef15181c7adc4e777342b9e5ae60ec1b12356d3742044d98e54d3f9a44012c377eafbc02f92afc7f80e963b32a22e5230f765a2f63345139050512bc3af0
     HEAD_REF main
+    PATCHES
+      artifact-names.diff
 )
 
 # NOTE: The CI overlay port (see .github/workflows/buildtest.yml,
@@ -15,7 +17,7 @@ if("parsers" IN_LIST FEATURES)
   # The parser libraries lack __declspec(dllexport) annotations,
   # so they must be built as static libraries on Windows.
   if(VCPKG_TARGET_IS_WINDOWS)
-    set(VCPKG_LIBRARY_LINKAGE static)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
   endif()
 else()
   set(BUILD_PARSERS OFF)
@@ -26,6 +28,7 @@ endif()
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        -DCMAKE_DISABLE_FIND_PACKAGE_Git=ON
         -DTRIESTE_USE_FETCH_CONTENT=OFF
         -DTRIESTE_BUILD_SAMPLES=OFF
         -DTRIESTE_BUILD_PARSERS=${BUILD_PARSERS}
@@ -34,11 +37,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-
-vcpkg_cmake_config_fixup(PACKAGE_NAME trieste CONFIG_PATH share/trieste/cmake)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/trieste/cmake)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
 if(NOT "parsers" IN_LIST FEATURES)
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 endif()
