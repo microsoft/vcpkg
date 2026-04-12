c97baebf840795a7a4ff91e918cbc67da2f2d388
Ethin Probst <ethindp@pm.me>
2026-04-12 14:17:37 -0500

    Update sha512

--- ports/libspeechd/portfile.cmake
+++ ports/libspeechd/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO brailcom/speechd
     REF "${VERSION}"
-    SHA512 0  # TODO: fill via  vcpkg hash <tarball>
+    SHA512 8747c2cd09e378533a8c756126623b659a5adce8991602f2c8a8e4318f24dee6fd518472095bfab31911d683c4624ad9909e1f2f5c3ab5412e574b067f50068d
     HEAD_REF master
     PATCHES
         libs-only.patch
