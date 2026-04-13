9ca7ca0b0cc5a2069f7ba6cb78fac79490e22d84
Arpit <arpitjaiswal9526@gmail.com>
2026-04-14 00:51:51 +0530

    Fix feature testing and config fixup

--- ports/minecraft-server-status/portfile.cmake
+++ ports/minecraft-server-status/portfile.cmake
@@ -10,8 +10,11 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
