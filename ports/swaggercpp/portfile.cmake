c610279c1c8a3e04256d878f8c9e115375e2d726
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2026-04-15 17:19:04 -0600

    [swaggercpp] Fix portfile: install usage file, remove debug/share, unquote REF

--- ports/swaggercpp/portfile.cmake
+++ ports/swaggercpp/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stescobedo92/swagger-cpp
-    REF "v0.2.1"
+    REF v0.2.1
     SHA512 8258929fd1d281b4d44395794fb0db4735e59fa50af085329be96642ac15d76231769363e49ae974001fa3843c6152c144f2598537eeeaa50252bd496ab3c01d
     HEAD_REF master
 )
@@ -18,4 +18,6 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME swaggercpp CONFIG_PATH lib/cmake/swaggercpp)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
