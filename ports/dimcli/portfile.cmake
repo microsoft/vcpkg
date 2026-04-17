7e267a5d5b0146145c25217899b7879d000fac55
toge <toge.mail@gmail.com>
2026-04-18 01:17:11 +0900

    support windows static build, include algorithm header

--- ports/dimcli/portfile.cmake
+++ ports/dimcli/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 e536e589c3e384a6a1899bfe6c6306a70d2e7614902f0beb487f00ae37181d208cb85d293e350353049dd3003c7096fcb5f187b31e4a3bb39767cc8bb85d48f3
     HEAD_REF master
+    PATCHES
+        include-algorithm.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" staticCrt)
