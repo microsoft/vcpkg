5dac36e38ec595bf8fbf7e407626cf6e4bc666a6
Nick Matteo <nmatteo@scitec.com>
2026-04-21 09:21:30 -0600

    [cpprestsdk] patch to fix clang error with unique_ptr to incomplete type

--- ports/cpprestsdk/portfile.cmake
+++ ports/cpprestsdk/portfile.cmake
@@ -11,6 +11,7 @@ vcpkg_from_github(
         fix-clang-dllimport.patch # workaround for https://github.com/microsoft/cpprestsdk/issues/1710
         silence-stdext-checked-array-iterators-warning.patch
         fix-asio-error.patch
+        fix-incomplete-json-value.patch
 )
 
 vcpkg_check_features(
