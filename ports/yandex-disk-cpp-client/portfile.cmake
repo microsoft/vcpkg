e6926402d7367ead53eb84660b3e5be1d9791e21
Fyodor Krasnov <f.krasnovvvvv@gmail.com>
2026-04-21 20:57:22 +0300

    [yandex-disk-cpp-client] Update to 1.0.4 (#51261)
    Co-authored-by: Billy O'Neal <bion@microsoft.com>

--- ports/yandex-disk-cpp-client/portfile.cmake
+++ ports/yandex-disk-cpp-client/portfile.cmake
@@ -3,20 +3,28 @@ set(VCPKG_POLICY_ALLOW_DEBUG_SHARE enabled)
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Krasnovvvvv/yandex-disk-cpp-client
-        REF v1.0.3
-        SHA512 de0e68aa0419f9918afea9fa7741477941d63c21e08cbe50d27a5fe9de7160a7a1f5ce4d307c906001aa757f82951295189c5d213b788987449d1a1b102da945
+        REF v1.0.4
+        SHA512 93a142d5bce93fe9f751decede2dfd715f5f21cd72f716a58b16202483cab8fdc4102f558e738864e1ac57e941e95d62ba4af6a9b60a89c1bdb72e8ffe24dd88
         HEAD_REF main
 )
+
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+            -DBUILD_EXAMPLES=OFF
+            -DBUILD_TESTS=OFF
 )
+
 vcpkg_cmake_install()
+
 vcpkg_cmake_config_fixup(
         PACKAGE_NAME "yandex-disk-cpp-client"
         CONFIG_PATH "lib/cmake/yandex-disk-cpp-client"
 )
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
