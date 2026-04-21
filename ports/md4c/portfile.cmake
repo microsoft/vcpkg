4895e1ef2608ce38f726b62cc573956996bd6613
toge <toge.mail@gmail.com>
2026-04-22 00:31:25 +0900

    [md4c] update to 0.5.3

--- ports/md4c/portfile.cmake
+++ ports/md4c/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mity/md4c
     REF "release-${VERSION}"
-    SHA512 30607ba39d6c59329f5a56a90cd816ff60b82ea752ac2b9df356d756529cfc49170019fae5df32fa94afc0e2a186c66eaf56fa6373d18436c06ace670675ba85
+    SHA512 213d6b9fbad24b2bfb4fa0a8124cb4c20861da2cb57790882aa0e5ff8c18903450f1d9ffdbcc0547debd103137777059f27a526cd818294f698b5ffdbfe7fbcb
     HEAD_REF master
     PATCHES
         "cmake.patch"
