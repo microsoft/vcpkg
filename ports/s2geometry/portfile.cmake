c09415a644586f33d6a071b580fd560916f8e6fb
toge <toge.mail@gmail.com>
2026-04-24 02:37:34 +0900

    fix msvc build

--- ports/s2geometry/portfile.cmake
+++ ports/s2geometry/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 345207937b8c1580eb7adc527e29c33b140d9042716425896c121b2661404675faf4e8ac5fdaf804e44a9165de049776897f24b56a90bfab3f8b1c632a34322b
     HEAD_REF main
+    PATCHES
+        fix-msvc-build.patch
 )
 
 vcpkg_cmake_configure(
