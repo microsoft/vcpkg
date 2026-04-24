10f79127aad50e63ad1554f3a7aef2747d8d099f
xiaozhuai <798047000@qq.com>
2026-04-24 19:22:57 +0800

    Optimize code

--- scripts/test_ports/vcpkg-ci-highway/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-highway/project/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <random>
 #include <vector>
 #include "scale.hpp"
--- scripts/test_ports/vcpkg-ci-highway/project/scale.cpp
+++ scripts/test_ports/vcpkg-ci-highway/project/scale.cpp
@@ -9,25 +9,30 @@ namespace HWY_NAMESPACE {
 namespace hn = hwy::HWY_NAMESPACE;
 
 void scale(float *HWY_RESTRICT dst, const float *HWY_RESTRICT src, size_t size, float factor) {
-    auto scale_one = [](auto d, float *HWY_RESTRICT dst, const float *HWY_RESTRICT src, auto s) {
-        const auto x = hn::Load(d, src);
-        const auto o = hn::Mul(x, s);
-        hn::Store(o, d, dst);
-    };
-
     size_t i = 0;
 
-    constexpr hn::ScalableTag<float> dn;
-    const auto sn = hn::Set(dn, factor);
-    const size_t N = hn::Lanes(dn);
-    for (; i + N <= size; i += N) {
-        scale_one(dn, dst + i, src + i, sn);
+    constexpr hn::ScalableTag<float> d;
+    const auto vf = hn::Set(d, factor);
+    const size_t N = hn::Lanes(d);
+
+    for (; i + 4 * N <= size; i += 4 * N) {
+        auto x0 = hn::Load(d, src + i + 0 * N);
+        auto x1 = hn::Load(d, src + i + 1 * N);
+        auto x2 = hn::Load(d, src + i + 2 * N);
+        auto x3 = hn::Load(d, src + i + 3 * N);
+        hn::Store(hn::Mul(x0, vf), d, dst + i + 0 * N);
+        hn::Store(hn::Mul(x1, vf), d, dst + i + 1 * N);
+        hn::Store(hn::Mul(x2, vf), d, dst + i + 2 * N);
+        hn::Store(hn::Mul(x3, vf), d, dst + i + 3 * N);
     }
 
-    constexpr hn::CappedTag<float, 1> d1;
-    const auto s1 = hn::Set(d1, factor);
+    for (; i + N <= size; i += N) {
+        auto x = hn::Load(d, src + i);
+        hn::Store(hn::Mul(x, vf), d, dst + i);
+    }
+
     for (; i < size; ++i) {
-        scale_one(d1, dst + i, src + i, s1);
+        dst[i] = src[i] * factor;
     }
 }
 
