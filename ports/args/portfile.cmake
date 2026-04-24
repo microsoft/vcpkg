5bbeac912e6ce6800110f4b01c2116a158d2671d
Weihang Ding <798047000@qq.com>
2026-04-25 04:35:19 +0800

    [args] Update to 6.4.11 (#51360)

--- ports/args/portfile.cmake
+++ ports/args/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Taywee/args
     REF "${VERSION}"
-    SHA512 940a2cc788987adbed3e017d1e90bb29bd71c20c825fe3e586318df6f4ed14817fefdbb8a39d8e2a09998f99d222b437cb613c484fa523c1d22fecf67fff1271
+    SHA512 0cb0b3bbb54ec4a8a5dc110b50e51e20cbe97ffefdaa726bd69d72ccfc726603b9df82f5f2a76bb53efbcff9d0de877a2679470fcdde13cdc985408c0a25c862
     HEAD_REF master
 )
 
