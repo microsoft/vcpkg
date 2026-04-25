33d2ecc9336bc888175071437de9ecaaeed8910f
Waffle <csshin9928@gmail.com>
2026-04-25 14:45:57 +0900

    [steamworks-sdk] Remove library directories and add usage file

--- ports/steamworks-sdk/portfile.cmake
+++ ports/steamworks-sdk/portfile.cmake
@@ -17,6 +17,17 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME SteamworksSDK CONFIG_PATH lib/cmake/SteamworksSDK)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/lib"
+    "${CURRENT_PACKAGES_DIR}/lib"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linux32"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linux64"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linuxarm64"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/osx"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/win32"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib/win64"
+)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
