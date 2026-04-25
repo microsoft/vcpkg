1820fefd3b67b1c45ffe843b2a9af2dcd8b7ce45
Waffle <csshin9928@gmail.com>
2026-04-25 14:59:59 +0900

    [steamworks-sdk] Simplify file removal in portfile and remove debug/include, include/steam/lib/

--- ports/steamworks-sdk/portfile.cmake
+++ ports/steamworks-sdk/portfile.cmake
@@ -19,13 +19,9 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME SteamworksSDK CONFIG_PATH lib/cmake/Steamw
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/lib"
+    "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/lib"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linux32"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linux64"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/linuxarm64"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/osx"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/win32"
-    "${CURRENT_PACKAGES_DIR}/include/steam/lib/win64"
+    "${CURRENT_PACKAGES_DIR}/include/steam/lib"
 )
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
