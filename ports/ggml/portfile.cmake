723db62da2b6d748ae307e1f459b97f7531d7e19
miyanyan <1138989048@qq.com>
2026-05-08 12:33:07 +0800

    fix vulkan errors

--- ports/ggml/portfile.cmake
+++ ports/ggml/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
         relax-link-options.diff
         vulkan-shaders-gen.diff
         fix-dequant_funcs.diff
+        fix-vulkan-spv-shadowing.diff
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
