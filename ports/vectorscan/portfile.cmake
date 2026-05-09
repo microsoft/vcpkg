261d9779368b15d9d55f2fb3a11db49fe62c54c7
toge <toge.mail@gmail.com>
2026-05-09 19:24:32 +0900

    [vectorscan] add chimera feature

--- ports/vectorscan/portfile.cmake
+++ ports/vectorscan/portfile.cmake
@@ -6,10 +6,12 @@ vcpkg_from_github(
     HEAD_REF develop
     PATCHES
         remove-Werror.patch
+        enable-chimera.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
+        chimera         BUILD_CHIMERA
         dump            DUMP_SUPPORT
 )
 
