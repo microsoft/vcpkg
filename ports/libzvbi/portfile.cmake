6b4c6929783d59595b740d87d7e5b164147eae7a
Christophe Greisberger <christophe@greisberger.fr>
2026-05-13 22:45:04 +0200

    refactored port with Copilot

--- ports/libzvbi/portfile.cmake
+++ ports/libzvbi/portfile.cmake
@@ -1,4 +1,4 @@
-# zvbi has no __declspec(dllexport) annotations, so static only on Windows
+# zvbi has no __declspec(dllexport) annotations, so static only on Windows.
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
@@ -13,66 +13,29 @@ vcpkg_from_github(
         patches/001-msvc-compat.patch
 )
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    # Copy our CMake build file into the source tree
-    file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}/src")
-    # config.h   - autotools config (HAVE_*, ssize_t, mode_t, S_IRUSR, SSIZE_MAX, etc.)
-    # site_def.h - empty build-config placeholder
-    file(COPY "${CMAKE_CURRENT_LIST_DIR}/config.h"   DESTINATION "${SOURCE_PATH}/src")
-    file(COPY "${CMAKE_CURRENT_LIST_DIR}/site_def.h" DESTINATION "${SOURCE_PATH}/src")
-else()
+if(NOT VCPKG_TARGET_IS_WINDOWS)
     # The MSVC-compat patch creates Windows-only shim headers that shadow POSIX equivalents.
-    # Remove them so the autotools build uses the real system headers.
+    # Remove them so the autotools build uses the real system headers on Unix/macOS.
     file(REMOVE "${SOURCE_PATH}/src/unistd.h")
     file(REMOVE "${SOURCE_PATH}/src/strings.h")
     file(REMOVE_RECURSE "${SOURCE_PATH}/src/sys")
 endif()
 
+vcpkg_make_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    AUTORECONF
+    OPTIONS
+        --disable-nls
+        --disable-examples
+        --disable-tests
+)
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}/src"
-    )
-    vcpkg_cmake_install()
-    vcpkg_copy_pdbs()
-    vcpkg_fixup_pkgconfig()
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-else()
-    vcpkg_find_acquire_program(PKGCONFIG)
-    vcpkg_configure_make(
-        SOURCE_PATH "${SOURCE_PATH}"
-        AUTOCONFIG
-        OPTIONS
-            --disable-static
-            --enable-shared
-    )
-    vcpkg_install_make()
-    vcpkg_fixup_pkgconfig()
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-endif()
+vcpkg_make_install()
+vcpkg_fixup_pkgconfig()
 
-# Some locale trees can be created without .mo files, leaving empty LC_MESSAGES
-# directories which fail vcpkg post-build validation.
-foreach(_locale_root IN ITEMS
-    "${CURRENT_PACKAGES_DIR}/share/libzvbi/locale"
-    "${CURRENT_PACKAGES_DIR}/debug/share/libzvbi/locale")
-    if(EXISTS "${_locale_root}")
-        file(GLOB_RECURSE _locale_dirs LIST_DIRECTORIES true "${_locale_root}/*")
-        list(REVERSE _locale_dirs)
-        foreach(_dir IN LISTS _locale_dirs)
-            if(IS_DIRECTORY "${_dir}")
-                file(GLOB _children "${_dir}/*")
-                if(_children STREQUAL "")
-                    file(REMOVE_RECURSE "${_dir}")
-                endif()
-            endif()
-        endforeach()
-        file(GLOB _root_children "${_locale_root}/*")
-        if(_root_children STREQUAL "")
-            file(REMOVE_RECURSE "${_locale_root}")
-        endif()
-    endif()
-endforeach()
+# Remove tools (not needed; avoids RPATH/headerpad issues on macOS).
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/tools")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING.md")
