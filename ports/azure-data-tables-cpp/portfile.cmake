18fb29da5022cba1c1959efce79eea48daf5a191
azure-sdk <azuresdk@microsoft.com>
2026-05-15 05:03:19 +0000

    Daily vcpkg ports for tables

--- ports/azure-data-tables-cpp/portfile.cmake
+++ ports/azure-data-tables-cpp/portfile.cmake
@@ -4,18 +4,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-data-tables_${VERSION}"
-    SHA512 368ad9a26e824de2f547263ce923e6ccdcadaadf43bdbbffe7d88948b2f9012f5f545a073caa4311d3ff5c5486ceb0b849a5e130cff295926fbed17153d959a5
+    REF 7ce20e70edb2ca3a1bdd74bd5d0aff0360172a1e
+    SHA512 6d33087269f09cdf241ab6a1aa9eae6469a5f4b84b54c3cce0d69da820d4cbe5a78f5f264f81f2cda9ca1e4176cf42c462099fa582de63a1faed674a4dbe6a57
     HEAD_REF main
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")
 file(REMOVE_RECURSE ${unused})
 
 if(EXISTS "${SOURCE_PATH}/sdk/tables/azure-data-tables")
