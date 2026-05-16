7c7b95b9e1f72372030a2f735eaa3a05d72a8c80
Greisberger Christophe <greisberger@zenon-media.com>
2026-05-16 20:27:36 +0200

    fixed .pc

--- ports/libtwolame/portfile.cmake
+++ ports/libtwolame/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_github(
         patches/002-disable-doc-subdir.patch
         patches/003-fix-kr-declaration.patch
         patches/004-fix-frontend-msvc.patch
+        patches/005-fix-pc-in-case.patch
 )
 
 if("tool" IN_LIST FEATURES)
@@ -30,18 +31,6 @@ vcpkg_configure_make(
 
 vcpkg_install_make()
 vcpkg_fixup_pkgconfig()
-# vcpkg_fixup_pkgconfig() does not do its job
-if(NOT VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    foreach(_pc_suffix IN ITEMS "lib/pkgconfig/twolame.pc" "debug/lib/pkgconfig/twolame.pc")
-        set(_pc_file "${CURRENT_PACKAGES_DIR}/${_pc_suffix}")
-        if(EXISTS "${_pc_file}")
-            file(READ "${_pc_file}" _pc_content)
-            string(REPLACE "-ltwolame" "-ltwolame -lm" _pc_content "${_pc_content}")
-            string(REGEX REPLACE "Libs\\.Private:[^\n]*" "Libs.Private:" _pc_content "${_pc_content}")
-            file(WRITE "${_pc_file}" "${_pc_content}")
-        endif()
-    endforeach()
-endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
