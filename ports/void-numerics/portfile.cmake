d2252d75ae4f53f1e2e335bc2ad1d591692a799c
Billy Robert O'Neal III <bion@microsoft.com>
2026-05-18 19:53:24 -0700

    Nitpicks: * Put the header only thing at the top * Trim trailing whitespace * LF-ize

--- ports/void-numerics/portfile.cmake
+++ ports/void-numerics/portfile.cmake
@@ -1,12 +1,12 @@
+set(VCPKG_BUILD_TYPE release) # header-only
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO nihilai-collective/void-numerics
-    REF "v${VERSION}"    
+    REF "v${VERSION}"
     SHA512 3a3cc255271bfd9ba4e8819d2290f8a2482d71316b960b548fde7b51e36dff41d9fcfc7b1e9997f327377c8232e92c24e2ad828dd337b85ec833c2450ed19958
     HEAD_REF main
 )
-
-set(VCPKG_BUILD_TYPE release) # header-only
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
