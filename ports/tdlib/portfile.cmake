99347a02d10c3817c138240ec2093ee0d6297ef9
Saikari <lin@sz.cn.eu.org>
2026-05-20 21:06:57 +0300

    tdlib: update to 1.8.64

--- ports/tdlib/portfile.cmake
+++ ports/tdlib/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tdlib/td
-    REF f06b0bac65278b03d26414c096080e7bfecfef52
+    REF 7f163d850abbebecf749e99fb412271cc1f4e805
     HEAD_REF master
-    SHA512 91967a24eee9f1491b780ce72a1323aa99e228c10ecd588979e325d57417c6897eeebf375c609c99b2fd0d6137bcb950628a30f5cfc2e6838fb14d2803d02b7a
+    SHA512 6cebc7f655267abbbdfca1717c36004c2ae11626420fe0ce748e402ca53e972b6cbad2ca34607d04e66e28cd740a5faf215505273845adb06075db4d09cd8e46
     PATCHES
         fix-pc.patch
         fix-cross-compile.patch
