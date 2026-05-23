84551dd273054ee4754b524504b5b8a8ab736e59
miyanyan <1138989048@qq.com>
2026-05-23 10:23:50 +0800

    [qtconnectivity] add default feature bluez

--- ports/qtconnectivity/portfile.cmake
+++ ports/qtconnectivity/portfile.cmake
@@ -5,9 +5,19 @@ set(${PORT}_PATCHES
     silence-qtconnectivity-coroutine-warnings.diff
 )
 
+vcpkg_check_features(OUT_FEATURE_OPTIONS feature_options
+    INVERTED_FEATURES
+        bluez CMAKE_DISABLE_FIND_PACKAGE_BlueZ
+)
+
+if("bluez" IN_LIST FEATURES)
+    message(WARNING "qtconnectivity[bluez] requires the BlueZ development headers from the system package manager. "
+    "They can be installed on Debian/Ubuntu systems via sudo apt install libbluetooth-dev.")
+endif()
+
 qt_install_submodule(PATCHES    ${${PORT}_PATCHES}
                      CONFIGURE_OPTIONS
-                      -DCMAKE_DISABLE_FIND_PACKAGE_BlueZ:BOOL=ON
+                      ${feature_options}
                       -DCMAKE_DISABLE_FIND_PACKAGE_PCSCLITE:BOOL=ON
                      CONFIGURE_OPTIONS_RELEASE
                      CONFIGURE_OPTIONS_DEBUG
