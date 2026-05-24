ac251cbafdfb6282a6c5f81ecd033d1cbd689d89
kunlinglio <142668432+kunlinglio@users.noreply.github.com>
2026-05-24 18:44:29 +0800

    [libproxy] Update to 0.5.12

--- ports/libproxy/portfile.cmake
+++ ports/libproxy/portfile.cmake
@@ -2,58 +2,66 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libproxy/libproxy
     REF "${VERSION}"
-    SHA512 1148d688a9f070273a1a2b110a788561789799089660292bbba59fbf0a9caf7d28cb039a9ccdcb935f752e1e34739b2d2f4c784b1bb3bbaa03d108e7b38a4754
+    SHA512 
+    19ffd1755e9ebfc645555e248020f9025ae81e93e73cbbb2583bff4495f7794b02ab1ef8ce944fa28e7604f5abf3e7e327c4966d21789648bd38f3d1c2cccecd
     HEAD_REF master
-    PATCHES
-        support-windows.patch
-        fix-install-py.patch
-        fix-module-lib-name.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "static" STATICCRT)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        bindings-csharp WITH_DOTNET
-        bindings-python WITH_PYTHON2
-        bindings-python WITH_PYTHON3
-        bindings-perl   WITH_PERL
-        bindings-vala   WITH_VALA
-        tools           BUILD_TOOLS
-        tests           BUILD_TESTING
+        curl          curl
+        introspection introspection
+        vala          vapi
+        duktape       pacrunner-duktape
+        tests         tests
 )
 
-vcpkg_cmake_configure(
+# Convert FEATURE_OPTIONS from "feature=ON/OFF" to "feature=true/false"
+vcpkg_list(SET FINAL_OPTIONS)
+foreach(_option IN LISTS FEATURE_OPTIONS)
+    string(REPLACE "=ON" "=true" _option "${_option}")
+    string(REPLACE "=OFF" "=false" _option "${_option}")
+    vcpkg_list(APPEND FINAL_OPTIONS "${_option}")
+endforeach()
+
+macro(set_bool_val VAR COND)
+    if(${COND})
+        set(${VAR} "true")
+    else()
+        set(${VAR} "false")
+    endif()
+endmacro()
+
+set_bool_val(CONF_WIN VCPKG_TARGET_IS_WINDOWS)
+set_bool_val(CONF_OSX VCPKG_TARGET_IS_OSX)
+set_bool_val(CONF_LIN VCPKG_TARGET_IS_LINUX)
+
+vcpkg_configure_meson(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS ${FEATURE_OPTIONS}
-        -DWITH_WEBKIT3=OFF
-        -DWITH_KDE=${VCPKG_TARGET_IS_LINUX}
-        -DMSVC_STATIC=${STATICCRT}
-        -DWITH_GNOME3=OFF
-    MAYBE_UNUSED_VARIABLES
-        WITH_DOTNET
-        WITH_PERL
-        WITH_PYTHON2
-        WITH_PYTHON3
-        WITH_VALA
-        MSVC_STATIC
-        BUILD_TOOLS
-        WITH_GNOME3
+    OPTIONS
+        ${FINAL_OPTIONS}
+        -Ddocs=false
+        -Drelease=true
+        -Dconfig-windows=${CONF_WIN}
+        -Dconfig-osx=${CONF_OSX}
+        -Dconfig-gnome=${CONF_LIN}
+        -Dconfig-kde=${CONF_LIN}
+        -Dconfig-xdp=${CONF_LIN}
+        -Dconfig-env=true
+        -Dconfig-sysconfig=true
 )
 
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/Modules)
-vcpkg_copy_pdbs()
+vcpkg_install_meson()
 vcpkg_fixup_pkgconfig()
 
 vcpkg_copy_tools(TOOL_NAMES proxy AUTO_CLEAN)
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/vcpkg-cmake-wrapper.cmake"
-          "${CMAKE_CURRENT_LIST_DIR}/usage"
-          DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
