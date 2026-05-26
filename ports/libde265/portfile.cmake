2e941948d30f029b136e88b5212549d7c00b2d6d
xiaozhuai <798047000@qq.com>
2026-05-26 18:33:46 +0800

    Fix

--- ports/libde265/portfile.cmake
+++ ports/libde265/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     SHA512 3e8de169ac6dfcdb67558ea762c02de2b71bd47e3755c5c604893c53b211ab7d43c4d55ee2d178819994303c917620c091a5a413e35ab0d25eb8c78e04f8f254
     HEAD_REF master
     PATCHES
-        pkgconfig-cxx-linkage.diff
+        fix-linkage.patch
 )
 
 vcpkg_cmake_configure(
