fdcd67ab08d06451383bbbb2e7911f5cb7ef0c2e
Risman Hadi Nata <rismanh158@gmail.com>
2026-05-29 08:32:10 +0700

    Update ports/rismanasdk-libhttp/portfile.cmake
    Co-authored-by: Victor Romero <romerosanchezv@gmail.com>

--- ports/rismanasdk-libhttp/portfile.cmake
+++ ports/rismanasdk-libhttp/portfile.cmake
@@ -12,10 +12,6 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME libhttp CONFIG_PATH lib/cmake/libhttp)
 
-file(REMOVE_RECURSE
-    "${CURRENT_PACKAGES_DIR}/debug/include"
-    "${CURRENT_PACKAGES_DIR}/debug/lib"
-    "${CURRENT_PACKAGES_DIR}/debug/share"
-)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
