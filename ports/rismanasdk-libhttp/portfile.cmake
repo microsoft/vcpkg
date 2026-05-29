57ebdb77819de06b9f812f88468acf3f771e06cd
Risman Hadi Nata <rismanh158@gmail.com>
2026-05-29 08:44:08 +0700

    Address reviewer feedback: ensure proper port naming and cleanup empty directories
    - Confirm port name is 'rismanasdk-libhttp' following <OWNER>-<REPO> convention
    - Ensure lib folder is removed after vcpkg_cmake_config_fixup() to avoid leftover empty directories
    - Mark as ready for review

--- ports/rismanasdk-libhttp/portfile.cmake
+++ ports/rismanasdk-libhttp/portfile.cmake
@@ -1,4 +1,4 @@
-set(VCPKG_BUILD_TYPE release)
+set(VCPKG_BUILD_TYPE release) # header-only library
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
@@ -12,6 +12,7 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME libhttp CONFIG_PATH lib/cmake/libhttp)
 
+# Remove leftover empty lib folder after cmake config fixup
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
