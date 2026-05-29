3024b2168cb38ee29c301d9be5cc8b3fe76187cd
xiaozhuai <798047000@qq.com>
2026-05-29 14:42:31 +0800

    Fix

--- scripts/test_ports/vcpkg-ci-libusb/project/main.c
+++ scripts/test_ports/vcpkg-ci-libusb/project/main.c
@@ -1,11 +1,9 @@
-#include <libusb-1.0/libusb.h>
+#include <libusb.h>
 
-int main()
-{
+int main() {
     libusb_context *ctx = NULL;
     int ret = libusb_init(&ctx);
-    if (ret != 0)
-        return 1;
+    if (ret != 0) return 1;
     libusb_exit(ctx);
     return 0;
 }
