209c3a9d643a8c1b477889c7759365472643c48a
xiaozhuai <798047000@qq.com>
2026-05-29 14:34:54 +0800

    [vcpkg-ci-libusb] New ci port

---
+++ scripts/test_ports/vcpkg-ci-libusb/project/main.c
@@ -0,0 +1,11 @@
+#include <libusb-1.0/libusb.h>
+
+int main()
+{
+    libusb_context *ctx = NULL;
+    int ret = libusb_init(&ctx);
+    if (ret != 0)
+        return 1;
+    libusb_exit(ctx);
+    return 0;
+}
