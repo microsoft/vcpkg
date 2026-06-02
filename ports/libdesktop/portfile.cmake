197c30194c365f3dee32ed848a341372789c4851
Nick Logozzo <nlogozzo225@gmail.com>
2026-06-01 21:15:31 -0400

    [libdesktop] Add package

---
+++ ports/libdesktop/portfile.cmake
@@ -0,0 +1,34 @@
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO NickvisionApps/libdesktop
+    REF "${VERSION}"
+    SHA512 8917464ca9a592e85ff34360e2a4001fb3cc87983596e409f2a78a9f91696ce2aa58404c424f456aee018dde0f0e769f16ed7eec9ab67ab8863aade36649e8e6
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTING=OFF
+		-DENABLE_CLANG_FORMAT=OFF
+		-DENABLE_CLANG_TIDY=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libdesktop)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_copy_pdbs()
+
+vcpkg_fixup_pkgconfig()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
