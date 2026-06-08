b3a1f4fae893e5bb47ce6ce403bcdcfe731dd407
Billy Robert O'Neal III <bion@microsoft.com>
2026-06-08 23:43:49 +0000

    LFize, add running the output, and diagnostics.

--- scripts/test_ports/vcpkg-ci-alibabacloud-oss-cpp-sdk-v2/portfile.cmake
+++ scripts/test_ports/vcpkg-ci-alibabacloud-oss-cpp-sdk-v2/portfile.cmake
@@ -4,4 +4,12 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${CURRENT_PORT_DIR}/project"
 )
 
-vcpkg_cmake_build()
+vcpkg_cmake_install()
+
+if(NOT VCPKG_CROSSCOMPILING)
+    vcpkg_execute_required_process(
+        COMMAND "${CURRENT_PACKAGES_DIR}/bin/${PORT}/main"
+        WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}"
+        LOGNAME runtime-${TARGET_TRIPLET}
+    )
+endif()
