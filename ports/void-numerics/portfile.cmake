5d326f8f88441be22039cb924ed76115f3a5a176
xiaozhuai <798047000@qq.com>
2026-06-09 11:44:34 +0800

    Fix cmake config

--- ports/void-numerics/portfile.cmake
+++ ports/void-numerics/portfile.cmake
@@ -6,6 +6,9 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 05a4cd716239dfe2a1ce9bb749f9f9eea8410cfab3f5334a62615be188d0d9ace76b934330ab46d3999f4983b95873f153b917c2731a2d483c36d335552958d5
     HEAD_REF main
+    PATCHES
+        001-fix-compile-and-link-options.patch
+        002-fix-cmake-config.patch
 )
 
 vcpkg_cmake_configure(
@@ -13,5 +16,6 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/nihilus)
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/License.md")
