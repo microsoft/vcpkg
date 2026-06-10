e3d4d5dc47e79c551ec875329e87122e061ceb73
cliquot22 <mpeterson@theiatech.com>
2026-06-09 17:05:21 -0700

    Debugging vcpkg files

--- ports/theiamcr-c/portfile.cmake
+++ ports/theiamcr-c/portfile.cmake
@@ -1,18 +1,30 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cliquot22/TheiaMCR_C
-    REF "${VERSION}"
-    SHA512 0  # vcpkg will auto-update this
+    REF "v${VERSION}"
+    SHA512 97d8730d51a09ba9a22ab86f5e541624e022b319e2731c23c9370994ac95dd69901bbfd69a987b1c20d0a2d634536b61fb4eda047bb19384f5773f87baa280ae  # keep your existing hash
     HEAD_REF main
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        python ENABLE_PYTHON_BINDINGS
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 
-file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+# Fix 1: Move CMake config files from lib/cmake to share/${PORT}
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/TheiaMCR_C)
 
-# Install headers
+# Fix 2: Remove duplicate headers from debug tree
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/license" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
 vcpkg_copy_pdbs()
