142bc19e72ac7b643c6e0413ae7d574a0f2848c3
Mark Peterson <mpeterson@theiatech.com>
2026-06-10 12:24:20 -0700

    Fix static build and restrict python feature to non-static triplets

--- ports/theiamcr-c/portfile.cmake
+++ ports/theiamcr-c/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO cliquot22/TheiaMCR_C
     REF "v${VERSION}"
-    SHA512 97d8730d51a09ba9a22ab86f5e541624e022b319e2731c23c9370994ac95dd69901bbfd69a987b1c20d0a2d634536b61fb4eda047bb19384f5773f87baa280ae  # keep your existing hash
+    SHA512 <your-current-hash>
     HEAD_REF main
 )
 
@@ -19,14 +19,19 @@ vcpkg_cmake_configure(
 
 vcpkg_cmake_install()
 
-# Fix 1: Move CMake config files from lib/cmake to share/${PORT}
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/TheiaMCR_C)
 
-# Fix 2: Remove duplicate headers from debug tree
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
-file(INSTALL "${SOURCE_PATH}/license" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin"
+                        "${CURRENT_PACKAGES_DIR}/bin")
+endif()
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(INSTALL "${SOURCE_PATH}/license"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage"
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_copy_pdbs()
