daebb9842b3b4267e301d5d86a49a6d797b981c8
kzhdev <kzhdev@gmail.com>
2026-06-10 20:45:59 -0500

    [slick-stream-buffer] Add new port

---
+++ ports/slick-stream-buffer/portfile.cmake
@@ -0,0 +1,28 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO SlickQuant/slick-stream-buffer
+    REF "v${VERSION}"
+    SHA512 d99d247993e23ead39f36806fdff91e085b5fcfbfe793ba59fa80e2b05f5879d19a9cfcf8849cf9621ea68be5fcb6b8e6b3b8d9db88fd5031c0b371d348c960a
+    HEAD_REF main
+    PATCHES
+        slick-shm.patch
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_SLICK_STREAM_BUFFER_TESTS=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+    PACKAGE_NAME slick-stream-buffer
+    CONFIG_PATH lib/cmake/slick-stream-buffer
+)
+
+# Header-only library - remove lib directory
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+
+# Install license
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
