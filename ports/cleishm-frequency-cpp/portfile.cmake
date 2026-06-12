7e25e97b07d3c790e498d42f863de5c1231dd063
Billy Robert O'Neal III <bion@microsoft.com>
2026-06-11 17:11:46 -0700

    [cleishm-frequency-cpp] Remove incorrect usage

--- ports/cleishm-frequency-cpp/portfile.cmake
+++ ports/cleishm-frequency-cpp/portfile.cmake
@@ -18,6 +18,4 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME frequency CONFIG_PATH lib/cmake/frequency)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
