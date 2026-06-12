ccfa35d81850f6898196e98205f3c38635597225
kzhdev <kzhdev@gmail.com>
2026-06-12 11:06:07 -0500

    address PR comments; moved dynamic_stream_buffer out

--- ports/slick-stream-buffer/portfile.cmake
+++ ports/slick-stream-buffer/portfile.cmake
@@ -4,26 +4,23 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SlickQuant/slick-stream-buffer
     REF "v${VERSION}"
-    SHA512 756201be1df980c7281a15684fbef96bb7caaf1412aa5b1e4e353fc06e2f7ea99ae383216275bcd33fbfc24d190c3a2bc25bcbe122a174da21099e5b1dec46ba
+    SHA512 bd940b4077385ecf00fff11d551a59fd6fd948eb96e6e8d36c8226d2e9f960c677699a8ca2cb5233ea445d622e5dded6cc67365a8632f9e58f9a9ffd83a6a8b7
     HEAD_REF main
-    PATCHES
-        slick-shm.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_SLICK_STREAM_BUFFER_TESTS=OFF
+        -DCMAKE_REQUIRE_FIND_PACKAGE_slick-shm=ON
 )
 
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(
-    PACKAGE_NAME slick-stream-buffer
     CONFIG_PATH lib/cmake/slick-stream-buffer
 )
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
-# Install license
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
