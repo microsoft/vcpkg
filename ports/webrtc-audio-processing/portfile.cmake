8333ce7a50f836b72c7257adcdef00e5a1792547
Ben Stolovitz <ben@stolovitz.com>
2026-06-15 23:11:47 -0400

    fix compiler errors on linux & arm Windows

--- ports/webrtc-audio-processing/portfile.cmake
+++ ports/webrtc-audio-processing/portfile.cmake
@@ -5,7 +5,16 @@ vcpkg_from_gitlab(
     REF "v2.1"
     SHA512 6851bf40b62a8f642eaa5e3e108a8331f43c0d6eb5e6d351e3252d6869066c991bd9eca6ba3a7f762bdb44251a253da05cf15a76fc1c55ca815f5a4e39cf5d4a
     PATCHES
+        # Fixed in master, not yet shipped: https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/commit/c8896801dfbfe03b56f85c1533abc077ff74a533
         fix-abseil-nullability-compat.patch
+
+        # Fixed in master, not yet shipped: https://gitlab.freedesktop.org/pulseaudio/webrtc-audio-processing/-/commit/e9c78dc4712fa6362b0c839ad57b6b46dce1ba83
+        fix-gcc15-missing-cstdint.patch
+
+        # WebRTC typically builds with clang, but VCPKG typically builds with
+        # MSVC on Windows. Avoid some clang-specific assembly code in that
+        # scenario, with the same fallback used for x86.
+        fix-asm-windows-arm.patch
 )
 
 set(MESON_OPTIONS "")
