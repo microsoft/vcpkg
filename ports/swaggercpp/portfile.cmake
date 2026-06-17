a25ff11342d61e9a4fe17dd6f05b0683f5ba915f
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2026-06-17 10:38:54 -0600

    Update SHA512 to match recreated v0.3.0 tag

--- ports/swaggercpp/portfile.cmake
+++ ports/swaggercpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stescobedo92/swagger-cpp
     REF v0.3.0
-    SHA512 19773bc0d903d3a4a50477fc33cfebb490e86f0761fedc619da26d5ae4dfcd084f95c1616573b347d29f1a3e1062c808a11811dcd5b9a61ef13b7aaf0eb3856e
+    SHA512 4b35a8db282613e6fc1a873f47ab2edfe21f7c8aefd1f0cdbdb7be92207930d7dcd5788134d87298bd89e421989a330ac9a1db6ef6d33a42317e2953336f30a9
     HEAD_REF master
 )
 
