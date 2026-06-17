0cac6b78aff91704963679a8d8a850655e517a71
kzhdev <kzhdev@gmail.com>
2026-06-17 11:18:47 -0500

    update to version 1.0.1

--- ports/slick-stream-buffer-multiplexer/portfile.cmake
+++ ports/slick-stream-buffer-multiplexer/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SlickQuant/slick-stream-buffer-multiplexer
     REF "v${VERSION}"
-    SHA512 4100f3677f7b4e9930f4f051ea16015facac16f22522ef2a1ad56ae410f4d816e07b66591105354e14f20c14641bbeab6b85eaf036d8bf43843fc70b1e9d0b3b
+    SHA512 935b3d516e996f6d25948ba8a54c1b7f70f7f0e3f517e36481fdf0196c2c5cfc2841f86e891f3df9517746b7fb605db47cdded1b8ff78d9482ddaa621db43a34
     HEAD_REF main
     PATCHES
         slick-dependencies-fetching.patch
