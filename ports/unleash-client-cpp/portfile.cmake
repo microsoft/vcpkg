3a5fc763a41d58793d487c19b26202f1d608d02c
Antonio Ruiz <ant.ruiz@gmail.com>
2026-06-19 20:26:08 +0200

    unleash-client-cpp: update to 1.5.1

--- ports/unleash-client-cpp/portfile.cmake
+++ ports/unleash-client-cpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO aruizs/unleash-client-cpp
     REF "v${VERSION}"
-    SHA512 0ba3fa89bacfded6aaf54c5595ec4affc621563dc0b2b7917f5a444cb322336fa8c85ce236ef7ba3726edce778d00c6ad827b3a5bd3c4022898ae0eba872d869
+    SHA512 fa8214715d376839147b0b3f8f89d5ea5bac8c824b5814128b21cfabc3d9376cce2962fc373188b151878c3efa067c0f51cb5c66c55ffd25003e73426a5e7555
     HEAD_REF main
 )
 
@@ -11,8 +11,8 @@ vcpkg_cmake_configure(
     OPTIONS
         -DCMAKE_REQUIRE_FIND_PACKAGE_cpr=ON
         -DCMAKE_REQUIRE_FIND_PACKAGE_nlohmann_json=ON
-        -DENABLE_TESTING=OFF
-        -DENABLE_TEST_COVERAGE=OFF
+        -DUNLEASH_ENABLE_TESTING=OFF
+        -DUNLEASH_BUILD_EXAMPLES=OFF
 )
 
 vcpkg_cmake_install()
