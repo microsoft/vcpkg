6b43cb90de4fa299129fff451965a126e837e61d
xiaozhuai <798047000@qq.com>
2026-06-22 13:28:11 +0800

    [clap-cleveraudio] Update to 1.2.9

--- ports/clap-cleveraudio/portfile.cmake
+++ ports/clap-cleveraudio/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO free-audio/clap
     REF "${VERSION}"
-    SHA512 627ad083e7403a3ba69ca151757997e6ec7e94f9ee6b165c45e57b5ff0033f66b6fbfe99a8daa55574b86239912bf9f4d3f4377d08db986edbdc53c6ab870ccb
+    SHA512 5637edee7e9d63b7c7eafd0a9f25e6ed4e3670412802578ef1749cde4d16d63ec3f776e233a847b747a8db2e23ae4c6c3eeac84fb18d14f93a47cdf9a11e346b
     HEAD_REF main
 )
 
