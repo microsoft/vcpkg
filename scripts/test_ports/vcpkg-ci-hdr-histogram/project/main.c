d88a11becc73f0c48d7176dba865b10c23262ee0
xiaozhuai <798047000@qq.com>
2026-06-30 20:48:02 +0800

    [vcpkg-ci-hdr-histogram] New ci port

---
+++ scripts/test_ports/vcpkg-ci-hdr-histogram/project/main.c
@@ -0,0 +1,12 @@
+#include <hdr/hdr_histogram.h>
+
+int main() {
+    struct hdr_histogram* histogram = NULL;
+    int rc = hdr_init(1, 1000000, 3, &histogram);
+    if (rc != 0) {
+        return 1;
+    }
+    hdr_record_value(histogram, 42);
+    hdr_close(histogram);
+    return 0;
+}
