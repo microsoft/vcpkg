1f2f9fbd268d9635b2a0286e19f3369e41f2dfca
Nick D'Ademo <nickdademo@gmail.com>
2026-07-02 10:24:28 +0800

    [libassert] add wasm support (#52675)

--- ports/libassert/portfile.cmake
+++ ports/libassert/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 877f7ddac1b3ffa77d6c30b9aa4c6bf2a32bd3089b5348b75b4f52ef474cf6ee1f754bab5f0396e3ee3df83f9a438a5154c0fefce683c479b2f3a8adaef3c0a7
     HEAD_REF main
+    PATCHES
+        emscripten-platform.diff
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED)
