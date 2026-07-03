0b8aaa64813c0d174ad0d061994a2e711bbf465c
vrtulka23 <vrtulka23@pm.me>
2026-07-03 12:55:55 +0200

    fixed installation directories

--- ports/scinumtools3/portfile.cmake
+++ ports/scinumtools3/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO vrtulka23/scinumtools3
-    REF v0.5.9
-    SHA512 b5164dbb80517e9827a3588556357dd2dbf9b952ad4131379408c070b21bfcef6b7c2cecf5504aa6d3a4f2d35c7064abe0601fb31ae7512b231cadd2a579c9ec
+    REF v0.5.11
+    SHA512 301484eb60e70baf703b97291f5ca668a0c74f7986641aa0ae09e57489e195cfa1e49c85d03bc37b566bfec849bb3f247a285b49a680f4a4a28e5133ac2f76d5
 )
 
 vcpkg_cmake_configure(
