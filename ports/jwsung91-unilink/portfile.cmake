d23ce167f158e4362eeba8849a744cef1d422886
Blake <jwsung91@gmail.com>
2026-07-07 22:03:55 +0900

    Fix pkg-config metadata for static Windows builds

--- ports/jwsung91-unilink/portfile.cmake
+++ ports/jwsung91-unilink/portfile.cmake
@@ -25,6 +25,19 @@ vcpkg_cmake_config_fixup(
 
 vcpkg_fixup_pkgconfig()
 
+if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    foreach(_pc_file IN ITEMS
+        "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/unilink.pc"
+        "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/unilink.pc")
+        if(EXISTS "${_pc_file}")
+            file(READ "${_pc_file}" _unilink_pc_contents)
+            string(REGEX REPLACE "([ \\t]+)-lboost_system" "\\1" _unilink_pc_contents "${_unilink_pc_contents}")
+            string(REPLACE "-lboost_system" "" _unilink_pc_contents "${_unilink_pc_contents}")
+            file(WRITE "${_pc_file}" "${_unilink_pc_contents}")
+        endif()
+    endforeach()
+endif()
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
