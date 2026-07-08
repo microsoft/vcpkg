aa3f68190f95b7c9011d9ad2de7f6b81842ce267
toge <toge.mail@gmail.com>
2026-07-09 07:37:21 +0900

    [c4core] update to 0.5.0 (#52798)

--- ports/c4core/portfile.cmake
+++ ports/c4core/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO biojppm/c4core
     REF "v${VERSION}"
-    SHA512 dd98740e9c970297c04ad8386ce8e53469b57ae827485a102fd6648575e15234c93ead24e7fdfcada7c2e94d7512524b49ddd60d0017c5959d258f4df89150b0
+    SHA512 dd7847b7aad705edf41c6ece246c345ccd92ce38344ade582d5c5e14a99c7cc94e732bea20ea5594b7a572225f7ad1820cc2cd003ee21a682e3102d9d0c8a6c5
     HEAD_REF master
     PATCHES
         disable-cpack.patch
