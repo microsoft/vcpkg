db59f21b626fe0c5ee70b54339547782afe89988
xiaozhuai <798047000@qq.com>
2026-07-14 16:42:36 +0800

    Fix

--- ports/taskflow/portfile.cmake
+++ ports/taskflow/portfile.cmake
@@ -20,8 +20,9 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(CONFIG_PATH share/cmake/Taskflow)
+vcpkg_fixup_pkgconfig()
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug" "${CURRENT_PACKAGES_DIR}/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
