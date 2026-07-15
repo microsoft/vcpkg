2e88a046b28deb7e473de1ad99cf59d2b12d57d3
a4z <harald.achitz@gmail.com>
2026-07-15 20:23:11 +0200

    Update to latest 1.2.53003

--- ports/libsl3/portfile.cmake
+++ ports/libsl3/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO a4z/libsl3
     REF "v${VERSION}"
-    SHA512 07b3b15ff5ff716c08ad4aa9792e95f75ea8a4f34ac271bf85a82baeff63d5159b21b2bd38c7206a061b4a51e8bea320315945438dc221bb7332de5788152368
+    SHA512 392c73c9387a17286ea48e2cb5b70b8fc4713f5749947235631d5a297f11e69cafb92aed15fe766c071784dccd4e5e2fbb3c3ce715c7ffbc6495ee3913f9b12d
     HEAD_REF main
 )
 
@@ -14,13 +14,13 @@ vcpkg_cmake_configure(
     OPTIONS
         -DCMAKE_CXX_STANDARD=17
         -DCMAKE_CXX_STANDARD_REQUIRED=ON
-        -DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=ON
+        -Dsl3_BUILD_DOCS=OFF
         -Dsl3_USE_COMMON_COMPILER_WARNINGS=OFF
         -Dsl3_USE_INTERNAL_SQLITE3=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/sl3)
+vcpkg_cmake_config_fixup(PACKAGE_NAME sl3 CONFIG_PATH lib/cmake/sl3)
 vcpkg_copy_pdbs()
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
