7ae168560e4cd5f11007957c52c824e5815dfaf6
xiaozhuai <798047000@qq.com>
2026-07-17 15:56:38 +0800

    Fix

--- ports/upa-url/portfile.cmake
+++ ports/upa-url/portfile.cmake
@@ -15,6 +15,7 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "upa" CONFIG_PATH "lib/cmake/upa")
+vcpkg_fixup_pkgconfig()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
