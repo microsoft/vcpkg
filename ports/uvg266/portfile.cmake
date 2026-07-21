1371051376332d02f70645ddbe8e0a6d7923dc20
torusrxxx <torusrxxx@users.noreply.github.com>
2026-07-21 10:50:55 +0800

    [uvg266] fixes

--- ports/uvg266/portfile.cmake
+++ ports/uvg266/portfile.cmake
@@ -4,29 +4,32 @@ vcpkg_from_github(
     REF v${VERSION}
     SHA512 892b0732516fe2639f93b250bbed342da8134deeaa6f0ccb429ff8451df727f971c7ee284fef93eaa431c5c54a8b8789ffc853d8b45ae93433ba17007989bbae
     HEAD_REF master
-    PATCHES
 )
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    SET(BUILD_SHARED_LIBS OFF)
-else()
-    SET(BUILD_SHARED_LIBS ON)
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    DISABLE_PARALLEL_CONFIGURE
     OPTIONS 
         -DBUILD_TESTS=OFF
-        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
         -DGIT_SUBMODULE=OFF
+    MAYBE_UNUSED_VARIABLES GIT_SUBMODULE
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
-vcpkg_copy_tools(TOOL_NAMES uvg266 AUTO_CLEAN)
+vcpkg_copy_tools(TOOL_NAMES uvg266)
+vcpkg_copy_tool_dependencies("${CURRENT_PACKAGES_DIR}/tools/uvg266")
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    vcpkg_replace_string(
+        "${CURRENT_PACKAGES_DIR}/include/uvg266.h"
+        "#define UVG266_H_"
+        "#define UVG266_H_\n#define UVG_STATIC_LIB"
+    )
+endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/LICENSE.EXT.greatest")
