8686070dee6eee6e0acb3b5b39b323b8b8d18871
SunBlack <SunBlack@users.noreply.github.com>
2026-07-24 17:22:27 +0200

    Readd hdf5 test and reduce C test code

--- scripts/test_ports/vcpkg-ci-flann/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-flann/project/main.cpp
@@ -1,15 +1,17 @@
 #include <flann/flann.hpp>
 #ifdef WITH_HDF5
-#include <flann/hdf5.hpp>
+#include <flann/io/hdf5.h>
 #endif
 
 #include <array>
 
 int main() {
-    flann::Index<flann::L2<float>> index({});
+    flann::Matrix<float> dataset;
+
+    flann::Index<flann::L2<float>> index(dataset, flann::KDTreeIndexParams());
     index.usedMemory();
 #ifdef WITH_HDF5
-    flann::load_from_file({}, {}, {});
+    flann::load_from_file(dataset, {}, {});
 #endif
 
     return 0;
