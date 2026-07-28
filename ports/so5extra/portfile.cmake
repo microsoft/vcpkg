da94fc933ace7cb87a64ad399f9f123377fff129
Yauheni Akhotnikau <eao197@gmail.com>
2026-07-28 15:38:47 +0300

    Updates for SObjectizer and so5extra.
    SObjectizer updated to v5.8.6.
    
    so5extra updated to v1.6.3.

--- ports/so5extra/portfile.cmake
+++ ports/so5extra/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stiffstream/so5extra
-    REF "v.${VERSION}"
-    SHA512 5744dbb3739d00fa9928718b734611c190ffe8a44ce2dd66f05333abe09dc0e59a4453a0ea4d0b714d81627e7f876493a8c87022bf10ea2011ce94f04b41b54f
+    REF "v${VERSION}"
+    SHA512 69ced225490addc26118f1e5691666b4dac570dfc2cacd66e20e1a55d785df824769af465ae4af46921dfb8a150ac3ca26bbc430fef04298989fcbe3e10890a5
 )
 
 vcpkg_cmake_configure(
