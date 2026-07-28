b88f8e4245429b282258fcab440f723c2d84cbba
Wan-Teh Chang <wtc@google.com>
2026-07-28 15:59:01 -0700

    [aom] Update to 3.14.1
    Fixes https://github.com/microsoft/vcpkg/issues/52884.

--- ports/aom/portfile.cmake
+++ ports/aom/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL "https://aomedia.googlesource.com/aom"
-    REF 92d4c37fbdd08944a0e721bbaeb13318f10aebb0
-	FETCH_REF "v${VERSION}"
+    REF 03087864cf4bea6abb0d28f95cf7843511413d8f
+    FETCH_REF "v${VERSION}"
     HEAD_REF main
     PATCHES
         aom-rename-static.diff
