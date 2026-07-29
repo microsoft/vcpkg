684f9d99c5678b26452a22cccaa47ac8e0a005a5
Saikari <lin@sz.cn.eu.org>
2026-07-29 14:26:13 +0300

    retry

--- ports/libbacktrace/libbacktraceConfig.cmake
+++ ports/libbacktrace/libbacktraceConfig.cmake
@@ -2,26 +2,62 @@ if(TARGET libbacktrace::libbacktrace)
     return()
 endif()
 
-find_path(LIBBACKTRACE_INCLUDE_DIR NAMES backtrace.h PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include" NO_DEFAULT_PATH)
-find_library(LIBBACKTRACE_LIBRARY_RELEASE NAMES backtrace libbacktrace PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib" NO_DEFAULT_PATH)
-find_library(LIBBACKTRACE_LIBRARY_DEBUG   NAMES backtrace libbacktrace PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib" NO_DEFAULT_PATH)
+find_path(
+  LIBBACKTRACE_INCLUDE_DIR
+  NAMES backtrace.h
+  PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include"
+  NO_DEFAULT_PATH
+)
+
+find_library(
+  LIBBACKTRACE_LIBRARY_DEBUG
+  NAMES backtrace libbacktrace
+  PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/debug/lib"
+  NO_DEFAULT_PATH
+)
+
+find_library(
+  LIBBACKTRACE_LIBRARY_RELEASE
+  NAMES backtrace libbacktrace
+  PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib"
+  NO_DEFAULT_PATH
+)
 
 include(SelectLibraryConfigurations)
 select_library_configurations(LIBBACKTRACE)
 
 if(NOT TARGET libbacktrace::libbacktrace)
     add_library(libbacktrace::libbacktrace UNKNOWN IMPORTED)
-    set_target_properties(libbacktrace::libbacktrace PROPERTIES
+    set_target_properties(
+      libbacktrace::libbacktrace
+      PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${LIBBACKTRACE_INCLUDE_DIR}"
-        IMPORTED_CONFIGURATIONS RELEASE
-        IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-        IMPORTED_LOCATION_RELEASE "${LIBBACKTRACE_LIBRARY_RELEASE}"
+        IMPORTED_LOCATION "${LIBBACKTRACE_LIBRARY}"
     )
+    if(LIBBACKTRACE_LIBRARY_RELEASE)
+        set_property(
+          TARGET libbacktrace::libbacktrace
+          APPEND
+          PROPERTY IMPORTED_CONFIGURATIONS RELEASE
+        )
+        set_target_properties(
+          libbacktrace::libbacktrace
+          PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+            IMPORTED_LOCATION_RELEASE "${LIBBACKTRACE_LIBRARY_RELEASE}"
+        )
+    endif()
     if(LIBBACKTRACE_LIBRARY_DEBUG)
-        set_property(TARGET libbacktrace::libbacktrace APPEND PROPERTY
-            IMPORTED_CONFIGURATIONS DEBUG)
-        set_target_properties(libbacktrace::libbacktrace PROPERTIES
+        set_property(
+          TARGET libbacktrace::libbacktrace
+          APPEND
+          PROPERTY IMPORTED_CONFIGURATIONS DEBUG
+        )
+        set_target_properties(
+          libbacktrace::libbacktrace
+          PROPERTIES
             IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
-            IMPORTED_LOCATION_DEBUG "${LIBBACKTRACE_LIBRARY_DEBUG}")
+            IMPORTED_LOCATION_DEBUG "${LIBBACKTRACE_LIBRARY_DEBUG}"
+        )
     endif()
 endif()
