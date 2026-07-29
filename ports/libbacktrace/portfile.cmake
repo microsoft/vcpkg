2bf720e95b65130c1263c5d968982168b0c6edbf
Saikari <lin@sz.cn.eu.org>
2026-07-29 13:50:26 +0300

    Update file copy destination for libbacktraceConfig.cmake in portfile.cmake

--- ports/libbacktrace/portfile.cmake
+++ ports/libbacktrace/portfile.cmake
@@ -16,9 +16,6 @@ vcpkg_make_configure(
 vcpkg_make_install()
 vcpkg_fixup_pkgconfig()
 
-file(
-    COPY "${CMAKE_CURRENT_LIST_DIR}/libbacktraceConfig.cmake"
-    DESTINATION "${CURRENT_PACKAGES_DIR}/share/libbacktrace"
-)
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/libbacktraceConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
