174c2011c89e41e320d4e5721aac2004079f708c
Ankur <ankurv@verma>
2026-07-28 23:12:24 -0700

    Acquire msys on windows

--- ports/libusb/portfile.cmake
+++ ports/libusb/portfile.cmake
@@ -2,6 +2,9 @@ if("udev" IN_LIST FEATURES)
     message("${PORT} currently requires the following tools and libraries from the system package manager:\n    libudev\n\nThese can be installed on Ubuntu systems via apt-get install libudev-dev")
 endif()
 
+vcpkg_acquire_msys(MSYS_ROOT)
+vcpkg_add_to_path("${MSYS_ROOT}/usr/bin")
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libusb/libusb-cmake
@@ -9,13 +12,15 @@ vcpkg_from_github(
     SHA512 a093b82c9cebca3dd1cd4be13e7dfe6e68a550e7bcc6649357722a7ea00a6cefa8044a62e26360c41721cdf427726b9000a93da2340a4f50119929d8d323f0fe
     HEAD_REF master
 )
+
 vcpkg_cmake_configure(
-	SOURCE_PATH ${SOURCE_PATH}
-	OPTIONS
-		-DLIBUSB_EXPORT_INSTALL_TARGETS=ON
-		-DLIBUSB_INSTALL_TARGETS=ON
-		-DLIBUSB_INSTALL_PKGCONFIG=ON
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DLIBUSB_EXPORT_INSTALL_TARGETS=ON
+        -DLIBUSB_INSTALL_TARGETS=ON
+        -DLIBUSB_INSTALL_PKGCONFIG=ON
 )
+
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
