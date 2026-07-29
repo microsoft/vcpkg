9101af7ade2dae9c744856e5a2d92e533e75dd1e
Ankur <ankurv@verma>
2026-07-28 18:05:08 -0700

    Update libusb commit

--- ports/libusb/portfile.cmake
+++ ports/libusb/portfile.cmake
@@ -5,14 +5,18 @@ endif()
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libusb/libusb-cmake
-    REF v${VERSION}
-    SHA512 d2be6014542e7063013b27b95c0cfa83bc57b9cae6d390d26e62ff2806a36a9ba42edc23b9052c2b06f2c49956491602761a589a84e9d64a5a0f6d9879b829de
+    REF bea6567b63796ab27c1c33257d230284fb2d8316
+    SHA512 a093b82c9cebca3dd1cd4be13e7dfe6e68a550e7bcc6649357722a7ea00a6cefa8044a62e26360c41721cdf427726b9000a93da2340a4f50119929d8d323f0fe
     HEAD_REF master
 )
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_configure(
+	SOURCE_PATH ${SOURCE_PATH}
+	OPTIONS
+		-DLIBUSB_EXPORT_INSTALL_TARGETS=ON
+		-DLIBUSB_INSTALL_TARGETS=ON
+)
 vcpkg_cmake_install()
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libusb)
-configure_file("${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" @ONLY)
 file(INSTALL "${SOURCE_PATH}/libusb/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
