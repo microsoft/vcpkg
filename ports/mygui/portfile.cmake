44c770f42e86f5fe70f1d85ee9b42e6f07e90067
Sarah Sunday <3252602+ssunday@users.noreply.github.com>
2026-07-31 15:53:57 -0500

    [MYGUI] 3.5.1 with opengl+emscripten feature

--- ports/mygui/portfile.cmake
+++ ports/mygui/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     REPO MyGUI/mygui
     REF v${VERSION}
     HEAD_REF master
-    SHA512 6bf0430d170a0a6a2afa3724973811025e11bf6622afab8077f4024eea12f4e2835e19b88b46f7a9231ba151445e2a79a8dabee13b6a1e5f6725ac3b880afbeb
+    SHA512 c1ab781b7777a3d991ea74cf06b91527d83736f97947fabb16b12d1089372015c783c3bc4afb2ce64c4e2e11659598dfc58b0e23c3653f50b1bb115e726eb539
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
@@ -13,6 +13,16 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     INVERTED_FEATURES
         obsolete MYGUI_DONT_USE_OBSOLETE
 )
+
+set(MYGUI_BUILD_RENDERSYSTEMS "1")
+
+if("emscripten" IN_LIST FEATURES)
+  string(APPEND MYGUI_BUILD_RENDERSYSTEMS ";8")
+endif()
+
+if("opengl" IN_LIST FEATURES)
+  string(APPEND MYGUI_BUILD_RENDERSYSTEMS ";7")
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -24,19 +34,13 @@ vcpkg_cmake_configure(
         -DMYGUI_BUILD_DOCS=FALSE
         -DMYGUI_BUILD_TOOLS=FALSE
         -DMYGUI_USE_SYSTEM_PUGIXML=TRUE
-        -DMYGUI_RENDERSYSTEM=1 # Use an overlay port to change the render system. Read the discussion at: https://github.com/microsoft/vcpkg/pull/52862
+        -DMYGUI_RENDERSYSTEM=1
+        -DMYGUI_BUILD_RENDERSYSTEMS=${MYGUI_BUILD_RENDERSYSTEMS}
         ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/MyGUI)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/MYGUI/MyGUI_Platform.h"
-        "#ifndef MYGUI_PLATFORM_H_\n#define MYGUI_PLATFORM_H_"
-        "#ifndef MYGUI_PLATFORM_H_\n#define MYGUI_PLATFORM_H_\n\n#define MYGUI_STATIC"
-    )
-endif()
 
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/debug/include"
