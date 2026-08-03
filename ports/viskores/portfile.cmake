b929e7a494c6fd06952db5e0e4c1b846afaa147f
Kai Pastor <dg0yt@darc.de>
2026-08-03 22:42:41 +0200

    WIP

--- ports/viskores/portfile.cmake
+++ ports/viskores/portfile.cmake
@@ -4,12 +4,18 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 4ec30e96e8f61161255145aad44e986fe34d4ffc3268600d4e54ab4d0a583eaf3ef499d26ae00442b1204c4b202c741b1720438d00f3dc42d05119020a86571c
     HEAD_REF master
+    PATCHES
+        external-fmt.diff
+        no-abs-path.diff
 )
-file(REMOVE_RECURSE "${SOURCE_PATH}/viskores/thirdparty/diy/viskoresdiy/include/viskoresdiy/thirdparty/fmt")
+file(REMOVE_RECURSE
+    "${SOURCE_PATH}/viskores/thirdparty/diy/viskoresdiy/include/viskoresdiy/thirdparty/fmt"
+    #[[ namespace viskores: "${SOURCE_PATH}/viskores/thirdparty/lodepng" ]]
+    #[[ anonymous namespace: "${SOURCE_PATH}/viskores/thirdparty/loguru" ]]
+)
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        anari   Viskores_ENABLE_ANARI
         hdf5    Viskores_ENABLE_HDF5_IO
 )
 
@@ -26,5 +32,8 @@ vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
