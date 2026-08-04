61cd897304d7c4704551c4cdb73af0ffd691fc85
toge <toge.mail@gmail.com>
2026-08-05 01:45:34 +0900

    [gumbo] fix sha512

--- ports/gumbo/portfile.cmake
+++ ports/gumbo/portfile.cmake
@@ -1,9 +1,11 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://codeberg.org/gumbo-parser/gumbo-parser/archive/${VERSION}.tar.gz"
+    # ON Codeberg the checksums are not guaranteed to be stable, so we use a specific commit instead.
+    # ref. https://codeberg.org/Codeberg/Community/issues/1366#issuecomment-1383136
+    URLS "https://codeberg.org/gumbo-parser/gumbo-parser/archive/322c54c178590ba42b8b04e8c0e4840595a1f717.tar.gz"
     FILENAME "gumbo-${VERSION}.tar.gz"
-    SHA512  1513e88acfc3240081039b30ab63ca6fc23a85b7d1415cb8e90e9c43f9c3d99d37634a7a50d81ef7f6d43a4facac3802feea6959256a2330b26b16523a288568
+    SHA512  9f59965e68ba2e4f5884d52c4126b62cdbefee158816334e317a0d07f10ba927be653490c69fc5b0f52eed1decf0a51715bb726aa84546a2d04cde5805e4a399
 )
 
 vcpkg_extract_source_archive(
