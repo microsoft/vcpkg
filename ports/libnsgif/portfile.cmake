14018249466a1f3be1e1ffe52b2605b6b8e1c08a
Saikari <lin@sz.cn.eu.org>
2026-08-04 12:47:29 +0300

    fixup

--- ports/libnsgif/portfile.cmake
+++ ports/libnsgif/portfile.cmake
@@ -23,3 +23,5 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-libnsgif CONFIG_PATH lib/cmake/unofficial-libnsgif)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
