10c7a49f7cf3799a50f9effca5726081b5ce7aed
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2026-08-04 18:24:44 -0400

    Updating the necessary files.

--- ports/jsonifier/portfile.cmake
+++ ports/jsonifier/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO realtimechris/jsonifier
+    REPO nihilai-collective/jsonifier
     REF "v${VERSION}"
-    SHA512 d417e8da4c4f64b7a37d70c85343daae46f52e72d5b0043b076fc5e18a0642cce7f5214aee7029b2acfb5953300977c5538f99098f0925fabc1199279408b820
+    SHA512 f5f4d8b9c6ab8b5e116c4d328ad4044cf13f26efa2787d5a77f1047a579305f570400ee100721c9e47c8c74044b43123460c4d511c3c66ce895f263ab72fd39a
     HEAD_REF main
 )
 
