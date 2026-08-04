ea19f1502f7df83c0deef179e69083805f3ae77f
Saikari <lin@sz.cn.eu.org>
2026-08-04 14:15:36 +0300

    Refactor success variable to be const

--- scripts/test_ports/vcpkg-ci-gif-load/project/main.c
+++ scripts/test_ports/vcpkg-ci-gif-load/project/main.c
@@ -1,13 +1,15 @@
 #include <gif_load.h>
+#include <stdbool.h>
 
 static const unsigned char gif_data[] = { 0x47 };
 static void frame_writer(void *anim, struct GIF_WHDR *whdr)
 {
-    (void)anim; (void)whdr;
+    (void)anim;
+    (void)whdr;
 }
 
 int main(void)
 {
-    bool success = GIF_Load((void *)gif_data, (long)sizeof(gif_data), frame_writer, 0, 0, 0) == 1 ? 0 : 1;
+    const bool success = GIF_Load((void *)gif_data, (long)sizeof(gif_data), frame_writer, 0, 0, 0) == 1;
     return 0;
 }
