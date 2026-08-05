3cbde8a75d5bfb880c4d1a6a1da1e7bea8b21fe0
Saikari <lin@sz.cn.eu.org>
2026-08-05 09:42:21 +0300

    Add CVE fix patch for gifdec port

--- ports/gifdec/portfile.cmake
+++ ports/gifdec/portfile.cmake
@@ -1,3 +1,10 @@
+vcpkg_download_distfile(
+    FIX_CVE
+    URLS https://github.com/lecram/gifdec/pull/23.patch?full_index=1
+    FILENAME fix-cve.patch
+    SHA512 457deb349492206c473edcc09fdc654261a2277307272c1716cc7ef42b0f75302039a7b453ddb355d7195bc5e1ac068a431f5fed5adc3161a3ee60ab03295a65
+)
+
 if(VCPKG_TARGET_IS_WINDOWS)
     vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 endif()
@@ -8,6 +15,8 @@ vcpkg_from_github(
     REF 1dcbae19363597314f6623010cc80abad4e47f7c
     SHA512 2756004cb7dd8be5560a32c188001da503b97f1b8e3eed908787563b7e36edf8cbca3605e2e75b2ebbc69b329e16d89b3237028dfd7a772d2763bf78b1675ad2
     HEAD_REF master
+    PATCHES
+        "${FIX_CVE}"
 )
 
 file(COPY
