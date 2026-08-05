e091044be8e9347e74e6b71bd16e2d18c0d555ad
jyu49 <yu_jack@apple.com>
2026-08-05 13:19:49 +0800

    [osgearth] update to 3.8.1

--- ports/osgearth/portfile.cmake
+++ ports/osgearth/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO pelicanmapping/osgearth
-    REF c980ad2ad6e9fb25c5a7f5b8c94b1cbf0e98a617
-    SHA512 4e3fe4f7c11d3fb3962cefb98400c6a0c0a491a3d57642da2040b6e0fd8f2cd27a4f58074b077a61151fde2d0b41ce97aa7fd0cf9901ddb6677f8f31392711e0
+    REF a656f468187854fde596bbcfff54f4c35860baa4
+    SHA512 1ea1a95ff84f927f6d91a281129eccd3d8825c98fb434b059cee054c7102dc0fc5dbbaa9092e7e2e4896b9778a510c6145bc21c7b7a670e47b4f798056515b53
     HEAD_REF master
     PATCHES devendor-imgui.diff
 )
