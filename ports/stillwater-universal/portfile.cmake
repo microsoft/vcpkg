761bd6c0128cbc33a8ba129933d29e1f8b55ba73
xiaozhuai <798047000@qq.com>
2026-08-05 11:55:50 +0800

    [stillwater-universal] Update to 4.7.9

--- ports/stillwater-universal/portfile.cmake
+++ ports/stillwater-universal/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stillwater-sc/universal
     REF "v${VERSION}"
-    SHA512 14988ab314e35adcf62fe22b8cf32047cb661ab300b4753c074a634a39635d21c5c6064538048185c66a95b83d18a4294bbc9514b8c2ef6364c78289fb423e1a
+    SHA512 099b634b2242243ac1319e5b6aafb5f3787ab1c03ad9b0cf3abbeec9eb5773d9efc64f1f80c1c566d112f6c77bea55de638ea38a0edc3eb98311ab4e89b0bd85
     HEAD_REF master
     PATCHES
         fix-install-path.patch
