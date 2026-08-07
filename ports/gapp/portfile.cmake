dac8281400816e8e82a0912d73b97fc49f9e2e2c
SunBlack <SunBlack@users.noreply.github.com>
2026-08-07 10:48:37 +0200

    [gapp] Update to 1.0.1

--- ports/gapp/portfile.cmake
+++ ports/gapp/portfile.cmake
@@ -6,7 +6,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO KRM7/gapp
   REF "v${VERSION}"
-  SHA512 de6e1d9e28590cc569c05fe3b2462245940fcca5c8ffbc2974758062f88d3165e527fdc273bb290eb1080dd899d78b540fc7d0f62d7236b289a63e138484f5f0
+  SHA512 16588f6feb60ebdea7fe8777f231fca78285b15a58975ea228ed6b706c6ccf46b17e3cd8bc8c2eb938222439ef28170bc25b15a527c3ac70d7eb76ce0d6665b3
   HEAD_REF master
 )
 
@@ -20,9 +20,11 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/gapp)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc/gapp/api")
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/share/doc/gapp/api"
+)
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
 
