dc597a1a553bf65d2883ac61efa5a42db41cdd51
SunBlack <SunBlack@users.noreply.github.com>
2026-08-11 23:21:49 +0200

    [crcpp] Update to 1.2.2.0 (#53351)

--- ports/crcpp/portfile.cmake
+++ ports/crcpp/portfile.cmake
@@ -1,13 +1,14 @@
+set(VCPKG_BUILD_TYPE "release") # header-only
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO d-bahr/CRCpp
     REF "release-${VERSION}"
-    SHA512 61d6d4636cbf42752568900a1267336721836b80cbe99e1795c74be9fffd9d6368697182565beecf5b4050d649c7a77acbacfac2a20eff9de4073dab4ea073cf
+    SHA512 0e1338eb1f0f2a05d119ffae6071be06c24c39f20dcc846ea2c6248367ce600797af23b1f31dcd0f9e389efadc769bcc7129d8b741c30782571500c731c75451
     HEAD_REF master
+    PATCHES
+        install_fixes.patch
 )
-
-# header-only
-set(VCPKG_BUILD_TYPE "release")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -17,5 +18,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+vcpkg_fixup_pkgconfig()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
