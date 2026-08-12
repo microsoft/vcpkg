a17ecf6e50cbb9c6d23fdfeb31cff46abda92779
SunBlack <SunBlack@users.noreply.github.com>
2026-08-12 22:01:17 +0200

    Split test port

--- scripts/test_ports/vcpkg-ci-flann-cuda/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-flann-cuda/project/main.cpp
@@ -1,5 +1,7 @@
 #include <flann/flann.hpp>
 
+#include <flann/algorithms/kdtree_cuda_3d_index.h>
+
 #include <array>
 
 int main() {
@@ -7,6 +9,8 @@ int main() {
 
     flann::Index<flann::L2<float>> index(dataset, flann::KDTreeIndexParams());
     index.usedMemory();
+
+    flann::KDTreeCuda3dIndexParams params;
 
     return 0;
 }
--- scripts/test_ports/vcpkg-ci-flann-hdf5/project/main.cpp
+++ scripts/test_ports/vcpkg-ci-flann-hdf5/project/main.cpp
@@ -1,5 +1,7 @@
 #include <flann/flann.hpp>
 
+#include <flann/io/hdf5.h>
+
 #include <array>
 
 int main() {
@@ -7,6 +9,8 @@ int main() {
 
     flann::Index<flann::L2<float>> index(dataset, flann::KDTreeIndexParams());
     index.usedMemory();
+
+    flann::load_from_file(dataset, {}, {});
 
     return 0;
 }
