a79b9292212595f044ff75146a196c880607b8a4
Ivet Galabova <galabovaa@gmail.com>
2026-08-13 16:03:41 +0300

    new port and features

--- ports/highs/portfile.cmake
+++ ports/highs/portfile.cmake
@@ -8,13 +8,21 @@ vcpkg_from_github(
         fix-pkgconfig-zlib.patch
 )
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        extras HIPO
+)
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
+        ${FEATURE_OPTIONS}
         -DFAST_BUILD=ON
         -DBUILD_TESTING=OFF
         -DBUILD_EXAMPLES=OFF
         -DCMAKE_REQUIRE_FIND_PACKAGE_ZLIB=ON
+        -DBUILD_SHARED_EXTRAS_LIB=OFF
 )
 
 vcpkg_cmake_install()
