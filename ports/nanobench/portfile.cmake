753ce7d649ae3c81c8e4faf7347b7e7a1c32e617
xiaozhuai <798047000@qq.com>
2026-08-18 11:56:21 +0800

    Keep custom CMakeLists.txt

--- ports/nanobench/portfile.cmake
+++ ports/nanobench/portfile.cmake
@@ -8,14 +8,15 @@ vcpkg_from_github(
     HEAD_REF master
 )
 
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
-    OPTIONS
-        -DNANOBENCH_BUILD_TEST=OFF
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/nanobench)
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
