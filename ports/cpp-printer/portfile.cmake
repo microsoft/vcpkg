53da89811c84138ae6a9bd11300ad4a05beaf426
BlueNova Studio <bluenovastudio.dev@gmail.com>
2026-08-18 22:50:18 -0500

    Fix cpp-printer Windows packaging

--- ports/cpp-printer/portfile.cmake
+++ ports/cpp-printer/portfile.cmake
@@ -1,14 +1,15 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BlueNovaStudio/bluenova-toolkit
-    REF v1.0.0
-    SHA512 5d4e4031e3218778587c32e589b9d206f614bbf9ed92c038da144d8fc0dfbdf2eb7cfa5974c119bdd1dca5d0f90490d369933ac5eb80276a21718f8907854e57
+    REF v1.0.1
+    SHA512 fea94740e9283cb1a839605f317ac7dcc8d6530e4ce74b7b5c61d6eb78cfd6c50da63f3622e8942eeb9ecd7800bea9b348fed3bfd492abcc0902e356217ab088
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
+        -DCPP_PRINTER_BUILD_CLI=OFF
 )
 
 vcpkg_cmake_install()
@@ -19,6 +20,7 @@ vcpkg_cmake_config_fixup(
 )
 
 file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug"
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
     "${CURRENT_PACKAGES_DIR}/debug/lib"
