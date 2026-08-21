780eeaeee7aa3881ff9e77e0e42f1d37624b38b8
jyu49 <yu_jack@apple.com>
2026-08-21 10:54:13 +0800

    [ktexttemplate] add new port and deprecate grantlee

--- ports/ktexttemplate/portfile.cmake
+++ ports/ktexttemplate/portfile.cmake
@@ -2,12 +2,11 @@ vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO steveire/grantlee
-    REF v5.3.1
-    SHA512 dc7192fe0553954fffc3e2c584e4fdd80fc1f22d25846cacc5f2dcd1db2673ca62464c8492a4ed3bfc9dfc3e62ef13322809dd29bd56fa4a3a153a8d373ddde5
+    REPO KDE/ktexttemplate
+    REF "v${VERSION}"
+    SHA512 c7296ea614ed61dd66b0a6fc0b5af104686dd3759064df48478147110b353d7def7396fb84b8f7ccca635ccf6bb86644cf1f890d983974c269512cb3cd475113
     HEAD_REF master
 )
-
 vcpkg_cmake_configure (
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
@@ -15,10 +14,8 @@ vcpkg_cmake_configure (
         -DBUILD_TESTS=OFF
 )
 
-vcpkg_replace_string("${SOURCE_PATH}/CMakeLists.txt" [[set( PLUGIN_INSTALL_DIR ${LIB_INSTALL_DIR}/grantlee/${Grantlee5_MAJOR_MINOR_VERSION_STRING} )]] [[set( PLUGIN_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/bin)]])
-
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/Grantlee5)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/ktexttemplate)
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
