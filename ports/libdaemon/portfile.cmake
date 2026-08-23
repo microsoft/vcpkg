dffe0d572271a1ee486a86c3af1eb99a21e02634
Robert Adam <dev@robert-adam.de>
2026-08-23 13:57:25 +0200

    Replace tab by spaces

--- ports/libdaemon/portfile.cmake
+++ ports/libdaemon/portfile.cmake
@@ -1,5 +1,5 @@
 vcpkg_download_distfile(ARCHIVE
-	URLS "http://0pointer.de/lennart/projects/libdaemon/libdaemon-${VERSION}.tar.gz"
+    URLS "http://0pointer.de/lennart/projects/libdaemon/libdaemon-${VERSION}.tar.gz"
     FILENAME "libdaemon-${VERSION}.tar.gz"
     SHA512 a96b25c09bd63cc192c1c5f8b5bf34cc6ad0c32d42ac14b520add611423b6ad3d64091a47e0c7ab9a94476a5e645529abccea3ed6b23596567163fba88131ff2
 )
