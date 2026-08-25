57737bf7ac6d55e29bb6ad705a12034e19282160
Alexander Zhang <alexanderzjs@qq.com>
2026-08-25 14:17:45 +0800

    Update glad to 2.0.8

--- ports/glad/portfile.cmake
+++ ports/glad/portfile.cmake
@@ -1,151 +1,115 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Dav1dde/glad
-    REF 1ecd45775d96f35170458e6b148eb0708967e402 # 0.1.36
-    SHA512 f6292e9dbce503a95b6eca927a90cf8a06f5e3de39b84f9a47e0d9273a6ea3f6591a64d35f2e33b5ea353e0c784f15c38579fc15aa6c5b180154e241ccbb16fe
+    REF 73db193f853e2ee079bf3ca8a64aa2eaf6459043 # 2.0.8
+    SHA512 f20873b108cda958c9a3d48dad46d09636e98ed7a6b3d176a8164d2994cafbb06b2b8692217afd95e5278c96cc430f70ceda68057ca059e25ba3f4b719316eaa
     HEAD_REF master
-    PATCHES 
-        encoding.patch
-        find_python.patch
 )
 
-if(NOT GLAD_PROFILE)
-    set(GLAD_PROFILE "compatibility")
-endif()
-message(STATUS "This version of glad uses the compatibility profile. To use the core profile instead, create an overlay port of this with GLAD_PROFILE set to 'core' or set GLAD_PROFILE to 'core' in a custom triplet.")
-message(STATUS "This recipe is at ${CMAKE_CURRENT_LIST_DIR}")
-message(STATUS "See the overlay ports documentation at https://github.com/microsoft/vcpkg/blob/master/docs/specifications/ports-overlay.md")
-
-vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-    FEATURES
-        extensions  GLAD_ALL_EXTENSIONS
-    INVERTED_FEATURES
-        loader      GLAD_NO_LOADER
-)
-
-set(GLAD_SPEC "gl")
-
-if("egl" IN_LIST FEATURES)
-    string(APPEND GLAD_SPEC ",egl")
-endif()
-
-if("wgl" IN_LIST FEATURES)
-    string(APPEND GLAD_SPEC ",wgl")
-endif()
-
-if("glx" IN_LIST FEATURES)
-    string(APPEND GLAD_SPEC ",glx")
-endif()
-
-# This needs to be ordered highest to lowest for it to be correct!
+set(GLAD_API "gl:compatibility=4.6")
+foreach(version IN ITEMS 46 45 44 43 42 41 40 33 32 31 30 21 20 15 14 13 12 11 10)
+    if("gl-api-${version}" IN_LIST FEATURES)
+        string(SUBSTRING "${version}" 0 1 major)
+        string(SUBSTRING "${version}" 1 1 minor)
+        set(GLAD_API "gl:compatibility=${major}.${minor}")
+        break()
+    endif()
+endforeach()
 if("gl-api-latest" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=")
-elseif("gl-api-46" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.6")
-elseif("gl-api-45" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.5")
-elseif("gl-api-44" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.4")
-elseif("gl-api-43" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.3")
-elseif("gl-api-42" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.2")
-elseif("gl-api-41" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.1")
-elseif("gl-api-40" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=4.0")
-elseif("gl-api-33" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=3.3")
-elseif("gl-api-32" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=3.2")
-elseif("gl-api-31" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=3.1")
-elseif("gl-api-30" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=3.0")
-elseif("gl-api-21" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=2.1")
-elseif("gl-api-20" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=2.0")
-elseif("gl-api-15" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.5")
-elseif("gl-api-14" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.4")
-elseif("gl-api-13" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.3")
-elseif("gl-api-12" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.2")
-elseif("gl-api-11" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.1")
-elseif("gl-api-10" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gl=1.0")
+    set(GLAD_API "gl:compatibility=4.6")
 endif()
-
-# This needs to be ordered highest to lowest for it to be correct!
-if("gles1-api-latest" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles1=")
-elseif("gles1-api-10" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles1=1.0")
+if("gles1-api-10" IN_LIST FEATURES OR "gles1-api-latest" IN_LIST FEATURES)
+    list(APPEND GLAD_API "gles1=1.0")
 endif()
-
-# This needs to be ordered highest to lowest for it to be correct!
-if("gles2-api-latest" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles2=")
-elseif("gles2-api-32" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles2=3.2")
+if("gles2-api-20" IN_LIST FEATURES OR "gles2-api-latest" IN_LIST FEATURES)
+    list(APPEND GLAD_API "gles2=2.0")
+endif()
+if("gles2-api-30" IN_LIST FEATURES)
+    list(APPEND GLAD_API "gles2=3.0")
 elseif("gles2-api-31" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles2=3.1")
-elseif("gles2-api-30" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles2=3.0")
-elseif("gles2-api-20" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "gles2=2.0")
+    list(APPEND GLAD_API "gles2=3.1")
+elseif("gles2-api-32" IN_LIST FEATURES)
+    list(APPEND GLAD_API "gles2=3.2")
+endif()
+if("glsc2-api-20" IN_LIST FEATURES OR "glsc2-api-latest" IN_LIST FEATURES)
+    list(APPEND GLAD_API "glsc2=2.0")
+endif()
+if("egl" IN_LIST FEATURES)
+    list(APPEND GLAD_API "egl=1.5")
+endif()
+if("glx" IN_LIST FEATURES)
+    list(APPEND GLAD_API "glx=1.4")
+endif()
+if("wgl" IN_LIST FEATURES)
+    list(APPEND GLAD_API "wgl=1.0")
 endif()
 
-# This needs to be ordered highest to lowest for it to be correct!
-if("glsc2-api-latest" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "glsc2=")
-elseif("glsc2-api-20" IN_LIST FEATURES)
-    LIST(APPEND GLAD_API "glsc2=2.0")
+set(GLAD_LOADER)
+if("loader" IN_LIST FEATURES)
+    set(GLAD_LOADER LOADER)
+endif()
+set(GLAD_ALL_EXTENSIONS OFF)
+if("extensions" IN_LIST FEATURES)
+    set(GLAD_ALL_EXTENSIONS ON)
 endif()
 
-string(REPLACE ";" "," GLAD_API "${GLAD_API}")
-
-vcpkg_find_acquire_program(PYTHON3)
-
-file(COPY
-    "${CURRENT_INSTALLED_DIR}/include/KHR/khrplatform.h"
-    "${CURRENT_INSTALLED_DIR}/include/EGL/eglplatform.h"
-    "${CURRENT_INSTALLED_DIR}/share/opengl/egl.xml"
-    "${CURRENT_INSTALLED_DIR}/share/opengl/gl.xml"
-    "${CURRENT_INSTALLED_DIR}/share/opengl/glx.xml"
-    "${CURRENT_INSTALLED_DIR}/share/opengl/wgl.xml"
-    DESTINATION "${SOURCE_PATH}/glad/files"
+x_vcpkg_get_python_packages(
+    PYTHON_VERSION 3
+    PACKAGES jinja2
+    OUT_PYTHON_VAR PYTHON3
 )
+
+file(WRITE "${SOURCE_PATH}/CMakeLists.txt" [=[
+cmake_minimum_required(VERSION 3.15)
+project(glad C)
+
+set(GLAD_SOURCES_DIR "${CMAKE_CURRENT_SOURCE_DIR}" CACHE PATH "" FORCE)
+include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/GladConfig.cmake")
+set(GLAD_ARGS STATIC REPRODUCIBLE)
+if(GLAD_LOADER)
+    list(APPEND GLAD_ARGS LOADER)
+endif()
+list(APPEND GLAD_ARGS API ${GLAD_API})
+if(NOT GLAD_ALL_EXTENSIONS)
+    list(APPEND GLAD_ARGS EXTENSIONS NONE)
+endif()
+glad_add_library(glad ${GLAD_ARGS})
+set_property(TARGET glad PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+    "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/gladsources/glad/include>;$<INSTALL_INTERFACE:include>"
+)
+
+include(CMakePackageConfigHelpers)
+install(TARGETS glad EXPORT glad-targets ARCHIVE DESTINATION lib INCLUDES DESTINATION include)
+install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/gladsources/glad/include/" DESTINATION include)
+install(EXPORT glad-targets FILE glad-targets.cmake NAMESPACE glad:: DESTINATION share/glad)
+configure_package_config_file(
+    "${CMAKE_CURRENT_SOURCE_DIR}/glad-config.cmake.in"
+    "${CMAKE_CURRENT_BINARY_DIR}/glad-config.cmake"
+    INSTALL_DESTINATION share/glad
+)
+install(FILES "${CMAKE_CURRENT_BINARY_DIR}/glad-config.cmake" DESTINATION share/glad)
+]=])
+file(WRITE "${SOURCE_PATH}/glad-config.cmake.in" [=[
+@PACKAGE_INIT@
+include("${CMAKE_CURRENT_LIST_DIR}/glad-targets.cmake")
+]=])
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DGLAD_EXPORT=OFF
-        -DGLAD_INSTALL=ON
-        -DGLAD_REPRODUCIBLE=ON
-        -DGLAD_SPEC=${GLAD_SPEC}
-        -DGLAD_API=${GLAD_API}
-        -DGLAD_PROFILE=${GLAD_PROFILE}
-        -DPYTHON_EXECUTABLE=${PYTHON3}
-        ${FEATURE_OPTIONS}
-    OPTIONS_DEBUG
-        -DGLAD_GENERATOR="c-debug"
-    OPTIONS_RELEASE
-        -DGLAD_GENERATOR="c"
+        -DGLAD_API:STRING=${GLAD_API}
+        -DGLAD_LOADER:BOOL=${GLAD_LOADER}
+        -DGLAD_ALL_EXTENSIONS:BOOL=${GLAD_ALL_EXTENSIONS}
+        -DPython_EXECUTABLE=${PYTHON3}
 )
 
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/glad)
+vcpkg_cmake_config_fixup(CONFIG_PATH share/glad)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/include/KHR")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/include/EGL")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(COPY "${SOURCE_PATH}/glad" DESTINATION "${CURRENT_PACKAGES_DIR}/tools")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
