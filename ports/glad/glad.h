ba00efbade58bd0c668e44c20250a2fd42eab811
Alexander Zhang <alexanderzjs@qq.com>
2026-08-26 15:25:24 +0800

    Fix glad legacy loader compatibility

--- ports/glad/glad.h
+++ ports/glad/glad.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <glad/gl.h>
+#ifdef __cplusplus
+static inline int gladLoadGL() {
+    return gladLoaderLoadGL();
+}
+#endif
 typedef GLADloadfunc GLADloadproc;
 static inline int gladLoadGLLoader(GLADloadproc load) {
     return gladLoadGL(load);
