c61793218f5615bfd120aa95feeddcb9647e46e5
Kai Pastor <dg0yt@darc.de>
2026-08-28 20:42:55 +0200

    More pkgconfig testing

--- scripts/test_ports/unit-test-cmake/test-z_vcpkg_setup_pkgconfig_path.cmake
+++ scripts/test_ports/unit-test-cmake/test-z_vcpkg_setup_pkgconfig_path.cmake
@@ -4,6 +4,7 @@
 # These functions use vcpkg_backup/restore_env_variables which use scoped variables
 # and cannot be called in unit_test_check_*.
 
+set(PKGCONFIG "")
 set(ENV{PKG_CONFIG} "/a/pkgconf")
 set(ENV{PKG_CONFIG_PATH} "1")
 set(saved_path "$ENV{PATH}")
@@ -23,6 +24,7 @@ z_vcpkg_setup_pkgconfig_path(CONFIG RELEASE)
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG} [[/a/pkgconf]])
 cmake_path(CONVERT "${CURRENT_PACKAGES_DIR}/lib/pkgconfig;${CURRENT_PACKAGES_DIR}/share/pkgconfig;${CURRENT_INSTALLED_DIR}/lib/pkgconfig;${CURRENT_INSTALLED_DIR}/share/pkgconfig;1" TO_NATIVE_PATH_LIST expected)
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG_PATH} "${expected}")
+unit_test_check_variable_equal([[]] ENV{PKG_CONFIG} "/a/pkgconf")
 
 z_vcpkg_restore_pkgconfig_path()
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG} [[/a/pkgconf]])
@@ -37,12 +39,17 @@ z_vcpkg_restore_pkgconfig_path()
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG} [[/a/pkgconf]])
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG_PATH} "1")
 
-# Absent path <installed>/share/pkgconfig; empty PKG_CONFIG_PATH before.
+# - absent path <installed>/share/pkgconfig
+# - empty PKG_CONFIG_PATH
+# - space in PKGCONFIG
 file(REMOVE_RECURSE "${CURRENT_BUILDTREES_DIR}/I/share/pkgconfig")
 set(ENV{PKG_CONFIG_PATH} "")
+set(ENV{PKG_CONFIG} "")
+set(PKGCONFIG "/a/b c/pkgconf")
 z_vcpkg_setup_pkgconfig_path(CONFIG DEBUG)
 cmake_path(CONVERT "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig;${CURRENT_PACKAGES_DIR}/share/pkgconfig;${CURRENT_INSTALLED_DIR}/debug/lib/pkgconfig" TO_NATIVE_PATH_LIST expected)
 unit_test_check_variable_equal([[]] ENV{PKG_CONFIG_PATH} "${expected}")
+unit_test_check_variable_equal([[]] ENV{PKG_CONFIG} "pkgconf")
 
 # z_vcpkg_setup_pkgconfig_path changes PATH but it is not restored.
 # It is hard to see which side effects a restore would have, so
