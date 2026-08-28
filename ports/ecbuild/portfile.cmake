e8aa5d287ca2b416a91db40aaf4cd10d4c4236c9
Kadir <kadir_altindag@outlook.com>
2026-08-28 23:25:06 +0300

    Preserve contrib modules and include BSL license

--- ports/ecbuild/portfile.cmake
+++ ports/ecbuild/portfile.cmake
@@ -8,11 +8,17 @@ vcpkg_from_github(
     HEAD_REF develop
 )
 
+vcpkg_download_distfile(
+    BSL_LICENSE
+    URLS "https://raw.githubusercontent.com/boostorg/boost/boost-1.91.0/LICENSE_1_0.txt"
+    FILENAME "boost_LICENSE_1_0.txt"
+    SHA512 d6078467835dba8932314c1c1e945569a64b065474d7aced27c9a7acc391d52e9f234138ed9f1aa9cd576f25f12f557e0b733c14891d42c16ecdc4a7bd4d60b8
+)
+
 file(COPY
     "${SOURCE_PATH}/cmake"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     PATTERN "Find*.cmake" EXCLUDE
-    PATTERN "contrib" EXCLUDE
 )
 
 file(COPY
@@ -40,4 +46,5 @@ file(INSTALL
 vcpkg_install_copyright(FILE_LIST
     "${SOURCE_PATH}/LICENSE"
     "${SOURCE_PATH}/NOTICE"
+    "${BSL_LICENSE}"
 )
