7fe771049218ab25f7c145cb3072b0d396bce857
Robert Adam <dev@robert-adam.de>
2026-08-29 17:14:35 +0200

    [soci] Disable LTO

--- ports/soci/portfile.cmake
+++ ports/soci/portfile.cmake
@@ -27,6 +27,7 @@ vcpkg_cmake_configure(
         -DSOCI_INSTALL=ON
         -DSOCI_FMT_BUILTIN=OFF
         -DSOCI_SQLITE3_BUILTIN=OFF
+        -DSOCI_LTO=OFF
         # SOCI components whose backends are not yet available through vcpkg
         -DSOCI_DB2=OFF
         -DSOCI_FIREBIRD=OFF
