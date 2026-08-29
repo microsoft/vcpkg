33c9e66a250dcc4899093be442c63cece053c32b
Kadir <kadir_altindag@outlook.com>
2026-08-29 10:41:54 +0300

    Remove BSL license download

--- ports/ecbuild/portfile.cmake
+++ ports/ecbuild/portfile.cmake
@@ -8,13 +8,6 @@ vcpkg_from_github(
     HEAD_REF develop
 )
 
-vcpkg_download_distfile(
-    BSL_LICENSE
-    URLS "https://raw.githubusercontent.com/boostorg/boost/boost-1.91.0/LICENSE_1_0.txt"
-    FILENAME "boost_LICENSE_1_0.txt"
-    SHA512 d6078467835dba8932314c1c1e945569a64b065474d7aced27c9a7acc391d52e9f234138ed9f1aa9cd576f25f12f557e0b733c14891d42c16ecdc4a7bd4d60b8
-)
-
 file(COPY
     "${SOURCE_PATH}/cmake"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
@@ -46,5 +39,5 @@ file(INSTALL
 vcpkg_install_copyright(FILE_LIST
     "${SOURCE_PATH}/LICENSE"
     "${SOURCE_PATH}/NOTICE"
-    "${BSL_LICENSE}"
+    "${SOURCE_PATH}/cmake/contrib/GetGitRevisionDescription.cmake"
 )
