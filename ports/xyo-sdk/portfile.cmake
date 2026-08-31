b5d57111c42f1c8768c23aad8b5c1e2d9eae0395
Hadi Tajallaei <68777073+syniol@users.noreply.github.com>
2026-08-31 23:05:44 +0100

    [xyo-sdk] Remove redundant usage file

--- ports/xyo-sdk/portfile.cmake
+++ ports/xyo-sdk/portfile.cmake
@@ -17,4 +17,3 @@ vcpkg_cmake_config_fixup(PACKAGE_NAME XYOSDK CONFIG_PATH lib/cmake/XYOSDK)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
