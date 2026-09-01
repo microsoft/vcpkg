f66147a232fa127986b187e6196254709f0db4a3
Dzmitry Baryshau <dima8w@gmail.com>
2026-09-02 01:24:48 +0400

    [sail] Fixed video libraries linking order

--- ports/sail/portfile.cmake
+++ ports/sail/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
         fix-always-nanosvg.diff
         fix-heif.patch
         fix-include-directory.patch
-        fix-video-zlib-debug-name.patch
+        fix-video-zlib-and-linking.patch
 )
 
 # Enable selected codecs
