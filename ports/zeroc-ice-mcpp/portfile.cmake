8024c64915d90538247c951200f0e985cc3b93db
Robert Adam <dev@robert-adam.de>
2026-09-05 18:39:06 +0200

    [zeroc-ice-mcpp] Update to v2.7.2.20

--- ports/zeroc-ice-mcpp/portfile.cmake
+++ ports/zeroc-ice-mcpp/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO zeroc-ice/mcpp
-	REF 1c4b0f26614bff331eb8a9f2b514309af6f31fd0 # 2.7.2.14 + 6 commits
-	SHA512 233300f128e9d1406fe104da2579298f2f02b44fa33a59887ea673cf6400ec58ea2d3d72b0d07e452ed603fbc7f19a21f9b80eb99ae7c7819159ddcf9f2c5abc
+	REF "v${VERSION}"
+	SHA512 06a2ccf461c09aec6916e623a1ae59da7db1509a0ba5ceedcfeec2d32a71986bd8ce249cbf99232eaa8f347ee035dd5da5868e7d96ab7ae6270ebdac1b06b498
 	HEAD_REF master
 	PATCHES
 		installation.diff
