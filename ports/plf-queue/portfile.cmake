a244b525b1fb9e8102495f476b7d51cdc31dab76
SunBlack <SunBlack@users.noreply.github.com>
2026-09-08 00:28:08 +0200

    [plf-queue] Update to 2.0.21

--- ports/plf-queue/portfile.cmake
+++ ports/plf-queue/portfile.cmake
@@ -1,13 +1,16 @@
-# header-only library
+set(VCPKG_BUILD_TYPE release) # header-only port
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mattreecebentley/plf_queue
-    REF 1fb9d87a210f7813450ee54a469f9f79ea4ec872
-    SHA512 bca6662f5b0c4dfad4b9c1192aced83cf379ed2f115b498ad98003b7201fa80cf00ee697c7c8f9a8f9fe7c979207a8e99dd58549e124ea041af25c9217d7ae6f 
+    REF c00f1052ac78beb9ab8b3f3eef28b3da953f9bde
+    SHA512 10f5f1cf4d1d81bd0863df8b2cd9d4bd1b273b620b5c860c95284b26009248f2aef5cc6e766a810c30921431cb7fac3d4140911555143a7e07e48c145d466ce2
     HEAD_REF main
 )
 
 file(COPY "${SOURCE_PATH}/plf_queue.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_install_copyright(
+    FILE_LIST
+        "${SOURCE_PATH}/LICENSE"
+)
