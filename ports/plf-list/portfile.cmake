7caa788c7956c3892d0dc6464915d0f48f0cb3d6
SunBlack <SunBlack@users.noreply.github.com>
2026-09-08 00:20:35 +0200

    [plf-list] Update to 7.6.21

--- ports/plf-list/portfile.cmake
+++ ports/plf-list/portfile.cmake
@@ -1,14 +1,16 @@
-# header-only library
+set(VCPKG_BUILD_TYPE release) # header-only port
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO mattreecebentley/plf_list
-    REF b60676915e82f9e686de2550d68a1866617cbf42
-    SHA512 af9e9278604caa06075ca989f082d57ea33122958fa13b45a47242c36ae588769e1b15a27ea4676e361aeff3cef69429d0cf2bbab5a782d97f90e06b00198192
+    REF e1b7b985f888ae54c19b54e0aaa2fddb08c3f35b
+    SHA512 cd871125d32aa233d0134195c21ac9e6b3d2670dcdd78ff04551a174997682b80bdf33906d380a0e67e636cb1f0f0abc6eec48071559392f6c05987237b9ffed
     HEAD_REF master
 )
 
-file(COPY ${SOURCE_PATH}/plf_list.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(COPY "${SOURCE_PATH}/plf_list.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-# Handle copyright
-configure_file(${SOURCE_PATH}/LICENSE.md ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
+vcpkg_install_copyright(
+    FILE_LIST
+        "${SOURCE_PATH}/LICENSE.md"
+)
