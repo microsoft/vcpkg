50d18498ca13144889ae94ef1d57a48c2a8f266e
ChampVansh <vanshshukla125@gmail.com>
2026-09-13 00:10:32 +0530

    Fix mercenary source archive hash

--- ports/mercenary/portfile.cmake
+++ ports/mercenary/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ChampionUgrax/mercenary
     REF v1.0.0
-    SHA512 "3eb477610dc850024976798a72887309007e5f17dcd8ab305e7e8b9195d8cd3f6a27bf79135767b93a0b12f7142d765ee4615a9e3a6c2f7b8f9a2f7c6e6b5d4c"
+    SHA512 "188b33c7e4d96bdb7ab1a58a44194a80145d59ae1d0bcd3375d4580165172c6923238f14dd40e7b4a0e97994d57eb53c846fcf3c996377b613cf5a134092ccc2"
 )
 #Move your header-only file directly into the system installation directory
 file(INSTALL "${SOURCE_PATH}/include/mercenary.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
