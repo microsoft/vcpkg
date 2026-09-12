ae0b2220e772d60b22102d073fbe66b325241ca9
ChampVansh <vanshshukla125@gmail.com>
2026-09-12 23:00:07 +0530

    Trigger: Rerun checks

--- ports/mercenary/portfile.cmake
+++ ports/mercenary/portfile.cmake
@@ -8,3 +8,5 @@ vcpkg_from_github(
 file(INSTALL "${SOURCE_PATH}/include/mercenary.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+ 
