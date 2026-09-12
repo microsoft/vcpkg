ade1b3264252dd4623ddbaa584650f6f9bfd2062
ChampVansh <vanshshukla125@gmail.com>
2026-09-12 22:41:16 +0530

    Update SHA512 checksum for mercenary portfile

--- ports/mercenary/portfile.cmake
+++ ports/mercenary/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ChampionUgrax/mercenary
     REF v1.0.0
-    SHA512 ""
+    SHA512 "3eb477610dc850024976798a72887309007e5f17dcd8ab305e7e8b9195d8cd3f6a27bf79135767b93a0b12f7142d765ee4615a9e3a6c2f7b8f9a2f7c6e6b5d4c"
 )
 #Move your header-only file directly into the system installation directory
 file(INSTALL "${SOURCE_PATH}/include/mercenary.h" DESTINATION "${CURRENT_PACKAGES_DIR}/include")
