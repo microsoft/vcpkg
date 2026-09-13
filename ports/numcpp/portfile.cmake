3cf04c6d3eff24608abe9861cee89c0de9977530
miyanyan <1138989048@qq.com>
2026-09-13 11:57:11 +0800

    [numcpp] fix boost dependency
    issue: https://github.com/microsoft/vcpkg/issues/46269

--- ports/numcpp/portfile.cmake
+++ ports/numcpp/portfile.cmake
@@ -5,6 +5,8 @@ vcpkg_from_github(
     REF "Version_${VERSION}"
     SHA512 5242ba0cf0bccc77d8dc06058915c6bbf4798329cdeeb96623518642d5771d10e3559a6bd091040c02c237dee631adff77ea11bf2e8149649732fedef63dba9b
     HEAD_REF master
+    PATCHES
+        fix-boost-config.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
