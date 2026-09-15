dd1a745155f36863439a66868f726779cdebe324
azure-sdk <azuresdk@microsoft.com>
2026-09-15 20:20:15 +0000

    Daily vcpkg ports for keyvault

--- ports/azure-security-keyvault-secrets-cpp/portfile.cmake
+++ ports/azure-security-keyvault-secrets-cpp/portfile.cmake
@@ -4,21 +4,18 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-security-keyvault-secrets_${VERSION}"
-    SHA512 8d944bbcc29b670b884234a789455bb6b33e33fa45c7b02a4200c2267189597cb849f3cbbaa5f7c4db013ec74a2ea0b073237254db7fcac96c290888c05dcba5
+    REF 99aa5b47a2e6535858c757cf96368177f5291e83
+    SHA512 2e6fda4abbd8da4436686fe1acadbd1471952f5e293b208fadafd51406186317d2289d7218510a763c055cdb49964453a9274e6e475048cb8261653e4ee892f5
     HEAD_REF main
-    PATCHES
-      useragent.patch
-      set_version.patch
 )
 
 file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.toml")
 file(REMOVE_RECURSE ${unused})
 
-file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")  
+file(GLOB_RECURSE unused "${SOURCE_PATH}/Cargo.lock")
 file(REMOVE_RECURSE ${unused})
 
 if(EXISTS "${SOURCE_PATH}/sdk/keyvault/azure-security-keyvault-secrets")
