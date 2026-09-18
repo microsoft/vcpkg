4f1c9eb4e3f1a9aec675e359113f68650d541a25
azure-sdk <azuresdk@microsoft.com>
2026-09-18 17:48:08 +0000

    Daily vcpkg ports for eventhubs

--- ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-checkpointstore-blob-cpp/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Azure/azure-sdk-for-cpp
-    REF "azure-messaging-eventhubs-checkpointstore-blob_${VERSION}"
-    SHA512 bf060947c511b3f3c5dc8d7d58744e267809e74d6d14e3703f0a154ea6cf363d14f59fe4d75ad59dd6693202e658c2d6ab96b346dd56d264f8dc159b07e96541
+    REF f9a7aa05dfc250b5b468ca0a54ebe62684cecb36
+    SHA512 0ed6a9b16238d21e56a5d8afeced70a34b70319e8bd1dc381d979264d7b0783b9013cd9622ecfb6e31cb9f9eb865b6f85df87740a3907d07700b2217329f7042
     HEAD_REF main
 )
 
