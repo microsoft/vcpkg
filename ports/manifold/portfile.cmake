f5d1691c9943ffd53215ddf21ae92775f2c7a45e
SunBlack <SunBlack@users.noreply.github.com>
2026-09-26 00:34:05 +0200

    [manifold] Update to 3.5.4

--- ports/manifold/portfile.cmake
+++ ports/manifold/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO elalish/manifold
     REF "v${VERSION}"
-    SHA512 837eaba3df67046aaed4e2912c68e8f9415218a524674442c56d8c31430a804fc70c405439c59584ad0d5219bee16b60bccd68e40ce9c8c2548c23f1b1006768
+    SHA512 8a6e192a9ca26d7b90b0b960a770dcb4ca1f651526f6beea9ad490aaf05e63d9e64ab591cdbeced34a5117dd676328e5ceab5f783acb609b76f3a18a7586775d
     PATCHES
         fix-pkgconfig-cflags.patch # https://github.com/elalish/manifold/pull/1824
 )
